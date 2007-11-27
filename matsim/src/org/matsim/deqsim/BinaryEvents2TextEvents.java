5f6601e9184c911f799bd876ea65490d1669ca2d
mrieser <rieser@gmail.com>
2007-11-27 16:29:23 +0000

    now fixes automatically the leg numbers when converting the events.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@34 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/deqsim/BinaryEvents2TextEvents.java
+++ matsim/src/org/matsim/deqsim/BinaryEvents2TextEvents.java
@@ -23,12 +23,15 @@ package org.matsim.deqsim;
 import java.io.File;
 
 import org.matsim.events.Events;
+import org.matsim.events.algorithms.CalcLegNumber;
 import org.matsim.events.algorithms.EventWriterTXT;
 
 public class BinaryEvents2TextEvents {
 
 	private static void convert(final String fromFilename, final String toFilename) {
 		final Events events = new Events();
+
+		events.addHandler(new CalcLegNumber());
 
 		EventWriterTXT ew_txt = new EventWriterTXT(toFilename);
 		events.addHandler(ew_txt);
