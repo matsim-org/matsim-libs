4c81d0d471c72c2b53a45a2c5ee0d5686d616277
grether <dominik.grether@gmail.com>
2007-11-28 13:13:24 +0000

    added style for links
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@43 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/utils/vis/matsimkml/MatsimKmlStyleFactory.java
+++ matsim/src/org/matsim/utils/vis/matsimkml/MatsimKmlStyleFactory.java
@@ -20,9 +20,12 @@
 
 package org.matsim.utils.vis.matsimkml;
 
+import java.io.IOException;
+
 import org.matsim.utils.vis.kml.ColorStyle;
 import org.matsim.utils.vis.kml.Icon;
 import org.matsim.utils.vis.kml.IconStyle;
+import org.matsim.utils.vis.kml.KMZWriter;
 import org.matsim.utils.vis.kml.LineStyle;
 import org.matsim.utils.vis.kml.Style;
 import org.matsim.utils.vis.kml.fields.Color;
@@ -33,10 +36,13 @@ import org.matsim.utils.vis.kml.fields.Color;
  *
  */
 public class MatsimKmlStyleFactory {
+	public static final String DEFAULTLINKICON ="icon181.png";
+
+	public static final String DEFAULTNODEICON ="icon18.png";
 	/**
-	 * 
+	 *
 	 */
-	public static final String DEFAULTNODEICON = "res/icon18.png";
+	public static final String DEFAULTNODEICONLOCATION = "res/icon18.png";
 	/**
 	 * the scale for the icons
 	 */
@@ -57,13 +63,33 @@ public class MatsimKmlStyleFactory {
 	 * some colors frequently used in matsim
 	 */
 	public static final Color MATSIMWHITE = new Color(230, 230, 230, 230);
-	
-	
-	
-	
-	public Style createDefaultNetworkStyle() {
-		Style style = new Style("defaultnetworkstyle");
+	/**
+	 * the kmz writer
+	 */
+	private KMZWriter writer = null;
+
+	public MatsimKmlStyleFactory(KMZWriter writer) {
+		this.writer = writer;
+	}
+
+
+	public Style createDefaultNetworkNodeStyle() throws IOException {
+		Style style = new Style("defaultnetworknodestyle");
 		Icon icon = new Icon(DEFAULTNODEICON);
+
+		this.writer.addNonKMLFile(DEFAULTNODEICONLOCATION, DEFAULTNODEICON);
+		IconStyle iStyle = new IconStyle(icon, MATSIMRED, IconStyle.DEFAULT_COLOR_MODE, ICONSCALE);
+		style.setIconStyle(iStyle);
+//		LineStyle lineStyle = new LineStyle(MATSIMGREY, ColorStyle.DEFAULT_COLOR_MODE, 12);
+//		style.setLineStyle(lineStyle);
+		return style;
+	}
+
+	public Style createDefaultNetworkLinkStyle() throws IOException {
+		Style style = new Style("defaultnetworklinkstyle");
+		Icon icon = new Icon(DEFAULTLINKICON);
+
+		this.writer.addNonKMLFile(DEFAULTNODEICONLOCATION, DEFAULTLINKICON);
 		IconStyle iStyle = new IconStyle(icon, MATSIMWHITE, IconStyle.DEFAULT_COLOR_MODE, ICONSCALE);
 		style.setIconStyle(iStyle);
 		LineStyle lineStyle = new LineStyle(MATSIMGREY, ColorStyle.DEFAULT_COLOR_MODE, 12);
@@ -71,10 +97,9 @@ public class MatsimKmlStyleFactory {
 		return style;
 	}
 
-	
-	
-	
-	
-	
-	
+
+
+
+
+
 }
