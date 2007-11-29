5686398094f2f34994af83b7b0d2594b4ebabbe8
yu_vsp <yu_vsp@users.sf.net>
2007-11-29 23:13:45 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@85 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/yu/graphUtils/BarChartUtil.java
+++ matsim/src/playground/yu/graphUtils/BarChartUtil.java
@@ -1,6 +1,6 @@
 /* *********************************************************************** *
  * project: org.matsim.*
- * GraphUtils.java
+ * BarChartUtil.java
  *                                                                         *
  * *********************************************************************** *
  *                                                                         *
@@ -17,10 +17,11 @@
  *   See also COPYING, LICENSE and WARRANTY file                           *
  *                                                                         *
  * *********************************************************************** */
+
 /**
  * 
  */
-package playground.yu;
+package playground.yu.graphUtils;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
@@ -45,66 +46,83 @@ import org.jfree.chart.renderer.category.LineAndShapeRenderer;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 /**
- * @author ychen
+ * @author yu
  *
  */
-public class GraphUtils {
-	protected JFreeChart chart_;
-	private DefaultCategoryDataset dataset0,dataset1;
-	public GraphUtils(){
+public abstract class BarChartUtil extends ChartUtil{
+
+	protected DefaultCategoryDataset dataset0, dataset1;
+	public BarChartUtil(){
 		dataset0 = new DefaultCategoryDataset();
 		dataset1 = new DefaultCategoryDataset();
 	}
-	public JFreeChart createBarChart(String title, String categoryAxisLabel, String valueAxisLabel){
-//		String title = this.getChartTitle() + ", Iteration: " + this.iteration_;
-		chart_ = ChartFactory.createBarChart(title, categoryAxisLabel, valueAxisLabel, dataset0,
-				PlotOrientation.VERTICAL, true, // legend?
+	public JFreeChart createChart(String title, String xAxisLabel,
+			String yAxisLabel) {
+		// String title = this.getChartTitle() + ", Iteration: " +
+		// this.iteration_;
+		chart_ = ChartFactory.createBarChart(title, xAxisLabel,
+				yAxisLabel, dataset0, PlotOrientation.VERTICAL, true, // legend?
 				true, // tooltips?
 				false // URLs?
 				);
 		CategoryPlot plot = chart_.getCategoryPlot();
-//		plot.getRangeAxis().setRange(0.0, 10000.0); // do not set a fixed range for the single link graphs
-		plot.getDomainAxis().setCategoryLabelPositions(CategoryLabelPositions.UP_45);
+		// plot.getRangeAxis().setRange(0.0, 10000.0); // do not set a fixed
+		// range for the single link graphs
+		plot.getDomainAxis().setCategoryLabelPositions(
+				CategoryLabelPositions.UP_45);
 
 		// BarRenderer renderer=(BarRenderer) plot.getRenderer();
 		BarRenderer renderer = new BarRenderer();
 		renderer.setSeriesOutlinePaint(0, Color.black);
 		renderer.setSeriesOutlinePaint(1, Color.black);
-		renderer.setSeriesPaint(0, Color.getHSBColor((float) 0.62, (float) 0.56, (float) 0.93));
+		renderer.setSeriesPaint(0, Color.getHSBColor((float) 0.62,
+				(float) 0.56, (float) 0.93));
 		// Color.orange gives a dirty yellow!
-		renderer.setSeriesPaint(1, Color.getHSBColor((float) 0.1, (float) 0.79, (float) 0.89));
-		renderer.setSeriesToolTipGenerator(0, new StandardCategoryToolTipGenerator());
-		renderer.setSeriesToolTipGenerator(1, new StandardCategoryToolTipGenerator());
+		renderer.setSeriesPaint(1, Color.getHSBColor((float) 0.1, (float) 0.79,
+				(float) 0.89));
+		renderer.setSeriesToolTipGenerator(0,
+				new StandardCategoryToolTipGenerator());
+		renderer.setSeriesToolTipGenerator(1,
+				new StandardCategoryToolTipGenerator());
 		renderer.setItemMargin(0.0);
 
 		plot.setRenderer(0, renderer);
 		plot.setDomainAxisLocation(AxisLocation.BOTTOM_OR_RIGHT);
-		plot.setDataset(1, this.dataset1);
+		plot.setDataset(1, dataset1);
 		plot.mapDatasetToRangeAxis(1, 1);
 
 		final CategoryAxis axis1 = plot.getDomainAxis();
 		axis1.setCategoryMargin(0.25); // leave a gap of 25% between categories
 
-		final ValueAxis axis2 = new NumberAxis("Signed Rel. Error [%]");
+		final ValueAxis axis2 = new NumberAxis("Signed Rel. Error [%]");// ???????ChartFactory.createBarChart...
 		plot.setRangeAxis(1, axis2);
 
 		final LineAndShapeRenderer renderer2 = new LineAndShapeRenderer();
-		renderer2.setSeriesToolTipGenerator(0, new StandardCategoryToolTipGenerator());
-		renderer2.setSeriesShape(0, new Rectangle2D.Double(-1.5, -1.5, 3.0, 3.0));
+		renderer2.setSeriesToolTipGenerator(0,
+				new StandardCategoryToolTipGenerator());
+		renderer2.setSeriesShape(0,
+				new Rectangle2D.Double(-1.5, -1.5, 3.0, 3.0));
 		renderer2.setSeriesPaint(0, Color.black);
 		renderer2.setBaseStroke(new BasicStroke(2.5f));
 		plot.setRenderer(1, renderer2);
 		plot.setDatasetRenderingOrder(DatasetRenderingOrder.FORWARD);
 		return chart_;
 	}
-	public void addData(double[] values, Comparable[] args, Comparable[] rowKeys){
-		
-	}
-	public void saveAsPng(String filename, int width, int height){
+	public void saveAsPng(String filename, int width, int height) {
 		try {
-			ChartUtilities.saveChartAsPNG(new File(filename), chart_, width, height);
+			ChartUtilities.saveChartAsPNG(new File(filename), chart_, width,
+					height);
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+	@Override
+	public void addData(Object[] values) {
+		// TODO Auto-generated method stub
+		Double[] vs=(Double[]) values;
+		double ds[] = null;
+		for(int i=0;i<vs.length;i++){
+			ds[i]=vs[i].doubleValue();
 		}
 	}
 }
