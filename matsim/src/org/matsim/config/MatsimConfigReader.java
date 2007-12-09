91629cb4966c85e9f8c658ed4a44435ff348ae08
mrieser <rieser@gmail.com>
2007-12-09 17:06:31 +0000

    added support for reading in config-files with a custom (local) dtd.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@173 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/config/MatsimConfigReader.java
+++ matsim/src/org/matsim/config/MatsimConfigReader.java
@@ -20,13 +20,16 @@
 
 package org.matsim.config;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Stack;
 
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.apache.log4j.Logger;
 import org.matsim.utils.io.MatsimXmlParser;
 import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 /**
@@ -41,6 +44,8 @@ public class MatsimConfigReader extends MatsimXmlParser {
 
 	private final Config config;
 	private MatsimXmlParser delegate = null;
+
+	private String localDtd;
 
 	/**
 	 * Creates a new reader for MATSim configuration files.
@@ -79,6 +84,19 @@ public class MatsimConfigReader extends MatsimXmlParser {
 		}
 	}
 
+	/**
+	 * Parses the specified config file, and uses the given dtd file as a local copy to use as dtd
+	 * if the one specified in the config file cannot be found.
+	 *
+	 * @param filename The name of the file to parse.
+	 * @param dtdFilename The name of a (local) dtd-file to be used for validating the config file.
+	 */
+	public void readFile(final String filename, final String dtdFilename) {
+		this.localDtd = dtdFilename;
+		readFile(filename);
+		this.localDtd = null;
+	}
+
 	@Override
 	protected void setDoctype(final String doctype) {
 		super.setDoctype(doctype);
@@ -89,6 +107,21 @@ public class MatsimConfigReader extends MatsimXmlParser {
 		} else {
 			throw new IllegalArgumentException("Doctype \"" + doctype + "\" not known.");
 		}
+	}
+
+	@Override
+	public InputSource resolveEntity(final String publicId, final String systemId) {
+
+		InputSource is = super.resolveEntity(publicId, systemId);
+		if (is == null && this.localDtd != null) {
+			File dtdFile = new File(this.localDtd);
+			if (dtdFile.exists() && dtdFile.isFile() && dtdFile.canRead()) {
+				Logger.getLogger(this.getClass()).info("Using the local DTD " + this.localDtd);
+				return new InputSource(this.localDtd);
+			}
+			return null;
+		}
+		return is;
 	}
 
 }
