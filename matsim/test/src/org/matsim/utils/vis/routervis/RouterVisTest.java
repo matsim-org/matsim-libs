a13cfed60c7398c2d0fc2d0c1b268063acf5c347
mrieser <rieser@gmail.com>
2007-12-31 10:13:27 +0000

    removed unnecessary import-statement.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@309 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/test/src/org/matsim/utils/vis/routervis/RouterVisTest.java
+++ matsim/test/src/org/matsim/utils/vis/routervis/RouterVisTest.java
@@ -30,7 +30,6 @@ import org.matsim.router.util.TravelCostI;
 import org.matsim.router.util.TravelTimeI;
 import org.matsim.testcases.MatsimTestCase;
 import org.matsim.utils.CRCChecksum;
-import org.matsim.utils.identifiers.IdI;
 
 public class RouterVisTest extends MatsimTestCase{
 	private NetworkLayer network;
@@ -39,44 +38,40 @@ public class RouterVisTest extends MatsimTestCase{
 
 	public void testRouterVis(){
 		init("test/input/" + this.getClass().getCanonicalName().replace('.', '/') + "/config.xml");
-		
-	
-		Node fromNode = network.getNode("13");
-		Node toNode = network.getNode("7");
-		
-		
+
+		Node fromNode = this.network.getNode("13");
+		Node toNode = this.network.getNode("7");
+
 		TravelTimeI costCalc = new FreespeedTravelTimeCost();
-		RouterVis routerVis = new RouterVis(network,(TravelCostI) costCalc,costCalc);
-		
+		RouterVis routerVis = new RouterVis(this.network,(TravelCostI) costCalc,costCalc);
+
 		routerVis.runRouter(fromNode, toNode, 0.0);
-		
+
 		String outDir = getOutputDirectory();
-		
+
 		String outConfig = outDir + "/SnapshotCONFIG.vis";
 		long checksumConfig = CRCChecksum.getCRCFromFile(outConfig);
-		
-		
+
 		String outSnapshot = outDir + "/Snapshot00-00-00.vis";
 		long checksumSnapshot = CRCChecksum.getCRCFromFile(outSnapshot);
-		
+
 		assertEquals(this.referenceChecksumConfig,checksumConfig);
 		assertEquals(this.referenceChecksumSnapshot,checksumSnapshot);
-
 	}
 
-	private void init(String configFile) {
+	private void init(final String configFile) {
 		loadConfig(configFile);
 		this.network = readNetwork();
 		String visConfigFile = "test/input/" + this.getClass().getCanonicalName().replace('.', '/') + "/SnapshotCONFIG.vis";
 		this.referenceChecksumConfig = CRCChecksum.getCRCFromFile(visConfigFile);
 		System.out.println("Reference checksum config = " + this.referenceChecksumConfig + " file: " + visConfigFile);
-		
+
 		String visSnapshotFile = "test/input/" + this.getClass().getCanonicalName().replace('.', '/') + "/Snapshot00-00-00.vis";
 		this.referenceChecksumSnapshot = CRCChecksum.getCRCFromFile(visSnapshotFile);
 		System.out.println("Reference checksum snapshot = " + this.referenceChecksumSnapshot + " file: " + visSnapshotFile);
 		System.out.println();
 	}
-	
+
 	private NetworkLayer readNetwork() {
 		System.out.println("  reading the network...");
 		NetworkLayerBuilder.setNetworkLayerType(NetworkLayerBuilder.NETWORK_DEFAULT);
