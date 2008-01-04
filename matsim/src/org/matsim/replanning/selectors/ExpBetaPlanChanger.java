f42a790ff6826f668f3206022e00acdeb0d168f5
mrieser <rieser@gmail.com>
2008-01-04 09:55:17 +0000

    fixed compile problems in the class, and simplified the code, as some of the functionality is now already available in Person and must not be implemented on its own here.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@351 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/replanning/selectors/ExpBetaPlanChanger.java
+++ matsim/src/org/matsim/replanning/selectors/ExpBetaPlanChanger.java
@@ -20,21 +20,17 @@
 
 package org.matsim.replanning.selectors;
 
-import org.matsim.basic.v01.BasicPlan;
 import org.matsim.gbl.Gbl;
 import org.matsim.plans.Person;
 import org.matsim.plans.Plan;
 
 /**
- * Changes to another plan if that plan is better.  Proba to change depends on score difference.
- * If there are unscored plans, a random one
- * of the unscored plans will be chosen (optimistic strategy).
+ * Changes to another plan if that plan is better.  Probability to change depends on score difference.
  *
  * @author kn based on mrieser
  */
 public class ExpBetaPlanChanger implements PlanSelectorI {
 
-	private static double MIN_WEIGHT = Double.MIN_VALUE;
 	private final double beta;
 
 	public ExpBetaPlanChanger() {
@@ -42,82 +38,26 @@ public class ExpBetaPlanChanger implements PlanSelectorI {
 	}
 
 	/**
-	 * Changes to another plan with a proba proportional to exp( Delta scores ).
-	 * Need to think through if this goes to Nash Eq. or to SUE !!!
+	 * Changes to another plan with a probability proportional to exp( Delta scores ).
+	 * Need to think through if this goes to Nash Equilibrium or to SUE !!!
 	 */
 	public Plan selectPlan(final Person person) {
-		// First check if there are any unscored plans
-		Plan selectedPlan = selectUnscoredPlan(person);
-		if (selectedPlan != null) {
-			return selectedPlan;
-		}
-		// Okay, no unscored plans...
-		
 		// current plan and random plan:
 		Plan currentPlan = person.getSelectedPlan();
-		Plan otherPlan = person.selectRandomPlan() ;
-		
+		Plan otherPlan = person.getRandomPlan();
+
 		double currentScore = currentPlan.getScore();
-		double otherScore = otherPlan.getScore() ;
-		
-		double weight = Math.exp( this.beta * (otherScore - currentScore) ) ;
+		double otherScore = otherPlan.getScore();
+
+		double weight = Math.exp( this.beta * (otherScore - currentScore) );
 		// (so far, this is >1 if otherScore>currentScore, and <=1 otherwise)
 		// (beta is the slope (strength) of the operation: large beta means strong reaction)
-		
-		if ( Gbl.random.nextDouble() < 0.01*weight ) { // as of now, 0.01 is hardcoded (proba to change when both 
+
+		if (Gbl.random.nextDouble() < 0.01*weight ) { // as of now, 0.01 is hardcoded (proba to change when both
 			                                           // scores are the same)
-			return otherPlan ;
-		} else {
-			return currentPlan ;
+			return otherPlan;
 		}
-		
-//		// this case should never happen, except a person has no plans at all.
-//		return null;
+		return currentPlan;
 	}
 
-	/**
-	 * Returns a plan with undefined score, chosen randomly among all plans
-	 * of the person with undefined score.
-	 *
-	 * @param person
-	 * @return a random plan with undefined score, or null if none such plan exists.
-	 */
-	private Plan selectUnscoredPlan(final Person person) {
-		// TODO [kn] this exists now both inside ExpBetaPlanSelect and in ExpBetaPlanChange.  Should be moved to
-		// ONE location, maybe "class Person".
-		int cntUnscored = 0;
-		for (Plan plan : person.getPlans()) {
-			if (Plan.isUndefinedScore(plan.getScore())) {
-				cntUnscored++;
-			}
-		}
-		if (cntUnscored > 0) {
-			// select one of the unscored plans
-			int idxUnscored = Gbl.random.nextInt(cntUnscored);
-			cntUnscored = 0;
-			for (Plan plan : person.getPlans()) {
-				if (Plan.isUndefinedScore(plan.getScore())) {
-					if (cntUnscored == idxUnscored) {
-						person.setSelectedPlan(plan);
-						return plan;
-					}
-					cntUnscored++;
-				}
-			}
-		}
-		return null;
-	}
-
-	/**
-	 * Calculates the weight of a single plan
-	 *
-	 * @param plan
-	 * @param maxScore
-	 * @return the weight of the plan
-	 */
-	private double calcPlanWeight(final Plan plan, final double maxScore) {
-		double weight = Math.exp(this.beta * (plan.getScore() - maxScore));
-		if (weight <= 0.0) weight = MIN_WEIGHT;
-		return weight;
-	}
 }
