17b8c749720f633d0c5ff4a4c4f51fa178732641
mrieser <rieser@gmail.com>
2008-01-06 11:19:03 +0000

    javadoc updates and fixes.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@359 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/utils/charts/package-info.java
+++ matsim/src/org/matsim/utils/charts/package-info.java
@@ -1,5 +1,5 @@
 /**
- * This package provides basic functionality to generic simple charts.
+ * This package provides basic functionality to generate simple charts.
  * The amount of possible customization on the charts is very limited in favor
  * of a simple usage. If more elaborate charts are needed, they have to be
  * implemented on their own, making full use of JFreeChart or another charting
---
+++ matsim/src/tutorial/package-info.java
@@ -0,0 +1,9 @@
+/**
+<p>This package contains the classes referenced in the MATSim Tutorial "Getting started with MATSim".</p>
+<p>{@link tutorial.MyControler1} introduced reading of network and plans, writing of events and running the mobility simulation a single time.</p>
+<p>{@link tutorial.MyControler2} adds visualizer output.</p>
+<p>{@link tutorial.MyControler3} makes use of a configuration file instead of hard-coding all settings.</p>
+<p>{@link tutorial.MyControler4} calculates the score of the individual plans as well as the average score.</p>
+<p>{@link tutorial.MyControler5} adds an iteration loop and re-planning to create a real agent-based simulation with agent learning.</p>
+*/
+package tutorial;
