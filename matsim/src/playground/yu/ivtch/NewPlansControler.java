80f70d398e83135612468c4a8c9e61b9c81b3fb1
yu_vsp <yu_vsp@users.sf.net>
2008-01-18 16:48:03 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@431 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/yu/ivtch/NewPlansControler.java
@@ -0,0 +1,85 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * NewPlansControler.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.yu.ivtch;
+/*
+ * $Id: NewPlansControler.java,v 1.7 2007/11/23 13:04:04 ychen Exp $
+ */
+
+/* *********************************************************************** *
+ *                                                                         *
+ *                            NewPlansControler.java                            *
+ *                          ---------------------                          *
+ * copyright       : (C) 2007 by Michael Balmer, Marcel Rieser,            *
+ *                   David Strippgen, Gunnar Flötteröd, Konrad Meister,    *
+ *                   Kai Nagel, Kay W. Axhausen                            *
+ *                   Technische Universitaet Berlin (TU-Berlin) and        *
+ *                   Swiss Federal Institute of Technology Zurich (ETHZ)   *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+import org.matsim.config.Config;
+import org.matsim.gbl.Gbl;
+import org.matsim.mobsim.QueueNetworkLayer;
+import org.matsim.network.MatsimNetworkReader;
+import org.matsim.plans.MatsimPlansReader;
+import org.matsim.plans.Plans;
+import org.matsim.plans.PlansReaderI;
+import org.matsim.world.World;
+
+
+/**
+ * test of NewAgentPtPlan
+ * @author ychen
+ *
+ */
+public class NewPlansControler {
+
+	public static void main(final String[] args) {
+		final String netFilename = "./test/yu/schweiz/input/ch.xml";
+		final String plansFilename = "./test/yu/schweiz/input/0.plans-hwh_with_routes_cleanedup.10pct.xml";
+
+		World world = Gbl.getWorld();
+		@SuppressWarnings("unused")
+		Config config = Gbl.createConfig(new String[] {"./test/yu/schweiz/multipleIterations.xml"});
+
+		QueueNetworkLayer network = new QueueNetworkLayer();
+		new MatsimNetworkReader(network).readFile(netFilename);
+		world.setNetworkLayer(network);
+
+		Plans population = new Plans();
+		NewAgentPtPlan nap=new NewAgentPtPlan(population);
+		population.addAlgorithm(nap);
+		PlansReaderI plansReader = new MatsimPlansReader(population);
+		plansReader.readFile(plansFilename);
+		world.setPopulation(population);
+		population.runAlgorithms();
+		nap.writeEndPlans();
+	}
+}
