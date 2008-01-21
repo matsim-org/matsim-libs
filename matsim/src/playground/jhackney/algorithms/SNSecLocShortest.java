d55e686580fd519b7185175f002483264656f291
jkhackney <jkhackney@users.sf.net>
2008-01-21 13:23:51 +0000

    calls controler to get the type of facility about which to exchange information: NOTE this should not come from controler but from config!
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@444 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/jhackney/algorithms/SNSecLocShortest.java
+++ matsim/src/playground/jhackney/algorithms/SNSecLocShortest.java
@@ -32,7 +32,7 @@ import org.matsim.plans.Plan;
 import org.matsim.plans.algorithms.PlanAlgorithmI;
 import org.matsim.utils.geometry.shared.Coord;
 
-import playground.jhackney.run.SNReplanning;
+import playground.jhackney.controler.SNController;
 
 public class SNSecLocShortest implements PlanAlgorithmI {
 
@@ -79,15 +79,15 @@ public class SNSecLocShortest implements PlanAlgorithmI {
 		double rand = Gbl.random.nextDouble();
 
 		if (rand < cum_p_factype[0]) {
-			factype = SNReplanning.activityTypesForEncounters[0];
+			factype = SNController.activityTypesForEncounters[0];
 		}else if (cum_p_factype[0] <= rand && rand < cum_p_factype[1]) {
-			factype = SNReplanning.activityTypesForEncounters[1];
+			factype = SNController.activityTypesForEncounters[1];
 		}else if (cum_p_factype[1] <= rand && rand < cum_p_factype[2]) {
-			factype = SNReplanning.activityTypesForEncounters[2];
+			factype = SNController.activityTypesForEncounters[2];
 		}else if (cum_p_factype[2] <= rand && rand < cum_p_factype[3]) {
-			factype = SNReplanning.activityTypesForEncounters[3];
+			factype = SNController.activityTypesForEncounters[3];
 		}else {
-			factype = SNReplanning.activityTypesForEncounters[4];
+			factype = SNController.activityTypesForEncounters[4];
 		}
 
 //		Figure out if the agent has knowledge about other facilities of this kind
