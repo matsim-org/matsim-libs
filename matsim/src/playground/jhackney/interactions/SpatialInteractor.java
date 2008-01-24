789d8013a2f3af6e85c8c953fcb7f66cd0682c72
jkhackney <jkhackney@users.sf.net>
2008-01-24 15:57:27 +0000

    updated to new controler structure with listener
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@473 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/jhackney/interactions/SpatialInteractor.java
+++ matsim/src/playground/jhackney/interactions/SpatialInteractor.java
@@ -32,124 +32,151 @@ import playground.jhackney.socialnet.SocialNetwork;
 
 public class SpatialInteractor {
 
-    SocialNetwork net;
+	SocialNetwork net;
 
-    double pBecomeFriends = Double.parseDouble(Gbl.getConfig().socnetmodule().getPBefriend());// [0.0,1.0]
+	double pBecomeFriends = Double.parseDouble(Gbl.getConfig().socnetmodule().getPBefriend());// [0.0,1.0]
 
-//  double pct_interacting = Double.parseDouble(Gbl.getConfig().socnetmodule().getFractSInteract());// [0.0,1.0]
+//	double pct_interacting = Double.parseDouble(Gbl.getConfig().socnetmodule().getFractSInteract());// [0.0,1.0]
 
-    String interaction_type = Gbl.getConfig().socnetmodule().getSocNetInteractor2();
+	String interaction_type = Gbl.getConfig().socnetmodule().getSocNetInteractor2();
 
-    public SpatialInteractor(SocialNetwork snet) {
-	this.net = snet;
-    }
-
-    public void interact(Collection<SocializingOpportunity> events, HashMap<String, Double> rndEncounterProb, int iteration) {
-
-	System.out.println(" Looking through social events and letting Persons interact "+iteration);
-	for (SocializingOpportunity event : events) {
-
-	    if (event.getAttendees().size() > 1) {
-
-//		Shuffle the list of attendees at the socializing opportunity
-//		and let a percentage of them interact
-		List<Person> people = java.util.Collections.list(java.util.Collections.enumeration(event.getAttendees()));
-		java.util.Collections.shuffle(people);
-		Object personList[] = people.toArray();
-		int numPersons = personList.length;
-
-//		for (int i = 0; i < (int) (numPersons * pct_interacting); i++) {
-		for (int i = 0; i < numPersons; i++) {
-		    Person p1 = (Person) personList[i];
-
-		    if (interaction_type.equals("random")) {
-			encounterOnePersonRandomlyFaceToFace(event, rndEncounterProb, p1, iteration);
-		    }else if(interaction_type.equals("timewindowrandom")){
-			double t1=event.getArrivalTime(p1);
-			double t2=event.getDepartureTime(p1);
-			encounterOnePersonRandomlyFaceToFaceInTimeWindow(event, rndEncounterProb, p1, t1, t2, iteration);
-		    }else if(interaction_type.equals("timewindowall")){
-			double t1=event.getArrivalTime(p1);
-			double t2=event.getDepartureTime(p1);
-			encounterAllPersonsFaceToFaceInTimeWindow(event, rndEncounterProb, p1, t1, t2, iteration);
-		    } else {
-			Gbl.errorMsg(" Spatial interaction type is \"" + interaction_type
-				+ "\". Only \"random\" and \"timewindow\" are supported at this time.");
-		    }
-		}
-	    }
+	public SpatialInteractor(SocialNetwork snet) {
+		this.net = snet;
 	}
-	System.out.println("...finished");
-    }
 
-    private void encounterOnePersonRandomlyFaceToFace(SocializingOpportunity event, HashMap<String, Double> rndEncounterProbability,
-	    Person p1, int iteration) {
+	public void interact(Collection<SocializingOpportunity> events, HashMap<String, Double> rndEncounterProb, int iteration) {
 
-	// Person p1 randomly encounters a person p2 at the socializing opportunity
-	// the probability of encountering depends on the activity type
+		System.out.println(" Looking through social events and letting Persons interact "+iteration);
+		for (SocializingOpportunity event : events) {
 
-	if(Gbl.random.nextDouble() <rndEncounterProbability.get(event.activity.getType())){
-	    Person p2 = event.getRandomInterlocutor(p1);
+			if (event.getAttendees().size() > 1) {
 
-	    // If they know each other, probability is 1.0 that the relationship is reinforced
-	    if (p1.getKnowledge().egoNet.knows(p2)) {
-		net.makeSocialContact(p1,p2,iteration,"renew"+event.activity.getType());
-System.out.println("Person "+p1.getId()+" renews with Person "+ p2.getId());
-	    } else {
-		// If the two do not already know each other,
+//				Shuffle the list of attendees at the socializing opportunity
+//				and let a percentage of them interact
+				List<Person> people = java.util.Collections.list(java.util.Collections.enumeration(event.getAttendees()));
+				java.util.Collections.shuffle(people);
+				Object personList[] = people.toArray();
+				int numPersons = personList.length;
 
-		if(Gbl.random.nextDouble() < pBecomeFriends){
-		    net.makeSocialContact(p1,p2,iteration,"new"+event.activity.getType());
-		    System.out.println("Person "+p1.getId()+" and Person "+ p2.getId()+" meet at "+event.activity.getFacility().getId()+" for activity "+event.activity.getType());
-		}
-	    }
-	}
-    }
-    private void encounterOnePersonRandomlyFaceToFaceInTimeWindow(SocializingOpportunity event, HashMap<String, Double> rndEncounterProbability,
-	    Person p1, double StartTime, double EndTime, int iteration) {
+//				for (int i = 0; i < (int) (numPersons * pct_interacting); i++) {
+				for (int i = 0; i < numPersons; i++) {
+					Person p1 = (Person) personList[i];
 
-	// Person p1 randomly encounters a person p2 at the socializing opportunity
-	// the probability of encountering depends on the activity type
-
-	if(Gbl.random.nextDouble() <rndEncounterProbability.get(event.activity.getType())){
-	    Person p2 = event.getRandomInterlocutorInTimeWindow(p1, StartTime, EndTime);
-
-	    // If they know each other, probability is 1.0 that the relationship is reinforced
-	    if (p1.getKnowledge().egoNet.knows(p2)) {
-		net.makeSocialContact(p1,p2,iteration,"renew"+event.activity.getType());
-
-	    } else {
-		// If the two do not already know each other,
-
-		if(Gbl.random.nextDouble() < pBecomeFriends){
-		    net.makeSocialContact(p1,p2,iteration,"new"+event.activity.getType());
-		}
-	    }
-	}
-    }
-    private void encounterAllPersonsFaceToFaceInTimeWindow(SocializingOpportunity event, HashMap<String, Double> rndEncounterProbability,
-	    Person p1, double StartTime, double EndTime, int iteration) {
-
-	// Person p1 encounters all persons p2 at the socializing opportunity
-	// the probability of encountering depends on the activity type
-
-	if(Gbl.random.nextDouble() <rndEncounterProbability.get(event.activity.getType())){
-
-	    Vector<Person> persons = event.getAttendeesInTimeWindow(p1, StartTime, EndTime);
-	    if(persons!=null){
-		int size = persons.size();
-		for(int i=0; i<size;i++){
-		    Person p2=persons.get(i);
-		    if(p1.getKnowledge().egoNet.knows(p2)){
-		    } else {
-			// If the two do not already know each other,
-
-			if(Gbl.random.nextDouble() < pBecomeFriends){
-			    net.makeSocialContact(p1,p2,iteration,"new"+event.activity.getType());
+					if (interaction_type.equals("random")) {
+						encounterOnePersonRandomlyFaceToFace(event, rndEncounterProb, p1, iteration);
+					}else if (interaction_type.equals("meetall")){
+						makeSocialLinkToAll(event,rndEncounterProb,p1,iteration);
+					}else if(interaction_type.equals("timewindowrandom")){
+						double t1=event.getArrivalTime(p1);
+						double t2=event.getDepartureTime(p1);
+						encounterOnePersonRandomlyFaceToFaceInTimeWindow(event, rndEncounterProb, p1, t1, t2, iteration);
+					}else if(interaction_type.equals("timewindowall")){
+						double t1=event.getArrivalTime(p1);
+						double t2=event.getDepartureTime(p1);
+						encounterAllPersonsFaceToFaceInTimeWindow(event, rndEncounterProb, p1, t1, t2, iteration);
+					} else {
+						Gbl.errorMsg(" Spatial interaction type is \"" + interaction_type
+								+ "\". Only \"random\" and \"timewindow\" are supported at this time.");
+					}
+				}
 			}
-		    }
 		}
-	    }
+		System.out.println("...finished");
 	}
-    }
+
+	private void makeSocialLinkToAll(SocializingOpportunity event, HashMap<String, Double> rndEncounterProbability, Person p1, int iteration) {
+		// TODO Auto-generated method stub
+		// Person p1 encounters all persons p2 at the socializing opportunity
+		// the probability of encountering depends on the activity type
+
+		if(Gbl.random.nextDouble() <rndEncounterProbability.get(event.activity.getType())){
+
+			Vector<Person> persons = event.getAttendees();
+			if(persons!=null){
+				int size = persons.size();
+				for(int i=0; i<size;i++){
+					Person p2=persons.get(i);
+					if(p1.getKnowledge().egoNet.knows(p2)){
+					} else {
+						// If the two do not already know each other,
+
+						
+							net.makeSocialContact(p1,p2,iteration,"new"+event.activity.getType());
+						
+					}
+				}
+			}
+		}
+	}
+
+	private void encounterOnePersonRandomlyFaceToFace(SocializingOpportunity event, HashMap<String, Double> rndEncounterProbability,
+			Person p1, int iteration) {
+
+		// Person p1 randomly encounters a person p2 at the socializing opportunity
+		// the probability of encountering depends on the activity type
+
+		if(Gbl.random.nextDouble() <rndEncounterProbability.get(event.activity.getType())){
+			Person p2 = event.getRandomInterlocutor(p1);
+
+			// If they know each other, probability is 1.0 that the relationship is reinforced
+			if (p1.getKnowledge().egoNet.knows(p2)) {
+				net.makeSocialContact(p1,p2,iteration,"renew"+event.activity.getType());
+				System.out.println("Person "+p1.getId()+" renews with Person "+ p2.getId());
+			} else {
+				// If the two do not already know each other,
+
+				if(Gbl.random.nextDouble() < pBecomeFriends){
+					net.makeSocialContact(p1,p2,iteration,"new"+event.activity.getType());
+					System.out.println("Person "+p1.getId()+" and Person "+ p2.getId()+" meet at "+event.activity.getFacility().getId()+" for activity "+event.activity.getType());
+				}
+			}
+		}
+	}
+	private void encounterOnePersonRandomlyFaceToFaceInTimeWindow(SocializingOpportunity event, HashMap<String, Double> rndEncounterProbability,
+			Person p1, double StartTime, double EndTime, int iteration) {
+
+		// Person p1 randomly encounters a person p2 at the socializing opportunity
+		// the probability of encountering depends on the activity type
+
+		if(Gbl.random.nextDouble() <rndEncounterProbability.get(event.activity.getType())){
+			Person p2 = event.getRandomInterlocutorInTimeWindow(p1, StartTime, EndTime);
+
+			// If they know each other, probability is 1.0 that the relationship is reinforced
+			if (p1.getKnowledge().egoNet.knows(p2)) {
+				net.makeSocialContact(p1,p2,iteration,"renew"+event.activity.getType());
+
+			} else {
+				// If the two do not already know each other,
+
+				if(Gbl.random.nextDouble() < pBecomeFriends){
+					net.makeSocialContact(p1,p2,iteration,"new"+event.activity.getType());
+				}
+			}
+		}
+	}
+	private void encounterAllPersonsFaceToFaceInTimeWindow(SocializingOpportunity event, HashMap<String, Double> rndEncounterProbability,
+			Person p1, double StartTime, double EndTime, int iteration) {
+
+		// Person p1 encounters all persons p2 at the socializing opportunity
+		// the probability of encountering depends on the activity type
+
+		if(Gbl.random.nextDouble() <rndEncounterProbability.get(event.activity.getType())){
+
+			Vector<Person> persons = event.getAttendeesInTimeWindow(p1, StartTime, EndTime);
+			if(persons!=null){
+				int size = persons.size();
+				for(int i=0; i<size;i++){
+					Person p2=persons.get(i);
+					if(p1.getKnowledge().egoNet.knows(p2)){
+					} else {
+						// If the two do not already know each other,
+
+						if(Gbl.random.nextDouble() < pBecomeFriends){
+							net.makeSocialContact(p1,p2,iteration,"new"+event.activity.getType());
+						}
+					}
+				}
+			}
+		}
+	}
 }
