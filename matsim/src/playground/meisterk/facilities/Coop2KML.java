0fa20566d47f62b4cc03230e6e47cfd4eebbd5d8
komeiste <komeiste@users.sf.net>
2008-01-28 11:01:17 +0000

    More and more facilities...
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@494 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/meisterk/facilities/Coop2KML.java
+++ matsim/src/playground/meisterk/facilities/Coop2KML.java
@@ -1,49 +1,32 @@
-/* *********************************************************************** *
- * project: org.matsim.*
- * Coop2KML.java
- *                                                                         *
- * *********************************************************************** *
- *                                                                         *
- * copyright       : (C) 2007 by the members listed in the COPYING,        *
- *                   LICENSE and WARRANTY file.                            *
- * email           : info at matsim dot org                                *
- *                                                                         *
- * *********************************************************************** *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *   See also COPYING, LICENSE and WARRANTY file                           *
- *                                                                         *
- * *********************************************************************** */
-
 package playground.meisterk.facilities;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
-import org.matsim.utils.misc.Time;
-import org.matsim.utils.vis.kml.Document;
-import org.matsim.utils.vis.kml.Feature;
-import org.matsim.utils.vis.kml.Folder;
-import org.matsim.utils.vis.kml.KML;
-import org.matsim.utils.vis.kml.KMZWriter;
-import org.matsim.utils.vis.kml.Placemark;
+import org.matsim.utils.misc.TimeFormatter;
+import org.matsim.utils.vis.kml.*;
 
 public class Coop2KML {
 
-	private static KML myKML;
-	private static Document myKMLDocument;
+	private static KML myKML = null;
+	private static Document myKMLDocument = null;
 	private static String inputFilename = "/Users/meisterk/Documents/workspace/matsimJ/input/coopzh.txt";
 	private static String kmlFilename = "/Users/meisterk/Documents/workspace/matsimJ/input/coopzh.kmz";
+	private static Style openStyle = null;
+	private static Style closedStyle = null;
 
 	/**
 	 * @param args
 	 */
-	public static void main(final String[] args) {
+	public static void main(String[] args) {
 
 		setUp();
 		generateFacilities();
@@ -57,12 +40,22 @@ public class Coop2KML {
 		myKMLDocument = new Document("the root document");
 		myKML.setFeature(myKMLDocument);
 
+		openStyle = new Style("openStyle");
+		myKMLDocument.addStyle(openStyle);
+		openStyle.setIconStyle(new IconStyle(new Icon("/Users/meisterk/Documents/workspace/matsimJ/input/coop_dhtml.jpg")));
+		closedStyle = new Style("closedStyle");
+		myKMLDocument.addStyle(closedStyle);
+		closedStyle.setIconStyle(new IconStyle(new Icon("/Users/meisterk/Documents/workspace/matsimJ/input/coop_dhtml_invert.jpg")));
+
 	}
 
 	private static void generateFacilities() {
 
 		List<String> lines = null;
 		String[] tokens = null;
+		Placemark aCoopPhase = null;
+		Folder aCoop = null;
+		GregorianCalendar gcBegin, gcEnd;
 
 		try {
 			lines = FileUtils.readLines(new File(inputFilename), "UTF-8");
@@ -84,41 +77,89 @@ public class Coop2KML {
 
 		myKMLDocument.addFeature(wednesdayFolder);
 
-		tokens = lines.get(0).split("\t");
-		System.out.println(tokens[7] + "\t" + tokens[9] + "\t" + tokens[10] + "\t" + tokens[11]);
+		TimeFormatter tf = new TimeFormatter(TimeFormatter.TIMEFORMAT_HHMM);
+		ArrayList<Double> openClose = new ArrayList<Double>();
 
-		String wedOpen1 = tokens[22];
-		System.out.println(Time.parseTime(wedOpen1) / 3600);
+		for (String line : lines) {
+		
+			tokens = line.split("\t");
+			System.out.println(tokens[7] + "\t" + tokens[9] + "\t" + tokens[10] + "\t" + tokens[11]);
+			
+			openClose.add(new Double(0.0));
 
+			try {
+				for (int pos = 22; pos <= 25; pos++) {
 
-		String wedOpen2 = tokens[23];
-		String wedOpen3 = tokens[24];
-		String wedOpen4 = tokens[25];
+					Double time = tf.parseTime(tokens[pos]) / 3600;
+					if (!time.equals(Double.NEGATIVE_INFINITY)) {
+						openClose.add(time);
+					}
 
-		Placemark aCoop = new Placemark(
-				tokens[7] + " " + tokens[9] + ", " + tokens[10] + " " + tokens[11],
-				tokens[7] + " " + tokens[9] + ", " + tokens[10] + " " + tokens[11],
-				tokens[7] + " " + tokens[9] + ", " + tokens[10] + " " + tokens[11],
-				tokens[9] + ", " + tokens[10] + " " + tokens[11],
-				Feature.DEFAULT_LOOK_AT,
-				Feature.DEFAULT_STYLE_URL,
-				Feature.DEFAULT_VISIBILITY,
-				Feature.DEFAULT_REGION,
-				Feature.DEFAULT_TIME_PRIMITIVE);
+				}
+			} catch (java.lang.ArrayIndexOutOfBoundsException e) {
+				System.err.println("invalid entry. Ignoring...");
+				
+				continue;
+			}
 
+			openClose.add(new Double(23.0 + 59/60));		
 
+			aCoop = new Folder(
+					tokens[7] + " " + tokens[9] + ", " + tokens[10] + " " + tokens[11],
+					tokens[7] + " " + tokens[9] + ", " + tokens[10] + " " + tokens[11],
+					Feature.DEFAULT_DESCRIPTION,
+					Feature.DEFAULT_ADDRESS,
+					Feature.DEFAULT_LOOK_AT,
+					Feature.DEFAULT_STYLE_URL,
+					Feature.DEFAULT_VISIBILITY,
+					Feature.DEFAULT_REGION,
+					Feature.DEFAULT_TIME_PRIMITIVE);
+			
+			wednesdayFolder.addFeature(aCoop);
+			
+			for (int ii=0; ii < openClose.size() - 1; ii++) {
 
-		wednesdayFolder.addFeature(aCoop);
+				gcBegin = new GregorianCalendar(
+						1970, 0, 1, 
+						openClose.get(ii).intValue(), 
+						(int) (openClose.get(ii).intValue() - openClose.get(ii).intValue()) * 60, 
+						0);
+				gcEnd = new GregorianCalendar(
+						1970, 0, 1, 
+						openClose.get(ii + 1).intValue(), 
+						(int) (openClose.get(ii + 1).intValue() - openClose.get(ii + 1).intValue()) * 60, 
+						0);
 
+				String styleURL = null;
+				String description = null;
+				
+				if (ii % 2 == 0) {
+					styleURL = closedStyle.getStyleUrl();
+					description = "closed";
+				} else {
+					styleURL = openStyle.getStyleUrl();
+					description = "opened";
+				}
+				
+				aCoopPhase = new Placemark(
+						tokens[7] + " " + tokens[9] + ", " + tokens[10] + " " + tokens[11] + Integer.toString(ii),
+						Feature.DEFAULT_NAME,
+						description,
+						tokens[9] + ", " + tokens[11] + ", Schweiz",
+						Feature.DEFAULT_LOOK_AT,
+						styleURL,
+						Feature.DEFAULT_VISIBILITY,
+						Feature.DEFAULT_REGION,
+						new TimeSpan(gcBegin, gcEnd));
 
-
-//		for (String str : lines) {
-//
-//			tokens = str.split("\t");
-//			System.out.println(tokens[7] + "\t" + tokens[9] + "\t" + tokens[10] + "\t" + tokens[11]);
-//
-//		}
-
+				aCoop.addFeature(aCoopPhase);		
+			}
+			
+			openClose.clear();
+		
+		}
+		
+		
 	}
 
 	private static void write() {
--- matsim/src/playground/meisterk/facilities/ShopsOf2005ToFacilities.java
+++ matsim/src/playground/meisterk/facilities/ShopsOf2005ToFacilities.java
@@ -23,14 +23,412 @@ package playground.meisterk.facilities;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.io.FileUtils;
+import org.matsim.utils.vis.kml.Document;
+import org.matsim.utils.vis.kml.Feature;
+import org.matsim.utils.vis.kml.Folder;
+import org.matsim.utils.vis.kml.Icon;
+import org.matsim.utils.vis.kml.IconStyle;
+import org.matsim.utils.vis.kml.KML;
+import org.matsim.utils.vis.kml.KMZWriter;
+import org.matsim.utils.vis.kml.Placemark;
+import org.matsim.utils.vis.kml.Style;
 
+/**
+ * In April 2005, I collected information on shop facilities of the major
+ * Swiss retailers, incl. Migros, Coop, Pickpay and Denner. The information
+ * includes addresses and opening times, as it is available on the respective
+ * company websites.
+ * 
+ * This class fuses this information and formats it to a MATSim compatible
+ * facility file.
+ * 
+ * @author meisterk
+ *
+ */
 public class ShopsOf2005ToFacilities {
 
-	private static String inputFilename = "/Volumes/share-baug-ivt-vpl-$/Public/exchange.temp/meister/scan_shops/pickpay_opentimes.txt";
+	private static String pickPayOpenTimesFilename = "/Users/meisterk/sandbox00/ivt/studies/switzerland/facilities/shopsOf2005/pickpay_opentimes.txt";
+	private static String pickPayAdressesFilename = "/Users/meisterk/sandbox00/ivt/studies/switzerland/facilities/shopsOf2005/pickpay_addresses_ge_ready.csv";
+	private static String coopZHFilename = "/Users/meisterk/sandbox00/ivt/studies/switzerland/facilities/shopsOf2005/coop-zh.csv";
+	private static String coopTGFilename = "/Users/meisterk/sandbox00/ivt/studies/switzerland/facilities/shopsOf2005/coop-tg.csv";
+	private static String migrosZHFilename = "/Users/meisterk/sandbox00/ivt/studies/switzerland/facilities/shopsOf2005/migros-zh.csv";
+	private static String migrosOstschweizFilename = "/Users/meisterk/sandbox00/ivt/studies/switzerland/facilities/shopsOf2005/migros-ostschweiz-filialen.csv";
+	private static String dennerTGZHFilename = "/Users/meisterk/sandbox00/ivt/studies/switzerland/facilities/shopsOf2005/denner-tg-zh.csv";
+
+	private static KML myKML = null;
+	private static Document myKMLDocument = null;
+	private static String kmlFilename = "/Users/meisterk/Documents/workspace/MATSim/output/shopsOf2005.kmz";
+
+	private static Style openCoopStyle = null;
+	private static Style closedCoopStyle = null;
+	private static Style openPickpayStyle = null;
+	private static Style closedPickpayStyle = null;
+	private static Style openMigrosStyle = null;
+	private static Style closedMigrosStyle = null;
+	private static Style openDennerStyle = null;
+	private static Style closedDennerStyle = null;
+
+	private static void setUp() {
+
+		myKML = new KML();
+		myKMLDocument = new Document("the root document");
+		myKML.setFeature(myKMLDocument);
+
+	}
+
+	private static void setupStyles() {
+
+		openCoopStyle = new Style("openCoopStyle");
+		myKMLDocument.addStyle(openCoopStyle);
+		openCoopStyle.setIconStyle(
+				new IconStyle(new Icon("/Users/meisterk/Documents/workspace/MATSim/src/playground/meisterk/facilities/res/coop_dhtml.jpg")));
+		closedCoopStyle = new Style("closedCoopStyle");
+		myKMLDocument.addStyle(closedCoopStyle);
+		closedCoopStyle.setIconStyle(
+				new IconStyle(new Icon("/Users/meisterk/Documents/workspace/MATSim/src/playground/meisterk/facilities/res/coop_dhtml_invert.jpg")));
+
+		openPickpayStyle = new Style("openPickpayStyle");
+		myKMLDocument.addStyle(openPickpayStyle);
+		openPickpayStyle.setIconStyle(
+				new IconStyle(new Icon("/Applications/Google Earth.app/Contents/Resources/paddle/p.png")));
+		closedPickpayStyle = new Style("closedPickpayStyle");
+		myKMLDocument.addStyle(closedPickpayStyle);
+		closedPickpayStyle.setIconStyle(
+				new IconStyle(new Icon("/Applications/Google Earth.app/Contents/Resources/paddle/p.png")));
+
+		openMigrosStyle = new Style("openMigrosStyle");
+		myKMLDocument.addStyle(openMigrosStyle);
+		openMigrosStyle.setIconStyle(
+				new IconStyle(new Icon("/Users/meisterk/Documents/workspace/MATSim/src/playground/meisterk/facilities/res/logo_head_migros.gif")));
+		closedMigrosStyle = new Style("closedMigrosStyle");
+		myKMLDocument.addStyle(closedMigrosStyle);
+		closedMigrosStyle.setIconStyle(
+				new IconStyle(new Icon("/Users/meisterk/Documents/workspace/MATSim/src/playground/meisterk/facilities/res/logo_head_migros.gif")));
+
+		openDennerStyle = new Style("openDennerStyle");
+		myKMLDocument.addStyle(openDennerStyle);
+		openDennerStyle.setIconStyle(
+				new IconStyle(new Icon("/Users/meisterk/Documents/workspace/MATSim/src/playground/meisterk/facilities/res/denner.jpeg")));
+		closedDennerStyle = new Style("closedDennerStyle");
+		myKMLDocument.addStyle(closedDennerStyle);
+		closedDennerStyle.setIconStyle(
+				new IconStyle(new Icon("/Users/meisterk/Documents/workspace/MATSim/src/playground/meisterk/facilities/res/denner.jpeg")));
+
+
+	}
+
+	private static void dennerTGZHAddressesToKML() {
+		
+		Folder aFolder = null;
+		Placemark aShop = null;
+
+		aFolder = new Folder(
+				"dennerFolder",
+				"Denner TG ZH",
+				"Alle Denner TG ZH Läden",
+				Feature.DEFAULT_ADDRESS,
+				Feature.DEFAULT_LOOK_AT,
+				Feature.DEFAULT_STYLE_URL,
+				Feature.DEFAULT_VISIBILITY,
+				Feature.DEFAULT_REGION,
+				Feature.DEFAULT_TIME_PRIMITIVE);
+
+		myKMLDocument.addFeature(aFolder);	
+		
+		List<String> lines = null;
+		String[] tokens = null;
+
+		try {
+
+			lines = FileUtils.readLines(new File(dennerTGZHFilename), null);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	
+		String beginsWith3Digits = "^[F]*";
+
+		for (String line : lines) {
+
+			if (Pattern.matches("[0-9]{3}", line)) {
+
+				System.out.println(line);
+				
+			}
+			
+//			tokens = line.split(",");
+			
+		}
+		
+	}	
+	
+	private static void coopZHAddressesToKML() {
+
+		Folder coopFolder = null;
+		Placemark aCoop = null;
+
+		coopFolder = new Folder(
+				"coopZHFolder",
+				"Coop ZH",
+				"Alle Coop ZH Läden",
+				Feature.DEFAULT_ADDRESS,
+				Feature.DEFAULT_LOOK_AT,
+				Feature.DEFAULT_STYLE_URL,
+				Feature.DEFAULT_VISIBILITY,
+				Feature.DEFAULT_REGION,
+				Feature.DEFAULT_TIME_PRIMITIVE);
+
+		myKMLDocument.addFeature(coopFolder);	
+
+		List<String> lines = null;
+		String[] tokens = null;
+		String VSTTyp = null;
+
+		try {
+
+			lines = FileUtils.readLines(new File(coopZHFilename), null);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		for (String line : lines) {
+
+			//System.out.println(line);
+			tokens = line.split(",");
+
+			VSTTyp = tokens[7];
+			if (
+					VSTTyp.equals("CC") || 
+					VSTTyp.equals("CL") ||
+					VSTTyp.equals("CSC") ||
+					VSTTyp.equals("M")) {
+
+				aCoop = new Placemark(
+						"coop_" + VSTTyp + "_" + tokens[8],
+						"Coop " + VSTTyp + " " + tokens[8],
+						Feature.DEFAULT_DESCRIPTION,
+						tokens[9] + ", " + tokens[11] + ", Schweiz",
+						Feature.DEFAULT_LOOK_AT,
+						openCoopStyle.getStyleUrl(),
+						Feature.DEFAULT_VISIBILITY,
+						Feature.DEFAULT_REGION,
+						Feature.DEFAULT_TIME_PRIMITIVE);
+
+				coopFolder.addFeature(aCoop);
+
+			}
+
+		}
+	}
+
+	private static void coopTGAddressesToKML() {
+
+		Folder coopFolder = null;
+		Placemark aCoop = null;
+
+		coopFolder = new Folder(
+				"coopTGFolder",
+				"Coop TG",
+				"Alle Coop TG Läden",
+				Feature.DEFAULT_ADDRESS,
+				Feature.DEFAULT_LOOK_AT,
+				Feature.DEFAULT_STYLE_URL,
+				Feature.DEFAULT_VISIBILITY,
+				Feature.DEFAULT_REGION,
+				Feature.DEFAULT_TIME_PRIMITIVE);
+
+		myKMLDocument.addFeature(coopFolder);	
+
+		List<String> lines = null;
+		String[] tokens = null;
+
+		try {
+
+			lines = FileUtils.readLines(new File(coopTGFilename), null);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		for (String line : lines) {
+
+			System.out.println(line);
+			tokens = line.split(",");
+
+			aCoop = new Placemark(
+					"coop_" + tokens[0],
+					"Coop " + tokens[0],
+					Feature.DEFAULT_DESCRIPTION,
+					tokens[1] + ", " + tokens[2] + ", Schweiz",
+					Feature.DEFAULT_LOOK_AT,
+					openCoopStyle.getStyleUrl(),
+					Feature.DEFAULT_VISIBILITY,
+					Feature.DEFAULT_REGION,
+					Feature.DEFAULT_TIME_PRIMITIVE);
+
+			coopFolder.addFeature(aCoop);
+
+		}
+	}
+
+	private static void pickPayAddressesToKML() {
+
+		Folder pickpayFolder = null;
+		Placemark aPickpay = null;
+
+		pickpayFolder = new Folder(
+				"pickpayFolder",
+				"Pickpay",
+				"Alle Pickpay Läden",
+				Feature.DEFAULT_ADDRESS,
+				Feature.DEFAULT_LOOK_AT,
+				Feature.DEFAULT_STYLE_URL,
+				Feature.DEFAULT_VISIBILITY,
+				Feature.DEFAULT_REGION,
+				Feature.DEFAULT_TIME_PRIMITIVE);
+
+		myKMLDocument.addFeature(pickpayFolder);
+
+		List<String> lines = null;
+		String[] tokens = null;
+
+		try {
+
+			lines = FileUtils.readLines(new File(pickPayAdressesFilename), null);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		for (String line : lines) {
+
+//			System.out.println(line);
+			tokens = line.split(",");
+
+			aPickpay = new Placemark(
+					"pickpay_" + tokens[0],
+					"Pickpay " + tokens[1],
+					Feature.DEFAULT_DESCRIPTION,
+					tokens[2] + ", " + tokens[5] + ", Schweiz",
+					Feature.DEFAULT_LOOK_AT,
+					openPickpayStyle.getStyleUrl(),
+					Feature.DEFAULT_VISIBILITY,
+					Feature.DEFAULT_REGION,
+					Feature.DEFAULT_TIME_PRIMITIVE);
+
+			pickpayFolder.addFeature(aPickpay);
+
+		}
+
+	}
+
+	private static void migrosZHAdressesToKML() {
+		
+		Folder migrosZHFolder = null;
+		Placemark aMigrosZH = null;
+
+		migrosZHFolder = new Folder(
+				"migrosZHFolder",
+				"Migros ZH",
+				"Alle Migros ZH Läden",
+				Feature.DEFAULT_ADDRESS,
+				Feature.DEFAULT_LOOK_AT,
+				Feature.DEFAULT_STYLE_URL,
+				Feature.DEFAULT_VISIBILITY,
+				Feature.DEFAULT_REGION,
+				Feature.DEFAULT_TIME_PRIMITIVE);
+
+		myKMLDocument.addFeature(migrosZHFolder);
+
+		List<String> lines = null;
+		String[] tokens = null;
+
+		try {
+
+			lines = FileUtils.readLines(new File(migrosZHFilename), null);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		for (String line : lines) {
+
+			System.out.println(line);
+			tokens = line.split(",");
+
+			aMigrosZH = new Placemark(
+					"migrosZH_" + tokens[1],
+					"Migros " + tokens[1],
+					Feature.DEFAULT_DESCRIPTION,
+					tokens[2] + ", " + tokens[3] + ", Schweiz",
+					Feature.DEFAULT_LOOK_AT,
+					openMigrosStyle.getStyleUrl(),
+					Feature.DEFAULT_VISIBILITY,
+					Feature.DEFAULT_REGION,
+					Feature.DEFAULT_TIME_PRIMITIVE);
+
+			migrosZHFolder.addFeature(aMigrosZH);
+
+		}
+
+		
+	}
+	
+	private static void migrosOstschweizAdressesToKML() {
+		
+		Folder migrosOstschweizFolder = null;
+		Placemark aMigrosOstschweiz = null;
+
+		migrosOstschweizFolder = new Folder(
+				"migrosOstschweizFolder",
+				"Migros Ostschweiz",
+				"Alle Migros Ostschweiz Läden",
+				Feature.DEFAULT_ADDRESS,
+				Feature.DEFAULT_LOOK_AT,
+				Feature.DEFAULT_STYLE_URL,
+				Feature.DEFAULT_VISIBILITY,
+				Feature.DEFAULT_REGION,
+				Feature.DEFAULT_TIME_PRIMITIVE);
+
+		myKMLDocument.addFeature(migrosOstschweizFolder);
+
+		List<String> lines = null;
+		String[] tokens = null;
+
+		try {
+
+			lines = FileUtils.readLines(new File(migrosOstschweizFilename), null);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		for (String line : lines) {
+
+			System.out.println(line);
+			tokens = line.split(",");
+
+			aMigrosOstschweiz = new Placemark(
+					"migrosOstschweiz_" + tokens[1],
+					tokens[0] + " " + tokens[1],
+					Feature.DEFAULT_DESCRIPTION,
+					tokens[5] + ", " + tokens[6] + ", Schweiz",
+					Feature.DEFAULT_LOOK_AT,
+					openMigrosStyle.getStyleUrl(),
+					Feature.DEFAULT_VISIBILITY,
+					Feature.DEFAULT_REGION,
+					Feature.DEFAULT_TIME_PRIMITIVE);
+
+			migrosOstschweizFolder.addFeature(aMigrosOstschweiz);
+
+		}
+
+		
+	}
 
 	private static void readPickPayOpenTimes() {
 
@@ -48,10 +446,9 @@ public class ShopsOf2005ToFacilities {
 		String anythingButDigits = "[^0-9]";
 
 		try {
-			lines = FileUtils.readLines(new File(inputFilename), "UTF-8");
+			lines = FileUtils.readLines(new File(pickPayOpenTimesFilename), "UTF-8");
 
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
@@ -86,13 +483,36 @@ public class ShopsOf2005ToFacilities {
 
 	}
 
+	private static void write() {
+
+		System.out.println("    writing KML files out...");
+
+		KMZWriter writer;
+		writer = new KMZWriter(kmlFilename);
+		writer.writeMainKml(myKML);
+		writer.close();
+
+		System.out.println("    done.");
+
+	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
-		ShopsOf2005ToFacilities.readPickPayOpenTimes();
-		
+		ShopsOf2005ToFacilities.setUp();
+		ShopsOf2005ToFacilities.setupStyles();
+		ShopsOf2005ToFacilities.dennerTGZHAddressesToKML();
+//		ShopsOf2005ToFacilities.pickPayAddressesToKML();
+//		ShopsOf2005ToFacilities.coopZHAddressesToKML();
+//		ShopsOf2005ToFacilities.coopTGAddressesToKML();
+//		ShopsOf2005ToFacilities.migrosZHAdressesToKML();
+//		ShopsOf2005ToFacilities.migrosOstschweizAdressesToKML();
+		ShopsOf2005ToFacilities.write();
+
+//		ShopsOf2005ToFacilities.readPickPayOpenTimes();
+
 	}
 
 }
