a56b8c548af0d03ab5efacf673bf4099e7e6062c
mrieser <rieser@gmail.com>
2008-01-30 17:58:54 +0000

    added javadoc to package.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@541 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/org/matsim/controler/corelisteners/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Core listeners are {@link org.matsim.controler.listener.ControlerListener}s
+ * that are loaded automatically by the {@link org.matsim.controler.Controler}.
+ */
+package org.matsim.controler.corelisteners;
