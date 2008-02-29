e0e54661b094d78199fb264735f1300486b11eff
mrieser <rieser@gmail.com>
2008-02-29 14:47:07 +0000

    new utility to easily create XY-Plots (aka Scatter Plots)
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@773 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/utils/charts/Demo.java
+++ matsim/src/org/matsim/utils/charts/Demo.java
@@ -49,10 +49,18 @@ public class Demo {
 		chart.saveAsPng(filename, 800, 600);
 	}
 
+	public void createXYScatterChart(final String filename) {
+		XYScatterChart chart = new XYScatterChart("TITLE", "x-axis", "y-axis");
+		chart.addSeries("serie 1", new double[] {1.0, 2.0, 3.0, 4.0, 5.0}, new double[] {1.0, 5.0, 2.0, 3.0, 4.5});
+		chart.addSeries("serie 2", new double[] {1.0, 5.0, 2.0, 4.0, 3.0}, new double[] {2.0, 3.0, 3.0, 1.5, 4.5});
+		chart.saveAsPng(filename, 800, 600);
+	}
+
 	public static void main(final String[] args) {
 		Demo demo = new Demo();
 		demo.createBarChart("./output/barchart.png");
 		demo.createLineChart("./output/linechart.png");
 		demo.createXYLineChart("./output/xylinechart.png");
+		demo.createXYScatterChart("./output/xyscatterchart.png");
 	}
 }
---
+++ matsim/src/org/matsim/utils/charts/XYScatterChart.java
@@ -0,0 +1,76 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * XYScatterChart.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.utils.charts;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+/**
+ * Creates a new XY-ScatterChart.
+ *
+ * @author mrieser
+ */
+public class XYScatterChart extends ChartUtil {
+
+	private final XYSeriesCollection dataset;
+
+	public XYScatterChart(final String title, final String xAxisLabel, final String yAxisLabel) {
+		super(title, xAxisLabel, yAxisLabel);
+		this.dataset = new XYSeriesCollection();
+		this.chart = createChart(title, xAxisLabel, yAxisLabel, this.dataset);
+		addDefaultFormatting();
+	}
+
+	@Override
+	protected JFreeChart getChart() {
+		return this.chart;
+	}
+
+	private JFreeChart createChart(final String title, final String categoryAxisLabel,
+			final String valueAxisLabel, final XYSeriesCollection dataset) {
+		return ChartFactory.createScatterPlot(title, categoryAxisLabel, valueAxisLabel,
+				dataset, PlotOrientation.VERTICAL, true, // legend?
+				false, // tooltips?
+				false // URLs?
+				);
+	}
+
+	/**
+	 * Adds a new data series to the chart with the specified title.
+	 * <code>xs<code> and <code>ys</code> should have the same length. If not, only as many items
+	 * are shown as the shorter array contains.
+	 *
+	 * @param title
+	 * @param xs The x values.
+	 * @param ys The y values.
+	 */
+	public void addSeries(final String title, final double[] xs, final double[] ys) {
+		XYSeries series = new XYSeries(title, false, true);
+		for (int i = 0, n = Math.min(xs.length, ys.length); i < n; i++) {
+			series.add(xs[i], ys[i]);
+		}
+		this.dataset.addSeries(series);
+	}
+
+}
