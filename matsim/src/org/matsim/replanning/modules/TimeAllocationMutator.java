71c24bdf28168d9b843a9c079b0e79f4be55fa8a
mrieser <rieser@gmail.com>
2008-03-10 16:30:43 +0000

    changed the code that queries the config for an optional param, so there won't be any longer an error message in the log if the (optional!) param could not be found. Other code cleanup, added javadoc.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@848 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/replanning/modules/TimeAllocationMutator.java
+++ matsim/src/org/matsim/replanning/modules/TimeAllocationMutator.java
@@ -20,30 +20,51 @@
 
 package org.matsim.replanning.modules;
 
+import org.apache.log4j.Logger;
 import org.matsim.gbl.Gbl;
 import org.matsim.plans.algorithms.PlanAlgorithmI;
 import org.matsim.plans.algorithms.PlanMutateTimeAllocation;
 
+/**
+ * Wraps the {@link org.matsim.plans.algorithms.PlanMutateTimeAllocation}-
+ * PlanAlgorithm into a {@link StrategyModuleI} so it can be used for plans
+ * replanning. Supports multiple threads.
+ *
+ * @author mrieser
+ * @see org.matsim.plans.algorithms.PlanMutateTimeAllocation
+ */
 public class TimeAllocationMutator extends MultithreadedModuleA {
 
-	private int mutationRange = 1800;// TODO [MR] should be a config-option
+	public final static String CONFIG_GROUP = "TimeAllocationMutator";
+	public final static String CONFIG_MUTATION_RANGE = "mutationRange";
+	
+	private final static Logger log = Logger.getLogger(TimeAllocationMutator.class);
 
+	private int mutationRange = 1800;
+
+	/**
+	 * Creates a new TimeAllocationMutator with a mutation range as defined in 
+	 * the configuration (module "TimeAllocationMutator", param "mutationRange"),
+	 * or the default value of 1800 (seconds) if there is no value specified in 
+	 * the configuration.
+	 */
 	public TimeAllocationMutator() {
-		String range = null;
-		try {
-			range = Gbl.getConfig().getParam("TimeAllocationMutator","mutationRange");
-		}
-		catch (IllegalArgumentException e) {
-			Gbl.noteMsg(this.getClass(),"TimeAllocationMutator()","No mutation range defined in the config file. Using 1800 sec.");
-		}
-		if (range != null) {
+		String range = Gbl.getConfig().findParam(CONFIG_GROUP, CONFIG_MUTATION_RANGE);
+		if (range == null) {
+			log.info("No mutation range defined in the config file. Using default of " + mutationRange + " sec.");			
+		} else {
 			this.mutationRange = Integer.parseInt(range);
-			Gbl.noteMsg(this.getClass(),"TimeAllocationMutator()","mutation range = " + this.mutationRange + ".");
+			log.info("mutation range = " + this.mutationRange);
 		}
 	}
 
-	public TimeAllocationMutator(final int muntation_range) {
-		this.mutationRange = muntation_range;
+	/**
+	 * Creates a new TimeAllocationMutator with the specified mutation range.
+	 * 
+	 * @param mutationRange
+	 */
+	public TimeAllocationMutator(final int mutationRange) {
+		this.mutationRange = mutationRange;
 	}
 
 	@Override
