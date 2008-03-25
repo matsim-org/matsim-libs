4344e688abbd689d9debe00e5d530b4e739b648f
grether <dominik.grether@gmail.com>
2008-03-25 15:11:45 +0000

    Refactored: class Link was renamed to LinkImpl, all classes use the Interface Link, which implements the same signature as the Link class in former versions. Same for Location: Class Location is now class AbstractLocation. The Signature of the former Location-Class is now reflected by the Location interface. No semantic changes should appear by this modification, tests are running.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@1000 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/org/matsim/network/Link.java
@@ -0,0 +1,41 @@
+package org.matsim.network;
+
+import org.matsim.interfaces.networks.basicNet.BasicLinkI;
+import org.matsim.utils.geometry.CoordI;
+import org.matsim.world.Location;
+
+public interface Link extends BasicLinkI, Location {
+
+	public double calcDistance(final CoordI coord);
+
+	// DS TODO try to remove these and update references
+	// (for the time being, they are here because otherwise the returned type is wrong. kai)
+	public Node getFromNode();
+
+	public Node getToNode();
+
+	public String getOrigId();
+
+	public String getType();
+
+	public Object getRole(final int idx);
+
+	/** @return Returns the euklidean distance between from- and to-node. */
+	public double getEuklideanDistance();
+
+	/**
+	 * This method returns the normalized capacity of the link, i.e. the
+	 * capacity of vehicles per second. Be aware that it will not consider the
+	 * capacity reduction factors set in the config and used in the simulation. If interested
+	 * in this values, check the appropriate methods of QueueLink.
+	 * @return the flow capacity of this link per second
+	 */
+	public double getFlowCapacity();
+
+	public void setOrigId(final String id);
+
+	public void setRole(final int idx, final Object role);
+
+	public void setMaxRoleIndex(final int index);
+
+}
