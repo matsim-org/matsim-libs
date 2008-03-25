87bd02ef660e379e934b3e51bbca438ef2db9c2e
laemmel <gregor.laemmel@gmail.com>
2008-03-25 11:00:24 +0000

    java 1.5 compatibility
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@988 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/gregor/treeTest/MyTests.java
+++ matsim/src/playground/gregor/treeTest/MyTests.java
@@ -61,40 +61,40 @@ public class MyTests {
 	
 	
 	
-	public static long testTreeMap() {
-	
-		Double dummy = 0.;
-		TreeMap<Integer, Double> map = new TreeMap<Integer,Double>();
-//		log.info("starting TreeMap test");
-		Double dbl = 10.0;
-		
-//		log.info("building up the tree ...");
-		long start = Long.valueOf(System.currentTimeMillis());
-		for (int i = 0; i < 3600*24;) {
-			
-			dbl = Gbl.random.nextDouble();
-			map.put(i, dbl);
-			i += Gbl.random.nextInt(3600);
-		}
-		long stop = Long.valueOf(System.currentTimeMillis());
-//		log.info("tree built-up took:" + (stop-start) + " ms. Tree size is:" + map.size() );
-		
-		
-		long time = 0;
-//		log.info("simulating " + LOOKUPS + " lookups");
-		for (int i = 0; i < LOOKUPS; i++) {
-			int query = Gbl.random.nextInt(20);
-			start = Long.valueOf(System.currentTimeMillis());
-			Double value = (Double) map.floorEntry(query).getValue();
-			stop = Long.valueOf(System.currentTimeMillis());
-			time += stop-start;
-			dummy = Math.max(value, dummy);
-		}
-//		System.out.println("... it took:" + time + " ms");
-//		System.out.println("dummy " + dummy);
-		
-		return time;
-	}
+//	public static long testTreeMap() {
+//	
+//		Double dummy = 0.;
+//		TreeMap<Integer, Double> map = new TreeMap<Integer,Double>();
+////		log.info("starting TreeMap test");
+//		Double dbl = 10.0;
+//		
+////		log.info("building up the tree ...");
+//		long start = Long.valueOf(System.currentTimeMillis());
+//		for (int i = 0; i < 3600*24;) {
+//			
+//			dbl = Gbl.random.nextDouble();
+//			map.put(i, dbl);
+//			i += Gbl.random.nextInt(3600);
+//		}
+//		long stop = Long.valueOf(System.currentTimeMillis());
+////		log.info("tree built-up took:" + (stop-start) + " ms. Tree size is:" + map.size() );
+//		
+//		
+//		long time = 0;
+////		log.info("simulating " + LOOKUPS + " lookups");
+//		for (int i = 0; i < LOOKUPS; i++) {
+//			int query = Gbl.random.nextInt(20);
+//			start = Long.valueOf(System.currentTimeMillis());
+//			Double value = (Double) map.floorEntry(query).getValue();
+//			stop = Long.valueOf(System.currentTimeMillis());
+//			time += stop-start;
+//			dummy = Math.max(value, dummy);
+//		}
+////		System.out.println("... it took:" + time + " ms");
+////		System.out.println("dummy " + dummy);
+//		
+//		return time;
+//	}
 	
 	public static long  testClasspathTreeMap(){
 		Double dummy = 0.;
@@ -131,45 +131,45 @@ public class MyTests {
 	}
 	
 	
-	private static boolean testCompatibility() {
-		
-		TreeMap<Integer, Double> map = new TreeMap<Integer,Double>();
-		org.matsim.utils.collections.gnuclasspath.TreeMap<Integer, Double> gnumap = new org.matsim.utils.collections.gnuclasspath.TreeMap<Integer,Double>();
-		Double dbl = 10.0;
-				
-				
-		for (int i = 0; i < 3600*24;) {
-			
-			dbl = Gbl.random.nextDouble();
-			map.put(i, dbl);
-			gnumap.put(i, dbl);
-//			i += Gbl.random.nextInt(3600);
-			i++;
-		}
-		for (int i = 0; i < LOOKUPS; i++) {
-			int query = Gbl.random.nextInt(20);
-		
-			Double value = (Double) map.floorEntry(query).getValue();
-			Double gnuvalue = (Double) gnumap.floorEntry(query).getValue();
-			
-			if (value != gnuvalue) {
-				return false;
-			}
-		
-		}
-		
-		
-		
-		// TODO Auto-generated method stub
-		return true;
-	}
+//	private static boolean testCompatibility() {
+//		
+//		TreeMap<Integer, Double> map = new TreeMap<Integer,Double>();
+//		org.matsim.utils.collections.gnuclasspath.TreeMap<Integer, Double> gnumap = new org.matsim.utils.collections.gnuclasspath.TreeMap<Integer,Double>();
+//		Double dbl = 10.0;
+//				
+//				
+//		for (int i = 0; i < 3600*24;) {
+//			
+//			dbl = Gbl.random.nextDouble();
+//			map.put(i, dbl);
+//			gnumap.put(i, dbl);
+////			i += Gbl.random.nextInt(3600);
+//			i++;
+//		}
+//		for (int i = 0; i < LOOKUPS; i++) {
+//			int query = Gbl.random.nextInt(20);
+//		
+//			Double value = (Double) map.floorEntry(query).getValue();
+//			Double gnuvalue = (Double) gnumap.floorEntry(query).getValue();
+//			
+//			if (value != gnuvalue) {
+//				return false;
+//			}
+//		
+//		}
+//		
+//		
+//		
+//		// TODO Auto-generated method stub
+//		return true;
+//	}
 	
 	
 	public static void main(String [] args) {
 		
 //		 
-		boolean compatible = testCompatibility();
-		System.out.println("combatible:" + compatible);
+//		boolean compatible = testCompatibility();
+//		System.out.println("combatible:" + compatible);
 		
 		long base = 0;
 		long tree = 0;
@@ -177,7 +177,7 @@ public class MyTests {
 		for (int i = 0; i < 1000; i++) {
 			base += baseLineTest();
 
-			tree += testTreeMap();
+//			tree += testTreeMap();
 			
 			classpathTree += testClasspathTreeMap();
 
