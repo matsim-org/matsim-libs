b53cb8561ff69961e15a4fc67e76ce7c5c9f0722
yu_vsp <yu_vsp@users.sf.net>
2008-03-30 22:29:16 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@1036 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/yu/newPlan/SelectedPlans.java
@@ -0,0 +1,67 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * NewAgentPtPlan.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.yu.newPlan;
+
+import org.matsim.plans.Person;
+import org.matsim.plans.Plan;
+import org.matsim.plans.Plans;
+import org.matsim.plans.PlansWriter;
+import org.matsim.plans.algorithms.PersonAlgorithm;
+import org.matsim.plans.algorithms.PersonAlgorithmI;
+
+/**
+ * writes new Plansfile, in which every person will has 2 plans, one with type
+ * "iv" and the other with type "oev", whose leg mode will be "pt" and who will
+ * have only a blank <Route></Rout>
+ * 
+ * @author ychen
+ * 
+ */
+public class SelectedPlans extends PersonAlgorithm implements PersonAlgorithmI {
+	/**
+	 * internal writer, which can be used by object of subclass.
+	 */
+	protected PlansWriter pw;
+
+	/**
+	 * Constructor, writes file-head
+	 * 
+	 * @param plans -
+	 *            a Plans Object, which derives from MATSim plansfile
+	 */
+	public SelectedPlans(Plans plans) {
+		pw = new PlansWriter(plans);
+		pw.writeStartPlans();
+	}
+
+	public void writeEndPlans() {
+		pw.writeEndPlans();
+	}
+
+	@Override
+	public void run(Person person) {
+		Plan selectedPlan = person.getSelectedPlan();
+		person.getPlans().clear();
+		person.addPlan(selectedPlan);
+		pw.writePerson(person);
+	}
+
+}
---
+++ matsim/src/playground/yu/newPlan/SelectedPlansControler.java
@@ -0,0 +1,63 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * NewPlansControler.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.yu.newPlan;
+
+import org.matsim.config.Config;
+import org.matsim.gbl.Gbl;
+import org.matsim.mobsim.QueueNetworkLayer;
+import org.matsim.network.MatsimNetworkReader;
+import org.matsim.plans.MatsimPlansReader;
+import org.matsim.plans.Plans;
+import org.matsim.plans.PlansReaderI;
+import org.matsim.world.World;
+
+/**
+ * test of NewAgentPtPlan
+ * 
+ * @author ychen
+ * 
+ */
+public class SelectedPlansControler {
+
+	public static void main(final String[] args) {
+		// final String netFilename = "./test/yu/ivtch/input/network.xml";
+		final String netFilename = "../data/schweiz/input/ch.xml";
+		final String plansFilename = "../data/schweiz/input/410.100.plans.xml.gz";
+
+		World world = Gbl.getWorld();
+		Config config = Gbl.createConfig(new String[] {
+		// "./test/yu/ivtch/config_for_10pctZuerich_car_pt_smallPlansl.xml"
+				"../data/schweiz/selectedPlans.xml" });
+
+		QueueNetworkLayer network = new QueueNetworkLayer();
+		new MatsimNetworkReader(network).readFile(netFilename);
+		world.setNetworkLayer(network);
+
+		Plans population = new Plans();
+		SelectedPlans sp = new SelectedPlans(population);
+		population.addAlgorithm(sp);
+		PlansReaderI plansReader = new MatsimPlansReader(population);
+		plansReader.readFile(plansFilename);
+		world.setPopulation(population);
+		population.runAlgorithms();
+		sp.writeEndPlans();
+	}
+}
