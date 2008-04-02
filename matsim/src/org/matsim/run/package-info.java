fa5bad3d9ec11beb16c3fdd8c1c92e3cb2c78c16
mrieser <rieser@gmail.com>
2008-04-02 17:48:53 +0000

    add javadoc for package org.matsim.run.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@1074 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/org/matsim/run/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * This package contains classes that can directly be started from the command
+ * line and that do something useful.
+ * <br>
+ * All the classes in this package should contain a main-method that
+ * parses arguments, displays a short usage-note if arguments are missing or
+ * make no sense, and otherwise do some (hopefully useful) work.
+ * <br>
+ * The code for the actual work these classes do should not be within this
+ * package, but in other packages (e.g. {@link org.matsim.analysis},
+ * {@link org.matsim.plans.algorithms}, ...). In this package are really only
+ * classes with main-methods that offer some functionality to the outside by
+ * using code from other packages.
+ */
+package org.matsim.run;
