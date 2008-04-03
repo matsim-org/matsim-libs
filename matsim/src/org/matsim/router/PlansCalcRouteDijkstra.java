17ac1293a26e96f58a6a7b877c59b50c36a22129
mrieser <rieser@gmail.com>
2008-04-03 17:05:20 +0000

    A class to explicitly use the Dijkstra-Router, similar to PlansCalcRouteLandmarks.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@1099 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/org/matsim/router/PlansCalcRouteDijkstra.java
@@ -0,0 +1,44 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * PlansCalcRouteLandmarks.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.router;
+
+import org.matsim.network.NetworkLayer;
+import org.matsim.router.costcalculators.FreespeedTravelTimeCost;
+import org.matsim.router.util.TravelCostI;
+import org.matsim.router.util.TravelTimeI;
+
+/**
+ * A PersonAlgorithm that calculates and sets the routes of a person's activities using {@link Dijkstra}.
+ *
+ * @author mrieser
+ */
+public class PlansCalcRouteDijkstra extends PlansCalcRoute {
+
+	public PlansCalcRouteDijkstra(final NetworkLayer network, final TravelCostI costCalculator, final TravelTimeI timeCalculator) {
+		this(network, costCalculator, timeCalculator, new FreespeedTravelTimeCost());
+	}
+
+	private PlansCalcRouteDijkstra(final NetworkLayer network, final TravelCostI costCalculator, final TravelTimeI timeCalculator,
+			final FreespeedTravelTimeCost freespeedTimeCost) {
+		super(new Dijkstra(network, costCalculator, timeCalculator),
+				new Dijkstra(network, freespeedTimeCost, freespeedTimeCost));
+	}
+}
