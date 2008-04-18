cc9b9a1342dfbc0bfe2996fcac7f854d18923c75
vsp_nagel <2008@kainagel.org>
2008-04-18 16:47:50 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@1240 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/kai/teach/MyControler.java
@@ -0,0 +1,122 @@
+package playground.kai.teach;
+
+import java.io.IOException;
+import java.util.*;
+
+import org.matsim.interfaces.networks.basicNet.*;
+import org.matsim.network.MatsimNetworkReader;
+import org.matsim.network.NetworkLayer;
+import org.matsim.utils.vis.netvis.DrawableAgentI;
+import org.matsim.utils.vis.netvis.NetVis;
+
+import teach.multiagent07.net.CANetStateWritableI;
+import teach.multiagent07.net.CANetStateWriter;
+
+class Veh implements DrawableAgentI {
+	
+	private double pos ;
+	public void setPosition ( double tmp ) {
+		pos = tmp ;
+	}
+
+	public int getLane() {
+		return 1;
+	}
+
+	public double getPosInLink_m() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
+
+class CALink implements CANetStateWritableI {	
+	
+	final double CELLSIZE=7.5 ;
+	
+	List cells = new ArrayList() ;
+	
+	public CALink( BasicLinkI ll ) {
+		// ...
+	}
+
+	public List<DrawableAgentI> getDisplayAgents() {
+		List<DrawableAgentI> list = new ArrayList() ;
+		for ( int ii=0 ; ii<cells.size() ; ii++ ) {
+			Veh veh = (Veh) cells.get(ii) ;
+			if ( veh!= null ) {
+				veh.setPosition(ii*CELLSIZE ) ;
+				list.add( veh ) ;
+			}
+		}
+		return list ;
+	}
+
+	public double getDisplayValue() {	
+		// count occupied cells
+		// return occ.cells/cells
+		return 0.5 ;
+	}
+}
+
+class CASim {
+	
+	CANetStateWriter netVis ;
+	
+	Map caLinks = new HashMap() ;
+	Map caNodes = new HashMap() ;
+
+	public CASim( BasicNetI net, CANetStateWriter netVis ) {
+		Map links = net.getLinks() ;
+		for ( Iterator it = links.values().iterator(); it.hasNext() ; ) {
+			BasicLinkI ll = (BasicLinkI) it.next();
+			CALink caLink = new CALink( ll ) ;	
+//			caLink.randomFill() ;
+//			caLink.tty() ;
+		}
+		// dto for nodes
+
+		// connect??
+		
+		try {
+			netVis.dump(0) ;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+}
+
+public class MyControler {
+	
+
+	public static void main(String[] args) {
+		String netFileName = "net.xml" ;
+		String visFileName = "output/ABC" ;
+	
+		BasicNetI net = new NetworkLayer() ;
+		MatsimNetworkReader reader = new MatsimNetworkReader((NetworkLayer)net);
+		reader.readFile(netFileName) ;
+		// from here on, net can do everything that is guaranteed in BasicNetI
+		
+		Map nodes = net.getNodes() ;
+		
+		nodes.get("123") ;
+		
+		for ( Iterator it = nodes.values().iterator(); it.hasNext() ; ) {
+			BasicNodeI nn = (BasicNodeI) it.next();
+			System.out.println ( nn.getId() ) ;
+		}
+		
+		CANetStateWriter netVis = CANetStateWriter.createWriter( net, netFileName, visFileName );		
+		
+		CASim sim = new CASim( net , netVis ) ;
+		
+		NetVis.start(visFileName) ;
+		
+	}
+
+}
