1b12ff30a3908e99f409bc4284540ee7353bdfdd
komeiste <komeiste@users.sf.net>
2008-04-20 20:04:43 +0000

    Slight changes in person ID filter test.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@1247 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/test/src/org/matsim/plans/filters/PersonIdFilterTest.java
+++ matsim/test/src/org/matsim/plans/filters/PersonIdFilterTest.java
@@ -12,18 +12,18 @@ public class PersonIdFilterTest extends MatsimTestCase {
 	 * @throws Exception
 	 */
 	public void testPersonIdFilter() throws Exception {
-		
+
 		// create fixture: 4 persons with special ids
 		TreeMap<String, Person> persons = new TreeMap<String, Person>();
 		for (String personId : new String[]{"1", "2102002002", "30", "3030"}) {
 			persons.put(personId, new Person(personId, "f", "30", "yes", "yes", "yes"));
 		}
- 
+
 		PersonIdFilter tenDigitIdFilter = new PersonIdFilter("[0-9]{10}", null);
 		PersonIdFilter agent30IdFilter =  new PersonIdFilter("30", null);
 		PersonIdFilter containsAOneIdFilter = new PersonIdFilter(".*1.*", null);
 		PersonIdFilter between100And9999IdFilter = new PersonIdFilter("[1-9][0-9]{2,3}", null);
-		
+
 		assertFalse(tenDigitIdFilter.judge(persons.get("1")));
 		assertTrue(tenDigitIdFilter.judge(persons.get("2102002002")));
 		assertFalse(tenDigitIdFilter.judge(persons.get("30")));
@@ -43,7 +43,7 @@ public class PersonIdFilterTest extends MatsimTestCase {
 		assertFalse(between100And9999IdFilter.judge(persons.get("2102002002")));
 		assertFalse(between100And9999IdFilter.judge(persons.get("30")));
 		assertTrue(between100And9999IdFilter.judge(persons.get("3030")));
-}
-	
-	
+	}
+
+
 }
