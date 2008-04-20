57d875adae053f33a0b3f02a8190e28719941015
komeiste <komeiste@users.sf.net>
2008-04-20 10:51:19 +0000

    Refined the person ID filter test once again.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@1244 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/test/src/org/matsim/plans/filters/PersonIdFilterTest.java
+++ matsim/test/src/org/matsim/plans/filters/PersonIdFilterTest.java
@@ -8,7 +8,7 @@ import org.matsim.testcases.MatsimTestCase;
 public class PersonIdFilterTest extends MatsimTestCase {
 
 	/**
-	 * @author kmeister
+	 * @author komeiste
 	 * @throws Exception
 	 */
 	public void testPersonIdFilter() throws Exception {
@@ -20,29 +20,29 @@ public class PersonIdFilterTest extends MatsimTestCase {
 		}
  
 		PersonIdFilter tenDigitIdFilter = new PersonIdFilter("[0-9]{10}", null);
-		PersonIdFilter agent30Filter =  new PersonIdFilter("30", null);
-		PersonIdFilter containsAOneFilter = new PersonIdFilter(".*1.*", null);
-		PersonIdFilter between100And9999Filter = new PersonIdFilter("[1-9][0-9]{2,3}", null);
+		PersonIdFilter agent30IdFilter =  new PersonIdFilter("30", null);
+		PersonIdFilter containsAOneIdFilter = new PersonIdFilter(".*1.*", null);
+		PersonIdFilter between100And9999IdFilter = new PersonIdFilter("[1-9][0-9]{2,3}", null);
 		
 		assertFalse(tenDigitIdFilter.judge(persons.get("1")));
 		assertTrue(tenDigitIdFilter.judge(persons.get("2102002002")));
 		assertFalse(tenDigitIdFilter.judge(persons.get("30")));
 		assertFalse(tenDigitIdFilter.judge(persons.get("3030")));
 
-		assertFalse(agent30Filter.judge(persons.get("1")));
-		assertFalse(agent30Filter.judge(persons.get("2102002002")));
-		assertTrue(agent30Filter.judge(persons.get("30")));
-		assertFalse(agent30Filter.judge(persons.get("3030")));
+		assertFalse(agent30IdFilter.judge(persons.get("1")));
+		assertFalse(agent30IdFilter.judge(persons.get("2102002002")));
+		assertTrue(agent30IdFilter.judge(persons.get("30")));
+		assertFalse(agent30IdFilter.judge(persons.get("3030")));
 
-		assertTrue(containsAOneFilter.judge(persons.get("1")));
-		assertTrue(containsAOneFilter.judge(persons.get("2102002002")));
-		assertFalse(containsAOneFilter.judge(persons.get("30")));
-		assertFalse(containsAOneFilter.judge(persons.get("3030")));
+		assertTrue(containsAOneIdFilter.judge(persons.get("1")));
+		assertTrue(containsAOneIdFilter.judge(persons.get("2102002002")));
+		assertFalse(containsAOneIdFilter.judge(persons.get("30")));
+		assertFalse(containsAOneIdFilter.judge(persons.get("3030")));
 
-		assertFalse(between100And9999Filter.judge(persons.get("1")));
-		assertFalse(between100And9999Filter.judge(persons.get("2102002002")));
-		assertFalse(between100And9999Filter.judge(persons.get("30")));
-		assertTrue(between100And9999Filter.judge(persons.get("3030")));
+		assertFalse(between100And9999IdFilter.judge(persons.get("1")));
+		assertFalse(between100And9999IdFilter.judge(persons.get("2102002002")));
+		assertFalse(between100And9999IdFilter.judge(persons.get("30")));
+		assertTrue(between100And9999IdFilter.judge(persons.get("3030")));
 }
 	
 	
