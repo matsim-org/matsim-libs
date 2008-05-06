441caa8e277b6a3e9e459cd3a9c45c72b3c732c0
mrieser <rieser@gmail.com>
2008-05-06 14:45:11 +0000

    removed unused import-statement.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@1465 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/network/NetworkLayer.java
+++ matsim/src/org/matsim/network/NetworkLayer.java
@@ -22,7 +22,6 @@ package org.matsim.network;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
