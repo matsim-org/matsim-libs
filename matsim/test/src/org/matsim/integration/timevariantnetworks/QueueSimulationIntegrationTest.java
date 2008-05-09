da3384b7e7609037a33b2c52352dd6c05a98a1d0
jillenberger <jillenberger@users.sf.net>
2008-05-09 10:15:32 +0000

    New test for time-variant networks with capacity reduction.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@1498 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/test/src/org/matsim/integration/timevariantnetworks/QueueSimulationIntegrationTest.java
+++ matsim/test/src/org/matsim/integration/timevariantnetworks/QueueSimulationIntegrationTest.java
@@ -20,6 +20,9 @@
 
 package org.matsim.integration.timevariantnetworks;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.matsim.basic.v01.IdImpl;
 import org.matsim.events.EventLinkEnter;
 import org.matsim.events.EventLinkLeave;
@@ -53,6 +56,106 @@ public class QueueSimulationIntegrationTest extends MatsimTestCase {
 	public void testFreespeed() throws Exception {
 		loadConfig(null);
 
+		NetworkLayer network = createNetwork();
+		Link link1 = network.getLink("1");
+		Link link2 = network.getLink("2");
+		Link link3 = network.getLink("3");
+		
+		// add a freespeed change to 20 at 8am.
+		NetworkChangeEvent change = new NetworkChangeEvent(8*3600.0);
+		change.addLink(link2);
+		change.setFreespeedChange(new ChangeValue(ChangeType.ABSOLUTE, 20));
+		network.addNetworkChangeEvent(change);
+
+		// create a population
+		Plans plans = new Plans(Plans.NO_STREAMING);
+		Person person1 = createPersons(7*3600, link1, link3, 1).get(0);
+		Person person2 = createPersons(9*3600, link1, link3, 1).get(0);
+		plans.addPerson(person1);
+		plans.addPerson(person2);
+		
+		// run the simulation with the timevariant network and the two persons
+		Events events = new Events();
+		TestTravelTimeCalculator ttcalc = new TestTravelTimeCalculator(person1, person2, link2);
+		events.addHandler(ttcalc);
+		QueueSimulation qsim = new QueueSimulation(network, plans, events);
+		qsim.run();
+
+		// check that we get the expected result
+		assertEquals("Person 1 should travel for 11 seconds.", 10.0 + 1.0, ttcalc.person1leaveTime - ttcalc.person1enterTime, EPSILON);
+		assertEquals("Person 2 should travel for 6 seconds.", 5.0 + 1.0, ttcalc.person2leaveTime - ttcalc.person2enterTime, EPSILON);
+	}
+
+	/**
+	 * Test the queue simulation for correct behavior if capacity of links is
+	 * reduced during the run.
+	 * 
+	 * @author illenberger
+	 */
+	public void testCapacity() throws Exception {
+		final int personsPerWave = 10;
+		final double capacityFactor = 0.5;
+		
+		loadConfig(null);
+
+		NetworkLayer network = createNetwork();
+		Link link1 = network.getLink("1");
+		Link link2 = network.getLink("2");
+		Link link3 = network.getLink("3");
+		/*
+		 * Create a network change event that reduces the capacity.
+		 */
+		NetworkChangeEvent change1 = new NetworkChangeEvent(0);
+		change1.addLink(link2);
+		change1.setFlowCapacityChange(new ChangeValue(ChangeType.FACTOR, capacityFactor));
+		network.addNetworkChangeEvent(change1);
+		/*
+		 * Create a network event the restores the capacity to its original value.
+		 */
+		NetworkChangeEvent change2 = new NetworkChangeEvent(3600);
+		change2.addLink(link2);
+		change2.setFlowCapacityChange(new ChangeValue(ChangeType.FACTOR, 1/capacityFactor));
+		network.addNetworkChangeEvent(change2);
+		/*
+		 * Create two waves of persons, each counting 10.
+		 */
+		Plans plans = new Plans(Plans.NO_STREAMING);
+		List<Person> persons1 = createPersons(0, link1, link3, personsPerWave);
+		for(Person p : persons1)
+			plans.addPerson(p);
+		Person person1 = persons1.get(personsPerWave - 1);
+		
+		List<Person> persons2 = createPersons(3600, link1, link3, personsPerWave);
+		for(Person p : persons2)
+			plans.addPerson(p);
+		Person person2 = persons2.get(personsPerWave - 1);
+		/*
+		 * Run the simulation with the time-variant network and the two waves of
+		 * persons. Observe the last person of each wave.
+		 */
+		Events events = new Events();
+		TestTravelTimeCalculator ttcalc = new TestTravelTimeCalculator(person1, person2, link2);
+		events.addHandler(ttcalc);
+		QueueSimulation qsim = new QueueSimulation(network, plans, events);
+		qsim.run();
+		/*
+		 * The last person of the first wave should have taken 20 s to travel
+		 * link 3 (because of the spill-back). The last person of the second
+		 * wave should have free-flow travel time.
+		 */
+		assertEquals("Person 1 should travel for 20 seconds.", 20.0, ttcalc.person1leaveTime - ttcalc.person1enterTime, EPSILON);
+		assertEquals("Person 2 should travel for 11 seconds.", 10.0 + 1.0, ttcalc.person2leaveTime - ttcalc.person2enterTime, EPSILON);
+
+	}
+	
+	/**
+	 * Creates a network with three links of length 100 m, capacity 3600 veh/h
+	 * and freespeed 10 m/s.
+	 * 
+	 * @return a network.
+	 * @author illenberger
+	 */
+	private NetworkLayer createNetwork() {
 		// create a network
 		NetworkFactory nf = new NetworkFactory();
 		nf.setLinkPrototype(TimeVariantLinkImpl.class);
@@ -65,51 +168,40 @@ public class QueueSimulationIntegrationTest extends MatsimTestCase {
 		network.createNode("2", "100", "0", null);
 		network.createNode("3", "200", "0", null);
 		network.createNode("4", "300", "0", null);
-		Link link1 = network.createLink("1", "1", "2", "100", "10", "3600", "1", null, null);
-		TimeVariantLinkImpl link2 = (TimeVariantLinkImpl)network.createLink("2", "2", "3", "100", "10", "3600", "1", null, null);
-		Link link3 = network.createLink("3", "3", "4", "100", "10", "3600", "1", null, null);
+		network.createLink("1", "1", "2", "100", "10", "3600", "1", null, null);
+		network.createLink("2", "2", "3", "100", "10", "3600", "1", null, null);
+		network.createLink("3", "3", "4", "100", "10", "3600", "1", null, null);
 
-		// add a freespeed change to 20 at 8am.
-		NetworkChangeEvent change = new NetworkChangeEvent(8*3600.0);
-		change.addLink(link2);
-		change.setFreespeedChange(new ChangeValue(ChangeType.ABSOLUTE, 20));
-		network.addNetworkChangeEvent(change);
-
-		// create a population
-		Plans plans = new Plans(Plans.NO_STREAMING);
-
-		// create a person that travels before the network change happens
-		Person person1 = new Person(new IdImpl(1), "m", 40, "yes", "always", "yes");
-		plans.addPerson(person1);
-		Plan plan1 = person1.createPlan(null, "yes");
-		plan1.createAct("h", 0, 0, link1, 0.0, 7*3600.0, 7*3600.0, false);
-		Leg leg1 = plan1.createLeg(0, "car", 7*3600.0, 10, 7*3600.0 + 10);
-		Route route = new Route();
-		route.setRoute("2 3");
-		leg1.setRoute(route);
-		plan1.createAct("w", 300, 0, link3, 7*3600.0+10, Time.UNDEFINED_TIME, Time.UNDEFINED_TIME, true);
-
-		// create a person that travels after the network change happens
-		Person person2 = new Person(new IdImpl(2), "f", 40, "yes", "always", "yes");
-		plans.addPerson(person2);
-		Plan plan2 = person2.createPlan(null, "yes");
-		plan2.createAct("h", 0, 0, link1, 0.0, 9*3600.0, 9*3600.0, false);
-		Leg leg2 = plan2.createLeg(0, "car", 9*3600.0, 10, 9*3600.0 + 10);
-		Route route2 = new Route();
-		route2.setRoute("2 3");
-		leg2.setRoute(route2);
-		plan2.createAct("w", 300, 0, link3, 9*3600.0+10, Time.UNDEFINED_TIME, Time.UNDEFINED_TIME, true);
-
-		// run the simulation with the timevariant network and the two persons
-		Events events = new Events();
-		TestTravelTimeCalculator ttcalc = new TestTravelTimeCalculator(person1, person2, link2);
-		events.addHandler(ttcalc);
-		QueueSimulation qsim = new QueueSimulation(network, plans, events);
-		qsim.run();
-
-		// check that we get the expected result
-		assertEquals("Person 1 should travel for 11 seconds.", 10.0 + 1.0, ttcalc.person1leaveTime - ttcalc.person1enterTime, EPSILON);
-		assertEquals("Person 2 should travel for 6 seconds.", 5.0 + 1.0, ttcalc.person2leaveTime - ttcalc.person2enterTime, EPSILON);
+		return network;
+	}
+	
+	/**
+	 * Creates <tt>count</tt> persons with departure time
+	 * <tt>depTime<tt> + index(person).
+	 * @param depTime the departure time for the first person.
+	 * @param depLink the departure link.
+	 * @param destLink the destination link.
+	 * @param count the number of persons to create
+	 * @return a list of persons where the ordering corresponds to the departure times.
+	 * @throws Exception
+	 * @author illenberger
+	 */
+	private List<Person> createPersons(double depTime, Link depLink, Link destLink, int count) throws Exception {
+		List<Person> persons = new ArrayList<Person>(count);
+		for(int i = 0; i < count; i++) {
+			Person person = new Person(new IdImpl(i + (int)depTime), "m", 40, "yes", "always", "yes");
+			Plan plan1 = person.createPlan(null, "yes");
+			plan1.createAct("h", 0, 0, depLink, 0.0, depTime, depTime, false);
+			Leg leg1 = plan1.createLeg(0, "car", depTime, 10, depTime + 10);
+			Route route = new Route();
+			route.setRoute("2 3");
+			leg1.setRoute(route);
+			plan1.createAct("w", 300, 0, destLink, depTime+10, Time.UNDEFINED_TIME, Time.UNDEFINED_TIME, true);
+			
+			persons.add(person);
+			depTime++;
+		}
+		return persons;
 	}
 
 	/**
