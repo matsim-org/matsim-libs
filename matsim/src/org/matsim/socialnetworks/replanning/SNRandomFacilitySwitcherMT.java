d8efc4802d2e2adac89866d10100272dac59d545
jkhackney <jkhackney@users.sf.net>
2008-06-02 09:41:25 +0000

    Two replanning modules replace the one. Now you can call a multhreaded module (MT) or a single-threaded module (SM).
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@1729 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/socialnetworks/replanning/SNRandomFacilitySwitcherMT.java
+++ matsim/src/org/matsim/socialnetworks/replanning/SNRandomFacilitySwitcherMT.java
@@ -20,14 +20,11 @@
 
 package org.matsim.socialnetworks.replanning;
 
-import org.matsim.gbl.Gbl;
 import org.matsim.network.NetworkLayer;
-import org.matsim.plans.Plan;
 import org.matsim.plans.algorithms.PlanAlgorithmI;
-import org.matsim.replanning.modules.StrategyModuleI;
+import org.matsim.replanning.modules.MultithreadedModuleA;
 import org.matsim.router.util.TravelCostI;
 import org.matsim.router.util.TravelTimeI;
-import org.matsim.replanning.modules.*;
 /**
  * The social network replanning StrategyModule is not multi-threaded because each
  * agent could refer to and alter other agent objects in a random manner.
@@ -36,8 +33,7 @@ import org.matsim.replanning.modules.*;
  *
  */
 
-//public class SNRandomFacilitySwitcher implements StrategyModuleI {
-public class SNRandomFacilitySwitcher extends MultithreadedModuleA {
+public class SNRandomFacilitySwitcherMT extends MultithreadedModuleA {
 	private NetworkLayer network=null;
 	private TravelCostI tcost=null;
 	private TravelTimeI ttime=null;
@@ -47,37 +43,18 @@ public class SNRandomFacilitySwitcher extends MultithreadedModuleA {
 	 */
 	private String[] factypes={"home","work","shop","education","leisure"};
 	
-    public SNRandomFacilitySwitcher(NetworkLayer network, TravelCostI tcost, TravelTimeI ttime) {
-//	public SNRandomFacilitySwitcher() {
+    public SNRandomFacilitySwitcherMT(NetworkLayer network, TravelCostI tcost, TravelTimeI ttime) {
+
 		System.out.println("initializing SNRandomFacilitySwitcher");
     	this.network=network;
     	this.tcost = tcost;
     	this.ttime = ttime;
-//    	This is a workaround until factypes can be put into a config file that is
-//    	available to all elements of the SN run
-//    	this.factypes = playground.jhackney.controler.SNControllerListenerRePlanSecLoc.activityTypesForEncounters;
     }
 
     public PlanAlgorithmI getPlanAlgoInstance() {
 //	return new SNSecLocShortest(factypes, network, tcost, ttime);
 	return new SNSecLocRandom(factypes, network, tcost, ttime);
     }
-//
-//	public void finish() {
-//		// TODO Auto-generated method stub
-//		
-//	}
-//
-//	public void handlePlan(Plan plan) {
-//		// TODO Auto-generated method stub
-//		
-//	}
-//
-//	public void init() {
-//		// TODO Auto-generated method stub
-//		
-//	}
-
 
 
 }
