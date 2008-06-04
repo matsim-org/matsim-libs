77191c1623784e2ed3cb72793b776628748e51f6
andreas77 <andreashorni@hotmail.com>
2008-06-04 08:28:20 +0000

    added project info added/formated comments
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@1758 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/run/CompareSelectedPlansTable.java
+++ matsim/src/org/matsim/run/CompareSelectedPlansTable.java
@@ -1,3 +1,23 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * OTFVis.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
 package org.matsim.run;
 
 import java.io.BufferedWriter;
@@ -15,11 +35,16 @@ import org.matsim.plans.Plans;
 import org.matsim.plans.PlansReaderI;
 import org.matsim.utils.io.IOUtils;
 
+/**
+ * Creates a table containing the comparison of two plans files
+ *
+ * @author anhorni
+ */
 public class CompareSelectedPlansTable {
 
 	private Plans plans0;
 	private Plans plans1;
-	private String header="personid\tsex\tage\tlicense\tcaravail\t" +
+	private final String header="personid\tsex\tage\tlicense\tcaravail\t" +
 			"employed\thomex\thomey\thomelink\tscore0\tscore1\tplantraveltime0\tplantraveltime1\t" +
 			"plantraveldistance0\tplantraveldistance1\tnumberoftrips0\tnumberoftrips1";
 	private NetworkLayer network;
@@ -27,9 +52,10 @@ public class CompareSelectedPlansTable {
 
 
 	/**
-	 * @param args array with 4 entries: {path to plans file 0, path to plans file 1, name of output file, path to network file}
+	 * @param args array with 4 entries:
+	 * {path to plans file 0, path to plans file 1, name of output file, path to network file}
 	 */
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 
 		if (args.length < 4) {
 			System.out.println("Too few arguments.");
@@ -38,13 +64,13 @@ public class CompareSelectedPlansTable {
 		}
 
 		Gbl.startMeasurement();
-		CompareSelectedPlansTable table=new CompareSelectedPlansTable();
+		final CompareSelectedPlansTable table=new CompareSelectedPlansTable();
 		table.run(args[0], args[1], args[2], args[3]);
 
 		Gbl.printElapsedTime();
 	}
 
-	private void init(String networkPath) {
+	private void init(final String networkPath) {
 		this.plans0=new Plans(false);
 		this.plans1=new Plans(false);
 
@@ -53,27 +79,27 @@ public class CompareSelectedPlansTable {
 		new MatsimNetworkReader(this.network).readFile(networkPath);
 	}
 
-	private void readFiles(String plansfilePath0, String plansfilePath1) {
+	private void readFiles(final String plansfilePath0, final String plansfilePath1) {
 		System.out.println("  reading file "+plansfilePath0);
-		PlansReaderI plansReader0 = new MatsimPlansReader(this.plans0);
+		final PlansReaderI plansReader0 = new MatsimPlansReader(this.plans0);
 		plansReader0.readFile(plansfilePath0);
 
 		System.out.println("  reading file "+plansfilePath1);
-		PlansReaderI plansReader1 = new MatsimPlansReader(this.plans1);
+		final PlansReaderI plansReader1 = new MatsimPlansReader(this.plans1);
 		plansReader1.readFile(plansfilePath1);
 	}
 
-	private void writeSummaryFile(String outfile) {
+	private void writeSummaryFile(final String outfile) {
 		try {
-			BufferedWriter out = IOUtils.getBufferedWriter(outfile);
+			final BufferedWriter out = IOUtils.getBufferedWriter(outfile);
 			out.write(this.header);
 			out.newLine();
 
-			for (Id person_id : this.plans0.getPersons().keySet()) {
+			for (final Id person_id : this.plans0.getPersons().keySet()) {
 
 				// method person.toString() not appropriate
 				out.write(person_id.toString()+"\t");
-				Person person=this.plans0.getPerson(person_id);
+				final Person person=this.plans0.getPerson(person_id);
 				out.write(person.getSex()+"\t");
 				out.write(person.getAge()+"\t");
 				out.write(person.getLicense()+"\t");
@@ -93,70 +119,71 @@ public class CompareSelectedPlansTable {
 
 				out.write(person.getSelectedPlan().getScore()+"\t");
 
-				Person person_comp=this.plans1.getPerson(person_id);
+				final Person person_comp=this.plans1.getPerson(person_id);
 				out.write(person_comp.getSelectedPlan().getScore()+"\t");
 
 				out.write(this.getTravelTime(person)+"\t");
 				out.write(this.getTravelTime(person_comp)+"\t");
-				
+
 				out.write(this.getTravelDist(person)+"\t");
 				out.write(this.getTravelDist(person_comp)+"\t");
-				
+
 				out.write(this.getNumberOfTrips(person)+"\t");
 				// TODO: using newline(). But still a tab is necessary (!?) Correct that later.
 				out.write(this.getNumberOfTrips(person_comp)+"\t");
-				
+
 				out.newLine();
 				out.flush();
 			}
 			out.close();
 		}
-		catch (IOException e) {
+		catch (final IOException e) {
 			Gbl.errorMsg(e);
 		}
 	}
 
-	/*  TODO: Put the next three methods into a "stats" class and use  
+	/*  TODO: Put the next three methods into a "stats" class and use
 	 *  traveltime, numberOfTrips and traveldist as attributes.
 	 *  At the moment it is "nicer" to have everything in one single class */
-	
-	private double getTravelTime(Person person) {
+
+	private double getTravelTime(final Person person) {
 
 		double travelTime=0.0;
-		LegIterator leg_it = person.getSelectedPlan().getIteratorLeg();
+		final LegIterator leg_it = person.getSelectedPlan().getIteratorLeg();
 		while (leg_it.hasNext()) {
-			Leg leg = (Leg)leg_it.next();
+			final Leg leg = (Leg)leg_it.next();
 			travelTime+=leg.getTravTime();
 		}
 		return travelTime;
 	}
-	
-	private double getTravelDist(Person person) {
+
+	private double getTravelDist(final Person person) {
 
 		double travelDist=0.0;
-		LegIterator leg_it = person.getSelectedPlan().getIteratorLeg();
-				
+		final LegIterator leg_it = person.getSelectedPlan().getIteratorLeg();
+
 		while (leg_it.hasNext()) {
-			Leg leg = (Leg)leg_it.next();
+			final Leg leg = (Leg)leg_it.next();
 			travelDist+=leg.getRoute().getDist();
 		}
 		return travelDist;
 	}
-	
-	private int getNumberOfTrips(Person person) {
+
+	private int getNumberOfTrips(final Person person) {
 
 		int numberOfLegs=0;
-		LegIterator leg_it = person.getSelectedPlan().getIteratorLeg();
+		final LegIterator leg_it = person.getSelectedPlan().getIteratorLeg();
 		while (leg_it.hasNext()) {
 			leg_it.next();
 			numberOfLegs++;
 		}
 		return numberOfLegs;
 	}
-	
+
 	// --------------------------------------------------------------------------
 
-	private void run(String plansfilePath0, String plansfilePath1, String outfile, String networkPath) {
+	private void run(final String plansfilePath0, final String plansfilePath1,
+			final String outfile, final String networkPath) {
 		this.init(networkPath);
 		readFiles(plansfilePath0, plansfilePath1);
 		writeSummaryFile(outfile);
