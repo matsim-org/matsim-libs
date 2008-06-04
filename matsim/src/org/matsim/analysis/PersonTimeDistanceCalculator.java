00974f66debf3bc177a417ae7a1f1cb13050decb
andreas77 <andreashorni@hotmail.com>
2008-06-04 15:30:09 +0000

    helper for TravelTimeandDistanceStats
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@1777 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/org/matsim/analysis/PersonTimeDistanceCalculator.java
@@ -0,0 +1,44 @@
+package org.matsim.analysis;
+
+import org.matsim.basic.v01.BasicPlanImpl.LegIterator;
+import org.matsim.plans.Leg;
+import org.matsim.plans.Person;
+
+public class PersonTimeDistanceCalculator {
+
+	private static double planTravelTime;
+	private static double planTravelDistance;
+	private static int numberOfLegs;
+
+
+	private static void init() {
+		planTravelTime=0.0;
+		planTravelDistance=0.0;
+		numberOfLegs=0;
+	}
+
+	public static void run(final Person person){
+
+		init();
+
+		final LegIterator leg_it = person.getSelectedPlan().getIteratorLeg();
+		while (leg_it.hasNext()) {
+			final Leg leg = (Leg)leg_it.next();
+			planTravelTime+=leg.getTravTime();
+			planTravelDistance+=leg.getRoute().getDist();
+			numberOfLegs++;
+		}
+	}
+
+	public static double getPlanTravelTime() {
+		return planTravelTime;
+	}
+
+	public static double getPlanTravelDistance() {
+		return planTravelDistance;
+	}
+
+	public static int getNumberOfLegs() {
+		return numberOfLegs;
+	}
+}
