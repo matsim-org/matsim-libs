883164dead8c8112cc0e07c8c896ad9f4ae4080d
andreas77 <andreashorni@hotmail.com>
2008-06-05 09:18:31 +0000

    moved from package analysis
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@1790 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/anhorni/locationchoice/analysis/PersonTimeDistanceCalculator.java
@@ -0,0 +1,49 @@
+package playground.anhorni.locationchoice.analysis;
+
+import org.matsim.basic.v01.BasicPlanImpl.LegIterator;
+import org.matsim.plans.Leg;
+import org.matsim.plans.Person;
+
+/*
+ * This is a helper class for TravelTimeandDistanceStats
+ * TODO: move this functionality to Person.java
+ */
+
+public class PersonTimeDistanceCalculator {
+
+	private static double planTravelTime;
+	private static double planTravelDistance;
+	private static int numberOfLegs;
+
+
+	private static void init() {
+		planTravelTime=0.0;
+		planTravelDistance=0.0;
+		numberOfLegs=0;
+	}
+
+	public static void run(final Person person){
+
+		init();
+
+		final LegIterator leg_it = person.getSelectedPlan().getIteratorLeg();
+		while (leg_it.hasNext()) {
+			final Leg leg = (Leg)leg_it.next();
+			planTravelTime+=leg.getTravTime();
+			planTravelDistance+=leg.getRoute().getDist();
+			numberOfLegs++;
+		}
+	}
+
+	public static double getPlanTravelTime() {
+		return planTravelTime;
+	}
+
+	public static double getPlanTravelDistance() {
+		return planTravelDistance;
+	}
+
+	public static int getNumberOfLegs() {
+		return numberOfLegs;
+	}
+}
