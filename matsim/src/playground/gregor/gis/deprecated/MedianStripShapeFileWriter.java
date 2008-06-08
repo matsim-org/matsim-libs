68074e9a03b816ac18b84436a68960261dc05bdb
laemmel <gregor.laemmel@gmail.com>
2008-06-08 13:51:51 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@1836 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/gregor/gis/deprecated/MedianStripShapeFileWriter.java
@@ -0,0 +1,219 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * MedianStripShapeFileWriter.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.gregor.gis.deprecated;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+
+import org.geotools.data.FeatureSource;
+import org.geotools.data.FeatureStore;
+import org.geotools.data.Transaction;
+import org.geotools.data.shapefile.ShapefileDataStore;
+import org.geotools.feature.DefaultFeatureCollection;
+import org.geotools.feature.Feature;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureType;
+import org.matsim.config.Config;
+import org.matsim.gbl.Gbl;
+import org.matsim.network.Link;
+import org.matsim.network.MatsimNetworkReader;
+import org.matsim.network.NetworkLayer;
+import org.matsim.utils.StringUtils;
+import org.matsim.utils.geometry.CoordI;
+import org.matsim.utils.geometry.CoordinateTransformationI;
+import org.matsim.utils.geometry.geotools.MGC;
+import org.matsim.utils.geometry.transformations.GeotoolsTransformation;
+import org.matsim.utils.io.IOUtils;
+import org.matsim.world.World;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.MultiLineString;
+import com.vividsolutions.jts.geom.PrecisionModel;
+
+public class MedianStripShapeFileWriter {
+
+	private ArrayList<Link> links;
+
+	private NetworkLayer network;
+
+	private String inputFile;
+
+	private Geometry [] geos;
+
+	private FeatureSource fs;
+
+	ShapefileDataStore ds = null;
+
+	public MedianStripShapeFileWriter(String filename) {
+
+
+		File inFile = new File("./padang/vd10_streetnetwork_padang_v0.5_utm47s.shp");
+
+	      // Load shapefile
+
+		try {
+			this.ds = new ShapefileDataStore(inFile.toURL());
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	      try {
+			this.fs = this.ds.getFeatureSource();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		this.links = new ArrayList<Link>();
+		String netfile = "./networks/padang_net.xml";
+		System.out.println("reading network xml file... ");
+		this.network = new NetworkLayer();
+		new MatsimNetworkReader(this.network).readFile(netfile);
+		System.out.println("done. ");
+		this.inputFile = filename;
+
+	}
+
+	public void run(){
+		readFile(this.inputFile);
+		createGeometries();
+		try {
+			writeGeometries();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+
+	private void writeGeometries() throws Exception {
+		FeatureType ft = this.fs.getSchema();
+		File out = new File("./padang/street_correction.shp");
+
+		String name = this.ds.getTypeNames()[0];
+
+		ShapefileDataStore outStore = new ShapefileDataStore(out.toURL());
+        outStore.createSchema(this.fs.getSchema());
+        FeatureSource newFeatureSource = outStore.getFeatureSource(name);
+        FeatureStore newFeatureStore = (FeatureStore)newFeatureSource;
+
+        // accquire a transaction to create the shapefile from FeatureStore
+        Transaction t = newFeatureStore.getTransaction();
+
+        FeatureCollection collect = new PFeatureCollection(ft);
+
+	      for (int i = 0; i < this.geos.length; i++){
+	    	  Geometry geo = this.geos[i];
+	    	Feature feature = ft.create(null);
+	    	feature.setDefaultGeometry(geo);
+	    	collect.add(feature);
+
+
+
+	      }
+	      newFeatureStore.addFeatures(collect);
+
+	      t.commit();
+	      t.close();
+
+
+	}
+
+	private void createGeometries() {
+
+		CoordinateTransformationI transform = new GeotoolsTransformation("WGS84_UTM47S","WGS84_UTM47S");
+
+		this.geos = new Geometry[this.links.size()];
+		PrecisionModel pm = new PrecisionModel(10);
+		GeometryFactory geofac = new GeometryFactory(pm);
+		int i = 0;
+		for (Link link : this.links){
+
+			CoordI a = transform.transform(link.getFromNode().getCoord());
+			CoordI b = transform.transform(link.getToNode().getCoord());
+
+			Coordinate [] coords = {MGC.coord2Coordinate(a),MGC.coord2Coordinate(b)};
+			LineString ls = new LineString(coords,pm,i);
+
+
+			MultiLineString lss = new MultiLineString(new LineString[]{ls},geofac);
+			this.geos[i++] = lss;
+
+		}
+	}
+
+	public void readFile(final String filename) {
+
+		BufferedReader  infile;
+		try {
+			infile = IOUtils.getBufferedReader(filename);
+			String line = infile.readLine();
+			if ((line != null) && (line.charAt(0) >= '0') && (line.charAt(0) <= '9')) {
+				/* The line starts with a number, so assume it's an event and parse it.
+				 * Otherwise it is likely the header.  */
+				parseLine(line);
+			}
+			// now all other lines should contain events
+			while ( (line = infile.readLine()) != null) {
+				parseLine(line);
+			}
+			infile.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+
+
+
+	private void parseLine(String line) {
+		String[] result = StringUtils.explode(line, ',', 7);
+		this.links.add(this.network.getLink(result[0]));
+
+	}
+
+	  public static class PFeatureCollection extends DefaultFeatureCollection {
+		    public PFeatureCollection(FeatureType ft) {
+		      super("error",ft );
+		    }
+		  }
+
+	public static void main(String [] args){
+		String configFile = "./configs/evacuationConf.xml";
+
+
+		World world = Gbl.getWorld();
+		Config config = Gbl.createConfig(new String[] {configFile});
+
+		String filename = "./padang/mittelstreifen.csv";
+		MedianStripShapeFileWriter mss = new MedianStripShapeFileWriter(filename);
+		mss.run();
+
+	}
+}
---
+++ matsim/src/playground/gregor/gis/deprecated/ShapeFileConverterDLR2KML.java
@@ -0,0 +1,132 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * ShapeFileConverterDLR2KML.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.gregor.gis.deprecated;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFinder;
+import org.geotools.data.FeatureSource;
+import org.geotools.feature.Feature;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureIterator;
+import org.geotools.referencing.CRS;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.operation.MathTransform;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.CoordinateSequence;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.geom.impl.CoordinateArraySequence;
+import org.geotools.geometry.jts.JTS;
+
+public class ShapeFileConverterDLR2KML {
+	
+
+    private Map<String,Vector<Geometry>> geometries;
+	
+	private final static String WGS84 = "GEOGCS[\"WGS84\", DATUM[\"WGS84\", SPHEROID[\"WGS84\", 6378137.0, 298.257223563]], PRIMEM[\"Greenwich\", 0.0], UNIT[\"degree\",0.017453292519943295], AXIS[\"Longitude\",EAST], AXIS[\"Latitude\",NORTH]]";
+	private final static String WGS84_UTM47S = "PROJCS[\"WGS_1984_UTM_Zone_47S\",GEOGCS[\"GCS_WGS_1984\",DATUM[\"D_WGS_1984\",SPHEROID[\"WGS_1984\",6378137.0,298.257223563]],PRIMEM[\"Greenwich\",0.0],UNIT[\"Degree\",0.0174532925199433]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"False_Easting\",500000.0],PARAMETER[\"False_Northing\",10000000.0],PARAMETER[\"Central_Meridian\",99.0],PARAMETER[\"Scale_Factor\",0.9996],PARAMETER[\"Latitude_Of_Origin\",0.0],UNIT[\"Meter\",1.0]]";
+	
+	private MathTransform transform;
+	
+	public void readDataFile(String fileName) throws Exception {
+		
+		File dataFile = new File(fileName);
+		
+		geometries = new HashMap<String,Vector<Geometry>>();
+	    Map connect = new HashMap();
+	    connect.put( "url", dataFile.toURL() );
+
+	    DataStore dataStore = DataStoreFinder.getDataStore( connect );
+	    String[] typeNames = dataStore.getTypeNames ();
+	    String typeName = typeNames[0];
+	    System.out.println( "Reading content "+ typeName + " ... performing coordinate transformation if needed");
+
+	    FeatureSource featureSource = dataStore.getFeatureSource( typeName );
+	    FeatureCollection collection = featureSource.getFeatures();
+	    
+	    FeatureIterator iterator = collection.features();
+
+	    
+		try {
+			this.transform = CRS.findMathTransform(CRS.parseWKT(WGS84_UTM47S), CRS.parseWKT(WGS84),true);
+		} catch (FactoryException e) {
+			e.printStackTrace();
+			System.exit(-1);	
+		}
+	    
+    	try {
+		       while( iterator.hasNext() ){
+       	   
+		    	   Feature feature = iterator.next();
+//		           String type = feature.getAttribute(2).toString();		  		           
+		           
+
+		    	   MultiPolygon geo = (MultiPolygon) feature.getDefaultGeometry();
+		    	   
+		    	   int polys = geo.getNumGeometries();	
+		    	   for (int i = 0; i < polys; i++){
+		    		   Polygon poly = (Polygon) JTS.transform(geo.getGeometryN(i), this.transform);
+		    			   
+		    			   
+		            	Coordinate [] c = new Coordinate[]{new Coordinate(650483.842005,9899500.877189,Double.NaN)};
+		            	
+		            	GeometryFactory geofac = new GeometryFactory();
+		        		CoordinateSequence seq = new CoordinateArraySequence(c);
+		        		Point point = new Point(seq,geofac);		        		
+		        		
+		        		if (!geo.contains(point)) {
+		        			continue;
+		        		}		        		
+		            	System.out.println("//////////////////////new link: ");
+       			            			            				            			            	
+		    	   }
+		       }		        
+		    }		    
+		    finally {
+		       iterator.close();
+		    }		
+	}
+
+	public static void main( String[] args ) throws Exception
+	{
+		String fileName;
+		if (args.length == 1)
+			fileName = args[0] ;
+		else
+//			fileName=  "jalan_padang/02_text__text.shp" ;
+		fileName=  "./padang/padang_water.shp" ;
+		
+		ShapeFileConverterDLR2KML shapeFileConverter = new ShapeFileConverterDLR2KML();
+		shapeFileConverter.readDataFile(fileName);
+	    
+	}
+
+}
+
