a8c0487d2b1c68a8825c4fc3b6c1ca56fe0bb91a
andreas77 <andreashorni@hotmail.com>
2008-06-24 11:06:47 +0000

    added -cap. restr. (penalty) -fac. attr. factor
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@2008 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/anhorni/locationchoice/scoring/LocationChoiceScoringFunctionFactory.java
@@ -0,0 +1,41 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * LocationChoiceScoringFunctionFactory.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.anhorni.locationchoice.scoring;
+
+import org.matsim.plans.Plan;
+import org.matsim.scoring.ScoringFunction;
+import org.matsim.scoring.ScoringFunctionFactory;
+
+/**
+ * A factory to create {@link LocationChoiceScoringFunction}s.
+ *
+ * @author anhorni
+ */
+public class LocationChoiceScoringFunctionFactory implements ScoringFunctionFactory {
+
+	/* (non-Javadoc)
+	 * @see org.matsim.demandmodeling.scoring.ScoringFunctionFactory#getNewScoringFunction(org.matsim.demandmodeling.plans.Plan)
+	 */
+	public ScoringFunction getNewScoringFunction(final Plan plan) {
+		return new LocationChoiceScoringFunction(plan);
+	}
+
+}
