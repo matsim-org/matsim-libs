03c70fa69a7e6e163d5bb5f9dbdd0af1963d802b
vsp_nagel <2008@kainagel.org>
2008-06-30 11:31:33 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@2070 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/kai/test/MyTestForTesting.java
@@ -0,0 +1,15 @@
+package playground.kai.test;
+
+import junit.framework.TestCase;
+
+public class MyTestForTesting extends TestCase {
+	
+	public void testSomething() {
+		assertEquals(0,1) ;
+	}
+
+	public void testSomethingElse() {
+		assertEquals(1,1) ;
+	}
+
+}
---
+++ matsim/src/playground/kai/test/OurNewTest.java
@@ -0,0 +1,16 @@
+package playground.kai.test;
+
+import junit.framework.TestCase;
+
+public class OurNewTest extends TestCase {
+	
+	public void testSomething() {
+		// run something
+		assertEquals(0.,0.) ;
+	}
+	
+	public void testSTElse() {
+		System.out.println( "hello " ) ;
+	}
+
+}
