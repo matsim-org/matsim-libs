5a6d8e912a32f0a6373f8ca05f49e65b5c44f7d2
andreas77 <andreashorni@hotmail.com>
2008-07-03 09:53:33 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@2130 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/anhorni/locationchoice/preprocess/RemoveBorderCrossingTraffic.java
@@ -0,0 +1,42 @@
+package playground.anhorni.locationchoice.preprocess;
+
+import java.util.Iterator;
+
+import org.apache.log4j.Logger;
+import org.matsim.basic.v01.IdImpl;
+import org.matsim.facilities.Facilities;
+import org.matsim.network.NetworkLayer;
+import org.matsim.plans.Person;
+import org.matsim.plans.Plans;
+import org.matsim.utils.misc.Counter;
+
+public class RemoveBorderCrossingTraffic extends Modifier {
+
+	private final static Logger log = Logger.getLogger(RemoveBorderCrossingTraffic.class);
+
+	public RemoveBorderCrossingTraffic(Plans plans, NetworkLayer network, Facilities  facilities){
+		super(plans, network, facilities);
+	}
+
+
+	@Override
+	public void modify() {
+		this.removeBorderCrossingPersonsTraffic();
+	}
+
+
+	private void removeBorderCrossingPersonsTraffic() {
+
+		log.info("running removeBorderCrossingPersonsTraffic:");
+		Iterator<Person> person_iter = this.plans.getPersons().values().iterator();
+		Counter counter = new Counter(" person # ");
+		while (person_iter.hasNext()) {
+			Person person = person_iter.next();
+			counter.incCounter();
+			if (person.getId().compareTo(new IdImpl(1000000000))>0) {
+				this.plans.getPersons().remove(person);
+			}
+		}
+		log.info("RemoveBorderCrossingTraffic done.");
+	}
+}
