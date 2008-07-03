900d1835d8db765a991b79f87d8f1e5987b10780
andreas77 <andreashorni@hotmail.com>
2008-07-03 10:26:11 +0000

    using Gbl.createWorld() instead of reading in one.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@2136 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/anhorni/locationchoice/preprocess/PlanModifications.java
+++ matsim/src/playground/anhorni/locationchoice/preprocess/PlanModifications.java
@@ -3,14 +3,13 @@ package playground.anhorni.locationchoice.preprocess;
 import org.apache.log4j.Logger;
 import org.matsim.facilities.Facilities;
 import org.matsim.facilities.FacilitiesReaderMatsimV1;
+import org.matsim.gbl.Gbl;
 import org.matsim.network.MatsimNetworkReader;
 import org.matsim.network.NetworkLayer;
 import org.matsim.plans.MatsimPlansReader;
 import org.matsim.plans.Plans;
 import org.matsim.plans.PlansReaderI;
 import org.matsim.plans.PlansWriter;
-import org.matsim.world.MatsimWorldReader;
-import org.matsim.world.World;
 import org.matsim.world.algorithms.WorldBottom2TopCompletion;
 import org.matsim.world.algorithms.WorldCheck;
 import org.matsim.world.algorithms.WorldValidation;
@@ -19,7 +18,6 @@ public class PlanModifications {
 
 	private Plans plans=null;
 	private NetworkLayer network=null;
-	private World world=null;
 	private Facilities  facilities =null;
 	private String outputpath="";
 	private Modifier modifier=null;
@@ -84,27 +82,27 @@ public class PlanModifications {
 			final String facilitiesfilePath, final String worldfilePath) {
 
 
-		System.out.println("  reading world xml file... ");
-		this.world=new World();
-		final MatsimWorldReader worldReader = new MatsimWorldReader(this.world);
-		worldReader.readFile(worldfilePath);
+		System.out.println("  create world ... ");
+		Gbl.createWorld();
+		//final MatsimWorldReader worldReader = new MatsimWorldReader(this.world);
+		//worldReader.readFile(worldfilePath);
 		System.out.println("  done.");
 
 
-		this.network = (NetworkLayer)this.world.createLayer(NetworkLayer.LAYER_TYPE,null);
+		this.network = (NetworkLayer)Gbl.getWorld().createLayer(NetworkLayer.LAYER_TYPE,null);
 		new MatsimNetworkReader(this.network).readFile(networkfilePath);
 		log.info("network reading done");
 
 		//this.facilities=new Facilities();
-		this.facilities=(Facilities)this.world.createLayer(Facilities.LAYER_TYPE, null);
+		this.facilities=(Facilities)Gbl.getWorld().createLayer(Facilities.LAYER_TYPE, null);
 		new FacilitiesReaderMatsimV1(this.facilities).readFile(facilitiesfilePath);
 		log.info("facilities reading done");
 
-		this.world.complete();
-		new WorldCheck().run(this.world);
-		new WorldBottom2TopCompletion().run(this.world);
-		new WorldValidation().run(this.world);
-		new WorldCheck().run(this.world);
+		Gbl.getWorld().complete();
+		new WorldCheck().run(Gbl.getWorld());
+		new WorldBottom2TopCompletion().run(Gbl.getWorld());
+		new WorldValidation().run(Gbl.getWorld());
+		new WorldCheck().run(Gbl.getWorld());
 		log.info("world checking done.");
 
 
