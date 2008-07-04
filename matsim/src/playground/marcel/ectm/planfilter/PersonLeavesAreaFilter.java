6e4b0ccd3688ff48d8da9d668021c2849961441b
mrieser <rieser@gmail.com>
2008-07-04 12:57:06 +0000

    some code to filter out plans for the ectm-zrh-scenario.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@2173 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/marcel/ectm/planfilter/PersonLeavesAreaFilter.java
@@ -0,0 +1,75 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * PersonLEavesArea.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.marcel.ectm.planfilter;
+
+import java.util.List;
+import java.util.Map;
+
+import org.matsim.basic.v01.Id;
+import org.matsim.network.Link;
+import org.matsim.plans.Act;
+import org.matsim.plans.Leg;
+import org.matsim.plans.Person;
+import org.matsim.plans.Plan;
+import org.matsim.plans.algorithms.PersonAlgorithmI;
+import org.matsim.plans.filters.AbstractPersonFilter;
+
+/**
+ * @author mrieser
+ */
+public class PersonLeavesAreaFilter extends AbstractPersonFilter {
+
+	private final Map<Id, Link> areaOfInterest;
+
+	public PersonLeavesAreaFilter(final PersonAlgorithmI nextAlgorithm, final Map<Id, Link> areaOfInterest) {
+		this.nextAlgorithm = nextAlgorithm;
+		this.areaOfInterest = areaOfInterest;
+	}
+
+	@Override
+	public boolean judge(final Person person) {
+		List<Plan> plans = person.getPlans();
+		for (Plan plan : plans) {
+			for (int i = 1, n = plan.getActsLegs().size(); i < n; i+=2) {
+				Leg leg = (Leg) plan.getActsLegs().get(i);
+				if (leg.getRoute() == null) {
+					return false;
+				}
+				Link[] links = leg.getRoute().getLinkRoute();
+				for (Link link : links) {
+					if (!this.areaOfInterest.containsKey(link.getId())) return true;
+				}
+				// test departure link
+				Link link = ((Act) plan.getActsLegs().get(i-1)).getLink();
+				if (link != null) {
+					if (!this.areaOfInterest.containsKey(link.getId())) return true;
+				}
+				// test arrival link
+				link = ((Act) plan.getActsLegs().get(i+1)).getLink();
+				if (link != null) {
+					if (!this.areaOfInterest.containsKey(link.getId())) return true;
+				}
+			}
+		}
+		return false;
+	}
+
+}
---
+++ matsim/src/playground/marcel/ectm/planfilter/PlanFilter.java
@@ -0,0 +1,107 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * PlanFilter.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.marcel.ectm.planfilter;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.matsim.basic.v01.Id;
+import org.matsim.config.Config;
+import org.matsim.gbl.Gbl;
+import org.matsim.network.Link;
+import org.matsim.network.MatsimNetworkReader;
+import org.matsim.network.NetworkLayer;
+import org.matsim.network.Node;
+import org.matsim.plans.MatsimPlansReader;
+import org.matsim.plans.Plans;
+import org.matsim.plans.PlansReaderI;
+import org.matsim.plans.algorithms.PersonIdRecorder;
+import org.matsim.plans.filters.PersonIntersectAreaFilter;
+import org.matsim.utils.geometry.shared.Coord;
+import org.matsim.world.World;
+
+public class PlanFilter {
+	public static void subPopulation(final String[] args) {
+		System.out.println("RUN: subPopulation");
+		
+		final Coord center = new Coord(683518.0, 246836.0); // Bellevue, Zrh
+		double[] smallRadiuses = {5000, 7000, 9000};
+		double[] bigRadiuses = {10000, 11000, 12000, 13000, 14000, 15000, 16000, 17000, 18000, 19000, 20000, 25000, 30000, 40000, 50000};
+
+		final Config config = Gbl.createConfig(args);
+		final World world = Gbl.createWorld();
+
+		System.out.println("  reading the network... " + (new Date()));
+		NetworkLayer network = null;
+		network = (NetworkLayer)world.createLayer(NetworkLayer.LAYER_TYPE, null);
+		new MatsimNetworkReader(network).readFile(config.network().getInputFile());
+		System.out.println("  done.");
+
+		System.out.println("  reading population... " + (new Date()));
+		final Plans population = new Plans(Plans.NO_STREAMING);
+		PlansReaderI plansReader = new MatsimPlansReader(population);
+		plansReader.readFile(config.plans().getInputFile());
+
+		System.out.println("  finding sub-networks... " + (new Date()));
+		System.out.println("smallRadius\tbigRadius\t#linksSmall\t#linksBig\t#peopleSmall\t#peopleLeavingBig");
+		for (double smallRadius : smallRadiuses) {
+			for (double bigRadius : bigRadiuses) {
+
+				final Map<Id, Link> smallAOI = new HashMap<Id, Link>();
+				final Map<Id, Link> bigAOI = new HashMap<Id, Link>();
+
+				for (Link link : network.getLinks().values()) {
+					final Node from = link.getFromNode();
+					final Node to = link.getToNode();
+					if ((from.getCoord().calcDistance(center) <= smallRadius) || (to.getCoord().calcDistance(center) <= smallRadius)) {
+						smallAOI.put(link.getId(),link);
+					}
+				}
+//				System.out.println("  aoi with radius=" + smallRadius + " contains " + smallAOI.size() + " links.");
+		
+				for (Link link : network.getLinks().values()) {
+					final Node from = link.getFromNode();
+					final Node to = link.getToNode();
+					if ((from.getCoord().calcDistance(center) <= bigRadius) || (to.getCoord().calcDistance(center) <= bigRadius)) {
+						bigAOI.put(link.getId(),link);
+					}
+				}
+//				System.out.println("  aoi with radius=" + bigRadius + " contains " + bigAOI.size() + " links.");
+		
+				final PersonIdRecorder recorder = new PersonIdRecorder();
+				final PersonLeavesAreaFilter outsideFilter = new PersonLeavesAreaFilter(recorder, bigAOI);
+				final PersonIntersectAreaFilter insideFilter = new PersonIntersectAreaFilter(outsideFilter, smallAOI);
+				insideFilter.run(population);
+//				System.out.println("  persons travelling in small area: " + insideFilter.getCount());
+//				System.out.println("  persons leaving big area: " + outsideFilter.getCount());
+				System.out.println(smallRadius + "\t" + bigRadius + "\t" + smallAOI.size() + "\t" + bigAOI.size() + "\t" + insideFilter.getCount() + "\t" + outsideFilter.getCount());
+
+			}
+		}
+				
+		System.out.println("RUN: subPopulation finished");
+	}
+	
+	public static void main(final String[] args) {
+		subPopulation(args);
+	}
+}
