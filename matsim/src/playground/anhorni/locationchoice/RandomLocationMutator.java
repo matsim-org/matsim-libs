595bb599416c878fa75d1419bbe9ea1e8b2e0fbe
andreas77 <andreashorni@hotmail.com>
2008-07-07 13:16:44 +0000

    only using zh locs for loc mod.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@2219 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/anhorni/locationchoice/RandomLocationMutator.java
+++ matsim/src/playground/anhorni/locationchoice/RandomLocationMutator.java
@@ -14,6 +14,8 @@ import org.matsim.plans.Person;
 import org.matsim.plans.Plan;
 import org.matsim.plans.algorithms.PersonAlgorithm;
 import org.matsim.plans.algorithms.PlanAlgorithmI;
+import org.matsim.utils.collections.QuadTree;
+import org.matsim.utils.geometry.shared.Coord;
 
 
 public class RandomLocationMutator extends PersonAlgorithm implements PlanAlgorithmI {
@@ -23,6 +25,11 @@ public class RandomLocationMutator extends PersonAlgorithm implements PlanAlgori
 	private final Facilities facilities = (Facilities)Gbl.getWorld().getLayer(Facilities.LAYER_TYPE);
 	private final TreeMap<Id,Facility> shop_facilities=new TreeMap<Id,Facility>();
 	private final TreeMap<Id,Facility> leisure_facilities=new TreeMap<Id,Facility>();
+	private QuadTree<Facility> shopFacQuadTree = null;
+	private QuadTree<Facility> leisFacQuadTree = null;
+	
+	ArrayList<Facility> zhShopFacilities = null;
+	ArrayList<Facility> zhLeisureFacilities = null;
 	// ----------------------------------------------------------
 
 	public RandomLocationMutator(final NetworkLayer network) {
@@ -44,29 +51,37 @@ public class RandomLocationMutator extends PersonAlgorithm implements PlanAlgori
 		this.leisure_facilities.putAll(this.facilities.getFacilities("leisure_gastro"));
 		this.leisure_facilities.putAll(this.facilities.getFacilities("leisure_culture"));
 		this.leisure_facilities.putAll(this.facilities.getFacilities("leisure_sports"));
+		
+		this.shopFacQuadTree=this.builFacQuadTree(this.shop_facilities);
+		this.leisFacQuadTree=this.builFacQuadTree(this.leisure_facilities);
+		
+		zhShopFacilities = (ArrayList<Facility>)this.shopFacQuadTree.get(683508.50, 246832.91, 30000);
+		zhLeisureFacilities = (ArrayList<Facility>)this.leisFacQuadTree.get(683508.50, 246832.91, 30000);
+		
 	}
 
 	// plan == selected plan
 	public void handlePlan(final Plan plan){
 
 		if (this.shop_facilities.size()>0) {
-			exchangeFacilities("s",this.shop_facilities, plan);
+			exchangeFacilities("s",this.zhShopFacilities, plan);
+			
 		}
 
 		if (this.leisure_facilities.size()>0) {
-			exchangeFacilities("l",this.leisure_facilities, plan);
+			exchangeFacilities("l",this.zhLeisureFacilities, plan);
 		}
 	}
 
 
-	public void exchangeFacilities(final String type, TreeMap<Id,Facility>  exchange_facilities, final Plan plan) {
+	public void exchangeFacilities(final String type, ArrayList<Facility>  exchange_facilities, final Plan plan) {
 		
 		final ArrayList<?> actslegs = plan.getActsLegs();
 		for (int j = 0; j < actslegs.size(); j=j+2) {
 			final Act act = (Act)actslegs.get(j);
 			if (act.getType().startsWith(type)) {
 				
-				final Facility facility=(Facility)exchange_facilities.values().toArray()[
+				final Facility facility=(Facility)exchange_facilities.toArray()[
 				           Gbl.random.nextInt(exchange_facilities.size()-1)];
 				// plans: link, coords
 				// facilities: coords
@@ -97,5 +112,32 @@ public class RandomLocationMutator extends PersonAlgorithm implements PlanAlgori
 
 	public void run(final Plan plan) {
 		handlePlan(plan);
+	}
+	
+	private QuadTree<Facility> builFacQuadTree(TreeMap<Id,Facility> facilities_of_type) {
+		Gbl.startMeasurement();
+		System.out.println("      building facility quad tree...");
+		double minx = Double.POSITIVE_INFINITY;
+		double miny = Double.POSITIVE_INFINITY;
+		double maxx = Double.NEGATIVE_INFINITY;
+		double maxy = Double.NEGATIVE_INFINITY;
+		for (final Facility f : facilities_of_type.values()) {
+			if (f.getCenter().getX() < minx) { minx = f.getCenter().getX(); }
+			if (f.getCenter().getY() < miny) { miny = f.getCenter().getY(); }
+			if (f.getCenter().getX() > maxx) { maxx = f.getCenter().getX(); }
+			if (f.getCenter().getY() > maxy) { maxy = f.getCenter().getY(); }
+		}
+		minx -= 1.0;
+		miny -= 1.0;
+		maxx += 1.0;
+		maxy += 1.0;
+		System.out.println("        xrange(" + minx + "," + maxx + "); yrange(" + miny + "," + maxy + ")");
+		QuadTree<Facility> quadtree = new QuadTree<Facility>(minx, miny, maxx, maxy);
+		for (final Facility f : facilities_of_type.values()) {
+			quadtree.put(f.getCenter().getX(),f.getCenter().getY(),f);
+		}
+		System.out.println("      done.");
+		Gbl.printRoundTime();
+		return quadtree;
 	}
 }
