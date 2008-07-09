41f37cc2079aeeb06642835221dbfcde9c128923
mrieser <rieser@gmail.com>
2008-07-09 22:01:38 +0000

    solved a potential problem with Math.abs() returning negative values, replaced the usage of ConcurrentLinkedQueue with LinkedList and Vector with ArrayList. ConcurrentLinkedQueue and Vector are both syncronized, but the objects were all allocated and used within one method, so there is no need for synchronization. some other code cleanup.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@2292 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/network/algorithms/NetworkSegmentDoubleLinks.java
+++ matsim/src/org/matsim/network/algorithms/NetworkSegmentDoubleLinks.java
@@ -20,10 +20,12 @@
 
 package org.matsim.network.algorithms;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Random;
-import java.util.Vector;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.apache.log4j.Logger;
 import org.matsim.basic.v01.Id;
@@ -31,74 +33,65 @@ import org.matsim.basic.v01.IdImpl;
 import org.matsim.network.Link;
 import org.matsim.network.NetworkLayer;
 import org.matsim.network.Node;
+import org.matsim.utils.misc.Time;
 
 
 /**
- * This algorithm handles double links (two links with same from and to node) by splitting 
+ * This algorithm handles double links (two links with same from and to node) by splitting
  * one of the link and adding an additional node. This is necessary, since the routes in MATSim
- * are node based.  
- * 
- * @author laemmel
+ * are node based.
  *
+ * @author laemmel
  */
 public class NetworkSegmentDoubleLinks {
 	private static final Logger log = Logger.getLogger(NetworkSegmentDoubleLinks.class);
 
-	
-	private NetworkLayer network; 
-	
+	private NetworkLayer network;
+
 	private int dblLinks = 0;
-	private int trblLinks = 0;
-	private int qtblLinks = 0;
-	private int ntblLinks = 0;
-	
+	private int trblLinks = 0; // what does trbl stand for?? please document! TODO [GL] documentation
+	private int qtblLinks = 0; // what does qtbl stand for?? please document!
+	private int ntblLinks = 0; // what does ntbl stand for?? pleaes document!
+
 	//////////////////////////////////////////////////////////////////////
 	// run methods
 	//////////////////////////////////////////////////////////////////////
-	
-	public void run(NetworkLayer network) {
+
+	public void run(final NetworkLayer network) {
 		this.network = network;
 		log.info("    running " + this.getClass().getName() + " algorithm...");
 
-		
-		ConcurrentLinkedQueue<Node> nodes = new ConcurrentLinkedQueue<Node>(network.getNodes().values());
+		Queue<Node> nodes = new LinkedList<Node>(network.getNodes().values());
 		while (nodes.peek() != null) {
 			Node n = nodes.poll();
-			HashMap<Id,Vector<Link>> toNodesMap = new HashMap<Id,Vector<Link>>();
+			HashMap<Id, List<Link>> toNodesMap = new HashMap<Id, List<Link>>();
 			for (Link l : n.getOutLinks().values()) {
-				Vector<Link> links = toNodesMap.get(l.getToNode().getId());
+				List<Link> links = toNodesMap.get(l.getToNode().getId());
 				if (links != null) {
 					links.add(l);
 				} else {
-					links = new Vector<Link>();
+					links = new ArrayList<Link>();
 					links.add(l);
 					toNodesMap.put(l.getToNode().getId(), links);
 				}
 			}
 			if (toNodesMap.size() > 0) {
-				
-				
-				
-
-				
-				handleDblLinks(toNodesMap); 
+				handleDblLinks(toNodesMap);
 			}
-			
 		}
-		
-		log.info("handled: "); 
+
+		log.info("handled: ");
 		log.info("\t" + this.dblLinks + " dblLinks.");
-		log.info("\t" + this.trblLinks + " trblLinks.");
+		log.info("\t" + this.trblLinks + " trblLinks."); // what does trbl stand for?? please document! TODO [GL] documentation
 		log.info("\t" + this.qtblLinks + " qtblLinks.");
 		log.info("\t" + this.ntblLinks + " ntblLinks.");
 		log.info("done.");
-
 	}
 
-	private void handleDblLinks(HashMap<Id, Vector<Link>> toNodesMap) {
-		for (Vector<Link> vec : toNodesMap.values()) {
+	private void handleDblLinks(HashMap<Id, List<Link>> toNodesMap) {
+		for (List<Link> vec : toNodesMap.values()) {
 			switch (vec.size()) {
-				case 1: 
+				case 1:
 					break;
 				case 2:
 					this.dblLinks++;
@@ -109,27 +102,24 @@ public class NetworkSegmentDoubleLinks {
 				case 4:
 					this.qtblLinks++;
 					break;
-				default: 
+				default:
 					this.ntblLinks++;
 			}
-			
+
 			for (int i = 1; i < vec.size(); i++) {
 				splitLink(vec.get(i));
 			}
-			
 		}
-		
 	}
-	
-	
+
 	private void splitLink(Link link) {
 		this.network.removeLink(link);
 		String from = link.getFromNode().getId().toString();
 		String to = link.getToNode().getId().toString();
 		String length = Double.toString(link.getLength()/2);
-		String freespeed = Double.toString(link.getFreespeed(org.matsim.utils.misc.Time.UNDEFINED_TIME));
-		String capacity = Double.toString(link.getCapacity(org.matsim.utils.misc.Time.UNDEFINED_TIME));
-		String permlanes = Double.toString(link.getLanes(org.matsim.utils.misc.Time.UNDEFINED_TIME));
+		String freespeed = Double.toString(link.getFreespeed(Time.UNDEFINED_TIME));
+		String capacity = Double.toString(link.getCapacity(Time.UNDEFINED_TIME));
+		String permlanes = Double.toString(link.getLanes(Time.UNDEFINED_TIME));
 		String type = null;
 		String median = this.network.createNode(getNewNodeId().toString(), Double.toString(link.getCenter().getX()), Double.toString(link.getCenter().getY()), null).getId().toString();
 		String l1Id = link.getId().toString();
@@ -140,20 +130,20 @@ public class NetworkSegmentDoubleLinks {
 
 	private Id getNewLinkId() {
 		Random r = new Random();
-		Id id = new IdImpl(Math.abs(r.nextInt()));
+		Id id = new IdImpl(r.nextInt(Integer.MAX_VALUE));
 		while (this.network.getLink(id) != null) {
-			id = new IdImpl(Math.abs(r.nextInt()));
+			id = new IdImpl(r.nextInt(Integer.MAX_VALUE));
 		}
-		return id;		
+		return id;
 	}
 
 	private Id getNewNodeId() {
 		Random r = new Random();
-		Id id = new IdImpl(Math.abs(r.nextInt()));
+		Id id = new IdImpl(r.nextInt(Integer.MAX_VALUE));
 		while (this.network.getNode(id) != null) {
-			id = new IdImpl(Math.abs(r.nextInt()));
+			id = new IdImpl(r.nextInt(Integer.MAX_VALUE));
 		}
-		return id;		
+		return id;
 	}
-	
+
 }
