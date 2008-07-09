0d8a998c947103ba669c6d989188fe41e9970c42
jkhackney <jkhackney@users.sf.net>
2008-07-09 11:55:35 +0000

    CompareActs contains methods to find time windows of overlapping presence and other such comparisons of Acts.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@2264 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/org/matsim/socialnetworks/algorithms/CompareActs.java
@@ -0,0 +1,55 @@
+package org.matsim.socialnetworks.algorithms;
+
+import org.matsim.plans.Act;
+
+public class CompareActs {
+
+	/**
+	 * If parts of the acts take place at the same time
+	 * 
+	 * @param act1
+	 * @param act2
+	 * @return
+	 */
+	public static boolean overlapTime(Act act1, Act act2){
+		boolean overlap=false;
+			if(act2.getEndTime() >=act1.getStartTime() && act2.getStartTime()<=act1.getEndTime()){
+				overlap=true;
+		}
+		return overlap;
+	}
+	/**
+	 * If the acts take place at the same facility and overlap in time
+	 * 
+	 * @param act1
+	 * @param act2
+	 * @return
+	 */
+	public static boolean overlapTimePlace(Act act1, Act act2){
+		boolean overlap=false;
+		if(act2.getFacility().equals(act1.getFacility())){
+			if(act2.getEndTime() >=act1.getStartTime() && act2.getStartTime()<=act1.getEndTime()){
+				overlap=true;
+			}
+		}
+		return overlap;
+	}
+	/**
+	 * If the acts take place at the same facility and overlap in time
+	 * and are the same type
+	 * 
+	 * @param act1
+	 * @param act2
+	 * @return
+	 */
+	public static boolean overlapTimePlaceType(Act act1, Act act2){
+//		System.out.println("Checking overlap "+act1.getType()+" "+act1.getFacility().getId()+": "+act2.getType()+" "+act2.getFacility().getId());
+		boolean overlap=false;
+		if(act2.getFacility().getActivity(act2.getType()).equals(act1.getFacility().getActivity(act1.getType()))){
+			if(act2.getEndTime() >=act1.getStartTime() && act2.getStartTime()<=act1.getEndTime()){
+				overlap=true;
+			}
+		}
+		return overlap;
+	}
+}
