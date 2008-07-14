e57a8b4c8440e59d70a5af4046d2ec849cdbcb4a
laemmel <gregor.laemmel@gmail.com>
2008-07-14 16:49:18 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@2370 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/gregor/gis/excluder/PlanExcluder.java
@@ -0,0 +1,150 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * PlanExcluder.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.gregor.gis.excluder;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.log4j.Logger;
+import org.geotools.data.FeatureSource;
+import org.geotools.feature.Feature;
+import org.geotools.feature.FeatureIterator;
+import org.matsim.gbl.Gbl;
+import org.matsim.network.MatsimNetworkReader;
+import org.matsim.network.NetworkLayer;
+import org.matsim.plans.MatsimPlansReader;
+import org.matsim.plans.Person;
+import org.matsim.plans.Plans;
+import org.matsim.plans.PlansWriter;
+import org.matsim.utils.geometry.CoordI;
+import org.matsim.utils.geometry.geotools.MGC;
+import org.matsim.utils.gis.ShapeFileReader;
+
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
+
+public class PlanExcluder {
+
+	private static final Logger log = Logger.getLogger(PlanExcluder.class);
+	private final NetworkLayer network;
+	private final Plans plans;
+	private final Collection<Polygon> ps;
+
+	
+	
+	
+	
+	
+	
+	
+	public PlanExcluder(NetworkLayer network, Plans population,
+			Collection<Polygon> ps) {
+		this.network = network;
+		this.plans = population;
+		this.ps = ps;
+	}
+
+
+
+	public Plans run() {
+		
+		Plans plans = new Plans();
+		for (Person person : this.plans.getPersons().values()) {
+			
+			CoordI c = person.getSelectedPlan().getFirstActivity().getCoord();
+			Point p  = MGC.coord2Point(c);
+			
+			for (Polygon po : this.ps) {
+				if (!po.contains(p)) {
+					try {
+						plans.addPerson(person);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			
+		}
+		
+		return plans;
+		
+	}
+	
+
+	public static void main(String [] args) {
+		
+
+		final String links = "./padang/extract_population/excludes.shp";
+		final String config = "./configs/timeVariantEvac.xml";
+		
+		FeatureSource l = null;
+		try {
+			l = ShapeFileReader.readDataFile(links);
+		} catch (final Exception e) {
+			e.printStackTrace();
+		}
+		final Collection<Polygon> ls = getPolygons(l);
+		
+		Gbl.createConfig(new String [] {config});
+		Gbl.createWorld();
+		
+		
+		final NetworkLayer network = new NetworkLayer();
+		new MatsimNetworkReader(network).readFile(Gbl.getConfig().network().getInputFile());
+		Gbl.getWorld().setNetworkLayer(network);
+		
+		
+		final Plans population = new Plans();
+		new MatsimPlansReader(population).readFile(Gbl.getConfig().plans().getInputFile());
+		
+		Plans toSave = new PlanExcluder(network,population,ls).run();
+		new PlansWriter(toSave,"padang_plans_v20080618_reduced.xml.gz", "v4").write();
+	}
+	
+	
+	
+	
+	private static Collection<Polygon> getPolygons(final FeatureSource n) {
+		final Collection<Polygon> polygons = new ArrayList<Polygon>();
+		FeatureIterator it = null;
+		try {
+			it = n.getFeatures().features();
+		} catch (final IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		while (it.hasNext()) {
+			final Feature feature = it.next();
+//			int id = (Integer) feature.getAttribute(1);
+			MultiPolygon multiPolygon = (MultiPolygon) feature.getDefaultGeometry();
+			if (multiPolygon.getNumGeometries() > 1) {
+				log.warn("MultiPolygons with more then 1 Geometry ignored!");
+				continue;
+			}
+			Polygon polygon = (Polygon) multiPolygon.getGeometryN(0);
+			polygons.add(polygon);
+	}
+	
+		return polygons;
+	}
+}
---
+++ matsim/src/playground/gregor/gis/featurechanger/ChangeAttributeOrder.java
@@ -0,0 +1,68 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * ChangeAttributeOrder.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.gregor.gis.featurechanger;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.geotools.data.FeatureSource;
+import org.geotools.feature.Feature;
+import org.geotools.feature.IllegalAttributeException;
+import org.geotools.feature.SchemaException;
+import org.matsim.utils.gis.ShapeFileReader;
+import org.opengis.referencing.FactoryException;
+
+import playground.gregor.gis.utils.ShapeFileWriter;
+
+
+public class ChangeAttributeOrder {
+	
+	
+	
+	public static void main(String [] args) throws IOException, FactoryException, SchemaException, ArrayIndexOutOfBoundsException, IllegalAttributeException {
+		String file = "./padang/referencing/referenced10.shp";
+		int from = 2;
+		int to = 3;
+		FeatureSource fs = ShapeFileReader.readDataFile(file);
+		Collection<Feature> fc = processFeatures(fs,from,to);
+		ShapeFileWriter.writeGeometries(fc, file);
+		
+		
+	}
+
+	private static Collection<Feature> processFeatures(FeatureSource fs, int from, int to) throws IOException, ArrayIndexOutOfBoundsException, IllegalAttributeException {
+		Iterator it = fs.getFeatures().iterator();
+		Collection<Feature> fc = new ArrayList<Feature>();
+		while (it.hasNext()){
+			Feature ft = (Feature)it.next();
+			Object tmp = ft.getAttribute(from);
+			ft.setAttribute(from, ft.getAttribute(to));
+			ft.setAttribute(to, tmp);
+			fc.add(ft);
+		}
+		
+		
+		return fc;
+	}
+
+}
