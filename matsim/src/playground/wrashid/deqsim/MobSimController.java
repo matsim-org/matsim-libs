4c3ceaedf3542dc1481b1469b2f7e87d405d51e2
rashid_waraich <rashid_waraich@users.sf.net>
2008-07-17 15:43:11 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@2417 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/wrashid/deqsim/MobSimController.java
@@ -0,0 +1,48 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * Controler.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.wrashid.deqsim;
+
+import org.matsim.controler.Controler;
+import playground.wrashid.DES.utils.Timer;
+
+
+/**
+ * The Controler is responsible for complete simulation runs, including
+ * the initialization of all required data, running the iterations and
+ * the replanning, analyses, etc.
+ *
+ * @author mrieser
+ */
+public class MobSimController extends Controler {
+
+	public MobSimController(final String[] args) {
+	    super(args);
+	  }
+
+	public static void main(final String[] args) {
+		Timer t=new Timer();
+		t.startTimer();
+		final MobSimController controler = new MobSimController(args);
+		controler.run();
+		t.endTimer();
+		t.printMeasuredTime("Time needed for MobSimController run: ");
+	}
+}
