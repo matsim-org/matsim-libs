3b73fbe42fa92beaf410db48ed8209aed5384bcb
komeiste <komeiste@users.sf.net>
2008-07-22 15:09:32 +0000

    Initial commit of first planomat test case.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@2488 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/test/src/org/matsim/planomat/costestimators/DepartureDelayAverageCalculatorTest.java
@@ -0,0 +1,78 @@
+package org.matsim.planomat.costestimators;
+
+import org.matsim.basic.v01.IdImpl;
+import org.matsim.events.BasicEvent;
+import org.matsim.events.EventAgentDeparture;
+import org.matsim.events.EventLinkLeave;
+import org.matsim.events.Events;
+import org.matsim.gbl.Gbl;
+import org.matsim.network.NetworkLayer;
+import org.matsim.network.Node;
+import org.matsim.testcases.MatsimTestCase;
+import org.matsim.utils.geometry.shared.Coord;
+
+public class DepartureDelayAverageCalculatorTest extends MatsimTestCase {
+
+	private NetworkLayer network = null;
+	private final String linkId = "1";
+	
+	@Override
+	protected void setUp() throws Exception {
+
+		// we need a network with just one link
+		network = (NetworkLayer)Gbl.getWorld().createLayer(NetworkLayer.LAYER_TYPE, null);
+
+		double fromX = 100.0;
+		double fromY = 100.0;
+		double toX = 100.0;
+		double toY = 200.0;
+		Node fromNode = network.createNode(new IdImpl("1"), new Coord(fromX, fromY));
+		Node toNode = network.createNode(new IdImpl("2"), new Coord(toX, toY));
+		network.createLink(new IdImpl(linkId), fromNode, toNode, 999.9, 50.0 / 3.6, 1000, 1);
+		
+	}
+
+	public void testGetLinkDepartureDelay() {
+		
+		double depDelay = 0.0;
+
+		Events events = new Events();
+		int timeBinSize = ((int) (0.25 * 3600));
+		DepartureDelayAverageCalculator testee = new DepartureDelayAverageCalculator(network, timeBinSize);
+		events.addHandler(testee);
+		events.printEventHandlers();
+		
+		String dummyPersonId = "1";
+		
+		// this gives a delay of 36s
+		EventAgentDeparture depEvent = new EventAgentDeparture(6.01 * 3600, dummyPersonId, 0, linkId);
+		EventLinkLeave leaveEvent = new EventLinkLeave(6.02 * 3600, dummyPersonId, 0, linkId);
+		
+		for (BasicEvent event : new BasicEvent[]{depEvent, leaveEvent}) {
+			events.processEvent(event);
+		}
+		
+		depDelay = testee.getLinkDepartureDelay(network.getLink(new IdImpl("1")), 6.00 * 3600);
+		assertEquals(depDelay, 36.0);
+		
+		// let's add another delay of 72s, should result in an average of 54s
+		depEvent = new EventAgentDeparture(6.02 * 3600, dummyPersonId, 0, linkId);
+		leaveEvent = new EventLinkLeave(6.04 * 3600, dummyPersonId, 0, linkId);
+		
+		for (BasicEvent event : new BasicEvent[]{depEvent, leaveEvent}) {
+			events.processEvent(event);
+		}
+
+		depDelay = testee.getLinkDepartureDelay(network.getLink(new IdImpl("1")), 6.00 * 3600);
+		assertEquals(depDelay, 54.0);
+
+		// the time interval for the previously tested events was for departure times from 6.00 to 6.25
+		// for other time intervals, we don't have event information, so estimated delay should be 0s
+		depDelay = testee.getLinkDepartureDelay(network.getLink(new IdImpl("1")), 5.9 * 3600);
+		assertEquals(depDelay, 0.0);
+		depDelay = testee.getLinkDepartureDelay(network.getLink(new IdImpl("1")), 6.26 * 3600);
+		assertEquals(depDelay, 0.0);
+		
+	}
+	
+}
