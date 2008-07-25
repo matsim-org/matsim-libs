b3745b5fc622135ec3bb336898d828eaa275a8bb
mrieser <rieser@gmail.com>
2008-07-25 23:10:52 +0000

    added option to convert event files to mvi-files. code reorganization for cleaner structure.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@2535 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/run/OTFVis.java
+++ matsim/src/org/matsim/run/OTFVis.java
@@ -27,10 +27,13 @@ import org.matsim.config.MatsimConfigReader;
 import org.matsim.controler.ScenarioData;
 import org.matsim.events.Events;
 import org.matsim.gbl.Gbl;
+import org.matsim.mobsim.QueueNetworkLayer;
+import org.matsim.network.MatsimNetworkReader;
+import org.matsim.network.NetworkLayer;
+import org.matsim.utils.vis.otfvis.executables.OTFEvent2MVI;
 import org.matsim.utils.vis.otfvis.opengl.OnTheFlyClientFileQuad;
 import org.matsim.utils.vis.otfvis.opengl.OnTheFlyClientQuad;
 import org.matsim.utils.vis.otfvis.opengl.OnTheFlyQueueSimQuad;
-
 
 /**
  * A generic starter for the OnTheFly Visualizer that supports
@@ -46,11 +49,12 @@ public class OTFVis {
 		System.out.println("OTFVis");
 		System.out.println("Starts the MATSim OnTheFly-Visualizer.");
 		System.out.println();
-		System.out.println("usage 1: OTFStarter mvi-file");
-		System.out.println("usage 2: OTFStarter veh-file network-file");
-		System.out.println("usage 3: OTFStarter config-file");
+		System.out.println("usage 1: OTFVis mvi-file");
+		System.out.println("usage 2: OTFVis veh-file network-file");
+		System.out.println("usage 3: OTFVis config-file");
+		System.out.println("usage 4: OTFVis -convert event-file network-file mvi-file [snapshot-period]");
 		System.out.println();
-		System.out.println("Usages:");
+		System.out.println("Usages 1-3: Starts the Visualizer");
 		System.out.println("mvi-file:      A MATSim visualizer file that contains a pre-recorder state");
 		System.out.println("               to be visualized (*.mvi).");
 		System.out.println("veh-file:      A TRANSIMS vehicle file to be visualized (*.veh).");
@@ -58,6 +62,10 @@ public class OTFVis {
 		System.out.println("config-file:   A complete MATSim config file to run a simulation. In that case,");
 		System.out.println("               a QueueSimulation will be started and visualized in real-time, ");
 		System.out.println("               allowing to interactively query the state of single agents");
+		System.out.println();
+		System.out.println("Usage 4: Convert events into a mvi-file");
+		System.out.println("snapshot-period:  Optional. Specify how often a snapshot should be taken when");
+		System.out.println("                  reading the events, in seconds. Default: 600 seconds");
 		System.out.println();
 		System.out.println("----------------");
 		System.out.println("2008, matsim.org");
@@ -71,47 +79,71 @@ public class OTFVis {
 		}
 		String arg0l = args[0].toLowerCase();
 		if (arg0l.endsWith(".veh.gz") || arg0l.toLowerCase().endsWith(".veh")) {
-			// assume it's a transims file, then we should have the network as second argument
-			String vehFileName = args[0];
-			if (args.length < 2) {
-				printUsage();
-				return;
-			}
-			if (args[1].toLowerCase().endsWith(".xml") || args[1].toLowerCase().endsWith(".xml.gz")) {
-				String netFileName = args[1];
-				if (Gbl.getConfig() == null) Gbl.createConfig(null); // is this really required?
-				new OnTheFlyClientQuad("tveh:"+vehFileName + "@" + netFileName).run();
-				return;
-			}
+			playVEH(args);
 		} else if (arg0l.endsWith(".mvi")) {
-			// it's a otf-movie file
-			new OnTheFlyClientFileQuad(args[0]).run();
-			return;
+			playMVI(args);
 		} else if (arg0l.contains("config") && (arg0l.endsWith(".xml") || arg0l.endsWith(".xml.gz"))) {
-			// it's a config file
-			Config config = Gbl.createConfig(null);
-			if ((args.length > 1) && args[1].toLowerCase().endsWith(".dtd")) {
-				try {
-					new MatsimConfigReader(config).readFile(args[0], args[1]);
-				} catch (IOException e) {
-					e.printStackTrace();
-					return;
-				}
-			} else {
-				new MatsimConfigReader(config).readFile(args[0]);
-			}
-			ScenarioData data = new ScenarioData(config);
-			Events events = new Events();
-			OnTheFlyQueueSimQuad client = new OnTheFlyQueueSimQuad(data.getNetwork(), data.getPopulation(), events);
-			config.simulation().setSnapshotPeriod(0.0);
-			config.simulation().setSnapshotFormat("");
-			client.run();
-			return;
+			playConfig(args);
+		} else if (arg0l.equals("-convert")) {
+			convert(args);
 		} else {
+			printUsage();
+		}
+	}
+
+	public static final void playMVI(String[] args) {
+		new OnTheFlyClientFileQuad(args[0]).run();
+	}
+
+	public static final void playVEH(String[] args) {
+		// we should have the network as second argument
+		String vehFileName = args[0];
+		if (args.length < 2) {
 			printUsage();
 			return;
 		}
-
+		if (args[1].toLowerCase().endsWith(".xml") || args[1].toLowerCase().endsWith(".xml.gz")) {
+			String netFileName = args[1];
+			if (Gbl.getConfig() == null) Gbl.createConfig(null); // is this really required?
+			new OnTheFlyClientQuad("tveh:"+vehFileName + "@" + netFileName).run();
+		}
 	}
 
+	public static final void playConfig(String[] args) {
+		Config config = Gbl.createConfig(null);
+		if ((args.length > 1) && args[1].toLowerCase().endsWith(".dtd")) {
+			try {
+				new MatsimConfigReader(config).readFile(args[0], args[1]);
+			} catch (IOException e) {
+				e.printStackTrace();
+				return;
+			}
+		} else {
+			new MatsimConfigReader(config).readFile(args[0]);
+		}
+		ScenarioData data = new ScenarioData(config);
+		Events events = new Events();
+		OnTheFlyQueueSimQuad client = new OnTheFlyQueueSimQuad(data.getNetwork(), data.getPopulation(), events);
+		client.run();
+	}
+
+	public static final void convert(String[] args) {
+		if ((args.length < 4) || (args.length > 5)) {
+			printUsage();
+			return;
+		}
+		String eventFile = args[1];
+		String networkFile = args[2];
+		String mviFile = args[3];
+		int snapshotPeriod = 600;
+		if (args.length == 5) {
+			snapshotPeriod = Integer.parseInt(args[5]);
+		}
+
+		NetworkLayer net = new NetworkLayer();
+		new MatsimNetworkReader(net).readFile(networkFile);
+
+		OTFEvent2MVI converter = new OTFEvent2MVI(new QueueNetworkLayer(net), eventFile, mviFile, snapshotPeriod);
+		converter.convert();
+	}
 }
