b5db75bf0b3a6f2c196072bdc98e4720e10c7f8d
jkhackney <jkhackney@users.sf.net>
2008-07-29 13:51:25 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@2580 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/jhackney/postprocessing/AnalyzeScores.java
+++ matsim/src/playground/jhackney/postprocessing/AnalyzeScores.java
@@ -72,7 +72,7 @@ public class AnalyzeScores {
 
 		for(int i=0; i<501; i+=10){
 //			config.socnetmodule().setInitIter(Integer.toString(i));
-			config.socnetmodule().setInitIter(Integer.toString(500));
+			config.socnetmodule().setInitIter(Integer.toString(0));
 			Plans plans = Scenario.readPlans(i);
 			//read in social network
 			System.out.println(" Initializing the social network ...");
