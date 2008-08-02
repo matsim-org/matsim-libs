81cad1b7b87253c450ca13933b8ef724012a6cf7
rashid_waraich <rashid_waraich@users.sf.net>
2008-08-02 21:54:44 +0000

    Reimplementation of java.util.concurrent.ConcurrentLinkedQueue (improvement by more than 100% in terms of time efficiency).
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@2647 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/wrashid/PDES/util/ConcurrentList.java
+++ matsim/src/playground/wrashid/PDES/util/ConcurrentList.java
@@ -1,7 +1,10 @@
 package playground.wrashid.PDES.util;
 
 import java.util.LinkedList;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
+import playground.wrashid.DES.utils.Timer;
+// optimized for single producer, single consumer
 public class ConcurrentList<T> {
 	private LinkedList<T> inputBuffer=new LinkedList<T>();
 	private LinkedList<T> outputBuffer=new LinkedList<T>(); 
@@ -11,13 +14,15 @@ public class ConcurrentList<T> {
 		}
 	}
 	
+	// returns null, if empty, else the first element
 	public T remove(){
 		if (!outputBuffer.isEmpty()){
 			return outputBuffer.poll();
 		}
 		if (!inputBuffer.isEmpty()){
 			synchronized (inputBuffer){
-				LinkedList<T> tempList=new LinkedList<T>(); 
+				//swap buffers
+				LinkedList<T> tempList=null; 
 				tempList=inputBuffer;
 				inputBuffer=outputBuffer;
 				outputBuffer=tempList;
@@ -27,27 +32,137 @@ public class ConcurrentList<T> {
 		return null;
 	}
 	
+	
+	
+	// this experiment effectivly demonstrates, why reimplement ConcurrentList
+	// ------------
+	// consumed Items:10000
+	// time required for ConcurrentList (consumer): 27031
+	// ----------
+	// consumed Items:10000
+	// time required for ConcurrentLinkedQueue (consumer): 60953
+	// ----------
+	// This experiment was done with adding 10000000 elements but only 10000 consumed
+	// It shows, that ConcurrentList much better decouples the producer from the consumer
+	// especially, when the consumer is slower than the producer (which was simulated by the sleep(1)
+	
+
 	public static void main(String[] args){
-		ConcurrentList<Integer> cl=new ConcurrentList<Integer>();
-		for (int i=0;i<100;i++){
-			cl.add(i);
-		}
-		Integer i=cl.remove();
-		while (i!=null){
-			System.out.println(i);
-			i=cl.remove();
-		}
-		for (int j=0;j<100;j++){
-			cl.add(j);
-		}
-		i=cl.remove();
-		while (i!=null){
-			System.out.println(i);
-			i=cl.remove();
-		}
+		
+		// part 1
+		
+		ConcurrentList<Integer> cList=new ConcurrentList<Integer>();
+		Thread t=new Thread(new ARunnable(cList));
+		t.start();
+		t=new Thread(new BRunnable(cList));
+		t.start();
+		
+		/*
+		// part 2
+		ConcurrentLinkedQueue<Integer> cList=new ConcurrentLinkedQueue<Integer>();
+		Thread t=new Thread(new CRunnable(cList));
+		t.start();
+		t=new Thread(new DRunnable(cList));
+		t.start();
+		*/
+		
+		
 		
 		
 	}
+
+static class ARunnable implements Runnable{
+	ConcurrentList<Integer> cList=new ConcurrentList<Integer>();
+	@Override
+	public void run() {
+		Timer timer=new Timer();
+		timer.startTimer();
+		for (int i=0;i<10000000;i++){
+			cList.add(i);
+		}
+		timer.endTimer();
+		timer.printMeasuredTime("time required for ConcurrentList (producer): ");
+	}
+	public ARunnable(ConcurrentList<Integer> cList){
+		super();
+		this.cList=cList;
+	}
+}
+
+static class BRunnable implements Runnable{
+	ConcurrentList<Integer> cList=new ConcurrentList<Integer>();
+	@Override
+	public void run() {
+		Timer timer=new Timer();
+		timer.startTimer();
+		int count=0;
+		while (count<10000){
+			if (cList.remove()!=null){
+				count++;
+			}
+			try {
+				Thread.currentThread().sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println("consumed Items:" + count);
+		timer.endTimer();
+		timer.printMeasuredTime("time required for ConcurrentList (consumer): ");
+	}
+	public BRunnable(ConcurrentList<Integer> cList){
+		super();
+		this.cList=cList;
+	}
+}
+
+static class CRunnable implements Runnable{
+	ConcurrentLinkedQueue<Integer> cList=new ConcurrentLinkedQueue<Integer>();
+	@Override
+	public void run() {
+		Timer timer=new Timer();
+		timer.startTimer();
+		for (int i=0;i<10000000;i++){
+			cList.add(i);
+		}
+		timer.endTimer();
+		timer.printMeasuredTime("time required for ConcurrentLinkedQueue (producer): ");
+	}
+	public CRunnable(ConcurrentLinkedQueue<Integer> cList){
+		super();
+		this.cList=cList;
+	}
+}
+
+static class DRunnable implements Runnable{
+	ConcurrentLinkedQueue<Integer> cList=new ConcurrentLinkedQueue<Integer>();
+	@Override
+	public void run() {
+		Timer timer=new Timer();
+		timer.startTimer();
+		int count=0;
+		while (count<10000){
+			if (cList.poll()!=null){
+				count++;
+				try {
+					Thread.currentThread().sleep(1);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		System.out.println("consumed Items:" + count);
+		timer.endTimer();
+		timer.printMeasuredTime("time required for ConcurrentLinkedQueue (consumer): ");
+	}
+	public DRunnable(ConcurrentLinkedQueue<Integer> cList){
+		super();
+		this.cList=cList;
+	}
+}
 	
 	
 }
+
