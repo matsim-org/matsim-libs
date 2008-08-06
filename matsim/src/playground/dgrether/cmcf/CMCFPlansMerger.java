6713ac696be7dfac0630e0a839db84cb32e20fbe
grether <dominik.grether@gmail.com>
2008-08-06 15:20:53 +0000

    commited playground stuff
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@2682 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/dgrether/cmcf/CMCFPlansMerger.java
@@ -0,0 +1,74 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * KmlNetworkWriter.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.dgrether.cmcf;
+
+import org.apache.log4j.Logger;
+import org.matsim.basic.v01.Id;
+import org.matsim.basic.v01.IdImpl;
+import org.matsim.network.NetworkLayer;
+import org.matsim.plans.Leg;
+import org.matsim.plans.Person;
+import org.matsim.plans.Plan;
+import org.matsim.plans.Plans;
+
+import playground.dgrether.DgPaths;
+import playground.dgrether.utils.MatsimIo;
+
+
+/**
+ * @author dgrether
+ *
+ */
+public class CMCFPlansMerger {
+
+	private static final Logger log = Logger.getLogger(CMCFPlansMerger.class);
+	
+	private static final String plansFile = DgPaths.VSPSVNBASE + "studies/schweiz-ivtch/baseCase/plans/plans_miv_zrh30km_10pct.xml.gz";
+	
+	private static final String cmcfPlansFile = DgPaths.VSPSVNBASE + "studies/schweiz-ivtch/cmcf/plans/plans_miv_zrh30km_10pct_one_act_cmcf.xml";
+
+	private static final String outPlansFile = DgPaths.VSPSVNBASE + "studies/schweiz-ivtch/cmcf/plans/plans_miv_zrh30km_10pct_all_acts_cmcf.xml.gz";
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		NetworkLayer net = MatsimIo.loadNetwork(DgPaths.IVTCHNET);
+		Plans plansCmcf = MatsimIo.loadPlans(cmcfPlansFile);
+		Plans plans = MatsimIo.loadPlans(plansFile);
+		
+		for (Person person : plansCmcf.getPersons().values()) {
+			String idstring = person.getId().toString();
+			String[] idLegNumber = idstring.split("leg");
+			Id id = new IdImpl(idLegNumber[0]);
+			Person p = plans.getPerson(id);
+			Plan plan = p.getSelectedPlan();
+			int legNumber = Integer.parseInt(idLegNumber[1]);
+			legNumber = (legNumber * 2) + 1;
+			Leg leg = (Leg) plan.getActsLegs().get(legNumber);
+			leg.setRoute(person.getSelectedPlan().getNextLeg(person.getSelectedPlan().getFirstActivity()).getRoute());
+		}
+		
+		MatsimIo.writePlans(plans, outPlansFile);
+		log.info("done");
+		
+	}
+
+}
