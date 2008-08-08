905d42058f8c5ac8d6f529a09cdd4e7160c7d617
mrieser <rieser@gmail.com>
2008-08-08 12:35:59 +0000

    added comments and a new constructor.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@2719 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/events/BasicEvent.java
+++ matsim/src/org/matsim/events/BasicEvent.java
@@ -26,21 +26,46 @@ import org.xml.sax.Attributes;
 
 public abstract class BasicEvent {
 
-	public double time;
-	public String agentId;
-	public transient Person agent;
+	public final double time;
+	public final String agentId;
+	public Person agent = null;
 
 	private static String timeString = null;
 	private static double timeCache = Time.UNDEFINED_TIME;
 
-	// supply one c'tor with reference to person and one without
-	BasicEvent(final double time, final String agentId, final Person agent) {this.time = time; this.agentId = agentId; this.agent = agent;}
-	BasicEvent(final double time, final String agentId) {this.time = time; this.agentId = agentId;}
+	BasicEvent(final double time, final Person agent) {
+		this.time = time;
+		this.agent = agent;
+		this.agentId = agent.getId().toString();
+	}
+
+	BasicEvent(final double time, final String agentId, final Person agent) {
+		this(time, agent);
+	}
+
+	BasicEvent(final double time, final String agentId) {
+		this.time = time;
+		this.agentId = agentId;
+	}
 
 	public abstract Attributes getAttributes();
+
+	/**
+	 * Returns a textual representation of this event used for writing the event to a text file.
+	 * The string <em>must</em> follow the following format:
+	 * <pre>time-stamp \t agentId \t legNumber \t linkId \t nodeId \t flag \t description</pre>
+	 */
 	@Override
 	public abstract String toString();
 
+	/**
+	 * Returns the passed time as Seconds, including a trailing tab-character.
+	 * Internally caches the returned result to speed up writing many events with the same time.
+	 * This may no longer be useful if times are switched to double.
+	 *
+	 * @param time
+	 * @return
+	 */
 	protected static String getTimeString(final double time) {
 		if (time != timeCache) {
 			timeCache = time;
