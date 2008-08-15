b246198ca235d483a331cb5558af6749066123e1
rashid_waraich <rashid_waraich@users.sf.net>
2008-08-15 05:55:28 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@2847 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/wrashid/deqsim/JavaPDEQSim2.java
+++ matsim/src/playground/wrashid/deqsim/JavaPDEQSim2.java
@@ -95,6 +95,10 @@ public class JavaPDEQSim2 {
 		SimulationParameters.xZoneDistance=(SimulationParameters.maxXCoodrinate-SimulationParameters.minXCoodrinate)/SimulationParameters.numberOfZones;
 		
 		
+		for (int i=1;i<=SimulationParameters.numberOfZones;i++){
+			System.out.println(i+"-th boundry:" + i*SimulationParameters.xZoneDistance);
+		}
+		
 		
 		// assign a zone to each road
 		for (Road r:Road.allRoads.values()){
