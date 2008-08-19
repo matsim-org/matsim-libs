71b8e1463859390cbcbc91090655b851337cd6d8
rashid_waraich <rashid_waraich@users.sf.net>
2008-08-19 16:57:54 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@2907 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/wrashid/deqsim/JavaPDEQSim2.java
+++ matsim/src/playground/wrashid/deqsim/JavaPDEQSim2.java
@@ -150,7 +150,9 @@ public class JavaPDEQSim2 {
 			sumOfBuckets+=bucketCount[i];
 			//System.out.println(bucketCount[i]);
 		}
-		double maxEventsPerBucket=sumOfBuckets/SimulationParameters.numberOfZones;
+		// because later, many bucket may get more than 'maxEventsPerBucket', we can get into the problem
+		// that there is not enough for the last few processors
+		double maxEventsPerBucket=sumOfBuckets/(SimulationParameters.numberOfZones+Runtime.getRuntime().availableProcessors()/6);
 		System.out.println("sumOfBuckets="+sumOfBuckets);
 		
 		
