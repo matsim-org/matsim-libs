de089aab2cea011fcf92742a94955c1e409cf24d
vsp_neumann <neumann@senozon.de>
2008-08-22 10:07:41 +0000

    - now sets the missing parameters
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@2989 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/andreas/intersection/dijkstra/NetworkWrapper.java
+++ matsim/src/playground/andreas/intersection/dijkstra/NetworkWrapper.java
@@ -4,11 +4,20 @@ import org.apache.log4j.Logger;
 import org.matsim.network.Link;
 import org.matsim.network.NetworkLayer;
 import org.matsim.network.Node;
+import org.matsim.utils.misc.Time;
 
 public class NetworkWrapper {
 	
 	final private static Logger log = Logger.getLogger(NetworkWrapper.class);	
-	
+		
+	/**
+	 * Converts a network to an inverted network. Inverted nodes are situated at the end of the real link. 
+	 * Inverted link attributes are copied from toLink of the real network, thus every inverted link actually
+	 * starts at the location of a real node. 
+	 * 
+	 * @param networkLayer The real network
+	 * @return The converted network
+	 */
 	public static NetworkLayer wrapNetwork(NetworkLayer networkLayer){		
 		
 		NetworkLayer wrappedNetwork = new NetworkLayer();		
@@ -16,16 +25,14 @@ public class NetworkWrapper {
 		int numberOfLinksGenerated = 0;
 		
 		for (Link link : networkLayer.getLinks().values()) {
-			// TODO [an] Coordinates and type not set
-			wrappedNetwork.createNode(link.getId().toString(), "0", "0", "type");
+			wrappedNetwork.createNode(link.getId().toString(), String.valueOf(link.getToNode().getCoord().getX()), String.valueOf(link.getToNode().getCoord().getY()), null);
 			numberOfNodesGenerated++;			
 		}
 		
 		for (Node node : networkLayer.getNodes().values()) {
 			for (Link inLink : node.getInLinks().values()) {
 				for (Link outLink : node.getOutLinks().values()) {
-					// TODO [an] FreeSpeed and some other parameters are not set
-					wrappedNetwork.createLink(String.valueOf(numberOfLinksGenerated), inLink.getId().toString(), outLink.getId().toString(), String.valueOf((inLink.getLength() + outLink.getLength()) / 2), "100", "1000", "1", String.valueOf(numberOfLinksGenerated), "type");
+					wrappedNetwork.createLink(String.valueOf(numberOfLinksGenerated), inLink.getId().toString(), outLink.getId().toString(), String.valueOf(outLink.getLength()), String.valueOf(outLink.getFreespeed(Time.UNDEFINED_TIME)), String.valueOf(outLink.getCapacity(Time.UNDEFINED_TIME)), String.valueOf(outLink.getLanes(Time.UNDEFINED_TIME)), String.valueOf(numberOfLinksGenerated), outLink.getType());
 					numberOfLinksGenerated++;
 				}
 			}
