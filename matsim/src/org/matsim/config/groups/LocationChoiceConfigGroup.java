f910a42a952a58ca84e200633b31ab7502650352
andreas77 <andreashorni@hotmail.com>
2008-08-25 15:07:36 +0000

    adding package location choice
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@3013 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/org/matsim/config/groups/LocationChoiceConfigGroup.java
@@ -0,0 +1,103 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * LocationChoiceConfigGroup.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.config.groups;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.matsim.config.Module;
+
+public class LocationChoiceConfigGroup extends Module {
+
+	public static final String GROUP_NAME = "locationchoice";
+	
+	// zh, ch
+	// TODO: Provide center coord + radius to define an area for 
+	// locatio choice. For the moment zh and ch scenario.
+	private static final String AREA = "area";
+	
+	// true; false
+	private static final String MODE = "constrained";
+
+	private String area = null;
+	private String mode = null;
+
+	public LocationChoiceConfigGroup() {
+		super(GROUP_NAME);
+	}
+
+	@Override
+	public String getValue(final String key) {
+		if (AREA.equals(key)) {
+			return getArea();
+		}
+		if (MODE.equals(key)) {
+			return getMode();
+		}
+		throw new IllegalArgumentException(key);
+	}
+
+	@Override
+	public void addParam(final String key, final String value) {
+		if (AREA.equals(key)) {
+			setArea(value);
+		} else if (MODE.equals(key)) {
+			setMode(value);
+		} else		
+		{
+			throw new IllegalArgumentException(key);
+		}
+	}
+
+	@Override
+	protected final TreeMap<String, String> getParams() {
+		TreeMap<String, String> map = new TreeMap<String, String>();
+		this.addNotNullParameterToMap(map, AREA);
+		this.addNotNullParameterToMap(map, MODE);
+		return map;
+	}
+
+	/* direct access */
+
+	public String getArea() {
+		return this.area;
+	}
+	public void setArea(final String area) {
+		this.area = area;
+	}
+	
+	public String getMode() {
+		return this.mode;
+	}
+	public void setMode(final String mode) {
+		this.area = mode;
+	}
+	
+	protected void addParameterToMap(final Map<String, String> map, final String paramName) {
+		String value = this.getValue(paramName);
+		if (!((value == null) || value.equalsIgnoreCase("null"))) {
+			map.put(paramName, value);
+		} else {
+			map.put(paramName, "null");
+		}
+	}
+	
+}
