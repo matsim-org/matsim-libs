95040ba609e9bbc497684f35b7514f0eeb72c7c8
Matthias_Feil <Matthias_Feil@users.sf.net>
2008-08-28 15:42:32 +0000

    just a test
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@3059 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/mfeil/Test.java
@@ -0,0 +1,15 @@
+package playground.mfeil;
+
+public class Test {
+
+	/**
+	 * This is just a short test
+	 * 
+	 * @param args: command call arguments
+	 * @author Matthias Feil
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello MATSim!");
+	}
+
+}
