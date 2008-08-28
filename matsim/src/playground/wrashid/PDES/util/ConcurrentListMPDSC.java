8de90c76e83b3e0a2772ab33a1fe3f447d52d16a
rashid_waraich <rashid_waraich@users.sf.net>
2008-08-28 11:07:22 +0000

    adapted for ZoneMessageQueue
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@3051 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/wrashid/PDES/util/ConcurrentListMPDSC.java
+++ matsim/src/playground/wrashid/PDES/util/ConcurrentListMPDSC.java
@@ -1,5 +1,6 @@
 package playground.wrashid.PDES.util;
 
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
@@ -12,10 +13,11 @@ public class ConcurrentListMPDSC<T> {
 	private LinkedList<T>[] inputBuffer;
 	private LinkedList<LinkedList<T>>[] outputBuffer;
 	private int maxInputPutListSize; 
-	private LinkedList<T> outputWorkingBuffer=null;
+	private LinkedList<T> outputWorkingBuffer=new LinkedList<T>();
 	
 	// producerId 0>=
 	public void add(T element,int producerId){
+		synchronized (inputBuffer[producerId]){
 			inputBuffer[producerId].add(element);
 			if (inputBuffer[producerId].size()>maxInputPutListSize){
 				synchronized(outputBuffer[producerId]){
@@ -23,9 +25,11 @@ public class ConcurrentListMPDSC<T> {
 				}
 				inputBuffer[producerId]=new LinkedList<T>();
 			}
+		}	
 	}
 	
 	// returns null, if empty, else the first element
+	/*
 	public T remove(){
 		if (outputWorkingBuffer!=null && !outputWorkingBuffer.isEmpty()){
 			return outputWorkingBuffer.poll();
@@ -42,6 +46,25 @@ public class ConcurrentListMPDSC<T> {
 		}
 		return null;
 	}
+	*/
+	
+	// only remove elements from outputWorkingBuffer and ignore new elements
+	// this means return null, when outputWorkingBuffer is empty
+	/*
+	public T nonCuncurrentRemove(){
+		if (outputWorkingBuffer!=null && !outputWorkingBuffer.isEmpty()){
+			return outputWorkingBuffer.poll();
+		}
+		return null;
+	}
+	*/
+	
+	public LinkedList <T> getCucurrencySafeElements(){
+		LinkedList<T> result=outputWorkingBuffer;
+		outputWorkingBuffer = new LinkedList<T>();
+		return result;
+	}
+	
 	
 	public ConcurrentListMPDSC (int numberOfProducers, int maxInputPutListSize){
 		inputBuffer=new LinkedList[numberOfProducers];
@@ -56,11 +79,26 @@ public class ConcurrentListMPDSC<T> {
 	
 	// this method should be invoked especially, when all producers have finished production
 	public void flushAllInputBuffers(){
+		LinkedList<LinkedList<T>> tempOutputBuffer;
 		for (int i=0;i<inputBuffer.length;i++){
 			synchronized (inputBuffer[i]){
 				synchronized (outputBuffer[i]){
 					outputBuffer[i].add(inputBuffer[i]);
 					inputBuffer[i]=new LinkedList<T>();
+				}
+			}
+			
+			synchronized (outputBuffer[i]){
+				tempOutputBuffer=outputBuffer[i];
+				outputBuffer[i]=new LinkedList<LinkedList<T>>();
+			}
+			
+
+			for (int j=0;j<tempOutputBuffer.size();j++){
+				if (!tempOutputBuffer.peek().isEmpty()){
+					outputWorkingBuffer.addAll(tempOutputBuffer.poll());
+				} else {
+					tempOutputBuffer.removeFirst();
 				}
 			}
 		}
