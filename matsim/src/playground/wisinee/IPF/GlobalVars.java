c0fb20fda617430f6ee5149af43bf336c69a50c3
wisineew <wisineew@users.sf.net>
2008-09-10 10:51:24 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@3175 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/wisinee/IPF/GlobalVars.java
@@ -0,0 +1,18 @@
+package globalVariables;
+import java.util.ArrayList;
+
+public class GlobalVars {
+//	input and output variables for matrix 1
+	public static double[] fixedR; // fixed row	
+	public static double[] fixedC; // fixed column	
+	public static double[][] intialRij;	//initial matrix	
+	public static double[][] finalRij; //final matrix
+//	input and output variables for matrix 2	
+	public static double[] fixedR1; // fixed row
+	public static double[] fixedC1; // fixed column
+	public static double[][] intialRij1;	//initial matrix
+	public static double[][] finalRij1; //final matrix
+	
+	public static ArrayList<OriginalData> orgn; //original data from Insee
+	public static String[] finalData; //final data to write to new file
+}
