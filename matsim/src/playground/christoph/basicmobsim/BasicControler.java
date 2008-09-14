2f031fb6ade3e3938da828f03b8adffc6725a3b3
skull5 <dobler@senozon.com>
2008-09-14 16:53:15 +0000

    BasicMobSim - für das Wissensmodell "kein Wissen", d.h. an jeder Kreuzung wird der Weg zufällig gewählt.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@3210 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/christoph/basicmobsim/BasicControler.java
@@ -0,0 +1,97 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * EventControler.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.christoph.basicmobsim;
+
+
+import java.util.Iterator;
+import java.util.Map;
+
+import org.matsim.basic.v01.Id;
+import org.matsim.controler.Controler;
+import org.matsim.network.Link;
+import org.matsim.router.costcalculators.TravelTimeDistanceCostCalculator;
+
+import playground.christoph.events.KnowledgeTravelTimeCalculator;
+import playground.christoph.events.algorithms.KnowledgeReplanner;
+import playground.christoph.mobsim.ReplanningQueueSimulation;
+
+
+
+/**
+ * The Controler is responsible for complete simulation runs, including
+ * the initialization of all required data, running the iterations and
+ * the replanning, analyses, etc.
+ *
+ * @author mrieser
+ */
+public class BasicControler extends Controler{
+
+
+	protected BasicSimulation sim;
+	protected TravelTimeDistanceCostCalculator travelCostCalculator;
+	protected KnowledgeTravelTimeCalculator travelTimeCalculator;
+	
+	private static final String FILENAME_EVENTS = "events.txt.gz";
+
+	/**
+	 * Initializes a new instance of Controler with the given arguments.
+	 *
+	 * @param args The arguments to initialize the controler with. <code>args[0]</code> is expected to
+	 * 		contain the path to a configuration file, <code>args[1]</code>, if set, is expected to contain
+	 * 		the path to a local copy of the DTD file used in the configuration file.
+	 */
+	public BasicControler(String[] args)
+	{
+		super(args);
+	}
+
+	// Workaround!
+	protected void setup() {
+				
+		super.setup();
+	}
+	
+	protected void runMobSim() 
+	{
+		sim = new BasicSimulation(this.network, this.population, this.events);
+		
+		sim.run();
+	}
+	
+	/* ===================================================================
+	 * main
+	 * =================================================================== */
+
+	public static void main(final String[] args) {
+		if ((args == null) || (args.length == 0)) {
+			System.out.println("No argument given!");
+			System.out.println("Usage: Controler config-file [dtd-file]");
+			System.out.println();
+		} else {
+			final BasicControler controler = new BasicControler(args);			
+			controler.setOverwriteFiles(true);
+			controler.run();
+		}
+		System.exit(0);
+	}
+
+	
+}
---
+++ matsim/src/playground/christoph/basicmobsim/BasicSimulation.java
@@ -0,0 +1,199 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * BasicSimulation.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+/**
+ * Basisimplementierung einer Verkehrssimulation.
+ * Die Simulation ist zeitunabhängig, die Agenten haben keinen Einfluss auf das 
+ * Verkehrsnetz und somit auch nicht aufeinander.
+ * 
+ * Die Datenstrukturen bzw. Klassen (Personen, Netzwerk, Pläne, usw.) werden von
+ * MATSim übernommen.
+ */
+
+package playground.christoph.basicmobsim;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Random;
+
+import org.apache.log4j.Logger;
+import org.matsim.config.Config;
+import org.matsim.events.Events;
+import org.matsim.gbl.Gbl;
+import org.matsim.mobsim.queuesim.Simulation;
+import org.matsim.network.Link;
+import org.matsim.network.NetworkLayer;
+import org.matsim.network.Node;
+import org.matsim.population.Act;
+import org.matsim.population.Leg;
+import org.matsim.population.Person;
+import org.matsim.population.Plan;
+import org.matsim.population.Population;
+
+public class BasicSimulation {
+	
+	private final Config config;
+	protected final Population plans;
+	protected NetworkLayer networkLayer;
+
+	protected static Events events = null; // TODO [MR] instead of making this static and Links/Nodes using QueueSimulation.getEvents(), Gbl should hold a global events-object
+
+	/**
+	 * Includes all vehicle that have transportation modes unknown to
+	 * the QueueSimulation (i.e. != "car") or have two activities on the same link
+ 	 */
+
+	final private static Logger log = Logger.getLogger(BasicSimulation.class);
+	
+	public BasicSimulation(final NetworkLayer network, final Population plans, final Events events) {
+		Simulation.reset();
+		this.config = Gbl.getConfig();
+		
+		setEvents(events);
+		this.plans = plans;
+
+		this.networkLayer = network;
+		
+	}
+
+	
+	protected void simulatePerson(Person person)
+	{
+		log.info("Simulating Person " + person.getId());
+		
+		Plan plan = person.getSelectedPlan();
+	
+		// Zufallsgenerator eindeutig mit der Id der Person initialisieren -> reproduzierbare Ergebnisse!
+		Random random = new Random( Long.valueOf(person.getId().toString()) );
+		
+		Iterator actIterator = plan.getIteratorAct();
+		Iterator legIterator = plan.getIteratorLeg();
+		
+		//while(actIterator.hasNext())
+		while(legIterator.hasNext())
+		{
+			ArrayList<Node> routeNodes = new ArrayList<Node>();
+			
+			Act act = (Act)actIterator.next();
+			
+			Leg leg = (Leg)legIterator.next();
+			
+			if (leg == null)
+			{
+				log.error("Leg is null!");
+				System.exit(0);
+			}
+			
+			Act nextAct = plan.getNextActivity(leg);
+			
+			// Ziellink holen
+			Link destinationLink = nextAct.getLink();
+			
+			Link currentLink = act.getLink();
+			Node currentNode = currentLink.getToNode();
+			
+			// solange wir das Ziel nicht erreicht habenMap		
+			while(!currentLink.equals(destinationLink))
+			{				
+				// gewählten Link speichern, um ihn später im Plan hinterlegen zu können
+				routeNodes.add(currentLink.getToNode());
+
+				log.info("Current Link: " + currentLink.getId() + " Destination Link: " + destinationLink.getId());
+				
+				Object[] links = currentNode.getOutLinks().values().toArray();
+				int linkCount = links.length;
+							
+				if (linkCount == 0)
+				{
+					log.error("Node has no outgoing links - Stopped search!");
+					break;
+				}
+				
+				/* Hier nicht nötig - alle Links sind gleich wahscheinlich. Die Verteilung
+				 * erfolgt über die Random Funktion - diese sollte gleichverteile Zufallswerte
+				 * generieren.
+				 * 
+				// Wahrscheinlichkeit für die ausgehenden Links berechnen
+				double probabilty = 1/linkCount;
+				*/
+				
+				// Link wählen
+				int nextLink = random.nextInt(linkCount);
+				
+				// den gewählten Link zum neuen CurrentLink machen
+				if(links[nextLink] instanceof Link)
+				{
+					currentLink = (Link)links[nextLink];
+					currentNode = currentLink.getToNode();
+				}
+				else
+				{
+					log.error("Return object was not from type Link! Class " + links[nextLink] + " was returned!");
+					break;
+				}
+				
+				
+			}	// while(!currentLink.equals(destinationLink))
+			
+			// gefahrene Route im aktuellen Leg des Agenten hinterlegen
+			leg.getRoute().setRoute(routeNodes);
+			//Route newRoute = new Route();
+			//newRoute.setRoute(routeNodes);
+			//leg.setRoute(newRoute);
+						
+		}	// while(legIterator.hasNext())
+		
+	}
+	
+	
+	
+	public final void run() 
+	{
+		Collection<Person> persons = plans.getPersons().values();
+		Iterator personIterator = persons.iterator();
+		
+		while(personIterator.hasNext())
+		{
+			this.simulatePerson((Person)personIterator.next());
+		}
+
+		cleanupSim();
+	}
+
+
+	/**
+	 * Close any files, etc.
+	 */
+	protected void cleanupSim() 
+	{
+		BasicSimulation.events = null; // delete events object to free events handlers, if they are nowhere else referenced
+	}
+
+	public static final Events getEvents() {
+		return events;
+	}
+
+	private static final void setEvents(final Events events) {
+		BasicSimulation.events = events;
+	}
+
+
+}
