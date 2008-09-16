835001fcf75059bdadf717bc48d120d309e54a17
fabrice_marchal <fabrice_marchal@users.sf.net>
2008-09-16 20:05:25 +0000

    Compact version using Frobenius norm for error computation
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@3260 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/test/src/org/matsim/demandmodeling/primloc/PrimlocCalibrationErrorMatrix.java
+++ matsim/test/src/org/matsim/demandmodeling/primloc/PrimlocCalibrationErrorMatrix.java
@@ -14,21 +14,8 @@ public class PrimlocCalibrationErrorMatrix implements PrimlocCalibrationError {
 	public double error( Matrix trips, double[] rents ) {
 		// We assume than both reference and trips matrix are
 		// based on the same ranking of zones
-		int numZ = reference.getColumnDimension();
-		double sum=0.0;
-		for( int i=0;i<numZ;i++){
-			for( int j=0;j<numZ;j++){
-				double s = reference.get(i,j);
-				if( s>0.0 ){
-					double dt = reference.get(i, j)-trips.get(i, j);
-					sum += dt*dt;
-				}
-			}
-		}
-		return Math.sqrt(sum/(numZ*numZ));
 		
-		//TODO
-		// return trips.minus(reference).norm2 / normF ?
-		// check consistencyproblems of Cii
+		return trips.minus(reference).normF() ;
+		
 	}
 }
