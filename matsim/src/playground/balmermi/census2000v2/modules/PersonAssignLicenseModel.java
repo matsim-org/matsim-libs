64398c316d4f40559a0849185b5511a0e3e850aa
balmermi <balmermi@users.sf.net>
2008-09-24 09:58:07 +0000

    using license model v5
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@3353 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/balmermi/census2000v2/modules/PersonAssignLicenseModel.java
+++ matsim/src/playground/balmermi/census2000v2/modules/PersonAssignLicenseModel.java
@@ -31,7 +31,7 @@ import org.matsim.population.algorithms.PlanAlgorithm;
 
 import playground.balmermi.census2000v2.data.CAtts;
 import playground.balmermi.census2000v2.data.Household;
-import playground.balmermi.census2000v2.models.ModelLicenseOwnershipV4;
+import playground.balmermi.census2000v2.models.ModelLicenseOwnershipV5;
 
 public class PersonAssignLicenseModel extends AbstractPersonAlgorithm implements PlanAlgorithm {
 
@@ -49,7 +49,8 @@ public class PersonAssignLicenseModel extends AbstractPersonAlgorithm implements
 //	private final ModelLicenseOwnership model = new ModelLicenseOwnership();
 //	private final ModelLicenseOwnershipV2 model = new ModelLicenseOwnershipV2();
 //	private final ModelLicenseOwnershipV3 model = new ModelLicenseOwnershipV3();
-	private final ModelLicenseOwnershipV4 model = new ModelLicenseOwnershipV4();
+//	private final ModelLicenseOwnershipV4 model = new ModelLicenseOwnershipV4();
+	private final ModelLicenseOwnershipV5 model = new ModelLicenseOwnershipV5();
 
 	//////////////////////////////////////////////////////////////////////
 	// constructors
