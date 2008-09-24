a34c0b4b9a10d6ea0833e94a5bb8427e15b9a2fd
vsp_nagel <2008@kainagel.org>
2008-09-24 10:27:01 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@3357 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/kai/otfvis/OnTheFlyClientFileMvi.java
+++ matsim/src/playground/kai/otfvis/OnTheFlyClientFileMvi.java
@@ -6,11 +6,11 @@ public class OnTheFlyClientFileMvi
 {
 	public static void main(String[] args) 
 	{
-//		String mviFile = "/home/nagel/vsp-cvs/runs/run476/99.T.mvi" ;
+		String mviFile = "/home/nagel/vsp-cvs/runs/run476/99.T.mvi" ;
 //		String mviFile = "/home/nagel/vsp-cvs/runs/run469/200.mvi.gz" ;
 //		String mviFile = "/home/nagel/vsp-cvs/runs/run465/it.550/550.T.mvi" ;
 //		String mviFile = "/home/nagel/vsp-cvs/runs/run465/it.550/550.T.mvi" ;
-		String mviFile = "/home/nagel/vsp-cvs/runs/run582/it.500/500.events.mvi" ;
+//		String mviFile = "/home/nagel/vsp-cvs/runs/run582/it.500/500.events.mvi" ;
 
 		new OnTheFlyClientFileQuad(mviFile).run();
 	}
