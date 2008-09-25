a30b274ed3dda8206864d027197d3d04b69100e2
yu_vsp <yu_vsp@users.sf.net>
2008-09-25 09:50:14 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@3373 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/yu/utils/vis/MyOTFEvents2Mvi.java
+++ matsim/src/playground/yu/utils/vis/MyOTFEvents2Mvi.java
@@ -8,18 +8,33 @@ import org.matsim.mobsim.queuesim.QueueNetwork;
 import org.matsim.network.MatsimNetworkReader;
 import org.matsim.network.NetworkLayer;
 import org.matsim.utils.vis.otfvis.executables.OTFEvent2MVI;
-import org.matsim.world.World;
 
 /**
+ *quote from org.matsim.utils.vis.otfvis.executables.OTFEvent2MVI of David
+ * Strippgen
+ * 
  * @author yu
  * 
  */
 public class MyOTFEvents2Mvi {
+	private static void printUsage() {
+		System.out.println();
+		System.out.println("MyOTFEvents2Mvi:");
+		System.out.println("----------------");
+		System.out
+				.println("Create a Converter from eventsfile to .mvi-file with 4 parameters");
+		System.out.println();
+		System.out.println("usage: MyOTFEvents2Mvi args");
+		System.out.println(" args[0]: netFilename incl. path (.xml)(required)");
+		System.out
+				.println(" arg[1]: eventsFilename incl. path (.txt[.gz])(required)");
+		System.out
+				.println(" arg[2]: .mvi file incl. path (.mvi) to output (required)");
+		System.out.println(" arg[3]: time-interval[s](required)");
+		System.out.println("----------------");
+	}
 
 	/**
-	 * quote from org.matsim.utils.vis.otfvis.executables.OTFEvent2MVI of David
-	 * Strippgen
-	 * 
 	 * @param args
 	 *            [0] - netFilename;
 	 * @param args
@@ -30,13 +45,16 @@ public class MyOTFEvents2Mvi {
 	 *            [3] - time-interval_s;
 	 */
 	public static void main(String[] args) {
+		if (args.length < 3) {
+			printUsage();
+			System.exit(0);
+		}
 		Gbl.createConfig(null);
 		Gbl.startMeasurement();
-		World world = Gbl.createWorld();
 
 		NetworkLayer net = new NetworkLayer();
 		new MatsimNetworkReader(net).readFile(args[0]);
-		world.setNetworkLayer(net);
+		Gbl.createWorld().setNetworkLayer(net);
 
 		new OTFEvent2MVI(new QueueNetwork(net), args[1], args[2], Integer
 				.parseInt(args[3])).convert();
