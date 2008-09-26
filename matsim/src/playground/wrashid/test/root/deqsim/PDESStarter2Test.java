ba5b42889b0b8cb3cbb4ed60c00180a749e34740
rashid_waraich <rashid_waraich@users.sf.net>
2008-09-26 13:20:21 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@3394 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/wrashid/test/root/deqsim/PDESStarter2Test.java
+++ matsim/src/playground/wrashid/test/root/deqsim/PDESStarter2Test.java
@@ -36,88 +36,123 @@ public class PDESStarter2Test extends MatsimTestCase {
 	public void testScenarios() {
 		t_equilPlans1();
 		Gbl.reset();
-		
-		//t_equilEvent();
-		//Gbl.reset();
-		
-		//t_Berlin();
-		//Gbl.reset();
+
+		// t_equilEvent();
+		// Gbl.reset();
+
+		// t_Berlin();
+		// Gbl.reset();
 		// only comment this, when test stabelized again.
-		//assertEquals(true, false);
-	}
-	
-	
-	private void t_equilEvent() {
-		TestHandlerDetailedEventChecker orderChecker = new TestHandlerDetailedEventChecker();
-		orderChecker.startTestPDES2("test/scenarios/equil/config.xml",false,null,null);
-	}
-	
-	private void t_equilPlans1() {
-		TestHandlerDetailedEventChecker orderChecker= new TestHandlerDetailedEventChecker();
-		orderChecker.startTestPDES2("test/scenarios/equil/config.xml",true,"test/scenarios/equil/plans1.xml",new EquilPopulationPlans1Modified1());
-	
-		Gbl.reset();
-	
-		EquilPlans1EventCheckHandler eventChecker= new EquilPlans1EventCheckHandler();
-		eventChecker.startTestPDES2("test/scenarios/equil/config.xml",true,"test/scenarios/equil/plans1.xml",new EquilPopulationPlans1Modified1());
-	
-	}
-	
-	private void t_Berlin() {
-		TestHandlerDetailedEventChecker orderChecker = new TestHandlerDetailedEventChecker();
-		orderChecker.startTestPDES2("test/scenarios/berlin/config.xml",false,null,null);
+		// assertEquals(true, false);
 	}
 
-	
-	private class EquilPlans1EventCheckHandler extends TestHandlerDetailedEventChecker{
+	private void t_equilEvent() {
+		TestHandlerDetailedEventChecker orderChecker = new TestHandlerDetailedEventChecker();
+		orderChecker.startTestPDES2("test/scenarios/equil/config.xml", false,
+				null, null);
+	}
+
+	private void t_equilPlans1() {
+		TestHandlerDetailedEventChecker orderChecker = new TestHandlerDetailedEventChecker();
+		orderChecker.startTestPDES2("test/scenarios/equil/config.xml", true,
+				"test/scenarios/equil/plans1.xml",
+				new EquilPopulationPlans1Modified1());
+
+		Gbl.reset();
+
+		EquilPlans1EventCheckHandler eventChecker = new EquilPlans1EventCheckHandler();
+		eventChecker.startTestPDES2("test/scenarios/equil/config.xml", true,
+				"test/scenarios/equil/plans1.xml",
+				new EquilPopulationPlans1Modified1());
+
+	}
+
+	private void t_Berlin() {
+		TestHandlerDetailedEventChecker orderChecker = new TestHandlerDetailedEventChecker();
+		orderChecker.startTestPDES2("test/scenarios/berlin/config.xml", false,
+				null, null);
+	}
+
+	private class EquilPlans1EventCheckHandler extends
+			TestHandlerDetailedEventChecker {
 		public void checkAssertions() {
-			LinkedList<BasicEvent> list=events.get("1");
-			
+			LinkedList<BasicEvent> list = events.get("1");
+
 			assertEquals(true, list.get(0) instanceof AgentDepartureEvent);
-			
-			
+
 			assertEquals(true, list.get(1) instanceof LinkEnterEvent);
-			
+
 			assertEquals(true, list.get(2) instanceof LinkLeaveEvent);
-			
+
 			assertEquals(true, list.get(3) instanceof LinkEnterEvent);
-			
+
 			assertEquals(true, list.get(4) instanceof LinkLeaveEvent);
-			
+
 			assertEquals(true, list.get(5) instanceof LinkEnterEvent);
-			
+
 			assertEquals(true, list.get(6) instanceof LinkLeaveEvent);
-			
+
 			assertEquals(true, list.get(7) instanceof LinkEnterEvent);
-			
+
 			assertEquals(true, list.get(8) instanceof LinkLeaveEvent);
-			
+
 			assertEquals(true, list.get(3) instanceof LinkEnterEvent);
-			
+
 			assertEquals(true, list.get(4) instanceof LinkLeaveEvent);
-			
+
 			assertEquals(true, list.get(3) instanceof LinkEnterEvent);
-			
+
 			assertEquals(true, list.get(4) instanceof LinkLeaveEvent);
-			
+
 			assertEquals(true, list.get(3) instanceof LinkEnterEvent);
-			
+
 			assertEquals(true, list.get(4) instanceof LinkLeaveEvent);
-			
+
 			assertEquals(true, list.get(3) instanceof LinkEnterEvent);
-			
+
 			assertEquals(true, list.get(4) instanceof LinkLeaveEvent);
-			
+
 			assertEquals(true, list.get(3) instanceof LinkEnterEvent);
-			
+
 			assertEquals(true, list.get(4) instanceof LinkLeaveEvent);
-			
+
 			assertEquals(true, list.get(3) instanceof LinkEnterEvent);
-			
+
 			assertEquals(true, list.get(4) instanceof LinkLeaveEvent);
-			
+
 			assertEquals(true, list.get(21) instanceof AgentArrivalEvent);
+
+			/*
+			 * 
+			 * <module name="deqsim" > 
+			 * <param name="endTime" value="1000:00:00" />
+			 * <param name="flowCapacityFactor" value="1.0" /> 
+			 * <param name="squeezeTime" value="00:01:40" />
+			 * <param name="carSize" value="7.5" />
+			 * <param name="gapTravelSpeed" value="15.0" />
+			 * <param name="startTime" value="00:00:00" />
+			 * <param name="storageCapacityFactor" value="1.0" />
+			 * </module>
+			 */
+
+			/*
+			 * 
+			 * 
+			 * 
+			 * <?xml version="1.0" ?> <!DOCTYPE plans SYSTEM
+			 * "http://www.matsim.org/files/dtd/plans_v4.dtd"> <plans> <person
+			 * id="1"> <plan> <act type="h" x="-25000" y="0" link="1"
+			 * end_time="06:00" /> <leg mode="car" dep_time="00:06:00"> <route>2
+			 * 7 12</route> </leg> <act type="w" x="10000" y="0" link="20"
+			 * dur="00:10" /> <leg mode="car" dep_time="00:15:00"> <route>
+			 * </route> </leg> <act type="w" x="10000" y="0" link="20"
+			 * dur="03:30" /> <leg mode="car" dep_time="00:33:20"> <route>13 14
+			 * 15 1</route> </leg> <act type="h" x="-25000" y="0" link="1" />
+			 * </plan> </person> </plans>
+			 * 
+			 */
+
 		}
 	}
-	
+
 }
