260d8387150a3d5c6321b3230cab8c7c7b33a08b
rashid_waraich <rashid_waraich@users.sf.net>
2008-09-30 12:00:05 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@3424 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/wrashid/tryouts/starting/CppEventFileParser.java
@@ -0,0 +1,110 @@
+package playground.wrashid.tryouts.starting;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.StringTokenizer;
+import java.util.TimeZone;
+
+import playground.wrashid.DES.EventLog;
+import playground.wrashid.DES.SimulationParameters;
+
+import sun.security.jgss.TokenTracker;
+
+public class CppEventFileParser {
+
+	public static ArrayList<EventLog> eventLog=null;
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String eventFileName=args[0];
+		CppEventFileParser eventFileParser=new CppEventFileParser();
+		eventFileParser.parse(eventFileName);
+	}
+	
+	public void parse(String eventFileName){
+		
+		CppEventFileParser test = new CppEventFileParser();
+		eventLog = test.parseFile(eventFileName);
+		for(int i=0;i<eventLog.size();i++) {
+			//eventLog.get(i).print();
+		}
+	}
+
+	public ArrayList<EventLog> parseFile(String filePath) {
+		int counter=0;
+		ArrayList<EventLog> rows = new ArrayList<EventLog>();
+		try {
+			FileReader fr = new FileReader(filePath);
+			BufferedReader br = new BufferedReader(fr);
+			String line = null;
+			StringTokenizer tokenizer = null;
+			line = br.readLine();
+			while (line != null) {
+				counter++;
+				
+				if (SimulationParameters.debugMode && counter % 1000000==0){
+					System.out.println("noOfLines Read:" + counter);
+				}
+				
+				
+				tokenizer = new StringTokenizer(line);
+				String token = null;
+				token = tokenizer.nextToken();
+				double first = Double.parseDouble(token);
+				token = tokenizer.nextToken();
+				int second = Integer.parseInt(token);
+				token = tokenizer.nextToken();
+				int third = Integer.parseInt(token);
+				token = tokenizer.nextToken();
+				int fourth = Integer.parseInt(token);
+				token = tokenizer.nextToken();
+				int fifth = Integer.parseInt(token);
+				token = tokenizer.nextToken();
+				int sixth = Integer.parseInt(token);
+				token = tokenizer.nextToken();
+				String eventType = token;
+				
+				
+				// there is one eventType called 'enter net' => it is split into two tockens, so need to take that into account
+				if (tokenizer.hasMoreTokens()){
+					token = tokenizer.nextToken();
+					eventType += " " + token;
+				}
+				
+				
+				//change type label
+				if (eventType.equalsIgnoreCase("starting")){
+					eventType=SimulationParameters.START_LEG;
+				} else if (eventType.equalsIgnoreCase("end")){
+					eventType=SimulationParameters.END_LEG;
+				} else if (eventType.equalsIgnoreCase("enter")){
+					eventType=SimulationParameters.ENTER_LINK;
+				} else if (eventType.equalsIgnoreCase("leave")){
+					eventType=SimulationParameters.LEAVE_LINK;
+				}
+				
+
+				// ignore 'enter net' events (which seem useless)
+				if (!eventType.equalsIgnoreCase("enter net")){
+					EventLog eventLog=new EventLog(first,second,third,fourth,fifth,sixth,eventType);
+					rows.add(eventLog);
+				}
+				
+				line = br.readLine();
+			}
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+
+		return rows;
+	}
+
+}
---
+++ matsim/src/playground/wrashid/tryouts/starting/PCppEventFileParser.java
@@ -0,0 +1,33 @@
+package playground.wrashid.tryouts.starting;
+
+import java.util.ArrayList;
+
+import playground.wrashid.DES.EventLog;
+import playground.wrashid.DES.SimulationParameters;
+import playground.wrashid.deqsim.DEQSimStarter;
+
+public class PCppEventFileParser extends Thread {
+	Object lock;
+	String fileName;
+	public volatile boolean taskCompleted=false;
+	public void run() {
+		CppEventFileParser parser=new CppEventFileParser();
+		String[] args=new String[1];
+		args[0]=fileName;
+		CppEventFileParser.main(args);
+		
+		taskCompleted=true;
+		
+		synchronized (lock){
+			lock.notify();
+		}
+		
+		System.out.println("cpp event file read completed.");
+	}
+
+	public PCppEventFileParser(String fileName, Object lock) {
+		this.lock=lock;
+		this.fileName=fileName;
+	}
+
+}
