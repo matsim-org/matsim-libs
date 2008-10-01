773ceaf767f41840fe3057f16cc069bc32f2bcb1
balmermi <balmermi@users.sf.net>
2008-10-01 07:30:10 +0000

    create the dilZrh scenario for the new demand
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@3433 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/balmermi/census2000v2/DilutedZurichFilter.java
+++ matsim/src/playground/balmermi/census2000v2/DilutedZurichFilter.java
@@ -44,8 +44,6 @@ import org.matsim.population.PopulationReader;
 import org.matsim.population.PopulationWriter;
 import org.matsim.population.filters.PersonIntersectAreaFilter;
 import org.matsim.utils.geometry.CoordImpl;
-import org.matsim.world.MatsimWorldReader;
-import org.matsim.world.WorldWriter;
 import org.matsim.world.algorithms.WorldBottom2TopCompletion;
 import org.matsim.world.algorithms.WorldCheck;
 import org.matsim.world.algorithms.WorldValidation;
@@ -62,7 +60,7 @@ public class DilutedZurichFilter {
 	// createPopulation()
 	//////////////////////////////////////////////////////////////////////
 
-	public static void createIIDM() {
+	public static void filterDemand() {
 
 		log.info("MATSim-DB: create iidm.");
 
@@ -82,11 +80,6 @@ public class DilutedZurichFilter {
 
 		//////////////////////////////////////////////////////////////////////
 
-//		log.info("  reading world xml file...");
-//		final MatsimWorldReader worldReader = new MatsimWorldReader(Gbl.getWorld());
-//		worldReader.readFile(Gbl.getConfig().world().getInputFile());
-//		log.info("  done.");
-
 		log.info("  reading facilities xml file...");
 		Facilities facilities = (Facilities)Gbl.getWorld().createLayer(Facilities.LAYER_TYPE, null);
 		new MatsimFacilitiesReader(facilities).readFile(Gbl.getConfig().facilities().getInputFile());
@@ -101,27 +94,27 @@ public class DilutedZurichFilter {
 
 		//////////////////////////////////////////////////////////////////////
 		
-//		log.info("  running world modules... ");
-//		// the link types where no facility can be placed on
-//		// Here: for the ivtch network (nationales Netzmodell)
-//		Set<String> excludingLinkTypes = new HashSet<String>();
-//		excludingLinkTypes.add("0"); excludingLinkTypes.add("1"); excludingLinkTypes.add("2"); excludingLinkTypes.add("3");
-//		excludingLinkTypes.add("4"); excludingLinkTypes.add("5"); excludingLinkTypes.add("6"); excludingLinkTypes.add("7");
-//		excludingLinkTypes.add("8"); excludingLinkTypes.add("9");
-//		excludingLinkTypes.add("10"); excludingLinkTypes.add("11"); excludingLinkTypes.add("12"); excludingLinkTypes.add("13");
-//		excludingLinkTypes.add("14"); excludingLinkTypes.add("15"); excludingLinkTypes.add("16"); excludingLinkTypes.add("17");
-//		excludingLinkTypes.add("18"); excludingLinkTypes.add("19");
-//		excludingLinkTypes.add("20"); excludingLinkTypes.add("21"); excludingLinkTypes.add("22"); excludingLinkTypes.add("23");
-//		excludingLinkTypes.add("24"); excludingLinkTypes.add("25"); excludingLinkTypes.add("26"); excludingLinkTypes.add("27");
-//		excludingLinkTypes.add("28"); excludingLinkTypes.add("29");
-//		excludingLinkTypes.add("90"); excludingLinkTypes.add("91"); excludingLinkTypes.add("92"); excludingLinkTypes.add("93");
-//		excludingLinkTypes.add("94"); excludingLinkTypes.add("95"); excludingLinkTypes.add("96"); excludingLinkTypes.add("97");
-//		excludingLinkTypes.add("98"); excludingLinkTypes.add("99");
-//		new WorldCheck().run(Gbl.getWorld());
-//		new WorldBottom2TopCompletion(excludingLinkTypes).run(Gbl.getWorld());
-//		new WorldValidation().run(Gbl.getWorld());
-//		new WorldCheck().run(Gbl.getWorld());
-//		log.info("  done.");
+		log.info("  running world modules... ");
+		// the link types where no facility can be placed on
+		// Here: for the ivtch network (nationales Netzmodell)
+		Set<String> excludingLinkTypes = new HashSet<String>();
+		excludingLinkTypes.add("0"); excludingLinkTypes.add("1"); excludingLinkTypes.add("2"); excludingLinkTypes.add("3");
+		excludingLinkTypes.add("4"); excludingLinkTypes.add("5"); excludingLinkTypes.add("6"); excludingLinkTypes.add("7");
+		excludingLinkTypes.add("8"); excludingLinkTypes.add("9");
+		excludingLinkTypes.add("10"); excludingLinkTypes.add("11"); excludingLinkTypes.add("12"); excludingLinkTypes.add("13");
+		excludingLinkTypes.add("14"); excludingLinkTypes.add("15"); excludingLinkTypes.add("16"); excludingLinkTypes.add("17");
+		excludingLinkTypes.add("18"); excludingLinkTypes.add("19");
+		excludingLinkTypes.add("20"); excludingLinkTypes.add("21"); excludingLinkTypes.add("22"); excludingLinkTypes.add("23");
+		excludingLinkTypes.add("24"); excludingLinkTypes.add("25"); excludingLinkTypes.add("26"); excludingLinkTypes.add("27");
+		excludingLinkTypes.add("28"); excludingLinkTypes.add("29");
+		excludingLinkTypes.add("90"); excludingLinkTypes.add("91"); excludingLinkTypes.add("92"); excludingLinkTypes.add("93");
+		excludingLinkTypes.add("94"); excludingLinkTypes.add("95"); excludingLinkTypes.add("96"); excludingLinkTypes.add("97");
+		excludingLinkTypes.add("98"); excludingLinkTypes.add("99");
+		new WorldCheck().run(Gbl.getWorld());
+		new WorldBottom2TopCompletion(excludingLinkTypes).run(Gbl.getWorld());
+		new WorldValidation().run(Gbl.getWorld());
+		new WorldCheck().run(Gbl.getWorld());
+		log.info("  done.");
 		
 		//////////////////////////////////////////////////////////////////////
 
@@ -178,10 +171,6 @@ public class DilutedZurichFilter {
 		new FacilitiesWriter(facilities).write();
 		log.info("  done.");
 
-//		log.info("  writing world xml file... ");
-//		new WorldWriter(Gbl.getWorld()).write();
-//		log.info("  done.");
-
 		log.info("  writing config xml file... ");
 		new ConfigWriter(Gbl.getConfig()).write();
 		log.info("  done.");
@@ -200,7 +189,7 @@ public class DilutedZurichFilter {
 		Gbl.createConfig(args);
 		Gbl.createWorld();
 
-		createIIDM();
+		filterDemand();
 
 		Gbl.printElapsedTime();
 	}
