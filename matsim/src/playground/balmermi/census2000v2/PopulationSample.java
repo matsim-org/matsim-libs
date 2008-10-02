163746135fecb31877ac99fa38c2da726fd46d1b
balmermi <balmermi@users.sf.net>
2008-10-02 11:26:57 +0000

    a small main
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@3443 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/balmermi/census2000v2/PopulationSample.java
@@ -0,0 +1,140 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * PopulationCreation.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.balmermi.census2000v2;
+
+import org.apache.log4j.Logger;
+import org.matsim.config.ConfigWriter;
+import org.matsim.facilities.Facilities;
+import org.matsim.facilities.FacilitiesWriter;
+import org.matsim.facilities.MatsimFacilitiesReader;
+import org.matsim.gbl.Gbl;
+import org.matsim.network.MatsimNetworkReader;
+import org.matsim.network.NetworkLayer;
+import org.matsim.network.NetworkWriter;
+import org.matsim.population.MatsimPopulationReader;
+import org.matsim.population.Population;
+import org.matsim.population.PopulationReader;
+import org.matsim.population.PopulationWriter;
+
+public class PopulationSample {
+
+	//////////////////////////////////////////////////////////////////////
+	// member variables
+	//////////////////////////////////////////////////////////////////////
+
+	private final static Logger log = Logger.getLogger(PopulationSample.class);
+
+	//////////////////////////////////////////////////////////////////////
+	// createPopulation()
+	//////////////////////////////////////////////////////////////////////
+
+	public static void samplePopulation() {
+
+		log.info("samplePopulation...");
+
+		//////////////////////////////////////////////////////////////////////
+
+		log.info("  extracting input directory... ");
+		String indir = Gbl.getConfig().facilities().getInputFile();
+		indir = indir.substring(0,indir.lastIndexOf("/"));
+		log.info("    "+indir);
+		Gbl.printMemoryUsage();
+		log.info("  done.");
+
+		log.info("  extracting output directory... ");
+		String outdir = Gbl.getConfig().facilities().getOutputFile();
+		outdir = outdir.substring(0,outdir.lastIndexOf("/"));
+		log.info("    "+outdir);
+		Gbl.printMemoryUsage();
+		log.info("  done.");
+
+		//////////////////////////////////////////////////////////////////////
+
+		log.info("  reading facilities xml file...");
+		Facilities facilities = (Facilities)Gbl.getWorld().createLayer(Facilities.LAYER_TYPE, null);
+		new MatsimFacilitiesReader(facilities).readFile(Gbl.getConfig().facilities().getInputFile());
+		Gbl.getWorld().complete();
+		Gbl.printMemoryUsage();
+		log.info("  done.");
+
+		System.out.println("  reading the network xml file...");
+		NetworkLayer network = (NetworkLayer)Gbl.getWorld().createLayer(NetworkLayer.LAYER_TYPE,null);
+		new MatsimNetworkReader(network).readFile(Gbl.getConfig().network().getInputFile());
+		Gbl.getWorld().complete();
+		Gbl.printMemoryUsage();
+		System.out.println("  done.");
+
+		//////////////////////////////////////////////////////////////////////
+		
+		System.out.println("  setting up population objects...");
+		Population pop = new Population(Population.USE_STREAMING);
+		PopulationWriter pop_writer = new PopulationWriter(pop);
+		pop.addAlgorithm(pop_writer);
+		PopulationReader pop_reader = new MatsimPopulationReader(pop);
+		Gbl.printMemoryUsage();
+		System.out.println("  done.");
+
+
+		//////////////////////////////////////////////////////////////////////
+		
+		System.out.println("  reading, processing, writing plans...");
+		pop_reader.readFile(Gbl.getConfig().plans().getInputFile());
+		pop_writer.write();
+		pop.printPlansCount();
+		Gbl.printMemoryUsage();
+		System.out.println("  done.");
+
+		//////////////////////////////////////////////////////////////////////
+
+		log.info("  writing network xml file... ");
+		new NetworkWriter(network).write();
+		Gbl.printMemoryUsage();
+		log.info("  done.");
+
+		log.info("  writing facilities xml file... ");
+		new FacilitiesWriter(facilities).write();
+		Gbl.printMemoryUsage();
+		log.info("  done.");
+
+		log.info("  writing config xml file... ");
+		new ConfigWriter(Gbl.getConfig()).write();
+		Gbl.printMemoryUsage();
+		log.info("  done.");
+
+		log.info("done.");
+	}
+
+	//////////////////////////////////////////////////////////////////////
+	// main
+	//////////////////////////////////////////////////////////////////////
+
+	public static void main(final String[] args) {
+
+		Gbl.startMeasurement();
+
+		Gbl.createConfig(args);
+		Gbl.createWorld();
+
+		samplePopulation();
+
+		Gbl.printElapsedTime();
+	}
+}
