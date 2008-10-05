0c0ac12b7ccd8c2f0d2a678b13249832df3e06e3
mrieser <rieser@gmail.com>
2008-10-05 07:25:18 +0000

    instead of implementing BasicEventHandler to get all events, only implement the event handlers for events that can be written and read again from the TXT-format. Future events may have additional attributes that cannot be reconstructed from the data in TXT event files, so we make sure they are not written in the first place.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@3465 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/events/algorithms/EventWriterTXT.java
+++ matsim/src/org/matsim/events/algorithms/EventWriterTXT.java
@@ -4,7 +4,7 @@
  *                                                                         *
  * *********************************************************************** *
  *                                                                         *
- * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ * copyright       : (C) 2007, 2008 by the members listed in the COPYING,  *
  *                   LICENSE and WARRANTY file.                            *
  * email           : info at matsim dot org                                *
  *                                                                         *
@@ -23,11 +23,39 @@ package org.matsim.events.algorithms;
 import java.io.BufferedWriter;
 import java.io.IOException;
 
-import org.matsim.events.BasicEvent;
-import org.matsim.events.handler.BasicEventHandler;
+import org.matsim.events.ActEndEvent;
+import org.matsim.events.ActStartEvent;
+import org.matsim.events.AgentArrivalEvent;
+import org.matsim.events.AgentDepartureEvent;
+import org.matsim.events.AgentReplanEvent;
+import org.matsim.events.AgentStuckEvent;
+import org.matsim.events.AgentUtilityEvent;
+import org.matsim.events.AgentWait2LinkEvent;
+import org.matsim.events.LinkEnterEvent;
+import org.matsim.events.LinkLeaveEvent;
+import org.matsim.events.handler.ActEndEventHandler;
+import org.matsim.events.handler.ActStartEventHandler;
+import org.matsim.events.handler.AgentArrivalEventHandler;
+import org.matsim.events.handler.AgentDepartureEventHandler;
+import org.matsim.events.handler.AgentReplanEventHandler;
+import org.matsim.events.handler.AgentStuckEventHandler;
+import org.matsim.events.handler.AgentUtilityEventHandler;
+import org.matsim.events.handler.AgentWait2LinkEventHandler;
+import org.matsim.events.handler.LinkEnterEventHandler;
+import org.matsim.events.handler.LinkLeaveEventHandler;
 import org.matsim.utils.io.IOUtils;
 
-public class EventWriterTXT implements BasicEventHandler {
+public class EventWriterTXT implements ActEndEventHandler, ActStartEventHandler, AgentArrivalEventHandler, 
+		AgentDepartureEventHandler, AgentReplanEventHandler, AgentStuckEventHandler, AgentUtilityEventHandler, 
+		AgentWait2LinkEventHandler, LinkEnterEventHandler, LinkLeaveEventHandler {
+	
+	/* Implement all the different event handlers by its own. Future event types will no longer be
+	 * suitable to be written to a TXT-format file, but will have additional attributes that need to be
+	 * stored in XML. Explicitly listing the event handlers makes sure only events are written to TXT that
+	 * can also correctly be read in again, and helps fix the test cases during introduction of the new,
+	 * additional events.
+	 */
+	
 	private BufferedWriter out = null;
 
 	public EventWriterTXT(final String filename) {
@@ -82,7 +110,43 @@ public class EventWriterTXT implements BasicEventHandler {
 		}
 	}
 
-	public void handleEvent(final BasicEvent event) {
+	public void handleEvent(ActEndEvent event) {
+		writeLine(event.toString());
+	}
+
+	public void handleEvent(ActStartEvent event) {
+		writeLine(event.toString());
+	}
+
+	public void handleEvent(AgentArrivalEvent event) {
+		writeLine(event.toString());
+	}
+
+	public void handleEvent(AgentDepartureEvent event) {
+		writeLine(event.toString());
+	}
+
+	public void handleEvent(AgentReplanEvent event) {
+		writeLine(event.toString());
+	}
+	
+	public void handleEvent(AgentStuckEvent event) {
+		writeLine(event.toString());
+	}
+
+	public void handleEvent(AgentUtilityEvent event) {
+		writeLine(event.toString());		
+	}
+
+	public void handleEvent(AgentWait2LinkEvent event) {
+		writeLine(event.toString());
+	}
+
+	public void handleEvent(LinkEnterEvent event) {
+		writeLine(event.toString());
+	}
+
+	public void handleEvent(LinkLeaveEvent event) {
 		writeLine(event.toString());
 	}
 }
