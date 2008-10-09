c531fcbf84dac01c181d65972ca66ae6c9308849
mrieser <rieser@gmail.com>
2008-10-09 15:08:05 +0000

    Replaced iterative calls to Plan.getNextLeg() and Plan.getNextActivity() with other methods, which should be a little bit faster.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@3500 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/planomat/PlanomatFitnessFunctionWrapper.java
+++ matsim/src/org/matsim/planomat/PlanomatFitnessFunctionWrapper.java
@@ -20,6 +20,7 @@
 
 package org.matsim.planomat;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.jgap.FitnessFunction;
@@ -39,37 +40,34 @@ import org.matsim.scoring.ScoringFunction;
 /**
  * This class connects the JGAP FitnessFunction class with the MATSim ScoringFunction interface.
  * This is done in order to use the MATSim scoring function in a JGAP optimization procedure like planomat.
- * 
+ *
  * @author meisterk
  *
  */
 public class PlanomatFitnessFunctionWrapper extends FitnessFunction {
 
 	public static final double FITNESS_OFFSET = 10000.0;
-	
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 
-	private Plan plan;
-	private LegTravelTimeEstimator legTravelTimeEstimator;
-	private ScoringFunction sf;
-	private PlanAnalyzeSubtours planAnalyzeSubtours;
+	private final Plan plan;
+	private final LegTravelTimeEstimator legTravelTimeEstimator;
+	private final ScoringFunction sf;
+	private final PlanAnalyzeSubtours planAnalyzeSubtours;
 
-	public PlanomatFitnessFunctionWrapper( 
-			final ScoringFunction sf, 
-			Plan plan, 
-			final LegTravelTimeEstimator legTravelTimeEstimator, 
+	public PlanomatFitnessFunctionWrapper(
+			final ScoringFunction sf,
+			final Plan plan,
+			final LegTravelTimeEstimator legTravelTimeEstimator,
 			final PlanAnalyzeSubtours planAnalyzeSubtours) {
 
 		this.sf = sf;
 		this.plan = plan;
 		this.legTravelTimeEstimator = legTravelTimeEstimator;
 		this.planAnalyzeSubtours = planAnalyzeSubtours;
-		
+
 	}
-	
+
 	@Override
 	protected double evaluate(IChromosome a_subject) {
 
@@ -80,39 +78,42 @@ public class PlanomatFitnessFunctionWrapper extends FitnessFunction {
 		Act origin = null, destination = null;
 		Leg legIntermediate = null;
 		HashMap<Leg, Route> originalRoutes = PlanOptimizeTimes.getLegsRoutes(plan);
-		
+
 		sf.reset();
 		double now = 0.0;
 		// process "middle" activities
 		int numActs = this.planAnalyzeSubtours.getSubtourIndexation().length;
+
+		ArrayList<Object> actslegs = this.plan.getActsLegs();
+
 		for (int ii=0; ii < numActs; ii++) {
 
 			now += ((DoubleGene) a_subject.getGene(ii)).doubleValue();
-			
+
 			sf.startLeg(now, null);
-			
-			origin = ((Act) plan.getActsLegs().get(ii * 2));
-			legIntermediate = plan.getNextLeg(origin);
-			destination = plan.getNextActivity(legIntermediate);
-			
+
+			origin = (Act) actslegs.get(ii * 2);
+			legIntermediate = (Leg) actslegs.get(ii * 2 + 1);
+			destination = (Act) actslegs.get(ii * 2 + 2);
+
 			// set mode
 			subtourIndex = this.planAnalyzeSubtours.getSubtourIndexation()[ii];
 			modeIndex = ((IntegerGene) a_subject.getGene(numActs + subtourIndex)).intValue();
 			modeName = Gbl.getConfig().planomat().getPossibleModes().get(modeIndex);
 //			System.out.println(ii + "\t" + subtourIndex + "\t" + modeIndex + "\t" + modeName);
 			legIntermediate.setMode(modeName);
-			
+
 			// set times
 			travelTime = this.legTravelTimeEstimator.getLegTravelTimeEstimation(
-					this.plan.getPerson().getId(), 
-					now, 
-					origin, 
-					destination, 
+					this.plan.getPerson().getId(),
+					now,
+					origin,
+					destination,
 					legIntermediate);
 
 //			System.out.println(Time.writeTime(travelTime));
 			now += travelTime;
-			
+
 			// recover original route
 			legIntermediate.setRoute(originalRoutes.get(legIntermediate));
 			sf.endLeg(now);
