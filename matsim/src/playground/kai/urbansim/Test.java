3371ae1ec9e2989b6e0c29f313351a2eed1b5454
vsp_nagel <2008@kainagel.org>
2008-10-28 03:45:34 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@3697 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/kai/urbansim/Test.java
+++ matsim/src/playground/kai/urbansim/Test.java
@@ -9,13 +9,22 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Vector;
 
+import org.matsim.basic.v01.Id;
 import org.matsim.basic.v01.IdImpl;
+import org.matsim.basic.v01.BasicLeg.Mode;
+import org.matsim.facilities.Activity;
+import org.matsim.facilities.Facilities;
+import org.matsim.facilities.Facility;
 import org.matsim.population.Person;
 import org.matsim.population.Plan;
 import org.matsim.population.Population;
 import org.matsim.utils.io.IOUtils;
+import org.matsim.world.Location;
 
 public class Test {
+
+	public static final String ACT_HOME = "home" ;
+	public static final String ACT_WORK = "work" ;
 
 	static Map<String,Integer> createIdxFromKey( String line ) {
 		String[] keys = line.split("[ \t\n]+") ;
@@ -30,19 +39,71 @@ public class Test {
 	public static void main ( String[] args ) {
 		BufferedReader reader;
 		
-		
+		// read urbansim grid cells:
+
+		Facilities gridCells = new Facilities("urbansim grid cells",
+				Facilities.FACILITIES_NO_STREAMING) ;
 
 		try {
 			reader = IOUtils.getBufferedReader("/home/nagel/tmp/tab/gridcells.tab" ) ;
 
 			String header = reader.readLine() ;
 			Map<String,Integer> idxFromKey = createIdxFromKey( header ) ;
-			
+
 			String line = reader.readLine() ;
 			while ( line != null ) {
 				String[] parts = line.split("[ \t\n]+");
 
+				int idx = idxFromKey.get("grid_id:i4") ;
+				String idAsString = parts[idx] ;
+
+				idx = idxFromKey.get("relative_x:i4") ;
+				String xxAsString = parts[idx] ;
+
+				idx = idxFromKey.get("relative_y:i4") ;
+				String yyAsString = parts[idx];
+
+				Facility facility = gridCells.createFacility(idAsString, xxAsString, yyAsString) ;
+
+//				idx = idxFromKey.get("industrial_sqft:i4") ;
+//				double industrialSqft = Double.parseDouble( parts[idx] ) ;
+//
+//				idx = idxFromKey.get("industrial_sqft_per_job:i4") ;
+//				double industrialSqftPerJob = Double.parseDouble( parts[idx] ) ;
+//
+//				double nIndustrialJobs = industrialSqft/industrialSqftPerJob ;
+
+				Activity work = facility.createActivity(ACT_WORK) ;
+				work.setCapacity( 0 ) ;
+
+				line = reader.readLine() ;
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// read urbansim jobs:
+		
+		try {
+			reader = IOUtils.getBufferedReader("/home/nagel/tmp/tab/jobs.tab" ) ;
+
+			String header = reader.readLine() ;
+			Map<String,Integer> idxFromKey = createIdxFromKey( header ) ;
+
+			String line = reader.readLine() ;
+			while ( line != null ) {
+				String[] parts = line.split("[ \t\n]+");
 				
+				int idx = idxFromKey.get("grid_id:i4") ;
+				Id gridId = new IdImpl( parts[idx] ) ;
+				Facility grid = (Facility) gridCells.getLocation(gridId) ;
+				Activity work = grid.getActivity(ACT_WORK) ;
+				work.setCapacity(work.getCapacity()+1) ;
 				
 				line = reader.readLine() ;
 			}
@@ -55,6 +116,7 @@ public class Test {
 			e.printStackTrace();
 		}
 
+		// read urbansim households and convert them into individual people
 
 		Population population = new Population(Population.NO_STREAMING);
 
@@ -100,11 +162,18 @@ public class Test {
 					}
 
 					Plan plan = person.createPlan(true);
-//					plan.createAct("home", fac); // use data from parts[x] here
+					plan.setSelected(true) ;
 
+					idx = idxFromKey.get("grid_id:i4") ;
+					Id gridId = new IdImpl( parts[idx] ) ;
+					Facility facility = (Facility) gridCells.getLocation( gridId ) ;
+					plan.createAct(ACT_HOME, facility) ;
 
-					//					plan.createLeg(Mode.car);
-//					plan.createAct(...);
+					plan.createLeg(Mode.car);
+					plan.createAct(ACT_WORK, (Facility) null ) ;
+					
+					plan.createLeg(Mode.car) ;
+					plan.createAct(ACT_HOME, facility ) ;
 
 					population.addPerson(person);
 				}
@@ -118,6 +187,19 @@ public class Test {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		// first implement singly constrained gravity model:
+		// (matsim person algorithm??)
+		
+		// mir ist nicht klar, warum es diese Konstruktion tut:
+		// liegt daran, dass population.getPersons eine normale Map zurückgibt. 
+		for (Person person : population.getPersons().values()) {
+			Plan plan = person.getSelectedPlan() ;
+			
+			
+			
+		}
+		
 
 	}
 }
