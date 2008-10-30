eb3db5b99e9ccd6836f9e51e8d3d35edb22e34de
vsp_nagel <2008@kainagel.org>
2008-10-30 03:26:52 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@3736 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/kai/urbansim/Test.java
+++ matsim/src/playground/kai/urbansim/Test.java
@@ -7,6 +7,7 @@ import java.io.Reader;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.Vector;
 
 import org.matsim.basic.v01.Id;
@@ -14,10 +15,17 @@ import org.matsim.basic.v01.IdImpl;
 import org.matsim.basic.v01.BasicLeg.Mode;
 import org.matsim.facilities.Activity;
 import org.matsim.facilities.Facilities;
+import org.matsim.facilities.FacilitiesWriter;
 import org.matsim.facilities.Facility;
+import org.matsim.interfaces.networks.basicNet.BasicNode;
+import org.matsim.network.Link;
+import org.matsim.network.NetworkLayer;
+import org.matsim.network.NetworkWriter;
+import org.matsim.network.Node;
 import org.matsim.population.Person;
 import org.matsim.population.Plan;
 import org.matsim.population.Population;
+import org.matsim.population.PopulationWriter;
 import org.matsim.utils.io.IOUtils;
 import org.matsim.world.Location;
 
@@ -38,7 +46,61 @@ public class Test {
 
 	public static void main ( String[] args ) {
 		BufferedReader reader;
-		
+
+		// read emme2 network:
+
+		NetworkLayer network = new NetworkLayer() ;
+		try {
+			reader = IOUtils.getBufferedReader("/home/nagel/tmp/tab/net1.out" ) ;
+
+			boolean weAreReadingNodes = true ;
+			long linkCnt = 0 ;
+
+			String line = reader.readLine() ;
+			while ( line != null ) {
+				String[] parts = line.split("[ \t\n]+");
+
+				if ( parts[0].equals("c") ) {
+					// is a comment; ignore
+				} else if ( parts[0].equals("t") ) {
+					if ( parts[1].equals("links") ) {
+						weAreReadingNodes = false ;
+					} 
+				} else if ( parts[0].equals("a") || parts[0].equals("a*") ) {
+					if ( weAreReadingNodes ) {
+						String idStr = parts[1] ;
+						String xxStr = parts[2] ;
+						String yyStr = parts[3] ;
+						Node node = network.createNode(idStr,xxStr,yyStr,"unknownType") ;
+						node.setOrigId(parts[7]);			
+					} else {
+						String fromNode = parts[1] ;
+						String toNode = parts[2] ;
+						String length = parts[3] ; // probably miles !!!
+						String type = parts[5] ;
+						String permlanes = parts[6] ;
+						String capacity = parts[8] ;
+						String freespeed = parts[9] ; // miles per hours !!!
+						String origid = parts[1] + ',' + parts[2] ;
+						String id = Long.toString(linkCnt) ; linkCnt++ ;
+
+						Link link = network.createLink(id, fromNode, toNode, length, freespeed, capacity, permlanes, origid, type);
+					}
+				} else {
+					// something else; do nothing
+				}
+
+				line = reader.readLine();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		NetworkWriter nwWriter = new NetworkWriter(network,"/home/nagel/tmp/net.xml") ;
+		nwWriter.write() ;
+
 		// read urbansim grid cells:
 
 		Facilities gridCells = new Facilities("urbansim grid cells",
@@ -67,10 +129,10 @@ public class Test {
 
 //				idx = idxFromKey.get("industrial_sqft:i4") ;
 //				double industrialSqft = Double.parseDouble( parts[idx] ) ;
-//
+
 //				idx = idxFromKey.get("industrial_sqft_per_job:i4") ;
 //				double industrialSqftPerJob = Double.parseDouble( parts[idx] ) ;
-//
+
 //				double nIndustrialJobs = industrialSqft/industrialSqftPerJob ;
 
 				Activity work = facility.createActivity(ACT_WORK) ;
@@ -88,7 +150,7 @@ public class Test {
 		}
 
 		// read urbansim jobs:
-		
+
 		try {
 			reader = IOUtils.getBufferedReader("/home/nagel/tmp/tab/jobs.tab" ) ;
 
@@ -98,13 +160,13 @@ public class Test {
 			String line = reader.readLine() ;
 			while ( line != null ) {
 				String[] parts = line.split("[ \t\n]+");
-				
+
 				int idx = idxFromKey.get("grid_id:i4") ;
 				Id gridId = new IdImpl( parts[idx] ) ;
 				Facility grid = (Facility) gridCells.getLocation(gridId) ;
 				Activity work = grid.getActivity(ACT_WORK) ;
 				work.setCapacity(work.getCapacity()+1) ;
-				
+
 				line = reader.readLine() ;
 			}
 
@@ -116,12 +178,86 @@ public class Test {
 			e.printStackTrace();
 		}
 
-		// read urbansim households and convert them into individual people
+		FacilitiesWriter facWriter = new FacilitiesWriter(gridCells,"/home/nagel/tmp/fac.xml") ;
+		facWriter.write();
+
 
 		Population population = new Population(Population.NO_STREAMING);
 
+//		// read urbansim households and convert them into individual people ... but I don't need that
+//		try {
+//		reader = IOUtils.getBufferedReader("/home/nagel/tmp/tab/households.tab");
+
+//		String header = reader.readLine();
+//		Map<String,Integer> idxFromKey = createIdxFromKey( header ) ;
+
+//		long counter=0 ;
+
+//		String line = reader.readLine(); {}
+
+//		while (line != null) {
+//		String[] parts = line.split("[ \t\n]+");
+
+//		int idx = idxFromKey.get("persons:i4") ;
+//		int nPersons = Integer.parseInt( parts[idx] ) ;
+
+//		idx = idxFromKey.get("workers:i4") ;
+//		int nWorkers = Integer.parseInt( parts[idx] ) ;
+
+//		idx = idxFromKey.get("cars:i4") ;
+//		int nCars = Integer.parseInt( parts[idx] ) ;
+
+//		for ( int ii=0 ; ii<nPersons ; ii++ ) {
+
+//		Person person = new Person(new IdImpl(counter++));
+
+//		idx = idxFromKey.get("age_of_head:i4") ;
+//		person.setAge( Integer.parseInt( parts[idx] ) ) ;
+
+//		if ( ii<nWorkers ) {
+//		person.setEmployed("worker") ;
+//		} else {
+//		person.setEmployed(null) ;
+//		}
+
+//		if ( ii<nCars ) {
+//		person.setCarAvail("car") ;
+//		} else {
+//		person.setCarAvail(null) ;
+//		}
+
+//		Plan plan = person.createPlan(true);
+//		plan.setSelected(true) ;
+
+//		idx = idxFromKey.get("grid_id:i4") ;
+//		Id gridId = new IdImpl( parts[idx] ) ;
+//		Facility facility = (Facility) gridCells.getLocation( gridId ) ;
+//		plan.createAct(ACT_HOME, facility) ;
+
+//		plan.createLeg(Mode.car);
+//		plan.createAct(ACT_WORK, (Facility) null ) ;
+
+//		plan.createLeg(Mode.car) ;
+//		plan.createAct(ACT_HOME, facility ) ;
+
+//		population.addPerson(person);
+//		}
+
+//		line = reader.readLine(); // next line
+//		}
+//		} catch (FileNotFoundException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//		} catch (IOException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//		}
+
+
+
+		// read urbansim persons
 		try {
-			reader = IOUtils.getBufferedReader("/home/nagel/tmp/tab/households.tab");
+			reader = IOUtils.getBufferedReader("/home/nagel/tmp/tab/persons.tab");
 
 			String header = reader.readLine();
 			Map<String,Integer> idxFromKey = createIdxFromKey( header ) ;
@@ -133,53 +269,46 @@ public class Test {
 			while (line != null) {
 				String[] parts = line.split("[ \t\n]+");
 
-				int idx = idxFromKey.get("persons:i4") ;
-				int nPersons = Integer.parseInt( parts[idx] ) ;
+				int idx = idxFromKey.get("person_id:i4") ;
+				Id id = new IdImpl( parts[idx] ) ;
 
-				idx = idxFromKey.get("workers:i4") ;
-				int nWorkers = Integer.parseInt( parts[idx] ) ;
+				Person person = new Person( id ) ;
 
-				idx = idxFromKey.get("cars:i4") ;
-				int nCars = Integer.parseInt( parts[idx] ) ;
+				idx = idxFromKey.get("age:i4") ;
+				person.setAge( Integer.parseInt( parts[idx] ) ) ;
 
-				for ( int ii=0 ; ii<nPersons ; ii++ ) {
+				idx = idxFromKey.get("employment_status:i4") ;
+				person.setEmployed(parts[idx]) ;
 
-					Person person = new Person(new IdImpl(counter++));
+				idx = idxFromKey.get("job_id:i4") ;
 
-					idx = idxFromKey.get("age_of_head:i4") ;
-					person.setAge( Integer.parseInt( parts[idx] ) ) ;
 
-					if ( ii<nWorkers ) {
-						person.setEmployed("worker") ;
-					} else {
-						person.setEmployed(null) ;
-					}
 
-					if ( ii<nCars ) {
-						person.setCarAvail("car") ;
-					} else {
-						person.setCarAvail(null) ;
-					}
+//				if ( ii<nCars ) {
+//					person.setCarAvail("car") ;
+//				} else {
+//					person.setCarAvail(null) ;
+//				}
 
-					Plan plan = person.createPlan(true);
-					plan.setSelected(true) ;
+				Plan plan = person.createPlan(true);
+				plan.setSelected(true) ;
 
-					idx = idxFromKey.get("grid_id:i4") ;
-					Id gridId = new IdImpl( parts[idx] ) ;
-					Facility facility = (Facility) gridCells.getLocation( gridId ) ;
-					plan.createAct(ACT_HOME, facility) ;
+				idx = idxFromKey.get("grid_id:i4") ;
+				Id gridId = new IdImpl( parts[idx] ) ;
+				Facility facility = (Facility) gridCells.getLocation( gridId ) ;
+				plan.createAct(ACT_HOME, facility) ;
 
-					plan.createLeg(Mode.car);
-					plan.createAct(ACT_WORK, (Facility) null ) ;
-					
-					plan.createLeg(Mode.car) ;
-					plan.createAct(ACT_HOME, facility ) ;
+				plan.createLeg(Mode.car);
+				plan.createAct(ACT_WORK, (Facility) null ) ;
 
-					population.addPerson(person);
-				}
+				plan.createLeg(Mode.car) ;
+				plan.createAct(ACT_HOME, facility ) ;
 
-				line = reader.readLine(); // next line
+				population.addPerson(person);
 			}
+
+			line = reader.readLine(); // next line
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -187,19 +316,27 @@ public class Test {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		// first implement singly constrained gravity model:
 		// (matsim person algorithm??)
-		
+
 		// mir ist nicht klar, warum es diese Konstruktion tut:
 		// liegt daran, dass population.getPersons eine normale Map zurŸckgibt. 
 		for (Person person : population.getPersons().values()) {
 			Plan plan = person.getSelectedPlan() ;
-			
-			
-			
+
+			Map<Id, ? extends Location> locations = gridCells.getLocations() ;
+
+//			Collections.
+
+//			locations.
+
 		}
-		
+
+		PopulationWriter popWriter = new PopulationWriter(population,"/home/nagel/tmp/pop.xml","v4",1) ;
+		popWriter.write();
+
+		System.out.println("### DONE ###") ;
 
 	}
 }
