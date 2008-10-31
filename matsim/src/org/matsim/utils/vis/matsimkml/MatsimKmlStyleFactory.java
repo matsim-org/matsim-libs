60b4cdb1b7a1979d8c95ba7077f459538833cddf
mrieser <rieser@gmail.com>
2008-10-31 14:43:12 +0000

    fixed definition of color, as kml has a non-typical abgr color-code (instead of the more widely used rgba). some minor code cleanup.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@3759 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/utils/vis/matsimkml/MatsimKmlStyleFactory.java
+++ matsim/src/org/matsim/utils/vis/matsimkml/MatsimKmlStyleFactory.java
@@ -4,7 +4,7 @@
  *                                                                         *
  * *********************************************************************** *
  *                                                                         *
- * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ * copyright       : (C) 2007, 2008 by the members listed in the COPYING,  *
  *                   LICENSE and WARRANTY file.                            *
  * email           : info at matsim dot org                                *
  *                                                                         *
@@ -32,38 +32,23 @@ import net.opengis.kml._2.StyleType;
 import org.matsim.gbl.MatsimResource;
 import org.matsim.utils.vis.kml.KMZWriter;
 
-
 /**
  * @author dgrether
- *
  */
 public class MatsimKmlStyleFactory {
-	public static final String DEFAULTLINKICON ="icon181.png";
+	public static final String DEFAULTLINKICON ="link.png";
 
-	public static final String DEFAULTNODEICON ="icon18.png";
+	public static final String DEFAULTNODEICON ="node.png";
 	/**
 	 * the resource to be used as icon
 	 */
 	public static final String DEFAULTNODEICONRESOURCE = "icon18.png";
-	/**
-	 * the scale for the icons
-	 */
-	private static final double ICONSCALE = 0.5;
-	/**
-	 * some colors frequently used in matsim: bgr: 15,15,190
-	 */
-	public static final byte[] MATSIMRED = new byte[]{(byte) 0xFF, (byte) 0x0F, (byte) 0x0F, (byte) 0xBE};
-	/**
-	 * some colors frequently used in matsim
-	 */
-//	public static final Color MATSIMBLUE = new Color(190, 10, 80, 190);
-	/**
-	 * some colors frequently used in matsim
-	 */
-	public static final byte[] MATSIMGREY = new byte[]{(byte) 210, (byte) 50, (byte) 50, (byte) 70};
-	/**
-	 * some colors frequently used in matsim
-	 */
+
+	private static final Double ICONSCALE = Double.valueOf(0.5);
+
+	public static final byte[] MATSIMRED = new byte[]{(byte) 255, (byte) 15, (byte) 15, (byte) 190};
+//	public static final Color MATSIMBLUE = new Color(190, 190, 80, 90);
+	public static final byte[] MATSIMGREY = new byte[]{(byte) 210, (byte) 70, (byte) 50, (byte) 50};
 	public static final byte[] MATSIMWHITE = new byte[]{(byte) 230, (byte) 230, (byte) 230, (byte) 230};
 	/**
 	 * the kmz writer
@@ -97,8 +82,6 @@ public class MatsimKmlStyleFactory {
 			iStyle.setScale(MatsimKmlStyleFactory.ICONSCALE);
 			this.defaultnetworknodestyle.setIconStyle(iStyle);
 			this.document.getAbstractStyleSelectorGroup().add(kmlObjectFactory.createStyle(this.defaultnetworknodestyle));
-//			LineStyle lineStyle = new LineStyle(MATSIMGREY, ColorStyle.DEFAULT_COLOR_MODE, 12);
-//			style.setLineStyle(lineStyle);
 		}
 		return this.defaultnetworknodestyle;
 	}
@@ -118,7 +101,7 @@ public class MatsimKmlStyleFactory {
 			this.defaultnetworklinkstyle.setIconStyle(iStyle);
 			LineStyleType lineStyle = kmlObjectFactory.createLineStyleType();
 			lineStyle.setColor(MatsimKmlStyleFactory.MATSIMGREY);
-			lineStyle.setWidth(12.0);
+			lineStyle.setWidth(Double.valueOf(12.0));
 			this.defaultnetworklinkstyle.setLineStyle(lineStyle);
 			this.document.getAbstractStyleSelectorGroup().add(kmlObjectFactory.createStyle(this.defaultnetworklinkstyle));
 		}
