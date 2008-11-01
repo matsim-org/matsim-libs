2a9f739a3244d5f8584d698ea745fd28521e1b09
rashid_waraich <rashid_waraich@users.sf.net>
2008-11-01 16:16:57 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@3776 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/wrashid/PHEV/estimationStreetParkingKantonZH/Main.java
+++ matsim/src/playground/wrashid/PHEV/estimationStreetParkingKantonZH/Main.java
@@ -17,6 +17,27 @@ import org.matsim.world.ZoneLayer;
 
 import playground.wrashid.PHEV.parking.data.Facility;
 
+
+/*
+ * - We have all the communities in the GG25Data. 
+ * - From these we can simple filter out only the communities in the Kanton Zurich
+ * - Using the communityId, we can use the world file, where the zone of each community is defined.
+ * - Using the zone information, we can make a collection of roads per community
+ * - We have street parking locations, which we can map to all the roads of Zurich
+ * - 
+ * 
+ * 
+ * - for each community, we do the following calculations: 
+ * 	   1.) The percentage of each street type (length).
+ *     2.) sum of the length of all streets divided by the number of streets
+ * - We randomly draw boxes in the zurich city area. And calculate the same two things.
+ * - Then we calculate the difference 
+ * - we select the area of the city of zurich, which has the least difference to a community
+ * - for the selected zurich city area, we calculate for each street type two things
+ *   1.) percentage of streets 
+ */
+
+
 public class Main {
 
 	/**
@@ -42,21 +63,21 @@ public class Main {
 		// TODO: continue here also
 		*/
 		
-		/*
+		
 		// read network layer
 		NetworkLayer network = new NetworkLayer();
 		NetworkReaderMatsimV1 nr = new NetworkReaderMatsimV1(network);
-		nr.readFile("C:\\data\\SandboxCVS\\ivt\\studies\\switzerland\\networks\\ivtch\\network.xml");
+		nr.readFile("C:\\data\\SandboxCVS\\ivt\\studies\\switzerland\\networks\\navteq\\network.xml\\network.xml");
 		//for (Link link:network.getLinks().values()){
 		//	System.out.println(link.getId() +  " - " + link.getCapacity(network.getCapacityPeriod()));
 		//}
-		*/
+		
 		
 		// read street parking data of zurich
 		LinkedList<Coord> streetData = StreetParkingReader.readData("C:\\data\\Projekte\\ETH TH-22 07-3 PHEV\\Parkhäuser\\facilities\\input\\streetParking2007_1.txt");
 		
 		
-		/*
+		
 		// calculate the number of parkings per link for the city of Zurich
 		// key=linkId, value=NumberOfParkings
 		HashMap<String, Integer> numberOfStreetParkingsPerLink=new HashMap<String, Integer>();
@@ -70,7 +91,9 @@ public class Main {
 			numberOfParkings++;
 			numberOfStreetParkingsPerLink.put(linkId,numberOfParkings);
 		}
-		*/
+		for (String linkId:numberOfStreetParkingsPerLink.keySet()){
+			System.out.println(linkId + "-" + numberOfStreetParkingsPerLink.get(linkId));
+		}
 		// ??? print some statistics about the existing parkings?
 		// ??? do some anylysis
 		// look for some good tool for producing charts
