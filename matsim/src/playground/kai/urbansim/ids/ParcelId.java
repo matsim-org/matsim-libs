01cce5c5bde2ac510ecc294265d1ad48750faaf4
vsp_nagel <2008@kainagel.org>
2008-11-22 01:38:57 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@4091 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/kai/urbansim/ids/ParcelId.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package playground.kai.urbansim.ids;
+
+import org.matsim.basic.v01.IdImpl;
+
+/**
+ * @author nagel
+ *
+ */
+public class ParcelId extends IdImpl {
+
+	/**
+	 * @param id
+	 */
+	public ParcelId(String id) {
+		super(id);
+	}
+
+	/**
+	 * @param id
+	 */
+	public ParcelId(long id) {
+		super(id);
+	}
+
+}
---
+++ matsim/src/playground/kai/urbansim/ids/ZoneId.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package playground.kai.urbansim.ids;
+
+import org.matsim.basic.v01.IdImpl;
+
+/**
+ * @author nagel
+ *
+ */
+public class ZoneId extends IdImpl {
+
+	/**
+	 * @param id
+	 */
+	public ZoneId(String id) {
+		super(id);
+	}
+
+	/**
+	 * @param id
+	 */
+	public ZoneId(long id) {
+		super(id);
+	}
+
+}
