86bb69ba48d5ac4a428ea7c82cc7acb5808369db
Matthias_Feil <Matthias_Feil@users.sf.net>
2008-11-24 18:35:26 +0000

    ScheduleCleaner stand-alone to make available for various classes
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@4131 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/mfeil/ScheduleCleaner.java
@@ -0,0 +1,82 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * ScheduleCleaner.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.mfeil;
+
+import org.matsim.planomat.costestimators.LegTravelTimeEstimator;
+import org.matsim.population.Act;
+import org.matsim.population.Leg;
+import org.matsim.population.Plan;
+
+public class ScheduleCleaner {
+	
+	private final LegTravelTimeEstimator	estimator;
+	private final double					minimumTime;
+	
+	public ScheduleCleaner(LegTravelTimeEstimator estimator, double minimumTime){
+		this.estimator = estimator;
+		this.minimumTime = minimumTime;
+	}
+	
+	public double run (double now, Plan plan){
+		((Act)(plan.getActsLegs().get(0))).setEndTime(now);
+		((Act)(plan.getActsLegs().get(0))).setDuration(now);
+			
+		double travelTime;
+		for (int i=1;i<=plan.getActsLegs().size()-2;i=i+2){
+			((Leg)(plan.getActsLegs().get(i))).setDepartureTime(now);
+			travelTime = this.estimator.getLegTravelTimeEstimation(plan.getPerson().getId(), now, (Act)(plan.getActsLegs().get(i-1)), (Act)(plan.getActsLegs().get(i+1)), (Leg)(plan.getActsLegs().get(i)));
+			((Leg)(plan.getActsLegs().get(i))).setArrivalTime(now+travelTime);
+			((Leg)(plan.getActsLegs().get(i))).setTravelTime(travelTime);
+			now+=travelTime;
+			
+			if (i!=plan.getActsLegs().size()-2){
+				((Act)(plan.getActsLegs().get(i+1))).setStartTime(now);
+				travelTime = java.lang.Math.max(((Act)(plan.getActsLegs().get(i+1))).getDuration()-travelTime, 0);
+				((Act)(plan.getActsLegs().get(i+1))).setDuration(travelTime);	
+				((Act)(plan.getActsLegs().get(i+1))).setEndTime(now+travelTime);	
+				now+=travelTime;
+			}
+			else {
+				((Act)(plan.getActsLegs().get(i+1))).setStartTime(now);
+				/* NEW NEW NEW NEW NEW NEW NEW NEW NEW NEW NEW NEW*/
+				if (86400>now){
+					((Act)(plan.getActsLegs().get(i+1))).setDuration(86400-now);
+					((Act)(plan.getActsLegs().get(i+1))).setEndTime(86400);
+				}
+				else if (86400+((Act)(plan.getActsLegs().get(0))).getDuration()>now){
+					if (now<86400){
+						((Act)(plan.getActsLegs().get(i+1))).setDuration(86400-now);
+						((Act)(plan.getActsLegs().get(i+1))).setEndTime(86400);
+					}
+					else {
+					((Act)(plan.getActsLegs().get(i+1))).setDuration(0);
+					((Act)(plan.getActsLegs().get(i+1))).setEndTime(now);
+					}
+				}
+				else {
+					return (now-(86400+((Act)(plan.getActsLegs().get(0))).getDuration()));
+				}
+			}
+		}
+		return 0;
+	}
+
+}
