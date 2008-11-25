21f0ffa0f25e7ba5db2bae6ab0cb0ba453d4d49c
mrieser <rieser@gmail.com>
2008-11-25 17:59:45 +0000

    catch IOExceptions, instead of the generic Exception. some other minor code cleanup.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@4146 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/test/src/org/matsim/demandmodeling/primloc/PrimlocIO.java
+++ matsim/test/src/org/matsim/demandmodeling/primloc/PrimlocIO.java
@@ -1,14 +1,12 @@
 /* *********************************************************************** *
  * project: org.matsim.*
- * SomeIO.java
+ * PrimlocIO.java
  *                                                                         *
  * *********************************************************************** *
  *                                                                         *
- * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
  *                   LICENSE and WARRANTY file.                            *
  * email           : info at matsim dot org                                *
- * 
- * @author Fabrice Marchal
  *                                                                         *
  * *********************************************************************** *
  *                                                                         *
@@ -22,100 +20,100 @@
 
 package org.matsim.demandmodeling.primloc;
 
-
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.LineNumberReader;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.StringTokenizer;
-import java.util.TreeMap;
 
 import org.matsim.utils.io.IOUtils;
 
 import Jama.Matrix;
 
-
+/**
+ * @author Fabrice Marchal
+ */
 public class PrimlocIO {
 
 	/* File format: Zone_ID	Zone_Attribute */
-	static double[] readZoneAttribute( int numZ, String filename, TreeMap<Integer,Integer> zonemap ){
+	static double[] readZoneAttribute( final int numZ, final String filename, final Map<Integer,Integer> zonemap ){
 		double[] array = new double[ numZ ];
 		try{
 			LineNumberReader lnr = new LineNumberReader( IOUtils.getBufferedReader(filename) );
 			String line=null;
 			while( (line=lnr.readLine()) != null ){
 				StringTokenizer st = new StringTokenizer( line );
-				Integer zoneid = zonemap.get(Integer.parseInt(st.nextToken()));
-				Double value = new Double(st.nextToken());
+				int zoneid = zonemap.get(Integer.valueOf(st.nextToken())).intValue();
+				double value = Double.parseDouble(st.nextToken());
 				array[ zoneid ] = value;
 			}
 		}
-		catch( Exception xc){
+		catch( IOException xc){
 			xc.printStackTrace();
 		}
 		return array;
 	}
-	
+
 	/* File format: One record Cij per line */
-	static Matrix readMatrix( String filename, int nrow, int ncol ){
+	static Matrix readMatrix( final String filename, final int nrow, final int ncol ){
 		Matrix cij = new Matrix( nrow, ncol );
 		try{
-			LineNumberReader lnr = new LineNumberReader( IOUtils.getBufferedReader(filename) );	
+			LineNumberReader lnr = new LineNumberReader( IOUtils.getBufferedReader(filename) );
 			for( int i=0; i<nrow; i++ )
-				for( int j=0; j<ncol; j++ )					
+				for( int j=0; j<ncol; j++ )
 					cij.set( i, j, Double.parseDouble(lnr.readLine()) );
-					
+
 		}
-		catch( Exception xc){
+		catch( IOException xc){
 			xc.printStackTrace();
 		}
 		return cij;
 	}
-	
-	static HashSet<Integer> readZoneIDs( String zoneFileName ){
+
+	static HashSet<Integer> readZoneIDs( final String zoneFileName ){
 		HashSet<Integer> zoneids = new HashSet<Integer>();
 		try{
 			LineNumberReader lnr = new LineNumberReader( IOUtils.getBufferedReader(zoneFileName) );
 			String line=null;
 			while( (line=lnr.readLine()) != null )
-				zoneids.add(Integer.parseInt(line) );
+				zoneids.add(Integer.valueOf(line) );
 		}
-		catch( Exception xc){
+		catch( IOException xc){
 			xc.printStackTrace();
 			System.exit(-1);
 		}
 		return zoneids;
 	}
-	
-	static void saveResults( double[] X, TreeMap<Integer,Integer> zonemap, String filename ){
-		
+
+	static void saveResults( final double[] X, final Map<Integer,Integer> zonemap, final String filename ){
+
 		double minix=Double.POSITIVE_INFINITY;
 		for( int i=0; i<X.length; i++ )
 			if( X[i] < minix )
 				minix = X[i];
 		try{
-			FileWriter out = new FileWriter( filename ); 
-			for( Integer id : zonemap.keySet() ){
-				int zone = zonemap.get(id);
+			FileWriter out = new FileWriter( filename );
+			for( Map.Entry<Integer, Integer> entry : zonemap.entrySet() ){
+				Integer id = entry.getKey();
+				int zone = entry.getValue().intValue();
 				double rent = Math.abs( X[zone] - minix );
 				out.write( id + "\t" + rent + "\n");
 			}
 			out.close();
 		}
-		catch( Exception xc){
+		catch( IOException xc){
 			xc.printStackTrace();
 		}
 	}
-	
-	
 
-	
-	static Matrix readODMatrix( String  costFileName, int nrow, int ncol ){
+	static Matrix readODMatrix( final String  costFileName, final int nrow, final int ncol ){
 //		readODMatrix method for csv file with ";" separate
 //		File format: origin; destination; travel time;/
 		String inputString = null, value= null;
 		int col = 0;
 		Matrix cij = new Matrix( nrow, ncol );
-		
+
 		try{
 			LineNumberReader lnr = new LineNumberReader( IOUtils.getBufferedReader(costFileName) );
 			for (int i=0; i<nrow; i++)
@@ -126,7 +124,7 @@ public class PrimlocIO {
 						found = inputString.indexOf(";");
 						if (found != -1) {
 							value = inputString.substring(0,found);
-							inputString = inputString.substring(found+1);						
+							inputString = inputString.substring(found+1);
 						}
 						else{
 							value = inputString;
@@ -136,11 +134,11 @@ public class PrimlocIO {
 							if(value.compareTo("")!=0) cij.set( i, j, Double.parseDouble(value));
 							else cij.set(i, j, 0);
 						}
-					}	
+					}
 					col = 0;
 			}
 		}
-		catch( Exception xc){
+		catch( IOException xc){
 			xc.printStackTrace();
 			System.exit(-1);
 		}
