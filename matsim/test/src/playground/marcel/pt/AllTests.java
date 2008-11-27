3a65c6140e8ad907d1467e7c51f6c084cbc9a766
mrieser <rieser@gmail.com>
2008-11-27 14:45:04 +0000

    removed my early trials for new Route hierarchy/structure, as they become now obsolete.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@4184 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/test/src/playground/marcel/pt/AllTests.java
+++ matsim/test/src/playground/marcel/pt/AllTests.java
@@ -29,7 +29,6 @@ public class AllTests {
 		TestSuite suite = new TestSuite("Tests for playground.marcel.pt");
 
 		suite.addTestSuite(VehicleImplTest.class);
-		suite.addTest(playground.marcel.pt.routes.AllTests.suite());
 
 		return suite;
 	}
