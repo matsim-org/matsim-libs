3274a5c0b01794dab5508dd382680b85636bf9b6
mrieser <rieser@gmail.com>
2008-11-28 13:11:15 +0000

    interface for creating routes
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@4216 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/org/matsim/population/routes/RouteFactory.java
@@ -0,0 +1,32 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * RouteFactory.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.population.routes;
+
+/**
+ * Provides a simple factory method to create new Route-objects.
+ * Implement this interface to provide one specific implementation
+ * of Routes.
+ *
+ * @author mrieser
+ */
+public interface RouteFactory {
+	public Route createRoute();
+}
