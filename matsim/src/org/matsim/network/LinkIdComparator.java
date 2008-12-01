78ab22b4911d8f6078f313afe89af8cceb813103
mrieser <rieser@gmail.com>
2008-12-01 16:41:24 +0000

    A small helper class to compare Links by their Id, e.g. in TreeMaps.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@4284 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/org/matsim/network/LinkIdComparator.java
@@ -0,0 +1,37 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * LinkIdComparator.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.network;
+
+import java.util.Comparator;
+
+/**
+ * Compares two links by their Id. A simple helper class so one is
+ * able to put Links in a TreeMap or other sorted data structure.
+ *
+ * @author mrieser
+ */
+public class LinkIdComparator implements Comparator<Link> {
+
+	public int compare(final Link o1, final Link o2) {
+		return o1.getId().compareTo(o2.getId());
+	}
+
+}
