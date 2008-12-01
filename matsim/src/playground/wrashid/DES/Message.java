61a45757ac93a22ab70f0462a2e5a0275af2d312
rashid_waraich <rashid_waraich@users.sf.net>
2008-12-01 12:11:53 +0000

    messageId removed
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@4275 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/wrashid/DES/Message.java
+++ matsim/src/playground/wrashid/DES/Message.java
@@ -2,20 +2,14 @@ package playground.wrashid.DES;
 
 public abstract class Message implements Comparable {
 	
-	private static long messageCounter=0;
 	public double messageArrivalTime=0;
 	public SimUnit sendingUnit;
 	public SimUnit receivingUnit;
-	public long messageType;
-	public long messageId;
 	protected int priority=0;
 	//public String queueKey=""; // only used because of implementation convenience (might be removed in future, if not needed)
 	
 	
 	public Message() {
-		//messageId=messageCounter;
-		//messageCounter++;
-		messageId=0;
 	}
 	
 	public double getMessageArrivalTime() {
