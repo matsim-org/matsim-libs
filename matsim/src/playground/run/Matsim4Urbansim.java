5039c39f1a474d25f10c3fbd98b6ebedd044a3f0
vsp_nagel <2008@kainagel.org>
2008-12-02 05:38:10 +0000

    added playground.run as an entry point for "non-core" code.  I don't know if this a good idea.  But current opus_matsim progress seemed difficult without it.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@4298 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/run/Matsim4Urbansim.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package playground.run;
+
+import java.util.*;
+
+import org.apache.log4j.Logger;
+
+/**
+ * This is a semi-stable entry point into Matsim4Urbansim.
+ * 
+ * @author nagel
+ *
+ */
+public class Matsim4Urbansim {
+	private static final Logger log = Logger.getLogger(Matsim4Urbansim.class);
+
+	public  static void main(String[] args) {
+		playground.kai.urbansim.Matsim4Urbansim.main(args);
+	}
+}
