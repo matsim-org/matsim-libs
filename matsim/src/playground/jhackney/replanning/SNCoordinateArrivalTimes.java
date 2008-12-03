c6d50321c89d67cff41d6a2635cd20dcefab6366
jkhackney <jkhackney@users.sf.net>
2008-12-03 14:13:56 +0000

    Moved test replanning module to playground
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@4367 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/jhackney/replanning/SNCoordinateArrivalTimes.java
@@ -0,0 +1,62 @@
+package playground.jhackney.replanning;
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * SNCoordinateArrivalTimes.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import org.matsim.controler.Controler;
+import org.matsim.facilities.Facilities;
+import org.matsim.facilities.Facility;
+import org.matsim.network.NetworkLayer;
+import org.matsim.population.algorithms.PlanAlgorithm;
+import org.matsim.replanning.modules.MultithreadedModuleA;
+import org.matsim.router.util.TravelCost;
+import org.matsim.router.util.TravelTime;
+import org.matsim.socialnetworks.mentalmap.TimeWindow;
+/**
+ * A sample of schedule coordination in a replanning StrategyModule. The departure
+ * time for an act is adjusted according to when friends will be at the act.
+ *  
+ * @author jhackney
+ *
+ */
+
+public class SNCoordinateArrivalTimes extends MultithreadedModuleA {
+//	private Hashtable<Facility,ArrayList<TimeWindow>> twm = null;
+	private Controler controler;
+
+//    public SNCoordinateArrivalTimes(Hashtable<Facility,ArrayList<TimeWindow>> timeWindowMap) {
+	public SNCoordinateArrivalTimes(Controler controler) {
+
+//    	this.twm=timeWindowMap;
+    	this.controler=controler;
+    }
+
+    public PlanAlgorithm getPlanAlgoInstance() {
+//	return new SNSecLocShortest(factypes, network, tcost, ttime);
+//	return new SNAdjustTimes(this.twm);
+    	return new SNAdjustTimes(this.controler);
+    }
+
+
+}
+
+
