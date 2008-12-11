7d0cf7ea7b9f7f3919545d2e0e2d75a9fda591fc
laemmel <gregor.laemmel@gmail.com>
2008-12-11 14:56:28 +0000

    tool to produce a slide show of plots
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@4535 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/gregor/mviProd/XYLineChartFixedRange.java
@@ -0,0 +1,41 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * XYLineChartFixedRange.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.gregor.mviProd;
+
+import org.matsim.utils.charts.XYLineChart;
+
+public class XYLineChartFixedRange extends XYLineChart {
+
+	public XYLineChartFixedRange(final String title, final String axisLabel,
+			final String axisLabel2, final double maxX, final double maxY) {
+		super(title, axisLabel, axisLabel2);
+		setRange(maxX,maxY);
+	}
+	
+
+
+	protected void setRange(final double maxX, final double maxY) {
+
+		this.chart.getXYPlot().getRangeAxis().setRange(0, maxY);
+		this.chart.getXYPlot().getDomainAxis().setRange(0, maxX);
+	}
+
+}
