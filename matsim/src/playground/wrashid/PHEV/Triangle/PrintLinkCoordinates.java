94ff90f63bcffdaa8af0cdacf016ff9af812ea50
rashid_waraich <rashid_waraich@users.sf.net>
2008-12-12 14:30:25 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@4557 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/wrashid/PHEV/Triangle/PrintLinkCoordinates.java
@@ -0,0 +1,38 @@
+package playground.wrashid.PHEV.Triangle;
+
+import org.matsim.config.Config;
+import org.matsim.gbl.Gbl;
+import org.matsim.network.Link;
+import org.matsim.network.MatsimNetworkReader;
+import org.matsim.network.NetworkLayer;
+
+public class PrintLinkCoordinates {
+
+	public static void main(String[] args) throws Exception {
+		Config config = Gbl.createConfig(args);
+
+		System.out.println("  reading the network...");
+		NetworkLayer network = null;
+		network = (NetworkLayer) Gbl.getWorld().createLayer(
+				NetworkLayer.LAYER_TYPE, null);
+		new MatsimNetworkReader(network).readFile(config.network()
+				.getInputFile());
+		System.out.println("  done.");
+		
+		System.out.println("linkId\tx\ty");
+		for (Link link:network.getLinks().values()){
+			System.out.println(link.getId().toString() +"\t"+ getXCoordinate(link) +"\t"+  getYCoordinate(link));
+		}
+		
+		
+	}
+	
+	public static double getXCoordinate(Link link){
+		return (link.getFromNode().getCoord().getX()+ link.getToNode().getCoord().getX())/2;
+	}
+	
+	public static double getYCoordinate(Link link){
+		return (link.getFromNode().getCoord().getY()+ link.getToNode().getCoord().getY())/2;
+	}
+	
+}
