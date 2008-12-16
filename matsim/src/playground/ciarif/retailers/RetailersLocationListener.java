c8502c144e8f40aaa48806384c10bd5bd08c5bfd
ciarif <ciarif@users.sf.net>
2008-12-16 10:47:19 +0000

    Retailers Module complete, functionality to be tested
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@4582 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/ciarif/retailers/RetailersLocationListener.java
+++ matsim/src/playground/ciarif/retailers/RetailersLocationListener.java
@@ -39,10 +39,19 @@ import org.matsim.facilities.Facilities;
 import org.matsim.facilities.Facility;
 import org.matsim.gbl.Gbl;
 import org.matsim.network.Link;
+import org.matsim.network.NetworkLayer;
+import org.matsim.population.Leg;
+import org.matsim.population.MatsimPopulationReader;
 import org.matsim.population.Person;
 import org.matsim.population.Plan;
+import org.matsim.population.Population;
+import org.matsim.population.PopulationReader;
+import org.matsim.population.PopulationWriter;
 import org.matsim.utils.geometry.Coord;
 import org.matsim.world.algorithms.WorldConnectLocations;
+import org.matsim.router.*;
+import org.matsim.router.costcalculators.FreespeedTravelTimeCost;
+import org.matsim.router.util.PreProcessLandmarks;
 
 
 public class RetailersLocationListener implements IterationStartsListener, BeforeMobsimListener{
@@ -70,44 +79,49 @@ public class RetailersLocationListener implements IterationStartsListener, Befor
 			Facility f = iter_fac.next();
 			Link link = nrl.findLocation();
 			Coord coord = link.getCenter();
-//			System.err.println("disabled code that didn't compile. 12dec2008/marcel");
-//			System.exit(-1);
 			f.moveTo(coord);
+			//f.setLocation(coord);
 			facilities.getFacilities().get(f.getId()).moveTo(coord);
 			this.retailers.getRetailers().put(f.getId(),facilities.getFacilities().get(f.getId()));
 		}
 		this.rs.write(this.retailers);
+		
+		Iterator<Person> pers_iter = controler.getPopulation().getPersons().values().iterator();
 
-		/*Iterator<Person> per_iter = controler.getPopulation().getPersons().values().iterator();
-
-		while (per_iter.hasNext()) {
-			Person person = per_iter.next();
-
-			Iterator<Plan> plan_iter = controler.getPopulation().getPersons().values().iterator().next().getPlans().iterator();
+		while (pers_iter.hasNext()) {
+			
+			Iterator<Plan> plan_iter = pers_iter.next().getPlans().iterator();
 
 			while (plan_iter.hasNext()) {
-
+					
+				Iterator<Leg> leg_iter = plan_iter.next().getIteratorLeg();
+			
+				while (leg_iter.hasNext()) {
+							
+					leg_iter.next().setRoute(null);
+				}
 			}
-			Plan plan = person.getSelectedPlan();
-
-		}*/
+		}
 	}
 
 	public void notifyBeforeMobsim (final BeforeMobsimEvent event) {
-		//TODO: Implement
-		System.out.println("test!!!!! ");
+		
 		Controler controler = event.getControler();
-		Iterator<Person> per_iter = controler.getPopulation().getPersons().values().iterator();
+		Iterator<Person> pers_iter = controler.getPopulation().getPersons().values().iterator();
 
-		//while (per_iter.hasNext()) {
-
-
-			//Plan plan = person.getPlans();
-
-			//plan.
-			//person.removePlan(plan);
-		//}
-		//set routes to null of selected plans
-		//router
-	}
+		while (pers_iter.hasNext()) {
+			
+			final FreespeedTravelTimeCost timeCostCalc = new FreespeedTravelTimeCost();
+			PreProcessLandmarks preprocess = new PreProcessLandmarks(timeCostCalc);
+			NetworkLayer network = controler.getNetwork();
+			preprocess.run(network);
+			PlansCalcRouteLandmarks pcrl = new PlansCalcRouteLandmarks(network, preprocess, timeCostCalc, timeCostCalc);
+			pcrl.run(pers_iter.next());
+			pcrl.run(pers_iter.next().getSelectedPlan());
+			//PlansCalcRouteDijkstra pcrd = new PlansCalcRouteDijkstra(network, timeCostCalc, timeCostCalc);
+			//pcrd.run(pers_iter.next());
+			//pcrd.run(pers_iter.next().getSelectedPlan());
+		}
+	}	
+			
 }
