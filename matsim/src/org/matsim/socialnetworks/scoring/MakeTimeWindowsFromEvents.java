ba90929c369e061aef13c2f42c80eedff43da61a
jkhackney <jkhackney@users.sf.net>
2008-12-17 16:40:22 +0000

    Refactor (rename) EventsPostProcess and add a plans algorithm to forget activities from knowledge.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@4612 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/socialnetworks/scoring/MakeTimeWindowsFromEvents.java
+++ matsim/src/org/matsim/socialnetworks/scoring/MakeTimeWindowsFromEvents.java
@@ -1,10 +1,33 @@
 package org.matsim.socialnetworks.scoring;
-
+/**
+ * 
+ * This class takes a log (Hashmap) of Person and its Startevents and Endevents,
+ * which is an instance of the EventHandler "EventsMapStartEndTimes".
+ * 
+ * It converts the log to TimeWindows, which are maps of who was
+ * at a Facility performing an Activity at the same time.
+ * 
+ * The same TimeWindows could be calculated in one step in an EventHandler
+ * in a MATSIM run, giving cleaner code,
+ * because the Events contain a reference to the Act involved.
+ * 
+ * This is done in the class TrackEventsOverlap.java
+ * 
+ * However, because the Events that are written out do not include the reference
+ * to the Act, but only to the Person, any postprocessing of Events
+ * which require the Act must be done in three steps: 1) Plans are read in, 2)Events
+ * are read in and united with the Plans via the agentId of the Event, 3) the Acts
+ * are re-united with the corresponding Events.
+ * 
+ * To ensure that postprocessing of events and acts gives exactly the same results
+ * as the MATSIM run that generated the events from the acts, use the combination
+ * of EventsMapStartEndTimes and this class.
+ * 
+ * @author jhackney
+ */
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Hashtable;
-
-import javax.swing.text.html.HTMLDocument.Iterator;
 
 import org.apache.log4j.Logger;
 import org.matsim.events.ActEndEvent;
@@ -13,20 +36,17 @@ import org.matsim.facilities.Facility;
 import org.matsim.population.Act;
 import org.matsim.population.Person;
 import org.matsim.population.Plan;
-import org.matsim.population.Population;
-import org.matsim.socialnetworks.algorithms.EventsPostProcess;
+import org.matsim.socialnetworks.algorithms.EventsMapStartEndTimes;
 import org.matsim.socialnetworks.mentalmap.TimeWindow;
 
 public class MakeTimeWindowsFromEvents {
 
 	Hashtable<Facility,ArrayList<TimeWindow>> timeWindowMap=new Hashtable<Facility,ArrayList<TimeWindow>>();
 	static final private Logger log = Logger.getLogger(MakeTimeWindowsFromEvents.class);
-//	EventsPostProcess epp;
 	
 	public MakeTimeWindowsFromEvents(){
-//		this.epp=epp;
 	}
-	public void calculate(EventsPostProcess epp){
+	public void calculate(EventsMapStartEndTimes epp){
 		HashMap<Person, ArrayList<ActStartEvent>> startMap = epp.startMap;
 		HashMap<Person, ArrayList<ActEndEvent>> endMap = epp.endMap;
 		Object[] persons = startMap.keySet().toArray();
