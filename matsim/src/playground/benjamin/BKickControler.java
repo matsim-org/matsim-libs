cf34d30137f0210c701f85da06346012e8bb61e0
grether <dominik.grether@gmail.com>
2008-12-19 15:39:34 +0000

    new scoring function for benjamin
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@4640 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/benjamin/BKickControler.java
@@ -0,0 +1,65 @@
+/* *********************************************************************** *
+ * project: org.matsim.*																															*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.benjamin;
+
+import org.matsim.controler.Controler;
+import org.matsim.run.OTFVis;
+import org.matsim.scoring.ScoringFunctionFactory;
+
+
+/**
+ * @author dgrether
+ *
+ */
+public class BKickControler extends Controler {
+
+	public BKickControler(String configFileName) {
+		super(configFileName);
+	}
+
+	@Override
+	protected ScoringFunctionFactory loadScoringFunctionFactory() {
+		return new BKickScoringFunctionFactory();
+	}
+
+	
+	
+	 public static void main(String[] args){
+			String equilExampleConfig = "examples/equil/configOTF.xml";
+			String oneRouteNoModeTest = "../studies/bkickhoefer/oneRouteNoModeTest/config.xml";
+//			String config = equilExampleConfig;
+			String config = oneRouteNoModeTest;
+			
+			BKickControler c = new BKickControler(config);
+			c.setOverwriteFiles(true);
+			c.run();
+			
+			int lastIteration = c.getConfig().controler().getLastIteration();
+			
+//			String out = c.getConfig().controler().getOutputDirectory() + "/ITERS/it."+lastIteration+"/Snapshot";
+			String out = c.getConfig().controler().getOutputDirectory() + "/ITERS/it."+lastIteration+"/"+lastIteration+".otfvis.mvi";
+			
+			String[] visargs = {out};
+			
+			OTFVis.main(new String[] {out});	
+	 }
+
+	
+	
+}
---
+++ matsim/src/playground/benjamin/BKickScoringFunctionFactory.java
@@ -0,0 +1,39 @@
+/* *********************************************************************** *
+ * project: org.matsim.*																															*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.benjamin;
+
+import org.matsim.population.Plan;
+import org.matsim.scoring.ScoringFunction;
+import org.matsim.scoring.ScoringFunctionFactory;
+
+
+/**
+ * @author dgrether
+ *
+ */
+public class BKickScoringFunctionFactory implements ScoringFunctionFactory {
+
+	/**
+	 * @see org.matsim.scoring.ScoringFunctionFactory#getNewScoringFunction(org.matsim.population.Plan)
+	 */
+	public ScoringFunction getNewScoringFunction(Plan plan) {
+		return new BKickScoringFunction(plan);
+	}
+
+}
