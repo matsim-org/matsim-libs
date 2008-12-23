ad1e0aa0ced1a2acdecedd4573d8892211b39cbb
rashid_waraich <rashid_waraich@users.sf.net>
2008-12-23 13:47:31 +0000

    performance tests on lists
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@4686 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/wrashid/tryouts/performance/Lists.java
@@ -0,0 +1,89 @@
+package playground.wrashid.tryouts.performance;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Lists {
+	public static void main(String[] args) {
+		
+
+		// new ArrayList<Integer>(0): 8250 ms
+		// new ArrayList<Integer>(20000000): 4422 ms
+		//testArrayListAdd();
+		
+		// 17814 ms
+		//testLinkedListAdd();
+		
+		// -----------------------------------------------
+		
+		// 204 ms
+		//testArrayListGet (testArrayListAdd());
+		
+		// infinity...
+		//testLinkedListGet (testLinkedListAdd());
+
+		//-----------------------------------------------
+		
+		// new ArrayList<Integer>(0): 2734 ms
+		// new ArrayList<Integer>(20000000): 2703 ms
+		//testArrayListAddAll(testArrayListAdd());
+
+		// infinity... / OutOfMemory
+		testLinkedListAddAll (testLinkedListAdd());
+		
+	}
+
+	public static ArrayList<Integer> testArrayListAdd() {
+		long time = System.currentTimeMillis();
+		ArrayList<Integer> list = new ArrayList<Integer>(20000000);
+
+		for (int i = 0; i < 20000000; i++) {
+			list.add(i);
+		}
+		System.out.println("time [ms]: " + (System.currentTimeMillis() - time));
+		return list;
+	}
+	
+	public static LinkedList<Integer> testLinkedListAdd() {
+		long time = System.currentTimeMillis();
+		LinkedList<Integer> list = new LinkedList<Integer>();
+
+		for (int i = 0; i < 20000000; i++) {
+			list.add(i);
+		}
+		System.out.println("time [ms]: " + (System.currentTimeMillis() - time));
+		return list;
+	}
+	
+	public static void testArrayListGet(ArrayList<Integer> list){
+		long time = System.currentTimeMillis();
+		for (int i=0;i<list.size();i++){
+			list.get(i);
+		}
+		System.out.println("time [ms]: " + (System.currentTimeMillis() - time));
+	}
+	
+	public static void testLinkedListGet(LinkedList<Integer> list){
+		long time = System.currentTimeMillis();
+		for (int i=0;i<list.size();i++){
+			list.get(i);
+		}
+		System.out.println("time [ms]: " + (System.currentTimeMillis() - time));
+	}
+	
+	public static void testArrayListAddAll(ArrayList<Integer> list){
+		long time = System.currentTimeMillis();
+		ArrayList<Integer> list1 = new ArrayList<Integer>(20000000);
+		list1.addAll(list);
+		System.out.println("time [ms]: " + (System.currentTimeMillis() - time));
+	}
+	
+	public static void testLinkedListAddAll(LinkedList<Integer> list){
+		long time = System.currentTimeMillis();
+		LinkedList<Integer> list1 = new LinkedList<Integer>();
+		list1.addAll(list);
+		System.out.println("time [ms]: " + (System.currentTimeMillis() - time));
+	}
+	
+
+}
