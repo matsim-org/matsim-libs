ed9a6526d4f6b4ed8e75d7cf68446aec66bd58f9
grether <dominik.grether@gmail.com>
2009-01-03 16:03:26 +0000

    converter utility for conversion of the different matsim xml formats
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@4756 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/org/matsim/utils/io/MatsimXMLFormatConverter.java
@@ -0,0 +1,55 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * PopulationConverterV4ToV5
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package org.matsim.utils.io;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.matsim.gbl.Gbl;
+import org.matsim.network.MatsimNetworkReader;
+import org.matsim.network.NetworkLayer;
+import org.matsim.population.MatsimPopulationReader;
+import org.matsim.population.Population;
+import org.matsim.population.PopulationWriterV5;
+
+
+/**
+ * This class provides helper methods to convert the
+ * different MATSim XML versions to other versions. 
+ *
+ * @author dgrether
+ *
+ */
+public class MatsimXMLFormatConverter {
+	
+	public static void convertPopulationV4ToV5(String popv4, String popv5, String network) throws FileNotFoundException, IOException{
+		NetworkLayer net = new NetworkLayer();
+		MatsimNetworkReader reader = new MatsimNetworkReader(net);
+		reader.readFile(network);
+		Gbl.getWorld().setNetworkLayer(net);
+		
+		Population pop = new Population(Population.NO_STREAMING);
+		MatsimPopulationReader popreader = new MatsimPopulationReader(pop, net);
+		popreader.readFile(popv4);
+		
+		PopulationWriterV5 writer = new PopulationWriterV5(pop);
+		writer.writeFile(popv5);
+	}
+}
