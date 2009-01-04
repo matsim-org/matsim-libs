ac1433156c759a367dc641eeb67a398e9b2a3fe9
jkhackney <jkhackney@users.sf.net>
2009-01-04 13:48:50 +0000

    Setting "multiple" <= zero permits no removal of knowledge at all.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@4763 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/socialnetworks/algorithms/PersonForgetKnowledge.java
+++ matsim/src/org/matsim/socialnetworks/algorithms/PersonForgetKnowledge.java
@@ -3,7 +3,7 @@ package org.matsim.socialnetworks.algorithms;
  * The rule for reducing the number of Locations a Person has in memory. Removes
  * Locations in excess of multiple*number of activities in all plans. An agent
  * with X plans that have Y activities each must retain X * Y Locations but may
- * retain up to X * Y * multiple locations. If multiple == 0, no Locations are
+ * retain up to X * Y * multiple locations. If multiple <= 0, no Locations are
  * removed from memory.
  * 
  * @author jhackney
