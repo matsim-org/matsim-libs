28aa5e044de9dee7f7402025a0add398d4174384
jwjoubert <johan.joubert@up.ac.za>
2009-01-30 19:41:17 +0000

    New code to split commercial vehicle files for Gauteng, South Africa
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@4986 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/jjoubert/CommercialTraffic/ProcessVehicles.java
@@ -0,0 +1,75 @@
+package playground.jjoubert.CommercialTraffic;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ProcessVehicles {
+	
+	// main method
+	public static void main( String[] args) throws IOException{
+		Scanner input = new Scanner( new BufferedReader( new FileReader( 
+				new File( "/Volumes/Data/DigiCore/Poslog_Research_Data.csv") ) ) );
+			
+		int numRecords = 1;
+		
+		if( input.hasNextLine() ){			
+			
+			// Read the first line, and split the fields based on the comma separator.
+			String [] inputString = input.nextLine().split(",");		
+						
+			// Open the file for the vehicle 
+			String vehFile = "/Volumes/Data/DigiCore/VehicleFiles/" + inputString[0] + ".txt";
+			BufferedWriter output = new BufferedWriter(new FileWriter(vehFile, true) , 100000 );
+			
+			// Write the record to the associated file
+			String outputString = 	inputString[0] + " " + // vehID
+									inputString[1] + " " + // time
+									inputString[2] + " " + // xCoord 
+									inputString[3] + " " + // yCoord 
+									inputString[4] + " " + // status 
+									inputString[5];        // speed
+			output.write(outputString);
+			String vehID = inputString[0];
+
+			while ( input.hasNextLine() ){
+				numRecords += 1;
+				
+				// Read the next input line
+				inputString = input.nextLine().split(",");
+								
+				if ( !vehID.equals(inputString[0]) ) {
+					// Close the file for the current vehicle.
+					output.close();
+					
+					// Open the file for the new vehicle .
+					vehFile = "/Volumes/Data/DigiCore/VehicleFiles/" + inputString[0] + ".txt";
+					output = new BufferedWriter(new FileWriter(vehFile, true) , 100000 );			
+				}		
+				// Write the record to the new file
+				outputString = 	inputString[0] + " " + // vehID
+								inputString[1] + " " + // time
+								inputString[2] + " " + // xCoord 
+								inputString[3] + " " + // yCoord 
+								inputString[4] + " " + // status 
+								inputString[5];        // speed
+				output.write(outputString);
+				output.newLine();
+
+				vehID = inputString[0];	
+			}
+		}					
+		
+		if( input != null ) {
+			input.close();
+		}
+		
+		String outputString = "There was a total of " + numRecords + " log records.";
+		System.out.println( outputString );
+	}
+
+}
---
+++ matsim/src/playground/jjoubert/CommercialTraffic/SelectGautengVehicles.java
@@ -0,0 +1,135 @@
+package playground.jjoubert.CommercialTraffic;
+/*
+ * This class searches through the separated DigiCore vehicle files, identifies which
+ * vehicles travel through Gauteng, and moves the files into a separate folder as soon
+ * as a point is found within the study area.
+ */
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.geotools.data.FeatureSource;
+import org.geotools.feature.Feature;
+import org.matsim.utils.gis.ShapeFileReader;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
+
+public class SelectGautengVehicles {
+
+	public static void main(String[] args) {
+		long startTime = System.currentTimeMillis();
+		Polygon gauteng = readGautengPolygon();
+		System.out.print("Initializing... ");
+		
+		File vehicleFolder = new File("/Users/johanwjoubert/MATSim/workspace/MATSimData/VehicleFiles/");
+		File gautengFolder = new File("/Users/johanwjoubert/MATSim/workspace/MATSimData/VehicleFiles/GautengVehicles");
+		gautengFolder.mkdir();
+		final int files = countVehicleFiles(vehicleFolder);
+		int inFiles = 0;
+		int outFiles = 0;
+		
+		File vehicleFile[] = vehicleFolder.listFiles();
+		System.out.println("Done\n" );
+		
+		System.out.println("Starting to process vehicle files.");
+		for (int i = 0; i < vehicleFile.length; i++ ) {
+			if( (vehicleFile[i].isFile()) && !(vehicleFile[i].getName().equalsIgnoreCase(".DS_Store"))){
+				boolean gautengStatus = getVehicleStatus(gauteng, vehicleFile[i] );
+				if (gautengStatus == true){
+					moveVehicleFile(gautengFolder, vehicleFile[i] );
+					inFiles++;
+				} else{
+					outFiles++;
+				}
+				
+				// Report on progress
+				if(((int)(i/100)) == Math.round(((float)i)/100) ){
+					float progress = ((float) i/(files-2)*100);
+					System.out.printf("Progress: %3.0f %% \t%d of a possible %d files already identified\n", 
+							progress, inFiles, (files-2) );		
+				}
+			}
+		}
+		long endTime = System.currentTimeMillis();
+		long seconds = 1000*(endTime - startTime);
+		System.out.println("\nDone");
+		System.out.println("           Total number of files: " + (inFiles + outFiles) );
+		System.out.println("Total number of files in Gauteng: " + inFiles );
+		
+		System.out.printf ("\n                      Total time: %d", seconds);
+	}
+
+	private static boolean getVehicleStatus(Polygon polygon, File file) {
+		boolean inStatus = false;		
+		try {
+			Scanner input = new Scanner(new BufferedReader(new FileReader(file) ) );
+			
+			while((input.hasNextLine() ) & !(inStatus) ){
+				String [] inputString = input.nextLine().split(" ");
+				double x = Double.valueOf(inputString[2]).doubleValue();
+				double y = Float.valueOf(inputString[3]).doubleValue();
+				Coordinate coord = new Coordinate(x,y);
+				GeometryFactory gf = new GeometryFactory();
+				Point p = gf.createPoint(coord);
+				
+				inStatus = testPolygon(polygon, p);				
+			}		
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}	
+		return inStatus;
+	}
+
+	public static int countVehicleFiles(File folder) {
+		int numberoffiles = folder.list().length;
+		return numberoffiles;
+	}
+
+	public static boolean moveVehicleFile(File theFolder, File theFile) {
+		String newFileName = theFolder.getAbsolutePath() + "/" + theFile.getName();
+		File newFile = new File(newFileName);
+		boolean moveResult = theFile.renameTo(newFile);
+		return moveResult;
+	}
+
+	public static boolean testPolygon(Polygon polygon, Point point) {
+		boolean inPolygon = polygon.contains(point);		
+		return inPolygon;
+	}
+	
+	public static Polygon readGautengPolygon() {
+		
+		String fileName = "/Users/johanwjoubert/MATSim/workspace/MATSimData/ShapeFiles/Gauteng/Gauteng.shp";
+		FeatureSource fs = null;
+		MultiPolygon mp = null;
+		Polygon p = null;
+		try {	
+			fs = ShapeFileReader.readDataFile(fileName);
+			for(Object o: fs.getFeatures() ){
+				Geometry geo = ((Feature)o).getDefaultGeometry();
+				if(geo instanceof MultiPolygon){
+					mp = (MultiPolygon)geo;
+				}
+			}
+			int numGeo = mp.getNumGeometries();
+			if(numGeo > 1 ){
+				System.out.println("There are multiple polygons...");
+			} else{
+				p = ((Polygon)mp.getGeometryN(0));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return p;
+	}
+	
+}
