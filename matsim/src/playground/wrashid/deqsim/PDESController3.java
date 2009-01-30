1ee4a86048ee36cf9158eadca531a48de16dca53
rashid_waraich <rashid_waraich@users.sf.net>
2009-01-30 17:35:19 +0000

    base version, not finished.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@4979 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/wrashid/deqsim/PDESController3.java
@@ -0,0 +1,24 @@
+package playground.wrashid.deqsim;
+
+import org.matsim.controler.Controler;
+
+import playground.wrashid.DES.util.Timer;
+
+public class PDESController3 extends Controler {
+	public PDESController3(final String[] args) {
+	    super(args);
+	  }
+
+	protected void runMobSim() {
+		
+		new JavaPDEQSim3(this.network, this.population, this.events).run();
+	}
+
+	public static void main(final String[] args) {
+		Timer t=new Timer();
+		t.startTimer();
+		new PDESController3(args).run();
+		t.endTimer();
+		t.printMeasuredTime("Time needed for PDESController run: ");
+	}
+}
