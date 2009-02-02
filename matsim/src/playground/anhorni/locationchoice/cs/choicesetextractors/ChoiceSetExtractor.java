0db7cd063e498f6eb1d49d4323a502058a01ac00
andreas77 <andreashorni@hotmail.com>
2009-02-02 18:28:50 +0000

    removed TravelTimeBudget
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@5020 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/anhorni/locationchoice/cs/choicesetextractors/ChoiceSetExtractor.java
+++ matsim/src/playground/anhorni/locationchoice/cs/choicesetextractors/ChoiceSetExtractor.java
@@ -4,21 +4,17 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.TreeMap;
-import java.util.Vector;
-
 import org.apache.log4j.Logger;
 import org.matsim.basic.v01.Id;
 import org.matsim.controler.Controler;
 import playground.anhorni.locationchoice.cs.helper.ChoiceSet;
 import playground.anhorni.locationchoice.cs.helper.SpanningTree;
-import playground.anhorni.locationchoice.cs.helper.TravelTimeBudget;
 import playground.anhorni.locationchoice.cs.helper.ZHFacility;
 
 public abstract class ChoiceSetExtractor {
 		
 	protected TreeMap<Id, ArrayList<ZHFacility>> zhFacilitiesByLink = null;
 	private Controler controler = null;
-	private List<TravelTimeBudget> budgets =  new Vector<TravelTimeBudget>();
 	private List<ChoiceSet> choiceSets;
 	
 	private final static Logger log = Logger.getLogger(ChoiceSetExtractor.class);
@@ -36,13 +32,8 @@ public abstract class ChoiceSetExtractor {
 			
 		Iterator<ChoiceSet> choiceSet_it = choiceSets.iterator();
 		while (choiceSet_it.hasNext()) {
-			ChoiceSet choiceSet = choiceSet_it.next();	
-			
-			log.info("Computing choice set: " + choiceSet.getId().toString());
-			
-			this.budgets.add(new TravelTimeBudget(choiceSet.getId(), choiceSet.getTravelTimeBudget(), 
-					choiceSet.getTrip().getTripNr()));
-							
+			ChoiceSet choiceSet = choiceSet_it.next();			
+			log.info("Computing choice set: " + choiceSet.getId().toString());							
 			this.computeChoiceSet(choiceSet, spanningTree, type, this.controler);
 		}		
 	}
@@ -50,13 +41,6 @@ public abstract class ChoiceSetExtractor {
 	protected abstract void computeChoiceSet(ChoiceSet choiceSet, SpanningTree spanningTree, String type,
 			Controler controler);
 		
-	public List<TravelTimeBudget> getBudgets() {
-		return budgets;
-	}
-	public void setBudgets(List<TravelTimeBudget> budgets) {
-		this.budgets = budgets;
-	}
-
 	public List<ChoiceSet> getChoiceSets() {
 		return choiceSets;
 	}
