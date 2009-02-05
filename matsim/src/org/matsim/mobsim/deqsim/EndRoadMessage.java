8aa47999fb0cfec9ca4ea4b45c81cc4b13ea373c
rashid_waraich <rashid_waraich@users.sf.net>
2009-02-05 09:49:59 +0000

    comment refactoring.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@5123 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/mobsim/deqsim/EndRoadMessage.java
+++ matsim/src/org/matsim/mobsim/deqsim/EndRoadMessage.java
@@ -24,23 +24,24 @@ public class EndRoadMessage extends EventMessage {
 	@Override
 	public void handleMessage() {
 		if (vehicle.isCurrentLegFinished()) {
-			// the leg is completed, try to enter the last link but do not enter
-			// it
-			// (just wait, until you have clearance for enter and then leave the
-			// road)
+			/*
+			 * the leg is completed, try to enter the last link but do not enter
+			 * it (just wait, until you have clearance for enter and then leave
+			 * the road)
+			 */
 
 			vehicle.initiateEndingLegMode();
 			vehicle.moveToFirstLinkInNextLeg();
 			Road road = Road.getRoad(vehicle.getCurrentLink().getId()
 					.toString());
-			road.enterRequest(vehicle,getMessageArrivalTime());
+			road.enterRequest(vehicle, getMessageArrivalTime());
 		} else if (!vehicle.isCurrentLegFinished()) {
 			// if leg is not finished yet
 			vehicle.moveToNextLinkInLeg();
 
 			Road nextRoad = Road.getRoad(vehicle.getCurrentLink().getId()
 					.toString());
-			nextRoad.enterRequest(vehicle,getMessageArrivalTime());
+			nextRoad.enterRequest(vehicle, getMessageArrivalTime());
 		}
 	}
 
