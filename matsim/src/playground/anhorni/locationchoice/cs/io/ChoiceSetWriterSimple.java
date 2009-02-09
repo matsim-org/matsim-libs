019498311f21af6c8a0129ae98747505225554f2
andreas77 <andreashorni@hotmail.com>
2009-02-09 16:11:22 +0000

    changed output
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@5194 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/anhorni/locationchoice/cs/io/ChoiceSetWriterSimple.java
+++ matsim/src/playground/anhorni/locationchoice/cs/io/ChoiceSetWriterSimple.java
@@ -24,7 +24,7 @@ public class ChoiceSetWriterSimple extends CSWriter {
 	public void write(String outdir, String name,List<ChoiceSet> choiceSets)  {
 		
 		String outfile = outdir + name + "_ChoiceSets.txt";
-		String outfile_alternatives = outdir + name + "_NumberOfAlternatives.txt";
+		String outfile_alternatives = outdir + name + "_NumberOfAlternativesInclusive.txt";
 		
 		if (!super.checkBeforeWriting(choiceSets)) {
 			log.warn(outfile +" not created");
@@ -38,7 +38,7 @@ public class ChoiceSetWriterSimple extends CSWriter {
 			final BufferedWriter out_alternatives = IOUtils.getBufferedWriter(outfile_alternatives);
 			out.write(header);
 			out.newLine();
-			out_alternatives.write("Id\tNumber of alternatives");
+			out_alternatives.write("Id\tNumber of alternatives (includes the chosen facility)");
 			out_alternatives.newLine();			
 			
 			Iterator<ChoiceSet> choiceSet_it = choiceSets.iterator();
