dcfb2fc34aaf4f25edd52c6e2772522d18d4946e
rashid_waraich <rashid_waraich@users.sf.net>
2009-02-10 15:50:16 +0000

    test case added.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@5204 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/test/src/org/matsim/mobsim/deqsim/ConfigParameterTest.java
@@ -0,0 +1,17 @@
+package org.matsim.mobsim.deqsim;
+
+import org.matsim.controler.Controler;
+import org.matsim.events.Events;
+import org.matsim.testcases.MatsimTestCase;
+
+public class ConfigParameterTest extends MatsimTestCase {
+
+	public void testParametersSetCorrectly(){
+		String args[]=new String[]{"test/input/org/matsim/mobsim/deqsim/config.xml"};
+		Controler controler=new Controler(args);
+		controler.setOverwriteFiles(true);
+		controler.run();
+	}
+	
+	
+}
