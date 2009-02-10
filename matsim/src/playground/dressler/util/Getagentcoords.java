75a4b46a84814a476fd9e2f5225830290244f142
DanielDressler <DanielDressler@users.sf.net>
2009-02-10 14:55:29 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@5199 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/dressler/util/Getagentcoords.java
+++ matsim/src/playground/dressler/util/Getagentcoords.java
@@ -60,11 +60,14 @@ public class Getagentcoords {
 
 	public static void main(final String[] args) {
 		// choose instance
-		final String netFilename = "/homes/combi/dressler/V/Project/padang/network/padang_net_evac.xml";
-		//final String plansFilename = "/homes/combi/dressler/V/Project/padang/plans/padang_plans_10p.xml.gz";
-		final String plansFilename = "/homes/combi/dressler/V/code/workspace/matsim/examples/meine_EA/padangplans.xml";
+		final String netFilename = "/homes/combi/dressler/V/Project/code/matsim/examples/meine_EA/siouxfalls_network_5s.xml";
+		//final String netFilename = "/homes/combi/dressler/V/Project/padang/network/padang_net_evac.xml";
 		
-		final String outputPngFilename = "/homes/combi/dressler/V/code/workspace/matsim/output/exitmap.png";
+		//final String plansFilename = "/homes/combi/dressler/V/Project/padang/plans/padang_plans_10p.xml.gz";
+		//final String plansFilename = "/homes/combi/dressler/V/code/workspace/matsim/examples/meine_EA/padangplans.xml";
+		final String plansFilename = "/homes/combi/dressler/V/code/workspace/matsim/examples/meine_EA/siouxfalls_5s.xml";
+		
+		final String outputPngFilename = "/homes/combi/dressler/V/code/workspace/matsim/output/exitmap_SF_5s.png";
 		boolean planstats = true;
 
 		@SuppressWarnings("unused")
@@ -82,32 +85,36 @@ public class Getagentcoords {
 
 
 		// get evac links
-		Node evac1node = network.getNode("en1");
+		Node evac1node = network.getNode("20");
 		Map<Id,? extends Link> evaclinks = null;		
 		if (evac1node != null) {
 			evaclinks = evac1node.getInLinks();
-			for (Link link : evaclinks.values()) {
+			/*for (Link link : evaclinks.values()) {
 				System.out.println(link.getId().toString());
-			}
+			}*/
 		}
 
 		// colours
-		Map<Id,Color> colours = new HashMap<Id, Color>();
+		Map<String,Color> colours = new HashMap<String, Color>();
 		Color noexit = Color.BLACK;
 
 		if (evaclinks != null) {
 			for (Link link : evaclinks.values()) {
-				colours.put(link.getId(), Color.getHSBColor(colours.size()/evaclinks.size(),1.0f,1.0f));
+				colours.put(link.getId().toString(), Color.getHSBColor((float) ((colours.size()*7) % evaclinks.size() )/ (float) evaclinks.size(),1.0f,1.0f));
+				//colours.put(link.getId(), Color.getHSBColor(0.3f,1.0f,1.0f));
+				//System.out.println(colours.get(link.getId().toString()));
 			}		   			
 		}
 
 		// create chosen-exit map		
 		if (planstats) {			
-			Double minc = +500000000d;
-			Double maxc = -500000000d;
-
-			int width = 300;
-			int height = 300;
+			Double minx = +500000000d;
+			Double maxx = -500000000d;
+			Double miny = +500000000d;
+			Double maxy = -500000000d;
+			
+			int width = 400;
+			int height = 400;
 			BufferedImage image = new BufferedImage(width,
 					height,BufferedImage.TYPE_INT_ARGB);
 			Graphics2D g2D = image.createGraphics(); 
@@ -117,41 +124,61 @@ public class Getagentcoords {
 				// p.setVisualizerData(visualizerData)
 				if (plan == null) continue;
 
-				Coord c = plan.getFirstActivity().getLink().getFromNode().getCoord();
+				 Id i = plan.getFirstActivity().getLinkId();
+				 Link l = network.getLink(i);
+				 Coord c = l.getFromNode().getCoord();
 				//Coord c = plan.getFirstActivity().getCoord();
 				//if (c == null) continue; // why would this happen? but happens ...
-				minc = Math.min(c.getX(), minc);
-				minc = Math.min(c.getY(), minc);
-				maxc = Math.max(c.getX(), maxc);
-				maxc = Math.max(c.getY(), maxc);
+				minx = Math.min(c.getX(), minx);
+				miny = Math.min(c.getY(), miny);
+				maxx = Math.max(c.getX(), maxx);
+				maxy = Math.max(c.getY(), maxy);
 			}
 
-			Double scale = ((double) width) / (maxc - minc)*0.98;			
-			Double offset = -minc*scale + 3;
+			Double scalex = ((double) width) / (maxx - minx)*0.98;
+			Double scaley = -((double) height) / (maxy - miny)*0.98;
+			Double offsetx = -minx*scalex + 3;
+			Double offsety = -maxy*scaley + 3;
 
-			System.out.println("minc "+ minc + " maxc " + maxc);
+			System.out.println("minx "+ minx + " maxx " + maxx);
+			System.out.println("miny "+ miny + " maxy " + maxy);
+			
+			// print exits ...
+			for (Link link : evaclinks.values()) {
+				Coord c = link.getFromNode().getCoord();
+				Double x = c.getX() * scalex + offsetx;
+				Double y = c.getY() * scaley + offsety;					  
+				int X = x.intValue();
+				int Y = y.intValue();
+				g2D.setColor(colours.get(link.getId().toString()));
+				g2D.fillOval(X-8, Y-8, 16, 16);
+			}
+
 			Integer foundpeople = 0;
+			Integer notfoundpeople = 0;			
 			for (Person p : population.getPersons().values()) {
 				Plan plan = p.getSelectedPlan();
 				// p.setVisualizerData(visualizerData)
-				if (plan == null) continue;
+				if (plan == null) {notfoundpeople++; continue;}
 
-				Coord c = plan.getFirstActivity().getLink().getFromNode().getCoord();
+				 Id i = plan.getFirstActivity().getLinkId();
+				 Link l = network.getLink(i);
+				 Coord c = l.getFromNode().getCoord();
 				
 				//if (c == null) continue; // why would this happen? but happens ...
 				
 				//System.out.println(c.getX() + " " + c.getY());
 				Leg leg = plan.getNextLeg(plan.getFirstActivity());
-				if (leg == null || evaclinks == null) {
-					//System.out.println(-99);								
+				if (leg == null || evaclinks == null) {												
+					notfoundpeople++;
 					continue;
 				}
 
-				Double x = c.getX() * scale + offset;
-				Double y = c.getY() * scale + offset;					  
+				Double x = c.getX() * scalex + offsetx;
+				Double y = c.getY() * scaley + offsety;					  
 				int X = x.intValue();
 				int Y = y.intValue();
-				//System.out.println(X + " " + Y);
+				//System.out.println(x + " " + y);
 
 
 				boolean found = false; 
@@ -160,16 +187,27 @@ public class Getagentcoords {
 						found = true;
 						//System.out.println("Juhu " + id);
 						foundpeople++;
-						g2D.setColor(colours.get(id));
-						g2D.drawOval(X, Y, 1, 1);
+						g2D.setColor(colours.get(id.toString()));
+						g2D.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.1f));
+						//g2D.drawLine(X, Y, X, Y);
+						g2D.fillOval(X-2, Y-2, 5, 5);
+						//g2D.fillOval(X-1, Y-1, 2, 2);
+						//g2D.drawOval(X, Y, 1, 1);
 					}
 				}
 				if (!found) {
+					notfoundpeople++;
 					g2D.setColor(noexit);
-					g2D.drawOval(X, Y, 1, 1);
+					//g2D.drawLine(X, Y, X, Y);
+					g2D.fillRect(X-1, Y-1, 2, 2);
 				}
+				
+			
+				
 			}
-			System.out.println(foundpeople);
+			
+			
+			System.out.println("Found: " + foundpeople +  " not found: " + notfoundpeople);
 			File pngfile = new File(outputPngFilename);
 			try {
 				ImageIO.write(image, "png", pngfile);
