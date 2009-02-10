2c12d097ca2e51c76e8287067428d88507010ac6
rashid_waraich <rashid_waraich@users.sf.net>
2009-02-10 18:53:35 +0000

    unused imports removed.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@5218 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/test/src/playground/wrashid/deqsim/TestPDESStarter2_EquilPopulationPlans1Modified1.java
+++ matsim/test/src/playground/wrashid/deqsim/TestPDESStarter2_EquilPopulationPlans1Modified1.java
@@ -1,20 +1,4 @@
 package playground.wrashid.deqsim;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-
-import org.matsim.events.AgentArrivalEvent;
-import org.matsim.events.AgentDepartureEvent;
-import org.matsim.events.LinkEnterEvent;
-import org.matsim.events.LinkLeaveEvent;
-import org.matsim.events.PersonEvent;
-import org.matsim.gbl.Gbl;
-import org.matsim.mobsim.deqsim.EventLog;
-import org.matsim.mobsim.deqsim.scenarios.EquilPopulationPlans1Modified1;
-import org.matsim.mobsim.deqsim.util.CppEventFileParser;
-import org.matsim.mobsim.deqsim.util.DEQSimEventFileComparator;
-import org.matsim.mobsim.deqsim.util.DEQSimEventFileTravelTimeComparator;
-import org.matsim.mobsim.deqsim.util.TestHandlerDetailedEventChecker;
 import org.matsim.testcases.MatsimTestCase;
 
 
