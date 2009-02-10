31bf1e335561cebc9c7e60c1a957431a5aa84358
rashid_waraich <rashid_waraich@users.sf.net>
2009-02-10 15:03:29 +0000

    name of test package verändert.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@5200 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/test/src/org/matsim/mobsim/deqsim/AllTests.java
+++ matsim/test/src/org/matsim/mobsim/deqsim/AllTests.java
@@ -26,7 +26,7 @@ import junit.framework.TestSuite;
 public class AllTests {
 
 	public static Test suite() {
-		TestSuite suite = new TestSuite("Tests for org.matsim.mobsim.queuesim");
+		TestSuite suite = new TestSuite("Tests for org.matsim.mobsim.deqsim");
 		//$JUnit-BEGIN$
 		
 		suite.addTestSuite(TestDESStarter_EquilPopulationPlans1Modified1.class);
