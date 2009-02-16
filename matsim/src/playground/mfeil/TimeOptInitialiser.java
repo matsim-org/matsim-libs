61d396c1768d532b7fed662e8c80852fd07594cb
Matthias_Feil <Matthias_Feil@users.sf.net>
2009-02-16 18:49:15 +0000

    New argument in constructor
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@5335 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/mfeil/TimeOptInitialiser.java
+++ matsim/src/playground/mfeil/TimeOptInitialiser.java
@@ -24,6 +24,7 @@ import org.matsim.population.algorithms.PlanAlgorithm;
 import org.matsim.replanning.modules.*;
 import org.matsim.scoring.ScoringFunctionFactory;
 import org.matsim.scoring.PlanScorer;
+import org.matsim.controler.Controler;
 
 
 /**
@@ -36,20 +37,22 @@ public class TimeOptInitialiser extends MultithreadedModuleA{
 	private final PlanScorer 				scorer;
 	private final LegTravelTimeEstimator	estimator;
 	private final ScoringFunctionFactory	factory;
+	private final Controler					controler;
 
 	
-	public TimeOptInitialiser (LegTravelTimeEstimator estimator, ScoringFunctionFactory factory) {
+	public TimeOptInitialiser (Controler controler, LegTravelTimeEstimator estimator, ScoringFunctionFactory factory) {
 		this.scorer = new PlanScorer(factory);
 		this.estimator = estimator;
 		this.factory = factory;
+		this.controler = controler;
 	}
 
 	
 	@Override
 	public PlanAlgorithm getPlanAlgoInstance() {		
 
-		//PlanAlgorithm timeOptAlgorithm = new TimeOptimizerPerformanceT (this.estimator, this.scorer, this.factory);
-		PlanAlgorithm timeOptAlgorithm = new TimeOptimizer14 (this.estimator, this.scorer);
+		PlanAlgorithm timeOptAlgorithm = new TimeOptimizerPerformanceT (this.controler, this.estimator, this.scorer, this.factory);
+		//PlanAlgorithm timeOptAlgorithm = new TimeOptimizer14 (this.estimator, this.scorer);
 
 		return timeOptAlgorithm;
 	}
