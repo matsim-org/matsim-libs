3eafcf62ba08c2045a35f2a73380deca6cbd7ee7
Matthias_Feil <Matthias_Feil@users.sf.net>
2009-02-16 18:50:09 +0000

    Update in TimeOptimizerPerformanceTest (now including routing)
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@5336 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/mfeil/TimeOptimizerPerformanceT.java
+++ matsim/src/playground/mfeil/TimeOptimizerPerformanceT.java
@@ -4,6 +4,9 @@ package playground.mfeil;
 import org.matsim.planomat.Planomat;
 import org.matsim.planomat.costestimators.LegTravelTimeEstimator;
 import org.matsim.population.algorithms.PlanAlgorithm;
+import org.matsim.router.PlansCalcRouteLandmarks;
+import org.matsim.router.costcalculators.FreespeedTravelTimeCost;
+import org.matsim.router.util.PreProcessLandmarks;
 import org.matsim.scoring.*;
 import org.matsim.population.Plan;
 import org.matsim.controler.Controler;
@@ -18,91 +21,103 @@ public class TimeOptimizerPerformanceT implements org.matsim.population.algorith
 	
 	private final PlanAlgorithm 	timeOptAlgorithm;
 	private final PlanScorer		scorer;
+	private final PlansCalcRouteLandmarks 	router;
+	private final PreProcessLandmarks		preProcessRoutingData;
 	
-	public TimeOptimizerPerformanceT (LegTravelTimeEstimator estimator, PlanScorer scorer, ScoringFunctionFactory factory){
-		this.timeOptAlgorithm = new TimeOptimizer14 (estimator, scorer);
-		//this.timeOptAlgorithm = new PlanOptimizeTimes (estimator, factory);
-		this.scorer			  = scorer;
+	public TimeOptimizerPerformanceT (Controler controler, LegTravelTimeEstimator estimator, PlanScorer scorer, ScoringFunctionFactory factory){
+		this.timeOptAlgorithm 		= new TimeOptimizer11 (estimator, scorer);
+		//this.timeOptAlgorithm 	= new Planomat (estimator, factory);
+		this.scorer			  		= scorer;
+		this.preProcessRoutingData 	= new PreProcessLandmarks(new FreespeedTravelTimeCost());
+		this.preProcessRoutingData.run(controler.getNetwork());
+		this.router 				= new PlansCalcRouteLandmarks (controler.getNetwork(), this.preProcessRoutingData, controler.getTravelCostCalculator(), controler.getTravelTimeCalculator());
+		
 	}
 	
 	public void run (Plan plan){
 		
-		String outputfile = Controler.getOutputFilename("TimeOptimizerTest.xls");
-		PrintStream stream;
-		try {
-			stream = new PrintStream (new File(outputfile));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			return;
-		}
-		stream.print(plan.getScore()+"\t");
-		for (int z= 0;z<plan.getActsLegs().size();z=z+2){
-		Act act = (Act)plan.getActsLegs().get(z);
-			stream.print(act.getType()+"\t");
-		}
-		stream.println();
-		stream.print("\t");
-		for (int z= 0;z<plan.getActsLegs().size();z=z+2){
-			stream.print(((Act)(plan.getActsLegs()).get(z)).getDuration()+"\t");
-		}
-		stream.println();
+		if (plan.getPerson().getId().toString().equals("2")){
 		
-		
-		// Variation of plan
-		PlanomatXPlan [] variation = new PlanomatXPlan [50];
-		double [][] statistics 	 = new double [variation.length][2];
-		for (int i=0;i<variation.length;i++){
-			variation[i] = new PlanomatXPlan (plan.getPerson());
-			variation[i].copyPlan(plan);
-		}
-		for (int i = 0;i<variation.length;i++){
-			double time = 70000;
-			((Act)variation[i].getActsLegs().get(0)).setDuration(MatsimRandom.random.nextDouble()*time);
-			((Act)variation[i].getActsLegs().get(0)).setEndTime(((Act)variation[i].getActsLegs().get(0)).getDuration());
-			time -=((Act)variation[i].getActsLegs().get(0)).getDuration();
-			for (int j=2; j<variation[i].getActsLegs().size()-2;j+=2){
-				((Act)variation[i].getActsLegs().get(j)).setStartTime(((Act)variation[i].getActsLegs().get(j-2)).getEndTime());
-				((Act)variation[i].getActsLegs().get(j)).setDuration(MatsimRandom.random.nextDouble()*time);
-				((Act)variation[i].getActsLegs().get(j)).setEndTime(((Act)variation[i].getActsLegs().get(j)).getDuration()+((Act)variation[i].getActsLegs().get(j)).getStartTime());
-				time -= ((Act)variation[i].getActsLegs().get(j)).getDuration();
+			String outputfile = Controler.getOutputFilename("TimeOptimizerTest.xls");
+			PrintStream stream;
+			try {
+				stream = new PrintStream (new File(outputfile));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+				return;
 			}
-			((Act)variation[i].getActsLegs().get(variation[i].getActsLegs().size()-1)).setStartTime(((Act)variation[i].getActsLegs().get(variation[i].getActsLegs().size()-3)).getEndTime());
-			((Act)variation[i].getActsLegs().get(variation[i].getActsLegs().size()-1)).setDuration(86400-((Act)variation[i].getActsLegs().get(variation[i].getActsLegs().size()-1)).getStartTime());
-			
-			stream.print("\t");
+			stream.print(plan.getScore()+"\t");
 			for (int z= 0;z<plan.getActsLegs().size();z=z+2){
-				stream.print(((Act)(variation[i].getActsLegs()).get(z)).getDuration()+"\t");
+			Act act = (Act)plan.getActsLegs().get(z);
+				stream.print(act.getType()+"\t");
 			}
 			stream.println();
-		}
-		
-		stream.println();
-		long average=0;
-		double mean=0;
-		for (int i = 0;i<variation.length;i++){
-			long runtime = System.currentTimeMillis();
-			timeOptAlgorithm.run(variation[i]);
-			statistics[i][1] = System.currentTimeMillis()-runtime;
-			average+=statistics[i][1];
-			
-			
-			variation[i].setScore(scorer.getScore(variation[i]));
-			statistics[i][0] = variation[i].getScore();
-			mean+=statistics[i][0];
-			
-			stream.print(variation[i].getScore()+"\t");
+			stream.print("\t");
 			for (int z= 0;z<plan.getActsLegs().size();z=z+2){
-				stream.print(((Act)(variation[i].getActsLegs()).get(z)).getDuration()+"\t");
+				stream.print(((Act)(plan.getActsLegs()).get(z)).getDuration()+"\t");
 			}
-			stream.println(statistics[i][1]);
+			stream.println();
+			
+			// Routing
+			this.router.run(plan);
+			
+			
+			// Variation of plan
+			PlanomatXPlan [] variation = new PlanomatXPlan [50];
+			double [][] statistics 	 = new double [variation.length][2];
+			for (int i=0;i<variation.length;i++){
+				variation[i] = new PlanomatXPlan (plan.getPerson());
+				variation[i].copyPlan(plan);
+			}
+			for (int i = 0;i<variation.length;i++){
+				double time = 70000;
+				((Act)variation[i].getActsLegs().get(0)).setDuration(MatsimRandom.random.nextDouble()*time);
+				((Act)variation[i].getActsLegs().get(0)).setEndTime(((Act)variation[i].getActsLegs().get(0)).getDuration());
+				time -=((Act)variation[i].getActsLegs().get(0)).getDuration();
+				for (int j=2; j<variation[i].getActsLegs().size()-2;j+=2){
+					((Act)variation[i].getActsLegs().get(j)).setStartTime(((Act)variation[i].getActsLegs().get(j-2)).getEndTime());
+					((Act)variation[i].getActsLegs().get(j)).setDuration(MatsimRandom.random.nextDouble()*time);
+					((Act)variation[i].getActsLegs().get(j)).setEndTime(((Act)variation[i].getActsLegs().get(j)).getDuration()+((Act)variation[i].getActsLegs().get(j)).getStartTime());
+					time -= ((Act)variation[i].getActsLegs().get(j)).getDuration();
+				}
+				((Act)variation[i].getActsLegs().get(variation[i].getActsLegs().size()-1)).setStartTime(((Act)variation[i].getActsLegs().get(variation[i].getActsLegs().size()-3)).getEndTime());
+				((Act)variation[i].getActsLegs().get(variation[i].getActsLegs().size()-1)).setDuration(86400-((Act)variation[i].getActsLegs().get(variation[i].getActsLegs().size()-1)).getStartTime());
+				
+				stream.print("\t");
+				for (int z= 0;z<plan.getActsLegs().size();z=z+2){
+					stream.print(((Act)(variation[i].getActsLegs()).get(z)).getDuration()+"\t");
+				}
+				stream.println();
+			}
+			
+			stream.println();
+			long average=0;
+			double mean=0;
+			for (int i = 0;i<variation.length;i++){
+				long runtime = System.currentTimeMillis();
+				timeOptAlgorithm.run(variation[i]);
+				statistics[i][1] = System.currentTimeMillis()-runtime;
+				average+=statistics[i][1];
+				
+				
+				variation[i].setScore(scorer.getScore(variation[i]));
+				statistics[i][0] = variation[i].getScore();
+				mean+=statistics[i][0];
+				
+				stream.print(variation[i].getScore()+"\t");
+				for (int z= 0;z<plan.getActsLegs().size();z=z+2){
+					stream.print(((Act)(variation[i].getActsLegs()).get(z)).getDuration()+"\t");
+				}
+				stream.println(statistics[i][1]);
+			}
+			mean = mean/statistics.length;
+			double varianz=0;
+			for (int i=0;i<statistics.length;i++){
+				//varianz += Math.exp(statistics[i][0]-mean);
+				varianz += (statistics[i][0]-mean)*(statistics[i][0]-mean);
+			}
+			stream.println(mean+"\t\t\t\t\t\t"+average/statistics.length);
+			stream.println(varianz/statistics.length);
 		}
-		mean = mean/statistics.length;
-		double varianz=0;
-		for (int i=0;i<statistics.length;i++){
-			//varianz += Math.exp(statistics[i][0]-mean);
-			varianz += (statistics[i][0]-mean)*(statistics[i][0]-mean);
-		}
-		stream.println(mean+"\t\t\t\t\t\t"+average/statistics.length);
-		stream.println(varianz/statistics.length);
 	}
 }
