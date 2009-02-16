a5c4ec90df408ce07598c9bbe7849cc815bdbc5d
yu_vsp <yu_vsp@users.sf.net>
2009-02-16 09:59:38 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@5308 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/yu/analysis/LegDepartureTimeChecker.java
@@ -0,0 +1,109 @@
+/**
+ * 
+ */
+package playground.yu.analysis;
+
+import java.io.IOException;
+
+import org.matsim.basic.v01.Id;
+import org.matsim.basic.v01.BasicPlanImpl.LegIterator;
+import org.matsim.gbl.Gbl;
+import org.matsim.network.MatsimNetworkReader;
+import org.matsim.network.NetworkLayer;
+import org.matsim.population.MatsimPopulationReader;
+import org.matsim.population.Person;
+import org.matsim.population.Plan;
+import org.matsim.population.Population;
+import org.matsim.population.algorithms.AbstractPersonAlgorithm;
+import org.matsim.population.algorithms.PlanAlgorithm;
+import org.matsim.utils.misc.Time;
+import org.matsim.world.World;
+
+import playground.yu.utils.SimpleWriter;
+
+/**
+ * check, whether the departure time of leg is later than 24:00
+ * 
+ * @author yu
+ * 
+ */
+public class LegDepartureTimeChecker extends AbstractPersonAlgorithm implements
+		PlanAlgorithm {
+	private SimpleWriter sw = null;
+	private Id personId = null;
+
+	/**
+	 * 
+	 */
+	public LegDepartureTimeChecker(String outputFilename) {
+		sw = new SimpleWriter(outputFilename);
+		sw.writeln("personId\ttime[s]\ttime[hh:mm:ss]\tlegNo.");
+	}
+
+	@Override
+	public void run(Person person) {
+		personId = person.getId();
+		run(person.getSelectedPlan());
+	}
+
+	public void run(Plan plan) {
+		int c = 0;
+		for (LegIterator li = plan.getIteratorLeg(); li.hasNext();) {
+			double legDepTime = li.next().getDepartureTime();
+			if (legDepTime >= 86400.0) {
+				sw.writeln(personId + "\t" + legDepTime + "\t"
+						+ Time.writeTime(legDepTime) + "\t" + c);
+				try {
+					sw.flush();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			c++;
+		}
+	}
+
+	public void close() {
+		try {
+			sw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Gbl.startMeasurement();
+
+		final String netFilename = "../schweiz-ivtch-SVN/baseCase/network/ivtch-osm.xml";
+		// final String plansFilename =
+		// "../schweiz-ivtch-SVN/baseCase/plans/plans_all_zrh30km_transitincl_10pct.xml.gz";
+		final String plansFilename = "../runs/run669/it.1000/1000.plans.xml.gz";
+		final String outputFilename = "output/legDepTime_669.1000.txt";
+
+		Gbl.createConfig(null);
+
+		World world = Gbl.getWorld();
+
+		NetworkLayer network = new NetworkLayer();
+		new MatsimNetworkReader(network).readFile(netFilename);
+
+		world.setNetworkLayer(network);
+		world.complete();
+
+		Population population = new Population();
+		LegDepartureTimeChecker fldtc = new LegDepartureTimeChecker(
+				outputFilename);
+		population.addAlgorithm(fldtc);
+		new MatsimPopulationReader(population).readFile(plansFilename);
+		population.runAlgorithms();
+
+		fldtc.close();
+
+		System.out.println("--> Done!");
+		Gbl.printElapsedTime();
+		System.exit(0);
+	}
+}
