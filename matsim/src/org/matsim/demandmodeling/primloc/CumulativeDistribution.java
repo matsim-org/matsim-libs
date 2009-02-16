b3d7d60035f291f8f3ad303d8e4f7ebddbcb679d
mrieser <rieser@gmail.com>
2009-02-16 20:58:02 +0000

    added or fixed GPL-Header
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@5341 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/demandmodeling/primloc/CumulativeDistribution.java
+++ matsim/src/org/matsim/demandmodeling/primloc/CumulativeDistribution.java
@@ -1,3 +1,22 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
 package org.matsim.demandmodeling.primloc;
 
 import java.io.FileNotFoundException;
@@ -94,9 +113,10 @@ public class CumulativeDistribution {
 			String line=null;
 			while( (line=lnr.readLine()) != null ){
 				StringTokenizer st = new StringTokenizer( line );
-				x.add( Double.parseDouble(st.nextToken()));
-				y.add( Double.parseDouble(st.nextToken()));
+				x.add( Double.valueOf(st.nextToken()));
+				y.add( Double.valueOf(st.nextToken()));
 			}
+			lnr.close();
 			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
