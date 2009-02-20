5db77bd0d8c0311704f51758f06c819fc14932b8
grether <dominik.grether@gmail.com>
2009-02-20 14:25:12 +0000

    new controler and costcalculator for bkick
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@5413 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/benjamin/BKickControler3.java
@@ -0,0 +1,82 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * BKickControler2
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.benjamin;
+
+import org.matsim.config.Config;
+import org.matsim.controler.Controler;
+import org.matsim.population.Population;
+import org.matsim.population.algorithms.PlanCalcType;
+import org.matsim.scoring.ScoringFunctionFactory;
+
+
+/**
+ * Controler for first zurich scenario test run of estimated scoring function.
+ * @author dgrether
+ *
+ */
+public class BKickControler3 extends Controler {
+
+	public BKickControler3(String configFileName) {
+		super(configFileName);
+		
+	}
+	
+	public BKickControler3(Config conf){
+		super(conf);
+	}
+
+	public BKickControler3(String[] args) {
+		super(args);
+	}
+
+	@Override
+	protected ScoringFunctionFactory loadScoringFunctionFactory() {
+		return new BKickScoringFunctionFactory(this.config.charyparNagelScoring());
+	}
+
+	@Override
+	protected void setup(){
+		BKickTravelCostCalculator tcc = new BKickTravelCostCalculator(this.config.charyparNagelScoring());
+		super.setTravelCostCalculator(tcc);
+		super.setup();
+		tcc.setTravelTimeCalculator(super.getTravelTimeCalculator());
+	}
+	
+	@Override
+	protected Population loadPopulation() {
+		Population pop = super.loadPopulation();
+		pop.addAlgorithm(new PlanCalcType());
+		pop.runAlgorithms();
+		return pop;
+	}
+	
+	public static void main(final String[] args) {
+		if ((args == null) || (args.length == 0)) {
+			System.out.println("No argument given!");
+			System.out.println("Usage: Controler config-file [dtd-file]");
+			System.out.println();
+		} else {
+			final Controler controler = new BKickControler3(args);
+			controler.run();
+		}
+		System.exit(0);
+	}
+
+}
---
+++ matsim/src/playground/benjamin/BKickTravelCostCalculator.java
@@ -0,0 +1,69 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * BKickTravelCostCalculator
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2009 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.benjamin;
+
+import org.matsim.config.groups.CharyparNagelScoringConfigGroup;
+import org.matsim.network.Link;
+import org.matsim.router.util.TravelMinCost;
+import org.matsim.router.util.TravelTime;
+import org.matsim.utils.misc.Time;
+
+
+/**
+ * @author dgrether
+ *
+ */
+public class BKickTravelCostCalculator implements TravelMinCost {
+
+	private double marginalUtilityOfFuel = Double.NaN;
+	private double marginalUtilityOfTraveling = Double.NaN;
+
+	
+	private CharyparNagelScoringConfigGroup scoringConfig;
+	private TravelTime travelTimeCalculator;
+
+	public BKickTravelCostCalculator(
+			CharyparNagelScoringConfigGroup charyparNagelScoring) {
+		this.scoringConfig = charyparNagelScoring;
+		this.marginalUtilityOfFuel = -scoringConfig.getMarginalUtlOfDistance();
+		this.marginalUtilityOfTraveling = -scoringConfig.getTraveling();
+	}
+
+	/**
+	 * @see org.matsim.router.util.TravelMinCost#getLinkMinimumTravelCost(org.matsim.network.Link)
+	 */
+	public double getLinkMinimumTravelCost(Link link) {
+		return (link.getLength() / link.getFreespeed(Time.UNDEFINED_TIME)) * marginalUtilityOfTraveling + marginalUtilityOfFuel * 0.12d/1000.0d * link.getLength();
+	}
+
+	/**
+	 * @see org.matsim.router.util.TravelCost#getLinkTravelCost(org.matsim.network.Link, double)
+	 */
+	public double getLinkTravelCost(Link link, double time) {
+		return travelTimeCalculator.getLinkTravelTime(link, time) * marginalUtilityOfTraveling + marginalUtilityOfFuel * 0.12d/1000.0d * link.getLength();
+	}
+
+	
+	
+	public void setTravelTimeCalculator(TravelTime travelTimeCalculator) {
+		this.travelTimeCalculator = travelTimeCalculator;
+	}
+
+}
