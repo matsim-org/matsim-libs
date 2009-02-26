852d3a2a339d89b2ac6775dba4bd0425a18819f6
yu_vsp <yu_vsp@users.sf.net>
2009-02-26 13:29:46 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@5557 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/yu/utils/CountsConverter.java
+++ matsim/src/playground/yu/utils/CountsConverter.java
@@ -4,7 +4,16 @@
 package playground.yu.utils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.matsim.counts.Count;
+import org.matsim.counts.Counts;
+import org.matsim.counts.CountsWriter;
+import org.matsim.interfaces.core.v01.Link;
+import org.matsim.network.MatsimNetworkReader;
+import org.matsim.network.NetworkLayer;
 
 import playground.yu.utils.io.SimpleReader;
 
@@ -18,12 +27,14 @@ public class CountsConverter {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-//		------------READ .ATT COUNTSFILE----------------
+		// ------------READ MATSIM NETWORK-----------------
+		NetworkLayer network = new NetworkLayer();
+		new MatsimNetworkReader(network).readFile("../berlin data/wip_net.xml");
+		// ------------READ .ATT COUNTSFILE----------------
 		SimpleReader sr = new SimpleReader(
 				"../berlin data/link_counts_PKW_hrs0-24.att");
-		List<String> NOs = new ArrayList<String>();
-		List<String> TONODENOs = new ArrayList<String>();
-		List<List<Double>> countsValues = new ArrayList<List<Double>>();
+		Map<String, String> NO_TONODENOs = new HashMap<String, String>();
+		Map<String, List<Double>> NO_CVs = new HashMap<String, List<Double>>();
 		// filehead
 		String line = sr.readLine();
 		// after filehead
@@ -31,13 +42,12 @@ public class CountsConverter {
 			line = sr.readLine();
 			if (line != null) {
 				String[] cells = line.split(";");
-				NOs.add(cells[0]);
-				TONODENOs.add(cells[1]);
+				NO_TONODENOs.put(cells[0], cells[1]);
 				List<Double> cvs = new ArrayList<Double>(24);
 				for (int i = 2; i < 26; i++) {
 					cvs.add(Double.parseDouble(cells[i]));
 				}
-				countsValues.add(cvs);
+				NO_CVs.put(cells[0], cvs);
 			}
 		}
 		try {
@@ -45,8 +55,38 @@ public class CountsConverter {
 		} catch (Exception e) {
 			System.err.println(e);
 		}
-//		-----------------------------------------------------
-		
+		// ---------------PREPARE COUNTS------------
+		Counts counts = new Counts();
+		counts.setYear(2000);
+		counts.setName("berlin counts");
+		counts
+				.setDescription("extracted from vsp-cvs/studies/berlin-wip/external-data/counts/senstadt-hand/link_counts_PKW_hrs0-24.att");
+		for (Link link : network.getLinks().values()) {
+			String origLinkId = link.getOrigId();
+			if (NO_TONODENOs.keySet().contains(origLinkId)) {
+				System.out.println("A:\torigId:\t" + origLinkId + "\tlinkId:\t"
+						+ link.getId().toString());
+				System.out.println("B:\tlinkToNodeId:\t"
+						+ link.getToNode().getId().toString() + "\tTONODENO:\t"
+						+ NO_TONODENOs.get(origLinkId));
+				if (link.getToNode().getId().toString().equals(
+						NO_TONODENOs.get(origLinkId))) {
+					System.out.println("C:\tlinkToNodeId:\t"
+							+ link.getToNode().getId().toString()
+							+ "\tTONODENO:\t" + NO_TONODENOs.get(origLinkId));
+					Count count = counts.createCount(link.getId(), null);
+					List<Double> cvs = NO_CVs.get(origLinkId);
+					for (int i = 0; i < cvs.size(); i++) {
+						double volume = cvs.get(i).doubleValue();
+						if (volume >= 0)
+							count.createVolume(i + 1, volume);
+					}
+				}
+				System.out.println("-------------------------");
+			}
+		}
+		// ------------------WRITE COUNTSFILE----------------
+		new CountsWriter(counts, "../berlin data/link_counts_PKW_hrs0-24.xml")
+				.write();
 	}
-
 }
