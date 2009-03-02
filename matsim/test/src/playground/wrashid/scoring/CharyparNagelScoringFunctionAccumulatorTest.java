3f6d5f6634fb406e825c7eac11da8dd310df7106
mrieser <rieser@gmail.com>
2009-03-02 18:28:25 +0000

    moved test to test-directory
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@5649 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/test/src/playground/wrashid/scoring/CharyparNagelScoringFunctionAccumulatorTest.java
+++ matsim/test/src/playground/wrashid/scoring/CharyparNagelScoringFunctionAccumulatorTest.java
@@ -2,7 +2,6 @@ package playground.wrashid.scoring;
 
 import org.matsim.gbl.Gbl;
 import org.matsim.interfaces.core.v01.Plan;
-import org.matsim.scoring.CharyparNagelScoringFunction;
 import org.matsim.scoring.CharyparNagelScoringFunctionTest;
 import org.matsim.scoring.CharyparNagelScoringParameters;
 import org.matsim.scoring.ScoringFunction;
