c8a58112fbc88014120e2097659035e450c5fb58
rashid_waraich <rashid_waraich@users.sf.net>
2009-03-03 15:48:03 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@5665 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/wrashid/scoring/CharyparNagelScoringFunctionAccumulatorTest.java
@@ -0,0 +1,22 @@
+package playground.wrashid.scoring;
+
+import org.matsim.gbl.Gbl;
+import org.matsim.interfaces.core.v01.Plan;
+import org.matsim.scoring.CharyparNagelScoringFunctionTest;
+import org.matsim.scoring.CharyparNagelScoringParameters;
+import org.matsim.scoring.ScoringFunction;
+
+public class CharyparNagelScoringFunctionAccumulatorTest extends CharyparNagelScoringFunctionTest {
+	protected ScoringFunction getScoringFunctionInstance(final Plan somePlan) {
+		
+		ScoringFunctionAccumulator scoringFunctionAccumulator = new ScoringFunctionAccumulator();
+
+		scoringFunctionAccumulator.addScoringFunction(new CharyparNagelLegScoringFunction( somePlan, new CharyparNagelScoringParameters(Gbl.getConfig().charyparNagelScoring())));
+		
+		scoringFunctionAccumulator.addScoringFunction(new CharyparNagelMoneyScoringFunction( somePlan, new CharyparNagelScoringParameters(Gbl.getConfig().charyparNagelScoring())));
+		
+		scoringFunctionAccumulator.addScoringFunction(new CharyparNagelAgentStuckScoringFunction(somePlan, new CharyparNagelScoringParameters(Gbl.getConfig().charyparNagelScoring())));
+		
+		return scoringFunctionAccumulator;
+	}
+}
