a569e8001c1aefe06f000b3f683c185c33ccb06e
jwjoubert <johan.joubert@up.ac.za>
2009-03-06 12:12:11 +0000

    Generalize the class to handle any province as study area, and not just Gauteng.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@5748 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/jjoubert/CommercialTraffic/AnalyseGAPDensity.java
+++ matsim/src/playground/jjoubert/CommercialTraffic/AnalyseGAPDensity.java
@@ -35,13 +35,20 @@ public class AnalyseGAPDensity {
 //	final static String ROOT = "/home/jjoubert/";
 	// Derived string values
 	final static String GAP_SHAPEFILE = ROOT + "ShapeFiles/" + PROVINCE + "/" + PROVINCE + "GAP_UTM35S.shp";
-	final static String INPUT_MINOR = ROOT + PROVINCE + "/Vehicles/Activities/" + PROVINCE + "MinorLocations.txt";
-	final static String INPUT_MAJOR = ROOT + PROVINCE + "/Vehicles/Activities/" + PROVINCE + "MajorLocations.txt";
-	final static String OUTPUT_MINOR = ROOT + PROVINCE + "/Vehicles/Activities/" + PROVINCE + "MinorGapStats.txt";
-	final static String OUTPUT_MAJOR = ROOT + PROVINCE + "/Vehicles/Activities/" + PROVINCE + "MajorGapStats.txt";
-	
+	final static String SHAPEFILE = ROOT + "ShapeFiles/" + PROVINCE + "/" + PROVINCE + "_UTM35S.shp";
+//	final static String INPUT_MINOR = ROOT + PROVINCE + "/Vehicles/Activities/" + PROVINCE + "MinorLocations.txt";
+//	final static String INPUT_MAJOR = ROOT + PROVINCE + "/Vehicles/Activities/" + PROVINCE + "MajorLocations.txt";
+//	final static String OUTPUT_MINOR = ROOT + PROVINCE + "/Vehicles/Activities/" + PROVINCE + "MinorGapStats.txt";
+//	final static String OUTPUT_MAJOR = ROOT + PROVINCE + "/Vehicles/Activities/" + PROVINCE + "MajorGapStats.txt";
+
+	final static String INPUT_MINOR = ROOT + PROVINCE + "/Activities/20090225-2030/" + PROVINCE + "MinorLocations.txt";
+	final static String INPUT_MAJOR = ROOT + PROVINCE + "/Activities/20090225-2030/" + PROVINCE + "MajorLocations.txt";
+	final static String OUTPUT_MINOR = ROOT + PROVINCE + "/Activities/20090225-2030/" + PROVINCE + "MinorGapStats.txt";
+	final static String OUTPUT_MAJOR = ROOT + PROVINCE + "/Activities/20090225-2030/" + PROVINCE + "MajorGapStats.txt";
+
 	public static final String DELIMITER = ",";
 	final static int GAP_SEARCH_AREA = 20000; // in METERS
+	final static int PROGRESS_INCREMENT = 100000;
 
 	public static void main( String args[] ) {
 		System.out.println("==========================================================================================");
@@ -49,9 +56,9 @@ public class AnalyseGAPDensity {
 		System.out.println();
 		
 		ArrayList<SAZone> zoneList = readGAPShapeFile( GAP_SHAPEFILE );
-		
+	
 		QuadTree<SAZone> zoneTree = buildQuadTree( zoneList );
-		
+
 		assignActivityToZone(zoneList, zoneTree );
 		
 		writeZoneStatsToFile( zoneList );
@@ -71,8 +78,8 @@ public class AnalyseGAPDensity {
 			ArrayList<Object> objectArray = (ArrayList<Object>) fs.getFeatures().getAttribute(0);
 			for (int i = 0; i < objectArray.size(); i++) {
 				Object thisZone = objectArray.get(i);
-				// For GAP files, field [2] contains the GAP ID
-				String name = String.valueOf( ((Feature) thisZone).getAttribute( 2 ) ); 
+				// For GAP files, field [1] contains the GAP_ID
+				String name = String.valueOf( ((Feature) thisZone).getAttribute( 1 ) ); 
 				Geometry shape = ((Feature) thisZone).getDefaultGeometry();
 				if( shape instanceof MultiPolygon ){
 					mp = (MultiPolygon)shape;
@@ -101,23 +108,29 @@ public class AnalyseGAPDensity {
 	}
 
 	private static QuadTree<SAZone> buildQuadTree(ArrayList<SAZone> mesozoneList) {
+		System.out.println("Establishing quad tree extent for " + PROVINCE + "... " );
+		Polygon envelope = (Polygon) (ReadStudyAreaShapeFile.readStudyAreaPolygon( SHAPEFILE ) ).getEnvelope();
+		Coordinate [] coords = envelope.getCoordinates();
+		double minX = coords[0].x;
+		double minY = coords[0].y;
+		double maxX = coords[2].x;
+		double maxY = coords[2].y;
+		System.out.println("Done." );
+
 		System.out.printf("Building quad tree from mesozones... " );
 
-		QuadTree<SAZone> mesozoneQT = new QuadTree<SAZone>(-560000, 6100000, 1100000, 7600000);
+		QuadTree<SAZone> mesozoneQT = new QuadTree<SAZone>(minX, minY, maxX, maxY);
 		for (SAZone zone : mesozoneList) {
 			mesozoneQT.put(zone.getCentroid().getX(), zone.getCentroid().getY(), zone);
 		}
 		System.out.printf("Done.\n\n");
 		return mesozoneQT;		
 	}
-	
-	
+		
 	private static void assignActivityToZone(ArrayList<SAZone> list, QuadTree<SAZone> tree ){
 		System.out.println("Assigning activity locations to GAP mesozones.");
 
 		GeometryFactory gf = new GeometryFactory();
-		int lostMinor = 0;
-		int lostMajor = 0;
 		
 		try { // Minor activities
 			Scanner inputMinor = new Scanner(new BufferedReader(new FileReader(new File( INPUT_MINOR ) ) ) );
@@ -128,7 +141,7 @@ public class AnalyseGAPDensity {
 				int numberOfPoints = 0;
 				while(inputMinor.hasNextLine() ){
 					// Report progress
-					if( numberOfPoints%100 == 0){
+					if( numberOfPoints%PROGRESS_INCREMENT == 0){
 						System.out.printf("     ...minor activities %8d\n", numberOfPoints );
 					}
 					String[] thisLine = inputMinor.nextLine().split( DELIMITER );
@@ -141,12 +154,13 @@ public class AnalyseGAPDensity {
 
 						Point thisActivity = gf.createPoint(new Coordinate(x, y) );
 
-						SAZone minorZone = findZoneInArrayList(thisActivity, (ArrayList<SAZone>) tree.get(x, y, GAP_SEARCH_AREA ) );
+						ArrayList<SAZone> shortlist = (ArrayList<SAZone>) tree.get(x, y, GAP_SEARCH_AREA );
+						SAZone minorZone = findZoneInArrayList(thisActivity, shortlist );
 						if ( minorZone != null ){
 							minorZone.incrementMinorActivityCountDetail( timeOfDay );
 							minorZone.increaseMinorActivityDurationDetail( timeOfDay, duration );
 						} else{
-							lostMinor++;			
+							// Point is not is study area		
 						}
 					}
 					numberOfPoints++;
@@ -167,7 +181,7 @@ public class AnalyseGAPDensity {
 				int numberOfPoints = 0;
 				while(inputMajor.hasNextLine() ){
 					// Report progress
-					if( numberOfPoints%100 == 0){
+					if( numberOfPoints%PROGRESS_INCREMENT == 0){
 						System.out.printf("     ...major activities %8d\n", numberOfPoints );
 					}
 					String[] thisLine = inputMajor.nextLine().split( DELIMITER );
@@ -179,13 +193,14 @@ public class AnalyseGAPDensity {
 						int duration = Integer.parseInt( thisLine[4] );
 
 						Point thisActivity = gf.createPoint(new Coordinate(x, y) );
-
-						SAZone majorZone = findZoneInArrayList(thisActivity, (ArrayList<SAZone>) tree.get(x, y, GAP_SEARCH_AREA ) );
+						
+						ArrayList<SAZone> shortlist = (ArrayList<SAZone>) tree.get(x, y, GAP_SEARCH_AREA );
+						SAZone majorZone = findZoneInArrayList(thisActivity, shortlist );
 						if ( majorZone != null ){
 							majorZone.incrementMajorActivityCountDetail( timeOfDay );
 							majorZone.increaseMajorActivityDurationDetail( timeOfDay, duration );
 						} else{
-							lostMajor++;			
+							// Point is not is study area		
 						}
 					}
 					numberOfPoints++;
@@ -200,10 +215,10 @@ public class AnalyseGAPDensity {
 		System.out.printf("Done.\n\n" );
 	}
 
-private static SAZone findZoneInArrayList(Point p, ArrayList<SAZone> list ) {
+	private static SAZone findZoneInArrayList(Point p, ArrayList<SAZone> list ) {
 	SAZone zone = null;
 	int i = 0;
-	while( (i < list.size() ) && zone != null ){
+	while( (i < list.size() ) & (zone == null) ){
 		SAZone thisZone = list.get(i);
 		if( thisZone.contains( p ) ){
 			zone = thisZone;				
@@ -251,7 +266,7 @@ private static SAZone findZoneInArrayList(Point p, ArrayList<SAZone> list ) {
 		} catch(Exception e){
 			e.printStackTrace();
 		}
-		System.out.println("Done.");
+		System.out.print("Done.\n\n");
 	}
 		
 	/*
