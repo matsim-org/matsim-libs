442d18b108e32624a96dcf4879a413d949c02b8c
vsp_nagel <2008@kainagel.org>
2009-03-06 20:20:22 +0000

    added some comments & deprecated markers
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@5769 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/interfaces/basic/v01/BasicLocation.java
+++ matsim/src/org/matsim/interfaces/basic/v01/BasicLocation.java
@@ -28,9 +28,14 @@ import org.matsim.basic.v01.LocationType;
 public interface BasicLocation {
 	
 	public Coord getCenter();
+	// FIXME [kn] rename to getCoord ... and then make all interfaces with getCoord to inherit from this.
 	
 	public Id getId();
 	
+	/**
+	 * @deprecated not yet clear
+	 */
+	@Deprecated // not yet clear
 	public LocationType getLocationType();
 	
 }
