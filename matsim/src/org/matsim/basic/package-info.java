ad8aa427797df435b6705ee0042167743d8305c1
grether <dominik.grether@gmail.com>
2009-03-08 12:44:06 +0000

    added some explanations for design of basic infrastructure
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@5779 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/org/matsim/basic/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * This package and its subpackages contain the MATSim basic classes and 
+ * interfaces that are not ready for publishing in the org.matsim.interfaces
+ * packages.
+ * The interfaces and classes placed here are designed by the following principles:
+ * <ul>
+ *   <li>There is an interface for each class </li>
+ *   <li>Interfaces are named BasicSomething, while the implementation has the name BasicSomethingImpl</li>
+ *   <li>For each toplevel xml definition there is a container type, e.g. network_v1.dtd has the container 
+ *   BasicNetwork</li> 
+ *   <li>The types define only those attributes that are specified by the xml grammar, the resulting objects
+ *   are purely used for data storage and have no state except the values of their attributes.</li>
+ * </ul>
+ * 
+ * @see org.matism.basic
+ * @see org.matsim.interfaces
+ */
+package org.matsim.basic;
