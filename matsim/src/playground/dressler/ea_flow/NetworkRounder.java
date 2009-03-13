ece668cd5b168ab8b524953a1ff338cd89265627
DanielDressler <DanielDressler@users.sf.net>
2009-03-13 15:56:26 +0000

    Now rounds transit times as well, and can output matsim-compatible values.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@5898 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/dressler/ea_flow/NetworkRounder.java
+++ matsim/src/playground/dressler/ea_flow/NetworkRounder.java
@@ -42,7 +42,7 @@ public class NetworkRounder {
 	 * @param network
 	 * @param newcap
 	 */
-	public static void roundNetwork(NetworkLayer network,int newcap, double flowCapacityFactor, double lengthFactor){
+	public static void roundNetwork(NetworkLayer network,int newcap, double flowCapacityFactor, double lengthFactor, boolean forEAF){
 		double oldcap = network.getCapacityPeriod();
 		int roundedtozerocap = 0;
 		int roundedtozerotime = 0;
@@ -54,23 +54,43 @@ public class NetworkRounder {
 			System.out.println(divisor);
 		}
 		for (Link link : network.getLinks().values()){
-			double newspeed = link.getFreespeed(0.)*newcap;
-			if(_debug){
-				System.out.println("old v: "+link.getFreespeed(0.)+" new v: "+newspeed);
-			}
-			link.setFreespeed(newspeed);
 			
 			link.setLength(link.getLength()*lengthFactor);
 			//link.setLength(link.getEuklideanDistance()*lengthFactor);
 			
+			double newTravelTime = link.getLength() / link.getFreespeed(0.);
+			
+			newTravelTime = Math.round(newTravelTime/newcap)*newcap;								
+
+			double newspeed;
+			if (newTravelTime == 0.) {
+				newspeed = 999999999999.;	
+			} else {
+				if (forEAF) {
+					newspeed = newcap * link.getLength() / newTravelTime;
+				} else {
+					newspeed = link.getLength() / newTravelTime;
+				}
+			}
+			
+			if(_debug){
+				System.out.println("old v: "+link.getFreespeed(0.)+" new v: "+newspeed);
+			}
+			link.setFreespeed(newspeed);
+											
 			//double newcapacity =Math.ceil(link.getCapacity(1.)/divisor*flowCapacityFactor);
 			double newcapacity =Math.round(link.getCapacity(1.)/divisor*flowCapacityFactor);
+			
 			if (newcapacity == 0d && link.getCapacity(1.) != 0d) roundedtozerocap++;
-			if (Math.round(link.getLength()/link.getFreespeed(0)) == 0) roundedtozerotime++; 
+			if (Math.round(link.getLength()/link.getFreespeed(0)) == 0) {
+				System.out.println(link.getId());
+				roundedtozerotime++; 
+			}
 							
 			if(_debug){
 				System.out.println("old c: "+link.getCapacity(1.)+" new c: "+newcapacity);
 			}
+			
 			link.setCapacity(newcapacity);
 		}
 		network.setCapacityPeriod(newcap);
@@ -80,53 +100,66 @@ public class NetworkRounder {
 		//}
 	}
 	
-	public static NetworkLayer roundNetwork(String filename, int newcap, double flowCapacityFactor, double lengthFactor){
+	public static NetworkLayer roundNetwork(String filename, int newcap, double flowCapacityFactor, double lengthFactor, boolean forEAF){
 		//read network
 		NetworkLayer network = new NetworkLayer();
 		MatsimNetworkReader networkReader = new MatsimNetworkReader(network);		
 		networkReader.readFile(filename);
 		System.out.println("Network stats: Nodes = " + network.getNodes().size() + ", Edges = " + network.getLinks().size());
-		roundNetwork(network, newcap, flowCapacityFactor, lengthFactor);
+		roundNetwork(network, newcap, flowCapacityFactor, lengthFactor, forEAF);
 		return network;
 		
 	}
 
 	public static void main(String[] args){
-		if (args.length!=3 && args.length!=1 && args.length!=0){
-			System.out.println("USAGE: NetworkRounder <inputfile> <outputfile> <cap> OR JUST: NetworkRounder <cap>");
-			return;
-		}
+//		if (args.length!=3 && args.length!=1 && args.length!=0){
+//			System.out.println("USAGE: NetworkRounder <inputfile> <outputfile> <cap> OR JUST: NetworkRounder <cap>");
+//			return;
+//		}
 		int cap = 5;
-		double flowCapacityFactor = 1.0d; // 12*3600.0d / 60.0d;
+		double flowCapacityFactor = 0.1d; // 12*3600.0d / 60.0d;
 		double lengthFactor = 1.0d;
-		String inputfile  = "/homes/combi/Projects/ADVEST/padang/network/padang_net_evac.xml";
+		String inputfile  = "/homes/combi/Projects/ADVEST/padang/network/padang_net_evac_v20080618.xml";
 		//String inputfile = "/homes/combi/dressler/V/Project/testcases/swiss_old/matsimevac/swiss_old_network_evac.xml";
 		//String inputfile = "./examples/meine_EA/siouxfalls_network.xml";
-		String outputfile = "./examples/meine_EA/siouxfalls_network_5s.xml";
+		
+		String outputfile_forEAF;
+		String outputfile_forMatsim;
+		//String outputfile = "./examples/meine_EA/siouxfalls_network_5s.xml";
+		outputfile_forEAF  = "/homes/combi/Projects/ADVEST/padang/network/padang_net_evac_v20080618_10p_5s_EAF.xml";
+		outputfile_forMatsim  = "/homes/combi/Projects/ADVEST/padang/network/padang_net_evac_v20080618_10p_5s_MATSIM.xml";
 		//String outputfile = "/homes/combi/Projects/ADVEST/padang/network/padang_net_evac_100p_flow_2s_cap.xml";
 		//String outputfile = "./examples/meine_EA/swissold_network_5s.xml";
 		
-		if(args.length >=2){
-			inputfile  = args[0];
-			outputfile = args[1];
-			cap = Integer.valueOf(args[2]);
-		} if (args.length==4){
-			inputfile  = args[0];
-			outputfile = args[1];
-			cap = Integer.valueOf(args[2]);
-			flowCapacityFactor = Double.valueOf(args[3]);
-		}
-		if (args.length >= 5) {
-			lengthFactor = Double.valueOf(args[4]); 
-		}
+//		if(args.length >=2){
+//			inputfile  = args[0];
+//			outputfile = args[1];
+//			cap = Integer.valueOf(args[2]);
+//		} if (args.length==4){
+//			inputfile  = args[0];
+//			outputfile = args[1];
+//			cap = Integer.valueOf(args[2]);
+//			flowCapacityFactor = Double.valueOf(args[3]);
+//		}
+//		if (args.length >= 5) {
+//			lengthFactor = Double.valueOf(args[4]); 
+//		}
+//		
+//		if (args.length == 1){
+//			cap =Integer.valueOf(args[0]);
+//		}
 		
-		if (args.length == 1){
-			cap =Integer.valueOf(args[0]);
+		if (outputfile_forEAF != null) {
+		  NetworkLayer network = roundNetwork(inputfile,cap, flowCapacityFactor, lengthFactor, true);
+		  NetworkWriter writer = new NetworkWriter( network, outputfile_forEAF);
+		  writer.write();
 		}
-		
-		NetworkLayer network = roundNetwork(inputfile,cap, flowCapacityFactor, lengthFactor);
-		NetworkWriter writer = new NetworkWriter( network, outputfile);
-		writer.write();
+		if (outputfile_forMatsim != null) {
+			  NetworkLayer network = roundNetwork(inputfile,cap, flowCapacityFactor, lengthFactor, false);
+			  NetworkWriter writer = new NetworkWriter( network, outputfile_forMatsim);
+			  writer.write();
+		}
+			
 		
 	}
 }
