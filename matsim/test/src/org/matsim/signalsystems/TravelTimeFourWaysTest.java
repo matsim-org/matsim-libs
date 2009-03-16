8a3576151e622fc2c3bf8f5fc2568b5cc0445aaa
mrieser <rieser@gmail.com>
2009-03-16 18:20:53 +0000

    cleaned up test: the test implemented its own event writer, which just added a lot of code, but didn't really benefit the test. replaced it with the official events writer. the events files include now a header line, why the files are replaced.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@5933 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/test/src/org/matsim/signalsystems/TravelTimeFourWaysTest.java
+++ matsim/test/src/org/matsim/signalsystems/TravelTimeFourWaysTest.java
@@ -20,48 +20,19 @@
 
 package org.matsim.signalsystems;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-
 import org.matsim.config.Config;
 import org.matsim.controler.ScenarioData;
-import org.matsim.events.ActEndEvent;
-import org.matsim.events.ActStartEvent;
-import org.matsim.events.AgentArrivalEvent;
-import org.matsim.events.AgentDepartureEvent;
-import org.matsim.events.AgentWait2LinkEvent;
 import org.matsim.events.Events;
-import org.matsim.events.LinkEnterEvent;
-import org.matsim.events.LinkLeaveEvent;
-import org.matsim.events.handler.ActEndEventHandler;
-import org.matsim.events.handler.ActStartEventHandler;
-import org.matsim.events.handler.AgentArrivalEventHandler;
-import org.matsim.events.handler.AgentDepartureEventHandler;
-import org.matsim.events.handler.AgentWait2LinkEventHandler;
-import org.matsim.events.handler.LinkEnterEventHandler;
-import org.matsim.events.handler.LinkLeaveEventHandler;
+import org.matsim.events.algorithms.EventWriterTXT;
 import org.matsim.mobsim.queuesim.QueueSimulation;
 import org.matsim.testcases.MatsimTestCase;
 import org.matsim.utils.CRCChecksum;
-import org.matsim.utils.io.IOUtils;
 
 /**
  * @author aneumann
  * @author dgrether
  */
-public class TravelTimeFourWaysTest extends MatsimTestCase implements	LinkLeaveEventHandler, LinkEnterEventHandler, ActEndEventHandler, ActStartEventHandler, AgentArrivalEventHandler, AgentDepartureEventHandler, AgentWait2LinkEventHandler {
-
-	BufferedWriter writer = null;
-
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-	}
-
-	@Override
-	protected void tearDown() throws Exception {
-		super.tearDown();
-	}
+public class TravelTimeFourWaysTest extends MatsimTestCase {
 
 	public void testTrafficLightIntersection4arms() {
 		Config conf = loadConfig(this.getClassInputDirectory() + "config.xml");
@@ -77,20 +48,15 @@ public class TravelTimeFourWaysTest extends MatsimTestCase implements	LinkLeaveE
 		ScenarioData data = new ScenarioData(conf);
 
 		Events events = new Events();
-		events.addHandler(this);
-		String tempout = this.getOutputDirectory() + "temp.txt.gz";
-		try {
-			this.writer = IOUtils.getBufferedWriter(tempout, true);
-			QueueSimulation sim = new QueueSimulation(data.getNetwork(), data.getPopulation(), events);
-			sim.setLaneDefinitions(data.getLaneDefinitions());
-			sim.setSignalSystems(data.getSignalSystems(), data.getSignalSystemsConfiguration());
-			sim.run();
-			this.writer.flush();
-			this.writer.close();
-			assertEquals(CRCChecksum.getCRCFromFile(this.getClassInputDirectory() + "reference4armsWoUTurn.txt.gz"), CRCChecksum.getCRCFromFile(tempout));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		String tempout = this.getOutputDirectory() + "events.txt.gz";
+		EventWriterTXT eWriter = new EventWriterTXT(tempout);
+		events.addHandler(eWriter);
+		QueueSimulation sim = new QueueSimulation(data.getNetwork(), data.getPopulation(), events);
+		sim.setLaneDefinitions(data.getLaneDefinitions());
+		sim.setSignalSystems(data.getSignalSystems(), data.getSignalSystemsConfiguration());
+		sim.run();
+		eWriter.closeFile();
+		assertEquals("different events files", CRCChecksum.getCRCFromFile(this.getClassInputDirectory() + "reference4armsWoUTurn.txt.gz"), CRCChecksum.getCRCFromFile(tempout));
 	}
 
 	public void testTrafficLightIntersection4armsWithUTurn() {
@@ -107,92 +73,14 @@ public class TravelTimeFourWaysTest extends MatsimTestCase implements	LinkLeaveE
 		conf.plans().setInputFile(this.getClassInputDirectory() + "plans_uturn.xml.gz");
 		ScenarioData data = new ScenarioData(conf);
 		Events events = new Events();
-		events.addHandler(this);
-		String tempout = this.getOutputDirectory() + "temp.txt.gz";
-		try {
-			this.writer = IOUtils.getBufferedWriter(tempout, true);
-//			new QSim(events, data.getPopulation(), data.getNetwork(), false, newLSADef, newLSADefCfg).run();
-			QueueSimulation sim = new QueueSimulation(data.getNetwork(), data.getPopulation(), events);
-			sim.setLaneDefinitions(data.getLaneDefinitions());
-			sim.setSignalSystems(data.getSignalSystems(), data.getSignalSystemsConfiguration());
-			sim.run();
-			this.writer.flush();
-			this.writer.close();
-			assertEquals(CRCChecksum.getCRCFromFile(tempout),	CRCChecksum.getCRCFromFile(this.getClassInputDirectory() + "reference_uturn.txt.gz"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void handleEvent(final LinkEnterEvent event) {
-		try {
-			this.writer.write(event.toString());
-			this.writer.newLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void handleEvent(final LinkLeaveEvent event) {
-		try {
-			this.writer.write(event.toString());
-			this.writer.newLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void reset(final int iteration) {
-		// Not used in that TestCase
-	}
-
-	public void handleEvent(final ActEndEvent event) {
-		try {
-			this.writer.write(event.toString());
-			this.writer.newLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	public void handleEvent(final ActStartEvent event) {
-		try {
-			this.writer.write(event.toString());
-			this.writer.newLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	public void handleEvent(final AgentArrivalEvent event) {
-		try {
-			this.writer.write(event.toString());
-			this.writer.newLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	public void handleEvent(final AgentDepartureEvent event) {
-		try {
-			this.writer.write(event.toString());
-			this.writer.newLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	public void handleEvent(final AgentWait2LinkEvent event) {
-		try {
-			this.writer.write(event.toString());
-			this.writer.newLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
+		String tempout = this.getOutputDirectory() + "events.txt";
+		EventWriterTXT eWriter = new EventWriterTXT(tempout);
+		events.addHandler(eWriter);
+		QueueSimulation sim = new QueueSimulation(data.getNetwork(), data.getPopulation(), events);
+		sim.setLaneDefinitions(data.getLaneDefinitions());
+		sim.setSignalSystems(data.getSignalSystems(), data.getSignalSystemsConfiguration());
+		sim.run();
+		eWriter.closeFile();
+		assertEquals("different events files", CRCChecksum.getCRCFromFile(this.getClassInputDirectory() + "reference_uturn.txt"), CRCChecksum.getCRCFromFile(tempout));
 	}
 }
