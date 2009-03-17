fc53855259d8e9c49047b12179a34fe95d0b723b
rashid_waraich <rashid_waraich@users.sf.net>
2009-03-17 10:56:05 +0000

    removed unused imports.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@5949 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/scoring/charyparNagel/ActivityScoringFunction.java
+++ matsim/src/org/matsim/scoring/charyparNagel/ActivityScoringFunction.java
@@ -21,12 +21,9 @@
 package org.matsim.scoring.charyparNagel;
 
 import org.apache.log4j.Logger;
-import org.matsim.interfaces.basic.v01.BasicLeg;
 import org.matsim.interfaces.core.v01.Activity;
-import org.matsim.interfaces.core.v01.Leg;
 import org.matsim.interfaces.core.v01.Person;
 import org.matsim.interfaces.core.v01.Plan;
-import org.matsim.interfaces.core.v01.Route;
 import org.matsim.population.ActUtilityParameters;
 import org.matsim.scoring.CharyparNagelScoringFunction;
 import org.matsim.scoring.CharyparNagelScoringParameters;
