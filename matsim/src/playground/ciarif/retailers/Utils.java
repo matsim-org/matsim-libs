6e73e0d19f10ffa56c27c3ee1932ee6db7e3c429
mrieser <rieser@gmail.com>
2009-03-20 13:10:06 +0000

    fixed so that automated mapping should be correct hopefully.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@6032 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/ciarif/retailers/Utils.java
+++ matsim/src/playground/ciarif/retailers/Utils.java
@@ -1,6 +1,5 @@
 package playground.ciarif.retailers;
 
-import org.matsim.basic.v01.BasicLinkImpl;
 import org.matsim.interfaces.basic.v01.Coord;
 import org.matsim.interfaces.core.v01.Facility;
 import org.matsim.interfaces.core.v01.Link;
@@ -9,16 +8,21 @@ import org.matsim.utils.collections.QuadTree;
 import org.matsim.utils.geometry.CoordImpl;
 
 public abstract class Utils {
-	private final static double EPSILON = 0.000000001;
-	public static final void moveFacility(Facility f, BasicLinkImpl link) {
+	private final static double EPSILON = 0.0001;
+	public static final void moveFacility(Facility f, Link link) {
 		double [] vector = new double[2];
 		vector[0] = link.getToNode().getCoord().getY()-link.getFromNode().getCoord().getY();
 		vector[1] = -(link.getToNode().getCoord().getX()-link.getFromNode().getCoord().getX());
-		double length = Math.sqrt(vector[0]*vector[0]+vector[1]*vector[1]);
-		Coord coord = new CoordImpl(link.getCoord().getX()+vector[0]*EPSILON*length,link.getCoord().getY()+vector[1]*EPSILON*length);
-		System.out.println("Coord of the new link = " + coord);
-		System.out.println("Coord of the old link = " + f.getLink().getCoord());
+//		double length = Math.sqrt(vector[0]*vector[0]+vector[1]*vector[1]);
+//		System.out.println("length = " + length);
+		Coord coord = new CoordImpl(link.getCoord().getX()+vector[0]*EPSILON,link.getCoord().getY()+vector[1]*EPSILON);
 		f.moveTo(coord);
+		
+//		Link oldL = f.getLink();
+//		if (oldL != null) {
+//			Gbl.getWorld().removeMapping(f, oldL);
+//		}
+//		Gbl.getWorld().addMapping(f, link);
 	}
 	
 	// BAD CODE STYLE but keep that anyway for the moment
