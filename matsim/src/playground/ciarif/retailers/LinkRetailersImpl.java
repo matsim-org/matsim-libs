90276971f15d4f701f48634b4ee07710d1ca67ef
balmermi <balmermi@users.sf.net>
2009-03-23 12:11:37 +0000

    kind of a bug fix
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@6073 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/ciarif/retailers/LinkRetailersImpl.java
+++ matsim/src/playground/ciarif/retailers/LinkRetailersImpl.java
@@ -1,12 +1,13 @@
 package playground.ciarif.retailers;
 
 
-import org.matsim.basic.v01.BasicLinkImpl;
 import org.matsim.interfaces.basic.v01.network.BasicNode;
 import org.matsim.interfaces.core.v01.Link;
+import org.matsim.network.LinkImpl;
 import org.matsim.network.NetworkLayer;
+import org.matsim.utils.misc.Time;
 
-public class LinkRetailersImpl extends BasicLinkImpl  { //AbstractLocation implements BasicLink {
+public class LinkRetailersImpl extends LinkImpl  { //AbstractLocation implements BasicLink {
 
 	protected BasicNode from = null;
 	protected BasicNode to = null;
@@ -18,13 +19,10 @@ public class LinkRetailersImpl extends BasicLinkImpl  { //AbstractLocation imple
 	protected double nofLanes = Double.NaN;
 
 	public LinkRetailersImpl(Link link, NetworkLayer network) {
-		super (network, link.getId(), link.getFromNode(), link.getToNode());
+		super(link.getId(),link.getFromNode(),link.getToNode(),network,link.getLength(),link.getFreespeed(Time.UNDEFINED_TIME),link.getCapacity(Time.UNDEFINED_TIME),link.getNumberOfLanes(Time.UNDEFINED_TIME));
 	}
 
 	public void setMaxFacOnLink(int max_number_facilities) {
 		this.maxFacOnLink = max_number_facilities;
 	}
 }	
-
-	
-
