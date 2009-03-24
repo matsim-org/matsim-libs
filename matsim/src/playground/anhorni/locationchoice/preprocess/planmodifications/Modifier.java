a49adab82bf35d92d17e19931cd85cfac0b06474
andreas77 <andreashorni@hotmail.com>
2009-03-24 16:54:07 +0000

    moved plan modifiers to new package
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@6122 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/anhorni/locationchoice/preprocess/planmodifications/Modifier.java
@@ -0,0 +1,22 @@
+package playground.anhorni.locationchoice.preprocess.planmodifications;
+
+import org.matsim.interfaces.core.v01.Facilities;
+import org.matsim.interfaces.core.v01.Population;
+import org.matsim.network.NetworkLayer;
+
+public abstract class Modifier {
+
+	protected Population plans=null;
+	protected NetworkLayer network=null;
+	protected Facilities  facilities=null;
+
+
+	public Modifier(Population plans, NetworkLayer network, Facilities  facilities) {
+		this.plans=plans;
+		this.network=network;
+		this.facilities=facilities;
+	}
+
+	public abstract void modify();
+
+}
