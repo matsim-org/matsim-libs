eb4c0adc694926fd3276b891487da4d382e5b1aa
yu_vsp <yu_vsp@users.sf.net>
2009-03-30 21:41:39 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@6254 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/yu/analysis/forZrh/Analysis4Zrh.java
@@ -0,0 +1,37 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * Analysis4Zrh.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+/**
+ * 
+ */
+package playground.yu.analysis.forZrh;
+
+/**
+ * keep some general tools for Analysis for Zurich and respectively Kanton
+ * Zurich
+ * 
+ * @author yu
+ * 
+ */
+public interface Analysis4Zrh {
+	public enum ActTypeStartWith {
+		h, w, s, e, l, o
+	}
+}
