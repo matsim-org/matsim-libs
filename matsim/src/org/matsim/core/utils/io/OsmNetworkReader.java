4185e01410e4601f5704d3921976116c8c7e806b
vsp_neumann <neumann@senozon.de>
2009-04-07 10:43:08 +0000

    - introduced layers and freespeed scaling, see javadoc
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@6337 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/core/utils/io/OsmNetworkReader.java
+++ matsim/src/org/matsim/core/utils/io/OsmNetworkReader.java
@@ -64,13 +64,15 @@ import org.xml.sax.SAXException;
  *
  * OpenStreetMap only contains limited information regarding traffic flow in the streets. The most valuable attribute
  * of OSM data is a <a href="http://wiki.openstreetmap.org/wiki/Map_Features#Highway" target="_blank">categorization</a>
- * of "ways". This reader allows to set {@link #setHighwayDefaults(String, double, double, double) defaults} how
+ * of "ways". This reader allows to set {@link #setHighwayDefaults(int, String, double, double, double, double) defaults} how
  * those categories should be interpreted to create a network with suitable attributes for traffic simulation.
- * For the most common highway-types, some basic defaults are loaded automatically (see code), but they can be
+ * For the most common highway-types, some basic defaults can be loaded automatically (see code), but they can be
  * overwritten if desired. If the optional attributes <code>lanes</code> and <code>oneway</code> are set in the
- * osm data, they overwrite the default values.
+ * osm data, they overwrite the default values. Using {@link #setHierarchyLayer(double, double, double, double, int) hierarchy layers},
+ * multiple overlapping areas can be specified, each with a different denseness, e.g. one only containing motorways,
+ * a second one containing every link down to footways.
  *
- * @author mrieser
+ * @author mrieser, aneumann
  */
 public class OsmNetworkReader {
 
@@ -84,6 +86,11 @@ public class OsmNetworkReader {
 	private final NetworkLayer network;
 	/*package*/ final CoordinateTransformation transform;
 	private boolean keepPaths = false;
+	private boolean scaleMaxSpeed = false;
+	private final List<OsmFilter> hierarchyLayers = new ArrayList<OsmFilter>();
+
+
+	
 	
 	/**
 	 * Creates a new Reader to convert OSM data into a MATSim network.
@@ -92,7 +99,7 @@ public class OsmNetworkReader {
 	 * @param transformation A coordinate transformation to be used. OSM-data comes as WGS84, which is often not optimal for MATSim.
 	 */
 	public OsmNetworkReader(final NetworkLayer network, final CoordinateTransformation transformation) {
-		this(network, transformation, -1);
+		this(network, transformation, true);
 	}
 
 	/**
@@ -100,63 +107,27 @@ public class OsmNetworkReader {
 	 *
 	 * @param network An empty network where the converted OSM data will be stored.
 	 * @param transformation A coordinate transformation to be used. OSM-data comes as WGS84, which is often not optimal for MATSim.
-	 * @param parseLvl Hierarchy level indicating, which roads to convert. Level specified:
-	 * <ul style="list-style-type:none">
-	 * <li>1 - Only motorways and its links / onramps</li>
-	 * <li>2 - Lvl 1 + trunk road and its links</li>
-	 * <li>3 - Lvl 2 + primary roads and its links</li>
-	 * <li>4 - Lvl 3 + secondary roads</li>
-	 * <li>5 - Lvl 4 + tertiary roads</li>
-	 * <li>6 - Lvl 5 + roads defined as living streets, minor, residential or unclassified.</li></ul>
+	 * @param useHighwayDefaults Highway defaults are set to standard values, if true.
 	 */
-	@SuppressWarnings("fallthrough")
-	public OsmNetworkReader(final NetworkLayer network, final CoordinateTransformation transformation, int parseLvl) {
+	public OsmNetworkReader(final NetworkLayer network, final CoordinateTransformation transformation, boolean useHighwayDefaults) {
 		this.network = network;
 		this.transform = transformation;
-		
-		switch (parseLvl) {
 
-		case 6:
-			this.setHighwayDefaults("minor", 1, 45.0 / 3.6, 600); // mostly nothing
-			this.setHighwayDefaults("unclassified", 1, 45.0 / 3.6, 600);
-			this.setHighwayDefaults("residential", 1, 30.0 / 3.6, 600);
-			this.setHighwayDefaults("living_street", 1, 15.0 / 3.6, 300);
-
-		case 5:
-			this.setHighwayDefaults("tertiary", 1, 45.0 / 3.6, 600); // ca wip
-
-		case 4:
-			this.setHighwayDefaults("secondary", 1, 60.0 / 3.6, 1000);
-
-		case 3:
-			this.setHighwayDefaults("primary", 1, 80.0 / 3.6, 1500);
-			this.setHighwayDefaults("primary_link", 1, 60.0 / 3.6, 1500);
-
-		case 2:
-			this.setHighwayDefaults("trunk", 1, 80.0 / 3.6, 2000);
-			this.setHighwayDefaults("trunk_link", 1, 50.0 / 3.6, 1500);
-
-		case 1:
-			this.setHighwayDefaults("motorway", 2, 120.0 / 3.6, 2000, true);
-			this.setHighwayDefaults("motorway_link", 1, 80.0 / 3.6, 1500, true);
-			break;
-
-		default:
-			log.info("Parse level not specified or unknown. Falling back to default values.");
-			this.setHighwayDefaults("motorway",      2, 120.0/3.6, 2000, true);
-			this.setHighwayDefaults("motorway_link", 1,  80.0/3.6, 1500, true);
-			this.setHighwayDefaults("trunk",         1,  80.0/3.6, 2000);
-			this.setHighwayDefaults("trunk_link",    1,  50.0/3.6, 1500);
-			this.setHighwayDefaults("primary",       1,  80.0/3.6, 1500);
-			this.setHighwayDefaults("primary_link",  1,  60.0/3.6, 1500);
-			this.setHighwayDefaults("secondary",     1,  60.0/3.6, 1000);
-			this.setHighwayDefaults("tertiary",      1,  45.0/3.6,  600);
-			this.setHighwayDefaults("minor",         1,  45.0/3.6,  600);
-			this.setHighwayDefaults("unclassified",  1,  45.0/3.6,  600);
-			this.setHighwayDefaults("residential",   1,  30.0/3.6,  600);
-			this.setHighwayDefaults("living_street", 1,  15.0/3.6,  300);
-			break;
-		}		
+		if (useHighwayDefaults) {
+			log.info("Falling back to default values.");
+			this.setHighwayDefaults(1, "motorway",      2, 120.0/3.6, 1.0, 2000, true);
+			this.setHighwayDefaults(1, "motorway_link", 1,  80.0/3.6, 1.0, 1500, true);
+			this.setHighwayDefaults(2, "trunk",         1,  80.0/3.6, 1.0, 2000);
+			this.setHighwayDefaults(2, "trunk_link",    1,  50.0/3.6, 1.0, 1500);
+			this.setHighwayDefaults(3, "primary",       1,  80.0/3.6, 1.0, 1500);
+			this.setHighwayDefaults(3, "primary_link",  1,  60.0/3.6, 1.0, 1500);
+			this.setHighwayDefaults(4, "secondary",     1,  60.0/3.6, 1.0, 1000);
+			this.setHighwayDefaults(5, "tertiary",      1,  45.0/3.6, 1.0,  600);
+			this.setHighwayDefaults(6, "minor",         1,  45.0/3.6, 1.0,  600);
+			this.setHighwayDefaults(6, "unclassified",  1,  45.0/3.6, 1.0,  600);
+			this.setHighwayDefaults(6, "residential",   1,  30.0/3.6, 1.0,  600);
+			this.setHighwayDefaults(6, "living_street", 1,  15.0/3.6, 1.0,  300);
+		}
 	}
 	
 	/**
@@ -189,29 +160,33 @@ public class OsmNetworkReader {
 	/**
 	 * Sets defaults for converting OSM highway paths into MATSim links, assuming it is no oneway road.
 	 *
+	 * @param hierarchy The hierarchy layer the highway appears.
 	 * @param highwayType The type of highway these defaults are for.
 	 * @param lanes number of lanes on that road type
 	 * @param freespeed the free speed vehicles can drive on that road type
+	 * @param freespeedFactor the factor the freespeed is scaled
 	 * @param laneCapacity_vehPerHour the capacity per lane [veh/h]
 	 *
 	 * @see <a href="http://wiki.openstreetmap.org/wiki/Map_Features#Highway">http://wiki.openstreetmap.org/wiki/Map_Features#Highway</a>
 	 */
-	public void setHighwayDefaults(final String highwayType, final double lanes, final double freespeed, final double laneCapacity_vehPerHour) {
-		setHighwayDefaults(highwayType, lanes, freespeed, laneCapacity_vehPerHour, false);
+	public void setHighwayDefaults(final int hierarchy , final String highwayType, final double lanes, final double freespeed, final double freespeedFactor, final double laneCapacity_vehPerHour) {
+		setHighwayDefaults(hierarchy, highwayType, lanes, freespeed, freespeedFactor, laneCapacity_vehPerHour, false);
 	}
 
 	/**
 	 * Sets defaults for converting OSM highway paths into MATSim links.
 	 *
+	 * @param hierarchy The hierarchy layer the highway appears in.
 	 * @param highwayType The type of highway these defaults are for.
 	 * @param lanes number of lanes on that road type
 	 * @param freespeed the free speed vehicles can drive on that road type
+	 * @param freespeedFactor the factor the freespeed is scaled
 	 * @param laneCapacity_vehPerHour the capacity per lane [veh/h]
 	 * @param oneway <code>true</code> to say that this road is a oneway road
 	 */
-	public void setHighwayDefaults(final String highwayType, final double lanes, final double freespeed,
-			final double laneCapacity_vehPerHour, final boolean oneway) {
-		this.highwayDefaults.put(highwayType, new OsmHighwayDefaults(lanes, freespeed, laneCapacity_vehPerHour, oneway));
+	public void setHighwayDefaults(final int hierarchy, final String highwayType, final double lanes, final double freespeed,
+			final double freespeedFactor, final double laneCapacity_vehPerHour, final boolean oneway) {
+		this.highwayDefaults.put(highwayType, new OsmHighwayDefaults(hierarchy, lanes, freespeed, freespeedFactor, laneCapacity_vehPerHour, oneway));
 	}
 
 	/**
@@ -228,15 +203,44 @@ public class OsmNetworkReader {
 	public void setKeepPaths(final boolean keepPaths) {
 		this.keepPaths = keepPaths;
 	}
+	
+	/**
+	 * In case the speed limit allowed does not represent the speed a vehicle can actually realize, e.g. by constrains of
+	 * traffic lights not explicitly modeled, a kind of "average simulated speed" can be used.
+	 *
+	 * Defaults to <code>false</code>.
+	 *
+	 * @param scaleMaxSpeed <code>true</code> to scale the speed limit down by the value specified by the 
+	 * {@link #setHighwayDefaults(int, String, double, double, double, double) defaults}.
+	 */
+	public void setScaleMaxSpeed(final boolean scaleMaxSpeed) {
+		this.scaleMaxSpeed = scaleMaxSpeed;
+	}
+	
+	/**
+	 * Defines a new hierarchy layer by specifying a rectangle and the hierarchy level to which highways will be converted. 
+	 * 
+	 * @param coordNWLat The latitude of the north western corner of the rectangle.
+	 * @param coordNWLon The longitude of the north western corner of the rectangle.
+	 * @param coordSELat The latitude of the south eastern corner of the rectangle.
+	 * @param coordSELon The longitude of the south eastern corner of the rectangle.
+	 * @param hierarchy Layer specifying the hierarchy of the layers starting with 1 as the top layer.
+	 */
+	public void setHierarchyLayer(final double coordNWLat, final double coordNWLon, final double coordSELat, final double coordSELon, final int hierarchy) {
+		this.hierarchyLayers.add(new OsmFilter(this.transform.transform(new CoordImpl(coordNWLon, coordNWLat)), this.transform.transform(new CoordImpl(coordSELon, coordSELat)), hierarchy));
+	}
 
 	private void convert() {
 		this.network.setCapacityPeriod(3600);
-
+		
 		// check which nodes are used
 		for (OsmWay way : this.ways.values()) {
 			String highway = way.tags.get("highway");
 			if (highway != null) {
 				if (this.highwayDefaults.containsKey(highway)) {
+					// check to which level a way belongs
+					way.hierarchy = this.highwayDefaults.get(highway).hierarchy;
+					
 					// first and last are counted twice, so they are kept in all cases
 					this.nodes.get(way.nodes.get(0)).ways++;
 					this.nodes.get(way.nodes.get(way.nodes.size()-1)).ways++;
@@ -313,7 +317,22 @@ public class OsmNetworkReader {
 						if (toNode != lastToNode) {
 							length += CoordUtils.calcDistance(lastToNode.coord, toNode.coord);
 							if (toNode.used) {
-								createLink(this.network, way, fromNode, toNode, length);
+
+								if(this.hierarchyLayers.isEmpty()){
+									createLink(this.network, way, fromNode, toNode, length);
+								} else {
+									for (OsmFilter osmFilter : this.hierarchyLayers) {
+										if(osmFilter.coordInFilter(fromNode.coord, way.hierarchy)){
+											createLink(this.network, way, fromNode, toNode, length);
+											break;
+										}
+										if(osmFilter.coordInFilter(toNode.coord, way.hierarchy)){
+											createLink(this.network, way, fromNode, toNode, length);
+											break;
+										}
+									}
+								}
+								
 								fromNode = toNode;
 								length = 0.0;
 							}
@@ -339,6 +358,7 @@ public class OsmNetworkReader {
 		double nofLanes = defaults.lanes;
 		double laneCapacity = defaults.laneCapacity;
 		double freespeed = defaults.freespeed;
+		double freespeedFactor = defaults.freespeedFactor;
 		boolean oneway = defaults.oneway;
 		boolean onewayReverse = false;
 
@@ -387,6 +407,10 @@ public class OsmNetworkReader {
 		
 		// create the link(s)
 		double capacity = nofLanes * laneCapacity;
+		
+		if (this.scaleMaxSpeed) {
+			freespeed = freespeed * freespeedFactor;
+		}
 
 		if (!onewayReverse) {
 			Link l = network.createLink(new IdImpl(this.id), network.getNode(fromNode.id), network.getNode(toNode.id), length, freespeed, capacity, nofLanes);
@@ -397,6 +421,33 @@ public class OsmNetworkReader {
 			Link l = network.createLink(new IdImpl(this.id), network.getNode(toNode.id), network.getNode(fromNode.id), length, freespeed, capacity, nofLanes);
 			l.setOrigId(origId);
 			this.id++;
+		}
+	}
+	
+	private static class OsmFilter {
+//		public final Id id;
+		public final Coord coordNW;
+		public final Coord coordSE;
+		public final int hierarchy;
+
+		public OsmFilter(final Coord coordNW, final Coord coordSE, final int hierarchy) {
+//			this.id = id;
+			this.coordNW = coordNW;
+			this.coordSE = coordSE;
+			this.hierarchy = hierarchy;
+		}
+		
+		public boolean coordInFilter(final Coord coord, final int hierarchyLevel){
+			if(this.hierarchy < hierarchyLevel){
+				return false;
+			}
+			
+			if(this.coordNW.getX() < coord.getX() && coord.getX() < this.coordSE.getX()){
+				if(this.coordNW.getY() > coord.getY() && coord.getY() > this.coordSE.getY()){
+					return true;
+				}
+			}
+			return false;
 		}
 	}
 
@@ -422,6 +473,7 @@ public class OsmNetworkReader {
 		public final Map<String, String> tags = new HashMap<String, String>();
 		public Count count = null;
 		public OsmNode countFromNode = null;
+		public int hierarchy;
 
 		public OsmWay(final long id) {
 			this.id = id;
@@ -430,14 +482,18 @@ public class OsmNetworkReader {
 
 	private static class OsmHighwayDefaults {
 
+		public final int hierarchy;
 		public final double lanes;
 		public final double freespeed;
+		public final double freespeedFactor;
 		public final double laneCapacity;
 		public final boolean oneway;
 
-		public OsmHighwayDefaults(final double lanes, final double freespeed, final double laneCapacity, final boolean oneway) {
+		public OsmHighwayDefaults(final int hierarchy, final double lanes, final double freespeed, final double freespeedFactor, final double laneCapacity, final boolean oneway) {
+			this.hierarchy = hierarchy;
 			this.lanes = lanes;
 			this.freespeed = freespeed;
+			this.freespeedFactor = freespeedFactor;
 			this.laneCapacity = laneCapacity;
 			this.oneway = oneway;
 		}
