d40fba59d3931aca039a8e413f89d6f19b92de2a
mrieser <rieser@gmail.com>
2009-04-09 11:57:54 +0000

    fixed typo in Javadoc
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@6355 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/core/mobsim/queuesim/DriverAgent.java
+++ matsim/src/org/matsim/core/mobsim/queuesim/DriverAgent.java
@@ -30,18 +30,18 @@ import org.matsim.core.api.population.Person;
 public interface DriverAgent {
 
 	/**
-	 * The time the agent wants to depart fromo an Activity. If the agent is currrently driving,
+	 * The time the agent wants to depart from an Activity. If the agent is currrently driving,
 	 * the return value cannot be interpreted (e.g. it is not defined if it is the departure time
 	 * from the previous activity, or from the next one).
-	 * 
+	 *
 	 * @return the time when the agent wants to depart from an activity.
 	 */
 	public double getDepartureTime();
-	
+
 	/**
 	 * Sets the time when the agent wants to leave the next (or current, if currently not driving)
 	 * Activity.
-	 * 
+	 *
 	 * @param departureTime
 	 */
 	public void setDepartureTime(final double departureTime);
@@ -50,13 +50,13 @@ public interface DriverAgent {
 	 * Informs the agent that it arrived at the destination of the current leg.
 	 * The agent can then decide if he wants to start an activity, or continue
 	 * on another leg.
-	 * 
+	 *
 	 * @param now the current time in the simulation
 	 */
 	public void legEnds(final double now);
-	
+
 	// the methods below are yet unclear how useful they are in the interface.
-	
+
 	public void setVehicle(final QueueVehicle veh);
 
 	public void setCurrentLink(final Link link);
@@ -66,7 +66,7 @@ public interface DriverAgent {
 	public Link getDestinationLink();
 
 	public void incCurrentNode();
-	
+
 	/**
 	 * Returns the next link the vehicle will drive along.
 	 *
@@ -75,17 +75,17 @@ public interface DriverAgent {
 	public Link chooseNextLink();
 
 	// those below shouldn't be part of the interface in my opinion...
-	
+
 	public Person getPerson();
 
 	public void reachActivity(final double now, final QueueLink currentQueueLink);
-	
+
 	public void leaveActivity(final double now);
 
 	public Leg getCurrentLeg();
-	
+
 	public int getCurrentNodeIndex();
-	
+
 	public int getNextActivity();
 
 	public List<? extends BasicPlanElement> getActsLegs();
