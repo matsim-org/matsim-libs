070a25256e41478368234ac56333564bf5eed1af
mrieser <rieser@gmail.com>
2009-04-12 07:40:28 +0000

    changed visibility of member EPSILON from public to protected.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@6384 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/test/src/org/matsim/testcases/MatsimTestCase.java
+++ matsim/test/src/org/matsim/testcases/MatsimTestCase.java
@@ -31,7 +31,7 @@ import org.matsim.core.utils.io.IOUtils;
 public class MatsimTestCase extends TestCase {
 
 	/** A constant for the exactness when comparing doubles. */
-	public static final double EPSILON = 1e-10;
+	protected static final double EPSILON = 1e-10;
 
 	/** The default output directory, where files of this test should be written to.
 	 * Includes the trailing '/' to denote a directory. */
