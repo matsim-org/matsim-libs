fc49e2520c6d64365166f013513bfac698620940
mrieser <rieser@gmail.com>
2009-04-12 09:48:56 +0000

    code cleanup, prevent compiler warnings.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@6389 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/test/src/org/matsim/population/algorithms/PlanAnalyzeSubtoursTest.java
+++ matsim/test/src/org/matsim/population/algorithms/PlanAnalyzeSubtoursTest.java
@@ -23,7 +23,6 @@ package org.matsim.population.algorithms;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
-import org.apache.log4j.Logger;
 import org.matsim.api.basic.v01.TransportMode;
 import org.matsim.core.api.facilities.Facility;
 import org.matsim.core.api.network.Link;
@@ -31,10 +30,10 @@ import org.matsim.core.api.population.Activity;
 import org.matsim.core.api.population.Person;
 import org.matsim.core.api.population.Plan;
 import org.matsim.core.basic.v01.IdImpl;
+import org.matsim.core.config.Config;
 import org.matsim.core.config.groups.PlanomatConfigGroup;
 import org.matsim.core.facilities.FacilitiesImpl;
 import org.matsim.core.facilities.MatsimFacilitiesReader;
-import org.matsim.core.gbl.Gbl;
 import org.matsim.core.network.MatsimNetworkReader;
 import org.matsim.core.network.NetworkLayer;
 import org.matsim.core.population.PersonImpl;
@@ -52,40 +51,29 @@ import org.matsim.world.Location;
  */
 public class PlanAnalyzeSubtoursTest extends MatsimTestCase {
 
-	public static final String SUBTOUR_ANALYSIS_MODULE_NAME = "subtourAnalysis";
-	public static final String LOCATION_TYPE = "locationType";
 	private static final String CONFIGFILE = "test/scenarios/equil/config.xml";
 
-	private static Logger log = Logger.getLogger(PlanAnalyzeSubtoursTest.class);
-
-	protected void setUp() throws Exception {
-		super.setUp();
-		super.loadConfig(PlanAnalyzeSubtoursTest.CONFIGFILE);
-	}
-
 	public void testNetworkBased() {
+		Config config = loadConfig(PlanAnalyzeSubtoursTest.CONFIGFILE);
 
-		log.info("Reading network xml file...");
 		NetworkLayer network = new NetworkLayer();
-		new MatsimNetworkReader(network).readFile(Gbl.getConfig().network().getInputFile());
-		log.info("Reading network xml file...done.");
-
-		Gbl.getConfig().planomat().setTripStructureAnalysisLayer("link");
-		this.runDemo((Layer) network);
+		new MatsimNetworkReader(network).readFile(config.network().getInputFile());
+		
+		config.planomat().setTripStructureAnalysisLayer("link");
+		this.runDemo(network);
 	}
 	
 	public void testFacilitiesBased() {
-
-		log.info("Reading facilities xml file...");
+		Config config = loadConfig(PlanAnalyzeSubtoursTest.CONFIGFILE);
 		FacilitiesImpl facilities = new FacilitiesImpl();
-		new MatsimFacilitiesReader(facilities).readFile(Gbl.getConfig().facilities().getInputFile());
-		log.info("Reading facilities xml file...done.");
+		new MatsimFacilitiesReader(facilities).readFile(config.facilities().getInputFile());
 
-		Gbl.getConfig().planomat().setTripStructureAnalysisLayer("facility");
-		this.runDemo((Layer) facilities);
+		config.planomat().setTripStructureAnalysisLayer("facility");
+		this.runDemo(facilities);
 	}
 	
 	protected void runDemo(Layer layer) {
+		Config config = loadConfig(PlanAnalyzeSubtoursTest.CONFIGFILE);
 
 		PlanAnalyzeSubtours testee = new PlanAnalyzeSubtours();
 
@@ -95,61 +83,70 @@ public class PlanAnalyzeSubtoursTest extends MatsimTestCase {
 		HashMap<String, String> expectedSubtourIndexations = new HashMap<String, String>();
 		HashMap<String, Integer> expectedNumSubtours = new HashMap<String, Integer>();
 		
+		Integer i0 = Integer.valueOf(0);
+		Integer i1 = Integer.valueOf(1);
+		Integer i2 = Integer.valueOf(2);
+		Integer i3 = Integer.valueOf(3);
+		Integer i4 = Integer.valueOf(4);
+		Integer i5 = Integer.valueOf(5);
+		Integer i6 = Integer.valueOf(6);
+		Integer i7 = Integer.valueOf(7);
+		
 		String testedActChainLocations = "1 2 1";
 		expectedSubtourIndexations.put(testedActChainLocations, "0 0");
-		expectedNumSubtours.put(testedActChainLocations, 1);
+		expectedNumSubtours.put(testedActChainLocations, i1);
 		
 		testedActChainLocations = "1 2 20 1";
 		expectedSubtourIndexations.put(testedActChainLocations, "0 0 0");
-		expectedNumSubtours.put(testedActChainLocations, 1);
+		expectedNumSubtours.put(testedActChainLocations, i1);
 
 		testedActChainLocations = "1 2 1 2 1";
 		expectedSubtourIndexations.put(testedActChainLocations, "0 0 1 1");
-		expectedNumSubtours.put(testedActChainLocations, 2);
+		expectedNumSubtours.put(testedActChainLocations, i2);
 
 		testedActChainLocations = "1 2 1 3 1";
 		expectedSubtourIndexations.put(testedActChainLocations, "0 0 1 1");
-		expectedNumSubtours.put(testedActChainLocations, 2);
+		expectedNumSubtours.put(testedActChainLocations, i2);
 
 		testedActChainLocations = "1 2 2 1";
 		expectedSubtourIndexations.put(testedActChainLocations, "1 0 1");
-		expectedNumSubtours.put(testedActChainLocations, 2);
+		expectedNumSubtours.put(testedActChainLocations, i2);
 		
 		testedActChainLocations = "1 2 2 2 2 2 2 2 1";
 		expectedSubtourIndexations.put(testedActChainLocations, "6 0 1 2 3 4 5 6");
-		expectedNumSubtours.put(testedActChainLocations, 7);
+		expectedNumSubtours.put(testedActChainLocations, i7);
 
 		testedActChainLocations = "1 2 3 2 1";
 		expectedSubtourIndexations.put(testedActChainLocations, "1 0 0 1");
-		expectedNumSubtours.put(testedActChainLocations, 2);
+		expectedNumSubtours.put(testedActChainLocations, i2);
 
 		testedActChainLocations = "1 2 3 4 3 2 1";
 		expectedSubtourIndexations.put(testedActChainLocations, "2 1 0 0 1 2");
-		expectedNumSubtours.put(testedActChainLocations, 3);
+		expectedNumSubtours.put(testedActChainLocations, i3);
 
 		testedActChainLocations = "1 2 14 2 14 2 1";
 		expectedSubtourIndexations.put(testedActChainLocations, "2 0 0 1 1 2");
-		expectedNumSubtours.put(testedActChainLocations, 3);
+		expectedNumSubtours.put(testedActChainLocations, i3);
 
 		testedActChainLocations = "1 2 14 14 2 14 2 1";
 		expectedSubtourIndexations.put(testedActChainLocations, "3 1 0 1 2 2 3");
-		expectedNumSubtours.put(testedActChainLocations, 4);
+		expectedNumSubtours.put(testedActChainLocations, i4);
 
 		testedActChainLocations = "1 2 3 4 3 2 5 4 5 1";
 		expectedSubtourIndexations.put(testedActChainLocations, "3 1 0 0 1 3 2 2 3");
-		expectedNumSubtours.put(testedActChainLocations, 4);
+		expectedNumSubtours.put(testedActChainLocations, i4);
 
 		testedActChainLocations = "1 2 3 2 3 2 1 2 1";
 		expectedSubtourIndexations.put(testedActChainLocations, "2 0 0 1 1 2 3 3");
-		expectedNumSubtours.put(testedActChainLocations, 4);
+		expectedNumSubtours.put(testedActChainLocations, i4);
 
 		testedActChainLocations = "1 1 1 1 1 2 1";
 		expectedSubtourIndexations.put(testedActChainLocations, "0 1 2 3 4 4");
-		expectedNumSubtours.put(testedActChainLocations, 5);
+		expectedNumSubtours.put(testedActChainLocations, i5);
 
 		testedActChainLocations = "1 2 1 1";
 		expectedSubtourIndexations.put(testedActChainLocations, "0 0 1");
-		expectedNumSubtours.put(testedActChainLocations, 2);
+		expectedNumSubtours.put(testedActChainLocations, i2);
 
 		testedActChainLocations = "1 2 3 4";
 		expectedSubtourIndexations.put(
@@ -157,22 +154,21 @@ public class PlanAnalyzeSubtoursTest extends MatsimTestCase {
 						Integer.toString(PlanAnalyzeSubtours.UNDEFINED) + " " + 
 						Integer.toString(PlanAnalyzeSubtours.UNDEFINED) + " " + 
 						Integer.toString(PlanAnalyzeSubtours.UNDEFINED));
-		expectedNumSubtours.put(testedActChainLocations, 0);
+		expectedNumSubtours.put(testedActChainLocations, i0);
 
 		testedActChainLocations = "1 2 2 3 2 2 2 1 4 1";
 		expectedSubtourIndexations.put(testedActChainLocations, "4 0 1 1 2 3 4 5 5");
-		expectedNumSubtours.put(testedActChainLocations, 6);
+		expectedNumSubtours.put(testedActChainLocations, i6);
 		
 		testedActChainLocations = "1 2 3 4 3 1";
 		expectedSubtourIndexations.put(testedActChainLocations, "1 1 0 0 1");
-		expectedNumSubtours.put(testedActChainLocations, 2);
+		expectedNumSubtours.put(testedActChainLocations, i2);
 		
-		PlanomatConfigGroup.TripStructureAnalysisLayerOption subtourAnalysisLocationType = Gbl.getConfig().planomat().getTripStructureAnalysisLayer();
+		PlanomatConfigGroup.TripStructureAnalysisLayerOption subtourAnalysisLocationType = config.planomat().getTripStructureAnalysisLayer();
 		Location location = null;
 		Activity act = null;
 		for (Entry<String, String> entry: expectedSubtourIndexations.entrySet()) {
 			String facString  = entry.getKey();
-			log.info("Testing location sequence: " + facString);
 
 			Plan plan = new org.matsim.core.population.PlanImpl(person);
 
@@ -197,9 +193,9 @@ public class PlanAnalyzeSubtoursTest extends MatsimTestCase {
 				builder.append(' ');
 			}
 			String actualSubtourIndexation = builder.substring(0, builder.length() - 1);
-			assertEquals(entry.getValue(), actualSubtourIndexation);
+			assertEquals("failure testing " + facString, entry.getValue(), actualSubtourIndexation);
 			
-			assertEquals(expectedNumSubtours.get(facString).intValue(), testee.getNumSubtours());
+			assertEquals("failure testing " + facString, expectedNumSubtours.get(facString).intValue(), testee.getNumSubtours());
 		}
 
 	}
