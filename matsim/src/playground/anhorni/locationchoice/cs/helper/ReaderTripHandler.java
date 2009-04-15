8a1602be42de053d1831847f27d850abff18efc0
andreas77 <andreashorni@hotmail.com>
2009-04-15 13:29:41 +0000

    coord -> coordEnd
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@6437 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/anhorni/locationchoice/cs/helper/ReaderTripHandler.java
+++ matsim/src/playground/anhorni/locationchoice/cs/helper/ReaderTripHandler.java
@@ -43,7 +43,7 @@ public class ReaderTripHandler {
 		
 		this.chosenFacilityId = chosenFacilityId;
 			
-		Coord afterShoppingCoord = mzTrip.getCoord();
+		Coord afterShoppingCoord = mzTrip.getCoordEnd();
 		Activity afterShoppingAct = new org.matsim.core.population.ActivityImpl("end", afterShoppingCoord);
 		afterShoppingAct.setLink(network.getNearestLink(afterShoppingCoord));
 		
