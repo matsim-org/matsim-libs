ad0e4bf610bec0b6152a32ea603e5246199c5ec9
ciarif <ciarif@users.sf.net>
2009-04-19 16:32:43 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@6507 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/ciarif/retailers/MaxLinkRetailerStrategy.java
+++ matsim/src/playground/ciarif/retailers/MaxLinkRetailerStrategy.java
@@ -35,7 +35,7 @@ public class MaxLinkRetailerStrategy implements RetailerStrategy {
 		for (Facility f : facilities.values()) {
 			//Object[] links = controler.getNetwork().getLinks().values().toArray();
 			double rd1 = MatsimRandom.getRandom().nextDouble();
-			if (rd1 < 0.1 & f.getActivityOption("shop").getCapacity()>50 ) { 
+			//if (rd1 < 0.1 & f.getActivityOption("shop").getCapacity()>50 ) { 
 				//First it is ensured that only a given percentage of facilities can be moved at one step,
 				// then the specific shop is moved only if it exceeds a given capacity. This allows for moving 
 				// only "interesting" facilities (large enough to notice differences in the customer data), but 
@@ -82,6 +82,6 @@ public class MaxLinkRetailerStrategy implements RetailerStrategy {
 					}
 				}
 			}
-		}
+		//}
 	}
 }
