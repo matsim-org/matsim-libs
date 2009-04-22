2f4b451b9d7e0331557ca06787d9ec4c889e0f80
vsp_nagel <2008@kainagel.org>
2009-04-22 12:47:20 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@6532 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/kai/usecases/basicdemandgen/MyControler2.java
+++ matsim/src/playground/kai/usecases/basicdemandgen/MyControler2.java
@@ -23,62 +23,41 @@ package playground.kai.usecases.basicdemandgen;
  */
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import org.apache.log4j.Logger;
 import org.geotools.data.FeatureSource;
-import org.geotools.feature.Feature;
-import org.geotools.feature.FeatureIterator;
-import org.matsim.api.basic.v01.Coord;
-import org.matsim.api.basic.v01.Id;
-import org.matsim.api.basic.v01.TransportMode;
-import org.matsim.api.basic.v01.population.BasicActivity;
-import org.matsim.api.basic.v01.population.BasicLeg;
-import org.matsim.api.basic.v01.population.BasicPerson;
-import org.matsim.api.basic.v01.population.BasicPlan;
-import org.matsim.api.basic.v01.population.BasicPopulation;
-import org.matsim.api.basic.v01.population.BasicPopulationBuilder;
-import org.matsim.api.core.v01.Scenario;
-import org.matsim.api.core.v01.ScenarioLoader;
-import org.matsim.core.api.population.Population;
-import org.matsim.core.basic.v01.IdImpl;
-import org.matsim.core.config.Config;
-import org.matsim.core.controler.Controler;
-import org.matsim.core.gbl.Gbl;
-import org.matsim.core.network.NetworkLayer;
-import org.matsim.core.network.algorithms.NetworkCleaner;
-import org.matsim.core.population.PopulationImpl;
-import org.matsim.core.population.PopulationWriter;
-import org.matsim.core.utils.geometry.CoordImpl;
-import org.matsim.core.utils.gis.ShapeFileReader;
+import org.geotools.feature.*;
 
-import com.vividsolutions.jts.geom.MultiPolygon;
-import com.vividsolutions.jts.geom.Point;
-import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.geom.*;
+
+import org.matsim.api.basic.v01.*;
+import org.matsim.api.basic.v01.population.*;
+import org.matsim.core.utils.gis.ShapeFileReader;
 
 
 
 public class MyControler2 {
 	private static final Logger log = Logger.getLogger(MyControler2.class);
 
+	@SuppressWarnings("unchecked")
 	private static BasicPopulation createPlansFromShp(final FeatureSource n) {
-		List<Coord> workPlaces = new ArrayList<Coord>() ;
-
-//		BasicPopulation<? extends BasicPerson<? extends BasicPlan>> population = new PopulationImpl(PopulationImpl.NO_STREAMING) ;
-		BasicPopulation population = new PopulationImpl() ;
-		// FIXME: select specific implementation here.  Makes sense, but is it what we want?  (Could also be empty population
-		// taken from controler.)
-		// TODO: The generics approach, as of now, is awful.
-		int popCnt = 0 ;
+		BasicScenario sc = new BasicScenarioImpl() ;
 		
+		BasicPopulation population = sc.getPopulation() ;
 		BasicPopulationBuilder pb = population.getPopulationBuilder() ;
 
+		// get all features from the shp file:
 		FeatureIterator it = null; try {
 			it = n.getFeatures().features();
 		} catch (final IOException e) {
 			e.printStackTrace();
 		}
+
+		long popCnt = 0 ;
+		List<Coord> workPlaces = new ArrayList<Coord>() ;
+		
+		// iterate through the features:
 		while (it.hasNext()) {
 			final Feature feature = it.next();
 
@@ -90,7 +69,7 @@ public class MyControler2 {
 			}
 			final Polygon polygon = (Polygon) multiPolygon.getGeometryN(0);
 			Point center = polygon.getCentroid();
-			Coord coord = new CoordImpl ( center.getY() , center.getX() ) ;
+			Coord coord = sc.createCoord( center.getY() , center.getX() ) ;
 
 			int nPersons = 0 ;
 			int nJobs = 0 ;
@@ -103,18 +82,14 @@ public class MyControler2 {
 
 			// generate correct number of persons:
 			for ( int ii=0 ; ii<nPersons ; ii++ ) {
-				Id id = new IdImpl( popCnt ) ; popCnt++ ;
+				Id id = sc.createId( Long.toString( popCnt ) ); popCnt++ ;
 
-				BasicPerson person = null;
-				try {
-					person = pb.createPerson(id); 
-				} catch (Exception e) {
-					e.printStackTrace();
-				} 
+				BasicPerson person = pb.createPerson(id); 
 				population.getPersons().put( id, person ) ;
 				
 				BasicPlan plan = pb.createPlan(person) ;
 				plan.setSelected(true) ; // will also work without
+
 				BasicActivity act = pb.createActivityFromCoord("home",coord) ;
 				plan.getPlanElements().add(act) ;
 			}
@@ -133,10 +108,21 @@ public class MyControler2 {
 			Coord workCoord = workPlaces.get( idx ) ;
 //			workPlaces.remove( idx ) ;
 			// (with replacement.  W/o replacement, make sure that there are enough workplaces!)
-//			playground.kai.urbansim.Utils.completePlanToHwh(plan, workCoord) ; // FIXME once createAct methods are there
 			
-			BasicLeg leg = pb.createLeg(TransportMode.bike) ;
-			plan.getPlanElements().add(leg) ;
+			BasicLeg legH2W = pb.createLeg(BasicLeg.Mode.car) ;
+			plan.getPlanElements().add(legH2W) ;
+			
+			BasicActivity workAct = pb.createActivityFromCoord("work", workCoord ) ;
+			plan.getPlanElements().add(workAct) ;
+			
+			BasicLeg legW2H = pb.createLeg(BasicLeg.Mode.bike) ;
+			plan.getPlanElements().add(legW2H) ;
+			
+			BasicActivity homeAct1 = (BasicActivity) plan.getPlanElements().get(0) ;
+			Coord homeCoord = homeAct1.getCoord() ;
+
+			BasicActivity homeAct2 = pb.createActivityFromCoord("home", homeCoord ) ;
+			plan.getPlanElements().add(homeAct2) ;
 			
 		}
 
@@ -155,39 +141,10 @@ public class MyControler2 {
 		}
 
 		// write the population for debugging purposes
-		PopulationWriter popWriter = new PopulationWriter( (Population) plans,"pop.xml.gz","v4",1) ;
-		popWriter.write();
+		BasicPopulationWriter popWriter = new BasicPopulationWriter( plans ) ;
+		popWriter.write( "pop.xml.gz" ) ;
 
-		log.info("### DONE with demand generation from urbansim ###") ;
-
-		// parse the config arguments so we have a config.  generate scenario data from this
-		Config config;
-		if ( args.length==0 ) {
-			config = Gbl.createConfig(new String[] {"./src/playground/duncan/myconfig1.xml"});
-		} else {
-			config = Gbl.createConfig(args) ;
-		}
-		ScenarioLoader loader = new ScenarioLoader(config);
-		loader.loadNetwork();
-		
-		Scenario scenarioData = loader.getScenario();
-
-		// get the network.  Always cleaning it seems a good idea since someone may have modified the input files manually in
-		// order to implement policy measures.
-		NetworkLayer network = (NetworkLayer) scenarioData.getNetwork() ;
-		log.info("") ; 	log.info("cleaning network ...");
-		NetworkCleaner nwCleaner = new NetworkCleaner() ;
-		nwCleaner.run( network ) ;
-		log.info("... finished cleaning network.") ; log.info("") ;
-
-		// start the control(l)er with the network and plans as defined above
-		Controler controler = new Controler(config,network,(Population) plans) ;
-
-		// this means existing files will be over-written.  Be careful!
-		controler.setOverwriteFiles(true);
-
-		// start the matsim iterations (configured by the config file)
-		controler.run();
+		log.info("### DONE with demand generation  ###") ;
 	}
 
 }
