a45c2673ffe9008078090f58ed332b9383392121
mrieser <rieser@gmail.com>
2009-04-27 15:18:57 +0000

    simple data structure without semantics for storing routes of non-car legs.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@6579 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/org/matsim/core/population/routes/GenericRouteImpl.java
@@ -0,0 +1,45 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * TransitRouteImpl.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2009 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.core.population.routes;
+
+import org.matsim.core.api.network.Link;
+import org.matsim.core.api.population.GenericRoute;
+
+
+public class GenericRouteImpl extends AbstractRoute implements GenericRoute {
+
+	private String routeDescription = null;
+	
+	public GenericRouteImpl(final Link startLink, final Link endLink	) {
+		super(startLink, endLink);
+	}
+
+	public String getRouteDescription() {
+		return this.routeDescription;
+	}
+
+	public void setRouteDescription(Link startLink, String routeDescription, Link endLink) {
+		setStartLink(startLink);
+		this.routeDescription = routeDescription;
+		setEndLink(endLink);
+	}
+
+}
