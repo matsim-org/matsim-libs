b8e105bd2ed61fe1d566241ad7b0ec749d955ca7
mrieser <rieser@gmail.com>
2009-04-27 15:19:32 +0000

    simple data structure without semantics for storing routes of non-car legs.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@6580 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/org/matsim/core/api/population/GenericRoute.java
@@ -0,0 +1,36 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * TransitRoute.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2009 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.core.api.population;
+
+import org.matsim.core.api.network.Link;
+
+/**
+ * A route for public transport trips.
+ *
+ * @author mrieser
+ */
+public interface GenericRoute extends Route {
+
+	public void setRouteDescription(final Link startLink, final String routeDescription, final Link endLink);
+	
+	public String getRouteDescription();
+
+}
