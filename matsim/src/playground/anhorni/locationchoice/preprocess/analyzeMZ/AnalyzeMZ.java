6c6d13cf4fbde119af5017f566c7463f0ddeaf3f
andreas77 <andreashorni@hotmail.com>
2009-04-30 12:28:44 +0000

    Merged MZ act and trip analysis
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@6660 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/anhorni/locationchoice/preprocess/analyzeMZ/AnalyzeMZ.java
+++ matsim/src/playground/anhorni/locationchoice/preprocess/analyzeMZ/AnalyzeMZ.java
@@ -1,44 +1,115 @@
 package playground.anhorni.locationchoice.preprocess.analyzeMZ;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
+import java.util.TreeMap;
 import java.util.Vector;
 import org.apache.log4j.Logger;
+import org.matsim.api.basic.v01.Id;
+import org.matsim.core.gbl.Gbl;
 
 import playground.anhorni.locationchoice.assemblefacilities.BZReader;
 import playground.anhorni.locationchoice.assemblefacilities.Hectare;
 import playground.anhorni.locationchoice.cs.helper.MZTrip;
 
-
-
-public class AnalyzeMZ {
-	
+public class AnalyzeMZ {	
 	private final static Logger log = Logger.getLogger(AnalyzeMZ.class);
-
+	private String analyzeTrips = "false";
+	private String analyzeActs = "false";
 	private List<MZTrip> mzTrips = new Vector<MZTrip>();
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
 		AnalyzeMZ analyzer = new AnalyzeMZ();
 		analyzer.run();
 	}
 	
 	public void run() {
-		MZReader mzReader = new MZReader();
-		mzTrips = mzReader.read("input/cs/MZ2005_Wege.dat");
-		log.info("Number of MZ trips: " + mzTrips.size());
+		String inputFile = "./input/trb/valid/input.txt";
+		this.readInputFile(inputFile);
 		
+		log.info("Reading MZ ...");
+		MZReader mzReader = new MZReader();
+		mzReader.read("input/MZ/MZ2005_Wege.dat");
+		log.info("Number of MZ trips: " + mzTrips.size());		
+		
+		if (this.analyzeTrips.equals("true")) {
+			this.analyzeTrips(mzReader);
+		}
+		if (this.analyzeActs.equals("true")) {
+			this.analyzeActs(mzReader);
+		}		
+	}
+		
+	public void analyzeActs(MZReader mzReader) {
+		log.info("Starting analyze acts");
 		GroceryFilter groceryfilter = new GroceryFilter();
 		mzTrips = groceryfilter.filterTrips(mzTrips);
 		
 		GeographicalFilter geographicalfilter = new GeographicalFilter();
 		mzTrips = geographicalfilter.filterTrips(mzTrips);
 		
+		log.info("Reading BZ");
 		BZReader bzReader = new BZReader();
-		List<Hectare> hectares = bzReader.readBZGrocery("input/facilities/BZ01_UNT_P_DSVIEW.TXT");
+		List<Hectare> hectares = bzReader.readBZGrocery("input/BZ/BZ01_UNT_P_DSVIEW.TXT");
 		
+		log.info("Create hectare relations");
 		CreateTripHectareRelation relationCreator = new CreateTripHectareRelation();
 		List<MZTripHectare> relations = relationCreator.createRelations(mzTrips, hectares);
 		
+		ActWriter writer = new ActWriter();
+		String outpath = "output/valid/acts/";
+		writer.write(relations, outpath);
+		log.info("Finished analyzeActs ----------------------------------------------");
+	}
+	
+	public void analyzeTrips(MZReader mzReader) {	
+		TreeMap<Id, PersonTrips>  personTrips = mzReader.getPersonTrips();
+		log.info("Number of persons: " + personTrips.size());
+		
+		GeographicalFilter geographicalFilter = new GeographicalFilter();
+		personTrips = geographicalFilter.filterPersons(personTrips);		
+		log.info("Number of persons after plausiblity check: " + personTrips.size());		
+		
+		log.info("Writting ch trips file ...");
+		String outpath = "./output/valid/trips/";
+		
 		TripWriter writer = new TripWriter();
-		writer.write(relations);
+		writer.write(personTrips, "ch", outpath);
+		
+		
+		TreeMap<Id, PersonTrips>  personTripsFiltered = new TreeMap<Id, PersonTrips>();
+		Iterator<PersonTrips> personTrips_it = personTrips.values().iterator();
+		while (personTrips_it.hasNext()) {
+			PersonTrips pt = personTrips_it.next();
+			if (pt.intersectZH()) {
+				personTripsFiltered.put(pt.getPersonId(), pt);
+			}
+		}
+		log.info("Number of persons after ZH filtering: " + personTripsFiltered.size());
+		
+		log.info("Writting zh trips file ...");
+		writer = new TripWriter();
+		writer.write(personTripsFiltered, "zh", outpath);
+		log.info("Finished analyzeTrips ----------------------------------------------");
+	}
+	
+	private void readInputFile(final String inputFile) {
+		try {
+			FileReader fileReader = new FileReader(inputFile);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+			this.analyzeTrips = bufferedReader.readLine();
+			this.analyzeActs = bufferedReader.readLine();
+
+			bufferedReader.close();
+			fileReader.close();
+
+		} catch (IOException e) {
+			Gbl.errorMsg(e);
+		}
 	}
 }
--- matsim/src/playground/anhorni/locationchoice/preprocess/analyzeMZ/TripWriter.java
+++ matsim/src/playground/anhorni/locationchoice/preprocess/analyzeMZ/TripWriter.java
@@ -3,100 +3,60 @@ package playground.anhorni.locationchoice.preprocess.analyzeMZ;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.util.Iterator;
-import java.util.List;
+import java.util.TreeMap;
 
+import org.apache.log4j.Logger;
+import org.matsim.api.basic.v01.Id;
+import org.matsim.core.utils.geometry.CoordImpl;
 import org.matsim.core.utils.io.IOUtils;
 
 public class TripWriter {
 	
+	private final static Logger log = Logger.getLogger(TripWriter.class);
 	
-	/*
-	B015211A = 408;	Verbrauchermaerkte (> 2500 m2) 
-	B015211B = 409; Grosse Supermaerkte (1000-2499 m2)
-	B015211C = 410;	Kleine Supermaerkte (400-999 m2)
-	B015211D = 411;	Grosse Geschaefte (100-399 m2)
-	B015211E = 412;	Kleine Geschaefte (< 100 m2)
-	B015212A = 413; Warenhaeuser
-	*
-	B015221A = 415; Detailhandel mit Obst und Gemuese
-	B015222A = 416; Detailhandel mit Fleisch und Fleischwaren	
-	B015223A = 417; Detailhandel mit Fisch und Meeresfruechten	
-	B015224A = 418; Detailhandel mit Brot, Back- und Suesswaren	
-	B015225A = 419;	Detailhandel mit Getraenken
-	*
-	B015227A = 421; Detailhandel mit Milcherzeugnissen und Eiern	
-	B015227B = 422; Sonstiger Fachdetailhandel mit Nahrungsmitteln, Getraenken und Tabak a.n.g. (in Verkaufsraeumen)
-	*/
-	
-	
-	public void write(List<MZTripHectare> relations) {
-		
-		double[] activitiesCount = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
-		double[] activitiesCountSingle = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
-		
-		double totalActCount = 0.0;
-		double totalActCountSingle = 0.0;
-		
-		String[] NOGA = {
-				"B015211A = 408;	Verbraucherm�rkte (> 2500 m2)",
-				"B015211B = 409; 	Grosse Superm�rkte (1000-2499 m2)",
-				"B015211C = 410;	Kleine Superm�rkte (400-999 m2)",
-				"B015211D = 411;	Grosse Gesch�fte (100-399 m2)",
-				"B015211E = 412;	Kleine Gesch�fte (< 100 m2)",
-				"B015212A = 413; 	Warenh�user",
-				"B015221A = 415; 	Detailhandel mit Obst und Gem�se",
-				"B015222A = 416; 	Detailhandel mit Fleisch und Fleischwaren",
-				"B015223A = 417; 	Detailhandel mit Fisch und Meeresfr�chten",	
-				"B015224A = 418; 	Detailhandel mit Brot, Back- und S�sswaren",	
-				"B015225A = 419;	Detailhandel mit Getr�nken",
-				"B015227A = 421; 	Detailhandel mit Milcherzeugnissen und Eiern",
-				"B015227B = 422; 	Sonstiger Fachdetailhandel mit Nahrungsmitteln, Getr�nken und Tabak a.n.g. (in Verkaufsr�umen)"
-		};
-		
-		
+	public void write(TreeMap<Id, PersonTrips> personTrips, String region, String outpath) {	
+		String header = "Person_id\tStart_X\tStart_Y\tEnd_X\tEnd_Y\tDist\texact_type\n";
 		try {
-			BufferedWriter out = IOUtils.getBufferedWriter("output/trips.txt");
+			BufferedWriter outShopping = 
+				IOUtils.getBufferedWriter(outpath + region + "_shoppingTrips.txt");
+			BufferedWriter outLeisure = 
+				IOUtils.getBufferedWriter(outpath + region + "_leisure.txt");
 			
-			Iterator<MZTripHectare> relations_it = relations.iterator();
-			while (relations_it.hasNext()) {
-				MZTripHectare relation = relations_it.next();
-				
-				//out.write(relation.getMzTrip().getId().toString() + ": ");
-				for (int i = 0; i < relation.getHectare().getShops().size(); i++) {
-					//out.write(relation.getHectare().getShops().get(i) + "\t");
+			outShopping.write(header);
+			outLeisure.write(header);
+			
+			Iterator<PersonTrips> personTrips_it = personTrips.values().iterator();
+			while (personTrips_it.hasNext()) {
+				PersonTrips pt = personTrips_it.next();
+				pt.finish();			
+				String out = pt.getPersonId().toString() + "\t";
+				for (int i = 0; i < pt.getShoppingTrips().size(); i++) {
+					CoordImpl coordStart = pt.getShoppingTrips().get(i).getCoordStart();
+					CoordImpl coordEnd = pt.getShoppingTrips().get(i).getCoordEnd();
 					
-					int index = transform(relation.getHectare().getShops().get(i));
-					if (relation.getHectare().getShops().size() == 1) {
-						activitiesCountSingle[index]++;
-						totalActCountSingle++;
-					}
-					activitiesCount[index] += (1.0 / relation.getHectare().getShops().size());	
-					totalActCount += (1.0 / relation.getHectare().getShops().size());	
+					outShopping.write(out + coordStart.getX() + "\t"+ coordStart.getY() + "\t" 
+							+ coordEnd.getX() + "\t" + coordEnd.getY() + "\t" +
+							coordStart.calcDistance(coordEnd) +  "\t" +
+							pt.getShoppingTrips().get(i).getPurposeCode() + "\n");
 				}
-				//out.newLine();
+
+				for (int i = 0; i < pt.getLeisureTrips().size(); i++) {
+					CoordImpl coordStart = pt.getLeisureTrips().get(i).getCoordStart();
+					CoordImpl coordEnd = pt.getLeisureTrips().get(i).getCoordEnd();
+					
+					outLeisure.write(out + coordStart.getX() + "\t" +  coordStart.getY() + "\t" + 
+							coordEnd.getX() +"\t" + coordEnd.getY() +"\t" + 
+							coordStart.calcDistance(coordEnd)  + "\t" +
+							pt.getLeisureTrips().get(i).getPurposeCode() + "\n");
+				}			
 			}
-			out.flush();
+			outShopping.flush();
+			outLeisure.flush();
 			
-			for (int i = 0; i < 13; i++) {
-				out.write(+ activitiesCount[i] + "\t" +  
-						100.0 * activitiesCount[i]/totalActCount + " per cent" + "\t" +  NOGA[i] + "\n");
-			}
-			out.newLine();
-			out.write("Only one store per hectare: \n");
-			for (int i = 0; i < 13; i++) {
-				out.write(+ activitiesCountSingle[i] + "\t " +  
-						100.0 * activitiesCountSingle[i]/totalActCountSingle + " per cent" + "\t" + NOGA[i] + "\n");
-			}
-			out.flush();
-			out.close();			
+			outShopping.close();	
+			outLeisure.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	private int transform(int i) {
-		if (i < 415) return i - 408;
-		else if (i < 421 && i > 413) return i - 409;
-		else return i -410;
 	}
 }
