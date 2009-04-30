d2dc3f771f6e4e4c8042d6a1954a1b3955e11692
jwjoubert <johan.joubert@up.ac.za>
2009-04-30 16:17:22 +0000

    Class to generate an initial demand plans file for commercial vehicles.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@6680 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/jjoubert/CommercialDemand/MyCommercialDemandGenerator.java
+++ matsim/src/playground/jjoubert/CommercialDemand/MyCommercialDemandGenerator.java
@@ -20,131 +20,132 @@
 
 package playground.jjoubert.CommercialDemand;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.geotools.data.FeatureSource;
+import org.geotools.feature.Feature;
+import org.matsim.api.basic.v01.BasicScenario;
+import org.matsim.api.basic.v01.BasicScenarioImpl;
+import org.matsim.api.basic.v01.Id;
+import org.matsim.api.basic.v01.TransportMode;
+import org.matsim.api.basic.v01.population.BasicActivity;
+import org.matsim.api.basic.v01.population.BasicLeg;
+import org.matsim.api.basic.v01.population.BasicPerson;
+import org.matsim.api.basic.v01.population.BasicPlan;
+import org.matsim.api.basic.v01.population.BasicPopulation;
+import org.matsim.api.basic.v01.population.BasicPopulationBuilder;
+import org.matsim.core.gbl.MatsimRandom;
+import org.matsim.core.population.PopulationWriter;
+import org.matsim.core.utils.gis.ShapeFileReader;
 import org.matsim.demandmodeling.primloc.CumulativeDistribution;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Point;
 
 public class MyCommercialDemandGenerator {
 	
-	private static final int populationSize = 10;
+	private static final int populationSize = 1000;
 	private static final String ROOT = "/Users/johanwjoubert/MATSim/workspace/MATSimData/CommercialDemand/";
 
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args){
-		//TODO Read major locations
 		
-		//TODO Read minor locations
+		// Read major locations
+		ArrayList<Point> majorPoints = readLocations(ROOT + "InputData/CommercialMajor10000.shp");
+		/**
+		 * TODO Currently I create points in ArcGIS based on the Kernel Density Estimation (KDE), 10000 in this
+		 * case. To pick a location, I simply draw a random point from the sample. In future, it might be better
+		 * to read the KDE raster into MATSim, and sample directly from it.
+		 */
+		
+		// Read minor locations
+		ArrayList<Point> minorPoints = readLocations(ROOT + "InputData/CommercialMinor10000.shp");
+
+		// Initiate the population builder
+		BasicScenario sc = new BasicScenarioImpl();		
+		BasicPopulation population = sc.getPopulation();
+		BasicPopulationBuilder pb = population.getPopulationBuilder();
+
 		
 		// Read cumulative distribution function (CDF) for number of activities per chain
 		CumulativeDistribution numberOfActivitiesCDF = CumulativeDistribution.readDistributionFromFile(ROOT + "InputData/numberOfActivitiesCDF.txt");
-		// read cumulative distribution function (CDF) for the activity duration
+
+		// Read cumulative distribution function (CDF) for the activity duration
+		@SuppressWarnings("unused")
 		CumulativeDistribution activityDurationCDF = CumulativeDistribution.readDistributionFromFile(ROOT + "InputData/activityDurationCDF.txt");
 		
-		
-		
 		for(int i = 0; i < populationSize; i++ ){
-			//TODO Sample major location
+			// Create a truck agent
+			Id id = sc.createId(Long.toString(i));
+			BasicPerson truck = pb.createPerson(id);
+			population.getPersons().put(id, truck);
 			
+			BasicPlan plan = pb.createPlan(truck);
+			truck.getPlans().add(plan);
+						
+			// Sample major location and add as first activity
+			Point major = majorPoints.get(MatsimRandom.getRandom().nextInt(majorPoints.size()));
+			BasicActivity majorActivityStart = pb.createActivityFromCoord("major", 
+					sc.createCoord(major.getCoordinate().x, major.getCoordinate().y));
+			majorActivityStart.setEndTime(28800); // 08:00:00 (HH:MM:SS)
+			plan.getPlanElements().add(majorActivityStart);
 			
+			BasicLeg leg = pb.createLeg(TransportMode.car);
+			plan.addLeg(leg);
+			
+			// Draw a sample to determine the number of 'minor' activities for this agent's chain
 			int activitiesPerChain = (int) numberOfActivitiesCDF.sampleFromCDF();
 			
+			// Establish time 'gaps' based on the number of activities
+			double gap = (79199 - 28800) / (activitiesPerChain + 1); // (21:59:59 - 08:00:00) / (n+1)
+			double endTime = 28800 + gap;
+			
 			for(int activity = 0; activity < activitiesPerChain; activity++){
-				//TODO Sample minor point
+				// Sample minor point
+				Point minor = minorPoints.get(MatsimRandom.getRandom().nextInt(minorPoints.size()));
+				BasicActivity minorActivity = pb.createActivityFromCoord("minor", 
+						sc.createCoord(minor.getCoordinate().x, minor.getCoordinate().y));
+				minorActivity.setEndTime(endTime);
+//				double activityDuration = activityDurationCDF.sampleFromCDF();
 				
-				@SuppressWarnings("unused")
-				double activityDuration = activityDurationCDF.sampleFromCDF();
+				plan.getPlanElements().add(minorActivity);	
+				endTime += gap;	
+				
+				plan.addLeg(leg);				
 			}
+			BasicActivity majorActivityEnd = pb.createActivityFromCoord("major", majorActivityStart.getCoord());
+			majorActivityEnd.setStartTime(79200);
+			plan.getPlanElements().add(majorActivityEnd);
+			
 		}
 		
-		//TODO Write plans.xml file
+		// Write plans.xml file
+		PopulationWriter pw = new PopulationWriter(population, ROOT + "plans.xml");
+		pw.write();
 	}
 
 
-
-
-
-
-//	private static int sampleNumberOfActivities() {
-//		int number = 0;
-//		float random = RAND.nextFloat();
-//		
-//		int index = 0;
-//		while(number == 0 & index < numberOfActivitiesCDF.size() ){
-//			if( numberOfActivitiesCDF.get(index) > random){
-//				number = numberOfActivities.get(index);
-//			} else{
-//				index++;
-//			}
-//		}
-//		if(number == 0){
-//			System.err.println("Number of activities found to be zero!");
-//			System.exit(1);
-//		}
-//		return number;
-//	}
-
-	
-
-//	private static void readNumberOfActivitiesCDF() {
-//		try {
-//			Scanner inputCDF = new Scanner(new BufferedReader(new FileReader(new File(ROOT + "InputData/numberOfActivitiesCDF.txt"))));
-//			@SuppressWarnings("unused")
-//			String header = inputCDF.nextLine();
-//			
-//			while(inputCDF.hasNextLine()){
-//				String [] line = inputCDF.nextLine().split(",");
-//				if(line.length == 2){
-//					int number = Integer.parseInt(line[0]);
-//					numberOfActivities.add(number);
-//					float prob = Float.parseFloat(line[1]);
-//					numberOfActivitiesCDF.add(prob);
-//				} else{
-//					System.err.println("In reading the cumulative distribution function for the number of activities per chain:");
-//					System.err.println("   Expected two values: numberOfActivities value and an associated probability!");
-//					System.exit(1);
-//				}
-//			}
-//			
-//			// Check that the distribution function is truly cumulative
-//			for(int i = 1; i < numberOfActivitiesCDF.size(); i++){
-//				if(numberOfActivitiesCDF.get(i) < numberOfActivitiesCDF.get(i-1)){
-//					System.err.println("The ArrayList numberOfActivitiesCDF is not cumulative!");
-//				}
-//			}
-//			
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		}
-//	}
-	
-//	private static void readActivityDurationCDF() {
-//		try {
-//			Scanner inputCDF = new Scanner(new BufferedReader(new FileReader(new File(ROOT + "InputData/activityDurationCDF.txt"))));
-//			@SuppressWarnings("unused")
-//			String header = inputCDF.nextLine();
-//			
-//			while(inputCDF.hasNextLine()){
-//				String [] line = inputCDF.nextLine().split(",");
-//				if(line.length == 2){
-//					int number = Integer.parseInt(line[0]);
-//					activityDuration.add(number);
-//					float prob = Float.parseFloat(line[1]);
-//					activityDurationCDF.add(prob);
-//				} else{
-//					System.err.println("In reading the cumulative distribution function for the activity duration:");
-//					System.err.println("   Expected two values: activityDuration value and an associated probability!");
-//					System.exit(1);
-//				}
-//			}
-//			
-//			// Check that the distribution function is truly cumulative
-//			for(int i = 1; i < activityDurationCDF.size(); i++){
-//				if(activityDurationCDF.get(i) < activityDurationCDF.get(i-1)){
-//					System.err.println("The ArrayList activityDurationCDF is not cumulative!");
-//				}
-//			}
-//			
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		}
-//	}
-
+	private static ArrayList<Point> readLocations( String source ) {
+		
+		FeatureSource fs = null;
+		ArrayList<Point> points = new ArrayList<Point>();
+		Point p = null;
+		try {	
+			fs = ShapeFileReader.readDataFile( source );
+			for(Object o: fs.getFeatures() ){
+				Geometry geo = ((Feature)o).getDefaultGeometry();
+				if(geo instanceof Point){
+					p = (Point)geo;
+					points.add(p);
+				} else{
+					System.err.println("The shapefile is not made up of only points!");
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return points;
+	}
 
 }
