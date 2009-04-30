80c3d77dcd1f950441246e15b8f742e77de52cef
andreas77 <andreashorni@hotmail.com>
2009-04-30 21:23:13 +0000

    merging plan analysis
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@6691 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/anhorni/locationchoice/preprocess/analyzePlansAndFacs/AnalyzePlans.java
+++ matsim/src/playground/anhorni/locationchoice/preprocess/analyzePlansAndFacs/AnalyzePlans.java
@@ -32,35 +32,57 @@ import org.matsim.core.api.population.Activity;
 import org.matsim.core.api.population.Person;
 import org.matsim.core.api.population.Plan;
 import org.matsim.core.api.population.Population;
+import org.matsim.core.facilities.FacilitiesReaderMatsimV1;
 import org.matsim.core.gbl.Gbl;
+import org.matsim.core.network.MatsimNetworkReader;
 import org.matsim.core.network.NetworkLayer;
 import org.matsim.core.population.MatsimPopulationReader;
 import org.matsim.core.population.PopulationImpl;
 import org.matsim.core.population.PopulationReader;
 import org.matsim.core.utils.io.IOUtils;
+import org.matsim.world.World;
 
 public class AnalyzePlans {
 
 	private Population plans = new PopulationImpl();
-	String plansfilePath;
+	private Facilities facilities;
+	private NetworkLayer network;
 	
 	private final static Logger log = Logger.getLogger(AnalyzePlans.class);
 	
-	public void run(final String plansfilePath, Facilities facilities, NetworkLayer network) {;
-		this.readPlansFile(plansfilePath, network);
+	
+	public static void main(final String[] args) {
 
-		write("./output/plan_activities_summary.txt");
-		System.out.println("finished");
+		Gbl.startMeasurement();
+		final AnalyzePlans analyzer = new AnalyzePlans();
+		if (args.length < 1) {
+			log.info("Too few arguments!");
+			System.exit(0);
+		}
+		analyzer.init("input/networks/ivtch.xml", args[0], "input/facilities/facilities.xml.gz");
+		analyzer.write("./output/valid/plans/plan_activities_summary.txt", args[0]);
+		Gbl.printElapsedTime();
 	}
-
-	private void readPlansFile(final String plansfilePath, NetworkLayer network) {
+	
+	public void init(String networkfilePath, String plansfilePath, String facilitiesfilePath) {
+		
+		//World world = Gbl.createWorld();
+				
+		log.info("reading the facilities ...");
+		this.facilities =(Facilities)Gbl.getWorld().createLayer(Facilities.LAYER_TYPE, null);
+		new FacilitiesReaderMatsimV1(this.facilities).readFile(facilitiesfilePath);
+			
+		log.info("reading the network ...");
+		this.network = new NetworkLayer();
+		new MatsimNetworkReader(this.network).readFile(networkfilePath);
+		
 		log.info("  reading file " + plansfilePath);
-		this.plansfilePath = plansfilePath;
 		final PopulationReader plansReader = new MatsimPopulationReader(this.plans, network);
-		plansReader.readFile(plansfilePath);
+		plansReader.readFile(plansfilePath);				
 	}
+	
 
-	private void write(String outfile) {
+	private void write(String outfile, String infile) {
 
 		try {
 			final BufferedWriter out = IOUtils.getBufferedWriter(outfile);
@@ -101,7 +123,7 @@ public class AnalyzePlans {
 					totalDesiredLeisureDuration += (desiredLeisurePerPerson / numberOfLeisureActsPerPerson);
 				}				
 			}
-			out.write("Plans file: " + this.plansfilePath);
+			out.write("Plans file: " + infile);
 			out.write("Number of shopping activities: \t" + numberOfShoppingActs + "\n");
 			out.write("Total desired duration of shopping activities: \t" + 1/3600.0 * totalDesiredShoppingDuration + " [h] \n");
 			out.write("Avg. desired shopping duration: \t" + 1/3600.0 * (totalDesiredShoppingDuration / numberOfShoppingActs) + " [h] \n");
