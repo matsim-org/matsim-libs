9456a026d9a422033dcf636e750d7bf36d981b50
vsp_neumann <neumann@senozon.de>
2009-05-04 10:25:38 +0000

    - cleanup
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@6725 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/andreas/bln/GenerateBlnPlan2.java
+++ matsim/src/playground/andreas/bln/GenerateBlnPlan2.java
@@ -29,12 +29,12 @@ public class GenerateBlnPlan2 {
 
 	private static final Logger log = Logger.getLogger(TabReader.class);
 	private static final int spreadingTime = 900; // 15min
-	private static final boolean setAllLegsToCar = true;
-	
+	private static final boolean setAllLegsToCar = false;
+
 	// statistics
 	private int[] actTypes = new int[14];
 	private int[] modalSplit = new int[9];
-		
+
 	/**
 	 * @param args
 	 */
@@ -56,12 +56,11 @@ public class GenerateBlnPlan2 {
 			// If a person has only one trip, delete that trip 
 			tripMap = myBlnPlanGenerator.filterPersonsWithOneTrip(tripMap);
 
-
 			// Some trips have invalid coordinates. Most of them are situated outside the survey area and weren't localized
 			// But some weren't localized because of faulty survey data, e.x. misspelling, spaces in Name, or a different federal state
 			// Those could be reconstructed by defining a common entry point to the survey area
-			tripMap = myBlnPlanGenerator.filterTripsWithoutCoordButEntryInCoordMap(tripMap, "z:/population/input/zero_coordinates_trips.csv");
-//			tripMap = myBlnPlanGenerator.filterTripsWithoutCoord(tripMap);
+			//			tripMap = myBlnPlanGenerator.filterTripsWithoutCoordButEntryInCoordMap(tripMap, "z:/population/input/zero_coordinates_trips.csv");
+			tripMap = myBlnPlanGenerator.filterTripsWithoutCoord(tripMap);
 			// Again statistics
 			tripMap = myBlnPlanGenerator.filterPersonsWithOneTrip(tripMap);
 
@@ -72,7 +71,7 @@ public class GenerateBlnPlan2 {
 			personMap = myBlnPlanGenerator.removePersonsWithoutPlan(personMap, tripMap);
 
 			// Write output to file
-			myBlnPlanGenerator.writePopulationToFile(personMap.values(), "z:/population/output/plans_out.xml");
+			myBlnPlanGenerator.writePopulationToFile(personMap.values(), "z:/population/output/plans.xml.gz");
 
 			// Print additional statistics
 			myBlnPlanGenerator.printStatistic();
@@ -81,112 +80,111 @@ public class GenerateBlnPlan2 {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private HashMap<Id,PersonImpl> generatePersons(String personsFileName) throws IOException{
 
 		HashMap<Id, PersonImpl> personList = new HashMap<Id, PersonImpl>();
-		
-			log.info("Start reading file " + personsFileName);
-			ArrayList<String[]> personData = TabReader.readFile(personsFileName);
-			log.info("...finished reading " + personData.size() + " entries.");
+
+		log.info("setAllLegsToCar is set to " + setAllLegsToCar);
+
+		log.info("Start reading file " + personsFileName);
+		ArrayList<String[]> personData = TabReader.readFile(personsFileName);
+		log.info("...finished reading " + personData.size() + " entries.");
 
 //			log.info("Start generating persons...");
-			for (String[] data : personData) {
+		for (String[] data : personData) {
 
-				PersonImpl person = new PersonImpl(new IdImpl(data[0]));
-				personList.put(person.getId(), person);
+			PersonImpl person = new PersonImpl(new IdImpl(data[0]));
+			personList.put(person.getId(), person);
 
-				// approximation: yearOfSurvey - yearOfBirth 
-				person.setAge(98 - Integer.parseInt(data[2]));
+			// approximation: yearOfSurvey - yearOfBirth 
+			person.setAge(98 - Integer.parseInt(data[2]));
 
-				// 1 = no, 2 occasionally, 3 yes
-				// TODO [an] any string can be written to file, but PersonReader expects
-				// "a value from the list always never sometimes"
-				if (data[19].equalsIgnoreCase("1")){
-					person.setCarAvail("never");
-				} else {
-					person.setCarAvail("always");
-				}
-
-				// filter unemployed persons and data without entry
-				if (Integer.parseInt(data[12]) != 6 && Integer.parseInt(data[12]) != 0){
-					person.setEmployed("yes");
-				}
-
-
-				// person.setHousehold(hh)(new IdImpl(data[1]));
-
-				if(Integer.parseInt(data[18]) == 2){
-					person.setLicence("yes");
-				} else if(Integer.parseInt(data[18]) == 1){
-					person.setLicence("no");
-				} // else don't know
-
-				// TODO [an] same as setCarAvail. Any string can be written to file, but PersonReader expects
-				// "a value from the list "f m "."
-				if (Integer.parseInt(data[3]) == 2 ) {
-					person.setSex("f");
-				} else if (Integer.parseInt(data[3]) == 1){
-					person.setSex("m");
-				}				
-
+			// 1 = no, 2 occasionally, 3 yes
+			// TODO [an] any string can be written to file, but PersonReader expects
+			// "a value from the list always never sometimes"
+			if (data[19].equalsIgnoreCase("1")){
+				person.setCarAvail("never");
+			} else {
+				person.setCarAvail("always");
 			}
-			log.info("...finished generating " + personList.size() + " persons.");
-		
-		return personList;
 
+			// filter unemployed persons and data without entry
+			if (Integer.parseInt(data[12]) != 6 && Integer.parseInt(data[12]) != 0){
+				person.setEmployed("yes");
+			}
+
+			// person.setHousehold(hh)(new IdImpl(data[1]));
+
+			if(Integer.parseInt(data[18]) == 2){
+				person.setLicence("yes");
+			} else if(Integer.parseInt(data[18]) == 1){
+				person.setLicence("no");
+			} // else don't know
+
+			// TODO [an] same as setCarAvail. Any string can be written to file, but PersonReader expects
+			// "a value from the list "f m "."
+			if (Integer.parseInt(data[3]) == 2 ) {
+				person.setSex("f");
+			} else if (Integer.parseInt(data[3]) == 1){
+				person.setSex("m");
+			}				
+
+		}
+		log.info("...finished generating " + personList.size() + " persons.");
+
+		return personList;
 	}
-	
+
 	private HashMap<Id,ArrayList<String[]>> readTrips(String filename) throws IOException{
-		
+
 		HashMap<Id, ArrayList<String[]>> tripData = new HashMap<Id, ArrayList<String[]>>();
-		
-			log.info("Start reading file " + filename);
-			ArrayList<String[]> unsortedTripData = TabReader.readFile(filename);
-			log.info("...finished reading " + unsortedTripData.size() + " entries in trip file.");
-			
-			for (String[] tripDataString : unsortedTripData) {
-				IdImpl personId = new IdImpl(tripDataString[1]);
-				if(tripData.get(personId) != null){
-					tripData.get(personId).add(tripDataString);
-				} else {
-					ArrayList<String[]> newArrayList = new ArrayList<String[]>();
-					newArrayList.add(tripDataString);
-					tripData.put(personId, newArrayList);
-				}
-			}		
-				
+
+		log.info("Start reading file " + filename);
+		ArrayList<String[]> unsortedTripData = TabReader.readFile(filename);
+		log.info("...finished reading " + unsortedTripData.size() + " entries in trip file.");
+
+		for (String[] tripDataString : unsortedTripData) {
+			IdImpl personId = new IdImpl(tripDataString[1]);
+			if(tripData.get(personId) != null){
+				tripData.get(personId).add(tripDataString);
+			} else {
+				ArrayList<String[]> newArrayList = new ArrayList<String[]>();
+				newArrayList.add(tripDataString);
+				tripData.put(personId, newArrayList);
+			}
+		}		
+
 		return tripData;
-		
 	}
 
 	private void countPersonsPlans(HashMap<Id,PersonImpl> personList, HashMap<Id,ArrayList<String[]>> tripData){
-		
+
 		int numberOfPersonsWithoutTrip = 0;
 		int numberOfTripsWithoutPerson = 0;
-		
+
 		// check if every person has at least one trip
 		for (Id personId : personList.keySet()) {
 			if(tripData.get(personId) == null){
 				numberOfPersonsWithoutTrip++;
 			}
 		}
-		
+
 		// check if every trip has a person
 		for (Id personId : tripData.keySet()){
 			if(personList.get(personId) == null){
 				numberOfTripsWithoutPerson++;
 			}
 		}
-		
+
 		log.info(numberOfPersonsWithoutTrip + " persons have no trip");
 		log.info(numberOfTripsWithoutPerson + " trips have no correspnding person in raw data.");		
 	}
-	
+
 	private HashMap<Id,ArrayList<String[]>> filterPersonsWithOneTrip(HashMap<Id,ArrayList<String[]>> unfilteredTripData){
 		HashMap<Id,ArrayList<String[]>> filteredTripData = new HashMap<Id,ArrayList<String[]>>();
 		int numberOfPersonsWithOnlyOneTrip = 0;
-		
+
 		for (Id personsId : unfilteredTripData.keySet()) {
 			if(unfilteredTripData.get(personsId).size() > 1){
 				filteredTripData.put(personsId, unfilteredTripData.get(personsId));
@@ -194,20 +192,20 @@ public class GenerateBlnPlan2 {
 				numberOfPersonsWithOnlyOneTrip++;
 			}
 		}
-		
+
 		log.info("Filtered " + numberOfPersonsWithOnlyOneTrip + " persons with only one trip.");
 		return filteredTripData;
 	}
-	
+
 	private HashMap<Id, ArrayList<String[]>> filterTripsWithoutCoord(HashMap<Id, ArrayList<String[]>> unfilteredTripData) {
 		HashMap<Id, ArrayList<String[]>> filteredTripData = new HashMap<Id, ArrayList<String[]>>();
 		int numberOfTripsWithInvalidCoord = 0;
-		
+
 		for (Id personsId : unfilteredTripData.keySet()) {
 			ArrayList<String[]> filteredArrayList = new ArrayList<String[]>();
-			
+
 			for (String[] dataString : unfilteredTripData.get(personsId)) {
-				
+
 				// Ignore trips with invalid coordinates
 				if (Double.parseDouble(dataString[11]) > 1.0 && Double.parseDouble(dataString[12]) > 1.0){
 					filteredArrayList.add(dataString);
@@ -217,20 +215,20 @@ public class GenerateBlnPlan2 {
 			}
 			filteredTripData.put(personsId, filteredArrayList);			
 		}
-		
+
 		log.info("Filtered " + numberOfTripsWithInvalidCoord + " trips, cause coords weren't specified.");
 		return filteredTripData;
 	}
-	
+
 	private HashMap<Id, ArrayList<String[]>> filterTripsWithoutCoordButEntryInCoordMap(HashMap<Id, ArrayList<String[]>> unfilteredTripData, String filename) throws IOException {
 		HashMap<Id, ArrayList<String[]>> filteredTripData = new HashMap<Id, ArrayList<String[]>>();
 		int numberOfTripsWithInvalidCoord = 0;
 		int numberOfReplacedCoords = 0;
-		
+
 		log.info("Start reading file " + filename);
 		ArrayList<String[]> unsortedCoordMapData = TabReader.readFile(filename);
 		log.info("...finished reading " + unsortedCoordMapData.size() + " entries in " + filename + " file.");
-		
+
 		HashMap<Id, ArrayList<String[]>> sortedCoordMapData = new HashMap<Id, ArrayList<String[]>>();
 		for (String[] coordMapEntry : unsortedCoordMapData) {
 			IdImpl personId = new IdImpl(coordMapEntry[0]);
@@ -242,20 +240,20 @@ public class GenerateBlnPlan2 {
 				sortedCoordMapData.put(personId, newArrayList);
 			}
 		}		
-		
+
 		for (Id personsId : unfilteredTripData.keySet()) {
 			ArrayList<String[]> filteredArrayList = new ArrayList<String[]>();
-			
+
 			for (String[] dataString : unfilteredTripData.get(personsId)) {
-				
+
 				// Ignore trips with invalid coordinates
 				if (Double.parseDouble(dataString[11]) > 1.0 && Double.parseDouble(dataString[12]) > 1.0){
 					filteredArrayList.add(dataString);
 				} else {
-					
+
 					if (sortedCoordMapData.get(personsId) != null){
 						for (String[] coordMapEntry : sortedCoordMapData.get(personsId)) {
-							
+
 							// replace coord, if possible
 							if (dataString[2].equalsIgnoreCase(coordMapEntry[1])){
 								dataString[11] = coordMapEntry[3];
@@ -273,36 +271,36 @@ public class GenerateBlnPlan2 {
 			}
 			filteredTripData.put(personsId, filteredArrayList);			
 		}
-		
+
 		log.info("Filtered " + numberOfTripsWithInvalidCoord + " trips, cause coords weren't specified.");
 		return filteredTripData;
 	}
 
 	private void addPlansToPersons(HashMap<Id,PersonImpl> personList, HashMap<Id,ArrayList<String[]>> tripData) {
-		
+
 		log.info("Adding Plans to Person, spreading time is " + GenerateBlnPlan2.spreadingTime + "s");
 		for (Id personId : tripData.keySet()) {
-			
+
 			ArrayList<String[]> curTripList = tripData.get(personId);
 			PersonImpl curPerson = personList.get(personId);
 			Plan curPlan;
-			
+
 			if(curPerson.getSelectedPlan() == null){
 				curPlan = curPerson.createPlan(true);
 			} else {
 				curPlan = curPerson.getSelectedPlan();
 			}
-			
+
 			ActivityImpl lastAct = null;
-			
+
 			for (Iterator<String[]> iterator = curTripList.iterator(); iterator.hasNext();) {
 				String[] tripEntry = iterator.next();
-				
+
 				// Read Activity from survey
 				ActivityImpl newAct = new ActivityImpl(this.getActType(tripEntry), new CoordImpl(Double.parseDouble(tripEntry[11]), Double.parseDouble(tripEntry[12])));
-		
+
 				Time.setDefaultTimeFormat(Time.TIMEFORMAT_HHMM);
-				
+
 				// Since data is from survey, it is more likely to get x:00, x:15, x:30, x:45 as answer,
 				// and therefore arbitrary peaks in departure and arrival time histogram -> spread it
 				double startTime = Time.parseTime(tripEntry[5]);
@@ -313,7 +311,7 @@ public class GenerateBlnPlan2 {
 				} else {
 					newAct.setStartTime(startTime);
 				}
-				
+
 				if(lastAct != null){				
 					// Since data is from survey, it is more likely to get x:00, x:15, x:30, x:45 as answer,
 					// and therefore arbitrary peaks in departure and arrival time histogram -> spread it
@@ -325,27 +323,27 @@ public class GenerateBlnPlan2 {
 					} else {
 						lastAct.setEndTime(endTime);
 					}
-					
+
 					curPlan.addLeg(this.createLeg(tripEntry));
-					
+
 					if(!iterator.hasNext()){
 						newAct.setEndTime(86400.0);
 					}
 				}
-				
+
 				curPlan.addActivity(newAct);	
 				lastAct = newAct;				 
 			}
 		}				
 	}
-	
+
 	private HashMap<Id, PersonImpl> removePersonsWithoutPlan(HashMap<Id, PersonImpl> personMap, HashMap<Id, ArrayList<String[]>> tripMap) {
-		
+
 		HashMap<Id, PersonImpl> filteredPersonMap = new HashMap<Id, PersonImpl>();
 		for (Id personId : tripMap.keySet()) {
 			filteredPersonMap.put(personId, personMap.get(personId));			
 		}
-		
+
 		return filteredPersonMap;
 	}
 
@@ -360,11 +358,11 @@ public class GenerateBlnPlan2 {
 		writer.write();
 		log.info(numberOfPersonWithPlans + " persons were written to " + filename);
 	}
-		
+
 	private String getActType(String[] tripData){
 		String actType;
 		int actNr = Integer.parseInt(tripData[38]);
-		
+
 		switch (actNr) {
 		case 0:
 			if (tripData[10].equalsIgnoreCase("WAHR")){
@@ -438,21 +436,21 @@ public class GenerateBlnPlan2 {
 			break;
 		default:
 			log.error("ActType not defined");
-			actType = "not defined";
-			this.actTypes[13]++;
+		actType = "not defined";
+		this.actTypes[13]++;
 		}
 		return actType;
 	}
-	
+
 	private LegImpl createLeg(String[] tripData){
 
 		LegImpl leg = null;
-		
+
 		if(GenerateBlnPlan2.setAllLegsToCar == true){
-			
+
 			leg = new LegImpl(TransportMode.car);
 			this.modalSplit[3]++;
-			
+
 		} else {		
 
 			switch (Integer.parseInt(tripData[48])) {
@@ -463,6 +461,9 @@ public class GenerateBlnPlan2 {
 				break;
 			case 1:
 				// "Fuss"
+//				leg = new LegImpl(TransportMode.pt);
+//				this.modalSplit[4]++;
+//				break;
 				leg = new LegImpl(TransportMode.walk);
 				this.modalSplit[1]++;
 				break;
@@ -498,15 +499,15 @@ public class GenerateBlnPlan2 {
 				break;
 			default:
 				log.error("transport mode not defined");
-				leg = new LegImpl(TransportMode.walk);
-				this.modalSplit[8]++;
+			leg = new LegImpl(TransportMode.walk);
+			this.modalSplit[8]++;
 			}
 
 			// Read travel trip time from survey (min) 
 			if (!tripData[50].equalsIgnoreCase("")){
 				leg.setTravelTime(Double.parseDouble(tripData[50]) * 60);
 			} else {
-				//			log.info("empty String");
+//			log.info("empty String");
 			}
 
 		}
@@ -524,7 +525,7 @@ public class GenerateBlnPlan2 {
 				+ (this.actTypes[6] + this.actTypes[7]) + " leisure | "
 				+ this.actTypes[9] + " see a doctor | " + this.actTypes[10] + " holiday | "
 				+ (this.actTypes[11] + this.actTypes[12] + this.actTypes[13]) + " other, multiple or not defined");
-		
+
 		int totalNumLegs = 0;
 		for (int i = 0; i < this.modalSplit.length; i++) {
 			totalNumLegs = totalNumLegs + this.modalSplit[i];
@@ -537,4 +538,3 @@ public class GenerateBlnPlan2 {
 	}
 
 }
-
