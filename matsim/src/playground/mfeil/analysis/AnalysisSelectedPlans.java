430a8a239edc6b0d626a23931e51026c84454bcd
Matthias_Feil <Matthias_Feil@users.sf.net>
2009-05-19 15:07:11 +0000

    Extension in the analyzer class: now also checking correctness of plans features (all prim acts in, durations above minimum duration, etc.)
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@6969 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/mfeil/analysis/AnalysisSelectedPlans.java
+++ matsim/src/playground/mfeil/analysis/AnalysisSelectedPlans.java
@@ -26,6 +26,8 @@ import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
+
 import org.matsim.api.basic.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.ScenarioImpl;
@@ -51,6 +53,7 @@ public class AnalysisSelectedPlans {
 	private final String outputDir;
 	private ArrayList<List<PlanElement>> activityChains;
 	private ArrayList<ArrayList<Plan>> plans;
+	private final Map<String,Double> minimumTime;
 	
 
 
@@ -58,6 +61,11 @@ public class AnalysisSelectedPlans {
 		this.population = population;
 		this.outputDir = outputDir;
 		initAnalysis();
+		this.minimumTime = new TreeMap<String, Double>();
+		this.minimumTime.put("home", 7200.0);
+		this.minimumTime.put("work", 3600.0);
+		this.minimumTime.put("shopping", 1800.0);
+		this.minimumTime.put("leisure", 3600.0);
 	}
 	
 	private void initAnalysis(){
@@ -79,6 +87,36 @@ public class AnalysisSelectedPlans {
 				this.activityChains.add(person.getSelectedPlan().getPlanElements());
 				this.plans.add(new ArrayList<Plan>());
 				this.plans.get(this.plans.size()-1).add(person.getSelectedPlan());
+			}
+		}
+	}
+	
+	private void checkConsistency(){
+		for (int i=0;i<this.plans.size();i++){
+			for (int j=0;j<this.plans.get(i).size();j++){
+				for (int k=0;k<this.plans.get(i).get(j).getPlanElements().size()-2;k+=2){
+					if (k==0){
+						if ((((Activity)(this.plans.get(i).get(j).getPlanElements().get(k))).getDuration()+((Activity)(this.plans.get(i).get(j).getPlanElements().get(this.plans.get(i).get(j).getPlanElements().size()-1))).getDuration())<=this.minimumTime.get(((Activity)(this.plans.get(i).get(j).getPlanElements().get(k))).getType())-1) {
+							System.out.println("Duration error in plan of person "+this.plans.get(i).get(j).getPerson().getId()+" at act position "+(k/2)+" with duration "+(((Activity)(this.plans.get(i).get(j).getPlanElements().get(k))).getDuration()+((Activity)(this.plans.get(i).get(j).getPlanElements().get(this.plans.get(i).get(j).getPlanElements().size()-1))).getDuration()));
+							break;
+						}
+					}
+					else if (((Activity)(this.plans.get(i).get(j).getPlanElements().get(k))).getDuration()<=this.minimumTime.get(((Activity)(this.plans.get(i).get(j).getPlanElements().get(k))).getType())-1) {
+						System.out.println("Duration error in plan of person "+this.plans.get(i).get(j).getPerson().getId()+" at act position "+(k/2));
+						break;
+					}
+				}
+				for (int k=0;k<this.plans.get(i).get(j).getPerson().getKnowledge().getActivities(true).size();k++){
+					boolean notIn = true;
+					for (int l=0;l<this.plans.get(i).get(j).getPlanElements().size()-2;l+=2){
+						if (((Activity)(this.plans.get(i).get(j).getPlanElements().get(l))).getType().equalsIgnoreCase(this.plans.get(i).get(j).getPerson().getKnowledge().getActivities(true).get(k).getType()) &&
+								((Activity)(this.plans.get(i).get(j).getPlanElements().get(l))).getFacilityId().toString().equalsIgnoreCase(this.plans.get(i).get(j).getPerson().getKnowledge().getActivities(true).get(k).getFacility().getId().toString())){
+							notIn = false;
+							break;
+						}
+					}
+					if (notIn) System.out.println("Prim act error in plan of person "+this.plans.get(i).get(j).getPerson().getId()+" for prim act "+this.plans.get(i).get(j).getPerson().getKnowledge().getActivities(true).get(k));
+				}
 			}
 		}
 	}
@@ -184,7 +222,7 @@ public class AnalysisSelectedPlans {
 		// FIXME hard-coded file names; does this class really need a main-method?
 //		final String populationFilename = "./examples/equil/plans100.xml";
 //		final String networkFilename = "./examples/equil/network.xml";
-		final String populationFilename = "./output/Test1/output_plans.xml.gz";
+		final String populationFilename = "./output/Test1/output_plans.xml";
 //		final String populationFilename = "./output/Test1/ITERS/it.0/0.plans.xml.gz";
 		final String networkFilename = "./test/scenarios/chessboard/network.xml";
 		final String facilitiesFilename = "./test/scenarios/chessboard/facilities.xml";
@@ -198,6 +236,7 @@ public class AnalysisSelectedPlans {
 
 		AnalysisSelectedPlans sp = new AnalysisSelectedPlans(scenario.getPopulation(), outputDir);
 		sp.analyze();
+		sp.checkConsistency();
 
 	}
 
