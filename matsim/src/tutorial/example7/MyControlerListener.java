26a65afb220cb9f79e6fcaf7c3630e056d03def1
vsp_nagel <2008@kainagel.org>
2009-05-27 19:22:12 +0000

    Separated the tutorials into example1 to example7 since sometimes multiple java files belong to one example.  Hope that's ok, otherwise revert.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@7137 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/tutorial/example7/MyControlerListener.java
@@ -0,0 +1,83 @@
+package tutorial.example7ControlerListener;
+
+import java.io.*;
+import java.util.*;
+
+import org.matsim.core.controler.events.*;
+import org.matsim.core.controler.listener.*;
+import org.matsim.core.utils.charts.XYLineChart;
+import org.matsim.core.utils.io.IOUtils;
+
+import tutorial.example6EventsHandling.MyEventHandler2;
+
+
+/**
+ * This class collects the average travel time per agent measured by the
+ * MyEventHandler instance after each iteration. When the Controler is shut down
+ * the travel times are written to a file, i.e. a JFreeChart plot.
+ * @author dgrether
+ *
+ */
+public class MyControlerListener implements StartupListener, IterationEndsListener, ShutdownListener {
+
+	private MyEventHandler2 eventHandler; // this refers to example6 but I think this is ok. kai, may09
+
+	private Map<Integer, Double> timePerIterationMap = new HashMap<Integer, Double>();
+
+
+	public void notifyStartup(StartupEvent event) {
+		// after the controler is started create and add the event handler for events of the mobility simulation
+		this.eventHandler = new MyEventHandler2(event.getControler().getPopulation().getPersons().size());
+		event.getControler().getEvents().addHandler(this.eventHandler);
+	}
+
+
+	public void notifyIterationEnds(IterationEndsEvent event) {
+		System.out.println("Average travel time in iteration " + event.getIteration() + " is: "
+				+ this.eventHandler.getAverageTravelTime());
+		this.timePerIterationMap.put(event.getIteration(), this.eventHandler.getAverageTravelTime());
+	}
+
+	public void notifyShutdown(ShutdownEvent event) {
+		//create the chart with the appropriate helper class of org.matsim.utils.charts
+		XYLineChart chart = new XYLineChart("Average travel times per iteration",
+				"Iterations", "ttimes");
+		chart.addMatsimLogo();
+		//create the arrays needed for chart creation
+		double[] iters = new double[this.timePerIterationMap.size()];
+		double[] times = new double[this.timePerIterationMap.size()];
+		int decrement = 1;
+		if (this.timePerIterationMap.containsKey(Integer.valueOf(0))) {
+			decrement = 0;
+		}
+		//unfortunately we have to do this as...
+		for (Integer k : this.timePerIterationMap.keySet()) {
+			iters[k - decrement] = k;
+			times[k - decrement] = this.timePerIterationMap.get(k);
+		}
+		//write to file doing this not in the loop above is cause we
+		//would like to have a sorted output.
+		StringBuffer buffer = new StringBuffer();
+		for (int i = 0; i < iters.length; i++) {
+			buffer.append(iters[i]);
+			buffer.append("\t");
+			buffer.append(times[i]);
+			buffer.append("\n");
+		}
+		//write the chart
+		chart.addSeries("tt", iters, times);
+		chart.saveAsPng("./output/travelTimes.png", 800, 600);
+		try {
+			BufferedWriter writer = IOUtils.getBufferedWriter("./output/travelTimes.txt");
+			writer.write(buffer.toString());
+			writer.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
+}
---
+++ matsim/src/tutorial/example7/MyMainClass.java
@@ -0,0 +1,30 @@
+package tutorial.example7ControlerListener;
+
+import org.matsim.core.controler.Controler;
+import org.matsim.vis.netvis.NetVis;
+
+
+
+
+public class MyMainClass {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//set a default config for convenience...
+		String [] config = {"./examples/tutorial/multipleIterations.xml"};
+		//Create an instance of the controler and
+		Controler controler = new Controler(config);
+		//so we don't have to delete the output directory
+		controler.setOverwriteFiles(true);
+		//add an instance of this class as ControlerListener
+		controler.addControlerListener(new MyControlerListener());
+		//call run() to start the simulation
+		controler.run();
+		//open snapshot of the 10th iteration
+		String[] visargs = {"output/ITERS/it.10/Snapshot"};
+		NetVis.main(visargs);
+	}
+
+
+}
