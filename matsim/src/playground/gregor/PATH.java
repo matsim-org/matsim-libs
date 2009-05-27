b7a1aa7a9c1e6de8203cb560a688bcaff7cea084
laemmel <gregor.laemmel@gmail.com>
2009-05-27 09:15:38 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@7120 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/gregor/PATH.java
+++ matsim/src/playground/gregor/PATH.java
@@ -2,6 +2,7 @@ package playground.gregor;
 
 public class PATH {
 	
-	public final static String OUTPUTS = "../../outputs/"; 
+	public final static String OUTPUTS = "../../outputs/";
+	public static final String CVS_GIS = "/home/laemmel/workspace/vsp-cvs/studies/padang/gis/"; 
 
 }
