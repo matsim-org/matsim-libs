6d8e6d8b1f892edfbe9869fb6ca69dfc0f39d194
mrieser <rieser@gmail.com>
2009-06-10 15:12:38 +0000

    additional test case for router
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@7367 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/test/src/playground/mmoyo/RouterTest.java
+++ matsim/test/src/playground/mmoyo/RouterTest.java
@@ -20,13 +20,32 @@
 
 package playground.mmoyo;
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.matsim.api.basic.v01.Coord;
+import org.matsim.api.basic.v01.TransportMode;
 import org.matsim.core.api.network.Link;
+import org.matsim.core.api.population.Activity;
+import org.matsim.core.api.population.Leg;
+import org.matsim.core.api.population.Person;
+import org.matsim.core.api.population.Population;
+import org.matsim.core.basic.v01.IdImpl;
+import org.matsim.core.network.MatsimNetworkReader;
+import org.matsim.core.network.NetworkLayer;
+import org.matsim.core.population.MatsimPopulationReader;
+import org.matsim.core.population.PopulationImpl;
 import org.matsim.core.router.util.LeastCostPathCalculator.Path;
 import org.matsim.core.utils.geometry.CoordImpl;
 import org.matsim.testcases.MatsimTestCase;
+import org.xml.sax.SAXException;
 
+import playground.marcel.pt.transitSchedule.TransitSchedule;
+import playground.marcel.pt.transitSchedule.TransitScheduleReaderV1;
 import playground.mmoyo.PTCase2.PTOb;
+import playground.mmoyo.TransitSimulation.TransitRouteFinder;
 
 public class RouterTest extends MatsimTestCase {
 
@@ -54,5 +73,41 @@ public class RouterTest extends MatsimTestCase {
 		assertEquals( 31, path2.links.size() ) ;
 		assertEquals( "1311" , path2.links.get(10).getId().toString() ) ;
 		assertEquals( "250" , path2.links.get(20).getId().toString() ) ;
+	}
+	
+	
+	public void testWithVerySimpleTransitSchedule() throws SAXException, ParserConfigurationException, IOException {
+		/* for integration into MATSim, the following must work */
+		
+		// setup very simple scenario
+		NetworkLayer network = new NetworkLayer();
+		new MatsimNetworkReader(network).readFile("test/input/playground/marcel/pt/transitSchedule/network.xml");
+		TransitSchedule schedule = new TransitSchedule();
+		new TransitScheduleReaderV1(schedule, network).readFile("test/input/playground/marcel/pt/transitSchedule/transitSchedule.xml");
+		Population population = new PopulationImpl();
+		new MatsimPopulationReader(population, network).readFile("test/input/playground/marcel/pt/plans.xml");
+		Person person = population.getPersons().get(new IdImpl(1));
+		Activity fromAct = (Activity) person.getPlans().get(0).getPlanElements().get(0);
+		Activity toAct = (Activity) person.getPlans().get(0).getPlanElements().get(2);
+		
+		// make sure our setup is as expected
+		assertNotNull(person);
+		assertNotNull(fromAct);
+		assertNotNull(toAct);
+		
+		// in the plans file, there are no coordinates for the activities... fix that.
+		fromAct.setCoord(new CoordImpl(400.0, 399.0));
+		toAct.setCoord(new CoordImpl(4700.0, 700.0));
+		
+		// now run the essential thing:
+		TransitRouteFinder routeFinder = new TransitRouteFinder(schedule);
+		List<Leg> legs = routeFinder.calculateRoute(fromAct, toAct, person);
+		
+		/* I would expect the following, although I could be wrong.
+		 * It must be checked once the code runs up to this line.     */
+		assertEquals(3, legs.size());
+		assertEquals(TransportMode.walk, legs.get(0).getMode());
+		assertEquals(TransportMode.pt, legs.get(1).getMode());
+		assertEquals(TransportMode.walk, legs.get(2).getMode());
 	}
 }
