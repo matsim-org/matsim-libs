03e2765a524600ff1d1ae1626961a07d698419f4
andreas77 <andreashorni@hotmail.com>
2009-06-11 11:05:37 +0000

    use this for modfication of TravelDistanceStats
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@7383 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/org/matsim/analysis/PlanLegsTravelDistanceCalculator.java
@@ -0,0 +1,59 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * PlanLegsTravelDistanceCalculator.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2009 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+
+package org.matsim.analysis;
+
+import org.matsim.core.api.population.Leg;
+import org.matsim.core.api.population.Plan;
+import org.matsim.core.api.population.PlanElement;
+
+public class PlanLegsTravelDistanceCalculator {
+	
+	private double sumLegsTravelDistance = 0.0;	
+	// later: distinguish between modes
+	private double nbrOfLegs;
+	
+	public void handle(final Plan plan){
+		for (PlanElement pe : plan.getPlanElements()) {
+			if (pe instanceof Leg) {
+				final Leg leg = (Leg) pe;
+				this.sumLegsTravelDistance += leg.getRoute().getDistance();
+				this.nbrOfLegs++;
+			}
+		}
+	}
+
+	public double getSumLegsTravelDistance() {
+		return sumLegsTravelDistance;
+	}
+
+	public void setSumLegsTravelDistance(double sumLegsTravelDistance) {
+		this.sumLegsTravelDistance = sumLegsTravelDistance;
+	}
+
+	public double getNbrOfLegs() {
+		return nbrOfLegs;
+	}
+
+	public void setNbrOfLegs(double nbrOfLegs) {
+		this.nbrOfLegs = nbrOfLegs;
+	}
+}
