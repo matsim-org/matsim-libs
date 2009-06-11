c4c6938e3df86c6536de8c3fc4760063f9dc0979
mrieser <rieser@gmail.com>
2009-06-11 17:32:59 +0000

    commented non-compiling line out
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@7394 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/anhorni/locationchoice/cs/helper/ReaderTripHandler.java
+++ matsim/src/playground/anhorni/locationchoice/cs/helper/ReaderTripHandler.java
@@ -50,7 +50,7 @@ public class ReaderTripHandler {
 		afterShoppingAct.setLink(network.getNearestLink(afterShoppingCoord));
 		
 		if (!(mzTrip.getEndTime() > 0.0)) {
-			log.error("No end time found for person : " + mzTrip.getPersonId());
+			log.error("No end time found for person : " /*+ mzTrip.getPersonId()*/);
 		}
 		double startTimeAfterShoppingAct = mzTrip.getEndTime(); 			
 		afterShoppingAct.setStartTime(startTimeAfterShoppingAct);
