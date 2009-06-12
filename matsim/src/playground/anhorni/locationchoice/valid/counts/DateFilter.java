b2b43bec301f960db59125634e33645ff3a8dc12
andreas77 <andreashorni@hotmail.com>
2009-06-12 08:13:54 +0000

    MOFR
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@7395 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/anhorni/locationchoice/valid/counts/DateFilter.java
+++ matsim/src/playground/anhorni/locationchoice/valid/counts/DateFilter.java
@@ -21,31 +21,35 @@ public class DateFilter {
 			// filter DI-DO
 			// filter summer holidays
 			// filter public holidays			
-			if (this.isDIDO(rawCount) && !this.inSummerHolidays(rawCount) && !this.isPublicHoliday(rawCount)) {
+//			if (this.isDIDO(rawCount) && !this.inSummerHolidays(rawCount) && !this.isPublicHoliday(rawCount)) {
+//				filteredRawCounts.add(rawCount);	
+//			}
+			
+			if (this.isMOFRI(rawCount) && !this.inSummerHolidays(rawCount) && !this.isPublicHoliday(rawCount)) {
 				filteredRawCounts.add(rawCount);	
 			}
+			
 		}
 		return filteredRawCounts;
 	}
 	
 	// SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
 	// 1		2		3		4		   5		6		7
-	private boolean isDIDO(RawCount count) {
-		
-		/*
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-		try {
-			Date date = (Date) format.parse(count.getYear() + "–" + count.getMonth() + "-" + count.getDay());
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		*/
-				
+	private boolean isDIDO(RawCount count) {	
 	    calendar.set(count.getYear(), count.getMonth() -1, count.getDay());
 	    int weekday = calendar.get(Calendar.DAY_OF_WEEK);
 	    calendar.clear();
 	    	    
 	    if (weekday >= 3 && weekday <= 5) return true;
+	    else return false; 
+	}
+	
+	private boolean isMOFRI(RawCount count) {
+	 	calendar.set(count.getYear(), count.getMonth() -1, count.getDay());
+	    int weekday = calendar.get(Calendar.DAY_OF_WEEK);
+	    calendar.clear();
+	    	    
+	    if (weekday >= 2 && weekday <= 6) return true;
 	    else return false; 
 	}
 	
@@ -56,29 +60,65 @@ public class DateFilter {
 	
 	private boolean isPublicHoliday(RawCount count) {
 		
-		// 2004: DI-DO
-		if (count.getYear() == 2004 && count.getMonth()== 1 && count.getDay()== 1) {
-			return true; // Neujahrstag
+		if (count.getYear() == 2004 || count.getYear() == 2005 || count.getYear() == 2006) {
+			if (count.getMonth()== 4 && count.getDay()== 9) {
+				return true; // Karfreitag
+			}
+			if (count.getMonth()== 4 && count.getDay()== 12) {
+				return true; // Ostermontag
+			}
+			if (count.getMonth()== 5 && count.getDay()== 31) {
+				return true; // Pfingstmontag
+			}
+		}		
+		// 2004:MO-FR
+		if (count.getYear() == 2004) {
+			if (count.getMonth()== 1 && count.getDay()== 1) {
+				return true; // Neujahrstag
+			}
+			if (count.getMonth()== 1 && count.getDay()== 2) {
+				return true; // Berchtoldstag
+			}
+			if (count.getMonth()== 5 && count.getDay()== 20) {
+				return true; // Auffahrt
+			}
+			if (count.getMonth()== 12 && count.getDay()== 31) {
+				return true; // Silvester
+			}
+		}		
+		// 2005: 
+		if (count.getYear() == 2005) {
+			if (count.getMonth()== 5 && count.getDay()== 5) {
+				return true; // Auffahrt
+			}
+			if (count.getMonth()== 8 && count.getDay()== 1) {
+				return true; // 1. August
+			}
+			if (count.getMonth()== 12 && count.getDay()== 26) {
+				return true; // Stephanstag
+			}
+		}		
+		// 2006:
+		if (count.getYear() == 2006) {
+			if (count.getMonth()== 1 && count.getDay()== 2) {
+				return true; // Berchtoldstag
+			}
+			if (count.getMonth()== 5 && count.getDay()== 1) {
+				return true; // 1. Mai
+			}
+			if (count.getMonth()== 5 && count.getDay()== 25) {
+				return true; // Auffahrt
+			}
+			if (count.getMonth()== 8 && count.getDay()== 1) {
+				return true; // 1. August
+			}
+			if (count.getMonth()== 12 && count.getDay()== 25) {
+				return true; // Weihnachten
+			}
+			if (count.getMonth()== 12 && count.getDay()== 26) {
+				return true; // Stephanstag
+			}
 		}
-		if (count.getYear() == 2004 && count.getMonth()== 5 && count.getDay()== 20) {
-			return true; // Auffahrt
-		}
-		
-		// 2005: DI-DO!
-		if (count.getYear() == 2005 && count.getMonth()== 5 && count.getDay()== 5) {
-			return true; // Auffahrt
-		}
-		
-		// 2006: DI-DO!
-		if (count.getYear() == 2006 && count.getMonth()== 5 && count.getDay()== 25) {
-			return true; // Auffahrt
-		}
-		if (count.getYear() == 2006 && count.getMonth()== 8 && count.getDay()== 1) {
-			return true; // 1. August
-		}
-		if (count.getYear() == 2006 && count.getMonth()== 12 && count.getDay()== 26) {
-			return true; // Stephanstag
-		}
-		else return false;
+		return false;
 	}
 }
