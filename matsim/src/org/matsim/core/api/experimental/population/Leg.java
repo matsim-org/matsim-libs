7f1fb8ee0ba4e0caddee87c533e6037874ed0d3d
vsp_nagel <2008@kainagel.org>
2009-06-30 16:44:21 +0000

    the moved "Leg" interface did not make it ...
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@7608 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/org/matsim/core/api/experimental/population/Leg.java
@@ -0,0 +1,32 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * Leg.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007, 2008 by the members listed in the COPYING,  *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.core.api.experimental.population;
+
+import org.matsim.api.basic.v01.population.BasicLeg;
+import org.matsim.core.api.population.PlanElement;
+import org.matsim.core.api.population.Route;
+
+public interface Leg extends BasicLeg, PlanElement {
+
+	// in order to get correct type(s):
+	public Route getRoute();
+
+}
