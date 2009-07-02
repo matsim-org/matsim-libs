14a339d5a3f34117e5dca160dd57c1f39c336fef
rashid_waraich <rashid_waraich@users.sf.net>
2009-07-02 15:40:06 +0000

    adding first template
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@7714 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/wrashid/template/ProcessPlansFile.java
@@ -0,0 +1,51 @@
+package playground.wrashid.template;
+
+import org.matsim.api.basic.v01.BasicScenario;
+import org.matsim.api.basic.v01.BasicScenarioImpl;
+import org.matsim.core.api.experimental.population.Population;
+import org.matsim.core.config.Config;
+import org.matsim.core.gbl.Gbl;
+import org.matsim.core.network.MatsimNetworkReader;
+import org.matsim.core.network.NetworkLayer;
+import org.matsim.core.population.MatsimPopulationReader;
+import org.matsim.core.population.PersonImpl;
+import org.matsim.core.population.PopulationImpl;
+import org.matsim.core.population.PopulationReader;
+import org.matsim.core.population.PopulationReaderMatsimV4;
+import org.matsim.core.population.PopulationWriter;
+
+import playground.andreas.bln.DuplicatePlans;
+import playground.andreas.bln.NewPopulation;
+
+public class ProcessPlansFile extends NewPopulation {
+	public static void main(String[] args) {
+
+		BasicScenarioImpl sc = new BasicScenarioImpl();
+		Gbl.setConfig(sc.getConfig());
+
+		String inputPlansFile = "./test/scenarios/berlin/plans_hwh_1pct.xml.gz";
+		String outputPlansFile = "./test.xml.gz";
+		String networkFile = "./test/scenarios/berlin/network.xml.gz";
+
+		Population inPop = new PopulationImpl();
+
+		NetworkLayer net = new NetworkLayer();
+		new MatsimNetworkReader(net).readFile(networkFile);
+
+		PopulationReader popReader = new MatsimPopulationReader(inPop, net);
+		popReader.readFile(inputPlansFile);
+
+		ProcessPlansFile dp = new ProcessPlansFile(inPop, outputPlansFile);
+		dp.run(inPop);
+		dp.writeEndPlans();
+	}
+
+	public ProcessPlansFile(Population plans, String filename) {
+		super(plans, filename);
+	}
+
+	public void run(PersonImpl person) {
+		this.popWriter.writePerson(person);
+
+	}
+}
