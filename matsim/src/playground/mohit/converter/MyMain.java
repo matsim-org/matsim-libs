bd8d7d68ce810b2eff2abec167d3e6b66352900c
mohit9920 <mohit9920@users.sf.net>
2009-07-02 13:38:53 +0000

    improved version of converter. won't compile yet, will fix that later (marcel)
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@7707 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/mohit/converter/MyMain.java
@@ -0,0 +1,89 @@
+package playground.mohit.converter;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.matsim.api.basic.v01.Id;
+
+//import playground.marcel.pt.transitSchedule.TransitRoute;
+//import playground.marcel.visum.VisumNetwork;
+//import playground.marcel.visum.VisumNetworkReader;
+//import playground.marcel.visum.VisumNetwork.Stop;
+import playground.marcel.pt.transitSchedule.TransitSchedule;
+import playground.marcel.pt.transitSchedule.TransitScheduleWriterV1;
+
+
+import playground.mohit.converter.VisumNetwork;
+import playground.mohit.converter.VisumNetworkReader;
+import playground.mohit.converter.VisumNetwork.Departure;
+import playground.mohit.converter.VisumNetwork.LineRouteItem;
+import playground.mohit.converter.VisumNetwork.Stop;
+import playground.mohit.converter.VisumNetwork.StopPoint;
+import playground.mohit.converter.VisumNetwork.TimeProfile;
+import playground.mohit.converter.VisumNetwork.TimeProfileItem;
+import playground.mohit.converter.VisumNetwork.TransitLine;
+import playground.mohit.converter.VisumNetwork.TransitLineRoute;
+public class MyMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		final VisumNetwork vNetwork = new VisumNetwork();
+		  
+		try {
+			new playground.mohit.VisumNetworkReader(vNetwork).read("C:\\Users\\MOSES\\Desktop\\netwF.net"); // yalcin
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		System.out.println("Stops......." );
+		for (Stop stop : vNetwork.stops.values()) {
+			System.out.println("> " + stop.name +"   "+ stop.id +"  "+ stop.coord);
+		}
+		System.out.println("Stop Points......." );
+		for (StopPoint sp1 : vNetwork.stopPoints.values()) {
+			System.out.println("> " + sp1.id + "  " + sp1.stopAreaId + "   " + sp1.name+ "   " + sp1.refLinkNo);
+		}
+		System.out.println("Lines......." );
+		for (TransitLine l1 : vNetwork.lines.values()) {
+			System.out.println("> " + l1.id + "  " + l1.tCode);
+		}
+		System.out.println("Lineroutes.......corr lines.." );
+		for (TransitLineRoute lr1 : vNetwork.lineRoutes.values()) {
+			System.out.println("> " + lr1.id +"          "+ lr1.lineName);
+		}
+		System.out.println("Lines Route Items......." );
+		for (LineRouteItem lri1 : vNetwork.lineRouteItems.values()) {
+			System.out.println("> " + lri1.lineRouteName + "  " + lri1.lineName + "   " + lri1.index+ "   " + lri1.stopPointNo);
+		}
+		System.out.println("Time profiles......." );
+		for (TimeProfile tp1 : vNetwork.timeProfiles.values()) {
+			System.out.println("> " + tp1.lineRouteName  + "   " + tp1.index);
+		}
+		System.out.println("Time Profile Items......." );
+		for (TimeProfileItem tpi1 : vNetwork.timeProfileItems.values()) {
+			System.out.println("> " + tpi1.lineRouteName +tpi1.timeProfileName+ "  " + tpi1.lineName+ "   " + tpi1.index + "   " + tpi1.arr+ "   " + tpi1.dep+ "   " + tpi1.lRIIndex );
+		
+		}
+		System.out.println("Departures....." );
+		for (Departure d1 : vNetwork.departures.values()) {
+			System.out.println("> " + d1.lineRouteName + "  " + d1.lineName + "   " + d1.index+ "   " + d1.TRI+ "   " + d1.dep);
+			
+		}
+		
+		TransitSchedule schedule = new TransitSchedule();
+		new Visum2TransitSchedule(vNetwork, schedule).convert();
+		
+		try {
+			new TransitScheduleWriterV1(schedule).write("C:\\Users\\MOSES\\Desktop\\transitschedulefina.xml");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
