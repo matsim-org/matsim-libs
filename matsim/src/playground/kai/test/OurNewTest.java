449bc4419481921192a035524485658a6ddf77ba
vsp_nagel <2008@kainagel.org>
2009-07-07 18:50:19 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@7804 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/kai/test/OurNewTest.java
+++ matsim/src/playground/kai/test/OurNewTest.java
@@ -1,11 +1,41 @@
 package playground.kai.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+interface Person {
+}
+
+class PersonImpl implements Person {
+}
+
+interface Population {
+	Map<Integer, PersonImpl> getPersons() ;
+}
+
+class PopulationImpl implements Population {
+	private Map<Integer,PersonImpl> persons = new HashMap<Integer,PersonImpl>() ;
+	
+	public Map<Integer,PersonImpl> getPersons() {
+		return persons ;
+	}
+	
+}
+
+
+
 
 public class OurNewTest {
-
+	
 	public static void main( String[] args ) {
+		
+		Population pop = new PopulationImpl() ;
+		
+		Person person = new PersonImpl() ;
+		
+//		pop.getPersons().put( 1, person ) ;
 
-//		BasicLeg leg = new BasicLeg() ;
+
 		
 	}
 
