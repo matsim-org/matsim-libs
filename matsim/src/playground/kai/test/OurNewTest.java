319b102f590b9adbbe9ba5f3343de3206e72774f
vsp_nagel <2008@kainagel.org>
2009-07-07 17:18:06 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@7803 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/kai/test/OurNewTest.java
+++ matsim/src/playground/kai/test/OurNewTest.java
@@ -1,32 +1,11 @@
 package playground.kai.test;
 
-import junit.framework.TestCase;
-
-class BaseClass {
-	private int age ;
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-}
-
-class ExtendedClass extends BaseClass {
-	public void incAge() {
-		setAge( getAge()+1 ) ;
-	}
-}
 
 public class OurNewTest {
 
 	public static void main( String[] args ) {
 
-		BaseClass bc = new ExtendedClass() ;
-
-		ExtendedClass ec = (ExtendedClass) bc ;
+//		BasicLeg leg = new BasicLeg() ;
 		
 	}
 
