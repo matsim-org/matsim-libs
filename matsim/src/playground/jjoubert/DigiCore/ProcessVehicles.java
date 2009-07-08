27acf7a3467da6c4995959caa37e182ac6e00080
jwjoubert <johan.joubert@up.ac.za>
2009-07-08 08:29:41 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@7813 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/jjoubert/DigiCore/ProcessVehicles.java
+++ matsim/src/playground/jjoubert/DigiCore/ProcessVehicles.java
@@ -9,123 +9,151 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
+import playground.jjoubert.DateString;
+
 public class ProcessVehicles {
 	private static String delimiter = ","; 
 	private static String root = "/home/jjoubert/DigiCore/";
+	private static long startLine = 0;
+	private static long numberOfLinesToRead = Long.MAX_VALUE;
 	
 	// main method
 	public static void main( String[] args){
-		
+
 		System.out.println("=================================================================");
 		System.out.println("  Splitting the DigiCore data file into seperate vehicle files.");
 		System.out.println("=================================================================");
 		System.out.println();
+		long line = 0;
 		long linesRead = 0;
 		long reportValue = 1;
-		
+
 		File outputFoler = new File(root + "Vehicles/");
 		if(outputFoler.exists()){
 			System.err.printf("The folder %s already exists! Delete, and rerun.", outputFoler.getPath());
 		} else{
 			outputFoler.mkdirs();
 		}		
+
+		Scanner input = null;
+		BufferedWriter output = null;
+		String vehicleFile = null;
 		
-		Scanner input = null;;
-		BufferedWriter output = null;;
-		String vehicleFile = null;;
-
+		DateString ds = new DateString();
 		try {
-			input = new Scanner(new BufferedReader(new FileReader(new File( root + "Poslog_Research_Data.csv"))));
+			BufferedWriter logRecords = new BufferedWriter(new FileWriter(new File(root + "logRecordsRead_" + ds.toString() + ".txt")));
+			try{
+				try {
+					input = new Scanner(new BufferedReader(new FileReader(new File( root + "Poslog_Research_Data.csv"))));
 
-			if( input.hasNextLine() ){			
+					if( input.hasNextLine() ){			
 
-				// Read the first line, and split the fields based on the comma separator.
-				String [] inputString = input.nextLine().split(delimiter);
+						// Read the first line, and split the fields based on the comma separator.
+						String [] inputString = input.nextLine().split(delimiter);
+						line++;
 
-				if(inputString.length == 6){
-					// Open the file for the vehicle 
-					vehicleFile = "/Volumes/Data/DigiCore/VehicleFiles/" + inputString[0] + ".txt";
-					output = new BufferedWriter(new FileWriter(vehicleFile, true) , 10000 );
+						if(line >= startLine || linesRead <= numberOfLinesToRead){
+							if(inputString.length == 6){
+								// Open the file for the vehicle 
+								vehicleFile = "/Volumes/Data/DigiCore/VehicleFiles/" + inputString[0] + ".txt";
+								output = new BufferedWriter(new FileWriter(vehicleFile, true) , 10000 );
 
-					// Write the record to the associated file
-					output.write(inputString[0]); // Vehicle ID
-					output.write(delimiter);
-					output.write(inputString[1]); // Time stamp
-					output.write(delimiter);
-					output.write(inputString[2]); // X (longitude)
-					output.write(delimiter);
-					output.write(inputString[3]); // Y (latitude)
-					output.write(delimiter);
-					output.write(inputString[4]); // Status
-					output.write(delimiter);
-					output.write(inputString[5]); // Speed
-					output.newLine();
+								// Write the record to the associated file
+								output.write(inputString[0]); // Vehicle ID
+								output.write(delimiter);
+								output.write(inputString[1]); // Time stamp
+								output.write(delimiter);
+								output.write(inputString[2]); // X (longitude)
+								output.write(delimiter);
+								output.write(inputString[3]); // Y (latitude)
+								output.write(delimiter);
+								output.write(inputString[4]); // Status
+								output.write(delimiter);
+								output.write(inputString[5]); // Speed
+								output.newLine();
 
-					linesRead++;
-				}
-				
-				// Update report
-				if(linesRead == reportValue){
-					System.out.println(Long.valueOf(linesRead));
-					System.out.printf("   Lines read... ");
-					reportValue *= 2;
-				}
+								linesRead++;
+								logRecords.write(String.valueOf(linesRead));
+								logRecords.newLine();
 
-				String vehID = inputString[0];
-				while ( input.hasNextLine() ){
+								// Update report
+								if(linesRead == reportValue){
+									System.out.println(String.valueOf(linesRead));
+									System.out.printf("   Lines read... ");
+									reportValue *= 2;
+								}
+							}
 
-					// Read the next input line
-					inputString = input.nextLine().split(delimiter);
 
-					if(inputString.length == 6){
-						if ( !vehID.equalsIgnoreCase(inputString[0])) {
-							// Close the file for the current vehicle.
-							output.close();
+							String vehID = inputString[0];
+							while ( input.hasNextLine() ){
 
-							// Open the file for the new vehicle.
-							vehicleFile = "/Volumes/Data/DigiCore/VehicleFiles/" + inputString[0] + ".txt";
-							try {
-								output = new BufferedWriter(new FileWriter(vehicleFile, true) , 100000 );
-							} catch (IOException e) {
-								e.printStackTrace();
-							}			
+								// Read the next input line
+								inputString = input.nextLine().split(delimiter);
+								line++;
+
+								if(line >= startLine || linesRead <= numberOfLinesToRead){
+									if(inputString.length == 6){
+										if ( !vehID.equalsIgnoreCase(inputString[0])) {
+											// Close the file for the current vehicle.
+											output.close();
+
+											// Open the file for the new vehicle.
+											vehicleFile = "/Volumes/Data/DigiCore/VehicleFiles/" + inputString[0] + ".txt";
+											try {
+												output = new BufferedWriter(new FileWriter(vehicleFile, true) , 100000 );
+											} catch (IOException e) {
+												e.printStackTrace();
+											}			
+										}
+
+										// Write the record to the new file
+										output.write(inputString[0]); // Vehicle ID
+										output.write(delimiter);
+										output.write(inputString[1]); // Time stamp
+										output.write(delimiter);
+										output.write(inputString[2]); // X (longitude)
+										output.write(delimiter);
+										output.write(inputString[3]); // Y (latitude)
+										output.write(delimiter);
+										output.write(inputString[4]); // Status
+										output.write(delimiter);
+										output.write(inputString[5]); // Speed
+										output.newLine();
+
+										linesRead++;
+
+										vehID = inputString[0];
+
+										// Update report
+										if(linesRead == reportValue){
+											System.out.println(String.valueOf(linesRead));
+											System.out.printf("   Lines read... ");
+											reportValue *= 2;
+										}
+									}
+								} else{
+									line++;
+								}
+
+							}
+						} else{
+							line++;
 						}
-
-						// Write the record to the new file
-						output.write(inputString[0]); // Vehicle ID
-						output.write(delimiter);
-						output.write(inputString[1]); // Time stamp
-						output.write(delimiter);
-						output.write(inputString[2]); // X (longitude)
-						output.write(delimiter);
-						output.write(inputString[3]); // Y (latitude)
-						output.write(delimiter);
-						output.write(inputString[4]); // Status
-						output.write(delimiter);
-						output.write(inputString[5]); // Speed
-						output.newLine();
-
-						linesRead++;
-
-						vehID = inputString[0];
 					}
-					// Update report
-					if(linesRead == reportValue){
-						System.out.println(Long.valueOf(linesRead));
-						System.out.printf("   Lines read... ");
-						reportValue *= 2;
-					}
-
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
-
+			} finally{
+				logRecords.close();
 			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
 		}
 					
-		System.out.print(Long.valueOf(linesRead));
+		System.out.print(String.valueOf(linesRead));
 		System.out.printf(" (Done)\n");
 	}
 }
