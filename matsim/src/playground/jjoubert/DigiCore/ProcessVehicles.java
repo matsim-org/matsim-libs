28525bee7978658697b181de54eb9c53b7433638
jwjoubert <johan.joubert@up.ac.za>
2009-07-08 14:35:46 +0000

    Added Javadoc to the DigiCore class.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@7817 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/jjoubert/DigiCore/ProcessVehicles.java
+++ matsim/src/playground/jjoubert/DigiCore/ProcessVehicles.java
@@ -1,3 +1,23 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * GetDateString.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2009 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
 package playground.jjoubert.DigiCore;
 
 import java.io.BufferedReader;
@@ -17,7 +37,35 @@ public class ProcessVehicles {
 	private static long startLine = 0;
 	private static long numberOfLinesToRead = Long.MAX_VALUE;
 	
-	// main method
+	/**
+	 * This class only has a main method. It is invoked on its own to process the single file 
+	 * provided by DigiCore holdings, <code>Poslog_Research_Data.csv</code>, and split it into 
+	 * separate files, one for each vehicle. The file provided by DigiCore has six comma 
+	 * delimited fields in the following order:
+	 * 
+	 * <nl>
+	 * 	<li> <em>VehicleId</em>, a unique vehicle identifier sending the GPS log entry; 
+	 * 	<li> <em>Time</em>, a UNIX-based time stamp (in seconds);
+	 * 	<li> <em>Longitude</em>, the x-coordinate of the GPS log (in decimal degrees);
+	 * 	<li> <em>Latitude</em>, the y-coordinate of the GPS log (in decimal degrees); 
+	 * 	<li> <em>Status</em>, an integer digit indicating a predefined vehicle status. Status codes are described in <code>Statuses.xls</code>;
+	 * 	<li> <em>Speed</em>, a field reflecting the speed of the vehicle when sending the log entry. This field, however, proved to be quite useless.
+	 * </nl>
+	 * <hr>
+	 * The extent of the data is known to be 1 January 2008 - 30 June 2008.
+	 * 
+	 * <h4>
+	 * Process
+	 * </h4>
+	 * A single line is read from the input file (comma delimited), and die vehicle ID is 
+	 * determined. The associated vehicle file is opened (with amending privileges), and the 
+	 * line is added (again comma delimited) to the vehicle file. In an attempt to save time,
+	 * I've implemented the reading such that the next line is also read. If the new line
+	 * relates to the same vehicle, the vehicle file is kept open. Otherwise, the file is closed,
+	 * and the new vehicle file is opened. 
+	 * 
+	 * @author jwjoubert
+	 */
 	public static void main( String[] args){
 
 		System.out.println("=================================================================");
