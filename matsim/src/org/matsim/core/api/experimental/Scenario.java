131f6c265742920e293cf3dabfac27e8f9014d03
vsp_nagel <2008@kainagel.org>
2009-07-15 15:47:30 +0000

    Replaced most occurences of "Scenario" (interface) by "ScenarioImpl" (implementation).  This was necessary so that the interface "Scenario" can indeed return interface types "Population" and "Network".  Sorry, overlooked this last week.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@7869 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/org/matsim/core/api/experimental/Scenario.java
@@ -0,0 +1,57 @@
+/* *********************************************************************** *
+ * project: org.matsim.*																															*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package org.matsim.core.api.experimental;
+import org.matsim.api.basic.v01.BasicScenario;
+import org.matsim.core.api.experimental.network.Network;
+import org.matsim.core.api.experimental.population.Population;
+
+
+
+/**
+ * The scenario is the entry point to MATSim 
+ * scenarios. An implementation of Scenario
+ * has to provide consistent implementations
+ * for the different return types, e.g. Network, 
+ * Facilities or Population.
+ * @see org.matsim.core.scenario.ScenarioLoader
+ * @author dgrether
+ *
+ */
+public interface Scenario extends BasicScenario {
+
+	public Network getNetwork();
+
+	public Population getPopulation() ;
+	
+	// the following are available via the Impl only
+//	public ActivityFacilities getActivityFacilities() ;
+//
+//	public Knowledges getKnowledges();
+//	
+//	public Households getHouseholds();
+//	
+//	public BasicVehicles getVehicles();
+//
+//	public BasicLaneDefinitions getLaneDefinitions();
+//	
+//	public BasicSignalSystems getSignalSystems();
+//	
+//	public BasicSignalSystemConfigurations getSignalSystemConfigurations();
+	
+}
