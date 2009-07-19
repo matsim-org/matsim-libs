93b5e9a690e0e1dcc09b5d7653c8855300233377
mrieser <rieser@gmail.com>
2009-07-19 22:10:23 +0000

    make the builder work with Scenario instead of ScenarioImpl, and internally with Network instead of NetworkLayer. fixed some typos.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@7939 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/core/population/PopulationBuilderImpl.java
+++ matsim/src/org/matsim/core/population/PopulationBuilderImpl.java
@@ -22,13 +22,13 @@ package org.matsim.core.population;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.matsim.api.basic.v01.Coord;
 import org.matsim.api.basic.v01.Id;
 import org.matsim.api.basic.v01.TransportMode;
 import org.matsim.api.basic.v01.population.BasicPerson;
-import org.matsim.core.api.experimental.ScenarioImpl;
+import org.matsim.core.api.experimental.Scenario;
 import org.matsim.core.api.experimental.network.Link;
+import org.matsim.core.api.experimental.network.Network;
 import org.matsim.core.api.experimental.population.Plan;
 import org.matsim.core.api.experimental.population.PopulationBuilder;
 import org.matsim.core.api.experimental.population.Route;
@@ -43,18 +43,18 @@ import org.matsim.core.population.routes.NetworkRoute;
  */
 public class PopulationBuilderImpl implements PopulationBuilder {
 
-	private static final Logger log = Logger.getLogger(PopulationBuilderImpl.class);
+//	private static final Logger log = Logger.getLogger(PopulationBuilderImpl.class);
 
-	private final NetworkLayer network;
+	private final Network network;
 	private final ActivityFacilities facilities;
 
 	@Deprecated
-	public PopulationBuilderImpl(NetworkLayer network, PopulationImpl population, ActivityFacilities facilities) {
+	public PopulationBuilderImpl(final NetworkLayer network, final PopulationImpl population, final ActivityFacilities facilities) {
 		this.network = network;
 		this.facilities = facilities;
 	}
-	
-	public PopulationBuilderImpl(final ScenarioImpl scenario) {
+
+	public PopulationBuilderImpl(final Scenario scenario) {
 //		this.scenario = scenario;
 		this.network = scenario.getNetwork();
 //		this.population = scenario.getPopulation();
@@ -65,55 +65,55 @@ public class PopulationBuilderImpl implements PopulationBuilder {
 		PersonImpl p = new PersonImpl(id);
 		return p;
 	}
-	
+
 	public Plan createPlan(){
 		return new PlanImpl();
 	}
-	
-	public PlanImpl createPlan(BasicPerson person) {
+
+	public PlanImpl createPlan(final BasicPerson person) {
 		if (!(person instanceof PersonImpl)) {
-			throw new IllegalArgumentException("person must be of type Person.");
+			throw new IllegalArgumentException("person must be of type PersonImpl.");
 		}
 		return new PlanImpl((PersonImpl) person);
 	}
 
-	public ActivityImpl createActivityFromCoord(String actType, Coord coord) {
+	public ActivityImpl createActivityFromCoord(final String actType, final Coord coord) {
 		ActivityImpl act = new ActivityImpl(actType, coord);
 		return act;
 	}
 
-	public ActivityImpl createActivityFromFacilityId(String actType, Id facilityId) {
+	public ActivityImpl createActivityFromFacilityId(final String actType, final Id facilityId) {
 		ActivityImpl act = new ActivityImpl(actType, this.facilities.getFacilities().get(facilityId));
 		return act;
 	}
 
-	public ActivityImpl createActivityFromLinkId(String actType, Id linkId) {
-		ActivityImpl act = new ActivityImpl(actType, this.network.getLinks().get(linkId));
+	public ActivityImpl createActivityFromLinkId(final String actType, final Id linkId) {
+		ActivityImpl act = new ActivityImpl(actType, (LinkImpl) this.network.getLinks().get(linkId));
 		return act;
 	}
-	
+
 	public LegImpl createLeg(final TransportMode legMode) {
 		return new LegImpl(legMode);
 	}
 
 	public Route createRoute(final Id startLinkId, final Id endLinkId, final List<Id> currentRouteLinkIds) {
-		LinkImpl start = this.network.getLink(startLinkId);
-		LinkImpl end = this.network.getLink(endLinkId);
+		Link start = this.network.getLinks().get(startLinkId);
+		Link end = this.network.getLinks().get(endLinkId);
 		if (start == null) {
-			throw new IllegalStateException("Cann't create Route with StartLink Id " + startLinkId + " because the Link cannot be found in the loaded Network.");
+			throw new IllegalStateException("Can't create Route with StartLink Id " + startLinkId + " because the Link cannot be found in the loaded Network.");
 		}
 		if (end == null) {
-			throw new IllegalStateException("Cann't create Route with EndLink Id " + startLinkId + " because the Link cannot be found in the loaded Network.");
+			throw new IllegalStateException("Can't create Route with EndLink Id " + startLinkId + " because the Link cannot be found in the loaded Network.");
 		}
 		NetworkRoute route = new LinkNetworkRoute(start, end);
 		List<Link> links = new ArrayList<Link>();
 		Link link;
 		for (Id id : currentRouteLinkIds) {
-			link = this.network.getLink(id);
+			link = this.network.getLinks().get(id);
 			if (link == null) {
-				throw new IllegalStateException("Cann't create Route over Link with Id " + id + " because the Link cannot be found in the loaded Network.");
+				throw new IllegalStateException("Can't create Route over Link with Id " + id + " because the Link cannot be found in the loaded Network.");
 			}
-			links.add(this.network.getLink(id));
+			links.add(this.network.getLinks().get(id));
 		}
 		route.setLinks(start, links, end);
 		return route;
