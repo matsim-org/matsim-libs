3e66856c95bb4d8ac9522655dcbe422a4b85ef55
jwjoubert <johan.joubert@up.ac.za>
2009-07-22 11:58:32 +0000

    Set up ActivityLocations for WesternCape.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8013 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/jjoubert/CommercialTraffic/ActivityLocations.java
+++ matsim/src/playground/jjoubert/CommercialTraffic/ActivityLocations.java
@@ -30,7 +30,7 @@ public class ActivityLocations {
 	 * 		- KZN
 	 * 		- WesternCape
 	 */
-	 static String studyAreaName = "KZN";
+	 static String studyAreaName = "WesternCape";
 
 	// Set the home directory, depending on where the job is executed.
 //	final static String ROOT = "/Users/johanwjoubert/MATSim/workspace/MATSimData/"; // Mac
