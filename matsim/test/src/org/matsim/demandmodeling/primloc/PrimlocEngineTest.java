afd8518788d9cef6c108c23387ce2be7a6c921ae
mrieser <rieser@gmail.com>
2009-07-23 16:48:37 +0000

    fix test case so the output directory is created and the test can write the data again.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8054 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/test/src/org/matsim/demandmodeling/primloc/PrimlocEngineTest.java
+++ matsim/test/src/org/matsim/demandmodeling/primloc/PrimlocEngineTest.java
@@ -20,37 +20,36 @@ public class PrimlocEngineTest  extends MatsimTestCase {
 private final static String testPropertyFile = "test/input/org/matsim/demandmodeling/primloc/PrimaryLocationChoice.xml";
 
 	PrimlocCore core = new PrimlocCore();
-	
+
 	public void testPLCM() throws InvalidPropertiesFormatException, FileNotFoundException, IOException {
-		
+
 		System.out.println("Starting Primary Location Choice Model (PLCM)");
 		System.out.println("PLCM: Looking for PrimaryLocationChoice.xml config file of the Zurich sample dataset in " + testPropertyFile);
-		
+
 
 		if( new File( testPropertyFile ).exists() ){
 			System.out.println("PLCM: ... config found");
-			String outputDirectory = "test/output/" + this.getClass().getCanonicalName().replace('.', '/') + "/" + getName() + "/";
-			String outputfile = outputDirectory + "/rent_patterns_testPLCM:txt";
+			String outputfile = getOutputDirectory() + "/rent_patterns_testPLCM:txt";
 			standAloneRun( testPropertyFile, outputfile );
 		}
 		else{
 			System.err.println("PLCM: Could not find a suitable data set");
 		}
 	}
-	
-	void standAloneRun( String propFile, String outputFile ) throws InvalidPropertiesFormatException, FileNotFoundException, IOException {
+
+	void standAloneRun( final String propFile, final String outputFile ) throws InvalidPropertiesFormatException, FileNotFoundException, IOException {
 		TreeMap<Integer,Integer> zonemap = initProperties( propFile ); // model_ID to internal_ID
-	
-		core.setCalibrationError( new PrimlocCalibrationErrorMatrix( gravityModelMatrix() ) );	
-		core.runCalibrationProcess();
+
+		this.core.setCalibrationError( new PrimlocCalibrationErrorMatrix( gravityModelMatrix() ) );
+		this.core.runCalibrationProcess();
 
 		System.out.println("PLCM: writing rent patterns to:" + new File(outputFile).getAbsolutePath() );
-		
-		PrimlocIO.saveResults( core.X, zonemap, outputFile);
+
+		PrimlocIO.saveResults( this.core.X, zonemap, outputFile);
 		System.out.println("PLCM: done");
 	}
-	
-	private TreeMap<Integer,Integer> initProperties( String propertyFile ) throws InvalidPropertiesFormatException, FileNotFoundException, IOException {
+
+	private TreeMap<Integer,Integer> initProperties( final String propertyFile ) throws InvalidPropertiesFormatException, FileNotFoundException, IOException {
 
 		String zoneFileName=null;
 		String costFileName=null;
@@ -64,24 +63,24 @@ private final static String testPropertyFile = "test/input/org/matsim/demandmode
 		costFileName = props.getProperty("costFileName");
 		homesFileName = props.getProperty("homesFileName");
 		jobsFileName = props.getProperty("jobsFileName");
-		
-		core.maxiter = Integer.parseInt(props.getProperty("maxiter"));
-		core.mu = Double.parseDouble(props.getProperty("mu"));
-		core.theta = Double.parseDouble( props.getProperty("theta"));
-		core.threshold1 = Double.parseDouble(props.getProperty("threshold1"));
-		core.threshold2 = Double.parseDouble(props.getProperty("threshold2"));
-		core.verbose = Boolean.parseBoolean(props.getProperty("verbose"));
+
+		this.core.maxiter = Integer.parseInt(props.getProperty("maxiter"));
+		this.core.mu = Double.parseDouble(props.getProperty("mu"));
+		this.core.theta = Double.parseDouble( props.getProperty("theta"));
+		this.core.threshold1 = Double.parseDouble(props.getProperty("threshold1"));
+		this.core.threshold2 = Double.parseDouble(props.getProperty("threshold2"));
+		this.core.verbose = Boolean.parseBoolean(props.getProperty("verbose"));
 
 		//
 		// 1) Read the zone indices
 		//
 
 		HashSet<Integer> zoneids = PrimlocIO.readZoneIDs(zoneFileName);
-		core.numZ=zoneids.size();
+		this.core.numZ=zoneids.size();
 
-		if( core.verbose ){
+		if( this.core.verbose ){
 			System.out.println("PLCM: Data:");
-			System.out.println("PLCM:\t#zones = "+core.numZ);
+			System.out.println("PLCM:\t#zones = "+this.core.numZ);
 		}
 
 		int idx=0;
@@ -98,51 +97,51 @@ private final static String testPropertyFile = "test/input/org/matsim/demandmode
 		// cii is select as min_j core.cij
 		//
 
-		core.cij = PrimlocIO.readMatrix(costFileName, core.numZ, core.numZ);
-		for( int i=0; i<core.numZ; i++ ){
+		this.core.cij = PrimlocIO.readMatrix(costFileName, this.core.numZ, this.core.numZ);
+		for( int i=0; i<this.core.numZ; i++ ){
 			double mincij = Double.POSITIVE_INFINITY;
-			for( int j=0; j<core.numZ; j++ ){
-				double v=core.cij.get(i, j);
+			for( int j=0; j<this.core.numZ; j++ ){
+				double v=this.core.cij.get(i, j);
 				if( (v < mincij) && (v>0.0) )
 					mincij = v;
 			}
-			if( core.cij.get(i, i) == 0.0 )
-				core.cij.set(i, i, mincij );
+			if( this.core.cij.get(i, i) == 0.0 )
+				this.core.cij.set(i, i, mincij );
 		}
 
-		core.setupCostStatistics();
-		if( core.verbose )
-			System.out.println("PLCM:\tTravel costs: mean="+core.df.format(core.avgCost)+" std.dev.= "+core.df.format(core.stdCost));
+		this.core.setupCostStatistics();
+		if( this.core.verbose )
+			System.out.println("PLCM:\tTravel costs: mean="+this.core.df.format(this.core.avgCost)+" std.dev.= "+this.core.df.format(this.core.stdCost));
 
 		//
 		// 3) read the homes and jobs files
 		// These matrices can be sparse
 		//
 
-		core.P = PrimlocIO.readZoneAttribute( core.numZ, homesFileName, zonemap );
-		core.J = PrimlocIO.readZoneAttribute( core.numZ, jobsFileName, zonemap );
+		this.core.P = PrimlocIO.readZoneAttribute( this.core.numZ, homesFileName, zonemap );
+		this.core.J = PrimlocIO.readZoneAttribute( this.core.numZ, jobsFileName, zonemap );
 
 		double maxpj=0.0;
 		double sp=0.0;
 		double sj=0.0;
 
-		for( int i=0; i<core.numZ; i++ ){
-			sp += core.P[i];
-			sj += core.J[i];
-			if( core.P[i] > maxpj )
-				maxpj = core.P[i];
-			if( core.J[i] > maxpj )
-				maxpj = core.J[i];
+		for( int i=0; i<this.core.numZ; i++ ){
+			sp += this.core.P[i];
+			sj += this.core.J[i];
+			if( this.core.P[i] > maxpj )
+				maxpj = this.core.P[i];
+			if( this.core.J[i] > maxpj )
+				maxpj = this.core.J[i];
 		}
 
 		if( Math.abs(sp-sj) > 1.0 ){
 			System.err.println("PLCM: Error: #jobs("+sj+")!= #homes("+sp+")");
 			System.exit(-1);
 		}
-		core.N=sp;
+		this.core.N=sp;
 
-		if( core.verbose )
-			System.out.println("PLCM:\tTrip table: #jobs = #homes= "+core.N);
+		if( this.core.verbose )
+			System.out.println("PLCM:\tTrip table: #jobs = #homes= "+this.core.N);
 
 		return zonemap;
 	}
@@ -150,20 +149,20 @@ private final static String testPropertyFile = "test/input/org/matsim/demandmode
 
 	Matrix gravityModelMatrix(){
 		double betaExponent = 0.7;
-		Matrix grav = new Matrix( core.numZ, core.numZ );
+		Matrix grav = new Matrix( this.core.numZ, this.core.numZ );
 		double sum1=0.0;
-		for( int i=0; i<core.numZ; i++ ){
-			for( int j=0; j<core.numZ; j++){
-				double v = core.P[i]*core.J[j] / Math.pow( core.cij.get(i, j), betaExponent );
+		for( int i=0; i<this.core.numZ; i++ ){
+			for( int j=0; j<this.core.numZ; j++){
+				double v = this.core.P[i]*this.core.J[j] / Math.pow( this.core.cij.get(i, j), betaExponent );
 				grav.set(i, j, v);
 				sum1+=v;
 			}
 		}
 
 		double v=0.0;
-		for( int i=0; i<core.numZ; i++ )
-			for( int j=0; j<core.numZ; j++){
-				grav.set(i, j, grav.get(i, j)*core.N/sum1);
+		for( int i=0; i<this.core.numZ; i++ )
+			for( int j=0; j<this.core.numZ; j++){
+				grav.set(i, j, grav.get(i, j)*this.core.N/sum1);
 				v+=grav.get(i, j);
 			}
 		return grav;
