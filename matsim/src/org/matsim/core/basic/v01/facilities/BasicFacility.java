df462872451b85dd813f6d7d6a1f2d23a530d688
vsp_nagel <2008@kainagel.org>
2009-07-27 14:30:30 +0000

    added a comment
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8112 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/core/basic/v01/facilities/BasicFacility.java
+++ matsim/src/org/matsim/core/basic/v01/facilities/BasicFacility.java
@@ -4,6 +4,11 @@ import org.matsim.api.basic.v01.BasicLocation;
 import org.matsim.api.basic.v01.Id;
 import org.matsim.api.basic.v01.Identifiable;
 
+/**
+ * A (Basic)Facility is a (Basic)Location ("getCoord") with an Id ("getId") that is connected to a Link ("getLinkId").
+ * 
+ * @author nagel
+ */
 public interface BasicFacility extends BasicLocation, Identifiable {
 
 	public Id getLinkId();
