5393e3a7c2afe580c73fbbe1f38042fc54456a70
Matthias_Feil <Matthias_Feil@users.sf.net>
2009-08-03 08:02:02 +0000

    Class that checks two activity chains for equality
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8186 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/mfeil/ActChainEqualityCheck.java
@@ -0,0 +1,55 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * ActChainEqualityCheck.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2009 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.mfeil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.matsim.api.basic.v01.population.PlanElement;
+import org.matsim.core.population.ActivityImpl;
+
+
+/**
+ * Simple class that check whether two activity chains are identical. Returns true if so, and false otherwise. *
+ * @author mfeil *
+ */
+
+public class ActChainEqualityCheck {
+
+	public boolean checkForEquality (List<PlanElement> ac1, List<PlanElement> ac2){
+		
+		if (ac1.size()!=ac2.size()){
+		
+			return false;
+		}
+		else{
+			ArrayList<String> acts1 = new ArrayList<String> ();
+			ArrayList<String> acts2 = new ArrayList<String> ();
+			for (int i = 0;i<ac1.size();i=i+2){
+				acts1.add(((ActivityImpl)(ac1.get(i))).getType().toString());				
+			}
+			for (int i = 0;i<ac2.size();i=i+2){
+				acts2.add(((ActivityImpl)(ac2.get(i))).getType().toString());				
+			}		
+			return (acts1.equals(acts2));
+		}
+	}	
+}	
