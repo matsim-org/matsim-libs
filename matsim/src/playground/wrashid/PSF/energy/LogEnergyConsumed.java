1ed6373d080c9901f8ce5226383c1d2c80147135
rashid_waraich <rashid_waraich@users.sf.net>
2009-08-06 18:07:38 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8285 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/wrashid/PSF/energy/LogEnergyConsumed.java
@@ -0,0 +1,33 @@
+package playground.wrashid.PSF.energy;
+
+import org.matsim.api.basic.v01.events.BasicLinkEnterEvent;
+import org.matsim.api.basic.v01.events.BasicLinkLeaveEvent;
+import org.matsim.api.basic.v01.events.handler.BasicLinkEnterEventHandler;
+import org.matsim.api.basic.v01.events.handler.BasicLinkLeaveEventHandler;
+
+/*
+ * During driving energy is being "consumed", log that for each vehicle and leg.
+ * - for this we need AverageSpeedEnergyConsumption for each link (later more inputs: e.g. maximum speed allowed on the road)
+ * - get also a class for AverageSpeedEnergyConsumption here...
+ * => we need to assign a different such curve to each agent (we need to put this attribute to the agent)
+ * 
+ */
+public class LogEnergyConsumed implements BasicLinkLeaveEventHandler,
+BasicLinkEnterEventHandler {
+
+	public void handleEvent(BasicLinkLeaveEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void reset(int iteration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void handleEvent(BasicLinkEnterEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ matsim/src/playground/wrashid/PSF/energy/PerformeOptimizedCharging.java
@@ -0,0 +1,11 @@
+package playground.wrashid.PSF.energy;
+
+/*
+ * From the consumed energy per leg, the parking times and the energy prices, 
+ * we can calculate the times, when energy was effectively charged for each vehicle (this is the output)
+ * 
+ * - we need to put a class for reading energy prices here.
+ */
+public class PerformeOptimizedCharging {
+
+}
