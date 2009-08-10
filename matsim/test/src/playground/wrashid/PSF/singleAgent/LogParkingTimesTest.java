7113fbe08cf591611dd2f7064bb5b00b4eb5e143
rashid_waraich <rashid_waraich@users.sf.net>
2009-08-10 15:02:02 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8316 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/test/src/playground/wrashid/PSF/singleAgent/LogParkingTimesTest.java
+++ matsim/test/src/playground/wrashid/PSF/singleAgent/LogParkingTimesTest.java
@@ -1,20 +1,35 @@
 package playground.wrashid.PSF.singleAgent;
 
+import org.matsim.api.basic.v01.Id;
+import org.matsim.core.basic.v01.IdImpl;
 import org.matsim.core.controler.Controler;
 import org.matsim.testcases.MatsimTestCase;
 
 import playground.wrashid.PSF.energy.AddEnergyScoreListener;
+import playground.wrashid.PSF.energy.SimulationStartupListener;
 import playground.wrashid.PSF.parking.LogParkingTimes;
+import playground.wrashid.PSF.parking.ParkLog;
+import playground.wrashid.PSF.parking.ParkingTimes;
 
 public class LogParkingTimesTest extends MatsimTestCase {
 	public void testLogParkingTime() {
 		Controler controler=new Controler("test/input/playground/wrashid/PSF/singleAgent/" + "config.xml");
 		controler.addControlerListener(new AddEnergyScoreListener());
-		controler.setOverwriteFiles(true);
-		controler.getConfig().controler().setOutputDirectory(this.getOutputDirectory());
+		//controler.setOverwriteFiles(true);
+		//controler.getConfig().controler().setOutputDirectory(this.getOutputDirectory());
 		
-		controler.getEvents().addHandler(new LogParkingTimes());
+		LogParkingTimes logParkingTimes=new LogParkingTimes(controler);		
+		controler.addControlerListener(new SimulationStartupListener(logParkingTimes));
 		
 		controler.run();
+		
+		
+		ParkingTimes parkingTimes=logParkingTimes.getParkingTimes().get(new IdImpl("1"));
+		
+		// allow small delta of one second (because the output time in the log file is truncated
+		assertEquals(parkingTimes.getCarLastTimeParked(),61449,1);
+		ParkLog parkLog=parkingTimes.getParkingTimes().get(0);
+		assertEquals(parkLog.getStartParkingTime(),22989,1);
+		assertEquals(parkLog.getEndParkingTime(),59349,1);
 	}
 }
