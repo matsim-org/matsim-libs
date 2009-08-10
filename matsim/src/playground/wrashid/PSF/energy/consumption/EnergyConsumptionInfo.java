13e9ef3cf8c36e3d09cf52d15d6015c2b2cee77d
rashid_waraich <rashid_waraich@users.sf.net>
2009-08-10 15:57:08 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8322 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/wrashid/PSF/energy/consumption/EnergyConsumptionInfo.java
@@ -0,0 +1,32 @@
+package playground.wrashid.PSF.energy.consumption;
+
+import org.matsim.api.basic.v01.Id;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.core.utils.misc.Time;
+
+public class EnergyConsumptionInfo {
+
+	/*
+	 * In [J]
+	 */
+	public static double getEnergyConsumption(Link link, double travelTime, int vehicleType){
+		double freeSpeed=link.getFreespeed(Time.UNDEFINED_TIME); //[m/s]
+		double vehicleSpeed=link.getLength()/travelTime; //[m/s]
+		
+		// TODO: read some data a file. For example for different velocities there is a different
+		// energy consumption
+		
+		// how much energy in Jules is consumed by driving the vehicleType one meter
+		// with the given speed
+		
+		// see playground.wrashid.PHEV.Utility.AverageSpeedEnergyConsumption
+		
+		// TODO: this is speed dependent!!!
+		double energyConsumptionPerMeter=4.784535E+02; // [J/m] 
+		
+		// TODO: change this, the speed should get a role here!!!
+		return link.getLength()*energyConsumptionPerMeter;
+	}
+	
+	
+}
