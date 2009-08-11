bf454b437260d99c40c2ea2442895c9021b63061
rashid_waraich <rashid_waraich@users.sf.net>
2009-08-11 09:35:12 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8332 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/wrashid/PSF/energy/charging/ChargeLog.java
@@ -0,0 +1,31 @@
+package playground.wrashid.PSF.energy.charging;
+
+import org.matsim.api.basic.v01.Id;
+
+public class ChargeLog {
+
+	private Id facilityId;
+
+	private double startChargingTime;
+	private double endChargingTime;
+
+	public Id getFacilityId() {
+		return facilityId;
+	}
+
+	public double getStartChargingTime() {
+		return startChargingTime;
+	}
+
+	public double getEndChargingTime() {
+		return endChargingTime;
+	}
+
+	public ChargeLog(Id facilityId, double startChargingTime, double endChargingTime) {
+		super();
+		this.facilityId = facilityId;
+		this.startChargingTime = startChargingTime;
+		this.endChargingTime = endChargingTime;
+	}
+
+}
---
+++ matsim/src/playground/wrashid/PSF/energy/charging/ChargingTimes.java
@@ -0,0 +1,15 @@
+package playground.wrashid.PSF.energy.charging;
+
+import java.util.LinkedList;
+
+import org.matsim.api.basic.v01.Id;
+
+import playground.wrashid.PSF.parking.ParkLog;
+
+public class ChargingTimes {
+	
+	private LinkedList<ChargeLog> chargingTimes=new LinkedList<ChargeLog>();
+	private double initialStateOfCharge=0;
+	private double finalStateOfCharge=0; // needed e.g. for 
+	
+}
