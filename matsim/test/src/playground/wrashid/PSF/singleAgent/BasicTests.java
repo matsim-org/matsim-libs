2f472936bb69ba8a343a8db5bb7d3b752689c6d5
rashid_waraich <rashid_waraich@users.sf.net>
2009-08-13 15:23:50 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8380 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/test/src/playground/wrashid/PSF/singleAgent/BasicTests.java
+++ matsim/test/src/playground/wrashid/PSF/singleAgent/BasicTests.java
@@ -1,5 +1,7 @@
 package playground.wrashid.PSF.singleAgent;
 
+import java.util.HashMap;
+
 import org.matsim.api.basic.v01.Id;
 import org.matsim.core.basic.v01.IdImpl;
 import org.matsim.core.controler.Controler;
@@ -7,6 +9,9 @@ import org.matsim.testcases.MatsimTestCase;
 
 import playground.wrashid.PSF.energy.AddEnergyScoreListener;
 import playground.wrashid.PSF.energy.SimulationStartupListener;
+import playground.wrashid.PSF.energy.charging.ChargeLog;
+import playground.wrashid.PSF.energy.charging.ChargingTimes;
+import playground.wrashid.PSF.energy.charging.optimizedCharging.OptimizedCharger;
 import playground.wrashid.PSF.energy.consumption.EnergyConsumption;
 import playground.wrashid.PSF.energy.consumption.LinkEnergyConsumptionLog;
 import playground.wrashid.PSF.energy.consumption.LogEnergyConsumption;
@@ -15,6 +20,34 @@ import playground.wrashid.PSF.parking.ParkLog;
 import playground.wrashid.PSF.parking.ParkingTimes;
 
 public class BasicTests extends MatsimTestCase {
+	
+	public void testOptimizedCharger() {
+		Controler controler=new Controler("test/input/playground/wrashid/PSF/singleAgent/" + "config.xml");
+		controler.addControlerListener(new AddEnergyScoreListener());
+		controler.setOverwriteFiles(true);
+		
+		LogEnergyConsumption logEnergyConsumption=new LogEnergyConsumption(controler);
+		LogParkingTimes logParkingTimes=new LogParkingTimes(controler);
+		SimulationStartupListener simulationStartupListener=new SimulationStartupListener();
+		controler.addControlerListener(simulationStartupListener);
+		
+		simulationStartupListener.addEventHandler(logEnergyConsumption);
+		simulationStartupListener.addEventHandler(logParkingTimes);
+		
+		controler.run();
+		
+		OptimizedCharger optimizedCharger= new OptimizedCharger(logEnergyConsumption.getEnergyConsumption(),logParkingTimes.getParkingTimes());
+		HashMap<Id, ChargingTimes> chargingTimes=optimizedCharger.getChargingTimes();
+		
+		ChargingTimes chargingTimesOfAgentOne=chargingTimes.get(new IdImpl("1"));
+		ChargeLog chargeLogOfAgentOne=chargingTimesOfAgentOne.getChargingTimes().get(0);
+		
+		
+		assertEquals(chargeLogOfAgentOne.getStartChargingTime(), 22989);
+		assertEquals(chargeLogOfAgentOne.getEndChargingTime(), 1111111111); // don't know this year, must calculate it...
+		
+	}
+	
 	public void testLogParkingTime() {
 		Controler controler=new Controler("test/input/playground/wrashid/PSF/singleAgent/" + "config.xml");
 		controler.addControlerListener(new AddEnergyScoreListener());
@@ -66,4 +99,6 @@ public class BasicTests extends MatsimTestCase {
 		assertEquals(energyLog.getEnterTime(),21670,1);
 		assertEquals(energyLog.getLeaveTime(),22029,1);
 	}
+	
+	
 }
