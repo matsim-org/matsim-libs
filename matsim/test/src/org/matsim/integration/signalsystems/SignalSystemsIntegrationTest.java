490034dece8a4b34974d21fe59aba207b2104a74
grether <dominik.grether@gmail.com>
2009-08-13 15:19:02 +0000

    integration test for signal system implementation
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8376 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/test/src/org/matsim/integration/signalsystems/SignalSystemsIntegrationTest.java
@@ -0,0 +1,64 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * SignalSystemIntegrationTest
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2009 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package org.matsim.integration.signalsystems;
+
+import org.matsim.core.config.Config;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.utils.misc.CRCChecksum;
+import org.matsim.testcases.MatsimTestCase;
+
+
+/**
+ * @author dgrether
+ *
+ */
+public class SignalSystemsIntegrationTest extends MatsimTestCase {
+
+	private final static String CONFIG_FILE_NAME = "signalSystemsIntegrationConfig.xml";
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testSignalSystems() {
+		Config config = this.loadConfig(this.getClassInputDirectory() + CONFIG_FILE_NAME);
+		config.controler().setOutputDirectory(this.getOutputDirectory());
+		Controler c = new Controler(config);
+		c.setCreateGraphs(false);
+		c.run();
+		
+		String iterationOutput = this.getOutputDirectory() + "/ITERS/it.10/";
+		assertEquals("different events files", 
+				CRCChecksum.getCRCFromFile(this.getInputDirectory() + "10.events.txt.gz"), 
+				CRCChecksum.getCRCFromFile(iterationOutput + "10.events.txt.gz"));
+
+		assertEquals("different population files", 
+				CRCChecksum.getCRCFromFile(this.getInputDirectory() + "10.plans.xml.gz"), 
+				CRCChecksum.getCRCFromFile(iterationOutput + "10.plans.xml.gz"));
+	}
+	
+	
+}
