be6c2a002753e002874e7f14a0dbd34593dfb8f2
mrieser <rieser@gmail.com>
2009-08-13 13:59:22 +0000

    deleted dead code that was commented out since more than 3 months.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8372 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/vis/otfvis/opengl/OnTheFlyQueueSimQuad.java
+++ matsim/src/org/matsim/vis/otfvis/opengl/OnTheFlyQueueSimQuad.java
@@ -66,8 +66,6 @@ public class OnTheFlyQueueSimQuad extends QueueSimulation{
 				e.printStackTrace();
 			}
 		}
-
-
 	}
 
 	@Override
@@ -100,56 +98,5 @@ public class OnTheFlyQueueSimQuad extends QueueSimulation{
 		this.network = net;
 		this.simEngine = new QueueSimEngine(this.network, MatsimRandom.getRandom());
 	}
-	
-	public static void main(final String[] args) {
-
-		// if you really think you need this method, please speak to me. marcel/29.apr2009 TODO [MR] delete main method
-		System.err.println("commented the whole main-method out. Please use org.matsim.run.OTFVis instead. See code for more details");
-		
-//		String studiesRoot = "../";
-//		String localDtdBase = "../matsimJ/dtd/";
-//
-//		// _FIXME hard-coded filenames
-//		String netFileName = studiesRoot + "berlin-wip/network/wip_net.xml";
-//		String popFileName = studiesRoot + "berlin-wip/synpop-2006-04/kutter_population/kutter001car_hwh.routes_wip.plans.xml.gz"; // 15931 agents
-////		String popFileName = studiesRoot + "berlin-wip/synpop-2006-04/kutter_population/kutter010car_hwh.routes_wip.plans.xml.gz"; // 160171 agents
-////		String popFileName = studiesRoot + "berlin-wip/synpop-2006-04/kutter_population/kutter010car.routes_wip.plans.xml.gz";  // 299394 agents
-////		String worldFileName = studiesRoot + "berlin-wip/synpop-2006-04/world_TVZ.xml";
-//
-//		Config config = Gbl.createConfig(args);
-//
-//		config.global().setLocalDtdBase(localDtdBase);
-//
-//		config.simulation().setStartTime(Time.parseTime("09:00:00"));
-//		config.simulation().setEndTime(Time.parseTime("19:10:00"));
-//
-//		config.simulation().setFlowCapFactor(0.1);
-//		config.simulation().setStorageCapFactor(0.1);
-//
-//
-//		if(args.length >= 1) {
-//			netFileName = config.network().getInputFile();
-//			popFileName = config.plans().getInputFile();
-////			worldFileName = config.world().getInputFile();
-//		}
-//
-//		NetworkLayer net = new NetworkLayer();
-//		new MatsimNetworkReader(net).readFile(netFileName);
-//
-//		Population population = new PopulationImpl();
-//		MatsimPopulationReader plansReader = new MatsimPopulationReader(population, net);
-//		plansReader.readFile(popFileName);
-//		population.printPlansCount();
-//
-//		Events events = new Events();
-//
-//		OnTheFlyQueueSimQuad sim = new OnTheFlyQueueSimQuad(net, population, events);
-//
-//		config.simulation().setSnapshotFormat("none");// or just set the snapshotPeriod to zero ;-)
-//
-//		sim.run();
-
-	}
-
 
 }
