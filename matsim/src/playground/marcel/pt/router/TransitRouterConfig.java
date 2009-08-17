02ce46ff1278451dd0bc4cd976ddfab92639ce77
mrieser <rieser@gmail.com>
2009-08-17 10:12:37 +0000

    added lots of Javadoc
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8412 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/marcel/pt/router/TransitRouterConfig.java
+++ matsim/src/playground/marcel/pt/router/TransitRouterConfig.java
@@ -22,11 +22,41 @@ package playground.marcel.pt.router;
 
 public class TransitRouterConfig {
 
+	/**
+	 * The distance in meters in which stop facilities should be searched for
+	 * around the start and end coordinate.
+	 */
 	public double searchRadius = 1000.0;
+	
+	/**
+	 * If no stop facility is found around start or end coordinate (see 
+	 * {@link #searchRadius}), the nearest stop location is searched for
+	 * and the distance from start/end coordinate to this location is
+	 * extended by the given amount.<br />
+	 * If only one stop facility is found within {@link #searchRadius},
+	 * the radius is also extended in the hope to find more stop
+	 * facilities (e.g. in the opposite direction of the already found
+	 * stop). 
+	 */
 	public double extensionRadius = 200.0;
+	
+	/**
+	 * The distance in meters that agents can walk to get from one stop to 
+	 * another stop of a nearby transit line.
+	 */
+	public double beelineWalkConnectionDistance = 100.0;
+	
+	/**
+	 * Walking speed of agents on transfer links, beeline distance.
+	 */
 	public double beelineWalkSpeed = 3.0/3.6;
-//	public double marginalUtilityOfDistanceWalk = 0.0;
+	
 	public double marginalUtilityOfTravelTimeWalk = -6.0 / 3600.0;
+	
 	public double marginalUtilityOfTravelTimeTransit = -6.0 / 3600.0;
+	
+	/**
+	 * The additional costs to be added when an agent switches lines.
+	 */
 	public double costLineSwitch = 60.0 * -this.marginalUtilityOfTravelTimeTransit; // == 1min travel time in vehicle
 }
