c0d85ac6c1f92969e56a69cc8ca9cfc33f56c15d
grether <dominik.grether@gmail.com>
2009-08-21 14:34:06 +0000

    went to oo style
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8510 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/andreas/intersection/zuerich/GenerateZuerrichOutput.java
+++ matsim/src/playground/andreas/intersection/zuerich/GenerateZuerrichOutput.java
@@ -5,7 +5,10 @@ import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.matsim.api.basic.v01.Id;
+import org.matsim.api.core.v01.network.Network;
 import org.matsim.core.basic.v01.IdImpl;
+import org.matsim.core.network.MatsimNetworkReader;
+import org.matsim.core.network.NetworkLayer;
 import org.matsim.lanes.MatsimLaneDefinitionsWriter;
 import org.matsim.lanes.basic.BasicLaneDefinitions;
 import org.matsim.lanes.basic.BasicLaneDefinitionsImpl;
@@ -20,9 +23,91 @@ import org.matsim.signalsystems.config.BasicSignalSystemConfiguration;
 import org.matsim.signalsystems.config.BasicSignalSystemConfigurations;
 import org.matsim.signalsystems.config.BasicSignalSystemConfigurationsImpl;
 
+import playground.dgrether.DgPaths;
+import playground.dgrether.lanes.LanesConsistencyChecker;
+
 public class GenerateZuerrichOutput {
 
 	private static final Logger log = Logger.getLogger(GenerateZuerrichOutput.class);
+	
+	private String inputDir = "/media/data/work/scmWorkspace/shared-svn/studies/dgrether/lsaZurich/mappingNeu/";
+	private 	String outputDir = "/media/data/work/scmWorkspace/shared-svn/studies/dgrether/signalSystemsZh/";
+	// lsa id \t cycle time
+	private 	String lsaTu = inputDir + "lsa_tu.txt";
+//	lsa id (=node id) \t signal group id \t start red 
+	private 	String sgGreentime = inputDir  + "sg_greentime.txt";
+	// node id \t suprnr \t linkid teleatlas \t linkid navteq \t linkid ivtch
+	private 	String spurLinkMappingFile = inputDir + "spur_link_mapping.txt";
+	//node id \t lsanr??? \t spurnr
+	private 	String lsaSpurMappingFile = inputDir + "lsa_spur_mapping.txt";
+	//node id \t vonspur \t nachspur
+	private 	String spurSpurMappingFile = inputDir + "spur_spur_mapping.txt";
+	
+	private 	String lanesOutputFile = outputDir + "laneDefinitions.xml";
+	private 	String signalConfigOutputFile = outputDir +  "signalSystemsConfig.xml";
+	private 	String signalSystemsOutputFile = outputDir + "signalSystems.xml";
+	
+	private 	boolean generateLanes = true;
+	private 	boolean generateSignalSystems = false;
+	private 	boolean generateSignalSystemsConfig = false;
+	private 	boolean removeDuplicates = false;
+	
+	public GenerateZuerrichOutput() {
+
+		
+		Network net = new NetworkLayer();
+		MatsimNetworkReader netReader = new MatsimNetworkReader((NetworkLayer) net);
+		netReader.readFile(DgPaths.IVTCHNET);
+		
+		Map<Integer, Map<Integer,  List<Integer>>> spurSpurMapping = null;
+		Map<Integer, Map<Integer, String>> spurLinkMapping = null;
+		BasicLaneDefinitions laneDefs = null;
+		if (generateLanes){
+			//knotennummer -> (vonspur 1->n nachspur)
+			spurSpurMapping = SpurSpurMappingReader.readBasicLightSignalSystemDefinition(spurSpurMappingFile);
+			//knotennummer -> (spurnummer -> linkid)
+			spurLinkMapping = new SpurLinkMappingReader().readBasicLightSignalSystemDefinition(spurLinkMappingFile);
+			
+			//create the lanes
+			laneDefs = processLaneDefinitions(spurSpurMapping, spurLinkMapping);
+			
+			new LanesConsistencyChecker(net, laneDefs).checkConsistency();
+			
+			//write data
+			MatsimLaneDefinitionsWriter laneWriter = new MatsimLaneDefinitionsWriter(laneDefs);
+			laneWriter.writeFile(lanesOutputFile);
+		}
+
+		if (generateSignalSystems){
+			BasicSignalSystems signalSystems = new BasicSignalSystemsImpl();
+			//read the mappings
+			//read the system id <-> cycle time mapping
+			new LSASystemsReader(signalSystems).readBasicLightSignalSystemDefinition(lsaTu);			
+			//node id \t lsanr??? \t spurnr
+			Map<Integer, Map<Integer,  List<Integer>>> lsaSpurMapping = LSASpurMappingReader.readBasicLightSignalSystemDefinition(lsaSpurMappingFile);
+			//create the signals
+			processSignalSystems(lsaSpurMapping, spurLinkMapping, signalSystems);
+			MatsimSignalSystemsWriter signalSytemswriter = new MatsimSignalSystemsWriter(signalSystems);
+			signalSytemswriter.writeFile(signalSystemsOutputFile );
+
+		}
+
+		if (generateSignalSystemsConfig){
+			// signal system configs
+			BasicSignalSystemConfigurations signalSystemConfig = processSignalSystemConfigurations(sgGreentime);
+			MatsimSignalSystemConfigurationsWriter matsimLightSignalSystemConfigurationWriter 
+			= new MatsimSignalSystemConfigurationsWriter(signalSystemConfig);
+			matsimLightSignalSystemConfigurationWriter.writeFile(signalConfigOutputFile);
+		}
+
+		if (removeDuplicates){
+			//remove duplicates
+			RemoveDuplicates.readBasicLightSignalSystemDefinition(signalConfigOutputFile);
+			RemoveDuplicates.readBasicLightSignalSystemDefinition(signalSystemsOutputFile);
+		}
+		log.info("Everything finshed");
+
+	}
 	
 	
 
@@ -33,7 +118,7 @@ public class GenerateZuerrichOutput {
 	 * @param knotenSpurLinkMapping knotennummer -> (spurnummer -> linkid)
 	 * @return
 	 */
-	private static BasicLaneDefinitions processLaneDefinitions(Map<Integer, Map<Integer, List<Integer>>> spurSpurMapping, Map<Integer, Map<Integer, String>> knotenSpurLinkMapping) {
+	private BasicLaneDefinitions processLaneDefinitions(Map<Integer, Map<Integer, List<Integer>>> spurSpurMapping, Map<Integer, Map<Integer, String>> knotenSpurLinkMapping) {
 		//create the lanes ...
 		BasicLaneDefinitions laneDefs = new BasicLaneDefinitionsImpl();
 		for (Integer nodeId : spurSpurMapping.keySet()) {
@@ -77,7 +162,7 @@ public class GenerateZuerrichOutput {
 	}
 	
 	
-	private static void processSignalSystems(Map<Integer, Map<Integer, List<Integer>>> lsaSpurMapping, Map<Integer, Map<Integer, String>> laneLinkMapping, BasicSignalSystems signalSystems){
+	private void processSignalSystems(Map<Integer, Map<Integer, List<Integer>>> lsaSpurMapping, Map<Integer, Map<Integer, String>> laneLinkMapping, BasicSignalSystems signalSystems){
 		//create the signal groups
 		for (Integer nodeId : lsaSpurMapping.keySet()) {
 			Map<Integer,  List<Integer>> nodeCombos = lsaSpurMapping.get(nodeId);
@@ -119,7 +204,7 @@ public class GenerateZuerrichOutput {
 		}
 	}
 	
-	private static BasicSignalSystemConfigurations processSignalSystemConfigurations(String sgGreentime) {
+	private BasicSignalSystemConfigurations processSignalSystemConfigurations(String sgGreentime) {
 		Map<Integer, BasicSignalSystemConfiguration> basicLightSignalSystemConfiguration = GreenTimeReader.readBasicLightSignalSystemDefinition(sgGreentime);
 		BasicSignalSystemConfigurations bssc = new BasicSignalSystemConfigurationsImpl();
 		for (BasicSignalSystemConfiguration ssc : basicLightSignalSystemConfiguration.values()){
@@ -132,76 +217,6 @@ public class GenerateZuerrichOutput {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		String inputDir = "/media/data/work/scmWorkspace/shared-svn/studies/dgrether/lsaZurich/mappingNeu/";
-		String outputDir = "/media/data/work/scmWorkspace/shared-svn/studies/dgrether/signalSystemsZh/";
-		// lsa id \t cycle time
-		String lsaTu = inputDir + "lsa_tu.txt";
-//		lsa id (=node id) \t signal group id \t start red 
-		String sgGreentime = inputDir  + "sg_greentime.txt";
-		// node id \t suprnr \t linkid teleatlas \t linkid navteq \t linkid ivtch
-		String spurLinkMappingFile = inputDir + "spur_link_mapping.txt";
-		//node id \t lsanr??? \t spurnr
-		String lsaSpurMappingFile = inputDir + "lsa_spur_mapping.txt";
-		//node id \t vonspur \t nachspur
-		String spurSpurMappingFile = inputDir + "spur_spur_mapping.txt";
-		
-		String lanesOutputFile = outputDir + "laneDefinitions.xml";
-		String signalConfigOutputFile = outputDir +  "signalSystemsConfig.xml";
-		String signalSystemsOutputFile = outputDir + "signalSystems.xml";
-		
-		boolean generateLanes = true;
-		boolean generateSignalSystems = true;
-		boolean generateSignalSystemsConfig = false;
-		boolean removeDuplicates = false;
-		
-		Map<Integer, Map<Integer,  List<Integer>>> spurSpurMapping = null;
-		Map<Integer, Map<Integer, String>> spurLinkMapping = null;
-		BasicLaneDefinitions laneDefs = null;
-		if (generateLanes){
-			//knotennummer -> (vonspur 1->n nachspur)
-			spurSpurMapping = SpurSpurMappingReader.readBasicLightSignalSystemDefinition(spurSpurMappingFile);
-			//knotennummer -> (spurnummer -> linkid)
-			spurLinkMapping = new SpurLinkMappingReader().readBasicLightSignalSystemDefinition(spurLinkMappingFile);
-			
-			//create the lanes
-			laneDefs = processLaneDefinitions(spurSpurMapping, spurLinkMapping);
-			//write data
-			MatsimLaneDefinitionsWriter laneWriter = new MatsimLaneDefinitionsWriter(laneDefs);
-			laneWriter.writeFile(lanesOutputFile);
-		}
-
-		if (generateSignalSystems){
-			BasicSignalSystems signalSystems = new BasicSignalSystemsImpl();
-			//read the mappings
-			//read the system id <-> cycle time mapping
-			new LSASystemsReader(signalSystems).readBasicLightSignalSystemDefinition(lsaTu);			
-			//node id \t lsanr??? \t spurnr
-			Map<Integer, Map<Integer,  List<Integer>>> lsaSpurMapping = LSASpurMappingReader.readBasicLightSignalSystemDefinition(lsaSpurMappingFile);
-			//create the signals
-			processSignalSystems(lsaSpurMapping, spurLinkMapping, signalSystems);
-			MatsimSignalSystemsWriter signalSytemswriter = new MatsimSignalSystemsWriter(signalSystems);
-			signalSytemswriter.writeFile(signalSystemsOutputFile );
-
-		}
-
-		if (generateSignalSystemsConfig){
-			// signal system configs
-			BasicSignalSystemConfigurations signalSystemConfig = processSignalSystemConfigurations(sgGreentime);
-			MatsimSignalSystemConfigurationsWriter matsimLightSignalSystemConfigurationWriter 
-			= new MatsimSignalSystemConfigurationsWriter(signalSystemConfig);
-			matsimLightSignalSystemConfigurationWriter.writeFile(signalConfigOutputFile);
-		}
-
-		if (removeDuplicates){
-			//remove duplicates
-			RemoveDuplicates.readBasicLightSignalSystemDefinition(signalConfigOutputFile);
-			RemoveDuplicates.readBasicLightSignalSystemDefinition(signalSystemsOutputFile);
-		}
-		log.info("Everything finshed");
-
+		new GenerateZuerrichOutput();
 	}
-	
-
-
 }
