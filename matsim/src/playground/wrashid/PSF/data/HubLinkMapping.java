3375e08d914d60bfa8377f604ac49f1c0e14054d
rashid_waraich <rashid_waraich@users.sf.net>
2009-08-26 17:08:45 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8566 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/wrashid/PSF/data/HubLinkMapping.java
@@ -0,0 +1,25 @@
+package playground.wrashid.PSF.data;
+
+import java.util.HashMap;
+
+/**
+ * 
+ * This 
+ * 
+ * @author rashid_waraich
+ *
+ */
+public class HubLinkMapping {
+	
+	HashMap<String,Integer> linkHubMapping=new HashMap();
+
+	
+	/**
+	 * reads the mappings from the file. The file has columns (first column for first hub and all the links corresponding to that hub below it) 
+	 * @param fileName
+	 */
+	public void HubLinkMapping(String fileName){
+		
+	}
+	
+}
