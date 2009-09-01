a8e5285be0d8cda576b6a52991cb3a6951a15e71
rashid_waraich <rashid_waraich@users.sf.net>
2009-09-01 20:29:14 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8625 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/test/src/playground/wrashid/PSF/data/HubPriceInfoTest.java
+++ matsim/test/src/playground/wrashid/PSF/data/HubPriceInfoTest.java
@@ -3,12 +3,18 @@ package playground.wrashid.PSF.data;
 import junit.framework.TestCase;
 
 public class HubPriceInfoTest extends TestCase {
-
+// assert, that 96 entries have been read
+//
 	
-	public void test(){
-		//HubPriceInfo hubPriceInfo=new HubPriceInfo();
+	public void test1(){
+		HubPriceInfo hubPriceInfo=new HubPriceInfo("test/input/playground/wrashid/PSF/data/hublinkMapping.txt",4);
 		
+		double price=hubPriceInfo.getPrice(1.5, 0);
+		
+		//assertEquals(8.0000000e+001,price);
 		
 	}
 	
+		
 }
+ 
