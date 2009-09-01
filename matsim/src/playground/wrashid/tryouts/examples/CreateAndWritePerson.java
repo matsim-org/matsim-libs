b8d0260d2c0b8b4333f921923e9920cf740b5f3d
rashid_waraich <rashid_waraich@users.sf.net>
2009-09-01 19:57:00 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8622 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/wrashid/tryouts/examples/CreateAndWritePerson.java
@@ -0,0 +1,72 @@
+package playground.wrashid.tryouts.examples;
+
+import org.matsim.api.basic.v01.Coord;
+import org.matsim.api.basic.v01.Id;
+import org.matsim.api.basic.v01.population.BasicPopulationWriter;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.ScenarioImpl;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.network.Node;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Population;
+import org.matsim.api.core.v01.population.PopulationWriter;
+import org.matsim.core.api.experimental.events.Events;
+import org.matsim.core.api.internal.MatsimWriter;
+import org.matsim.core.network.NetworkLayer;
+
+public class CreateAndWritePerson {
+
+	/*
+	public MyMobSim(Scenario sc,Events ev){
+		Network net = sc.getNetwork();
+		Population pop=sc.getPopulation();	
+	}
+	*/
+	
+	public static void main(String[] args) {
+		createAndWritePerson();
+	}
+	
+	/*
+	 * 
+	 */
+	public static void createAndWritePerson(){
+		Scenario sc=new ScenarioImpl();
+		Id nodeId1= sc.createId("nodeId1");
+		Id nodeId2= sc.createId("nodeId2");
+		Id linkId= sc.createId("linkId");
+		Id personId = sc.createId("personId");
+		Coord coord= sc.createCoord(1.0, 1.0);
+		
+		Node node1= sc.getNetwork().getBuilder().createNode(nodeId1,coord);
+		Node node2= sc.getNetwork().getBuilder().createNode(nodeId2,coord);
+		
+		sc.getNetwork().addNode(node1);
+		sc.getNetwork().addNode(node2);
+
+		Link link= sc.getNetwork().getBuilder().createLink(linkId,nodeId1,nodeId2);
+		sc.getNetwork().addLink(link);
+		
+		Person person=sc.getPopulation().getBuilder().createPerson(personId);
+		
+		sc.getPopulation().addPerson(person);
+		
+		BasicPopulationWriter writer=new BasicPopulationWriter(sc.getPopulation()) ;
+		writer.write("abcccc.xml");
+	}
+	
+	public static void missingAPIFunctions(){
+		Scenario sc=new ScenarioImpl();
+		
+		// reader for population is missing
+		// reader, writer for population are missing
+		// facilities are missing
+		// replanning: how to add strategy module? parts still in core.
+		// events is missing
+		
+		// class module is not in api package...
+		//Module module = sc.getConfig().getModule("");
+	}
+	
+}
