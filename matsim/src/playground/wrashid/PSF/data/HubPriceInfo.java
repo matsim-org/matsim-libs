f781038fce71141b362934314c82f9804da61065
rashid_waraich <rashid_waraich@users.sf.net>
2009-09-01 20:28:27 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8624 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/wrashid/PSF/data/HubPriceInfo.java
@@ -0,0 +1,24 @@
+package playground.wrashid.PSF.data;
+
+/*
+ * - The file is split into 15 minute bins, this means we have 96 entries in the file.
+ * - numbering of hubs starts with zero
+ */
+public class HubPriceInfo {
+
+	public HubPriceInfo(String fileName, int numberOfHubs) {
+		
+	}
+
+	/**
+	 * time: time of day in seconds
+	 * hub
+	 * @param time
+	 * @param hubNumber
+	 */
+	public double getPrice(double time, int hubNumber) {
+		// TODO Auto-generated method stub
+		return  0.0;
+	}
+
+}
