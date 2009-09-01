83ee2794ad852d158a15b8ee66fcd76a4c7bcafb
rashid_waraich <rashid_waraich@users.sf.net>
2009-09-01 19:59:01 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8623 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/test/src/playground/wrashid/PSF/data/HubPriceInfoTest.java
+++ matsim/test/src/playground/wrashid/PSF/data/HubPriceInfoTest.java
@@ -2,10 +2,12 @@ package playground.wrashid.PSF.data;
 
 import junit.framework.TestCase;
 
-public class HubPriceInfo extends TestCase {
+public class HubPriceInfoTest extends TestCase {
 
 	
 	public void test(){
+		//HubPriceInfo hubPriceInfo=new HubPriceInfo();
+		
 		
 	}
 	
