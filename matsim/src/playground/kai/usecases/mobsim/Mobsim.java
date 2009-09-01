0cc883e6bb531e5bae3398f7311baad7aeff064d
vsp_nagel <2008@kainagel.org>
2009-09-01 21:55:05 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8627 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/kai/usecases/mobsim/Mobsim.java
+++ matsim/src/playground/kai/usecases/mobsim/Mobsim.java
@@ -4,23 +4,9 @@ import org.apache.log4j.Logger;
 
 import org.matsim.api.basic.v01.Id;
 import org.matsim.api.core.v01.Scenario;
-import org.matsim.core.api.experimental.events.ActivityEndEvent;
-import org.matsim.core.api.experimental.events.ActivityStartEvent;
-import org.matsim.core.api.experimental.events.AgentArrivalEvent;
-import org.matsim.core.api.experimental.events.AgentDepartureEvent;
-import org.matsim.core.api.experimental.events.AgentWait2LinkEvent;
 import org.matsim.core.api.experimental.events.Events;
 import org.matsim.core.api.experimental.events.EventsBuilder;
-import org.matsim.core.api.experimental.events.LinkEnterEvent;
-import org.matsim.core.api.experimental.events.LinkLeaveEvent;
 import org.matsim.core.basic.v01.IdImpl;
-import org.matsim.core.events.ActivityEndEventImpl;
-import org.matsim.core.events.ActivityStartEventImpl;
-import org.matsim.core.events.AgentArrivalEventImpl;
-import org.matsim.core.events.AgentDepartureEventImpl;
-import org.matsim.core.events.AgentWait2LinkEventImpl;
-import org.matsim.core.events.LinkEnterEventImpl;
-import org.matsim.core.events.LinkLeaveEventImpl;
 
 public class Mobsim {
 	private static final Logger log = Logger.getLogger(Mobsim.class);
@@ -33,20 +19,20 @@ public class Mobsim {
 		Id linkId = new IdImpl("linkId");
 		double time = 1. ;
 		
-		ActivityEndEvent aee = eb.createActivityEndEvent( time, agentId, linkId, "actType" ) ); 
-		ev.processEvent( aee ) ;
-
-		AgentDepartureEvent ade = eb.createAgentDepartureEvent( time, agentId, linkId ) ;
-
-		AgentWait2LinkEvent aw2le = eb.createAgentWait2LinkEvent(time,agentId,linkId) ;
-
-		LinkLeaveEvent lle = eb.createLinkLeaveEvent( time, agentId, linkId ) ;
-
-		LinkEnterEvent lee = eb.createLinkEnterEvent( time, agentId, linkId ) ;
-
-		AgentArrivalEvent aae = eb.createAgentArrivalEvent( time, agentId, linkId ) ;
-
-		ActivityStartEvent ase = eb.createActivityStartEvent( time, agentId, linkId, "acttype" ) ;
+//		ActivityEndEvent aee = eb.createActivityEndEvent( time, agentId, linkId, "actType" ) ); 
+//		ev.processEvent( aee ) ;
+//
+//		AgentDepartureEvent ade = eb.createAgentDepartureEvent( time, agentId, linkId ) ;
+//
+//		AgentWait2LinkEvent aw2le = eb.createAgentWait2LinkEvent(time,agentId,linkId) ;
+//
+//		LinkLeaveEvent lle = eb.createLinkLeaveEvent( time, agentId, linkId ) ;
+//
+//		LinkEnterEvent lee = eb.createLinkEnterEvent( time, agentId, linkId ) ;
+//
+//		AgentArrivalEvent aae = eb.createAgentArrivalEvent( time, agentId, linkId ) ;
+//
+//		ActivityStartEvent ase = eb.createActivityStartEvent( time, agentId, linkId, "acttype" ) ;
 
 		// TODO: None of this is behind interfaces.  Needed if we want to accept "external" mobsims.  Do we want that?
 		// If so, we would need to be sure that we want to maintain the create methods.
