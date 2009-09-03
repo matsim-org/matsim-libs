6981a7487eaba38c6907da67e84dba913c914b9f
vsp_nagel <2008@kainagel.org>
2009-09-03 07:01:11 +0000

    FacilityWRefs extends now Facility instead of BasicFacility
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8641 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/core/facilities/FacilityWRefs.java
+++ matsim/src/org/matsim/core/facilities/FacilityWRefs.java
@@ -1,9 +1,9 @@
 package org.matsim.core.facilities;
 
 import org.matsim.api.core.v01.network.Link;
-import org.matsim.core.basic.v01.facilities.BasicFacility;
+import org.matsim.core.api.experimental.facilities.Facility;
 
-public interface FacilityWRefs extends BasicFacility {
+public interface FacilityWRefs extends Facility {
 
 	public Link getLink();
 
