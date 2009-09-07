69834b7b4676c9d204857e3214422242b92a7edc
rashid_waraich <rashid_waraich@users.sf.net>
2009-09-07 06:34:08 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8669 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/wrashid/PSF/data/energyConsumption/AverageEnergyConsumptionBins.java
@@ -0,0 +1,9 @@
+package playground.wrashid.PSF.data.energyConsumption;
+
+public interface AverageEnergyConsumptionBins {
+
+	// speed: in m/s
+	// distance: in meters
+	public double getEnergyConsumption(double speed, double distance);
+
+}
