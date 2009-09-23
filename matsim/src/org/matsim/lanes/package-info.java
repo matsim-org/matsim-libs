3ccf8e86a4d7f012cf53262ef1c4bc82a93d0cd0
grether <dominik.grether@gmail.com>
2009-09-23 15:56:35 +0000

    added package info for lanes
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8817 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/org/matsim/lanes/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * This package contains classes to add lanes at the end of links.
+ *
+ * <h2>Usage restrictions:</h2>
+ * <ul>
+ *   <li> Each link's lanes must cover all toLinks of the link's toNode within the toLink information
+ *   of the lanes. </li>
+ * </ul>
+ * 
+ * <h2>Package Maintainer(s):</h2>
+ * <ul>
+ *   <li>Dominik Grether</li>
+ * </ul>
+ * 
+ * Changes by non-maintainers are prohibited.
+ */
+package org.matsim.lanes;
