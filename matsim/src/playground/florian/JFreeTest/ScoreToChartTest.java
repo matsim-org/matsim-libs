c2d5f2feee64c3f91846f39d83723a214fde2074
Flovie <Flovie@users.sf.net>
2009-09-24 10:10:24 +0000

    Removed the comments, because Dom4j is now added to the library
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8828 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/florian/JFreeTest/ScoreToChartTest.java
+++ matsim/src/playground/florian/JFreeTest/ScoreToChartTest.java
@@ -6,10 +6,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
-//import org.dom4j.Document;
-//import org.dom4j.DocumentException;
-//import org.dom4j.DocumentException;
-//import org.dom4j.io.XMLWriter;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentException;
+import org.dom4j.io.XMLWriter;
 import org.jfree.chart.JFreeChart;
 import org.jfree.ui.RefineryUtilities;
 import org.matsim.core.utils.charts.ChartUtil;
@@ -24,22 +24,22 @@ public class ScoreToChartTest{
 
 	public static JFreeChart createChartFromXMLScore(String filename) {
 		JFreeChart chart = null;
-//		JFreeTest converter = new JFreeTest("Score Statistic");	
-//		Dom4jTest dom = new Dom4jTest();
-//		
-//		try {
-//			Document originalDoc = dom.parse(filename);
-//			//style the original document using the stylesheet
-//			Document resultDoc = dom.styleDocument(originalDoc, stylesheet);
-//			//create InputStream from the transformed XML
-//			InputStream in = Dom4jTest.serializeToXMLInput(resultDoc);		
-//			//create chart
-//			chart = converter.createChartFromStream(in);
-//			
-//		} catch (DocumentException e) {
-//			e.printStackTrace();
-//		}
-//		
+		JFreeTest converter = new JFreeTest("Score Statistic");	
+		Dom4jTest dom = new Dom4jTest();
+		
+		try {
+			Document originalDoc = dom.parse(filename);
+			//style the original document using the stylesheet
+			Document resultDoc = dom.styleDocument(originalDoc, stylesheet);
+			//create InputStream from the transformed XML
+			InputStream in = Dom4jTest.serializeToXMLInput(resultDoc);		
+			//create chart
+			chart = converter.createChartFromStream(in);
+			
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+		
 		return chart;
 	}
 }
