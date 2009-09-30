13746309ba7a5ac2bd5ed7e7d40b3c9442574ef9
grether <dominik.grether@gmail.com>
2009-09-30 14:37:23 +0000

    added listeners
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@8914 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/dgrether/daganzosignal/DaganzoRunner.java
+++ matsim/src/playground/dgrether/daganzosignal/DaganzoRunner.java
@@ -20,10 +20,17 @@
 package playground.dgrether.daganzosignal;
 
 import org.apache.log4j.Logger;
+import org.matsim.core.basic.v01.IdImpl;
 import org.matsim.core.config.Config;
-import org.matsim.core.config.MatsimConfigReader;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.events.IterationEndsEvent;
+import org.matsim.core.controler.events.StartupEvent;
+import org.matsim.core.controler.listener.IterationEndsListener;
+import org.matsim.core.controler.listener.StartupListener;
 import org.matsim.run.OTFVis;
+
+import playground.dgrether.linkanalysis.TTGraphWriter;
+import playground.dgrether.linkanalysis.TTInOutflowEventHandler;
 
 
 /**
@@ -34,27 +41,49 @@ public class DaganzoRunner {
 	
 	private static final Logger log = Logger.getLogger(DaganzoRunner.class);
 	
-	private static final boolean visualizationOnly = false;
+	private TTInOutflowEventHandler handler3, handler4;
 	
-	public DaganzoRunner(){
-	}
+	public DaganzoRunner(){}
 
 	public void runScenario(){
 		DaganzoScenarioGenerator scenarioGenerator = new DaganzoScenarioGenerator();
-		Config config = null;
-		if (visualizationOnly){
-			config = new Config();
-			MatsimConfigReader reader = new MatsimConfigReader(config);
-			reader.readFile(scenarioGenerator.configOut);
-		}
-		else {
-			Controler controler = new Controler(scenarioGenerator.configOut);
-			controler.setOverwriteFiles(true);
-			config = controler.getConfig();
-			controler.run();
-		}
+		Controler controler = new Controler(scenarioGenerator.configOut);
+		controler.setOverwriteFiles(true);
+		Config config = controler.getConfig();
 		
-		//Visualize
+		this.addListener(controler);
+		
+		controler.run();
+		this.startVisualizer(config);
+	}
+	
+	private void addListener(Controler c) {
+//		handler3 = new TTInOutflowEventHandler(new IdImpl("3"), new IdImpl("5"));
+		handler4 = new TTInOutflowEventHandler(new IdImpl("4"));
+		c.addControlerListener(new StartupListener() {
+			public void notifyStartup(StartupEvent e) {
+//				e.getControler().getEvents().addHandler(handler3);
+				e.getControler().getEvents().addHandler(handler4);
+			}});
+
+		
+		c.addControlerListener(new IterationEndsListener() {
+			public void notifyIterationEnds(IterationEndsEvent e) {
+				TTGraphWriter ttWriter = new TTGraphWriter();
+//				ttWriter.addTTEventHandler(handler3);
+				ttWriter.addTTEventHandler(handler4);
+				ttWriter.writeTTChart(e.getControler().getIterationPath(e.getIteration()), e.getIteration());
+				
+//				InOutGraphWriter inoutWriter = new InOutGraphWriter();
+//				inoutWriter.addInOutEventHandler(handler3);
+//				inoutWriter.addInOutEventHandler(handler4);
+//				inoutWriter.writeInOutChart(e.getControler().getIterationPath(event.getIteration()), event.getIteration());
+			}
+		});
+	}
+	
+	
+	private void startVisualizer(Config config) {
 		String[] args = {config.controler().getOutputDirectory() + 
 				"/ITERS/it." + config.controler().getLastIteration() + 
 				"/" + config.controler().getLastIteration() + ".otfvis.mvi"};
