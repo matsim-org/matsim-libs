ab2081fce8b3704ab0f2e3c2ae76ec491153ec74
ciarif <ciarif@users.sf.net>
2009-10-10 15:17:07 +0000

    small changes in CarPooling
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@9045 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/ciarif/carpooling/CarPoolingControler.java
+++ matsim/src/playground/ciarif/carpooling/CarPoolingControler.java
@@ -4,8 +4,8 @@ import org.matsim.core.controler.Controler;
 
 import playground.meisterk.kti.controler.KTIControler;
 
-//public class CarPoolingControler extends KTIControler {
-public class CarPoolingControler extends Controler {
+public class CarPoolingControler extends KTIControler {
+//public class CarPoolingControler extends Controler {
 	
 	public CarPoolingControler (String[] args){
 		super(args);
