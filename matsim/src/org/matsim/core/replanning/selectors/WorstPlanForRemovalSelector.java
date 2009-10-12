b48416670b2203bc815f1512766f953b1b69e202
vsp_nagel <2008@kainagel.org>
2009-10-12 16:24:36 +0000

    added explanatory comments
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@9061 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/core/replanning/selectors/WorstPlanForRemovalSelector.java
+++ matsim/src/org/matsim/core/replanning/selectors/WorstPlanForRemovalSelector.java
@@ -26,18 +26,24 @@ import org.matsim.core.population.PersonImpl;
 import org.matsim.core.population.PlanImpl;
 
 /**
- * Selects the worst plan of a person (most likely for removal), but respects
+ * <p>Selects the worst plan of a person (most likely for removal), but respects
  * the set plan types in a way the no plan is selected that is the last one of
- * its type. Plans without a score are seen as worst and selected accordingly.
+ * its type.</p> 
+ * <p>(I would say that it can select the last of its type if it finds nothing else.  However, this algo should only
+ * be used if an agent has more plans than maxPlansPerAgent, so make sure that that parameter is set large enough for
+ * your purposes.  kai, oct'09)</p>
+ * <p>Plans without a score are seen as worst and selected accordingly.</p>
  *
  * @author mrieser
  */
 public class WorstPlanForRemovalSelector implements PlanSelector {
 
 	public PlanImpl selectPlan(PersonImpl person) {
-		
+
+		// hashmap that returns "Integer" count for given plans type:
 		HashMap<PlanImpl.Type, Integer> typeCounts = new HashMap<PlanImpl.Type, Integer>();
-		// initialize list of types
+
+		// count how many plans per type an agent has:
 		for (PlanImpl plan : person.getPlans()) {
 			Integer cnt = typeCounts.get(plan.getType());
 			if (cnt == null) {
@@ -46,20 +52,31 @@ public class WorstPlanForRemovalSelector implements PlanSelector {
 				typeCounts.put(plan.getType(), Integer.valueOf(cnt.intValue() + 1));
 			}
 		}
-			PlanImpl worst = null;
-			double worstScore = Double.POSITIVE_INFINITY;
-			for (PlanImpl plan : person.getPlans()) {
-				if (typeCounts.get(plan.getType()).intValue() > 1) {
-					if (plan.getScore() == null) {
-						worst = plan;
-						// make sure no other score could be less than this
-						worstScore = Double.NEGATIVE_INFINITY;
-					} else if (plan.getScore().doubleValue() < worstScore) {
-						worst = plan;
-						worstScore = plan.getScore().doubleValue();
-					}
+
+		PlanImpl worst = null;
+		double worstScore = Double.POSITIVE_INFINITY;
+		for (PlanImpl plan : person.getPlans()) {
+
+			// if we have more than one plan of the same type:
+			if (typeCounts.get(plan.getType()).intValue() > 1) {
+
+				// if there is a plan without score:
+				if (plan.getScore() == null) {
+					// say that the plan without score now is the "worst":
+					worst = plan;
+
+					// make sure that this one remains the selected plan:
+					worstScore = Double.NEGATIVE_INFINITY;
+
+				// otherwise do the usual logic to find the plan with the minimum score:
+				} else if (plan.getScore().doubleValue() < worstScore) {
+					worst = plan;
+					worstScore = plan.getScore().doubleValue();
 				}
 			}
+			
+			// (otherwise we just keep "worst=null") 
+		}
 
 		if (worst == null) {
 			// there is exactly one plan, or we have of each plan-type exactly one.
