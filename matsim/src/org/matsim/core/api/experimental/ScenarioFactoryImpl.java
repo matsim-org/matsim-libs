de9a447f538771e4fd916741b54d3fdafa9824e4
vsp_nagel <2008@kainagel.org>
2009-10-14 17:12:08 +0000

    suggesting ScenarioFactoryImpl, ScenarioLoaderFactoryImpl, and a ScenarioLoader Interface (not correctly named)
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@9085 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/org/matsim/core/api/experimental/ScenarioFactoryImpl.java
@@ -0,0 +1,42 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * Controler.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.core.api.experimental;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.ScenarioImpl;
+import org.matsim.core.config.Config;
+
+/**
+ * @author nagel
+ *
+ * @deprecated this is, as of now, a _proposal_.  
+ */
+public class ScenarioFactoryImpl {
+	
+	public Scenario createScenario() {
+		return new ScenarioImpl() ;
+	}
+	
+	public Scenario createScenario( Config config ) {
+		return new ScenarioImpl( config ) ;
+	}
+
+}
