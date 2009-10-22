2e9def6718aae6c85b051f190e7df26db6f1543c
vsp_nagel <2008@kainagel.org>
2009-10-22 19:38:13 +0000

    added a comment
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@9174 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/api/core/v01/network/Link.java
+++ matsim/src/org/matsim/api/core/v01/network/Link.java
@@ -3,6 +3,12 @@ package org.matsim.api.core.v01.network;
 import org.matsim.api.basic.v01.network.BasicLink;
 import org.matsim.world.Location;
 
+/**
+ * This interface deliberately does NOT have a back pointer ...
+ * ... since, at this level, one should be able to get the relevant container from
+ * the context.
+ * (This becomes clear if you think about a nodeId/linkId given by person.)
+ */
 public interface Link extends BasicLink, Location {
 	
 	/**
@@ -14,5 +20,5 @@ public interface Link extends BasicLink, Location {
 	 * @return this link's upstream node
 	 */
 	public Node getFromNode();
-
+	
 }
