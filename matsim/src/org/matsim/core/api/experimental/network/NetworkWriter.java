a5ae980543529b730def514bc009f96a66a26e64
vsp_nagel <2008@kainagel.org>
2009-10-22 19:41:07 +0000

    added a NetworkWriter class into the "experimental" api
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@9178 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/org/matsim/core/api/experimental/network/NetworkWriter.java
@@ -0,0 +1,55 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * Controler.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.core.api.experimental.network;
+
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.core.api.internal.MatsimWriter;
+
+/**
+ * @author nagel
+ *
+ */
+public class NetworkWriter implements MatsimWriter {
+	// this really is essentially a factory. :-)
+
+	private final Network network ;
+	
+	public NetworkWriter(final Network network) {
+		this.network = network ;
+	}
+	
+	/**
+	 * Writes the network in the current default format (currently network_v1.dtd). 
+	 */
+	public void write(final String filename) {
+		writeV1(filename);
+	}
+		
+	/**
+	 * Writes the network in the format of network_v1.dtd
+	 * 
+	 * @param filename
+	 */
+	public void writeV1(final String filename) {
+		new org.matsim.core.network.NetworkWriter(network, filename).write();
+	}
+	
+}
