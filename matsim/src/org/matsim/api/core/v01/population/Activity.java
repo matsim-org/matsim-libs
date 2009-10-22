dab0e503aeb07db3daf26bcbfb285f25d62beaf3
vsp_nagel <2008@kainagel.org>
2009-10-22 19:39:01 +0000

    added a comment
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@9176 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/api/core/v01/population/Activity.java
+++ matsim/src/org/matsim/api/core/v01/population/Activity.java
@@ -3,6 +3,15 @@ package org.matsim.api.core.v01.population;
 import org.matsim.api.basic.v01.population.BasicActivity;
 import org.matsim.api.basic.v01.population.PlanElement;
 
+/**
+ * <p> Regarding "duration vs. end time":
+ * <li> check if run693 (w/o duration) is similar to other runs </li>
+ * <li> ask (per email) if moving to a design where we use <em> either </em> duration <em> or </em> end time
+ *      would be ok (if both are encountered, duration would be ignored, and also not written) </li>
+ * <li> only after these things are fixed, "duration" could be added to interface </li>
+ * </p>
+ *
+ */
 public interface Activity extends BasicActivity, PlanElement
 //, BasicLocation 
 {
