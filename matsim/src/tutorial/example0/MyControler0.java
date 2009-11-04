612f0698df540d4ddc12a0e355939f0339eeab68
vsp_nagel <2008@kainagel.org>
2009-11-04 20:03:35 +0000

    Example for a tutorial starting point based on the controler
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@9317 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/tutorial/example0/MyControler0.java
@@ -0,0 +1,18 @@
+package tutorial.example0;
+
+import org.matsim.run.Controler;
+
+public class MyControler0 {
+	
+	public static void main ( String[] args ) {
+	
+		String configFile = "examples/tutorial/myConfig.xml" ;
+		
+		Controler controler = new Controler( configFile ) ;
+		
+		controler.setOverwriteFiles(true) ;
+		controler.run() ;
+		
+	}
+
+}
