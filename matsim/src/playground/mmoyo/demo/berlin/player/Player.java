7a5e9fa17ad76b1925a7089dd6ddcad16130bfee
mmoyo <mmoyo@users.sf.net>
2009-11-04 18:29:03 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@9312 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/mmoyo/demo/berlin/player/Player.java
@@ -0,0 +1,17 @@
+package playground.mmoyo.demo.berlin.simplePlan1;
+
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+import playground.mmoyo.demo.ScenarioPlayer;
+
+public class Player {
+
+	public static void main(String[] args) throws SAXException, ParserConfigurationException, IOException {
+		//invoke controler first
+		
+		String configFile = "src/playground/mmoyo/demo/berlin/simplePlan1/config.xml";
+		String scheduleFile =  "../shared-svn/studies/countries/de/berlin-bvg09/pt/baseplan_900s_smallnetwork/transitSchedule.networkOevModellBln.xml.gz";
+		ScenarioPlayer.main(new String[]{configFile, scheduleFile});
+	}
+}
