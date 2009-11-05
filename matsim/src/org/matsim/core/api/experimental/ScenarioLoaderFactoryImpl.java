828b40ab15b4b1400e073803fa531f24c0157f36
vsp_nagel <2008@kainagel.org>
2009-11-05 08:44:00 +0000

    removed
      createScenarioLoader( configFileName, scenario)
      
    from the interface
    
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@9320 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/core/api/experimental/ScenarioLoaderFactoryImpl.java
+++ matsim/src/org/matsim/core/api/experimental/ScenarioLoaderFactoryImpl.java
@@ -1,16 +1,21 @@
 package org.matsim.core.api.experimental;
 
-import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.scenario.ScenarioLoader;
 
+/**<p>
+ * Factory for the ScenarioLoader (what a surprise).
+ * </p><p>
+ * Note: There is deliberately NO constructor that takes a Scenario argument to avoid confusion what to do
+ * with Scenario containers that already contain information.
+ */
 public class ScenarioLoaderFactoryImpl {
 
 	public ScenarioLoaderI createScenarioLoader(String configFileName) {
 		return new ScenarioLoader( configFileName ) ;
 	}
 
-	public ScenarioLoaderI createScenarioLoader(String configFileName, Scenario scenario) {
-		return new ScenarioLoader(configFileName, scenario);
-	}
+//	public ScenarioLoaderI createScenarioLoader(String configFileName, Scenario scenario) {
+//		return new ScenarioLoader(configFileName, scenario);
+//	}
 
 }
