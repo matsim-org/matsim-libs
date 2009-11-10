394ec1cd451ca0297d86ec84f1f088e534cf0837
mmoyo <mmoyo@users.sf.net>
2009-11-10 19:13:16 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@9344 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/mmoyo/demo/X5/transfer/SimplePlanPlayer2.java
+++ matsim/src/playground/mmoyo/demo/X5/transfer/SimplePlanPlayer2.java
@@ -1,4 +1,4 @@
-package playground.mmoyo.demo.X5.simplePlan2;
+package playground.mmoyo.demo.X5.transfer;
 
 import java.io.IOException;
 import javax.xml.parsers.ParserConfigurationException;
@@ -9,8 +9,8 @@ public class SimplePlanPlayer2 {
 	public static void main(String[] args) throws SAXException, ParserConfigurationException, IOException {
 		//invoke controler first
 		
-		String configFile = "src/playground/mmoyo/demo/X5/simplePlan2/config.xml";
-		String scheduleFile =  "src/playground/mmoyo/demo/X5/simplePlan2/simple1TransitSchedule.xml";
+		String configFile = "src/playground/mmoyo/demo/X5/transfer/config.xml";
+		String scheduleFile =  "src/playground/mmoyo/demo/X5/transfer/simple1TransitSchedule.xml";
 		ScenarioPlayer.main(new String[]{configFile, scheduleFile});
 	}
 
