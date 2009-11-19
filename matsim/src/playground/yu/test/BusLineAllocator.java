bbbb54d5c2b27a02e61c5af143f67133ed649cf2
yu_vsp <yu_vsp@users.sf.net>
2009-11-19 18:19:33 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@9435 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/yu/test/BusLineAllocator.java
+++ matsim/src/playground/yu/test/BusLineAllocator.java
@@ -27,6 +27,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -36,14 +37,23 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.matsim.api.basic.v01.Coord;
 import org.matsim.api.basic.v01.Id;
+import org.matsim.api.basic.v01.TransportMode;
 import org.matsim.api.core.v01.ScenarioImpl;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Node;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Plan;
+import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.core.network.LinkImpl;
 import org.matsim.core.network.MatsimNetworkReader;
 import org.matsim.core.network.NetworkImpl;
 import org.matsim.core.network.NetworkLayer;
 import org.matsim.core.network.NetworkWriter;
+import org.matsim.core.population.ActivityImpl;
+import org.matsim.core.population.MatsimPopulationReader;
+import org.matsim.core.population.PopulationImpl;
+import org.matsim.core.population.PopulationWriter;
+import org.matsim.core.population.routes.LinkNetworkRouteImpl;
 import org.matsim.core.population.routes.NetworkRouteWRefs;
 import org.matsim.core.router.Dijkstra;
 import org.matsim.core.router.util.TravelCost;
@@ -54,6 +64,8 @@ import org.matsim.transitSchedule.api.TransitLine;
 import org.matsim.transitSchedule.api.TransitRoute;
 import org.matsim.transitSchedule.api.TransitSchedule;
 import org.matsim.transitSchedule.api.TransitScheduleReader;
+import org.matsim.transitSchedule.api.TransitScheduleWriter;
+import org.matsim.transitSchedule.api.TransitStopFacility;
 import org.xml.sax.SAXException;
 
 import playground.yu.utils.io.SimpleWriter;
@@ -79,7 +91,7 @@ public class BusLineAllocator {
 		}
 	}
 
-	private NetworkImpl carNetwork;
+	private static NetworkImpl carNetwork;
 	private Map<Id, List<Tuple<Link, Tuple<Coord, Coord>>>> coordPairs;// <ptRouteId,<ptLink<fromNodeCoord,toNodeCoord>>>
 	private Map<Id, List<Tuple<String, Tuple<Coord, Coord>>>> coordPairs4rectification;// <ptRouteId,<ptLinkId:next_ptLinkId<fromNodeCoord,toNodeCoord>>>
 	private Map<Id, List<Tuple<Id, Path>>> resultPaths = new HashMap<Id, List<Tuple<Id, Path>>>();// <ptRouteId,List<ptLinkId,Path(linkId
@@ -91,10 +103,13 @@ public class BusLineAllocator {
 	private Link tmpPtLink = null;
 	private Id tmpPtRouteId = null;
 	private static Set<Link> startLinks = new HashSet<Link>();
+	private static Set<Link> nullLinks = new HashSet<Link>();
 	private Map<Link, Node> startLinksNewToNodes = new HashMap<Link, Node>();
+	private TransitSchedule schedule;
+	private NetworkLayer multiModalNetwork;
 
 	/**
-	 * @param network
+	 * @param carNetwork
 	 *            a nomral MATsim <code>NetworkImpl</code>, in which there
 	 *            aren't "pt" links
 	 * @param stopCoords
@@ -103,17 +118,17 @@ public class BusLineAllocator {
 	 * @param outputFile
 	 *            file path of the output file
 	 */
-	public BusLineAllocator(
-			NetworkImpl network,
-			Map<Id, List<Tuple<Link, Tuple<Coord, Coord>>>> coordPairs,
-			Map<Id, List<Tuple<String, Tuple<Coord, Coord>>>> coordPairs4rectification,
+	public BusLineAllocator(NetworkImpl carNetwork,
+			NetworkLayer multiModalNetwork, TransitSchedule schedule,
 			String outputFile) {
-		this.carNetwork = network;
-		this.coordPairs = coordPairs;
-		this.coordPairs4rectification = coordPairs4rectification;
+		this.carNetwork = carNetwork;
+		this.multiModalNetwork = multiModalNetwork;
+		this.schedule = schedule;
+		this.coordPairs = createCoordPairs(schedule);
+		this.coordPairs4rectification = createCoordPairs4rectification(schedule);
 		this.outputFile = outputFile;
-		this.dijkstra = new Dijkstra(network, new TravelCostFunctionDistance(),
-				new TravelTimeFunctionFree());
+		this.dijkstra = new Dijkstra(carNetwork,
+				new TravelCostFunctionDistance(), new TravelTimeFunctionFree());
 	}
 
 	protected void allocateAllRouteLinks() {
@@ -208,7 +223,7 @@ public class BusLineAllocator {
 	 *         >>>>
 	 */
 	private static Map<Id, List<Tuple<Link, Tuple<Coord, Coord>>>> createCoordPairs(
-			NetworkLayer ptNet, TransitSchedule schedule) {
+			TransitSchedule schedule) {
 		Map<Id, List<Tuple<Link, Tuple<Coord, Coord>>>> coordPairs = new HashMap<Id, List<Tuple<Link, Tuple<Coord, Coord>>>>();
 		for (TransitLine ptLine : schedule.getTransitLines().values()) {
 			for (TransitRoute ptRoute : ptLine.getRoutes().values()) {
@@ -219,8 +234,8 @@ public class BusLineAllocator {
 					ptLinkCoordPairs = new ArrayList<Tuple<Link, Tuple<Coord, Coord>>>();
 
 				NetworkRouteWRefs route = ptRoute.getRoute();
-				Link startLink = route.getStartLink();
 
+				Link startLink = route.getStartLink();
 				if (startLink.getLength() > 0) {
 					ptLinkCoordPairs.add(new Tuple<Link, Tuple<Coord, Coord>>(
 							startLink, new Tuple<Coord, Coord>(startLink
@@ -230,10 +245,12 @@ public class BusLineAllocator {
 					startLinks.add(startLink);
 					System.out.println("ptLink :\t" + startLinkId
 							+ "\tis the first link of a route.");
-				} else
+				} else {
 					System.err.println("ptLink : " + startLink.getId()
 							+ "\thas a length of\t" + startLink.getLength()
 							+ "\t[m]");
+					nullLinks.add(startLink);
+				}
 
 				for (Link link : route.getLinks()) {
 					if (link.getLength() > 0)
@@ -242,10 +259,12 @@ public class BusLineAllocator {
 										new Tuple<Coord, Coord>(link
 												.getFromNode().getCoord(), link
 												.getToNode().getCoord())));
-					else
+					else {
 						System.err.println("ptLink : " + link.getId()
 								+ "\thas a length of\t" + link.getLength()
 								+ "\t[m]");
+						nullLinks.add(link);
+					}
 				}
 
 				Link endLink = route.getEndLink();
@@ -254,11 +273,12 @@ public class BusLineAllocator {
 							endLink, new Tuple<Coord, Coord>(endLink
 									.getFromNode().getCoord(), endLink
 									.getToNode().getCoord())));
-				else
+				else {
 					System.err.println("ptLink : " + endLink.getId()
 							+ "\thas a length of\t" + endLink.getLength()
 							+ "\t[m]");
-
+					nullLinks.add(endLink);
+				}
 				coordPairs.put(ptRouteId, ptLinkCoordPairs);
 			}
 		}
@@ -272,7 +292,7 @@ public class BusLineAllocator {
 	 * @return
 	 */
 	private static Map<Id, List<Tuple<String, Tuple<Coord, Coord>>>> createCoordPairs4rectification(
-			NetworkLayer ptNet, TransitSchedule schedule) {
+			TransitSchedule schedule) {
 		Map<Id, List<Tuple<String, Tuple<Coord, Coord>>>> coordPairs4rectification = new HashMap<Id, List<Tuple<String, Tuple<Coord, Coord>>>>();
 		for (TransitLine ptLine : schedule.getTransitLines().values()) {
 			for (TransitRoute ptRoute : ptLine.getRoutes().values()) {
@@ -397,27 +417,131 @@ public class BusLineAllocator {
 		output();
 	}
 
-	private void generateNewFiles() {
-		generateNewNetwork(null);
-		generateNewPlans();
-		generateNewSchedule();
+	// private void generateNewFiles() {
+	// generateNewNetwork(null);
+	// generateNewPlans();
+	// generateNewSchedule();
+	// }
+
+	private void generateNewSchedule(String newTransitScheduleFilename) {
+		Map<Id, List<Link>> ptLinkIdLinks = convertResult();
+		Set<TransitStopFacility> stops = new HashSet<TransitStopFacility>();
+		stops.addAll(schedule.getFacilities().values());
+
+		for (TransitStopFacility stop : stops) {
+			List<Link> links = ptLinkIdLinks.get(stop.getLinkId());
+			if (links != null)
+				if (links.size() > 0)
+					stop.setLink(links.get(links.size() - 1));
+		}
+
+		Map<Id, NetworkRouteWRefs> ptRouteIdRoutes = convertResult2();
+		for (TransitLine ptLine : schedule.getTransitLines().values())
+			for (Entry<Id, TransitRoute> ptRouteIdRoutePair : ptLine
+					.getRoutes().entrySet())
+				ptRouteIdRoutePair.getValue().setRoute(
+						ptRouteIdRoutes.get(ptRouteIdRoutePair.getKey()));
+
+		try {
+			new TransitScheduleWriter(schedule)
+					.writeFile(newTransitScheduleFilename);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	private void generateNewSchedule() {
-		// TODO Auto-generated method stub
-
+	private Map<Id, List<Link>> convertResult() {
+		Map<Id, List<Link>> ptLinkIdPathMap = new HashMap<Id, List<Link>>();
+		for (List<Tuple<Id, Path>> ptLinkIdPathlist : resultPaths.values())
+			for (Tuple<Id, Path> ptLinkIdPathPair : ptLinkIdPathlist) {
+				Id ptLinkId = ptLinkIdPathPair.getFirst();
+				Link ptLink = multiModalNetwork.getLink(ptLinkId);
+				if (!startLinksNewToNodes.keySet().contains(ptLink))
+					ptLinkIdPathMap.put(ptLinkId,
+							ptLinkIdPathPair.getSecond().links);
+				else {
+					LinkedList<Link> links = new LinkedList<Link>();
+					links.add(ptLink);
+					ptLinkIdPathMap.put(ptLinkId, links);
+				}
+			}
+		return ptLinkIdPathMap;
 	}
 
-	private void generateNewPlans() {
-		// TODO Auto-generated method stub
+	/**
+	 * @return Map<ptRouteId,Route of a TransitRoute>
+	 */
+	private Map<Id, NetworkRouteWRefs> convertResult2() {
+		Map<Id, NetworkRouteWRefs> ptRouteIdRoutes = new HashMap<Id, NetworkRouteWRefs>();
+		for (Entry<Id, List<Tuple<Id, Path>>> ptRouteIdLinkPathPair : resultPaths
+				.entrySet()) {
+			Id ptRouteId = ptRouteIdLinkPathPair.getKey();
+			List<Tuple<Id, Path>> ptLinksIdPaths = ptRouteIdLinkPathPair
+					.getValue();
+			Link startLink = null, endLink = null;
+			LinkedList<Link> routeLinks = new LinkedList<Link>();
+			int size = ptLinksIdPaths.size();
+			for (int i = 0; i < size; i++) {
+				Tuple<Id, Path> ptLinkIdPathPair = ptLinksIdPaths.get(i);
+				if (i != 0)
+					routeLinks.addAll(ptLinkIdPathPair.getSecond().links);
+				else {
+					Link ptLink = multiModalNetwork.getLink(ptLinkIdPathPair
+							.getFirst());
+					if (startLinksNewToNodes.keySet().contains(ptLink))
+						startLink = ptLink;
+					else {
+						List<Link> linkList = ptLinkIdPathPair.getSecond().links;
+						startLink = linkList.remove(0);
+						routeLinks.addAll(linkList);
+					}
+				}
+			}
+			endLink = routeLinks.removeLast();
+			NetworkRouteWRefs route = new LinkNetworkRouteImpl(startLink,
+					endLink);
+			route.setLinks(startLink, routeLinks, endLink);
+			ptRouteIdRoutes.put(ptRouteId, route);
+		}
+		return ptRouteIdRoutes;
+	}
 
+	private void generateNewPlans(PopulationImpl pop, String newPopFile) {
+		Map<Id, List<Link>> ptLinkIdCarLinks = convertResult();
+		for (Person person : pop.getPersons().values()) {
+			for (Plan plan : person.getPlans()) {
+				List<PlanElement> pes = plan.getPlanElements();
+				for (int i = 0; i < pes.size(); i += 2) {
+					ActivityImpl act = (ActivityImpl) pes.get(i);
+					Id linkId = act.getLinkId();
+					if (act.getType().equals("pt interaction")
+							&& linkId.toString().startsWith("tr_")) {
+						List<Link> links = ptLinkIdCarLinks.get(linkId);
+						act.setLink(links.get(links.size() - 1));
+					}
+				}
+			}
+		}
+		new PopulationWriter(pop, newPopFile).write();
 	}
 
 	private void generateNewNetwork(String newNetFilename) {
-		for (Link link : startLinks) {
+		Set<TransportMode> modes = new HashSet<TransportMode>();
+		modes.add(TransportMode.car);
+		modes.add(TransportMode.pt);
+		for (Link link : startLinksNewToNodes.keySet()) {
 			link.setToNode(startLinksNewToNodes.get(link));
-			carNetwork.addNode(link.getFromNode());
+			link.getFromNode().getOutLinks().clear();
+			link.setAllowedModes(modes);
 			carNetwork.addLink(link);
+			carNetwork.addNode(link.getFromNode());
+		}
+		for (Link link : nullLinks) {
+			link.getFromNode().getOutLinks().clear();
+			link.getToNode().getInLinks().clear();
+			link.setAllowedModes(modes);
+			carNetwork.addLink(link);
+			carNetwork.addNode(link.getFromNode());
 		}
 		new NetworkWriter(carNetwork, newNetFilename).write();
 	}
@@ -426,7 +550,15 @@ public class BusLineAllocator {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String multiModalNetworkFile = "../berlin-bvg09/pt/baseplan_900s_smallnetwork/test/network.multimodal.mini.xml", transitScheduleFile = "../berlin-bvg09/pt/baseplan_900s_smallnetwork/test/scheduleTest.xml", carNetworkFile = "../berlin-bvg09/pt/baseplan_900s_smallnetwork/test/network.car.mini.xml", outputFile = "../berlin-bvg09/pt/baseplan_900s_smallnetwork/test/busLineAllocation.txt";
+		String multiModalNetworkFile = "../berlin-bvg09/pt/baseplan_900s_smallnetwork/test/network.multimodal.mini.xml";
+		String transitScheduleFile = "../berlin-bvg09/pt/baseplan_900s_smallnetwork/test/transitSchedule.networkOevModellBln.xml";
+		String carNetworkFile = "../berlin-bvg09/pt/baseplan_900s_smallnetwork/test/network.car.mini.xml";
+		String outputFile = "../berlin-bvg09/pt/baseplan_900s_smallnetwork/test/busLineAllocation.txt";
+		String popFile = "../berlin-bvg09/pt/baseplan_900s_smallnetwork/test/plan.routedOevModell.BVB344.moreLegPlan_Agent.xml";
+
+		String newNetworkFile = "../berlin-bvg09/pt/baseplan_900s_smallnetwork/test/newNetTest.xml";
+		String newTransitScheduleFile = "../berlin-bvg09/pt/baseplan_900s_smallnetwork/test/newSchedule.xml";
+		String newPopFile = "../berlin-bvg09/pt/baseplan_900s_smallnetwork/test/newPopTest.xml";
 
 		ScenarioImpl scenario = new ScenarioImpl();
 		scenario.getConfig().scenario().setUseTransit(true);
@@ -449,10 +581,14 @@ public class BusLineAllocator {
 		NetworkLayer carNetwork = new NetworkLayer();
 		new MatsimNetworkReader(carNetwork).readFile(carNetworkFile);
 
-		BusLineAllocator BusLineAllocator = new BusLineAllocator(carNetwork,
-				createCoordPairs(multiModalNetwork, schedule),
-				createCoordPairs4rectification(multiModalNetwork, schedule),
-				outputFile);
-		BusLineAllocator.run();
+		BusLineAllocator busLineAllocator = new BusLineAllocator(carNetwork,
+				multiModalNetwork, schedule, outputFile);
+		busLineAllocator.run();
+		busLineAllocator.generateNewNetwork(newNetworkFile);
+		busLineAllocator.generateNewSchedule(newTransitScheduleFile);
+
+		PopulationImpl pop = scenario.getPopulation();
+		new MatsimPopulationReader(pop, multiModalNetwork).readFile(popFile);
+		busLineAllocator.generateNewPlans(pop, newPopFile);
 	}
 }
