78a776f72340256f62937f433f7fed9ec6987048
grether <dominik.grether@gmail.com>
2009-11-23 16:37:49 +0000

    io for controler
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@9455 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/playground/dgrether/ControlerIo.java
@@ -0,0 +1,103 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * ControlerIo
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2009 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.dgrether;
+
+import org.matsim.api.basic.v01.Id;
+/**
+ * @author dgrether
+ *
+ */
+public class ControlerIo {
+	
+	private static final String DIRECTORY_ITERS = "ITERS";
+	
+	private Id runId = null;
+	
+	private String outputPath;
+	
+	public ControlerIo(String outputDirectory){
+		this.outputPath = outputDirectory;
+	}
+	
+	public ControlerIo(String outputDirectory, Id runId){
+		this(outputDirectory);
+		this.runId = runId;	
+	}
+		
+	/**
+	 * Returns the path to a directory where temporary files can be stored.
+	 *
+	 * @return path to a temp-directory.
+	 */
+	public final String getTempPath() {
+		return outputPath + "/tmp";
+	}
+
+	/**
+	 * Returns the path to the specified iteration directory. The directory path
+	 * does not include the trailing '/'.
+	 *
+	 * @param iteration
+	 *            the iteration the path to should be returned
+	 * @return path to the specified iteration directory
+	 */
+	public final String getIterationPath(final int iteration) {
+		return outputPath + "/" + DIRECTORY_ITERS + "/it." + iteration;
+	}
+
+	/**
+	 * Returns the complete filename to access an iteration-file with the given
+	 * basename.
+	 *
+	 * @param filename
+	 *            the basename of the file to access
+	 * @return complete path and filename to a file, if set prefixed with the runId, in a iteration directory
+	 */
+	public final String getIterationFilename(final int iteration, final String filename) {
+		StringBuilder s = new StringBuilder(getIterationPath(iteration));
+		s.append('/');
+		if (runId != null) {
+			s.append(runId);
+			s.append('.');
+		}
+		s.append(iteration);
+		s.append(".");
+		s.append(filename);
+		return s.toString();
+	}
+	
+	/**
+	 * Returns the complete filename to access a file in the output-directory.
+	 *
+	 * @param filename
+	 *            the basename of the file to access
+	 * @return complete path and filename to a file, if set prefixed with the runId,  in the output-directory
+	 */
+	public final String getOutputFilename(final String filename) {
+		StringBuilder s = new StringBuilder(outputPath);
+		s.append('/');
+		if (runId != null) {
+			s.append(runId);
+			s.append('.');
+		}
+		s.append(filename);
+		return s.toString();
+	}
+}
