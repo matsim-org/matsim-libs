66d97b411a9b7410949a1f670f3186a6f2f952ba
rashid_waraich <rashid_waraich@users.sf.net>
2009-11-26 17:50:57 +0000

    test reordering.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@9494 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/test/src/org/matsim/core/mobsim/jdeqsim/AllTests.java
+++ matsim/test/src/org/matsim/core/mobsim/jdeqsim/AllTests.java
@@ -29,6 +29,7 @@ public class AllTests {
 		TestSuite suite = new TestSuite("Tests for " + AllTests.class.getPackage().getName());
 		//$JUnit-BEGIN$
 		
+		suite.addTestSuite(ConfigParameterTest.class);
 		
 		suite.addTestSuite(EmptyCarLegTest.class);
 		suite.addTestSuite(NonCarLegTest.class);
@@ -43,10 +44,6 @@ public class AllTests {
 		suite.addTestSuite(TestMessageFactory.class);
 		suite.addTestSuite(TestMessageQueue.class);
 		suite.addTestSuite(TestScheduler.class);
-		
-		// need to put this test at the end, because somehow this effects the outcome of the controler
-		// tests
-		suite.addTestSuite(ConfigParameterTest.class);
 		
 		suite.addTest(org.matsim.core.mobsim.jdeqsim.util.AllTests.suite());
 		//$JUnit-END$
