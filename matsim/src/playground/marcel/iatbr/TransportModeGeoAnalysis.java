caf227208533caf5321966322bde2da2d38b45c0
mrieser <rieser@gmail.com>
2009-11-29 14:15:28 +0000

    analyses for iatbr and phd, respectively.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@9513 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/playground/marcel/iatbr/TransportModeGeoAnalysis.java
+++ matsim/src/playground/marcel/iatbr/TransportModeGeoAnalysis.java
@@ -22,10 +22,14 @@ package playground.marcel.iatbr;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.log4j.Logger;
+import org.geotools.data.FeatureSource;
+import org.geotools.feature.Feature;
+import org.matsim.api.basic.v01.Coord;
 import org.matsim.api.core.v01.ScenarioImpl;
 import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Leg;
@@ -33,18 +37,53 @@ import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.core.network.MatsimNetworkReader;
 import org.matsim.core.population.MatsimPopulationReader;
+import org.matsim.core.utils.gis.ShapeFileReader;
 import org.matsim.core.utils.io.IOUtils;
 import org.matsim.population.algorithms.AbstractPersonAlgorithm;
 import org.xml.sax.SAXException;
 
-public class TransportModeAnalysis extends AbstractPersonAlgorithm {
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
+
+public class TransportModeGeoAnalysis extends AbstractPersonAlgorithm {
 
 	private final BufferedWriter out;
+	private final ArrayList<Feature> featuers = new ArrayList<Feature>();
 
-	public TransportModeAnalysis(final BufferedWriter out) {
+	public TransportModeGeoAnalysis(final BufferedWriter out) {
 		this.out = out;
 	}
+	
+	public void readGemeindegrenzen(final String filename) {
+		try {
+			FeatureSource fs = ShapeFileReader.readDataFile(filename);
+			for (Object o: fs.getFeatures()) {
+				this.featuers.add((Feature) o);
+	//			Coordinate c = feature.getBounds().centre();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
+	private Feature findFeature(final Coord coord) {
+		double x = coord.getX();
+		double y = coord.getY();
+
+		GeometryFactory gf = new GeometryFactory();
+		Point p = gf.createPoint(new Coordinate(x, y));
+		
+		for (Feature f : this.featuers) {
+			if (f.getBounds().contains(x, y)) {
+				if (f.getDefaultGeometry().contains(p)) {
+					return f;
+				}
+			}
+		}
+		return null;
+	}
+	
 	@Override
 	public void run(final Person person) {
 		boolean hasCarLeg = false;
@@ -77,11 +116,16 @@ public class TransportModeAnalysis extends AbstractPersonAlgorithm {
 			type = "undefined";
 		}
 		Activity homeAct = (Activity) person.getSelectedPlan().getPlanElements().get(0);
+		Feature f = findFeature(homeAct.getCoord());
+		Integer gid = f == null ? Integer.valueOf(-9999) : (Integer) f.getAttribute("GMDE");
+		String gname = f == null ? "UNKNOWN" : (String) f.getAttribute("NAME");
 		try {
 			this.out.write(homeAct.getCoord().getX() + "\t" +
 					homeAct.getCoord().getY() + "\t" +
 					person.getId() + "\t" +
-					type + "\n");
+					type + "\t" +
+					gid.toString() + "\t" +
+					gname + "\n");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -90,16 +134,18 @@ public class TransportModeAnalysis extends AbstractPersonAlgorithm {
 	public static void main(final String[] args) {
 		ScenarioImpl scenario = new ScenarioImpl();
 
-		Logger log = Logger.getLogger(TransportModeAnalysis.class);
+		Logger log = Logger.getLogger(TransportModeGeoAnalysis.class);
 		try {
 			log.info("reading network");
-			new MatsimNetworkReader(scenario.getNetwork()).parse("/Volumes/Data/VSP/projects/diss/runs/tr100pct1NoTr/output_network.xml.gz");
+			new MatsimNetworkReader(scenario.getNetwork()).parse("/Volumes/Data/VSP/projects/diss/runs/tr100pct1S7/output_network.xml.gz");
 			log.info("analyzing plans");
-			BufferedWriter infoFile = IOUtils.getBufferedWriter("/Volumes/Data/VSP/projects/diss/runs/tr100pct1NoTr/coords.txt");
-			infoFile.write("X\tY\tID\tTYPE\n");
+			BufferedWriter infoFile = IOUtils.getBufferedWriter("/Volumes/Data/VSP/projects/diss/runs/tr100pct1S7/coordsGmde.txt");
+			infoFile.write("X\tY\tID\tTYPE\tGID\tGNAME\n");
 			scenario.getPopulation().setIsStreaming(true);
-			scenario.getPopulation().addAlgorithm(new TransportModeAnalysis(infoFile));
-			new MatsimPopulationReader(scenario).parse("/Volumes/Data/VSP/projects/diss/runs/tr100pct1NoTr/output_plans.xml.gz");
+			TransportModeGeoAnalysis analysis = new TransportModeGeoAnalysis(infoFile);
+			analysis.readGemeindegrenzen("/Users/cello/Desktop/Gemeindegrenzen/gemeindegrenzen2008/g1g08_shp_080606/G1G08.shp");
+			scenario.getPopulation().addAlgorithm(analysis);
+			new MatsimPopulationReader(scenario).parse("/Volumes/Data/VSP/projects/diss/runs/tr100pct1S7/output_plans.xml.gz");
 			scenario.getPopulation().printPlansCount();
 			infoFile.close();
 			log.info("done");
