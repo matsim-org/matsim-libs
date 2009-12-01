45999f7ced3b615afe16a31a6b9706d14d0d5f49
mrieser <rieser@gmail.com>
2009-12-01 10:59:50 +0000

    added copyright/license header
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@9547 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/org/matsim/api/core/v01/population/Activity.java
+++ matsim/src/org/matsim/api/core/v01/population/Activity.java
@@ -1,9 +1,31 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * Activity.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007, 2009 by the members listed in the COPYING,  *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
 package org.matsim.api.core.v01.population;
 
 import org.matsim.api.basic.v01.Coord;
 import org.matsim.api.basic.v01.Id;
 
 /**
+ * Specifies the kind of activity an agent performs during its day.
+ * 
  * <p> Regarding "duration vs. end time":
  * <li> check if run693 (w/o duration) is similar to other runs </li>
  * <li> ask (per email) if moving to a design where we use <em> either </em> duration <em> or </em> end time
@@ -14,7 +36,7 @@ import org.matsim.api.basic.v01.Id;
  */
 public interface Activity extends PlanElement {
 // NOTE: Activity is NOT a location, since there is no consistent way to set the coord directly.  
-// If we need an interface for getCoord funktionality (e.g. for quadtree entries), it needs to have a different name.
+// If we need an interface for getCoord functionality (e.g. for quadtree entries), it needs to have a different name.
 // kai, jul09
 
 public double getEndTime();
