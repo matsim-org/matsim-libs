ede37b7cd1240d413223f75f0c23ce2dc565f93f
komeiste <komeiste@users.sf.net>
2009-12-16 15:30:36 +0000

    Implementing some playground tests.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@9699 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/test/src/playground/meisterk/phd/controler/PhDControlerTest.java
+++ matsim/test/src/playground/meisterk/phd/controler/PhDControlerTest.java
@@ -20,36 +20,69 @@
 
 package playground.meisterk.phd.controler;
 
+import java.util.HashMap;
+
+import org.matsim.api.basic.v01.Id;
+import org.matsim.core.basic.v01.IdImpl;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.events.BeforeMobsimEvent;
+import org.matsim.core.controler.listener.BeforeMobsimListener;
 import org.matsim.testcases.MatsimTestCase;
 
+/**
+ * Use scores of arbitrarily chosen agents after 10th iteration to verify correct working of the system.
+ * 
+ * @author meisterk
+ *
+ */
 public class PhDControlerTest extends MatsimTestCase {
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		Controler testee = new PhDControler(new String[]{this.getInputDirectory() + "config.xml"});
-		testee.getConfig().controler().setOutputDirectory(this.getOutputDirectory());
-
-		testee.setCreateGraphs(false);
-		testee.setWriteEventsInterval(0);
-		testee.run();
-	}
-
+	public static final int TEST_ITERATION = 10;
+	
 	public void testTAMRouter() {
-		fail("Not yet implemented.");
+		
+		HashMap<Id, Double> expectedPlanScores = new HashMap<Id, Double>();
+		expectedPlanScores.put(new IdImpl(1012), 102.3995485156362);
+		expectedPlanScores.put(new IdImpl(1033), 92.3175207822995);
+		expectedPlanScores.put(new IdImpl(1072), 107.02180374798185);
+		expectedPlanScores.put(new IdImpl(947), 102.39001983518614);
+
+		this.runATest(expectedPlanScores);
+		
 	}
 	
 	public void testPlanomatOnly() {
-		fail("Not yet implemented.");
+
+		HashMap<Id, Double> expectedPlanScores = new HashMap<Id, Double>();
+		expectedPlanScores.put(new IdImpl(1012), 99.02855025439848);
+		expectedPlanScores.put(new IdImpl(1033), -22.11377578357179);
+		expectedPlanScores.put(new IdImpl(1072), 104.98460992294699);
+		expectedPlanScores.put(new IdImpl(947), 103.71560343846069);
+
+		this.runATest(expectedPlanScores);
 	}
 	
 	public void testPlanomatRouter() {
-		fail("Not yet implemented.");
+		
+		HashMap<Id, Double> expectedPlanScores = new HashMap<Id, Double>();
+		expectedPlanScores.put(new IdImpl(1012), -203.22393647708262);
+		expectedPlanScores.put(new IdImpl(1033), 99.5184077860448);
+		expectedPlanScores.put(new IdImpl(1072), 100.38099695690359);
+		expectedPlanScores.put(new IdImpl(947), 106.20589087540449);
+
+		this.runATest(expectedPlanScores);
+		
 	}
 	
 	public void testPlanomatRouterCarPt() {
-		fail("Not yet implemented.");
+
+		HashMap<Id, Double> expectedPlanScores = new HashMap<Id, Double>();
+		expectedPlanScores.put(new IdImpl(1012), -203.22393647708262);
+		expectedPlanScores.put(new IdImpl(1033), 106.53017916647772);
+		expectedPlanScores.put(new IdImpl(1072), null);
+		expectedPlanScores.put(new IdImpl(947), 103.45836480987339);
+
+		this.runATest(expectedPlanScores);
 	}
 	
 	public void xtestImprovePtTravelTime() {
@@ -76,4 +109,48 @@ public class PhDControlerTest extends MatsimTestCase {
 		fail("Not yet implemented.");
 	}
 	
+	private void runATest(HashMap<Id,Double> expectedPlanScores) {
+		
+		Controler testee = new PhDControler(new String[]{this.getInputDirectory() + "config.xml"});
+		testee.getConfig().controler().setOutputDirectory(this.getOutputDirectory());
+
+		testee.addControlerListener(new ScoreChecker(expectedPlanScores));
+		
+		testee.setCreateGraphs(false);
+		testee.setWriteEventsInterval(0);
+		testee.run();
+		
+	}
+	
+	private class ScoreChecker implements BeforeMobsimListener {
+
+		private final HashMap<Id,Double> expectedPlanScores;
+		
+		public ScoreChecker(HashMap<Id, Double> expectedPlanScores) {
+			super();
+			this.expectedPlanScores = expectedPlanScores;
+		}
+
+		public void notifyBeforeMobsim(BeforeMobsimEvent event) {
+
+			int iterationNumber = event.getIteration();
+			if (iterationNumber == PhDControlerTest.TEST_ITERATION) {
+				
+				for (Id personId : this.expectedPlanScores.keySet()) {
+					Double expectedScore = this.expectedPlanScores.get(personId);
+					Double actualScore = event.getControler().getPopulation().getPersons().get(personId).getSelectedPlan().getScore();
+					if (expectedScore == null) {
+						assertNull(actualScore);
+					} else {
+						assertEquals("Unexpected score for selected plan of agent with id " + personId.toString() + ".", expectedScore, actualScore, MatsimTestCase.EPSILON);
+					}
+				}
+				
+			}
+			
+		}
+
+	}
+
+
 }
