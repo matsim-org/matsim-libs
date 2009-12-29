96c03093cbb4edb480e86266653198121eb4dd7f
mrieser <rieser@gmail.com>
2009-12-29 12:49:47 +0000

    include tests for org.matsim.ptproject
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@9770 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/test/src/org/matsim/AllTests.java
+++ matsim/test/src/org/matsim/AllTests.java
@@ -61,6 +61,7 @@ public class AllTests {
 		suite.addTest(org.matsim.planomat.AllTests.suite());
 		suite.addTest(org.matsim.population.AllTests.suite());
 		suite.addTest(org.matsim.pt.AllTests.suite());
+		suite.addTest(org.matsim.ptproject.AllTests.suite());
 		suite.addTest(org.matsim.roadpricing.AllTests.suite());
 		suite.addTest(org.matsim.signalsystems.AllTests.suite());
 		suite.addTest(org.matsim.transitSchedule.AllTests.suite());
