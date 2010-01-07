b5e4d744ce4d2d9e423dfc89f6d065e116533555
grether <dominik.grether@gmail.com>
2010-01-07 16:32:05 +0000

    added documentation
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@9876 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/lanes/package-info.java
+++ matsim/src/main/java/org/matsim/lanes/package-info.java
@@ -1,8 +1,59 @@
 /**
- *  This package contains classes to add lanes at the end of links.
- *  There is no extra test package for the lanes implementation. However
- *  tests for lanes can be found in the signalsystems test package.
+ *  This package contains classes to add lanes to links.
+ * <p> 
+ * A link may contain one or more lanes, that may have the following layout
+ * (Dashes stand for the borders of the QueueLink, equal signs (===) depict one lane, 
+ * plus signs (+) symbolize a decision point where one lane splits into several lanes):
+ * <pre>
+ * ----------------
+ * ================
+ * ----------------
+ *</pre>
  *
+ *<pre>
+ * ----------------
+ *          =======
+ * ========+
+ *          =======
+ * ----------------
+ *</pre>
+ *
+ *<pre>
+ * ----------------
+ *         ========
+ * =======+========
+ *         ========
+ * ----------------
+ * </pre>
+ * 
+ * 
+ * The following layouts are not allowed:
+ * <pre>
+ * ----------------
+ * ================
+ * ================
+ * ----------------
+ *</pre>
+ *
+ *<pre>
+ * ----------------
+ * =======
+ *        +========
+ * =======
+ * ----------------
+ * </pre> 
+ * </p> 
+ * <p> 
+ * All lane information is given by the top-level container LaneDefinitions. 
+ * 
+ * A lane is added to a link by adding a LaneToLinkAssignment instance to the container.
+ * For each link one LaneToLinkAssignment instance is needed, that holds all Lane 
+ * instances for this specific link.
+ * </p>
+ * <p> 
+ * There is no extra test package for the lanes implementation. However
+ * tests for lanes can be found in the signalsystems test package.
+ * </p>
  * <h2>Usage restrictions:</h2>
  * <ul>
  *   <li> Each link's lanes must cover all toLinks of the link's toNode within the toLink information
