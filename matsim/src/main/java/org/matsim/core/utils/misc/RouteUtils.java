bb5890f8af670e9adbf2b66f4e812874e69ae076
mrieser <rieser@gmail.com>
2010-01-14 07:43:03 +0000

    new helper method to get the Nodes from a route for backward-compatibility, including test
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@9998 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/main/java/org/matsim/core/utils/misc/RouteUtils.java
@@ -0,0 +1,56 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2010 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.core.utils.misc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.network.Node;
+import org.matsim.core.population.routes.NetworkRouteWRefs;
+
+/**
+ * Provides helper methods to work with routes.
+ *
+ * @author mrieser
+ */
+public class RouteUtils {
+
+	/**
+	 * Returns all nodes the route passes between the start- and the end-link of the route.
+	 *
+	 * @param route
+	 * @param network
+	 * @return
+	 */
+	public static List<Node> getNodes(final NetworkRouteWRefs route, final Network network) {
+		List<Node> nodes = new ArrayList<Node>(route.getLinkIds().size() + 1);
+		if (route.getStartLinkId() != route.getEndLinkId()) {
+			nodes.add(network.getLinks().get(route.getStartLinkId()).getToNode());
+			for (Id linkId : route.getLinkIds()) {
+				Link link = network.getLinks().get(linkId);
+				nodes.add(link.getToNode());
+			}
+		}
+		return nodes;
+	}
+}
---
+++ matsim/src/test/java/org/matsim/core/utils/misc/RouteUtilsTest.java
@@ -0,0 +1,118 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2010 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.core.utils.misc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.ScenarioImpl;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.network.NetworkFactory;
+import org.matsim.api.core.v01.network.Node;
+import org.matsim.core.population.routes.LinkNetworkRouteImpl;
+import org.matsim.core.population.routes.NetworkRouteWRefs;
+
+public class RouteUtilsTest {
+
+	@Test
+	public void testGetNodes() {
+		Fixture f = new Fixture();
+		Link startLink = f.network.getLinks().get(f.scenario.createId("0"));
+		Link endLink = f.network.getLinks().get(f.scenario.createId("5"));
+		List<Link> links = new ArrayList<Link>(4);
+		links.add(f.network.getLinks().get(f.scenario.createId("1")));
+		links.add(f.network.getLinks().get(f.scenario.createId("2")));
+		links.add(f.network.getLinks().get(f.scenario.createId("3")));
+		links.add(f.network.getLinks().get(f.scenario.createId("4")));
+		NetworkRouteWRefs route = new LinkNetworkRouteImpl(startLink, endLink);
+		route.setLinks(startLink, links, endLink);
+
+		List<Node> nodes = RouteUtils.getNodes(route, f.network);
+		Assert.assertEquals(5, nodes.size());
+		Assert.assertEquals(f.scenario.createId("1"), nodes.get(0).getId());
+		Assert.assertEquals(f.scenario.createId("2"), nodes.get(1).getId());
+		Assert.assertEquals(f.scenario.createId("3"), nodes.get(2).getId());
+		Assert.assertEquals(f.scenario.createId("4"), nodes.get(3).getId());
+		Assert.assertEquals(f.scenario.createId("5"), nodes.get(4).getId());
+	}
+
+	@Test
+	public void testGetNodes_SameStartEndLink() {
+		Fixture f = new Fixture();
+		Link startLink = f.network.getLinks().get(f.scenario.createId("2"));
+		Link endLink = f.network.getLinks().get(f.scenario.createId("2"));
+		List<Link> links = new ArrayList<Link>(0);
+		NetworkRouteWRefs route = new LinkNetworkRouteImpl(startLink, endLink);
+		route.setLinks(startLink, links, endLink);
+
+		List<Node> nodes = RouteUtils.getNodes(route, f.network);
+		Assert.assertEquals(0, nodes.size());
+	}
+
+	@Test
+	public void testGetNodes_NoLinksBetween() {
+		Fixture f = new Fixture();
+		Link startLink = f.network.getLinks().get(f.scenario.createId("3"));
+		Link endLink = f.network.getLinks().get(f.scenario.createId("4"));
+		List<Link> links = new ArrayList<Link>(0);
+		NetworkRouteWRefs route = new LinkNetworkRouteImpl(startLink, endLink);
+		route.setLinks(startLink, links, endLink);
+
+		List<Node> nodes = RouteUtils.getNodes(route, f.network);
+		Assert.assertEquals(1, nodes.size());
+		Assert.assertEquals(f.scenario.createId("4"), nodes.get(0).getId());
+	}
+
+	private static class Fixture {
+		protected final Scenario scenario;
+		protected final Network network;
+
+		protected Fixture() {
+			this.scenario = new ScenarioImpl();
+			this.network = this.scenario.getNetwork();
+			NetworkFactory nf = this.network.getFactory();
+
+			Id[] ids = new Id[7];
+			for (int i = 0; i < ids.length; i++) {
+				ids[i] = this.scenario.createId(Integer.toString(i));
+			}
+
+			this.network.addNode(nf.createNode(ids[0], this.scenario.createCoord(0, 0)));
+			this.network.addNode(nf.createNode(ids[1], this.scenario.createCoord(100, 0)));
+			this.network.addNode(nf.createNode(ids[2], this.scenario.createCoord(200, 0)));
+			this.network.addNode(nf.createNode(ids[3], this.scenario.createCoord(300, 0)));
+			this.network.addNode(nf.createNode(ids[4], this.scenario.createCoord(400, 0)));
+			this.network.addNode(nf.createNode(ids[5], this.scenario.createCoord(500, 0)));
+			this.network.addNode(nf.createNode(ids[6], this.scenario.createCoord(600, 0)));
+			this.network.addLink(nf.createLink(ids[0], ids[0], ids[1]));
+			this.network.addLink(nf.createLink(ids[1], ids[1], ids[2]));
+			this.network.addLink(nf.createLink(ids[2], ids[2], ids[3]));
+			this.network.addLink(nf.createLink(ids[3], ids[3], ids[4]));
+			this.network.addLink(nf.createLink(ids[4], ids[4], ids[5]));
+			this.network.addLink(nf.createLink(ids[5], ids[5], ids[6]));
+		}
+	}
+
+}
