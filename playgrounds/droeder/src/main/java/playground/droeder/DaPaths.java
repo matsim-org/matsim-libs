ffbbf2c7cf41c30944c0532c84cb76f6efbfe8bb
dan-ro <daniel_roeder@web.de>
2010-01-15 10:46:39 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@10049 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/droeder/src/main/java/playground/droeder/DaPaths.java
@@ -0,0 +1,37 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.droeder;
+
+
+/**
+ * @author droeder
+ *
+ */
+public interface DaPaths {
+	
+	final String DATA = "D:\\Uni\\Bachelor\\Eclipse\\";
+	
+	final String INPUT = DATA + "input\\";
+	final String OUTPUT = DATA + "output\\";
+	
+	
+
+
+
+}
