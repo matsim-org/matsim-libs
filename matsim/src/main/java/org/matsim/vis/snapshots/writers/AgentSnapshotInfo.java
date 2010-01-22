bb365d83e64ac0e647fc42b0a23c8a1137b798fa
vsp_nagel <2008@kainagel.org>
2010-01-22 15:55:12 +0000

    added a comment
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@10153 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/vis/snapshots/writers/AgentSnapshotInfo.java
+++ matsim/src/main/java/org/matsim/vis/snapshots/writers/AgentSnapshotInfo.java
@@ -4,7 +4,10 @@ import org.matsim.api.core.v01.Identifiable;
 
 public interface AgentSnapshotInfo extends Identifiable {
 
+	// !!! WARNING: The enum list can only be extended.  Making it shorter or changing the sequence of existing elements
+	// will break the otfvis binary channel, meaning that *.mvi files generated until then will become weird. kai, jan'10
 	public enum AgentState { PERSON_AT_ACTIVITY, PERSON_DRIVING_CAR, PERSON_OTHER_MODE, TRANSIT_DRIVER }
+	// !!! WARNING: See comment above this enum.
 
 	double getEasting();
 
