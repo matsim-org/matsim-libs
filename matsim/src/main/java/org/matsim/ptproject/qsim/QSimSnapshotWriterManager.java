400e7f186728c207ff567c01453aa13061c47392
grether <dominik.grether@gmail.com>
2010-01-25 16:45:53 +0000

    - removed signal support for mvis due to a general problem with dyn data in mvi files
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@10181 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/ptproject/qsim/QSimSnapshotWriterManager.java
+++ matsim/src/main/java/org/matsim/ptproject/qsim/QSimSnapshotWriterManager.java
@@ -27,7 +27,6 @@ import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.controler.ControlerIO;
 import org.matsim.core.utils.geometry.transformations.TransformationFactory;
 import org.matsim.lanes.otfvis.OTFLanesConnectionManagerFactory;
-import org.matsim.signalsystems.otfvis.OTFSignalsConnectionManagerFactory;
 import org.matsim.vis.otfvis.data.OTFConnectionManagerFactory;
 import org.matsim.vis.otfvis.data.fileio.OTFFileWriter;
 import org.matsim.vis.otfvis.data.fileio.qsim.OTFFileWriterQSimConnectionManagerFactory;
@@ -89,9 +88,9 @@ public class QSimSnapshotWriterManager {
         if (scenario.getConfig().scenario().isUseLanes() && (!scenario.getConfig().scenario().isUseSignalSystems())) {
           connectionManagerFactory = new OTFLanesConnectionManagerFactory(connectionManagerFactory);
         }
-        else if (scenario.getConfig().scenario().isUseLanes() && scenario.getConfig().scenario().isUseSignalSystems()){
-          connectionManagerFactory = new OTFSignalsConnectionManagerFactory(connectionManagerFactory);
-        }
+//        else if (scenario.getConfig().scenario().isUseLanes() && scenario.getConfig().scenario().isUseSignalSystems()){
+//          connectionManagerFactory = new OTFSignalsConnectionManagerFactory(connectionManagerFactory);
+//        }
         writer = new OTFFileWriter(snapshotPeriod, new OTFQSimServerQuadBuilder(network), snapshotFile, connectionManagerFactory);
         this.snapshotWriters.add(writer);
       }
