346c6c706d72d4a5328d1233ef57504b13aa272f
mrieser <rieser@gmail.com>
2010-02-01 09:00:53 +0000

    cleaned up test case, so it no longer needs an input file, and changed it to JUnit 4
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@10235 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/test/java/org/matsim/core/population/PopulationReaderMatsimV4Test.java
+++ matsim/src/test/java/org/matsim/core/population/PopulationReaderMatsimV4Test.java
@@ -25,7 +25,8 @@ import java.util.Stack;
 
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
 import org.matsim.api.core.v01.ScenarioImpl;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
@@ -36,13 +37,12 @@ import org.matsim.api.core.v01.population.Plan;
 import org.matsim.core.network.MatsimNetworkReader;
 import org.matsim.core.network.NetworkLayer;
 import org.matsim.core.population.routes.RouteWRefs;
-import org.matsim.testcases.MatsimTestCase;
+import org.matsim.core.utils.io.MatsimXmlParser;
 import org.matsim.testcases.utils.AttributesBuilder;
+import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 
-public class PopulationReaderMatsimV4Test extends MatsimTestCase {
-
-	private static final Logger log = Logger.getLogger(PopulationReaderMatsimV4Test.class);
+public class PopulationReaderMatsimV4Test {
 
 	/**
 	 * @author mrieser
@@ -51,47 +51,100 @@ public class PopulationReaderMatsimV4Test extends MatsimTestCase {
 	 * @throws ParserConfigurationException
 	 * @throws IOException
 	 */
+	@Test
 	public void testReadRoute() throws SAXException, ParserConfigurationException, IOException {
 		final ScenarioImpl scenario = new ScenarioImpl();
 		final Network network = scenario.getNetwork();
 		final PopulationImpl population = scenario.getPopulation();
-
 		new MatsimNetworkReader(scenario).parse("test/scenarios/equil/network.xml");
-		new PopulationReaderMatsimV4(scenario).parse(getInputDirectory() + "plans2.xml");
 
-		assertEquals("population size.", 2, population.getPersons().size());
+		MatsimXmlParser parser = new PopulationReaderMatsimV4(scenario);
+		XmlParserTestHelper tester = new XmlParserTestHelper(parser);
+
+		tester.startTag("plans");
+
+		tester.startTag("person", new String[][]{{"id", "1"}});
+		tester.startTag("plan", new String[][]{{"selected", "no"}});
+		tester.startTag("act", new String[][]{{"type", "h"}, {"x", "-25000"}, {"y", "0"}, {"link", "1"}, {"end_time", "06:00"}});
+		tester.endTag();
+		tester.startTag("leg", new String[][]{{"mode", "car"}});
+		tester.startTag("route");
+		tester.endTag("2 7 12");
+		tester.endTag();
+		tester.startTag("act", new String[][]{{"type", "w"}, {"x", "10000"}, {"y", "0"}, {"link", "20"}, {"dur", "00:10"}});
+		tester.endTag();
+		tester.startTag("leg", new String[][]{{"mode", "car"}});
+		tester.startTag("route");
+		tester.endTag(" ");
+		tester.endTag();
+		tester.startTag("act", new String[][]{{"type", "w"}, {"x", "10000"}, {"y", "0"}, {"link", "20"}, {"dur", "03:30"}});
+		tester.endTag();
+		tester.startTag("leg", new String[][]{{"mode", "car"}});
+		tester.startTag("route");
+		tester.endTag("13 14 15 1");
+		tester.endTag();
+		tester.startTag("act", new String[][]{{"type", "4"}, {"x", "-25000"}, {"y", "0"}, {"link", "1"}});
+		tester.endTag();
+		tester.endTag();
+		tester.endTag();
+
+		tester.startTag("person", new String[][]{{"id", "2"}});
+		tester.startTag("plan", new String[][]{{"selected", "no"}});
+		tester.startTag("act", new String[][]{{"type", "h"}, {"x", "-25000"}, {"y", "0"}, {"link", "2"}, {"end_time", "06:00"}});
+		tester.endTag();
+		tester.startTag("leg", new String[][]{{"mode", "car"}});
+		tester.startTag("route");
+		tester.endTag("6 12");
+		tester.endTag();
+		tester.startTag("act", new String[][]{{"type", "w"}, {"x", "10000"}, {"y", "0"}, {"link", "20"}, {"dur", "03:30"}});
+		tester.endTag();
+		tester.startTag("leg", new String[][]{{"mode", "car"}});
+		tester.startTag("route");
+		tester.endTag("13 14 15 1");
+		tester.endTag();
+		tester.startTag("act", new String[][]{{"type", "4"}, {"x", "-25000"}, {"y", "0"}, {"link", "1"}});
+		tester.endTag();
+		tester.endTag();
+		tester.endTag();
+
+		tester.endTag();
+
+		Assert.assertEquals("population size.", 2, population.getPersons().size());
 		Person person1 = population.getPersons().get(scenario.createId("1"));
 		Plan plan1 = person1.getPlans().get(0);
 		LegImpl leg1a = (LegImpl) plan1.getPlanElements().get(1);
 		RouteWRefs route1a = leg1a.getRoute();
-		assertEquals("different startLink for first leg.", network.getLinks().get(scenario.createId("1")).getId(), route1a.getStartLinkId());
-		assertEquals("different endLink for first leg.", network.getLinks().get(scenario.createId("20")).getId(), route1a.getEndLinkId());
+		Assert.assertEquals("different startLink for first leg.", network.getLinks().get(scenario.createId("1")).getId(), route1a.getStartLinkId());
+		Assert.assertEquals("different endLink for first leg.", network.getLinks().get(scenario.createId("20")).getId(), route1a.getEndLinkId());
 		LegImpl leg1b = (LegImpl) plan1.getPlanElements().get(3);
 		RouteWRefs route1b = leg1b.getRoute();
-		assertEquals("different startLink for second leg.", network.getLinks().get(scenario.createId("20")).getId(), route1b.getStartLinkId());
-		assertEquals("different endLink for second leg.", network.getLinks().get(scenario.createId("20")).getId(), route1b.getEndLinkId());
+		Assert.assertEquals("different startLink for second leg.", network.getLinks().get(scenario.createId("20")).getId(), route1b.getStartLinkId());
+		Assert.assertEquals("different endLink for second leg.", network.getLinks().get(scenario.createId("20")).getId(), route1b.getEndLinkId());
 		LegImpl leg1c = (LegImpl) plan1.getPlanElements().get(5);
 		RouteWRefs route1c = leg1c.getRoute();
-		assertEquals("different startLink for third leg.", network.getLinks().get(scenario.createId("20")).getId(), route1c.getStartLinkId());
-		assertEquals("different endLink for third leg.", network.getLinks().get(scenario.createId("1")).getId(), route1c.getEndLinkId());
+		Assert.assertEquals("different startLink for third leg.", network.getLinks().get(scenario.createId("20")).getId(), route1c.getStartLinkId());
+		Assert.assertEquals("different endLink for third leg.", network.getLinks().get(scenario.createId("1")).getId(), route1c.getEndLinkId());
 
 		Person person2 = population.getPersons().get(scenario.createId("2"));
 		Plan plan2 = person2.getPlans().get(0);
 		LegImpl leg2a = (LegImpl) plan2.getPlanElements().get(1);
 		RouteWRefs route2a = leg2a.getRoute();
-		assertEquals("different startLink for first leg.", network.getLinks().get(scenario.createId("2")).getId(), route2a.getStartLinkId());
-		assertEquals("different endLink for first leg.", network.getLinks().get(scenario.createId("20")).getId(), route2a.getEndLinkId());
+		Assert.assertEquals("different startLink for first leg.", network.getLinks().get(scenario.createId("2")).getId(), route2a.getStartLinkId());
+		Assert.assertEquals("different endLink for first leg.", network.getLinks().get(scenario.createId("20")).getId(), route2a.getEndLinkId());
 		LegImpl leg2b = (LegImpl) plan2.getPlanElements().get(3);
 		RouteWRefs route2b = leg2b.getRoute();
-		assertEquals("different startLink for third leg.", network.getLinks().get(scenario.createId("20")).getId(), route2b.getStartLinkId());
-		assertEquals("different endLink for third leg.", network.getLinks().get(scenario.createId("1")).getId(), route2b.getEndLinkId());
+		Assert.assertEquals("different startLink for third leg.", network.getLinks().get(scenario.createId("20")).getId(), route2b.getStartLinkId());
+		Assert.assertEquals("different endLink for third leg.", network.getLinks().get(scenario.createId("1")).getId(), route2b.getEndLinkId());
 	}
 
 	/**
 	 * Tests that reading in plans that contain route-elements, but the activities before or after do
 	 * not contain any link-information, works. This can be the case if the given routes are part of
 	 * a teleported transport mode, where links may not be required.
+	 *
+	 * @author mrieser
 	 */
+	@Test
 	public void testReadRouteWithoutActivityLinks() {
 		final ScenarioImpl scenario = new ScenarioImpl();
 		final PopulationImpl population = scenario.getPopulation();
@@ -114,14 +167,18 @@ public class PopulationReaderMatsimV4Test extends MatsimTestCase {
 		parser.endTag("person", null, context);
 		parser.endTag("plans", null, context);
 
-		assertEquals("population size.", 1, population.getPersons().size());
+		Assert.assertEquals("population size.", 1, population.getPersons().size());
 		Person person1 = population.getPersons().get(scenario.createId("981"));
 		Plan plan1 = person1.getPlans().get(0);
 		LegImpl leg1 = (LegImpl) plan1.getPlanElements().get(1);
 		RouteWRefs route1 = leg1.getRoute();
-		assertNotNull(route1);
+		Assert.assertNotNull(route1);
 	}
 
+	/**
+	 * @author mrieser
+	 */
+	@Test
 	public void testReadActivity() {
 		final ScenarioImpl scenario = new ScenarioImpl();
 		final NetworkLayer network = scenario.getNetwork();
@@ -145,11 +202,46 @@ public class PopulationReaderMatsimV4Test extends MatsimTestCase {
 		reader.endTag("person", "", context);
 		reader.endTag("plans", "", context);
 
-		assertEquals(1, population.getPersons().size());
+		Assert.assertEquals(1, population.getPersons().size());
 		Person person = population.getPersons().get(scenario.createId("2"));
 		Plan plan = person.getPlans().get(0);
-		assertEquals(link3.getId(), ((Activity) plan.getPlanElements().get(0)).getLinkId());
-		assertEquals(scenario.createId("2"), ((Activity) plan.getPlanElements().get(2)).getLinkId());
+		Assert.assertEquals(link3.getId(), ((Activity) plan.getPlanElements().get(0)).getLinkId());
+		Assert.assertEquals(scenario.createId("2"), ((Activity) plan.getPlanElements().get(2)).getLinkId());
+	}
+
+	private static class XmlParserTestHelper {
+		private final MatsimXmlParser parser;
+		private final Stack<String> context = new Stack<String>();
+
+		public XmlParserTestHelper(MatsimXmlParser parser) {
+			this.parser = parser;
+		}
+
+		public void startTag(String name) {
+			startTag(name, AttributesBuilder.getEmpty());
+		}
+
+		public void startTag(String name, String[][] atts) {
+			AttributesBuilder builder = new AttributesBuilder();
+			for (String[] attribute : atts) {
+				builder.add(attribute[0], attribute[1]);
+			}
+			startTag(name, builder.get());
+		}
+
+		private void startTag(String name, Attributes atts) {
+			parser.startTag(name, atts, context);
+			this.context.push(name);
+		}
+
+		public void endTag() {
+			endTag(null);
+		}
+
+		public void endTag(String content) {
+			String name = this.context.pop();
+			parser.endTag(name, content, this.context);
+		}
 	}
 
 }
