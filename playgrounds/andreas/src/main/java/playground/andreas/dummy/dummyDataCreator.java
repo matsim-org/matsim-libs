f4ed6b3fc0bb70331f8871231816c752aecd8ee7
vsp_neumann <neumann@senozon.de>
2010-02-04 09:17:28 +0000

    dummy
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@10284 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/andreas/src/main/java/playground/andreas/dummy/dummyDataCreator.java
@@ -0,0 +1,47 @@
+package playground.andreas.dummy;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class dummyDataCreator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(new File("dummy.dat")));
+			
+			writer.write("person id; cars; bikes; ships; busses; airplanes; trains");
+			writer.newLine();
+			
+			for (int i = 1; i <= 2000000; i++) {
+				
+				double cars = (int) (Math.random() * 10);
+				double bikes = (int) (Math.random() * 20);
+				double ships = (int) (Math.random() * 5);
+				double busses = (int) (Math.random() * 4);
+				double airplanes = (int) (Math.random() * 2);
+				double trains = (int) (Math.random() * 2);
+				
+				writer.write(i + "; " + cars + "; " + bikes + "; " + ships + "; " + busses + "; " + airplanes + "; " + trains);
+				writer.newLine();
+				
+			}
+			
+			writer.flush();
+			writer.close();
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+
+	}
+
+}
