c347b0064aab18e63208468deeb6e1df6799a0d6
dan-ro <daniel_roeder@web.de>
2010-02-10 21:21:26 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@10368 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/droeder/src/main/java/playground/droeder/gershensonSignals/GershensonScenarioGenerator.java
+++ playgrounds/droeder/src/main/java/playground/droeder/gershensonSignals/GershensonScenarioGenerator.java
@@ -32,6 +32,7 @@ import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigWriter;
 import org.matsim.core.config.groups.QSimConfigGroup;
 import org.matsim.core.config.groups.StrategyConfigGroup;
+import org.matsim.core.config.groups.CharyparNagelScoringConfigGroup.ActivityParams;
 import org.matsim.core.population.ActivityImpl;
 import org.matsim.core.population.LegImpl;
 import org.matsim.core.population.PersonImpl;
@@ -56,6 +57,10 @@ import org.matsim.signalsystems.systems.SignalSystems;
 import org.matsim.signalsystems.systems.SignalSystemsFactory;
 
 import playground.droeder.DaPaths;
+import playground.dgrether.consistency.ConsistencyChecker;
+import playground.dgrether.lanes.LanesConsistencyChecker;
+import playground.dgrether.signalsystems.SignalSystemsConsistencyChecker;
+
 
 /**
  * @author droeder
@@ -72,46 +77,51 @@ public class GershensonScenarioGenerator {
 	public static final String SIGNALSYSTEMCONFIGURATIONSOUTPUTFILE = OUTPUT + "gershensonSignalSystemsConfig.xml";
 	public static final String POPULATIONOUTPUTFILE = OUTPUT + "gershensonPopulation.xml";
 	private static final String PLANSOUTPUTFILE = OUTPUT + "gershensonPlans.xml";
-	private static final String OUTPUTDIRECTORY = OUTPUT + "route//";
-
+	private static final String OUTPUTDIRECTORY = OUTPUT ;
+	
 	private static final boolean isUseLanes = true;
 	private static final boolean isUseSignalSystems = true;
-
+	
 	private static final String controllerClass = GershensonAdaptiveTrafficLightController.class.getCanonicalName();
-	private static final int iterations = 1000;
+	private static final int iterations = 10;
 
 
-
-	private Id id1, id2, id3, id11, id12, id13, id14;
-
+	
+	private Id id1, id2, id3, id4, id11, id12, id13, id14, id15, id16, id17, id18;
+	
 	private void createIds(ScenarioImpl sc){
 		id1 = sc.createId("1");
 		id2 = sc.createId("2");
 		id3 = sc.createId("3");
+		id4 = sc.createId("4");
 		id11 = sc.createId("11");
 		id12 = sc.createId("12");
 		id13 = sc.createId("13");
 		id14 = sc.createId("14");
+		id15 = sc.createId("15");
+		id16 = sc.createId("16");
+		id17 = sc.createId("17");
+		id18 = sc.createId("18");
 
 	}
-
+	
 	public void createScenario() {
 		//create a scenario
 		ScenarioImpl scenario = new ScenarioImpl();
-
+		
 		//get the config
 		Config config = scenario.getConfig();
-
+		
 		//set the network input file to the config and load it
 		config.network().setInputFile(NETWORKFILE);
 		ScenarioLoaderImpl loader = new ScenarioLoaderImpl(scenario);
 		loader.loadNetwork();
 		createIds(scenario);
-
+		
 		//create the plans and write them
 		createPlans(scenario);
 		new PopulationWriter(scenario.getPopulation(), scenario.getNetwork()).writeFile(POPULATIONOUTPUTFILE);
-
+		
 		if (isUseLanes) {
 			config.scenario().setUseLanes(true);
 			config.network().setLaneDefinitionsFile(LANESOUTPUTFILE);
@@ -119,30 +129,40 @@ public class GershensonScenarioGenerator {
 			LaneDefinitions lanes = createLanes(scenario);
 			MatsimLaneDefinitionsWriter laneWriter = new MatsimLaneDefinitionsWriter(lanes);
 			laneWriter.writeFile(LANESOUTPUTFILE);
+			
+			LanesConsistencyChecker lcc =new LanesConsistencyChecker(scenario.getNetwork(), scenario.getLaneDefinitions());
+			lcc.checkConsistency();
 		}
 		if (isUseSignalSystems) {
 			//enable lanes and signal system feature in config
 			config.scenario().setUseSignalSystems(true);
 			config.signalSystems().setSignalSystemFile(SIGNALSYSTEMSOUTPUTFILE);
 			config.signalSystems().setSignalSystemConfigFile(SIGNALSYSTEMCONFIGURATIONSOUTPUTFILE);
-
+			
 			//create the signal systems and write them
 			SignalSystems signalSystems = createSignalSystems(scenario);
 			MatsimSignalSystemsWriter ssWriter = new MatsimSignalSystemsWriter(signalSystems);
 			ssWriter.writeFile(SIGNALSYSTEMSOUTPUTFILE);
-
+			
 			//create the signal system's configurations and write them
 			SignalSystemConfigurations ssConfigs = createSignalSystemsConfig(scenario);
-			MatsimSignalSystemConfigurationsWriter ssConfigsWriter = new MatsimSignalSystemConfigurationsWriter(ssConfigs);
+			MatsimSignalSystemConfigurationsWriter ssConfigsWriter = new MatsimSignalSystemConfigurationsWriter(ssConfigs);	
 			ssConfigsWriter.writeFile(SIGNALSYSTEMCONFIGURATIONSOUTPUTFILE);
+			
+			SignalSystemsConsistencyChecker sscc = new SignalSystemsConsistencyChecker(scenario.getNetwork(), scenario.getLaneDefinitions(), scenario.getSignalSystems());
+			sscc.checkConsistency();
 		}
-
-		//create and write the config
+		
+		//create and write the config 
 		createConfig(config);
 		new ConfigWriter(config).writeFile(CONFIGOUTPUTFILE);
+		
 
+		
+		
+		
 		Log.info("Scenario Written!");
-
+		
 
 	}
 
@@ -150,43 +170,79 @@ public class GershensonScenarioGenerator {
 	private LaneDefinitions createLanes(ScenarioImpl scenario) {
 		LaneDefinitions lanes = scenario.getLaneDefinitions();
 		LaneDefinitionsFactory factory = lanes.getFactory();
-
-
+		
 		//lanes for link 11
 		LanesToLinkAssignment lanesForLink11 = factory.createLanesToLinkAssignment(id11);
-//		Lane link11lane1 = factory.createLane(id1);
+		Lane link11lane1 = factory.createLane(id1);
 		Lane link11lane2 = factory.createLane(id2);
-//		Lane link11lane3 = factory.createLane(id3);
-
-//		link11lane1.addToLinkId(id13);
-//		link11lane1.setNumberOfRepresentedLanes(1);
+		Lane link11lane3 = factory.createLane(id3);
+		
+		link11lane1.addToLinkId(id17);
+		link11lane1.setNumberOfRepresentedLanes(1);
 		link11lane2.addToLinkId(id12);
 		link11lane2.setNumberOfRepresentedLanes(1);
-//		link11lane3.addToLinkId(id14);
-//		link11lane3.setNumberOfRepresentedLanes(1);
-
-//		lanesForLink11.addLane(link11lane1);
+		link11lane3.addToLinkId(id14);
+		link11lane3.setNumberOfRepresentedLanes(1);
+		
+		lanesForLink11.addLane(link11lane1);
 		lanesForLink11.addLane(link11lane2);
-//		lanesForLink11.addLane(link11lane3);
+		lanesForLink11.addLane(link11lane3);
 		lanes.addLanesToLinkAssignment(lanesForLink11);
-
+		
 		//lanes for link 13
 		LanesToLinkAssignment lanesForLink13 = factory.createLanesToLinkAssignment(id13);
-//		Lane link13lane1 = factory.createLane(id1);
+		Lane link13lane1 = factory.createLane(id1);
 		Lane link13lane2 = factory.createLane(id2);
-//		Lane link13lane3 = factory.createLane(id3);
-
-//		link13lane1.addToLinkId(id12);
-//		link13lane1.setNumberOfRepresentedLanes(1);
+		Lane link13lane3 = factory.createLane(id3);
+		
+		link13lane1.addToLinkId(id12);
+		link13lane1.setNumberOfRepresentedLanes(1);
 		link13lane2.addToLinkId(id14);
 		link13lane2.setNumberOfRepresentedLanes(1);
-//		link13lane3.addToLinkId(id11);
-//		link13lane3.setNumberOfRepresentedLanes(1);
-
-//		lanesForLink13.addLane(link13lane1);
+		link13lane3.addToLinkId(id15);
+		link13lane3.setNumberOfRepresentedLanes(1);
+		
+		lanesForLink13.addLane(link13lane1);
 		lanesForLink13.addLane(link13lane2);
-//		lanesForLink13.addLane(link13lane3);
+		lanesForLink13.addLane(link13lane3);
 		lanes.addLanesToLinkAssignment(lanesForLink13);
+		
+		//lanes for link 16
+		LanesToLinkAssignment lanesForLink16 = factory.createLanesToLinkAssignment(id16);
+		Lane link16lane1 = factory.createLane(id1);
+		Lane link16lane2 = factory.createLane(id2);
+		Lane link16lane3 = factory.createLane(id3);
+		
+		link16lane1.addToLinkId(id14);
+		link16lane1.setNumberOfRepresentedLanes(1);
+		link16lane2.addToLinkId(id15);
+		link16lane2.setNumberOfRepresentedLanes(1);
+		link16lane3.addToLinkId(id17);
+		link16lane3.setNumberOfRepresentedLanes(1);
+		
+		lanesForLink16.addLane(link16lane1);
+		lanesForLink16.addLane(link16lane2);
+		lanesForLink16.addLane(link16lane3);
+		lanes.addLanesToLinkAssignment(lanesForLink16);
+		
+		//lanes for link 18
+		LanesToLinkAssignment lanesForLink18 = factory.createLanesToLinkAssignment(id18);
+		Lane link18lane1 = factory.createLane(id1);
+		Lane link18lane2 = factory.createLane(id2);
+		Lane link18lane3 = factory.createLane(id3);
+		
+		link18lane1.addToLinkId(id15);
+		link18lane1.setNumberOfRepresentedLanes(1);
+		link18lane2.addToLinkId(id17);
+		link18lane2.setNumberOfRepresentedLanes(1);
+		link18lane3.addToLinkId(id12);
+		link18lane3.setNumberOfRepresentedLanes(1);
+		
+		lanesForLink18.addLane(link18lane1);
+		lanesForLink18.addLane(link18lane2);
+		lanesForLink18.addLane(link18lane3);
+		lanes.addLanesToLinkAssignment(lanesForLink18);
+		
 		return lanes;
 	}
 
@@ -195,56 +251,80 @@ public class GershensonScenarioGenerator {
 	private SignalSystemConfigurations createSignalSystemsConfig(ScenarioImpl scenario) {
 		SignalSystemConfigurations configs = scenario.getSignalSystemConfigurations();
 		SignalSystemConfigurationsFactory factory = configs.getFactory();
-
+		
 		SignalSystemConfiguration systemConfig = factory.createSignalSystemConfiguration(id1);
 		AdaptiveSignalSystemControlInfo controlInfo = factory.createAdaptiveSignalSystemControlInfo();
 		controlInfo.addSignalGroupId(id1);
 		controlInfo.addSignalGroupId(id2);
+		controlInfo.addSignalGroupId(id3);
+		controlInfo.addSignalGroupId(id4);
 		controlInfo.setAdaptiveControlerClass(controllerClass);
 		systemConfig.setSignalSystemControlInfo(controlInfo);
-
+		
 		configs.getSignalSystemConfigurations().put(systemConfig.getSignalSystemId(), systemConfig);
-
+		
 		return configs;
 	}
 
 	private SignalSystems createSignalSystems(ScenarioImpl scenario) {
 		SignalSystems systems = scenario.getSignalSystems();
 		SignalSystemsFactory factory = systems.getFactory();
-
+		
 		//create the signal system no 1
 		SignalSystemDefinition definition = factory.createSignalSystemDefinition(id1);
 		systems.addSignalSystemDefinition(definition);
-
-		//create signal group for traffic on link 11
+		//create signal group for traffic on link 11 
 		SignalGroupDefinition groupLink11 = factory.createSignalGroupDefinition(id11, id1);
-//		groupLink11.addLaneId(id1);
+		groupLink11.addLaneId(id1);
 		groupLink11.addLaneId(id2);
-//		groupLink11.addLaneId(id3);
+		groupLink11.addLaneId(id3);
 		groupLink11.addToLinkId(id12);
-//		groupLink11.addToLinkId(id13);
-//		groupLink11.addToLinkId(id14);
-
+		groupLink11.addToLinkId(id14);
+		groupLink11.addToLinkId(id17);
+		
+		
 		//create signal group for traffic on link 13
 		SignalGroupDefinition groupLink13 = factory.createSignalGroupDefinition(id13, id2);
-//		groupLink13.addLaneId(id1);
+		groupLink13.addLaneId(id1);
 		groupLink13.addLaneId(id2);
-//		groupLink13.addLaneId(id3);
-//		groupLink13.addToLinkId(id11);
-//		groupLink13.addToLinkId(id12);
+		groupLink13.addLaneId(id3);
+		groupLink13.addToLinkId(id12);
 		groupLink13.addToLinkId(id14);
-
-
+		groupLink13.addToLinkId(id15);
+		
+		//create signal group for traffic on link 16
+		SignalGroupDefinition groupLink16 = factory.createSignalGroupDefinition(id16, id3);
+		groupLink16.addLaneId(id1);
+		groupLink16.addLaneId(id2);
+		groupLink16.addLaneId(id3);
+		groupLink16.addToLinkId(id14);
+		groupLink16.addToLinkId(id15);
+		groupLink16.addToLinkId(id17);
+		
+		//create signal group for traffic on link 18
+		SignalGroupDefinition groupLink18 = factory.createSignalGroupDefinition(id18, id4);
+		groupLink18.addLaneId(id1);
+		groupLink18.addLaneId(id2);
+		groupLink18.addLaneId(id3);
+		groupLink18.addToLinkId(id15);
+		groupLink18.addToLinkId(id17);
+		groupLink18.addToLinkId(id12);
+		
+	
 		//adding groups to the system
 		groupLink11.setSignalSystemDefinitionId(id1);
-		groupLink13.setSignalSystemDefinitionId(id2);
+		groupLink13.setSignalSystemDefinitionId(id1);
+		groupLink16.setSignalSystemDefinitionId(id1);
+		groupLink18.setSignalSystemDefinitionId(id1);
 
+		
 		//adding signalGroupDefinitions to the container
 		systems.addSignalGroupDefinition(groupLink11);
 		systems.addSignalGroupDefinition(groupLink13);
+		systems.addSignalGroupDefinition(groupLink16);
+		systems.addSignalGroupDefinition(groupLink18);
 
-
-
+		
 		return systems;
 	}
 
@@ -253,10 +333,10 @@ public class GershensonScenarioGenerator {
 		PopulationImpl population = scenario.getPopulation();
 		int firstHomeEndTime =  1 * 3600;
 		int homeEndTime = firstHomeEndTime;
-
+		
 		Link l11 = network.getLinks().get(scenario.createId("11"));
 		Link l12 = network.getLinks().get(scenario.createId("12"));
-		Link l13 = network.getLinks().get(scenario.createId("13"));
+		Link l13 = network.getLinks().get(scenario.createId("13"));		
 		Link l14 = network.getLinks().get(scenario.createId("14"));
 		PopulationFactory factory = population.getFactory();
 
@@ -273,16 +353,16 @@ public class GershensonScenarioGenerator {
 			// leg to home
 			LegImpl leg1 = (LegImpl) factory.createLeg(TransportMode.car);
 			route1 = new LinkNetworkRouteImpl(l11.getId(), l12.getId(), network);
-
+			
 			leg1.setRoute(route1);
 			plan1.addLeg(leg1);
-
+				
 			ActivityImpl act12 = (ActivityImpl) factory.createActivityFromLinkId("h", l12.getId());
 			act12.setLinkId(l12.getId());
-			plan1.addActivity(act12);
-
+			plan1.addActivity(act12);	
+			
 			population.addPerson(p1);
-
+			
 			PersonImpl p2 = (PersonImpl) factory.createPerson(scenario.createId(Integer.toString(i+500)));
 			Plan plan = factory.createPlan();
 			p2.addPlan(plan);
@@ -295,27 +375,28 @@ public class GershensonScenarioGenerator {
 			// leg to home
 			LegImpl leg2 = (LegImpl) factory.createLeg(TransportMode.car);
 			route = new LinkNetworkRouteImpl(l13.getId(), l14.getId(), network);
-
+			
 			leg2.setRoute(route);
 			plan.addLeg(leg2);
-
+				
 			ActivityImpl act22 = (ActivityImpl) factory.createActivityFromLinkId("h", l14.getId());
 			act22.setLinkId(l14.getId());
-			plan.addActivity(act22);
-
+			plan.addActivity(act22);	
+			
 			population.addPerson(p2);
-
-
-
+					
+			
+			
 		}
-
+		
 
 	}
 
 	private void createConfig(Config config) {
-
+		
 		config.network().setInputFile(NETWORKFILE);
 		config.plans().setInputFile(POPULATIONOUTPUTFILE);
+	
 
 		// configure scoring for plans
 		config.charyparNagelScoring().setLateArrival(0.0);
@@ -330,6 +411,7 @@ public class GershensonScenarioGenerator {
 		config.travelTimeCalculator().setTraveltimeBinSize(1);
 		config.controler().setLastIteration(iterations);
 		config.controler().setOutputDirectory(OUTPUTDIRECTORY);
+	
 
 
 		// configure simulation and snapshot writing
@@ -338,8 +420,9 @@ public class GershensonScenarioGenerator {
 		config.getQSimConfigGroup().setSnapshotFile("cmcf.mvi");
 		config.getQSimConfigGroup().setSnapshotPeriod(60.0);
 		config.getQSimConfigGroup().setSnapshotStyle("queue");
+		config.otfVis().setDrawLinkIds(true);
 		// configure strategies for replanning
-
+		
 		config.strategy().setMaxAgentPlanMemorySize(4);
 		StrategyConfigGroup.StrategySettings selectExp = new StrategyConfigGroup.StrategySettings(id1);
 		selectExp.setProbability(0.9);
@@ -353,8 +436,8 @@ public class GershensonScenarioGenerator {
 		config.strategy().addStrategySettings(reRoute);
 	}
 
-
-
+	
+	
 	public static void main(final String[] args) {
 		try {
 			new GershensonScenarioGenerator().createScenario();
