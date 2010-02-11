25ee9522f60e836e6919f964e66f35738370d1a3
grether <dominik.grether@gmail.com>
2010-02-11 14:40:23 +0000

    implementation of mobismfactory to start otfvis without overwriting the controler even if the otfviscontroler is broken
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@10393 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/main/java/org/matsim/vis/otfvis/OTFVisMobsimFactoryImpl.java
@@ -0,0 +1,42 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * OTFVisMobsimFactoryImpl
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2010 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package org.matsim.vis.otfvis;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.mobsim.framework.MobsimFactory;
+import org.matsim.core.mobsim.framework.Simulation;
+
+
+/**
+ * @author dgrether
+ *
+ */
+public class OTFVisMobsimFactoryImpl implements MobsimFactory {
+
+  /**
+   * @see org.matsim.core.mobsim.framework.MobsimFactory#createMobsim(org.matsim.api.core.v01.Scenario, org.matsim.core.api.experimental.events.EventsManager)
+   */
+  @Override
+  public Simulation createMobsim(Scenario sc, EventsManager eventsManager) {
+    return new OTFVisQSim(sc, eventsManager);
+  }
+
+}
