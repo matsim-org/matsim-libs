5fd94eccdea269e874fa0d6bde4789e834c38e81
grether <dominik.grether@gmail.com>
2010-02-20 10:31:54 +0000

    fixed bug in active state of QLanes affecting the active state when there are only vehicles in the buffer of the original QLane leading to another QLane
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@10521 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/ptproject/qsim/QLane.java
+++ matsim/src/main/java/org/matsim/ptproject/qsim/QLane.java
@@ -412,7 +412,7 @@ public class QLane {
 		 * link active until buffercap has accumulated (so a newly arriving vehicle
 		 * is not delayed).
 		 */
-		boolean active = (this.buffercap_accumulate < 1.0) || (!this.vehQueue.isEmpty()) || (!this.waitingList.isEmpty() || transitQueueLaneFeature.isFeatureActive());
+		boolean active = (this.buffercap_accumulate < 1.0) || (!this.vehQueue.isEmpty()) || (!this.waitingList.isEmpty() || (this.toLanes != null && !this.bufferIsEmpty()) || transitQueueLaneFeature.isFeatureActive());
 		return active;
 	}
 
