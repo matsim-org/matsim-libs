3c07bbe3e06f31e7fe5ab070108716a42de11811
balmermi <balmermi@users.sf.net>
2010-03-03 13:18:42 +0000

    input link id file for analyzing only a subset of links
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@10649 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/balmermi/src/main/java/playground/balmermi/datapuls/LinkFrequencyCalculator.java
+++ playgrounds/balmermi/src/main/java/playground/balmermi/datapuls/LinkFrequencyCalculator.java
@@ -1,18 +1,23 @@
 package playground.balmermi.datapuls;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.ScenarioImpl;
 import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.basic.v01.IdImpl;
 import org.matsim.core.events.EventsManagerImpl;
 import org.matsim.core.events.EventsReaderTXTv1;
 import org.matsim.core.network.MatsimNetworkReader;
@@ -20,33 +25,53 @@ import org.matsim.core.network.MatsimNetworkReader;
 import playground.balmermi.datapuls.modules.FrequencyAnalyser;
 
 public class LinkFrequencyCalculator {
+	
+	private static Set<Id> readLinkIds(File linkIdFile) throws IOException {
+		FileReader fr = new FileReader(linkIdFile);
+		BufferedReader br = new BufferedReader(fr);
+		String currLine = null;
+		Set<Id> linkIds = new HashSet<Id>();
+		while ((currLine = br.readLine()) != null) {
+			Id id = new IdImpl(currLine.trim());
+			linkIds.add(id);
+		}
+		return linkIds;
+	}
 
 	public static void main(String[] args) throws IOException {
 		File net = new File(args[0]);
 		if (!net.isFile() || !net.canRead()) { throw new RuntimeException("net="+net+" is not a File or cannot be read"); }
+		System.out.println("network file: "+net);
 
 		File inDir = new File(args[1]);
 		if (!inDir.isDirectory()) { throw new RuntimeException("inDir="+inDir+" is not a directory"); }
+		System.out.println("events input directory: "+inDir);
 
 		File outDir = new File(args[2]);
 		if (!outDir.isDirectory()) { throw new RuntimeException("outDir="+outDir+" is not a directory"); }
-
-		System.out.println("network file: "+net);
-		System.out.println("events input directory: "+inDir);
 		System.out.println("analysis output directory: "+outDir);
-		
+
 		Scenario scenario = new ScenarioImpl();
 		new MatsimNetworkReader(scenario).readFile(net.getPath());
 		
-		FrequencyAnalyser fa = new FrequencyAnalyser(scenario.getNetwork());
+		Set<Id> linkIds = null;
+		if (args.length == 4) {
+			File linkIdFile = new File(args[3]);
+			if (!linkIdFile.isFile() || !linkIdFile.canRead()) { throw new RuntimeException("linkIdFile="+linkIdFile+" is not a File or cannot be read"); }
+			System.out.println("link id file: "+linkIdFile);
+			linkIds = readLinkIds(linkIdFile);
+		}
+		else {
+			linkIds = scenario.getNetwork().getLinks().keySet();
+		}
+
+		FrequencyAnalyser fa = new FrequencyAnalyser(scenario.getNetwork(),linkIds);
 		EventsManager eManager = new EventsManagerImpl();
 		eManager.addHandler(fa);
 		EventsReaderTXTv1 reader = new EventsReaderTXTv1(eManager);
 		
 		Map<Id,List<Integer>> freqs = new TreeMap<Id, List<Integer>>();
-		for (Id lid : scenario.getNetwork().getLinks().keySet()) {
-			freqs.put(lid,new ArrayList<Integer>());
-		}
+		for (Id lid : linkIds) { freqs.put(lid,new ArrayList<Integer>()); }
 
 		Map<Integer,File> eventFiles = new TreeMap<Integer, File>();
 		for (File eventfile : inDir.listFiles()) {
@@ -68,6 +93,7 @@ public class LinkFrequencyCalculator {
 			for (Id lid : fa.getFrequencies().keySet()) {
 				freqs.get(lid).add(fa.getFrequencies().get(lid).size());
 			}
+			fa.resetLog();
 			System.out.println("done. (processing)");
 		}
 		
--- playgrounds/balmermi/src/main/java/playground/balmermi/datapuls/modules/FrequencyAnalyser.java
+++ playgrounds/balmermi/src/main/java/playground/balmermi/datapuls/modules/FrequencyAnalyser.java
@@ -5,11 +5,13 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.lang.NullArgumentException;
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.core.api.experimental.events.LinkLeaveEvent;
 import org.matsim.core.api.experimental.events.handler.LinkLeaveEventHandler;
+import org.matsim.core.gbl.Gbl;
 
 public class FrequencyAnalyser implements LinkLeaveEventHandler {
 
@@ -19,17 +21,23 @@ public class FrequencyAnalyser implements LinkLeaveEventHandler {
 	
 	private static final Logger log = Logger.getLogger(FrequencyAnalyser.class);
 	private final Map<Id,Set<Id>> freqs;
+	private long entryCnt = 0;
+	private long hour = -1;
 
 	//////////////////////////////////////////////////////////////////////
 	// constructor
 	//////////////////////////////////////////////////////////////////////
 	
 	public FrequencyAnalyser(final Network network) {
+		this(network,network.getLinks().keySet());
+	}
+
+	public FrequencyAnalyser(final Network network, Set<Id> linkIdSet) {
 		log.info("init " + this.getClass().getName() + " module...");
+		if (linkIdSet == null) { throw new NullArgumentException("linkIdSet"); }
 		freqs = new HashMap<Id, Set<Id>>((int)(network.getLinks().size()*1.4));
-		for (Id lid : network.getLinks().keySet()) {
-			freqs.put(lid,new HashSet<Id>());
-		}
+		for (Id lid : linkIdSet) { freqs.put(lid,new HashSet<Id>()); }
+		log.info("=> "+freqs.size()+" sets allocated.");
 		log.info("done. (init)");
 	}
 
@@ -39,14 +47,31 @@ public class FrequencyAnalyser implements LinkLeaveEventHandler {
 	
 	@Override
 	public void handleEvent(LinkLeaveEvent event) {
-		freqs.get(event.getLinkId()).add(event.getPersonId());
+		Set<Id> pids = freqs.get(event.getLinkId());
+		if (pids == null) { return; }
+		if (pids.add(event.getPersonId())) { entryCnt++; }
+		// logging info
+		if ((entryCnt != 0) && (entryCnt % freqs.size() == 0)) {
+			log.info(entryCnt+" entries added to the frequency map.");
+			Gbl.printMemoryUsage();
+		}
+		if (((int)(event.getTime()/3600.0)) != hour) { hour++; log.info("at hour "+hour); }
 	}
 
 	@Override
 	public void reset(int iteration) {
 		log.info("reset " + this.getClass().getName() + " module...");
 		for (Set<Id> idSet : freqs.values()) { idSet.clear(); }
+		entryCnt = 0;
+		hour = -1;
 		log.info("done. (reset)");
+	}
+	
+	public void resetLog() {
+		log.info("reset log of " + this.getClass().getName() + " module...");
+		entryCnt = 0;
+		hour = -1;
+		log.info("done. (reset log)");
 	}
 
 	//////////////////////////////////////////////////////////////////////
