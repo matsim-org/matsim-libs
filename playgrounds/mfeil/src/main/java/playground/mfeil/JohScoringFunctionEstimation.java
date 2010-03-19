bab5b050f4a03494f4d72aacd9587c77ca286969
Matthias_Feil <Matthias_Feil@users.sf.net>
2010-03-19 13:44:07 +0000

    62 settings
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@10773 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/mfeil/src/main/java/playground/mfeil/JohScoringFunctionEstimation.java
+++ playgrounds/mfeil/src/main/java/playground/mfeil/JohScoringFunctionEstimation.java
@@ -91,17 +91,17 @@ public class JohScoringFunctionEstimation implements ScoringFunction {
 	private static double factorOfLateArrival = 3; 
 	private static double marginalUtilityOfEarlyDeparture = 0; 
 	
-	// Settings of 7
-	private static double beta_time_car = -3; // war -3.77
-	private static double beta_time_pt = 0.05; // war 0.563
+	// Settings of 62
+	private static double beta_time_car = -6; // war -3.77
+	private static double beta_time_pt = -1; // war 0.563
 	private static double beta_time_bike = -1.07;
-	private static double beta_time_walk = -1.48; // war -1.8
+	private static double beta_time_walk = -3; // war -1.8
 	
 	private static double constantPt = -0.578;
-	private static double constantBike = -0.3;
-	private static double constantWalk = 0.854;
+	private static double constantBike = -0.4;
+	private static double constantWalk = 0.4;
 	
-	private static double beta_cost_car = 1.0; 
+	private static double beta_cost_car = 0.1; 
 	private static double beta_cost_pt = -0.117;
 	
 	private static double lambda_cost_income_car = 0.185;
@@ -130,20 +130,20 @@ public class JohScoringFunctionEstimation implements ScoringFunction {
 	private static final double uMin_leisure = 0;
 	
 	private static final double uMax_home = 5.5; 
-	private static final double uMax_innerHome = 1.0; 
-	private static final double uMax_work= 6.0;  
-	private static final double uMax_education = 4.5;
+	private static final double uMax_innerHome = 1.3; 
+	private static final double uMax_work= 7.0;  
+	private static final double uMax_education = 4.0;
 	private static final double uMax_shopping = 0.3; 
-	private static final double uMax_leisure = 2.3;  
+	private static final double uMax_leisure = 2.0;  
 	
 	private static final double alpha_home = 12;
-	private static final double alpha_innerHome = 1.9;
-	private static final double alpha_work = 8;
-	private static final double alpha_education = 6;
+	private static final double alpha_innerHome = 2.0;
+	private static final double alpha_work = 4;
+	private static final double alpha_education = 3.5;
 	private static final double alpha_shopping = 0.7;
-	private static final double alpha_leisure = 2;
+	private static final double alpha_leisure = 1.7;
 	
-	private static final double beta_home = 0.43;
+	private static final double beta_home = 0.249;
 	private static final double beta_innerHome = 15.2;
 	private static final double beta_work = 0.491;
 	private static final double beta_education = 2.29;
@@ -477,8 +477,8 @@ public class JohScoringFunctionEstimation implements ScoringFunction {
 		
 		type = "work";
 		actParams = new JohActUtilityParametersExtended("work", uMin_work, uMax_work, alpha_work, beta_work, gamma_work, beta_age_work);
-		actParams.setOpeningTime(8*3600);
-		actParams.setClosingTime(18*3600);
+		actParams.setOpeningTime(8.5*3600);
+		actParams.setClosingTime(19*3600);
 	//	actParams.setLatestStartTime(10*3600);
 	//	actParams.setEarliestEndTime(15*3600);
 		utilParams.put(type, actParams);
@@ -545,16 +545,16 @@ public class JohScoringFunctionEstimation implements ScoringFunction {
 		
 		type = "work_sector2";
 		actParams = new JohActUtilityParametersExtended("work_sector2", uMin_work, uMax_work, alpha_work, beta_work, gamma_work, beta_age_work);
-		actParams.setOpeningTime(8*3600);
-		actParams.setClosingTime(18*3600);
+		actParams.setOpeningTime(8.5*3600);
+		actParams.setClosingTime(19*3600);
 	//	actParams.setLatestStartTime(10*3600);
 	//	actParams.setEarliestEndTime(15*3600);
 		utilParams.put(type, actParams);
 		
 		type = "work_sector3";
 		actParams = new JohActUtilityParametersExtended("work_sector3", uMin_work, uMax_work, alpha_work, beta_work, gamma_work, beta_age_work);
-		actParams.setOpeningTime(8*3600);
-		actParams.setClosingTime(18*3600);
+		actParams.setOpeningTime(8.5*3600);
+		actParams.setClosingTime(19*3600);
 	//	actParams.setLatestStartTime(10*3600);
 	//	actParams.setEarliestEndTime(15*3600);
 		utilParams.put(type, actParams);
@@ -567,8 +567,8 @@ public class JohScoringFunctionEstimation implements ScoringFunction {
 		
 		type = "w";
 		actParams = new JohActUtilityParametersExtended("w", uMin_work, uMax_work, alpha_work, beta_work, gamma_work, beta_age_work);
-		actParams.setOpeningTime(8*3600);
-		actParams.setClosingTime(18*3600);
+		actParams.setOpeningTime(8.5*3600);
+		actParams.setClosingTime(19*3600);
 	//	actParams.setLatestStartTime(10*3600);
 	//	actParams.setEarliestEndTime(15*3600);
 		utilParams.put(type, actParams);
