fa7fc16e6c16f7f2f7932fb5c4a2d91737a4f815
tnicolai79 <tnicolai79@users.sf.net>
2010-03-26 11:22:07 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@10881 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/tnicolai/src/test/java/playground/tnicolai/urbansim/ReadFromUrbansimTestClass.java
@@ -0,0 +1,71 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * ReadFromUrbansimTestClass.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2010 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+/**
+ * 
+ */
+package playground.tnicolai.urbansim;
+
+/**
+ * @author thomas
+ *
+ */
+public class ReadFromUrbansimTestClass {
+
+	/**
+	 * compares the execution speed of different implementations to process urbansim data
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+//		ReadFromUrbansimParcelModel readFromUrbansim = new ReadFromUrbansimParcelModel( 2001 );
+//		
+//		long start, end, total1, total2;
+//		
+//		// case 1
+//
+//		start = System.currentTimeMillis();
+//		for(int i= 0; i < 10000; i++){
+//			ActivityFacilitiesImpl facilities = new ActivityFacilitiesImpl("urbansim locations (gridcells _or_ parcels _or_ ...)");
+//			ActivityFacilitiesImpl zones      = new ActivityFacilitiesImpl("urbansim zones");
+//			readFromUrbansim.readFacilities(facilities, zones);
+//		}
+//		end = System.currentTimeMillis();
+//		total1 = (end - start)/1000;
+//		
+//		// case 2
+//
+//		start = System.currentTimeMillis();
+//		for(int i= 0; i < 10000; i++){
+//			ActivityFacilitiesImpl facilities2 = new ActivityFacilitiesImpl("urbansim locations (gridcells _or_ parcels _or_ ...)");
+//			ActivityFacilitiesImpl zones2      = new ActivityFacilitiesImpl("urbansim zones");
+//			readFromUrbansim.readFacilities1(facilities2, zones2);
+//		}
+//		end = System.currentTimeMillis();
+//		total2 = (end - start)/1000;
+//		
+//		System.out.println("Time case 1: " + total1 + " sec" );
+//		System.out.println("Time case 2: " + total2 + " sec" );
+	
+	}
+
+}
+
