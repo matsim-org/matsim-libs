f877aeaf853470f266ab547d6514cbe0695a36d7
grether <dominik.grether@gmail.com>
2010-03-29 13:00:44 +0000

    playground for kyoto
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@10909 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/_template/src/main/java/WelcomeToMatsim.java
@@ -0,0 +1,35 @@
+
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * WelcomeToMatsim
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2010 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+/**
+ * @author dgrether
+ *
+ */
+public class WelcomeToMatsim {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("Welcome to the MATSim framework!");
+  }
+
+}
