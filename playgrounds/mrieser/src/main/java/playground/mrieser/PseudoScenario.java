76eeafad40b92cf7b6d5a341f47c02f7b48ae093
mrieser <rieser@gmail.com>
2010-03-30 11:45:03 +0000

    updated implementations of Scenario with the recently added features.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@10940 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/mrieser/src/main/java/playground/mrieser/PseudoScenario.java
+++ playgrounds/mrieser/src/main/java/playground/mrieser/PseudoScenario.java
@@ -12,42 +12,57 @@ import org.matsim.core.population.PopulationImpl;
 /**
  * Provides a real scenario, but exchanges the population.
  * Still, network and facilities can be reused that way.
- * 
+ *
  * @author mrieser
  */
 public class PseudoScenario implements Scenario {
-		
+
 	private final ScenarioImpl scenario;
 	private Population myPopulation;
-	
+
 	public PseudoScenario(final ScenarioImpl scenario, final PopulationImpl population) {
 		this.scenario = scenario;
 		this.myPopulation = population;
 	}
-	
+
 	@Override
 	public Population getPopulation() {
 		return this.myPopulation;
 	}
-	
+
 	@Override
 	public Coord createCoord(double x, double y) {
 		return this.scenario.createCoord(x, y);
 	}
-	
+
 	@Override
 	public Id createId(String string) {
 		return this.scenario.createId(string);
 	}
-	
+
 	@Override
 	public Config getConfig() {
 		return this.scenario.getConfig();
 	}
-	
+
 	@Override
 	public Network getNetwork() {
 		return this.scenario.getNetwork();
+	}
+
+	@Override
+	public void addScenarioElement(Object o) {
+		this.scenario.addScenarioElement(o);
+	}
+
+	@Override
+	public <T> T getScenarioElement(Class<? extends T> klass) {
+		return this.scenario.getScenarioElement(klass);
+	}
+
+	@Override
+	public boolean removeScenarioElement(Object o) {
+		return this.scenario.removeScenarioElement(o);
 	}
 
 }
