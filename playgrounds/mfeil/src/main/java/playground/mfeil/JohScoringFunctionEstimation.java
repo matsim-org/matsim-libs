7419c15be20b50ce0c1ab3c8da042fdcafcfb111
Matthias_Feil <Matthias_Feil@users.sf.net>
2010-03-31 13:34:37 +0000

    102 settings
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@10957 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/mfeil/src/main/java/playground/mfeil/JohScoringFunctionEstimation.java
+++ playgrounds/mfeil/src/main/java/playground/mfeil/JohScoringFunctionEstimation.java
@@ -91,14 +91,14 @@ public class JohScoringFunctionEstimation implements ScoringFunction {
 	private static double factorOfLateArrival = 3; 
 	private static double marginalUtilityOfEarlyDeparture = 0; 
 	
-	// Settings of 101
+	// Settings of 102
 	private static double beta_time_car = -3.2; 
 	private static double beta_time_pt = 0.563; 
 	private static double beta_time_bike = -1.07;
 	private static double beta_time_walk = -2; 
 	
-	private static double constantPt = -0.578;
-	private static double constantBike = -0.05;
+	private static double constantPt = -0.3;
+	private static double constantBike = -0.08;
 	private static double constantWalk = 0.4;
 	
 	private static double beta_cost_car = 0.0374; 
