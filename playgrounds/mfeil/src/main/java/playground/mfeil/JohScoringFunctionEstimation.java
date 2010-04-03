9213e9a8120fb178f97706087b334c971ae99d5d
Matthias_Feil <Matthias_Feil@users.sf.net>
2010-04-03 06:55:40 +0000

    112 settings
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@10992 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/mfeil/src/main/java/playground/mfeil/JohScoringFunctionEstimation.java
+++ playgrounds/mfeil/src/main/java/playground/mfeil/JohScoringFunctionEstimation.java
@@ -91,7 +91,7 @@ public class JohScoringFunctionEstimation implements ScoringFunction {
 	private static double factorOfLateArrival = 3; 
 	private static double marginalUtilityOfEarlyDeparture = 0; 
 	
-	// Settings of 111
+	// Settings of 112
 	private static double beta_time_car = -3.13; 
 	private static double beta_time_pt = 0.563; 
 	private static double beta_time_bike = -1.07;
@@ -131,14 +131,14 @@ public class JohScoringFunctionEstimation implements ScoringFunction {
 	
 	private static final double uMax_home = 5.41; 
 	private static final double uMax_innerHome = 1.1; 
-	private static final double uMax_work= 6.0;  
+	private static final double uMax_work= 5;  
 	private static final double uMax_education = 4.0;
-	private static final double uMax_shopping = 0.4; 
-	private static final double uMax_leisure = 2.0;  
+	private static final double uMax_shopping = 0.35; 
+	private static final double uMax_leisure = 1.9;  
 	
 	private static final double alpha_home = 12;
 	private static final double alpha_innerHome = 1.9;
-	private static final double alpha_work = 5;
+	private static final double alpha_work = 4.75;
 	private static final double alpha_education = 6;
 	private static final double alpha_shopping = 0.7;
 	private static final double alpha_leisure = 2;
