873cf3f96b645ca9aa1593cdbef534db9c04de5b
vsp_nagel <2008@kainagel.org>
2010-04-05 09:08:56 +0000

    extended a comment
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@11005 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/core/mobsim/framework/DriverAgent.java
+++ matsim/src/main/java/org/matsim/core/mobsim/framework/DriverAgent.java
@@ -37,7 +37,9 @@ public interface DriverAgent extends PersonAgentI {
 
 	// the methods below are yet unclear how useful they are in the interface, or if they should be moved to a Vehicle interface.
 
-	// yyyy "Teleportation" certainly does NOT belong into a vehicle.  Might go directly into the person.  kai, jan'10
+	// yyyy "Teleportation" certainly does NOT belong into a vehicle.  Also not into the driver.  
+	// Might go directly into the person, as some kind of minimal mobsim convenience method
+	// (although I am not convinced).  kai, jan/apr'10
 	public void teleportToLink(final Id linkId);
 
 	public void moveOverNode();
