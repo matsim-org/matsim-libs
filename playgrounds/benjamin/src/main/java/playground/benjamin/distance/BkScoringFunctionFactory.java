436346317920ddc72bda905a523b632eebade53e
benkick <benkick@users.sf.net>
2010-04-21 15:39:22 +0000

    playground refactoring
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@11177 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/benjamin/src/main/java/playground/benjamin/distance/BkScoringFunctionFactory.java
@@ -0,0 +1,44 @@
+/* *********************************************************************** *
+ * project: org.matsim.*																															*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.benjamin.distance;
+
+import org.matsim.api.core.v01.population.Plan;
+import org.matsim.core.config.groups.CharyparNagelScoringConfigGroup;
+import org.matsim.core.scoring.ScoringFunction;
+import org.matsim.core.scoring.ScoringFunctionFactory;
+
+
+/**
+ * @author dgrether
+ *
+ */
+public class BkScoringFunctionFactory implements ScoringFunctionFactory {
+
+	private CharyparNagelScoringConfigGroup configGroup;
+
+	public BkScoringFunctionFactory(
+			CharyparNagelScoringConfigGroup charyparNagelScoring) {
+		this.configGroup = charyparNagelScoring;
+	}
+
+	public ScoringFunction getNewScoringFunction(Plan plan) {
+		return new BkScoringFunction(plan, this.configGroup);
+	}
+
+}
