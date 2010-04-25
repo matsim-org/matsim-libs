e48e38820c245da15c549b40446721f8f78289ee
vsp_nagel <2008@kainagel.org>
2010-04-25 14:05:00 +0000

    added "MatsimParameters" marker interface
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@11259 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/main/java/org/matsim/core/api/internal/MatsimParameters.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package org.matsim.core.api.internal;
+
+/**Marker interface for classes that contain matsim parameters. 
+ * 
+ * @author nagel
+ */
+public interface MatsimParameters {
+
+}
