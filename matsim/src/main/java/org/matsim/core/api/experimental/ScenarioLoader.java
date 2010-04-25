038344dd8ad842150b19c02b05e477bef2bd1577
vsp_nagel <2008@kainagel.org>
2010-04-25 13:49:57 +0000

    made this inherit from a marker interface
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@11253 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/core/api/experimental/ScenarioLoader.java
+++ matsim/src/main/java/org/matsim/core/api/experimental/ScenarioLoader.java
@@ -1,8 +1,11 @@
 package org.matsim.core.api.experimental;
 
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.api.internal.MatsimSomeReader;
 
-public interface ScenarioLoader {
+public interface ScenarioLoader extends MatsimSomeReader {
+	// yy one can debate if this is really a "reader", but since the plan is to eventually be able to read
+	// the toplevel containers independently, this belongs into the same design "box". kai, apr'10
 
 	public Scenario loadScenario();
 	
