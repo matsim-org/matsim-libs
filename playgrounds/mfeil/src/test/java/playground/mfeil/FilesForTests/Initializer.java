430670a0efd62d0f84431f229495f285d8dace13
mrieser <rieser@gmail.com>
2010-04-26 09:55:32 +0000

    fix path to test-config
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@11284 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/mfeil/src/test/java/playground/mfeil/FilesForTests/Initializer.java
+++ playgrounds/mfeil/src/test/java/playground/mfeil/FilesForTests/Initializer.java
@@ -5,20 +5,20 @@ import org.matsim.core.gbl.Gbl;
 import org.matsim.testcases.MatsimTestCase;
 
 public class Initializer {
-	
+
 	private Controler controler;
-	
-	public Initializer() {	
+
+	public Initializer() {
 	}
-	
+
 	public void init(MatsimTestCase testCase) {
 		System.out.println(testCase.getPackageInputDirectory());
-		String path = "mfeil/"+ testCase.getPackageInputDirectory() + "config.xml";		
+		String path = testCase.getPackageInputDirectory() + "config.xml";
 		testCase.loadConfig(path);
 		this.controler = new ControlerForTests(Gbl.getConfig());
-		this.controler.setOverwriteFiles(true);		
+		this.controler.setOverwriteFiles(true);
 	}
-	
+
 	public void run (){
 		this.controler.run();
 	}
