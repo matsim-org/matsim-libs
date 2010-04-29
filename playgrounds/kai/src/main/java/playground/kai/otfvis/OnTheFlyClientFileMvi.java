c2cfcd490509549d3a886e1aa5d5bcfb40e50c9b
vsp_nagel <2008@kainagel.org>
2010-04-29 16:28:33 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@11329 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/kai/src/main/java/playground/kai/otfvis/OnTheFlyClientFileMvi.java
+++ playgrounds/kai/src/main/java/playground/kai/otfvis/OnTheFlyClientFileMvi.java
@@ -12,12 +12,14 @@ public class OnTheFlyClientFileMvi {
 		// String mviFile = "/home/nagel/vsp-cvs/runs/run628/it.500/500.events.mvi" ; // basecase new pt
 		// String mviFile = "/home/nagel/vsp-cvs/runs/run651/it.100/100.events.mvi" ; //gauteng
 		// String mviFile = "/home/nagel/vsp-cvs/runs/run650/it.600/600.events.mvi" ; // "same" as 628
-		 String mviFile = "/home/nagel/vsp-cvs/runs/run652/it.500/500.events.mvi" ; // "same" as 628
+//		 String mviFile = "/home/nagel/vsp-cvs/runs/run652/it.500/500.events.mvi" ; // "same" as 628
 //		 String mviFile = "/home/nagel/vsp-cvs/runs/run657/it.1000/1000.events.mvi" ; // best w/ new op.times
 
 //		 String mviFile = "/home/nagel/eclipse/matsim-trunk/myoutput/test1/ITERS/it.0/0.otfvis.mvi";
 //		 String mviFile = "/home/nagel/eclipse/matsim-trunk/psrc-parcel-output/ITERS/it.100/100.otfvis.mvi" ;
 //		String mviFile = "/home/nagel/eclipse/opus/opus_matsim/runs/25nov08-w-viaduct/ITERS/it.200/200.otfvis.mvi" ;
+		
+		String mviFile = "/Users/nagel/runs-svn/evac-no-risk-inc-moves/output/ITERS/it.0/0.movie.mvi" ;
 
 		new OTFClientFile(mviFile).run();
 	}
