d25d356311a2a64c7b8b7c99bf8c14b11d709592
rashid_waraich <rashid_waraich@users.sf.net>
2010-04-29 18:45:51 +0000

    adding chess parking case.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@11333 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/main/java/playground/wrashid/parkingSearch/planLevel/Main.java
+++ playgrounds/wrashid/src/main/java/playground/wrashid/parkingSearch/planLevel/Main.java
@@ -1,7 +1,18 @@
 package playground.wrashid.parkingSearch.planLevel;
 
+import org.matsim.core.controler.Controler;
+
+import playground.wrashid.lib.EventHandlerAtStartupAdder;
+import playground.wrashid.parkingSearch.ReplanParkingSearchRoute;
+
 public class Main {
 	public static void main(String[] args) {
+		Controler controler;
+		String configFilePath="test/input/playground/wrashid/parkingSearch/planLevel/chessConfig2.xml";
+		controler = new Controler(configFilePath);
 		
+		controler.setOverwriteFiles(true);
+		
+		controler.run();
 	}
 }
