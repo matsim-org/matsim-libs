9f1639a7b9bfbeb479a17b4b7dd03e00ba3c93b9
jwjoubert <johan.joubert@up.ac.za>
2010-04-30 20:50:12 +0000

    Fix code after refactorings.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@11360 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/jjoubert/src/main/java/playground/jjoubert/TemporaryCode/MyNetworkGenerator.java
+++ playgrounds/jjoubert/src/main/java/playground/jjoubert/TemporaryCode/MyNetworkGenerator.java
@@ -61,7 +61,7 @@ public class MyNetworkGenerator {
 			e.printStackTrace();
 		}
 		new NetworkCleaner().run(net);
-		new NetworkWriter(net).write("/Users/johanwjoubert/Desktop/Temp/network.xml");
+		new NetworkWriter(net).writeFile("/Users/johanwjoubert/Desktop/Temp/network.xml");
 		
 		Config c = new ScenarioImpl().getConfig();
 		c.global().setCoordinateSystem(UTM35S);
