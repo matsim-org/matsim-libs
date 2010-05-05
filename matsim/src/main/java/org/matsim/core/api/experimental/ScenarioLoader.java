699ed7a4770d8c6bcedf6df6f6cc49b64467f057
mrieser <rieser@gmail.com>
2010-05-05 08:05:42 +0000

    added gpl-header
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@11407 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/core/api/experimental/ScenarioLoader.java
+++ matsim/src/main/java/org/matsim/core/api/experimental/ScenarioLoader.java
@@ -1,3 +1,22 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2010 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
 package org.matsim.core.api.experimental;
 
 import org.matsim.api.core.v01.Scenario;
@@ -8,7 +27,7 @@ public interface ScenarioLoader extends MatsimSomeReader {
 	// the toplevel containers independently, this belongs into the same design "box". kai, apr'10
 
 	public Scenario loadScenario();
-	
-	public Scenario getScenario() ;
+
+	public Scenario getScenario();
 
 }
