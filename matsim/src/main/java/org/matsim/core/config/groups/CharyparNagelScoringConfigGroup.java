876da321c6de16ca9267beb1d9e9cf02abba13b8
vsp_nagel <2008@kainagel.org>
2010-05-20 07:52:27 +0000

    added some more logfile comments
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@11552 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/core/config/groups/CharyparNagelScoringConfigGroup.java
+++ matsim/src/main/java/org/matsim/core/config/groups/CharyparNagelScoringConfigGroup.java
@@ -269,6 +269,11 @@ public class CharyparNagelScoringConfigGroup extends Module {
 		map.put(WAITING, "[utils/hr] utility offset for waiting.  this comes on top of the opportunity cost of time.  Probably implemented correctly, but not tested.") ;
 		map.put(BRAIN_EXP_BETA, "[1/utils] conversion factor of utility model for use in logit-based choice model.  Set to 1 if your utl function is estimated") ;
 		map.put(LEARNING_RATE, "new_score = (1-learningRate)*old_score + learningRate * score_from_mobsim.  learning rates close to zero emulate score averaging, but slow down initial convergence") ;
+		
+		map.put(MARGINAL_UTL_OF_DISTANCE_CAR, "DON'T USE THIS!  It is not clear if this is in [utils/m] or in [Eu/m]!") ;
+		map.put(MARGINAL_UTL_OF_DISTANCE_PT, "DON'T USE THIS!  It is not clear if this is in [utils/m] or in [Eu/m]!") ;
+		map.put(MARGINAL_UTL_OF_DISTANCE_WALK, "DON'T USE THIS!  It is not clear if this is in [utils/m] or in [Eu/m]!") ;
+		
 		return map ;
 	}
 
