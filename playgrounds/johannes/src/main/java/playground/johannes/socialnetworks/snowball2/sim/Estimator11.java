fecee98abc493998493747253b81121d9119171e
mrieser <rieser@gmail.com>
2010-05-31 07:28:03 +0000

    removing bad import for UnsupportedOperationException
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@11654 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/johannes/src/main/java/playground/johannes/socialnetworks/snowball2/sim/Estimator11.java
+++ playgrounds/johannes/src/main/java/playground/johannes/socialnetworks/snowball2/sim/Estimator11.java
@@ -28,15 +28,12 @@ import gnu.trove.TObjectDoubleHashMap;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.collections.list.FixedSizeList;
 import org.matsim.contrib.sna.graph.Vertex;
 import org.matsim.contrib.sna.snowball.SampledGraph;
 import org.matsim.contrib.sna.snowball.SampledVertex;
 
 import playground.johannes.socialnetworks.statistics.Discretizer;
 import playground.johannes.socialnetworks.statistics.FixedSampleSizeDiscretizer;
-
-import visad.data.netcdf.UnsupportedOperationException;
 
 /**
  * @author illenberger
@@ -45,25 +42,25 @@ import visad.data.netcdf.UnsupportedOperationException;
 public class Estimator11 implements BiasedDistribution {
 
 	private SampleStats stats;
-	
+
 	private SampledGraph graph;
-	
+
 	private final int N;
-	
+
 	private TIntObjectHashMap<TObjectDoubleHashMap<SampledVertex>> probaItMap = new TIntObjectHashMap<TObjectDoubleHashMap<SampledVertex>>();
-	
+
 	private TIntObjectHashMap<TIntIntHashMap> numNeighborKIt = new TIntObjectHashMap<TIntIntHashMap>();
-	
+
 	private TIntObjectHashMap<TIntDoubleHashMap> probaNeighborKIt = new TIntObjectHashMap<TIntDoubleHashMap>();
-	
+
 	private Map<SampledVertex, TIntIntHashMap> deltaVertex = new HashMap<SampledVertex, TIntIntHashMap>();
-	
+
 	private Discretizer discretizer;
-	
+
 	public Estimator11(int N) {
 		this.N = N;
 	}
-	
+
 	@Override
 	public double getProbability(SampledVertex vertex) {
 		return getProbability(vertex, stats.getMaxIteration());
@@ -75,26 +72,26 @@ public class Estimator11 implements BiasedDistribution {
 			probas = new TObjectDoubleHashMap<SampledVertex>();
 			probaItMap.put(it, probas);
 		}
-		
+
 		double p = probas.get(vertex);
-		
+
 		if(p == 0) {
 			if(it == 0)
 				p = stats.getNumSampled(0)/(double)N;
-			
+
 			else if(it == 1) {
-				int n = stats.getAccumulatedNumSampled(it - 1);	
+				int n = stats.getAccumulatedNumSampled(it - 1);
 				p = 1 - Math.pow(1 - n/(double)N, vertex.getNeighbours().size());
-				
+
 			} else {
 				int k = vertex.getNeighbours().size();
-				
+
 				TIntIntHashMap numK = numNeighborKIt.get(it);
 				if(numK == null) {
 					numK = new TIntIntHashMap();
 					numNeighborKIt.put(it, numK);
 				}
-				
+
 				int num_k = 0;
 				if(numK.contains(k)) {
 					num_k = numK.get(k);
@@ -102,13 +99,13 @@ public class Estimator11 implements BiasedDistribution {
 					num_k = numWithNeighborK(it, k);
 					numK.put(k, num_k);
 				}
-				
+
 				TIntDoubleHashMap probaK = probaNeighborKIt.get(it);
 				if(probaK == null) {
 					probaK = new TIntDoubleHashMap();
 					probaNeighborKIt.put(it, probaK);
 				}
-				
+
 				double proba_k = 0;
 				if(probaK.contains(k)) {
 					proba_k = probaK.get(k);
@@ -116,65 +113,65 @@ public class Estimator11 implements BiasedDistribution {
 					proba_k = probaNeighborK(k, it);
 					probaK.put(k, proba_k);
 				}
-				
+
 				p = 1 - Math.pow(1 - num_k / proba_k, k);
 			}
-		
+
 			p *= stats.getResonseRate();
-			
+
 			probas.put(vertex, p);
 		}
-		
+
 		return p;
 	}
-	
+
 	private int numWithNeighborK(int it, int k) {
 		int count = 0;
-		
+
 		for(Vertex v : graph.getVertices()) {
-			
+
 			SampledVertex vertex = (SampledVertex)v;
 			if(vertex.isSampled() && vertex.getIterationSampled() < it) {
-				
+
 				count += hasNeighborK(vertex, k);
-				
+
 			}
-			
+
 		}
-		
+
 		return count;
 	}
-	
+
 	private double probaNeighborK(int k, int it) {
 //		int k = vertex.getNeighbours().size();
 		double p_sum = 0;
-		
+
 		for(Vertex v : graph.getVertices()) {
-			
+
 			SampledVertex sv = (SampledVertex)v;
 			if(sv.isSampled() && sv.getIterationSampled() < it) {
-				
+
 				int delta = hasNeighborK(sv, k);
-				
+
 				if(delta > 0) {
 					double p = getProbability(sv, it-1);
 					p_sum += delta/p;
 				}
 			}
 		}
-		
+
 		return p_sum;
 	}
-	
+
 	final double binsize = 10.0;
-	
+
 	private int hasNeighborK(SampledVertex vertex, int k) {
 		TIntIntHashMap deltas = deltaVertex.get(vertex);
 		if(deltas == null) {
 			deltas = new TIntIntHashMap();
 			deltaVertex.put(vertex, deltas);
 		}
-		
+
 		int delta = 0;
 		if(deltas.contains(k))
 			delta = deltas.get(k);
@@ -183,26 +180,26 @@ public class Estimator11 implements BiasedDistribution {
 			for(Vertex n : vertex.getNeighbours()) {
 				SampledVertex neighbor = (SampledVertex)n;
 				if(neighbor.isSampled()) {
-					int k_n = neighbor.getNeighbours().size(); 
+					int k_n = neighbor.getNeighbours().size();
 					if(discretize(k_n) == bin) {
 						delta = 1;
 						break;
 					}
 				}
 			}
-			
+
 			deltas.put(k, delta);
 		}
-		
+
 		return delta;
 	}
-	
+
 	private int discretize(int k) {
 //		int bin = (int) Math.floor(Math.log(k)/Math.log(2.0));
 //		return Math.max(bin, 0);
 		return (int) discretizer.discretize(k);
 	}
-	
+
 	@Override
 	public double getWeight(SampledVertex vertex) {
 		throw new UnsupportedOperationException();
@@ -212,7 +209,7 @@ public class Estimator11 implements BiasedDistribution {
 	public void update(SampledGraph graph) {
 		stats = new SampleStats(graph);
 		this.graph = graph;
-		
+
 		TDoubleArrayList degrees = new TDoubleArrayList(graph.getVertices().size());
 		for(Vertex vertex : graph.getVertices()) {
 			if(((SampledVertex)vertex).isSampled()) {
