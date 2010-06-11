a481b14e0020871267e8207246ecef2c6221decd
grether <dominik.grether@gmail.com>
2010-06-11 11:55:24 +0000

    interface for signalized links and lanes
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@11771 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/dgrether/src/main/java/playground/dgrether/signalsystems/SignalizedItem.java
@@ -0,0 +1,38 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * SignalizedItem
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2010 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.dgrether.signalsystems;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.signalsystems.control.SignalGroupState;
+
+
+/**
+ * @author dgrether
+ *
+ */
+public interface SignalizedItem {
+	
+	public void setSignalized(boolean isSignalized);
+	
+	public void setSignalStateAllTurningMoves(SignalGroupState state);
+	
+	public void setSignalStateForTurningMove(SignalGroupState state, Id toLinkId);
+
+}
