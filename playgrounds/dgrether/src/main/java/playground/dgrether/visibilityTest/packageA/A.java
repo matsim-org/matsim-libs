a0b72725b9a80a5cbf69fd4bc6f9c9931066a9c8
grether <dominik.grether@gmail.com>
2010-06-17 08:13:44 +0000

    visibility test classes for package proteced interfaces
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@11825 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/dgrether/src/main/java/playground/dgrether/visibilityTest/packageA/A.java
@@ -0,0 +1,27 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * A
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2010 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.dgrether.visibilityTest.packageA;
+
+
+public interface A {
+
+	public void testA();
+	
+}
---
+++ playgrounds/dgrether/src/main/java/playground/dgrether/visibilityTest/packageB/Main.java
@@ -0,0 +1,40 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * Main
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2010 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.dgrether.visibilityTest.packageB;
+
+import playground.dgrether.visibilityTest.packageA.A;
+import playground.dgrether.visibilityTest.packageA.AFactory;
+
+
+
+/**
+ * @author dgrether
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		A  a = new AFactory().createA();
+//		AA aa = (AA)a;
+	}
+}
