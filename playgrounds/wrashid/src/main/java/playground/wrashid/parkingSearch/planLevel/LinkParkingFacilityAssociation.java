f8104c5a1496260071204667ecd90d11b5ca0bf8
rashid_waraich <rashid_waraich@users.sf.net>
2010-07-09 18:29:08 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12035 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/wrashid/src/main/java/playground/wrashid/parkingSearch/planLevel/LinkParkingFacilityAssociation.java
@@ -0,0 +1,33 @@
+package playground.wrashid.parkingSearch.planLevel;
+
+import java.util.ArrayList;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.core.facilities.ActivityFacilitiesImpl;
+import org.matsim.core.facilities.ActivityFacilityImpl;
+import org.matsim.core.network.NetworkLayer;
+
+public class LinkParkingFacilityAssociation extends LinkFacilityAssociation {
+
+	public LinkParkingFacilityAssociation(ActivityFacilitiesImpl facilities, NetworkLayer network) {
+		this.network=network;
+		
+		for (ActivityFacilityImpl facility: facilities.getFacilities().values()){
+			addFacilityToHashMap(facility);
+		}		
+	}
+	
+	private void addFacilityToHashMap(ActivityFacilityImpl facility) {
+		Id facilityLink=getClosestLink(facility);
+		
+		assureHashMapInitializedForLink(facilityLink);
+		
+		ArrayList<ActivityFacilityImpl> list=linkFacilityMapping.get(facilityLink);
+		
+		if (facility.getActivityOptions().containsKey("parking")){
+			list.add(facility);
+		}
+	}
+
+	
+}
