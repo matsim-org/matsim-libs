008af100fc0fc6b63dd3f05b0e2f4d929cebbf53
rashid_waraich <rashid_waraich@users.sf.net>
2010-07-09 18:29:25 +0000

    refactoring.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12036 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/test/java/playground/wrashid/parkingSearch/planLevel/LinkFacilityAssociationTest.java
+++ playgrounds/wrashid/src/test/java/playground/wrashid/parkingSearch/planLevel/LinkFacilityAssociationTest.java
@@ -12,31 +12,8 @@ import org.matsim.core.population.PopulationReader;
 import org.matsim.testcases.MatsimTestCase;
 
 public class LinkFacilityAssociationTest extends MatsimTestCase {
-/*
-	public void testConstructor(){   
-		Controler controler;
-		String configFilePath="test/input/playground/wrashid/parkingSearch/planLevel/chessConfig2.xml";
-		controler = new Controler(configFilePath);
-	
-		
-		controler.setOverwriteFiles(true);
-		
-		controler.run();
-		
-		LinkFacilityAssociation lfa=new LinkFacilityAssociation(controler);
-		
-		assertEquals("1", lfa.getFacilities(new IdImpl("1")).get(0).getId().toString());
-		
-		
-		
-		
-		
-	}	
-	*/
-	
-	public void testGeneralFacility(){
-		ScenarioImpl sc = new ScenarioImpl();
-		
+
+	public static NetworkLayer loadNetwork(ScenarioImpl sc) {
 		String facilitiesPath = "test/input/playground/wrashid/parkingSearch/planLevel/chessFacilities.xml";
 		String networkFile = "test/input/playground/wrashid/parkingSearch/planLevel/network.xml";
 
@@ -44,47 +21,44 @@ public class LinkFacilityAssociationTest extends MatsimTestCase {
 
 		Population inPop = sc.getPopulation();
 
-		NetworkLayer net = sc.getNetwork();
 		new MatsimNetworkReader(sc).readFile(networkFile);
 
-		
-		LinkFacilityAssociation lfa=new LinkFacilityAssociation(sc.getActivityFacilities(),net);
-		
+		return sc.getNetwork();
+	}
+
+	public void testGeneralFacility() {
+		ScenarioImpl sc = new ScenarioImpl();
+
+		NetworkLayer net = loadNetwork(sc);
+
+		LinkFacilityAssociation lfa = new LinkFacilityAssociation(sc.getActivityFacilities(), net);
+
 		// parking is possible at facility 19
-		
+
 		assertEquals("19", lfa.getFacilities(new IdImpl("1")).get(0).getId().toString());
 		assertEquals(1, lfa.getFacilities(new IdImpl("1")).size());
-		
-		// only leisure is possible at facility 42 => used in the parking test afterwards for verification
+
+		// only leisure is possible at facility 42 => used in the parking test
+		// afterwards for verification
 		assertEquals("42", lfa.getFacilities(new IdImpl("50")).get(0).getId().toString());
 		assertEquals(1, lfa.getFacilities(new IdImpl("50")).size());
 	}
-	
-	public void testParkingFacility(){
+
+	public void testParkingFacility() {
 		ScenarioImpl sc = new ScenarioImpl();
-		
-		String facilitiesPath = "test/input/playground/wrashid/parkingSearch/planLevel/chessFacilities.xml";
-		String networkFile = "test/input/playground/wrashid/parkingSearch/planLevel/network.xml";
 
-		new MatsimFacilitiesReader(sc).readFile(facilitiesPath);
+		NetworkLayer net = loadNetwork(sc);
 
-		Population inPop = sc.getPopulation();
+		LinkParkingFacilityAssociation lpfa = new LinkParkingFacilityAssociation(sc.getActivityFacilities(), net);
 
-		NetworkLayer net = sc.getNetwork();
-		new MatsimNetworkReader(sc).readFile(networkFile);
-
-		
-		LinkParkingFacilityAssociation lpfa=new LinkParkingFacilityAssociation(sc.getActivityFacilities(),net);
-		
 		// parking is possible at facility 19
-		
+
 		assertEquals("19", lpfa.getFacilities(new IdImpl("1")).get(0).getId().toString());
 		assertEquals(1, lpfa.getFacilities(new IdImpl("1")).size());
-		
-		// only shopping is possible at facility 42 => no parking available at link 50
+
+		// only shopping is possible at facility 42 => no parking available at
+		// link 50
 		assertEquals(0, lpfa.getFacilities(new IdImpl("50")).size());
 	}
-	
-	
-	
+
 }
