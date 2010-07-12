2acb310ba3289afead117a09999e03ca057d826d
rashid_waraich <rashid_waraich@users.sf.net>
2010-07-12 16:08:21 +0000

    adding class.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12049 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/wrashid/src/main/java/playground/wrashid/lib/Reflection.java
@@ -0,0 +1,30 @@
+package playground.wrashid.lib;
+
+import java.lang.reflect.Field;
+
+import org.matsim.core.population.ActivityImpl;
+
+public class Reflection {
+
+	public static void setField(Object targetObject, String fieldName,Object newValue){
+		Class<?> c = targetObject.getClass();
+	    try {
+			Field f = c.getDeclaredField(fieldName);
+			f.setAccessible(true);
+			f.set(targetObject, newValue);
+	    } catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
