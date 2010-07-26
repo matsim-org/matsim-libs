3e38bd22e939e032ff847f8347cfb11a4c2fbd56
fuerbas <fuerbas@users.sf.net>
2010-07-26 12:23:23 +0000

    new version with output of MatrixCentrality LinkId / MATSim LinkId and output of MatrixCentrality LinkId with Betweenness
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12169 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/fuerbas/src/main/java/matrix/MatsimToMatrix.java
+++ playgrounds/fuerbas/src/main/java/matrix/MatsimToMatrix.java
@@ -1,5 +1,10 @@
 package matrix;
 
+import gnu.trove.TIntLongHashMap;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -22,27 +27,23 @@ import playground.johannes.socialnetworks.graph.matrix.WeightedDijkstraFactory;
 public class MatsimToMatrix {
 	
 	@SuppressWarnings("unchecked")
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
 	
 	Scenario scenario = new ScenarioImpl();
 	NetworkLayer network = (NetworkLayer) scenario.getNetwork();
 	new MatsimNetworkReader(scenario).readFile(args[0]);
+	BufferedWriter bwLinks = new BufferedWriter(new FileWriter(args[1]));	// Output Directory for List of Links
+	BufferedWriter bwBetw = new BufferedWriter(new FileWriter(args[2]));	//Output Directory for Betweenness Calculation Results
 	
 	final List<Node> nodeList = new ArrayList<Node>(network.getNodes().size());
-	final List<Link> linkList = new ArrayList<Link>(network.getLinks().size());
-//	final int[][] linkIdxFromTo = new int[network.getLinks().size()][network.getLinks().size()];
-	
+	final List<Link> linkList = new ArrayList<Link>(network.getLinks().size());	
 	final List<HashMap<Integer, Double>> costList = new ArrayList<HashMap<Integer, Double>>();
 	
 	AdjacencyMatrix y = new AdjacencyMatrix();
-	
-	System.out.println("EINLESEN DER MATRIX KOMPLETT "+Runtime.getRuntime().freeMemory());
-	
+		
 	for (Node node : network.getNodes().values()) {
 		int idx = y.addVertex();
-//		System.out.println(idx+node.toString());
 		nodeList.add(idx, node);
-//		System.out.println(nodeList.get(idx));
 	}
 	
 	for (int i=0; i<=network.getLinks().size(); i++){
@@ -57,44 +58,16 @@ public class MatsimToMatrix {
 			to=nodeList.indexOf(link.getToNode());
 			if (!y.getEdge(to, from) || !y.getEdge(from, to)) {
 			y.addEdge(nodeList.indexOf(link.getFromNode()), nodeList.indexOf(link.getToNode()));
-			linkList.add(linkIdx, link);
-			
-//			linkIdxFromTo[nodeList.indexOf(link.getFromNode())][nodeList.indexOf(link.getToNode())] = linkIdx;
-//			linkIdx++;			
-//			System.out.println(linkIdx);
-			
-//			if (costList.get(nodeList.indexOf(link.getFromNode())) == null){
-//			costList.add(nodeList.indexOf(link.getFromNode()), new HashMap<Integer, Double>());
-			
-			costList.get(from).put((to), link.getLength()/link.getFreespeed());
+			linkList.add(linkIdx, link);		
+			costList.get(from).put((to), link.getLength()/link.getFreespeed());					
 			costList.get(to).put((from), link.getLength()/link.getFreespeed());
-		
-			
-//			System.out.println("FROM "+from+" TO "+to);		
-//			System.out.println(costList.get(from).get(to));
-	
-//			}
-//			else 
-//			costList.get(nodeList.indexOf(link.getFromNode())).put(nodeList.indexOf(link.getToNode()), link.getLength()/link.getFreespeed());
+			bwLinks.write(linkIdx+"\t"+link.getId().toString());
+			bwLinks.newLine();
+			linkIdx++;
 			}
 		}			
 	}
-	
-	System.out.println("LINK ADDIEREN KOMPLETT "+Runtime.getRuntime().freeMemory());
-	
-//	for (int i=0; i<100; i++){
-//		for (int j=0; j<100; j++)
-//			if (linkIdxFromTo[i][j]>0){
-//			System.out.println(linkIdxFromTo[i][j]);
-//			System.out.println(linkList.get(linkIdxFromTo[i][j]).getFreespeed());
-//			System.out.println("COSTS "+(linkList.get(linkIdxFromTo[i][j]).getLength()/linkList.get(linkIdxFromTo[i][j]).getFreespeed()));
-//			if (linkList.get(linkIdxFromTo[i][j]).getLength()/linkList.get(linkIdxFromTo[i][j]).getFreespeed()==0 || linkList.get(linkIdxFromTo[i][j]).getLength()/linkList.get(linkIdxFromTo[i][j]).getFreespeed() == Double.POSITIVE_INFINITY)
-//					System.out.println("COSTS ZERO OR POSITIVE INFINITY!!!");
-//			}
-//	}
-	
-	System.out.println("EDGE ADDIEREN KOMPLETT "+Runtime.getRuntime().freeMemory());
-	
+	bwLinks.close();
 	
 	
 	class EdgeCost implements EdgeCostFunction {
@@ -102,8 +75,6 @@ public class MatsimToMatrix {
 		private double costs;
 		@Override
 		public double edgeCost(int i, int j) {
-//			int linkIndex = linkIdxFromTo[i][j];  
-//			costs=(linkList.get(linkIndex).getLength()/linkList.get(linkIndex).getFreespeed());		
 			
 			costs = costList.get(i).get(j);
 			return costs;
@@ -126,7 +97,21 @@ public class MatsimToMatrix {
 	MatrixCent.run(y);
 	System.out.println("MATRIX CENT RUN CPLT "+Runtime.getRuntime().freeMemory());
 	
-	int[] betweenness = MatrixCent.getVertexBetweenness();
-//	for (int i=0; i<betweenness.length; i++) System.out.println("BETWEENNESS: "+betweenness[i]);
+	TIntLongHashMap[] betweenness = MatrixCent.getEdgeBetweenness();
+
+	Long betwFromTo;	
+	
+	for (Link link : linkList) {
+
+		Integer from=0; Integer to=0;		
+		from=nodeList.indexOf(link.getFromNode());
+		to=nodeList.indexOf(link.getToNode());		
+		betwFromTo=betweenness[from].get(to);
+		
+		bwBetw.write(link.getId().toString()+"\t"+betwFromTo);	//Output: linkIdx TAB Betweenness
+		bwBetw.newLine();
+				
+	}
+	bwBetw.close();
 	}
 }
