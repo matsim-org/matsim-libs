4e89f63ce1035aa622883ecbfc740a29f28f2765
mrieser <rieser@gmail.com>
2010-08-02 10:46:13 +0000

    improved bugfix from r12201 for more special cases, and added tests for it.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12203 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/core/mobsim/framework/listeners/SimulationListenerManager.java
+++ matsim/src/main/java/org/matsim/core/mobsim/framework/listeners/SimulationListenerManager.java
@@ -20,9 +20,6 @@
 
 package org.matsim.core.mobsim.framework.listeners;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.swing.event.EventListenerList;
 
 import org.apache.log4j.Logger;
@@ -35,6 +32,7 @@ import org.matsim.core.mobsim.framework.events.SimulationBeforeSimStepEvent;
 import org.matsim.core.mobsim.framework.events.SimulationBeforeSimStepEventImpl;
 import org.matsim.core.mobsim.framework.events.SimulationInitializedEvent;
 import org.matsim.core.mobsim.framework.events.SimulationInitializedEventImpl;
+import org.matsim.core.utils.misc.ClassUtils;
 
 /**
  * Helper for the QueueSimulation to manage their listeners and fire
@@ -44,46 +42,30 @@ import org.matsim.core.mobsim.framework.events.SimulationInitializedEventImpl;
  */
 public class SimulationListenerManager<T extends Simulation> {
 
-	private static final Logger log = Logger.getLogger("noname");
+	private final static Logger log = Logger.getLogger(SimulationListenerManager.class);
 
-	private final T queuesim;
+	private final T sim;
 
 	private final EventListenerList listenerList = new EventListenerList();
 
-	public SimulationListenerManager(T qsim){
-		this.queuesim = qsim;
+	public SimulationListenerManager(T sim){
+		this.sim = sim;
 	}
-		
+
 	@SuppressWarnings("unchecked")
-	public void addQueueSimulationListener(final SimulationListener l) {
-		log.warn( "calling addQueueSimulationListener ") ;
-		Set<Class> interfaces = new HashSet<Class>();
-		Class<?> test = l.getClass();
-		while (test != Object.class) {
-			for (Class<?> theInterface : test.getInterfaces()) {
-				interfaces.add(theInterface);
-			}
-			test = test.getSuperclass();
-		}
-		for (Class interfaceClass : interfaces) {
+	public void addQueueSimulationListener(final SimulationListener<T> l) {
+		log.info("calling addQueueSimulationListener");
+		for (Class interfaceClass : ClassUtils.getAllTypes(l.getClass())) {
 			if (SimulationListener.class.isAssignableFrom(interfaceClass)) {
 				this.listenerList.add(interfaceClass, l);
-				log.warn( " just assigned class " + SimulationListener.class.getName() + " to interface " + interfaceClass.getName() ) ;
+				log.info("  assigned class " + SimulationListener.class.getName() + " to interface " + interfaceClass.getName());
 			}
 		}
 	}
 
 	@SuppressWarnings("unchecked")
-	public void removeQueueSimulationListener(final SimulationListener l) {
-		Set<Class> interfaces = new HashSet<Class>();
-		Class<?> test = l.getClass();
-		while (test != Object.class) {
-			for (Class<?> theInterface : test.getInterfaces()) {
-				interfaces.add(theInterface);					
-			}
-			test = test.getSuperclass();
-		}
-		for (Class interfaceClass : interfaces) {
+	public void removeQueueSimulationListener(final SimulationListener<T> l) {
+		for (Class interfaceClass : ClassUtils.getAllTypes(l.getClass())) {
 			if (SimulationListener.class.isAssignableFrom(interfaceClass)) {
 				this.listenerList.remove(interfaceClass, l);
 			}
@@ -94,9 +76,8 @@ public class SimulationListenerManager<T extends Simulation> {
 	 * Creates the event and notifies all listeners
 	 */
 	public void fireQueueSimulationInitializedEvent() {
-		SimulationInitializedEvent event = new SimulationInitializedEventImpl(queuesim);
+		SimulationInitializedEvent event = new SimulationInitializedEventImpl(sim);
 		SimulationInitializedListener[] listener = this.listenerList.getListeners(SimulationInitializedListener.class);
-		log.warn("firing simulationInitializedEvent") ;
     for (int i = 0; i < listener.length; i++) {
     	listener[i].notifySimulationInitialized(event);
     }
@@ -104,11 +85,11 @@ public class SimulationListenerManager<T extends Simulation> {
 
 	/**
 	 * Creates the event and notifies all listeners
-	 * 
+	 *
 	 * @param simTime the current time in the simulation
 	 */
 	public void fireQueueSimulationAfterSimStepEvent(final double simTime) {
-		SimulationAfterSimStepEvent event = new SimulationAfterSimStepEventImpl(queuesim, simTime);
+		SimulationAfterSimStepEvent event = new SimulationAfterSimStepEventImpl(sim, simTime);
 		SimulationAfterSimStepListener[] listener = this.listenerList.getListeners(SimulationAfterSimStepListener.class);
 		for (int i = 0; i < listener.length; i++) {
 			listener[i].notifySimulationAfterSimStep(event);
@@ -119,7 +100,7 @@ public class SimulationListenerManager<T extends Simulation> {
 	 * Creates the event and notifies all listeners
 	 */
 	public void fireQueueSimulationBeforeCleanupEvent(){
-		SimulationBeforeCleanupEvent event = new SimulationBeforeCleanupEventImpl(this.queuesim);
+		SimulationBeforeCleanupEvent event = new SimulationBeforeCleanupEventImpl(this.sim);
 		SimulationBeforeCleanupListener[] listener = this.listenerList.getListeners(SimulationBeforeCleanupListener.class);
 		for (int i = 0; i < listener.length; i++){
 			listener[i].notifySimulationBeforeCleanup(event);
@@ -127,7 +108,7 @@ public class SimulationListenerManager<T extends Simulation> {
 	}
 
 	public void fireQueueSimulationBeforeSimStepEvent(double time) {
-		SimulationBeforeSimStepEvent event = new SimulationBeforeSimStepEventImpl(queuesim, time);
+		SimulationBeforeSimStepEvent event = new SimulationBeforeSimStepEventImpl(sim, time);
 		SimulationBeforeSimStepListener[] listener = this.listenerList.getListeners(SimulationBeforeSimStepListener.class);
 		for (int i = 0; i < listener.length; i++) {
 			listener[i].notifySimulationBeforeSimStep(event);
