2cb2426f893af7178ce1960a8a01893c9c9fc8d6
grether <dominik.grether@gmail.com>
2010-08-04 16:04:26 +0000

    commented dependencies to other playground
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12237 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/dgrether/src/main/java/playground/dgrether/tests/satellic/DgSatellicNetwork2WGS84Converter.java
+++ playgrounds/dgrether/src/main/java/playground/dgrether/tests/satellic/DgSatellicNetwork2WGS84Converter.java
@@ -22,43 +22,71 @@ package playground.dgrether.tests.satellic;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.ScenarioImpl;
+import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Node;
 import org.matsim.core.network.MatsimNetworkReader;
 import org.matsim.core.network.NetworkWriter;
+import org.matsim.core.utils.geometry.geotools.MGC;
 import org.matsim.core.utils.geometry.transformations.GeotoolsTransformation;
 import org.matsim.core.utils.geometry.transformations.TransformationFactory;
+import org.matsim.utils.gis.matsim2esri.network.FeatureGenerator;
+import org.matsim.utils.gis.matsim2esri.network.FeatureGeneratorBuilder;
+import org.matsim.utils.gis.matsim2esri.network.LineStringBasedFeatureGenerator;
+import org.matsim.utils.gis.matsim2esri.network.Links2ESRIShape;
+import org.matsim.utils.gis.matsim2esri.network.WidthCalculator;
 
 import playground.dgrether.DgPaths;
 import playground.dgrether.visualization.KmlNetworkVisualizer;
-import playground.gregor.gis.coordinatetransform.ApproximatelyCoordianteTransformation;
 
 
 public class DgSatellicNetwork2WGS84Converter {
 	
 	public static void main(String[] args) {
 		
-		String netbase = DgPaths.SHAREDSVN + "studies/countries/de/prognose_2025/demand/network_ab";
-		String net = netbase + ".xml";
+//		String netbase = DgPaths.SHAREDSVN + "studies/countries/de/prognose_2025/demand/network_ab";
+		String netbase = DgPaths.SHAREDSVN + "studies/countries/de/prognose_2025/demand/network_cleaned";
+		String net = netbase + ".xml.gz";
 		String f = DgPaths.SCMWORKSPACE + "shared-svn/studies/countries/de/prognose_2025/orig/netze/coordinateTransformationLookupTable.csv";
-		ApproximatelyCoordianteTransformation transform = new ApproximatelyCoordianteTransformation(f);
+//		ApproximatelyCoordianteTransformation transform = new ApproximatelyCoordianteTransformation(f);
 
-		String netOut = netbase + "_wgs84.xml";
+		String netOut = netbase + "_wgs84.xml.gz";
 
 		Scenario sc = new ScenarioImpl();
 		MatsimNetworkReader reader = new MatsimNetworkReader(sc);
 		reader.readFile(net);
 
-		Coord c;
+		Coord c = null;
 		for (Node n : sc.getNetwork().getNodes().values()){
-			c = transform.getTransformed(n.getCoord());
+//			c = transform.getTransformed(n.getCoord());
 			n.getCoord().setXY(c.getX(), c.getY());
 		}
+		
+		NetworkWriter netWriter = new NetworkWriter(sc.getNetwork());
+		netWriter.write(netOut);
 		
 		KmlNetworkVisualizer kmlwriter = new KmlNetworkVisualizer(sc.getNetwork());
 		kmlwriter.write(netbase + ".kmz", new GeotoolsTransformation(TransformationFactory.WGS84, TransformationFactory.WGS84));
 		
-		NetworkWriter netWriter = new NetworkWriter(sc.getNetwork());
-		netWriter.write(netOut);
+		//write shape file
+		final WidthCalculator wc = new WidthCalculator() {
+			@Override
+			public double getWidth(Link link) {
+				return 1.0;
+			}
+		};
+		
+		FeatureGeneratorBuilder builder = new FeatureGeneratorBuilder() {
+			@Override
+			public FeatureGenerator createFeatureGenerator() {
+				FeatureGenerator fg = new LineStringBasedFeatureGenerator(wc, MGC.getCRS(TransformationFactory.WGS84));
+				return fg;
+			}
+		};
+		Links2ESRIShape linksToEsri = new Links2ESRIShape(sc.getNetwork(), netbase + "_wgs84.shp", builder);
+		linksToEsri.write();
+
+		
+		
 	}
 	
 }
