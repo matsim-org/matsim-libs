ce894bb82a7fdad510a3b7608f2f31490e84f8df
Matthias_Feil <Matthias_Feil@users.sf.net>
2010-08-05 18:53:05 +0000

    final test cases
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12261 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/mfeil/src/test/java/playground/mfeil/PlanomatXTest.java
+++ playgrounds/mfeil/src/test/java/playground/mfeil/PlanomatXTest.java
@@ -33,7 +33,6 @@ import org.matsim.locationchoice.constrained.LocationMutatorwChoiceSet;
 import org.matsim.planomat.costestimators.DepartureDelayAverageCalculator;
 import org.matsim.testcases.MatsimTestCase;
 import playground.mfeil.FilesForTests.Initializer;
-import playground.mfeil.attributes.ActivityTypeFinder;
 
 
 
