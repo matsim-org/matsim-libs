490e737b016f83ba24c41375ca129f1c8cd972cc
mrieser <rieser@gmail.com>
2010-08-06 13:35:19 +0000

    add GPL header
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12269 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/main/java/playground/wrashid/parkingSearch/planLevel/scenario/BaseControlerScenario.java
+++ playgrounds/wrashid/src/main/java/playground/wrashid/parkingSearch/planLevel/scenario/BaseControlerScenario.java
@@ -1,20 +1,33 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2010 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
 package playground.wrashid.parkingSearch.planLevel.scenario;
 
 import org.matsim.core.controler.Controler;
 
 import playground.wrashid.lib.EventHandlerAtStartupAdder;
 import playground.wrashid.parkingSearch.planLevel.init.InitializeParkings;
-import playground.wrashid.parkingSearch.planLevel.init.ParkingRoot;
 import playground.wrashid.parkingSearch.planLevel.occupancy.FinishParkingOccupancyMaintainer;
 import playground.wrashid.parkingSearch.planLevel.occupancy.ParkingBookKeeper;
-import playground.wrashid.parkingSearch.planLevel.parkingPrice.IncomeRelevantForParking;
-import playground.wrashid.parkingSearch.planLevel.parkingPrice.ParkingPriceMapping1;
-import playground.wrashid.parkingSearch.planLevel.scoring.ParkingScoreExecutor;
-import playground.wrashid.parkingSearch.planLevel.scoring.ParkingScoringFunction;
-import playground.wrashid.parkingSearch.planLevel.scoring.ParkingDefaultScoringFunction;
 
 public class BaseControlerScenario {
-	
+
 	public ParkingBookKeeper parkingBookKeeper;
 
 	public BaseControlerScenario(Controler controler) {
@@ -28,10 +41,10 @@ public class BaseControlerScenario {
 		this.parkingBookKeeper = new ParkingBookKeeper(controler);
 		eventHandlerAdder.addEventHandler(parkingBookKeeper);
 		controler.addControlerListener(eventHandlerAdder);
-		
+
 		controler.addControlerListener(new FinishParkingOccupancyMaintainer());
-		
-		 
+
+
 	}
 
 }
