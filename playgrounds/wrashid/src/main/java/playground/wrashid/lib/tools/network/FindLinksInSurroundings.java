a7d1011515ebf407b19bb4a1b83a65dbb01ee025
rashid_waraich <rashid_waraich@users.sf.net>
2010-08-09 14:28:45 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12285 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/wrashid/src/main/java/playground/wrashid/lib/tools/network/FindLinksInSurroundings.java
@@ -0,0 +1,16 @@
+package playground.wrashid.lib.tools.network;
+
+
+/**
+ * Sometimes, one need to find out the id, coordinates of the links within a certain radius from a coordinate.
+ * @author wrashid
+ *
+ */
+public class FindLinksInSurroundings {
+
+	public static void main(String[] args) {
+		String inputNetworkPath="";
+		
+	}
+	
+}
