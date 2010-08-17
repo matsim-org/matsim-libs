493f0f66bac71c5083a4be10166ed6440dbc5cb6
jillenberger <jillenberger@users.sf.net>
2010-08-17 13:45:33 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12353 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/fuerbas/src/main/java/matrix/BetweennessLoader.java
@@ -0,0 +1,70 @@
+package matrix;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import gnu.trove.TObjectLongHashMap;
+
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.network.Node;
+
+public class BetweennessLoader {
+
+	public static TObjectLongHashMap<Link> loadBetweenness(String file, Network network) throws IOException {
+		/*
+		 * Create node list...
+		 */
+//		logger.info("Indexing nodes...");
+		List<Node> nodeList = new ArrayList<Node>(network.getNodes().size());
+		for(Node node : network.getNodes().values()) {
+			nodeList.add(node);
+		}
+		/*
+		 * Load betweenness data
+		 */
+		TObjectLongHashMap<Link> values = new TObjectLongHashMap<Link>();
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		String line;
+		int cnt = 0;
+		while((line = reader.readLine()) != null) {
+			String tokens[] = line.split(" ");
+			
+			int fromNodeIdx = Integer.parseInt(tokens[1]);
+			int toNodeIdx = Integer.parseInt(tokens[3]);
+			
+			Node fromNode = nodeList.get(fromNodeIdx);
+			Node toNode = nodeList.get(toNodeIdx);
+			
+			Link link = null;
+			for(Link outLink : fromNode.getOutLinks().values()) {
+				if(outLink.getToNode() == toNode)
+					link = outLink;
+			}
+			
+//			if(link == null) {
+//				logger.warn("Link not found!");
+//				System.exit(-1);
+//			}
+//			linkSet.add(link);
+			
+			line = reader.readLine();
+			
+			long value = Long.parseLong(line);
+			if(value < 0) {
+//				logger.warn("Value < 0!");
+//				System.exit(-1);
+			} else {
+				values.put(link, value);
+			}
+			if(value < nodeList.size()) {
+				cnt++;
+			}
+		}
+		
+		return values;
+	}
+}
--- playgrounds/fuerbas/src/main/java/matrix/NetworkScaler.java
+++ playgrounds/fuerbas/src/main/java/matrix/NetworkScaler.java
@@ -3,13 +3,11 @@
  */
 package matrix;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import gnu.trove.TObjectLongHashMap;
+import gnu.trove.TObjectLongIterator;
+
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map.Entry;
 
 import org.matsim.api.core.v01.Id;
@@ -17,8 +15,9 @@ import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.ScenarioImpl;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.network.Node;
 import org.matsim.core.network.MatsimNetworkReader;
-import org.matsim.core.network.NetworkLayer;
+import org.matsim.core.network.NetworkWriter;
 import org.matsim.run.NetworkCleaner;
 
 /**
@@ -32,44 +31,83 @@ public class NetworkScaler {
 	 * @throws IOException 
 	 */
 	
-	final static double BETW_MIN = 1500000.;	//minimum betweenness required to remain in network
-	final static double CAPACITY_MIN = 600;	//minimim capacity required to remain in network
+	static double BETW_MIN = 1500000.;	//minimum betweenness required to remain in network
+	final static double CAPACITY_MIN = Double.MAX_VALUE;	//minimim capacity required to remain in network
 	
 	public static void main(String[] args) throws IOException {
 		Scenario scenario = new ScenarioImpl();
-		new MatsimNetworkReader(scenario).readFile(args[0]);	//provide MATSim network
+		new MatsimNetworkReader(scenario).readFile("/Users/jillenberger/Work/work/socialnets/data/schweiz/network/switzerland_matsim_cl_simple.xml");	//provide MATSim network
 		Network network = scenario.getNetwork();
 		HashMap<String, Double> linkBetw = new HashMap<String, Double>();
 		HashMap<Id, Link> linkDeletion = new HashMap<Id, Link>();	//HashMap containing all links up for deletion from network
 //		List<Link> linkDeletion = new ArrayList<Link>();
 		Double betw = 0.;
 		String matsimLinkId = null;
-		BufferedReader br = new BufferedReader(new FileReader(args[1]));	//as args[1] provide file with results from MatrixCentrality in format: Matsim Id "TAB" Betweenness
 		
-		while (br.ready()) {
-			String aLine = br.readLine();
-			String splitLine[] = aLine.split("\t");
-			matsimLinkId = splitLine[0].trim();
-			betw = Double.parseDouble(splitLine[1]);
-			linkBetw.put(matsimLinkId, betw);
-		}
+//		BETW_MIN = network.getNodes().size();
+		BETW_MIN = 200000;
+//		BufferedReader br = new BufferedReader(new FileReader(args[1]));	//as args[1] provide file with results from MatrixCentrality in format: Matsim Id "TAB" Betweenness
+//		
+//		while (br.ready()) {
+//			String aLine = br.readLine();
+//			String splitLine[] = aLine.split("\t");
+//			matsimLinkId = splitLine[0].trim();
+//			betw = Double.parseDouble(splitLine[1]);
+//			linkBetw.put(matsimLinkId, betw);
+//		}
 		
-		for (Link link : network.getLinks().values()) {
-			betw = linkBetw.get(link.getId().toString());
-			if (betw==null) continue;	//bricht teilweise ab, weil betw "null" wird. ursache habe ich noch nicht gefunden!
-			System.out.println(betw);
-			if (betw<=BETW_MIN && link.getCapacity()<=CAPACITY_MIN) {
-				linkDeletion.put(link.getId(), link);
-//				System.out.println(linkDeletion.toString());
+		TObjectLongHashMap<Link> values = BetweennessLoader.loadBetweenness("/Users/jillenberger/Work/work/socialnets/data/schweiz/network/BetweennessSchweiz.txt", network);
+		TObjectLongIterator<Link> it = values.iterator();
+		
+		for(int i = 0; i < values.size(); i++) {
+			it.advance();
+			if(it.value() <= BETW_MIN && it.key().getCapacity() <= CAPACITY_MIN) {
+				linkDeletion.put(it.key().getId(), it.key());
+				/*
+				 * get back link
+				 */
+				Link link = it.key();
+				Link backLink = null;
+				Node from = link.getFromNode();
+				Node to = link.getToNode();
+				for(Link outLink : to.getOutLinks().values()) {
+					if(outLink.getToNode() == from) {
+						backLink = outLink;
+						break;
+					}
+				}
+				if(backLink != null)
+					linkDeletion.put(backLink.getId(), backLink);
 			}
 		}
 		
+//		for (Link link : network.getLinks().values()) {
+//			betw = linkBetw.get(link.getId().toString());
+//			if (betw==null) continue;	//bricht teilweise ab, weil betw "null" wird. ursache habe ich noch nicht gefunden!
+//			System.out.println(betw);
+//			if (betw<=BETW_MIN && link.getCapacity()<=CAPACITY_MIN) {
+//				linkDeletion.put(link.getId(), link);
+////				System.out.println(linkDeletion.toString());
+//			}
+//		}
+		
+		int cnt = 0;
         for (Entry<Id, Link> entry : linkDeletion.entrySet()) {        
 //            System.out.println(linkDeletion.get(entry.getKey()));
-            network.removeLink(entry.getKey());	//tut leider nicht das, was ich will, nämlich den link löschen. gibt es dafür eine methode?
+            if(network.removeLink(entry.getKey()) == null) {
+            	//tut leider nicht das, was ich will, nämlich den link löschen. gibt es dafür eine methode?
+            	System.err.println("Link not found!");
+            } else {
+            	cnt++;
+            }
         }
+        
+        System.out.println("Removed " + cnt + " links");
 		
-		new NetworkCleaner().run(new String[] {args[0], args[2]});
+        NetworkWriter writer = new NetworkWriter(network);
+        writer.write("/Users/jillenberger/Work/work/socialnets/data/schweiz/network/network-out1.xml");
+
+        new NetworkCleaner().run(new String[] {"/Users/jillenberger/Work/work/socialnets/data/schweiz/network/network-out1.xml", "/Users/jillenberger/Work/work/socialnets/data/schweiz/network/network-out2.xml"});
 
 		
 
