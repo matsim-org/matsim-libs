3fddf367efa7998762c89587e6daad1a290d1242
rashid_waraich <rashid_waraich@users.sf.net>
2010-08-17 11:29:48 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12346 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/wrashid/src/main/java/playground/wrashid/parkingSearch/planLevel/trb/dataPreparation/PlanClonerWithIncreasingAndOrderedStartTimes.java
@@ -0,0 +1,54 @@
+package playground.wrashid.parkingSearch.planLevel.trb.dataPreparation;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Leg;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Plan;
+import org.matsim.core.basic.v01.IdImpl;
+import org.matsim.core.network.NetworkLayer;
+import org.matsim.core.population.PersonImpl;
+
+import playground.wrashid.lib.GeneralLib;
+
+/**
+ * TODO: tidy up this class a bit!
+ * @author wrashid
+ *
+ */
+public class PlanClonerWithIncreasingAndOrderedStartTimes {
+
+	public static void main(String[] args) {
+		
+		// input parameters
+		String inputPlansFile="C:/data/workspace/playgrounds/wrashid/test/input/playground/wrashid/parkingSearch/planLevel/chessPlans2.xml";
+		String inputNetworkFile="C:/data/workspace/playgrounds/wrashid/test/scenarios/chessboard/network.xml";
+		String outputPlansFile="C:/data/workspace/playgrounds/wrashid/test/input/playground/wrashid/parkingSearch/planLevel/chessPlan6.xml";
+		int numberOfClones=100;	
+		String idOfPersonForCloning="1";
+		
+		// start program
+		
+		Scenario scenario= GeneralLib.readScenario(inputPlansFile, inputNetworkFile);
+		
+		Person selectedPersonForCloning=scenario.getPopulation().getPersons().get(new IdImpl(idOfPersonForCloning));
+		
+		scenario.getPopulation().getPersons().clear();
+		
+		for (int i=1;i<=numberOfClones;i++){
+			Person person=GeneralLib.copyPerson(selectedPersonForCloning);
+			person.setId(new IdImpl(i));
+			Leg firstLeg=((Leg)person.getSelectedPlan().getPlanElements().get(1));
+			double depTime=firstLeg.getDepartureTime();
+			// the departure time of the agents is ordered in increasing order
+			firstLeg.setDepartureTime(depTime+i);
+			
+			scenario.getPopulation().addPerson(person);
+		}
+		
+		GeneralLib.writePersons(scenario.getPopulation().getPersons().values(), outputPlansFile, (NetworkLayer) scenario.getNetwork());
+	}
+	
+	
+
+	
+}
