3e46551eee7d7cbd9d3fb0d269a6dd1c4ca21d29
vsp_neumann <neumann@senozon.de>
2010-08-19 08:07:10 +0000

    adopted new QSim and Controler structure
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12382 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/andreas/src/main/java/playground/andreas/TransitControler.java
+++ playgrounds/andreas/src/main/java/playground/andreas/TransitControler.java
@@ -20,240 +20,119 @@
 
 package playground.andreas;
 
-import java.io.IOException;
-import java.util.EnumSet;
-import java.util.Set;
-
-import javax.xml.parsers.ParserConfigurationException;
-
 import org.apache.log4j.Logger;
-import org.matsim.api.core.v01.ScenarioImpl;
-import org.matsim.api.core.v01.TransportMode;
-import org.matsim.core.config.Module;
-import org.matsim.core.config.groups.CharyparNagelScoringConfigGroup.ActivityParams;
-import org.matsim.core.config.groups.ControlerConfigGroup.EventsFileFormat;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.MatsimConfigReader;
 import org.matsim.core.controler.Controler;
-import org.matsim.core.controler.events.AfterMobsimEvent;
-import org.matsim.core.controler.events.BeforeMobsimEvent;
-import org.matsim.core.controler.events.StartupEvent;
-import org.matsim.core.controler.listener.AfterMobsimListener;
-import org.matsim.core.controler.listener.BeforeMobsimListener;
-import org.matsim.core.controler.listener.StartupListener;
 import org.matsim.core.mobsim.framework.IOSimulation;
-import org.matsim.core.replanning.StrategyManager;
-import org.matsim.core.router.util.PersonalizableTravelCost;
-import org.matsim.core.router.util.TravelTime;
-import org.matsim.population.algorithms.PlanAlgorithm;
-import org.matsim.pt.PtConstants;
-import org.matsim.pt.ReconstructingUmlaufBuilder;
-import org.matsim.pt.config.TransitConfigGroup;
-import org.matsim.pt.counts.OccupancyAnalyzer;
-import org.matsim.pt.counts.PtCountControlerListener;
+import org.matsim.core.mobsim.framework.ObservableSimulation;
+import org.matsim.core.mobsim.framework.listeners.SimulationListener;
 import org.matsim.pt.qsim.ComplexTransitStopHandlerFactory;
-import org.matsim.pt.qsim.TransitQSimulation;
-import org.matsim.pt.replanning.TransitStrategyManagerConfigLoader;
-import org.matsim.pt.router.PlansCalcTransitRoute;
-import org.matsim.pt.routes.ExperimentalTransitRouteFactory;
-import org.matsim.transitSchedule.TransitScheduleReaderV1;
-import org.matsim.vehicles.VehicleReaderV1;
+import org.matsim.ptproject.qsim.QSim;
+import org.matsim.ptproject.qsim.QSimFactory;
 import org.matsim.vis.otfvis.OTFVisMobsimFeature;
-import org.xml.sax.SAXException;
 
 import playground.andreas.fixedHeadway.FixedHeadwayControler;
 import playground.andreas.fixedHeadway.FixedHeadwayCycleUmlaufDriverFactory;
 
 /**
- * @author mrieser
+ * @author aneumann
  */
 public class TransitControler extends Controler {
 
-	private final static Logger logger = Logger.getLogger(TransitControler.class);
+	private final static Logger log = Logger.getLogger(TransitControler.class);
 
-	private final static String COUNTS_MODULE_NAME = "ptCounts";
+//	private final static String COUNTS_MODULE_NAME = "ptCounts";
 
-	private final TransitConfigGroup transitConfig;
-
-	private boolean useOTFVis = true;
-	private boolean useHeadwayControler = false;
-
-	public TransitControler(final String[] args) {
-		super(args);
-		this.transitConfig = new TransitConfigGroup();
-		init();
+	private boolean useOTFVis = false;
+	private boolean useHeadwayControler = true;
+	
+	public TransitControler(Config config) {
+		super(config);
 	}
-
-	public TransitControler(final ScenarioImpl scenario) {
-		super(scenario);
-		this.transitConfig = new TransitConfigGroup();
-		init();
-	}
-
-	private final void init() {
-		if (this.config.getModule(TransitConfigGroup.GROUP_NAME) == null) {
-			this.config.addModule(TransitConfigGroup.GROUP_NAME, this.transitConfig);
-		} else {
-			// this would not be necessary if TransitConfigGroup is part of core config
-			Module oldModule = this.config.getModule(TransitConfigGroup.GROUP_NAME);
-			this.config.removeModule(TransitConfigGroup.GROUP_NAME);
-			this.transitConfig.addParam("transitScheduleFile", oldModule.getValue("transitScheduleFile"));
-			this.transitConfig.addParam("vehiclesFile", oldModule.getValue("vehiclesFile"));
-			this.transitConfig.addParam("transitModes", oldModule.getValue("transitModes"));
-		}
-		this.config.scenario().setUseTransit(true);
-		this.config.scenario().setUseVehicles(true);
-		Set<EventsFileFormat> formats = EnumSet.copyOf(this.config.controler().getEventsFileFormats());
-		formats.add(EventsFileFormat.xml);
-		this.config.controler().setEventsFileFormats(formats);
-		ActivityParams params = new ActivityParams(PtConstants.TRANSIT_ACTIVITY_TYPE);
-		params.setTypicalDuration(120.0);
-		this.config.charyparNagelScoring().addActivityParams(params);
-		this.getNetwork().getFactory().setRouteFactory(TransportMode.pt, new ExperimentalTransitRouteFactory());
-	}
-
-	@Override
-	protected StrategyManager loadStrategyManager() {
-		StrategyManager manager = new StrategyManager();
-		TransitStrategyManagerConfigLoader.load(this, this.config, manager);
-		return manager;
-	}
-
-	@Override
-	protected void loadControlerListeners() {
-		super.loadControlerListeners();
-		addTransitControlerListener();
-		if (config.getModule(COUNTS_MODULE_NAME) != null) {
-			addPtCountControlerListener();
-		}
-	}
-
-	private void addPtCountControlerListener() {
-		OccupancyAnalyzer occupancyAnalyzer = new OccupancyAnalyzer(3600, 24 * 3600 - 1);
-		logger.info("Using counts.");
-		OccupancyAnalyzerListener oal = new OccupancyAnalyzerListener(occupancyAnalyzer);
-		addControlerListener(oal);
-		addControlerListener(new PtCountControlerListener(config, occupancyAnalyzer));
-		setCreateGraphs(false);
-	}
-
-	private void addTransitControlerListener() {
-		TransitControlerListener cl = new TransitControlerListener(this.transitConfig);
-		addControlerListener(cl);
-	}
-
+	
 	@Override
 	protected void runMobSim() {
-		TransitQSimulation sim = new TransitQSimulation(this.scenarioData, this.events);
-		if (useOTFVis) {
-			OTFVisMobsimFeature otfVisQSimFeature = new OTFVisMobsimFeature(sim);
-			otfVisQSimFeature.setVisualizeTeleportedAgents(sim.getScenario().getConfig().otfVis().isShowTeleportedAgents());
-			sim.addFeature(otfVisQSimFeature);
-		}
-		sim.getQSimTransitEngine().setUseUmlaeufe(true);
-		sim.getQSimTransitEngine().setTransitStopHandlerFactory(new ComplexTransitStopHandlerFactory());
 		
-		if(useHeadwayControler){
-			sim.getQSimTransitEngine().setAbstractTransitDriverFactory(new FixedHeadwayCycleUmlaufDriverFactory());
-			this.events.addHandler(new FixedHeadwayControler(sim));		
-		}		
-//		this.events.addHandler(new LogOutputEventHandler());	
-		
-		if (sim instanceof IOSimulation){
-			((IOSimulation)sim).setControlerIO(this.getControlerIO());
-			((IOSimulation)sim).setIterationNumber(this.getIterationNumber());
-		}
-		sim.run();
-	}
+		log.info("Overriding runMobSim()");
 
-	@Override
-	public PlanAlgorithm createRoutingAlgorithm(final PersonalizableTravelCost travelCosts, final TravelTime travelTimes) {
-		return new PlansCalcTransitRoute(this.config.plansCalcRoute(), this.network, travelCosts, travelTimes,
-				this.getLeastCostPathCalculatorFactory(), this.scenarioData.getTransitSchedule(), this.transitConfig);
-	}
+		QSim simulation = (QSim) new QSimFactory().createMobsim(this.getScenario(), this.getEvents());
 
-	public static class TransitControlerListener implements StartupListener {
+		simulation.getQSimTransitEngine().setTransitStopHandlerFactory(new ComplexTransitStopHandlerFactory());
+//		this.events.addHandler(new LogOutputEventHandler());
 
-		private final TransitConfigGroup config;
-
-		public TransitControlerListener(final TransitConfigGroup config) {
-			this.config = config;
+		if (this.useOTFVis) {
+			OTFVisMobsimFeature otfVisQSimFeature = new OTFVisMobsimFeature(simulation);
+			otfVisQSimFeature.setVisualizeTeleportedAgents(simulation.getScenario().getConfig().otfVis().isShowTeleportedAgents());
+			simulation.addFeature(otfVisQSimFeature);
 		}
 
-		public void notifyStartup(final StartupEvent event) {
-			if (this.config.getTransitScheduleFile() != null) {
-				try {
-					new TransitScheduleReaderV1(event.getControler().getScenario().getTransitSchedule(), event.getControler().getScenario().getNetwork()).readFile(this.config.getTransitScheduleFile());
-				} catch (SAXException e) {
-					throw new RuntimeException("could not read transit schedule.", e);
-				} catch (ParserConfigurationException e) {
-					throw new RuntimeException("could not read transit schedule.", e);
-				} catch (IOException e) {
-					throw new RuntimeException("could not read transit schedule.", e);
-				}
+		if(this.useHeadwayControler){
+			simulation.getQSimTransitEngine().setAbstractTransitDriverFactory(new FixedHeadwayCycleUmlaufDriverFactory());
+			this.events.addHandler(new FixedHeadwayControler(simulation));		
+		}
+
+		if (simulation instanceof IOSimulation){
+			((IOSimulation)simulation).setControlerIO(this.getControlerIO());
+			((IOSimulation)simulation).setIterationNumber(this.getIterationNumber());
+		}
+		if (simulation instanceof ObservableSimulation){
+			for (SimulationListener l : this.getQueueSimulationListener()) {
+				((ObservableSimulation)simulation).addQueueSimulationListeners(l);
 			}
-			if (this.config.getVehiclesFile() != null) {
-				try {
-					new VehicleReaderV1(event.getControler().getScenario().getVehicles()).parse(this.config.getVehiclesFile());
-				} catch (SAXException e) {
-					throw new RuntimeException("could not read vehicles.", e);
-				} catch (ParserConfigurationException e) {
-					throw new RuntimeException("could not read vehicles.", e);
-				} catch (IOException e) {
-					throw new RuntimeException("could not read vehicles.", e);
-				}
-			}
-			ReconstructingUmlaufBuilder reconstructingUmlaufBuilder = new ReconstructingUmlaufBuilder(
-					event.getControler().getScenario().getNetwork(), event
-							.getControler().getScenario()
-							.getTransitSchedule().getTransitLines().values(),
-					event.getControler().getScenario().getVehicles(),
-					event.getControler().getScenario().getConfig().charyparNagelScoring());
-			reconstructingUmlaufBuilder.build();
 		}
-
+		simulation.run();
 	}
+
+	//	public static class OccupancyAnalyzerListener implements
+	//			BeforeMobsimListener, AfterMobsimListener {
+	//
+	//		private OccupancyAnalyzer occupancyAnalyzer;
+	//
+	//		public OccupancyAnalyzerListener(OccupancyAnalyzer occupancyAnalyzer) {
+	//			this.occupancyAnalyzer = occupancyAnalyzer;
+	//		}
+	//
+	//		public void notifyBeforeMobsim(BeforeMobsimEvent event) {
+	//			int iter = event.getIteration();
+	//			if (iter % 10 == 0&& iter > event.getControler().getFirstIteration()) {
+	//				occupancyAnalyzer.reset(iter);
+	//				event.getControler().getEvents().addHandler(occupancyAnalyzer);
+	//			}
+	//		}
+	//
+	//		public void notifyAfterMobsim(AfterMobsimEvent event) {
+	//			int it = event.getIteration();
+	//			if (it % 10 == 0 && it > event.getControler().getFirstIteration()) {
+	//				event.getControler().getEvents().removeHandler(occupancyAnalyzer);
+	//				occupancyAnalyzer.write(event.getControler().getControlerIO()
+	//						.getIterationFilename(it, "occupancyAnalysis.txt"));
+	//			}
+	//		}
+	//
+	//	}
+	
+	//	boolean isUseOTFVis() {
+	//		return this.useOTFVis;
+	//	}
+	
+		void setUseOTFVis(boolean useOTFVis) {
+			this.useOTFVis = useOTFVis;
+		}
 
 	public static void main(final String[] args) {
-		TransitControler tc = new TransitControler(args);
+		Config config = new Config();
+		config.addCoreModules();
+		new MatsimConfigReader(config).readFile(args[0]);
+		config.scenario().setUseTransit(true);
+		config.scenario().setUseVehicles(true);
+		
+		TransitControler tc = new TransitControler(config);
+		if(args.length > 1 && args[1].equalsIgnoreCase("true")){
+			tc.setUseOTFVis(true);
+		}
 		tc.setOverwriteFiles(true);
 //		tc.setCreateGraphs(false);
 		tc.run();
 	}
-
-	public static class OccupancyAnalyzerListener implements
-			BeforeMobsimListener, AfterMobsimListener {
-
-		private OccupancyAnalyzer occupancyAnalyzer;
-
-		public OccupancyAnalyzerListener(OccupancyAnalyzer occupancyAnalyzer) {
-			this.occupancyAnalyzer = occupancyAnalyzer;
-		}
-
-		public void notifyBeforeMobsim(BeforeMobsimEvent event) {
-			int iter = event.getIteration();
-			if (iter % 10 == 0&& iter > event.getControler().getFirstIteration()) {
-				occupancyAnalyzer.reset(iter);
-				event.getControler().getEvents().addHandler(occupancyAnalyzer);
-			}
-		}
-
-		public void notifyAfterMobsim(AfterMobsimEvent event) {
-			int it = event.getIteration();
-			if (it % 10 == 0 && it > event.getControler().getFirstIteration()) {
-				event.getControler().getEvents().removeHandler(occupancyAnalyzer);
-				occupancyAnalyzer.write(event.getControler().getControlerIO()
-						.getIterationFilename(it, "occupancyAnalysis.txt"));
-			}
-		}
-
-	}
-
-	boolean isUseOTFVis() {
-		return useOTFVis;
-	}
-
-	void setUseOTFVis(boolean useOTFVis) {
-		this.useOTFVis = useOTFVis;
-	}
-
-
 }
