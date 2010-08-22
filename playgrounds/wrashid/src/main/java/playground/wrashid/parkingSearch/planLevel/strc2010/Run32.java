d1e5db4b82e8eb3c43a20838b4ebaca6fef15340
rashid_waraich <rashid_waraich@users.sf.net>
2010-08-22 16:57:19 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12412 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/wrashid/src/main/java/playground/wrashid/parkingSearch/planLevel/strc2010/Run32.java
@@ -0,0 +1,25 @@
+package playground.wrashid.parkingSearch.planLevel.strc2010;
+
+import org.matsim.core.basic.v01.IdImpl;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.trafficmonitoring.PessimisticTravelTimeAggregator;
+
+import playground.wrashid.lib.GlobalRegistry;
+import playground.wrashid.lib.RunLib;
+import playground.wrashid.lib.obj.plan.PersonGroups;
+import playground.wrashid.parkingSearch.planLevel.init.ParkingRoot;
+import playground.wrashid.parkingSearch.planLevel.scenario.BaseControlerScenario;
+import playground.wrashid.parkingSearch.planLevel.scenario.BaseControlerScenarioOneLiner;
+
+/**
+ * 
+ * @author wrashid
+ * 
+ */
+public class Run32 {
+	public static void main(String[] args) {
+		int runNumber=RunLib.getRunNumber(new Object() { }.getClass().getEnclosingClass());
+		RunSeries.getControler(runNumber).run();
+	}
+
+}
