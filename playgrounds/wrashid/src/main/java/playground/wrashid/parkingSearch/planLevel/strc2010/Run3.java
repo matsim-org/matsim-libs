c0074cd0a6e42d04f499ed6d78b91bd79f2dea9a
rashid_waraich <rashid_waraich@users.sf.net>
2010-08-22 06:00:42 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12400 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/wrashid/src/main/java/playground/wrashid/parkingSearch/planLevel/strc2010/Run3.java
@@ -0,0 +1,43 @@
+package playground.wrashid.parkingSearch.planLevel.strc2010;
+
+import org.matsim.core.basic.v01.IdImpl;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.trafficmonitoring.PessimisticTravelTimeAggregator;
+
+import playground.wrashid.lib.GlobalRegistry;
+import playground.wrashid.lib.obj.plan.PersonGroups;
+import playground.wrashid.parkingSearch.planLevel.init.ParkingRoot;
+import playground.wrashid.parkingSearch.planLevel.scenario.BaseControlerScenario;
+import playground.wrashid.parkingSearch.planLevel.scenario.BaseControlerScenarioOneLiner;
+
+/**
+ * 
+ * @author wrashid
+ * 
+ */
+public class Run3 {
+		public static void main(String[] args) {
+		Controler controler;
+		String configFilePath = "H:/data/experiments/STRC2010/input/config3.xml";
+		controler = new Controler(configFilePath);
+
+		new BaseControlerScenario(controler);
+
+		initPersonGroupsForStatistics();
+
+		controler.run();
+	}
+
+	private static void initPersonGroupsForStatistics() {
+		PersonGroups personGroupsForStatistics = new PersonGroups();
+
+		for (int i = 0; i <= 999; i++) {
+			personGroupsForStatistics.addPersonToGroup(
+					"Group-" + Integer.toString(i / 333 + 1), new IdImpl(i));
+		}
+
+		ParkingRoot.setPersonGroupsForStatistics(personGroupsForStatistics);
+	}
+	
+	
+}
---
+++ playgrounds/wrashid/src/main/java/playground/wrashid/parkingSearch/planLevel/strc2010/Run5.java
@@ -0,0 +1,24 @@
+package playground.wrashid.parkingSearch.planLevel.strc2010;
+
+import org.matsim.core.basic.v01.IdImpl;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.trafficmonitoring.PessimisticTravelTimeAggregator;
+
+import playground.wrashid.lib.GlobalRegistry;
+import playground.wrashid.lib.obj.plan.PersonGroups;
+import playground.wrashid.parkingSearch.planLevel.init.ParkingRoot;
+import playground.wrashid.parkingSearch.planLevel.scenario.BaseControlerScenario;
+import playground.wrashid.parkingSearch.planLevel.scenario.BaseControlerScenarioOneLiner;
+
+/**
+ * 
+ * @author wrashid
+ * 
+ */
+public class Run5 {
+	public static void main(String[] args) {
+		new BaseControlerScenarioOneLiner("H:/data/experiments/STRC2010/input/config5.xml").run();
+		GlobalRegistry.doPrintGraficDataToConsole=true;
+	}
+
+}
