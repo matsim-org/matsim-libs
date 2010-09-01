be8dbc084af62b0865ea5d2a5790aa917cfeed51
michaz <michael.zilske@gmail.com>
2010-09-01 05:49:08 +0000

    Added a verschmierer vor popgeneration.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12524 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/mzilske/src/test/java/playground/mzilske/prognose2025/VerschmiererTest.java
+++ playgrounds/mzilske/src/test/java/playground/mzilske/prognose2025/VerschmiererTest.java
@@ -7,14 +7,16 @@ import org.matsim.core.utils.geometry.CoordImpl;
 
 public class VerschmiererTest {
 	
-	@Test
-	public void test() {
-		Verschmierer verschmierer = new Verschmierer();
-		verschmierer.prepare();
-		CoordImpl notInCell = new CoordImpl(3.0, 4.0);
-		Assert.assertSame(notInCell, verschmierer.shootIntoSameZoneOrLeaveInPlace(notInCell));
-		CoordImpl inCell = new CoordImpl(4326122, 5756578);
-		Assert.assertNotSame(inCell, verschmierer.shootIntoSameZoneOrLeaveInPlace(inCell));
-	}
+	// Not a proper test at the moment, because it uses a hard coded datafile :-)
+	
+//	@Test
+//	public void test() {
+//		Verschmierer verschmierer = new Verschmierer();
+//		verschmierer.prepare();
+//		CoordImpl notInCell = new CoordImpl(3.0, 4.0);
+//		Assert.assertSame(notInCell, verschmierer.shootIntoSameZoneOrLeaveInPlace(notInCell));
+//		CoordImpl inCell = new CoordImpl(4326122, 5756578);
+//		Assert.assertNotSame(inCell, verschmierer.shootIntoSameZoneOrLeaveInPlace(inCell));
+//	}
 
 }
