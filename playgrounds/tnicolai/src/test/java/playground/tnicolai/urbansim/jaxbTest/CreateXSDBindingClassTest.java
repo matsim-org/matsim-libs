d7370b196fdf64fbd4e41b34459138c2ff6c39f8
tnicolai79 <tnicolai79@users.sf.net>
2010-09-03 16:14:03 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12547 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/tnicolai/src/test/java/playground/tnicolai/urbansim/jaxbTest/CreateXSDBindingClassTest.java
+++ playgrounds/tnicolai/src/test/java/playground/tnicolai/urbansim/jaxbTest/CreateXSDBindingClassTest.java
@@ -23,9 +23,14 @@
  */
 package playground.tnicolai.urbansim.jaxbTest;
 
+import org.apache.log4j.Logger;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.matsim.testcases.MatsimTestCase;
+
+import playground.tnicolai.urbansim.UpdateXMLBindingClasses;
+import playground.tnicolai.urbansim.utils.CommonUtilities;
+import playground.tnicolai.urbansim.utils.io.TempDirectoryUtil;
 
 
 
@@ -35,26 +40,69 @@ import org.matsim.testcases.MatsimTestCase;
  */
 public class CreateXSDBindingClassTest extends MatsimTestCase {
 	
+	private static final Logger log = Logger.getLogger(CreateXSDBindingClassTest.class);
+	
 	@Ignore
 	//@Rule public MatsimTestUtils utils = new MatsimTestUtils();
 	@Test
 	public void testCreateBindingClass(){
-		//UpdateXMLParser updater = new UpdateXMLParser();
+		
+		prepareTest();
+		
 	}
-
-//	/**
-//	 * @throws java.lang.Exception
-//	 */
-//	@Before
-//	public void setUp() throws Exception {
-//	}
-//
-//	/**
-//	 * @throws java.lang.Exception
-//	 */
-//	@After
-//	public void tearDown() throws Exception {
-//	}
+	
+	/**
+	 * preparing creation of binding classes test run
+	 */
+	private void prepareTest(){
+		
+		String jaxbLocation = findJAXBLibLocation();
+		String destination = createDestinationDirectory();
+		String packageName = "org.test.bindingClasses";
+		
+		String[] args = {"--jaxbLocation="+jaxbLocation, 
+						 "--destination="+destination, 
+						 "--package="+packageName};
+		log.info("Strating UpdateXMLBindingClasses with following arguments: ");
+		log.info("JAXB location : " + jaxbLocation);
+		log.info("Destination : " + destination);
+		log.info("Package name : " + packageName);
+		
+		UpdateXMLBindingClasses.main(args);
+		
+		// TODO test Binding classes with 
+		
+		// remove all created temp directories
+		TempDirectoryUtil.deleteDirectory(destination);
+	}
+	
+	/**
+	 * gets the location of JAXB library
+	 * @return path of JAXB library
+	 */
+	private String findJAXBLibLocation(){
+		
+		String path = CommonUtilities.getCurrentPath(CreateXSDBindingClassTest.class);
+		String subPath = "lib/jaxb-2.1.7/lib/jaxb-xjc.jar";
+		
+		return CommonUtilities.replaceSubPath(1, path, subPath);
+	}
+	
+	/**
+	 * returns the path to the temp destination directory for generated 
+	 * binding classes
+	 * @return path to the temp destination directory for generated 
+	 * binding classes
+	 */
+	private String createDestinationDirectory(){
+		
+		String path = CommonUtilities.getCurrentPath(CreateXSDBindingClassTest.class);
+		path = CommonUtilities.replaceSubPath(1, path, "tmp/jaxbBindingClasses");
+		
+		if (TempDirectoryUtil.createDirectory(path))
+			return path;
+		return null;
+	}
 
 }
 
