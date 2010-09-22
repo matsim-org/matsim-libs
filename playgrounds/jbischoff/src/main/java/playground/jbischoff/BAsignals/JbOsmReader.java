34ec34c67e7f7ae2ac7b05a5a1df0a469de66b07
mrieser <rieser@gmail.com>
2010-09-22 08:17:35 +0000

    removed usages of NetworkLayer
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12713 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/jbischoff/src/main/java/playground/jbischoff/BAsignals/JbOsmReader.java
+++ playgrounds/jbischoff/src/main/java/playground/jbischoff/BAsignals/JbOsmReader.java
@@ -1,15 +1,30 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2010 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
 package playground.jbischoff.BAsignals;
 
 import java.io.IOException;
-import java.util.Set;
-import java.util.TreeSet;
 
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.matsim.api.core.v01.Scenario;
-import org.matsim.api.core.v01.ScenarioImpl;
-import org.matsim.core.network.MatsimNetworkReader;
-import org.matsim.core.network.NetworkLayer;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.core.network.NetworkImpl;
 import org.matsim.core.network.NetworkWriter;
 import org.matsim.core.utils.geometry.transformations.TransformationFactory;
 import org.matsim.core.utils.io.OsmNetworkReader;
@@ -23,8 +38,7 @@ public class JbOsmReader {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		NetworkLayer network = new NetworkLayer();
+		Network network = NetworkImpl.createNetwork();
 		OsmNetworkReader osmReader = new OsmNetworkReader(network,
 				TransformationFactory.getCoordinateTransformation(TransformationFactory.WGS84,
 						TransformationFactory.WGS84_UTM33N), false);
@@ -32,8 +46,8 @@ public class JbOsmReader {
 		osmReader.setScaleMaxSpeed(true);
 		String input = "/Users/JB/Desktop/BA-Arbeit/sim/brandenburg.osm";
 		String output = "/Users/JB/Desktop/BA-Arbeit/sim/brandenburg";
-		
-//		 set osmReader useHighwayDefaults false 
+
+//		 set osmReader useHighwayDefaults false
 //		 Autobahn
 		osmReader.setHighwayDefaults(1, "motorway",      2, 120.0/3.6, 1.0, 2000, true);
 		osmReader.setHighwayDefaults(1, "motorway_link", 1,  80.0/3.6, 1.0, 1500, true);
@@ -57,7 +71,7 @@ public class JbOsmReader {
 //		 osmReader.setHighwayDefaults(6, "living_street", 1,  15.0/3.6, 1.0,  300);
 		osmReader.setHierarchyLayer(52.04382,13.499222, 51.258248,14.887619, 1);
 		osmReader.setHierarchyLayer(51.820578,14.247866, 51.684789,14.507332, 2);
-		
+
 		try {
 			osmReader.parse(input);
 		} catch (SAXException e) {
@@ -67,16 +81,16 @@ public class JbOsmReader {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		// Write network to file
 		new NetworkWriter(network).write(output + ".xml.gz");
 		System.out.println("Done! Unprocessed MATSim Network saved as " + output + ".xml.gz");
 		// Clean network
 		new NetworkCleaner().run(new String[] {output + ".xml.gz", output + "_cl.xml.gz"});
 		System.out.println("NetworkCleaner done! Network saved as " + output + "_cl.xml.gz");
-		
 
-	
+
+
 
 	}
 
