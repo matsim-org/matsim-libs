d1c254630b88be802b3eb19bc538a749e217456b
mrieser <rieser@gmail.com>
2010-09-27 07:12:19 +0000

    A custom Controller to test/compare different mobsims
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12760 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/mrieser/src/main/java/playground/mrieser/core/mobsim/integration/Controller.java
@@ -0,0 +1,72 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2010 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.mrieser.core.mobsim.integration;
+
+import org.matsim.core.controler.Controler;
+import org.matsim.core.mobsim.framework.MobsimFactory;
+import org.matsim.core.mobsim.queuesim.QueueSimulationFactory;
+import org.matsim.ptproject.qsim.ParallelQSimFactory;
+import org.matsim.ptproject.qsim.QSimFactory;
+
+import playground.mrieser.core.mobsim.usecases.OptimizedCarSimFactory;
+import playground.mrieser.core.mobsim.usecases.RefSimFactory;
+import playground.mrieser.core.mobsim.usecases.TeleportOnlySimFactory;
+
+public class Controller {
+
+	public static void main(String[] args) {
+		MobsimFactory mobsimFactory = null;
+		int shiftBy = 0;
+		if ("-teleportOnly".equals(args[0])) {
+			mobsimFactory = new TeleportOnlySimFactory();
+			shiftBy = 1;
+		} else if ("-refSim".equals(args[0])) {
+			mobsimFactory = new RefSimFactory();
+			shiftBy = 1;
+		} else if ("-fastCarSim".equals(args[0])) {
+			int numberOfThreads = Integer.parseInt(args[1]);
+			mobsimFactory = new OptimizedCarSimFactory(numberOfThreads);
+			shiftBy = 2;
+		} else if ("-qsim".equals(args[0])) {
+			mobsimFactory = new QSimFactory();
+			shiftBy = 1;
+		} else if ("-pqsim".equals(args[0])) {
+			mobsimFactory = new ParallelQSimFactory();
+			shiftBy = 1;
+		} else if ("-queuesim".equals(args[0])) {
+			mobsimFactory = new QueueSimulationFactory();
+			shiftBy = 1;
+		}
+		if (mobsimFactory == null) {
+			throw new RuntimeException("no mobsim specified!");
+		}
+
+		Controler c = new Controler(shiftArray(shiftBy, args));
+		c.setMobsimFactory(mobsimFactory);
+		c.run();
+	}
+
+	private static String[] shiftArray(int shiftBy, final String[] array) {
+		String[] newArray = new String[array.length - shiftBy];
+		System.arraycopy(array, shiftBy, newArray, 0, newArray.length);
+		return newArray;
+	}
+
+}
