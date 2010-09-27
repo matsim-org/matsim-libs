ef963bf36e150b3117e1bbe3d319222ec87b7ee9
grether <dominik.grether@gmail.com>
2010-09-27 13:25:17 +0000

    Improvement of ShapeFileReader: As FeatureSource is implemented as a direct pointer to the file on disk one can safe 99,9% of computing time by reading the FeatureSource once and then use the features stored in memory instead of reading from file again and again
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12771 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/core/utils/gis/ShapeFileReader.java
+++ matsim/src/main/java/org/matsim/core/utils/gis/ShapeFileReader.java
@@ -24,41 +24,121 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 import org.geotools.data.DataStore;
 import org.geotools.data.DataStoreFinder;
 import org.geotools.data.FeatureSource;
+import org.geotools.feature.Feature;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureType;
 import org.matsim.core.api.internal.MatsimSomeReader;
+
+import com.vividsolutions.jts.geom.Envelope;
 
 /**
  * @author glaemmel
+ * @author dgrether
  */
 public class ShapeFileReader implements MatsimSomeReader {
 
 	private static final Logger log = Logger.getLogger(ShapeFileReader.class);
 
+	private FeatureSource featureSource = null;
+
+	private Envelope bounds = null;
+
+	private DataStore dataStore = null;
+
+	private FeatureCollection featureCollection = null;
+
+	private FeatureType schema = null;
+	
+	private Set<Feature> featureSet = null;
+	
 	/**
-	 * Reads a shape file and returns a <code>FeatureSource</code> containing all features.
+	 * Provides access to a shape file and returns a <code>FeatureSource</code> containing all features.
+	 * Take care access means on disk access, i.e. the FeatureSource is only a pointer to the information 
+	 * stored in the file. This can be horribly slow if invoked many times and throw exceptions if two many read
+	 * operations to the same file are performed. In those cases it is recommended to use the method readDataFileToMemory
+	 * of this class.
 	 *
 	 * @param fileName File name of a shape file (ending in <code>*.shp</code>)
 	 * @return FeatureSource containing all features.
 	 * @throws IOException if the file cannot be found or another error happens during reading
+	 * @deprecated use non static readFileAndInitialize Method
 	 */
+	@Deprecated
 	public static FeatureSource readDataFile(final String fileName) throws IOException {
-		log.info("reading features from: " + fileName);
-
-		File dataFile = new File(fileName);
-
+		return new ShapeFileReader().openFeatureSource(fileName);
+	}
+	
+	private FeatureSource openFeatureSource(final String filename) throws IOException{
+		log.info("reading features from: " + filename);
+		File dataFile = new File(filename);
 		HashMap<String, URL> connect = new HashMap<String, URL>();
 		connect.put("url", dataFile.toURL());
-
 		DataStore dataStore = DataStoreFinder.getDataStore(connect);
 		String[] typeNames = dataStore.getTypeNames();
 		String typeName = typeNames[0];
 		FeatureSource fs = dataStore.getFeatureSource(typeName);
-
 		return fs;
 	}
+	
+	/**
+	 * Reads all Features in the file into the returned Set and initializes the instance of this class.
+	 */
+	public Set<Feature> readFileAndInitialize(final String filename) throws IOException{
+		this.featureSource = ShapeFileReader.readDataFile(filename);
+		this.init();
+		Feature ft = null;
+		Iterator it = this.featureSource.getFeatures().iterator();
+		this.featureSet = new HashSet<Feature>();
+		while (it.hasNext()){
+			ft = (Feature) it.next();
+			this.featureSet.add(ft);
+		}
+		return this.featureSet;
+	}
+
+
+	private void init() throws IOException {
+		this.bounds = this.featureSource.getBounds();
+		this.dataStore = this.featureSource.getDataStore();
+		this.featureCollection = this.featureSource.getFeatures();
+		this.schema = this.featureSource.getSchema();
+	}
+
+
+	
+	public FeatureSource getFeatureSource() {
+		return featureSource;
+	}
+	
+	public Envelope getBounds() {
+		return bounds;
+	}
+	
+	public DataStore getDataStore() {
+		return dataStore;
+	}
+	
+	public FeatureCollection getFeatureCollection() {
+		return featureCollection;
+	}
+	
+	public FeatureType getSchema() {
+		return schema;
+	}
+
+	public Set<Feature> getFeatureSet() {
+		return featureSet;
+	}
+
+	
+	
 
 }
