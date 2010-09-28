a3ad868e0958c195dc9901676fcc8b69918fe845
grether <dominik.grether@gmail.com>
2010-09-28 12:27:59 +0000

    some first documentation
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12789 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/signalsystems/package-info.java
+++ matsim/src/main/java/org/matsim/signalsystems/package-info.java
@@ -1,47 +1,41 @@
 /**
- *  This package contains classes to add lanes at the end of links.
- *  There is no extra test package for the lanes implementation. However
- *  tests for lanes can be found in the signalsystems test package.
- *
+ *  This package contains interfaces and classes that provide functionality to plug a signal system model 
+ *  in the MATSim framework.
+ *  
+ *  <h2>Model description</h2>
+ *  
+ *  <h3>Overview</h3>
+ *  
+ *  The model consists of several layers:
+ *  
+ *	<ul>
+ *		<li>A data layer</li>
+ *		<li>A model layer</li>
+ *		<li>A layer to couple the model to the mobility simulation</li>
+ *		<li>A layer to build the other layers</li>
+ *	</ul>
+ * 
+ * Each layer can be found in a subpackage of this package. 
+ * 
+ * TODO short overview 
+ * 
+ * In addition to the main layers, the package contains some utility classes and needful helpers to plug 
+ * the model into the MATSim Controler and to generate the required/desired input data.
+ * 
+ * <h3>Extensibility</h3>
+ * 
+ * The layers of the model can be used separately and independently from each other. The default implementation 
+ * uses all layers of the model using the building layer to couple the other layers together. If you want to exchange or extend 
+ * a certain part of the model look for the factory and builders of the default implementation that create the components of
+ * interest. Implement your customized factories and builders using or extending the provided interfaces and replace 
+ * the factories/builders of the default implementation by your instances to get your extension or customization into the model. 
+ *  
+ * TODO pointer to example
+ *  
  * <h2>Usage restrictions:</h2>
  * <ul>
- *   <li> Not yet edited</li>
+ *   <li> Do not use, yet!</li>
  * </ul>
- * 
- * <h2>Signal Control</h2>
- * <ul>
- *   <li>SignalSystemController implementations are responsible to throw the
- *   appropriate SignalGroupStateChangedEvents.</li>
- *   <li>SingalSystemController used by the QSim of MATSim are automatically
- *   registered as a SimulationListener if one of the SimulationListener interfaces
- *   is implemented<li>
- * </ul>
- * 
- * <h3>Plan Based Signal Control<h3>
- * Plan based signal control uses the attributes 
- * <ul>
- *   <li>cycle time (ct)</li>
- *   <li>roughcast (rc)</li>
- *   <li>dropping (dr)</li>
- *   <li>intergreen time roughcast (igrc)</li>
- *   <li>intergreen time dropping (igdr)</li>
- * </ul>
- * to determine the state of a signal group by the following schemes:
- * 
- * If rc <= dr:
- * <p>
- *  |__red___|_redyellow_|_green_____________|_yellow_|_red______|  <br>
- *  |------------| ---------------|-------------------------------|------------|----------------|  <br>
- *  0_______rc_________rc+igrc______________dr______dr+igdr____ct <br>
- * <p>
- * if dr < rc:
- * <p>
- *  |_green___|_yellow____|_red______________|_redyellow_|_green_|  <br>
- *  |---------------|_---------------|----------------------------|-----------------|-----------_|  <br>
- *  0_________dr_________dr+igdr____________rc________rc+igrc___ct <br>
- * <p>
- * 
- * At which state vehicles moved is responsibility of the SignalSystemControler.
  * 
  * <h2>Package Maintainer(s):</h2>
  * <ul>
