dbf636819c2bda1ee3f53ba2449f5110ba0941c7
tnicolai79 <tnicolai79@users.sf.net>
2010-09-28 15:38:33 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12804 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/tnicolai/xsd/backup/createMATSimXMLConfigBindingTest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# script to create jaxb bindings for People
-# im Terminal im Ordner xsds (/Users/thomas/Development/workspace/OPUS_MATSim_Config_Test/xsds) folgendes Kommando eingeben.
-../libs/jaxb-2.1.7/bin/xjc.sh -p com.matsim.config -d ../playground/thomas/urbansim/ MATSim4UrbanSimConfigSchema.xsd
--- playgrounds/tnicolai/xsd/createMATSimXMLConfigBinding.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# script to create jaxb bindings
-# the command is structured as folloed:
-# location/of/xjc.sh -p package_name -d directory location/of/xsd_file
-
-../libs/jaxb-2.1.7/bin/xjc.sh -p com.matsim.config -d ../playground/thomas/urbansim/ MATSim4UrbanSimTestConfig2.xsd
