fb2ce0a96a738c3db0692df6cb2d5db06e53b89e
grether <dominik.grether@gmail.com>
2010-09-28 12:24:38 +0000

    added config parameters for amber and signal group files
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12788 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/core/config/groups/SignalSystemsConfigGroup.java
+++ matsim/src/main/java/org/matsim/core/config/groups/SignalSystemsConfigGroup.java
@@ -35,13 +35,25 @@ public class SignalSystemsConfigGroup extends Module {
 
 	private static final long serialVersionUID = 2346649035049406334L;
 
-	private static final String SIGNALSYSTEMFILE = "signalsystems";
-	private static final String SIGNALSYSTEMCONFIGFILE = "signalsystemsconfiguration";
+	private static final String SIGNALSYSTEM_FILE = "signalsystems";
+	private static final String SIGNALSYSTEMCONFIG_FILE = "signalsystemsconfiguration";
+	private static final String SIGNALCONTROL_FILE = "signalcontrol";
+	private static final String SIGNALGROUPS_FILE = "signalgroups";
+	private static final String AMBERTIMES_FILE = "ambertimes";
+	private static final String INTERGREENTIMES_FILE = "intergreentimes";
 
 	public static final String GROUPNAME = "signalsystems";
 
 	private String signalSystemFile;
 	private String signalSystemConfigFile;
+
+	private String signalGroupsFile;
+
+	private String amberTimesFile;
+	
+	private String intergreenTimesFile;
+
+	private String signalControlFile;
 
 	public SignalSystemsConfigGroup() {
 		super(GROUPNAME);
@@ -49,16 +61,25 @@ public class SignalSystemsConfigGroup extends Module {
 
 	@Override
 	public String getValue(final String key) {
-		throw new UnsupportedOperationException("This method is only implemented if compatibility with old code is needed, which is not the case for withinday replanning");
+		throw new UnsupportedOperationException("This method is only implemented if compatibility with old code is needed, which is not the case for signals");
 	}
 
 	@Override
 	public void addParam(final String key, final String value){
-		if (SIGNALSYSTEMFILE.equalsIgnoreCase(key)){
+		if (SIGNALSYSTEM_FILE.equalsIgnoreCase(key)){
 			this.signalSystemFile = value.replace("\\", "/").trim();
 		}
-		else if (SIGNALSYSTEMCONFIGFILE.equalsIgnoreCase(key)) {
+		else if (SIGNALSYSTEMCONFIG_FILE.equalsIgnoreCase(key)) {
 			this.signalSystemConfigFile = value.replace("\\", "/").trim();
+		}
+		else if (SIGNALGROUPS_FILE.equalsIgnoreCase(key) || SIGNALCONTROL_FILE.equalsIgnoreCase(key)){
+			this.signalGroupsFile = value.replace("\\", "/").trim();
+		}
+		else if (AMBERTIMES_FILE.equalsIgnoreCase(key)){
+			this.amberTimesFile = value.replace("\\", "/").trim();
+		}
+		else if (INTERGREENTIMES_FILE.equalsIgnoreCase(key)){
+			this.intergreenTimesFile = value.replace("\\", "/").trim();
 		}
 		else {
 			throw new IllegalArgumentException("The key : " + key + " is not supported by this config group");
@@ -68,8 +89,11 @@ public class SignalSystemsConfigGroup extends Module {
 	@Override
 	public final TreeMap<String, String> getParams() {
 		TreeMap<String, String> map = new TreeMap<String, String>();
-		map.put(SIGNALSYSTEMFILE, this.getSignalSystemFile());
-		map.put(SIGNALSYSTEMCONFIGFILE, this.getSignalSystemConfigFile());
+		map.put(SIGNALSYSTEM_FILE, this.getSignalSystemFile());
+		map.put(SIGNALCONTROL_FILE, this.getSignalSystemConfigFile());
+		map.put(SIGNALGROUPS_FILE, this.getSignalGroupsFile());
+		map.put(AMBERTIMES_FILE, this.getAmberTimesFile());
+		map.put(INTERGREENTIMES_FILE, this.getIntergreenTimesFile());
 		return map;
 	}
 
@@ -102,5 +126,36 @@ public class SignalSystemsConfigGroup extends Module {
 		this.signalSystemConfigFile = signalSystemConfigFile;
 	}
 
+	public String getSignalGroupsFile() {
+		return this.signalGroupsFile;
+	}
+	
+	public void setSignalGroupsFile(String filename){
+		this.signalGroupsFile = filename;
+	}
+
+	public String getAmberTimesFile() {
+		return this.amberTimesFile;
+	}
+	
+	public void setAmberTimesFile(String filename){
+		this.amberTimesFile = filename;
+	}
+	
+	public String getIntergreenTimesFile() {
+		return intergreenTimesFile;
+	}
+	
+	public void setIntergreenTimesFile(String intergreenTimesFile) {
+		this.intergreenTimesFile = intergreenTimesFile;
+	}
+
+	public String getSignalControlFile() {
+		return this.signalControlFile;
+	}
+	
+	public void setSignalControlFile(String filename){
+		this.signalControlFile = filename;
+	}
 
 }
