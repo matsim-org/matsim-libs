232f649b34612c2ad2b5a9f8b6aab80955b7e561
jwjoubert <johan.joubert@up.ac.za>
2010-10-04 21:19:27 +0000

    Added shell for sanral config files.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12898 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/jjoubert/src/main/java/playground/jjoubert/roadpricing/controler/SanralControler_base.java
@@ -0,0 +1,24 @@
+package playground.jjoubert.roadpricing.controler;
+
+import org.matsim.core.config.Config;
+
+import playground.jjoubert.Utilities.matsim2urbansim.controler.MyBasicConfig;
+
+public class SanralControler_base {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Parse the necessary arguments.
+		
+		// Set up the basic config.
+		MyBasicConfig mbc = new MyBasicConfig();
+		Config config = mbc.getConfig();
+		
+		// Customise the config file.
+		
+
+	}
+
+}
