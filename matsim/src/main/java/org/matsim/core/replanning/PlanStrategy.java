82419df8bbcfafb84e1179273479a1fcb0814360
mrieser <rieser@gmail.com>
2010-10-05 11:34:05 +0000

    - adding GPL-Header - replace usage of PersonImpl with Person
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@12904 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/core/replanning/PlanStrategy.java
+++ matsim/src/main/java/org/matsim/core/replanning/PlanStrategy.java
@@ -1,8 +1,26 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2010 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
 package org.matsim.core.replanning;
 
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.replanning.PlanStrategyModule;
-import org.matsim.core.population.PersonImpl;
 import org.matsim.core.replanning.selectors.PlanSelector;
 
 public interface PlanStrategy {
@@ -40,7 +58,7 @@ public interface PlanStrategy {
 	 * Indicates that no additional persons will be handed to this module and
 	 * waits until this strategy has finished handling all persons.
 	 *
-	 * @see #run(PersonImpl)
+	 * @see #run(Person)
 	 */
 	public void finish();
 
@@ -49,6 +67,7 @@ public interface PlanStrategy {
 	 *
 	 * @return An automatically generated name for this strategy.
 	 */
+	@Override
 	public String toString();
 
 	public PlanSelector getPlanSelector();
