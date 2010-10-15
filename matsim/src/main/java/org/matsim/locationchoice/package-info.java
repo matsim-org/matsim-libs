0a2801663f6441fc8bc95bc79e542409f9f4aebd
andreas77 <andreashorni@hotmail.com>
2010-10-15 15:26:19 +0000

    javadoc
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@13048 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/locationchoice/package-info.java
+++ matsim/src/main/java/org/matsim/locationchoice/package-info.java
@@ -1,10 +1,16 @@
 /**
- * This package provides secondary location choice.
+ * This package provides location choice for discretionary activities.
  * 
- * <h2>Package Maintainer(s):</h2>
+ * <h2>Package Maintainer:</h2>
  * <ul>
  *   <li>Andreas Horni</li>
  * </ul>
+ * 
+ * <h2>Parameters:</h2>
+ * <ul>
+ *   <li><i>constrained</i> value:boolean default:false <br> Random location choice ("false") or constrained location choice ("true") based on travel time budgets (time geography).</li>
+ * </ul>
+ * 
  * 
  */
 package org.matsim.locationchoice;
