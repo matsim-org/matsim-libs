be5fde588f27bc16878b7aa755617b9a6c6da788
laemmel <gregor.laemmel@gmail.com>
2010-10-15 16:15:28 +0000

    Framework for sim2D v 2.0
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@13049 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/gregor/src/main/java/playground/gregor/sim2_v2/simulation/floor/ForceModule.java
@@ -0,0 +1,28 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * ForceModule.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.gregor.sim2_v2.simulation.floor;
+
+/**
+ * @author laemmel
+ * 
+ */
+public interface ForceModule {
+
+}
