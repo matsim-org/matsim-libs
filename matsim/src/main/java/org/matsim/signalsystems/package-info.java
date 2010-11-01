203ed248e7c103b1ca3ad670b360934a18f7cbe2
grether <dominik.grether@gmail.com>
2010-11-01 14:17:58 +0000

    extended documentation
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@13274 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/signalsystems/package-info.java
+++ matsim/src/main/java/org/matsim/signalsystems/package-info.java
@@ -2,11 +2,30 @@
  *  This package contains interfaces and classes that provide functionality to plug a signal system model 
  *  in the MATSim framework.
  *  
+ *  <h2>Usage</h2>
+ *  
+ *  If input files for signal systems are already available, simulation of traffic lights can be enabled via MATSim config options:
+ *  <ol>
+ *    <li>Set the parameter <pre><code>useSignalsystems</code></pre> to <pre><code>true</code></pre> in the 
+ *    config module <pre><code>scenario</code></pre></li>
+ *    <li>Set at least three input file names in the config module <pre><code>signalsystems</code></pre>:
+ *    <ol>
+ *    	<li> parameter name: <pre><code>signalsystems</code></pre> value: path to a file in the <pre><code>signalSystems_v2.0.xsd</code></pre> file format </li>
+ *    	<li> parameter name: <pre><code>signalgroups</code></pre> value: path to a file in the <pre><code>signalGroups_v2.0.xsd</code></pre> file format </li>
+ *    	<li> parameter name: <pre><code>signalcontrol</code></pre> value: path to a file in the <pre><code>signalControl_v2.0.xsd</code></pre> file format </li>
+ *    	<li> parameter name: <pre><code>ambertimes</code></pre> (optional) value: path to a file in the <pre><code>amberTimes_v1.0.xsd</code></pre> file format </li>
+ *    </ol>
+ *    </li>
+ *  </ol>
+ *  
+ *  An example of such a configuration can be found in the input folder of the SignalSystemsIntegrationTest. If your data is in an older file format have a look at the
+ *  package playground.dgrether.signalsystems.data.conversion for converters to the required file formats.
+ *  
  *  <h2>Model description</h2>
  *  
  *  <h3>Overview</h3>
  *  
- *  The model consists of several layers:
+ *  The model consists of several logical layers:
  *  
  *	<ul>
  *		<li>A data layer</li>
@@ -15,12 +34,20 @@
  *		<li>A layer to build the other layers</li>
  *	</ul>
  * 
- * Each layer can be found in a subpackage of this package. 
+ * Each layer can be found in a subpackage of this package, namely:
  * 
- * TODO short overview 
+ * <ul>
+ *		<li><pre><code>signalsystems.data</code></pre> the package containing the data layer</li>
+ *		<li><pre><code>signalsystems.model </code></pre> the package containing the model layer</li>
+ *		<li><pre><code>signalsystems.mobsim</code></pre>the package containing the layer to couple the model to the mobility simulation</li>
+ *		<li><pre><code>signalsystems.builder </code></pre>the package containing the layer to build the other layers</li>
+ *	</ul>
+ * 
+ * Each layer is documented separately in the package-info.java of the subpackage.
  * 
  * In addition to the main layers, the package contains some utility classes and needful helpers to plug 
  * the model into the MATSim Controler and to generate the required/desired input data.
+ * 
  * 
  * <h3>Extensibility</h3>
  * 
