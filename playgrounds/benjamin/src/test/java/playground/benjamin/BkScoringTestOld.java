3654c99159867cca1bed9c8b769ecbc01e31280f
benkick <benkick@users.sf.net>
2010-11-03 15:03:09 +0000

    restructuring playground
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@13309 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/benjamin/src/test/java/playground/benjamin/BkScoringTestOld.java
+++ playgrounds/benjamin/src/test/java/playground/benjamin/BkScoringTestOld.java
@@ -29,7 +29,7 @@ import org.matsim.core.scoring.EventsToScore;
 import org.matsim.testcases.MatsimTestCase;
 
 import playground.benjamin.distance.BkControlerDistance;
-import playground.benjamin.old.BKickIncomeControler;
+import playground.benjamin.old.income.BKickIncomeControler;
 
 
 /**
