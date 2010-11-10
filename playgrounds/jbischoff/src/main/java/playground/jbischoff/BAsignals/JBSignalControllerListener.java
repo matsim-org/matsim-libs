9c4c5162ea4e24ff72e4fe5dce419459bda32f36
jbischoff <bischoff@vsp.tu-berlin.de>
2010-11-10 09:01:22 +0000

    updated imports
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@13365 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/jbischoff/src/main/java/playground/jbischoff/BAsignals/JBSignalControllerListener.java
+++ playgrounds/jbischoff/src/main/java/playground/jbischoff/BAsignals/JBSignalControllerListener.java
@@ -23,11 +23,11 @@ import org.matsim.signalsystems.initialization.SignalsControllerListener;
 import org.matsim.signalsystems.model.QSimSignalEngine;
 import org.matsim.signalsystems.model.SignalEngine;
 import org.matsim.signalsystems.model.SignalSystemsManager;
-
+/*
 import playground.dgrether.analysis.charts.utils.DgChartWriter;
 import playground.dgrether.signalsystems.analysis.DgGreenSplitPerIterationGraph;
 import playground.dgrether.signalsystems.analysis.DgSignalGreenSplitHandler;
-
+/*
 
 /**
  * @author jbischoff
@@ -38,11 +38,11 @@ public class JBSignalControllerListener implements StartupListener, IterationSta
 	private SignalSystemsManager manager;
 	private SignalsControllerListener delegate;
 	private CarsOnLaneHandler collh;
-	private DgGreenSplitPerIterationGraph greenSplitPerIterationGraph;
-	private DgGreenSplitPerIterationGraph greenSplitPerIterationGraph1;
-	private DgGreenSplitPerIterationGraph greenSplitPerIterationGraph2;
-
-	private DgSignalGreenSplitHandler signalGreenSplitHandler;
+//	private DgGreenSplitPerIterationGraph greenSplitPerIterationGraph;
+//	private DgGreenSplitPerIterationGraph greenSplitPerIterationGraph1;
+//	private DgGreenSplitPerIterationGraph greenSplitPerIterationGraph2;
+//
+//	private DgSignalGreenSplitHandler signalGreenSplitHandler;
 	
 	public JBSignalControllerListener(SignalsControllerListener delegate){
 		this.collh = new CarsOnLaneHandler();
@@ -73,22 +73,22 @@ public class JBSignalControllerListener implements StartupListener, IterationSta
 
 	private void addControlerListeners(Controler c) {
 		// TODO Auto-generated method stub
-		signalGreenSplitHandler = new DgSignalGreenSplitHandler();
-		signalGreenSplitHandler.addSignalSystem(new IdImpl("18"));
-		signalGreenSplitHandler.addSignalSystem(new IdImpl("17"));
-		signalGreenSplitHandler.addSignalSystem(new IdImpl("1"));
-
-		greenSplitPerIterationGraph = new DgGreenSplitPerIterationGraph(c.getConfig().controler(), new IdImpl("18"));
-		greenSplitPerIterationGraph1 = new DgGreenSplitPerIterationGraph(c.getConfig().controler(), new IdImpl("18"));
-		greenSplitPerIterationGraph2 = new DgGreenSplitPerIterationGraph(c.getConfig().controler(), new IdImpl("18"));
-		
-		c.getEvents().addHandler(signalGreenSplitHandler);
-		c.addControlerListener(new StartupListener() {
-
-			public void notifyStartup(StartupEvent e) {
-				e.getControler().getEvents().addHandler(signalGreenSplitHandler);
-			}
-		} );
+//		signalGreenSplitHandler = new DgSignalGreenSplitHandler();
+//		signalGreenSplitHandler.addSignalSystem(new IdImpl("18"));
+//		signalGreenSplitHandler.addSignalSystem(new IdImpl("17"));
+//		signalGreenSplitHandler.addSignalSystem(new IdImpl("1"));
+//
+//		greenSplitPerIterationGraph = new DgGreenSplitPerIterationGraph(c.getConfig().controler(), new IdImpl("18"));
+//		greenSplitPerIterationGraph1 = new DgGreenSplitPerIterationGraph(c.getConfig().controler(), new IdImpl("18"));
+//		greenSplitPerIterationGraph2 = new DgGreenSplitPerIterationGraph(c.getConfig().controler(), new IdImpl("18"));
+//		
+//		c.getEvents().addHandler(signalGreenSplitHandler);
+//		c.addControlerListener(new StartupListener() {
+//
+//			public void notifyStartup(StartupEvent e) {
+//				e.getControler().getEvents().addHandler(signalGreenSplitHandler);
+//			}
+//		} );
 		
 		
 		c.addControlerListener((new ShutdownListener() {
@@ -103,32 +103,34 @@ public class JBSignalControllerListener implements StartupListener, IterationSta
 		c.addControlerListener(new IterationEndsListener() {
 			public void notifyIterationEnds(IterationEndsEvent e) {
 
-				greenSplitPerIterationGraph.addIterationData(signalGreenSplitHandler, e.getIteration());
-				greenSplitPerIterationGraph1.addIterationData(signalGreenSplitHandler, e.getIteration());
-				greenSplitPerIterationGraph2.addIterationData(signalGreenSplitHandler, e.getIteration());
-				}});
+//				greenSplitPerIterationGraph.addIterationData(signalGreenSplitHandler, e.getIteration());
+//				greenSplitPerIterationGraph1.addIterationData(signalGreenSplitHandler, e.getIteration());
+//				greenSplitPerIterationGraph2.addIterationData(signalGreenSplitHandler, e.getIteration());
+//				}});
 		
-		c.addControlerListener(new ShutdownListener() {
-			  private final Logger logg = Logger
-		      .getLogger(ShutdownListener.class);
-			public void notifyShutdown(ShutdownEvent e) {
+//		c.addControlerListener(new ShutdownListener() {
+//			  private final Logger logg = Logger
+//		      .getLogger(ShutdownListener.class);
+//			public void notifyShutdown(ShutdownEvent e) {
 				
 
-				DgChartWriter.writeChart(e.getControler().getControlerIO().getOutputFilename("greensplit"), greenSplitPerIterationGraph.createChart());
-				for (Id ssid : signalGreenSplitHandler.getSystemIdAnalysisDataMap().keySet()){
-					//logg.info("=======Statistic for SignalSystem: "+ssid+" =============");
-					/*	for (Entry<Id, DgSignalGroupAnalysisData> entry : signalGreenSplitHandler.getSystemIdAnalysisDataMap().get(ssid).getSystemGroupAnalysisDataMap().entrySet() ){
+//				DgChartWriter.writeChart(e.getControler().getControlerIO().getOutputFilename("greensplit"), greenSplitPerIterationGraph.createChart());
+//				for (Id ssid : signalGreenSplitHandler.getSystemIdAnalysisDataMap().keySet()){
+//					//logg.info("=======Statistic for SignalSystem: "+ssid+" =============");
+//					/*	for (Entry<Id, DgSignalGroupAnalysisData> entry : signalGreenSplitHandler.getSystemIdAnalysisDataMap().get(ssid).getSystemGroupAnalysisDataMap().entrySet() ){
 //logg.info("for signalgroup: "+entry.getKey());
-for (Entry <SignalGroupState,Double> ee : entry.getValue().getStateTimeMap().entrySet()){
+//for (Entry <SignalGroupState,Double> ee : entry.getValue().getStateTimeMap().entrySet()){
 //logg.info(ee.getKey()+": "+ee.getValue());
-logg.info("b;"+ssid+";"+entry.getKey()+";"+ee.getKey()+";"+ee.getValue());
+//logg.info("b;"+ssid+";"+entry.getKey()+";"+ee.getKey()+";"+ee.getValue());
 //TravelTimeCalculator ttc = e.getControler().getTravelTimeCalculatorFactory().createTravelTimeCalculator(e.getControler().getNetwork(),e.getControler().getConfig().travelTimeCalculator());
-						
-					}
-				}*/
-				}
-			}
-		});
+//						
+//					}
+//				}
+//				}
+//			}
+		}}
+			)
+			;
 		
 		
 		
