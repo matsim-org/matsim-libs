3d49259cc097f9c38e6cf2936ae2caa60d66b499
andreas77 <andreashorni@hotmail.com>
2010-11-12 13:07:17 +0000

    excluding border-crossing traffic
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@13383 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/anhorni/src/main/java/playground/anhorni/analysis/CalculatePlanTravelStatsDiscretionaryActs.java
+++ playgrounds/anhorni/src/main/java/playground/anhorni/analysis/CalculatePlanTravelStatsDiscretionaryActs.java
@@ -26,7 +26,6 @@ import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.Plan;
 import org.matsim.api.core.v01.population.PlanElement;
-import org.matsim.core.facilities.FacilitiesReaderMatsimV1;
 import org.matsim.core.network.MatsimNetworkReader;
 import org.matsim.core.population.MatsimPopulationReader;
 import org.matsim.core.population.PlanImpl;
@@ -39,10 +38,11 @@ public class CalculatePlanTravelStatsDiscretionaryActs {
 	private Bins leisureBins;
 	private ScenarioImpl scenario = new ScenarioImpl();
 	private String outPath;
+	private int analysisPopulationOffset = 0;
 	private final static Logger log = Logger.getLogger(CalculatePlanTravelStatsDiscretionaryActs.class);
 
 
-	public CalculatePlanTravelStatsDiscretionaryActs(double spacing, double maxNetworkDistance, String outPath) {	
+	public CalculatePlanTravelStatsDiscretionaryActs(double spacing, double maxNetworkDistance, String outPath, int analysisPopulationOffset) {	
 		this.shopBins = new Bins(spacing, maxNetworkDistance, "shop_distance");
 		this.leisureBins = new Bins(spacing, maxNetworkDistance, "leisure_distance");
 		this.outPath = outPath;
@@ -50,21 +50,23 @@ public class CalculatePlanTravelStatsDiscretionaryActs {
 	
 	public static void main(String [] args) {
 		CalculatePlanTravelStatsDiscretionaryActs calculator = new CalculatePlanTravelStatsDiscretionaryActs(
-				Double.parseDouble(args[3]), Double.parseDouble(args[4]), args[5]);
-		calculator.init(args[0], args[1], args[2]);
+				Double.parseDouble(args[2]), Double.parseDouble(args[3]), args[4], Integer.parseInt(args[5]));
+		calculator.init(args[0], args[1]);
 		calculator.run();
 		log.info("Analysis finished -----------------------------------------");
 	}
 	
-	private void init(final String plansFilePath, final String networkFilePath, final String facilitiesFilePath) {
+	private void init(final String plansFilePath, final String networkFilePath) {
 		new MatsimNetworkReader(scenario).readFile(networkFilePath);		
-		new FacilitiesReaderMatsimV1(scenario).readFile(facilitiesFilePath);
 		MatsimPopulationReader populationReader = new MatsimPopulationReader(this.scenario);
 		populationReader.readFile(plansFilePath);
 	}
 
 	private void run() {			
-		for (Person p : this.scenario.getPopulation().getPersons().values()) {			
+		for (Person p : this.scenario.getPopulation().getPersons().values()) {	
+			
+			if (Integer.parseInt(p.getId().toString()) > this.analysisPopulationOffset) continue;
+			
 			PlanImpl plan = (PlanImpl) p.getSelectedPlan();
 				
 			// find best plan
@@ -95,7 +97,7 @@ public class CalculatePlanTravelStatsDiscretionaryActs {
 				}
 			}
 		}
-		this.shopBins.plotBinnedDistribution(this.outPath + "shop_bins", "#", "m");
-		this.leisureBins.plotBinnedDistribution(this.outPath + "leisure_bins", "#", "m");
+		this.shopBins.plotBinnedDistribution(this.outPath + this.shopBins.interval + "_", "#", "m");
+		this.leisureBins.plotBinnedDistribution(this.outPath + this.leisureBins.interval + "_", "#", "m");
 	}
 }
