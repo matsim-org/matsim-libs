d6282fc84c0ebdccaea4a7d58070e82ada07604c
vsp_nagel <2008@kainagel.org>
2010-11-17 12:31:06 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@13487 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/main/java/org/matsim/ptproject/qsim/comparators/PlanAgentDepartureTimeComparator.java
@@ -0,0 +1,50 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * DriverAgentDepartureTimeComparator.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2009 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.ptproject.qsim.comparators;
+
+import java.io.Serializable;
+import java.util.Comparator;
+
+import org.matsim.core.mobsim.framework.PersonDriverAgent;
+import org.matsim.core.mobsim.framework.PlanAgent;
+
+/**
+ * Compares two {@link PersonDriverAgent}s according to their (planned) departure time. If the 
+ * departure times are the same, the agent with the higher id is considered smaller.
+ *
+ * @author mrieser
+ * 
+ * @see PersonDriverAgent#getActivityEndTime()
+ */
+public class PlanAgentDepartureTimeComparator implements Comparator<PlanAgent>, Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public int compare(PlanAgent agent1, PlanAgent agent2) {
+		int cmp = Double.compare(agent1.getActivityEndTime(), agent2.getActivityEndTime());
+		if (cmp == 0) {
+			// Both depart at the same time -> let the one with the larger id be first (=smaller)
+			return agent2.getId().compareTo(agent1.getId());
+		}
+		return cmp;
+	}
+
+}
