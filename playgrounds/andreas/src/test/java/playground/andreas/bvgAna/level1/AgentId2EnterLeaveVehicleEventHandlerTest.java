55b28b8d51c739ff4c17539b01b1c78672841108
fuerbas <fuerbas@users.sf.net>
2010-11-18 14:47:46 +0000

    completed functionality
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@13534 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/andreas/src/test/java/playground/andreas/bvgAna/level1/AgentId2EnterLeaveVehicleEventHandlerTest.java
+++ playgrounds/andreas/src/test/java/playground/andreas/bvgAna/level1/AgentId2EnterLeaveVehicleEventHandlerTest.java
@@ -24,12 +24,14 @@ public class AgentId2EnterLeaveVehicleEventHandlerTest {
     
     public void testAgentId2EnterLeaveVehicleEventHandler() {
     	       
-        Id[] ida= new Id[10];
+        Id[] ida= new Id[9];
     	Set<Id> id = new TreeSet<Id>();
-        for (int ii=0; ii<10; ii++){
+        for (int ii=0; ii<9; ii++){
         	ida[ii] = new IdImpl(ii); 
             id.add(ida[ii]);
         }
+        
+//        assign Ids to routes, vehicles and agents to be used in Test
         
         Id routeId1 = ida[2];
         Id routeId2 = ida[3];
@@ -38,18 +40,34 @@ public class AgentId2EnterLeaveVehicleEventHandlerTest {
         Id vehId2 = ida[1];
         Id vehId3 = ida[6];
         Id persId1 = ida[0];
-        Id persId2 =ida[5];
+        Id persId2 = ida[5];
+        Id persId3 = ida[8];
         
         EventsFactoryImpl ef = new EventsFactoryImpl();
         AgentId2EnterLeaveVehicleEventHandler handler = new AgentId2EnterLeaveVehicleEventHandler(id);
         
+//        create Events
         
-        PersonEntersVehicleEvent event1 = ef.createPersonEntersVehicleEvent(3.0*3600, persId1, vehId1, routeId1);
+        PersonEntersVehicleEvent event1 = ef.createPersonEntersVehicleEvent(3.0*3600, persId1, vehId1, routeId2);
         handler.handleEvent(event1);
-        Assert.assertNotNull(handler.getAgentId2EnterEventMap().get(persId1));
-//        Assert.assertEquals(event1, handler.getAgentId2EnterEventMap().get(persId1));
+        PersonEntersVehicleEvent event2 = ef.createPersonEntersVehicleEvent(3.7*3600, persId2, vehId3, routeId1);
+        handler.handleEvent(event2);
+        PersonEntersVehicleEvent event3 = ef.createPersonEntersVehicleEvent(3.2*3600, persId3, vehId2, routeId3);
+        handler.handleEvent(event3);
+        
+        PersonLeavesVehicleEvent event4 = ef.createPersonLeavesVehicleEvent(4.0*3600, persId1, vehId2, routeId3);
+        handler.handleEvent(event4);
+        PersonLeavesVehicleEvent event5 = ef.createPersonLeavesVehicleEvent(4.7*3600, persId2, vehId3, routeId1);
+        handler.handleEvent(event5);
+        PersonLeavesVehicleEvent event6 = ef.createPersonLeavesVehicleEvent(4.2*3600, persId3, vehId1, routeId2);
+        handler.handleEvent(event6);
+        
         Assert.assertSame(event1, handler.getAgentId2EnterEventMap().get(persId1).get(0));
-
+        Assert.assertSame(event2, handler.getAgentId2EnterEventMap().get(persId2).get(0));
+        Assert.assertSame(event3, handler.getAgentId2EnterEventMap().get(persId3).get(0));
+        Assert.assertSame(event4, handler.getAgentId2LeaveEventMap().get(persId1).get(0));
+        Assert.assertSame(event5, handler.getAgentId2LeaveEventMap().get(persId2).get(0));
+        Assert.assertSame(event6, handler.getAgentId2LeaveEventMap().get(persId3).get(0));
         
         
     }
