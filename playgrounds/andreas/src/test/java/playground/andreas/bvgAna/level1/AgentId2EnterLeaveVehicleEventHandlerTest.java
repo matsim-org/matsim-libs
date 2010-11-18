b24e75b5ac80558a57678cb5790e3ca1edc8a66c
fuerbas <fuerbas@users.sf.net>
2010-11-18 14:28:24 +0000

    seems to function now, pls check if it works the way it is supposed to.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@13533 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/andreas/src/test/java/playground/andreas/bvgAna/level1/AgentId2EnterLeaveVehicleEventHandlerTest.java
+++ playgrounds/andreas/src/test/java/playground/andreas/bvgAna/level1/AgentId2EnterLeaveVehicleEventHandlerTest.java
@@ -1,6 +1,10 @@
 package playground.andreas.bvgAna.level1;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,6 +13,7 @@ import org.matsim.core.api.experimental.events.Event;
 import org.matsim.core.basic.v01.IdImpl;
 import org.matsim.core.events.EventsFactoryImpl;
 import org.matsim.core.events.PersonEntersVehicleEvent;
+import org.matsim.core.events.PersonLeavesVehicleEvent;
 
 public class AgentId2EnterLeaveVehicleEventHandlerTest {
 
@@ -16,25 +21,35 @@ public class AgentId2EnterLeaveVehicleEventHandlerTest {
      * Test method for {@link playground.andreas.bvgAna.level1.AgentId2EnterLeaveVehicleEventHandler#AgentId2EnterLeaveVehicleEventHandler(java.util.Set)}.
      */
     @Test
+    
     public void testAgentId2EnterLeaveVehicleEventHandler() {
-        
-        Set<Id> id = null;
-        for (int ii=0; ii<5; ii++){
-            id.add(new IdImpl(ii));
+    	       
+        Id[] ida= new Id[10];
+    	Set<Id> id = new TreeSet<Id>();
+        for (int ii=0; ii<10; ii++){
+        	ida[ii] = new IdImpl(ii); 
+            id.add(ida[ii]);
         }
-        Id[] ida = (Id[]) id.toArray();
-        Id routeId = ida[2];
+        
+        Id routeId1 = ida[2];
+        Id routeId2 = ida[3];
+        Id routeId3 = ida[7]; 
         Id vehId1 = ida[4];
         Id vehId2 = ida[1];
+        Id vehId3 = ida[6];
+        Id persId1 = ida[0];
+        Id persId2 =ida[5];
         
         EventsFactoryImpl ef = new EventsFactoryImpl();
         AgentId2EnterLeaveVehicleEventHandler handler = new AgentId2EnterLeaveVehicleEventHandler(id);
         
-//        TUT BISHER NOCH NICHTS!
         
-//        PersonEntersVehicleEvent event1 = ef.createPersonEntersVehicleEvent(3.0*3600-15, ida[0], vehId1, routeId);
-//        handler.handleEvent(event1);
-//        Assert.assertEquals(handler.getAgentId2EnterEventMap().get(ida[0]), event1);
+        PersonEntersVehicleEvent event1 = ef.createPersonEntersVehicleEvent(3.0*3600, persId1, vehId1, routeId1);
+        handler.handleEvent(event1);
+        Assert.assertNotNull(handler.getAgentId2EnterEventMap().get(persId1));
+//        Assert.assertEquals(event1, handler.getAgentId2EnterEventMap().get(persId1));
+        Assert.assertSame(event1, handler.getAgentId2EnterEventMap().get(persId1).get(0));
+
         
         
     }
