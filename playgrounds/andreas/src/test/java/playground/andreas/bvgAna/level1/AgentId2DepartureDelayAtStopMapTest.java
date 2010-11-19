84a874476c8aef466334aeb3b61a126b65b220f1
fuerbas <fuerbas@users.sf.net>
2010-11-19 11:31:57 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@13549 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/andreas/src/test/java/playground/andreas/bvgAna/level1/AgentId2DepartureDelayAtStopMapTest.java
@@ -0,0 +1,73 @@
+package playground.andreas.bvgAna.level1;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.core.api.experimental.events.AgentDepartureEvent;
+import org.matsim.core.basic.v01.IdImpl;
+import org.matsim.core.events.EventsFactoryImpl;
+import org.matsim.core.events.PersonEntersVehicleEvent;
+
+public class AgentId2DepartureDelayAtStopMapTest {
+
+	@Test
+	public void testAgentId2DepartureDelayAtStopMap() {
+		
+        Id[] ida= new Id[15];
+    	Set<Id> idSet = new TreeSet<Id>();
+        for (int ii=0; ii<15; ii++){
+        	ida[ii] = new IdImpl(ii); 
+            idSet.add(ida[ii]);
+        }
+        
+//        assign Ids to routes, vehicles and agents to be used in Test
+        
+        Id routeId1 = ida[2];
+        Id routeId2 = ida[3];
+        Id routeId3 = ida[7]; 
+        Id vehId1 = ida[4];
+        Id vehId2 = ida[1];
+        Id vehId3 = ida[6];
+        final Id persId1 = ida[0];
+        Id persId2 = ida[5];
+        Id persId3 = ida[8];
+        Id facilId1 = ida[9];
+        Id facilId2 = ida[11];
+        final Id linkId1 = ida[14];
+        Id linkId2 = ida[12];
+        Id linkId3 = ida[13];
+        Id linkId4 = ida[10];
+        
+        EventsFactoryImpl ef = new EventsFactoryImpl();
+        AgentId2DepartureDelayAtStopMap handler = new AgentId2DepartureDelayAtStopMap(idSet);
+        
+//        create Events
+        
+        AgentDepartureEvent event3 = ef.createAgentDepartureEvent(2.9*3600, persId1, linkId3, TransportMode.pt);
+		handler.handleEvent(event3);
+		AgentDepartureEvent event4 = ef.createAgentDepartureEvent(2.1*3600, persId2, linkId1, TransportMode.pt);
+		handler.handleEvent(event4);
+        
+        PersonEntersVehicleEvent event1 = ef.createPersonEntersVehicleEvent(2.9*3600, persId1, vehId1, routeId1);
+        handler.handleEvent(event1);
+        PersonEntersVehicleEvent event2 = ef.createPersonEntersVehicleEvent(2.1*3600, persId2, vehId2, routeId2);
+        handler.handleEvent(event2);
+		
+        Assert.assertTrue(handler.getStopId2DelayAtStopMap().containsKey(persId1));
+		Assert.assertEquals(event3.getTime(), handler.getStopId2DelayAtStopMap().get(persId1).getAgentEntersVehicle().get(0));
+		
+		Assert.assertTrue(handler.getStopId2DelayAtStopMap().containsKey(persId2));
+		Assert.assertEquals(event4.getTime(), handler.getStopId2DelayAtStopMap().get(persId2).getAgentEntersVehicle().get(0));
+		
+		
+	}
+
+}
