c3ab7a3a899e6e0895dc4c4e326b0749de815a31
mrieser <rieser@gmail.com>
2010-11-26 17:08:52 +0000

    add catchment area statistics, should work in principle, reports still quite a lot agents that cannot be analyzed, needs more thougths to solve this.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@13652 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/andreas/src/main/java/playground/andreas/bvgAna/mrieser/RunAnalyses.java
+++ playgrounds/andreas/src/main/java/playground/andreas/bvgAna/mrieser/RunAnalyses.java
@@ -26,7 +26,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -34,15 +36,25 @@ import java.util.TreeMap;
 
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.ScenarioImpl;
 import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.PlanElement;
+import org.matsim.api.core.v01.population.Population;
+import org.matsim.core.api.experimental.events.ActivityEndEvent;
+import org.matsim.core.api.experimental.events.ActivityStartEvent;
+import org.matsim.core.api.experimental.events.Event;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.basic.v01.IdImpl;
 import org.matsim.core.events.EventsManagerImpl;
 import org.matsim.core.events.MatsimEventsReader;
+import org.matsim.core.events.PersonEntersVehicleEvent;
+import org.matsim.core.events.PersonLeavesVehicleEvent;
 import org.matsim.core.network.MatsimNetworkReader;
 import org.matsim.core.network.NetworkFactoryImpl;
 import org.matsim.core.population.MatsimPopulationReader;
@@ -54,17 +66,26 @@ import org.matsim.core.utils.io.IOUtils;
 import org.matsim.core.utils.misc.Time;
 import org.matsim.population.algorithms.PersonAlgorithm;
 import org.matsim.population.algorithms.PersonFilterSelectedPlan;
+import org.matsim.pt.PtConstants;
 import org.matsim.pt.routes.ExperimentalTransitRouteFactory;
+import org.matsim.pt.transitSchedule.api.Departure;
+import org.matsim.pt.transitSchedule.api.TransitLine;
+import org.matsim.pt.transitSchedule.api.TransitRoute;
+import org.matsim.pt.transitSchedule.api.TransitRouteStop;
 import org.matsim.pt.transitSchedule.api.TransitSchedule;
 import org.matsim.pt.transitSchedule.api.TransitScheduleReader;
 import org.matsim.pt.transitSchedule.api.TransitStopFacility;
 import org.xml.sax.SAXException;
 
+import playground.andreas.bvgAna.level1.PersonEnterLeaveVehicle2ActivityHandler;
+import playground.andreas.bvgAna.level1.StopId2PersonEnterLeaveVehicleHandler;
 import playground.andreas.bvgAna.level2.StopId2RemainSeatedDataMap;
 import playground.andreas.bvgAna.level2.StopId2RemainSeatedDataMapData;
 import playground.andreas.bvgAna.level3.AgentId2StopDifferenceMap;
 
 public class RunAnalyses {
+
+	private final static Logger log = Logger.getLogger(RunAnalyses.class);
 
 	private final static String networkFilename = "/Volumes/Data/projects/bvg2010/runs/2010-11-21-run01/output_network.xml.gz";
 	private final static String plansFilename = "/Volumes/Data/projects/bvg2010/runs/2010-11-21-run01/output_plans.xml.gz";
@@ -317,14 +338,14 @@ public class RunAnalyses {
 
 	public void createMissedConnectionStats(Set<Id> agentIds) {
 		AgentId2StopDifferenceMap missedConnections = new AgentId2StopDifferenceMap(this.scenario.getPopulation(), agentIds);
-//		VehiclePlannedRealizedMissedDepartures missedDepartures = new VehiclePlannedRealizedMissedDepartures(vehicleDelayHandler);
-//		AgentId2DepartureDelayAtStopMap
 
 		EventsManager em = new EventsManagerImpl();
 		em.addHandler(missedConnections);
 		new MatsimEventsReader(em).readFile(eventsFilename);
 
 		Map<Id, List<Tuple<Id, Integer>>> missedVehiclesMap = missedConnections.getNumberOfMissedVehiclesMap();
+
+		// Histogram: Distribution of missed connections per user
 		System.out.println("# persons with data: " + missedVehiclesMap.size());
 
 		int[] nOfMissedConnectionsHistogram = new int[11];
@@ -348,6 +369,217 @@ public class RunAnalyses {
 		for (int i = 0; i < nOfMissedConnectionsHistogram.length; i++) {
 			System.out.println("with " + i + " missed connections: " + nOfMissedConnectionsHistogram[i]);
 		}
+
+		// List of #agents with missed connections per stop
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		Map<Id, Integer> missedConnectionsPerStop = new HashMap<Id, Integer>();
+		for (List<Tuple<Id, Integer>> entries : missedVehiclesMap.values()) {
+			for (Tuple<Id, Integer> t : entries) {
+				Id stopId = getBaseId(t.getFirst());
+				int missedVehicles = t.getSecond().intValue();
+				if (missedVehicles > 0) {
+					Integer cnt = missedConnectionsPerStop.get(stopId);
+					if (cnt == null) {
+						missedConnectionsPerStop.put(stopId, 1);
+					} else {
+						missedConnectionsPerStop.put(stopId, cnt.intValue() + 1);
+					}
+				}
+			}
+		}
+		TransitSchedule ts = ((ScenarioImpl) this.scenario).getTransitSchedule();
+		System.out.println("stopId\tstopX\tstopY\tstopName\t#affectPrs");
+		Set<Id> handledStops = new HashSet<Id>();
+		for (TransitStopFacility stop : ts.getFacilities().values()) {
+			Id stopId = getBaseId(stop.getId());
+			if (!handledStops.contains(stopId)) {
+				handledStops.add(stopId);
+				System.out.print(stopId + "\t" + stop.getCoord().getX() + "\t" + stop.getCoord().getY() + "\t" + stop.getName());
+				Integer affectPrs = missedConnectionsPerStop.get(stop.getId());
+				if (affectPrs == null) {
+					System.out.println("\t0");
+				} else {
+					System.out.println("\t" + affectPrs.intValue());
+				}
+			}
+		}
+	}
+
+	public void createCatchmentAreaStats(Set<Id> agentIds, Id lineId, Id[] routeIds) {
+		TransitSchedule ts = ((ScenarioImpl) this.scenario).getTransitSchedule();
+		Set<Id> stopIds = new HashSet<Id>();
+		Set<Id> vehicleIds = new HashSet<Id>();
+		TransitLine line = ts.getTransitLines().get(lineId);
+		for (Id routeId : routeIds) {
+			TransitRoute route = line.getRoutes().get(routeId);
+			for (TransitRouteStop stop : route.getStops()) {
+				stopIds.add(stop.getStopFacility().getId());
+			}
+			for (Departure dep : route.getDepartures().values()) {
+				vehicleIds.add(dep.getVehicleId());
+			}
+		}
+
+		StopId2PersonEnterLeaveVehicleHandler enterLeave = new StopId2PersonEnterLeaveVehicleHandler(stopIds);
+		PersonEnterLeaveVehicle2ActivityHandler enterLeave2Act = new PersonEnterLeaveVehicle2ActivityHandler(agentIds);
+		EventsManager em = new EventsManagerImpl();
+		em.addHandler(enterLeave);
+		em.addHandler(enterLeave2Act);
+		new MatsimEventsReader(em).readFile(eventsFilename);
+
+//		Set<Id> agentIds = new HashSet<Id>(1000);
+//		for (List<PersonEntersVehicleEvent> events : enterLeave.getStopId2PersonEnterEventMap().values()) {
+//			for (PersonEntersVehicleEvent event : events) {
+//				agentIds.add(event.getPersonId());
+//			}
+//		}
+
+		Map<Id, List<PersonEntersVehicleEvent>> enterEventsPerPerson = new HashMap<Id, List<PersonEntersVehicleEvent>>();
+		Map<Id, List<PersonLeavesVehicleEvent>> leaveEventsPerPerson = new HashMap<Id, List<PersonLeavesVehicleEvent>>();
+
+		for (List<PersonEntersVehicleEvent> events : enterLeave.getStopId2PersonEnterEventMap().values()) {
+			for (PersonEntersVehicleEvent event : events) {
+				if (vehicleIds.contains(event.getVehicleId())) {
+					List<PersonEntersVehicleEvent> list = enterEventsPerPerson.get(event.getPersonId());
+					if (list == null) {
+						list = new ArrayList<PersonEntersVehicleEvent>(3);
+						enterEventsPerPerson.put(event.getPersonId(), list);
+					}
+					list.add(event);
+				}
+			}
+		}
+		for (List<PersonLeavesVehicleEvent> events : enterLeave.getStopId2PersonLeaveEventMap().values()) {
+			for (PersonLeavesVehicleEvent event : events) {
+				if (vehicleIds.contains(event.getVehicleId())) {
+					List<PersonLeavesVehicleEvent> list = leaveEventsPerPerson.get(event.getPersonId());
+					if (list == null) {
+						list = new ArrayList<PersonLeavesVehicleEvent>(3);
+						leaveEventsPerPerson.put(event.getPersonId(), list);
+					}
+					list.add(event);
+				}
+			}
+		}
+
+		// step in parallel through both lists, match the data
+		Map<PersonEntersVehicleEvent, ActivityEndEvent> activityEnds = enterLeave2Act.getPersonEntersVehicleEvent2ActivityEndEvent();
+		Map<PersonLeavesVehicleEvent, ActivityStartEvent> activityStarts = enterLeave2Act.getPersonLeavesVehicleEvent2ActivityStartEvent();
+		Map<Id, List<AgentTripData>> agentData = new HashMap<Id, List<AgentTripData>>();
+		for (Id personId : enterEventsPerPerson.keySet()) {
+			List<PersonEntersVehicleEvent> enterList = enterEventsPerPerson.get(personId);
+			List<PersonLeavesVehicleEvent> leaveList = leaveEventsPerPerson.get(personId);
+
+			Comparator<Event> eventComparator = new Comparator<Event>() {
+				@Override
+				public int compare(Event o1, Event o2) {
+					return Double.compare(o1.getTime(), o2.getTime());
+				}
+			};
+			Collections.sort(enterList, eventComparator);
+			Collections.sort(leaveList, eventComparator);
+
+			int n = Math.min(enterList.size(), leaveList.size());
+			List<AgentTripData> datalist = new LinkedList<AgentTripData>();
+			agentData.put(personId, datalist);
+			for (int i = 0; i < n; i++) {
+				AgentTripData data = new AgentTripData(personId);
+				PersonEntersVehicleEvent enterEvent = enterList.get(i);
+				ActivityEndEvent endEvent = activityEnds.get(enterEvent);
+				data.fromActType = endEvent.getActType();
+				data.fromLinkId = endEvent.getLinkId();
+
+				PersonLeavesVehicleEvent leaveEvent = leaveList.get(i);
+				ActivityStartEvent startEvent = activityStarts.get(leaveEvent);
+				if (startEvent == null) {
+					log.warn("No ActivityStartEvent found for PersonLeavesVehicleEvent: time=" + leaveEvent.getTime() + " person=" + leaveEvent.getPersonId() + " vehicle=" + leaveEvent.getVehicleId());
+				} else {
+					data.toActType = startEvent.getActType();
+					data.toLinkId = startEvent.getLinkId();
+				}
+				datalist.add(data);
+			}
+		}
+
+		// now go through population and figure out exact coordinates if available
+
+		Population pop = this.scenario.getPopulation();
+		for (List<AgentTripData> list : agentData.values()) {
+			for (AgentTripData data : list) {
+				Person p = pop.getPersons().get(data.agentId);
+				Activity prevActivity = null;
+				for (PlanElement pe : p.getSelectedPlan().getPlanElements()) {
+					if (pe instanceof Activity) {
+						Activity a = (Activity) pe;
+
+						if (prevActivity != null) {
+							if (data.toActType == null) {
+								// we can only compare from activity, use first match
+								if (prevActivity.getType().equals(data.fromActType) && prevActivity.getLinkId().equals(data.fromLinkId)) {
+									data.fromActCoord = prevActivity.getCoord();
+									data.toActCoord = a.getCoord();
+									break;
+								}
+							} else {
+								// compare from and to activity
+								if (prevActivity.getType().equals(data.fromActType) && a.getType().equals(data.toActType)
+										&& prevActivity.getLinkId().equals(data.fromLinkId) && a.getLinkId().equals(data.toLinkId)) {
+									data.fromActCoord = prevActivity.getCoord();
+									data.toActCoord = a.getCoord();
+									break;
+								}
+							}
+						}
+						if (!a.getType().equals(PtConstants.TRANSIT_ACTIVITY_TYPE)) {
+							prevActivity = a;
+						}
+					}
+				}
+				if (data.fromActCoord == null) {
+					log.error("No from.coord found for person=" + data.agentId + " from.act=" + data.fromActType + " from.link=" + data.fromLinkId + " to.act=" + data.toActType + " to.link=" + data.toLinkId);
+				}
+			}
+		}
+
+		// now write everything out
+		System.out.println();
+		System.out.println();
+		System.out.println("Passenger-Analysis for line " + lineId + " on the following routes:");
+		for (Id routeId : routeIds) {
+			System.out.println("  " + routeId.toString());
+		}
+		System.out.println("AgentId\tfromType\tfromX\tfromY\ttoType\ttoX\ttoY");
+		for (List<AgentTripData> list : agentData.values()) {
+			for (AgentTripData data : list) {
+				if (data.fromActCoord == null) {
+					// write nothing
+				} else if (data.toActCoord == null) {
+					System.out.println(data.agentId + "\t" + data.fromActType + "\t" + data.fromActCoord.getX() +
+							"\t" + data.fromActCoord.getY() + "\t" + data.toActType + "\t\t");
+				} else {
+					System.out.println(data.agentId + "\t" + data.fromActType + "\t" + data.fromActCoord.getX() +
+							"\t" + data.fromActCoord.getY() + "\t" + data.toActType + "\t" + data.toActCoord.getX() +
+							"\t" + data.toActCoord.getY());
+				}
+			}
+		}
+
+	}
+
+	private static class AgentTripData {
+		public final Id agentId;
+		public String fromActType = null;
+		public Id fromLinkId = null;
+		public Coord fromActCoord = null;
+		public String toActType = null;
+		public Id toLinkId = null;
+		public Coord toActCoord = null;
+
+		public AgentTripData(final Id agentId) {
+			this.agentId = agentId;
+		}
 	}
 
 	public static void main(String[] args) {
@@ -361,6 +593,8 @@ public class RunAnalyses {
 //		System.out.println(allPersonIds.size());
 		app.readTransitSchedule();
 //		app.createRemainSeatedStats();
-		app.createMissedConnectionStats(allPersonIds);
+//		app.createMissedConnectionStats(allPersonIds);
+		app.createCatchmentAreaStats(allPersonIds, new IdImpl("BVB----145"),
+				new Id[] {new IdImpl("BVB----145.10.GEN12.R"), new IdImpl("BVB----145.10.GEN5.R"), new IdImpl("BVB----145.10.GEN8.R")});
 	}
 }
