bd98cbc30668de5deb75fc87912316b01da0f747
mrieser <rieser@gmail.com>
2010-11-26 17:06:27 +0000

    bugfixes/improvements: one ActivityEndEvent can relate to multiple PersonEntersVehicleEvents, and multiple PersonLeavesVehicleEvent relate to one ActivityStartEvent
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@13651 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/andreas/src/main/java/playground/andreas/bvgAna/level1/PersonEnterLeaveVehicle2ActivityHandler.java
+++ playgrounds/andreas/src/main/java/playground/andreas/bvgAna/level1/PersonEnterLeaveVehicle2ActivityHandler.java
@@ -19,7 +19,10 @@
 
 package playground.andreas.bvgAna.level1;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 
@@ -39,37 +42,37 @@ import org.matsim.pt.PtConstants;
 /**
  * Collects all <code>PersonEntersVehicleEvent</code> and <code>PersonLeavesVehicleEvent</code> with their
  * corresponding <code>ActivityEndEvent</code> and <code>ActivityStartEvent</code> for a given set of agent ids ignoring <code>pt interaction</code> events.
- * 
+ *
  * @author aneumann
  *
  */
 public class PersonEnterLeaveVehicle2ActivityHandler implements PersonEntersVehicleEventHandler, PersonLeavesVehicleEventHandler, ActivityStartEventHandler, ActivityEndEventHandler{
-	
+
 	private final Logger log = Logger.getLogger(PersonEnterLeaveVehicle2ActivityHandler.class);
 	private final Level logLevel = Level.DEBUG;
 	private Set<Id> agentIds;
 	private TreeMap<Id, ActivityEndEvent> agentId2ActEndEvent = new TreeMap<Id, ActivityEndEvent>();
-	private TreeMap<Id, PersonLeavesVehicleEvent> agentId2LeaveVehEvent = new TreeMap<Id, PersonLeavesVehicleEvent>();
-	
+	private TreeMap<Id, List<PersonLeavesVehicleEvent>> agentId2LeaveVehEvent = new TreeMap<Id, List<PersonLeavesVehicleEvent>>();
+
 	private HashMap<PersonEntersVehicleEvent, ActivityEndEvent> personsEntersVehicleEvent2ActivityEndEvent = new HashMap<PersonEntersVehicleEvent, ActivityEndEvent>();
 	private HashMap<PersonLeavesVehicleEvent, ActivityStartEvent> personLeavesVehicleEvent2ActivityStartEvent = new HashMap<PersonLeavesVehicleEvent, ActivityStartEvent>();
-	
+
 	public PersonEnterLeaveVehicle2ActivityHandler(Set<Id> agentIds){
 		this.log.setLevel(this.logLevel);
 		this.agentIds = agentIds;
 	}
-	
+
 	/**
 	 * @return Returns a map containing the preceding <code>ActivityEndEvent</code> for a given <code>PersonEntersVehicleEvent</code>
 	 */
-	public HashMap<PersonEntersVehicleEvent, ActivityEndEvent> getPersonsEntersVehicleEvent2ActivityEndEvent() {
+	public Map<PersonEntersVehicleEvent, ActivityEndEvent> getPersonEntersVehicleEvent2ActivityEndEvent() {
 		return this.personsEntersVehicleEvent2ActivityEndEvent;
 	}
 
 	/**
 	 * @return Returns a map containing the following <code>ActivityStartEvent</code> for a given <code>PersonLeavesVehicleEvent</code>
 	 */
-	public HashMap<PersonLeavesVehicleEvent, ActivityStartEvent> getPersonLeavesVehicleEvent2ActivityStartEvent() {
+	public Map<PersonLeavesVehicleEvent, ActivityStartEvent> getPersonLeavesVehicleEvent2ActivityStartEvent() {
 		return this.personLeavesVehicleEvent2ActivityStartEvent;
 	}
 
@@ -77,18 +80,22 @@ public class PersonEnterLeaveVehicle2ActivityHandler implements PersonEntersVehi
 	public void handleEvent(PersonEntersVehicleEvent event) {
 		// pair the events
 		Id personId = event.getPersonId();
-		if(this.agentIds.contains(personId)){
-			if(this.agentId2ActEndEvent.get(personId) != null){				
-				this.personsEntersVehicleEvent2ActivityEndEvent.put(event, this.agentId2ActEndEvent.remove(personId));
-			}
+		ActivityEndEvent endEvent = this.agentId2ActEndEvent.get(personId);
+		if (endEvent != null) {
+			this.personsEntersVehicleEvent2ActivityEndEvent.put(event, endEvent);
 		}
 	}
 
 	@Override
 	public void handleEvent(PersonLeavesVehicleEvent event) {
 		// simply collect the event
-		if(this.agentIds.contains(event.getPersonId())){
-			this.agentId2LeaveVehEvent.put(event.getPersonId(), event);
+		if (this.agentIds.contains(event.getPersonId())) {
+			List<PersonLeavesVehicleEvent> events = this.agentId2LeaveVehEvent.get(event.getPersonId());
+			if (events == null) {
+				events = new ArrayList<PersonLeavesVehicleEvent>(3);
+				this.agentId2LeaveVehEvent.put(event.getPersonId(), events);
+			}
+			events.add(event);
 		}
 	}
 
@@ -98,8 +105,12 @@ public class PersonEnterLeaveVehicle2ActivityHandler implements PersonEntersVehi
 			// pair the events
 			Id personId = event.getPersonId();
 			if(this.agentIds.contains(personId)){
-				if(this.agentId2LeaveVehEvent.get(personId) != null){
-					this.personLeavesVehicleEvent2ActivityStartEvent.put(this.agentId2LeaveVehEvent.remove(personId), event);
+				this.agentId2ActEndEvent.remove(personId); // should not be necessary if we trust the events
+				List<PersonLeavesVehicleEvent> events = this.agentId2LeaveVehEvent.get(event.getPersonId());
+				if (events != null) {
+					for (PersonLeavesVehicleEvent leaveEvent : events) {
+						this.personLeavesVehicleEvent2ActivityStartEvent.put(leaveEvent, event);
+					}
 				}
 			}
 		}
