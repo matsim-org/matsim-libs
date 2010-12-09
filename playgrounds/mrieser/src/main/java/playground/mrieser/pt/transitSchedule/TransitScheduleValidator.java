3ce0bbe681e5799bb18c5cd72936040158f1ad56
mrieser <rieser@gmail.com>
2010-12-09 21:00:18 +0000

    added additional validation rule as well as a "validateAll" method.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@13773 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/mrieser/src/main/java/playground/mrieser/pt/transitSchedule/TransitScheduleValidator.java
+++ playgrounds/mrieser/src/main/java/playground/mrieser/pt/transitSchedule/TransitScheduleValidator.java
@@ -22,6 +22,7 @@ package playground.mrieser.pt.transitSchedule;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -34,6 +35,7 @@ import org.matsim.core.network.MatsimNetworkReader;
 import org.matsim.core.population.routes.NetworkRoute;
 import org.matsim.pt.transitSchedule.api.TransitLine;
 import org.matsim.pt.transitSchedule.api.TransitRoute;
+import org.matsim.pt.transitSchedule.api.TransitRouteStop;
 import org.matsim.pt.transitSchedule.api.TransitSchedule;
 import org.matsim.pt.transitSchedule.api.TransitScheduleReader;
 import org.xml.sax.SAXException;
@@ -43,6 +45,13 @@ import org.xml.sax.SAXException;
  */
 public abstract class TransitScheduleValidator {
 
+	/**
+	 * Checks that the links specified for a network route really builds a complete route that can be driven along.
+	 *
+	 * @param schedule
+	 * @param network
+	 * @return
+	 */
 	public static ValidationResult validateNetworkRoutes(final TransitSchedule schedule, final Network network) {
 		ValidationResult result = new ValidationResult();
 
@@ -64,10 +73,68 @@ public abstract class TransitScheduleValidator {
 				}
 			}
 		}
-
 		return result;
 	}
 
+	/**
+	 * Checks that all the listed stops in a route appear in that order when driving along the network route
+	 *
+	 * @param schedule
+	 * @param network
+	 * @return
+	 */
+	public static ValidationResult validateStopsOnNetworkRoute(final TransitSchedule schedule, final Network network) {
+		ValidationResult result = new ValidationResult();
+
+		for (TransitLine line : schedule.getTransitLines().values()) {
+			for (TransitRoute route : line.getRoutes().values()) {
+				NetworkRoute netRoute = route.getRoute();
+				if (netRoute == null) {
+					result.addError("Transit line " + line.getId() + ", route " + route.getId() + " has no network route.");
+				} else {
+					List<Id> linkIds = new ArrayList<Id>();
+					linkIds.add(netRoute.getStartLinkId());
+					linkIds.addAll(netRoute.getLinkIds());
+					linkIds.add(netRoute.getEndLinkId());
+					Iterator<Id> linkIdIterator = linkIds.iterator();
+					Id nextLinkId = linkIdIterator.next();
+					boolean error = false;
+					for (TransitRouteStop stop : route.getStops()) {
+						Id linkRefId = stop.getStopFacility().getLinkId();
+
+						while (!linkRefId.equals(nextLinkId)) {
+							if (linkIdIterator.hasNext()) {
+								nextLinkId = linkIdIterator.next();
+							} else {
+								result.addError("Transit line " + line.getId() + ", route " + route.getId() + ": Stop " + stop.getStopFacility().getId() + " cannot be reached along network route.");
+								error = true;
+								break;
+							}
+						}
+						if (error) {
+							break;
+						}
+
+					}
+				}
+			}
+		}
+		return result;
+	}
+
+	public static ValidationResult validateAll(final TransitSchedule schedule, final Network network) {
+		ValidationResult v = validateNetworkRoutes(schedule, network);
+		ValidationResult v2 = validateStopsOnNetworkRoute(schedule, network);
+		v.add(v2);
+		return v;
+	}
+
+	/**
+	 * @param args [0] path to transitSchedule.xml, [1] path to network.xml
+	 * @throws IOException
+	 * @throws SAXException
+	 * @throws ParserConfigurationException
+	 */
 	public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
 		ScenarioImpl s = new ScenarioImpl();
 		s.getConfig().scenario().setUseTransit(true);
@@ -75,14 +142,15 @@ public abstract class TransitScheduleValidator {
 		Network net = s.getNetwork();
 
 //		args = new String[] {"/data/projects/bvg2010/Daten/transitSchedule.oevnet.xml", "/data/projects/bvg2010/Daten/transit-network.xml"};
-		args = new String[] {"/data/senozon/visumData/matsim/transitSchedule.xml", "/data/senozon/visumData/matsim/network.cleaned.xml"};
+//		args = new String[] {"/Users/cello/Desktop/MATSim/pt-test/matsim-0.2.0-rc1-release/pt-rdam/transitSchedule.xml", "/Users/cello/Desktop/MATSim/pt-test/matsim-0.2.0-rc1-release/pt-rdam/rdamnetwork.xml"};
+//		args = new String[] {"/data/senozon/visumData/matsim/transitSchedule.xml", "/data/senozon/visumData/matsim/network.cleaned.xml"};
 
 		new MatsimNetworkReader(s).parse(args[1]);
 		new TransitScheduleReader(s).readFile(args[0]);
 
-		ValidationResult v = validateNetworkRoutes(ts, net);
+		ValidationResult v = validateAll(ts, net);
 		if (v.isValid()) {
-			System.out.println("Schedule is valid!");
+			System.out.println("Schedule appears valid!");
 		} else {
 			System.out.println("Schedule is NOT valid!");
 		}
@@ -124,6 +192,12 @@ public abstract class TransitScheduleValidator {
 		/*package*/ void addError(final String error) {
 			this.errors.add(error);
 			this.isValid = false;
+		}
+
+		/*package*/ void add(final ValidationResult otherResult) {
+			this.warnings.addAll(otherResult.warnings);
+			this.errors.addAll(otherResult.errors);
+			this.isValid = this.isValid && otherResult.isValid;
 		}
 	}
 }
