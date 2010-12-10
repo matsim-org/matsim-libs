1368e88114bbfd2713c4bf87a81abcbf28de9b20
jwjoubert <johan.joubert@up.ac.za>
2010-12-10 14:27:22 +0000

    Added work duration to plans generation.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@13782 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/jjoubert/src/main/java/playground/jjoubert/Utilities/roadpricing/MyDemandMatrix.java
+++ playgrounds/jjoubert/src/main/java/playground/jjoubert/Utilities/roadpricing/MyDemandMatrix.java
@@ -266,6 +266,7 @@ public class MyDemandMatrix {
 					plan.addLeg(toWork);
 					Activity work = pf.createActivityFromCoord("work", locationCoordinates.get(theDestination));
 					work.setStartTime(25200); // 07:00
+					work.setEndTime(57600);
 					plan.addActivity(work);
 					Leg fromWork = pf.createLeg("car");
 					plan.addLeg(fromWork);
