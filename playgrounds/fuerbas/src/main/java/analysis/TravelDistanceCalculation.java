52495995404fa519d64223466de5b3b15f588a5d
fuerbas <fuerbas@users.sf.net>
2010-12-16 13:31:09 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@13837 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/fuerbas/src/main/java/analysis/TravelDistanceCalculation.java
+++ playgrounds/fuerbas/src/main/java/analysis/TravelDistanceCalculation.java
@@ -1,25 +1,48 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
 package analysis;
 
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.events.EventsManagerImpl;
 import org.matsim.core.events.MatsimEventsReader;
 
-import tutorial.programming.example06EventsHandling.MyEventHandler1;
-
 public class TravelDistanceCalculation {
 
 	/**
 	 * @param args
 	 */
+	
+	
 	public static void main(String[] args) {
 		//path to events file
-		String inputFile = "...";
+//		String inputFile = "/home01/sfuerbas/workspace/893.2200.events.txt.gz";
+		
+		String inputFile = args[0];
 		
 		//create an event object
 		EventsManager events = new EventsManagerImpl();
 		
 		//create the handler
 		TravelDistanceHandler handler = new TravelDistanceHandler();
+		
+		handler.createHandlerScenario();
 		
 		//add the handler
 		events.addHandler(handler);
@@ -28,6 +51,8 @@ public class TravelDistanceCalculation {
 		MatsimEventsReader reader = new MatsimEventsReader(events);
 		reader.readFile(inputFile);
 		System.out.println("Events file read!");
+		
+		handler.returnDistances();
 		
 //		...
 
