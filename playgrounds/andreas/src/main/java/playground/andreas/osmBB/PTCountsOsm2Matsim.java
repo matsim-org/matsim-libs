af2fc1b164424be0a3fa26ed1f58b31bb352d95f
vsp_neumann <neumann@senozon.de>
2010-12-17 08:21:09 +0000

    removed playground
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@13839 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/andreas/src/main/java/playground/andreas/osmBB/PTCountsOsm2Matsim.java
+++ playgrounds/andreas/src/main/java/playground/andreas/osmBB/PTCountsOsm2Matsim.java
@@ -13,9 +13,9 @@ import org.matsim.counts.CountsReaderMatsimV1;
 import org.xml.sax.SAXException;
 
 import playground.andreas.osmBB.osm2counts.Osm2Counts;
-import playground.droeder.osm.ResizeLinksByCount2;
-import playground.droeder.osm.ResizeLinksByCount3;
-import playground.droeder.osm.ResizeLinksByCount4;
+//import playground.droeder.osm.ResizeLinksByCount2;
+//import playground.droeder.osm.ResizeLinksByCount3;
+//import playground.droeder.osm.ResizeLinksByCount4;
 //import playground.droeder.osm.ResizeLinksByCount2;
 //import playground.droeder.osm.ResizeLinksByCount3;
 import playground.mzilske.osm.OsmPrepare;
@@ -71,8 +71,8 @@ public class PTCountsOsm2Matsim {
 //		ResizeLinksByCount3 r = new ResizeLinksByCount3(outDir + outName + "_network.xml", counts, shortNameMap, 1.0);
 //		r.run(outDir + outName + "_network_resized.xml");
 		
-		ResizeLinksByCount4 r = new ResizeLinksByCount4(outDir + outName + "_network.xml", counts, shortNameMap, 1.0);
-		r.run(outDir + outName + "_network_resized.xml");
+//		ResizeLinksByCount4 r = new ResizeLinksByCount4(outDir + outName + "_network.xml", counts, shortNameMap, 1.0);
+//		r.run(outDir + outName + "_network_resized.xml");
 		
 		PTCountsNetworkSimplifier ptCountNetSimplifier = new PTCountsNetworkSimplifier(outDir + outName + "_network_resized.xml", outDir + outName + "_schedule.xml", outDir + outName + "_network_merged.xml", outDir + outName + "_schedule_merged.xml", shortNameMap, counts, countsOutFile);
 		Set<Integer> nodeTypesToMerge = new TreeSet<Integer>();
