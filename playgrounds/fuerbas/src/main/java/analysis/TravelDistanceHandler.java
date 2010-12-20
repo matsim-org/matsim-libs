f7ef75a60b4097361fafc99df2621449b4168d36
fuerbas <fuerbas@users.sf.net>
2010-12-20 14:48:20 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@13860 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/fuerbas/src/main/java/analysis/TravelDistanceHandler.java
+++ playgrounds/fuerbas/src/main/java/analysis/TravelDistanceHandler.java
@@ -1,44 +1,93 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
 package analysis;
 
+import java.util.HashMap;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.core.api.experimental.ScenarioFactoryImpl;
+import org.matsim.core.api.experimental.ScenarioLoader;
 import org.matsim.core.api.experimental.events.LinkEnterEvent;
 import org.matsim.core.api.experimental.events.LinkLeaveEvent;
 import org.matsim.core.api.experimental.events.handler.LinkEnterEventHandler;
 import org.matsim.core.api.experimental.events.handler.LinkLeaveEventHandler;
+import org.matsim.core.config.Config;
+import org.matsim.core.network.NetworkImpl;
+import org.matsim.core.scenario.ScenarioLoaderImpl;
+
+import playground.benjamin.analysis.BkAnalysis;
 
 public class TravelDistanceHandler implements LinkLeaveEventHandler, LinkEnterEventHandler {
-
-
+	
+	
+	HashMap<Id, Double> distance = new HashMap<Id, Double>();
+	Network network = null;
+//	String networkfile = "/home01/sfuerbas/workspace/893.output_network.xml.gz";
+//	String plansfile = "/home01/sfuerbas/workspace/893.2200.plans.xml.gz";
+	
+	String networkfile = "893.output_network.xml.gz";
+	String plansfile = "893.2200.plans.xml.gz";
+	
+	
+	public void createHandlerScenario() {
+		Scenario scen = new ScenarioFactoryImpl().createScenario();
+		Config config = scen.getConfig();
+		config.network().setInputFile(networkfile);
+		config.plans().setInputFile(plansfile);
+		ScenarioLoader sl = new ScenarioLoaderImpl(scen);
+		sl.loadScenario();
+		network = scen.getNetwork();
+	}
+	
+	
+	public void returnDistances(){
+		System.out.println(distance.toString());
+	}
+	
+	
 
 	@Override
 	public void reset(int iteration) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public void handleEvent(LinkEnterEvent event) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("I AM TOTALLY HANDLING STUFF");
+		
+		if (distance.containsKey(event.getPersonId())) {
+			double distanceSoFar = distance.get(event.getPersonId());
+			distance.put(event.getPersonId(), distanceSoFar+network.getLinks().get(event.getLinkId()).getLength());
+		}
+		else
+			distance.put(event.getPersonId(), network.getLinks().get(event.getLinkId()).getLength());
 	}
 
 	@Override
 	public void handleEvent(LinkLeaveEvent event) {
 		// TODO Auto-generated method stub
-
+		
 	}
-
-	public void createHandlerScenario() {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void returnDistances() {
-		// TODO Auto-generated method stub
-
-	}
-
-
-
-
-
+	
 }
