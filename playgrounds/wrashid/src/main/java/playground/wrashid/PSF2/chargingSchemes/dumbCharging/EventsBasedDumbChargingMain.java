006abdd250daf69b7477fce11bae704be95032a6
rashid_waraich <rashid_waraich@users.sf.net>
2010-12-21 14:17:33 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@13876 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/main/java/playground/wrashid/PSF2/chargingSchemes/dumbCharging/EventsBasedDumbChargingMain.java
+++ playgrounds/wrashid/src/main/java/playground/wrashid/PSF2/chargingSchemes/dumbCharging/EventsBasedDumbChargingMain.java
@@ -5,7 +5,8 @@ import playground.wrashid.PSF.PSS.PSSControler;
 public class EventsBasedDumbChargingMain {
 
 	public static void main(String[] args) {
-		runEventsBasedPSSControler("H:\\data\\experiments\\ARTEMIS\\input\\pss\\zh\\config-event-file-based.xml");
+		// run on slanger4
+		runEventsBasedPSSControler("H:\\data\\experiments\\ARTEMIS\\zh\\dumb charging\\input\\config-event-file-based.xml");
 	}
 	
 	public static void runEventsBasedPSSControler(String configPath){
