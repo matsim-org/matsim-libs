599a47e5c792d17add0716585ef58fc4f66def78
fuerbas <fuerbas@users.sf.net>
2011-01-06 13:59:27 +0000

    removed unused variables
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@13936 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/andreas/src/test/java/playground/andreas/bvgAna/level1/AgentId2DepartureDelayAtStopMapDataTest.java
+++ playgrounds/andreas/src/test/java/playground/andreas/bvgAna/level1/AgentId2DepartureDelayAtStopMapDataTest.java
@@ -17,11 +17,7 @@ import org.matsim.core.events.PersonEntersVehicleEvent;
 
 public class AgentId2DepartureDelayAtStopMapDataTest {
 	
-	
-	private ArrayList<Double> agentDepartsPTInteraction = new ArrayList<Double>();
-	private ArrayList<Double> agentEntersVehicle = new ArrayList<Double>();
-	
-	
+		
 
 	@Test
 	public void testAgentId2DepartureDelayAtStopMapData() {
@@ -33,21 +29,13 @@ public class AgentId2DepartureDelayAtStopMapDataTest {
             idSet.add(ida[ii]);
         }
         
-//        assign Ids to routes, vehicles and agents to be used in Test, same as in AgentIdDepartureDelayAtStopMapTest
+//        assign Ids to routes, vehicles and agents to be used in Test
         
         Id routeId1 = ida[2];
-        Id routeId2 = ida[3];
-        Id routeId3 = ida[7]; 
         Id vehId1 = ida[4];
-        Id vehId2 = ida[1];
-        Id vehId3 = ida[6];
         Id persId1 = ida[0];
-        Id facilId1 = ida[9];
-        Id facilId2 = ida[11];
-        Id linkId1 = ida[14];
-        Id linkId2 = ida[12];
         Id linkId3 = ida[13];
-        Id linkId4 = ida[10];
+
         
         EventsFactoryImpl ef = new EventsFactoryImpl();
         AgentId2DepartureDelayAtStopMapData data = new AgentId2DepartureDelayAtStopMapData(persId1);
@@ -55,14 +43,10 @@ public class AgentId2DepartureDelayAtStopMapDataTest {
 //        create Events
         
         AgentDepartureEvent event3 = ef.createAgentDepartureEvent(2.9*3600, persId1, linkId3, TransportMode.pt);
-//		AgentDepartureEvent event4 = ef.createAgentDepartureEvent(2.1*3600, persId1, linkId1, TransportMode.pt);     
         PersonEntersVehicleEvent event1 = ef.createPersonEntersVehicleEvent(2.9*3600, persId1, vehId1, routeId1);
-//        PersonEntersVehicleEvent event2 = ef.createPersonEntersVehicleEvent(2.1*3600, persId1, vehId2, routeId2);
         
         data.addAgentDepartureEvent(event3);
-//        data.addAgentDepartureEvent(event4);
         data.addPersonEntersVehicleEvent(event1);
-//        data.addPersonEntersVehicleEvent(event2);
         
 //        run tests for 1 event each, can be expanded later if needed
         
