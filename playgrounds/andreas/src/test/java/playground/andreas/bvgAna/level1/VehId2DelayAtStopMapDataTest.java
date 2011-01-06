f97a103bc653cb1f5b413fdb7e6414a0dd404a5f
fuerbas <fuerbas@users.sf.net>
2011-01-06 14:01:50 +0000

    not finished yet
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@13938 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/andreas/src/test/java/playground/andreas/bvgAna/level1/VehId2DelayAtStopMapDataTest.java
@@ -0,0 +1,55 @@
+package playground.andreas.bvgAna.level1;
+
+import static org.junit.Assert.*;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.matsim.api.core.v01.Id;
+import org.matsim.core.basic.v01.IdImpl;
+import org.matsim.core.events.EventsFactoryImpl;
+import org.matsim.core.events.TransitDriverStartsEvent;
+import org.matsim.core.events.VehicleDepartsAtFacilityEvent;
+import org.matsim.core.events.VehicleDepartsAtFacilityEventImpl;
+
+public class VehId2DelayAtStopMapDataTest {
+
+	@Test
+	public void testVehId2DelayAtStopMapData() {
+		
+        Id[] ida= new Id[15];
+    	Set<Id> idSet = new TreeSet<Id>();
+        for (int ii=0; ii<15; ii++){
+        	ida[ii] = new IdImpl(ii); 
+            idSet.add(ida[ii]);
+        }
+        
+//        assign Ids to routes, vehicles and agents to be used in Test
+        
+
+        Id vehId1 = ida[4];
+        Id driverId1 = ida[0];
+        Id facilId1 = ida[9];
+        Id transitLineId1 = ida[14];
+        Id transitRouteId1 = ida[12];
+        Id departureId1 = ida[13];
+		
+        EventsFactoryImpl ef = new EventsFactoryImpl();
+        
+        TransitDriverStartsEvent event1 = new TransitDriverStartsEvent(2.4*3600, driverId1, vehId1, transitLineId1, transitRouteId1, departureId1);
+        VehicleDepartsAtFacilityEvent event2 = new VehicleDepartsAtFacilityEventImpl(2.3*3600, vehId1, facilId1, 1.);
+        
+        VehId2DelayAtStopMapData mapData = new VehId2DelayAtStopMapData(event1);
+        
+        mapData.addVehicleDepartsAtFacilityEvent(event2);
+        
+//        now that I completed the test setup I am wondering, what should I test here?
+
+		
+	}
+
+
+}
