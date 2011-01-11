16f56d6940ca984a787d79c38ad19c921b8dd55c
rashid_waraich <rashid_waraich@users.sf.net>
2011-01-11 12:56:34 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@13977 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/wrashid/src/main/java/playground/wrashid/PSF2/tools/chargingLog/EnergyConsumptionPerLink.java
@@ -0,0 +1,57 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * EnergyConsumptionPerLink.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2011 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.wrashid.PSF2.tools.chargingLog;
+
+import org.omg.CORBA.DoubleHolder;
+
+import playground.wrashid.lib.DebugLib;
+import playground.wrashid.lib.GeneralLib;
+import playground.wrashid.lib.obj.DoubleValueHashMap;
+import playground.wrashid.lib.obj.StringMatrix;
+
+public class EnergyConsumptionPerLink {
+
+	public static void main(String[] args) {
+		String chargingLogFileNamePath="A:/data/PSS/input/chargingLog.txt";
+		StringMatrix matrix=GeneralLib.readStringMatrix(chargingLogFileNamePath);
+		DoubleValueHashMap<String> energyConsumptionPerLink=new DoubleValueHashMap<String>();
+		
+		
+		// starting with index 1 (ignoring first line)
+		for (int i=1;i<matrix.getNumberOfRows();i++){
+			String linkId=matrix.getString(i, 0);
+			Double startSOC=matrix.getDouble(i, 4);
+			Double endSOC=matrix.getDouble(i, 5);
+			double energyCharged=endSOC-startSOC;
+			
+			DebugLib.assertTrue(energyCharged>0, "startSOC:" + startSOC + " - endSOC:" + endSOC);
+			
+			energyConsumptionPerLink.incrementBy(linkId, energyCharged);			
+		}
+		
+		System.out.println("linkId\tenergyConsumption");
+		energyConsumptionPerLink.printToConsole();
+		
+		System.out.println("link with highest energyConsumption: " + energyConsumptionPerLink.getKeyForMaxValue());
+		
+	}
+	
+}
