ab6aefb7197c487e616f6d98530a8d5e520131e2
mrieser <rieser@gmail.com>
2011-01-11 22:32:51 +0000

    add GPL header
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@13985 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/vehicles/VehicleImpl.java
+++ matsim/src/main/java/org/matsim/vehicles/VehicleImpl.java
@@ -1,3 +1,22 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2011 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
 package org.matsim.vehicles;
 
 import org.matsim.api.core.v01.Id;
@@ -13,13 +32,15 @@ public class VehicleImpl implements Vehicle {
 		this.type = type;
 	}
 
+	@Override
 	public Id getId() {
 		return id;
 	}
 
+	@Override
 	public VehicleType getType() {
 		return this.type;
 	}
 
-	
+
 }
