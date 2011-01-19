3dcb483c56db14a6e9e31d45568ac0ede51147bd
fuerbas <fuerbas@users.sf.net>
2011-01-19 13:59:56 +0000

    not finished yet
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@14055 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/andreas/src/test/java/playground/andreas/bvgAna/level1/StopId2RouteId2DelayAtStopMapTest.java
@@ -0,0 +1,67 @@
+package playground.andreas.bvgAna.level1;
+
+import static org.junit.Assert.*;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.junit.Test;
+import org.matsim.api.core.v01.Id;
+import org.matsim.core.basic.v01.IdImpl;
+import org.matsim.core.events.EventsFactoryImpl;
+import org.matsim.core.events.TransitDriverStartsEvent;
+import org.matsim.core.events.VehicleDepartsAtFacilityEvent;
+
+public class StopId2RouteId2DelayAtStopMapTest {
+
+	@Test
+	public void testStopId2RouteId2DelayAtStopMap() {
+	    
+		Id[] ida= new Id[15];
+		Set<Id> idSet = new TreeSet<Id>();
+	    for (int ii=0; ii<15; ii++){
+	    	ida[ii] = new IdImpl(ii); 
+	        idSet.add(ida[ii]);
+	    }
+	    
+//	    assign Ids to routes, vehicles and agents to be used in Test
+		
+		Id vehId1 = ida[1];
+	    Id vehId2 = ida[2];
+	    Id driverId1 = ida[4];
+	    Id driverId2 = ida[5];
+	    Id departureId1 = ida[6];
+	    Id departureId2 = ida[7];
+	    Id transitRouteId1 = ida[14];
+	    Id transitRouteId2 = ida[2];
+	    Id transitLineId1 = ida[4];
+	    Id transitLineId2 = ida[6];
+	    Id facilityId1 = ida[8];
+	    Id facilityId2 = ida[9];
+	    
+//	    create events
+	    
+	    EventsFactoryImpl ef = new EventsFactoryImpl();
+	    
+	    VehicleDepartsAtFacilityEvent event1 = ef.createVehicleDepartsAtFacilityEvent(2., vehId1, facilityId1, 0.5);
+	    VehicleDepartsAtFacilityEvent event2 = ef.createVehicleDepartsAtFacilityEvent(2.7, vehId2, facilityId2, 0.7);
+	    TransitDriverStartsEvent event3 = ef.createTransitDriverStartsEvent(2.8, driverId1, vehId1, transitLineId1, transitRouteId1, departureId1);
+	    TransitDriverStartsEvent event4 = ef.createTransitDriverStartsEvent(2.3, driverId2, vehId2, transitLineId2, transitRouteId2, departureId2);
+	    
+	    StopId2RouteId2DelayAtStopMap test = new StopId2RouteId2DelayAtStopMap();
+	    
+	    test.handleEvent(event3);
+	    test.handleEvent(event4);
+	    test.handleEvent(event1);
+	    test.handleEvent(event2);
+	    
+	    System.out.println(test.getStopId2RouteId2DelayAtStopMap().toString());
+	    
+	    // to be completed
+	    
+	    
+	}
+
+
+
+}
