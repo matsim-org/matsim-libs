491fa25a5dde59c4b70acd761f8d1654e5964531
fuerbas <fuerbas@users.sf.net>
2011-01-30 16:36:47 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@14117 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/fuerbas/src/main/java/air/SfAirNetworkBuilder.java
+++ playgrounds/fuerbas/src/main/java/air/SfAirNetworkBuilder.java
@@ -4,9 +4,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Link;
 import org.matsim.core.basic.v01.IdImpl;
 import org.matsim.core.network.NetworkImpl;
 import org.matsim.core.network.NetworkWriter;
@@ -22,8 +25,11 @@ public class SfAirNetworkBuilder {
 		// TODO Auto-generated method stub
 		
 		String output = "/home/soeren/workspace/testnetzwerk";
+		Set<String> allowedModes = new HashSet<String>();
+		allowedModes.add("pt");
 
 		NetworkImpl network = NetworkImpl.createNetwork();
+		network.setCapacityPeriod(60.0);		//60 seconds capacity period --> 1 take-off per minute
 		
 		BufferedReader brAirports = new BufferedReader(new FileReader(new File("/home/soeren/workspace/OsmTest.txt")));
 		BufferedReader brRoutes = new BufferedReader(new FileReader(new File("/home/soeren/workspace/cityPairs.txt")));
@@ -48,18 +54,21 @@ public class SfAirNetworkBuilder {
 			new MATSimAirport(new IdImpl(airportCode), airportCoord).createRunways(network);			
 		}
 		
-		int ii = 0;
 		
 		while (brRoutes.ready()) {
 			String oneLine = brRoutes.readLine();
-			String origin = oneLine.substring(0, 3);
-			String destination = oneLine.substring(3, 6);
-			System.out.println(oneLine);
+			String[] lineEntries = oneLine.split("\t");
+			double length = Double.parseDouble(lineEntries[1]);
+			String origin = lineEntries[0].substring(0, 3);
+			String destination = lineEntries[0].substring(3, 6);
 			Id originRunway = new IdImpl(origin+"runwayOutbound");
 			Id destinationRunway = new IdImpl(destination+"runwayInbound");
-			network.addLink(network.getFactory().createLink(new IdImpl(origin+destination), originRunway, destinationRunway));
-			ii++;
-
+			Link originToDestination = network.getFactory().createLink(new IdImpl(origin+destination), originRunway, destinationRunway);
+			originToDestination.setAllowedModes(allowedModes);
+			originToDestination.setCapacity(1.0);
+			originToDestination.setFreespeed(750.0/3.6);
+			originToDestination.setLength(length);
+			network.addLink(originToDestination);
 		}
 		
 
