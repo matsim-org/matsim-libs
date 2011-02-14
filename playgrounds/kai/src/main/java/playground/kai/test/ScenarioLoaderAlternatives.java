6e9e03df18bcaef2f8a0d3887da51b537d089391
vsp_nagel <2008@kainagel.org>
2011-02-14 16:41:21 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@14260 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/kai/src/main/java/playground/kai/test/ScenarioLoaderAlternatives.java
+++ playgrounds/kai/src/main/java/playground/kai/test/ScenarioLoaderAlternatives.java
@@ -1,40 +1,89 @@
 package playground.kai.test;
 
+import java.io.IOException;
+
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.ScenarioImpl;
 import org.matsim.core.api.experimental.ScenarioLoader;
-import org.matsim.core.api.experimental.ScenarioLoaderFactoryImpl;
+import org.matsim.core.config.Config;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.scenario.ScenarioLoaderImpl;
+import org.matsim.core.utils.misc.ConfigUtils;
 
 
 public class ScenarioLoaderAlternatives {
 
-	public static void main ( String[] args ) {
-
-		String configFileName = args[0] ;
-
-		{
-
-			// create scenario loader from configfile:
-			ScenarioLoader scl = (new ScenarioLoaderFactoryImpl()).createScenarioLoader( configFileName ) ;
-
-			// load scenario:
-			Scenario sc = scl.loadScenario() ;
-
-		}
-
-		/////
-
-		{
-
-//			// create my own scenario from nothing:
-////			Scenario scenario = new MyScenarioImpl() ;
-//			Scenario scenario = (new ScenarioFactoryImpl()).createScenario() ;
+	public static void main ( String[] args ) throws IOException {
+		
+		String cFilename = "config.xml" ;
+		
+//		// option 1
+//		{		
+//			Controler ctrl = new Controler( cFilename ) ; // convenience implementation of 3
+//		}
+//		
+//		// option 2
+//		{
+//			ScenarioLoader scl = new ScenarioLoaderImpl( cFilename ) ;
 //
-//			// load scenario based on my own Scenario and based on configFileName:
-//			ScenarioLoaderI scl = (new ScenarioLoaderFactoryImpl()).createScenarioLoader( configFileName, scenario ) ;
-//			
-//			scl.loadScenario() ;
+//			Config config = scl.getScenario().getConfig() ; // unschoen, 3 ist besser
+//			config.scenario().isUseVehicles();
+//
+//			Scenario sc = scl.loadScenario() ;
+//		}
+		
+		// option 3
+		{
+			Config config = ConfigUtils.loadConfig( cFilename ) ;
+			
+			config.scenario().isUseVehicles();
+			
+			ScenarioLoader scl = new ScenarioLoaderImpl( config ) ;
+			
+			Scenario sc = scl.loadScenario() ;
+			
+			Controler ctrl = new Controler( (ScenarioImpl) sc ) ; // besser mit "Scenario"
 			
 		}
+		
+//		// option 4
+//		{
+//			Config config = ConfigUtils.loadConfig( cFilename );
+//			config.scenario().isUseVehicles();
+//			
+//			Controler ctrl = new Controler( config ) ; // convenience implementation of 3
+//		}			
+		
+		
+		
+		
+
+//		String configFileName = args[0] ;
+//
+//		{
+//
+//			// create scenario loader from configfile:
+//			ScenarioLoader scl = (new ScenarioLoaderFactoryImpl()).createScenarioLoader( configFileName ) ;
+//
+//			// load scenario:
+//			Scenario sc = scl.loadScenario() ;
+//
+//		}
+//
+//		/////
+//
+//		{
+//
+////			// create my own scenario from nothing:
+//////			Scenario scenario = new MyScenarioImpl() ;
+////			Scenario scenario = (new ScenarioFactoryImpl()).createScenario() ;
+////
+////			// load scenario based on my own Scenario and based on configFileName:
+////			ScenarioLoaderI scl = (new ScenarioLoaderFactoryImpl()).createScenarioLoader( configFileName, scenario ) ;
+////			
+////			scl.loadScenario() ;
+//			
+//		}
 
 
 	}
