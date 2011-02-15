98536885d37758870d0553c888824d8a7f6363d7
andreas77 <andreashorni@hotmail.com>
2011-02-15 14:14:02 +0000

    debugging finished
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@14275 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/anhorni/src/main/java/playground/anhorni/kti/CountsAnalyser.java
+++ playgrounds/anhorni/src/main/java/playground/anhorni/kti/CountsAnalyser.java
@@ -57,122 +57,67 @@ import playground.dgrether.utils.DoubleArrayTableWriter;
 public class CountsAnalyser {
 
 	private String linkstatsfile;
-	/**
-	 * name of the counts module in config
-	 */
 	public static final String COUNTS = "counts";
-
-	/**
-	 * name of the output format parameter in config
-	 */
 	public static final String OUTPUTFORMAT = "outputformat";
-
-	/**
-	 * name of the output file parameter in config
-	 */
-	public static final String OUTFILE = "outputCountsFile";
-
-	/**
-	 * name of the timefilter parameter in config
-	 */
-	public static final String TIMEFILTER = "timeFilter";
-
-	/**
-	 * name of the distancefilter parameter in config
-	 */
 	public static final String DISTANCEFILTER = "distanceFilter";
-
-	/**
-	 * name of the distancefilterCenterNode parameter in config
-	 */
 	public static final String DISTANCEFITLERCENTERNODE = "distanceFilterCenterNode";
-
 	private Scenario scenario;
-	/**
-	 * the network
-	 */
 	private NetworkImpl network;
-
-	/**
-	 * the name(path) to the output file
-	 */
-	private String outputFile;
-
-	/**
-	 * the output format
-	 */
+	private String outputPath = "../../matsim/output/";
 	private String outputFormat;
-
-	/**
-	 * the distance filter in m
-	 */
 	private Double distanceFilter;
-
-	/**
-	 * the id of the node used as center for the distance filter
-	 */
 	private String distanceFilterCenterNode;
-	/**
-	 * the CalcLinkStats read for the analysis
-	 */
 	private CalcLinkStats linkStats;
-
 	private String coordSystem;
-
+	
 	final private Counts counts =  new Counts();
 
-	/**
-	 *
-	 * @param config
-	 */
 	public CountsAnalyser(final String linkstatsfile) {
 
 		this.linkstatsfile = linkstatsfile;
 
 		try {
 			this.readConfig("../../matsim/input/config.xml");
-			this.writeCountsComparisonList(this.outputFile, this.outputFormat);
+			this.writeCountsComparisonList(this.outputPath, this.outputFormat);
 		} catch (Exception e) {
 			e.printStackTrace();
-			throw new RuntimeException(
-					"Something wrong in config file, execution aborted!");
+			throw new RuntimeException("Something wrong in config file, execution aborted!");
 		}
 	}
 
-	/**
-	 * Reads the parameters from the config file
-	 *
-	 * @param configFile
-	 * @throws Exception
-	 */
 	private void readConfig(final String configFile) throws Exception {
 		this.scenario = new ScenarioLoaderImpl(configFile).getScenario();
 		Config config = this.scenario.getConfig();
 		System.out.println("  reading counts xml file... ");
-		MatsimCountsReader counts_parser = new MatsimCountsReader(counts);
+		MatsimCountsReader counts_parser = new MatsimCountsReader(this.counts);
 		counts_parser.readFile(config.counts().getCountsFileName());
 		System.out.println("  reading counts done.");
-		System.out.println("  reading network...");
+				
+		System.out.println("  reading network: " + this.scenario.getConfig().network().getInputFile() + "...");
 		this.network = loadNetwork();
 		System.out.println("  done.");
-		// reading config parameters
-		System.out.println("Reading parameters...");
+		
+		// -------------------------------------
+		System.out.println("Reading config parameters...");
 		String linksAttributeFilename = this.linkstatsfile;
 		System.out.println("  Linkattribute File: " + linksAttributeFilename);
+				
 		this.outputFormat = config.getParam(COUNTS, OUTPUTFORMAT);
-		this.outputFile = config.getParam(COUNTS, OUTFILE);
-		System.out.println("  Output file: " + this.outputFile);
+		System.out.println("  Output path: " + this.outputPath);
+		
 		this.distanceFilterCenterNode = config.counts().getDistanceFilterCenterNode();
 		System.out.println("  Distance filter center node: " + this.distanceFilterCenterNode);
 		this.distanceFilter = config.counts().getDistanceFilter();
 		System.out.println("  Distance filter: " + this.distanceFilter);
+		
 		System.out.println("  Scale Factor: " + config.counts().getCountsScaleFactor());
+		
 		this.coordSystem = config.global().getCoordinateSystem();
 		System.out.println("  Coordinate System: " + this.coordSystem);
-		System.out.println("  reading LinkAttributes from: " + linksAttributeFilename);
-		this.linkStats = new CalcLinkStats(this.network);
+		// -------------------------------------
+		System.out.println("  reading LinkAttributes from: " + linksAttributeFilename);	
+		this.linkStats = new CalcLinkStats(this.network);		
 		this.linkStats.readFile(linksAttributeFilename);
-
 		System.out.println("  done.");
 	}
 
@@ -202,17 +147,17 @@ public class CountsAnalyser {
 	 * @param format
 	 *          the format kml or txt
 	 */
-	private void writeCountsComparisonList(final String filename, final String format) {
+	private void writeCountsComparisonList(final String path, final String format) {
 		List<CountSimComparison> countsComparisonList = createCountsComparisonList(this.linkStats);
 		if ((format.compareToIgnoreCase("kml") == 0) || (format.compareToIgnoreCase("all") == 0)) {
 			CountSimComparisonKMLWriter kmlWriter = new CountSimComparisonKMLWriter(
 					countsComparisonList, this.network, TransformationFactory.getCoordinateTransformation(this.coordSystem, TransformationFactory.WGS84));
-			kmlWriter.writeFile(filename);
+			kmlWriter.writeFile(path + "counts");
 		}
 		
 		if (format.compareToIgnoreCase("txt") == 0 || (format.compareToIgnoreCase("all") == 0)) {
 			CountSimComparisonTableWriter writer = new CountSimComparisonTableWriter(countsComparisonList, Locale.US);
-			writer.writeFile(filename);
+			writer.writeFile(path + "counts.txt");
 			
 			ComparisonErrorStatsCalculator errorStats = new ComparisonErrorStatsCalculator(countsComparisonList);
 
@@ -223,33 +168,23 @@ public class CountsAnalyser {
 			DoubleArrayTableWriter tableWriter = new DoubleArrayTableWriter();
 			tableWriter.addColumn(hours);
 			tableWriter.addColumn(errorStats.getMeanRelError());
-			tableWriter.writeFile(filename.replace(".", "_") + "errortable.txt");
+			tableWriter.writeFile(path + "errortable.txt");
 		}
-		CountsGraphWriter writer = new CountsGraphWriter("output", countsComparisonList, 0 , true , true);
-		writer.setGraphsCreator(new CountsSimRealPerHourGraphCreator("sim and real volumes"));
-		writer.setGraphsCreator(new CountsErrorGraphCreator("errors"));
-		writer.setGraphsCreator(new CountsLoadCurveGraphCreator("link volumes"));
-		writer.setGraphsCreator(new CountsSimReal24GraphCreator("average working day sim and count volumes"));
-		writer.createGraphs();
-		writer.createGraphs();		
+		if (format.compareToIgnoreCase("html") == 0 || (format.compareToIgnoreCase("all") == 0)) {
+			CountsGraphWriter writer = new CountsGraphWriter(path, countsComparisonList, 0 , true , true);
+			writer.setGraphsCreator(new CountsSimRealPerHourGraphCreator("sim and real volumes"));
+			writer.setGraphsCreator(new CountsErrorGraphCreator("errors"));
+			writer.setGraphsCreator(new CountsLoadCurveGraphCreator("link volumes"));
+			writer.setGraphsCreator(new CountsSimReal24GraphCreator("average working day sim and count volumes"));
+			writer.createGraphs();
+		}
 	}
 
-	/**
-	 * load the network
-	 *
-	 * @return the network layer
-	 */
 	protected NetworkImpl loadNetwork() {
-		// - read network: which buildertype??
-		printNote("", "  creating network layer... ");
-		NetworkImpl network = NetworkImpl.createNetwork();
-		printNote("", "  done");
-
 		printNote("", "  reading network xml file... ");
-		new MatsimNetworkReader(this.scenario).readFile(this.scenario.getConfig().network().getInputFile());
+		new MatsimNetworkReader(scenario).readFile(this.scenario.getConfig().network().getInputFile());
 		printNote("", "  done");
-
-		return network;
+		return (NetworkImpl) scenario.getNetwork();
 	}
 
 	/**
@@ -282,14 +217,16 @@ public class CountsAnalyser {
 	 *
 	 */
 	private static void printHelp() {
-		System.out.println("This tool needs one config argument: The path to the linkstats.txt file!");
+		System.out.println("This tool needs one config argument: The path to the linkstats.txt file");
 		System.out.println("A config file must be provided under the following path: input/config.xml");
 		System.out.println("The config file must contain the following attributes: \n\n");
 
-		System.out.println("  - The path to the file to which the output is written (mandatory)");
-		System.out.println("  - The time filter (mandatory) 0 for 0 to 1 am, 1 for 1 to 2 am...");
+		System.out.println("  - The counts scale factor");
 		System.out.println("  - The distance filter (optional) the distance in km to filter the counts around a node that must be given in the subsequent argument.");
-		System.out.println("  - The node id for the center of the distance filter (optinal, however mandatory if distance filter is set)");
+		System.out.println("  - The node id for the center of the distance filter (optional, however mandatory if distance filter is set)");
+		
+		System.out.println("  - The path to the network file (config group 'network')");
+		System.out.println("  - The coordinate sytem used (config group 'global')");
 	}
 
 	/**
@@ -300,12 +237,13 @@ public class CountsAnalyser {
 	public static void main(String[] args) {
 
 		CountsAnalyser ca = null;
-		if (args.length == 0) {
+		if (args.length != 1) {
 			printHelp();
 		}
 		else {
 			ca = new CountsAnalyser(args[0]);
-			System.out.println("File written to " + ca.outputFile);
+			System.out.println("File written to " + ca.outputPath);
 		}
+		System.out.println("Creation finished ===========================================");
 	}
 }
