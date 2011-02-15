64095cd13ea4ae91a6b1462a111b22c55f11a2e9
vsp_nagel <2008@kainagel.org>
2011-02-15 16:07:33 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@14278 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/kai/src/main/java/playground/kai/test/Test3.java
+++ playgrounds/kai/src/main/java/playground/kai/test/Test3.java
@@ -1,8 +1,9 @@
 package playground.kai.test;
 
+
 public class Test3 {
-	
-	public static void main( String[] args ) {
+
+		public static void main( String[] args ) {
 		System.out.println( 1.04 - 0.42 ) ;
 	}
 
