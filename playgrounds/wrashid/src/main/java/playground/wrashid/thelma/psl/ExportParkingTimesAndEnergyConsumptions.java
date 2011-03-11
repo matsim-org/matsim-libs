55507e014d57e0d072c0c26a269ea551410f1235
rashid_waraich <rashid_waraich@users.sf.net>
2011-03-11 15:43:56 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@14599 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/wrashid/src/main/java/playground/wrashid/thelma/psl/ExportParkingTimesAndEnergyConsumptions.java
@@ -0,0 +1,86 @@
+package playground.wrashid.thelma.psl;
+
+import java.util.LinkedList;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.core.basic.v01.IdImpl;
+import org.matsim.core.events.EventsManagerImpl;
+import org.matsim.core.events.EventsReaderTXTv1;
+import org.matsim.core.network.NetworkImpl;
+
+import playground.wrashid.PSF2.pluggable.energyConsumption.EnergyConsumptionModel;
+import playground.wrashid.PSF2.pluggable.energyConsumption.EnergyConsumptionModelPSL;
+import playground.wrashid.PSF2.pluggable.energyConsumption.EnergyConsumptionPlugin;
+import playground.wrashid.PSF2.pluggable.parkingTimes.ParkingIntervalInfo;
+import playground.wrashid.PSF2.pluggable.parkingTimes.ParkingTimesPlugin;
+import playground.wrashid.PSF2.vehicle.vehicleFleet.PlugInHybridElectricVehicle;
+import playground.wrashid.PSF2.vehicle.vehicleFleet.Vehicle;
+import playground.wrashid.lib.GeneralLib;
+import playground.wrashid.lib.obj.LinkedListValueHashMap;
+
+/**
+ * The goal is to output the parking times and energy consumptions based on a events file.
+ * 
+ * The output format is:
+ * agentId, startParking, endParking, linkId (where parked), actTypeOfActivity, energyConsumptionsInJoules (for previous trip).
+ * @author wrashid
+ *
+ */
+
+public class ExportParkingTimesAndEnergyConsumptions {
+
+	public static void main(String[] args) {
+		String eventsFile="C:/data/workspace/matsim/output/berlin/ITERS/it.17/17.events.txt.gz";
+		String networkFile="C:/data/workspace/playgrounds/wrashid/test/scenarios/berlin/network.xml";
+		EventsManagerImpl events = new EventsManagerImpl();
+
+		
+		
+		ParkingTimesPlugin parkingTimesPlugin = new ParkingTimesPlugin();
+		
+		//addActivityFilter(parkingTimesPlugin);
+		
+		events.addHandler(parkingTimesPlugin);
+		
+		EnergyConsumptionPlugin energyConsumptionPlugin = getEnergyConsumptionPlugin(networkFile);
+		
+		events.addHandler(energyConsumptionPlugin);
+		
+		EventsReaderTXTv1 reader = new EventsReaderTXTv1(events);
+		
+		reader.readFile(eventsFile);
+		
+		parkingTimesPlugin.closeLastAndFirstParkingIntervals();
+		
+		printParkingTimesAndEnergyConsumptionTable(parkingTimesPlugin, energyConsumptionPlugin);
+	}
+
+	private static void addActivityFilter(ParkingTimesPlugin parkingTimesPlugin) {
+		LinkedList<String> actTypesFilter=new LinkedList<String>();
+		actTypesFilter.add("w");
+		parkingTimesPlugin.setActTypesFilter(actTypesFilter);
+	}
+
+	private static void printParkingTimesAndEnergyConsumptionTable(ParkingTimesPlugin parkingTimesPlugin,
+			EnergyConsumptionPlugin energyConsumptionPlugin) {
+		System.out.println("agentId\tstartParking\tendParking\tlinkId\tactType\tenergyConsumptionsInJoules");
+		for (Id personId: parkingTimesPlugin.getParkingTimeIntervals().getKeySet()){
+			LinkedList<ParkingIntervalInfo> parkingIntervals = parkingTimesPlugin.getParkingTimeIntervals().get(personId);
+			LinkedList<Double> energyConsumptionOfLegs = energyConsumptionPlugin.getEnergyConsumptionOfLegs().get(personId);
+			
+			for (int i=0;i<parkingIntervals.size();i++){
+				System.out.println(personId + "\t" + parkingIntervals.get(i).getArrivalTime() + "\t" + parkingIntervals.get(i).getDepartureTime() + "\t" + parkingIntervals.get(i).getLinkId() + "\t" + parkingIntervals.get(i).getActTypeOfFirstActDuringParking() + "\t"  + energyConsumptionOfLegs.get(i));
+			}
+		}
+	}
+
+	private static EnergyConsumptionPlugin getEnergyConsumptionPlugin(String networkFile) {
+		EnergyConsumptionModel energyConsumptionModel = new EnergyConsumptionModelPSL(140);
+		LinkedListValueHashMap<Id, Vehicle> vehicles=new LinkedListValueHashMap<Id, Vehicle>();
+		vehicles.put(Vehicle.getPlaceholderForUnmappedPersonIds(), new PlugInHybridElectricVehicle(new IdImpl(1)));
+		NetworkImpl network=GeneralLib.readNetwork(networkFile);
+		EnergyConsumptionPlugin energyConsumptionPlugin = new EnergyConsumptionPlugin(energyConsumptionModel,vehicles,network);
+		return energyConsumptionPlugin;
+	}
+	
+}
