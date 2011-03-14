f84592c33e3ac8f24e90c5852ff94c04e0c5dd7a
mrieser <rieser@gmail.com>
2011-03-14 13:30:12 +0000

    explicit NPE when config==null in createScenario() with helpful text in exception.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@14620 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/test/java/org/matsim/core/scenario/ScenarioUtilsTest.java
@@ -0,0 +1,45 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2011 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.core.scenario;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+import org.matsim.api.core.v01.Scenario;
+
+/**
+ * @author mrieser
+ */
+public class ScenarioUtilsTest {
+
+	private final static Logger log = Logger.getLogger(ScenarioUtilsTest.class);
+
+	@Test
+	public void testCreateScenario_nullConfig() {
+		try {
+			Scenario s = ScenarioUtils.createScenario(null);
+			Assert.fail("expected NPE, but got none." + s.toString());
+		}
+		catch (NullPointerException e) {
+			log.info("Catched expected NPE.", e);
+			Assert.assertTrue("Message in NPE should not be empty.", e.getMessage().length() > 0);
+		}
+	}
+}
