4e39a46df44aa2e23c788685760f2602aacd65ac
fuerbas <fuerbas@users.sf.net>
2011-03-15 07:52:37 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@14631 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/andreas/src/test/java/playground/andreas/bvgAna/level1/AgentId2PtTripTravelTimeMapTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package playground.andreas.bvgAna.level1;
+
+import org.junit.Test;
+
+/**
+ * @author fuerbas
+ *
+ */
+public class AgentId2PtTripTravelTimeMapTest {
+
+	/**
+	 * Test method for {@link playground.andreas.bvgAna.level1.AgentId2PtTripTravelTimeMap#AgentId2PtTripTravelTimeMap(java.util.Set)}.
+	 */
+	@Test
+	public void testAgentId2PtTripTravelTimeMap() {
+	
+		
+		
+		
+		
+		
+	}
+
+
+
+}
