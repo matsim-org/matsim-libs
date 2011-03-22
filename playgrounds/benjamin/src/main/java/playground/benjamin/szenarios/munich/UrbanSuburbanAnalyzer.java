6091a7d14cdfa4251e7d7830d1722d6eabeaf25b
benkick <benkick@users.sf.net>
2011-03-22 11:59:45 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@14735 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/benjamin/src/main/java/playground/benjamin/szenarios/munich/UrbanSuburbanAnalyzer.java
+++ playgrounds/benjamin/src/main/java/playground/benjamin/szenarios/munich/UrbanSuburbanAnalyzer.java
@@ -17,7 +17,7 @@
  *   See also COPYING, LICENSE and WARRANTY file                           *
  *                                                                         *
  * *********************************************************************** */
-package playground.benjamin.szenarios.munich.geo;
+package playground.benjamin.szenarios.munich;
 
 import java.io.IOException;
 import java.util.ArrayList;
