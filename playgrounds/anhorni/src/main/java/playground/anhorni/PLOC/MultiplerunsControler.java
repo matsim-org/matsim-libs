cbd4edbcc37f5c781bf7783462e5a5bdb018be10
andreas77 <andreashorni@hotmail.com>
2011-03-27 10:30:40 +0000

    10 facilities
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@14801 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/anhorni/src/main/java/playground/anhorni/PLOC/MultiplerunsControler.java
+++ playgrounds/anhorni/src/main/java/playground/anhorni/PLOC/MultiplerunsControler.java
@@ -40,7 +40,7 @@ public class MultiplerunsControler {
 	private ConfigReader configReader = new ConfigReader();
 	private ObjectAttributes personAttributes = new ObjectAttributes();
 	
-	public static int shoppingFacilities[] = {1, 2, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+	public static int shoppingFacilities[] = {1, 2, 5, 6, 7, 8, 9, 10};
 	
     public static void main (final String[] args) { 
     	MultiplerunsControler runControler = new MultiplerunsControler();
