14d733bd088b80d9ee785fc97f69600ffad10a49
benkick <benkick@users.sf.net>
2011-03-29 16:18:12 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@14822 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/benjamin/src/main/java/playground/benjamin/szenarios/munich/LegModeDistanceDistribution.java
+++ playgrounds/benjamin/src/main/java/playground/benjamin/szenarios/munich/LegModeDistanceDistribution.java
@@ -19,20 +19,19 @@
  * *********************************************************************** */
 package playground.benjamin.szenarios.munich;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.SortedMap;
 import java.util.SortedSet;
 import java.util.TreeMap;
 import java.util.TreeSet;
-import java.util.Map.Entry;
 
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Scenario;
-import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.PlanElement;
@@ -55,18 +54,20 @@ public class LegModeDistanceDistribution {
 	private static final Logger logger = Logger.getLogger(LegModeDistanceDistribution.class);
 
 	// INPUT
-	private static String runDirectory = "../../detailedEval/testRuns/output/1pct/v0-default/run14/";
+	private static String runDirectory = "../../detailedEval/testRuns/output/1pct/v0-default/run17/";
 	private static String netFile = runDirectory + "output_network.xml.gz";
-//	private static String plansFile = runDirectory + "output_plans.xml.gz";
-	private static String plansFile = runDirectory + "ITERS/it.0/0.plans.xml.gz";
+	private static String initialPlansFile = runDirectory + "ITERS/it.0/0.plans.xml.gz";
+	private static String finalPlansFile = runDirectory + "output_plans.xml.gz";
 
-	private final Scenario scenario;
+	private final Scenario initialScenario;
+	private final Scenario finalScenario;
 	private List<Integer> distanceClasses;
 	private SortedSet<String> usedModes;
 
 	public LegModeDistanceDistribution(){
 		Config config = ConfigUtils.createConfig();
-		this.scenario = (ScenarioImpl) ScenarioUtils.createScenario(config);
+		this.initialScenario = (ScenarioImpl) ScenarioUtils.createScenario(config);
+		this.finalScenario = (ScenarioImpl) ScenarioUtils.createScenario(config);
 		this.distanceClasses = new ArrayList<Integer>();
 		this.usedModes = new TreeSet<String>();
 	}
@@ -77,45 +78,52 @@ public class LegModeDistanceDistribution {
 	}
 
 	private void run(String[] args) {
-		loadSceanrio();
+		loadSceanrio(this.initialScenario, netFile, initialPlansFile);
+		loadSceanrio(this.finalScenario, netFile, finalPlansFile);
 		setDistanceClasses();
 		getUsedModes();
-		Population population = scenario.getPopulation();
-		Population miDPop = getMiDPopulation(population);
-		Map<String, Map<Integer, Integer>> mode2DistanceClassNoOfLegs = calculateMode2DistanceClassNoOfLegs(miDPop);
+		Population initialPop = this.initialScenario.getPopulation();
+		Population finalPop = this.finalScenario.getPopulation();
+		Population initialMiDPop = getMiDPopulation(initialPop);
+		Population finalMiDPop = getMiDPopulation(finalPop);
+		SortedMap<String, Map<Integer, Integer>> initialMode2DistanceClassNoOfLegs = calculateMode2DistanceClassNoOfLegs(initialMiDPop);
+		SortedMap<String, Map<Integer, Integer>> finalMode2DistanceClassNoOfLegs = calculateMode2DistanceClassNoOfLegs(finalMiDPop);
 
-		System.out.println(mode2DistanceClassNoOfLegs);
-		writeInformation(mode2DistanceClassNoOfLegs);
+		logger.info("The initial LegModeDistanceDistribution is :" + initialMode2DistanceClassNoOfLegs);
+		logger.info("The final LegModeDistanceDistribution is :" + finalMode2DistanceClassNoOfLegs);
+		writeInformation(initialMode2DistanceClassNoOfLegs, "legModeDistanceDistributionInitial");
+		writeInformation(finalMode2DistanceClassNoOfLegs, "legModeDistanceDistributionFinal");
 	}
 
-	private void writeInformation(Map<String, Map<Integer, Integer>> mode2DistanceClassNoOfLegs) {
-
-		for(String mode : this.usedModes){
-			System.out.print("\t" + mode);
-		}
-		System.out.print("\n");
-		String outLine = null;
-		for(int i = 0; i < this.distanceClasses.size() - 1 ; i++){
-			String bikeLegs = mode2DistanceClassNoOfLegs.get("bike").get(this.distanceClasses.get(i + 1)).toString();
-			String carLegs = mode2DistanceClassNoOfLegs.get("car").get(this.distanceClasses.get(i + 1)).toString();
-			String ptLegs = mode2DistanceClassNoOfLegs.get("pt").get(this.distanceClasses.get(i + 1)).toString();
-			String rideLegs = mode2DistanceClassNoOfLegs.get("ride").get(this.distanceClasses.get(i + 1)).toString();
-			String undefinedLegs = mode2DistanceClassNoOfLegs.get("undefined").get(this.distanceClasses.get(i + 1)).toString();
-			String walkLegs = mode2DistanceClassNoOfLegs.get("walk").get(this.distanceClasses.get(i + 1)).toString();
-
-			outLine = this.distanceClasses.get(i + 1).toString() + "\t" + 
-			bikeLegs + "\t" + 
-			carLegs + "\t" + 
-			ptLegs + "\t" + 
-			rideLegs + "\t" + 
-			undefinedLegs + "\t" +
-			walkLegs;
-			System.out.println(outLine);
+	private void writeInformation(Map<String, Map<Integer, Integer>> mode2DistanceClassNoOfLegs, String fileName) {
+		String outFile = runDirectory + fileName + ".txt";
+		try{
+			FileWriter fstream = new FileWriter(outFile);			
+			BufferedWriter out = new BufferedWriter(fstream);
+			for(String mode : this.usedModes){
+				out.write("\t" + mode);
+			}
+			out.write("\n");
+			for(int i = 0; i < this.distanceClasses.size() - 1 ; i++){
+				Integer middleOfDistanceClass = ((this.distanceClasses.get(i) + this.distanceClasses.get(i + 1)) / 2);
+				out.write(middleOfDistanceClass + "\t");
+				for(String mode : this.usedModes){
+					String modeLegs = null;
+					modeLegs = mode2DistanceClassNoOfLegs.get(mode).get(this.distanceClasses.get(i + 1)).toString() + "\t";
+					out.write(modeLegs);
+				}
+				out.write("\n");
+			}
+			//Close the output stream
+			out.close();
+			logger.info("Finished writing output to " + outFile);
+		}catch (Exception e){
+			logger.error("Error: " + e.getMessage());
 		}
 	}
 
-	private Map<String, Map<Integer, Integer>> calculateMode2DistanceClassNoOfLegs(Population population) {
-		Map<String, Map<Integer, Integer>> mode2DistanceClassNoOfLegs = new HashMap<String, Map<Integer, Integer>>();
+	private SortedMap<String, Map<Integer, Integer>> calculateMode2DistanceClassNoOfLegs(Population population) {
+		SortedMap<String, Map<Integer, Integer>> mode2DistanceClassNoOfLegs = new TreeMap<String, Map<Integer, Integer>>();
 
 		for(String mode : this.usedModes){
 			SortedMap<Integer, Integer> distanceClass2NoOfLegs = new TreeMap<Integer, Integer>();
@@ -167,7 +175,7 @@ public class LegModeDistanceDistribution {
 	}
 
 	private void getUsedModes() {
-		Population population = scenario.getPopulation();
+		Population population = this.initialScenario.getPopulation();
 		for(Person person : population.getPersons().values()){
 			PlanImpl plan = (PlanImpl) person.getSelectedPlan();
 			List<PlanElement> planElements = plan.getPlanElements();
@@ -181,19 +189,19 @@ public class LegModeDistanceDistribution {
 				}
 			}
 		}
-		logger.info("The following transport modes are found in the population: " + this.usedModes);
+		logger.info("The following transport modes are found in the initial population: " + this.usedModes);
 	}
 
 	private void setDistanceClasses() {
 		this.distanceClasses.add(0);
-		for(int noOfClasses = 0; noOfClasses < 14; noOfClasses++){
+		for(int noOfClasses = 0; noOfClasses < 9; noOfClasses++){
 			int distanceClass = 100 * (int) Math.pow(2, noOfClasses);
 			this.distanceClasses.add(distanceClass);
 		}
 		logger.info("The following distance classes are defined: " + this.distanceClasses);
 	}
 
-	private void loadSceanrio() {
+	private void loadSceanrio(Scenario scenario, String netFile, String plansFile) {
 		Config config = scenario.getConfig();
 		config.network().setInputFile(netFile);
 		config.plans().setInputFile(plansFile);
