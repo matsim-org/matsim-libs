14ade90632e78f886a458de0ef6408d258291dc0
andreas77 <andreashorni@hotmail.com>
2011-03-31 15:18:33 +0000

    stubs
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@14857 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/ivtExt/src/main/java/herbie/CreateBorderCrossingFacilities.java
@@ -0,0 +1,14 @@
+package herbie;
+
+public class CreateBorderCrossingFacilities {
+	
+	
+	public void create() {
+		this.createFacility();
+	}
+	
+	// Create randomized coordinates and facilities. Assignment to links will be done later
+	private void createFacility() {
+		
+	}
+}
