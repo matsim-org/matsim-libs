37bbd4d07645096dff83f1c102df13cdf8a339fb
sergio537 <sergio537@users.sf.net>
2011-04-04 01:12:24 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@14873 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/sergioo/src/main/java/playground/sergioo/PathEditor/gui/PanelPathEditor.java
+++ playgrounds/sergioo/src/main/java/playground/sergioo/PathEditor/gui/PanelPathEditor.java
@@ -64,18 +64,12 @@ public class PanelPathEditor extends JPanel {
 	private void paintPoints(Graphics2D g2) {
 		g2.setColor(pointsColor);
 		g2.setStroke(pointsStroke);
-		for(Point2D point:pointsLines.getPoints()) {
-			g2.drawLine(getIntX(point.getX())-pointsSize, getIntY(point.getY())+pointsSize, getIntX(point.getX())+pointsSize, getIntY(point.getY())-pointsSize);
-			g2.drawLine(getIntX(point.getX())-pointsSize, getIntY(point.getY())-pointsSize, getIntX(point.getX())+pointsSize, getIntY(point.getY())+pointsSize);
-		}
+		
 	}
 	private void paintLines(Graphics2D g2) {
 		g2.setColor(linesColor);
 		g2.setStroke(linesStroke);
-		for(Line2D line:pointsLines.getLines())
-			g2.drawLine(getIntX(line.getPI().getX()), getIntY(line.getPI().getY()), getIntX(line.getPF().getX()), getIntY(line.getPF().getY()));
-		/*for(Line2D line:((SimpleMapMatcher)pointsLines).getLines2())
-			g2.drawLine(getIntX(line.getPI().getX()), getIntY(line.getPI().getY()), getIntX(line.getPF().getX()), getIntY(line.getPF().getY()));*/
+
 	}
 	public int getIntX(double x) {
 		return (int) ((x-upLeftCorner.getX())*(Window.WIDTH-2*Window.FRAMESIZE)/sizeDownRight.getX())+Window.FRAMESIZE;
@@ -85,36 +79,6 @@ public class PanelPathEditor extends JPanel {
 	}
 	private void calculateBoundaries() {
 		double xMin=Double.POSITIVE_INFINITY, yMin=Double.POSITIVE_INFINITY, xMax=Double.NEGATIVE_INFINITY, yMax=Double.NEGATIVE_INFINITY;
-		for(Point2D point:pointsLines.getPoints()) {
-			if(point.getX()<xMin)
-				xMin = point.getX();
-			if(point.getX()>xMax)
-				xMax = point.getX();
-			if(point.getY()<yMin)
-				yMin = point.getY();
-			if(point.getY()>yMax)
-				yMax = point.getY();
-		}
-		for(Line2D line:pointsLines.getLines()) {
-			Point2D point = line.getPI();
-			if(point.getX()<xMin)
-				xMin = point.getX();
-			if(point.getX()>xMax)
-				xMax = point.getX();
-			if(point.getY()<yMin)
-				yMin = point.getY();
-			if(point.getY()>yMax)
-				yMax = point.getY();
-			point = line.getPF();
-			if(point.getX()<xMin)
-				xMin = point.getX();
-			if(point.getX()>xMax)
-				xMax = point.getX();
-			if(point.getY()<yMin)
-				yMin = point.getY();
-			if(point.getY()>yMax)
-				yMax = point.getY();
-		}
 		upLeftCorner = new Point2D(xMin, yMax);
 		sizeDownRight = new Vector2D(new Point2D(xMin, yMin), new Point2D(xMax, yMax));
 	}
