f0f4a1dfdc6d04f5b50bb6584362797d8891f313
jbischoff <bischoff@vsp.tu-berlin.de>
2011-04-26 14:30:50 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@15157 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/jbischoff/src/main/java/playground/jbischoff/commuterDemand/CommuterGenerator.java
+++ playgrounds/jbischoff/src/main/java/playground/jbischoff/commuterDemand/CommuterGenerator.java
@@ -25,22 +25,37 @@ package playground.jbischoff.commuterDemand;
  */
 public class CommuterGenerator {
 
-	public static void main(String[] args) {
+	public 	static void main(String[] args) {
 		
 		CommuterDataReader cdr = new CommuterDataReader();
-		cdr.addFilterRange(12071000);
-		cdr.addFilter("12052000");
-		cdr.readFile("C:\\Users\\Joschka Bischoff\\Documents\\Brandenburg\\CD_Pendler_Gemeindeebene_30_06_2009\\brandenburg_einpendler.csv");
+//		cdr.addFilter("12051000"); //BB
+		cdr.addFilter("12054000"); //P
+//		cdr.addFilterRange(11000000); //B
+		cdr.addFilterRange(12069000); //PM
+		cdr.addFilterRange(12072000); //TF
+		cdr.addFilterRange(12063000); //HVL
+		cdr.addFilterRange(12069000); //PM
+//		cdr.addFilterRange(12065000); //OHV
+		
+		cdr.readFile("/home/jbischoff/matsimkurs/Brandenburg 2009/pendlerdaten_brb.csv");
 		
 		
 		MunicipalityShapeReader msr = new MunicipalityShapeReader();
-		msr.addFilter("12052000");
-		msr.addFilterRange(12071000);
-		msr.readShapeFile("C:\\Users\\Joschka Bischoff\\Documents\\Brandenburg\\Gemeinden\\dlm_gemeinden.shp");
+//		msr.addFilter("12051000"); //BB
+		msr.addFilter("12054000"); //P
+//		msr.addFilterRange(11000000); //B
+		msr.addFilterRange(12069000); //PM
+		msr.addFilterRange(12072000); //TF
+		msr.addFilterRange(12063000); //HVL
+		msr.addFilterRange(12069000); //PM
+//		msr.addFilterRange(12065000); //OHV
+
+		
+		msr.readShapeFile("/home/jbischoff/matsimkurs/brandenburg_gemeinde_kreisgrenzen/gemeinden/dlm_gemeinden.shp");
 		
 		CommuterDemandWriter cdw = new CommuterDemandWriter(msr.getShapeMap(),cdr.getCommuterRelations());
-		cdw.setScalefactor(1.0);//1.0 is default already
-		cdw.writeDemand("C:\\Users\\Joschka Bischoff\\Documents\\Brandenburg\\demand\\demand.xml");
+		cdw.setScalefactor(0.1);//1.0 is default already
+		cdw.writeDemand("/home/jbischoff/matsimkurs/demand.xml");
 		
 	}
 
