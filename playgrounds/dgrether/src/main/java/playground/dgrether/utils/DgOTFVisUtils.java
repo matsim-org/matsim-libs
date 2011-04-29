87051aff9e74371052309ddc9de292be2068e9bb
grether <dominik.grether@gmail.com>
2011-04-29 17:52:14 +0000

    no need for astar landmarks
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@15222 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/dgrether/src/main/java/playground/dgrether/utils/DgOTFVisUtils.java
+++ playgrounds/dgrether/src/main/java/playground/dgrether/utils/DgOTFVisUtils.java
@@ -26,7 +26,7 @@ import org.matsim.core.network.NetworkImpl;
 import org.matsim.core.population.PopulationImpl;
 import org.matsim.core.router.PlansCalcRoute;
 import org.matsim.core.router.costcalculators.FreespeedTravelTimeCost;
-import org.matsim.core.router.util.AStarLandmarksFactory;
+import org.matsim.core.router.util.DijkstraFactory;
 import org.matsim.population.algorithms.PersonPrepareForSim;
 import org.matsim.population.algorithms.XY2Links;
 import org.matsim.vis.otfvis.data.OTFConnectionManager;
@@ -47,13 +47,13 @@ public class DgOTFVisUtils {
 	public static void locateAndRoutePopulation(Scenario scenario){
 		((PopulationImpl)scenario.getPopulation()).addAlgorithm(new XY2Links((NetworkImpl) scenario.getNetwork()));
 		final FreespeedTravelTimeCost timeCostCalc = new FreespeedTravelTimeCost(scenario.getConfig().planCalcScore());
-		((PopulationImpl)scenario.getPopulation()).addAlgorithm(new PlansCalcRoute(scenario.getConfig().plansCalcRoute(), scenario.getNetwork(), timeCostCalc, timeCostCalc, new AStarLandmarksFactory(scenario.getNetwork(), timeCostCalc)));
+		((PopulationImpl)scenario.getPopulation()).addAlgorithm(new PlansCalcRoute(scenario.getConfig().plansCalcRoute(), scenario.getNetwork(), timeCostCalc, timeCostCalc, new DijkstraFactory()));
 		((PopulationImpl)scenario.getPopulation()).runAlgorithms();
 	}
 	
 	public static void preparePopulation4Simulation(Scenario scenario) {
 		final FreespeedTravelTimeCost timeCostCalc = new FreespeedTravelTimeCost(scenario.getConfig().planCalcScore());
-		PlansCalcRoute router = new PlansCalcRoute(scenario.getConfig().plansCalcRoute(), scenario.getNetwork(), timeCostCalc, timeCostCalc, new AStarLandmarksFactory(scenario.getNetwork(), timeCostCalc));
+		PlansCalcRoute router = new PlansCalcRoute(scenario.getConfig().plansCalcRoute(), scenario.getNetwork(), timeCostCalc, timeCostCalc, new DijkstraFactory());
 		PersonPrepareForSim pp4s = new PersonPrepareForSim(router, (NetworkImpl) scenario.getNetwork());
 		for (Person p : scenario.getPopulation().getPersons().values()){
 			pp4s.run(p);
