302d9fed1b101b6ab77b7edd3914acded3b6d7fc
andreas77 <andreashorni@hotmail.com>
2011-05-09 15:36:41 +0000

    adapted input path
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@15352 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/ivtExt/src/main/java/tutorial/CreateNetwork.java
+++ playgrounds/ivtExt/src/main/java/tutorial/CreateNetwork.java
@@ -1,5 +1,31 @@
 package tutorial;
 
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.core.api.experimental.network.NetworkWriter;
+import org.matsim.core.config.Config;
+import org.matsim.core.network.algorithms.NetworkCleaner;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.geometry.CoordinateTransformation;
+import org.matsim.core.utils.geometry.transformations.TransformationFactory;
+import org.matsim.core.utils.io.OsmNetworkReader;
+import org.matsim.core.utils.misc.ConfigUtils;
+
+
 public class CreateNetwork {
 
+   public static void main(String[] args) {
+      String osm = "./input/merged-network.osm";
+      Config config = ConfigUtils.createConfig();
+      Scenario sc = ScenarioUtils.createScenario(config);
+      Network net = sc.getNetwork();
+      CoordinateTransformation ct = TransformationFactory.getCoordinateTransformation(
+    		  TransformationFactory.WGS84, TransformationFactory.CH1903_LV03);
+      OsmNetworkReader onr = new OsmNetworkReader(net,ct);
+      onr.parse(osm); 
+      new NetworkCleaner().run(net);
+      new NetworkWriter(net).write("./output/network.xml");
+   }
 }
+
+
