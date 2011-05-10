b479de1c9c10825d40cd5e4cf88b1cf2331185fb
yu_vsp <yu_vsp@users.sf.net>
2011-05-10 14:21:34 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@15369 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/yu/src/main/java/playground/yu/analysis/forZrh/AnalysisTest4ZrhWithXMLEvents.java
@@ -0,0 +1,278 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * AvgTolledTripLengthControler.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+/**
+ *
+ */
+package playground.yu.analysis.forZrh;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.matsim.analysis.CalcAverageTripLength;
+import org.matsim.api.core.v01.population.Population;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.events.EventsUtils;
+import org.matsim.core.events.MatsimEventsReader;
+import org.matsim.core.facilities.MatsimFacilitiesReader;
+import org.matsim.core.network.MatsimNetworkReader;
+import org.matsim.core.network.NetworkImpl;
+import org.matsim.core.population.MatsimPopulationReader;
+import org.matsim.core.scenario.ScenarioImpl;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.io.MatsimFileTypeGuesser;
+import org.matsim.core.utils.io.MatsimFileTypeGuesser.FileType;
+import org.matsim.core.utils.misc.ConfigUtils;
+import org.matsim.roadpricing.RoadPricingReaderXMLv1;
+import org.matsim.roadpricing.RoadPricingScheme;
+import org.xml.sax.SAXException;
+
+import playground.yu.analysis.CalcLinksAvgSpeed;
+import playground.yu.analysis.CalcNetAvgSpeed;
+import playground.yu.analysis.CalcTrafficPerformance;
+import playground.yu.analysis.GeometricDistanceExtractor;
+import playground.yu.analysis.LegDistance;
+import playground.yu.analysis.ModeSplit;
+import playground.yu.analysis.MyCalcAverageTripLength;
+import playground.yu.utils.io.SimpleWriter;
+
+/**
+ * @author ychen
+ *
+ */
+public class AnalysisTest4ZrhWithXMLEvents implements Analysis4Zrh {
+
+	private static void printUsage() {
+		System.out.println();
+		System.out.println("AnalysisTest4Zrh:");
+		System.out.println("----------------");
+		System.out
+				.println("Create an additional analysis for the runs, which were done with only org.matsim.run.Controler");
+		System.out.println();
+		System.out.println("usage: AnalysisTest args");
+		System.out.println(" arg 0:\tname of scenario (for Zurich required)");
+		System.out.println(" arg 1:\trunId");
+		System.out
+				.println(" arg 2:\tname incl. path to network file (.xml[.gz])(required)");
+		System.out
+				.println(" arg 3:\tpath to events file, plans file, and analysis files, without the last \"/\"");
+		System.out.println(" args 4:\twhether there is plansfile (true/false)");
+		System.out
+				.println(" arg 5:\tsnapshot-period of OTFVis:  Specify how often a snapshot should be taken when reading the events, in seconds.");
+		System.out
+				.println(" arg 6:\tname incl. path to facilities file (.xml[.gz])(optional)");
+		System.out
+				.println(" arg 7:\tname incl. path to toll file (.xml)(optional)");
+		System.out.println("----------------");
+	}
+
+	private static void runIntern(final String[] args, final String scenario) {
+		final String netFilename = args[2];
+
+		String[] tmp = args[3]/* iterationPath */.split("it.");
+		String outputBase = args[3]/* iterationPath */+ "/" + args[1]/* runId */
+				+ "." + tmp[tmp.length - 1] + ".";
+		final String eventsFilename = outputBase + "events.xml.gz";
+
+		String plansFilename = Boolean.parseBoolean(args[4])/* hasPop? */? outputBase
+				+ "plans.xml.gz"
+				: null;
+
+		String outputBase4analysis = outputBase + "analysis"
+				+ (scenario.equals(KANTON_ZURICH) ? ".Kanton/" : "/") + args[1]/* runId */
+				+ "." + (scenario.equals("normal") ? "" : scenario + ".");
+
+		String tollFilename = !scenario.equals(KANTON_ZURICH) ? null
+				: args[args.length - 1];// last parameter, man needs tollFile
+		// only for Kanton_Zurich.
+
+		String facilitiesFilename = null;
+		if (args.length >= 7) {
+			try {
+				if (new MatsimFileTypeGuesser(args[6]).getGuessedFileType()
+						.equals(FileType.Facilities)) {
+					facilitiesFilename = args[6];
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		ScenarioImpl s = (ScenarioImpl) ScenarioUtils.createScenario(ConfigUtils.createConfig());
+
+		NetworkImpl network = s.getNetwork();
+		new MatsimNetworkReader(s).readFile(netFilename);
+		// facilities
+		if (facilitiesFilename != null) {
+			new MatsimFacilitiesReader(s).readFile(facilitiesFilename);
+		}
+		// toll
+		RoadPricingScheme toll = null;
+		if (scenario.equals(KANTON_ZURICH)) {
+			s.getConfig().scenario().setUseRoadpricing(true);
+			toll = s.getRoadPricingScheme();
+			RoadPricingReaderXMLv1 tollReader = new RoadPricingReaderXMLv1(toll);
+			try {
+				tollReader.parse(tollFilename);
+			} catch (SAXException e) {
+				e.printStackTrace();
+			} catch (ParserConfigurationException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// EventsHandlers with parameter of "Population":
+		EnRouteModalSplit4Zrh orms = null;
+		LegTravelTimeModalSplit4Zrh lttms = null;
+		// PersonAlgorithm
+		CalcAverageTripLength catl = null;
+		DailyDistance4Zrh dd = null;
+		DailyEnRouteTime4Zrh dert = null;
+		ModeSplit ms = null;
+		LegDistance ld = null;
+		GeometricDistanceExtractor gde = null;
+		// only PersonAlgorithm begins.
+		if (plansFilename != null) {
+			Population population = s.getPopulation();
+
+			catl = new MyCalcAverageTripLength(network);
+			ms = new ModeSplit(toll);
+			orms = new EnRouteModalSplit4Zrh(scenario, population, toll);
+			lttms = new LegTravelTimeModalSplit4Zrh(population, toll);
+			dd = new DailyDistance4Zrh(toll, network);
+			dert = new DailyEnRouteTime4Zrh(toll);
+			ld = new LegDistance(network, toll, population);
+			gde = new GeometricDistanceExtractor(toll, outputBase
+					+ "geoDistKanton");
+			// in future, add some PersonAlgorithm and EventsHandler
+
+			new MatsimPopulationReader(s).readFile(plansFilename);
+
+			catl.run(population);
+			dd.run(population);
+			dert.run(population);
+			ms.run(population);
+			gde.run(population);
+		} else {
+			ld = new LegDistance(network);
+		}
+		// only PersonAlgorithm ends.
+		EventsManager events = EventsUtils.createEventsManager();
+		// EventsHandlers without parameter of "Population":
+		CalcTrafficPerformance ctpf = new CalcTrafficPerformance(network, toll);
+		CalcNetAvgSpeed cas = new CalcNetAvgSpeed(network, toll);
+		CalcLinksAvgSpeed clas = null;
+		if (scenario.equals(ZURICH)) {
+			clas = new CalcLinksAvgSpeed(network, 682845.0, 247388.0, 2000.0);
+		} else if (scenario.equals(KANTON_ZURICH)) {
+			clas = new CalcLinksAvgSpeed(network, toll);
+		} else {
+			clas = new CalcLinksAvgSpeed(network);
+		}
+
+		events.addHandler(ctpf);
+		events.addHandler(cas);
+		events.addHandler(clas);
+		events.addHandler(ld);
+
+		if (orms != null) {
+			events.addHandler(orms);
+		}
+		if (lttms != null) {
+			events.addHandler(lttms);
+		}
+
+		new MatsimEventsReader(events).readFile(eventsFilename);
+
+		if (orms != null) {
+
+			orms.write(outputBase4analysis + "onRoute.txt");
+			orms.writeCharts(outputBase4analysis);
+		}
+		if (lttms != null) {
+			lttms.write(outputBase4analysis + "legtraveltimes.txt.gz");
+			lttms.writeCharts(outputBase4analysis + "legtraveltimes");
+		}
+		clas.write(outputBase4analysis + "avgSpeed.txt.gz");
+		clas.writeChart(outputBase4analysis + "avgSpeedCityArea.png");
+		ld.write(outputBase4analysis + "legDistances.txt.gz");
+		ld.writeCharts(outputBase4analysis + "legDistances");
+
+		SimpleWriter sw = new SimpleWriter(outputBase4analysis + "output.txt");
+		sw.write("netfile:\t" + netFilename + "\neventsFile:\t"
+				+ eventsFilename + "\noutputpath:\t" + outputBase4analysis
+				+ "\n");
+		if (catl != null) {
+			sw.write("avg. Trip length:\t" + catl.getAverageTripLength()
+					+ " [m]\n");
+		}
+		sw.write("traffic performance (car):\t" + ctpf.getTrafficPerformance()
+				+ " [Pkm]\n");
+		sw.write("avg. speed of the total network (car):\t"
+				+ cas.getNetAvgSpeed() + " [km/h]\n");
+		sw.close();
+
+		if (dd != null) {
+			dd.write(outputBase4analysis);
+		}
+		if (dert != null) {
+			dert.write(outputBase4analysis);
+		}
+		if (ms != null) {
+			ms.write(outputBase4analysis);
+		}
+		if (gde != null) {
+			gde.write();
+		}
+		System.out.println("done.");
+	}
+
+	public static void run(final String[] args) {
+		runIntern(args, "normal");
+	}
+
+	public static void runZurich(final String[] args) {
+		runIntern(args, ZURICH);
+	}
+
+	public static void runKantonZurich(String[] args) {
+		runIntern(args, KANTON_ZURICH);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+		if (args.length < 5) {
+			System.err.println("too few parameters!");
+			printUsage();
+			System.exit(1);
+		} else if (args[0].equals(ZURICH)) {
+			runZurich(args);
+		} else if (args[0].equals(KANTON_ZURICH)) {
+			runKantonZurich(args);
+		} else {
+			run(args);
+		}
+	}
+}
