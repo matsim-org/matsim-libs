209e236e7933f705f864eeaf684a9bec238e7170
benkick <benkick@users.sf.net>
2011-05-10 13:37:35 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@15368 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/benjamin/src/main/java/playground/benjamin/otfvis/BkVis.java
+++ playgrounds/benjamin/src/main/java/playground/benjamin/otfvis/BkVis.java
@@ -21,15 +21,36 @@ package playground.benjamin.otfvis;
 
 import org.matsim.run.OTFVis;
 
+import playground.benjamin.BkPaths;
+
 
 public class BkVis {
 
 	public static void main(String[] args) {
-
+		
+		boolean convert = false;
+		
+//===========================================================================================================
+//		Für den converter-modus:
+			
+		if(convert){
+			String networkFile = BkPaths.RUNSSVN + "run970/970.output_network.xml.gz";
+			String eventsFile = BkPaths.RUNSSVN + "run970/ITERS/it.1000/970.1000.events.txt.gz";
+			String outputFile = BkPaths.RUNSSVN + "run970/ITERS/it.1000/970.1000.events.mvi";
+			String[] array = {
+					"blah",
+					eventsFile, 
+					networkFile,
+					outputFile,
+					"600"};
+			OTFVis.convert(array);
+		}
+		else{
+			
 //===========================================================================================================		
 //		Für den mvi-modus:
 //			String otffile = "../../detailedEval/teststrecke/sim/output/20090707/ITERS/it.0/0.otfvis.mvi";
-			String otffile = "../../detailedEval/testRuns/output/10pct/run2/it.100/100.otfvis.mvi";
+//			String otffile = "../../detailedEval/testRuns/output/10pct/run2/it.100/100.otfvis.mvi";
 //			String otffile = "../../detailedEval/Net/network-86-85-87-84_simplified.xml";
 //			String otffile = "../../matsim/output/singleIteration/ITERS/it.0/0.otfvis.mvi";
 //			String otffile = BkPaths.RUNSSVN + "run749/it.2000/749.2000.Zurich.otfvis.mvi";
@@ -61,9 +82,10 @@ public class BkVis {
 //		String otffile = "../../detailedEval/pop/140k-synthetische-personen/config-for-visualisation.xml";
 //		String otffile = "../../detailedEval/teststrecke/sim/input/liveConfig_benjamin.xml";
 //		String otffile = "../../detailedEval/testRuns/input/config.xml";
-		
 //		String otffile = BkPaths.RUNSSVN + "run950/950.output_config.xml";
+		String otffile = BkPaths.RUNSSVN + "run970/ITERS/it.1000/970.1000.events.mvi";
 		
 		OTFVis.main(new String[] {otffile});
-	}
+		}
+	}	
 }
