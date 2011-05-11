bd362b79191383d57243e4ca729843e5b7b6334a
stellaschieffer <stellaschieffer@users.sf.net>
2011-05-11 11:36:35 +0000

    adding TimeTesting class
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@15381 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/wrashid/src/test/java/playground/wrashid/sschieffer/DecentralizedSmartCharger/TimeTesting.java
@@ -0,0 +1,94 @@
+package playground.wrashid.sschieffer.DecentralizedSmartCharger;
+
+import java.io.IOException;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.events.IterationEndsEvent;
+import org.matsim.core.controler.listener.IterationEndsListener;
+
+import playground.wrashid.PSF.data.HubLinkMapping;
+
+
+/**
+ * change the input  configName Strings
+ * to get a summary about total time taken for computation of Decentralized Smart Charger
+ * @author Stella
+ *
+ */
+public class TimeTesting {
+
+	
+	public static void main(String[] args) throws IOException {
+		
+		final String configName= "config_plans100.xml";
+		String configPath="test/input/playground/wrashid/sschieffer/"+configName;
+		final String outputPath ="D:\\ETH\\MasterThesis\\TestOutput\\";
+		
+		final double phev=1.0;
+		final double ev=0.0;
+		final double combustion=0.0;
+		
+		final double bufferBatteryCharge=0.0;
+		
+		final double MINCHARGINGLENGTH=5*60;
+		
+		DecentralizedSmartCharger myDecentralizedSmartCharger;
+		
+		final TestSimulationSetUp mySimulation = new TestSimulationSetUp(
+				configPath, 
+				phev, 
+				ev, 
+				combustion);
+		
+		Controler controler= mySimulation.getControler();
+		
+		controler.addControlerListener(new IterationEndsListener() {
+			
+			@Override
+			public void notifyIterationEnds(IterationEndsEvent event) {
+				
+				try {
+					
+					HubLinkMapping hubLinkMapping = mySimulation.mapHubsTest();
+					
+					DecentralizedSmartCharger myDecentralizedSmartCharger = new DecentralizedSmartCharger(
+							event.getControler(), 
+							mySimulation.getParkingTimesPlugIn(),
+							mySimulation.getEnergyConsumptionInit().getEnergyConsumptionPlugin(),
+							outputPath,
+							mySimulation.getVehicleTypeCollector()
+							);
+					
+					myDecentralizedSmartCharger.initializeLP(bufferBatteryCharge);
+					
+					myDecentralizedSmartCharger.initializeChargingSlotDistributor(MINCHARGINGLENGTH);
+					
+					myDecentralizedSmartCharger.setLinkedListValueHashMapVehicles(
+							mySimulation.getEnergyConsumptionInit().getVehicles());
+					
+					myDecentralizedSmartCharger.initializeHubLoadDistributionReader(
+							hubLinkMapping, 
+							mySimulation.getDeterministicLoadSchedule(),							
+							mySimulation.getDetermisiticPricing()
+							);
+					
+					
+					myDecentralizedSmartCharger.run();
+					myDecentralizedSmartCharger.writeSummary(configName);
+					
+					
+				} catch (Exception e1) {
+					
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+							
+			}
+		});
+		controler.run();
+
+	}
+
+}
