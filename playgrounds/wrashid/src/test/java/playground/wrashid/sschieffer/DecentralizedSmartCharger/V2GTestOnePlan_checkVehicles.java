3f3b1892adf98c337c478c2a364ceaba267e5398
stellaschieffer <stellaschieffer@users.sf.net>
2011-05-16 09:13:54 +0000

    commiting latest Version of V2GTestOnePLan_checkVehicles
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@15427 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/test/java/playground/wrashid/sschieffer/DecentralizedSmartCharger/V2GTestOnePlan_checkVehicles.java
+++ playgrounds/wrashid/src/test/java/playground/wrashid/sschieffer/DecentralizedSmartCharger/V2GTestOnePlan_checkVehicles.java
@@ -21,46 +21,54 @@
 package playground.wrashid.sschieffer.DecentralizedSmartCharger;
 
 import java.io.IOException;
-
-import junit.framework.TestCase;
-import lpsolve.LpSolveException;
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 import org.apache.commons.math.FunctionEvaluationException;
 import org.apache.commons.math.MaxIterationsExceededException;
 import org.apache.commons.math.analysis.polynomials.PolynomialFunction;
 import org.apache.commons.math.optimization.OptimizationException;
+
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Link;
+import org.matsim.core.basic.v01.IdImpl;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.events.IterationEndsEvent;
 import org.matsim.core.controler.listener.IterationEndsListener;
 
-import playground.wrashid.PSF.data.HubLinkMapping;
-import playground.wrashid.PSF2.pluggable.energyConsumption.EnergyConsumptionPlugin;
-import playground.wrashid.PSF2.pluggable.parkingTimes.ParkingTimesPlugin;
-import playground.wrashid.lib.EventHandlerAtStartupAdder;
-import playground.wrashid.lib.obj.LinkedListValueHashMap;
+import junit.framework.TestCase;
+import lpsolve.LpSolveException;
 
 /**
  * tests methods:
- * <li> checkVehicle sources
+ * <li> checks if vehicle source is reduced in simulation as expected
+ * <li> check if addRevenueToAgentFromV2G has expected results
  * 
  * @author Stella
  *
  */
 public class V2GTestOnePlan_checkVehicles extends TestCase{
 
-	/**
-	 * @param args
-	 */
-	final String outputPath="D:\\ETH\\MasterThesis\\TestOutput\\";
 	String configPath="test/input/playground/wrashid/sschieffer/config_plans1.xml";
-	final Controler controler=new Controler(configPath);
-	
-	
+	final String outputPath ="D:\\ETH\\MasterThesis\\TestOutput\\";
 	public Id agentOne=null;
 	
-	public static V2G myV2G;
+	Controler controler; 
+	
+	final double phev=1.0;
+	final double ev=0.0;
+	final double combustion=0.0;
+	
+	final double bufferBatteryCharge=0.0;
+	
+	final double standardChargingSlotLength=15*60;
+	
+	double compensationPerKWHRegulationUp=0.15;
+	double compensationPerKWHRegulationDown=0.15;
+	double xPercentNone=0;
+	double xPercentDown=0;
+	double xPercentDownUp=1.0;
 	
 	public static DecentralizedSmartCharger myDecentralizedSmartCharger;
 	
@@ -80,56 +88,14 @@ public class V2GTestOnePlan_checkVehicles extends TestCase{
 	 */
 	public void testV2GCheckVehicles() throws MaxIterationsExceededException, FunctionEvaluationException, IllegalArgumentException, LpSolveException, OptimizationException, IOException{
 		
+		final TestSimulationSetUp mySimulation = new TestSimulationSetUp(
+				configPath, 
+				phev, 
+				ev, 
+				combustion);
 		
-		final ParkingTimesPlugin parkingTimesPlugin;
-		final EnergyConsumptionPlugin energyConsumptionPlugin;
+		controler= mySimulation.getControler();
 		
-		double gasPricePerLiter= 0.25; 
-		double gasJoulesPerLiter = 43.0*1000000.0;// Benzin 42,7–44,2 MJ/kg
-		double emissionPerLiter = 23.2/10; // 23,2kg/10l= xx/mass   1kg=1l
-		//--> gas Price per second for PHEV 4.6511627906976747E-4
-		
-		
-		double optimalPrice=0.25*1/1000*1/3600*3500;//0.25 CHF per kWh		
-		double suboptimalPrice=optimalPrice*3; // cost/second  
-		//0.24*10^-4 EUR per second
-			
-		final LinkedListValueHashMap<Integer, Schedule> deterministicHubLoadDistribution= readHubsTest();
-		final LinkedListValueHashMap<Integer, Schedule> pricingHubDistribution=readHubsPricingTest(optimalPrice, suboptimalPrice);
-		
-		
-		final HubLinkMapping hubLinkMapping=new HubLinkMapping(deterministicHubLoadDistribution.size());//= new HubLinkMapping(0);
-		
-		
-		final double phev=1.0;
-		final double ev=0.0;
-		final double combustion=0.0;
-		
-		
-		final double bufferBatteryCharge=0.0;
-		
-		
-		SetUpVehicleCollector sv= new SetUpVehicleCollector();
-		final VehicleTypeCollector myVehicleTypes = sv.setUp();		
-		
-		final double MINCHARGINGLENGTH=5*60;//5 minutes
-		
-		
-		EventHandlerAtStartupAdder eventHandlerAtStartupAdder = new EventHandlerAtStartupAdder();
-		
-		parkingTimesPlugin = new ParkingTimesPlugin(controler);
-		
-		eventHandlerAtStartupAdder.addEventHandler(parkingTimesPlugin);
-		
-		final EnergyConsumptionInit e= new EnergyConsumptionInit(
-				phev, ev, combustion);
-		
-		
-		controler.addControlerListener(e);
-				
-		controler.addControlerListener(eventHandlerAtStartupAdder);
-		
-		controler.setOverwriteFiles(true);
 		
 		controler.addControlerListener(new IterationEndsListener() {
 			
@@ -138,29 +104,12 @@ public class V2GTestOnePlan_checkVehicles extends TestCase{
 				
 				try {
 					
-					mapHubsTest(controler,hubLinkMapping);
+					mySimulation.setUpStochasticLoadDistributions();
 					
-					DecentralizedSmartCharger myDecentralizedSmartCharger = new DecentralizedSmartCharger(
-							event.getControler(), 
-							parkingTimesPlugin,
-							e.getEnergyConsumptionPlugin(),
+					DecentralizedSmartCharger myDecentralizedSmartCharger = mySimulation.setUpSmartCharger(
 							outputPath,
-							myVehicleTypes
-							);
-					
-					
-					myDecentralizedSmartCharger.initializeLP(bufferBatteryCharge);
-					
-					myDecentralizedSmartCharger.initializeChargingSlotDistributor(MINCHARGINGLENGTH);
-					
-					myDecentralizedSmartCharger.setLinkedListValueHashMapVehicles(
-							e.getVehicles());
-					
-					myDecentralizedSmartCharger.initializeHubLoadDistributionReader(
-							hubLinkMapping, 
-							deterministicHubLoadDistribution,							
-							pricingHubDistribution
-							);
+							bufferBatteryCharge,
+							standardChargingSlotLength);
 					
 					myDecentralizedSmartCharger.run();
 					
@@ -168,91 +117,64 @@ public class V2GTestOnePlan_checkVehicles extends TestCase{
 					 * V2G
 					 * *********************************
 					 */
-					
-					LinkedListValueHashMap<Integer, Schedule> stochasticLoad = 
-						readStochasticLoad(1);
+					// all day 3500
+					HashMap<Integer, Schedule> stochasticLoad = 
+						mySimulation.getStochasticLoadSchedule();
 						
-					LinkedListValueHashMap<Id, Schedule> agentVehicleSourceMapping =
-						makeAgentVehicleSourceNegativeAndPositive(controler);
+					HashMap<Id, Schedule> agentVehicleSourceMapping =
+						mySimulation.getAgentStochasticLoadSources();
 					
-					linkedListIntegerPrinter(stochasticLoad, "Before stochastic general");
+					DecentralizedSmartCharger.linkedListIntegerPrinter(stochasticLoad, "Before stochastic general");
 					
-					linkedListIdPrinter(agentVehicleSourceMapping,  "Before agent");
+					DecentralizedSmartCharger.linkedListIdPrinter(agentVehicleSourceMapping,  "Before agent");
 					
 					myDecentralizedSmartCharger.setStochasticSources(
-							stochasticLoad, 
+							mySimulation.getStochasticLoadSchedule(), 
 							null, 
-							agentVehicleSourceMapping);
+							mySimulation.getAgentStochasticLoadSources());
 					
-					// setting agent Contracts
+					mySimulation.setUpAgentSchedules(
+							myDecentralizedSmartCharger, 
+							compensationPerKWHRegulationUp, 
+							compensationPerKWHRegulationDown, 
+							xPercentNone, 
+							xPercentDown, 
+							xPercentDownUp);
 					
-					double compensationPerKWHRegulationUp=0.15;
-					double compensationPerKWHRegulationDown=0.15;
-					 
-					AgentContractCollector myAgentContractsCollector= new AgentContractCollector (
-							myDecentralizedSmartCharger,
-							 compensationPerKWHRegulationUp,
-							 compensationPerKWHRegulationDown);
+					myDecentralizedSmartCharger.setAgentContracts(mySimulation.getAgentContracts());
 					
-					LinkedListValueHashMap<Id, ContractTypeAgent> agentContracts= 
-						myAgentContractsCollector.makeAgentContracts(
-								controler,
-								0,
-								0,
-								1);
-					
-					myDecentralizedSmartCharger.setAgentContracts(agentContracts);
-					
-					
-					
-					// instead of initalize and run go through steps
-					System.out.println("START CHECKING VEHICLE SOURCES");
-					
-					/*
-					 * CHECK reduceAgentVehicleLoadsByGivenLoadInterval
-					 */
 					
 					for(Id id: controler.getPopulation().getPersons().keySet()){
 						agentOne=id;
 						System.out.println("AGENT VEHICLE SOURCE BEFORE V2G of -3500 between 0-300");
-						agentVehicleSourceMapping.getValue(id).printSchedule();
+						agentVehicleSourceMapping.get(id).printSchedule();
+						LoadDistributionInterval lFirst= 
+							(LoadDistributionInterval)agentVehicleSourceMapping.get(agentOne).timesInSchedule.get(0);
+						assertEquals(lFirst.getPolynomialFunction().getCoefficients()[0], 
+								-3500.0);
 					}
 					
 					myDecentralizedSmartCharger.initializeAndRunV2G();
 						
 					
-						System.out.println("AGENT VEHICLE SOURCE BEFORE V2G of -3500 between 0-300");
-						agentVehicleSourceMapping.getValue(agentOne).printSchedule();
+					System.out.println("AGENT VEHICLE SOURCE AFTER V2G ");
+					agentVehicleSourceMapping.get(agentOne).printSchedule();
 						
 						
-						LoadDistributionInterval lFirst= 
-							(LoadDistributionInterval)agentVehicleSourceMapping.getValue(agentOne).timesInSchedule.get(0);
-						assertEquals(lFirst.getPolynomialFunction().getCoefficients()[0], 
+					LoadDistributionInterval lFirst= 
+							(LoadDistributionInterval)agentVehicleSourceMapping.get(agentOne).timesInSchedule.get(0);
+					assertEquals(lFirst.getPolynomialFunction().getCoefficients()[0], 
 								0.0);
 					
 					
-										
-					/*
-					 * reassign stochastic load and check results after  checkVehicleLoads()
-					 * and checkHubStochasticLoads();
-					 * 
-					public void testFindAndReturnAgentScheduleWithinLoadIntervalWhichIsAtSpecificHub(){
-						
-					}
+					double revenue=myDecentralizedSmartCharger.getAgentV2GRevenues().get(agentOne);
 					
-					public void testReassignJoulesToSchedule(){
-						
-					}
-					 */
+					myDecentralizedSmartCharger.myV2G.addRevenueToAgentFromV2G(100.0, agentOne);
 					
-					//System.out.println("START CHECKING STOCHASTIC HUB LOADS");
-					//myDecentralizedSmartCharger.checkHubStochasticLoads();
+					double revenueNew=myDecentralizedSmartCharger.getAgentV2GRevenues().get(agentOne);
 					
+					assertEquals(revenue+100.0, revenueNew);
 					
-					/*
-					linkedListIntegerPrinter(stochasticLoad, "After stochastic general");
-					linkedListIdPrinter(agentVehicleSourceMapping,  "After agent");
-					*/
 					
 				} catch (Exception e1) {
 					
@@ -268,166 +190,10 @@ public class V2GTestOnePlan_checkVehicles extends TestCase{
 	}
 
 
-	public void linkedListIdPrinter(LinkedListValueHashMap<Id, Schedule> list, String info){
-		System.out.println("Print LinkedList "+ info);
-		for(Id id: list.getKeySet()){
-			list.getValue(id).printSchedule();
-		}
-		
-	}
-	
-	public void linkedListIntegerPrinter(LinkedListValueHashMap<Integer, Schedule> list, String info){
-		System.out.println("Print LinkedList "+ info);
-		for(Integer id: list.getKeySet()){
-			list.getValue(id).printSchedule();
-		}
-		
-	}
 	
 	
 	
 	
-	public LinkedListValueHashMap<Id, Schedule> makeAgentVehicleSourceNegativeAndPositive(Controler controler){
-		LinkedListValueHashMap<Id, Schedule> agentSource= 
-			new LinkedListValueHashMap<Id, Schedule>();
-		
-		//Id
-		for(Id id : controler.getPopulation().getPersons().keySet()){
-			
-				Schedule bullShitMinus= new Schedule();
-				PolynomialFunction pMinus = new PolynomialFunction(new double[] {-3500.0});
-				bullShitMinus.addTimeInterval(new LoadDistributionInterval(0, 2000.0, pMinus, false));
-				
-				PolynomialFunction pPlus = new PolynomialFunction(new double[] {3500.0});
-				bullShitMinus.addTimeInterval(new LoadDistributionInterval(20000.0, 20300.0, pPlus, true));
-				
-				
-				agentSource.put(id, bullShitMinus);	
-			
-			
-		}
-		return agentSource;
-	}
-	
-	
-	public static LinkedListValueHashMap<Integer, Schedule> readHubsTest() throws IOException{
-		LinkedListValueHashMap<Integer, Schedule> hubLoadDistribution1= new  LinkedListValueHashMap<Integer, Schedule>();
-		hubLoadDistribution1.put(1, makeBullshitScheduleTest());
-		
-		return hubLoadDistribution1;
-		
-	}
-	
-	
-	public static LinkedListValueHashMap<Integer, Schedule> readHubsPricingTest(double optimal, double suboptimal) throws IOException{
-		LinkedListValueHashMap<Integer, Schedule> hubLoadDistribution1= new  LinkedListValueHashMap<Integer, Schedule>();
-		hubLoadDistribution1.put(1, makeBullshitPricingScheduleTest(optimal, suboptimal));
-		return hubLoadDistribution1;
-		
-	}
-	
-	
-	
-	
-	public static Schedule makeBullshitScheduleTest() throws IOException{
-		
-		Schedule bullShitSchedule= new Schedule();
-		
-		double[] bullshitCoeffs = new double[]{10};// 
-		double[] bullshitCoeffs2 = new double[]{-10};
-		
-		PolynomialFunction bullShitFunc= new PolynomialFunction(bullshitCoeffs);
-		PolynomialFunction bullShitFunc2= new PolynomialFunction(bullshitCoeffs2);
-		LoadDistributionInterval l1= new LoadDistributionInterval(
-				0.0,
-				62490.0,
-				bullShitFunc,//p
-				true//boolean
-		);
-		
-		bullShitSchedule.addTimeInterval(l1);
-		
-		
-		LoadDistributionInterval l2= new LoadDistributionInterval(					
-				62490.0,
-				DecentralizedSmartCharger.SECONDSPERDAY,
-				bullShitFunc2,//p
-				false//boolean
-		);
-	
-		bullShitSchedule.addTimeInterval(l2);
-		//bullShitSchedule.printSchedule();
-		
-		return bullShitSchedule;
-	}
-	
-	
-	public void mapHubsTest(Controler controler, HubLinkMapping hubLinkMapping){
-		
-		
-		for (Link link:controler.getNetwork().getLinks().values()){
-			
-			hubLinkMapping.addMapping(link.getId().toString(), 1);
-			}
-			
-	}
-	
-	
-	
-public static LinkedListValueHashMap<Integer, Schedule> readStochasticLoad(int num){
-		
-		LinkedListValueHashMap<Integer, Schedule> stochastic= new LinkedListValueHashMap<Integer, Schedule>();
-		
-		Schedule bullShitStochastic= new Schedule();
-		PolynomialFunction p = new PolynomialFunction(new double[] {3500});
-		
-		bullShitStochastic.addTimeInterval(new LoadDistributionInterval(0, 24*3600, p, true));
-		for (int i=0; i<num; i++){
-			stochastic.put(i+1, bullShitStochastic);
-		}
-		return stochastic;
-	
-		
-	}
-	
-		
-	
-
-public static Schedule makeBullshitPricingScheduleTest(double optimal, double suboptimal) throws IOException{
-	
-	Schedule bullShitSchedule= new Schedule();
-	
-	PolynomialFunction pOpt = new PolynomialFunction(new double[] {optimal});	
-	PolynomialFunction pSubopt = new PolynomialFunction(new double[] {suboptimal});
-	
-	
-	LoadDistributionInterval l1= new LoadDistributionInterval(
-			0.0,
-			62490.0,
-			pOpt,//p
-			true//boolean
-	);
-	
-	bullShitSchedule.addTimeInterval(l1);
-	
-	
-	LoadDistributionInterval l2= new LoadDistributionInterval(					
-			62490.0,
-			DecentralizedSmartCharger.SECONDSPERDAY,
-			pSubopt,//p
-			false//boolean
-	);
-
-	bullShitSchedule.addTimeInterval(l2);
-	//bullShitSchedule.printSchedule();
-	
-	return bullShitSchedule;
-}
-
-	
-	
-	
-
 	
 }
 
