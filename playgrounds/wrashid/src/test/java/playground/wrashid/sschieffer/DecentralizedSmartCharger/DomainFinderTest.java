ba02544500114dd71172219c6f6e3e5d6ee9c8d6
stellaschieffer <stellaschieffer@users.sf.net>
2011-05-16 12:57:39 +0000

    committing test class package Decentralized Smart Charger
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@15437 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/wrashid/src/test/java/playground/wrashid/sschieffer/DecentralizedSmartCharger/DomainFinderTest.java
@@ -0,0 +1,39 @@
+package playground.wrashid.sschieffer.DecentralizedSmartCharger;
+
+import org.apache.commons.math.analysis.polynomials.PolynomialFunction;
+import org.matsim.testcases.MatsimTestCase;
+
+public class DomainFinderTest extends MatsimTestCase{
+
+	
+	public DomainFinderTest(){
+		
+	}
+	
+	
+	public void testLinear(){
+		PolynomialFunction func = new PolynomialFunction(new double[]{0, 1});
+		
+		DomainFinder finder= new DomainFinder(0, 10, func);
+		
+		double maxDomain= finder.getDomainMax();
+		double minDomain= finder.getDomainMin();
+		
+		assertEquals(0.0, minDomain);
+		assertEquals(10.0, maxDomain);
+	}
+	
+	
+	public void testQuadratic(){
+		PolynomialFunction func = new PolynomialFunction(new double[]{0, 0, 1});
+		
+		DomainFinder finder= new DomainFinder(-5, 5, func);
+		
+		double maxDomain= finder.getDomainMax();
+		double minDomain= finder.getDomainMin();
+		
+		assertEquals(0.0, minDomain);
+		assertEquals(25.0, maxDomain);
+	}
+	
+}
