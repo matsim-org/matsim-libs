5fcc44adf50b73403145b8c32734b93b57e3b12f
stellaschieffer <stellaschieffer@users.sf.net>
2011-06-02 22:55:17 +0000

    updating test class
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@15686 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/test/java/playground/wrashid/sschieffer/V2G/V2GTestOnePlan_StatisticCheck.java
+++ playgrounds/wrashid/src/test/java/playground/wrashid/sschieffer/V2G/V2GTestOnePlan_StatisticCheck.java
@@ -39,6 +39,7 @@ import org.matsim.core.controler.listener.IterationEndsListener;
 
 import playground.wrashid.sschieffer.DSC.DecentralizedSmartCharger;
 import playground.wrashid.sschieffer.DSC.LoadDistributionInterval;
+import playground.wrashid.sschieffer.DSC.ParkingInterval;
 import playground.wrashid.sschieffer.DSC.Schedule;
 import playground.wrashid.sschieffer.DSC.TimeDataCollector;
 import playground.wrashid.sschieffer.DecentralizedSmartCharger.TestSimulationSetUp;
@@ -101,8 +102,6 @@ public class V2GTestOnePlan_StatisticCheck extends TestCase{
 				);
 		
 		controler= mySimulation.getControler();
-		
-		
 		controler.addControlerListener(new IterationEndsListener() {
 			
 			@Override
@@ -120,12 +119,7 @@ public class V2GTestOnePlan_StatisticCheck extends TestCase{
 				
 					// all day 3500
 					HashMap<Integer, Schedule> stochasticLoad = mySimulation.getStochasticLoadSchedule();
-						
 					HashMap<Id, Schedule> agentVehicleSourceMapping =mySimulation.getAgentStochasticLoadSources();
-					
-					DecentralizedSmartCharger.linkedListIntegerPrinter(stochasticLoad, "Before stochastic general");
-					
-					DecentralizedSmartCharger.linkedListIdPrinter(agentVehicleSourceMapping,  "Before agent");
 					
 					myDecentralizedSmartCharger.setStochasticSources(
 							stochasticLoad, 
@@ -142,41 +136,20 @@ public class V2GTestOnePlan_StatisticCheck extends TestCase{
 					
 					myDecentralizedSmartCharger.setAgentContracts(mySimulation.getAgentContracts());
 					
-					myDecentralizedSmartCharger.initializeAndRunV2G(
-							xPercentDown, xPercentDownUp);
+					myDecentralizedSmartCharger.myV2G.initializeAgentStats();
+					myDecentralizedSmartCharger.setV2GRegUpAndDownStats(xPercentDown,xPercentDownUp);
 					
-					double revenue=myDecentralizedSmartCharger.getV2GRevenueForAgent(agentOne);
-					
-					myDecentralizedSmartCharger.myV2G.addRevenueToAgentFromV2G(100.0, agentOne);
-					
-					double revenueNew=myDecentralizedSmartCharger.getV2GRevenueForAgent(agentOne);
-					
-					assertEquals(revenue+100.0, revenueNew);
+					for(Id id: mySimulation.getControler().getPopulation().getPersons().keySet()){
+						agentOne=id;
+						break;
+					}
 					
 					
-					/**********************************************
-					 *  * CHECK
-					 * addJoulesV2G
-					 */
 					
-					double down = myDecentralizedSmartCharger.myV2G.getTotalRegulationUp();
-					myDecentralizedSmartCharger.myV2G.addJoulesUpDownToAgentStats(1000.0, agentOne);
-					double newDown = myDecentralizedSmartCharger.myV2G.getTotalRegulationUp();
-					myDecentralizedSmartCharger.myV2G.calcV2GVehicleStats();
-					assertEquals(1000.0, newDown-down);
+					checkRevenueStats();
 					
-					down = myDecentralizedSmartCharger.myV2G.getTotalRegulationDown();
-					myDecentralizedSmartCharger.myV2G.addJoulesUpDownToAgentStats(-1000.0, agentOne);
-					newDown = myDecentralizedSmartCharger.myV2G.getTotalRegulationDown();
-					myDecentralizedSmartCharger.myV2G.calcV2GVehicleStats();
-					assertEquals(1000.0, down-newDown);
 					
-					/**********************************************
-					 *  * CHECK
-					 * sensible outcome averageV2G
-					 */
-					
-					assertEquals(myDecentralizedSmartCharger.myV2G.getAverageV2GRevenuePHEV(), myDecentralizedSmartCharger.myV2G.getAverageV2GRevenueAgent());
+					checkRescheduling();
 					
 					
 				} catch (Exception e1) {
@@ -194,8 +167,117 @@ public class V2GTestOnePlan_StatisticCheck extends TestCase{
 
 
 	
+	public void checkRescheduling() throws Exception{
+		
+		myDecentralizedSmartCharger.getAllAgentParkingAndDrivingSchedules().get(agentOne).printSchedule();
+		double chargingTimeFirstInterval= ((ParkingInterval)myDecentralizedSmartCharger.getAllAgentParkingAndDrivingSchedules().get(agentOne).
+				timesInSchedule.get(0)).getRequiredChargingDuration();
+		/*
+		 * Decentralized Smart Charger DONE
+			*************************
+			Starting SOC: 8640000.0
+			Parking Interval 	 start: 0.0	  end: 21600.0	  ChargingTime:  19748.571428571428	  Optimal:  true	  Joules per Interval:  216000.0	  ChargingSchedule of size:  1
+			Driving Interval 	  start: 21600.0	  end: 22500.0	  consumption: 1.8786265875308476E7
+			Parking Interval 	 start: 22500.0	  end: 35700.0	  ChargingTime:  5367.504535803354	  Optimal:  true	  Joules per Interval:  132000.0	  ChargingSchedule of size:  6
+			Driving Interval 	  start: 35700.0	  end: 38040.0	  consumption: 4.879471387207076E7
+			Parking Interval 	 start: 38040.0	  end: 62490.0	  ChargingTime:  13941.346820583964	  Optimal:  true	  Joules per Interval:  244500.0	  ChargingSchedule of size:  16
+			Parking Interval 	 start: 62490.0	  end: 86400.0	  ChargingTime:  0.0	  Optimal:  false	  Joules per Interval:  -239100.0	  ChargingSchedule of size:  0
+			*************************
+		 */
+		
+		
+		double costChargingBefore= myDecentralizedSmartCharger.getChargingCostsForAgents().get(agentOne);
+		
+		//PREPARATION
+		Schedule answerScheduleAfterElectricSourceInterval= myDecentralizedSmartCharger.getAllAgentParkingAndDrivingSchedules().get(agentOne).
+								cutScheduleAtTimeSecondHalf(21600, 0, agentOne);
+		((ParkingInterval)answerScheduleAfterElectricSourceInterval.timesInSchedule.get(1)).setRequiredChargingDuration(0);
+		
+		LoadDistributionInterval electricSourceInterval = new LoadDistributionInterval(0, 21600, 1);
+		
+		
+		// RESCHEDULE
+		myDecentralizedSmartCharger.myV2G.reschedule(agentOne, 
+				answerScheduleAfterElectricSourceInterval, 
+				electricSourceInterval, 
+				1, 
+				0, 3500);
+		
+		
+		//CHECKS
+		double costChargingAfter= myDecentralizedSmartCharger.getChargingCostsForAgents().get(agentOne);
+		assertEquals(costChargingBefore-1, costChargingAfter);
+		
+		
+		/*UPDATE AGENTPARKINGADNDRIVINGSCHEDULE WITH NEW required charging times
+		 * 3500 joules >0 is reg down, is as if agent charge longer 
+		 * 1 second longer charging in first interval joules= 3500 correponds to 1sec charging time at standard connection speed
+		 */
+		double newChargingTimeFirstInterval= ((ParkingInterval)myDecentralizedSmartCharger.getAllAgentParkingAndDrivingSchedules().get(agentOne).
+				timesInSchedule.get(0)).getRequiredChargingDuration();
+		
+		assertEquals(newChargingTimeFirstInterval-chargingTimeFirstInterval, 1.0);
+		
+		// charging time was set 0
+		double newChargingTimeThirdInterval= ((ParkingInterval)myDecentralizedSmartCharger.getAllAgentParkingAndDrivingSchedules().get(agentOne).
+				timesInSchedule.get(2)).getRequiredChargingDuration();
+		assertEquals(newChargingTimeThirdInterval, 0.0);
+		
+		
+		/*
+		 * DISTRIBUTE new found required charging times for agent	
+		//check times required charging after
+		 */
+		Schedule newChargingSchedule = ((ParkingInterval)myDecentralizedSmartCharger.getAllAgentParkingAndDrivingSchedules().get(agentOne).
+				timesInSchedule.get(0)).getChargingSchedule();
+		assertEquals(newChargingTimeFirstInterval, newChargingSchedule.getTotalTimeOfIntervalsInSchedule());
+		
+		newChargingSchedule = ((ParkingInterval)myDecentralizedSmartCharger.getAllAgentParkingAndDrivingSchedules().get(agentOne).
+				timesInSchedule.get(2)).getChargingSchedule();
+		assertEquals(newChargingSchedule, null);	
+			
+	}
 	
-	
+		
+		
+		
+		public void checkRevenueStats(){
+			double revenue=myDecentralizedSmartCharger.getV2GRevenueForAgent(agentOne);
+			myDecentralizedSmartCharger.myV2G.addRevenueToAgentFromV2G(100.0, agentOne);
+			double revenueNew=myDecentralizedSmartCharger.getV2GRevenueForAgent(agentOne);
+			
+			assertEquals(revenue+100.0, revenueNew);
+			
+			
+			/**********************************************
+			 *  * CHECK
+			 * addJoulesV2G
+			 */
+			
+			// regulation up joules<0
+			double up = myDecentralizedSmartCharger.myV2G.getTotalRegulationUp();
+			myDecentralizedSmartCharger.myV2G.addJoulesUpDownToAgentStats(-1000.0, agentOne);
+			myDecentralizedSmartCharger.myV2G.calcV2GVehicleStats();
+			double newUp = myDecentralizedSmartCharger.myV2G.getTotalRegulationUp();
+			
+			assertEquals(-1000.0, newUp-up);
+			
+			// regulation up joules>0
+			double down = myDecentralizedSmartCharger.myV2G.getTotalRegulationDown();
+			myDecentralizedSmartCharger.myV2G.addJoulesUpDownToAgentStats(1000.0, agentOne);
+			myDecentralizedSmartCharger.myV2G.calcV2GVehicleStats();
+			double newDown = myDecentralizedSmartCharger.myV2G.getTotalRegulationDown();
+		
+			assertEquals(1000.0, newDown-down);
+			
+			/**********************************************
+			 *  * CHECK
+			 * sensible outcome averageV2G
+			 */
+			myDecentralizedSmartCharger.myV2G.calcV2GVehicleStats();
+			assertEquals(myDecentralizedSmartCharger.myV2G.getAverageV2GRevenuePHEV(), myDecentralizedSmartCharger.myV2G.getAverageV2GRevenueAgent());
+			
+		}
 	
 	
 }
