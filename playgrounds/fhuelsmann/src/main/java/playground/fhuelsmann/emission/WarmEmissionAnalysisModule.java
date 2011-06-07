396e489734aa1ae4eda68f95318c5b82523de630
vsp_nagel <2008@kainagel.org>
2011-06-07 07:15:24 +0000

    cleaning up imports so that code compiles for me in eclipse
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@15766 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/fhuelsmann/src/main/java/playground/fhuelsmann/emission/WarmEmissionAnalysisModule.java
+++ playgrounds/fhuelsmann/src/main/java/playground/fhuelsmann/emission/WarmEmissionAnalysisModule.java
@@ -21,22 +21,18 @@
 package playground.fhuelsmann.emission;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
-import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.Map.Entry;
 
 import org.matsim.api.core.v01.Id;
 
 import playground.fhuelsmann.emission.objects.HbefaObject;
 import playground.fhuelsmann.emission.objects.HotValue;
 import playground.fhuelsmann.emission.objects.VisumObject;
-import playground.fhuelsmann.emission.objects.FuelSizeHroad;
 
 public class WarmEmissionAnalysisModule implements AnalysisModule{
 
