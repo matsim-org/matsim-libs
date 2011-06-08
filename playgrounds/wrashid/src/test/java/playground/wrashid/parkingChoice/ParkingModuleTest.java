fb8077ecc6348835d95b2a0fafb5dc21baa80669
rashid_waraich <rashid_waraich@users.sf.net>
2011-06-08 12:06:44 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@15794 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/wrashid/src/test/java/playground/wrashid/parkingChoice/ParkingModuleTest.java
@@ -0,0 +1,56 @@
+package playground.wrashid.parkingChoice;
+
+import java.util.LinkedList;
+
+import org.matsim.core.config.Config;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.utils.geometry.CoordImpl;
+import org.matsim.core.utils.misc.ConfigUtils;
+
+import playground.wrashid.parkingChoice.infrastructure.Parking;
+
+import junit.framework.TestCase;
+
+public class ParkingModuleTest extends TestCase {
+
+	public void testBaseCase(){
+		assertEquals(3489, walkingDistanceFor3CarScenarioWithVariableParkingCapacity(1),1.0);
+	}
+	
+	public void testHigherParkingCapacityMakesWalkingDistanceShorter(){
+		assertEquals(1997, walkingDistanceFor3CarScenarioWithVariableParkingCapacity(3),1.0);
+	}
+	
+	public void testMakingTheCapacityHigherThanNumberOfCarsWillNotMakeWalkingDistanceShorter(){
+		assertEquals(1997, walkingDistanceFor3CarScenarioWithVariableParkingCapacity(10),1.0);
+	}
+	
+	private double walkingDistanceFor3CarScenarioWithVariableParkingCapacity(int parkingCapacity) {
+		Config config=ConfigUtils.loadConfig("test/input/playground/wrashid/parkingChoice/chessConfig.xml");
+		Controler controler=new Controler(config);
+		
+		// setup parking infrastructure
+		LinkedList<Parking> parkingCollection = new LinkedList<Parking>();
+
+		for (int i=0;i<10;i++){
+			for (int j=0;j<10;j++){
+				Parking parking = new Parking(new CoordImpl(i*1000+500,j*1000+500));
+				parking.setMaxCapacity(parkingCapacity);
+				parkingCollection.add(parking);
+			}
+		}
+		
+		
+		ParkingModule parkingModule = new ParkingModule(controler,parkingCollection);
+		
+		controler.setOverwriteFiles(true);
+		
+		controler.run();
+		
+		return parkingModule.getAverageWalkingDistance();
+	}
+	
+	
+	
+	
+}
