2bc6ccc580395842f2e883e6042f58e1e478d928
vsp_nagel <2008@kainagel.org>
2011-06-22 16:18:15 +0000

    Introducing a warning for the wrong sign of the distanceCost.  Will lead to vsp-abort when this is set accordingly.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@15929 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/core/config/consistency/VspConfigConsistencyCheckerImpl.java
+++ matsim/src/main/java/org/matsim/core/config/consistency/VspConfigConsistencyCheckerImpl.java
@@ -20,17 +20,43 @@
 
 package org.matsim.core.config.consistency;
 
+import org.apache.log4j.Logger;
 import org.matsim.core.config.Config;
+import org.matsim.core.config.groups.VspExperimentalConfigGroup;
 
 /**
  * @author nagel
  *
  */
 public class VspConfigConsistencyCheckerImpl implements ConfigConsistencyChecker {
+	private static Logger log = Logger.getLogger(VspConfigConsistencyCheckerImpl.class) ;
+	
+	public VspConfigConsistencyCheckerImpl() {
+		// empty.  only here to find out where it is called.
+	}
 
 	@Override
 	public void checkConsistency(Config config) {
-		// doing nothing at this point
+		boolean problem = false ;
+
+		if ( config.planCalcScore().getMonetaryDistanceCostRateCar() > 0 ) {
+			problem = true ;
+			log.warn("found monetary distance cost rate car > 0.  You probably want a value < 0 here.  " +
+					"This is a bug and may be changed eventually.  kai, jun'11") ;
+		}
+		
+		if ( config.planCalcScore().getMonetaryDistanceCostRatePt() > 0 ) {
+			problem = true ;
+			log.warn("found monetary distance cost rate pt > 0.  You probably want a value < 0 here.  " +
+					"This is a bug and may be changed eventually.  kai, jun'11") ;
+		}
+		
+		if ( problem && config.vspExperimental().getVspDefaultsCheckingLevel().equals( VspExperimentalConfigGroup.ABORT ) ) {
+			String str = "found a situation that leads to vsp-abort.  aborting ..." ; 
+			log.fatal( str ) ; 
+			throw new RuntimeException( str ) ;
+		}
+		
 	}
 
 }
