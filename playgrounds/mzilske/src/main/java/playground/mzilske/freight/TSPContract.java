be63ab49d848e2944aca23052d7a2973e6bb76a4
oblonski <oblonski@users.sf.net>
2011-07-07 09:28:22 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@16040 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/mzilske/src/main/java/playground/mzilske/freight/TSPContract.java
+++ playgrounds/mzilske/src/main/java/playground/mzilske/freight/TSPContract.java
@@ -1,39 +1,22 @@
 package playground.mzilske.freight;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 
 
 public class TSPContract {
 	private TSPShipment shipment;
 	
 	private TSPOffer offer;
-
-	private Collection<TSPShipment> shipments;
 	
 	public TSPContract(TSPShipment shiment, TSPOffer offer) {
 		super();
 		this.shipment = shiment;
 		this.offer = offer;
-		shipments = new ArrayList<TSPShipment>();
-		shipments.add(shiment);
 	}
 	
-	public TSPContract(Collection<TSPShipment> shipments, TSPOffer offer) {
-		super();
-		this.offer = offer;
-		this.shipments = shipments;
-	}
-
 	public TSPShipment getShipment() {
 		return shipment;
 	}
 	
-	public Collection<TSPShipment> getShipments(){
-		return Collections.unmodifiableCollection(shipments);
-	}
-
 	public TSPOffer getOffer() {
 		return offer;
 	}
