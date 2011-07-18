55a2e55f43de34bb4186810b15129dc460484a72
rashid_waraich <rashid_waraich@users.sf.net>
2011-07-18 12:55:11 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@16151 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/main/java/playground/wrashid/lib/DebugLib.java
+++ playgrounds/wrashid/src/main/java/playground/wrashid/lib/DebugLib.java
@@ -5,7 +5,7 @@ import org.matsim.api.core.v01.Id;
 public class DebugLib {
 
 	public static void traceAgent(Id personId){
-		if (personId.toString().equalsIgnoreCase("255")){
+		if (personId.toString().equalsIgnoreCase("1086454")){
 			System.out.println();
 		}
 	}
