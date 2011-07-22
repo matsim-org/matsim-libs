3919974a2ac01c9597331ffe444781bb78e0fb51
benkick <benkick@users.sf.net>
2011-07-22 11:49:50 +0000

    added emissionEvents; need probably a reader later on...
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@16209 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/fhuelsmann/src/main/java/playground/fhuelsmann/emission/event/EmissionTestWriter.java
@@ -0,0 +1,65 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * EmissionTestWriter.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2009 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.fhuelsmann.emission.event;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Id;
+import org.matsim.core.api.experimental.events.Event;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.basic.v01.IdImpl;
+import org.matsim.core.events.EventsUtils;
+import org.matsim.core.events.algorithms.EventWriterXML;
+
+/**
+ * @author benjamin
+ *
+ */
+public class EmissionTestWriter {
+	private static final Logger logger = Logger.getLogger(EmissionTestWriter.class);
+
+	public static void main(String[] args) {
+		
+		EventsManager eventsManager = EventsUtils.createEventsManager();
+				
+		Id linkId = new IdImpl("17");
+		Id vehicleId = new IdImpl("38");
+		
+		Map<String, Double> hotEmissions = new HashMap<String, Double>();
+		hotEmissions.put("FC", 1000.0);
+		hotEmissions.put("CO2", 3000.0);
+		Event hotEvent = new HotEmissionEventImpl(3600, linkId, vehicleId, hotEmissions);
+		
+		Map<String, Double> coldEmissions = new HashMap<String, Double>();
+		coldEmissions.put("FC", 20.0);
+		coldEmissions.put("VOC", 365.0);
+		Event coldEvent = new ColdEmissionEventImpl(3500, linkId, vehicleId, coldEmissions);
+		
+		String outputfile = "../../runs-svn/testEvents.xml";
+		EventWriterXML eWriter = new EventWriterXML(outputfile);
+		eventsManager.addHandler(eWriter);
+		eventsManager.processEvent(hotEvent);
+		eventsManager.processEvent(coldEvent);
+		eWriter.closeFile();
+		logger.info("Finished writing output to " + outputfile);
+	}
+}
---
+++ playgrounds/fhuelsmann/src/main/java/playground/fhuelsmann/emission/event/HotEmissionEventImpl.java
@@ -0,0 +1,76 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * EmissionEventHotImpl.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2009 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.fhuelsmann.emission.event;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.core.events.EventImpl;
+
+/**
+ * @author benjamin
+ *
+ */
+public class HotEmissionEventImpl extends EventImpl implements HotEmissionEvent{
+	private Id linkId;
+	private Id vehicleId;
+	private Map<String, Double> hotEmissions;
+	
+	public HotEmissionEventImpl(double time, Id linkId, Id vehicleId, Map<String, Double> hotEmissions) {
+		super(time);
+		this.linkId = linkId;
+		this.vehicleId = vehicleId;
+		this.hotEmissions = hotEmissions;
+	}
+
+	@Override
+	public Id getLinkId() {
+		return linkId;
+	}
+	
+	@Override
+	public Id getVehicleId() {
+		return vehicleId;
+	}
+	
+	@Override
+	public Map<String, Double> getHotEmissions() {
+		return hotEmissions;
+	}
+
+	@Override
+	public Map<String, String> getAttributes(){
+		Map<String, String> attributes = super.getAttributes();
+		attributes.put(ATTRIBUTE_LINK_ID, this.linkId.toString());
+		attributes.put(ATTRIBUTE_VEHICLE_ID, this.vehicleId.toString());
+		for(Entry<String, Double> entry : hotEmissions.entrySet()){
+			String pollutant = entry.getKey();
+			String value = entry.getValue().toString();
+			attributes.put(pollutant, value);
+		}
+		return attributes;
+	}
+
+	@Override
+	public String getEventType() {
+		return HotEmissionEvent.EVENT_TYPE;
+	}
+}
