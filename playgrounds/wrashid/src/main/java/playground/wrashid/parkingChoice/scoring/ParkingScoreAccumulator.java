a7cf50c37b52d7e80ba6bf2bbdb0cb4298e526bb
rashid_waraich <rashid_waraich@users.sf.net>
2011-07-25 13:38:07 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@16225 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/main/java/playground/wrashid/parkingChoice/scoring/ParkingScoreAccumulator.java
+++ playgrounds/wrashid/src/main/java/playground/wrashid/parkingChoice/scoring/ParkingScoreAccumulator.java
@@ -35,8 +35,8 @@ public class ParkingScoreAccumulator implements AfterMobsimListener {
 	private static Set<String> selectedParkings;
 	private static double[] sumOfOccupancyCountsOfSelectedParkings;
 	private final ParkingScoreCollector parkingScoreCollector;
-	private Double averageWalkingDistance=null;
-	public static DoubleValueHashMap<Id> scores=new DoubleValueHashMap<Id>();
+	private Double averageWalkingDistance = null;
+	public static DoubleValueHashMap<Id> scores = new DoubleValueHashMap<Id>();
 
 	public Double getAverageWalkingDistance() {
 		return averageWalkingDistance;
@@ -47,7 +47,7 @@ public class ParkingScoreAccumulator implements AfterMobsimListener {
 
 	public ParkingScoreAccumulator(ParkingScoreCollector parkingScoreCollector, ParkingManager parkingManager) {
 		this.parkingScoreCollector = parkingScoreCollector;
-		this.parkingManager=parkingManager;
+		this.parkingManager = parkingManager;
 	}
 
 	@Override
@@ -78,165 +78,171 @@ public class ParkingScoreAccumulator implements AfterMobsimListener {
 				double disutilityOfWalking = 0;
 				double sumOfWalkingTimes = parkingScoreCollector.getSumOfWalkingTimes(personId);
 				double sumOfParkingDurations = parkingScoreCollector.getSumOfParkingDurations(personId);
-				walkingDistances.put(personId, sumOfWalkingTimes * event.getControler().getConfig().plansCalcRoute().getWalkSpeed());
+				walkingDistances.put(personId, sumOfWalkingTimes
+						* event.getControler().getConfig().plansCalcRoute().getWalkSpeed());
 
-				
-				String parkingSelectionManager = controler.getConfig().findParam("parking", "parkingSelectionManager");
-				if (parkingSelectionManager.equalsIgnoreCase("shortestWalkingDistance")){
-					// this is only the implicit disutility (not an explicit one)
-					disutilityOfWalking = -1 * Math.abs(sumOfActTotalScore) * sumOfWalkingTimes / sumOfParkingDurations;
-					scoringFuncAccumulator.addMoney(disutilityOfWalking);
-					//TODO: here the explicit disutility of walking is totally missing
-					
-				} else if (parkingSelectionManager.equalsIgnoreCase("PriceAndDistance_v1")) {
-					//TODO: perhaps later make the parking price based on acutal parking duration instead of
-					//an estimation.
-					scoringFuncAccumulator.addMoney(scores.get(personId));
-					// implicity disutility of not beeing able to perform
-					disutilityOfWalking = -1 * Math.abs(sumOfActTotalScore) * sumOfWalkingTimes / sumOfParkingDurations;
-					scoringFuncAccumulator.addMoney(disutilityOfWalking);
-				} else {
-					DebugLib.stopSystemAndReportInconsistency("unknown parkingSelectionManager:" + parkingSelectionManager);
+				if (!ParkingChoiceLib.isTestCaseRun) {
+					String parkingSelectionManager = controler.getConfig().findParam("parking", "parkingSelectionManager");
+					if (parkingSelectionManager.equalsIgnoreCase("shortestWalkingDistance")) {
+						// this is only the implicit disutility (not an explicit
+						// one)
+						disutilityOfWalking = -1 * Math.abs(sumOfActTotalScore) * sumOfWalkingTimes / sumOfParkingDurations;
+						scoringFuncAccumulator.addMoney(disutilityOfWalking);
+						// TODO: here the explicit disutility of walking is
+						// totally missing
+
+					} else if (parkingSelectionManager.equalsIgnoreCase("PriceAndDistance_v1")) {
+						// TODO: perhaps later make the parking price based on
+						// acutal parking duration instead of
+						// an estimation.
+						scoringFuncAccumulator.addMoney(scores.get(personId));
+						// implicity disutility of not beeing able to perform
+						disutilityOfWalking = -1 * Math.abs(sumOfActTotalScore) * sumOfWalkingTimes / sumOfParkingDurations;
+						scoringFuncAccumulator.addMoney(disutilityOfWalking);
+					} else {
+						DebugLib.stopSystemAndReportInconsistency("unknown parkingSelectionManager:" + parkingSelectionManager);
+					}
 				}
-				
 
-				
 			}
 
 		}
 		writeWalkingDistanceStatisticsGraph(controler, walkingDistances);
 		printWalkingDistanceHistogramm(controler, walkingDistances);
-		if (!ParkingChoiceLib.isTestCaseRun){
+		if (!ParkingChoiceLib.isTestCaseRun) {
 			writeOutParkingOccupancies(controler);
 			writeOutGraphParkingTypeOccupancies(controler);
 			writeOutGraphComparingSumOfSelectedParkingsToCounts(controler);
 		}
-		
-		//eventsToScore.finish();
+
+		// eventsToScore.finish();
 	}
 
 	private void writeOutGraphComparingSumOfSelectedParkingsToCounts(Controler controler) {
-		String iterationFilename = controler.getControlerIO().getIterationFilename(controler.getIterationNumber(), "parkingOccupancyCountsComparison.png");
+		String iterationFilename = controler.getControlerIO().getIterationFilename(controler.getIterationNumber(),
+				"parkingOccupancyCountsComparison.png");
 
-		HashMap<String, String> mappingOfParkingNameToParkingId = SingleDayGarageParkingsCount.getMappingOfParkingNameToParkingId();
-		int[] sumOfSelectedParkingSimulatedCounts=new int[96];
-		
-		for (String parkingName:selectedParkings){
-			ParkingOccupancyBins parkingOccupancyBins = parkingScoreCollector.parkingOccupancies.get(new IdImpl(mappingOfParkingNameToParkingId.get(parkingName)));
-			
-			if (parkingOccupancyBins==null){
+		HashMap<String, String> mappingOfParkingNameToParkingId = SingleDayGarageParkingsCount
+				.getMappingOfParkingNameToParkingId();
+		int[] sumOfSelectedParkingSimulatedCounts = new int[96];
+
+		for (String parkingName : selectedParkings) {
+			ParkingOccupancyBins parkingOccupancyBins = parkingScoreCollector.parkingOccupancies.get(new IdImpl(
+					mappingOfParkingNameToParkingId.get(parkingName)));
+
+			if (parkingOccupancyBins == null) {
 				continue;
 			}
-			
+
 			int[] occupancy = parkingOccupancyBins.getOccupancy();
-			for (int i=0;i<96;i++){
-				sumOfSelectedParkingSimulatedCounts[i]+=occupancy[i];
+			for (int i = 0; i < 96; i++) {
+				sumOfSelectedParkingSimulatedCounts[i] += occupancy[i];
 			}
 		}
-		
-		
-		double matrix[][]=new double[96][2];
-		
-		for (int i=0;i<96;i++){
-			matrix[i][0]=sumOfSelectedParkingSimulatedCounts[i];
-			matrix[i][1]=sumOfOccupancyCountsOfSelectedParkings[i];
+
+		double matrix[][] = new double[96][2];
+
+		for (int i = 0; i < 96; i++) {
+			matrix[i][0] = sumOfSelectedParkingSimulatedCounts[i];
+			matrix[i][1] = sumOfOccupancyCountsOfSelectedParkings[i];
 		}
-		
-		
-		String title="Parking Garage Counts Comparison";
-		String xLabel="time (15min-bin)";
-		String yLabel="# of occupied parkings";
-		String[] seriesLabels=new String[2];
-		seriesLabels[0]="simulated counts";
-		seriesLabels[1]="real counts";
-		double[] xValues=new double[96];
-		
-		for (int i=0;i<96;i++){
-			xValues[i]=i/(double)4;
+
+		String title = "Parking Garage Counts Comparison";
+		String xLabel = "time (15min-bin)";
+		String yLabel = "# of occupied parkings";
+		String[] seriesLabels = new String[2];
+		seriesLabels[0] = "simulated counts";
+		seriesLabels[1] = "real counts";
+		double[] xValues = new double[96];
+
+		for (int i = 0; i < 96; i++) {
+			xValues[i] = i / (double) 4;
 		}
-		
-		GeneralLib.writeGraphic(iterationFilename, matrix, title, xLabel, yLabel, seriesLabels, xValues);		
+
+		GeneralLib.writeGraphic(iterationFilename, matrix, title, xLabel, yLabel, seriesLabels, xValues);
 	}
 
 	private void writeOutGraphParkingTypeOccupancies(Controler controler) {
-		String iterationFilename = controler.getControlerIO().getIterationFilename(controler.getIterationNumber(), "parkingOccupancy.png");
+		String iterationFilename = controler.getControlerIO().getIterationFilename(controler.getIterationNumber(),
+				"parkingOccupancy.png");
 
-		double matrix[][]=new double[96][4];
-		
-		for (Id parkingId:parkingScoreCollector.parkingOccupancies.keySet()){
-			Parking parking=parkingManager.getParkingsHashMap().get(parkingId);
-			int graphIndex=-1;
-			if (parking.getId().toString().startsWith("gp")){
-				graphIndex=0;
-			} else if (parking.getId().toString().startsWith("privateParkings")){
-				graphIndex=1;
-			}else if (parking.getId().toString().startsWith("publicPOutsideCityZH")){
-				graphIndex=2;
-			}else if (parking.getId().toString().startsWith("stp")){
-				graphIndex=3;
+		double matrix[][] = new double[96][4];
+
+		for (Id parkingId : parkingScoreCollector.parkingOccupancies.keySet()) {
+			Parking parking = parkingManager.getParkingsHashMap().get(parkingId);
+			int graphIndex = -1;
+			if (parking.getId().toString().startsWith("gp")) {
+				graphIndex = 0;
+			} else if (parking.getId().toString().startsWith("privateParkings")) {
+				graphIndex = 1;
+			} else if (parking.getId().toString().startsWith("publicPOutsideCityZH")) {
+				graphIndex = 2;
+			} else if (parking.getId().toString().startsWith("stp")) {
+				graphIndex = 3;
 			} else {
 				DebugLib.stopSystemAndReportInconsistency("parking type (Id) unknown: " + parking.getId());
 			}
-			
+
 			int[] occupancy = parkingScoreCollector.parkingOccupancies.get(parking.getId()).getOccupancy();
-			for (int i=0;i<96;i++){
-				matrix[i][graphIndex]+=occupancy[i];
+			for (int i = 0; i < 96; i++) {
+				matrix[i][graphIndex] += occupancy[i];
 			}
 		}
-		
-		String title="ParkingTypeOccupancies";
-		String xLabel="time (15min-bin)";
-		String yLabel="# of occupied parkings";
-		String[] seriesLabels=new String[4];
-		seriesLabels[0]="garageParkings";
-		seriesLabels[1]="privateParkings";
-		seriesLabels[2]="publicParkingsOutsideCityZH";
-		seriesLabels[3]="streetParkings";
-		double[] xValues=new double[96];
-		
-		for (int i=0;i<96;i++){
-			xValues[i]=i/(double)4;
+
+		String title = "ParkingTypeOccupancies";
+		String xLabel = "time (15min-bin)";
+		String yLabel = "# of occupied parkings";
+		String[] seriesLabels = new String[4];
+		seriesLabels[0] = "garageParkings";
+		seriesLabels[1] = "privateParkings";
+		seriesLabels[2] = "publicParkingsOutsideCityZH";
+		seriesLabels[3] = "streetParkings";
+		double[] xValues = new double[96];
+
+		for (int i = 0; i < 96; i++) {
+			xValues[i] = i / (double) 4;
 		}
-		
+
 		GeneralLib.writeGraphic(iterationFilename, matrix, title, xLabel, yLabel, seriesLabels, xValues);
 	}
 
 	private void writeOutParkingOccupancies(Controler controler) {
-		String iterationFilename = controler.getControlerIO().getIterationFilename(controler.getIterationNumber(), "parkingOccupancy.txt");
-		
-		
-		ArrayList<String> list=new ArrayList<String>();
-		
-		for (Id parkingId:parkingScoreCollector.parkingOccupancies.keySet()){
-			Parking parking=parkingManager.getParkingsHashMap().get(parkingId);
-			StringBuffer row = new StringBuffer (parking.getId().toString());
-			
+		String iterationFilename = controler.getControlerIO().getIterationFilename(controler.getIterationNumber(),
+				"parkingOccupancy.txt");
+
+		ArrayList<String> list = new ArrayList<String>();
+
+		for (Id parkingId : parkingScoreCollector.parkingOccupancies.keySet()) {
+			Parking parking = parkingManager.getParkingsHashMap().get(parkingId);
+			StringBuffer row = new StringBuffer(parking.getId().toString());
+
 			ParkingOccupancyBins parkingOccupancyBins = parkingScoreCollector.parkingOccupancies.get(parking.getId());
-			
-			for (int i=0;i<96;i++){
+
+			for (int i = 0; i < 96; i++) {
 				row.append("\t");
-				row.append(parkingOccupancyBins.getOccupancy(i*900));
+				row.append(parkingOccupancyBins.getOccupancy(i * 900));
 			}
-			
+
 			list.add(row.toString());
 		}
-		
+
 		GeneralLib.writeList(list, iterationFilename);
 	}
 
-	private void printWalkingDistanceHistogramm(Controler controler, HashMap<Id, Double> walkingDistance){
-		double[] values=Collections.convertDoubleCollectionToArray(walkingDistance.values());
-		
-		if (values.length==0){
-			values=new double[1];
-			values[0]=-1.0;
+	private void printWalkingDistanceHistogramm(Controler controler, HashMap<Id, Double> walkingDistance) {
+		double[] values = Collections.convertDoubleCollectionToArray(walkingDistance.values());
+
+		if (values.length == 0) {
+			values = new double[1];
+			values[0] = -1.0;
 		}
-		
-		averageWalkingDistance=new Mean().evaluate(values);
-		
+
+		averageWalkingDistance = new Mean().evaluate(values);
+
 		String fileName = controler.getControlerIO().getOutputFilename("walkingDistanceHistogramm.png");
-		
-		GeneralLib.generateHistogram(fileName, values, 10, "Histogram Parking Walking Distance - It."+controler.getIterationNumber(), "distance", "number");
+
+		GeneralLib.generateHistogram(fileName, values, 10,
+				"Histogram Parking Walking Distance - It." + controler.getIterationNumber(), "distance", "number");
 	}
 
 	private void writeWalkingDistanceStatisticsGraph(Controler controler, HashMap<Id, Double> walkingDistance) {
@@ -246,36 +252,35 @@ public class ParkingScoreAccumulator implements AfterMobsimListener {
 	}
 
 	public static void initializeParkingCounts(Controler controler) {
-		String baseFolder=null;
+		String baseFolder = null;
 		Double countsScalingFactor = Double.parseDouble(controler.getConfig().findParam("parking", "countsScalingFactor"));
-		
-		if (ParkingHerbieControler.isRunningOnServer){
+
+		if (ParkingHerbieControler.isRunningOnServer) {
 			baseFolder = "/Network/Servers/kosrae.ethz.ch/Volumes/ivt-home/wrashid/data/experiments/TRBAug2011/parkings/counts/";
 		} else {
 			baseFolder = "H:/data/experiments/TRBAug2011/parkings/counts/";
 		}
 		StringMatrix countsMatrix = GeneralLib.readStringMatrix(baseFolder + "parkingGarageCountsCityZH27-April-2011.txt", "\t");
-		
-		HashMap<String, Double[]> occupancyOfAllSelectedParkings = SingleDayGarageParkingsCount.getOccupancyOfAllSelectedParkings(countsMatrix);
-		
-		selectedParkings=occupancyOfAllSelectedParkings.keySet();
-		
-		sumOfOccupancyCountsOfSelectedParkings=new double[96];
-		
-		for (String parkingName:selectedParkings){
+
+		HashMap<String, Double[]> occupancyOfAllSelectedParkings = SingleDayGarageParkingsCount
+				.getOccupancyOfAllSelectedParkings(countsMatrix);
+
+		selectedParkings = occupancyOfAllSelectedParkings.keySet();
+
+		sumOfOccupancyCountsOfSelectedParkings = new double[96];
+
+		for (String parkingName : selectedParkings) {
 			Double[] occupancyBins = occupancyOfAllSelectedParkings.get(parkingName);
-			
-			if (occupancyBins==null){
+
+			if (occupancyBins == null) {
 				DebugLib.stopSystemAndReportInconsistency();
 			}
-			
-			for (int i=0;i<96;i++){
-				sumOfOccupancyCountsOfSelectedParkings[i]+=countsScalingFactor*occupancyBins[i];
+
+			for (int i = 0; i < 96; i++) {
+				sumOfOccupancyCountsOfSelectedParkings[i] += countsScalingFactor * occupancyBins[i];
 			}
 		}
-		
-	}
 
-	
+	}
 
 }
