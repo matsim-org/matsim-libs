9fd7ff929ace81c36f6f2acc1a25151cf279223e
vsp_nagel <2008@kainagel.org>
2011-07-28 08:25:00 +0000

    added some comments to VehicleType
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@16256 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/vehicles/VehicleType.java
+++ matsim/src/main/java/org/matsim/vehicles/VehicleType.java
@@ -51,16 +51,52 @@ public interface VehicleType {
 	
 	public String getDescription();
 	
+	/**
+	 * Comments:<ul>
+	 * <li> What happens with multi-vehicle trains?  I assume they need to be defined as a single vehicle?  kai, jul'11
+	 * </ul>
+	 */
 	public VehicleCapacity getCapacity();
 	
 	public Id getId();
 	
+	/**
+	 * Comments:<ul>
+	 * <li> In my understanding, ``access'' time is the time to reach the public transit system (e.g. by walking).  
+	 * See, e.g., http://en.wikipedia.org/wiki/Public_Transport_Accessibility_Level .  It is thus not the same as
+	 * the time for one person to enter the vehicle.  Or is there an alternative definition somewhere? kai, jul'11
+	 * <li> Even if this is understood as the time per person to enter the vehicle, it needs to be clear that this is, at
+	 * best, the emtpy vehicle entering time, and there may be additional functions computing longer vehicle entering times 
+	 * when the vehicle is full. kai, jul'11
+	 * <li> Finally, the time needs to be understood as capacitated.  I.e. when there are enough doors so that 9 passengers
+	 * can enter the vehicle per second, then this time should be 1/9=0.111. kai, jul'11
+	 * </ul>
+	 */
+	@Deprecated
 	public double getAccessTime();
 	
+	/**
+	 * See comments under getter.
+	 */
+	@Deprecated
 	public void setAccessTime(double seconds);
 	
+	/**
+	 * Comments:<ul>
+	 * <li> In my understanding, ``egress'' time is either the time from the vehicle to the final destination
+	 * (e.g. http://www.worldtransitresearch.info/research/1507/ ), or 
+	 * the time that is necessary to evacuate the vehicle.  Both definitions are decidedly not the same as the 
+	 * time for one person to leave the vehicle.  kai, jul'11  
+	 * <li> Also see remarks under getAccessTime() .
+	 * </ul>
+	 */
+	@Deprecated
 	public double getEgressTime();
 	
+	/**
+	 * See comments under getter.
+	 */
+	@Deprecated
 	public void setEgressTime(double seconds);
 	
 	public DoorOperationMode getDoorOperationMode();
