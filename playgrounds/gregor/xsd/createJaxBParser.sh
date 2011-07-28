9ff0a6b7b58eff99f37bb236a4aec0d706f0b697
laemmel <gregor.laemmel@gmail.com>
2011-07-28 16:52:04 +0000

    proposal for an Evacuation Domain Language (EDL)
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@16264 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/gregor/xsd/createJaxBParser.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# script to create jaxb bindings for a given schmema and its bindings
+# editor dgrether
+# modifier gregor
+
+function print_help()
+{
+  echo "Wrong number of parameters!"
+  echo "Expected parameters:"
+  echo "  1.) bindings file for the schema"
+  echo "  2.) schema file"
+}
+
+if [ "$#" = 2 ]; then
+command="xjc -d ../src/main/java/  -b $1 gml.jxb xmlschemaBindings.xsd $2"
+echo $command
+$command
+else
+  print_help
+fi
