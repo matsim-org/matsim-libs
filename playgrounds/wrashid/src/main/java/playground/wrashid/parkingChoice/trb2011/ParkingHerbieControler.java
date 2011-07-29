215769dcaafa099c5d90cf1106472d76ab16d3e5
rashid_waraich <rashid_waraich@users.sf.net>
2011-07-29 05:02:33 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@16270 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/main/java/playground/wrashid/parkingChoice/trb2011/ParkingHerbieControler.java
+++ playgrounds/wrashid/src/main/java/playground/wrashid/parkingChoice/trb2011/ParkingHerbieControler.java
@@ -18,207 +18,219 @@ import playground.wrashid.parkingChoice.apiDefImpl.ParkingScoringFunctionZhScena
 import playground.wrashid.parkingChoice.apiDefImpl.PriceAndDistanceParkingSelectionManager;
 import playground.wrashid.parkingChoice.apiDefImpl.ShortestWalkingDistanceParkingSelectionManager;
 import playground.wrashid.parkingChoice.infrastructure.FlatParkingFormatReaderV1;
+import playground.wrashid.parkingChoice.infrastructure.PrivateParking;
 import playground.wrashid.parkingChoice.infrastructure.api.Parking;
 import playground.wrashid.parkingChoice.scoring.ParkingScoreAccumulator;
 
 public class ParkingHerbieControler {
 
-	static String parkingDataBase="H:/data/experiments/TRBAug2011/parkings/flat/";
+	static String parkingDataBase = "H:/data/experiments/TRBAug2011/parkings/flat/";
 	static ParkingModule parkingModule;
-	public static boolean isRunningOnServer=false;
-	public static boolean isKTIMode=true;
-	
+	public static boolean isRunningOnServer = false;
+	public static boolean isKTIMode = true;
+
 	public static void main(String[] args) {
-		Controler controler=null;
-		if (isKTIMode){
-			controler=new KTIControler(args);
+		Controler controler = null;
+		if (isKTIMode) {
+			controler = new KTIControler(args);
 		} else {
-			controler=new HerbieControler(args);
+			controler = new HerbieControler(args);
 		}
-		GeneralLib.controler=controler;
-		
-		
-		parkingModule=new ParkingModule(controler, null);
-		
+		GeneralLib.controler = controler;
+
+		parkingModule = new ParkingModule(controler, null);
+
 		prepareParkingsForScenario(controler);
-		
+
 		controler.setOverwriteFiles(true);
 
 		controler.run();
-		
+
 	}
-	
-	
-
-
 
 	private static void prepareParkingsForScenario(Controler controler) {
 		controler.addControlerListener(new StartupListener() {
-			
+
 			@Override
 			public void notifyStartup(StartupEvent event) {
 				String isRunningOnServer = event.getControler().getConfig().findParam("parking", "isRunningOnServer");
-				if (Boolean.parseBoolean(isRunningOnServer)){
-					parkingDataBase="/Network/Servers/kosrae.ethz.ch/Volumes/ivt-home/wrashid/data/experiments/TRBAug2011/parkings/flat/";
-					ParkingHerbieControler.isRunningOnServer=true;
+				if (Boolean.parseBoolean(isRunningOnServer)) {
+					parkingDataBase = "/Network/Servers/kosrae.ethz.ch/Volumes/ivt-home/wrashid/data/experiments/TRBAug2011/parkings/flat/";
+					ParkingHerbieControler.isRunningOnServer = true;
 				} else {
-					parkingDataBase="H:/data/experiments/TRBAug2011/parkings/flat/";
-					ParkingHerbieControler.isRunningOnServer=false;
+					parkingDataBase = "H:/data/experiments/TRBAug2011/parkings/flat/";
+					ParkingHerbieControler.isRunningOnServer = false;
 				}
-				
+
 				LinkedList<Parking> parkingCollection = getParkingsForScenario(event.getControler());
-				modifyParkingCollectionIfMainExperimentForTRB2011(event.getControler(),parkingCollection);
+				modifyParkingCollectionIfMainExperimentForTRB2011(event.getControler(), parkingCollection);
 				parkingModule.getParkingManager().setParkingCollection(parkingCollection);
-				
+
 				ParkingScoreAccumulator.initializeParkingCounts(event.getControler());
-				
+
 				initParkingSelectionManager(event.getControler());
 			}
 
-			private void modifyParkingCollectionIfMainExperimentForTRB2011(Controler controler, LinkedList<Parking> parkingCollection) {
-				if (controler.getConfig().findParam("parking", "isMainTRB2011Experiment")!=null){
-					LinkedList<Parking> tmpList=new LinkedList<Parking>();
+			private void modifyParkingCollectionIfMainExperimentForTRB2011(Controler controler,
+					LinkedList<Parking> parkingCollection) {
+				if (controler.getConfig().findParam("parking", "isMainTRB2011Experiment") != null) {
+					LinkedList<Parking> tmpList = new LinkedList<Parking>();
 					System.out.println("mainExperimentTRB2011 - initNumberOfParkings:" + parkingCollection.size());
-					double clusterRadius=500.0;
-					for (Parking parking:parkingCollection){
-						Coord clusterCenter1=new CoordImpl(682914.5,247209.3);
-						Coord clusterCenter2=new CoordImpl(684773.4,253849.4);
-						Coord clusterCenter3=new CoordImpl(681902.0,247578.1);
-						Coord clusterCenter4=new CoordImpl(683430.2,247702.8);
-						
-						if (GeneralLib.getDistance(parking.getCoord(), clusterCenter1)<clusterRadius){
-							tmpList.add(parking);
+					double clusterRadius = 500.0;
+					for (Parking parking : parkingCollection) {
+						Coord clusterCenter1 = new CoordImpl(682914.5, 247209.3);
+						Coord clusterCenter2 = new CoordImpl(684773.4, 253849.4);
+						Coord clusterCenter3 = new CoordImpl(681902.0, 247578.1);
+						Coord clusterCenter4 = new CoordImpl(683430.2, 247702.8);
+
+						if (!(parking instanceof PrivateParking)) {
+							if (GeneralLib.getDistance(parking.getCoord(), clusterCenter1) < clusterRadius) {
+								tmpList.add(parking);
+							}
+							if (GeneralLib.getDistance(parking.getCoord(), clusterCenter2) < clusterRadius) {
+								tmpList.add(parking);
+							}
+							if (GeneralLib.getDistance(parking.getCoord(), clusterCenter3) < clusterRadius) {
+								tmpList.add(parking);
+							}
+							if (GeneralLib.getDistance(parking.getCoord(), clusterCenter4) < clusterRadius) {
+								tmpList.add(parking);
+							}
 						}
-						if (GeneralLib.getDistance(parking.getCoord(), clusterCenter2)<clusterRadius){
-							tmpList.add(parking);
-						}
-						if (GeneralLib.getDistance(parking.getCoord(), clusterCenter3)<clusterRadius){
-							tmpList.add(parking);
-						}
-						if (GeneralLib.getDistance(parking.getCoord(), clusterCenter4)<clusterRadius){
-							tmpList.add(parking);
-						}
-						
+
 					}
 					parkingCollection.removeAll(tmpList);
 					System.out.println("mainExperimentTRB2011 - updatedNumberOfParkings:" + parkingCollection.size());
 				}
-				
+
 			}
 
 			private void initParkingSelectionManager(Controler controler) {
 				String parkingSelectionManager = controler.getConfig().findParam("parking", "parkingSelectionManager");
-				if (parkingSelectionManager.equalsIgnoreCase("shortestWalkingDistance")){
-					parkingModule.setParkingSelectionManager(new ShortestWalkingDistanceParkingSelectionManager(parkingModule.getParkingManager()) );
+				if (parkingSelectionManager.equalsIgnoreCase("shortestWalkingDistance")) {
+					parkingModule.setParkingSelectionManager(new ShortestWalkingDistanceParkingSelectionManager(parkingModule
+							.getParkingManager()));
 				} else if (parkingSelectionManager.equalsIgnoreCase("PriceAndDistance_v1")) {
-					parkingModule.setParkingSelectionManager(new PriceAndDistanceParkingSelectionManager(parkingModule.getParkingManager(), new ParkingScoringFunctionZhScenario_v1()));				
-					ParkingScoringFunctionZhScenario_v1.disutilityOfWalkingPerMeterShorterThanhresholdDistance=Double.parseDouble(controler.getConfig().findParam("parking", "disutilityOfWalkingPerMeterShorterThanhresholdDistance"));
-					ParkingScoringFunctionZhScenario_v1.disutilityOfWalkingPerMeterLongerThanThresholdDistance=Double.parseDouble(controler.getConfig().findParam("parking", "disutilityOfWalkingPerMeterLongerThanThresholdDistance"));
-					ParkingScoringFunctionZhScenario_v1.thresholdWalkingDistance=Double.parseDouble(controler.getConfig().findParam("parking", "thresholdWalkingDistance"));
-					ParkingScoringFunctionZhScenario_v1.boardingDurationInSeconds=Double.parseDouble(controler.getConfig().findParam("parking", "boardingDurationInSeconds"));
-					ParkingScoringFunctionZhScenario_v1.streetParkingPricePerSecond=Double.parseDouble(controler.getConfig().findParam("parking", "streetParkingPricePerSecond"));
-					ParkingScoringFunctionZhScenario_v1.garageParkingPricePerSecond=Double.parseDouble(controler.getConfig().findParam("parking", "garageParkingPricePerSecond"));
-					
+					parkingModule.setParkingSelectionManager(new PriceAndDistanceParkingSelectionManager(parkingModule
+							.getParkingManager(), new ParkingScoringFunctionZhScenario_v1()));
+					ParkingScoringFunctionZhScenario_v1.disutilityOfWalkingPerMeterShorterThanhresholdDistance = Double
+							.parseDouble(controler.getConfig().findParam("parking",
+									"disutilityOfWalkingPerMeterShorterThanhresholdDistance"));
+					ParkingScoringFunctionZhScenario_v1.disutilityOfWalkingPerMeterLongerThanThresholdDistance = Double
+							.parseDouble(controler.getConfig().findParam("parking",
+									"disutilityOfWalkingPerMeterLongerThanThresholdDistance"));
+					ParkingScoringFunctionZhScenario_v1.thresholdWalkingDistance = Double.parseDouble(controler.getConfig()
+							.findParam("parking", "thresholdWalkingDistance"));
+					ParkingScoringFunctionZhScenario_v1.boardingDurationInSeconds = Double.parseDouble(controler.getConfig()
+							.findParam("parking", "boardingDurationInSeconds"));
+					ParkingScoringFunctionZhScenario_v1.streetParkingPricePerSecond = Double.parseDouble(controler.getConfig()
+							.findParam("parking", "streetParkingPricePerSecond"));
+					ParkingScoringFunctionZhScenario_v1.garageParkingPricePerSecond = Double.parseDouble(controler.getConfig()
+							.findParam("parking", "garageParkingPricePerSecond"));
+
 				} else {
 					DebugLib.stopSystemAndReportInconsistency("unknown parkingSelectionManager:" + parkingSelectionManager);
 				}
-				
+
 			}
 		});
-		
+
 	}
-	
-	
-
-
 
 	public static LinkedList<Parking> getParkingsForScenario(Controler controler) {
-		double parkingsOutsideZHCityScaling=Double.parseDouble(controler.getConfig().findParam("parking", "publicParkingsCalibrationFactorOutsideZHCity"));
-		
-		LinkedList<Parking> parkingCollection=getParkingCollectionZHCity(controler);
-		String streetParkingsFile=null;
-		if (isKTIMode){
-			streetParkingsFile=parkingDataBase + "publicParkingsOutsideZHCity_v0_dilZh30km_10pct.xml";
+		double parkingsOutsideZHCityScaling = Double.parseDouble(controler.getConfig().findParam("parking",
+				"publicParkingsCalibrationFactorOutsideZHCity"));
+
+		LinkedList<Parking> parkingCollection = getParkingCollectionZHCity(controler);
+		String streetParkingsFile = null;
+		if (isKTIMode) {
+			streetParkingsFile = parkingDataBase + "publicParkingsOutsideZHCity_v0_dilZh30km_10pct.xml";
 		} else {
-			streetParkingsFile=parkingDataBase + "publicParkingsOutsideZHCity_v0.xml";
+			streetParkingsFile = parkingDataBase + "publicParkingsOutsideZHCity_v0.xml";
 		}
-		
-		readParkings(parkingsOutsideZHCityScaling, streetParkingsFile,parkingCollection);
-		
+
+		readParkings(parkingsOutsideZHCityScaling, streetParkingsFile, parkingCollection);
+
 		return parkingCollection;
 	}
 
 	public static LinkedList<Parking> getParkingCollectionZHCity(Controler controler) {
-		double streetParkingCalibrationFactor=Double.parseDouble(controler.getConfig().findParam("parking", "streetParkingCalibrationFactorZHCity"));
-		double garageParkingCalibrationFactor=Double.parseDouble(controler.getConfig().findParam("parking", "garageParkingCalibrationFactorZHCity"));
-		double privateParkingCalibrationFactorZHCity=Double.parseDouble(controler.getConfig().findParam("parking", "privateParkingCalibrationFactorZHCity"));
-		//double privateParkingsOutdoorCalibrationFactor=Double.parseDouble(controler.getConfig().findParam("parking", "privateParkingsOutdoorCalibrationFactorZHCity"));
-		
-		LinkedList<Parking> parkingCollection=new LinkedList<Parking>();
-		
-		String streetParkingsFile=parkingDataBase + "streetParkings.xml";
-		readParkings(streetParkingCalibrationFactor, streetParkingsFile,parkingCollection);
-		
-		String garageParkingsFile=parkingDataBase + "garageParkings.xml";
-		readParkings(garageParkingCalibrationFactor, garageParkingsFile,parkingCollection);
-		
-		String privateIndoorParkingsFile=null;
-		if (isKTIMode){
-			privateIndoorParkingsFile=parkingDataBase + "privateParkings_v1_kti.xml";
+		double streetParkingCalibrationFactor = Double.parseDouble(controler.getConfig().findParam("parking",
+				"streetParkingCalibrationFactorZHCity"));
+		double garageParkingCalibrationFactor = Double.parseDouble(controler.getConfig().findParam("parking",
+				"garageParkingCalibrationFactorZHCity"));
+		double privateParkingCalibrationFactorZHCity = Double.parseDouble(controler.getConfig().findParam("parking",
+				"privateParkingCalibrationFactorZHCity"));
+		// double
+		// privateParkingsOutdoorCalibrationFactor=Double.parseDouble(controler.getConfig().findParam("parking",
+		// "privateParkingsOutdoorCalibrationFactorZHCity"));
+
+		LinkedList<Parking> parkingCollection = new LinkedList<Parking>();
+
+		String streetParkingsFile = parkingDataBase + "streetParkings.xml";
+		readParkings(streetParkingCalibrationFactor, streetParkingsFile, parkingCollection);
+
+		String garageParkingsFile = parkingDataBase + "garageParkings.xml";
+		readParkings(garageParkingCalibrationFactor, garageParkingsFile, parkingCollection);
+
+		String privateIndoorParkingsFile = null;
+		if (isKTIMode) {
+			privateIndoorParkingsFile = parkingDataBase + "privateParkings_v1_kti.xml";
 		} else {
-			privateIndoorParkingsFile=parkingDataBase + "privateParkings_v1.xml";
+			privateIndoorParkingsFile = parkingDataBase + "privateParkings_v1.xml";
 		}
-		
-		readParkings(privateParkingCalibrationFactorZHCity, privateIndoorParkingsFile,parkingCollection);
-		
-		//String privateOutdoorParkingsFile=parkingDataBase + "privateParkingsOutdoor.xml";
-		//readParkings(privateParkingsOutdoorCalibrationFactor, privateOutdoorParkingsFile,parkingCollection);
-		
+
+		readParkings(privateParkingCalibrationFactorZHCity, privateIndoorParkingsFile, parkingCollection);
+
+		// String privateOutdoorParkingsFile=parkingDataBase +
+		// "privateParkingsOutdoor.xml";
+		// readParkings(privateParkingsOutdoorCalibrationFactor,
+		// privateOutdoorParkingsFile,parkingCollection);
+
 		return parkingCollection;
 	}
-	
-	public static LinkedList<Parking> getParkingCollectionZHCity(){
-		LinkedList<Parking> parkingCollection=new LinkedList<Parking>();
-		
-		String streetParkingsFile=parkingDataBase + "streetParkings.xml";
-		readParkings(1.0, streetParkingsFile,parkingCollection);
-		
-		String garageParkingsFile=parkingDataBase + "garageParkings.xml";
-		readParkings(1.0, garageParkingsFile,parkingCollection);
-		
-		String privateIndoorParkingsFile=parkingDataBase + "privateParkingsIndoor_v0.xml";
-		readParkings(1.0, privateIndoorParkingsFile,parkingCollection);
-		
-		String privateOutdoorParkingsFile=parkingDataBase + "privateParkingsOutdoor_v0.xml";
-		readParkings(1.0, privateOutdoorParkingsFile,parkingCollection);
-		
+
+	public static LinkedList<Parking> getParkingCollectionZHCity() {
+		LinkedList<Parking> parkingCollection = new LinkedList<Parking>();
+
+		String streetParkingsFile = parkingDataBase + "streetParkings.xml";
+		readParkings(1.0, streetParkingsFile, parkingCollection);
+
+		String garageParkingsFile = parkingDataBase + "garageParkings.xml";
+		readParkings(1.0, garageParkingsFile, parkingCollection);
+
+		String privateIndoorParkingsFile = parkingDataBase + "privateParkingsIndoor_v0.xml";
+		readParkings(1.0, privateIndoorParkingsFile, parkingCollection);
+
+		String privateOutdoorParkingsFile = parkingDataBase + "privateParkingsOutdoor_v0.xml";
+		readParkings(1.0, privateOutdoorParkingsFile, parkingCollection);
+
 		return parkingCollection;
 	}
 
 	public static void readParkings(double parkingCalibrationFactor, String parkingsFile, LinkedList<Parking> parkingCollection) {
 		FlatParkingFormatReaderV1 flatParkingFormatReaderV1 = new FlatParkingFormatReaderV1();
 		flatParkingFormatReaderV1.parse(parkingsFile);
-		
-		LinkedList<Parking> parkings= flatParkingFormatReaderV1.getParkings();
-		calibarteParkings(parkings,parkingCalibrationFactor);
-		
+
+		LinkedList<Parking> parkings = flatParkingFormatReaderV1.getParkings();
+		calibarteParkings(parkings, parkingCalibrationFactor);
+
 		parkingCollection.addAll(parkings);
 	}
-	
-	private static void calibarteParkings(LinkedList<Parking> parkingCollection, double calibrationFactor){
-		for (Parking parking:parkingCollection){
+
+	private static void calibarteParkings(LinkedList<Parking> parkingCollection, double calibrationFactor) {
+		for (Parking parking : parkingCollection) {
 			double capacity = parking.getCapacity();
-			parking.setCapacity(capacity*calibrationFactor);
+			parking.setCapacity(capacity * calibrationFactor);
 		}
 	}
-	
-	public static Coord getCoordinatesQuaiBridgeZH(){
-		return new CoordImpl(683423.0,246819.0);
+
+	public static Coord getCoordinatesQuaiBridgeZH() {
+		return new CoordImpl(683423.0, 246819.0);
 	}
-	
-	public static Coord getCoordinatesLindenhofZH(){
-		return new CoordImpl(683235.0,247497.0);
+
+	public static Coord getCoordinatesLindenhofZH() {
+		return new CoordImpl(683235.0, 247497.0);
 	}
-	
-	
+
 }
