a349723b62ce7c7f90dc943f4a55313aa38a1622
stellaschieffer <stellaschieffer@users.sf.net>
2011-08-10 19:55:37 +0000

    updating tests sschieffer
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@16403 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/wrashid/src/test/java/playground/wrashid/sschieffer/DecentralizedSmartCharger/LP/DummyLPEVTest.java
@@ -0,0 +1,78 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * LPTest.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.wrashid.sschieffer.DecentralizedSmartCharger.LP;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+import org.apache.commons.math.FunctionEvaluationException;
+import org.apache.commons.math.MaxIterationsExceededException;
+import org.apache.commons.math.analysis.polynomials.PolynomialFunction;
+import org.apache.commons.math.optimization.OptimizationException;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.core.basic.v01.IdImpl;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.events.IterationEndsEvent;
+import org.matsim.core.controler.listener.IterationEndsListener;
+
+import playground.wrashid.PSF.data.HubLinkMapping;
+import playground.wrashid.PSF2.pluggable.energyConsumption.EnergyConsumptionPlugin;
+import playground.wrashid.PSF2.pluggable.parkingTimes.ParkingTimesPlugin;
+import playground.wrashid.PSF2.vehicle.vehicleFleet.ElectricVehicle;
+import playground.wrashid.PSF2.vehicle.vehicleFleet.PlugInHybridElectricVehicle;
+import playground.wrashid.PSF2.vehicle.vehicleFleet.Vehicle;
+import playground.wrashid.lib.EventHandlerAtStartupAdder;
+import playground.wrashid.lib.obj.LinkedListValueHashMap;
+import playground.wrashid.sschieffer.DSC.DecentralizedSmartCharger;
+import playground.wrashid.sschieffer.DSC.LP.LPEV;
+import playground.wrashid.sschieffer.DecentralizedSmartCharger.TestSimulationSetUp;
+import playground.wrashid.sschieffer.SetUp.IntervalScheduleClasses.Schedule;
+import junit.framework.TestCase;
+import lpsolve.LpSolve;
+import lpsolve.LpSolveException;
+
+/**
+ 
+ * @author Stella
+ *
+ */
+public class DummyLPEVTest extends TestCase{
+
+	public static void testMain(String[] args) throws MaxIterationsExceededException, OptimizationException, FunctionEvaluationException, IllegalArgumentException, LpSolveException, IOException {
+		
+	}
+	
+	public void testLPDummy() throws LpSolveException{
+		LPEV l= new LPEV(0.0, true);
+		LpSolve solver = LpSolve.makeLp(0, 1);
+		assertEquals(true, true);
+	}
+
+	
+}
+
+
