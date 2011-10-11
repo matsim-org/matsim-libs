2b04af6e3cfec18431206c7c3df973fabead7d1e
tnicolai79 <tnicolai79@users.sf.net>
2011-10-11 13:16:11 +0000

    moved tests from tnicolai to contrib
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@16886 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ contribs/matsim4urbansim/src/test/java/org/matsim/contrib/matsim4urbansim/matsimTestData/GenerateMATSimConfig.java
@@ -0,0 +1,199 @@
+package org.matsim.contrib.matsim4urbansim.matsimTestData;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.math.BigInteger;
+
+import javax.xml.XMLConstants;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.namespace.QName;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+
+import org.matsim.contrib.matsim4opus.constants.Constants;
+import org.matsim.contrib.matsim4opus.org.matsim.config.ConfigType;
+import org.matsim.contrib.matsim4opus.org.matsim.config.ControlerType;
+import org.matsim.contrib.matsim4opus.org.matsim.config.InputPlansFileType;
+import org.matsim.contrib.matsim4opus.org.matsim.config.Matsim4UrbansimType;
+import org.matsim.contrib.matsim4opus.org.matsim.config.MatsimConfigType;
+import org.matsim.contrib.matsim4opus.org.matsim.config.NetworkType;
+import org.matsim.contrib.matsim4opus.org.matsim.config.ObjectFactory;
+import org.matsim.contrib.matsim4opus.org.matsim.config.PlanCalcScoreType;
+import org.matsim.contrib.matsim4opus.org.matsim.config.UrbansimParameterType;
+import org.matsim.contrib.matsim4opus.utils.io.LoadFile;
+import org.matsim.contrib.matsim4opus.utils.io.TempDirectoryUtil;
+import org.matsim.core.utils.io.IOUtils;
+import org.xml.sax.SAXException;
+
+public class GenerateMATSimConfig {
+	
+	private String destination;
+	private boolean isTestRun;
+	private String network;
+	private String warmStartPlansFile;
+	private String hotStartPlansFile;
+	
+	/**
+	 * Constructor
+	 * @param destination
+	 * @param isTestRun
+	 * @param network
+	 * @param warmStartPlansFile
+	 */
+	public GenerateMATSimConfig(boolean isTestRun, String network, String warmStartPlansFile){
+		this.destination = Constants.MATSIM_4_OPUS_CONFIG + "matsim_config.xml";
+		this.isTestRun = isTestRun;
+		this.network = network;
+		this.warmStartPlansFile = warmStartPlansFile;
+		this.hotStartPlansFile = "";
+	}
+	
+	/**
+	 * Constructor
+	 * @param isTestRun
+	 * @param network
+	 * @param warmStartPlansFile
+	 * @param hotStartPlansFile
+	 */
+	public GenerateMATSimConfig(boolean isTestRun, String network, String warmStartPlansFile, String hotStartPlansFile){
+		this.destination = Constants.MATSIM_4_OPUS_CONFIG + "matsim_config.xml";
+		this.isTestRun = isTestRun;
+		this.network = network;
+		this.warmStartPlansFile = warmStartPlansFile;
+		this.hotStartPlansFile = hotStartPlansFile;
+	}
+	
+	/**
+	 * Constructor
+	 * @param destination
+	 * @param isTestRun
+	 * @param network
+	 */
+	public GenerateMATSimConfig(String destination, boolean isTestRun, String network){
+		this.destination = destination;
+		this.isTestRun = isTestRun;
+		this.network = network;
+		this.warmStartPlansFile = "";
+		this.hotStartPlansFile = "";
+	}
+	
+	/**
+	 * Constructor
+	 * @param isTestRun
+	 * @param network
+	 */
+	public GenerateMATSimConfig(boolean isTestRun, String network){
+		this.destination = Constants.MATSIM_4_OPUS_CONFIG + "matsim_config.xml";
+		this.isTestRun = isTestRun;
+		this.network = network;
+		this.warmStartPlansFile = "";
+		this.hotStartPlansFile = "";
+	}
+	
+	/**
+	 * generates a MATSim config for testing purposes
+	 */
+	public void generate(){
+		
+		BufferedWriter bw = IOUtils.getBufferedWriter( this.destination );
+		ObjectFactory of = new ObjectFactory();	
+		
+		// create "ConfigType"
+		ControlerType controlerType = of.createControlerType();
+		InputPlansFileType inputPlansFileType = of.createInputPlansFileType();
+		InputPlansFileType hotStartPlansFileType = of.createInputPlansFileType();
+		NetworkType networkType = of.createNetworkType();
+		PlanCalcScoreType planCalcSoreType = of.createPlanCalcScoreType();
+		controlerType.setFirstIteration(new BigInteger("0"));
+		controlerType.setLastIteration(new BigInteger("1"));
+		inputPlansFileType.setInputFile( this.warmStartPlansFile );
+		hotStartPlansFileType.setInputFile( this.hotStartPlansFile );
+		networkType.setInputFile( this.network );
+		planCalcSoreType.setActivityType0( "home" );
+		planCalcSoreType.setActivityType1( "work" );
+		
+		ConfigType configType = of.createConfigType();
+		configType.setControler( controlerType );
+		configType.setInputPlansFile( inputPlansFileType );
+		configType.setHotStartPlansFile( hotStartPlansFileType );
+		configType.setNetwork( networkType );
+		configType.setPlanCalcScore( planCalcSoreType );
+		
+		// Create "Matsim4UrbanSimType"
+		UrbansimParameterType urbansimParameterType = of.createUrbansimParameterType();
+		urbansimParameterType.setYear(new BigInteger("2001"));
+		urbansimParameterType.setOpusHome( Constants.OPUS_HOME );
+		urbansimParameterType.setOpusDataPath( Constants.OPUS_DATA_PATH );
+		urbansimParameterType.setMatsim4Opus( Constants.MATSIM_4_OPUS );
+		urbansimParameterType.setMatsim4OpusConfig( Constants.MATSIM_4_OPUS_CONFIG );
+		urbansimParameterType.setMatsim4OpusOutput( Constants.MATSIM_4_OPUS_OUTPUT );
+		urbansimParameterType.setMatsim4OpusTemp( Constants.MATSIM_4_OPUS_TEMP );
+		urbansimParameterType.setSamplingRate( 0.01 );		// just 1% random sample for testing ...
+		urbansimParameterType.setIsTestRun( this.isTestRun );
+		
+		Matsim4UrbansimType matsim4UrbanSimType = of.createMatsim4UrbansimType();
+		matsim4UrbanSimType.setUrbansimParameter(urbansimParameterType);
+		
+		// assign both to "MatsimConfigType"
+		MatsimConfigType matsimConfigType = of.createMatsimConfigType();
+		matsimConfigType.setConfig( configType );
+		matsimConfigType.setMatsim4Urbansim( matsim4UrbanSimType );
+		
+		try {
+			String tempDir = TempDirectoryUtil.createCustomTempDirectory("tmp");
+			// init loadFile object: it downloads a xsd from matsim.org into a temp directory
+			LoadFile loadFile = new LoadFile(Constants.MATSIM_4_URBANSIM_XSD, tempDir , Constants.XSD_FILE_NAME);
+			File file2XSD = loadFile.loadMATSim4UrbanSimXSD(); // trigger loadFile
+			if(file2XSD == null || !file2XSD.exists()){
+				System.err.println("Did not find xml schema!");
+				System.exit(1);
+			}
+			System.out.println("Using following xsd schema: " + file2XSD.getCanonicalPath());
+			
+			// crate a schema factory ...
+			SchemaFactory schemaFactory = SchemaFactory.newInstance( XMLConstants.W3C_XML_SCHEMA_NS_URI );
+			// create a schema object via the given xsd to validate the MATSim xml config.
+			Schema schema = schemaFactory.newSchema(file2XSD);
+			
+			JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
+			Marshaller m = jaxbContext.createMarshaller();
+			m.setSchema(schema);
+			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+			JAXBElement elem = new JAXBElement( new QName("","matsim_config"), MatsimConfigType.class, matsimConfigType); // this is because there is no XMLRootElemet annotation
+			m.marshal(elem, bw );
+			
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String getMATSimConfigPath(){
+		return this.destination;
+	}
+	
+	/**
+	 * Test GenerateMATSimConfig
+	 * @param args
+	 */
+	public static void main(String args[]){
+		
+		TempDirectoryUtil.createOPUSDirectories();
+		String destination = TempDirectoryUtil.createCustomTempDirectory( "jaxbOutput" );
+		
+		GenerateMATSimConfig g = new GenerateMATSimConfig(destination + "/test", true, "");
+		
+		g.generate();
+		
+		TempDirectoryUtil.cleaningUpCustomTempDirectories();
+		TempDirectoryUtil.cleaningUpOPUSDirectories();
+	}
+
+}
---
+++ contribs/matsim4urbansim/src/test/java/org/matsim/contrib/matsim4urbansim/matsimTestData/GenerateOPUSTestEnvironment.java
@@ -0,0 +1,127 @@
+package org.matsim.contrib.matsim4urbansim.matsimTestData;
+
+import org.apache.log4j.Logger;
+import org.matsim.contrib.matsim4opus.constants.Constants;
+import org.matsim.contrib.matsim4opus.utils.UtilityCollection;
+import org.matsim.contrib.matsim4opus.utils.io.FileCopy;
+import org.matsim.contrib.matsim4opus.utils.io.TempDirectoryUtil;
+
+public class GenerateOPUSTestEnvironment {
+	
+	// Logger
+	private static final Logger log = Logger.getLogger(GenerateOPUSTestEnvironment.class);
+	
+	private boolean isTestRun;
+	
+	/**
+	 * Constructor
+	 * @param isTestRun
+	 */
+	public GenerateOPUSTestEnvironment(boolean isTestRun){
+		this.isTestRun = isTestRun;
+	}
+	
+	/**
+	 * Constructor
+	 */
+	public GenerateOPUSTestEnvironment(){
+		this.isTestRun = Boolean.FALSE;
+	}
+	
+	/**
+	 * creates a default opus environment for testing purposes 
+	 */
+	public String createOPUSTestEnvironment(){
+		
+		// creates temporary opus directories
+		TempDirectoryUtil.createOPUSDirectories();
+		
+		// copying matsim input data into fresh created opus directories
+		String urbanSimOutputDir = UtilityCollection.getTestUrbanSimInputDataDir( GenerateOPUSTestEnvironment.class );
+		String matsimNetwork = UtilityCollection.getWarmStartNetwork( GenerateOPUSTestEnvironment.class );
+
+		// copy UrbanSim data to temp dir
+		copyTestData(urbanSimOutputDir, Constants.MATSIM_4_OPUS_TEMP);
+		// copy MATSim network to temp dir
+		copyTestData(matsimNetwork, Constants.MATSIM_4_OPUS_TEMP);
+		
+		return generateMATSimConfig ( matsimNetwork, "", MATSimRunMode.coldStart );
+	}
+	
+	/**
+	 * creates a opus environment for warm start testing
+	 */
+	public String createWarmStartOPUSTestEnvironment(String warmStartPlansFile, byte runMode){
+		
+		// creates temporary opus directories
+		TempDirectoryUtil.createOPUSDirectories();
+		
+		// copying matsim input data into fresh created opus directories
+		String urbanSimOutputDir = UtilityCollection.getWarmStartUrbanSimInputData( GenerateOPUSTestEnvironment.class );
+		String plansFileDir = UtilityCollection.getWarmStartInputPlansFile( GenerateOPUSTestEnvironment.class );
+		String matsimNetwork = UtilityCollection.getWarmStartNetwork( GenerateOPUSTestEnvironment.class );
+
+		// copy UrbanSim data to temp dir
+		copyTestData(urbanSimOutputDir, Constants.MATSIM_4_OPUS_TEMP);
+		// copy plans files to temp dir
+		copyTestData(plansFileDir, Constants.MATSIM_4_OPUS_TEMP);
+		// copy MATSim network to temp dir
+		copyTestData(matsimNetwork, Constants.MATSIM_4_OPUS_TEMP);
+		
+		return generateMATSimConfig ( matsimNetwork, plansFileDir + warmStartPlansFile, runMode);
+	}
+
+	/**
+	 * @param matsimNetwork
+	 * @return path to generated MATSim config
+	 */
+	private String generateMATSimConfig(String matsimNetwork, String plansFile, byte runMode) {
+		// generate MATSim config
+		GenerateMATSimConfig gmc;
+
+		if(runMode == MATSimRunMode.warmStart)
+			gmc = new GenerateMATSimConfig(this.isTestRun, matsimNetwork + "/psrc.xml.gz", plansFile);
+		else if(runMode ==MATSimRunMode.hotStart)
+			gmc = new GenerateMATSimConfig(this.isTestRun, matsimNetwork + "/psrc.xml.gz", "", plansFile);
+		else // coldStart
+			gmc = new GenerateMATSimConfig(isTestRun, matsimNetwork + "/psrc.xml.gz");
+		
+		gmc.generate();
+		
+		log.info("MATSim config file is located at: " + gmc.getMATSimConfigPath());
+		
+		return gmc.getMATSimConfigPath(); // returns the path to MATSim config
+	}
+
+	/**
+	 * @param source
+	 */
+	private void copyTestData(String source, String destination) {
+		
+		log.info("Copying UrbanSim data from " + source + " to " + destination);
+		if( !FileCopy.copyTree(source, destination) ){
+			log.error("Error while copying matsim4opus test data.");
+			System.exit(-1);
+		}
+	}
+	
+	/**
+	 * clean up temporary test files 
+	 */
+	public void cleanOPUSTestEnvironment(){
+		TempDirectoryUtil.cleaningUpOPUSDirectories();
+	}
+		
+	/**
+	 * Test GenerateOPUSTestEnvironment
+	 * @param args
+	 */
+	public static void main (String args[]){
+		log.info("Starting test ...");
+		GenerateOPUSTestEnvironment gote = new GenerateOPUSTestEnvironment();
+		gote.createOPUSTestEnvironment();
+		gote.cleanOPUSTestEnvironment();
+		log.info("... done!");
+	}
+
+}
