6e604b839be7311367fa5b9c571a7320a7d0387c
tnicolai79 <tnicolai79@users.sf.net>
2011-10-27 16:10:50 +0000

    removing matsim4urbansim
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@17084 a0517364-e873-4167-b1c8-416daa5a6b91

--- contribs/matsim4urbansim/src/test/java/org/matsim/contrib/matsim4urbansim/matsimTestData/MATSimRunMode.java
+++
@@ -1,8 +0,0 @@
-package org.matsim.contrib.matsim4urbansim.matsimTestData;
-
-public class MATSimRunMode {
-	
-	public final static byte coldStart = 0;
-	public final static byte warmStart = 1;
-	public final static byte hotStart  = 2;
-}
