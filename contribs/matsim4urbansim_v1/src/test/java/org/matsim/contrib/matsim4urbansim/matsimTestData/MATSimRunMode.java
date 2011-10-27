118ee2917a87aa991c672f6fe0e91b7a14367b6f
tnicolai79 <tnicolai79@users.sf.net>
2011-10-27 16:22:34 +0000

    committing matsim4urbansim_v1
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@17085 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ contribs/matsim4urbansim_v1/src/test/java/org/matsim/contrib/matsim4urbansim/matsimTestData/MATSimRunMode.java
@@ -0,0 +1,8 @@
+package org.matsim.contrib.matsim4urbansim.matsimTestData;
+
+public class MATSimRunMode {
+	
+	public final static byte coldStart = 0;
+	public final static byte warmStart = 1;
+	public final static byte hotStart  = 2;
+}
