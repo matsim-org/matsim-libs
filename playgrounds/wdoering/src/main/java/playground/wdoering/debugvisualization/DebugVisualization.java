028e1dd36aec3b87ee389b3af1230d9749332b8f
wdoering <wdoering@users.sf.net>
2011-10-28 15:59:48 +0000

    -changed entire start up structure (network file incl nodes and links now via scenario)
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@17127 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wdoering/src/main/java/playground/wdoering/debugvisualization/DebugVisualization.java
+++ playgrounds/wdoering/src/main/java/playground/wdoering/debugvisualization/DebugVisualization.java
@@ -19,6 +19,14 @@
 
 package playground.wdoering.debugvisualization;
 
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.config.Config;
+import org.matsim.core.events.EventsUtils;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.misc.ConfigUtils;
+
+import playground.gregor.sim2d_v2.events.XYVxVyEventsFileReader;
 import playground.wdoering.debugvisualization.controller.Console;
 import playground.wdoering.debugvisualization.controller.ConsoleImpl;
 import playground.wdoering.debugvisualization.controller.Controller;
@@ -39,27 +47,27 @@ public class DebugVisualization {
 
 		//console interface for status and debug tracking
 		Console console = new ConsoleImpl(false);
-//
-//		Config c = ConfigUtils.createConfig();
-//
-//		c.network().setInputFile(args[1]);
-//
-//		Scenario sc = ScenarioUtils.loadScenario(c);
-//		//sc.getNetwork().get -> nur über Links (hat from / to nodes (getcoord (get x y)))
-//
-//		EventsManager e = EventsUtils.createEventsManager();
-//
-
-
+		
+		Config c = ConfigUtils.createConfig();
+		
+		c.network().setInputFile(args[1]);
+		
+		Scenario sc = ScenarioUtils.loadScenario(c);
+		//sc.getNetwork().get -> nur über Links (hat from / to nodes (getcoord (get x y)))
+		
+		EventsManager e = EventsUtils.createEventsManager();
+		
+		
+		
 		//argument syntax: DebugSim.java eventfile.xml networkfile.xml liveMode [=true / false / null||else(=false) ]
 		if (args.length > 0)
 		{
 			//console.println("Initializing Debug Simulation.");
-			Controller controller = new Controller(args[0], args[1], console, 3, true);
-			//Controller controller = new Controller(e, sc);
+			//Controller controller = new Controller(args[0], args[1], console, 3, true);
+			Controller controller = new Controller(e, sc, console);
 
-			//XYVxVyEventsFileReader reader = new XYVxVyEventsFileReader(e);
-			//reader.parse(args[0]);
+			XYVxVyEventsFileReader reader = new XYVxVyEventsFileReader(e);
+			reader.parse(args[0]);
 
 		}
 		else
