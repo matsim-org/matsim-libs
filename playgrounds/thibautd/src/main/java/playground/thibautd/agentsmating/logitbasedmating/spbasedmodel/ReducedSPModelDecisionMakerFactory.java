a64e4ae59dca1dd39a0c17556b73c220d1184788
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2011-10-28 09:21:24 +0000

    implemented
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@17093 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/java/playground/thibautd/agentsmating/logitbasedmating/spbasedmodel/ReducedSPModelDecisionMakerFactory.java
+++ playgrounds/thibautd/src/main/java/playground/thibautd/agentsmating/logitbasedmating/spbasedmodel/ReducedSPModelDecisionMakerFactory.java
@@ -19,8 +19,13 @@
  * *********************************************************************** */
 package playground.thibautd.agentsmating.logitbasedmating.spbasedmodel;
 
-import org.matsim.api.core.v01.population.Person;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.core.population.PersonImpl;
+
+import playground.thibautd.agentsmating.logitbasedmating.basic.DecisionMakerImpl;
 import playground.thibautd.agentsmating.logitbasedmating.framework.DecisionMaker;
 import playground.thibautd.agentsmating.logitbasedmating.framework.DecisionMakerFactory;
 
@@ -30,9 +35,41 @@ import playground.thibautd.agentsmating.logitbasedmating.framework.DecisionMaker
 public class ReducedSPModelDecisionMakerFactory implements DecisionMakerFactory {
 
 	@Override
-	public DecisionMaker createDecisionMaker(final Person agent) {
-		// TODO Auto-generated method stub
-		return null;
+	public DecisionMaker createDecisionMaker(final Person agent) throws UnelectableAgentException {
+		PersonImpl pImpl;
+
+		try {
+			pImpl = (PersonImpl) agent;
+		} catch (ClassCastException e) {
+			throw new RuntimeException("can only handle PersonImpl agents", e);
+		};
+
+		return createDecisionMaker( pImpl );
+	}
+
+	public DecisionMaker createDecisionMaker(final PersonImpl agent) throws UnelectableAgentException {
+		checkEligible( agent );
+
+		Map<String, Object> attributes = new HashMap<String, Object>();
+
+		attributes.put( ReducedSPModel.A_AGE , agent.getAge() );
+		attributes.put( ReducedSPModel.A_IS_MALE , agent.getSex().matches("m.*") );
+		// idealy, only General Abonnements should be counted.
+		// but in the population, the only card type available is "unknown"...
+		attributes.put( ReducedSPModel.A_HAS_PT_ABO , agent.getTravelcards().size() > 0 );
+		attributes.put( ReducedSPModel.A_SPEAKS_GERMAN , true );
+		attributes.put( ReducedSPModel.A_IS_CAR_ALWAYS_AVAIL , agent.getCarAvail().equals("always") );
+
+		return new DecisionMakerImpl( agent.getId() , attributes );
+	}
+
+	private void checkEligible(final PersonImpl agent) throws UnelectableAgentException {
+		if ( !agent.hasLicense() ) {
+			throw new UnelectableAgentException("agent has no driving license");
+		}
+		if ( agent.getCarAvail().equals("never") ) {
+			throw new UnelectableAgentException("agent has a driving license but no car");
+		}
 	}
 }
 
