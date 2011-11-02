d168720784f970866458ca3af7d5346a409a9c9c
tnicolai79 <tnicolai79@users.sf.net>
2011-11-02 16:37:35 +0000

    implementing accessibility interpolation
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@17171 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/tnicolai/src/main/java/playground/tnicolai/matsim4opus/utils/helperObjects/SquareLayer.java
@@ -0,0 +1,33 @@
+package playground.tnicolai.matsim4opus.utils.helperObjects;
+
+import java.util.ArrayList;
+
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.network.Node;
+
+public class SquareLayer {
+
+	/** fields regarding square centroid (Layer1) */
+	private Coord squareCentroid = null;
+	private Node squareCentroidNode = null;
+	private double squareCentroidAccessibilityValue = 0.;
+	
+	/** fields regarding square interpolation (Layer2) */
+	private ArrayList<Node> squareInterpolationNodeList = null;
+	private double squareInterpolationAccessibilityValue = 0.;
+	
+	/** fields regarding derivation between Layer 1 and 2 (Layer3) */
+	private double squareDerivation = 0.;
+	private double targetTolerance = 0.;
+	
+	public void setSquareCentroid(Coord centroidCoord, Node nearestNode){
+		this.squareCentroid = centroidCoord;
+		this.squareCentroidNode = nearestNode;
+	}
+	
+	public void addNode(Node node){
+		if(this.squareInterpolationNodeList == null)
+			this.squareInterpolationNodeList = new ArrayList<Node>();
+		this.squareInterpolationNodeList.add( node );
+	}
+}
