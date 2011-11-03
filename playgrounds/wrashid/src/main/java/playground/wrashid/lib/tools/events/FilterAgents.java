44a9e2b2c1c5f4558aa67c03c668382a12f9742e
rashid_waraich <rashid_waraich@users.sf.net>
2011-11-03 13:01:02 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@17193 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/main/java/playground/wrashid/lib/tools/events/FilterAgents.java
+++ playgrounds/wrashid/src/main/java/playground/wrashid/lib/tools/events/FilterAgents.java
@@ -13,14 +13,32 @@ import org.matsim.core.api.experimental.events.AgentWait2LinkEvent;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.api.experimental.events.LinkEnterEvent;
 import org.matsim.core.api.experimental.events.LinkLeaveEvent;
+import org.matsim.core.basic.v01.IdImpl;
 import org.matsim.core.events.EventsReaderTXTv1;
+import org.matsim.core.events.EventsReaderXMLv1;
 import org.matsim.core.events.EventsUtils;
 import org.matsim.core.events.algorithms.EventWriterTXT;
+import org.matsim.core.events.algorithms.EventWriterXML;
 
 public class FilterAgents {
 
 	public static void main(String[] args) {
-
+		String inputEventsFile="H:/data/experiments/TRBAug2011/runs/ktiRun22/output/ITERS/it.50/50.events.xml.gz";
+		
+		String outputEventsFile="c:/tmp/output-events.xml.gz";
+		
+		HashSet<Id> agentIds = new HashSet<Id>();
+		
+		agentIds.add(new IdImpl("7223866"));
+		agentIds.add(new IdImpl("2781839"));
+		agentIds.add(new IdImpl("3220050"));
+		agentIds.add(new IdImpl("3463142"));
+		agentIds.add(new IdImpl("3702380"));
+		agentIds.add(new IdImpl("1691781"));
+		agentIds.add(new IdImpl("5023315"));
+		
+		
+		FilterAgents.keepAgents(agentIds, inputEventsFile, outputEventsFile);
 	}
 
 	public static void keepAgents(HashSet<Id> agentIds, String inputEventsFilePath, String outputEventsFilePath) {
@@ -43,14 +61,18 @@ public class FilterAgents {
 
 		events.addHandler(eventsFilter);
 
-		EventsReaderTXTv1 reader = new EventsReaderTXTv1(events);
-
-		reader.readFile(inputEventsFilePath);
+		// TODO: make switch between txt/xml general.
+		//EventsReaderTXTv1 reader = new EventsReaderTXTv1(events);
+		//reader.readFile(inputEventsFilePath);
+		EventsReaderXMLv1 reader = new EventsReaderXMLv1(events);
+		reader.parse(inputEventsFilePath);
 
 		eventsFilter.closeFile();
 	}
 
-	private static class EventsFilter extends EventWriterTXT {
+	// TODO: make switch between txt/xml general.
+	//private static class EventsFilter extends EventWriterTXT {
+	private static class EventsFilter extends EventWriterXML {
 		private HashSet<Id> filterPersonsSet;
 		boolean keepAgentsInFilter;
 
