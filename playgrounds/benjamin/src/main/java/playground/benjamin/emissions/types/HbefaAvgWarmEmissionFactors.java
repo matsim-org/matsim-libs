44dd3593bb13afe8dbd89c8cc8427e62af7703d4
benkick <benkick@users.sf.net>
2011-11-16 18:43:24 +0000

    major refactoring emission tool part 2; introduced database like structure for warm emission factor parsing
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@17375 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/benjamin/src/main/java/playground/benjamin/emissions/types/HbefaAvgWarmEmissionFactors.java
@@ -0,0 +1,49 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * FhMain.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2009 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.benjamin.emissions.types;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class HbefaAvgWarmEmissionFactors {
+
+	private double speed;
+	private final Map<WarmPollutant, Double> emissionFactors = new HashMap<WarmPollutant, Double>();
+
+	public HbefaAvgWarmEmissionFactors(){
+	}
+
+	public double getSpeed() {
+		return speed;
+	}
+	
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+	
+	public double getEmissionFactor(WarmPollutant warmPollutant) {
+		return this.emissionFactors.get(warmPollutant);
+	}
+	
+	public void setEmissionFactor(WarmPollutant warmPollutant, double emissionFactor) {
+		this.emissionFactors.put(warmPollutant, emissionFactor);
+	}
+}
