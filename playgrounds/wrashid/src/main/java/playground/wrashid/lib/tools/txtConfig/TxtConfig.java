ac43056b0781a912aa46603323fd42cf49846c25
rashid_waraich <rashid_waraich@users.sf.net>
2011-11-16 13:30:20 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@17354 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/main/java/playground/wrashid/lib/tools/txtConfig/TxtConfig.java
+++ playgrounds/wrashid/src/main/java/playground/wrashid/lib/tools/txtConfig/TxtConfig.java
@@ -4,22 +4,47 @@ import java.util.HashMap;
 
 import playground.wrashid.lib.GeneralLib;
 import playground.wrashid.lib.obj.StringMatrix;
-
+/**
+ * usage: put tab separated key, value pairs into a file and read them afterwards.
+ * 
+ * keys can be used in other keys in the form of #key# and will be substituted, up to level 2.
+ * 
+ * 
+ * 
+ * @author wrashid
+ *
+ */
 public class TxtConfig {
 
 	HashMap<String, String> parameterValues;
-	
-	public TxtConfig(String fileName){
-		parameterValues=new HashMap<String, String>();
+
+	public TxtConfig(String fileName) {
+		parameterValues = new HashMap<String, String>();
 		StringMatrix stringMatrix = GeneralLib.readStringMatrix(fileName, "\t");
-		
-		for (int i=0;i<stringMatrix.getNumberOfRows();i++){
+
+		for (int i = 0; i < stringMatrix.getNumberOfRows(); i++) {
 			parameterValues.put(stringMatrix.getString(i, 0), stringMatrix.getString(i, 1));
 		}
+
+		processSubstituions();
+		processSubstituions(); // allow several levels of substitutions
 	}
-	
-	public String getParameterValue(String key){
+
+	private void processSubstituions() {
+		for (String mainKey : parameterValues.keySet()) {
+			String value = parameterValues.get(mainKey);
+
+			
+			for (String substituionKey : parameterValues.keySet()) {
+				value=value.replaceAll("#" + substituionKey + "#", parameterValues.get(substituionKey));
+			}
+			
+			parameterValues.put(mainKey,value);
+		}
+	}
+
+	public String getParameterValue(String key) {
 		return parameterValues.get(key);
 	}
-	
+
 }
