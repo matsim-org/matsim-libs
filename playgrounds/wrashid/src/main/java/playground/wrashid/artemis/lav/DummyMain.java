67754c3218260cccedfc3758f1bc07d6d7411005
rashid_waraich <rashid_waraich@users.sf.net>
2011-11-16 09:56:32 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@17347 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/main/java/playground/wrashid/artemis/lav/DummyMain.java
+++ playgrounds/wrashid/src/main/java/playground/wrashid/artemis/lav/DummyMain.java
@@ -5,7 +5,7 @@ import playground.wrashid.lib.tools.txtConfig.TxtConfig;
 public class DummyMain {
 
 	public static void main(String[] args) {
-		TxtConfig config=new TxtConfig(args[1]);
+		TxtConfig config=new TxtConfig(args[0]);
 		System.out.println(config.getParameterValue("test"));
 	}
 	
