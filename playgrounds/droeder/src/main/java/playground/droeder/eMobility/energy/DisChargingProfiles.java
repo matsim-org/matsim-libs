88aaf5b831accbe5d4496c3998802ba864020f39
dan-ro <daniel_roeder@web.de>
2011-12-07 13:51:15 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@17647 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/droeder/src/main/java/playground/droeder/eMobility/energy/DisChargingProfiles.java
@@ -0,0 +1,83 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * Plansgenerator.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.droeder.eMobility.energy;
+
+import java.util.HashMap;
+import java.util.Set;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.core.basic.v01.IdImpl;
+
+import playground.droeder.DaFileReader;
+
+/**
+ * @author droeder
+ *
+ */
+public class DisChargingProfiles {
+
+	private HashMap<Id, DisChargingProfile> profiles;
+
+	public DisChargingProfiles(){
+		this.profiles = new HashMap<Id , DisChargingProfile>();
+	}
+	
+	public void addValue(Id id, Double slope, Double speed, double newState){
+		if(!this.profiles.containsKey(id)){
+			this.profiles.put(id, new DisChargingProfile(id));
+		}
+		this.profiles.get(id).addNewEntry(slope, speed, newState);
+	}
+	
+	public Double getJoulePerKm(Id id, Double  speed, Double slope){
+		return this.profiles.get(id).getNewState(speed, slope);
+	}
+	
+	//TODO probably own reader
+	public void readAndAddDataFromFile(String inputFile){
+		Set<String[]> values = DaFileReader.readFileContent(inputFile, "\t", true);
+		
+		for(String[] s: values){
+			this.addValue(new IdImpl(s[0]), Double.parseDouble(s[2]), Double.parseDouble(s[1]), Double.parseDouble(s[3]));
+		}
+	}
+	
+	@Override
+	public String toString(){
+		StringBuffer b = new StringBuffer();
+		
+		for(DisChargingProfile p: this.profiles.values()){
+			b.append(p.toString());
+			b.append("\n");
+		}
+		
+		return b.toString();
+	}
+	
+	public static void main(String[] args){
+		String inputFile =  "C:/Users/Daniel/Desktop/Dokumente_MATSim_AP1und2/DrivingLookupTable_2011-11-25.txt";
+		DisChargingProfiles profiles = new DisChargingProfiles();
+		profiles.readAndAddDataFromFile(inputFile);
+		
+		System.out.println(profiles.toString());
+		System.out.println(profiles.getJoulePerKm(new IdImpl("LOW"), 21.773336, 22.));
+		System.out.println(profiles.getJoulePerKm(new IdImpl("LOW"), 21.773336, 20.));
+	}
+}
