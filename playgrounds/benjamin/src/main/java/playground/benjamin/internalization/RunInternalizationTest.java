5d8ed6f29ddc72b3d43c5f9a46d5de305a8178a1
benkick <benkick@users.sf.net>
2011-12-09 17:39:52 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@17695 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/benjamin/src/main/java/playground/benjamin/internalization/RunInternalizationTest.java
+++ playgrounds/benjamin/src/main/java/playground/benjamin/internalization/RunInternalizationTest.java
@@ -19,7 +19,29 @@
  * *********************************************************************** */
 package playground.benjamin.internalization;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.network.Node;
+import org.matsim.core.basic.v01.IdImpl;
 import org.matsim.core.config.Config;
+import org.matsim.core.config.groups.ControlerConfigGroup;
+import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
+import org.matsim.core.config.groups.QSimConfigGroup;
+import org.matsim.core.config.groups.StrategyConfigGroup;
+import org.matsim.core.config.groups.ControlerConfigGroup.EventsFileFormat;
+import org.matsim.core.config.groups.ControlerConfigGroup.RoutingAlgorithmType;
+import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ActivityParams;
+import org.matsim.core.config.groups.StrategyConfigGroup.StrategySettings;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.network.NetworkImpl;
+import org.matsim.core.population.ActivityImpl;
+import org.matsim.core.population.PersonImpl;
+import org.matsim.core.population.PlanImpl;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.geometry.CoordImpl;
 
 /**
  * @author benjamin
@@ -31,18 +53,150 @@ public class RunInternalizationTest {
 
 	private Config config;
 
-	private void run() {
-		
-		setUpTest();
-		
-	}
+	private Scenario scenario;
+	
+	private Controler controler;
 
-	private void setUpTest() {
+	private void run() {
 		
 		this.config = new Config();
 		this.config.addCoreModules();
 		this.config.controler().setOutputDirectory(this.outputDirectory);
+
+		this.scenario = ScenarioUtils.createScenario(this.config);
 		
+		createNetwork();
+		
+		createActiveAgents();
+		
+		createPassiveAgents();
+		
+		this.controler = new Controler(this.scenario);
+
+		specifyControler();
+		
+		this.controler.run();
+	}
+
+	private void specifyControler() {
+		// controler settings	
+		controler.setOverwriteFiles(true);
+		controler.setCreateGraphs(false);
+		
+	// controlerConfigGroup
+		ControlerConfigGroup ccg = controler.getConfig().controler();
+		ccg.setOutputDirectory(outputDirectory);
+		ccg.setFirstIteration(0);
+		ccg.setLastIteration(0);
+		ccg.setMobsim("qsim");
+		Set set = new HashSet();
+		set.add(EventsFileFormat.xml);
+		ccg.setEventsFileFormats(set);
+//		ccg.setRunId("321");
+		ccg.setRoutingAlgorithmType(RoutingAlgorithmType.Dijkstra);
+		
+	// qsimConfigGroup
+		QSimConfigGroup qcg = new QSimConfigGroup();
+		qcg.setStartTime(0 * 3600.);
+		qcg.setEndTime(24 * 3600.);
+		qcg.setFlowCapFactor(1);
+		qcg.setStorageCapFactor(1);
+		qcg.setNumberOfThreads(1);
+		qcg.setRemoveStuckVehicles(false);
+		controler.getConfig().addQSimConfigGroup(qcg);
+		
+	// planCalcScoreConfigGroup
+		PlanCalcScoreConfigGroup pcs = controler.getConfig().planCalcScore();
+			
+		ActivityParams act1Params = new ActivityParams("home");
+		act1Params.setTypicalDuration(16 * 3600);
+		pcs.addActivityParams(act1Params);
+		
+		ActivityParams act2Params = new ActivityParams("work");
+		act2Params.setTypicalDuration(8 * 3600);
+		pcs.addActivityParams(act2Params);
+
+	// strategy
+		StrategyConfigGroup scg = controler.getConfig().strategy();
+		
+		StrategySettings changePlan = new StrategySettings(new IdImpl("1"));
+		changePlan.setModuleName("BestScore");
+		changePlan.setProbability(0.7);
+		
+		StrategySettings reRoute = new StrategySettings(new IdImpl("2"));
+		reRoute.setModuleName("ReRoute");
+		reRoute.setProbability(0.3);
+		
+		scg.addStrategySettings(changePlan);
+		scg.addStrategySettings(reRoute);
+		
+//	// define emission tool input files	
+//		VspExperimentalConfigGroup vcg = controler.getConfig().vspExperimental() ;
+//		vcg.setEmissionRoadTypeMappingFile(roadTypeMappingFile);
+//		vcg.setEmissionVehicleFile(emissionVehicleFile);
+//		
+//		vcg.setAverageWarmEmissionFactorsFile(averageFleetWarmEmissionFactorsFile);
+//		vcg.setAverageColdEmissionFactorsFile(averageFleetColdEmissionFactorsFile);
+//		
+//		vcg.setIsUsingDetailedEmissionCalculation(isUsingDetailedEmissionCalculation);
+//		vcg.setDetailedWarmEmissionFactorsFile(detailedWarmEmissionFactorsFile);
+//		vcg.setDetailedColdEmissionFactorsFile(detailedColdEmissionFactorsFile);
+//		
+//	// TODO: the following does not work yet. Need to force controler to always write events in the last iteration.
+//		vcg.setWritingOutputEvents(false) ;
+	}
+
+	private void createPassiveAgents() {
+		for(int i=0; i<10; i++){
+			PersonImpl person = new PersonImpl (new IdImpl(i));
+			PlanImpl plan = person.createAndAddPlan(true);
+			plan.createAndAddActivity("home", new IdImpl("11"));
+			
+			this.scenario.getPopulation().addPerson(person);
+		}
+	}
+
+	private void createActiveAgents() {
+		PersonImpl person = new PersonImpl (new IdImpl("worker"));
+		PlanImpl plan = person.createAndAddPlan(true);
+		
+		ActivityImpl home = plan.createAndAddActivity("home", new IdImpl("1"));
+		home.setEndTime(6 * 3600);
+		plan.createAndAddLeg(TransportMode.car);
+		
+		ActivityImpl work = plan.createAndAddActivity("work", new IdImpl("4"));
+		work.setEndTime(home.getEndTime() + 600 + 8 * 3600);
+		plan.createAndAddLeg(TransportMode.car);
+		
+		plan.createAndAddActivity("home", new IdImpl("1"));
+		
+		this.scenario.getPopulation().addPerson(person);
+	}
+
+	private void createNetwork() {
+		NetworkImpl network = (NetworkImpl) this.scenario.getNetwork();
+		
+		Node node1 = network.createAndAddNode(new IdImpl("1"), new CoordImpl(-20000.0,     0.0));
+        Node node2 = network.createAndAddNode(new IdImpl("2"), new CoordImpl(-17500.0,     0.0));
+        Node node3 = network.createAndAddNode(new IdImpl("3"), new CoordImpl(-10000.0,     0.0));
+        Node node4 = network.createAndAddNode(new IdImpl("4"), new CoordImpl( -5000.0,     0.0));
+        Node node5 = network.createAndAddNode(new IdImpl("5"), new CoordImpl(     0.0,     0.0));
+        Node node6 = network.createAndAddNode(new IdImpl("6"), new CoordImpl( -5000.0, -8660.0));
+        Node node7 = network.createAndAddNode(new IdImpl("7"), new CoordImpl(-15000.0, -8660.0));
+        Node node8 = network.createAndAddNode(new IdImpl("8"), new CoordImpl( -7500.0,  2500.0));
+        Node node9 = network.createAndAddNode(new IdImpl("9"), new CoordImpl( -7500.0, -2500.0));
+        
+        network.createAndAddLink(new IdImpl("1"), node1, node2, 2500, 27.78, 3600, 1);
+        network.createAndAddLink(new IdImpl("2"), node2, node3, 2000, 27.78, 3600, 1);
+//      network.createAndAddLink(new IdImpl("3"), node3, node4, 75000, 10.42, 3600, 1);
+        network.createAndAddLink(new IdImpl("4"), node4, node5, 2500, 27.78, 3600, 1);
+        network.createAndAddLink(new IdImpl("5"), node5, node6, 10000, 27.78, 3600, 1);
+        network.createAndAddLink(new IdImpl("6"), node6, node7, 10000, 27.78, 3600, 1);
+        network.createAndAddLink(new IdImpl("7"), node7, node1, 10000, 27.78, 3600, 1);
+        network.createAndAddLink(new IdImpl("8"), node3, node8, 5000, 27.78, 3600, 1);
+        network.createAndAddLink(new IdImpl("9"), node8, node4, 5000, 27.78, 3600, 1);
+        network.createAndAddLink(new IdImpl("10"), node3, node9, 5000, 27.78, 3600, 1);
+        network.createAndAddLink(new IdImpl("11"), node9, node4, 4999, 27.78, 3600, 1);
 	}
 
 	public static void main(String[] args) {
