b1563057a3a3c4cf47ddf57b7a65fb5374702c3d
vsp_nagel <2008@kainagel.org>
2011-12-19 23:22:19 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@17828 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/kai/src/main/java/playground/kai/otfvis/TransitControler.java
+++ playgrounds/kai/src/main/java/playground/kai/otfvis/TransitControler.java
@@ -47,80 +47,78 @@ import org.matsim.vis.otfvis.gui.OTFVisConfigGroup;
 public class TransitControler {
 
 	public static void main(final String[] args) {
-//		args[0] = "/Users/nagel/kw/rotterdam/config.xml" ;
+		//		args[0] = "/Users/nagel/kw/rotterdam/config.xml" ;
 		Config config = new Config();
 		config.addCoreModules();
 		new MatsimConfigReader(config).readFile(args[0]);
 		config.scenario().setUseTransit(true);
 		config.scenario().setUseVehicles(true);
 		config.otfVis().setColoringScheme( OTFVisConfigGroup.COLORING_BVG ) ;
-		
+
 		config.getQSimConfigGroup().setVehicleBehavior( QSimConfigGroup.VEHICLE_BEHAVIOR_TELEPORT ) ;
-		
+
 		Controler tc = new Controler(config) ;
-		
+
 		MobsimFactory mobsimFactory = new MyMobsimFactory() ; 
 		tc.setMobsimFactory(mobsimFactory) ;
-				
+
 		tc.setOverwriteFiles(true);
-//		tc.setCreateGraphs(false);
+		//		tc.setCreateGraphs(false);
 		tc.run();
 	}
-	
+
 	static class MyMobsimFactory implements MobsimFactory {
 		private boolean useOTFVis = true ;
 
 		@Override
 		public Simulation createMobsim(Scenario sc, EventsManager eventsManager) {
-            EventsManager eventsManager1 = eventsManager;
+			EventsManager eventsManager1 = eventsManager;
 
-            QSimConfigGroup conf = sc.getConfig().getQSimConfigGroup();
-            if (conf == null) {
-                throw new NullPointerException("There is no configuration set for the QSim. Please add the module 'qsim' to your config file.");
-            }
+			QSimConfigGroup conf = sc.getConfig().getQSimConfigGroup();
+			if (conf == null) {
+				throw new NullPointerException("There is no configuration set for the QSim. Please add the module 'qsim' to your config file.");
+			}
 
-            // Get number of parallel Threads
-            int numOfThreads = conf.getNumberOfThreads();
-            QNetsimEngineFactory netsimEngFactory;
-            if (numOfThreads > 1) {
-                eventsManager1 = new SynchronizedEventsManagerImpl(eventsManager1);
-                netsimEngFactory = new ParallelQNetsimEngineFactory();
-            } else {
-                netsimEngFactory = new DefaultQSimEngineFactory();
-            }
-            QSim qSim = new QSim(sc, eventsManager1, netsimEngFactory);
-            AgentFactory agentFactory;
-                agentFactory = new TransitAgentFactory(qSim);
-                TransitQSimEngine transitEngine = new TransitQSimEngine(qSim);
-                transitEngine.setUseUmlaeufe(true);
-                transitEngine.setTransitStopHandlerFactory(new ComplexTransitStopHandlerFactory());
-                qSim.addDepartureHandler(transitEngine);
-                qSim.addAgentSource(transitEngine);
-            PopulationAgentSource agentSource = new PopulationAgentSource(sc.getPopulation(), agentFactory, qSim);
-            qSim.addAgentSource(agentSource);
+			// Get number of parallel Threads
+			int numOfThreads = conf.getNumberOfThreads();
+			QNetsimEngineFactory netsimEngFactory;
+			if (numOfThreads > 1) {
+				eventsManager1 = new SynchronizedEventsManagerImpl(eventsManager1);
+				netsimEngFactory = new ParallelQNetsimEngineFactory();
+			} else {
+				netsimEngFactory = new DefaultQSimEngineFactory();
+			}
+			QSim qSim = new QSim(sc, eventsManager1, netsimEngFactory);
+			AgentFactory agentFactory= new TransitAgentFactory(qSim);
+			TransitQSimEngine transitEngine = new TransitQSimEngine(qSim);
+			transitEngine.setUseUmlaeufe(true);
+			transitEngine.setTransitStopHandlerFactory(new ComplexTransitStopHandlerFactory());
+			qSim.addDepartureHandler(transitEngine);
+			qSim.addMobsimEngine(transitEngine);
+			qSim.addAgentSource(transitEngine);
+			PopulationAgentSource agentSource = new PopulationAgentSource(sc.getPopulation(), agentFactory, qSim);
+			qSim.addAgentSource(agentSource);
 
-            QSim simulation = (QSim) qSim;
+			//			simulation.getTransitEngine().setTransitStopHandlerFactory(new ComplexTransitStopHandlerFactory());
+//			transitEngine.setTransitStopHandlerFactory(new SimpleTransitStopHandlerFactory());
+			//			this.events.addHandler(new LogOutputEventHandler());
 
-//			simulation.getTransitEngine().setTransitStopHandlerFactory(new ComplexTransitStopHandlerFactory());
-			transitEngine.setTransitStopHandlerFactory(new SimpleTransitStopHandlerFactory());
-//			this.events.addHandler(new LogOutputEventHandler());
-			
 			if ( useOTFVis ) {
 				// otfvis configuration.  There is more you can do here than via file!
-				final OTFVisConfigGroup otfVisConfig = simulation.getScenario().getConfig().otfVis();
+				final OTFVisConfigGroup otfVisConfig = qSim.getScenario().getConfig().otfVis();
 				otfVisConfig.setDrawTransitFacilities(false) ; // this DOES work
-//				otfVisConfig.setShowParking(true) ; // this does not really work
+				//				otfVisConfig.setShowParking(true) ; // this does not really work
 
 			}
 
-//			if(this.useHeadwayControler){
-//				simulation.getQSimTransitEngine().setAbstractTransitDriverFactory(new FixedHeadwayCycleUmlaufDriverFactory());
-//				this.events.addHandler(new FixedHeadwayControler(simulation));		
-//			}
-			OnTheFlyServer server = OTFVis.startServerAndRegisterWithQSim(sc.getConfig(), sc, eventsManager, simulation);
+			//			if(this.useHeadwayControler){
+			//				simulation.getQSimTransitEngine().setAbstractTransitDriverFactory(new FixedHeadwayCycleUmlaufDriverFactory());
+			//				this.events.addHandler(new FixedHeadwayControler(simulation));		
+			//			}
+			OnTheFlyServer server = OTFVis.startServerAndRegisterWithQSim(sc.getConfig(), sc, eventsManager, qSim);
 			OTFClientLive.run(sc.getConfig(), server);
 
-			return simulation ;
+			return qSim ;
 		}
 	}
 }
