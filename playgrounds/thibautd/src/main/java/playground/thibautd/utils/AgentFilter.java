6203bd6d336974ea1b70d7eb5621a693130549da
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2011-12-21 14:39:52 +0000

    organised imports
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@17860 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/java/playground/thibautd/utils/AgentFilter.java
+++ playgrounds/thibautd/src/main/java/playground/thibautd/utils/AgentFilter.java
@@ -23,16 +23,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.Population;
-import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.basic.v01.IdImpl;
 import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.network.MatsimNetworkReader;
 import org.matsim.core.population.MatsimPopulationReader;
 import org.matsim.core.population.PopulationWriter;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.core.config.ConfigUtils;
 
 public class AgentFilter {
 	// todo: import from cliques definition
