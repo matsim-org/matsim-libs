28ac5d69224cca4d29d9de38f2330b4133aba7c8
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2012-01-06 15:51:46 +0000

    added necessary class to perform test runs for planomat v2
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@17934 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/thibautd/src/main/java/playground/thibautd/planomat/strategies/package-info.java
@@ -0,0 +1,31 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * package-info.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+/**
+ * Defines strategies using planomat v2.
+ * This package is meant to define strategies to load by name from the
+ * config file; it should be deleted and replaced by cases in the
+ * StategyConfigLoader when the v2 is mature enough to be included in the
+ * core.
+ *
+ * @author thibautd
+ */
+package playground.thibautd.planomat.strategies;
+
