4b7cc6476d0dcf3a452089eef604bc72972796c2
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2012-01-12 16:46:57 +0000

    added mode analysis
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@17995 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/java/playground/thibautd/planomat/run/Run.java
+++ playgrounds/thibautd/src/main/java/playground/thibautd/planomat/run/Run.java
@@ -25,6 +25,7 @@ import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.scenario.ScenarioUtils;
 
+import playground.thibautd.analysis.listeners.ModeAnalysis;
 import playground.thibautd.planomat.config.Planomat2ConfigGroup;
 
 /**
@@ -40,6 +41,7 @@ public class Run {
 
 		Scenario scenario = ScenarioUtils.loadScenario(config);
 		Controler controler = new Controler(scenario);
+		controler.addControlerListener(new ModeAnalysis( true ));
 
 		controler.run();
 	}
