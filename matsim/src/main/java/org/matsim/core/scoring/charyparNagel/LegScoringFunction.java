f6727dd189b5d4be5b3284d2b5d9eea39d1da6a6
michaz <michael.zilske@gmail.com>
2012-01-12 14:23:08 +0000

    not necessarily expecting a NetworkRoute for a car leg.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@17988 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/core/scoring/charyparNagel/LegScoringFunction.java
+++ matsim/src/main/java/org/matsim/core/scoring/charyparNagel/LegScoringFunction.java
@@ -20,12 +20,10 @@
 
 package org.matsim.core.scoring.charyparNagel;
 
-import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.Route;
-import org.matsim.core.gbl.Gbl;
 import org.matsim.core.population.routes.NetworkRoute;
 import org.matsim.core.scoring.CharyparNagelScoringParameters;
 import org.matsim.core.scoring.interfaces.BasicScoring;
@@ -39,8 +37,6 @@ import org.matsim.core.utils.misc.RouteUtils;
  * @author rashid_waraich
  */
 public class LegScoringFunction implements LegScoring, BasicScoring {
-
-	private final static Logger log = Logger.getLogger(LegScoringFunction.class);
 
 	protected double score;
 	private double lastTime;
@@ -78,6 +74,10 @@ public class LegScoringFunction implements LegScoring, BasicScoring {
 		this.lastTime = time;
 	}
 
+	private void handleLeg(Leg leg, final double time) {
+		this.score += calcLegScore(this.lastTime, time, leg);
+	}
+
 	@Override
 	public void finish() {
 
@@ -88,53 +88,30 @@ public class LegScoringFunction implements LegScoring, BasicScoring {
 		return this.score;
 	}
 
-	private static int distanceWrnCnt = 0 ;
 	protected double calcLegScore(final double departureTime, final double arrivalTime, final Leg leg) {
 		double tmpScore = 0.0;
-		double travelTime = arrivalTime - departureTime; // traveltime in
-															// seconds
-
-		/*
-		 * we only as for the route when we have to calculate a distance cost,
-		 * because route.getDist() may calculate the distance if not yet
-		 * available, which is quite an expensive operation
-		 */
-		double dist = 0.0; // distance in meters
-
+		double travelTime = arrivalTime - departureTime; // travel time in seconds	
 		if (TransportMode.car.equals(leg.getMode())) {
+			double dist = 0.0; // distance in meters
 			if (this.params.marginalUtilityOfDistanceCar_m != 0.0) {
 				Route route = leg.getRoute();
-				dist = RouteUtils.calcDistance((NetworkRoute) route, network);
-                if ( distanceWrnCnt<1 ) {
-				/*
-				 * TODO the route-distance does not contain the length of the
-				 * first or last link of the route, because the route doesn't
-				 * know those. Should be fixed somehow, but how? MR, jan07
-				 */
-				/*
-				 * TODO in the case of within-day replanning, we cannot be sure
-				 * that the distance in the leg is the actual distance driven by
-				 * the agent.
-				 */
-					log.warn("leg distance for scoring computed from plan, not from execution (=events)." +
-							"This is not how it is meant to be, and it will fail for within-day replanning." ) ;
-					log.warn("Also means that first and last link are not included." ) ;
-					log.warn( Gbl.ONLYONCE ) ;
-					distanceWrnCnt++ ;
-				}
+				dist = getDistance(route);
 			}
 			tmpScore += travelTime * this.params.marginalUtilityOfTraveling_s + this.params.marginalUtilityOfDistanceCar_m * dist;
 			tmpScore += this.params.constantCar ;
 		} else if (TransportMode.pt.equals(leg.getMode())) {
+			double dist = 0.0; // distance in meters
 			if (this.params.marginalUtilityOfDistancePt_m != 0.0) {
-				dist = leg.getRoute().getDistance();
+				Route route = leg.getRoute();
+				dist = getDistance(route);
 			}
 			tmpScore += travelTime * this.params.marginalUtilityOfTravelingPT_s + this.params.marginalUtilityOfDistancePt_m * dist;
 			tmpScore += this.params.constantPt ;
-		} else if (TransportMode.walk.equals(leg.getMode())
-				|| TransportMode.transit_walk.equals(leg.getMode())) {
+		} else if (TransportMode.walk.equals(leg.getMode()) || TransportMode.transit_walk.equals(leg.getMode())) {
+			double dist = 0.0; // distance in meters
 			if (this.params.marginalUtilityOfDistanceWalk_m != 0.0) {
-				dist = leg.getRoute().getDistance();
+				Route route = leg.getRoute();
+				dist = getDistance(route);
 			}
 			tmpScore += travelTime * this.params.marginalUtilityOfTravelingWalk_s + this.params.marginalUtilityOfDistanceWalk_m * dist;
 			tmpScore += this.params.constantWalk ;
@@ -142,19 +119,26 @@ public class LegScoringFunction implements LegScoring, BasicScoring {
 			tmpScore += travelTime * this.params.marginalUtilityOfTravelingBike_s;
 			tmpScore += this.params.constantBike ;
 		} else {
+			double dist = 0.0; // distance in meters
 			if (this.params.marginalUtilityOfDistanceCar_m != 0.0) {
-				dist = leg.getRoute().getDistance();
+				Route route = leg.getRoute();
+				dist = getDistance(route);
 			}
 			// use the same values as for "car"
 			tmpScore += travelTime * this.params.marginalUtilityOfTraveling_s + this.params.marginalUtilityOfDistanceCar_m * dist;
 			tmpScore += this.params.constantCar ;
 		}
-
 		return tmpScore;
 	}
 
-	private void handleLeg(Leg leg, final double time) {
-		this.score += calcLegScore(this.lastTime, time, leg);
+	private double getDistance(Route route) {
+		double dist;
+		if (route instanceof NetworkRoute) {
+			dist =  RouteUtils.calcDistance((NetworkRoute) route, network);
+		} else {
+			dist = route.getDistance();
+		}
+		return dist;
 	}
 
 }
