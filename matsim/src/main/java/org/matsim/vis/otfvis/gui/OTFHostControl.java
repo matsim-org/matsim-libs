matsim-org/matsim-libs

commit 339aebda072ddb6379e42bb94cd632543b43b5d5

fixed a threading bug
git-svn-id: file:///srv/local/data/cleanRepo/trunk@15490 a0517364-e873-4167-b1c8-416daa5a6b91

matsim/src/main/java/org/matsim/vis/otfvis/gui/OTFHostControl.java
@@ -25,6 +25,7 @@ import java.util.List;
 
 import javax.swing.BoundedRangeModel;
 import javax.swing.DefaultBoundedRangeModel;
+import javax.swing.SwingUtilities;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -236,25 +237,33 @@ public class OTFHostControl {
 		@Override
 		public void run() {
 			int delay = 30;
-			int actTime = 0;
 			while (!terminate) {
 				try {
 					delay = OTFClientControl.getInstance().getOTFVisConfig().getDelay_ms();
 					sleep(delay);
-					if (hostControlBar.isSynchronizedPlay() && ((getSimTime() >= loopEnd) || !requestNewTime(getSimTime() + 1, OTFServerRemote.TimePreference.LATER))) {
-						requestNewTime(loopStart, OTFServerRemote.TimePreference.LATER);
-					}
-					actTime = getSimTime();
-					simTime.setValue(masterHostConnectionManager.getOTFServer().getLocalTime());
-					for (OTFHostConnectionManager slave : hostControls) {
-						if (!slave.equals(masterHostConnectionManager)) {
-							slave.getOTFServer().requestNewTime(simTime.getValue(), OTFServerRemote.TimePreference.LATER);
+					SwingUtilities.invokeLater(new Runnable() { // This is important. Code below modifies stuff which is "owned" by swing, so it must run on the Swing thread! 
+						
+						int actTime = 0;
+						
+						@Override
+						public void run() {
+							if (hostControlBar.isSynchronizedPlay() && ((getSimTime() >= loopEnd) || !requestNewTime(getSimTime() + 1, OTFServerRemote.TimePreference.LATER))) {
+								requestNewTime(loopStart, OTFServerRemote.TimePreference.LATER);
+							}
+							actTime = getSimTime();
+							simTime.setValue(masterHostConnectionManager.getOTFServer().getLocalTime());
+							for (OTFHostConnectionManager slave : hostControls) {
+								if (!slave.equals(masterHostConnectionManager)) {
+									slave.getOTFServer().requestNewTime(simTime.getValue(), OTFServerRemote.TimePreference.LATER);
+								}
+							}
+							hostControlBar.updateTimeLabel();
+							if (simTime.getValue() != actTime) {
+								hostControlBar.repaint();
+							}
 						}
-					}
-					hostControlBar.updateTimeLabel();
-					if (simTime.getValue() != actTime) {
-						hostControlBar.repaint();
-					}
+					});
+					
 				} catch (InterruptedException e) {
 					throw new RuntimeException(e);
 				}

matsim/src/main/java/org/matsim/vis/otfvis/opengl/drawer/OTFOGLDrawer.java
@@ -67,6 +67,7 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -128,8 +129,6 @@ public class OTFOGLDrawer implements OTFDrawer, GLEventListener {
 
 	private final List<OTFGLAbstractDrawable> overlayItems = new ArrayList<OTFGLAbstractDrawable>();
 
-	private static List<OTFGLAbstractDrawable> newItems = new ArrayList<OTFGLAbstractDrawable>();
-
 	private StatusTextDrawer statusDrawer = null;
 
 	private OTFQueryHandler queryHandler = null;
@@ -141,8 +140,6 @@ public class OTFOGLDrawer implements OTFDrawer, GLEventListener {
 	private BufferedImage current;
 
 	private ZoomEntry lastZoom = null;
-
-	private final Object blockRefresh = new Object();
 
 	private JDialog zoomD;
 
@@ -603,12 +600,8 @@ public class OTFOGLDrawer implements OTFDrawer, GLEventListener {
 			this.now = time;
 			this.lastTime = Time.writeTime(time, ':');
 		}
-		synchronized (this.blockRefresh) {
-			QuadTree.Rect rect = this.mouseMan.getBounds();
-			synchronized (newItems) {
-				this.currentSceneGraph  = this.clientQ.getSceneGraph(time, rect, this);
-			}
-		}
+		QuadTree.Rect rect = this.mouseMan.getBounds();
+		this.currentSceneGraph  = this.clientQ.getSceneGraph(time, rect, this);
 		if (this.queryHandler != null) {
 			this.queryHandler.updateQueries();
 		}

matsim/src/main/java/org/matsim/vis/otfvis/OTFClient.java
@@ -52,7 +52,7 @@ import org.matsim.vis.otfvis.opengl.gui.SettingsSaver;
 public abstract class OTFClient implements Runnable {
 
 	private static final Logger log = Logger.getLogger(OTFClient.class);
-	
+
 	// Keine Ahnung.
 	private static final String id = "id";
 
@@ -67,29 +67,17 @@ public abstract class OTFClient implements Runnable {
 	protected OTFHostConnectionManager masterHostControl;
 
 	public OTFClient() {
-		
+
 	}
 
 	@Override
 	public void run() {
-		createMainFrame();
-		log.info("created MainFrame");
-		OTFVisConfigGroup visconf = createOTFVisConfig();
-		OTFClientControl.getInstance().setOTFVisConfig(visconf);
-		log.info("got OTFVis config");
-		createHostControlBar();
-		log.info("created HostControlBar");
-		OTFDrawer mainDrawer = createDrawer();
-		OTFClientControl.getInstance().setMainOTFDrawer(mainDrawer);
-		log.info("created drawer");
-		JPanel panel = new JPanel(new BorderLayout());
-		panel.add(mainDrawer.getComponent(), BorderLayout.CENTER);
-		pane.setRightComponent(panel);
-		pane.validate();
-		this.hostControlBar.addDrawer(mainDrawer);
-		mainDrawer.redraw();
-		frame.setVisible(true);
-		log.info("OTFVis finished init");
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				initializeSwingClient();
+			}
+		});
 	}
 
 	public void setHostConnectionManager(OTFHostConnectionManager otfHostConnectionManager) {
@@ -187,5 +175,26 @@ public abstract class OTFClient implements Runnable {
 	protected abstract OTFDrawer createDrawer();
 
 	protected abstract OTFVisConfigGroup createOTFVisConfig();
+
+	private void initializeSwingClient() {
+		createMainFrame();
+		log.info("created MainFrame");
+		OTFVisConfigGroup visconf = createOTFVisConfig();
+		OTFClientControl.getInstance().setOTFVisConfig(visconf);
+		log.info("got OTFVis config");
+		createHostControlBar();
+		log.info("created HostControlBar");
+		OTFDrawer mainDrawer = createDrawer();
+		OTFClientControl.getInstance().setMainOTFDrawer(mainDrawer);
+		log.info("created drawer");
+		JPanel panel = new JPanel(new BorderLayout());
+		panel.add(mainDrawer.getComponent(), BorderLayout.CENTER);
+		pane.setRightComponent(panel);
+		pane.validate();
+		hostControlBar.addDrawer(mainDrawer);
+		mainDrawer.redraw();
+		frame.setVisible(true);
+		log.info("OTFVis finished init");
+	}
 
 }

commit 931feea67519581184c2feb5cb1fb719b5b2dee9

made otfvis fast again (broken since r15490) and fixed the sync checkbox (broken since r15418)
git-svn-id: file:///srv/local/data/cleanRepo/trunk@18123 a0517364-e873-4167-b1c8-416daa5a6b91

matsim/src/main/java/org/matsim/vis/otfvis/gui/OTFHostControl.java
@@ -234,17 +234,17 @@ public class OTFHostControl {
 				try {
 					delay = OTFClientControl.getInstance().getOTFVisConfig().getDelay_ms();
 					sleep(delay);
+					if (hostControlBar.isSynchronizedPlay() && ((getSimTime() >= loopEnd) || !requestNewTime(getSimTime() + 1, OTFServer.TimePreference.LATER))) {
+						requestNewTime(loopStart, OTFServer.TimePreference.LATER);
+					}
+					simTime.setValue(server.getLocalTime());
 					SwingUtilities.invokeLater(new Runnable() { // This is important. Code below modifies stuff which is "owned" by swing, so it must run on the Swing thread! 
 						
 						int actTime = 0;
 						
 						@Override
 						public void run() {
-							if (hostControlBar.isSynchronizedPlay() && ((getSimTime() >= loopEnd) || !requestNewTime(getSimTime() + 1, OTFServer.TimePreference.LATER))) {
-								requestNewTime(loopStart, OTFServer.TimePreference.LATER);
-							}
 							actTime = getSimTime();
-							simTime.setValue(server.getLocalTime());
 							hostControlBar.updateTimeLabel();
 							if (simTime.getValue() != actTime) {
 								hostControlBar.repaint();

matsim/src/main/java/org/matsim/vis/otfvis/opengl/drawer/OTFOGLDrawer.java
@@ -737,9 +737,9 @@ public class OTFOGLDrawer implements GLEventListener {
 			// So we pass the size of the whole network here and don't rely on anybody else for that.
 			// michaz May '11
 			rect = new QuadTree.Rect((float)clientQ.getMinEasting(), (float)clientQ.getMinNorthing(), (float)clientQ.getMaxEasting(), (float)clientQ.getMaxNorthing());
+			this.hostControlBar.getOTFHostControl().fetchTimeAndStatus();
 		}
 		this.currentSceneGraph  = this.clientQ.getSceneGraph(time, rect, this);
-		this.hostControlBar.getOTFHostControl().fetchTimeAndStatus();
 		if (this.queryHandler != null) {
 			this.queryHandler.updateQueries();
 		}