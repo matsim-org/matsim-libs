7dc4d55cfe8ba42d9616ade68d8381b74582ecfa
grether <dominik.grether@gmail.com>
2012-01-25 00:31:53 +0000

    some changes
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@18154 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/fuerbas/src/main/java/air/scenario/SfUtcOffset.java
+++ playgrounds/fuerbas/src/main/java/air/scenario/SfUtcOffset.java
@@ -1,3 +1,22 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * SfUtcOffset
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2011 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
 package air.scenario;
 
 import java.io.BufferedReader;
@@ -7,48 +26,57 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
+import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.core.utils.geometry.transformations.TransformationFactory;
 
-
+/**
+ * @author sfuerbas
+ * @author dgrether
+ *
+ */
 public class SfUtcOffset {
 	
-	private static final String FILE_DIR = "";
-	private static final String INPUTFILE_OSM = FILE_DIR+"";
-	private static final String OUTPUTFILE_UTC= FILE_DIR+"";
+	private static final Logger log = Logger.getLogger(SfUtcOffset.class);
 	
-//	public static void main(String args[]) throws Exception {
-//		SfUtcOffset test = new SfUtcOffset();
-//		test.getUtcOffset(writeUtcOffset(INPUTFILE_OSM, OUTPUTFILE_UTC));
-//		
-//	}
+	private static final String INPUTFILE_OSM = "/home/dgrether/shared-svn/projects/throughFlightData/osm_daten/2010-12-28_aeroway_nodes.osm";
+	private static final String OUTPUTFILE_UTC= "/media/data/work/repos/"
+				+ "shared-svn/studies/countries/world/flight/sf_oag_flight_model/utc_offsets.txt";
 	
 	private Map<String, Coord> airportsInOsm;
 	
+	
 	public void writeUtcOffset(String inputOsmFile, String outputFile) throws IOException, InterruptedException{
-		BufferedWriter bw = new BufferedWriter(new FileWriter(new File(outputFile)));
 		SfOsmAerowayParser osmReader = new SfOsmAerowayParser(
 				TransformationFactory.getCoordinateTransformation(TransformationFactory.WGS84,
 						TransformationFactory.WGS84));
 		osmReader.parse(inputOsmFile);
+	
 		this.airportsInOsm = osmReader.airports;
 		
-		Iterator it = this.airportsInOsm.entrySet().iterator();
-		while (it.hasNext()) {
-			Map.Entry pairs = (Map.Entry) it.next();
-			bw.write(pairs.getKey().toString() + "\t" + getUtcOffset(this.airportsInOsm.get(pairs.getKey())));
+		Map<String, Double> airportUTCOffsetMap = new HashMap<String, Double>();
+		for (Entry<String, Coord> e : this.airportsInOsm.entrySet()) {
+			Double utcOffset = getUtcOffset(this.airportsInOsm.get(e.getKey()));
+			log.info("Got offset for airport: " + e.getKey() + " offset: " + utcOffset);
+			airportUTCOffsetMap.put(e.getKey(), utcOffset);
+		}
+		
+		//Output
+		BufferedWriter bw = new BufferedWriter(new FileWriter(new File(outputFile)));
+		for (Entry<String, Double> e : airportUTCOffsetMap.entrySet()) {
+			bw.write(e.getKey().toString() + "\t" +  e.getValue());
 			bw.newLine();
 		}
 		bw.close();
 	}
 	
-	public static int getUtcOffset(Coord coord) throws IOException, InterruptedException {
+	public static Double getUtcOffset(Coord coord) throws IOException, InterruptedException {
 		
-		Thread.sleep(2*1000);
-		
+		Thread.sleep(2*1000); //requirement by the web service provider
 		String inputURL = "http://www.earthtools.org/timezone/"+coord.getX()+"/"+coord.getY();	
 		
 		URL oracle = new URL(inputURL);
@@ -56,8 +84,7 @@ public class SfUtcOffset {
 		new InputStreamReader(oracle.openStream()));
 		
 		String inputLine;
-		
-		int offset = 0;
+		double offset = 0;
 		
 		while ((inputLine = in.readLine()) != null) {
 			
@@ -65,27 +92,28 @@ public class SfUtcOffset {
 				inputLine = inputLine.replaceAll("<offset>", "");
 				inputLine = inputLine.replaceAll("</offset>", "");
 				inputLine = inputLine.replaceAll(" ", "");
-				offset = Integer.parseInt(inputLine);
+				offset = Double.parseDouble(inputLine);
 //				System.out.println(offset);
 			}
 			else if (inputLine.contains("dst")) {
 				inputLine = inputLine.replaceAll("<dst>", "");
 				inputLine = inputLine.replaceAll("</dst>", "");
 				inputLine = inputLine.replaceAll(" ", "");
-				if (inputLine.equalsIgnoreCase("true")) offset++;
+				if (inputLine.equalsIgnoreCase("true"))  {
+					offset++;
+				}
 //				System.out.println("neu"+offset);
 //				System.out.println(inputLine);
 			}
-			
 		}
-		
 		in.close();
-	    	    
-
 		return offset;
-		
-		
-		
 	}
 
+	public static void main(String args[]) throws Exception {
+		SfUtcOffset test = new SfUtcOffset();
+		test.writeUtcOffset(INPUTFILE_OSM, OUTPUTFILE_UTC);
+	}
+
+	
 }
