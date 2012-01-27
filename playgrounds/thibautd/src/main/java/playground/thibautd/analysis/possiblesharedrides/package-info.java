ff36caeb7ee850ba80f787c72b997eb3e34cdef3
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2012-01-27 12:32:44 +0000

    javadoc
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@18191 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/java/playground/thibautd/analysis/possiblesharedrides/package-info.java
+++ playgrounds/thibautd/src/main/java/playground/thibautd/analysis/possiblesharedrides/package-info.java
@@ -1,8 +1,9 @@
-package playground.thibautd.analysis.possiblesharedrides;
 
 /**
  * Package aimed at analysing shared ride possibilities from the output events
  * from an "autistic" simulation.
  *
+ * Deprecated: use  {@link playground.thibautd.analysis.joinabletripsidentifier} instead
  * @author thibautd
  */
+package playground.thibautd.analysis.possiblesharedrides;
