b60f3e79b776418dd7cc3ed6319c346070eb75e9
kaddoura <kaddoura@users.sf.net>
2012-02-08 11:53:40 +0000

    bvwp2003main
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@18319 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/kai/src/main/java/playground/kai/bvwp/Main.java
+++ playgrounds/kai/src/main/java/playground/kai/bvwp/Main.java
@@ -4,10 +4,11 @@ package playground.kai.bvwp;
 public class Main {
 
 	public static void main(String[] args) {
-
+		
 		// create the economic values
 		Values economicValues = EconomicValues4.createEconomicValues1();
-		
+		System.out.println("\n===\nFolgende Rechnung entspricht exakt ``rechnungen>javaRechnungen>BVWP2003.xlsx'' (EconomicValues, Scenario und Methodik) ");
+
 		// create the base case:
 		ScenarioForEval nullfall = Scenario4.createNullfall1();
 		
@@ -15,7 +16,6 @@ public class Main {
 		ScenarioForEval planfall = Scenario4.createPlanfall1(nullfall);
 		
 		// instantiate the class that defines the utility calculation:
-//		UtilityChanges utilityChanges = new UtilityChangesRuleOfHalf();
 		UtilityChanges utilityChanges = new UtilityChangesBVWP2003();
 
 		// compute the utilities (currently, results are printed to the console):
