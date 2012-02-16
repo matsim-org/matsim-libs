f1172a540f2a908d6172cd2dd998ccface09e052
juliakern <juliakern@users.sf.net>
2012-02-16 16:00:39 +0000

    J-unit test classes for different routings
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@18467 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/juliakern/src/test/java/playground/julia/incomeScoring/RoadUsedHandler.java
+++ playgrounds/juliakern/src/test/java/playground/julia/incomeScoring/RoadUsedHandler.java
@@ -13,8 +13,13 @@ import org.matsim.core.basic.v01.IdImpl;
 public class RoadUsedHandler implements LinkLeaveEventHandler{
 
 	static boolean mer;
-	int i=3;
+	int i=20;
+	String usedRoadVariant = new String("");
 	
+
+public RoadUsedHandler(int j) {
+		i= j;// TODO Auto-generated constructor stub
+	}
 
 @Override
 public void reset(int iteration) {
@@ -23,15 +28,25 @@ public void reset(int iteration) {
 
 @Override
 public void handleEvent(LinkLeaveEvent event) {
-	System.out.println("LinkId"+event.getLinkId().toString());
+	//System.out.println("LinkId"+event.getLinkId().toString());
 	if (event.getLinkId().equals(new IdImpl(Integer.toString(i)))){
 		mer = true;
+	}
+	else{
+		if(event.getLinkId().equals(new IdImpl(Integer.toString(9)))) usedRoadVariant="9";
+		if(event.getLinkId().equals(new IdImpl(Integer.toString(11)))) usedRoadVariant="11";
+		if(event.getLinkId().equals(new IdImpl(Integer.toString(13)))) usedRoadVariant="13";
+		
 	}
 }
 
 
 public static boolean getWasRoadSelected() {
 	return mer;
+}
+
+public String getLink() {
+	return usedRoadVariant;
 }
 
 
