afb709b2dc89deb208a73786725464b25689799d
mmoyo <mmoyo@users.sf.net>
2012-02-21 12:36:05 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@18500 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/mmoyo/src/main/java/playground/mmoyo/algorithms/SelectedPlanLister2.java
@@ -0,0 +1,64 @@
+package playground.mmoyo.algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.core.scenario.ScenarioImpl;
+import org.matsim.population.algorithms.PersonAlgorithm;
+
+import playground.mmoyo.io.PopSecReader;
+import playground.mmoyo.utils.DataLoader;
+
+/**list number of agents related to selected plan index in the population*/
+public class SelectedPlanLister2 implements PersonAlgorithm {
+	private Map <Integer, List<Id>> selIndex2personMap = new TreeMap <Integer, List<Id>>();
+	final String STR_SPACE = " ";
+	int size=0;
+	
+	public SelectedPlanLister2(){
+	}
+
+	@Override
+	public void run(Person person) {
+		int i= person.getPlans().indexOf(person.getSelectedPlan());
+		if(!selIndex2personMap.keySet().contains(i)){
+			selIndex2personMap.put(i, new ArrayList<Id>());
+		}
+		selIndex2personMap.get(i).add(person.getId());
+		size++;
+	}
+	
+	private void printSelectedIndex(){
+		System.out.println("Population size: " + size); 
+		for(Map.Entry <Integer,List<Id>> entry: selIndex2personMap.entrySet() ){
+			Integer key = entry.getKey(); 
+			List<Id> indexList = entry.getValue();
+			
+			System.out.println("\nIndex: " + key + "; selected plans: " + indexList.size() + STR_SPACE);
+			for (Id id : entry.getValue()){
+				System.out.print(id + STR_SPACE);
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		String popFilePath;
+		if (args.length>0){
+			popFilePath = args[0];
+		}else{
+			popFilePath = "../../runs_manuel/CalibLineM44/automCalib10xTimeMutated/10xrun/it.500/500.plans.xml.gz";
+		}
+
+		DataLoader dataLoader = new DataLoader ();
+		ScenarioImpl scn = (ScenarioImpl) dataLoader.createScenario(); 
+			
+		SelectedPlanLister2 selectedPlanLister2 = new SelectedPlanLister2();
+		new PopSecReader(scn, selectedPlanLister2).readFile(popFilePath);
+		selectedPlanLister2.printSelectedIndex();
+	}
+	
+}
