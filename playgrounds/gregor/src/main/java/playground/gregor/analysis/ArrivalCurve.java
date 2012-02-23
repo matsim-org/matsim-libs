f1b49587ed6eaf35289048838b581bcc6949abdd
laemmel <gregor.laemmel@gmail.com>
2012-02-23 16:12:43 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@18534 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/gregor/src/main/java/playground/gregor/analysis/ArrivalCurve.java
@@ -0,0 +1,78 @@
+package playground.gregor.analysis;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.matsim.core.api.experimental.events.AgentArrivalEvent;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.api.experimental.events.handler.AgentArrivalEventHandler;
+import org.matsim.core.events.EventsReaderXMLv1;
+import org.matsim.core.events.EventsUtils;
+
+public class ArrivalCurve implements AgentArrivalEventHandler{
+
+	int time = -1;
+	int arrived = 0;
+	private final BufferedWriter w;
+	
+	public ArrivalCurve(BufferedWriter w) {
+		this.w = w;
+	}
+	
+	@Override
+	public void handleEvent(AgentArrivalEvent event) {
+		int intTime = (int) (event.getTime() + 0.5);
+		
+		if (intTime > this.time) {
+			dump();
+			this.time = intTime;
+		}
+		this.arrived++;
+	}
+	
+	private void dump() {
+		try {
+			this.w.append(this.time + "\t" + this.arrived + "\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		
+	}
+
+	@Override
+	public void reset(int iteration) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String [] args) {
+		String events = "/Users/laemmel/devel/crisscross/output/ITERS/it.0/0.events.xml.gz";
+		EventsManager mgr = EventsUtils.createEventsManager();
+		
+		BufferedWriter w = null;
+		try {
+			w = new BufferedWriter(new FileWriter(new File("/Users/laemmel/devel/crisscross/dbg/arr")));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		ArrivalCurve arrv = new ArrivalCurve(w);
+		mgr.addHandler(arrv);
+		new EventsReaderXMLv1(mgr).parse(events);
+		try {
+			w.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+
+
+
+
+}
