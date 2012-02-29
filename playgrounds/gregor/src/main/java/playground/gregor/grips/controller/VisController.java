9a93992bf2a75a460a1e3886430ccb78d83a9811
laemmel <gregor.laemmel@gmail.com>
2012-02-29 11:19:05 +0000

    VisController + JOGL auto configuration
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@18569 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/gregor/src/main/java/playground/gregor/grips/controller/VisController.java
+++ playgrounds/gregor/src/main/java/playground/gregor/grips/controller/VisController.java
@@ -1,72 +1,56 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * VisController.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
 package playground.gregor.grips.controller;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-
-import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.config.ConfigWriter;
 import org.matsim.core.config.groups.QSimConfigGroup;
-import org.matsim.core.controler.ControlerIO;
 import org.matsim.core.events.EventsUtils;
-import org.matsim.core.gbl.MatsimRandom;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.ptproject.qsim.QSim;
 import org.matsim.ptproject.qsim.QSimFactory;
 import org.matsim.run.OTFVis;
-import org.matsim.signalsystems.builder.FromDataBuilder;
-import org.matsim.signalsystems.mobsim.QSimSignalEngine;
-import org.matsim.signalsystems.mobsim.SignalEngine;
+import org.matsim.vis.otfvis.OTFClientLive;
 import org.matsim.vis.otfvis.OnTheFlyServer;
 
-import playground.mzilske.osm.JXMapOTFVisClient;
+import playground.gregor.JOGLSetup;
 
 public class VisController {
-
-	private static final Logger log = Logger.getLogger(VisController.class);
-
 	public static void main (String [] args) {
 		Config config = ConfigUtils.loadConfig(args[0]);
-
-		MatsimRandom.reset(config.global().getRandomSeed());
-		Scenario scenario = ScenarioUtils.createScenario(config);
-
-		ScenarioUtils.loadScenario(scenario);
-
-		log.info("Complete config dump:");
-		StringWriter writer = new StringWriter();
-		new ConfigWriter(config).writeStream(new PrintWriter(writer));
-		log.info("\n\n" + writer.getBuffer().toString());
-		log.info("Complete config dump done.");
-		if (config.getQSimConfigGroup() == null){
-			log.error("Cannot play live config without config module for QSim (in Java QSimConfigGroup). " +
-					"Fixing this by adding default config module for QSim. " +
-					"Please check if default values fit your needs, otherwise correct them in " +
-			"the config given as parameter to get a valid visualization!");
-			config.addQSimConfigGroup(new QSimConfigGroup());
+		Config c = config;
+		String simOutput = c.controler().getOutputDirectory();
+		c.plans().setInputFile(simOutput + "/output_plans.xml.gz");
+		c.otfVis().setMapOverlayMode(true);
+		if (c.getQSimConfigGroup() == null) {
+			c.addQSimConfigGroup(new QSimConfigGroup());
 		}
-
-
+		Scenario sc = ScenarioUtils.createScenario(c);
+		ScenarioUtils.loadScenario(sc);
 		EventsManager events = EventsUtils.createEventsManager();
-
-
-		ControlerIO controlerIO = new ControlerIO(config.controler().getOutputDirectory());
-		QSim qSim = (QSim) new QSimFactory().createMobsim(scenario, events);
-		if (scenario.getConfig().scenario().isUseSignalSystems()){
-			SignalEngine engine = new QSimSignalEngine(new FromDataBuilder(scenario, events).createAndInitializeSignalSystemsManager());
-			qSim.addQueueSimulationListeners(engine);
-		}
-
-
-		OnTheFlyServer server = OTFVis.startServerAndRegisterWithQSim(scenario.getConfig(), scenario, events, qSim);
-		//		WMSService wms = new WMSService("http://localhost:8080/geoserver/wms?service=WMS&","hh");
-		//
-		//		JXMapOTFVisClient.run(scenario.getConfig(), server, wms);
-		JXMapOTFVisClient.run(scenario.getConfig(), server);
-
+		QSim qSim = (QSim) new QSimFactory().createMobsim(sc,events);
+		OnTheFlyServer server = OTFVis.startServerAndRegisterWithQSim(c,sc,events,qSim);
+		JOGLSetup.configureJOGL();
+		OTFClientLive.run(config,server);
 		qSim.run();
 	}
 }
