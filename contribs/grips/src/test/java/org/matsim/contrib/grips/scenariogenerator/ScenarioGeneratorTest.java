4e80ab95b620e2dc35a4484457e9c388b5a011a5
laemmel <gregor.laemmel@gmail.com>
2012-03-14 15:42:12 +0000

    moved grips to contrib added test case
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@18726 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ contribs/grips/src/test/java/org/matsim/contrib/grips/scenariogenerator/ScenarioGeneratorTest.java
@@ -0,0 +1,42 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * ScenarioGeneratorTest.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.contrib.grips.scenariogenerator;
+
+import org.junit.Test;
+import org.matsim.core.utils.misc.CRCChecksum;
+import org.matsim.testcases.MatsimTestCase;
+
+public class ScenarioGeneratorTest extends MatsimTestCase {
+	
+	@Test
+	public void testScenarioGenerator() {
+		String inputDir = getInputDirectory();
+		String outputDir = getOutputDirectory();
+		new ScenarioGenerator(inputDir + "/sgen_config.xml").run();
+		
+		assertEquals("different config-files.", CRCChecksum.getCRCFromFile(inputDir + "/config.xml"), CRCChecksum.getCRCFromFile(outputDir + "/config.xml"));
+		
+		assertEquals("different network-files.", CRCChecksum.getCRCFromFile(inputDir + "/network.xml.gz"), CRCChecksum.getCRCFromFile(outputDir + "/network.xml.gz"));
+		
+		assertEquals("different plans-files.", CRCChecksum.getCRCFromFile(inputDir + "/population.xml.gz"), CRCChecksum.getCRCFromFile(outputDir + "/population.xml.gz"));
+	}
+
+}
