2fe2425277114aa60a0ae997be7e8ab577f111b6
vsp_nagel <2008@kainagel.org>
2012-03-15 09:43:47 +0000

    comment change
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@18741 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/core/scoring/ScoringFunctionFactory.java
+++ matsim/src/main/java/org/matsim/core/scoring/ScoringFunctionFactory.java
@@ -27,8 +27,17 @@ import org.matsim.core.api.internal.MatsimFactory;
  * The <code>ScoringFunctionFactory</code> creates new scoring functions.
  * <br>
  * <br>
- * Note: I think this factory will no longer be needed in this form as soon
+ * Design comments:<ul>
+ * <li> I think this factory will no longer be needed in this form as soon
  * as every agent has its own instance of a scoring function. [marcel, 21jun07]
+ * <li> I find it a bit surprising that the factory obtains a plan as an argument.  I would have thought that the scoring
+ * function is attached to the agent (or to all agents), and thus constant over the iterations.  It is, however, recreated
+ * in every iteration, and so passing the selected plan makes _some_ sense.  kai, mar'12
+ * <li> yyyy Still, it pretends that the plan is always necessary for scoring, which is not only a small oversight, but a true
+ * conceptual flaw ... since there is absolutely no reason why agents without plans should not be scoreable.  I my intuition,
+ * it would make far more sense to explicitly add a planscorer (similar to legscorer, activityscorer, etc.) into the agent in 
+ * such cases.  kai, mar'12 
+ * </ul>
  *
  * @author mrieser
  */
@@ -38,7 +47,7 @@ public interface ScoringFunctionFactory extends MatsimFactory {
 	 * Creates a new scoring function for the given plan.
 	 *
 	 * @param plan A reference plan when calculating the score. This plan may be
-	 * used by the scoring function to look additional information.
+	 * used by the scoring function to look additional information. (But see comment above.  kai, mar'12)
 	 * @return A scoring function.
 	 */
 	public ScoringFunction createNewScoringFunction(final Plan plan);
