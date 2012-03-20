f91a11314ed4d90ba6aa0deec27797e7d6c1a15d
mmoyo <mmoyo@users.sf.net>
2012-03-20 12:50:42 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@18793 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/mmoyo/src/main/java/playground/mmoyo/ptRouterAdapted/AdaptedTransitRouterNetworkTravelTimeCost.java
+++ playgrounds/mmoyo/src/main/java/playground/mmoyo/ptRouterAdapted/AdaptedTransitRouterNetworkTravelTimeCost.java
@@ -22,7 +22,6 @@ package playground.mmoyo.ptRouterAdapted;
 
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.network.Link;
-import org.matsim.core.utils.misc.Time;
 import org.matsim.pt.router.TransitRouterNetworkTravelTimeAndDisutility;
 import org.matsim.pt.router.TransitRouterNetwork.TransitRouterNetworkLink;
 import org.matsim.pt.transitSchedule.api.TransitRouteStop;
@@ -31,12 +30,6 @@ import org.matsim.pt.transitSchedule.api.TransitRouteStop;
  *  This version of TransitRouterNetworkTravelTimeCost reads values from a MyTransitRouterConfig object
  */
 public class AdaptedTransitRouterNetworkTravelTimeCost extends TransitRouterNetworkTravelTimeAndDisutility {
-	private final static double MIDNIGHT = 24.0*3600;
-	
-	//these variables are protected in super class
-	private Link previousLink = null;
-	private double previousTime = Double.NaN;
-	private double cachedTravelTime = Double.NaN;
 
 	private static final Logger log = Logger.getLogger(AdaptedTransitRouterNetworkTravelTimeCost.class);
 
@@ -79,45 +72,7 @@ public class AdaptedTransitRouterNetworkTravelTimeCost extends TransitRouterNetw
 		}
 		return cost;
 	}
-	
-	@Override   //time from stop to next stop arrivals
-	public double getLinkTravelTime(final Link link, final double now) {
-		if ((link == this.previousLink) && (now == this.previousTime)) {
-			return this.cachedTravelTime;
-		}
-		this.previousLink = link;
-		this.previousTime = now;
 
-		TransitRouterNetworkLink wrapped = (TransitRouterNetworkLink) link;
-		TransitRouteStop fromStop = wrapped.fromNode.stop;
-		TransitRouteStop toStop = wrapped.toNode.stop;
-		if (wrapped.getRoute() != null) { // (agent is on board, so use transit route travel time)
-			
-			// this modified version considers only effective travel time in vehicle, not waiting time at station, Manuel
-
-			// the travel time on the link is 
-			//   the time until the departure (``dpTime - now'')
-			//   + the travel time on the link (there.arrivalTime - here.departureTime)
-			
-			// But quite often, we only have the departure time at the next stop.  Then we use that:
-			double toStopArrivalOffset = (toStop.getArrivalOffset() != Time.UNDEFINED_TIME) ? toStop.getArrivalOffset() : toStop.getDepartureOffset();
-			// in the same way, we might only have the departure time at the "from" stop.  Then we use that: //Manuel mar 2012
-			double fromStopArrivalOffset = (fromStop.getArrivalOffset() != Time.UNDEFINED_TIME) ? fromStop.getArrivalOffset() : fromStop.getDepartureOffset();
-			
-			double trueVehicleInMotionTimeToNextStop = toStopArrivalOffset - fromStopArrivalOffset ;//<--adapted here
-			if (trueVehicleInMotionTimeToNextStop < 0) {
-				// ( this can only happen, I think, when ``bestDepartureTime'' is after midnight but ``time'' was before )
-				trueVehicleInMotionTimeToNextStop += MIDNIGHT;
-			}
-			this.cachedTravelTime = trueVehicleInMotionTimeToNextStop;
-			return trueVehicleInMotionTimeToNextStop;
-		}
-		// different transit routes, so it must be a line switch
-		double distance = wrapped.getLength();
-		double time2 = distance / this.config.getBeelineWalkSpeed() + this.config.additionalTransferTime;
-		this.cachedTravelTime = time2;
-		return time2;
-	}
 	
 	
 	//variables for caching offVehWaitTime
@@ -125,7 +80,7 @@ public class AdaptedTransitRouterNetworkTravelTimeCost extends TransitRouterNetw
 	double previousWaitTime;
 	double cachedOffVehWaitTime;
 	
-	public double getVehArrivalTime(final Link link, final double now){
+	double getVehArrivalTime(final Link link, final double now){
 		if ((link == this.previousWaitLink) && (now == this.previousWaitTime)) {
 			return this.cachedOffVehWaitTime;
 		}
