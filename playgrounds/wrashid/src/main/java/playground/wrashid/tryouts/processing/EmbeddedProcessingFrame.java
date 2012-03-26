a6089e913a426b3b7525272e0d442c5946bbc08f
rashid_waraich <rashid_waraich@users.sf.net>
2012-03-26 16:58:54 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@18876 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/wrashid/src/main/java/playground/wrashid/tryouts/processing/EmbeddedProcessingFrame.java
@@ -0,0 +1,85 @@
+package playground.wrashid.tryouts.processing;
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.Scrollbar;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import processing.core.PApplet;
+
+public class EmbeddedProcessingFrame extends Frame {
+
+	NetworkWithHeatMap networkWithHeatMap;
+	TextField tf1 ;
+	Scrollbar sb;
+	TextField tf2 ;
+	public EmbeddedProcessingFrame() {
+        super("Embedded PApplet");
+        setSize(1200,1100);   
+        setLayout(new FlowLayout());
+        networkWithHeatMap=new NetworkWithHeatMap();
+        add(networkWithHeatMap);
+        tf1 = new TextField();
+        tf1.setText(Double.toString(networkWithHeatMap.getMaxDistanceInMeters()));
+        add(tf1);
+        Button btn1 = new Button("Redraw");
+        
+        btn1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				networkWithHeatMap.setMaxDistanceInMeters(Double.parseDouble(tf1.getText()));
+				
+			}
+		});
+        add(btn1);
+        
+        
+        
+        
+        sb=new  Scrollbar(Scrollbar.HORIZONTAL, 1, 100, 1, 1000);
+        //sb.setSize(500, 40);
+        add(sb);
+        sb.addAdjustmentListener(new AdjustmentListener() {
+			
+			@Override
+			public void adjustmentValueChanged(AdjustmentEvent e) {
+				int value = sb.getValue();
+				networkWithHeatMap.setScaler(value);
+				tf2.setText(String.valueOf(value));
+			}
+		});
+        
+        tf2=new TextField("1");
+        add(tf2);
+        
+        // important to call this whenever embedding a PApplet.
+        // It ensures that the animation thread is started and
+        // that other internal variables are properly set.
+        networkWithHeatMap.init();
+        setVisible(true);
+    }
+	
+	class TestWindowListener extends WindowAdapter
+	  {
+	    public void windowClosing(WindowEvent e)
+	    {
+	      e.getWindow().dispose();                   // Fenster "killen"
+	      System.exit(0);                            // VM "killen" 
+	    }           
+	  }
+	
+	public static void main (String args[]) 
+	  {
+	    new EmbeddedProcessingFrame ();
+	  }
+	
+}
--- playgrounds/wrashid/src/main/java/playground/wrashid/tryouts/processing/NetworkWithHeatMap.java
+++ playgrounds/wrashid/src/main/java/playground/wrashid/tryouts/processing/NetworkWithHeatMap.java
@@ -8,16 +8,24 @@ import org.matsim.core.utils.geometry.CoordImpl;
 import playground.wrashid.lib.GeneralLib;
 import processing.core.*;
 
+// how to zoom properly + select area?
+// how to put time bar on right side
+// draw parking?
+// draw charging behaviour
+
 public class NetworkWithHeatMap extends PApplet {
 
-	float scaler = 1;
+	private float scaler = 1;
 	private Network network;
 	float smallestX = Float.MAX_VALUE;
 	float smallestY = Float.MAX_VALUE;
 	float biggestX = Float.MIN_VALUE;
 	float biggestY = Float.MIN_VALUE;
+	private double maxDistanceInMeters = 500000;
 
 	public void setup() {
+		
+		
 		size(1000, 1000);
 		smooth();
 		noStroke();
@@ -53,9 +61,9 @@ public class NetworkWithHeatMap extends PApplet {
 	public void draw() {
 
 		strokeWeight((float) 0.01);
-		translate(width / 2, height / 2); // use translate around scale
-		scale(scaler);
-		translate(-width / 6, (float) (-height / 1.05)); // to scale from the
+		//translate(width / 2, height / 2); // use translate around scale
+		scale(getScaler());
+		//translate(-width / 6, (float) (-height / 1.05)); // to scale from the
 															// center
 
 		background(255);
@@ -77,7 +85,7 @@ public class NetworkWithHeatMap extends PApplet {
 		// function.
 
 		CoordImpl zuerichCoord = new CoordImpl(683248, 248161);
-		double maxDistanceInMeters = 500000;
+		
 
 		float scalingFactor = width / (biggestX - smallestX);
 		// System.out.println(scalingFactor);
@@ -88,19 +96,19 @@ public class NetworkWithHeatMap extends PApplet {
 			float xEnd = (float) link.getToNode().getCoord().getX() * scalingFactor;
 			float yEnd = height - (float) link.getToNode().getCoord().getY() * scalingFactor;
 
-			if (GeneralLib.getDistance(zuerichCoord, link.getFromNode().getCoord()) < maxDistanceInMeters / 2) {
+			if (GeneralLib.getDistance(zuerichCoord, link.getFromNode().getCoord()) < getMaxDistanceInMeters() / 2) {
 
 				float distanceScaled = (float) ((GeneralLib.getDistance(zuerichCoord, link.getFromNode().getCoord()))
-						/ (maxDistanceInMeters / 2));
+						/ (getMaxDistanceInMeters() / 2));
 
 				float r = 255 * (1 - distanceScaled);
 				float g = 255 * distanceScaled;
 
 				stroke(r, g, 0);
-			} else if (GeneralLib.getDistance(zuerichCoord, link.getFromNode().getCoord()) < maxDistanceInMeters) {
+			} else if (GeneralLib.getDistance(zuerichCoord, link.getFromNode().getCoord()) < getMaxDistanceInMeters()) {
 
-				float distanceScaled = (float) ((GeneralLib.getDistance(zuerichCoord, link.getFromNode().getCoord()) - (maxDistanceInMeters / 2))
-						/ (maxDistanceInMeters / 2));
+				float distanceScaled = (float) ((GeneralLib.getDistance(zuerichCoord, link.getFromNode().getCoord()) - (getMaxDistanceInMeters() / 2))
+						/ (getMaxDistanceInMeters() / 2));
 
 				float g = 255 * (1 - distanceScaled);
 				float b = 255 * distanceScaled;
@@ -117,13 +125,29 @@ public class NetworkWithHeatMap extends PApplet {
 
 	public void keyPressed() {
 		if (key == 'z') {
-			scaler -= 0.1;
+			setScaler(getScaler() - 0.2f);
 		} // smaller
 		if (key == 'x') {
-			scaler += 0.1;
+			setScaler(getScaler() + 0.2f);
 		} // bigger
 		if (key == 'c') {
-			scaler = 1;
+			setScaler(1);
 		} // reset scale
+	}
+
+	public void setMaxDistanceInMeters(double maxDistanceInMeters) {
+		this.maxDistanceInMeters = maxDistanceInMeters;
+	}
+
+	public double getMaxDistanceInMeters() {
+		return maxDistanceInMeters;
+	}
+
+	public void setScaler(float scaler) {
+		this.scaler = scaler;
+	}
+
+	public float getScaler() {
+		return scaler;
 	}
 }
