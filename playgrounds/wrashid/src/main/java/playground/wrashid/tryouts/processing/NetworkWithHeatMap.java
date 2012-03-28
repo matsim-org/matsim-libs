04d8d20f3aa5209cf081c03fef85571d42575d09
rashid_waraich <rashid_waraich@users.sf.net>
2012-03-28 13:08:03 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@18918 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/main/java/playground/wrashid/tryouts/processing/NetworkWithHeatMap.java
+++ playgrounds/wrashid/src/main/java/playground/wrashid/tryouts/processing/NetworkWithHeatMap.java
@@ -7,6 +7,8 @@ import java.awt.event.MouseMotionListener;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MoveAction;
+
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.network.Node;
@@ -20,32 +22,19 @@ import processing.core.*;
 // draw parking?
 // draw charging behaviour
 
-public class NetworkWithHeatMap extends PApplet {
+public class NetworkWithHeatMap extends MovableAndZoomable {
 
-	private float scaler = 1;
 	private Network network;
 	float smallestX = Float.MAX_VALUE;
 	float smallestY = Float.MAX_VALUE;
 	float biggestX = Float.MIN_VALUE;
 	float biggestY = Float.MIN_VALUE;
 	private double maxDistanceInMeters = 500000;
-	private float moveTranslateX=0;
-	private float moveTranslateY=0;
-	
-	
+
 	public void setup() {
-
-		addMouseWheelListener(new MouseWheelListener() {
-
-			@Override
-			public void mouseWheelMoved(MouseWheelEvent e) {
-				adjustScaleFactor(e);
-			}
-		});
-
-		size(1000, 1000);
-		smooth();
-		noStroke();
+		super.setup();
+		//smooth();
+		//noStroke();
 
 		network = GeneralLib.readNetwork("H:/data/THELMA/for psi/10pctSwitzerland-ivtch-27.02.2012/network.xml");
 		// network =
@@ -73,89 +62,21 @@ public class NetworkWithHeatMap extends PApplet {
 
 		}
 
-	addMouseMotionListener(new MouseMotionListener() {
-			
-			@Override
-			public void mouseMoved(MouseEvent e) {
-				mouseX = e.getX();
-				mouseY = e.getY();
-			}
-			
-			@Override
-			public void mouseDragged(MouseEvent e) {
-				//System.out.println(moveTranslateX);
-				//System.out.println(moveTranslateY);
-				moveTranslateX+=e.getX()-mouseX;
-				moveTranslateY+=e.getY()-mouseY;
-				mouseX = e.getX();
-				mouseY = e.getY();
-			
-			}
-		});
-	
-	addMouseListener(new MouseListener() {
-		
-		@Override
-		public void mouseReleased(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void mousePressed(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void mouseExited(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void mouseEntered(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void mouseClicked(MouseEvent e) {
-			if (mouseEvent.getClickCount()==2) println("<double click>");
-			
-		}
-	});
-		
 	}
 
-	public void adjustScaleFactor(MouseWheelEvent e) {
-
-		if (e.getWheelRotation() < 0) {
-			setScaler(getScaler() + 0.1f);
-		}
-
-		if (e.getWheelRotation() > 0) {
-			setScaler(getScaler() - 0.1f);
-		}
-		
-	}
-
+	@Override
 	public void draw() {
+		super.draw();
 
-		
 		strokeWeight((float) 0.01);
+
+		// the following line is very important, because when moving/scaling, one sees
+		// the old locations.
+		background(255);
 		
 
-		translate(moveTranslateX,moveTranslateY);
-		translate(-scaler*width/2,-scaler*height/2);
-		scale(scaler);
-	
-
-		background(255);
-	
 		drawNetwork();
 
-	
 	}
 
 	private void drawNetwork() {
@@ -196,18 +117,6 @@ public class NetworkWithHeatMap extends PApplet {
 			line(xStart, yStart, xEnd, yEnd);
 		}
 
-	}
-
-	public void keyPressed() {
-		if (key == 'z') {
-			setScaler(getScaler() - 0.2f);
-		} // smaller
-		if (key == 'x') {
-			setScaler(getScaler() + 0.2f);
-		} // bigger
-		if (key == 'c') {
-			setScaler(1);
-		} // reset scale
 	}
 
 	public void setMaxDistanceInMeters(double maxDistanceInMeters) {
