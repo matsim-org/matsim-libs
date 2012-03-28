4d9f7581d7ca47b5b57dcca5301879f6aa402e06
vsp_nagel <2008@kainagel.org>
2012-03-28 14:41:35 +0000

    moving gauteng stuff from kai playground into southafrica playground
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@18925 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/southAfrica/src/main/java/playground/southafrica/gauteng/GautengControler.java
@@ -0,0 +1,83 @@
+package playground.southafrica.gauteng;
+
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.controler.Controler;
+import org.matsim.roadpricing.RoadPricingSchemeI;
+
+import playground.kai.run.KaiAnalysisListener;
+import playground.southafrica.gauteng.roadpricingscheme.GautengRoadPricingScheme;
+import playground.southafrica.gauteng.routing.GautengTravelDisutilityInclTollFactory;
+import playground.southafrica.gauteng.scoring.GautengScoringFunctionFactory;
+import playground.southafrica.gauteng.scoring.GenerationOfMoneyEvents;
+import playground.southafrica.gauteng.utilityofmoney.GautengUtilityOfMoney;
+import playground.southafrica.gauteng.utilityofmoney.UtilityOfMoneyI;
+
+/**
+ * Design comments:<ul>
+ * <li> The money (toll) is converted into utils both for the router and for the scoring.
+ * <li> However, setting up the toll scheme in terms of disutilities does not seem right.
+ * </ul>
+ *
+ */
+class GautengControler {
+	static Logger log = Logger.getLogger(GautengControler.class) ;
+	
+	public static void main ( String[] args ) {
+
+		String configFileName = "/Users/nagel/ie-calvin/MATSim-SA/trunk/data/sanral2010/config/kaiconfig.xml" ;
+
+		final Controler controler = new Controler( configFileName ) ;
+
+		controler.setOverwriteFiles(true) ;
+		
+		Scenario sc = controler.getScenario();
+
+		
+		
+		if (sc.getConfig().scenario().isUseRoadpricing()) {
+			throw new RuntimeException("roadpricing must NOT be enabled in config.scenario in order to use special " +
+					"road pricing features.  aborting ...");
+		}
+
+		
+		// CONSTRUCT VEH-DEP ROAD PRICING SCHEME:
+		RoadPricingSchemeI vehDepScheme = 
+			new GautengRoadPricingScheme( sc.getConfig(), sc.getNetwork() , sc.getPopulation() );
+
+		// CONSTRUCT UTILITY OF MONEY:
+		UtilityOfMoneyI personSpecificUtilityOfMoney = new GautengUtilityOfMoney( sc.getConfig().planCalcScore() ) ;
+
+		// INSTALL ROAD PRICING (in the longer run, re-merge with RoadPricing class):
+		// insert into scoring:
+		controler.addControlerListener(
+				new GenerationOfMoneyEvents( sc.getNetwork(), sc.getPopulation(), vehDepScheme) 
+		) ;
+		
+		
+		controler.setScoringFunctionFactory(
+				new GautengScoringFunctionFactory(sc.getConfig(), sc.getNetwork(), personSpecificUtilityOfMoney )
+		);
+
+		// insert into routing:
+		controler.setTravelDisutilityFactory( 
+				new GautengTravelDisutilityInclTollFactory( vehDepScheme, personSpecificUtilityOfMoney ) 
+		);
+		
+		
+		
+		// ADDITIONAL ANALYSIS:
+		// This is not truly necessary.  It could be removed or copied in order to remove the dependency on the kai
+		// playground.  For the time being, I (kai) would prefer to leave it the way it is since I am running the Gauteng
+		// scenario and I don't want to maintain two separate analysis listeners.  But once that period is over, this
+		// argument does no longer apply.  kai, mar'12
+		controler.addControlerListener(new KaiAnalysisListener()) ;
+		
+		// RUN:
+		controler.run();
+	
+	}
+
+
+
+}
