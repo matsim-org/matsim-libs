e3c381e02e3bea4d7800ea2f8dbd789960699d96
vsp_nagel <2008@kainagel.org>
2012-03-28 14:44:03 +0000

    moving gauteng from kai playground to southafrica playground
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@18926 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/kai/src/main/java/playground/kai/gauteng/utilityofmoney/UtilityOfMoneyI.java
+++
@@ -1,7 +0,0 @@
-package playground.kai.gauteng.utilityofmoney;
-
-import org.matsim.api.core.v01.Id;
-
-public interface UtilityOfMoneyI {
-	double getUtilityOfMoney_normally_positive( Id personId ) ;
-}
