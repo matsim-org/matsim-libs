380c1ece2f22cf60f268401e27cafd6358652614
yu_vsp <yu_vsp@users.sf.net>
2012-03-29 11:25:38 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@18940 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/yu/src/main/java/playground/yu/integration/cadyts/parameterCalibration/withCarCounts/testLls/ParameterEstimator.java
+++ playgrounds/yu/src/main/java/playground/yu/integration/cadyts/parameterCalibration/withCarCounts/testLls/ParameterEstimator.java
@@ -23,20 +23,59 @@ package playground.yu.integration.cadyts.parameterCalibration.withCarCounts.test
 import org.ejml.alg.dense.linsol.SolvePseudoInverse;
 import org.ejml.data.DenseMatrix64F;
 
-/**
- *
+/***
+ * tries to solve the overdetermined problem of:
+ * <p>
+ * [Attr]<sub>m x n</sub> x [&Delta;&beta;]<sub>n x 1</sub> = [utility
+ * correction (from cadyts)]<sub>m x 1</sub>,
+ * <p>
+ * [Attr]<sub>m x n</sub> and [utility correction]<sub>m x 1</sub> are gegeben.
+ * <p>
+ * In order to realize min||[utility correction]<sub>m x 1</sub>- [Attr]<sub>m x
+ * n</sub> x [&Delta;&beta;]<sub>n x 1</sub>||<sup>2</sup> (The method of least
+ * squares can be used to find an approximate solution),
+ * <p>
+ * [&Delta;&beta;]<sub>n x 1</sub> (vector) is estimated in this class with
+ * Moore-Penrose pseudoinverse ( (Attr<sup>T</sup>Attr)&Delta;&beta; =
+ * Attr<sup>T</sup>UC &rarr; &Delta;&beta; = Attr<sup>+</sup>UC )
+ * <p>
+ * (http://en.wikipedia.org/wiki/Moore%E2%80%93Penrose_pseudoinverse),
+ * <p>
+ * and the code from EJML
+ * (http://code.google.com/p/efficient-java-matrix-library)
+ * 
  * @author yu
- *
+ * 
  */
 public class ParameterEstimator {
 	private DenseMatrix64F attrM = null, utilCorrV = null;
 
 	/**
+	 * A small [Attr]<sub>m x n</sub> x [&Delta;&beta;]<sub>n x 1</sub> =
+	 * [utility correction]<sub>m x 1</sub>.
+	 * <P>
+	 * [Attr]<sub>3 x 2</sub> = (1, 0) (1, 1) (1, 3),
+	 * <p>
+	 * [utility correction]<sub>3 x 1</sub> = (4, 3, 2)<sup>T</sup>.
+	 * <p>
+	 * The result should be (27/7, -9/14)<sup>T</sup>
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ParameterEstimator pe = new ParameterEstimator();
 
+		pe.setAttrM(new DenseMatrix64F(new double[][] { { 1d, 0d }, { 1d, 1d },
+				{ 1d, 3d } }));
+		System.out.println("AttrM:\n|1, 0|\n|1, 1|\n|1, 3|");
+
+		pe.setUtilCorrV(new DenseMatrix64F(
+				new double[][] { { 4 }, { 3 }, { 2 } }));
+		System.out.println("UtilCorrM:\n|4|\n|3|\n|2|");
+
+		DenseMatrix64F betaM = pe.getDeltaParameters();
+		System.out.println("betaM:\n|" + betaM.get(0, 0) + "|\n|"
+				+ betaM.get(1, 0) + "|");
 	}
 
 	/**
