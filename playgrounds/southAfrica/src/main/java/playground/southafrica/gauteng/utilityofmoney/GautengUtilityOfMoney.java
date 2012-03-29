2fb24684d5041a590373e24c6500cb162e52c445
vsp_nagel <2008@kainagel.org>
2012-03-29 11:04:59 +0000

    added code that would print out the various values of travel time savings.  Extracted some methods for that.  Also set the tolls for commercial vehicles to some reasonable levels.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@18938 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/southAfrica/src/main/java/playground/southafrica/gauteng/utilityofmoney/GautengUtilityOfMoney.java
+++ playgrounds/southAfrica/src/main/java/playground/southafrica/gauteng/utilityofmoney/GautengUtilityOfMoney.java
@@ -3,10 +3,12 @@
  */
 package playground.southafrica.gauteng.utilityofmoney;
 
+import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Id;
 import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
 
 import playground.southafrica.gauteng.roadpricingscheme.SanralTollFactor;
+import playground.southafrica.gauteng.roadpricingscheme.SanralTollFactor.Type;
 
 /**This is a comment.
  * @author nagel
@@ -18,35 +20,61 @@ public class GautengUtilityOfMoney implements UtilityOfMoneyI {
 
 	public GautengUtilityOfMoney( final PlanCalcScoreConfigGroup cnScoringGroup ) {
 		this.planCalcScore = cnScoringGroup ;
+		for ( Type vehType : Type.values() ) {
+			Logger.getLogger(this.getClass()).info( " vehType: " + vehType.toString() 
+					+ "; utility of travel time savings per hr: " + getUtilityOfTravelTime_hr()
+					+ "; value of travel time savings per hr: " + getValueOfTime_hr(vehType)
+					+ "; => utility of money: " + getUtilityOfMoneyFromValueOfTime( getValueOfTime_hr(vehType)) ) ;
+		}
 	}
 
 	public double getUtilityOfMoney_normally_positive(final Id personId ) {
-		double valueOfTime_hr = 100 ;
-		switch( SanralTollFactor.typeOf(personId) ) {
-		case carWithTag:
-		case carWithoutTag:
-			break ;
-		case commercialClassBWithTag:
-		case commercialClassCWithTag:
-		case commercialClassBWithoutTag:
-		case commercialClassCWithoutTag:
-		case busWithTag:
-		case busWithoutTag:
-		case taxiWithTag:
-		case taxiWithoutTag:
-		case extWithTag:
-		case extWithoutTag:
-			valueOfTime_hr = 1000.;
-			break ;
-		}
+		Type vehicleType = SanralTollFactor.typeOf(personId);
+		double valueOfTime_hr = getValueOfTime_hr(vehicleType);
+		double utilityOfMoney = getUtilityOfMoneyFromValueOfTime(valueOfTime_hr);
+		
+		return utilityOfMoney ;
+	}
+
+	private double getUtilityOfMoneyFromValueOfTime(double valueOfTime_hr) {
+		final double utilityOfTravelTime_hr = getUtilityOfTravelTime_hr();
+	
+		double utilityOfMoney = utilityOfTravelTime_hr / valueOfTime_hr ;
+		return utilityOfMoney;
+	}
+
+	private double getUtilityOfTravelTime_hr() {
 		final double utilityOfTravelTime_hr = 
 			this.planCalcScore.getPerforming_utils_hr() - this.planCalcScore.getTraveling_utils_hr() ;
 		// "performing" is normally positive
 		// "traveling" is normally negative
-	
-		double utilityOfMoney = utilityOfTravelTime_hr / valueOfTime_hr ;
-		
-		return utilityOfMoney ;
+		return utilityOfTravelTime_hr;
+	}
+
+	private double getValueOfTime_hr(Type vehicleType) {
+		double valueOfTime_hr = 100 ;
+		switch( vehicleType ) {
+		case carWithTag:
+		case carWithoutTag:
+			break ;
+		case commercialClassBWithTag:
+		case commercialClassBWithoutTag:
+		case busWithTag:
+		case busWithoutTag:
+			valueOfTime_hr = 300. ; 
+			break;
+		case commercialClassCWithTag:
+		case commercialClassCWithoutTag:
+			valueOfTime_hr = 600. ; 
+			break ;
+		case taxiWithTag:
+		case taxiWithoutTag:
+		case extWithTag:
+		case extWithoutTag:
+			valueOfTime_hr = 100.;
+			break ;
+		}
+		return valueOfTime_hr;
 	}
 
 }
