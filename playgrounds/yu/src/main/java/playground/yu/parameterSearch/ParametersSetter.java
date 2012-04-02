e8e7237a23fc969f0cfc9a054d0009b40ba43d46
yu_vsp <yu_vsp@users.sf.net>
2012-04-02 21:42:20 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@18992 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/yu/src/main/java/playground/yu/parameterSearch/ParametersSetter.java
@@ -0,0 +1,62 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * ParametersSetter.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+/**
+ * 
+ */
+package playground.yu.parameterSearch;
+
+import org.matsim.core.config.Config;
+import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.utils.collections.Tuple;
+
+import playground.yu.integration.cadyts.CalibrationConfig;
+import playground.yu.scoring.withAttrRecorder.leftTurn.CharyparNagelScoringFunctionFactoryWithLeftTurnPenalty;
+
+/**
+ * sets parameters of scoringfunction into config and also into
+ * {@code Controler}
+ * 
+ * @author yu
+ * 
+ */
+public class ParametersSetter {
+	public static void setParameters(Controler ctl,
+			Tuple<String, Double>... parameters) {
+		// set new parameters in config
+		Config config = ctl.getConfig();
+		PlanCalcScoreConfigGroup scoringCfg = config.planCalcScore();
+		for (Tuple<String, Double> parameter : parameters) {
+			String name = parameter.getFirst();
+			String value = Double.toString(parameter.getSecond());
+
+			if (scoringCfg.getParams().containsKey(name)) {
+				scoringCfg.addParam(name, value);
+			} else {
+				config.setParam(CalibrationConfig.BSE_CONFIG_MODULE_NAME, name,
+						value);
+			}
+		}
+		// set new parameters in Controler
+		ctl.setScoringFunctionFactory(new CharyparNagelScoringFunctionFactoryWithLeftTurnPenalty(
+				config, ctl.getNetwork()));
+	}
+}
