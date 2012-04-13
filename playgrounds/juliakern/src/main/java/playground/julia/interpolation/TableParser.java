a12c1f0bbd5db2f613098dc6dbef1cdc55b69668
juliakern <juliakern@users.sf.net>
2012-04-13 09:03:32 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@19126 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/juliakern/src/main/java/playground/julia/interpolation/TableParser.java
+++ playgrounds/juliakern/src/main/java/playground/julia/interpolation/TableParser.java
@@ -1,3 +1,23 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * TableParser.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2009 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
 package playground.julia.interpolation;
 
 import java.io.BufferedReader;
@@ -132,6 +152,7 @@ public class TableParser {
 			valueString="";
 		}
 		buff.close();
+		System.out.println("new out");
 	}
 
 	private static String getStringFromEValue(Double value) {
