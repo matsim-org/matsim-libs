63952acc1c094ef590a33d1c18ec6a967a3c8999
juliakern <juliakern@users.sf.net>
2012-04-18 15:14:20 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@19194 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/juliakern/src/test/java/playground/julia/incomeScoring/EmissionRoutingTest.java
+++ playgrounds/juliakern/src/test/java/playground/julia/incomeScoring/EmissionRoutingTest.java
@@ -121,7 +121,6 @@ public class EmissionRoutingTest extends MatsimTestCase{
 		};
 		
 		this.controler.addControlerListener(startupListener);
-		this.controler.setDumpDataAtEnd(true);
 		this.controler.run();
 		assertTrue("Person was expected to be routed through link 11, but was " + handler.getLink(), RoadUsedHandler.getWasRoadSelected()==true);
 	}
@@ -160,7 +159,6 @@ public class EmissionRoutingTest extends MatsimTestCase{
 		};
 		
 		this.controler.addControlerListener(startupListener);
-		this.controler.setDumpDataAtEnd(true);
 		this.controler.run();
 		assertTrue("Person was expected to be routed through link 13, but was " + handler.getLink(), RoadUsedHandler.getWasRoadSelected()==true);
 		
