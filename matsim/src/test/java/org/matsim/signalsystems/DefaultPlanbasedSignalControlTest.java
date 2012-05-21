b36a824dab1f931ad7712142843b81294a9789eb
grether <dominik.grether@gmail.com>
2012-05-21 21:07:10 +0000

    added test case for multiple fixed-time signal control plans over mobsim runtime
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@19649 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/test/java/org/matsim/signalsystems/DefaultPlanbasedSignalControlTest.java
@@ -0,0 +1,194 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * DefaultPlanbasedSignalControlTest
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package org.matsim.signalsystems;
+
+import junit.framework.Assert;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.matsim.core.basic.v01.IdImpl;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.QSimConfigGroup;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.events.StartupEvent;
+import org.matsim.core.controler.listener.StartupListener;
+import org.matsim.core.events.SignalGroupStateChangedEvent;
+import org.matsim.core.events.handler.SignalGroupStateChangedEventHandler;
+import org.matsim.signalsystems.model.SignalGroupState;
+import org.matsim.testcases.MatsimTestUtils;
+
+
+/**
+ * Test for the default fixed-time control implementation of MATSim.
+ * @author dgrether
+ *
+ */
+public class DefaultPlanbasedSignalControlTest {
+
+	@Rule
+	public MatsimTestUtils utils = new MatsimTestUtils();
+	
+	/**
+	 * Tests fixed-time control, especially for multiple fixed-time plans over a day.
+	 */
+	@Test
+	public final void planSwitchingTest() {
+		Config config = ConfigUtils.createConfig();
+		config.controler().setFirstIteration(0);
+		config.controler().setLastIteration(0);
+		config.controler().setOutputDirectory(utils.getOutputDirectory());
+		//qsim
+		config.controler().setMobsim("qsim");
+		QSimConfigGroup qsimConfig = new QSimConfigGroup();
+		config.addQSimConfigGroup(qsimConfig);
+		qsimConfig.setStartTime(0.0);
+		qsimConfig.setEndTime(400.0);
+		qsimConfig.setSimStarttimeInterpretation(QSimConfigGroup.ONLY_USE_STARTTIME);
+	
+		String inputDir = utils.getClassInputDirectory();
+		//scenario
+		config.scenario().setUseSignalSystems(true);
+		String networkInputFile = inputDir + "network.xml";
+		config.network().setInputFile(networkInputFile);
+		//signals
+		String signalsFile = inputDir + "signal_systems.xml";
+		config.signalSystems().setSignalSystemFile(signalsFile);
+		String signalGroupsFile = inputDir + "signal_groups.xml";
+		config.signalSystems().setSignalGroupsFile(signalGroupsFile);
+		String signalControlFile = inputDir + "signal_control.xml";
+		config.signalSystems().setSignalControlFile(signalControlFile);
+	
+		Controler controler = new Controler(config);
+		controler.setCreateGraphs(false);
+		controler.setDumpDataAtEnd(false);
+		PlanSwitchingTestListener testListener = new PlanSwitchingTestListener();
+		controler.addControlerListener(testListener);
+		controler.run();
+		Assert.assertTrue(testListener.catchedAllEvents);
+	}
+
+
+	private static class PlanSwitchingTestListener implements StartupListener, SignalGroupStateChangedEventHandler {
+
+		boolean catchedAllEvents = true;
+		
+		@Override
+		public void notifyStartup(StartupEvent e) {
+			e.getControler().getEvents().addHandler(this);
+		}
+
+		@Override
+		public void handleEvent(SignalGroupStateChangedEvent e) {
+			if (e.getTime() == 0.0){
+				if (e.getSignalGroupId().equals(new IdImpl("4"))){
+					Assert.assertEquals(SignalGroupState.GREEN, e.getNewState());
+				}
+				else if (e.getSignalGroupId().equals(new IdImpl("5"))){
+					Assert.assertEquals(SignalGroupState.RED, e.getNewState());
+				}
+			}
+			else if (e.getTime() == 45.0){
+				if (e.getSignalGroupId().equals(new IdImpl("4"))){
+					Assert.assertEquals(SignalGroupState.RED, e.getNewState());
+				}
+				else if (e.getSignalGroupId().equals(new IdImpl("5"))){
+					Assert.assertEquals(SignalGroupState.GREEN, e.getNewState());
+				}
+			}
+			else if (e.getTime() == 90.0){
+				if (e.getSignalGroupId().equals(new IdImpl("4"))){
+					Assert.assertEquals(SignalGroupState.GREEN, e.getNewState());
+				}
+				else if (e.getSignalGroupId().equals(new IdImpl("5"))){
+					Assert.assertEquals(SignalGroupState.RED, e.getNewState());
+				}
+			}
+			else if (e.getTime() == 135.0){
+				if (e.getSignalGroupId().equals(new IdImpl("4"))){
+					Assert.assertEquals(SignalGroupState.RED, e.getNewState());
+				}
+				else if (e.getSignalGroupId().equals(new IdImpl("5"))){
+					Assert.assertEquals(SignalGroupState.GREEN, e.getNewState());
+				}
+			}
+			else if (e.getTime() == 165.0){
+				if (e.getSignalGroupId().equals(new IdImpl("4"))){
+					Assert.assertEquals(SignalGroupState.GREEN, e.getNewState());
+				}
+				else if (e.getSignalGroupId().equals(new IdImpl("5"))){
+					Assert.assertEquals(SignalGroupState.RED, e.getNewState());
+				}
+			}
+			else if (e.getTime() == 195.0){
+				if (e.getSignalGroupId().equals(new IdImpl("4"))){
+					Assert.assertEquals(SignalGroupState.RED, e.getNewState());
+				}
+				else if (e.getSignalGroupId().equals(new IdImpl("5"))){
+					Assert.assertEquals(SignalGroupState.GREEN, e.getNewState());
+				}
+			}
+			else if (e.getTime() == 210.0){
+					Assert.assertEquals(SignalGroupState.YELLOW, e.getNewState());
+			}
+			else if (e.getTime() == 215.0){
+					Assert.assertEquals(SignalGroupState.OFF, e.getNewState());
+			}
+			else if (e.getTime() == 240.0){
+				if (e.getSignalGroupId().equals(new IdImpl("4"))){
+					Assert.assertEquals(SignalGroupState.GREEN, e.getNewState());
+				}
+				else if (e.getSignalGroupId().equals(new IdImpl("5"))){
+					Assert.assertEquals(SignalGroupState.RED, e.getNewState());
+				}
+			}
+			else if (e.getTime() == 255.0){
+				if (e.getSignalGroupId().equals(new IdImpl("4"))){
+					Assert.assertEquals(SignalGroupState.RED, e.getNewState());
+				}
+				else if (e.getSignalGroupId().equals(new IdImpl("5"))){
+					Assert.assertEquals(SignalGroupState.GREEN, e.getNewState());
+				}
+			}
+			else if (e.getTime() == 300.0){
+				if (e.getSignalGroupId().equals(new IdImpl("4"))){
+					Assert.assertEquals(SignalGroupState.GREEN, e.getNewState());
+				}
+				else if (e.getSignalGroupId().equals(new IdImpl("5"))){
+					Assert.assertEquals(SignalGroupState.RED, e.getNewState());
+				}
+			}
+			else if (e.getTime() == 315.0){
+				Assert.assertEquals(SignalGroupState.YELLOW, e.getNewState());
+			}
+			else if (e.getTime() == 320.0){
+				Assert.assertEquals(SignalGroupState.OFF, e.getNewState());
+			}
+			else {
+				catchedAllEvents = false;
+			}
+		}
+
+		@Override
+		public void reset(int iteration) {}
+		
+	}
+	
+}
