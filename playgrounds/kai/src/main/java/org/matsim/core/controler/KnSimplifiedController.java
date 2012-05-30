731b761606d41a710988cae299f19b1c1c69e728
vsp_nagel <2008@kainagel.org>
2012-05-30 17:25:14 +0000

    playground changes.  removing gautengGautengOwnController since that was not functional (in fact moving it to kai playground)
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@19749 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/kai/src/main/java/org/matsim/core/controler/KnSimplifiedController.java
+++ playgrounds/kai/src/main/java/org/matsim/core/controler/KnSimplifiedController.java
@@ -18,7 +18,7 @@
  *                                                                         *
  * *********************************************************************** */
 
-package playground.southafrica.gauteng;
+package org.matsim.core.controler;
 
 import java.io.File;
 import java.io.IOException;
@@ -38,22 +38,22 @@ import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.config.ConfigWriter;
 import org.matsim.core.config.consistency.ConfigConsistencyCheckerImpl;
-import org.matsim.core.controler.ControlerIO;
 import org.matsim.core.controler.corelisteners.EventsHandling;
 import org.matsim.core.controler.corelisteners.PlansDumping;
 import org.matsim.core.controler.corelisteners.PlansReplanning;
 import org.matsim.core.controler.corelisteners.PlansScoring;
-import org.matsim.core.controler.corelisteners.RoadPricing;
-import org.matsim.core.controler.listener.ControlerListener;
 import org.matsim.core.events.EventsManagerImpl;
 import org.matsim.core.events.EventsUtils;
 import org.matsim.core.events.parallelEventsHandler.ParallelEventsManagerImpl;
 import org.matsim.core.gbl.Gbl;
 import org.matsim.core.network.NetworkWriter;
+import org.matsim.core.population.PopulationFactoryImpl;
 import org.matsim.core.population.PopulationWriter;
+import org.matsim.core.population.routes.ModeRouteFactory;
 import org.matsim.core.replanning.StrategyManager;
-import org.matsim.core.router.costcalculators.TravelCostCalculatorFactoryImpl;
-import org.matsim.core.router.costcalculators.TravelDisutilityFactory;
+import org.matsim.core.replanning.StrategyManagerConfigLoader;
+import org.matsim.core.router.PlansCalcRoute;
+import org.matsim.core.router.costcalculators.TravelTimeAndDistanceBasedTravelDisutility;
 import org.matsim.core.router.util.DijkstraFactory;
 import org.matsim.core.router.util.LeastCostPathCalculatorFactory;
 import org.matsim.core.router.util.TravelDisutility;
@@ -65,12 +65,13 @@ import org.matsim.core.trafficmonitoring.TravelTimeCalculatorFactory;
 import org.matsim.core.trafficmonitoring.TravelTimeCalculatorFactoryImpl;
 import org.matsim.core.utils.io.CollectLogMessagesAppender;
 import org.matsim.core.utils.io.IOUtils;
+import org.matsim.population.algorithms.PlanAlgorithm;
 
 /**
  * @author nagel
  *
  */
-public class GautengOwnController {
+public class KnSimplifiedController {
 	
 	public static final String DIRECTORY_ITERS = "ITERS";
 	public static final String FILENAME_EVENTS_TXT = "events.txt.gz";
@@ -86,7 +87,7 @@ public class GautengOwnController {
 	// (yy could use the above from the official Controler ... but want to live without
 	// the import statement for the time being.  kai, mar'12)
 
-	private static final Logger log = Logger.getLogger(GautengOwnController.class);
+	private static final Logger log = Logger.getLogger(KnSimplifiedController.class);
 
 	private CollectLogMessagesAppender collectLogMessagesAppender;
 	private String dtdFileName;
@@ -111,34 +112,26 @@ public class GautengOwnController {
 	private boolean dumpDataAtEnd;
 	private EventsManager events;
 	
-//	private final ControlerListenerManager controlerListenerManager = new ControlerListenerManager(this);
+	Controler dummyCtrl = new Controler(scenarioData) ;
+	private final ControlerListenerManager controlerListenerManager = new ControlerListenerManager(dummyCtrl);
+	// these cannot be used, since the controler events assume that the controler including all of its interface is passed
+	// to the plugins.  And that seems much more access than we want to provide here. ????  kai, may'12 
 
 	
 	private TravelTimeCalculatorFactory travelTimeCalculatorFactory = new TravelTimeCalculatorFactoryImpl();
-	private TravelDisutilityFactory travelCostCalculatorFactory = new TravelCostCalculatorFactoryImpl();
 
-	private TravelTimeCalculator travelTimeCalculator;
-	private TravelDisutility travelCostCalculator;
-	// (yyyyyy one is just travel xxx, the other is travel xxx calculator, but both fields are called
-	// calculator.  Something is asymmetric???  kai, mar'12)
-	
-	private LeastCostPathCalculatorFactory leastCostPathCalculatorFactory;
+	private TravelTimeCalculator travelTime;
 	
 	private ScoringFunctionFactory scoringFunctionFactory;
 
-	private CalcLinkStats linkStats;
-	private VolumesAnalyzer volumes;
-	private CalcLegTimes legTimes;
 	private StrategyManager strategyManager;
-	private PlansScoring plansScoring;
-	private RoadPricing roadPricing;
 
 	
 	public static void main(String[] args) {
-		GautengOwnController gautengOwnController = new GautengOwnController() ;
+		KnSimplifiedController gautengOwnController = new KnSimplifiedController() ;
 		gautengOwnController.run() ;
 	}
-	GautengOwnController() {
+	KnSimplifiedController() {
 		// catch logs before doing something
 		this.collectLogMessagesAppender = new CollectLogMessagesAppender();
 		Logger.getRootLogger().addAppender(this.collectLogMessagesAppender);
@@ -148,16 +141,13 @@ public class GautengOwnController {
 
 		this.dtdFileName = null ;
 
-		// make sure we know about any exceptions that lead to abortion of the
-		// program
+		// make sure we know about any exceptions that lead to abortion of the program
 		Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler() {
-
 			@Override
 			public void uncaughtException(Thread t, Throwable e) {
 				log.warn("Getting uncaught Exception in Thread " + t.getName(), e);
-				GautengOwnController.this.uncaughtException = e;
+				KnSimplifiedController.this.uncaughtException = e;
 			}
-
 		});
 
 		// now do other stuff
@@ -176,10 +166,13 @@ public class GautengOwnController {
 
 	}
 	void run() {
-		init() ;
+		setUpOutputDir();
+		initEvents();
+		initLogging();
+		setUp();
+		loadCoreListeners();
 
-//		this.controlerListenerManager.fireControlerStartupEvent();
-		log.error("yyyyyy check if we strictly need this infrastructure (core ctrl listeners?)") ;
+		this.controlerListenerManager.fireControlerStartupEvent();
 		
 		this.checkConfigConsistencyAndWriteToLog("Config dump before doIterations:");
 
@@ -188,26 +181,77 @@ public class GautengOwnController {
 
 		shutdown(false);
 	}
-	void init() {
-//		loadConfig(); // all done in c'tor (I think)
-		setUpOutputDir();
-//		if (this.config.multiModal().isMultiModalSimulationEnabled()) {
-//			setupMultiModalSimulation();
-//		}
-//		if (this.config.scenario().isUseTransit()) {
-//			log.warn("setting up the transit config _after_ the config dump :-( ...");
-//			setupTransitSimulation();
-//		}
-		initEvents();
-		initLogging();
-//		loadData(); // all done in c'tor (I think)
-		setUp();
-		loadCoreListeners();
+	/**
+	 * Initializes the Controler with the parameters from the configuration.
+	 * This method is called after the configuration is loaded, and after the
+	 * scenario data (network, population) is read.
+	 */
+	private void setUp() {
+		this.travelTime = this.travelTimeCalculatorFactory
+			.createTravelTimeCalculator(this.network, this.config.travelTimeCalculator());
 
-//		loadControlerListeners();
-		log.error( "loadControlerListeners needs to be implemented.  Skipping ...") ;
+		this.events.addHandler(this.travelTime);
+
+		this.events.addHandler(new VolumesAnalyzer(3600, 24 * 3600 - 1, this.network));
+		this.events.addHandler(new CalcLegTimes());
+
+		this.scoringFunctionFactory = new CharyparNagelScoringFunctionFactory( this.config.planCalcScore(), this.getNetwork() );
+
+		this.strategyManager = new StrategyManager() ;
+
+		StrategyManagerConfigLoader.load(this.dummyCtrl,strategyManager) ;
+		throw new RuntimeException("this will not work with the above line.  aborting ...") ;
 		
 	}
+	private void loadCoreListeners() {
+
+		/*
+		 * The order how the listeners are added is very important! As
+		 * dependencies between different listeners exist or listeners may read
+		 * and write to common variables, the order is important. Example: The
+		 * RoadPricing-Listener modifies the scoringFunctionFactory, which in
+		 * turn is used by the PlansScoring-Listener.
+		 * 
+		 * IMPORTANT: The execution order is reverse to the order the listeners
+		 * are added to the list.
+		 */
+
+		this.controlerListenerManager.addCoreControlerListener(new PlansScoring());
+
+		this.controlerListenerManager.addCoreControlerListener(new PlansReplanning());
+		this.controlerListenerManager.addCoreControlerListener(new PlansDumping());
+
+		this.controlerListenerManager.addCoreControlerListener(new EventsHandling((EventsManagerImpl) this.events)); 
+		// must be last being added (=first being executed)
+	}
+
+	/**Design comments:<ul>
+	 * <li> yyyy It seems to me that one would need a factory at <i>this</i> level. kai, may'12
+	 * <li> An issue is that the TravelTime(Calculator) object needs to be passed into the factory.  I don't think that
+	 * this is a large problem, but it needs to be dealt with. kai, may'12
+	 * </ul>
+	 * @return a new instance of a {@link PlanAlgorithm} to calculate the routes
+	 *         of plans with the default (= the current from the last or current
+	 *         iteration) travel costs and travel times. Only to be used by a
+	 *         single thread, use multiple instances for multiple threads!
+	 */
+	public PlanAlgorithm createRoutingAlgorithm() {
+		ModeRouteFactory routeFactory = ((PopulationFactoryImpl) (this.population.getFactory())).getModeRouteFactory();
+		
+		final TravelDisutility travelDisutility = 
+			new TravelTimeAndDistanceBasedTravelDisutility(this.travelTime, this.config.planCalcScore());
+		
+		final LeastCostPathCalculatorFactory leastCostPathFactory = new DijkstraFactory();
+		
+		PlansCalcRoute plansCalcRoute = new PlansCalcRoute(this.config.plansCalcRoute(),this.network, travelDisutility, 
+				this.travelTime, leastCostPathFactory, routeFactory);
+
+		return plansCalcRoute;
+	}
+
+	// ############################################################################################################################
+	//	everything below here is probably not critical 	
+	
 	/**
 	 * in particular select if single cpu handler to use or parallel
 	 */
@@ -246,104 +290,6 @@ public class GautengOwnController {
 			e.printStackTrace();
 		}
 	}
-	/**
-	 * Initializes the Controler with the parameters from the configuration.
-	 * This method is called after the configuration is loaded, and after the
-	 * scenario data (network, population) is read.
-	 */
-	protected void setUp() {
-//		if (this.travelTimeCalculator == null) {
-			this.travelTimeCalculator = this.travelTimeCalculatorFactory
-			.createTravelTimeCalculator(this.network, this.config.travelTimeCalculator());
-//		}
-//		if (this.travelCostCalculator == null) {
-			this.travelCostCalculator = this.travelCostCalculatorFactory
-			.createTravelDisutility(this.travelTimeCalculator, this.config.planCalcScore());
-//		}
-		this.events.addHandler(this.travelTimeCalculator);
-
-//		if (this.leastCostPathCalculatorFactory != null) {
-//			log.info("leastCostPathCalculatorFactory already set, ignoring RoutingAlgorithmType specified in config");
-//		} else {
-//			if (this.config.controler().getRoutingAlgorithmType().equals(RoutingAlgorithmType.Dijkstra)) {
-				this.leastCostPathCalculatorFactory = new DijkstraFactory();
-//			} else if (this.config.controler().getRoutingAlgorithmType().equals(RoutingAlgorithmType.AStarLandmarks)) {
-//				this.leastCostPathCalculatorFactory = new AStarLandmarksFactory(
-//						this.network, new FreespeedTravelTimeCost(this.config.planCalcScore()), this.config.global().getNumberOfThreads());
-//			} else if (this.config.controler().getRoutingAlgorithmType().equals(RoutingAlgorithmType.FastDijkstra)) {
-//				this.leastCostPathCalculatorFactory = new FastDijkstraFactory();
-//			} else if (this.config.controler().getRoutingAlgorithmType().equals(RoutingAlgorithmType.FastAStarLandmarks)) {
-//				this.leastCostPathCalculatorFactory = new FastAStarLandmarksFactory(
-//						this.network, new FreespeedTravelTimeCost(this.config.planCalcScore()));
-//			} else {
-//				throw new IllegalStateException("Enumeration Type RoutingAlgorithmType was extended without adaptation of Controler!");
-//			}
-//		}
-
-		/*
-		 * TODO [MR] linkStats uses ttcalc and volumes, but ttcalc has
-		 * 15min-steps, while volumes uses 60min-steps! It works a.t.m., but the
-		 * traveltimes in linkStats are the avg. traveltimes between xx.00 and
-		 * xx.15, and not between xx.00 and xx.59
-		 */
-		this.linkStats = new CalcLinkStats(this.network);
-		this.volumes = new VolumesAnalyzer(3600, 24 * 3600 - 1, this.network);
-		this.events.addHandler(this.volumes);
-		this.legTimes = new CalcLegTimes();
-		this.events.addHandler(this.legTimes);
-
-//		if (this.scoringFunctionFactory == null) {
-			this.scoringFunctionFactory = new CharyparNagelScoringFunctionFactory(
-					this.config.planCalcScore(), this.getNetwork() );
-//		}
-
-		this.strategyManager = new StrategyManager() ;
-
-//		StrategyManagerConfigLoader.load(this,strategyManager) ;
-		throw new RuntimeException("this will not work without the above line.  aborting ...") ;
-		
-	}
-	/**
-	 * Loads a default set of {@link org.matsim.core.controler.listener
-	 * ControlerListener} to provide basic functionality. <b>Note:</b> Be very
-	 * careful if you overwrite this method! The order how the listeners are
-	 * added is very important. Check the comments in the source file before
-	 * overwriting this method!
-	 */
-	protected void loadCoreListeners() {
-
-		/*
-		 * The order how the listeners are added is very important! As
-		 * dependencies between different listeners exist or listeners may read
-		 * and write to common variables, the order is important. Example: The
-		 * RoadPricing-Listener modifies the scoringFunctionFactory, which in
-		 * turn is used by the PlansScoring-Listener.
-		 * 
-		 * IMPORTANT: The execution order is reverse to the order the listeners
-		 * are added to the list.
-		 */
-
-//		this.addCoreControlerListener(new CoreControlerListener());
-		// (CoreControlerListener looks pretty complicated, but I think that it just configures the mobsim. kai, mar'12)
-
-		// the default handling of plans
-		this.plansScoring = new PlansScoring();
-		this.addCoreControlerListener(this.plansScoring);
-
-		// load road pricing, if requested
-		if (this.config.scenario().isUseRoadpricing()) {
-			this.roadPricing = new RoadPricing();
-			this.addCoreControlerListener(this.roadPricing);
-		}
-
-		this.addCoreControlerListener(new PlansReplanning());
-		this.addCoreControlerListener(new PlansDumping());
-
-		this.addCoreControlerListener(new EventsHandling((EventsManagerImpl) this.events)); // must be last being added (=first being executed)
-	}
-
-	
-	
 	void shutdown(final boolean unexpected) {
 		// yyyyyy this has not yet been trimmed
 
@@ -357,8 +303,7 @@ public class GautengOwnController {
 					"Shutdown probably caused by the following Exception.", this.uncaughtException);
 		}
 
-//		this.controlerListenerManager.fireControlerShutdownEvent(unexpected);
-		log.error("check if we need the controler listener infrastructure") ;
+		this.controlerListenerManager.fireControlerShutdownEvent(unexpected);
 
 		if (this.dumpDataAtEnd) {
 			// dump plans
@@ -367,24 +312,7 @@ public class GautengOwnController {
 			new NetworkWriter(this.network).write(this.controlerIO.getOutputFilename(FILENAME_NETWORK));
 			// dump config
 			new ConfigWriter(this.config).write(this.controlerIO.getOutputFilename(FILENAME_CONFIG));
-			// dump facilities
-//			ActivityFacilities facilities = this.getFacilities();
-//			if (facilities != null) {
-//				new FacilitiesWriter((ActivityFacilitiesImpl) facilities)
-//				.write(this.controlerIO.getOutputFilename("output_facilities.xml.gz"));
-//			}
-//			if (((NetworkFactoryImpl) this.network.getFactory()).isTimeVariant()) {
-//				new NetworkChangeEventsWriter().write(this.controlerIO.getOutputFilename("output_change_events.xml.gz"),
-//						((NetworkImpl) this.network).getNetworkChangeEvents());
-//			}
-//			if (this.config.scenario().isUseHouseholds()) {
-//				new HouseholdsWriterV10(this.scenarioData.getHouseholds())
-//				.writeFile(this.controlerIO.getOutputFilename(FILENAME_HOUSEHOLDS));
-//			}
-//			if (this.config.scenario().isUseLanes()) {
-//				new LaneDefinitionsWriter20(
-//						this.scenarioData.getScenarioElement(LaneDefinitions20.class)).write(this.controlerIO.getOutputFilename(FILENAME_LANES));
-//			}
+
 			if (!unexpected	&& this.getConfig().vspExperimental().isWritingOutputEvents()) {
 				File toFile = new File(	this.controlerIO.getOutputFilename("output_events.xml.gz"));
 				File fromFile = new File(this.controlerIO.getIterationFilename(this.getLastIteration(), "events.xml.gz"));
@@ -401,8 +329,7 @@ public class GautengOwnController {
 		} catch (IllegalStateException e) {
 			log.info("Cannot remove shutdown hook. " + e.getMessage());
 		}
-		this.shutdownHook = null; // important for test cases to free the
-		// memory
+		this.shutdownHook = null; // important for test cases to free the memory
 		this.collectLogMessagesAppender = null;
 		IOUtils.closeOutputDirLogging();
 	}
@@ -450,7 +377,8 @@ public class GautengOwnController {
 					// files!
 					throw new RuntimeException(
 							"The output directory " + this.outputPath
-									+ " exists already but has files in it! Please delete its content or the directory and start again. We will not delete or overwrite any existing files.");
+									+ " exists already but has files in it! Please delete its content or the directory and " +
+											"start again. We will not delete or overwrite any existing files.");
 				}
 			}
 		} else {
@@ -474,22 +402,6 @@ public class GautengOwnController {
 		}
 	}
 
-	
-	/**
-	 * Add a core ControlerListener to the Controler instance
-	 * 
-	 * @param l
-	 */
-	protected final void addCoreControlerListener(final ControlerListener l) {
-
-//		this.controlerListenerManager.addCoreControlerListener(l);
-		throw new RuntimeException("will not work without above line; aborting ...") ;
-
-	}
-
-
-
-	
 	public Config getConfig() {
 		return config;
 	}
