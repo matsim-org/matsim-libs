3356da4773af3b680d201b3af1ba76c63f6af539
vsp_nagel <2008@kainagel.org>
2012-05-30 17:15:26 +0000

    playground change
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@19742 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/kai/src/main/java/playground/kai/run/MyControler.java
+++ playgrounds/kai/src/main/java/playground/kai/run/MyControler.java
@@ -1,11 +1,13 @@
 package playground.kai.run;
 
+import org.apache.log4j.Logger;
 import org.matsim.core.controler.Controler;
 import org.matsim.vis.otfvis.OTFFileWriterFactory;
 
 class MyControler {
 	
 	public static void main ( String[] args ) {
+		Logger.getLogger("blabla").warn("here") ;
 
 		Controler controler = new Controler( args ) ;
 
