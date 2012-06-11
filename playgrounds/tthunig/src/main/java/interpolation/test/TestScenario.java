a5ed80ec33b6db73bfbe843c0c20ef88d1a34047
tthunig <tthunig@vsp.tu-berlin.de>
2012-06-11 12:16:50 +0000

    access to spatial grid with coordinates
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@19876 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/tthunig/src/main/java/interpolation/test/TestScenario.java
+++ playgrounds/tthunig/src/main/java/interpolation/test/TestScenario.java
@@ -4,42 +4,66 @@ import interpolation.Interpolation;
 
 import org.apache.log4j.Logger;
 
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+
 import playground.tnicolai.matsim4opus.gis.SpatialGrid;
 
+/**
+ * class for testing the implemented interpolation methods visually
+ * uses the car accessibilities of the zurich scenario at resolution 100m
+ * 
+ * @author tthunig
+ *
+ */
 public class TestScenario {
 
 	private static final Logger log = Logger.getLogger(Interpolation.class);
 	
 	//information about the given data
-	private static String filename = "zurich_accessibility_grid_cellsize100m.txt";
-	
-	//information about the interpolation method
-	private static int interpolationMethod = Interpolation.BILINEAR;
-	private static double expForIDW = 1.;
+	private static String filename = "zurich_accessibility_grid_cellsize100m";
 	
 	/**
+	 * reads the data from the scenario and interpolates the grid of known values to a higher resolution first with bilinear interpolation second with bicubic spline interpolation
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		java.net.URL input = ClassLoader.getSystemResource(filename);
+		java.net.URL input = ClassLoader.getSystemResource(filename+".txt");
 		
-		log.info("interpolate file " + filename + " with interpolation method " + interpolationMethod + ":");
+		log.info("interpolate file " + filename + ":");
 
 		log.info("reading data...");
 		SpatialGrid sg = SpatialGrid.readFromFile(input.getFile());		
 		
-		log.info("interpolating...");
-		Interpolation interpolation = new Interpolation(sg, interpolationMethod, expForIDW);
+		testOneMethod(sg, Interpolation.BILINEAR);
+		testOneMethod(sg, Interpolation.BICUBIC);
+		testOneMethod(sg, Interpolation.INVERSE_DISTANCE_WEIGHTING_EXPERIMENTAL);
+		
+		log.info("done");
+	}
+	
+	/**
+	 * interpolates the grid with one chosen interpolation method
+	 * 
+	 * @param sg
+	 * @param interpolationMethod
+	 */
+	private static void testOneMethod(SpatialGrid sg, int interpolationMethod){
+		log.info("interpolating data with the interpolation method " + interpolationMethod + "...");
+		Interpolation interpolation = new Interpolation(sg, interpolationMethod, 1);
+		
 		SpatialGrid sg_new = new SpatialGrid(sg.getXmin(), sg.getYmin(), sg.getXmax(), sg.getYmax(), sg.getResolution() / 2);
+		GeometryFactory factory = new GeometryFactory();
 		// calculate new values for higher resolution
 		for (double y = sg.getYmin(); y <= sg.getYmax(); y = y+ sg.getResolution()/2) {
 			for (double x = sg.getXmin(); x <= sg.getXmax(); x = x+ sg.getResolution()/2) {
-				sg_new.setValue(sg_new.getRow(y), sg_new.getColumn(x), interpolation.interpolate(x, y));
+				sg_new.setValue(interpolation.interpolate(x, y), factory.createPoint(new Coordinate(x, y)));
 			}
 		}
 		
-		log.info("writing interpolated data");
-		sg_new.writeToFile("Z:/WinHome/Docs/Interpolation/zurich_test/" + filename + "_" + interpolationMethod + ".txt");
+		log.info("writing interpolated data...");
+		sg_new.writeToFile("Z:/WinHome/Docs/Interpolation/zurich_test/" + filename + "_" + interpolationMethod + ".txt"); //TODO in resources speichern
 	}
 
 }
