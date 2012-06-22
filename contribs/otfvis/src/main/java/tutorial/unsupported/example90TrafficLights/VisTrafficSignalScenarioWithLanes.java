a266ebd75e40d2fe5e124ceb13a784dd0e4b9ead
grether <dominik.grether@gmail.com>
2012-06-22 09:28:11 +0000

    moved a slightly modified version of traffic signal tutorial back to org.matsim as it had nothing to do with otfvis except one line of code. added vis tutorial for traffic signals
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@20021 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ contribs/otfvis/src/main/java/tutorial/unsupported/example90TrafficLights/VisTrafficSignalScenarioWithLanes.java
@@ -0,0 +1,46 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * CreateTrafficSignalScenarioWithLanes
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2011 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package tutorial.unsupported.example90TrafficLights;
+
+import org.matsim.contrib.otfvis.OTFVis;
+
+/**
+ * This class contains a simple example how to visualize a scenario with lanes and signalized intersections.
+ * 
+ * @author dgrether
+ * 
+ * @see org.matsim.signalsystems
+ * @see http://matsim.org/node/384
+ */
+public class VisTrafficSignalScenarioWithLanes {
+
+	private void run() {
+		String configFile = new CreateTrafficSignalScenarioWithLanes().run();
+		OTFVis.playConfig(configFile);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new VisTrafficSignalScenarioWithLanes().run();
+	}
+
+}
