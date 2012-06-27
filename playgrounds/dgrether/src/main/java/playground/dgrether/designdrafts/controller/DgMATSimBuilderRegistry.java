a00a7aee26c0c56dbfaf5409276d96b3718fc014
grether <dominik.grether@gmail.com>
2012-06-27 14:05:37 +0000

    design draft concerning AbstractController discussion
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@20085 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/dgrether/src/main/java/playground/dgrether/designdrafts/controller/DgMATSimBuilderRegistry.java
@@ -0,0 +1,45 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * MATSimFactoryRegistry
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.dgrether.designdrafts.controller;
+
+
+/**
+ * @author dgrether
+ *
+ */
+public interface DgMATSimBuilderRegistry {
+
+	public void setScoringBuilder(DgScoringBuilder scoringBuilder);
+
+	public DgScoringBuilder getScoringBuilder();
+
+	public void setReplanningBuilder(DgReplanningBuilder replanningBuilder);
+
+	public DgReplanningBuilder getReplanningBuilder();
+
+	public void setMobsimBuilder(DgMobsimBuilder mobsimBuilder);
+
+	public  DgMobsimBuilder getMobsimBuilder();
+	
+	
+	
+	
+
+}
