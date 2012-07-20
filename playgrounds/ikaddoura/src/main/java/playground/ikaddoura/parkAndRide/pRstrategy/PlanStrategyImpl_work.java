9dac9b56d8e83c906ab5e702d56d9e1495bc1131
kaddoura <kaddoura@users.sf.net>
2012-07-20 03:40:47 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@20313 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/ikaddoura/src/main/java/playground/ikaddoura/parkAndRide/pRstrategy/PlanStrategyImpl_work.java
@@ -0,0 +1,171 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * PlanStrategy.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.ikaddoura.parkAndRide.pRstrategy;
+
+import java.util.ArrayList;
+
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Plan;
+import org.matsim.api.core.v01.population.PlanElement;
+import org.matsim.api.core.v01.replanning.PlanStrategyModule;
+import org.matsim.core.population.ActivityImpl;
+import org.matsim.core.population.PersonImpl;
+import org.matsim.core.replanning.PlanStrategy;
+import org.matsim.core.replanning.selectors.PlanSelector;
+
+/**
+ * A strategy defines how an agent can be modified during re-planning.
+ * Only modifying plans if person has a work activity.
+ *
+ * @author mrieser, modified by ikaddoura
+ * @see org.matsim.core.replanning
+ */
+public final class PlanStrategyImpl_work implements PlanStrategy {
+
+	private PlanSelector planSelector = null;
+	private PlanStrategyModule firstModule = null;
+	private final ArrayList<PlanStrategyModule> modules = new ArrayList<PlanStrategyModule>();
+	private final ArrayList<Plan> plans = new ArrayList<Plan>();
+	private long counter = 0;
+	private final static Logger log = Logger.getLogger(PlanStrategyImpl_work.class);
+
+	/**
+	 * Creates a new strategy using the specified planSelector.
+	 *
+	 * @param planSelector
+	 */
+	public PlanStrategyImpl_work(final PlanSelector planSelector) {
+		this.planSelector = planSelector;
+	}
+
+	@Override
+	public void addStrategyModule(final PlanStrategyModule module) {
+		if (this.firstModule == null) {
+			this.firstModule = module;
+		} else {
+			this.modules.add(module);
+		}
+	}
+	
+	@Override
+	public int getNumberOfStrategyModules() {
+		if (this.firstModule == null) {
+			return 0;
+		}
+		return this.modules.size() + 1; // we also have to count "firstModule", thus +1
+	}
+	
+	@Override
+	public void run(final Person person) {
+		
+		// checks if person has a work activity
+		boolean hasWorkAct = false;
+		for (Plan plan : person.getPlans()){
+			for (PlanElement pe: plan.getPlanElements()) {
+				if (pe instanceof ActivityImpl) {
+					ActivityImpl act = (ActivityImpl)pe;
+					if (act.getType().equals("work")){
+						hasWorkAct = true;
+					}
+				}
+			}
+		}
+		
+		// if person has a work activity proceed with strategy modules...
+		if (hasWorkAct){
+			this.counter++;
+			
+			// if there is at least one unscored plan, find that one:
+			Plan plan = ((PersonImpl) person).getRandomUnscoredPlan();
+			
+			// otherwise, find one according to selector (often defined in PlanStrategy ctor):
+			if (plan == null) {
+				plan = this.planSelector.selectPlan(person);
+			}
+			
+			// "select" that plan:
+			((PersonImpl) person).setSelectedPlan(plan);
+			
+			// if there is a "module" (i.e. "innovation"):
+			if (this.firstModule != null) {
+				
+				// set the working plan to a copy of the selected plan:
+				plan = ((PersonImpl) person).copySelectedPlan();
+				// (this makes, as a side effect, the _new_ plan selected)
+				
+				// add new plan to container that contains the plans that are handled by this PlanStrategy:
+				this.plans.add(plan);
+	
+				// start working on this new plan:
+				this.firstModule.handlePlan(plan);
+			}
+		}
+	}
+
+	@Override
+	public void init() {
+		if (this.firstModule != null) {
+			this.firstModule.prepareReplanning();
+		}
+	}
+
+	@Override
+	public void finish() {
+		// yyyy I don't think this needs to be public once StrategyManager.run is final.  kai, sep'10
+		if (this.firstModule != null) {
+			// finish the first module
+				this.firstModule.finishReplanning();
+			// now work through the others
+			for (PlanStrategyModule module : this.modules) {
+				module.prepareReplanning();
+				for (Plan plan : this.plans) {
+					module.handlePlan(plan);
+				}
+				module.finishReplanning();
+			}
+		}
+		this.plans.clear();
+		log.info("Plan-Strategy finished, " + this.counter + " plans handled. Strategy: " + this.toString());
+		this.counter = 0;
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer name = new StringBuffer(20);
+		name.append(this.planSelector.getClass().getSimpleName());
+		if (this.firstModule != null) {
+			name.append('_');
+			name.append(this.firstModule.getClass().getSimpleName());
+			for (Object module : this.modules) {
+				name.append('_');
+				name.append(module.getClass().getSimpleName());
+			}
+		}
+		return name.toString();
+	}
+
+	@Override
+	public PlanSelector getPlanSelector() {
+		return planSelector;
+	}
+	
+}
