b607de9a53d02ba34f879c3b04a11508297cee0b
sergio537 <sergio537@users.sf.net>
2012-07-21 02:34:34 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@20334 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/sergioo/src/main/java/playground/sergioo/bestTravelTimeRouter/TestBestTravelTimeRouter.java
+++ playgrounds/sergioo/src/main/java/playground/sergioo/bestTravelTimeRouter/TestBestTravelTimeRouter.java
@@ -1,18 +1,15 @@
 package playground.sergioo.bestTravelTimeRouter;
 
-import java.util.ArrayList;
-import java.util.Collection;
 
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Node;
-import org.matsim.core.api.experimental.network.NetworkWriter;
 import org.matsim.core.basic.v01.IdImpl;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.network.MatsimNetworkReader;
-import org.matsim.core.router.BestTravelTimePathCalculatorImpl;
+/*import org.matsim.core.router.BestTravelTimePathCalculatorImpl;
 import org.matsim.core.router.util.BestTravelTimePathCalculator;
-import org.matsim.core.router.util.BestTravelTimePathCalculator.Path;
+import org.matsim.core.router.util.BestTravelTimePathCalculator.Path;*/
 import org.matsim.core.router.util.TravelTime;
 import org.matsim.core.scenario.ScenarioUtils;
 
@@ -30,14 +27,14 @@ public class TestBestTravelTimeRouter {
 				return link.getLength();
 			}
 		};
-		BestTravelTimePathCalculator bttpc = new BestTravelTimePathCalculatorImpl(scenario.getNetwork(), tt);
+		//BestTravelTimePathCalculator bttpc = new BestTravelTimePathCalculatorImpl(scenario.getNetwork(), tt);
 		Node o = scenario.getNetwork().getNodes().get(new IdImpl(1380010516));
 		Node d = scenario.getNetwork().getNodes().get(new IdImpl(1380021814));
 		long time = System.currentTimeMillis();
-		Path path = bttpc.calcBestTravelTimePath(o, d, 3, 0);
+		//Path path = bttpc.calcBestTravelTimePath(o, d, 3, 0);
 		System.out.println(System.currentTimeMillis()-time);
-		System.out.println(path);
-		System.out.println(path.travelTime);
+		//System.out.println(path);
+		//System.out.println(path.travelTime);
 	}
 
 	private static boolean isIn(Node node) {
