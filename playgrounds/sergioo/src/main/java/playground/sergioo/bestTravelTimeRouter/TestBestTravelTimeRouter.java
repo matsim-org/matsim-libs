2bfa4a3db7377a895f493bc5ec556ffcb6191651
sergio537 <sergio537@users.sf.net>
2012-07-21 02:21:24 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@20333 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/sergioo/src/main/java/playground/sergioo/bestTravelTimeRouter/TestBestTravelTimeRouter.java
@@ -0,0 +1,48 @@
+package playground.sergioo.bestTravelTimeRouter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Node;
+import org.matsim.core.api.experimental.network.NetworkWriter;
+import org.matsim.core.basic.v01.IdImpl;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.network.MatsimNetworkReader;
+import org.matsim.core.router.BestTravelTimePathCalculatorImpl;
+import org.matsim.core.router.util.BestTravelTimePathCalculator;
+import org.matsim.core.router.util.BestTravelTimePathCalculator.Path;
+import org.matsim.core.router.util.TravelTime;
+import org.matsim.core.scenario.ScenarioUtils;
+
+public class TestBestTravelTimeRouter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+		new MatsimNetworkReader(scenario).parse("./data/networks/singapart.xml");
+		TravelTime tt = new TravelTime() {
+			@Override
+			public double getLinkTravelTime(Link link, double time) {
+				return link.getLength();
+			}
+		};
+		BestTravelTimePathCalculator bttpc = new BestTravelTimePathCalculatorImpl(scenario.getNetwork(), tt);
+		Node o = scenario.getNetwork().getNodes().get(new IdImpl(1380010516));
+		Node d = scenario.getNetwork().getNodes().get(new IdImpl(1380021814));
+		long time = System.currentTimeMillis();
+		Path path = bttpc.calcBestTravelTimePath(o, d, 3, 0);
+		System.out.println(System.currentTimeMillis()-time);
+		System.out.println(path);
+		System.out.println(path.travelTime);
+	}
+
+	private static boolean isIn(Node node) {
+		double x=node.getCoord().getX(), y=node.getCoord().getY(); 
+		return x>103.8372 && x<103.8544 && y>1.2741 && y<1.2867;
+	}
+
+}
