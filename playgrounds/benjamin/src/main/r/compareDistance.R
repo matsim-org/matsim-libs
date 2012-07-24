95af8935e3c2dc9339f17f4d42d56534a35a232a
juliakern <juliakern@users.sf.net>
2012-07-24 09:46:45 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@20356 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/benjamin/src/main/r/compareDistance.R
+++ playgrounds/benjamin/src/main/r/compareDistance.R
@@ -7,6 +7,7 @@ graphics.off()		# Close graphics windows
 
 groupColors <- c("yellow","red") #z30, pricing
 meanColor <- c("red","green") #z30, pricing
+userGroupColors<-c("orange1", "orange2", "orange3","orange4")
 
 #read files and set directories
 directory <- commandArgs()[3]
@@ -15,6 +16,9 @@ z30File <- file.path(directory,"detailedCarDistanceInformation_policyCase_zone30
 z30 <- read.table(file = z30File, header=T, sep = "\t", comment.char="")
 pri <- read.table(file = priFile, header=T, sep = "\t", comment.char="")
 outFile <- file.path(commandArgs()[4], "distanceDifference.pdf")
+
+maximum<-max(z30[,"total.car.distance..km."],pri[,"total.car.distance..km."])
+minimum<-min(z30[,"total.car.distance..km."],pri[,"total.car.distance..km."])
 
 pdf(outFile, width=7, height=7)
 #first plot without outline
@@ -29,17 +33,16 @@ bline <- mean(pri[,"total.car.distance..km."])
 #draw means as lines
 segments(seq(along = aline) - 0.4, aline, seq(along = aline) - 0.2, aline, lwd = 2, col = meanColor[1]) 
 segments(seq(along = bline) + 0.2, bline, seq(along = bline) + 0.4, bline, lwd = 2, col = meanColor[2]) 
-
 axis(1, c(0.7,1.3), labels=c("Zone 30","Pricing"), tick=F)
 
 #second plot with outline
 boxplot(z30[,"total.car.distance..km."], notch=T, outline=T, boxwex = 0.3, col=groupColors[1], 
-main= "Difference to base case", xlab="Zone 30, Pricing", ylab="Distance [km]", at=1:1-0.3)
+main= "Difference to base case", ylab="Distance [km]", at=1:1-0.3)
 boxplot(pri[,"total.car.distance..km."], notch=T, outline=T, boxwex = 0.3, col=groupColors[2], add=T, at=1:1+0.3)
 
 #draw means as lines
 segments(seq(along = aline) - 0.4, aline, seq(along = aline) - 0.2, aline, lwd = 2, col = meanColor[1]) 
 segments(seq(along = bline) + 0.2, bline, seq(along = bline) + 0.4, bline, lwd = 2, col = meanColor[2]) 
-
 axis(1, c(0.7,1.3), labels=c("Zone 30","Pricing"), tick=F)
+
 dev.off()
