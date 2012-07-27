572cb30b0c0ce07d147bebef169b26b44631912d
rashid_waraich <rashid_waraich@users.sf.net>
2012-07-27 17:57:19 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@20414 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/main/java/playground/wrashid/lib/tools/plan/PlanShrinker.java
+++ playgrounds/wrashid/src/main/java/playground/wrashid/lib/tools/plan/PlanShrinker.java
@@ -8,13 +8,13 @@ import playground.wrashid.lib.GeneralLib;
 public class PlanShrinker {
 
 	public static void main(final String[] args) {
-		String inputPlansFile="H:/data/experiments/TRBAug2011/runs/ktiRun24/output/output_plans.xml.gz";
+		String inputPlansFile="H:/data/experiments/TRBAug2012/input/census2000v2_8kmCut_5pct_no_network_info.xml.gz";
 		String inputNetworkFile="H:/data/cvs/ivt/studies/switzerland/networks/teleatlas-ivtcheu/network.xml.gz";
 		String inputFacilities="H:/data/cvs/ivt/studies/switzerland/facilities/facilities.xml.gz";
-		double populationFraction=0.5;
+		double populationFraction=0.2;
 		// e.g. 0.1 means 10% of input population
 		
-		String outputPlansFile="c:/eTmp/plans_census2000v2_zrhCutC_5pml_no_network_info.xml";		
+		String outputPlansFile="H:/data/experiments/TRBAug2012/input/census2000v2_8kmCut_1pct_no_network_info.xml.gz";		
 		
 		// NOTE: if no facilities file is available for the scenario, just specify any valid facilities file
 		Scenario scenario= GeneralLib.readScenario(inputPlansFile, inputNetworkFile,inputFacilities);
