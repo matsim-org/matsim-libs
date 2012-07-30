8ff14d117935a06b8cdb8b54009cf37551d9de89
marmolea <marmolea@users.sf.net>
2012-07-30 15:35:26 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@20431 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/acmarmol/src/main/java/playground/acmarmol/microcensus2010/HouseholdsFromMZ.java
+++ playgrounds/acmarmol/src/main/java/playground/acmarmol/microcensus2010/HouseholdsFromMZ.java
@@ -1,3 +1,22 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
 package playground.acmarmol.microcensus2010;
 
 import java.io.BufferedReader;
@@ -36,6 +55,10 @@ public class HouseholdsFromMZ{
 	private ObjectAttributes vehicles_attributes;
 	private Households households;
 	private Vehicles vehicles;
+	
+	private static final String NO_ANSWER = "no answer";
+	private static final String UNSPECIFIED = "unspecified";
+	private static final String NOT_KNOWN = "not known";
 		
 	
 //////////////////////////////////////////////////////////////////////
@@ -82,7 +105,7 @@ public class HouseholdsFromMZ{
 		String size = entries[76].trim();
 		household_attributes.putAttribute(hhnr, "size", size);
 		
-		//holsehold income
+		//household income
 		String income = entries[98].trim();
 		household_attributes.putAttribute(hhnr, "income", income);
 		
@@ -91,12 +114,42 @@ public class HouseholdsFromMZ{
 		Coord location = new CoordImpl(entries[7].trim(),entries[8].trim());
 		location.setX(Math.round(location.getX()/100.0)*100);
 		location.setY(Math.round(location.getY()/100.0)*100);
-		household_attributes.putAttribute(hhnr, "location_coord", location);
+		household_attributes.putAttribute(hhnr, "location coord", location);
 		
 		//municipality BFS number
 		String municipality =  entries[10].trim();
 		household_attributes.putAttribute(hhnr, "municipality", municipality);
 		
+		
+		//number of cars
+		String nr_cars = entries[77];
+		if(nr_cars.equals("-98")){nr_cars = NO_ANSWER;}
+		else if(nr_cars.equals("-97")){nr_cars = NOT_KNOWN;}
+		household_attributes.putAttribute(hhnr, "total cars ", nr_cars);
+		
+		//number of motorcycles
+		String nr_mcycles = entries[79];
+		if(nr_mcycles.equals("-98")){nr_mcycles = NO_ANSWER;}
+		else if(nr_mcycles.equals("-97")){nr_mcycles = NOT_KNOWN;}
+		household_attributes.putAttribute(hhnr, "total motorcycles ", nr_mcycles);
+		
+		//number of small motorcycles
+		String nr_smcycles = entries[79];
+		if(nr_smcycles.equals("-98")){nr_smcycles = NO_ANSWER;}
+		else if(nr_smcycles.equals("-97")){nr_smcycles = NOT_KNOWN;}
+		household_attributes.putAttribute(hhnr, "total small motorcycles ", nr_smcycles);
+		
+		//number of mofa
+		String nr_mofas = entries[79];
+		if(nr_mofas.equals("-98")){nr_mofas = NO_ANSWER;}
+		else if(nr_mofas.equals("-97")){nr_mofas = NOT_KNOWN;}
+		household_attributes.putAttribute(hhnr, "total mofas ", nr_mofas);
+		
+		//number of bicycles
+		String nr_bikes = entries[79];
+		if(nr_bikes.equals("-98")){nr_bikes = NO_ANSWER;}
+		else if(nr_bikes.equals("-97")){nr_bikes = NOT_KNOWN;}
+		household_attributes.putAttribute(hhnr, "total bicycles", nr_bikes);
 		
 		// creating matsim household
 		Household hh = households.getFactory().createHousehold(new IdImpl(hhnr));
@@ -124,24 +177,22 @@ public class HouseholdsFromMZ{
 		int nr_persons = 0; 
 		
 		while ((curr_line = br.readLine()) != null) {
-		nr_persons++;	
-		String[] entries = curr_line.split("\t", -1);
-		
-		//household number
-		String hhnr = entries[0].trim();
-		
-		//household person number (hpnr)
-		String hpnr = entries[2].trim();
-		
-		
-		//filling person data into matsim households
-		IdImpl hhid = new IdImpl(hhnr);
-		if(!this.households.getHouseholds().containsKey(hhid)){
-			Gbl.errorMsg("This should never happen!  Household hhnr: " + hhnr+ " doesn't exist");
-		}		
-		this.households.getHouseholds().get(hhid).getMemberIds().add(new IdImpl(hhnr.concat(hpnr)));  // id = hhnr + hpnr??
-		
-		
+			nr_persons++;	
+			String[] entries = curr_line.split("\t", -1);
+			
+			//household number
+			String hhnr = entries[0].trim();
+			
+			//household person number (hpnr)
+			String hpnr = entries[2].trim();
+			
+			
+			//filling person data into matsim households
+			IdImpl hhid = new IdImpl(hhnr);
+			if(!this.households.getHouseholds().containsKey(hhid)){
+				Gbl.errorMsg("This should never happen!  Household hhnr: " + hhnr+ " doesn't exist");
+			}		
+			this.households.getHouseholds().get(hhid).getMemberIds().add(new IdImpl(hhnr.concat(hpnr)));  // id = hhnr + hpnr??
 		}
 		
 		
@@ -163,62 +214,71 @@ public class HouseholdsFromMZ{
 		BufferedReader br = new BufferedReader(fr);
 		String curr_line = br.readLine(); // Skip header
 		
-		while ((curr_line = br.readLine()) != null) {
+			while ((curr_line = br.readLine()) != null) {
+				
+			String[] entries = curr_line.split("\t", -1);
 			
-		String[] entries = curr_line.split("\t", -1);
-		
-		//household number
-		String hhnr = entries[0].trim();
-		
-		//vehicle number
-		String fznr = entries[2].trim();
-		
-		//type
-		String type = entries[3].trim();
-		if(type.equals("1")){type = "Auto";}
-		else if(type.equals("2")){type = "Motorrad";}
-		else Gbl.errorMsg("This should never happen!  Vehicle type: " + type+ " doesn't exist");
-
-		vehicles_attributes.putAttribute(hhnr.concat(fznr), "type", type);
-		
-		//type of fuel
-		String fuel = entries[4].trim();
-		if(fuel.equals("1")){fuel = "benzin";}
-		else if(fuel.equals("2")){fuel = "diesel";}
-		else if(fuel.equals("3")){fuel = "hybridE85Gas";}
-		else if(fuel.equals("4")){fuel = "other";}
-		else if(fuel.equals("-98")){fuel = "unspecified";}
-		else if(fuel.equals("-97")){fuel = "notKnown";}
-		else if(fuel.equals("-99")){fuel = "FrageNurBeiAuto";}
-		else Gbl.errorMsg("This should never happen!  Fuel type: " + fuel+ " doesn't exist");
-		vehicles_attributes.putAttribute(hhnr.concat(fznr), "fuelType", fuel);
-		
-		//year of registration
-		String year = entries[6].trim();
-		if(year.equals("-98")){year = "unspecified";}
-		else if(year.equals("-97")){year = "notKnown";}
-		vehicles_attributes.putAttribute(hhnr.concat(fznr), "year of registration", year);
-		
-		//month of registration
-		String month = entries[7].trim();
-		if(month.equals("1")){month = "january";}else if(month.equals("2")){month = "febrary";}
-		else if(month.equals("3")){month = "march";}else if(month.equals("4")){month = "april";}
-		else if(month.equals("5")){month = "may";}else if(month.equals("6")){month = "june";}
-		else if(month.equals("7")){month = "july";}else if(month.equals("8")){month = "august";}
-		else if(month.equals("9")){month = "september";}else if(month.equals("10")){month = "october";}
-		else if(month.equals("11")){month = "november";}else if(month.equals("12")){month = "december";}
-		else if(month.equals("-98")){month = "unspecified";}
-		else if(month.equals("-97")){month = "notKnown";}
-		else if(month.equals("-99")){month = "NichtÜberMofis";}
-		else Gbl.errorMsg("This should never happen!  Month: " + month+ " doesn't exist");
-		vehicles_attributes.putAttribute(hhnr.concat(fznr), "month of registration", month);
-		
-					
-		// creating matsim vehicle
-		Vehicle vehicle = vehicles.getFactory().createVehicle(new IdImpl(hhnr.concat(fznr)), new VehicleTypeImpl(new IdImpl(type))); 
-		vehicles.getVehicles().put(vehicle.getId(), vehicle);
-
-		}
+			//household number
+			String hhnr = entries[0].trim();
+			
+			//vehicle number
+			String fznr = entries[2].trim();
+			
+			//type
+			String type = entries[3].trim();
+			if(type.equals("1")){type = "Auto";}
+			else if(type.equals("2")){type = "Motorcycle";}
+			else Gbl.errorMsg("This should never happen!  Vehicle type: " + type+ " doesn't exist");
+	
+			vehicles_attributes.putAttribute(hhnr.concat(fznr), "type", type);
+			
+			//type of fuel
+			String fuel = entries[4].trim();
+			if(fuel.equals("1")){fuel = "benzin";}
+			else if(fuel.equals("2")){fuel = "diesel";}
+			else if(fuel.equals("3")){fuel = "hybridE85Gas";}
+			else if(fuel.equals("4")){fuel = "other";}
+			else if(fuel.equals("-98")){fuel = UNSPECIFIED;}
+			else if(fuel.equals("-97")){fuel = NOT_KNOWN;}
+			else if(fuel.equals("-99")){fuel = "FrageNurBeiAuto";} // -review
+			else Gbl.errorMsg("This should never happen!  Fuel type: " + fuel+ " doesn't exist");
+			vehicles_attributes.putAttribute(hhnr.concat(fznr), "fuel type", fuel);
+			
+			//year of registration
+			String year = entries[6].trim();
+			if(year.equals("-98")){year = UNSPECIFIED;}
+			else if(year.equals("-97")){year = NOT_KNOWN;}
+			vehicles_attributes.putAttribute(hhnr.concat(fznr), "year of registration", year);
+			
+			//month of registration
+			String month = entries[7].trim();
+			if(month.equals("1")){month = "january";}else if(month.equals("2")){month = "febrary";}
+			else if(month.equals("3")){month = "march";}else if(month.equals("4")){month = "april";}
+			else if(month.equals("5")){month = "may";}else if(month.equals("6")){month = "june";}
+			else if(month.equals("7")){month = "july";}else if(month.equals("8")){month = "august";}
+			else if(month.equals("9")){month = "september";}else if(month.equals("10")){month = "october";}
+			else if(month.equals("11")){month = "november";}else if(month.equals("12")){month = "december";}
+			else if(month.equals("-98")){month = UNSPECIFIED;} 
+			else if(month.equals("-97")){month = NOT_KNOWN;}
+			else if(month.equals("-99")){month = "NichtÜberMofis";} // -review
+			else Gbl.errorMsg("This should never happen!  Month: " + month+ " doesn't exist");
+			vehicles_attributes.putAttribute(hhnr.concat(fznr), "month of registration", month);
+			
+						
+			// creating matsim vehicle
+			Vehicle vehicle = vehicles.getFactory().createVehicle(new IdImpl(hhnr.concat(fznr)), new VehicleTypeImpl(new IdImpl(type))); 
+			vehicles.getVehicles().put(vehicle.getId(), vehicle);
+			
+			
+			//filling vehicles data into matsim households
+			IdImpl hhid = new IdImpl(hhnr);
+			if(!this.households.getHouseholds().containsKey(hhid)){
+				Gbl.errorMsg("This should never happen!  Household hhnr: " + hhnr+ " doesn't exist");
+			}		
+			this.households.getHouseholds().get(hhid).getVehicleIds().add(new IdImpl(hhnr.concat(fznr)));  // id = hhnr + fznr??
+			
+			
+		}//end while
 		
 		
 		br.close();
@@ -243,29 +303,29 @@ public class HouseholdsFromMZ{
 		createHouseholdsPeople();
 		createHouseholdsVehicles();
 		
-		System.out.println("############################################ \n " +
+		System.out.println("################################################################################# \n " +
 						   "Writing households  xml file \n" +
-						   "############################################");		
+						   "#################################################################################");		
 		new HouseholdsWriterV10(this.households).writeFile("./output/MicroCensus2010/households.xml");
 		System.out.println("  done.");
 		
-		System.out.println("############################################ \n " +
+		System.out.println("################################################################################# \n " +
 						   "Writing households' attributes xml file \n" +
-							"############################################");			
+							"#################################################################################");			
 		ObjectAttributesXmlWriter households_axmlw = new ObjectAttributesXmlWriter(household_attributes);
 		households_axmlw.putAttributeConverter(CoordImpl.class, new CoordConverter());
 		households_axmlw.writeFile("./output/MicroCensus2010/householdsAttributes.xml");
 		System.out.println("  done.");
 		
-		System.out.println("############################################ \n " +
+		System.out.println("################################################################################# \n " +
 				   		   "Writing vehicles xml file \n" +
-					       "############################################");
+					       "#################################################################################");
 		new VehicleWriterV1(this.vehicles).writeFile("./output/MicroCensus2010/vehicles.xml");
 		System.out.println("  done.");
 		
-		System.out.println("############################################ \n " +
+		System.out.println("################################################################################# \n " +
 						   "Writing vehicles' attributes xml file \n" +
-						   "############################################");	
+						   "#################################################################################");	
 		new ObjectAttributesXmlWriter(vehicles_attributes).writeFile("./output/MicroCensus2010/vehiclesAttributes.xml");
 		System.out.println("  done.");
 		
--- playgrounds/acmarmol/src/main/java/playground/acmarmol/microcensus2010/MZ2010ToXmlFiles.java
+++ playgrounds/acmarmol/src/main/java/playground/acmarmol/microcensus2010/MZ2010ToXmlFiles.java
@@ -1,3 +1,22 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
 package playground.acmarmol.microcensus2010;
 
 import org.matsim.api.core.v01.Scenario;
@@ -25,12 +44,10 @@ public class MZ2010ToXmlFiles {
 	
 	//population xmls...
 	new PopulationFromMZ("input/Microcensus2010/zielpersonen.dat","input/Microcensus2010/wege.dat").run(scenario.getPopulation());
-	
-	
+		
 	}
 	
-	
-	
+		
 	//////////////////////////////////////////////////////////////////////
 	// main
 	//////////////////////////////////////////////////////////////////////
