6978ea523d7fcba2090d945005e014f8251a85ba
vsp_neumann <neumann@senozon.de>
2012-08-13 09:09:13 +0000

    subsetstratmanager
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@20567 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/andreas/src/main/java/playground/andreas/subSetStrategyManager/SubSetStrategyManager.java
@@ -0,0 +1,90 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.andreas.subSetStrategyManager;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Population;
+import org.matsim.core.replanning.PlanStrategy;
+import org.matsim.core.replanning.StrategyManager;
+
+/**
+ * @author mrieser
+ */
+public class SubSetStrategyManager extends StrategyManager {
+
+	private Map<Set<Id>, StrategyManager> managers = new LinkedHashMap<Set<Id>, StrategyManager>();
+	private boolean allowDefaultFallback = true;
+
+	public void addSubset(final Set<Id> ids, final StrategyManager manager) {
+		this.managers.put(ids, manager);
+	}
+
+	/**
+	 * Specifies if the default strategies (strategies registered in this instance, e.g. using
+	 * {@link SubSetStrategyManager#addStrategy(PlanStrategy, double)}) should be used
+	 * for all persons who are not listed in one of the special Id-sets
+	 * ({@link #addSubset(Set, StrategyManager)}. Defaults to <code>true</code>.
+	 *
+	 * @param allowDefault
+	 */
+	public void setAllowDefaultFallback(boolean allowDefault) {
+		this.allowDefaultFallback = allowDefault;
+	}
+
+	@Override
+	protected void beforePopulationRunHook(Population population) {
+		super.beforePopulationRunHook(population);
+		for (StrategyManager mgr : this.managers.values()) {
+			for (PlanStrategy strategy : mgr.getStrategies()) {
+				strategy.init();
+			}
+		}
+	}
+
+	@Override
+	protected void afterRunHook(Population population) {
+		super.afterRunHook(population);
+		for (StrategyManager mgr : this.managers.values()) {
+			for (PlanStrategy strategy : mgr.getStrategies()) {
+				strategy.finish();
+			}
+		}
+	}
+
+	@Override
+	public PlanStrategy chooseStrategy(Person person) {
+		for (Map.Entry<Set<Id>, StrategyManager> e : this.managers.entrySet()) {
+			Set<Id> ids = e.getKey();
+			if (ids.contains(person.getId())) {
+				StrategyManager m = e.getValue();
+				return m.chooseStrategy(person);
+			}
+		}
+		if (this.allowDefaultFallback) {
+			return super.chooseStrategy(person);
+		}
+		return null;
+	}
+} 
