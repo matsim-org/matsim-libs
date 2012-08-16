e848148d0b65f843565660ac4d2cde4f039f2671
grether <dominik.grether@gmail.com>
2012-08-16 18:22:23 +0000

    some cleanup
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@20588 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/fuerbas/src/main/java/air/demand/PopulationGenerator.java
+++ playgrounds/fuerbas/src/main/java/air/demand/PopulationGenerator.java
@@ -22,6 +22,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Random;
 
+import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Link;
@@ -49,57 +50,49 @@ import org.matsim.core.scenario.ScenarioUtils;
  */
 public class PopulationGenerator {
 	
-	public static final String repos = "/media/data/work/repos/";
+	private static final Logger log = Logger.getLogger(PopulationGenerator.class);
 	
-	public static final String inputFile = repos + "shared-svn/studies/countries/de/flight/ct_demand/";
-	public static final String outputPopulation = repos + "shared-svn/studies/countries/de/flight/ct_demand/";
-	
-	public static void main(String[] args) {
-					
-		/*
-		 * First, load config and scenario
-		 */
-		Config config = ConfigUtils.loadConfig("./input/config_de.xml");
-		Scenario sc = ScenarioUtils.loadScenario(config);
-		
-		
-		/*
-		 * Initialize randomizer
-		 * Setze startzeit (3 Uhr UTC)
+	/*
+	 * Setze startzeit (3 Uhr UTC)
 		 * airportoffen (15 Stunden) 
 		 * 5-20 Uhr => Ausgabewert 15:00:00 (15 Stunden) 
 		 * bzw. auf UTC umgerechnet Zeitfenster 03-18 Uhr UTC
+		 
+	 */
+	private static final double startzeit = 3.0 * 3600.0;	
+	private static final double airportoffen = 15.0 * 3600.0;
+	private static final String repos = "/media/data/work/repos/";
+	private static final String inputFlightOdDemand = repos + "lehre-svn/abschlussarbeiten/2011/christopher_treczka/treczka/Modellierung_DE/input/Eingangsdaten_September2010.txt";
+	private static final String inputNetworkFile = repos + "shared-svn/studies/countries/de/flight/dg_oag_flight_model_2_runways_60vph/air_network.xml";
+	private static final String outputPopulation = repos + "shared-svn/studies/countries/de/flight/ct_demand/population.xml.gz";
+	
+	public void createPopulation(){
+		Config config = ConfigUtils.createConfig();
+		config.network().setInputFile(inputNetworkFile);
+		Scenario sc = ScenarioUtils.loadScenario(config);
+		/*
+		 * Initialize randomizer
 		 */
 		Random random = MatsimRandom.getLocalInstance();
-
-		double startzeit = 10800;	
-		double airportoffen = 54000;	
-		
 		/*
 		 * Pick the Network and the Population out of the Scenario for convenience. 
 		 */
 		Network network = sc.getNetwork();
 		Population population = sc.getPopulation();
-
-		
 		/*
 		 * Pick the PopulationFactory out of the Population for convenience.
 		 * It contains methods to create new Population items.
 		 */
 		PopulationFactory populationFactory = population.getFactory();
-
-		
 		/*
 		 * Eingangsdaten importieren
 		 */
 		EingangsdatenParser eingang;
 		eingang = new EingangsdatenParser();
 		List<String> airportdaten = null;
-		
 		try {
-			airportdaten = eingang.EingangAirport();
+			airportdaten = eingang.readFlightODData(inputFlightOdDemand);
 		} catch (IOException e) {
-			
 			e.printStackTrace();
 		}	
 
@@ -111,54 +104,34 @@ public class PopulationGenerator {
 			Id linkId = sc.createId(linkIdStringStart);
 			Link link = sc.getNetwork().getLinks().get(linkId);
 			if (link == null) {
+				log.warn("Link id " + linkIdStringStart + " not found in network!");
 				continue;
 			}
 			
 			int aktuellepassagierzahl = Integer.valueOf((airportdaten.get(eingangsdatearraynindex+2))); // aktuelle Passagieranzahl in Variable schreiben
 			
-//			double startzeit = 10800;	// 3 Uhr UTC (Angabe in Sekunden)
-//			double airportoffen = 54000;	// 5-20 Uhr => Ausgabewert 15:00:00 (15 Stunden) bzw auf UTC umgerechnet Öffnungszeiten 03-18 Uhr UTC
-			
-			
-		
-//			System.out.println("Passagiere: "+aktuellepassagierzahl);	// visuelle Prüfung in Konsole
-//			double verteilungsfaktor = (double) aktuellepassagierzahl/ (double)(airportoffen/3600);	// Berechnung wieviele Passagiere pro Stunde erscheinen sollen wenn man 18Stunden Öffnungszeiten zu Grunde legt
-//			System.out.println("Verteilungsfaktor: "+verteilungsfaktor);	// visuelle Prüfung in Konsole
-//			double angepasstestartzeit = startzeit;
-			
-			
 			for ( int k=0; k< aktuellepassagierzahl; k++){		// eingangsdatearraynindex+2 ist aktuelle Passagieranzahl
 				Person person = populationFactory.createPerson(sc.createId(String.valueOf(personid_zaehler + k)));	// ID für aktuellen Passagier
-				
-				
-
 				/*
 				 * Create a of Plan for the Person
 				 */
 				Plan plan = populationFactory.createPlan();
-			
 				/*
 				 * Create a "home" Activity for the Person. In order to have the Person end its day at the same location,
 				 * we keep the home coordinates for later use (see below).
-				 * Note that we use the CoordinateTransformation created above.
 				 */
-
-
-				
 //				Activity activity1 = populationFactory.createActivityFromCoord("home", link.getCoord());
 				ActivityImpl activity1 = (ActivityImpl) populationFactory.createActivityFromLinkId("home", link.getId());
 				activity1.setCoord(link.getCoord());
+				plan.addActivity(activity1); // add the Activity to the Plan
 				
 				double actstart = random.nextDouble() * airportoffen + startzeit;
 				activity1.setEndTime(actstart); // zufällig generierte ActivityStartTime als Endzeit gesetzt
-
-				plan.addActivity(activity1); // add the Activity to the Plan
 
 				/*
 				 * Create a Leg. A Leg initially hasn't got many attributes. It just says that a pt will be used.
 				 */
 				plan.addLeg(populationFactory.createLeg("pt"));
-				
 				/*
 				 * Create another Activity, at a different location.
 				 */
@@ -166,16 +139,13 @@ public class PopulationGenerator {
 				Id linkId2 = sc.createId(linkIdString2);
 				Link destinationLink = sc.getNetwork().getLinks().get(linkId2);
 				if (destinationLink == null) {	// abfangen von Flughäfen die in den Passagierdaten von DeStatis vorkommen, allerdings nicht im verwendeten Flugnetzwerk vorkommen
+					log.warn("Link id " + linkIdStringStart + " not found in network!");
 					continue;
 				}
-					
 
 				ActivityImpl activity2 = (ActivityImpl) populationFactory.createActivityFromLinkId("home", destinationLink.getId());
 				activity2.setCoord(destinationLink.getCoord());
-				
 				plan.addActivity(activity2);
-				
-				
 				/*
 				 * Wichtig! Fuege Plaene und Personen hinzu
 				 */
@@ -185,16 +155,15 @@ public class PopulationGenerator {
 			
 			personid_zaehler = personid_zaehler + Integer.valueOf(airportdaten.get(eingangsdatearraynindex+2)); //ID Zaehler hochsetzen mit akt. Passagierzahlen
 			eingangsdatearraynindex= eingangsdatearraynindex+3;		// Wichtig damit der korrekte Wert in der Arraylist angesprochen wird
-
-			
 		}
-		
 		/*
 		 * Write the population  to a file.
 		 */
 		MatsimWriter popWriter = new org.matsim.api.core.v01.population.PopulationWriter(population, network);
-		popWriter.write("./input/population.xml");
-
-		
+		popWriter.write(outputPopulation);
+	}
+	
+	public static void main(String[] args) {
+		new PopulationGenerator().createPopulation();
 	}
 }		
