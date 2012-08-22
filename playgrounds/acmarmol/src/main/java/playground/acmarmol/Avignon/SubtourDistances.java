8711b52d0d7e9861772c21e7a87cf13b3688c672
marmolea <marmolea@users.sf.net>
2012-08-22 13:14:58 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@20641 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/acmarmol/src/main/java/playground/acmarmol/Avignon/SubtourDistances.java
@@ -0,0 +1,129 @@
+package playground.acmarmol.Avignon;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
+
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Leg;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.core.gbl.Gbl;
+import org.matsim.core.population.routes.NetworkRoute;
+import org.matsim.core.utils.collections.CollectionUtils;
+import org.matsim.core.utils.misc.RouteUtils;
+
+public abstract class SubtourDistances {
+	
+	
+	
+	public static LinkedHashMap<Id, String> calcDistancesForPurpose(TreeMap<Id, ArrayList<SubtourInfo>> subtours, Scenario scenario){
+	
+		
+		final  Logger log = Logger.getLogger(SubtourDistances.class);
+		
+		log.info("Calculating distances by purpose...");
+		
+		final  DecimalFormat df = new DecimalFormat("#.##");
+		LinkedHashMap<Id, String> distancesByPurpose = new LinkedHashMap<Id, String>();
+		
+		for(Id id : subtours.keySet()){
+			
+			Person person = scenario.getPopulation().getPersons().get(id);
+			
+			double[] distances = new double[MyTransportMode.PURPOSES.size()];	
+			
+			for(SubtourInfo  personSubtour: subtours.get(id)){
+					
+				int index = MyTransportMode.PURPOSES.indexOf(getSubtourHierarchicAct(personSubtour));
+				ArrayList<Integer> subtour = personSubtour.getSubtour();
+				
+				for (int position = 1; position < subtour.size(); position++){
+						
+					Leg leg = (Leg) person.getSelectedPlan().getPlanElements().get(subtour.get(position)-1);
+					if(index!=-1){
+							if(leg.getMode().equals("car")){
+								//i.e home-home trips
+								distances[index] += RouteUtils.calcDistance((NetworkRoute) leg.getRoute(), scenario.getNetwork());
+							}else{ 
+								distances[index] += leg.getRoute().getDistance();
+							}
+								
+						
+					}else{
+						//never goes in here
+						try{distances[0] += RouteUtils.calcDistance((NetworkRoute) leg.getRoute(), scenario.getNetwork());}
+						catch(java.lang.ClassCastException e){ }
+					}
+				}
+				
+							
+			}
+			
+				
+			String strDistance = Arrays.toString(distances);
+			strDistance = strDistance.replace( '[', ' ');
+			strDistance = strDistance.replace( ']', ' ');
+			
+			distancesByPurpose.put(person.getId(), strDistance);
+			
+			
+		}
+		log.info("...done");
+		log.info("number of subtours: " + subtours.size());
+		return distancesByPurpose;
+	}
+
+	
+	private static String getSubtourHierarchicAct(SubtourInfo personSubtour){
+		
+		String actSequence = personSubtour.getActSequence();
+		
+		if(actSequence.charAt(0) == 'w'){//activity sequence starts at work
+							
+					if(actSequence.contains("e")){
+						return "toEducation";
+					}
+					else if(actSequence.contains("s")){
+						return "toShop";
+					}
+					else if(actSequence.contains("l")){
+						return "toLeisure";
+					}
+					else{Gbl.errorMsg("error handling activity sequence: " + actSequence); return null;}
+					//subtours H-H assigned to distance to work for completeness
+		
+		}else{
+					if(actSequence.contains("w")){
+						return "toWork";
+					}
+					else if(actSequence.contains("e")){
+						return "toEducation";
+					}
+					else if(actSequence.contains("s")){
+						return "toShop";
+					}
+					else if(actSequence.contains("l")){
+						return "toLeisure";
+					}
+					else{return "toLeisure";}
+					//"subtours" H-H assigned to distance to leisure for completeness
+					// shouldn't be a problem since dist H to H should be 0
+			
+		}
+		
+		
+		
+	}
+
+
+
+}
+
+
+
+	 
+
---
+++ playgrounds/acmarmol/src/main/java/playground/acmarmol/Avignon/controler/AvignonControler.java
@@ -0,0 +1,37 @@
+package playground.acmarmol.Avignon.controler;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.Module;
+import org.matsim.core.config.groups.QSimConfigGroup;
+import org.matsim.run.Controler;
+
+public class AvignonControler {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		String inputBase = "C:/local/marmolea/input/Avignon/";
+		String configFile =  inputBase + "config_1.xml";
+		
+		Controler controler = new Controler(configFile) ;
+		
+		Scenario sc = controler.getScenario() ;
+		Config cf = sc.getConfig() ;
+		Module aa = cf.getModule("planscalcroute");
+		System.out.println(aa.getParams().toString());
+		
+		controler.setOverwriteFiles(true) ;
+		controler.run() ;
+		
+		
+		String dir = cf.controler().getOutputDirectory();
+		System.out.println("Output is in " + dir + ".  Use otfvis (preferably hardware-accelerated) to play movies." ) ; 
+		
+		
+
+	}
+
+}
