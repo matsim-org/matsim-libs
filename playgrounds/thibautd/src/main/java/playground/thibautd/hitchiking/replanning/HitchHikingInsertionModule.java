0f0f977219804fb078e497f9c7dc7f68cda61b68
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2012-08-22 09:37:40 +0000

    removed unused created instances of the routing modules.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@20630 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/java/playground/thibautd/hitchiking/replanning/HitchHikingInsertionModule.java
+++ playgrounds/thibautd/src/main/java/playground/thibautd/hitchiking/replanning/HitchHikingInsertionModule.java
@@ -19,18 +19,11 @@
  * *********************************************************************** */
 package playground.thibautd.hitchiking.replanning;
 
-import org.matsim.api.core.v01.TransportMode;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.gbl.MatsimRandom;
-import org.matsim.core.population.PopulationFactoryImpl;
 import org.matsim.core.replanning.modules.AbstractMultithreadedModule;
 import org.matsim.population.algorithms.PlanAlgorithm;
 
-import playground.thibautd.hitchiking.HitchHikingConfigGroup;
-import playground.thibautd.hitchiking.HitchHikingSpots;
-import playground.thibautd.hitchiking.HitchHikingUtils;
-import playground.thibautd.hitchiking.routing.HitchHikingDriverRoutingModule;
-import playground.thibautd.hitchiking.routing.HitchHikingPassengerRoutingModule;
 import playground.thibautd.router.controler.MultiLegRoutingControler;
 import playground.thibautd.router.TripRouter;
 import playground.thibautd.router.TripRouterFactory;
@@ -40,13 +33,11 @@ import playground.thibautd.router.TripRouterFactory;
  */
 public class HitchHikingInsertionModule extends AbstractMultithreadedModule {
 	private final MultiLegRoutingControler controler;
-	private final HitchHikingSpots spots;
 
 	public HitchHikingInsertionModule(
 			final Controler controler) {
 		super( controler.getConfig().global() );
 		this.controler = (MultiLegRoutingControler) controler;
-		this.spots = HitchHikingUtils.getSpots( controler.getScenario() );
 	}
 
 	@Override
@@ -54,16 +45,6 @@ public class HitchHikingInsertionModule extends AbstractMultithreadedModule {
 		TripRouterFactory factory = controler.getTripRouterFactory();
 
 		TripRouter tripRouter = factory.createTripRouter();
-		HitchHikingDriverRoutingModule driverModule =
-			new HitchHikingDriverRoutingModule(
-					factory.getRoutingModuleFactories().get( TransportMode.car ).createModule( TransportMode.car , factory ),
-					spots,
-					(HitchHikingConfigGroup) controler.getConfig().getModule( HitchHikingConfigGroup.GROUP_NAME ));
-		HitchHikingPassengerRoutingModule passengerModule =
-			new HitchHikingPassengerRoutingModule(
-					factory.getRoutingModuleFactories().get( TransportMode.pt ).createModule( TransportMode.pt , factory ),
-					spots,
-					((PopulationFactoryImpl) controler.getPopulation().getFactory()).getModeRouteFactory());
 
 		return new HitchHikingInsertionAlgorithm(
 				MatsimRandom.getLocalInstance(),
