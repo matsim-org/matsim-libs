890452c02c21b53572a470d0bc2cc12b264f4641
rashid_waraich <rashid_waraich@users.sf.net>
2012-08-25 02:51:12 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@20694 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/test/java/playground/wrashid/PHEV/Utility/EnergyConsumptionSamplesTest.java
+++ playgrounds/wrashid/src/test/java/playground/wrashid/PHEV/Utility/EnergyConsumptionSamplesTest.java
@@ -1,16 +1,18 @@
 package playground.wrashid.PHEV.Utility;
 
+import org.matsim.contrib.eMob.vehicles.energyConsumption.galus.EnergyConsumption;
+
 import junit.framework.TestCase;
 
 public class EnergyConsumptionSamplesTest extends TestCase {
 
 	public void testGetInterpolatedEnergyConsumption() {
 		EnergyConsumptionSamples ecs = new EnergyConsumptionSamples();
-		ecs.add(new AverageSpeedEnergyConsumption(20, 100));
+		ecs.add(new EnergyConsumption(20, 100));
 
 		assertEquals(200.0, ecs.getInterpolatedEnergyConsumption(40, 1));
 
-		ecs.add(new AverageSpeedEnergyConsumption(40, 200));
+		ecs.add(new EnergyConsumption(40, 200));
 
 		assertEquals(300.0, ecs.getInterpolatedEnergyConsumption(30, 2));
 
@@ -18,12 +20,12 @@ public class EnergyConsumptionSamplesTest extends TestCase {
 
 	public void testGetInterpolatedValue() {
 
-		assertEquals(125.0, EnergyConsumptionSamples.getInterpolatedValue(new AverageSpeedEnergyConsumption(20, 100),
-				new AverageSpeedEnergyConsumption(30, 150), 25));
-		assertEquals(60.0, EnergyConsumptionSamples.getInterpolatedValue(new AverageSpeedEnergyConsumption(20, 100),
-				new AverageSpeedEnergyConsumption(30, 20), 25));
-		assertEquals(91.0, EnergyConsumptionSamples.getInterpolatedValue(new AverageSpeedEnergyConsumption(20, 100),
-				new AverageSpeedEnergyConsumption(30, 10), 21));
+		assertEquals(125.0, EnergyConsumptionSamples.getInterpolatedValue(new EnergyConsumption(20, 100),
+				new EnergyConsumption(30, 150), 25));
+		assertEquals(60.0, EnergyConsumptionSamples.getInterpolatedValue(new EnergyConsumption(20, 100),
+				new EnergyConsumption(30, 20), 25));
+		assertEquals(91.0, EnergyConsumptionSamples.getInterpolatedValue(new EnergyConsumption(20, 100),
+				new EnergyConsumption(30, 10), 21));
 
 	} 
 } 
