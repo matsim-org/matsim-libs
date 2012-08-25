f91df77a94e6be939d54ecd3d62956f04c21ff0e
rashid_waraich <rashid_waraich@users.sf.net>
2012-08-25 09:32:45 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@20712 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/main/java/org/matsim/contrib/transEnergySim/chargingInfrastructure/road/InductiveStreetCharger.java
+++ playgrounds/wrashid/src/main/java/org/matsim/contrib/transEnergySim/chargingInfrastructure/road/InductiveStreetCharger.java
@@ -19,6 +19,15 @@
 
 package org.matsim.contrib.transEnergySim.chargingInfrastructure.road;
 
+import java.util.HashMap;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.contrib.transEnergySim.vehicles.api.BatteryElectricVehicle;
+import org.matsim.contrib.transEnergySim.vehicles.api.InductivlyChargable;
+import org.matsim.contrib.transEnergySim.vehicles.api.Vehicle;
 import org.matsim.core.api.experimental.events.AgentArrivalEvent;
 import org.matsim.core.api.experimental.events.AgentDepartureEvent;
 import org.matsim.core.api.experimental.events.LinkEnterEvent;
@@ -28,45 +37,124 @@ import org.matsim.core.api.experimental.events.handler.AgentDepartureEventHandle
 import org.matsim.core.api.experimental.events.handler.LinkEnterEventHandler;
 import org.matsim.core.api.experimental.events.handler.LinkLeaveEventHandler;
 
+import playground.wrashid.lib.GeneralLib;
 import playground.wrashid.lib.obj.DoubleValueHashMap;
 
 /**
  * This class should work both with jdeqsim and mobsim.
+ * 
  * @author wrashid
- *
+ * 
  */
-public class InductiveStreetCharger implements AgentDepartureEventHandler, LinkEnterEventHandler,LinkLeaveEventHandler, AgentArrivalEventHandler  {
+public class InductiveStreetCharger implements AgentDepartureEventHandler, LinkEnterEventHandler, LinkLeaveEventHandler,
+		AgentArrivalEventHandler {
 
-	private double chargingPowerOfStreetInWatt=3500;
-	
+	private DoubleValueHashMap<Id> chargableStreets;
+
+	HashMap<Id, Vehicle> vehicles;
+
+	DoubleValueHashMap<Id> linkEnterTime;
+	HashMap<Id, Id> previousLinkEntered;
+
+	private final Network network;
+
+	public InductiveStreetCharger(DoubleValueHashMap<Id> powerAtInductiveStreets, HashMap<Id, Vehicle> vehicles, Network network) {
+		this.chargableStreets = powerAtInductiveStreets;
+		this.vehicles = vehicles;
+		this.network = network;
+	}
+
 	@Override
 	public void reset(int iteration) {
-		// TODO Auto-generated method stub
-		
+		linkEnterTime = new DoubleValueHashMap<Id>();
+		previousLinkEntered = new HashMap<Id, Id>();
 	}
 
 	@Override
 	public void handleEvent(AgentArrivalEvent event) {
-		// TODO Auto-generated method stub
-		
+		if (notChargableRoad(event.getLinkId())) {
+			return;
+		}
+
+		handleCharging(event.getPersonId(), event.getLinkId(), event.getTime());
 	}
 
-	@Override
-	public void handleEvent(LinkLeaveEvent event) {
-		
-		
-	}
-
-	@Override
-	public void handleEvent(LinkEnterEvent event) {
-		// TODO Auto-generated method stub
-		
+	private boolean notChargableRoad(Id id) {
+		return !chargableStreets.containsKey(id);
 	}
 
 	@Override
 	public void handleEvent(AgentDepartureEvent event) {
-		// TODO Auto-generated method stub
-		
+		if (notChargableRoad(event.getLinkId())) {
+			return;
+		}
+
+		if (event.getLegMode().equals(TransportMode.car)) {
+			Id personId = event.getPersonId();
+			linkEnterTime.put(personId, event.getTime());
+		}
+	}
+
+	@Override
+	public void handleEvent(LinkLeaveEvent event) {
+		if (notChargableRoad(event.getLinkId())) {
+			return;
+		}
+
+		handleCharging(event.getPersonId(), event.getLinkId(), event.getTime());
+	}
+
+	private void handleCharging(Id personId, Id linkId, double linkLeaveTime) {
+		if (!(vehicles.get(personId) instanceof InductivlyChargable)) {
+			return;
+		}
+
+		double linkEnterTime = this.linkEnterTime.get(personId);
+		double timeSpendOnLink = GeneralLib.getIntervalDuration(linkEnterTime, linkLeaveTime);
+
+		Link link = network.getLinks().get(linkId);
+		double averageSpeedDrivenInMetersPerSecond = link.getLength() / timeSpendOnLink;
+
+		if (shouldLinkBeIgnored(linkLeaveTime, timeSpendOnLink, link, averageSpeedDrivenInMetersPerSecond)) {
+			return;
+		}
+
+		double availablePowerInWatt = 0;
+
+		double chargableEnergyInJoules = availablePowerInWatt * timeSpendOnLink;
+
+		BatteryElectricVehicle vehicleWithBattery = (BatteryElectricVehicle) vehicles.get(personId);
+		double energyToChargeInJoules = 0;
+		if (vehicleWithBattery.getRequiredEnergyInJoules() <= chargableEnergyInJoules) {
+			energyToChargeInJoules = vehicleWithBattery.getRequiredEnergyInJoules();
+		} else {
+			energyToChargeInJoules = chargableEnergyInJoules;
+		}
+		vehicleWithBattery.chargeBattery(energyToChargeInJoules);
+
+		// TODO: also log this... (per vehicle energy consumption)
+	}
+
+	private boolean shouldLinkBeIgnored(double linkLeaveTime, double timeSpendOnLink, Link link,
+			double averageSpeedDrivenInMetersPerSecond) {
+		return timeSpendOnLink == linkLeaveTime || averageSpeedDrivenInMetersPerSecond > link.getFreespeed();
+	}
+
+	@Override
+	public void handleEvent(LinkEnterEvent event) {
+		if (notChargableRoad(event.getLinkId())) {
+			return;
+		}
+
+		Id personId = event.getPersonId();
+		linkEnterTime.put(personId, event.getTime());
+	}
+
+	public void allStreetsCanChargeWithPower(double powerInWatt) {
+		chargableStreets=new DoubleValueHashMap<Id>();
+		for (Id linkId:network.getLinks().keySet()){
+			chargableStreets.put(linkId, powerInWatt);
+		}
 	}
 
 }
--- playgrounds/wrashid/src/main/java/org/matsim/contrib/transEnergySim/controllers/InductiveChargingController.java
+++ playgrounds/wrashid/src/main/java/org/matsim/contrib/transEnergySim/controllers/InductiveChargingController.java
@@ -19,13 +19,23 @@
 
 package org.matsim.contrib.transEnergySim.controllers;
 
+import java.util.HashMap;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Network;
 import org.matsim.contrib.transEnergySim.chargingInfrastructure.road.InductiveStreetCharger;
+import org.matsim.contrib.transEnergySim.vehicles.api.Vehicle;
 import org.matsim.contrib.transEnergySim.vehicles.energyConsumption.EnergyConsumptionTracker;
+import org.matsim.contrib.transEnergySim.vehicles.energyConsumption.api.EnergyConsumptionModel;
+import org.matsim.contrib.transEnergySim.vehicles.energyConsumption.galus.EnergyConsumptionModelGalus;
+import org.matsim.contrib.transEnergySim.vehicles.impl.IC_BEV;
+import org.matsim.core.basic.v01.IdImpl;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.events.StartupEvent;
 import org.matsim.core.controler.listener.StartupListener;
 
 import playground.wrashid.PSF2.pluggable.energyConsumption.EnergyConsumptionModelPSL;
+import playground.wrashid.lib.obj.DoubleValueHashMap;
 
 public class InductiveChargingController extends Controler {
 
@@ -40,15 +50,22 @@ public class InductiveChargingController extends Controler {
 			
 			@Override
 			public void notifyStartup(StartupEvent event) {
-				event.getControler().getEvents().addHandler(new InductiveStreetCharger());
-				//event.getControler().getEvents().addHandler(new EnergyConsumptionTracker());
+				//TODO: set this properly for the scenario
+				
+				EnergyConsumptionModel ecm=new EnergyConsumptionModelGalus();
+				Network network = event.getControler().getNetwork();
+				HashMap<Id, Vehicle> vehicles=new HashMap<Id, Vehicle>();
+				vehicles.put(new IdImpl("1"), new IC_BEV(ecm));
+				
+				InductiveStreetCharger inductiveCharger = new InductiveStreetCharger(null,vehicles,network);
+				inductiveCharger.allStreetsCanChargeWithPower(3000);
+				event.getControler().getEvents().addHandler(inductiveCharger);
+				
+				event.getControler().getEvents().addHandler(new EnergyConsumptionTracker(vehicles, network));
 			}
 		});
 		
 		eMobInductiveChargingController.run();
-		
-		
-		
 	}
 	
 }
