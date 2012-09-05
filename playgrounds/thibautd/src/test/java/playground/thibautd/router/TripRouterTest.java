f74a1087f5df725fe963fa33289636f20c3426b3
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2012-09-05 14:46:33 +0000

    new tests of critical behaviors
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@20867 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/thibautd/src/test/java/playground/thibautd/router/TripRouterTest.java
@@ -0,0 +1,76 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * TripRouterTest.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.thibautd.router;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.matsim.api.core.v01.population.Activity;
+import org.matsim.api.core.v01.population.Leg;
+import org.matsim.api.core.v01.population.PlanElement;
+import org.matsim.core.basic.v01.IdImpl;
+import org.matsim.core.population.ActivityImpl;
+import org.matsim.core.population.LegImpl;
+import org.matsim.core.population.PlanImpl;
+
+/**
+ * @author thibautd
+ */
+public class TripRouterTest {
+	@Test
+	public void testTripInsertion() {
+		PlanImpl plan = new PlanImpl();
+		Activity o = plan.createAndAddActivity( "1" );
+		Activity d = plan.createAndAddActivity( "5" );
+
+		List<PlanElement> trip = new ArrayList<PlanElement>();
+		trip.add( new LegImpl( "2" ) );
+		trip.add( new ActivityImpl( "3" , new IdImpl( "coucou" ) ) );
+		trip.add( new LegImpl( "4" ) );
+
+		TripRouter.insertTrip( plan , o , trip , d );
+
+		assertEquals(
+				"insertion did not produce the expected plan length!",
+				5,
+				plan.getPlanElements().size());
+
+		int oldIndex = 0;
+		for (PlanElement pe : plan.getPlanElements()) {
+			int newIndex = -1;
+
+			if (pe instanceof Activity) {
+				newIndex = Integer.parseInt( ((Activity) pe).getType() );
+			}
+			else {
+				newIndex = Integer.parseInt( ((Leg) pe).getMode() );
+			}
+
+			assertTrue(
+					"wrong inserted sequence: "+plan.getPlanElements(),
+					newIndex > oldIndex);
+			oldIndex = newIndex;
+		}
+	}
+}
+
---
+++ playgrounds/thibautd/src/test/java/playground/thibautd/router/replanning/TripsToLegsAlgorithmTest.java
@@ -0,0 +1,153 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * TripsToLegsAlgorithmTest.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.thibautd.router.replanning;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.matsim.api.core.v01.population.Activity;
+import org.matsim.api.core.v01.population.Plan;
+import org.matsim.api.core.v01.population.PlanElement;
+import org.matsim.core.population.PlanImpl;
+
+import playground.thibautd.router.StageActivityTypesImpl;
+import playground.thibautd.router.TripRouter;
+
+/**
+ * @author thibautd
+ */
+public class TripsToLegsAlgorithmTest {
+	private final static String STAGE_TYPE = "stage";
+
+	private List<Plan> plans;
+	private TripsToLegsAlgorithm t2lAlgo;
+
+	@Before
+	public void initPlans() {
+		plans = new ArrayList<Plan>();
+		// strict alternance
+		PlanImpl newPlan = new PlanImpl();
+		plans.add( newPlan );
+
+		newPlan.createAndAddActivity( "act" );
+		for (int i=0; i < 20; i++) {
+			newPlan.createAndAddLeg( "leg" );
+			newPlan.createAndAddActivity( "act" );
+		}
+
+		// multi-leg trips
+		newPlan = new PlanImpl();
+		plans.add( newPlan );
+
+		newPlan.createAndAddActivity( "act" );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddActivity( "act" );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddActivity( "act" );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddActivity( "act" );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddActivity( "act" );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddActivity( "act" );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddActivity( "act" );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddActivity( "act" );
+
+		// multi-leg-act trips
+		newPlan = new PlanImpl();
+		plans.add( newPlan );
+
+		newPlan.createAndAddActivity( "act" );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddActivity( STAGE_TYPE );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddActivity( "act" );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddActivity( STAGE_TYPE );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddActivity( "act" );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddActivity( STAGE_TYPE );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddActivity( STAGE_TYPE );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddActivity( "act" );
+		newPlan.createAndAddLeg( "leg" );
+		newPlan.createAndAddActivity( STAGE_TYPE );
+		newPlan.createAndAddActivity( "act" );
+	}
+
+	@Before
+	public void initAlgo() {
+		t2lAlgo =
+			new TripsToLegsAlgorithm(
+					new TripRouter(),
+					new StageActivityTypesImpl( Arrays.asList( STAGE_TYPE ) ) );
+	}
+
+	@Test
+	public void testActLegAlternance() throws Exception {
+		for (Plan plan : plans) {
+			t2lAlgo.run( plan );
+
+			boolean lastWasAct = false;
+
+			for ( PlanElement pe : plan.getPlanElements() ) {
+				boolean isAct = pe instanceof Activity;
+				Assert.assertFalse(
+						"wrong act/trip alternance after algo: "+plan.getPlanElements(),
+						isAct && lastWasAct);
+			}
+		}
+	}
+
+	@Test
+	public void testStageActivityPresence() throws Exception {
+		for (Plan plan : plans) {
+			t2lAlgo.run( plan );
+
+			for ( PlanElement pe : plan.getPlanElements() ) {
+				boolean isAct = pe instanceof Activity;
+				Assert.assertFalse(
+						"got a stage act after algo: "+plan.getPlanElements(),
+						isAct && ((Activity) pe).getType().equals( STAGE_TYPE ));
+			}
+		}	
+	}
+}
+
