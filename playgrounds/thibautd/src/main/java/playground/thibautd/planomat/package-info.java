0fc3d8cd8943f533da14b789595c2beea539624a
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2012-09-05 14:45:10 +0000

    removed planomat v2: the replacement for planomat is based on tabu search, not genetic algorithms
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@20863 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/java/playground/thibautd/planomat/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package provides version 2 of <em>Planomat</em>.
- *
- * This version is meant to be easier to extend than the previous one.
- *
- * @author thibautd, based on Konrad Meister previous version
- */
-package playground.thibautd.planomat;
