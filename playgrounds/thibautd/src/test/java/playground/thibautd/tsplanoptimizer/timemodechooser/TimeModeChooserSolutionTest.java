e9ca55b03a136b3e9331452299f8fa31bfcf5d21
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2012-09-06 09:18:38 +0000

    test of critical behavior (particularly cloning)
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@20884 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/thibautd/src/test/java/playground/thibautd/tsplanoptimizer/timemodechooser/TimeModeChooserSolutionTest.java
@@ -0,0 +1,87 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * TimeModeChooserSolutionTest.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.thibautd.tsplanoptimizer.timemodechooser;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.matsim.api.core.v01.population.Plan;
+import org.matsim.core.population.PlanImpl;
+
+import playground.thibautd.router.TripRouter;
+import playground.thibautd.tsplanoptimizer.framework.Value;
+
+/**
+ * @author thibautd
+ */
+public class TimeModeChooserSolutionTest {
+	private List<Plan> testPlans;
+
+	@Before
+	public void initPlans() {
+		// TODO: read different plan forms from a file
+		testPlans = new ArrayList<Plan>();
+		PlanImpl p = new PlanImpl();
+		testPlans.add( p );
+
+		p.createAndAddActivity( "h" ).setEndTime( 12 );
+		p.createAndAddLeg( "bike" );
+		p.createAndAddActivity( "w" ).setMaximumDuration( 298 );
+		p.createAndAddLeg( "bike" );
+		p.createAndAddActivity( "h" );
+	}
+	
+	@Test
+	public void testClone() throws Exception {
+		for (Plan p : testPlans) {
+			TimeModeChooserSolution sol = new TimeModeChooserSolution( p , new TripRouter() );
+			Iterator<? extends Value> cloned = sol.getRepresentation().iterator();
+			Iterator<? extends Value> clone = sol.createClone().getRepresentation().iterator();
+
+			while (cloned.hasNext()) {
+				assertTrue(
+						"reprensentation of clone have less elements than the cloned!",
+						clone.hasNext());
+				Value cloneValue = clone.next();
+				Value clonedValue = cloned.next();
+
+				assertNotSame(
+						"representation of clone and cloned reference the same instances!",
+						cloneValue,
+						clonedValue);
+
+				assertEquals(
+						"values of clone and clone are not equal!",
+						cloneValue,
+						clonedValue);
+			}
+
+			assertFalse(
+					"reprensentation of clone have more elements than the cloned!",
+					clone.hasNext());
+		}
+	}
+}
+
