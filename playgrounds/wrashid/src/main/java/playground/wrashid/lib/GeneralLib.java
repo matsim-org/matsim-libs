4bcc8367f8487f1df1e35d32030500d507ea6663
rashid_waraich <rashid_waraich@users.sf.net>
2012-09-07 13:12:26 +0000

    adding class again, as other playground uses it (removing all methods not needed).
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@20897 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/wrashid/src/main/java/playground/wrashid/lib/GeneralLib.java
@@ -0,0 +1,30 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.wrashid.lib;
+
+import java.util.ArrayList;
+
+public class GeneralLib {
+
+	public static void writeList(ArrayList<String> list, String fileName) {
+		org.matsim.contrib.parking.lib.GeneralLib.writeList(list, fileName);
+	}
+	
+}
