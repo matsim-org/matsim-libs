36c87ccf9bd6d70c423bc285efc8ba921151ee15
pkucirek <pkucirek@users.sf.net>
2012-09-12 23:34:37 +0000

    Added two handlers to the toronto playground. Removed one redundant handler.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@21000 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/toronto/src/main/java/playground/toronto/analysis/handlers/LineBoardingAndAlightingsByTimeBines.java
@@ -0,0 +1,96 @@
+package playground.toronto.analysis.handlers;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.core.events.PersonEntersVehicleEvent;
+import org.matsim.core.events.PersonLeavesVehicleEvent;
+import org.matsim.core.events.TransitDriverStartsEvent;
+import org.matsim.core.events.handler.PersonEntersVehicleEventHandler;
+import org.matsim.core.events.handler.PersonLeavesVehicleEventHandler;
+import org.matsim.core.events.handler.TransitDriverStartsEventHandler;
+import org.matsim.core.utils.misc.Time;
+
+public class LineBoardingAndAlightingsByTimeBines implements
+		PersonEntersVehicleEventHandler, PersonLeavesVehicleEventHandler,
+		TransitDriverStartsEventHandler {
+
+	private HashMap<Id, Id> vehicleLineCache;
+	private final TreeMap<Double, Integer> timeBins;
+	private HashMap<Id, int[]> lineBoardings;
+	private HashMap<Id, int[]> lineAlightings;
+	
+	public LineBoardingAndAlightingsByTimeBines(String binsFile) throws IOException{
+		this.timeBins = generateBinsFromFile(binsFile);
+	}
+	
+	private int[] getEmptyBins(){
+		return new int[this.timeBins.size()];
+	}
+	
+	private TreeMap<Double, Integer> generateBinsFromFile(String fileName) throws IOException{
+		TreeMap<Double, Integer> set = new TreeMap<Double, Integer>();
+		int i = 0;
+		set.put(0.0, i++);
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		String line = "";
+		while ((line = br.readLine()) != null){
+			
+			Double time = null;
+			
+			try {
+				int t = Integer.parseInt(line);
+				time = Time.convertHHMMInteger(t);
+			} catch (NumberFormatException e) {
+				try {
+					time = Time.parseTime(line);
+				} catch (IllegalArgumentException e1) {
+				}
+			}
+			
+			if (time != null) set.put(time, i++);
+		}
+		
+		set.put(Double.MAX_VALUE, i);
+		return set;
+	}
+	
+	@Override
+	public void reset(int iteration) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void handleEvent(TransitDriverStartsEvent event) {
+		this.vehicleLineCache.put(event.getVehicleId(), event.getTransitLineId());
+	}
+
+	@Override
+	public void handleEvent(PersonLeavesVehicleEvent event) {
+		if (this.vehicleLineCache.containsKey(event.getVehicleId())){
+			Id lineId = this.vehicleLineCache.get(event.getVehicleId());
+			if (!this.lineAlightings.containsKey(lineId)){
+				this.lineAlightings.put(lineId, getEmptyBins());
+			}
+			this.lineAlightings.get(lineId)[this.timeBins.ceilingEntry(event.getTime()).getValue()]++;
+		}
+	}
+
+	@Override
+	public void handleEvent(PersonEntersVehicleEvent event) {
+		if (this.vehicleLineCache.containsKey(event.getVehicleId())){
+			Id lineId = this.vehicleLineCache.get(event.getVehicleId());
+			if (!this.lineBoardings.containsKey(lineId)){
+				this.lineBoardings.put(lineId, getEmptyBins());
+			}
+			this.lineBoardings.get(lineId)[this.timeBins.ceilingEntry(event.getTime()).getValue()]++;
+		}
+
+	}
+
+}
---
+++ playgrounds/toronto/src/main/java/playground/toronto/analysis/handlers/StationBoardingsAndAlightings.java
@@ -0,0 +1,125 @@
+package playground.toronto.analysis.handlers;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.core.events.PersonEntersVehicleEvent;
+import org.matsim.core.events.PersonLeavesVehicleEvent;
+import org.matsim.core.events.VehicleArrivesAtFacilityEvent;
+import org.matsim.core.events.VehicleDepartsAtFacilityEvent;
+import org.matsim.core.events.handler.PersonEntersVehicleEventHandler;
+import org.matsim.core.events.handler.PersonLeavesVehicleEventHandler;
+import org.matsim.core.events.handler.VehicleArrivesAtFacilityEventHandler;
+import org.matsim.core.events.handler.VehicleDepartsAtFacilityEventHandler;
+
+/**
+ * Counts all agents boarding to or alighting from all transit facilities during a specified time period.
+ * 
+ * @author pkucirek
+ *
+ */
+public class StationBoardingsAndAlightings implements
+		VehicleArrivesAtFacilityEventHandler,
+		VehicleDepartsAtFacilityEventHandler, PersonEntersVehicleEventHandler,
+		PersonLeavesVehicleEventHandler {
+
+	private final double startTime;
+	private final double endTime;
+	
+	private HashMap<Id, Id> vehicleStopCache;
+	private HashMap<Id, Integer> facilityBoardings;
+	private HashMap<Id, Integer> facilityAlightings;
+	private HashSet<Id> facilities;
+	
+	public StationBoardingsAndAlightings(){
+		this.startTime = 0;
+		this.endTime = Double.MAX_VALUE;
+		init();
+	}
+	
+	public StationBoardingsAndAlightings(double start, double end){
+		this.startTime = start;
+		this.endTime = end;
+		init();
+	}
+	
+	private void init(){
+		this.facilities = new HashSet<Id>();
+		this.facilityAlightings = new HashMap<Id, Integer>();
+		this.facilityBoardings = new HashMap<Id, Integer>();
+		this.vehicleStopCache = new HashMap<Id, Id>();
+	}
+	
+	public HashSet<Id> getActiveStops(){
+		return this.facilities;
+	}
+	
+	public Integer getAlightingsAtStop(Id stopId){
+		return this.facilityAlightings.get(stopId);
+	}
+	
+	public Integer getBoardingsAtStop(Id stopId){
+		return this.facilityBoardings.get(stopId);
+	}
+	
+	@Override
+	public void reset(int iteration) {
+		this.vehicleStopCache.clear();
+		this.facilities.clear();
+		this.facilityAlightings.clear();
+		this.facilityBoardings.clear();
+	}
+	
+	@Override
+	public void handleEvent(PersonLeavesVehicleEvent event) {
+		if (this.vehicleStopCache.containsKey(event.getVehicleId())
+				&& event.getTime() >= this.startTime && event.getTime() <= this.endTime){
+			//Skips persons exiting vehicles not at stops and not in the time period.
+			
+			Id facilityId = this.vehicleStopCache.get(event.getVehicleId());
+			Integer i;
+			if (!this.facilityBoardings.containsKey(facilityId)){
+				i = new Integer(1);
+			}else{
+				i = this.facilityBoardings.get(facilityId) + 1;
+			}
+			this.facilityBoardings.put(facilityId, i);
+			
+			this.facilities.add(facilityId);
+		}
+
+	}
+
+	@Override
+	public void handleEvent(PersonEntersVehicleEvent event) {
+		if (this.vehicleStopCache.containsKey(event.getVehicleId())
+				&& event.getTime() >= this.startTime && event.getTime() <= this.endTime){
+			//Skips persons entering vehicles not at stops and not in the time period.
+			
+			Id facilityId = this.vehicleStopCache.get(event.getVehicleId());
+			Integer i;
+			if (!this.facilityAlightings.containsKey(facilityId)){
+				i = new Integer(1);
+			}else{
+				i = this.facilityAlightings.get(facilityId) + 1;
+			}
+			this.facilityAlightings.put(facilityId, i);
+			
+			this.facilities.add(facilityId);
+		}
+
+	}
+
+	@Override
+	public void handleEvent(VehicleDepartsAtFacilityEvent event) {
+		this.vehicleStopCache.remove(event.getVehicleId());
+
+	}
+
+	@Override
+	public void handleEvent(VehicleArrivesAtFacilityEvent event) {
+		this.vehicleStopCache.put(event.getVehicleId(), event.getFacilityId());
+	}
+
+}
