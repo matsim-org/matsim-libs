9fb80962e7337d2b6addec8b907855cd5240ab7e
mrieser <rieser@gmail.com>
2012-09-12 19:43:49 +0000

    reverting back commits r20969 and r20971, as we sadly recognized that the generated routes are not valid in the context of the transit-mobsim. As this seems to be a problem by design, the commits are reverted and the problems have to be solved on another way.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@20995 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/pt/router/TransitActsRemover.java
+++ matsim/src/main/java/org/matsim/pt/router/TransitActsRemover.java
@@ -22,15 +22,12 @@ package org.matsim.pt.router;
 
 import java.util.List;
 
-import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.Plan;
 import org.matsim.api.core.v01.population.PlanElement;
-import org.matsim.api.core.v01.population.Route;
 import org.matsim.core.population.PlanImpl;
-import org.matsim.core.population.routes.GenericRouteImpl;
 import org.matsim.population.algorithms.PlanAlgorithm;
 import org.matsim.pt.PtConstants;
 
@@ -45,7 +42,6 @@ import org.matsim.pt.PtConstants;
  */
 public class TransitActsRemover implements PlanAlgorithm {
 
-	@Override
 	public void run(final Plan plan) {
 		List<PlanElement> planElements = plan.getPlanElements();
 		for (int i = 0, n = planElements.size(); i < n; i++) {
@@ -53,11 +49,7 @@ public class TransitActsRemover implements PlanAlgorithm {
 			if (pe instanceof Activity) {
 				Activity act = (Activity) pe;
 				if (PtConstants.TRANSIT_ACTIVITY_TYPE.equals(act.getType())) {
-					double travelTime = ((Leg) plan.getPlanElements().get(i-1)).getTravelTime();
-					Route route = ((Leg) plan.getPlanElements().get(i-1)).getRoute();
 					((PlanImpl) plan).removeActivity(i);
-					((Leg) plan.getPlanElements().get(i-1)).setTravelTime(travelTime);
-					((Leg) plan.getPlanElements().get(i-1)).setRoute(route);
 					n -= 2;
 					i--; // i will be incremented again in next loop-iteration, so we'll check the next act
 				}
@@ -65,29 +57,7 @@ public class TransitActsRemover implements PlanAlgorithm {
 				Leg leg = (Leg) pe;
 				if (TransportMode.transit_walk.equals(leg.getMode())) {
 					leg.setMode(TransportMode.pt);
-					double distance = 0;
-					double travelTime = 0;
-					Id endLinkId = null;
-					DISTANCE_CALC:
-					for(int j = i; ;j++) {
-						PlanElement pe2 = planElements.get(j);
-						if(!(pe2 instanceof Activity && !PtConstants.TRANSIT_ACTIVITY_TYPE.equals(((Activity)pe2).getType()))) {
-							if(pe2 instanceof Leg) {
-								travelTime += ((Leg)pe2).getTravelTime();
-								if(((Leg)pe2).getRoute()!=null) {
-									distance += ((Leg)pe2).getRoute().getDistance();
-									endLinkId = ((Leg) pe2).getRoute().getEndLinkId();
-								}
-							}
-						}
-						else
-							break DISTANCE_CALC;
-					}
-					leg.setTravelTime(travelTime);
-					Route route = new GenericRouteImpl(leg.getRoute()==null?null:leg.getRoute().getStartLinkId(), endLinkId);
-					route.setTravelTime(travelTime);
-					route.setDistance(distance);
-					leg.setRoute(route);
+					leg.setRoute(null);
 				}
 			}
 		}
