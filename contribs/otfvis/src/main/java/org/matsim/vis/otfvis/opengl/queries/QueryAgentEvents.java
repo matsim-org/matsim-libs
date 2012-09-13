c071236210c07a1600d19153b082f74f42847dba
michaz <michael.zilske@gmail.com>
2012-09-13 16:30:25 +0000

    Simplified Events hierarchy
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@21019 a0517364-e873-4167-b1c8-416daa5a6b91

--- contribs/otfvis/src/main/java/org/matsim/vis/otfvis/opengl/queries/QueryAgentEvents.java
+++ contribs/otfvis/src/main/java/org/matsim/vis/otfvis/opengl/queries/QueryAgentEvents.java
@@ -27,10 +27,27 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Id;
+import org.matsim.core.api.experimental.events.ActivityEndEvent;
+import org.matsim.core.api.experimental.events.ActivityStartEvent;
+import org.matsim.core.api.experimental.events.AgentArrivalEvent;
+import org.matsim.core.api.experimental.events.AgentDepartureEvent;
+import org.matsim.core.api.experimental.events.AgentWait2LinkEvent;
+import org.matsim.core.api.experimental.events.Event;
 import org.matsim.core.api.experimental.events.EventsManager;
-import org.matsim.core.api.experimental.events.PersonEvent;
-import org.matsim.core.api.experimental.events.handler.PersonEventHandler;
+import org.matsim.core.api.experimental.events.LinkEnterEvent;
+import org.matsim.core.api.experimental.events.LinkLeaveEvent;
+import org.matsim.core.api.experimental.events.handler.ActivityEndEventHandler;
+import org.matsim.core.api.experimental.events.handler.ActivityStartEventHandler;
+import org.matsim.core.api.experimental.events.handler.AgentArrivalEventHandler;
+import org.matsim.core.api.experimental.events.handler.AgentDepartureEventHandler;
+import org.matsim.core.api.experimental.events.handler.AgentWait2LinkEventHandler;
+import org.matsim.core.api.experimental.events.handler.LinkEnterEventHandler;
+import org.matsim.core.api.experimental.events.handler.LinkLeaveEventHandler;
 import org.matsim.core.basic.v01.IdImpl;
+import org.matsim.core.events.PersonEntersVehicleEvent;
+import org.matsim.core.events.PersonLeavesVehicleEvent;
+import org.matsim.core.events.handler.PersonEntersVehicleEventHandler;
+import org.matsim.core.events.handler.PersonLeavesVehicleEventHandler;
 import org.matsim.vis.otfvis.VisMobsimFeature;
 import org.matsim.vis.otfvis.data.OTFServerQuadTree;
 import org.matsim.vis.otfvis.interfaces.OTFQuery;
@@ -44,7 +61,7 @@ import org.matsim.vis.otfvis.opengl.drawer.OTFOGLDrawer;
  * @author michaz
  *
  */
-public class QueryAgentEvents extends AbstractQuery implements PersonEventHandler {
+public class QueryAgentEvents extends AbstractQuery implements AgentDepartureEventHandler, AgentArrivalEventHandler, ActivityStartEventHandler, ActivityEndEventHandler, LinkEnterEventHandler, LinkLeaveEventHandler, AgentWait2LinkEventHandler, PersonEntersVehicleEventHandler, PersonLeavesVehicleEventHandler {
 
 	private static transient Logger logger = Logger.getLogger(QueryAgentEvents.class);
 	
@@ -77,16 +94,9 @@ public class QueryAgentEvents extends AbstractQuery implements PersonEventHandle
 	
 	private EventsManager eventsManager = null;
 	
-	private BlockingQueue<PersonEvent> queue = new LinkedBlockingQueue<PersonEvent>();
+	private BlockingQueue<Event> queue = new LinkedBlockingQueue<Event>();
 	
 	private Result result = null;
-
-	@Override
-	public void handleEvent(PersonEvent event) {
-		if(event.getPersonId().equals(this.agentId)){
-			queue.add(event);
-		}
-	}
 	
 	@Override
 	public void installQuery(VisMobsimFeature queueSimulation, EventsManager events, OTFServerQuadTree quad) {
@@ -105,9 +115,9 @@ public class QueryAgentEvents extends AbstractQuery implements PersonEventHandle
 	@Override
 	public OTFQueryResult query() {
 		result.newEventStrings.clear();
-		List<PersonEvent> newEvents = new ArrayList<PersonEvent>();
+		List<Event> newEvents = new ArrayList<Event>();
 		queue.drainTo(newEvents);
-		for (PersonEvent personEvent : newEvents) {
+		for (Event personEvent : newEvents) {
 			result.newEventStrings.add(personEvent.toString());
 		}
 		return result;
@@ -127,6 +137,69 @@ public class QueryAgentEvents extends AbstractQuery implements PersonEventHandle
 	public void uninstall() {
 		eventsManager.removeHandler(this);
 		logger.debug("Events query deregistered from handler.");
+	}
+
+	@Override
+	public void handleEvent(PersonLeavesVehicleEvent event) {
+		if(event.getPersonId().equals(this.agentId)){
+			queue.add(event);
+		}
+	}
+
+	@Override
+	public void handleEvent(PersonEntersVehicleEvent event) {
+		if(event.getPersonId().equals(this.agentId)){
+			queue.add(event);
+		}
+	}
+
+	@Override
+	public void handleEvent(AgentWait2LinkEvent event) {
+		if(event.getPersonId().equals(this.agentId)){
+			queue.add(event);
+		}
+	}
+
+	@Override
+	public void handleEvent(LinkLeaveEvent event) {
+		if(event.getPersonId().equals(this.agentId)){
+			queue.add(event);
+		}
+	}
+
+	@Override
+	public void handleEvent(LinkEnterEvent event) {
+		if(event.getPersonId().equals(this.agentId)){
+			queue.add(event);
+		}
+	}
+
+	@Override
+	public void handleEvent(ActivityEndEvent event) {
+		if(event.getPersonId().equals(this.agentId)){
+			queue.add(event);
+		}
+	}
+
+	@Override
+	public void handleEvent(ActivityStartEvent event) {
+		if(event.getPersonId().equals(this.agentId)){
+			queue.add(event);
+		}
+	}
+
+	@Override
+	public void handleEvent(AgentArrivalEvent event) {
+		if(event.getPersonId().equals(this.agentId)){
+			queue.add(event);
+		}
+	}
+
+	@Override
+	public void handleEvent(AgentDepartureEvent event) {
+		if(event.getPersonId().equals(this.agentId)){
+			queue.add(event);
+		}
 	}
 
 }
