0eec4253cb3f463ef90ab9c8c20da90236c2f0d9
rashid_waraich <rashid_waraich@users.sf.net>
2012-09-20 13:47:35 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@21096 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/main/java/playground/wrashid/tryouts/travelingSalesmanProblem/TravelingSalesmanSolver.java
+++ playgrounds/wrashid/src/main/java/playground/wrashid/tryouts/travelingSalesmanProblem/TravelingSalesmanSolver.java
@@ -19,7 +19,6 @@
 
 package playground.wrashid.tryouts.travelingSalesmanProblem;
 
-
 /*
  * Created on 08-Jan-2006
  * 
@@ -32,57 +31,72 @@ package playground.wrashid.tryouts.travelingSalesmanProblem;
 
 import java.util.Random;
 
-
 /**
  * Run the traveling salesman solver
  * 
- * The genetic algorithm will run untill it is aborted, and prints the 
- * current best solution from time to time
+ * The genetic algorithm will run untill it is aborted, and prints the current
+ * best solution from time to time
  * 
- * Edit the source to switch to brute force algorithm, change the random seed or the number of cities 
- * Some parameters are hardcoded in the Environment class
- * This should be improved in future versions. The current state of the code is merely a proof of concept,
- * little attention has been paid to proper engineering. 
+ * Edit the source to switch to brute force algorithm, change the random seed or
+ * the number of cities Some parameters are hardcoded in the Environment class
+ * This should be improved in future versions. The current state of the code is
+ * merely a proof of concept, little attention has been paid to proper
+ * engineering.
  * 
  * @author Bjoern Guenzel - http://blog.blinker.net
  * @author rashid_waraich (added some code)
  */
 public class TravelingSalesmanSolver {
 
-        public final static int RANDOM_SEED = 1234;
-        
-        public final static boolean RUN_BRUTE_FORCE = true;
-        public final static boolean RUN_GENETIC_ALGORITHM = true;
-        
-        public static Random random = new Random(RANDOM_SEED);
-        
-        /**
-         * @param args
-         */
-        public static void main(String[] args) {
-                //TODO interpret arguments, for example random seed and number of cities, and brute force or genetic algorithm could be 
-                
-        	
-        	
-        	
-        	TravelingSalesman salesman = new TravelingSalesman(30, random);
-                
-                salesman.printCosts();
-                
-                //uncomment for brute force: 
-                if(RUN_BRUTE_FORCE){
-                        System.out.println("*** running brute force algorithm ***");
-                        TravelingSalesmanBruteForce bruteForce = new TravelingSalesmanBruteForce(salesman);
-                        bruteForce.run();
-                } 
-                
-                if(RUN_GENETIC_ALGORITHM) {
-                        System.out.println("*** running genetic algorithm algorithm ***");
-                        Environment environment = new Environment(salesman);
+	public final static int RANDOM_SEED = 1234;
 
-                        environment.run();
-                }
-        }
+	public final static boolean RUN_BRUTE_FORCE = false;
+	public final static boolean RUN_GENETIC_ALGORITHM = true;
+
+	public static Random random = new Random(RANDOM_SEED);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO interpret arguments, for example random seed and number of
+		// cities, and brute force or genetic algorithm could be
+
+		int[][] coordinates = new int[4][2];
+		// car_park_name = M Hotel
+		coordinates[0][0] = 371500;
+		coordinates[0][1] = 140784;
+
+		// car_park_name = Amara Hotel
+		coordinates[1][0] = 371389;
+		coordinates[1][1] = 140960;
+
+		// car_park_name = International Plaza
+
+		coordinates[2][0] = 371611;
+		coordinates[2][1] = 141024;
+
+		// Icon Village
+		coordinates[3][0] = 371389;
+		coordinates[3][1] = 140971;
+
+		TravelingSalesman salesman = new TravelingSalesman(coordinates);
+
+		salesman.printCosts();
+
+		// uncomment for brute force:
+		if (RUN_BRUTE_FORCE) {
+			System.out.println("*** running brute force algorithm ***");
+			TravelingSalesmanBruteForce bruteForce = new TravelingSalesmanBruteForce(salesman);
+			bruteForce.run();
+		}
+
+		if (RUN_GENETIC_ALGORITHM) {
+			System.out.println("*** running genetic algorithm algorithm ***");
+			Environment environment = new Environment(salesman);
+
+			environment.run();
+		}
+	}
 
 }
-
