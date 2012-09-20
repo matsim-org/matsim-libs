e047c7efbb2bdf8ced480b315c5886bdd4aa828a
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2012-09-20 13:47:55 +0000

    removed unused package
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@21097 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/java/playground/thibautd/analysis/possiblesharedrides/package-info.java
+++
@@ -1,9 +0,0 @@
-
-/**
- * Package aimed at analysing shared ride possibilities from the output events
- * from an "autistic" simulation.
- *
- * Deprecated: use  {@link playground.thibautd.analysis.joinabletripsidentifier} instead
- * @author thibautd
- */
-package playground.thibautd.analysis.possiblesharedrides;
