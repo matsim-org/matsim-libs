3c03590c3e07904cd121959f9c96fe7e58288538
vgefeller <vgefeller@users.sf.net>
2012-09-25 15:43:00 +0000

    xy line chart implemented
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@21123 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wdoering/src/main/java/playground/wdoering/grips/evacuationanalysis/EvacuationTimeGraphPanel.java
+++ playgrounds/wdoering/src/main/java/playground/wdoering/grips/evacuationanalysis/EvacuationTimeGraphPanel.java
@@ -22,6 +22,14 @@ package playground.wdoering.grips.evacuationanalysis;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.util.List;
+
+import org.jfree.chart.JFreeChart;
+import org.matsim.core.utils.charts.XYLineChart;
+import org.matsim.core.utils.collections.Tuple;
 
 public class EvacuationTimeGraphPanel extends AbstractGraphPanel {
 	
@@ -50,6 +58,29 @@ public class EvacuationTimeGraphPanel extends AbstractGraphPanel {
 		System.out.println("cell size:" + data.getCellSize());
 		System.out.println("time sum:" + data.getTimeSum());
 		System.out.println("arrivals:" + data.getArrivals());
+		
+		List<Tuple<Double, Double>> xyData;
+		
+		XYLineChart chart = new XYLineChart("Evakuierungszeit", "x", "y");
+		
+		double [] xs = new double[10];
+		double [] ys = new double[10];
+		
+		int pos = 0;
+		for (int i = 0; i < 10; i++)
+		{
+
+			xs[i] = i;
+			ys[i] = i*i;
+		}
+		
+		JFreeChart freeChart = chart.getChart();
+		
+		Graphics g = this.getGraphics();
+		Graphics2D g2d = (Graphics2D)g;
+		
+		freeChart.draw(g2d, new Rectangle2D.Double(0,0,200,200));
+		
 		
 	}
 
