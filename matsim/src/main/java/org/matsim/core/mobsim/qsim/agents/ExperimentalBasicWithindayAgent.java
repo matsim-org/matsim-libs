0e6f0af2dfc7217e58ec8ed06afd841fdef89e29
skull5 <dobler@senozon.com>
2012-09-26 13:59:29 +0000

    ExperimentalBasicWithinDayAgents can now optionally (this is not the default behavior!) overwrite the person's selected plan instead of creating a copy which is changed during the simulation.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@21137 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/core/mobsim/qsim/agents/ExperimentalBasicWithindayAgent.java
+++ matsim/src/main/java/org/matsim/core/mobsim/qsim/agents/ExperimentalBasicWithindayAgent.java
@@ -28,11 +28,13 @@ import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.core.mobsim.qsim.interfaces.Netsim;
 import org.matsim.core.population.PlanImpl;
 
-/**This class is an attempt to provide access to the internals of DefaultPersonDriverAgent
+/**
+ * <p>
+ * This class is an attempt to provide access to the internals of DefaultPersonDriverAgent
  * in a way that it can be used for within-day replanning.
- * <p/>
- * <i>The class is, as its name states, experimental.  Use at your own risk, and expect even less support than with other pieces
- * of matsim. </i>
+ * </p>
+ * <i>The class is, as its name states, experimental. Use at your own risk, and expect even 
+ * less support than with other pieces of matsim. </i>
  * @author nagel
  */
 public class ExperimentalBasicWithindayAgent extends PersonDriverAgentImpl implements PlanBasedWithinDayAgent {
@@ -42,6 +44,17 @@ public class ExperimentalBasicWithindayAgent extends PersonDriverAgentImpl imple
 		return agent;
 	}
 	
+	/*
+	 * By default, an ExperimentalBasicWithindayAgent creates a copy of a
+	 * plan that it executes. All replanning operations only change this copy
+	 * but do not affect the person's selected plan. However, the score for
+	 * the execution of the plan is still written to the person's plan.
+	 * 
+	 * If this switch is set to false, the agent modifies the person's original
+	 * plan. Do not use this unless you know what you are doing!! cdobler, sep'12
+	 */
+	public static boolean copySelectedPlan = true;
+	
 	protected ExperimentalBasicWithindayAgent(Person p, Netsim simulation) {
 		/*
 		 * Create a copy of the person's selected plan.
@@ -49,13 +62,17 @@ public class ExperimentalBasicWithindayAgent extends PersonDriverAgentImpl imple
 		 * simulated with this Agent but is NOT added to the person's list of plans!
 		 * The simulation should still score the person's selected plan. 
 		 */
-		super(p, copySelectedPlan(p), simulation);
+		super(p, prepareAgentsPlan(p), simulation);
 	}
 
-	private static Plan copySelectedPlan(Person p) {
-		Plan executedPlan = new PlanImpl(p);
-		((PlanImpl)executedPlan).copyPlan(p.getSelectedPlan());
-		return executedPlan;
+	private static Plan prepareAgentsPlan(Person p) {
+		if (copySelectedPlan) {
+			Plan executedPlan = new PlanImpl(p);
+			((PlanImpl)executedPlan).copyPlan(p.getSelectedPlan());			
+			return executedPlan;
+		} else {
+			return p.getSelectedPlan();
+		}
 	}
 
 	@Override
