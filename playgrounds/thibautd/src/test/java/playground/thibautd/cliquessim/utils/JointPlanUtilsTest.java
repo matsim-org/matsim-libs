2ed0975591a91c70d515541e369afb0d14b26cfd
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2012-09-27 11:15:24 +0000

    added a test case
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@21163 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/test/java/playground/thibautd/cliquessim/utils/JointPlanUtilsTest.java
+++ playgrounds/thibautd/src/test/java/playground/thibautd/cliquessim/utils/JointPlanUtilsTest.java
@@ -200,14 +200,128 @@ public class JointPlanUtilsTest {
 					new JointPlan(
 						new Clique( null ),
 						plans )));
+
+		// plan 3
+		// two passenger, two ODs, two JT for one passenger
+		plans = new HashMap<Id, Plan>();
+		jointTrips = new ArrayList<JointTrip>();
+		plan = new PlanImpl( driver );
+		plan.createAndAddActivity( "home" , link1 );
+		plan.createAndAddLeg( TransportMode.walk );
+		plan.createAndAddActivity( JointActingTypes.PICK_UP , link2 );
+		driverLeg = plan.createAndAddLeg( JointActingTypes.DRIVER );
+		driverLeg.setRoute( new DriverRoute(
+					new LinkNetworkRouteImpl( link2 , link3 ),
+					Arrays.asList( passenger1.getId() )));
+
+		plan.createAndAddActivity( JointActingTypes.DROP_OFF , link3 );
+		driverLeg2 = plan.createAndAddLeg( JointActingTypes.DRIVER );
+		driverLeg2.setRoute( new DriverRoute(
+					new LinkNetworkRouteImpl( link3 , link4 ),
+					Arrays.asList(
+						passenger1.getId(),
+						passenger2.getId())));
+		plan.createAndAddActivity( JointActingTypes.DROP_OFF , link4 );
+		plan.createAndAddLeg( TransportMode.walk );
+		plan.createAndAddActivity( "take a nap" , link4 );
+		plan.createAndAddLeg( TransportMode.walk );
+		plan.createAndAddActivity( JointActingTypes.PICK_UP , link3 );
+		Leg driverLeg3 = plan.createAndAddLeg( JointActingTypes.DRIVER );
+		driverLeg3.setRoute( new DriverRoute(
+					new LinkNetworkRouteImpl( link3 , link1 ),
+					Arrays.asList(
+						passenger1.getId())));
+		plan.createAndAddActivity( JointActingTypes.DROP_OFF , link1 );
+		plan.createAndAddLeg( TransportMode.walk );
+		plan.createAndAddActivity( "home" , link1);
+
+		plans.put( driver.getId() , plan );
+
+		plan = new PlanImpl( passenger1 );
+		plan.createAndAddActivity( "home" , link1 );
+		plan.createAndAddLeg( TransportMode.walk );
+		plan.createAndAddActivity( JointActingTypes.PICK_UP , link2 );
+		passengerLeg = plan.createAndAddLeg( JointActingTypes.PASSENGER );
+		passengerRoute = new PassengerRoute( link2 , link4 );
+		passengerRoute.setDriverId( driver.getId() );
+		passengerLeg.setRoute( passengerRoute );
+		plan.createAndAddActivity( JointActingTypes.DROP_OFF , link4 );
+		plan.createAndAddLeg( TransportMode.walk );
+		plan.createAndAddActivity( "sunbath" , link4);
+		plan.createAndAddLeg( TransportMode.walk );
+		plan.createAndAddActivity( JointActingTypes.PICK_UP , link3 );
+		Leg passengerLeg3 = plan.createAndAddLeg( JointActingTypes.PASSENGER );
+		passengerRoute = new PassengerRoute( link3 , link1 );
+		passengerRoute.setDriverId( driver.getId() );
+		passengerLeg3.setRoute( passengerRoute );
+		plan.createAndAddActivity( JointActingTypes.DROP_OFF , link1 );
+		plan.createAndAddLeg( TransportMode.walk );
+		plan.createAndAddActivity( "home" , link1);
+
+		plans.put( passenger1.getId() , plan );
+
+		plan = new PlanImpl( passenger2 );
+		plan.createAndAddActivity( "home" , link1 );
+		plan.createAndAddLeg( TransportMode.walk );
+		plan.createAndAddActivity( JointActingTypes.PICK_UP , link3 );
+		passengerLeg2 = plan.createAndAddLeg( JointActingTypes.PASSENGER );
+		passengerRoute = new PassengerRoute( link3 , link4 );
+		passengerRoute.setDriverId( driver.getId() );
+		passengerLeg2.setRoute( passengerRoute );
+		plan.createAndAddActivity( JointActingTypes.DROP_OFF , link4 );
+		plan.createAndAddLeg( TransportMode.walk );
+		plan.createAndAddActivity( "home" , link1);
+
+		plans.put( passenger2.getId() , plan );
+
+		driverTrip = new DriverTrip( driver.getId() );
+		driverTrip.driverTrip.add( driverLeg );
+		driverTrip.driverTrip.add( driverLeg2 );
+		driverTrip.passengerOrigins.put( passenger1.getId() , link2 );
+		driverTrip.passengerDestinations.put( passenger1.getId() , link4 );
+		driverTrip.passengerOrigins.put( passenger2.getId() , link3 );
+		driverTrip.passengerDestinations.put( passenger2.getId() , link4 );
+
+		DriverTrip driverTrip2 = new DriverTrip( driver.getId() );
+		driverTrip2.driverTrip.add( driverLeg3 );
+		driverTrip2.passengerOrigins.put( passenger1.getId() , link3 );
+		driverTrip2.passengerDestinations.put( passenger1.getId() , link1 );
+
+		fixtures.add(
+				new Fixture(
+					"two passengers two trips",
+					Arrays.asList( driverTrip , driverTrip2 ),
+					new JointTravelStructure(
+						Arrays.asList(
+							new JointTrip(
+								driver.getId(),
+								Arrays.asList( driverLeg , driverLeg2 ),
+								passenger1.getId(),
+								passengerLeg),
+							new JointTrip(
+								driver.getId(),
+								Arrays.asList( driverLeg2 ),
+								passenger2.getId(),
+								passengerLeg2),
+							new JointTrip(
+								driver.getId(),
+								Arrays.asList( driverLeg3 ),
+								passenger1.getId(),
+								passengerLeg3))),
+					new JointPlan(
+						new Clique( null ),
+						plans )));
 	}
 
 	@Test
 	public void testExtractJointTrips() throws Exception {
 		for ( Fixture f : fixtures ) {
 			JointTravelStructure struct = JointPlanUtils.analyseJointTravel( f.plan );
+
 			Assert.assertEquals(
-					"wrong structure for fixture "+f.name,
+					"wrong structure for fixture "+f.name+
+					" of size "+f.structure.jointTrips.size()+
+					" compared to result of size "+struct.jointTrips.size(),
 					f.structure,
 					struct);
 		}
