7abf2a1baa0cfed2b668831bea9eb5d982b62b0e
mrieser <rieser@gmail.com>
2012-10-05 11:53:44 +0000

    add GPL-Header, fixed formatting to be consistent.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@21245 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/core/mobsim/qsim/InternalInterface.java
+++ matsim/src/main/java/org/matsim/core/mobsim/qsim/InternalInterface.java
@@ -1,3 +1,22 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
 package org.matsim.core.mobsim.qsim;
 
 import org.matsim.api.core.v01.Id;
@@ -5,8 +24,8 @@ import org.matsim.core.mobsim.framework.MobsimAgent;
 import org.matsim.core.mobsim.qsim.interfaces.Netsim;
 
 public interface InternalInterface {
-	public Netsim getMobsim() ; 
-	public void arrangeNextAgentState(MobsimAgent agent) ;
+	public Netsim getMobsim(); 
+	public void arrangeNextAgentState(MobsimAgent agent);
 	void registerAdditionalAgentOnLink(MobsimAgent agent);
 	MobsimAgent unregisterAdditionalAgentOnLink(Id agentId, Id linkId);
 	public void rescheduleActivityEnd(MobsimAgent agent);
