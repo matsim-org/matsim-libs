8ef28ec3ce75e2d1673683217221712049a2e95b
michaz <michael.zilske@gmail.com>
2012-10-09 13:24:44 +0000

    Removed functionality from the AbstractQLink which is duplicated by the VehicularDepartureHandler - checking if start link is destination link.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@21271 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/core/mobsim/qsim/qnetsimengine/AbstractQLink.java
+++ matsim/src/main/java/org/matsim/core/mobsim/qsim/qnetsimengine/AbstractQLink.java
@@ -49,27 +49,27 @@ import org.matsim.pt.transitSchedule.api.TransitStopFacility;
  *
  */
 abstract class AbstractQLink extends QLinkInternalI {
-	
+
 	private static final Comparator<QVehicle> VEHICLE_EXIT_COMPARATOR = new QVehicleEarliestLinkExitTimeComparator();
 
-	final Link link ;
-	
+	final Link link;
+
 	final QNetwork network;	
-	
+
 	// joint implementation for Customizable
 	private Map<String, Object> customAttributes = new HashMap<String, Object>();
 
 	private final Map<Id, QVehicle> parkedVehicles = new LinkedHashMap<Id, QVehicle>(10);
 
 	private final Map<Id, MobsimAgent> additionalAgentsOnLink = new LinkedHashMap<Id, MobsimAgent>();
-	
+
 	private final Map<Id, MobsimDriverAgent> agentsWaitingForCars = new LinkedHashMap<Id, MobsimDriverAgent>();
 
 	/**
 	 * A list containing all transit vehicles that are at a stop but not
 	 * blocking other traffic on the lane.
 	 */
- /*package*/ final Queue<QVehicle> transitVehicleStopQueue = new PriorityQueue<QVehicle>(5, VEHICLE_EXIT_COMPARATOR);
+	/*package*/ final Queue<QVehicle> transitVehicleStopQueue = new PriorityQueue<QVehicle>(5, VEHICLE_EXIT_COMPARATOR);
 
 	/**
 	 * All vehicles from parkingList move to the waitingList as soon as their time
@@ -79,23 +79,23 @@ abstract class AbstractQLink extends QLinkInternalI {
 	/*package*/ final Queue<QVehicle> waitingList = new LinkedList<QVehicle>();
 
 	/*package*/ NetElementActivator netElementActivator;
-	
+
 	AbstractQLink(Link link, QNetwork network) {
 		this.link = link ;
 		this.network = network;
 		this.netElementActivator = network.simEngine;
 	}
-	
+
 	abstract boolean doSimStep(double now);
 
 	abstract void activateLink();
 
 	abstract void addFromIntersection(final QVehicle veh);
-	
-	abstract QNode getToNode() ;
+
+	abstract QNode getToNode();
 
 	/*package*/ final void addParkedVehicle(MobsimVehicle vehicle) {
-		QVehicle qveh = (QVehicle) vehicle ; // cast ok: when it gets here, it needs to be a qvehicle to work.
+		QVehicle qveh = (QVehicle) vehicle; // cast ok: when it gets here, it needs to be a qvehicle to work.
 		this.parkedVehicles.put(qveh.getId(), qveh);
 		qveh.setCurrentLink(this.link);
 	}
@@ -105,7 +105,7 @@ abstract class AbstractQLink extends QLinkInternalI {
 	}
 
 	/*package*/ final void addDepartingVehicle(MobsimVehicle mvehicle) {
-		QVehicle vehicle = (QVehicle) mvehicle ;
+		QVehicle vehicle = (QVehicle) mvehicle;
 		this.waitingList.add(vehicle);
 		vehicle.setCurrentLink(this.getLink());
 		this.activateLink();
@@ -119,25 +119,20 @@ abstract class AbstractQLink extends QLinkInternalI {
 		return this.additionalAgentsOnLink.remove(mobsimAgentId);
 	}
 
-	/*package*/ Collection<MobsimAgent> getAdditionalAgentsOnLink(){
+	/*package*/ Collection<MobsimAgent> getAdditionalAgentsOnLink() {
 		return Collections.unmodifiableCollection( this.additionalAgentsOnLink.values());
 	}
-	
+
 	void clearVehicles() {
 		this.parkedVehicles.clear();
-
 		double now = this.network.simEngine.getMobsim().getSimTimer().getTimeOfDay();
-
 		for (QVehicle veh : this.waitingList) {
 			this.network.simEngine.getMobsim().getEventsManager().processEvent(
 					new AgentStuckEvent(now, veh.getDriver().getId(), veh.getCurrentLink().getId(), veh.getDriver().getMode()));
 			this.network.simEngine.getMobsim().getAgentCounter().incLost();
 			this.network.simEngine.getMobsim().getAgentCounter().decLiving();
 		}
-//		this.network.simEngine.getMobsim().getAgentCounter().decLiving(this.waitingList.size());
-//		this.network.simEngine.getMobsim().getAgentCounter().incLost(this.waitingList.size());
 		this.waitingList.clear();
-
 	}
 
 	void makeVehicleAvailableToNextDriver(QVehicle veh, double now) {
@@ -146,24 +141,13 @@ abstract class AbstractQLink extends QLinkInternalI {
 			this.letAgentDepartWithVehicle(agentWaitingForCar, veh, now);
 		}
 	}
-	
+
 	final void letAgentDepartWithVehicle(MobsimDriverAgent agent, QVehicle vehicle, double now) {
 		vehicle.setDriver(agent);
-		if ( agent.getDestinationLinkId().equals(link.getId()) && (agent.chooseNextLinkId() == null)) {
-			// yyyy this should be handled at person level, not vehicle level.  kai, feb'10
-
-			agent.endLegAndComputeNextState(now);
-			this.addParkedVehicle(vehicle);
-			this.network.simEngine.internalInterface.arrangeNextAgentState(agent) ;
-			// yyyyyy I think this neither works this way nor when I exchange the last two lines. kai, dec'11
-			
-		} else {
-			EventsManager eventsManager = network.simEngine.getMobsim().getEventsManager();
-			eventsManager.processEvent(eventsManager.getFactory().createPersonEntersVehicleEvent(now, agent.getId(), vehicle.getId()));
-			this.addDepartingVehicle(vehicle);
-		}
+		EventsManager eventsManager = network.simEngine.getMobsim().getEventsManager();
+		eventsManager.processEvent(eventsManager.getFactory().createPersonEntersVehicleEvent(now, agent.getId(), vehicle.getId()));
+		this.addDepartingVehicle(vehicle);
 	}
-	
 
 	final boolean addTransitToBuffer(final double now, final QVehicle veh) {
 		if (veh.getDriver() instanceof TransitDriverAgent) {
@@ -190,7 +174,7 @@ abstract class AbstractQLink extends QLinkInternalI {
 		QVehicle ret = this.parkedVehicles.get(vehicleId);
 		return ret;
 	}
-	
+
 	@Override
 	public final Collection<MobsimVehicle> getAllVehicles() {
 		Collection<MobsimVehicle> vehicles = this.getAllNonParkedVehicles();
@@ -198,8 +182,6 @@ abstract class AbstractQLink extends QLinkInternalI {
 		return vehicles;
 	}
 
-	
-	
 	@Override
 	public final Map<String, Object> getCustomAttributes() {
 		return customAttributes;
@@ -213,5 +195,5 @@ abstract class AbstractQLink extends QLinkInternalI {
 		Id vehicleId = agent.getPlannedVehicleId() ;
 		agentsWaitingForCars.put(vehicleId, agent);
 	}
-	
+
 }
