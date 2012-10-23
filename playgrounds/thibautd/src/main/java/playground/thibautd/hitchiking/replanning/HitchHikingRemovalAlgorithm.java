a6413afda6cb2183f7bfa41c6a8aab19d3c5af67
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2012-10-23 07:37:36 +0000

    finally an algorithm to remove hitch-hiking trips!
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@21464 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/thibautd/src/main/java/playground/thibautd/hitchiking/replanning/HitchHikingRemovalAlgorithm.java
@@ -0,0 +1,93 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * HitchHikingRemovalAlgorithm.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.thibautd.hitchiking.replanning;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.matsim.api.core.v01.population.Leg;
+import org.matsim.api.core.v01.population.Plan;
+import org.matsim.api.core.v01.population.PlanElement;
+import org.matsim.population.algorithms.PlanAlgorithm;
+
+import playground.thibautd.hitchiking.HitchHikingConstants;
+import playground.thibautd.utils.SubtourStructure;
+import playground.thibautd.utils.SubtourStructure.Subtour;
+
+/**
+ * Algorithm which removes a random hitch hiking trip and replaces it by a leg
+ * of the subtour mode.
+ * @author thibautd
+ */
+public class HitchHikingRemovalAlgorithm implements PlanAlgorithm {
+	private final Random random;
+
+	public HitchHikingRemovalAlgorithm(final Random random) {
+		this.random = random;
+	}
+
+	@Override
+	public void run(final Plan plan) {
+		SubtourStructure structure = new SubtourStructure( plan );
+
+		Subtour toActOn = getRandomHhSubtour( structure );
+
+		String mode = null;
+		for (PlanElement pe : toActOn.getPlanElements()) {
+			if ( !(pe instanceof Leg) ) continue;
+			Leg l = (Leg) pe;
+			if ( !HitchHikingConstants.DRIVER_MODE.equals( l.getMode() ) &&
+					!HitchHikingConstants.PASSENGER_MODE.equals( l.getMode() ) ) {
+				mode = l.getMode();
+				break;
+			}
+		}
+
+		for (PlanElement pe : toActOn.getPlanElements()) {
+			if ( !(pe instanceof Leg) ) continue;
+			Leg l = (Leg) pe;
+			if ( HitchHikingConstants.DRIVER_MODE.equals( l.getMode() ) &&
+					HitchHikingConstants.PASSENGER_MODE.equals( l.getMode() ) ) {
+				l.setMode( mode );
+				l.setRoute( null );
+			}
+		}
+	}
+
+	private Subtour getRandomHhSubtour(final SubtourStructure structure) {
+		List<Subtour> eligibleSubtours = new ArrayList<Subtour>();
+
+		for (Subtour s : structure.getSubtours()) {
+			for ( PlanElement pe : s.getPlanElements() ) {
+				if ( !(pe instanceof Leg) ) continue;
+				Leg l = (Leg) pe;
+				if ( HitchHikingConstants.DRIVER_MODE.equals( l.getMode() ) ||
+						HitchHikingConstants.PASSENGER_MODE.equals( l.getMode() ) ) {
+					eligibleSubtours.add( s );
+					break;
+				}
+			}
+		}
+
+		return eligibleSubtours.get( random.nextInt( eligibleSubtours.size() ) );
+	}
+}
+
