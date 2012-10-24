d75053794ab3204f308521e2e56a75fe3e2d5e53
michaz <michael.zilske@gmail.com>
2012-10-24 17:03:58 +0000

    Moved Bin* to core
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@21498 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/main/java/org/matsim/analysis/BinEntry.java
@@ -0,0 +1,48 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.analysis;
+
+public class BinEntry {
+	
+	private double value;
+	private double weight;
+	
+	public BinEntry(double value, double weight) {
+		this.value = value;
+		this.weight = weight;
+	}
+	
+	
+	public double getValue() {
+		return value;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+}
