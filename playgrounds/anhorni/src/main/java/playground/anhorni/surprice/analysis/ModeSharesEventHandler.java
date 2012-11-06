a4347a0b8da51887385d0c1b7a04777d33e89960
andreas77 <andreashorni@hotmail.com>
2012-11-06 14:18:26 +0000

    share of leg numbers
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@21687 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/anhorni/src/main/java/playground/anhorni/surprice/analysis/ModeSharesEventHandler.java
+++ playgrounds/anhorni/src/main/java/playground/anhorni/surprice/analysis/ModeSharesEventHandler.java
@@ -203,12 +203,15 @@ public class ModeSharesEventHandler extends AbstractClassifiedFrequencyAnalysis 
 		log.info("mode shares: ---------------------------");
 		for (Map.Entry<String, Double> mode : currentModeXYs.entrySet()) {
 			if (this.xy.equals("times")) {
-				log.info("Share of " + mode.getKey() + ":\t" + "time [s]: "+ (100d*mode.getValue() / totalXY) +"%"+ "\tnumber of legs: "+
-						(100d * this.getNumberOfLegs(mode.getKey()) / nLegs)+"%");
+				log.info("Share of " + mode.getKey() + ":\t" + "time [s]: "+ (100d*mode.getValue() / totalXY) +"%");
 			}
 			else {
-				log.info("Share of " + mode.getKey() + ":\t" + "distance [km]: "+ (100d*mode.getValue() / totalXY) +"%"+ "\tnumber of legs: "+
-					(100d * this.getNumberOfLegs(mode.getKey()) / nLegs)+"%");
+				log.info("Share of " + mode.getKey() + ":\t" + "distance [km]: "+ (100d*mode.getValue() / totalXY) +"%");
+			}
+		}
+		for (Map.Entry<String, Double> mode : currentModeXYs.entrySet()) {
+			if (this.xy.equals("times")) {
+				log.info("number of legs: "+ (100d * this.getNumberOfLegs(mode.getKey()) / nLegs)+"%");
 			}
 		}
 	}
