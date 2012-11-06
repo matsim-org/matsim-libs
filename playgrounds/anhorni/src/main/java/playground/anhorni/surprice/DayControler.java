398dc0f11944beaf6aa707f1c78429e079182e0b
andreas77 <andreashorni@hotmail.com>
2012-11-06 12:19:59 +0000

    adapting mode share analysis
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@21679 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/anhorni/src/main/java/playground/anhorni/surprice/DayControler.java
+++ playgrounds/anhorni/src/main/java/playground/anhorni/surprice/DayControler.java
@@ -19,13 +19,11 @@
 
 package playground.anhorni.surprice;
 
-import herbie.running.controler.listeners.CalcLegTimesHerbieListener;
-import herbie.running.controler.listeners.LegDistanceDistributionWriter;
-import herbie.running.controler.listeners.ScoreElements;
-
 import org.matsim.core.config.Config;
 import org.matsim.core.controler.Controler;
 import org.matsim.utils.objectattributes.ObjectAttributes;
+
+import playground.anhorni.surprice.analysis.ModeSharesControlerListener;
 import playground.anhorni.surprice.scoring.SurpriceScoringFunctionFactory;
 
 public class DayControler extends Controler {
@@ -33,12 +31,7 @@ public class DayControler extends Controler {
 	private AgentMemories memories = new AgentMemories();
 	private String day;	
 	private ObjectAttributes preferences;
-	
-	protected static final String SCORE_ELEMENTS_FILE_NAME = "scoreElementsAverages.txt";
-	protected static final String CALC_LEG_TIMES_FILE_NAME = "calcLegTimes.txt";
-	protected static final String LEG_DISTANCE_DISTRIBUTION_FILE_NAME = "legDistanceDistribution.txt";
-	protected static final String LEG_TRAVEL_TIME_DISTRIBUTION_FILE_NAME = "legTravelTimeDistribution.txt";
-	
+		
 	public DayControler(final Config config, AgentMemories memories, String day, ObjectAttributes preferences) {
 		super(config);	
 		super.setOverwriteFiles(true);
@@ -56,8 +49,6 @@ public class DayControler extends Controler {
 	  	
 	  	//this.addControlerListener(new ScoringFunctionResetter()); TODO: check if really not necessary anymore!
 	  	this.addControlerListener(new Memorizer(this.memories, this.day));
-	  	this.addControlerListener(new ScoreElements(SCORE_ELEMENTS_FILE_NAME));
-		this.addControlerListener(new CalcLegTimesHerbieListener(CALC_LEG_TIMES_FILE_NAME, LEG_TRAVEL_TIME_DISTRIBUTION_FILE_NAME));
-		this.addControlerListener(new LegDistanceDistributionWriter(LEG_DISTANCE_DISTRIBUTION_FILE_NAME, this.scenarioData.getNetwork()));
+	  	this.addControlerListener(new ModeSharesControlerListener());
 	}
 }
