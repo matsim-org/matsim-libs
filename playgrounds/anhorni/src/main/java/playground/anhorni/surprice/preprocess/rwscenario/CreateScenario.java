fb5bec635b476ba4875be9ca4c1df4011b2e1726
andreas77 <andreashorni@hotmail.com>
2012-11-12 10:06:26 +0000

    change to area toll
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@21745 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/anhorni/src/main/java/playground/anhorni/surprice/preprocess/rwscenario/CreateScenario.java
+++ playgrounds/anhorni/src/main/java/playground/anhorni/surprice/preprocess/rwscenario/CreateScenario.java
@@ -224,8 +224,8 @@ public class CreateScenario {
 				tollZone,
 				8.0 * 3600.0,
 				18.0 * 3600.0,
-				0.5,
-				"link",
+				10.0,
+				"area",
 				"ZH scenario"); 
 		// TODO: different schemes for different days
 	}
