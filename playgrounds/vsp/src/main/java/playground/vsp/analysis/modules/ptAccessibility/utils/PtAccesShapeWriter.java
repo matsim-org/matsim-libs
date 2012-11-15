f750ff85c0f7a86e1b09ee677164ccb9e3cc45b5
dan-ro <daniel_roeder@web.de>
2012-11-15 18:19:04 +0000

    added ptAccessibilityAnalysis
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@21826 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/vsp/src/main/java/playground/vsp/analysis/modules/ptAccessibility/utils/PtAccesShapeWriter.java
@@ -0,0 +1,142 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.vsp.analysis.modules.ptAccessibility.utils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.ServiceConfigurationError;
+
+import org.apache.log4j.Logger;
+import org.geotools.factory.FactoryRegistryException;
+import org.geotools.feature.AttributeType;
+import org.geotools.feature.AttributeTypeFactory;
+import org.geotools.feature.DefaultAttributeTypeFactory;
+import org.geotools.feature.Feature;
+import org.geotools.feature.FeatureType;
+import org.geotools.feature.FeatureTypeBuilder;
+import org.geotools.feature.IllegalAttributeException;
+import org.geotools.feature.SchemaException;
+import org.matsim.core.utils.geometry.geotools.MGC;
+import org.matsim.core.utils.geometry.transformations.TransformationFactory;
+import org.matsim.core.utils.gis.ShapeFileWriter;
+
+import playground.vsp.analysis.modules.ptAccessibility.activity.ActivityLocation;
+import playground.vsp.analysis.modules.ptAccessibility.activity.LocationMap;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.MultiPoint;
+import com.vividsolutions.jts.geom.MultiPolygon;
+
+/**
+ * @author droeder
+ * just a helper-class 
+ *
+ */
+public class PtAccesShapeWriter {
+
+	@SuppressWarnings("unused")
+	private static final Logger log = Logger
+			.getLogger(PtAccesShapeWriter.class);
+
+	private PtAccesShapeWriter() {
+		
+	}
+
+	/**
+	 * @param mps
+	 * @param string
+	 */
+	public static void writeMultiPolygons(Map<String, MultiPolygon> mps, String filename, String name) {
+		AttributeType[] attribs = new AttributeType[2];
+		attribs[0] = DefaultAttributeTypeFactory.newAttributeType("MultiPolygon", MultiPolygon.class, true, null, null, MGC.getCRS(TransformationFactory.WGS84_UTM35S));
+		attribs[1] = AttributeTypeFactory.newAttributeType("name", String.class);
+		FeatureType featureType = null ;
+		try {
+			featureType = FeatureTypeBuilder.newFeatureType(attribs, name);
+		} catch (FactoryRegistryException e) {
+			e.printStackTrace();
+		} catch (SchemaException e) {
+			e.printStackTrace();
+		}
+		Collection<Feature> features = new ArrayList<Feature>();
+		
+		Object[] featureAttribs ;
+		
+		for(Entry<String, MultiPolygon> e: mps.entrySet()){
+			featureAttribs = new Object[2];
+			featureAttribs[0] = e.getValue();
+			featureAttribs[1] = e.getKey();
+			try {
+				features.add(featureType.create(featureAttribs));
+			} catch (IllegalAttributeException e1) {
+				e1.printStackTrace();
+			}
+		}
+		try{
+			ShapeFileWriter.writeGeometries(features, filename);
+		}catch(ServiceConfigurationError e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static void writeActivityLocations(LocationMap locationMap, String filename, String name){
+		AttributeType[] attribs = new AttributeType[2];
+		attribs[0] = DefaultAttributeTypeFactory.newAttributeType("MultiPoint", MultiPoint.class, true, null, null, MGC.getCRS(TransformationFactory.WGS84_UTM35S));
+		attribs[1] = AttributeTypeFactory.newAttributeType("name", String.class);
+		FeatureType featureType = null ;
+		try {
+			featureType = FeatureTypeBuilder.newFeatureType(attribs, name);
+		} catch (FactoryRegistryException e) {
+			e.printStackTrace();
+		} catch (SchemaException e) {
+			e.printStackTrace();
+		}
+		Collection<Feature> features = new ArrayList<Feature>();
+		
+		Object[] featureAttribs ;
+		
+		GeometryFactory factory = new GeometryFactory();
+		Coordinate[] c;
+		for(Entry<String, List<ActivityLocation>> e: locationMap.getType2Locations().entrySet()){
+			c = new Coordinate[e.getValue().size()];
+			for(int i  = 0; i < e.getValue().size(); i++){
+				c[i] = e.getValue().get(i).getCoord();
+			}
+			featureAttribs = new Object[2];
+			featureAttribs[0] = factory.createMultiPoint(c);
+			featureAttribs[1] = e.getKey();
+			try {
+				features.add(featureType.create(featureAttribs));
+			} catch (IllegalAttributeException e1) {
+				e1.printStackTrace();
+			}
+		}
+		try{
+			ShapeFileWriter.writeGeometries(features, filename);
+		}catch(ServiceConfigurationError e){
+			e.printStackTrace();
+		}
+	}
+
+}
+
