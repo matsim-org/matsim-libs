0ff656b481476bb560e5acace1e659ca460ea209
benkick <benkick@users.sf.net>
2012-11-19 03:35:52 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@21867 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/benjamin/src/main/java/playground/benjamin/processing/TestProcessing.java
+++ playgrounds/benjamin/src/main/java/playground/benjamin/processing/TestProcessing.java
@@ -19,30 +19,29 @@
  * *********************************************************************** */
 package playground.benjamin.processing;
 
-import processing.core.PApplet;
 
 
 /**
  * @author benjamin
  *
  */
-public class TestProcessing extends PApplet{
+public class TestProcessing {}//extends PApplet{
 
-	@Override
-	public void setup() {
-		size(200,200);
-		background(0);
-	}
-
-	@Override
-	public void draw() {
-		stroke(255);
-		if (mousePressed) {
-			line(mouseX,mouseY,pmouseX,pmouseY);
-		}
-	}
-
-	public static void main(String[] args) {
-		PApplet.main(new String[] {"--present", "playground.benjamin.processing.TestProcessing"});
-	}
-}
+//	@Override
+//	public void setup() {
+//		size(200,200);
+//		background(0);
+//	}
+//
+//	@Override
+//	public void draw() {
+//		stroke(255);
+//		if (mousePressed) {
+//			line(mouseX,mouseY,pmouseX,pmouseY);
+//		}
+//	}
+//
+//	public static void main(String[] args) {
+//		PApplet.main(new String[] {"--present", "playground.benjamin.processing.TestProcessing"});
+//	}
+//}
