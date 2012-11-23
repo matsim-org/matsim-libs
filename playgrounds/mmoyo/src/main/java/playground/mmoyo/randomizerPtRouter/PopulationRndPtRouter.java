6781f8f69f9fda3c485e232170e7ab0fb39e33e7
mmoyo <mmoyo@users.sf.net>
2012-11-23 14:37:25 +0000

    Added launchers for randomized ptRouter
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@21951 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/mmoyo/src/main/java/playground/mmoyo/randomizerPtRouter/PopulationRndPtRouter.java
@@ -0,0 +1,90 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.mmoyo.randomizerPtRouter;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.population.Population;
+import org.matsim.api.core.v01.population.PopulationWriter;
+import org.matsim.core.config.groups.PlansCalcRouteConfigGroup;
+import org.matsim.core.population.PopulationFactoryImpl;
+import org.matsim.core.population.routes.ModeRouteFactory;
+import org.matsim.core.router.costcalculators.FreespeedTravelTimeAndDisutility;
+import org.matsim.core.router.util.DijkstraFactory;
+import org.matsim.core.router.util.LeastCostPathCalculatorFactory;
+import org.matsim.core.router.util.TravelDisutility;
+import org.matsim.core.router.util.TravelTime;
+import org.matsim.pt.config.TransitConfigGroup;
+import org.matsim.pt.router.PlansCalcTransitRoute;
+import org.matsim.pt.router.TransitRouter;
+import org.matsim.pt.router.TransitRouterConfig;
+import org.matsim.pt.router.TransitRouterImpl;
+import org.matsim.pt.router.TransitRouterNetwork;
+import org.matsim.pt.router.TransitRouterNetworkTravelTimeAndDisutility;
+import org.matsim.pt.transitSchedule.api.TransitSchedule;
+
+import playground.kai.usecases.randomizedptrouter.RandomizedTransitRouterNetworkTravelTimeAndDisutility;
+import playground.mmoyo.utils.DataLoader;
+
+class PopulationRndPtRouter{ 
+	
+	
+		public static void main(String[] args) {
+			String configFile; 
+			if (args.length==1){
+				configFile = args[0];
+			}else{
+				configFile = "";
+			}
+			
+			//load data
+			DataLoader dataLoader = new DataLoader();
+			Scenario scn =dataLoader.loadScenario(configFile);
+			final TransitRouterConfig trConfig = new TransitRouterConfig( scn.getConfig() ) ; 
+			final TransitSchedule schedule = scn.getTransitSchedule();
+			final TransitRouterNetwork routerNetwork = TransitRouterNetwork.createFromSchedule(schedule, trConfig.beelineWalkConnectionDistance);
+			Population population = scn.getPopulation();
+			
+			//create randomizedPtRouter and PlansCalcTransitRoute
+			TransitRouterNetworkTravelTimeAndDisutility ttCalculator = new RandomizedTransitRouterNetworkTravelTimeAndDisutility(trConfig);
+			TransitRouter randomizedPtRouter = new TransitRouterImpl(trConfig, routerNetwork, ttCalculator, ttCalculator);
+			final PlansCalcRouteConfigGroup config = scn.getConfig().plansCalcRoute();
+			final Network network = scn.getNetwork();
+			FreespeedTravelTimeAndDisutility freespeedTravelTimeCost = new FreespeedTravelTimeAndDisutility(scn.getConfig().planCalcScore());
+			final TravelDisutility costCalculator = freespeedTravelTimeCost;
+			final TravelTime timeCalculator = freespeedTravelTimeCost;
+			final LeastCostPathCalculatorFactory factory = new DijkstraFactory();
+			final ModeRouteFactory routeFactory =((PopulationFactoryImpl)population.getFactory()).getModeRouteFactory();
+			final TransitConfigGroup transitConfigGroup = scn.getConfig().transit(); 
+			PlansCalcTransitRoute plansCalcTransitRoute = new PlansCalcTransitRoute(config, network, (TravelDisutility)costCalculator, timeCalculator, factory, routeFactory, transitConfigGroup, randomizedPtRouter, schedule);
+			
+			//route population
+			plansCalcTransitRoute.run(population);
+		
+			//write agents individual parameters in output directory  ??
+			
+			//write routed population
+			String outDir = scn.getConfig().controler().getOutputDirectory();
+			String routedPlansFile = outDir + "routedWithRandomizedPtRouter.xml.gz";
+			System.out.println("writing output plan file..." + routedPlansFile);
+			PopulationWriter popwriter = new PopulationWriter(population, network) ;
+			popwriter.write(routedPlansFile) ;
+		}
+	}
---
+++ playgrounds/mmoyo/src/main/java/playground/mmoyo/randomizerPtRouter/RndPtRouterLauncher.java
@@ -0,0 +1,75 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.mmoyo.randomizerPtRouter;
+
+import org.matsim.core.controler.Controler;
+import org.matsim.core.scenario.ScenarioImpl;
+import org.matsim.pt.router.TransitRouter;
+import org.matsim.pt.router.TransitRouterConfig;
+import org.matsim.pt.router.TransitRouterFactory;
+import org.matsim.pt.router.TransitRouterImpl;
+import org.matsim.pt.router.TransitRouterNetwork;
+import org.matsim.pt.transitSchedule.api.TransitSchedule;
+
+import playground.kai.usecases.randomizedptrouter.RandomizedTransitRouterNetworkTravelTimeAndDisutility;
+import playground.kai.usecases.randomizedptrouter.RandomizedTransitRouterNetworkTravelTimeAndDisutility.DataCollection;
+import playground.mmoyo.utils.DataLoader;
+
+public class RndPtRouterLauncher {
+
+	private static TransitRouterFactory createRandomizedTransitRouterFactory (final TransitSchedule schedule, final TransitRouterConfig trConfig, final TransitRouterNetwork routerNetwork){
+		return 
+		new TransitRouterFactory() {
+			@Override
+			public TransitRouter createTransitRouter() {
+				RandomizedTransitRouterNetworkTravelTimeAndDisutility ttCalculator = new RandomizedTransitRouterNetworkTravelTimeAndDisutility(trConfig);
+				ttCalculator.setDataCollection(DataCollection.randomizedParameters, true) ;
+				ttCalculator.setDataCollection(DataCollection.additionInformation, false) ;
+				return new TransitRouterImpl(trConfig, routerNetwork, ttCalculator, ttCalculator);
+			}
+		};
+	}
+		 
+	public static void main(final String[] args) {
+		String configFile ;
+		if(args.length==0){
+			configFile = "";
+		}else{
+			configFile = args[0];
+		}
+
+		//load data
+		ScenarioImpl scn = new DataLoader().loadScenario(configFile);
+		final TransitRouterConfig trConfig = new TransitRouterConfig( scn.getConfig() ) ; 
+		final TransitRouterNetwork routerNetwork = TransitRouterNetwork.createFromSchedule(scn.getTransitSchedule(), trConfig.beelineWalkConnectionDistance);
+		
+		//create the factory for rndizedRouter
+		TransitRouterFactory randomizedTransitRouterFactory = createRandomizedTransitRouterFactory (scn.getTransitSchedule(), trConfig, routerNetwork);
+		
+		//set the controler
+		Controler controler = new Controler(scn);
+		controler.setTransitRouterFactory(randomizedTransitRouterFactory);
+		//controler.addControlerListener(new CadytsPtControlerListener(controler));  //<-set cadyts as controler listener
+		controler.setOverwriteFiles(true);
+		
+		controler.run();
+	}
+
+}
