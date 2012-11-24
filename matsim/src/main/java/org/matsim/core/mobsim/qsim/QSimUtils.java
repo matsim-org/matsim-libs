6e024e77b24e675ad33399e933bdb94b991ddbb9
vsp_nagel <2008@kainagel.org>
2012-11-24 19:21:16 +0000

    adding QSimUtils to collect static functions
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@21966 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/main/java/org/matsim/core/mobsim/qsim/QSimUtils.java
@@ -0,0 +1,49 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+/**
+ * 
+ */
+package org.matsim.core.mobsim.qsim;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.basic.v01.IdImpl;
+import org.matsim.core.config.groups.OTFVisConfigGroup;
+import org.matsim.core.mobsim.qsim.interfaces.MobsimVehicle;
+import org.matsim.core.mobsim.qsim.qnetsimengine.QVehicle;
+import org.matsim.vehicles.VehicleImpl;
+import org.matsim.vehicles.VehicleType;
+import org.matsim.vehicles.VehicleTypeImpl;
+
+/**
+ * @author nagel
+ *
+ */
+public class QSimUtils {
+	 // should only contain static methods; should thus not be instantiated
+	private QSimUtils() {}
+
+	public static MobsimVehicle createDefaultVehicle(IdImpl idImpl) {
+		VehicleType basicVehicleType = new VehicleTypeImpl(new IdImpl("basicVehicleType")) ; 
+		MobsimVehicle veh = new QVehicle(new VehicleImpl(idImpl, basicVehicleType));
+		return veh ;
+	} ;
+	
+}
