8909502aa48b43607a19bc1ddb24121c3447d437
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2012-11-27 12:19:53 +0000

    extracts duration of non-pnr parking from events
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@21988 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/thibautd/src/main/java/playground/thibautd/parknride/analysis/ExtractNonPnrParkingDurations.java
@@ -0,0 +1,119 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * ExtractNonPnrParkingDurations.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.thibautd.parknride.analysis;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.core.api.experimental.events.ActivityStartEvent;
+import org.matsim.core.api.experimental.events.AgentArrivalEvent;
+import org.matsim.core.api.experimental.events.AgentDepartureEvent;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.api.experimental.events.handler.ActivityStartEventHandler;
+import org.matsim.core.api.experimental.events.handler.AgentArrivalEventHandler;
+import org.matsim.core.api.experimental.events.handler.AgentDepartureEventHandler;
+import org.matsim.core.events.EventsReaderXMLv1;
+import org.matsim.core.events.EventsUtils;
+import org.matsim.core.utils.io.IOUtils;
+import org.matsim.core.utils.io.UncheckedIOException;
+
+import playground.thibautd.parknride.ParkAndRideConstants;
+
+/**
+ * @author thibautd
+ */
+public class ExtractNonPnrParkingDurations {
+	public static void main(final String[] args) {
+		final String eventsFile = args[ 0 ];
+		final String outFile = args[ 1 ];
+
+		Handler handler = new Handler( outFile );
+		EventsManager events = EventsUtils.createEventsManager();
+		events.addHandler( handler );
+		new EventsReaderXMLv1( events ).parse( eventsFile );
+		handler.close();
+	}
+
+	private static class Handler implements AgentArrivalEventHandler, AgentDepartureEventHandler, ActivityStartEventHandler {
+		private final BufferedWriter writer;
+		private final List<Id> justParkedAgents = new ArrayList<Id>();
+		private final Map<Id, Double> payedParkingStarts = new HashMap<Id, Double>();
+
+		public Handler(final String file) {
+			this.writer = IOUtils.getBufferedWriter( file );
+
+			try {
+				writer.write( "agentId\tduration" );
+			} catch (IOException e) {
+				throw new UncheckedIOException( e );
+			}
+		}
+
+		public void close() {
+			try {
+				writer.close();
+			} catch (IOException e) {
+				throw new UncheckedIOException( e );
+			}
+		}
+
+		@Override
+		public void handleEvent(final AgentArrivalEvent event) {
+			if (event.getLegMode().equals( TransportMode.car )) {
+				justParkedAgents.add( event.getPersonId() );
+			}
+		}
+
+		@Override
+		public void handleEvent(final ActivityStartEvent event) {
+			boolean hasParked = justParkedAgents.remove( event.getPersonId() );
+			if (event.getActType().equals( ParkAndRideConstants.PARKING_ACT )) return;
+			if (hasParked) payedParkingStarts.put( event.getPersonId() , event.getTime() );
+		}
+
+		@Override
+		public void handleEvent(final AgentDepartureEvent event) {
+			if ( !event.getLegMode().equals( TransportMode.car ) ) return;
+
+			Double startOfParking = payedParkingStarts.remove( event.getPersonId() );
+			if (startOfParking == null) return;
+
+			try {
+				writer.newLine();
+				writer.write( event.getPersonId()+"\t"+(event.getTime() - startOfParking) );
+			} catch (IOException e) {
+				throw new UncheckedIOException( e );
+			}
+		}
+
+		@Override
+		public void reset(final int iteration) {
+			// TODO Auto-generated method stub
+		}
+	}
+}
+
