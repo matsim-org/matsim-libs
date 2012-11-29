93009f09dab91466b77dabbc67fa471414724ca7
grether <dominik.grether@gmail.com>
2012-11-29 14:53:29 +0000

    cleanup
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@22035 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/fuerbas/src/main/java/air/analysis/FlightSimStuckedAnalysis.java
+++ playgrounds/fuerbas/src/main/java/air/analysis/FlightSimStuckedAnalysis.java
@@ -24,11 +24,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.SortedMap;
-import java.util.TreeMap;
 
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Activity;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Plan;
+import org.matsim.api.core.v01.population.Population;
 import org.matsim.core.api.experimental.events.AgentDepartureEvent;
 import org.matsim.core.api.experimental.events.AgentStuckEvent;
 import org.matsim.core.api.experimental.events.BoardingDeniedEvent;
@@ -36,10 +39,12 @@ import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.api.experimental.events.handler.AgentDepartureEventHandler;
 import org.matsim.core.api.experimental.events.handler.AgentStuckEventHandler;
 import org.matsim.core.api.experimental.events.handler.BoardingDeniedEventHandler;
+import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.events.EventsUtils;
 import org.matsim.core.events.MatsimEventsReader;
-
-import air.demand.FlightODRelation;
+import org.matsim.core.population.MatsimPopulationReader;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.collections.Tuple;
 
 
 
@@ -59,15 +64,20 @@ public class FlightSimStuckedAnalysis implements AgentDepartureEventHandler, Age
 	
 	private Map<Id, AgentDepartureEvent> agent2DepartureEventMap;
 	private Map<Id, PersonEvents> personStats;
-	private SortedMap<String, SortedMap<String, FlightODRelation>> fromAirport2FlightOdRelMap;
-	private int totalStuck;
-	private int totalDirectFlights;
-	private double totalBoardingDenied;
+	private Population population;
 	
-	public FlightSimStuckedAnalysis(){
+	public FlightSimStuckedAnalysis(Population population){
+		this.population = population;
 		this.reset(0);
 	}
 
+	@Override
+	public void reset(int iteration) {
+		this.personStats = new HashMap<Id, PersonEvents>();
+		this.agent2DepartureEventMap = new HashMap<Id, AgentDepartureEvent>();
+	}
+
+	
 	@Override
 	public void handleEvent(BoardingDeniedEvent e) {
 		if (! this.personStats.containsKey(e.getPersonId())){
@@ -80,15 +90,10 @@ public class FlightSimStuckedAnalysis implements AgentDepartureEventHandler, Age
 	@Override
 	public void handleEvent(AgentStuckEvent event) {
 		if (event.getLegMode().compareToIgnoreCase("pt") == 0 && !event.getPersonId().toString().startsWith("pt_")){
-			this.totalStuck++;
 			if (! this.personStats.containsKey(event.getPersonId())){
 				this.personStats.put(event.getPersonId(), new PersonEvents());
 			}
 			this.personStats.get(event.getPersonId()).stuckEvent = event;
-			
-			AgentDepartureEvent departureEvent = this.agent2DepartureEventMap.get(event.getPersonId());
-			String fromAirport = departureEvent.getLinkId().toString();
-			String toAirport = "stuck";
 		}
 	}
 
@@ -108,6 +113,7 @@ public class FlightSimStuckedAnalysis implements AgentDepartureEventHandler, Age
 			if (! entry.getValue().boardingDeniedEvents.isEmpty()){
 				if (entry.getValue().stuckEvent == null){
 					boardingDeniedNoStuck.put(entry.getKey(), entry.getValue());
+					this.printToLog(entry);
 				}
 				else {
 					boardingDeniedAndStuck.put(entry.getKey(), entry.getValue());
@@ -130,6 +136,8 @@ public class FlightSimStuckedAnalysis implements AgentDepartureEventHandler, Age
 	
 	private void evalutateStuckStats(Map<Id, PersonEvents> map){
 		Map<Id, Integer> airportStuckCountMap = new HashMap<Id, Integer>();
+		Map<Tuple<Id, Id>, Integer> odStuckCountMap = new HashMap<Tuple<Id, Id>, Integer>();
+		
 		for (Entry<Id, PersonEvents> entry : map.entrySet()){
 			PersonEvents pe = entry.getValue();
 			Id airportCode = pe.stuckEvent.getLinkId();
@@ -139,9 +147,24 @@ public class FlightSimStuckedAnalysis implements AgentDepartureEventHandler, Age
 			}
 			count++;
 			airportStuckCountMap.put(airportCode, count);
+			
+			Person person = this.population.getPersons().get(entry.getKey());
+			Plan plan = person.getPlans().get(0);
+			Id fromLinkId = ((Activity)plan.getPlanElements().get(0)).getLinkId();
+			Id toLinkId = ((Activity)plan.getPlanElements().get(2)).getLinkId();
+			Tuple<Id, Id> t = new Tuple<Id, Id>(fromLinkId, toLinkId);
+			Integer count2 = odStuckCountMap.get(t);
+			if (count2 == null) {
+				count2 = 0;
+			}
+			count2++;
+			odStuckCountMap.put(t, count2);
 		}
 		log.info("Persons stucked at airports: ");
 		this.printMapToLog(airportStuckCountMap);
+
+		log.info("Persons stucked by od pairs: ");
+		this.printMapToLog(odStuckCountMap);
 		
 	}
 	
@@ -161,20 +184,12 @@ public class FlightSimStuckedAnalysis implements AgentDepartureEventHandler, Age
 		log.info("  " + entry.getValue().stuckEvent);
 	}
 	
-	@Override
-	public void reset(int iteration) {
-		this.personStats = new HashMap<Id, PersonEvents>();
-		this.agent2DepartureEventMap = new HashMap<Id, AgentDepartureEvent>();
-		this.fromAirport2FlightOdRelMap = new TreeMap<String, SortedMap<String, FlightODRelation>>();
-		this.totalStuck = 0;
-		this.totalDirectFlights = 0;
-		this.totalBoardingDenied = 0;
-	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		String populationFile = "/home/dgrether/data/work/repos/shared-svn/studies/countries/de/flight/demand/destatis/2011_september/population_september_2011_tabelle_2.2.2.xml.gz";
 		String[] runNumbers ={
 				"1836", 
 //				"1837",
@@ -184,13 +199,15 @@ public class FlightSimStuckedAnalysis implements AgentDepartureEventHandler, Age
 //				"1841"				
 		};
 		String iteration = "600";
-		
+		Scenario sc = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+		MatsimPopulationReader popReader = new MatsimPopulationReader(sc);
+		popReader.readFile(populationFile);
 		for (String runNumber : runNumbers) {
 			String events = "/home/dgrether/data/work/repos/runs-svn/run"+runNumber+"/ITERS/it."+iteration+"/"+runNumber+"."+iteration+".events.xml.gz";
 			String outputFile = "/home/dgrether/data/work/repos/runs-svn/run"+runNumber+"/ITERS/it."+iteration+"/"+runNumber+"."+iteration+".simulated_direct_flights.csv";
 
 			EventsManager eventsManager = EventsUtils.createEventsManager();
-			FlightSimStuckedAnalysis handler = new FlightSimStuckedAnalysis();
+			FlightSimStuckedAnalysis handler = new FlightSimStuckedAnalysis(sc.getPopulation());
 			eventsManager.addHandler(handler);
 			MatsimEventsReader reader = new MatsimEventsReader(eventsManager);
 			reader.readFile(events);
