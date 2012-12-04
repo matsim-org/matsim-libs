9b655c1ced1b63681a9639930f4ac13cedc389a1
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2012-12-04 16:24:46 +0000

    add a static "copy" method
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@22123 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/java/playground/thibautd/socnetsim/replanning/GroupPlans.java
+++ playgrounds/thibautd/src/main/java/playground/thibautd/socnetsim/replanning/GroupPlans.java
@@ -19,16 +19,23 @@
  * *********************************************************************** */
 package playground.thibautd.socnetsim.replanning;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.matsim.api.core.v01.population.Plan;
+import org.matsim.core.population.PlanImpl;
 
 import playground.thibautd.socnetsim.population.JointPlan;
+import playground.thibautd.socnetsim.population.JointPlanFactory;
 
 /**
  * Stores plans for a group.
  * For plans pertaining to a joint plan, the joint plan is stored;
  * for individual plans, the individual plan is stored.
+ * <br>
+ * The collections returned are the interal references.
+ * GroupStrategyModules may modify them if needed.
  * @author thibautd
  */
 public class GroupPlans {
@@ -67,6 +74,23 @@ public class GroupPlans {
 	@Override
 	public String toString() {
 		return "{GroupPlans: jointPlans="+jointPlans+"; individualPlans="+individualPlans+"}";
+	}
+
+	public static GroupPlans copyPlans(final GroupPlans plans) {
+		List<JointPlan> jps = new ArrayList<JointPlan>();
+		List<Plan> ps = new ArrayList<Plan>();
+
+		for (JointPlan jp : plans.getJointPlans()) {
+			jps.add( JointPlanFactory.copyJointPlan( jp ) );
+		}
+
+		for (Plan p : plans.getIndividualPlans()) {
+			PlanImpl newPlan = new PlanImpl( p.getPerson() );
+			newPlan.copyPlan( p );
+			ps.add( newPlan );
+		}
+
+		return new GroupPlans( jps , ps );
 	}
 }
 
