552fdfe7a000f8f915f068047290628121e05b96
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2012-12-04 16:25:12 +0000

    GroupPlanStrategy now really implemented
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@22124 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/java/playground/thibautd/socnetsim/replanning/GroupPlanStrategy.java
+++ playgrounds/thibautd/src/main/java/playground/thibautd/socnetsim/replanning/GroupPlanStrategy.java
@@ -19,9 +19,60 @@
  * *********************************************************************** */
 package playground.thibautd.socnetsim.replanning;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.matsim.api.core.v01.population.Plan;
+import org.matsim.core.population.PersonImpl;
+
+import playground.thibautd.socnetsim.population.JointPlan;
+
 /**
+ * Generalizes the PlanStrategy concept to joint replanning.
  * @author thibautd
  */
 public class GroupPlanStrategy {
+	private final GroupLevelPlanSelector selector;
+	private final List<GroupStrategyModule> strategyModules = new ArrayList<GroupStrategyModule>();
+
+	public GroupPlanStrategy(final GroupLevelPlanSelector selector) {
+		this.selector = selector;
+	}
+
+	public void addStrategyModule(final GroupStrategyModule module) {
+		strategyModules.add( module );
+	}
+
+	public void run(final Collection<ReplanningGroup> groups) {
+		List<GroupPlans> plansToHandle = new ArrayList<GroupPlans>();
+
+		for (ReplanningGroup group : groups) {
+			GroupPlans plans = selector.selectPlans( group );
+
+			if (plansToHandle.size() > 0) {
+				plans = GroupPlans.copyPlans( plans );
+				plansToHandle.add( plans );
+			}
+
+			select( plans );
+		}
+
+		for (GroupStrategyModule module : strategyModules) {
+			module.handlePlans( plansToHandle );
+		}
+	}
+
+	private static void select(final GroupPlans plans) {
+		for (JointPlan jp : plans.getJointPlans()) {
+			for (Plan p : jp.getIndividualPlans().values()) {
+				((PersonImpl) p.getPerson()).setSelectedPlan( p );
+			}
+		}
+
+		for (Plan p : plans.getIndividualPlans()) {
+			((PersonImpl) p.getPerson()).setSelectedPlan( p );
+		}
+	}
 }
 
