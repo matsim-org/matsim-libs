a80981fa75ac23802b8186d66eb0fa2cf53249a8
laemmel <gregor.laemmel@gmail.com>
2012-12-05 14:18:37 +0000

    some of those geometry algorithms provided by jts/vividsolutions/geotools/whatever are really not robust. in this case offseting the geometries so that minX,minY overall geometries == 0 seems to help a little bit.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@22159 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/gregor/src/main/java/playground/gregor/sim2d_v3/helper/experimentalgraphgenerator/GraphGenerator.java
+++ playgrounds/gregor/src/main/java/playground/gregor/sim2d_v3/helper/experimentalgraphgenerator/GraphGenerator.java
@@ -3,7 +3,9 @@ package playground.gregor.sim2d_v3.helper.experimentalgraphgenerator;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.geotools.factory.FactoryRegistryException;
 import org.geotools.feature.Feature;
@@ -15,15 +17,14 @@ import org.matsim.api.core.v01.network.NetworkFactory;
 import org.matsim.api.core.v01.network.Node;
 import org.matsim.core.api.experimental.network.NetworkWriter;
 import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.network.algorithms.NetworkCleaner;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.geometry.geotools.MGC;
 import org.matsim.core.utils.gis.ShapeFileReader;
-import org.matsim.core.config.ConfigUtils;
 import org.matsim.utils.gis.matsim2esri.network.Links2ESRIShape;
 
-
-
+import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Envelope;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryCollection;
@@ -31,6 +32,8 @@ import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.MultiLineString;
 import com.vividsolutions.jts.geom.MultiPoint;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jts.triangulate.VoronoiDiagramBuilder;
 
 public class GraphGenerator {
@@ -40,44 +43,61 @@ public class GraphGenerator {
 
 	private final Envelope envelope;
 	private final Collection<Geometry> geometries;
+	private final double offsetX;
+	private final double offsetY;
 
 
 	public GraphGenerator(Scenario sc, Collection<Geometry> geometries, Envelope envelope) {
 		this.sc = sc;
 		this.geometries = geometries;
-		this.envelope = envelope;
+		this.offsetX = -envelope.getMinX();
+		this.offsetY = -envelope.getMinY();
+		
+		this.envelope = new Envelope(0,envelope.getMaxX()+this.offsetX,0,envelope.getMaxY()+this.offsetY);
 	}
 
 
 	public void run() {
 
+		offsetGeometries();
+		
+		
 		List<LineString> ls =  extractLineStrings();
 		//		new LineStringSnapper().run(ls, this.envelope);
 
+
 		MultiPoint mp = new DenseMultiPointFromLineString().getDenseMultiPointFromLineString(ls);
+		
+		
+//		PrecisionModel p = new PrecisionModel(PrecisionModel.FLOATING_SINGLE);
+//		mp = (MultiPoint) com.vividsolutions.jts.precision.SimpleGeometryPrecisionReducer.reduce(mp, p);
 		Geometry boundary = new MultiPolygonFromLineStrings().getMultiPolygon(ls, this.envelope);
 
 		VoronoiDiagramBuilder vdb = new VoronoiDiagramBuilder();
-		//		vdb.setTolerance(0.2);
+		vdb.setTolerance(0.2);
+
 		vdb.setSites(mp);
 		GeometryCollection dia = (GeometryCollection) vdb.getDiagram(geofac);
 		boundary = boundary.buffer(0.01);
 
 		Skeleton skeleton = new SkeletonExtractor().extractSkeleton(dia, boundary);
-		//		skeleton.dumpLinks("/Users/laemmel/tmp/vis/skeleton.shp");
-		//		skeleton.dumpIntersectingNodes("/Users/laemmel/tmp/vis/skeletonNodes.shp");
+		skeleton.dumpLinks("/Users/laemmel/tmp/vis/skeleton.shp");
+		skeleton.dumpIntersectingNodes("/Users/laemmel/tmp/vis/skeletonNodes.shp");
 
 		new SkeletonSimplifier().simplifySkeleton(skeleton,boundary);
-		//		skeleton.dumpLinks("/Users/laemmel/tmp/vis/skeletonSimpl.shp");
+		skeleton.dumpLinks("/Users/laemmel/tmp/vis/skeletonSimpl.shp");
 
 		new SkeletonLinksContraction().contractShortSkeletonLinks(skeleton, boundary);
-		//		skeleton.dumpLinks("/Users/laemmel/tmp/vis/skeletonContr.shp");
+		skeleton.dumpLinks("/Users/laemmel/tmp/vis/skeletonContr.shp");
 
 		new Puncher().punchSkeleton(skeleton, this.envelope);
 
 		new StubRemover().run(skeleton);
-		//		skeleton.dumpLinks("/Users/laemmel/tmp/vis/skeletonStubRM.shp");
+		skeleton.dumpLinks("/Users/laemmel/tmp/vis/skeletonStubRM.shp");
 
+		
+		unoffsetSkeleton(skeleton);
+		
 		createNetwork(skeleton);
 
 		new NetworkCleaner().run(this.sc.getNetwork());
@@ -85,14 +105,58 @@ public class GraphGenerator {
 	}
 
 
+	private void unoffsetSkeleton(Skeleton skeleton) {
+		Set<Coordinate> handled = new HashSet<Coordinate>();
+		for (SkeletonLink l : skeleton.getLinks()) {
+			Coordinate from = l.getFromNode().getCoord();
+			if (!handled.contains(from)) {
+				from.x -= this.offsetX;
+				from.y -= this.offsetY;
+				handled.add(from);
+			}
+			Coordinate to = l.getToNode().getCoord();
+			if (!handled.contains(to)) {
+				to.x -= this.offsetX;
+				to.y -= this.offsetY;
+				handled.add(to);
+			}
+		}
+		
+	}
+
+
+	private void offsetGeometries() {
+		Set<Coordinate> handled = new HashSet<Coordinate>();
+		for (Geometry geo : this.geometries) {
+			for (Coordinate c : geo.getCoordinates()) {
+				if (handled.contains(c)){
+					continue;
+				}
+				c.x += this.offsetX;
+				c.y += this.offsetY;
+				handled.add(c);
+			}
+		}
+	}
+
+
 	private void createNetwork(Skeleton skeleton) {
 		NetworkFactory fac = this.sc.getNetwork().getFactory();
-		for (SkeletonNode n : skeleton.getNodes()) {
-			Node mn = fac.createNode(n.getId(), MGC.coordinate2Coord(n.getCoord()));
-			this.sc.getNetwork().addNode(mn);
-		}
+//		for (SkeletonNode n : skeleton.getNodes()) {
+//			Node mn = fac.createNode(n.getId(), MGC.coordinate2Coord(n.getCoord()));
+//			this.sc.getNetwork().addNode(mn);
+//		}
 		int linkNums = 0;
 		for (SkeletonLink l : skeleton.getLinks()) {
+			if (this.sc.getNetwork().getNodes().get(l.getFromNode().getId()) == null) {
+				Node mn = fac.createNode(l.getFromNode().getId(), MGC.coordinate2Coord(l.getFromNode().getCoord()));
+				this.sc.getNetwork().addNode(mn);
+			}
+			if (this.sc.getNetwork().getNodes().get(l.getToNode().getId()) == null) {
+				Node mn = fac.createNode(l.getToNode().getId(), MGC.coordinate2Coord(l.getToNode().getCoord()));
+				this.sc.getNetwork().addNode(mn);
+			}
+			
 			Node fromNode = this.sc.getNetwork().getNodes().get(l.getFromNode().getId());
 			Node toNode = this.sc.getNetwork().getNodes().get(l.getToNode().getId());
 
@@ -121,7 +185,17 @@ public class GraphGenerator {
 					Geometry ggeo = ml.getGeometryN(i);
 					ret.add((LineString) ggeo);
 				}
-			} else {
+			} else if (geo instanceof MultiPolygon){
+				MultiPolygon mp = (MultiPolygon)geo;
+				for (int i = 0; i < mp.getNumGeometries(); i++) {
+					Geometry gg = mp.getGeometryN(i);
+					Polygon p = (Polygon)gg;
+					ret.add(p.getExteriorRing());
+				}
+			}else if (geo instanceof Polygon){
+				Polygon p = (Polygon)geo;
+				ret.add(p.getExteriorRing());
+			}else{
 				throw new RuntimeException("Geometry type: " +geo.getGeometryType() +" not (yet) supported.");
 			}
 		}
@@ -132,7 +206,8 @@ public class GraphGenerator {
 
 	public static void main(String [] args) throws FactoryRegistryException, SchemaException, IllegalAttributeException {
 		//		String floorplan = "/Users/laemmel/tmp/voronoi/test.shp";
-		String floorplan = "/Users/laemmel/devel/sim2dDemoII/raw_input/floorplan.shp";
+				String floorplan = "/Users/laemmel/devel/burgdorf2d/raw_input/raw_env.shp";
+//		String floorplan = "/Users/laemmel/devel/convexdecomp/03.shp";
 		Config c = ConfigUtils.createConfig();
 		Scenario sc = ScenarioUtils.createScenario(c);
 		ShapeFileReader reader = new ShapeFileReader();
@@ -143,10 +218,10 @@ public class GraphGenerator {
 		}
 
 		new GraphGenerator(sc,geos,reader.getBounds()).run();
-		new NetworkWriter(sc.getNetwork()).write("/Users/laemmel/devel/sim2dDemoII/input/network.xml");
+		new NetworkWriter(sc.getNetwork()).write("/Users/laemmel/devel/burgdorf2d/input/network2d_0.xml");
 		//		String [] argsII = {"/Users/laemmel/devel/sim2dDemoII/input/network.xml","/Users/laemmel/devel/sim2dDemoII/raw_input/networkL.shp","/Users/laemmel/devel/sim2dDemoII/raw_input/networkP.shp","EPSG:3395"};
-		new NetworkWriter(sc.getNetwork()).write("/Users/laemmel/tmp/voronoi/network.xml");
-		String [] argsII = {"/Users/laemmel/tmp/voronoi/network.xml","/Users/laemmel/devel/sim2dDemoII/raw_input/networkL.shp","/Users/laemmel/devel/sim2dDemoII/raw_input/networkP.shp","EPSG:3395"};
+		//		new NetworkWriter(sc.getNetwork()).write("/Users/laemmel/tmp/vis/network.xml");
+		String [] argsII = {"/Users/laemmel/devel/burgdorf2d/input/network2d_0.xml","/Users/laemmel/tmp/vis/networkL.shp","/Users/laemmel/tmp/vis/networkP.shp","EPSG:3395"};
 		Links2ESRIShape.main(argsII);
 	}
 
