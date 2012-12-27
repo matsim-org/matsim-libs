15d1bc91cc8a2b43c9f87d139c6913a15c08d67e
kaddoura <kaddoura@users.sf.net>
2012-12-27 16:14:05 +0000

    a simple way of adding park-and-ride functionality to the existing framework...
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@22390 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/vsp/src/main/java/playground/vsp/parkAndRide/PRFileReader.java
@@ -0,0 +1,93 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * PRfileReader.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+/**
+ * 
+ */
+package playground.vsp.parkAndRide;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Id;
+import org.matsim.core.basic.v01.IdImpl;
+
+/**
+ * @author ikaddoura
+ *
+ */
+public class PRFileReader {
+	private static final Logger log = Logger.getLogger(PRFileReader.class);
+	private Map<Id, PRFacility> id2prFacility = new HashMap<Id, PRFacility>();
+	private String prFacilityFile;
+
+	public PRFileReader(String prFacilityFile) {
+		this.prFacilityFile = prFacilityFile;
+	}
+
+	public Map<Id, PRFacility> getId2prFacility() {
+		
+		log.info("Reading Facilities from file " + this.prFacilityFile + "...");
+		
+		 BufferedReader br = null;
+	        try {
+	            br = new BufferedReader(new FileReader(new File(this.prFacilityFile)));
+	            String line = null;
+	            int lineCounter = 0;
+	            while((line = br.readLine()) != null) {
+	                if (lineCounter > 0) {
+	                	String[] parts = line.split(";"); 
+	                	PRFacility prFacility = new PRFacility();
+	                	prFacility.setId(new IdImpl(parts[0]));
+	                	prFacility.setPrLink1in(new IdImpl(parts[1]));
+	                	prFacility.setPrLink1out(new IdImpl(parts[2]));
+	                	prFacility.setPrLink2in(new IdImpl(parts[3]));
+	                	prFacility.setPrLink2out(new IdImpl(parts[4]));
+	                	prFacility.setPrLink3in(new IdImpl(parts[5]));
+	                	prFacility.setPrLink3out(new IdImpl(parts[6]));
+	                	prFacility.setStopFacilityName(parts[7]);
+	                	prFacility.setCapacity(Integer.valueOf(parts[8]));
+	                	this.id2prFacility.put(prFacility.getId(), prFacility);
+	                }
+	                lineCounter++;
+	            }
+	        } catch(FileNotFoundException e) {
+	            e.printStackTrace();
+	        } catch(IOException e) {
+	            e.printStackTrace();
+	        } finally {
+	            if(br != null) {
+	                try {
+	                    br.close();
+	                } catch(IOException e) {
+	                    e.printStackTrace();
+	                }
+	            }
+	        }
+	        log.info("Reading Facilities from file " + this.prFacilityFile + "... Done.");
+	        return this.id2prFacility;
+	}
+}
---
+++ playgrounds/vsp/src/main/java/playground/vsp/parkAndRide/example/PRRunner.java
@@ -0,0 +1,106 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * TestControler.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2011 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+/**
+ * 
+ */
+package playground.vsp.parkAndRide.example;
+
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ActivityParams;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.mobsim.framework.Mobsim;
+import org.matsim.core.mobsim.framework.MobsimFactory;
+import org.matsim.core.mobsim.qsim.QSim;
+import org.matsim.core.mobsim.qsim.QSimFactory;
+
+import playground.vsp.parkAndRide.PRAdaptiveCapacityControl;
+import playground.vsp.parkAndRide.PRConfigGroup;
+import playground.vsp.parkAndRide.PRConstants;
+import playground.vsp.parkAndRide.PRFacility;
+import playground.vsp.parkAndRide.PRFileReader;
+import playground.vsp.parkAndRide.scoring.PRScoringFunctionFactory;
+
+/**
+ * @author ikaddoura
+ *
+ */
+public class PRRunner {
+	
+	static String configFile;
+		
+	public static void main(String[] args) throws IOException {
+		
+		configFile = "/path-to/config.xml";
+		
+		PRRunner main = new PRRunner();
+		main.run();
+	}
+	
+	private void run() {
+		
+		Config config = new Config();
+		config.addModule(PRConfigGroup.GROUP_NAME, new PRConfigGroup());
+		ConfigUtils.loadConfig(config, configFile);
+				
+		Controler controler = new Controler(config);
+		controler.setOverwriteFiles(true);
+		
+		PRConfigGroup prSettings = (PRConfigGroup) controler.getConfig().getModule(PRConfigGroup.GROUP_NAME);
+
+		ActivityParams prActivityParams = new ActivityParams(PRConstants.PARKANDRIDE_ACTIVITY_TYPE);
+		prActivityParams.setTypicalDuration(prSettings.getTypicalDuration());
+		controler.getConfig().planCalcScore().addActivityParams(prActivityParams);
+		
+		controler.setScoringFunctionFactory(new PRScoringFunctionFactory(controler.getConfig().planCalcScore(), controler.getNetwork(), prSettings.getIntermodalTransferPenalty()));
+		
+		PRFileReader prReader = new PRFileReader(prSettings.getInputFile());
+		Map<Id, PRFacility> id2prFacility = prReader.getId2prFacility();
+		final PRAdaptiveCapacityControl adaptiveControl = new PRAdaptiveCapacityControl(id2prFacility);
+
+		PRControlerListener prControlerListener = new PRControlerListener(controler, adaptiveControl);
+		controler.addControlerListener(prControlerListener);
+		
+		final MobsimFactory mf = new QSimFactory();
+		controler.setMobsimFactory(new MobsimFactory() {
+			private QSim mobsim;
+
+			@Override
+			public Mobsim createMobsim(Scenario sc, EventsManager eventsManager) {
+				mobsim = (QSim) mf.createMobsim(sc, eventsManager);
+				mobsim.addMobsimEngine(adaptiveControl);
+				return mobsim;
+			}
+		});
+			
+		controler.run();
+		
+	}
+
+}
+	
