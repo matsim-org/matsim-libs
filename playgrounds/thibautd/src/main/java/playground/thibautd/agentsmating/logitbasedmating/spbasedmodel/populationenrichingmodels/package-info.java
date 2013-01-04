a7888fea8e347cf45b63cc86c115c9c28b778ac2
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2013-01-04 09:11:53 +0000

    removed unused package
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@22431 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/java/playground/thibautd/agentsmating/logitbasedmating/spbasedmodel/populationenrichingmodels/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package contains models meant to add attributes to agents before
- * passing them to the affectation models.
- *
- * @author thibautd
- */
-
-package playground.thibautd.agentsmating.logitbasedmating.spbasedmodel.populationenrichingmodels;
