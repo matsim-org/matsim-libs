6339720b26778f0f00e4e63340e0a042eec8a161
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2013-01-08 13:59:37 +0000

    script to generate test scenario
    generates a population with even-sized cliques for the equil network. In
    each clique, half the agents have a car available, half the agent have
    not.
    
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@22495 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/thibautd/src/main/java/playground/thibautd/scripts/GenerateEquilPlansForJointTrips.java
@@ -0,0 +1,162 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * GenerateEquilPlansForJointTrips.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2013 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.thibautd.scripts;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Population;
+import org.matsim.api.core.v01.population.PopulationWriter;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.core.basic.v01.IdImpl;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.population.ActivityImpl;
+import org.matsim.core.population.PersonImpl;
+import org.matsim.core.population.PlanImpl;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.geometry.CoordImpl;
+import org.matsim.core.utils.misc.Counter;
+
+import playground.thibautd.householdsfromcensus.CliquesWriter;
+
+/**
+ * @author thibautd
+ */
+public class GenerateEquilPlansForJointTrips {
+	private static final Id HOME_LINK_ID = new IdImpl( 1 );
+	private static final Coord HOME_COORD = new CoordImpl( -25000 , 0 );
+	private static final Id WORK_LINK_ID = new IdImpl( 20 );
+	private static final Coord WORK_COORD = new CoordImpl( 10000 , 0 );
+
+	public static void main(final String[] args) {
+		final String outputCliquesFile = args[ 0 ];
+		final String outputPopFile = args[ 1 ];
+		// only even clique sizes will be generated
+		final int maxCliqueSize = Integer.valueOf( args[ 2 ] );
+		final int popSize = Integer.valueOf( args[ 3 ] );
+
+		final Scenario scenario = ScenarioUtils.createScenario( ConfigUtils.createConfig() );
+		final Population population = scenario.getPopulation();
+
+		final Random random = new Random( 19239784 );
+		final SizeIterator sizes = new SizeIterator( maxCliqueSize );
+		final CliquesTracker cliques = new CliquesTracker();
+		final IdIterator ids = new IdIterator( "person-" );
+		final Counter counter = new Counter( "generating clique # ");
+		while ( population.getPersons().size() < popSize ) {
+			counter.incCounter();
+			final int size = sizes.next();
+			Collection<Person> persons = createClique( random , ids , size );
+			for (Person p : persons) population.addPerson( p );
+			cliques.addClique( persons );
+		}
+		counter.printCounter();
+
+		new PopulationWriter( population , scenario.getNetwork() ).write( outputPopFile );
+		new CliquesWriter( cliques.getCliques() ).writeFile( outputCliquesFile );
+	}
+
+	private static Collection<Person> createClique(
+			final Random random,
+			final IdIterator ids,
+			final int size) {
+		if (size % 2 != 0) throw new IllegalArgumentException( ""+size );
+
+		final List<Person> persons = new ArrayList<Person>();
+		for (int i=1; i <= size; i++) {
+			final boolean isDriver = i % 2 == 0;
+			final String mode = isDriver ? TransportMode.car : TransportMode.pt;
+
+			// TODO: make times random and stupid
+			PersonImpl person = new PersonImpl( ids.next() );
+			if (!isDriver) person.setCarAvail( "never" );
+			persons.add( person );
+			PlanImpl plan = person.createAndAddPlan( true );
+			ActivityImpl act = plan.createAndAddActivity( "h" , HOME_LINK_ID );
+			act.setCoord( HOME_COORD );
+			act.setEndTime( random.nextDouble() * 12 * 3600 );
+			plan.createAndAddLeg( mode );
+			act = plan.createAndAddActivity( "w" , WORK_LINK_ID );
+			act.setCoord( WORK_COORD );
+			act.setEndTime( (1 + random.nextDouble()) * 12 * 3600 );
+			plan.createAndAddLeg( mode );
+			act = plan.createAndAddActivity( "h" , HOME_LINK_ID );
+			act.setCoord( HOME_COORD );
+		}
+
+		return persons;
+	}
+
+	private static class SizeIterator {
+		private int current = 0;
+		private final int max;
+
+		public SizeIterator(final int max) {
+			this.max = max;
+		}
+
+		public int next() {
+			current += 2;
+			if (current > max) current = 2;
+			return current;
+		}
+	}
+
+	private static class IdIterator {
+		private int count = 0;
+		private final String prefix;
+
+		public IdIterator( final String prefix ) {
+			this.prefix = prefix;
+		}
+
+		public Id next() {
+			return new IdImpl( prefix+(count++) );
+		}
+	}
+
+	private static class CliquesTracker {
+		private final IdIterator ids = new IdIterator( "clique-" );
+		private final Map<Id, List<Id>> cliques = new HashMap<Id, List<Id>>();
+
+		public void addClique( final Collection<Person> persons ) {
+			final List<Id> memberIds = new ArrayList<Id>();
+
+			for (Person p : persons) {
+				memberIds.add( p.getId() );
+			}
+
+			cliques.put( ids.next() , memberIds );
+		}
+
+		public Map<Id, List<Id>> getCliques() {
+			return cliques;
+		}
+	}
+}
+
