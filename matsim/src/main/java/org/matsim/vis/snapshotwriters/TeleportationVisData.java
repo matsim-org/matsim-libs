4b341ff981c6af95d449b87b1b16d3a93d403947
vsp_nagel <2008@kainagel.org>
2013-01-09 18:33:57 +0000

    trying to fix the problem that teleportation with short distances cannot be visualized ("unstable normal") ... can do this without normal.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@22521 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/vis/snapshotwriters/TeleportationVisData.java
+++ matsim/src/main/java/org/matsim/vis/snapshotwriters/TeleportationVisData.java
@@ -30,36 +30,40 @@ import org.matsim.api.core.v01.Id;
  */
 public class TeleportationVisData implements AgentSnapshotInfo {
 
-	private double stepsize;
-	private double startX;
-	private double startY;
-	private double normalX;
-	private double normalY;
+//	private double stepsize;
+	private final double startX;
+	private final double startY;
+//	private double normalX;
+//	private double normalY;
 	private double currentX;
 	private double currentY;
-	private double starttime;
-	private Id agentId;
+	private final double starttime;
+	private final Id agentId;
 	private int userDefined;
 	private int type;
 	private double colorval;
 	private AgentState state = AgentSnapshotInfo.AgentState.PERSON_OTHER_MODE;
 	private int intX;
 	private int intY;
-	private static int offset = 100;
+	private final double endX;
+	private final double endY;
+	private final double travelTime;
+	private final static int offset = 100;
 
 	public TeleportationVisData(double now, Id personId, Coord fromCoord, Coord toCoord, double travelTime ) {
 		this.starttime = now;
+		this.travelTime = travelTime ;
 		this.agentId = personId;
 		this.startX = fromCoord.getX();
 		this.startY = fromCoord.getY();
-		double endX = toCoord.getX();
-		double endY = toCoord.getY();
-		double dX = endX - startX;
-		double dY = endY - startY;
-		double euclideanLength = Math.sqrt(Math.pow(dX, 2) + Math.pow(dY, 2));
-		this.stepsize = euclideanLength / travelTime;
-		this.normalX = dX / euclideanLength;
-		this.normalY = dY / euclideanLength;
+		this.endX = toCoord.getX();
+		this.endY = toCoord.getY();
+//		double dX = endX - startX;
+//		double dY = endY - startY;
+//		double euclideanLength = Math.sqrt(Math.pow(dX, 2) + Math.pow(dY, 2));
+//		this.stepsize = euclideanLength / travelTime;
+//		this.normalX = dX / euclideanLength;
+//		this.normalY = dY / euclideanLength;
 		this.currentX = startX;
 		this.currentY = startY;
 		String idstr = personId.toString();
@@ -86,9 +90,14 @@ public class TeleportationVisData implements AgentSnapshotInfo {
 	}
 
 	public void calculatePosition(double time) {
-		double step = (time - starttime) * this.stepsize;
-		this.currentX = this.startX + (step * this.normalX) + 0.1*(intX-offset/2);
-		this.currentY = this.startY  + (step * this.normalY) + 0.1*(intY-offset/2);
+//		double step = (time - starttime) * this.stepsize;
+//		this.currentX = this.startX + (step * this.normalX) + 0.1*(intX-offset/2);
+//		this.currentY = this.startY  + (step * this.normalY) + 0.1*(intY-offset/2);
+		
+		double frac = (time - starttime) / travelTime ;
+		this.currentX = (1.-frac) * this.startX + frac * this.endX + 0.1*(intX-offset/2) ;
+		this.currentY = (1.-frac) * this.startY + frac * this.endY + 0.1*(intY-offset/2) ;
+		
 	}
 
 	@Override
