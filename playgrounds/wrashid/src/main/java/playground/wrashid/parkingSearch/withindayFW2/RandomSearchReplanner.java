dab0a48b20240b55bbb0aa47607ee6addeb7b29b
rashid_waraich <rashid_waraich@users.sf.net>
2013-01-28 12:59:53 +0000

    new tryout withinday
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@22777 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/wrashid/src/main/java/playground/wrashid/parkingSearch/withindayFW2/RandomSearchReplanner.java
@@ -0,0 +1,201 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * RandomSearchReplanner.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2011 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.wrashid.parkingSearch.withindayFW2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.population.Leg;
+import org.matsim.api.core.v01.population.Plan;
+import org.matsim.api.core.v01.population.PlanElement;
+import org.matsim.core.api.experimental.facilities.ActivityFacility;
+import org.matsim.core.gbl.MatsimRandom;
+import org.matsim.core.mobsim.qsim.InternalInterface;
+import org.matsim.core.mobsim.qsim.agents.PlanBasedWithinDayAgent;
+import org.matsim.core.population.ActivityImpl;
+import org.matsim.core.population.routes.NetworkRoute;
+import org.matsim.core.scenario.ScenarioImpl;
+import org.matsim.withinday.replanning.replanners.interfaces.WithinDayDuringLegReplanner;
+import org.matsim.withinday.utils.EditRoutes;
+
+/**
+ * 
+ * @author cdobler
+ */
+public class RandomSearchReplanner extends WithinDayDuringLegReplanner {
+
+	private final Random random;
+	private final EditRoutes editRoutes;
+	private final ParkingAgentsTracker parkingAgentsTracker;
+	
+	RandomSearchReplanner(Id id, Scenario scenario, InternalInterface internalInterface, 
+			ParkingAgentsTracker parkingAgentsTracker) {
+		super(id, scenario, internalInterface);
+		this.parkingAgentsTracker = parkingAgentsTracker;
+
+		this.editRoutes = new EditRoutes();
+		this.random = MatsimRandom.getLocalInstance();
+	}
+
+	@Override
+	public boolean doReplanning(PlanBasedWithinDayAgent withinDayAgent) {
+
+		Plan plan = withinDayAgent.getSelectedPlan();
+
+		Leg leg = withinDayAgent.getCurrentLeg();
+
+		ActivityImpl activity = (ActivityImpl) withinDayAgent.getNextPlanElement();
+		Id linkId = withinDayAgent.getCurrentLinkId();
+		Link link = scenario.getNetwork().getLinks().get(linkId);
+
+		int routeIndex = withinDayAgent.getCurrentRouteLinkIdIndex();
+
+		NetworkRoute route = (NetworkRoute) leg.getRoute();
+		Id startLink = route.getStartLinkId();
+		List<Id> links = new ArrayList<Id>(route.getLinkIds()); // create a copy
+																// that can be
+																// modified
+		Id endLink = route.getEndLinkId();
+
+		/*
+		 * If a parking has been selected, set it as destination and adapt the
+		 * current leg, the next activity and the next leg. Otherwise just add
+		 * another link to the current leg.
+		 * 
+		 * RW comment: Der autofahrer fährt bis zum original planned parking, falls er nicht vorh
+		 * -er einen parkplatz findet. 
+		 */
+		Id parkingFacilityId = parkingAgentsTracker.getSelectedParking(withinDayAgent.getId());
+
+		if (parkingFacilityId == null) {
+
+			// verlängere route, nur wenn am original planned parking angekommen.
+			
+			// check whether the car is at the routes
+			// start link
+			if (routeIndex == 0) {
+
+				// if the route ends at the same link
+				if (startLink.equals(endLink)) {
+					Link l = randomNextLink(link);
+					links.add(l.getId());
+				} else {
+					// nothing to do here since more links available in the
+					// route
+				}
+			}
+			// end link
+			else if (routeIndex == links.size() + 1) {
+				links.add(endLink);
+				endLink = randomNextLink(link).getId();
+			}
+			// link in between
+			else {
+				// nothing to do here since more links available in the route
+			}
+
+			// update agent's route
+			route.setLinkIds(startLink, links, endLink);
+		}
+
+		else {
+			// mit next leg ist der walk leg nach der nächsten parking activity gemeint.
+			boolean updateNextLeg = false;
+
+			// if the current linkId is different than the parking activity planned
+	
+			if (linkId != activity.getLinkId()) {
+				/*
+				 * move the parking activity after this leg
+				 */
+				ActivityFacility facility = ((ScenarioImpl) scenario).getActivityFacilities().getFacilities()
+						.get(parkingFacilityId);
+				activity.setCoord(facility.getCoord());
+				activity.setLinkId(linkId);
+				activity.setFacilityId(parkingFacilityId);
+
+				updateNextLeg = true;
+
+				/*
+				 * as the parking has changed, we must also
+				 * change the next parking when departing from the actual activity.
+				 * 
+				 * RW new comment: wir müssen beim letzen parking das nicht
+				 * machen, weil hier geht es ja nur um nicht ausgeführte Teile
+				 * des Plans.
+				 */
+				for (int i = withinDayAgent.getCurrentPlanElementIndex() + 2; i < plan.getPlanElements().size(); i++) {
+					PlanElement planElement = plan.getPlanElements().get(i);
+					if (planElement instanceof ActivityImpl) {
+						ActivityImpl a = (ActivityImpl) planElement;
+						if (a.getType().equals("parking")) {
+							a.setCoord(facility.getCoord());
+							a.setLinkId(linkId);
+							a.setFacilityId(parkingFacilityId);
+
+							// update walk leg to parking activity
+							editRoutes.replanFutureLegRoute(withinDayAgent.getSelectedPlan(), i - 1, routeAlgo);
+
+							// update car leg from parking activity
+							editRoutes.replanFutureLegRoute(withinDayAgent.getSelectedPlan(), i + 1, routeAlgo);
+						}
+					}
+				}
+			}
+
+			// as we have found a parking on our way to the planned parking, we discard the rest
+			// of the route.
+			if (linkId != route.getEndLinkId()) {
+				// set the current link as the route's end link
+				endLink = linkId;
+
+				// update agent's route
+				route.setLinkIds(startLink, links, endLink);
+
+				// update agent's route
+				editRoutes.replanCurrentLegRoute(plan, withinDayAgent.getCurrentPlanElementIndex(),
+						withinDayAgent.getCurrentRouteLinkIdIndex(), routeAlgo, this.time);
+
+				updateNextLeg = true;
+			}
+
+			// adapt next walk leg.
+			if (updateNextLeg) {
+				editRoutes.replanFutureLegRoute(withinDayAgent.getSelectedPlan(),
+						withinDayAgent.getCurrentPlanElementIndex() + 2, routeAlgo);
+			}
+		}
+
+		withinDayAgent.resetCaches();
+		return true;
+	}
+
+	private Link randomNextLink(Link link) {
+		List<Link> links = new ArrayList<Link>(link.getToNode().getOutLinks().values());
+
+		int i = random.nextInt(links.size());
+		return links.get(i);
+	}
+}
---
+++ playgrounds/wrashid/src/main/java/playground/wrashid/parkingSearch/withindayFW2/WithinDayParkingController.java
@@ -0,0 +1,202 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * WithinDayParkingController.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2011 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.wrashid.parkingSearch.withindayFW2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.core.api.experimental.facilities.ActivityFacility;
+import org.matsim.core.controler.events.ReplanningEvent;
+import org.matsim.core.controler.events.StartupEvent;
+import org.matsim.core.controler.listener.ReplanningListener;
+import org.matsim.core.controler.listener.StartupListener;
+import org.matsim.core.mobsim.framework.MobsimFactory;
+import org.matsim.core.mobsim.qsim.multimodalsimengine.router.util.BikeTravelTime;
+import org.matsim.core.mobsim.qsim.multimodalsimengine.router.util.PTTravelTime;
+import org.matsim.core.mobsim.qsim.multimodalsimengine.router.util.RideTravelTime;
+import org.matsim.core.mobsim.qsim.multimodalsimengine.router.util.WalkTravelTime;
+import org.matsim.core.network.NetworkImpl;
+import org.matsim.core.population.PopulationFactoryImpl;
+import org.matsim.core.population.routes.ModeRouteFactory;
+import org.matsim.core.replanning.modules.AbstractMultithreadedModule;
+import org.matsim.core.router.costcalculators.FreespeedTravelTimeAndDisutility;
+import org.matsim.core.router.costcalculators.OnlyTimeDependentTravelCostCalculatorFactory;
+import org.matsim.core.router.costcalculators.TravelDisutilityFactory;
+import org.matsim.core.router.util.AStarLandmarksFactory;
+import org.matsim.core.router.util.LeastCostPathCalculatorFactory;
+import org.matsim.core.router.util.TravelTime;
+import org.matsim.facilities.algorithms.WorldConnectLocations;
+import org.matsim.withinday.controller.WithinDayController;
+import org.matsim.withinday.replanning.modules.ReplanningModule;
+
+import playground.wrashid.lib.obj.IntegerValueHashMap;
+import playground.wrashid.parkingSearch.withinday.LegModeChecker;
+import playground.wrashid.parkingSearch.withindayFW.core.InsertParkingActivities;
+import playground.wrashid.parkingSearch.withindayFW.core.ParkingInfrastructure;
+import playground.wrashid.parkingSearch.withindayFW.core.mobsim.ParkingQSimFactory;
+
+public class WithinDayParkingController extends WithinDayController implements StartupListener, ReplanningListener {
+
+	/*
+	 * How many parallel Threads shall do the Replanning.
+	 */
+	protected int numReplanningThreads = 8;
+
+	protected RandomSearchIdentifier randomSearchIdentifier;
+	protected RandomSearchReplannerFactory randomSearchReplannerFactory;
+
+	protected LegModeChecker legModeChecker;
+	protected ParkingAgentsTracker parkingAgentsTracker;
+	protected InsertParkingActivities insertParkingActivities;
+	protected ParkingInfrastructure parkingInfrastructure;
+	
+	public WithinDayParkingController(String[] args) {
+		super(args);
+		
+		// register this as a Controller Listener
+		super.addControlerListener(this);
+	}
+
+	protected void initIdentifiers() {
+
+		this.randomSearchIdentifier = new RandomSearchIdentifier(parkingAgentsTracker, parkingInfrastructure); 
+		this.getFixedOrderSimulationListener().addSimulationListener(this.randomSearchIdentifier);
+	}
+	
+	/*
+	 * New Routers for the Replanning are used instead of using the controler's.
+	 * By doing this every person can use a personalised Router.
+	 */
+	protected void initReplanners() {
+
+		LeastCostPathCalculatorFactory factory = new AStarLandmarksFactory(this.network, new FreespeedTravelTimeAndDisutility(this.config.planCalcScore()));
+		ModeRouteFactory routeFactory = ((PopulationFactoryImpl) this.scenarioData.getPopulation().getFactory()).getModeRouteFactory();
+
+		// create a copy of the MultiModalTravelTimeWrapperFactory and set the TravelTimeCollector for car mode
+		Map<String, TravelTime> times = new HashMap<String, TravelTime>();
+		times.put(TransportMode.walk, new WalkTravelTime(this.config.plansCalcRoute()));
+		times.put(TransportMode.bike, new BikeTravelTime(this.config.plansCalcRoute(),
+				new WalkTravelTime(this.config.plansCalcRoute())));
+		times.put(TransportMode.ride, new RideTravelTime(this.getLinkTravelTimes(), 
+				new WalkTravelTime(this.config.plansCalcRoute())));
+		times.put(TransportMode.pt, new PTTravelTime(this.config.plansCalcRoute(), 
+				this.getLinkTravelTimes(), new WalkTravelTime(this.config.plansCalcRoute())));
+		times.put(TransportMode.car, super.getTravelTimeCollector());
+		
+		TravelDisutilityFactory costFactory = new OnlyTimeDependentTravelCostCalculatorFactory();
+		
+		AbstractMultithreadedModule router = new ReplanningModule(config, network, costFactory, times, factory, routeFactory);
+	
+		this.randomSearchReplannerFactory = new RandomSearchReplannerFactory(this.getWithinDayEngine(), router, 1.0, this.scenarioData, parkingAgentsTracker);
+		this.randomSearchReplannerFactory.addIdentifier(this.randomSearchIdentifier);		
+		this.getWithinDayEngine().addDuringLegReplannerFactory(this.randomSearchReplannerFactory);
+	}
+	
+	protected void setUp() {
+		super.setUp();
+		
+		
+// connect facilities to network
+		new WorldConnectLocations(this.config).connectFacilitiesWithLinks(getFacilities(), (NetworkImpl) getNetwork());
+		
+		super.initWithinDayEngine(numReplanningThreads);
+		super.createAndInitTravelTimeCollector();
+		super.createAndInitLinkReplanningMap();
+		
+// ensure that all agents' plans have valid mode chains
+		legModeChecker = new LegModeChecker(this.scenarioData, this.createRoutingAlgorithm());
+		legModeChecker.setValidNonCarModes(new String[]{TransportMode.walk});
+		legModeChecker.setToCarProbability(0.5);
+		legModeChecker.run(this.scenarioData.getPopulation());
+		
+		parkingInfrastructure = new ParkingInfrastructure(this.scenarioData,null,null);
+		
+		// init parking facility capacities
+				IntegerValueHashMap<Id> facilityCapacities = new IntegerValueHashMap<Id>();
+				parkingInfrastructure.setFacilityCapacities(facilityCapacities);
+				for (ActivityFacility parkingFacility : parkingInfrastructure.getParkingFacilities()) {
+					facilityCapacities.incrementBy(parkingFacility.getId(), 10);
+				}
+		
+		
+//		Set<Id> parkingFacilityIds = parkingInfrastructure.getFacilityCapacities().getKeySet();
+//		for (Id id : parkingFacilityIds) parkingInfrastructure.getFacilityCapacities().incrementBy(id, 1000);
+		
+		parkingAgentsTracker = new ParkingAgentsTracker(this.scenarioData, 2000.0);
+		this.getFixedOrderSimulationListener().addSimulationListener(this.parkingAgentsTracker);
+		this.getEvents().addHandler(this.parkingAgentsTracker);
+		
+		insertParkingActivities = new InsertParkingActivities(scenarioData, this.createRoutingAlgorithm(), parkingInfrastructure);
+		
+		MobsimFactory mobsimFactory = new ParkingQSimFactory(insertParkingActivities, parkingInfrastructure, this.getWithinDayEngine());
+		this.setMobsimFactory(mobsimFactory);
+		
+		this.initIdentifiers();
+		this.initReplanners();
+	}
+	
+	/*
+	 * When the Controller Startup Event is created, the EventsManager
+	 * has already been initialized. Therefore we can initialize now
+	 * all Objects, that have to be registered at the EventsManager.
+	 */
+	@Override
+	public void notifyStartup(StartupEvent event) {
+
+	}
+
+	@Override
+	public void notifyReplanning(ReplanningEvent event) {
+		/*
+		 * During the replanning the mode chain of the agents' selected plans
+		 * might have been changed. Therefore, we have to ensure that the 
+		 * chains are still valid.
+		 */
+		for (Person person : this.scenarioData.getPopulation().getPersons().values()) {
+			legModeChecker.run(person.getSelectedPlan());			
+		}
+	}
+		
+	/*
+	 * ===================================================================
+	 * main
+	 * ===================================================================
+	 */
+	public static void main(String[] args) {
+		if ((args == null) || (args.length == 0)) {
+			System.out.println("No argument given!");
+			System.out.println("Usage: Controler config-file [dtd-file]");
+			System.out.println("using default config");
+			args=new String[]{"test/input/playground/wrashid/parkingSearch/withinday/chessboard/config_plans1.xml"};
+//			args=new String[]{"test/input/playground/wrashid/parkingSearch/withinday/chessboard/config.xml"};
+		}
+		final WithinDayParkingController controller = new WithinDayParkingController(args);
+		controller.setOverwriteFiles(true);
+		
+		
+		controller.run();
+		
+		System.exit(0);
+	}
+}
