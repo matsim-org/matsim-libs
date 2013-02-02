72369cc2bd3322372c329d13d4450057f47a2c3f
andreas77 <andreashorni@hotmail.com>
2013-02-02 11:06:08 +0000

    comment
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@22855 a0517364-e873-4167-b1c8-416daa5a6b91

--- contribs/locationchoice/src/main/java/org/matsim/contrib/locationchoice/bestresponse/DestinationSampler.java
+++ contribs/locationchoice/src/main/java/org/matsim/contrib/locationchoice/bestresponse/DestinationSampler.java
@@ -51,6 +51,18 @@ public class DestinationSampler {
 			Logger.getLogger(this.getClass()).warn("Proba that they are equal is 1% (=0.1*0.1), and not 10% as it seems to pretend.") ;
 			Logger.getLogger(this.getClass()).warn("???? kai, jan'13") ;
 		}
+		/*
+		 * ah: Not sure if I should even try to think today, but let me see ...
+		 * Isn't it a cond. proba?
+		 * personValue = 1 -> 10% proba for facilityValue == 1
+		 * personValue = 2 -> 10% proba for facilityValue == 2
+		 * i.e., whatever personValue might is, we have 10% proba for facilityValue being the same.
+		 * In other words, we have 100 combinations and not only 1 but 10 matchings. 
+		 * Or do I make the same error in thinking again?
+		 * 
+		 * This is the case for samplePercent == 10, need to think about the other percentages. 
+		 * Maybe we have another problem there.
+		 */
 		
 		int facilityValue = (int)Math.floor(100.0 / samplePercent * 
 				(Double) this.facilitiesKValues.getAttribute(facilityId.toString(), "k"));
