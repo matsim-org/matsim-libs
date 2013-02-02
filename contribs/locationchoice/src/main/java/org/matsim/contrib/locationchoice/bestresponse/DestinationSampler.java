83fee4c8e2961d1add353ec79e62a942362e3a3c
andreas77 <andreashorni@hotmail.com>
2013-02-02 15:56:58 +0000

    comment
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@22867 a0517364-e873-4167-b1c8-416daa5a6b91

--- contribs/locationchoice/src/main/java/org/matsim/contrib/locationchoice/bestresponse/DestinationSampler.java
+++ contribs/locationchoice/src/main/java/org/matsim/contrib/locationchoice/bestresponse/DestinationSampler.java
@@ -19,7 +19,6 @@
 
 package org.matsim.contrib.locationchoice.bestresponse;
 
-import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Id;
 import org.matsim.core.config.groups.LocationChoiceConfigGroup;
 import org.matsim.utils.objectattributes.ObjectAttributes;
@@ -36,39 +35,50 @@ public class DestinationSampler {
 		this.personsKValues = personsKValues;
 		this.samplePercent = Double.parseDouble(config.getDestinationSamplePercent());
 	}
-	
-	private static int wrnCnt = 0 ;
-	
-	// at the moment only working for powers of 10
-	public boolean sample(Id facilityId, Id personId) { 
-		if (Math.ceil(this.samplePercent) == 100) return true;
 		
-		if (wrnCnt < 1 ) {
-			wrnCnt++ ;
+	/* ah: At the moment this method is only working for powers of 10 in the range [0..100]
+		
+		The sample must remain stable over the course of iterations. Thus, person as well as facility k values 
+		are used. 
+		
+		(Conditional) probability (in %) of a facility k-value matching the given person k-value is 
+		this.samplePercent	
+		
+		example: this.samplePercent = 10;
+		person i has a personValue k between 0 and 9
+		probability that facilityValue is k is expected 0.1 (or 10%)
+	*/
+	public boolean sample(Id facilityId, Id personId) { 
+		if (Math.ceil(this.samplePercent) == 100) return true;		
+		int facilityValue = (int)Math.floor(100.0 / samplePercent * 
+				(Double) this.facilitiesKValues.getAttribute(facilityId.toString(), "k"));
+		int personValue = (int)Math.floor(100.0 / samplePercent * 
+				(Double) this.personsKValues.getAttribute(personId.toString(), "k"));
+		return (facilityValue == personValue);		
+	}
+}
+
+/* To be deleted as soon as comment above is meaningful enough: ----------------------------
+  
+   if (wrnCnt < 1 ) {
+ 			wrnCnt++ ;
 			Logger.getLogger(this.getClass()).warn("I do not understand what is happening when samplePercent!=100.") ;
 			Logger.getLogger(this.getClass()).warn("E.g. samplePercent=10.  Then (int)(100./10.*rnd) = an integer number between 0 and 9.") ;
 			Logger.getLogger(this.getClass()).warn("That was for facilities; same will happen for persons.") ;
 			Logger.getLogger(this.getClass()).warn("Proba that they are equal is 1% (=0.1*0.1), and not 10% as it seems to pretend.") ;
 			Logger.getLogger(this.getClass()).warn("???? kai, jan'13") ;
-		}
-		/*
-		 * ah: Not sure if I should even try to think today, but let me see ...
-		 * Isn't it a cond. proba?
-		 * personValue = 1 -> 10% proba for facilityValue == 1
-		 * personValue = 2 -> 10% proba for facilityValue == 2
-		 * i.e., whatever personValue might is, we have 10% proba for facilityValue being the same.
-		 * In other words, we have 100 combinations and not only 1 but 10 matchings. 
-		 * Or do I make the same error in thinking again?
-		 * 
-		 * This is the case for samplePercent == 10, need to think about the other percentages. 
-		 * Maybe we have another problem there.
-		 */
-		
-		int facilityValue = (int)Math.floor(100.0 / samplePercent * 
-				(Double) this.facilitiesKValues.getAttribute(facilityId.toString(), "k"));
-		int personValue = (int)Math.floor(100.0 / samplePercent * 
-				(Double) this.personsKValues.getAttribute(personId.toString(), "k"));
-		return (facilityValue == personValue);
-		
 	}
-}
+	
+	
+	 * ah: Not sure if I should even try to think today, but let me see ...
+	 * Isn't it a cond. proba?
+	 * personValue = 1 -> 10% proba for facilityValue == 1
+	 * personValue = 2 -> 10% proba for facilityValue == 2
+	 * i.e., whatever personValue might is, we have 10% proba for facilityValue being the same.
+	 * In other words, we have 100 combinations and not only 1 but 10 matchings. 
+	 * Or do I make the same error in thinking again?
+	 * 
+	 * This is the case for samplePercent == 10, need to think about the other percentages. 
+	 * Maybe we have another problem there.
+
+ */
