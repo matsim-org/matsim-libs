dd9722333207450ea4c3ae930e64efa7051c3ed8
dhosse <dhosse@users.sf.net>
2013-02-05 11:07:29 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@22889 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/dhosse/src/main/java/playground/dhosse/bachelorarbeit/Main.java
+++ playgrounds/dhosse/src/main/java/playground/dhosse/bachelorarbeit/Main.java
@@ -1,15 +1,14 @@
 package playground.dhosse.bachelorarbeit;
 
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.PlanElement;
-import org.matsim.contrib.matsim4opus.constants.InternalConstants;
 import org.matsim.contrib.matsim4opus.gis.SpatialGrid;
 import org.matsim.contrib.matsim4opus.gis.Zone;
 import org.matsim.contrib.matsim4opus.gis.ZoneLayer;
@@ -24,13 +23,9 @@ import org.matsim.core.controler.Controler;
 import org.matsim.core.facilities.ActivityFacilitiesImpl;
 import org.matsim.core.scenario.ScenarioImpl;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.core.utils.gis.ShapeFileReader;
-import org.opengis.feature.Feature;
-import org.opengis.feature.simple.SimpleFeature;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.CoordinateSequence;
-import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.impl.CoordinateArraySequence;
@@ -63,12 +58,11 @@ public class Main {
 			
 			@Override
 			public double getOpportunitySampleRate() {
-				return 0;
+				return 1.;
 			}
 		};
 		
 		NetworkBoundaryBox bbox = new NetworkBoundaryBox();
-		
 		bbox.setDefaultBoundaryBox(scenario.getNetwork());
 		
 		SpatialGrid freeSpeedGrid = new SpatialGrid(bbox.getBoundingBox(), 100);
--- playgrounds/dhosse/src/main/java/playground/dhosse/bachelorarbeit/MyParcelBasedAccessibilityControlerListener.java
+++ playgrounds/dhosse/src/main/java/playground/dhosse/bachelorarbeit/MyParcelBasedAccessibilityControlerListener.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
@@ -30,9 +29,7 @@ import org.matsim.contrib.matsim4opus.matsim4urbansim.router.PtMatrix;
 import org.matsim.contrib.matsim4opus.utils.helperObjects.AggregateObject2NearestNode;
 import org.matsim.contrib.matsim4opus.utils.helperObjects.Benchmark;
 import org.matsim.contrib.matsim4opus.utils.helperObjects.Distances;
-import org.matsim.contrib.matsim4opus.utils.helperObjects.SpatialReferenceObject;
 import org.matsim.contrib.matsim4opus.utils.io.writer.AnalysisCellBasedAccessibilityCSVWriterV2;
-import org.matsim.contrib.matsim4opus.utils.io.writer.AnalysisWorkplaceCSVWriter;
 import org.matsim.contrib.matsim4opus.utils.io.writer.UrbanSimParcelCSVWriter;
 import org.matsim.contrib.matsim4opus.utils.misc.ProgressBar;
 import org.matsim.contrib.matsim4opus.utils.network.NetworkUtil;
@@ -46,13 +43,6 @@ import org.matsim.core.scenario.ScenarioImpl;
 import org.matsim.core.utils.geometry.CoordImpl;
 import org.matsim.utils.LeastCostPathTree;
 
-import com.vividsolutions.jts.geom.CoordinateFilter;
-import com.vividsolutions.jts.geom.CoordinateSequenceComparator;
-import com.vividsolutions.jts.geom.CoordinateSequenceFilter;
-import com.vividsolutions.jts.geom.Envelope;
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.GeometryComponentFilter;
-import com.vividsolutions.jts.geom.GeometryFilter;
 import com.vividsolutions.jts.geom.Point;
 
 public class MyParcelBasedAccessibilityControlerListener extends
@@ -203,7 +193,7 @@ public class MyParcelBasedAccessibilityControlerListener extends
 			
 			AnalysisCellBasedAccessibilityCSVWriterV2.close(); 
 			writePlottingData();						// plotting data for visual analysis via R
-//			writeInterpolatedParcelAccessibilities();	// UrbanSim input file with interpolated accessibilities on parcel level
+			writeInterpolatedParcelAccessibilities();	// UrbanSim input file with interpolated accessibilities on parcel level
 		
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -216,7 +206,7 @@ public class MyParcelBasedAccessibilityControlerListener extends
 		
 		log.info("Writing plotting files ...");
 		// tnicolai: can be disabled for final release
-		
+	
 		GridUtils.writeSpatialGridTable(freeSpeedGrid, InternalConstants.MATSIM_4_OPUS_TEMP	// freespeed results for plotting in R
 				+ FREESEED_FILENAME + freeSpeedGrid.getResolution()
 				+ InternalConstants.FILE_TYPE_TXT);
@@ -233,9 +223,9 @@ public class MyParcelBasedAccessibilityControlerListener extends
 				+ WALK_FILENAME + walkGrid.getResolution()
 				+ InternalConstants.FILE_TYPE_TXT);
 		// tnicolai: can be disabled for final release
-//		GridUtils.writeSpatialGridTable(ptGrid, InternalConstants.MATSIM_4_OPUS_TEMP	// walk results for plotting in R
-//				+ PT_FILENAME + ptGrid.getResolution()
-//				+ InternalConstants.FILE_TYPE_TXT);
+		GridUtils.writeSpatialGridTable(ptGrid, InternalConstants.MATSIM_4_OPUS_TEMP	// walk results for plotting in R
+				+ PT_FILENAME + ptGrid.getResolution()
+				+ InternalConstants.FILE_TYPE_TXT);
 
 		log.info("Writing plotting files done!");
 	}
@@ -325,11 +315,6 @@ public class MyParcelBasedAccessibilityControlerListener extends
 			
 			moduleAPCM.setAccessibilityDestinationSamplingRate(1);
 			moduleAPCM.setLogitScaleParameter(1);
-			moduleAPCM.setUseBikeParameterFromMATSim(true);
-			moduleAPCM.setUseCarParameterFromMATSim(true);
-			moduleAPCM.setUseLogitScaleParameterFromMATSim(true);
-			moduleAPCM.setUseRawSumsWithoutLn(true);
-			moduleAPCM.setUseWalkParameterFromMATSim(true);
 			
 			useRawSum			= moduleAPCM.isUseRawSumsWithoutLn();
 			logitScaleParameter = moduleAPCM.getLogitScaleParameter();
@@ -381,200 +366,4 @@ public class MyParcelBasedAccessibilityControlerListener extends
 			printParameterSettings();
 		}
 		
-		@Override
-		protected void accessibilityComputation(TravelTime ttc,
-				LeastCostPathTree lcptFreeSpeedCarTravelTime,
-				LeastCostPathTree lcptCongestedCarTravelTime,
-				LeastCostPathTree lcptTravelDistance, 
-				PtMatrix ptMatrix,
-				NetworkImpl network,
-				Iterator<Zone<Id>> measuringPointIterator,
-				int numberOfMeasuringPoints, 
-				int mode) {
-
-			GeneralizedCostSum gcs = new GeneralizedCostSum();
-
-			//// tnicolai: only for testing, disable afterwards
-			//ZoneLayer<Id> testSet = createTestPoints();
-			//measuringPointIterator = testSet.getZones().iterator();
-
-			// this data structure condense measuring points (origins) that have the same nearest node on the network ...
-			Map<Id,ArrayList<Zone<Id>>> aggregatedMeasurementPoints = new HashMap<Id, ArrayList<Zone<Id>>>();
-			
-			// go through all measuring points ...
-			while( measuringPointIterator.hasNext() ){
-
-				Zone<Id> measurePoint = measuringPointIterator.next();
-				Point point = measurePoint.getGeometry().getCentroid();
-				// get coordinate from origin (start point)
-				Coord coordFromZone = new CoordImpl( point.getX(), point.getY());
-				// captures the distance (as walk time) between a cell centroid and the road network
-				Link nearestLink = network.getNearestLinkExactly(coordFromZone);
-				// determine nearest network node (from- or toNode) based on the link 
-				Node fromNode = NetworkUtil.getNearestNode(coordFromZone, nearestLink);
-
-				// this is used as a key for hash map lookups
-				Id id = fromNode.getId();
-
-				// create new entry if key does not exist!
-				if(!aggregatedMeasurementPoints.containsKey(id))
-					aggregatedMeasurementPoints.put(id, new ArrayList<Zone<Id>>());
-				// assign measure point (origin) to it's nearest node
-				aggregatedMeasurementPoints.get(id).add(measurePoint);
-			}
-
-			log.info("");
-			log.info("Number of measure points: " + numberOfMeasuringPoints);
-			log.info("Number of aggregated measure points: " + aggregatedMeasurementPoints.size());
-			log.info("");
-
-
-			ProgressBar bar = new ProgressBar( aggregatedMeasurementPoints.size() );
-
-			// contains all nodes that have a measuring point (origin) assigned
-			Iterator<Id> keyIterator = aggregatedMeasurementPoints.keySet().iterator();
-			// contains all network nodes
-			Map<Id, Node> networkNodesMap = network.getNodes();
-
-			// go through all nodes (key's) that have a measuring point (origin) assigned
-			while( keyIterator.hasNext() ){
-
-				bar.update();
-
-				Id nodeId = keyIterator.next();
-				Node fromNode = networkNodesMap.get( nodeId );
-
-				// run dijkstra on network
-				// this is done once for all origins in the "origins" list, see below
-				lcptFreeSpeedCarTravelTime.calculate(network, fromNode, depatureTime);
-				lcptCongestedCarTravelTime.calculate(network, fromNode, depatureTime);		
-				lcptTravelDistance.calculate(network, fromNode, depatureTime);
-
-				// get list with origins that are assigned to "fromNode"
-				ArrayList<Zone<Id>> origins = aggregatedMeasurementPoints.get( nodeId );
-				Iterator<Zone<Id>> originsIterator = origins.iterator();
-
-				while( originsIterator.hasNext() ){
-
-					Zone<Id> measurePoint = originsIterator.next();
-					Point point = measurePoint.getGeometry().getCentroid();
-					// get coordinate from origin (start point)
-					Coord coordFromZone = new CoordImpl( point.getX(), point.getY());
-					assert( coordFromZone!=null );
-					// captures the distance (as walk time) between a cell centroid and the road network
-					Link nearestLink = network.getNearestLinkExactly(coordFromZone);
-
-					// captures the distance (as walk time) between a zone centroid and its nearest node
-					Distances distance = NetworkUtil.getDistance2NodeV2(nearestLink, point, fromNode);
-
-					double distanceMeasuringPoint2Road_meter 	= distance.getDisatancePoint2Road(); // distance measuring point 2 road (link or node)
-					double distanceRoad2Node_meter 				= distance.getDistanceRoad2Node();	 // distance intersection 2 node (only for orthogonal distance)
-
-					double walkTravelTimePoint2Road_h 			= distanceMeasuringPoint2Road_meter / this.walkSpeedMeterPerHour;
-
-					double freeSpeedTravelTimeOnNearestLink_meterpersec= nearestLink.getFreespeed();
-					double carTravelTimeOnNearestLink_meterpersec= nearestLink.getLength() / ttc.getLinkTravelTime(nearestLink, depatureTime, null, null);
-
-					double road2NodeFreeSpeedTime_h				= distanceRoad2Node_meter / (freeSpeedTravelTimeOnNearestLink_meterpersec * 3600);
-					double road2NodeCongestedCarTime_h 			= distanceRoad2Node_meter / (carTravelTimeOnNearestLink_meterpersec * 3600.);
-					double road2NodeBikeTime_h					= distanceRoad2Node_meter / this.bikeSpeedMeterPerHour;
-					double road2NodeWalkTime_h					= distanceRoad2Node_meter / this.walkSpeedMeterPerHour;
-
-
-					// Possible offsets to calculate the gap between measuring (start) point and start node (fromNode)
-					// Euclidean Distance (measuring point 2 nearest node):
-					// double walkTimeOffset_min = NetworkUtil.getEuclideanDistanceAsWalkTimeInSeconds(coordFromZone, fromNode.getCoord()) / 60.;
-					// Orthogonal Distance (measuring point 2 nearest link, does not include remaining distance between link intersection and nearest node)
-					// LinkImpl nearestLink = network.getNearestLink( coordFromZone );
-					// double walkTimeOffset_min = (nearestLink.calcDistance( coordFromZone ) / this.walkSpeedMeterPerMin); 
-					// or use NetworkUtil.getOrthogonalDistance(link, point) instead!
-
-					gcs.reset();
-
-					// goes through all opportunities, e.g. jobs, (nearest network node) and calculate the accessibility
-					for ( int i = 0; i < this.aggregatedOpportunities.length; i++ ) {
-
-						// get stored network node (this is the nearest node next to an aggregated work place)
-						Node destinationNode = this.aggregatedOpportunities[i].getNearestNode();
-						Id nodeID = destinationNode.getId();
-
-						// tnicolai not needed anymore since having the aggregated costs on the opportunity side
-						// using number of aggregated opportunities as weight for log sum measure
-						// int opportunityWeight = this.aggregatedOpportunities[i].getNumberOfObjects(); 
-
-						// congested car travel times in hours
-						double arrivalTime 			= lcptCongestedCarTravelTime.getTree().get( nodeID ).getTime(); // may also use .getCost() !!!
-						double congestedCarTravelTime_h = ((arrivalTime - depatureTime) / 3600.) + road2NodeCongestedCarTime_h;
-						// free speed car travel times in hours
-						double freeSpeedTravelTime_h= (lcptFreeSpeedCarTravelTime.getTree().get( nodeID ).getCost() / 3600.) + road2NodeFreeSpeedTime_h;
-						// travel distance in meter
-						double travelDistance_meter = lcptTravelDistance.getTree().get( nodeID ).getCost();
-						// bike travel times in hours
-						double bikeTravelTime_h 	= (travelDistance_meter / this.bikeSpeedMeterPerHour) + road2NodeBikeTime_h; // using a constant speed of 15km/h
-						// walk travel times in hours
-						double walkTravelTime_h		= (travelDistance_meter / this.walkSpeedMeterPerHour) + road2NodeWalkTime_h;
-
-						// travel times and distances for pseudo pt
-						double ptTravelTime_h		= Double.MAX_VALUE;	// travel time with pt
-						double ptTotalWalkTime_h	= Double.MAX_VALUE;	// total walking time including (i) to get to pt stop and (ii) to get from destination pt stop to destination location
-						double ptTravelDistance_meter=Double.MAX_VALUE; // total travel distance including walking and pt distance from/to origin/destination location
-						double ptTotalWalkDistance_meter=Double.MAX_VALUE;// total walk distance  including (i) to get to pt stop and (ii) to get from destination pt stop to destination location
-						if(ptMatrix != null){
-							ptTravelTime_h 			= ptMatrix.getPtTravelTime(fromNode.getCoord(), destinationNode.getCoord()) / 3600.;
-							ptTotalWalkTime_h		=ptMatrix.getTotalWalkTravelTime(fromNode.getCoord(), destinationNode.getCoord()) / 3600.;
-
-							ptTotalWalkDistance_meter=ptMatrix.getTotalWalkTravelDistance(fromNode.getCoord(), destinationNode.getCoord());
-							ptTravelDistance_meter  = ptMatrix.getPtTravelDistance(fromNode.getCoord(), destinationNode.getCoord());
-						}
-
-						sumDisutilityOfTravel(gcs, 
-								this.aggregatedOpportunities[i],
-								distanceMeasuringPoint2Road_meter,
-								distanceRoad2Node_meter, 
-								travelDistance_meter,
-								walkTravelTimePoint2Road_h,
-								freeSpeedTravelTime_h,
-								bikeTravelTime_h,
-								walkTravelTime_h, 
-								congestedCarTravelTime_h,
-								ptTravelTime_h,
-								ptTotalWalkTime_h,
-								ptTravelDistance_meter,
-								ptTotalWalkDistance_meter);
-					}
-
-					// aggregated value
-					double freeSpeedAccessibility, carAccessibility, bikeAccessibility, walkAccessibility, ptAccessibility;
-					if(!useRawSum){ 	// get log sum
-						freeSpeedAccessibility = inverseOfLogitScaleParameter * Math.log( gcs.getFreeSpeedSum() );
-						carAccessibility = inverseOfLogitScaleParameter * Math.log( gcs.getCarSum() );
-						bikeAccessibility= inverseOfLogitScaleParameter * Math.log( gcs.getBikeSum() );
-						walkAccessibility= inverseOfLogitScaleParameter * Math.log( gcs.getWalkSum() );
-						ptAccessibility	 = inverseOfLogitScaleParameter * Math.log( gcs.getPtSum() );
-					}
-					else{ 				// get raw sum
-						freeSpeedAccessibility = inverseOfLogitScaleParameter * gcs.getFreeSpeedSum();
-						carAccessibility = inverseOfLogitScaleParameter * gcs.getCarSum();
-						bikeAccessibility= inverseOfLogitScaleParameter * gcs.getBikeSum();
-						walkAccessibility= inverseOfLogitScaleParameter * gcs.getWalkSum();
-						ptAccessibility  = inverseOfLogitScaleParameter * gcs.getPtSum();
-					}
-
-					if(mode == PARCEL_BASED){ // only for cell-based accessibility computation
-//						 assign log sums to current starZone object and spatial grid
-						freeSpeedGrid.setValue(freeSpeedAccessibility, measurePoint.getGeometry().getCentroid());
-						carGrid.setValue(carAccessibility , measurePoint.getGeometry().getCentroid());
-						bikeGrid.setValue(bikeAccessibility , measurePoint.getGeometry().getCentroid());
-						walkGrid.setValue(walkAccessibility , measurePoint.getGeometry().getCentroid());
-						ptGrid.setValue(ptAccessibility, measurePoint.getGeometry().getCentroid());
-//						log.info(freeSpeedGrid.getValue(measurePoint.getGeometry().getCentroid()));
-					}
-
-//					writeCSVData(measurePoint, coordFromZone, fromNode, 
-//							freeSpeedAccessibility, carAccessibility,
-//							bikeAccessibility, walkAccessibility, ptAccessibility);
-				}
-			}
-		}
-
 }
