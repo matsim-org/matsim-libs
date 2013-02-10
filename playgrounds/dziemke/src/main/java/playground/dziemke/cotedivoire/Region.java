b4e9d4e73bb543e780dffbd3a0c749fb92deb0c3
ziemke <ziemke@vsp.tu-berlin.de>
2013-02-10 14:16:38 +0000

    Cote dIvoire Population Analysis changed package structure
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@22965 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/dziemke/src/main/java/playground/dziemke/cotedivoire/Region.java
@@ -0,0 +1,54 @@
+package playground.dziemke.cotedivoire;
+
+import java.util.List;
+import com.vividsolutions.jts.geom.Geometry;
+
+public class Region {
+
+	private int id;
+	private String name;
+	private Geometry geometry;
+	private List <Prefecture> subprefectures;
+		
+	public Region() {
+	}	
+		
+	public Region(int id, String name, Geometry geometry) {
+		this.id = id;
+		this.name = name;
+		this.geometry = geometry;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Geometry getGeometry() {
+		return geometry;
+	}
+
+	public void setGeometry(Geometry geometry) {
+		this.geometry = geometry;
+	}
+	
+	public List <Prefecture> getPrefecture() {
+		return subprefectures;
+	}
+
+	public void setPrefectures(List <Prefecture> subprefectures) {
+		this.subprefectures = subprefectures;
+	}
+
+}
---
+++ playgrounds/dziemke/src/main/java/playground/dziemke/cotedivoire/RegionSubprefectureMatcher.java
@@ -0,0 +1,49 @@
+package playground.dziemke.cotedivoire;
+
+import java.util.List;
+import org.matsim.core.utils.geometry.geotools.MGC;
+import com.vividsolutions.jts.geom.Point;
+
+public class RegionSubprefectureMatcher implements Runnable{
+	private List <Region> regions = ShapeReader.read("D:/Workspace/matsim-0.3.0/input/cotedivoire/CIV_adm1.shp");
+	private List <Prefecture> subprefectures = PrefectureReader.read("D:/Workspace/matsim-0.3.0/input/cotedivoire/SUBPREF_POS_LONLAT.TSV");
+	
+	
+	public static void main(String[] args) {
+		RegionSubprefectureMatcher potsdamPop = new RegionSubprefectureMatcher();
+		potsdamPop.run();	
+	}
+	
+	
+	public void run() {
+		matchPrefecturesToRegions();
+	}
+	
+	
+	public void matchPrefecturesToRegions() {
+		List <Region> regions = this.regions;
+		List <Prefecture> subprefectures = this.subprefectures;
+		System.out.println("Anzahl an Region: " + regions.size());
+		
+		int counter = 0;
+		
+		for (int n = 0; n < regions.size(); n++) {
+			System.out.println("Region: " + regions.get(n).getName() + "; Fläche: " + regions.get(n).getGeometry().getArea());
+			
+			for (int i=0; i<subprefectures.size(); i++) {
+				Point point;
+				double longitude = subprefectures.get(i).getLongitude();
+				double latitude = subprefectures.get(i).getLatitude();
+				point = MGC.xy2Point(longitude, latitude);
+				if (regions.get(n).getGeometry().contains(point)) {
+					//System.out.println(point.toString());
+					System.out.println("Die Subpräfektur mit der ID " + subprefectures.get(i).getId() + " befindet sich in der Region " +
+							regions.get(n).getName());
+					counter++;
+				}
+			}
+		}
+		System.out.println("Es wurden insgesamt " + counter + " Subpräfekturen zugewiesen.");
+	}
+	
+}
