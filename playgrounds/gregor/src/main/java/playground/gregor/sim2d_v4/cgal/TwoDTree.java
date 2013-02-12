0eb7530fb9a92ec095ff4283edcf741aec68bf8d
laemmel <gregor.laemmel@gmail.com>
2013-02-12 11:19:55 +0000

    refined TwoDTree implementation.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@23007 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/gregor/src/main/java/playground/gregor/sim2d_v4/cgal/TwoDTree.java
+++ playgrounds/gregor/src/main/java/playground/gregor/sim2d_v4/cgal/TwoDTree.java
@@ -22,8 +22,15 @@
 package playground.gregor.sim2d_v4.cgal;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+import org.matsim.core.gbl.MatsimRandom;
+import org.matsim.core.utils.collections.QuadTree;
 
 import com.vividsolutions.jts.geom.Envelope;
 
@@ -32,13 +39,18 @@ import com.vividsolutions.jts.geom.Envelope;
 /**
  * implementation of 2-dimensional kd-tree (== 2d-tree)
  * similar to de Berg et al (2000), Computational Geometry: Algorithms and Applications. Springer
+ * The main differences are:
+ * 	1. for small data sets (<128) data is stored in a list, since for small data sets a linear search is usually faster
+ * 	2. the tree has a max depth to avoid stack overflow exceptions. If the max depth is reached for a region in the tree all data that
+ * 	is subsequent inserted into this region will be stored in a list. This means the algorithm performs a linear search for the region in question if range search
+ * 	is performed on it.  
  * @author laemmel
  *
  * @param <T>
  */
 public class TwoDTree<T extends TwoDObject> {
 
-
+	
 	private TwoDNode<T> root;
 
 	public TwoDTree(Envelope e) {
@@ -46,6 +58,7 @@ public class TwoDTree<T extends TwoDObject> {
 	}
 
 	public void buildTwoDTree(List<T> values) {
+		
 		for (T val : values) {
 			insert(val);
 		}
@@ -74,19 +87,25 @@ public class TwoDTree<T extends TwoDObject> {
 	}
 
 	private static class TwoDNode<T extends TwoDObject> {
-		private final static int cacheSize = 16; 
-
+		private static final Logger log = Logger.getLogger(TwoDNode.class);
+		private static final int cacheSize = 128; 
+		private static final int maxDepth = 128; 
+		
 		private final int depth;
 		private final Envelope envelope;
 		private TwoDNode<T> left = null;
 		private TwoDNode<T> right = null;
-		private final List<T> cache = new ArrayList<T>(cacheSize);
+		private final List<T> cache = new ArrayList<T>(this.cacheSize);
 
 		private boolean internalNode = false;
 
 		public TwoDNode(Envelope e, int i) {
 			this.depth = i;
 			this.envelope = e;
+			if (this.depth >= maxDepth) {
+				log.warn("Maximum recursion depth reached! The region is: " + this.envelope + " Data inserted into this region will be stored in a linear list.");
+				
+			}
 		}
 
 		private boolean insert(T val) {
@@ -94,7 +113,7 @@ public class TwoDTree<T extends TwoDObject> {
 			if (this.envelope.intersects(val.getXLocation(), val.getYLocation())) {
 				if (!this.internalNode) {
 					this.cache.add(val);
-					if (this.cache.size() >= cacheSize) {
+					if (this.cache.size() >= TwoDNode.cacheSize && this.depth < TwoDNode.maxDepth) {
 						split();
 					}
 
@@ -150,25 +169,47 @@ public class TwoDTree<T extends TwoDObject> {
 			}
 			return ret;
 		}
-
-		private void split() {
+		
+		void split() {
 			final double minX = this.envelope.getMinX();
 			final double minY = this.envelope.getMinY();
 			final double maxX = this.envelope.getMaxX();
 			final double maxY = this.envelope.getMaxY();
 
 			if (this.depth % 2 == 0) { //vertical split
-				final double width = this.envelope.getWidth();
-				Envelope e1 = new Envelope(minX, minX +width/2,  minY, maxY);
+				final float [] xs = new float[this.cache.size()];
+				int idx = 0;
+				Iterator<T> it = this.cache.iterator();
+				while (it.hasNext()) {
+					T next = it.next();
+					float x = next.getXLocation();
+					xs[idx++] = x;
+				}
+				Arrays.sort(xs);
+				float median = xs[this.cache.size()/2];
+				
+//				final double width = median - this.envelope.getMinX();
+				Envelope e1 = new Envelope(minX, median,  minY, maxY);
 				this.left = new TwoDNode<T>(e1, this.depth+1);
-				Envelope e2 = new Envelope(minX +width/2,maxX, minY,maxY);
+				Envelope e2 = new Envelope(median,maxX, minY,maxY);
 				this.right = new TwoDNode<T>(e2, this.depth+1);
 
 			} else {//horizontal split
-				final double height = this.envelope.getHeight();
-				Envelope e1 = new Envelope(minX, maxX,  minY, minY+height/2);
+				final float [] ys = new float[this.cache.size()];
+				int idx = 0;
+				Iterator<T> it = this.cache.iterator();
+				while (it.hasNext()) {
+					T next = it.next();
+					float y = next.getYLocation();
+					ys[idx++] = y;
+				}
+				Arrays.sort(ys);
+				float median = ys[this.cache.size()/2];
+				
+//				final double height = this.envelope.getHeight();
+				Envelope e1 = new Envelope(minX, maxX,  minY, median);
 				this.left = new TwoDNode<T>(e1, this.depth+1);
-				Envelope e2 = new Envelope(minX ,maxX, minY+height/2,maxY);
+				Envelope e2 = new Envelope(minX ,maxX, median,maxY);
 				this.right = new TwoDNode<T>(e2, this.depth+1);
 			}
 
@@ -195,109 +236,107 @@ public class TwoDTree<T extends TwoDObject> {
 	}
 
 
-//	//testing only!!
-//	public static void main (String [] args) {
-//		int TEST = 10000;
-//		Envelope e = new Envelope(-100,100,-100,100);
-//		List<TwoDObj> os = new ArrayList<TwoDObj>();
-//		for (int i = 0; i < TEST; i++) {
-//			float x = (MatsimRandom.getRandom().nextFloat() - .5f) * 200;
-//			float y = (MatsimRandom.getRandom().nextFloat() - .5f) * 200;
-//			TwoDObj o = new TwoDObj(x,y);
-//			os.add(o);
-//		}
-//
-//
-//		//1. build
-//		long start = System.nanoTime();
-//		for (int i = 0; i < 1000; i ++) {
-//			QuadTree<TwoDObj> quadTree = new QuadTree<TwoDObj>(-100,-100,100,100);
-//			for (TwoDObj o : os) {
-//				quadTree.put(o.getXLocation(), o.getYLocation(), o);
-//			}
-//		}
-//		long stop = System.nanoTime();
-//		System.out.println("quad-tree build took: " + ((stop-start)/1000/1000.) + " ms");
-//
-//
-//		long start2 = System.nanoTime();
-//		for (int i = 0; i < 1000; i ++) {
-//			TwoDTree<TwoDObj> twoDTree = new TwoDTree<TwoDObj>(e);
-//			for (TwoDObj o : os) {
-//				twoDTree.insert(o);
-//			}
-//		}
-//		long stop2 = System.nanoTime();
-//		System.out.println("2D-tree build took:   " + ((stop2-start2)/1000/1000.) + " ms");
-//
-//		//2. range search
-//		QuadTree<TwoDObj> quadTree = new QuadTree<TwoDObj>(-100,-100,100,100);
-//		TwoDTree<TwoDObj> twoDTree = new TwoDTree<TwoDObj>(e);
-//		for (TwoDObj o : os) {
-//			quadTree.put(o.getXLocation(), o.getYLocation(), o);
-//			twoDTree.insert(o);
-//		}		
-//
-//		List<Envelope> ranges = new ArrayList<Envelope>();
-//		for (int i = 0; i < TEST; i++) {
-//			double minX = (MatsimRandom.getRandom().nextDouble() - .5)*180; 
-//			double minY= (MatsimRandom.getRandom().nextDouble() - .5)*180;
-//			double rangeX = (1+2*MatsimRandom.getRandom().nextDouble())*10;
-//			double rangeY = (1+2*MatsimRandom.getRandom().nextDouble())*10;
-//			Envelope ee = new Envelope(minX, minX+rangeX, minY, minY+rangeY);
-//			ranges.add(ee);
-//		}
-//
-//		long start3 = System.nanoTime();
-//		for (int i = 0; i < 100; i++) {
-//			for (Envelope ee : ranges) {
-//				quadTree.get(ee.getMinX(), ee.getMinY(), ee.getMaxX(), ee.getMaxY(), new ArrayList<TwoDObj>());
-//			}}
-//		long stop3 = System.nanoTime();
-//		System.out.println("quad-tree range search took: " + ((stop3-start3)/1000/1000.) + " ms");
-//
-//		long start4 = System.nanoTime();
-//		for (int i = 0; i < 100; i++) {
-//			for (Envelope ee : ranges) {
-//				twoDTree.get(ee);
-//			}}
-//		long stop4 = System.nanoTime();
-//		System.out.println("2D-tree range search took: " + ((stop4-start4)/1000/1000.) + " ms");
-//
-//		for (Envelope ee: ranges) {
-//			Set<TwoDObj> q = new HashSet<TwoDTree.TwoDObj>();
-//			quadTree.get(ee.getMinX(), ee.getMinY(), ee.getMaxX(), ee.getMaxY(), q);
-//			List<TwoDObj> k = twoDTree.get(ee);
-//			if (q.size() != k.size()) {
-//				System.err.println("should not happen");
-//			}
-//			for (TwoDObj t : k) {
-//				if (!q.contains(t)){
-//					System.err.println("should not happen");	
-//				}
-//			}
-//		}
-//	}
-//
-//	private static final class TwoDObj implements TwoDObject {
-//
-//		private final float x;
-//		private final float y;
-//
-//		public TwoDObj(float x, float y) {
-//			this.x = x;
-//			this.y = y;
-//		}
-//
-//		@Override
-//		public float getXLocation() {
-//			return this.x;
-//		}
-//
-//		@Override
-//		public float getYLocation() {
-//			return this.y;
-//		}
-//
-//	}
+	//testing only!!
+	public static void main (String [] args) {
+		int TEST = 10000;
+		Envelope e = new Envelope(-100,100,-100,100);
+		List<TwoDObj> os = new ArrayList<TwoDObj>();
+		for (int i = 0; i < TEST; i++) {
+			float x = (MatsimRandom.getRandom().nextFloat() - .5f) * 200;
+			float y = (MatsimRandom.getRandom().nextFloat() - .5f) * 200;
+			TwoDObj o = new TwoDObj(x,y);
+			os.add(o);
+		}
+
+
+		//1. build
+		long start = System.nanoTime();
+		for (int i = 0; i < 1000; i ++) {
+			QuadTree<TwoDObj> quadTree = new QuadTree<TwoDObj>(-100,-100,100,100);
+			for (TwoDObj o : os) {
+				quadTree.put(o.getXLocation(), o.getYLocation(), o);
+			}
+		}
+		long stop = System.nanoTime();
+		System.out.println("quad-tree build took: " + ((stop-start)/1000/1000.) + " ms");
+
+
+		long start2 = System.nanoTime();
+		for (int i = 0; i < 1000; i ++) {
+			TwoDTree<TwoDObj> twoDTree = new TwoDTree<TwoDObj>(e);
+			twoDTree.buildTwoDTree(os);
+		}
+		long stop2 = System.nanoTime();
+		System.out.println("2D-tree build took:   " + ((stop2-start2)/1000/1000.) + " ms");
+
+		//2. range search
+		QuadTree<TwoDObj> quadTree = new QuadTree<TwoDObj>(-100,-100,100,100);
+		TwoDTree<TwoDObj> twoDTree = new TwoDTree<TwoDObj>(e);
+		for (TwoDObj o : os) {
+			quadTree.put(o.getXLocation(), o.getYLocation(), o);
+		}
+		twoDTree.buildTwoDTree(os);
+
+		List<Envelope> ranges = new ArrayList<Envelope>();
+		for (int i = 0; i < TEST; i++) {
+			double minX = (MatsimRandom.getRandom().nextDouble() - .5)*180; 
+			double minY= (MatsimRandom.getRandom().nextDouble() - .5)*180;
+			double rangeX = (1+2*MatsimRandom.getRandom().nextDouble())*10;
+			double rangeY = (1+2*MatsimRandom.getRandom().nextDouble())*10;
+			Envelope ee = new Envelope(minX, minX+rangeX, minY, minY+rangeY);
+			ranges.add(ee);
+		}
+
+		long start3 = System.nanoTime();
+		for (int i = 0; i < 100; i++) {
+			for (Envelope ee : ranges) {
+				quadTree.get(ee.getMinX(), ee.getMinY(), ee.getMaxX(), ee.getMaxY(), new ArrayList<TwoDObj>());
+			}}
+		long stop3 = System.nanoTime();
+		System.out.println("quad-tree range search took: " + ((stop3-start3)/1000/1000.) + " ms");
+
+		long start4 = System.nanoTime();
+		for (int i = 0; i < 100; i++) {
+			for (Envelope ee : ranges) {
+				twoDTree.get(ee);
+			}}
+		long stop4 = System.nanoTime();
+		System.out.println("2D-tree range search took: " + ((stop4-start4)/1000/1000.) + " ms");
+
+		for (Envelope ee: ranges) {
+			Set<TwoDObj> q = new HashSet<TwoDTree.TwoDObj>();
+			quadTree.get(ee.getMinX(), ee.getMinY(), ee.getMaxX(), ee.getMaxY(), q);
+			List<TwoDObj> k = twoDTree.get(ee);
+			if (q.size() != k.size()) {
+				System.err.println("should not happen");
+			}
+			for (TwoDObj t : k) {
+				if (!q.contains(t)){
+					System.err.println("should not happen");	
+				}
+			}
+		}
+	}
+
+	private static final class TwoDObj implements TwoDObject {
+
+		private final float x;
+		private final float y;
+
+		public TwoDObj(float x, float y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public float getXLocation() {
+			return this.x;
+		}
+
+		@Override
+		public float getYLocation() {
+			return this.y;
+		}
+
+	}
 }
