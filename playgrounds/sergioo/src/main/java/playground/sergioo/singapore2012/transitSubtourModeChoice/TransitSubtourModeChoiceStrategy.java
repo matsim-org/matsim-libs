d8b0d826862204422d9b31b9a633d8e1014021dd
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2013-02-22 15:46:47 +0000

    delete non-compiling useless hacked copy of SubtourModeChoice
    car availability can be considered by properly setting the config
    options, hence this class hasn't any use anymore.
    
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@23182 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/sergioo/src/main/java/playground/sergioo/singapore2012/transitSubtourModeChoice/TransitSubtourModeChoiceStrategy.java
+++ playgrounds/sergioo/src/main/java/playground/sergioo/singapore2012/transitSubtourModeChoice/TransitSubtourModeChoiceStrategy.java
@@ -1,5 +1,7 @@
 package playground.sergioo.singapore2012.transitSubtourModeChoice;
 
+import org.apache.log4j.Logger;
+
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.replanning.PlanStrategyModule;
 import org.matsim.core.controler.Controler;
@@ -7,17 +9,24 @@ import org.matsim.core.replanning.PlanStrategy;
 import org.matsim.core.replanning.PlanStrategyImpl;
 import org.matsim.core.replanning.ReplanningContext;
 import org.matsim.core.replanning.modules.ReRoute;
+import org.matsim.core.replanning.modules.SubtourModeChoice;
 import org.matsim.core.replanning.selectors.RandomPlanSelector;
 
 import playground.sergioo.singapore2012.transitLocationChoice.TransitActsRemoverStrategy;
 
 public class TransitSubtourModeChoiceStrategy implements PlanStrategy {
+	private static final Logger log =
+		Logger.getLogger(TransitSubtourModeChoiceStrategy.class);
+
 
 	private PlanStrategyImpl delegate;
 	
 	public TransitSubtourModeChoiceStrategy(Controler controler) {
 		delegate = new PlanStrategyImpl(new RandomPlanSelector());
 		delegate.addStrategyModule(new TransitActsRemoverStrategy(controler.getConfig()));
+		log.warn( "your stategy now uses vanilla SubtourModeChoice, not a hacked copy thereof" );
+		log.warn( "just set config.subtourModeChoice.considerCarAvailability to true in the config to get the same behavior" );
+		log.warn( "... but actually, you may just delete this strategy altogether, it does not provide anything matsim doesn't provide. td, 22. feb. 2013" );
 		delegate.addStrategyModule(new SubtourModeChoice(controler.getConfig()));
 		delegate.addStrategyModule(new ReRoute(controler.getScenario()));
 	}
