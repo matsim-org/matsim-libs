13f60f130d36e224f02032b3e6b1c9b4571bbfdb
andreas77 <andreashorni@hotmail.com>
2013-02-26 17:30:58 +0000

    handling opening interval
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@23229 a0517364-e873-4167-b1c8-416daa5a6b91

--- contribs/locationchoice/src/main/java/org/matsim/contrib/locationchoice/bestresponse/scoring/DCActivityScoringFunction.java
+++ contribs/locationchoice/src/main/java/org/matsim/contrib/locationchoice/bestresponse/scoring/DCActivityScoringFunction.java
@@ -19,6 +19,8 @@
 
 package org.matsim.contrib.locationchoice.bestresponse.scoring;
 
+import java.util.Iterator;
+import java.util.Set;
 import java.util.TreeMap;
 
 import org.apache.log4j.Logger;
@@ -29,29 +31,84 @@ import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.contrib.locationchoice.BestReplyDestinationChoice;
 import org.matsim.contrib.locationchoice.bestresponse.DestinationChoiceBestResponseContext;
 import org.matsim.contrib.locationchoice.facilityload.FacilityPenalty;
+import org.matsim.core.api.experimental.facilities.ActivityFacilities;
+import org.matsim.core.api.experimental.facilities.ActivityFacility;
+import org.matsim.core.facilities.ActivityFacilityImpl;
+import org.matsim.core.facilities.OpeningTime;
+import org.matsim.core.facilities.OpeningTime.DayType;
+import org.matsim.core.gbl.Gbl;
 import org.matsim.core.population.ActivityImpl;
 import org.matsim.core.population.PlanImpl;
 import org.matsim.core.scenario.ScenarioImpl;
-import org.matsim.core.scoring.functions.CharyparNagelOpenTimesActivityScoring;
+import org.matsim.core.scoring.functions.CharyparNagelActivityScoring;
+import org.matsim.core.utils.misc.Time;
 
-public class DCActivityScoringFunction extends CharyparNagelOpenTimesActivityScoring {
+public class DCActivityScoringFunction extends CharyparNagelActivityScoring {
 	static final Logger log = Logger.getLogger(DCActivityScoringFunction.class);
 	private DestinationScoring destinationChoiceScoring;	
+	private final ActivityFacilities facilities;
+	private Plan plan;
 	
 	public DCActivityScoringFunction(Plan plan, final TreeMap<Id, FacilityPenalty> facilityPenalties, DestinationChoiceBestResponseContext lcContext) {
-		super(plan, lcContext.getParams(), ((ScenarioImpl)lcContext.getScenario()).getActivityFacilities());
+		super(lcContext.getParams());
 		this.destinationChoiceScoring = new DestinationScoring(lcContext);
+		this.facilities = ((ScenarioImpl)lcContext.getScenario()).getActivityFacilities();
+		this.plan = plan;
 	}
 	
 	@Override
 	public void finish() {				
-		super.finish();
-		
-		for (PlanElement pe : super.plan.getPlanElements()) {
+		super.finish();	
+		for (PlanElement pe : this.plan.getPlanElements()) {
 			if (pe instanceof Activity) {
 				this.score += destinationChoiceScoring.getDestinationScore((PlanImpl)plan, (ActivityImpl)pe, 
 						BestReplyDestinationChoice.useScaleEpsilonFromConfig);
 			}
 		}
+	}
+	
+	@Override
+	protected double[] getOpeningInterval(Activity act) {
+		// openInterval has two values
+		// openInterval[0] will be the opening time
+		// openInterval[1] will be the closing time
+		double[] openInterval = new double[]{Time.UNDEFINED_TIME, Time.UNDEFINED_TIME};
+		boolean foundAct = false;
+
+		ActivityFacility facility = this.facilities.getFacilities().get(act.getFacilityId());
+		Iterator<String> facilityActTypeIterator = facility.getActivityOptions().keySet().iterator();
+		String facilityActType = null;
+		Set<OpeningTime> opentimes = null;
+
+		while (facilityActTypeIterator.hasNext() && !foundAct) {
+			facilityActType = facilityActTypeIterator.next();
+			if (act.getType().substring(0, 1).equals(facilityActType.substring(0, 1))) { // TODO: check here actType conversions
+				foundAct = true;
+				// choose appropriate opentime: either wed, wkday or wk
+				// if none is given, use undefined opentimes
+				opentimes = ((ActivityFacilityImpl) facility).getActivityOptions().get(facilityActType).getOpeningTimes(DayType.wed);
+				if (opentimes == null) {
+					opentimes = ((ActivityFacilityImpl) facility).getActivityOptions().get(facilityActType).getOpeningTimes(DayType.wkday);
+				}
+				if (opentimes == null) {
+					opentimes = ((ActivityFacilityImpl) facility).getActivityOptions().get(facilityActType).getOpeningTimes(DayType.wk);
+				}
+				if (opentimes != null) {
+					// ignoring lunch breaks with the following procedure:
+					// if there is only one wed/wkday/wk open time interval, use it
+					// if there are two or more, use the earliest start time and the latest end time
+					openInterval[0] = Double.MAX_VALUE;
+					openInterval[1] = Double.MIN_VALUE;
+					for (OpeningTime opentime : opentimes) {
+						openInterval[0] = Math.min(openInterval[0], opentime.getStartTime());
+						openInterval[1] = Math.max(openInterval[1], opentime.getEndTime());
+					}
+				}
+			}
+		}
+		if (!foundAct) {
+			Gbl.errorMsg("No suitable facility activity type found. Aborting..." + act.getType());
+		}
+		return openInterval;
 	}
 }
