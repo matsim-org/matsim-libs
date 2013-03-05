b9de0c7b7d78790205099901ac95408404a94b5b
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2013-03-05 15:05:26 +0000

    do not plot joint trip evolution if they are not mutated
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@23297 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/java/playground/thibautd/socnetsim/run/RunUtils.java
+++ playgrounds/thibautd/src/main/java/playground/thibautd/socnetsim/run/RunUtils.java
@@ -136,11 +136,18 @@ public class RunUtils {
 					controller.getRegistry().getScenario(),
 					groupIdentifier));
 
-		controller.addControlerListener(
-				new JointTripsStats(
-					controller.getControlerIO(),
-					controller.getRegistry().getScenario(),
-					groupIdentifier));
+		final WeightsConfigGroup weights = (WeightsConfigGroup)
+			controller.getRegistry().getScenario().getConfig().getModule(
+					WeightsConfigGroup.GROUP_NAME );
+
+		// no need to track evolution of joint trips if they do not evolve
+		if ( weights.getJointTripMutationWeight() > 0 ) {
+			controller.addControlerListener(
+					new JointTripsStats(
+						controller.getControlerIO(),
+						controller.getRegistry().getScenario(),
+						groupIdentifier));
+		}
 
 		final CompositeStageActivityTypes actTypesForAnalysis = new CompositeStageActivityTypes();
 		actTypesForAnalysis.addActivityTypes(
