2b05773c2bc591b3622102c940f39c2eb009aefc
jbischoff <bischoff@vsp.tu-berlin.de>
2013-03-06 11:34:40 +0000

    implemented compatibility with non-electric vehicles
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@23299 a0517364-e873-4167-b1c8-416daa5a6b91

--- contribs/transEnergySim/src/main/java/org/matsim/contrib/transEnergySim/vehicles/api/BatteryElectricVehicle.java
+++ contribs/transEnergySim/src/main/java/org/matsim/contrib/transEnergySim/vehicles/api/BatteryElectricVehicle.java
@@ -48,9 +48,11 @@ public abstract class BatteryElectricVehicle extends AbstractVehicleWithBattery 
 	@Override
 	public void useBattery(double energyConsumptionInJoule) {
 		super.useBattery(energyConsumptionInJoule);
-
+//		System.out.println("battery: "+this.socInJoules/3600./1000.);
 		if (socInJoules < 0) {
 			didRunOutOfBattery = true;
+//			System.out.println("Vehicle out of battery");
+
 		}
 	}
 
