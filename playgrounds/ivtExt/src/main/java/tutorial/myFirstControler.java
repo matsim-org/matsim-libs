f7dbf9f9b34909882b2d5570827cbc989028df9f
michaz <michael.zilske@gmail.com>
2013-04-02 17:45:24 +0000

    putting in the config filename directly; i consider this a script; taking it as an arg is one level of indirection too much, i think
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@23474 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/ivtExt/src/main/java/tutorial/myFirstControler.java
+++ playgrounds/ivtExt/src/main/java/tutorial/myFirstControler.java
@@ -1,12 +1,16 @@
 package tutorial;
 
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 
-public class myFirstControler {
+public class MyFirstControler {
+	
 	public static void main(String[] args) {
-		String configFile = args[0] ;
-		Controler controler = new Controler( configFile ) ;
+		Config config = ConfigUtils.loadConfig("input/config.xml");
+		Controler controler = new Controler(config) ;
 		//controler.setOverwriteFiles(true) ;
 		controler.run() ;
 	}
+	
 }
