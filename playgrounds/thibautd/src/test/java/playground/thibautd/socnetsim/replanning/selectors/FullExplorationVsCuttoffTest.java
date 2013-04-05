a1f51a1683c4dc22510428f1f9ea457ec23b1295
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2013-04-05 09:53:16 +0000

    more test cliques and more diversity among them
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@23509 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/test/java/playground/thibautd/socnetsim/replanning/selectors/FullExplorationVsCuttoffTest.java
+++ playgrounds/thibautd/src/test/java/playground/thibautd/socnetsim/replanning/selectors/FullExplorationVsCuttoffTest.java
@@ -19,38 +19,29 @@
  * *********************************************************************** */
 package playground.thibautd.socnetsim.replanning.selectors;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-
-import java.util.ArrayList;
-import java.util.Collection;
+import org.junit.Assert;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Random;
 
 import org.apache.commons.lang.time.StopWatch;
 import org.apache.log4j.Logger;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.matsim.api.core.v01.Id;
-import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.PopulationFactory;
 import org.matsim.api.core.v01.population.Plan;
 import org.matsim.core.basic.v01.IdImpl;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.population.PersonImpl;
-import org.matsim.core.population.PlanImpl;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.misc.Counter;
 import org.matsim.testcases.MatsimTestUtils;
 
 import playground.thibautd.socnetsim.population.JointPlan;
 import playground.thibautd.socnetsim.population.JointPlans;
-import playground.thibautd.socnetsim.replanning.grouping.FixedGroupsIdentifier;
-import playground.thibautd.socnetsim.replanning.grouping.GroupIdentifier;
 import playground.thibautd.socnetsim.replanning.grouping.GroupPlans;
 import playground.thibautd.socnetsim.replanning.grouping.ReplanningGroup;
 import playground.thibautd.socnetsim.replanning.selectors.highestweightselection.FullExplorationSelector;
@@ -66,45 +57,6 @@ public class FullExplorationVsCuttoffTest {
 	public final MatsimTestUtils utils = new MatsimTestUtils();
 
 	@Test
-	@Ignore
-	public void testExistenceOfBlockingDoesNotDependOnOrder() throws Exception {
-		final Scenario sc = createTestScenario();
-		final HighestScoreSumSelector highestSelector = new HighestScoreSumSelector( true );
-		final LowestScoreSumSelectorForRemoval lowestSelector = new LowestScoreSumSelectorForRemoval( );
-
-		final JointPlans jointPlans = sc.getScenarioElement( JointPlans.class );
-		final GroupIdentifier cliquesIdentifier = sc.getScenarioElement( GroupIdentifier.class );
-
-		final Counter count = new Counter( "test group # " );
-		int c = 0;
-		for (ReplanningGroup clique : cliquesIdentifier.identifyGroups( sc.getPopulation() )) {
-			count.incCounter();
-			final GroupPlans highResult = highestSelector.selectPlans(
-					new EmptyIncompatiblePlansIdentifierFactory(),
-					jointPlans,
-					clique );
-			final GroupPlans lowResult = lowestSelector.selectPlans(
-					new EmptyIncompatiblePlansIdentifierFactory(),
-					jointPlans,
-					clique );
-
-			if (highResult == null || lowResult == null) {
-				c++;
-				assertNull(
-						"lowest selector finds no non-blocking comb. but highest finds! "+clique,
-						highResult);
-
-				assertNull(
-						"highest selector finds no non-blocking comb. but lowest finds! "+clique,
-						lowResult);
-			}
-
-		}
-		count.printCounter();
-		if ( c == 0 ) throw new RuntimeException( "nothing tested" );
-	}
-
-	@Test
 	public void testFullExplorationVsCuttoffNonBlocking() throws Exception {
 		log.info( "test: testFullExplorationVsCuttoffNonBlocking()" );
 		testFullExplorationVsCuttoff( false );
@@ -117,7 +69,6 @@ public class FullExplorationVsCuttoffTest {
 	}
 
 	private void testFullExplorationVsCuttoff(final boolean blocking) throws Exception {
-		final Scenario sc = createTestScenario();
 		final HighestScoreSumSelector fastSelector = new HighestScoreSumSelector( blocking );
 		final GroupLevelPlanSelector fullSelector =
 			new FullExplorationSelector(
@@ -132,12 +83,13 @@ public class FullExplorationVsCuttoffTest {
 						}
 					});
 
-		final JointPlans jointPlans = sc.getScenarioElement( JointPlans.class );
-		final GroupIdentifier cliquesIdentifier = sc.getScenarioElement( GroupIdentifier.class );
-
 		final Counter count = new Counter( "test plan # " );
 		final SelectionStopWatch stopWatch = new SelectionStopWatch();
-		for (ReplanningGroup clique : cliquesIdentifier.identifyGroups( sc.getPopulation() )) {
+		final JointPlans jointPlans = new JointPlans();
+		final Random random = new Random( 1234 );
+		int countNull = 0;
+		for ( int i=0; i < 20000; i++ ) {
+			final ReplanningGroup clique = createNextTestClique( jointPlans , random );
 			count.incCounter();
 			stopWatch.startFast();
 			final GroupPlans fastResult = fastSelector.selectPlans(
@@ -156,31 +108,28 @@ public class FullExplorationVsCuttoffTest {
 			final double scoreFull = calcScore( fullResult );
 
 			// allow different results, as long as the scores are identical
-			assertEquals(
-					"different scores with both exploration types."+
-					"          full: "+fullResult+
-					"          fast: "+fastResult+
+			Assert.assertEquals(
+					"different solutions with both exploration types."+
+					"          full score: "+scoreFull+
+					"          fast: "+scoreFast+
 					"          ",
-					scoreFull,
-					scoreFast,
-					MatsimTestUtils.EPSILON);
-
-			if ( !fastResult.equals( fullResult ) ) {
-				log.warn( "full result "+fullResult+" with score "+scoreFull+
-						" is not identical to fast result "+fastResult+" with score "+scoreFast );
-			}
+					fastResult,
+					fullResult);
+			if ( fastResult == null ) countNull++;
 		}
 		count.printCounter();
 
 		// print some stats on time needed
 		final double timeFast = stopWatch.endFastAndGetTime();
 		final double timeFull = stopWatch.endFullAndGetTime();
+		log.info( "no valid plan could be found for "+countNull+" groups" );
 		log.info( "full took "+timeFull+"ms" );
 		log.info( "fast took "+timeFast+"ms" );
-		log.info( "full took "+(timeFull / timeFast)+" times the time needed by fast" );
+		log.info( "full took "+(timeFull / timeFast)+" times the time needed by fast (with assertions enabled...)" );
 	}
 
 	private double calcScore(final GroupPlans plans) {
+		if ( plans == null ) return Double.NaN;
 		double score = 0;
 
 		for (Plan indivPlan : plans.getIndividualPlans()) {
@@ -196,55 +145,49 @@ public class FullExplorationVsCuttoffTest {
 		return score;
 	}
 
-	private Scenario createTestScenario() {
+	private ReplanningGroup createNextTestClique(
+			final JointPlans jointPlans,
+			final Random random) {
 		// attempt to get a high diversity of joint structures.
-		final int nCliques = 1500;
-		final int nMembers = 10;
-		final int nPlans = 10;
-		final int maxJointPlans = 60;
-		final double pJoin = 0.2;
-		final List<Collection<Id>> cliques = new ArrayList<Collection<Id>>();
-		final Scenario scenario = ScenarioUtils.createScenario( ConfigUtils.createConfig() );
-		final JointPlans jointPlans = new JointPlans();
+		final int nMembers = 1 + random.nextInt( 20 );
+		final int nPlans = 1 + random.nextInt( 10 );
+		// this is the max number of attempts to create a joint plan
+		final int maxJointPlans = 1000;
+		final double pJoin = random.nextDouble();
+		final ReplanningGroup group = new ReplanningGroup();
+		final PopulationFactory factory = ScenarioUtils.createScenario( ConfigUtils.createConfig() ).getPopulation().getFactory();
 
-		final Random random = new Random( 1234 );
 		int currentId = 0;
-		for (int i=0; i < nCliques; i++) {
-			final Map<Id, Queue<Plan>> plansPerPerson = new LinkedHashMap<Id, Queue<Plan>>();
-			final List<Id> currentClique = new ArrayList<Id>();
-			cliques.add( currentClique );
+		final Map<Id, Queue<Plan>> plansPerPerson = new LinkedHashMap<Id, Queue<Plan>>();
 
-			// create plans
-			for (int j=0; j < nMembers; j++) {
-				final Id id = new IdImpl( currentId++ );
-				final PersonImpl person = new PersonImpl( id );
-				scenario.getPopulation().addPerson( person );
-				for (int k=0; k < nPlans; k++) {
-					final Plan plan = new PlanImpl( person );
-					person.addPlan( plan );
-					plan.setScore( random.nextDouble() * 1000 );
-				}
-				plansPerPerson.put( id , new LinkedList<Plan>( person.getPlans() ) );
-				currentClique.add( id );
+		// create plans
+		for (int j=0; j < nMembers; j++) {
+			final Id id = new IdImpl( currentId++ );
+			final Person person = factory.createPerson( id );
+			group.addPerson( person );
+			for (int k=0; k < nPlans; k++) {
+				final Plan plan = factory.createPlan();
+				plan.setPerson( person );
+				person.addPlan( plan );
+				plan.setScore( random.nextDouble() * 1000 );
 			}
-
-			// join plans randomly
-			final int nJointPlans = random.nextInt( maxJointPlans );
-			for (int p=0; p < nJointPlans; p++) {
-				final Map<Id, Plan> jointPlan = new LinkedHashMap<Id, Plan>();
-				for (Queue<Plan> plans : plansPerPerson.values()) {
-					if ( random.nextDouble() > pJoin ) continue;
-					final Plan plan = plans.poll();
-					if (plan != null) jointPlan.put( plan.getPerson().getId() , plan );
-				}
-				if (jointPlan.size() <= 1) continue;
-				jointPlans.addJointPlan( jointPlans.getFactory().createJointPlan( jointPlan ) );
-			}
+			plansPerPerson.put( id , new LinkedList<Plan>( person.getPlans() ) );
 		}
 
-		scenario.addScenarioElement( jointPlans );
-		scenario.addScenarioElement( new FixedGroupsIdentifier( cliques ) );
-		return scenario;
+		// join plans randomly
+		final int nJointPlans = random.nextInt( maxJointPlans );
+		for (int p=0; p < nJointPlans; p++) {
+			final Map<Id, Plan> jointPlan = new LinkedHashMap<Id, Plan>();
+			for (Queue<Plan> plans : plansPerPerson.values()) {
+				if ( random.nextDouble() > pJoin ) continue;
+				final Plan plan = plans.poll();
+				if (plan != null) jointPlan.put( plan.getPerson().getId() , plan );
+			}
+			if (jointPlan.size() <= 1) continue;
+			jointPlans.addJointPlan( jointPlans.getFactory().createJointPlan( jointPlan ) );
+		}
+
+		return group;
 	}
 
 	private static class SelectionStopWatch {
