e57abf68d5de7a4f5d147b1e9cec5b5aecc25ee3
oblonski <oblonski@users.sf.net>
2013-04-05 15:37:48 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@23523 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/sschroeder/src/main/java/vehicles/VehicleTransportCostsPerLink.java
+++ playgrounds/sschroeder/src/main/java/vehicles/VehicleTransportCostsPerLink.java
@@ -3,6 +3,7 @@ package vehicles;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.contrib.freight.carrier.CarrierVehicleType;
+import org.matsim.contrib.freight.carrier.CarrierVehicleTypes;
 import org.matsim.core.router.util.TravelDisutility;
 import org.matsim.core.router.util.TravelTime;
 import org.matsim.vehicles.Vehicle;
