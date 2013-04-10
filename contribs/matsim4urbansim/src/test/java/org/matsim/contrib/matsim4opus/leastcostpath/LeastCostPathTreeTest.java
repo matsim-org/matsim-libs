8620e1768a5e7180fb21b746a56a3bb4808b85f9
tnicolai79 <tnicolai79@users.sf.net>
2013-04-10 16:29:45 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@23558 a0517364-e873-4167-b1c8-416daa5a6b91

--- contribs/matsim4urbansim/src/test/java/org/matsim/contrib/matsim4opus/leastcostpath/LeastCostPathTreeTest.java
+++ contribs/matsim4urbansim/src/test/java/org/matsim/contrib/matsim4opus/leastcostpath/LeastCostPathTreeTest.java
@@ -12,9 +12,9 @@ import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Node;
 import org.matsim.contrib.matsim4opus.matsim4urbansim.costcalculators.TravelDistanceCalculator;
 import org.matsim.contrib.matsim4opus.matsim4urbansim.costcalculators.TravelTimeCostCalculator;
+import org.matsim.contrib.matsim4opus.utils.CreateTestNetwork;
 import org.matsim.core.basic.v01.IdImpl;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.network.NetworkImpl;
 import org.matsim.core.router.util.TravelTime;
 import org.matsim.core.scenario.ScenarioImpl;
 import org.matsim.core.scenario.ScenarioUtils;
@@ -30,7 +30,8 @@ public class LeastCostPathTreeTest extends MatsimTestCase{
 	@Test
 	public void testRouteChoiceTestSpanningTree(){
 		this.scenario = (ScenarioImpl) ScenarioUtils.createScenario(ConfigUtils.createConfig());
-		createNetwork();
+		// createNetwork();
+		this.scenario.setNetwork( CreateTestNetwork.createTriangularNetwork() );
 		compareRouteChoices();
 	}
 	
@@ -122,40 +123,6 @@ public class LeastCostPathTreeTest extends MatsimTestCase{
 			
 		}
 		return false;
-	}
-	
-	/**
-	 * creating a test network
-	 * the path 1,2,4 has a total length of 1000m with a free speed travel time of 10m/s
-	 * the second path 1,3,4 has a total length of 100m but only a free speed travel time of 0.1m/s
-	 */
-	private void createNetwork() {
-		/*
-		 * 			(2)
-		 *         /   \
-		 *        /     \
-		 *(10m/s)/       \(10m/s)
-		 *(500m)/	      \(500m)
-		 *     /           \
-		 *    /             \
-		 *	 /               \
-		 *(1)-------(3)-------(4)
-		 *(50m,0.1m/s)(50m,0.1m/s) 			
-		 */
-		
-		NetworkImpl network = (NetworkImpl) this.scenario.getNetwork();
-		
-		// add nodes
-		Node node1 = network.createAndAddNode(new IdImpl(1), this.scenario.createCoord(0, 0));
-		Node node2 = network.createAndAddNode(new IdImpl(2), this.scenario.createCoord(50, 100));
-		Node node3 = network.createAndAddNode(new IdImpl(3), this.scenario.createCoord(50, 0));
-		Node node4 = network.createAndAddNode(new IdImpl(4), this.scenario.createCoord(100, 0));
-
-		// add links
-		network.createAndAddLink(new IdImpl(1), node1, node2, 500.0, 10.0, 3600.0, 1);
-		network.createAndAddLink(new IdImpl(2), node2, node4, 500.0, 10.0, 3600.0, 1);
-		network.createAndAddLink(new IdImpl(3), node1, node3, 50.0, 0.1, 3600.0, 1);
-		network.createAndAddLink(new IdImpl(4), node3, node4, 50.0, 0.1, 3600.0, 1);
 	}
 
 }
