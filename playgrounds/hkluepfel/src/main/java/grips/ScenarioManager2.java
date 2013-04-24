c921edb93fdd5dc95d1fdf6761bc02691ebd70f1
hubertkluepfel <hubertkluepfel@yahoo.de>
2013-04-24 13:52:07 +0000

    The first step. Created a ScenarioManager2 that extends playground.wdoering.scenariomanager.ScenarioManager.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@23748 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/hkluepfel/src/main/java/grips/ScenarioManager2.java
@@ -0,0 +1,17 @@
+package grips;
+
+import playground.wdoering.grips.scenariomanager.control.Controller;
+
+public class ScenarioManager2 extends playground.wdoering.grips.scenariomanager.ScenarioManager
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ScenarioManager2(Controller controller) {
+		super(controller);
+		// TODO Auto-generated constructor stub
+	}
+
+}
