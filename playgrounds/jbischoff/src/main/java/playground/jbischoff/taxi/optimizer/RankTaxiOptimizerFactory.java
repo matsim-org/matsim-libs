1086bd2c99aba84a0005d241c23878c8a0d2d0d2
michal-m <michal.mac@gmail.com>
2013-04-28 10:58:34 +0000

    switching to maven (util, gandalf, vrp) + refactorings of the taxi functionality (incl. changes for Joschka)
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@23803 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/jbischoff/src/main/java/playground/jbischoff/taxi/optimizer/RankTaxiOptimizerFactory.java
+++ playgrounds/jbischoff/src/main/java/playground/jbischoff/taxi/optimizer/RankTaxiOptimizerFactory.java
@@ -1,7 +1,6 @@
 package playground.jbischoff.taxi.optimizer;
 
 import pl.poznan.put.vrp.dynamic.data.VrpData;
-import pl.poznan.put.vrp.dynamic.optimizer.VrpOptimizerFactory;
 import pl.poznan.put.vrp.dynamic.optimizer.taxi.TaxiOptimizationPolicy;
 
 public interface RankTaxiOptimizerFactory {
