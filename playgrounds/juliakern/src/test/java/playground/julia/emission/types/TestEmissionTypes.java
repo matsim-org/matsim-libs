ecb34c3c995491b465167b23a7a6fd28aa8a1396
juliakern <juliakern@users.sf.net>
2013-04-30 10:52:22 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@23848 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/juliakern/src/test/java/playground/julia/emission/types/TestEmissionTypes.java
@@ -0,0 +1,19 @@
+package playground.julia.emission.types;
+
+public class TestEmissionTypes {
+	
+	/*
+	 * zu testen:
+	 * ColdPollutant
+	 * HbefaColdEmissionFactor
+	 * HbefaColdEmissionFactorKey
+	 * HbefaTrafficSituation
+	 * HbefaVehicleAttributes
+	 * wie kalt? HbefaWarmEmissionFactor
+	 * wie kalt? HbefaWarmEmissionFactorKey
+	 * wie kalt? WarmPollutant
+	 * 
+	 * */
+	
+
+}
