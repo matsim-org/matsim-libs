1688f01b2d5bf925acbf0203f5c2c32be7715a8c
vsp_nagel <2008@kainagel.org>
2013-04-30 21:16:45 +0000

    fuzzified the initial first activity end times between 6 and 8
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@23875 a0517364-e873-4167-b1c8-416daa5a6b91

--- contribs/matsim4urbansim/src/main/java/org/matsim/contrib/matsim4opus/utils/CreateHomeWorkHomePlan.java
+++ contribs/matsim4urbansim/src/main/java/org/matsim/contrib/matsim4opus/utils/CreateHomeWorkHomePlan.java
@@ -5,6 +5,7 @@ import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.contrib.matsim4opus.constants.InternalConstants;
 import org.matsim.core.api.experimental.facilities.ActivityFacility;
+import org.matsim.core.gbl.MatsimRandom;
 import org.matsim.core.population.ActivityImpl;
 import org.matsim.core.population.PlanImpl;
 
@@ -40,7 +41,12 @@ public class CreateHomeWorkHomePlan {
 		
 		// complete the first activity (home) by setting end time. 
 		ActivityImpl act = (ActivityImpl)plan.getFirstActivity();
-		act.setEndTime( 7.*3600. ) ; // tnicolai: make configurable: see actType1.setOpeningTime(7*3600)
+
+		act.setEndTime( (6.+2.*MatsimRandom.getRandom().nextDouble())*3600. ) ;
+		// (I just made this random between 6am and 8am to avoid the departure peak at 7.  In particular for pt users, it does not
+		// seem to go away. kai, apr'13)
+		// (tnicolai: make configurable: see actType1.setOpeningTime(7*3600))
+
 		// gather coordinate and facility id needed for last activity
 		Coord homeCoord = act.getCoord();
 		Id homeId = act.getFacilityId();
