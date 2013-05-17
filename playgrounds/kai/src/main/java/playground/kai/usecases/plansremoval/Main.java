6e98e60a69da3b079bfff8fa8acc70ed54b90f90
vsp_nagel <2008@kainagel.org>
2013-05-17 11:20:57 +0000

    adding plansremoval use case in kai playground
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@24152 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/kai/src/main/java/playground/kai/usecases/plansremoval/Main.java
@@ -0,0 +1,52 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2013 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+/**
+ * 
+ */
+package playground.kai.usecases.plansremoval;
+
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.replanning.selectors.PathSizeLogitSelector;
+import org.matsim.core.replanning.selectors.PlanSelector;
+
+/**
+ * @author nagel
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Controler ctrl = new Controler( args ) ;
+		
+		PlanSelector planSelector = new PathSizeLogitSelector( ctrl.getConfig().planCalcScore(), ctrl.getNetwork() );
+
+		ctrl.getStrategyManager().setPlanSelectorForRemoval(planSelector) ;
+		// yy should probably be replaced by a factory pattern
+		
+		ctrl.run();
+
+	}
+
+}
