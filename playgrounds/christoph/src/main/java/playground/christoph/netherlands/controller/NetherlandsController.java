9874dc8219e2450477dd50efa821f0955fd325de
vsp_nagel <2008@kainagel.org>
2013-05-24 07:40:36 +0000

    modifying playground.christoph...NetherlandsController so that it does not extend Controler any more
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@24266 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/christoph/src/main/java/playground/christoph/netherlands/controller/NetherlandsController.java
+++ playgrounds/christoph/src/main/java/playground/christoph/netherlands/controller/NetherlandsController.java
@@ -21,23 +21,22 @@
 package playground.christoph.netherlands.controller;
 
 import org.matsim.core.controler.Controler;
-import org.matsim.core.scoring.ScoringFunctionFactory;
 import org.matsim.core.scoring.functions.CharyparNagelOpenTimesScoringFunctionFactory;
 
-public class NetherlandsController extends Controler {
+public class NetherlandsController /*extends Controler*/ {
 
-	public NetherlandsController(final String[] args) {
-		super(args);
-	}
+//	public NetherlandsController(final String[] args) {
+//		super(args);
+//	}
 	
-	/*
-	 * We use a Scoring Function that get the Facility Opening Times from
-	 * the Facilities instead of the Config File.
-	 */
-	@Override
-	protected ScoringFunctionFactory loadScoringFunctionFactory() {
-		return new CharyparNagelOpenTimesScoringFunctionFactory(this.config.planCalcScore(), this.getScenario());
-	}
+//	/*
+//	 * We use a Scoring Function that get the Facility Opening Times from
+//	 * the Facilities instead of the Config File.
+//	 */
+//	@Override
+//	protected ScoringFunctionFactory loadScoringFunctionFactory() {
+//		return new CharyparNagelOpenTimesScoringFunctionFactory(this.config.planCalcScore(), this.getScenario());
+//	}
 
 	public static void main(final String[] args) {
 		if ((args == null) || (args.length == 0)) {
@@ -45,7 +44,16 @@ public class NetherlandsController extends Controler {
 			System.out.println("Usage: NetherlandsController config-file [dtd-file]");
 			System.out.println();
 		} else {
-			final Controler controler = new NetherlandsController(args);
+//			final Controler controler = new NetherlandsController(args);
+			final Controler controler = new Controler(args) ;
+
+			/*
+			 * We use a Scoring Function that get the Facility Opening Times from
+			 * the Facilities instead of the Config File.
+			 */
+			controler.setScoringFunctionFactory(new CharyparNagelOpenTimesScoringFunctionFactory(controler.getConfig().planCalcScore(), 
+					controler.getScenario()) ) ;
+
 			controler.run();
 		}
 		System.exit(0);
