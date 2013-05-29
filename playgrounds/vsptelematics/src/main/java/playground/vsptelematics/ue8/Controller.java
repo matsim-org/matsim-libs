da69a36027b2118f2ee021c9fcfbc3153fc7e8bd
grether <dominik.grether@gmail.com>
2013-05-29 21:34:45 +0000

    removing previous commits
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@24316 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/vsptelematics/src/main/java/playground/vsptelematics/ue8/Controller.java
+++ playgrounds/vsptelematics/src/main/java/playground/vsptelematics/ue8/Controller.java
@@ -40,7 +40,7 @@ import org.matsim.roadpricing.RoadPricingScheme;
 import org.matsim.roadpricing.RoadPricingSchemeImpl;
 
 import playground.vsptelematics.common.IncidentGenerator;
-import playground.vsptelematics.ub6.RouteTTObserver;
+import playground.vsptelematics.ha1.RouteTTObserver;
 
 
 /**
