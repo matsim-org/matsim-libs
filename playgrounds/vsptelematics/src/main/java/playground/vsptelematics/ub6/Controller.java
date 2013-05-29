5cb4f671adac918452349c43f1b1628b26ccdd62
grether <dominik.grether@gmail.com>
2013-05-29 21:33:29 +0000

    removing previous commits
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@24315 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/vsptelematics/src/main/java/playground/vsptelematics/ub6/Controller.java
+++ playgrounds/vsptelematics/src/main/java/playground/vsptelematics/ub6/Controller.java
@@ -23,8 +23,6 @@ import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.events.StartupEvent;
 import org.matsim.core.controler.listener.StartupListener;
 
-import playground.vsptelematics.common.IncidentGenerator;
-
 
 /**
  * @author dgrether
@@ -51,12 +49,6 @@ public class Controller {
 				final RouteTTObserver observer = new RouteTTObserver(con.getControlerIO().getOutputFilename("routeTravelTimes.txt"));
 				con.addControlerListener(observer);
 				con.getEvents().addHandler(observer);
-				
-				if (con.getScenario().getConfig().network().isTimeVariantNetwork()){
-					IncidentGenerator generator = new IncidentGenerator(con.getScenario().getConfig().getParam("telematics", "incidentsFile"), con.getScenario().getNetwork());
-					con.addControlerListener(generator);
-				}
-				
 			}});
 	}
 
