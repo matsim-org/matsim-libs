cb21d15ef092b52cf20e1dada088933fbc4aa304
sergio537 <sergio537@users.sf.net>
2013-06-04 07:05:38 +0000

    Test of the transit router
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@24401 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/sergioo/src/main/java/playground/sergioo/transitRouters2013/MainTR.java
+++ playgrounds/sergioo/src/main/java/playground/sergioo/transitRouters2013/MainTR.java
@@ -30,12 +30,13 @@ import org.matsim.pt.router.TransitRouterNetworkTravelTimeAndDisutility;
 import org.matsim.pt.transitSchedule.api.TransitScheduleReader;
 import org.matsim.vehicles.VehicleReaderV1;
 
-import playground.sergioo.singapore2012.transitRouterVariable.StopStopTimeCalculator2;
-import playground.sergioo.singapore2012.transitRouterVariable.TransitRouterNetworkTravelTimeAndDisutilityWS2;
-import playground.sergioo.singapore2012.transitRouterVariable.TransitRouterNetworkTravelTimeAndDisutilityWW2;
+import playground.sergioo.singapore2012.transitRouterVariable.StopStopTimeCalculator;
+import playground.sergioo.singapore2012.transitRouterVariable.TransitRouterNetworkTravelTimeAndDisutilityWS;
+import playground.sergioo.singapore2012.transitRouterVariable.TransitRouterNetworkTravelTimeAndDisutilityWW;
 import playground.sergioo.singapore2012.transitRouterVariable.TransitRouterNetworkWW;
 import playground.sergioo.singapore2012.transitRouterVariable.TransitRouterVariableImpl;
-import playground.sergioo.singapore2012.transitRouterVariable.WaitTimeCalculator2;
+import playground.sergioo.singapore2012.transitRouterVariable.WaitTimeStuckCalculator;
+import playground.sergioo.singapore2012.transitRouterVariable.WaitTimeCalculator;
 
 public class MainTR {
 
@@ -54,10 +55,12 @@ public class MainTR {
 		(new MatsimPopulationReader(scenario)).readFile(args[2]);
 		(new TransitScheduleReader(scenario)).readFile(args[3]);
 		(new VehicleReaderV1(((ScenarioImpl)scenario).getVehicles())).readFile(args[4]);
-		WaitTimeCalculator2 waitTimeCalculator = new WaitTimeCalculator2(scenario.getPopulation(), scenario.getTransitSchedule(), (int)binSize, (int) (endTime-startTime));
-		StopStopTimeCalculator2 stopStopTimeCalculator = new StopStopTimeCalculator2(scenario.getTransitSchedule(), ((ScenarioImpl)scenario).getVehicles(), (int)binSize, (int) (endTime-startTime));
+		WaitTimeCalculator waitTimeCalculator = new WaitTimeCalculator(scenario.getTransitSchedule(), (int)binSize, (int) (endTime-startTime));
+		WaitTimeStuckCalculator waitTimeCalculator2 = new WaitTimeStuckCalculator(scenario.getPopulation(), scenario.getTransitSchedule(), (int)binSize, (int) (endTime-startTime));
+		StopStopTimeCalculator stopStopTimeCalculator = new StopStopTimeCalculator(scenario.getTransitSchedule(), ((ScenarioImpl)scenario).getVehicles(), (int)binSize, (int) (endTime-startTime));
 		TravelTimeCalculator travelTimeCalculator = new TravelTimeCalculatorFactoryImpl().createTravelTimeCalculator(scenario.getNetwork(), scenario.getConfig().travelTimeCalculator());
 		EventsManager eventsManager = EventsUtils.createEventsManager(scenario.getConfig());
+		eventsManager.addHandler(waitTimeCalculator2);
 		eventsManager.addHandler(waitTimeCalculator);
 		eventsManager.addHandler(stopStopTimeCalculator);
 		eventsManager.addHandler(travelTimeCalculator);
@@ -68,11 +71,13 @@ public class MainTR {
 		TransitRouterNetwork network = TransitRouterNetwork.createFromSchedule(scenario.getTransitSchedule(), transitRouterConfig.beelineWalkConnectionDistance);
 		TransitRouterNetworkWW networkWW = TransitRouterNetworkWW.createFromSchedule(scenario.getNetwork(), scenario.getTransitSchedule(), transitRouterConfig.beelineWalkConnectionDistance);
 		TransitRouterNetworkTravelTimeAndDisutility travelFunction = new TransitRouterNetworkTravelTimeAndDisutility(transitRouterConfig, preparedTransitSchedule);
-		TransitRouterNetworkTravelTimeAndDisutilityWW2 travelFunctionWW = new TransitRouterNetworkTravelTimeAndDisutilityWW2(transitRouterConfig, scenario.getNetwork(), networkWW, travelTimeCalculator.getLinkTravelTimes(), waitTimeCalculator.getWaitTimes(), scenario.getConfig().travelTimeCalculator(), startTime, endTime, preparedTransitSchedule);
-		TransitRouterNetworkTravelTimeAndDisutilityWS2 travelFunctionWS = new TransitRouterNetworkTravelTimeAndDisutilityWS2(transitRouterConfig, scenario.getNetwork(), networkWW, waitTimeCalculator.getWaitTimes(), stopStopTimeCalculator.getStopStopTimes(), scenario.getConfig().travelTimeCalculator(), startTime, endTime, preparedTransitSchedule);
+		TransitRouterNetworkTravelTimeAndDisutilityWW travelFunctionWW = new TransitRouterNetworkTravelTimeAndDisutilityWW(transitRouterConfig, scenario.getNetwork(), networkWW, travelTimeCalculator.getLinkTravelTimes(), waitTimeCalculator.getWaitTimes(), scenario.getConfig().travelTimeCalculator(), startTime, endTime, preparedTransitSchedule);
+		TransitRouterNetworkTravelTimeAndDisutilityWS travelFunctionWS = new TransitRouterNetworkTravelTimeAndDisutilityWS(transitRouterConfig, scenario.getNetwork(), networkWW, waitTimeCalculator2.getWaitTimes(), stopStopTimeCalculator.getStopStopTimes(), scenario.getConfig().travelTimeCalculator(), startTime, endTime, preparedTransitSchedule);
+		TransitRouterNetworkTravelTimeAndDisutilityWW travelFunctionWW2 = new TransitRouterNetworkTravelTimeAndDisutilityWW(transitRouterConfig, scenario.getNetwork(), networkWW, travelTimeCalculator.getLinkTravelTimes(), waitTimeCalculator2.getWaitTimes(), scenario.getConfig().travelTimeCalculator(), startTime, endTime, preparedTransitSchedule);
 		TransitRouterImpl transitRouter = new TransitRouterImpl(transitRouterConfig, preparedTransitSchedule, network, travelFunction, travelFunction);
 		TransitRouterVariableImpl transitRouterWW = new TransitRouterVariableImpl(transitRouterConfig, travelFunctionWW, networkWW, scenario.getNetwork());
 		TransitRouterVariableImpl transitRouterWS = new TransitRouterVariableImpl(transitRouterConfig, travelFunctionWS, networkWW, scenario.getNetwork());
+		TransitRouterVariableImpl transitRouterWW2 = new TransitRouterVariableImpl(transitRouterConfig, travelFunctionWW2, networkWW, scenario.getNetwork());
 		List<Leg> path = null;
 		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("C:/Users/sergioo/workspace2/playgrounds/sergioo/data/routes.dat"));
 		Coord[] origin = (Coord[]) ois.readObject(), destination = (Coord[]) ois.readObject();
@@ -90,6 +95,10 @@ public class MainTR {
 		time = System.currentTimeMillis();
 		for(int i=0; i<numTests; i++)
 			path = transitRouterWW.calcRoute(origin[i], destination[i], dayTime[i], null);
+		System.out.println(System.currentTimeMillis()-time+" "+path.size());
+		time = System.currentTimeMillis();
+		for(int i=0; i<numTests; i++)
+			path = transitRouterWW2.calcRoute(origin[i], destination[i], dayTime[i], null);
 		System.out.println(System.currentTimeMillis()-time+" "+path.size());
 	}
 
