2c838c95740eb81ff99514dad6134192683f85b7
benkick <benkick@users.sf.net>
2013-06-07 08:19:19 +0000

    zone 30 runs now write out a shapefile with links where speed limits apply
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@24458 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/benjamin/src/main/java/playground/benjamin/scenarios/munich/RunMunich.java
+++ playgrounds/benjamin/src/main/java/playground/benjamin/scenarios/munich/RunMunich.java
@@ -21,24 +21,12 @@ package playground.benjamin.scenarios.munich;
 
 import java.util.Collection;
 
-import org.matsim.api.core.v01.Coord;
-import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
-import org.matsim.api.core.v01.network.Link;
-import org.matsim.api.core.v01.network.Network;
-import org.matsim.api.core.v01.network.Node;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
-import org.matsim.core.network.LinkImpl;
-import org.matsim.core.network.NetworkImpl;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.gis.ShapeFileReader;
-import org.matsim.utils.gis.matsim2esri.network.Links2ESRIShape;
 import org.opengis.feature.simple.SimpleFeature;
-
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.GeometryFactory;
 
 /**
  * @author benjamin
@@ -47,7 +35,9 @@ import com.vividsolutions.jts.geom.GeometryFactory;
 public class RunMunich {
 	
 	static String configFile = "../../runs-svn/detEval/test/input/config.xml";
-	static String zone30Shape = "../../detailedEval/policies/mobilTUM/zone30.shp";
+	static String zone30Path = "../../detailedEval/policies/mobilTUM/";
+	static String zone30Shape = zone30Path + "zone30.shp";
+	static String zone30Links = zone30Path + "zone30Links.shp";
 	static boolean considerZone30 = true;
 	static Collection<SimpleFeature> featuresInZone30;
 	
@@ -60,69 +50,11 @@ public class RunMunich {
 		controler.setCreateGraphs(true);
 
 		if(considerZone30){
-			featuresInZone30 = ShapeFileReader.getAllFeatures(zone30Shape);
-
-			Scenario sc = ScenarioUtils.createScenario(ConfigUtils.createConfig());
-			NetworkImpl zone30Links = (NetworkImpl) sc.getNetwork();
-
-			for(Link link : scenario.getNetwork().getLinks().values()){
-				Id linkId = link.getId();
-				LinkImpl ll = (LinkImpl) scenario.getNetwork().getLinks().get(linkId);
-				if(isLinkInShape(ll)){
-					Id fromId = ll.getFromNode().getId();
-					Id toId = ll.getToNode().getId();
-					Node from = null;
-					Node to = null;
-					Node nn;
-					//check if from node already exists
-					if (! zone30Links.getNodes().containsKey(fromId)) {
-						nn = scenario.getNetwork().getNodes().get(fromId);
-						from = addNode(zone30Links, nn);
-					}
-					else {
-						from = zone30Links.getNodes().get(fromId);
-					}
-					//check if to node already exists
-					if (! zone30Links.getNodes().containsKey(toId)){
-						nn = scenario.getNetwork().getNodes().get(toId);
-						to = addNode(zone30Links, nn);
-					}
-					else {
-						to = zone30Links.getNodes().get(toId);
-					}
-					Link lll = zone30Links.getFactory().createLink(ll.getId(), from, to);
-					lll.setAllowedModes(ll.getAllowedModes());
-					lll.setCapacity(ll.getCapacity());
-					lll.setFreespeed(ll.getFreespeed());
-					lll.setLength(ll.getLength());
-					lll.setNumberOfLanes(ll.getNumberOfLanes());
-					zone30Links.addLink(lll);
-				}
-			}
-			new Links2ESRIShape(zone30Links, "../../detailedEval/policies/mobilTUM/zone30Links.shp", "DHDN_GK4").write();
-//			controler.addControlerListener(new SetLinkAttributesControlerListener (featuresInZone30));
+			ShapeFileReader shapeFileReader = new ShapeFileReader();
+			shapeFileReader.readFileAndInitialize(zone30Shape);
+			featuresInZone30 = shapeFileReader.getFeatureSet();
+			controler.addControlerListener(new SetLinkAttributesControlerListener (featuresInZone30, zone30Links));
 		}
-//		controler.run();
+		controler.run();
 	}
-
-	private static Node addNode(Network net, Node n){
-		Node newNode = net.getFactory().createNode(n.getId(), n.getCoord());
-		net.addNode(newNode);
-		return newNode;
-	}
-
-	private static boolean isLinkInShape(Link link) {
-		boolean isInShape = false;
-		Coord coord = link.getCoord();
-		GeometryFactory factory = new GeometryFactory();
-		Geometry geo = factory.createPoint(new Coordinate(coord.getX(), coord.getY()));
-		for(SimpleFeature feature : featuresInZone30){
-			if(((Geometry) feature.getDefaultGeometry()).contains(geo)){
-				isInShape = true;
-				break;
-			}
-		}
-		return isInShape;
-	}
-
 }
--- playgrounds/benjamin/src/main/java/playground/benjamin/scenarios/munich/SetLinkAttributesControlerListener.java
+++ playgrounds/benjamin/src/main/java/playground/benjamin/scenarios/munich/SetLinkAttributesControlerListener.java
@@ -21,13 +21,20 @@ package playground.benjamin.scenarios.munich;
 
 import java.util.Collection;
 
+import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.network.Node;
+import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.events.StartupEvent;
 import org.matsim.core.controler.listener.StartupListener;
 import org.matsim.core.network.LinkImpl;
+import org.matsim.core.network.NetworkImpl;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.utils.gis.matsim2esri.network.Links2ESRIShape;
 import org.opengis.feature.simple.SimpleFeature;
 
 import com.vividsolutions.jts.geom.Coordinate;
@@ -39,39 +46,81 @@ import com.vividsolutions.jts.geom.GeometryFactory;
  *
  */
 public class SetLinkAttributesControlerListener implements StartupListener {
+	private static final Logger logger = Logger.getLogger(SetLinkAttributesControlerListener.class);
 
 	private final Collection<SimpleFeature> featuresInZone30;
+	private final String outputFile;
 
-	public SetLinkAttributesControlerListener(Collection<SimpleFeature> featuresInZone30) {
+	public SetLinkAttributesControlerListener(Collection<SimpleFeature> featuresInZone30, String outputFile) {
 		this.featuresInZone30 = featuresInZone30;
+		this.outputFile = outputFile;
 	}
 
 	@Override
 	public void notifyStartup(StartupEvent event) {
 		Network network = event.getControler().getNetwork();
 		
+		Scenario sc = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+		NetworkImpl zone30Links = (NetworkImpl) sc.getNetwork();
+
 		for(Link link : network.getLinks().values()){
 			Id linkId = link.getId();
 			LinkImpl ll = (LinkImpl) network.getLinks().get(linkId);
 			if(isLinkInShape(ll)){
-				System.out.println("Changing freespeed of link " + ll.getId() + " from " + ll.getFreespeed() + " to 8.3333333334.");
+				logger.info("Changing freespeed of link " + ll.getId() + " from " + ll.getFreespeed() + " to 8.3333333334.");
 				ll.setFreespeed(30 / 3.6);
 				if(ll.getNumberOfLanes() == 1){
-					System.out.println("Changing type of link " + ll.getId() + " from " + ll.getType() + " to 75.");
+					logger.info("Changing type of link " + ll.getId() + " from " + ll.getType() + " to 75.");
 					ll.setType("75");
-					System.out.println("Changing capacity of link " + ll.getId() + " from " + ll.getCapacity() + " to 11200.");
+					logger.info("Changing capacity of link " + ll.getId() + " from " + ll.getCapacity() + " to 11200.");
 					ll.setCapacity(11200);
 				}
 				else{
-					System.out.println("Changing type of link " + ll.getId() + " from " + ll.getType() + " to 83.");
+					logger.info("Changing type of link " + ll.getId() + " from " + ll.getType() + " to 83.");
 					ll.setType("83");
-					System.out.println("Changing capacity of link " + ll.getId() + " from " + ll.getCapacity() + " to 20000.");
+					logger.info("Changing capacity of link " + ll.getId() + " from " + ll.getCapacity() + " to 20000.");
 					ll.setCapacity(20000);
 				}
+
+				Id fromId = ll.getFromNode().getId();
+				Id toId = ll.getToNode().getId();
+				Node from = null;
+				Node to = null;
+				Node nn;
+				//check if from node already exists
+				if (! zone30Links.getNodes().containsKey(fromId)) {
+					nn = network.getNodes().get(fromId);
+					from = addNode(zone30Links, nn);
+				}
+				else {
+					from = zone30Links.getNodes().get(fromId);
+				}
+				//check if to node already exists
+				if (! zone30Links.getNodes().containsKey(toId)){
+					nn = network.getNodes().get(toId);
+					to = addNode(zone30Links, nn);
+				}
+				else {
+					to = zone30Links.getNodes().get(toId);
+				}
+				Link lll = zone30Links.getFactory().createLink(ll.getId(), from, to);
+				lll.setAllowedModes(ll.getAllowedModes());
+				lll.setCapacity(ll.getCapacity());
+				lll.setFreespeed(ll.getFreespeed());
+				lll.setLength(ll.getLength());
+				lll.setNumberOfLanes(ll.getNumberOfLanes());
+				zone30Links.addLink(lll);
 			}
 		}
+		new Links2ESRIShape(zone30Links, outputFile, "DHDN_GK4").write();
 	}
 
+	private Node addNode(Network net, Node n){
+		Node newNode = net.getFactory().createNode(n.getId(), n.getCoord());
+		net.addNode(newNode);
+		return newNode;
+	}
+	
 	private boolean isLinkInShape(Link link) {
 		boolean isInShape = false;
 		Coord coord = link.getCoord();
