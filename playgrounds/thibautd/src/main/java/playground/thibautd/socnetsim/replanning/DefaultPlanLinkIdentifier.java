2a1ee508f6ccdf19effadde87ed05bf64930e977
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2013-06-07 13:45:09 +0000

    enrich error msg
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@24476 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/java/playground/thibautd/socnetsim/replanning/DefaultPlanLinkIdentifier.java
+++ playgrounds/thibautd/src/main/java/playground/thibautd/socnetsim/replanning/DefaultPlanLinkIdentifier.java
@@ -56,7 +56,7 @@ public class DefaultPlanLinkIdentifier implements PlanLinkIdentifier {
 			final Plan p1,
 			final Plan p2) {
 		final boolean areLinked = containsCoTraveler( p1 , p2.getPerson().getId() );
-		assert areLinked == containsCoTraveler( p2 , p1.getPerson().getId() ) : "inconsistent plans";
+		assert areLinked == containsCoTraveler( p2 , p1.getPerson().getId() ) : "inconsistent plans "+p1+" and "+p2;
 		return areLinked;
 	}
 
