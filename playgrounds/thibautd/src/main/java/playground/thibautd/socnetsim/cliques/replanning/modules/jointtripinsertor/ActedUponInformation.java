30c3827993fca0e904a853e79db35093f55d9310
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2013-06-07 13:50:29 +0000

    remove commented out code
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@24482 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/java/playground/thibautd/socnetsim/cliques/replanning/modules/jointtripinsertor/ActedUponInformation.java
+++ playgrounds/thibautd/src/main/java/playground/thibautd/socnetsim/cliques/replanning/modules/jointtripinsertor/ActedUponInformation.java
@@ -4,22 +4,12 @@ import org.matsim.api.core.v01.Id;
 
 final class ActedUponInformation {
 	private final Id driverId, passengerId;
-	//private final Activity driverDeparture, driverArrival;
-	//private final Activity passengerDeparture, passengerArrival;
 
 	ActedUponInformation(
 			final Id driverId,
-			//final Activity driverDeparture,
-			//final Activity driverArrival,
 			final Id passengerId) {
-			//final Activity passengerDeparture,
-			//final Activity passengerArrival) {
 		this.driverId = driverId;
-		//this.driverDeparture = driverDeparture;
-		//this.driverArrival = driverArrival;
 		this.passengerId = passengerId;
-		//this.passengerDeparture = passengerDeparture;
-		//this.passengerArrival = passengerArrival;
 	}
 
 	public Id getDriverId() {
@@ -29,22 +19,6 @@ final class ActedUponInformation {
 	public Id getPassengerId() {
 		return passengerId;
 	}
-
-	//public Activity getDriverDeparture() {
-	//	return driverDeparture;
-	//}
-
-	//public Activity getDriverArrival() {
-	//	return driverArrival;
-	//}
-
-	//public Activity getPassengerDeparture() {
-	//	return passengerDeparture;
-	//}
-
-	//public Activity getPassengerArrival() {
-	//	return passengerArrival;
-	//}
 
 	@Override
 	public String toString() {
