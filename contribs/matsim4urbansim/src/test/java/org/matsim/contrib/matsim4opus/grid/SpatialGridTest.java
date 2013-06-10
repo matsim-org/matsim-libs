5cc31be5e27d0ee99dff994983461b8231ff5a56
tnicolai79 <tnicolai79@users.sf.net>
2013-06-10 15:02:45 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@24535 a0517364-e873-4167-b1c8-416daa5a6b91

--- contribs/matsim4urbansim/src/test/java/org/matsim/contrib/matsim4opus/grid/SpatialGridTest.java
+++ contribs/matsim4urbansim/src/test/java/org/matsim/contrib/matsim4opus/grid/SpatialGridTest.java
@@ -2,10 +2,7 @@ package org.matsim.contrib.matsim4opus.grid;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.matsim.api.core.v01.Id;
-import org.matsim.contrib.matsim4opus.gis.GridUtils;
 import org.matsim.contrib.matsim4opus.gis.SpatialGrid;
-import org.matsim.contrib.matsim4opus.gis.ZoneLayer;
 import org.matsim.contrib.matsim4opus.utils.CreateTestNetwork;
 import org.matsim.contrib.matsim4opus.utils.network.NetworkBoundaryBox;
 import org.matsim.core.network.NetworkImpl;
@@ -37,19 +34,5 @@ public class SpatialGridTest extends MatsimTestCase{
 		int cols = testGrid.getNumCols(0);
 		double numOfExpectedCols = ((nbb.getXMax() - nbb.getXMin()) / cellSize) + 1;
 		Assert.assertTrue(cols == numOfExpectedCols);
-	}
-
-	@Test
-	public void testCellCentroids() {
-		
-		// get network
-		NetworkImpl network = CreateTestNetwork.createTestNetwork();
-		
-		// get boundaries of network, i.e. x and y coordinates
-		NetworkBoundaryBox nbb = new NetworkBoundaryBox();
-		nbb.setDefaultBoundaryBox(network);
-
-		ZoneLayer<Id> measuringPoints = GridUtils.createGridLayerByGridSizeByNetwork(cellSize, nbb.getBoundingBox());
-	}
-	
+	}	
 }
