a26cfb4c62ca059683cef93e0659329b92143f53
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2013-06-18 14:09:41 +0000

    adapt rashid's scripts
    putting the two into one: enabling roadpricing only if the
    isUseRoadPricing flag is set to true in the config
    
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@24732 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/main/java/playground/wrashid/kti/KTIScenarioWithRoadPricing.java
+++ playgrounds/wrashid/src/main/java/playground/wrashid/kti/KTIScenarioWithRoadPricing.java
@@ -1,28 +1,38 @@
 package playground.wrashid.kti;
 
+import org.apache.log4j.Logger;
 
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryLogging;
+import org.matsim.core.router.StageActivityTypesImpl;
 import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.pt.PtConstants;
 import org.matsim.roadpricing.RoadPricing;
 
+import playground.ivt.kticompatibility.KtiLikeActivitiesScoringFunctionFactory;
+import playground.ivt.kticompatibility.KtiLikeScoringConfigGroup;
 import playground.ivt.kticompatibility.KtiPtConfigGroup;
 import playground.ivt.kticompatibility.KtiTripRouterFactory;
-import playground.meisterk.kti.controler.KTIControler;
 
 public class KTIScenarioWithRoadPricing {
+	private static final Logger log =
+		Logger.getLogger(KTIScenarioWithRoadPricing.class);
+
 	
 	public static void main(final String[] args) {
 		OutputDirectoryLogging.catchLogEntries();
 		final String configFile = args[ 0 ];
 
 		// read the config with our special parameters
-		// Note that you need 
+		// Note that you need scoring parameters converted
+		// from the KTI config by something like
+		// playground.thibautd.scripts.KtiToSimiliKtiConfig
 		final Config config = ConfigUtils.createConfig();
 		config.addModule( new KtiPtConfigGroup() );
+		config.addModule( new KtiLikeScoringConfigGroup() );
 		ConfigUtils.loadConfig( config , configFile );
 
 		// just make sure the scenario is loaded
@@ -34,10 +44,22 @@ public class KTIScenarioWithRoadPricing {
 		controler.setTripRouterFactory(
 				new KtiTripRouterFactory(
 					controler ) );
-		controler.addControlerListener(new RoadPricing());
+		controler.setScoringFunctionFactory(
+				new KtiLikeActivitiesScoringFunctionFactory(
+					new StageActivityTypesImpl( PtConstants.TRANSIT_ACTIVITY_TYPE ),
+					(KtiLikeScoringConfigGroup) config.getModule( KtiLikeScoringConfigGroup.GROUP_NAME ),
+					config.planCalcScore(),
+					scenario) );
+
+		if ( config.scenario().isUseRoadpricing() ) {
+			log.info( "adding the roadpricing listenner." );
+			controler.addControlerListener( new RoadPricing() );
+		}
+		else {
+			log.info( "NOT adding the roadpricing listenner." );
+		}
 
 		// we're done!
 		controler.run();
 	}
-	
 }
