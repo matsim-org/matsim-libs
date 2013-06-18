a967afc166db01cbf3039024de143d188a6c6e48
fuerbas <fuerbas@users.sf.net>
2013-06-18 12:12:48 +0000

    still some work to do
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@24711 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/vsp/src/main/java/playground/vsp/analysis/modules/ptLines2PaxAnalysis/TransitLines2PaxCounts.java
+++ playgrounds/vsp/src/main/java/playground/vsp/analysis/modules/ptLines2PaxAnalysis/TransitLines2PaxCounts.java
@@ -19,10 +19,14 @@
 
 package playground.vsp.analysis.modules.ptLines2PaxAnalysis;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Id;
 import org.matsim.core.basic.v01.IdImpl;
+import org.matsim.counts.Count;
 import org.matsim.counts.Counts;
+import org.matsim.counts.Volume;
 import org.matsim.pt.transitSchedule.api.TransitLine;
 import org.matsim.pt.transitSchedule.api.TransitRoute;
 import org.matsim.pt.transitSchedule.api.TransitRouteStop;
@@ -45,6 +49,8 @@ public class TransitLines2PaxCounts {
 	private Counts capacity;
 	private Counts totalPax;
 	private Counts occupancy;
+	private List<TransitRouteStop> longestRoute_a;
+	private List<TransitRouteStop> longestRoute_b;
 	
 	public TransitLines2PaxCounts (TransitLine tl, double countsInterval, int maxSlice) {
 		this.id = tl.getId();
@@ -56,7 +62,9 @@ public class TransitLines2PaxCounts {
 		this.interval = countsInterval;
 		this.maxSlice = maxSlice;
 		for (TransitRoute tr : tl.getRoutes().values()) {
-			for (int ii=0; ii < tr.getStops().size(); ii++) {
+			findLongestRoute(tr.getStops());
+			int numberOfStops = tr.getStops().size();
+			for (int ii=0; ii < numberOfStops; ii++) {
 				TransitRouteStop s = tr.getStops().get(ii);
 				Id stopFacilId = s.getStopFacility().getId();
 				if (this.boarding.getCounts().get(stopFacilId)==null) {
@@ -67,10 +75,152 @@ public class TransitLines2PaxCounts {
 					this.occupancy.createCount(stopFacilId, stopFacilId.toString());
 				}
 			}
+		}		
+	}
+	
+//	not sure this works
+	
+	public void findLongestRoute (List<TransitRouteStop> route_c) {
+		int length_a;
+		int length_b;
+		int length_c = route_c.size();
+		if (this.longestRoute_a!=null) {
+			length_a = this.longestRoute_a.size();
+		}
+		else { 
+			length_a=0;
+			this.longestRoute_a = route_c;
+			this.longestRoute_b = null;
+		}
+		if (this.longestRoute_b!=null) {
+			length_b = this.longestRoute_b.size();
+		}
+		else length_b=0;
+		if (length_c > length_a) {
+			if (route_c.containsAll(longestRoute_a)) {
+				this.longestRoute_a = route_c;
+			}
+			else if (this.longestRoute_b==null) {
+				this.longestRoute_b = route_c;
+			}
+		}
+		if (length_c > length_b) {
+			if (route_c.containsAll(longestRoute_b)) {
+				this.longestRoute_b = route_c;
+			}
 		}
 	}
 	
 	
+	public Id getId(){
+		return this.id;
+	}
+
+	/**
+	 * @param facilityId
+	 * @param time
+	 */
+	public void paxBoarding(Id facilityId, double time) {
+		increase(this.boarding, facilityId, time, 1.);
+	}
+
+	/**
+	 * @param facilityId
+	 * @param time
+	 */
+	public void paxAlighting(Id facilityId, double time) {
+		increase(this.alighting, facilityId, time, 1.);
+	}
+
+	/**
+	 * @param time
+	 * @param vehCapacity
+	 * @param nrSeatsInUse
+	 * @param stopIndexId
+	 */
+	public void vehicleDeparts(double time, double vehCapacity,	double nrSeatsInUse, Id stopIndexId) {
+		if(this.alighting.getCount(stopIndexId).getVolume(getTimeSlice(time)) == null){
+			set(this.alighting, stopIndexId, time, 0);
+		}
+		if(this.boarding.getCount(stopIndexId).getVolume(getTimeSlice(time)) == null){
+			set(this.boarding, stopIndexId, time, 0);
+		}
+		increase(this.capacity, stopIndexId, time, vehCapacity);
+		increase(this.totalPax, stopIndexId, time, nrSeatsInUse);
+		Integer slice = getTimeSlice(time);
+		set(this.occupancy, stopIndexId, time, this.totalPax.getCount(stopIndexId).getVolume(slice).getValue() /
+				this.capacity.getCount(stopIndexId).getVolume(slice).getValue());
+	}
 	
+	private void increase(Counts counts, Id stopId, Double time, double increaseBy){
+		Count count = counts.getCount(stopId);
+		Integer slice = getTimeSlice(time);
+		Volume v;
+		if(count.getVolumes().containsKey(slice)){
+			v = count.getVolume(slice);
+		}else{
+			v = count.createVolume(slice, 0);
+		}
+		v.setValue(v.getValue() + increaseBy);
+	}
+	
+	private void set(Counts counts, Id stopIndexId, Double time, double value){
+		Count count =  counts.getCount(stopIndexId);
+		Integer slice = getTimeSlice(time);
+		Volume v;
+		if(count.getVolumes().containsKey(slice)){
+			v = count.getVolume(slice);
+		}else{
+			v = count.createVolume(slice, 0);
+		}
+		v.setValue(value);
+	}
+	
+	private Integer getTimeSlice(double time){
+		int slice =  (int) (time / this.interval);
+		if(slice >= this.maxSlice){
+			return this.maxSlice;
+		}
+		return slice;
+	}
+
+	/**
+	 * @return the boarding
+	 */
+	public Counts getBoarding() {
+		return boarding;
+	}
+
+	/**
+	 * @return the alighting
+	 */
+	public Counts getAlighting() {
+		return alighting;
+	}
+
+	/**
+	 * @return the capacity
+	 */
+	public Counts getCapacity() {
+		return capacity;
+	}
+
+	/**
+	 * @return the totalPax
+	 */
+	public Counts getTotalPax() {
+		return totalPax;
+	}
+
+	/**
+	 * @return the occupancy
+	 */
+	public Counts getOccupancy() {
+		return occupancy;
+	}
+	
+	public Integer getMaxSlice(){
+		return this.maxSlice;
+	}
 
 }
