a59d3bbcbebf264c73f60c1412f8f2ee27f63a8b
kaddoura <kaddoura@users.sf.net>
2013-06-28 09:22:06 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@24859 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/ikaddoura/src/main/java/playground/ikaddoura/optimization/operatorProfitModel/LinksEventHandler.java
+++ playgrounds/ikaddoura/src/main/java/playground/ikaddoura/optimization/operatorProfitModel/LinksEventHandler.java
@@ -1,6 +1,5 @@
 /* *********************************************************************** *
  * project: org.matsim.*
- * LinksEventHandler.java
  *                                                                         *
  * *********************************************************************** *
  *                                                                         *
@@ -23,11 +22,6 @@
  */
 package playground.ikaddoura.optimization.operatorProfitModel;
 
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
-
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.core.api.experimental.events.LinkEnterEvent;
@@ -42,7 +36,6 @@ import org.matsim.core.api.experimental.events.handler.LinkLeaveEventHandler;
 public class LinksEventHandler implements LinkLeaveEventHandler, LinkEnterEventHandler {
 	private double vehicleKm;
 	private final Network network;
-//	private final Map<Id, List<Double>> linkId2linkLeaveTime = new HashMap<Id, List<Double>>();
 	
 	public LinksEventHandler(Network network) {
 		this.network = network;
@@ -61,19 +54,7 @@ public class LinksEventHandler implements LinkLeaveEventHandler, LinkEnterEventH
 		Id vehicleId = event.getVehicleId();
 		if (vehicleId.toString().contains("bus")){
 			// vehicleKm
-			this.vehicleKm = this.vehicleKm + network.getLinks().get(event.getLinkId()).getLength()/1000;
-			
-//			// takt
-//			if (this.linkId2linkLeaveTime.containsKey(event.getLinkId())){
-//				List<Double> depTimes = this.linkId2linkLeaveTime.get(event.getLinkId());
-//				depTimes.add(event.getTime());
-//				this.linkId2linkLeaveTime.put(event.getLinkId(), depTimes);
-//			}
-//			else {
-//				List<Double> depTimes = new ArrayList<Double>();
-//				depTimes.add(event.getTime());
-//				this.linkId2linkLeaveTime.put(event.getLinkId(), depTimes);
-//			}
+			this.vehicleKm = this.vehicleKm + network.getLinks().get(event.getLinkId()).getLength() / 1000;
 		}
 		else {}		
 	}
@@ -81,48 +62,5 @@ public class LinksEventHandler implements LinkLeaveEventHandler, LinkEnterEventH
 	public double getVehicleKm() {
 		return this.vehicleKm;
 	}
-
-//	public void setTakt(Map<Integer, TimePeriod> day) {		
-//		for (TimePeriod period : day.values()){
-//			Map<Id, List<Double>> linkId2diff = new HashMap<Id, List<Double>>();
-//			double fromTime = period.getFromTime();
-//			double toTime = period.getToTime();
-//			for (Id linkId : this.linkId2linkLeaveTime.keySet()){
-//				List<Double> depTimes = this.linkId2linkLeaveTime.get(linkId);
-//				double depTimeBefore = 0;
-//				List<Double> diffs = new ArrayList<Double>();
-//				
-//				for (Double depTime : depTimes){
-//					if (depTime >= fromTime && depTime < toTime){
-//						if (depTimeBefore > 0){
-//							double diff = depTime - depTimeBefore;
-//							diffs.add(diff);
-//						}
-//						depTimeBefore = depTime;
-//					}
-//				}
-//				linkId2diff.put(linkId, diffs);
-//			}
-//			
-//			Map<Id, Double> linkId2takt = new HashMap<Id, Double>();
-//			for(Id linkId : linkId2diff.keySet()){
-//				double diffSum = 0;
-//				List<Double> diffs = linkId2diff.get(linkId);
-//				for (Double diff : diffs){
-//					diffSum = diffSum + diff;
-//				}
-//				double linkTakt = diffSum/(double)diffs.size();
-//				linkId2takt.put(linkId, linkTakt);
-//			}
-//			
-//			double linkTaktSum = 0;
-//			for (Double linkTakt : linkId2takt.values()){
-//				linkTaktSum = linkTaktSum + linkTakt;
-//			}
-//	
-//			double takt = linkTaktSum/linkId2takt.size();
-//			period.setAverageTaktFromEvents(takt);
-//		}
-//	}
 
 }
