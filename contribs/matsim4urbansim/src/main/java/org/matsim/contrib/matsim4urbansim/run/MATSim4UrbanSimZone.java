3179f22e5e2b180ff263bedf28845d4e7822efd5
dan-ro <daniel_roeder@web.de>
2013-07-03 11:13:05 +0000

    removed network-cut for brussels
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@24899 a0517364-e873-4167-b1c8-416daa5a6b91

--- contribs/matsim4urbansim/src/main/java/org/matsim/contrib/matsim4urbansim/run/MATSim4UrbanSimZone.java
+++ contribs/matsim4urbansim/src/main/java/org/matsim/contrib/matsim4urbansim/run/MATSim4UrbanSimZone.java
@@ -36,7 +36,6 @@ import org.matsim.contrib.analysis.kai.KaiAnalysisListener;
 import org.matsim.contrib.matsim4urbansim.analysis.DanielAnalysisListenerEvents;
 import org.matsim.core.api.experimental.facilities.ActivityFacilities;
 import org.matsim.core.controler.Controler;
-import org.matsim.core.controler.listener.ControlerListener;
 import org.matsim.core.network.algorithms.NetworkScenarioCut;
 import org.matsim.core.utils.collections.Tuple;
 import org.matsim.core.utils.geometry.CoordImpl;
@@ -94,46 +93,45 @@ public class MATSim4UrbanSimZone extends MATSim4UrbanSimParcel{
 		isParcelMode = false;
 	}
 	
-	@Override
-	void modifyNetwork( Network net ) {
-
-		if ( BRUSSELS_SCENARIO_MODIFY_NETWORK ) {
-
-			log.error("cutting the Brussels network.  It is really really dangerous to leave this in the code as such.  kai, apr'13") ;
-			Coord min = new CoordImpl(94902.,112575.) ;
-			Coord max = new CoordImpl(220907.,220000.) ;
-			NetworkScenarioCut nsc = new NetworkScenarioCut(min, max) ;
-			nsc.run(net) ;
-
-			log.error("modifying the Brussels network.  It is really really dangerous to leave this in the code as such.  kai, apr'13") ;
-			long cnt = 0 ;
-			for ( Link link : net.getLinks().values() ) {
-				double spd = link.getFreespeed() ;
-				double cap = link.getCapacity() ;
-				// the free speeds do not seem very reliable (frontage roads sometimes have faster speeds than main roads) 
-				if ( cap <= 1000. ) {
-					// something like a one-lane local street
-					link.setFreespeed(15./3.6) ;
-					cnt++ ;
-				} else if ( cap <= 2000. && spd < 61./3.6 ) {
-					// something like a two-lane slow arterial
-					link.setFreespeed( 20./3.6 ) ;
-					cnt++ ;
-				} else if ( cap <= 3000. && spd < 61/3.6 ) {
-					// something like a three-lane boulevard
-					link.setFreespeed( 25./3.6 ) ;
-					cnt++ ;
-				} else if ( cap <= 3500. && spd < 81/3.6 ) {
-					// something like a narrow two-lane tunnel
-					link.setFreespeed( 60./3.6 ) ;
-					cnt++ ;
-				}
-			}
-			log.warn("modified the free speed on " + cnt + " of " + net.getLinks().size() + " links.") ;
-			
-		}
-
-	}
+//	@Override
+//	void modifyNetwork( Network net ) {
+//
+//		if ( BRUSSELS_SCENARIO_MODIFY_NETWORK ) {
+//
+//			log.error("cutting the Brussels network.  It is really really dangerous to leave this in the code as such.  kai, apr'13") ;
+//			Coord min = new CoordImpl(94902.,112575.) ;
+//			Coord max = new CoordImpl(220907.,220000.) ;
+//			NetworkScenarioCut nsc = new NetworkScenarioCut(min, max) ;
+//			nsc.run(net) ;
+//
+//			log.error("modifying the Brussels network.  It is really really dangerous to leave this in the code as such.  kai, apr'13") ;
+//			long cnt = 0 ;
+//			for ( Link link : net.getLinks().values() ) {
+//				double spd = link.getFreespeed() ;
+//				double cap = link.getCapacity() ;
+//				// the free speeds do not seem very reliable (frontage roads sometimes have faster speeds than main roads) 
+//				if ( cap <= 1000. ) {
+//					// something like a one-lane local street
+//					link.setFreespeed(15./3.6) ;
+//					cnt++ ;
+//				} else if ( cap <= 2000. && spd < 61./3.6 ) {
+//					// something like a two-lane slow arterial
+//					link.setFreespeed( 20./3.6 ) ;
+//					cnt++ ;
+//				} else if ( cap <= 3000. && spd < 61/3.6 ) {
+//					// something like a three-lane boulevard
+//					link.setFreespeed( 25./3.6 ) ;
+//					cnt++ ;
+//				} else if ( cap <= 3500. && spd < 81/3.6 ) {
+//					// something like a narrow two-lane tunnel
+//					link.setFreespeed( 60./3.6 ) ;
+//					cnt++ ;
+//				}
+//			}
+//			log.warn("modified the free speed on " + cnt + " of " + net.getLinks().size() + " links.") ;
+//		}
+//
+//	}
 	
 	@Override
 	void addFurtherControlerListener(ActivityFacilities zones, ActivityFacilities parcels, Controler controler) {
