6534aed52fa4b8cb08a3072d741a0bc3581de36e
rashid_waraich <rashid_waraich@users.sf.net>
2013-07-10 21:22:06 +0000

    function added for writing to file, if ev ran out of battery
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@25040 a0517364-e873-4167-b1c8-416daa5a6b91

--- contribs/transEnergySim/src/main/java/org/matsim/contrib/transEnergySim/vehicles/VehicleUtils.java
+++ contribs/transEnergySim/src/main/java/org/matsim/contrib/transEnergySim/vehicles/VehicleUtils.java
@@ -19,33 +19,54 @@
 
 package org.matsim.contrib.transEnergySim.vehicles;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.matsim.api.core.v01.Id;
+import org.matsim.contrib.parking.lib.GeneralLib;
+import org.matsim.contrib.transEnergySim.analysis.energyConsumption.EnergyConsumptionLogRow;
 import org.matsim.contrib.transEnergySim.vehicles.api.BatteryElectricVehicle;
 import org.matsim.contrib.transEnergySim.vehicles.api.Vehicle;
 
 public class VehicleUtils {
 
 	/**
-	 * Prints all electric vehicles (the drivers agentId), which ran out of battery.
+	 * Prints all electric vehicles (the drivers agentId), which ran out of
+	 * battery.
+	 * 
 	 * @param vehicles
 	 */
-	public static void printToConsoleVehiclesWhichRanOutOfBattery(HashMap<Id, Vehicle> vehicles){
+	public static void printToConsoleVehiclesWhichRanOutOfBattery(
+			HashMap<Id, Vehicle> vehicles) {
 		System.out.println("agentId");
-		
-		
-		for (Id personId:vehicles.keySet()){
-			Vehicle vehicle=vehicles.get(personId);
-			
-			if (vehicle instanceof BatteryElectricVehicle){
-				BatteryElectricVehicle bev=(BatteryElectricVehicle) vehicle;
-				
-				if (bev.didVehicleRunOutOfBattery()){
+
+		for (Id personId : vehicles.keySet()) {
+			Vehicle vehicle = vehicles.get(personId);
+
+			if (vehicle instanceof BatteryElectricVehicle) {
+				BatteryElectricVehicle bev = (BatteryElectricVehicle) vehicle;
+
+				if (bev.didVehicleRunOutOfBattery()) {
 					System.out.println(personId);
 				}
 			}
 		}
 	}
-	
+
+	public static void writeToFile(HashMap<Id, Vehicle> vehicles,
+			String outputFile) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("agentId");
+		for (Id personId : vehicles.keySet()) {
+			Vehicle vehicle = vehicles.get(personId);
+			if (vehicle instanceof BatteryElectricVehicle) {
+				BatteryElectricVehicle bev = (BatteryElectricVehicle) vehicle;
+				if (bev.didVehicleRunOutOfBattery()) {
+					list.add(personId.toString());
+				}
+			}
+		}
+		GeneralLib.writeList(list, outputFile);
+	}
+
 }
