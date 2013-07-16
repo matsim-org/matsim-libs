365d2b2611fd207ee75d4d9a1403d424b02e650f
juliakern <juliakern@users.sf.net>
2013-07-16 15:01:35 +0000

    not done yet
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@25135 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/juliakern/src/test/java/playground/julia/scenarios/munich/analysis/nectar/TestEmissionsPerLinkWarmEventHandler.java
+++ playgrounds/juliakern/src/test/java/playground/julia/scenarios/munich/analysis/nectar/TestEmissionsPerLinkWarmEventHandler.java
@@ -11,7 +11,6 @@ import org.matsim.core.basic.v01.IdImpl;
 import org.matsim.testcases.MatsimTestUtils;
 
 import playground.julia.emissions.EmissionsPerLinkWarmEventHandler;
-//import playground.julia.scenarios.munich.analysis.nectar.*;
 import playground.vsp.emissions.events.WarmEmissionEvent;
 import playground.vsp.emissions.events.WarmEmissionEventImpl;
 import playground.vsp.emissions.types.WarmPollutant;
@@ -22,22 +21,25 @@ public class TestEmissionsPerLinkWarmEventHandler {
 	 * @param args
 	 */
 
+	boolean excep = false;		
+	Double coValue = 2000., c2Value = 220. , fcValue = 22.2, hcValue = 2.222, nmValue= 0.22222, 
+				n2Value = 3000., nxValue = 330., pmValue = 33.3, soValue = 3.333;
 	
 	@Test
 	public void handleEventTest(){
 		
-		double simulationEndTime = 200.;
+		double simulationEndTime = 200., simulationStartTime=0.0;
 		int noOfTimeBins = 30; // 0.0, 6.666, 13.333, 20.0, ...
 		EmissionsPerLinkWarmEventHandler handler = new EmissionsPerLinkWarmEventHandler(simulationEndTime, noOfTimeBins);
 		
-		// event 1
-		// time inside one time intervall, all possible values for pollutants set
+		// initialize event 1
+		// time inside one time interval, all possible values for pollutants set
 		double time = 5.0;
-		double endOfTimeIntervall = simulationEndTime/noOfTimeBins*1;
+		double endOfTimeInterval = simulationEndTime/noOfTimeBins*1;
+		endOfTimeInterval = Math.round(endOfTimeInterval);
 		Id linkId = new IdImpl("link1");
 		Id vehicleId = new IdImpl("veh 1");
-		Double coValue = 2000., c2Value = 220. , fcValue = 22.2, hcValue = 2.222, nmValue= 0.22222, 
-				n2Value = 3000., nxValue = 330., pmValue = 33.3, soValue = 3.333;
+
 		Map<WarmPollutant, Double> warmEmissions = new HashMap<WarmPollutant, Double>();
 		warmEmissions.put(WarmPollutant.CO, coValue);
 		warmEmissions.put(WarmPollutant.CO2_TOTAL, c2Value);
@@ -51,50 +53,122 @@ public class TestEmissionsPerLinkWarmEventHandler {
 		
 		WarmEmissionEvent event1 = new WarmEmissionEventImpl(time, linkId, vehicleId, warmEmissions);
 		handler.handleEvent(event1);
-		// test event 1
-		Assert.assertEquals(1.0, handler.getTime2linkIdLeaveCount().get(endOfTimeIntervall).get(linkId));
-		Assert.assertEquals(coValue, handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeIntervall).get(linkId).get(WarmPollutant.CO), MatsimTestUtils.EPSILON);
-		Assert.assertEquals(c2Value, handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeIntervall).get(linkId).get(WarmPollutant.CO2_TOTAL), MatsimTestUtils.EPSILON);
-		Assert.assertEquals(fcValue, handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeIntervall).get(linkId).get(WarmPollutant.FC), MatsimTestUtils.EPSILON);
-		Assert.assertEquals(hcValue, handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeIntervall).get(linkId).get(WarmPollutant.HC), MatsimTestUtils.EPSILON);
-		Assert.assertEquals(nmValue, handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeIntervall).get(linkId).get(WarmPollutant.NMHC), MatsimTestUtils.EPSILON);
-		Assert.assertEquals(n2Value, handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeIntervall).get(linkId).get(WarmPollutant.NO2), MatsimTestUtils.EPSILON);
-		Assert.assertEquals(nxValue, handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeIntervall).get(linkId).get(WarmPollutant.NOX), MatsimTestUtils.EPSILON);
-		Assert.assertEquals(pmValue, handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeIntervall).get(linkId).get(WarmPollutant.PM), MatsimTestUtils.EPSILON);
-		Assert.assertEquals(soValue, handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeIntervall).get(linkId).get(WarmPollutant.SO2), MatsimTestUtils.EPSILON);
-		// everything else zero or null
-		Assert.assertEquals(0.0, handler.getWarmEmissionsPerLinkAndTimeInterval().get(0.0).get(linkId).get(WarmPollutant.CO2_TOTAL), MatsimTestUtils.EPSILON);
-		Assert.assertEquals(0.0, handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeIntervall).get(new IdImpl("unused link")).get(WarmPollutant.CO2_TOTAL), MatsimTestUtils.EPSILON);
-		Assert.assertNull(handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeIntervall).get(linkId).get("some pollutant")); //TODO exception?
-		Assert.assertEquals(0.0, handler.getTime2linkIdLeaveCount().get(0.0).get(linkId));
-		Assert.assertNull(handler.getTime2linkIdLeaveCount().get(endOfTimeIntervall).get(new IdImpl("unused link")));
 		
-		// TODO go on here
+		// test event 1 
+		Assert.assertEquals(1.0, handler.getTime2linkIdLeaveCount().get(endOfTimeInterval).get(linkId));
+		Assert.assertEquals(coValue, handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeInterval).get(linkId).get(WarmPollutant.CO), MatsimTestUtils.EPSILON);
+		Assert.assertEquals(c2Value, handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeInterval).get(linkId).get(WarmPollutant.CO2_TOTAL), MatsimTestUtils.EPSILON);
+		Assert.assertEquals(fcValue, handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeInterval).get(linkId).get(WarmPollutant.FC), MatsimTestUtils.EPSILON);
+		Assert.assertEquals(hcValue, handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeInterval).get(linkId).get(WarmPollutant.HC), MatsimTestUtils.EPSILON);
+		Assert.assertEquals(nmValue, handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeInterval).get(linkId).get(WarmPollutant.NMHC), MatsimTestUtils.EPSILON);
+		Assert.assertEquals(n2Value, handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeInterval).get(linkId).get(WarmPollutant.NO2), MatsimTestUtils.EPSILON);
+		Assert.assertEquals(nxValue, handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeInterval).get(linkId).get(WarmPollutant.NOX), MatsimTestUtils.EPSILON);
+		Assert.assertEquals(pmValue, handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeInterval).get(linkId).get(WarmPollutant.PM), MatsimTestUtils.EPSILON);
+		Assert.assertEquals(soValue, handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeInterval).get(linkId).get(WarmPollutant.SO2), MatsimTestUtils.EPSILON);
 		
-		WarmEmissionEvent event2 = new WarmEmissionEventImpl(0.0, linkId, vehicleId, warmEmissions);
+		// everything else should be zero or null or throw an exception
+		// es gibt keinen Eintrag unter dieser LinkId .... das soll vermutlich auch so sein. 
+		// ansonsten muesste fuer jedes Zeitintervall fuer jeden link eine leere map hinterlegt werden
+		try{
+			handler.getWarmEmissionsPerLinkAndTimeInterval().get(0.0).get(linkId);
+		}catch(NullPointerException e){
+			excep = true;
+		}
+		Assert.assertTrue(excep); excep = false;
+		
+		try{
+			handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeInterval).get(new IdImpl("unused link")).get(WarmPollutant.CO2_TOTAL);
+		}catch(NullPointerException e){
+			excep = true;
+		}
+		Assert.assertTrue(excep); excep = false;
+		Assert.assertNull(handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeInterval).get(linkId).get("some pollutant"));
+		
+		try{
+			handler.getTime2linkIdLeaveCount().get(0.0).get(linkId);
+		}catch(NullPointerException e){
+			excep=true;
+		}
+		Assert.assertTrue(excep); excep = false;
+		
+		Assert.assertNull(handler.getTime2linkIdLeaveCount().get(endOfTimeInterval).get(new IdImpl("unused link")));
+		
+		// initialize event 2,3,4 and handle them
+		// event belongs to the same time interval as event 1! 
+		WarmEmissionEvent event2 = new WarmEmissionEventImpl(simulationStartTime, linkId, vehicleId, warmEmissions);
 		handler.handleEvent(event2);
 		WarmEmissionEvent event3 = new WarmEmissionEventImpl(simulationEndTime, linkId, vehicleId, warmEmissions);
 		handler.handleEvent(event3);
-		WarmEmissionEvent event4 = new WarmEmissionEventImpl(simulationEndTime/noOfTimeBins*2, linkId, vehicleId, warmEmissions);
+		
+		double timeOfEvent4 = simulationEndTime/noOfTimeBins*14.3;
+		double intervalEndOfEvent4 = -1.0;
+		double timebinsize = simulationEndTime/noOfTimeBins;
+		for(int i=0; i<noOfTimeBins; i++){
+			if(i*timebinsize<timeOfEvent4 && (i+1)*timebinsize>=timeOfEvent4){
+				intervalEndOfEvent4 = Math.round((i+1)*timebinsize);
+				break;
+			}
+		}
+		
+		Assert.assertTrue(intervalEndOfEvent4>-1.);
+		WarmEmissionEvent event4 = new WarmEmissionEventImpl(timeOfEvent4, linkId, vehicleId, warmEmissions);
+		
+		
 		handler.handleEvent(event4);
+		//intervalEndOfEvent4 = 100.00000000000001;
+		// check time2linkIdLeaveCount
+		Assert.assertEquals(2.0, handler.getTime2linkIdLeaveCount().get(endOfTimeInterval).get(linkId));
+		Assert.assertEquals(1.0, handler.getTime2linkIdLeaveCount().get(simulationEndTime).get(linkId)); 
+		Assert.assertEquals(1.0, handler.getTime2linkIdLeaveCount().get(intervalEndOfEvent4).get(linkId));
+		// handler.getTime2linkIdLeaveCount should contain nothing else
+		Assert.assertEquals(3, handler.getTime2linkIdLeaveCount().size());
+		
+		// check warmEmissionsPerLinkAndTimeInterval
+		Assert.assertEquals(2*coValue, handler.getWarmEmissionsPerLinkAndTimeInterval().get(endOfTimeInterval).get(linkId).get(WarmPollutant.CO), MatsimTestUtils.EPSILON);
+		Assert.assertEquals(coValue, handler.getWarmEmissionsPerLinkAndTimeInterval().get(simulationEndTime).get(linkId).get(WarmPollutant.CO), MatsimTestUtils.EPSILON);
+		Assert.assertEquals(coValue, handler.getWarmEmissionsPerLinkAndTimeInterval().get(intervalEndOfEvent4).get(linkId).get(WarmPollutant.CO), MatsimTestUtils.EPSILON);
+		
 		
 		// ins richtige intervall?
 		//ins falsche intervall?
 		
-		WarmEmissionEvent event5 = new WarmEmissionEventImpl(endOfTimeIntervall, linkId, vehicleId, null);
+		double timeOfEvent5 = simulationEndTime/noOfTimeBins*14.3;
+		double intervalEndOfEvent5 = -1.0;
+		Id link2Id = new IdImpl("link 2");
+		for(int i=0; i<noOfTimeBins; i++){
+			if(i*timebinsize<timeOfEvent5 && (i+1)*timebinsize>=timeOfEvent5){
+				intervalEndOfEvent5 = (i+1)*timebinsize;
+				break;
+			}
+		}
+		WarmEmissionEvent event5 = new WarmEmissionEventImpl(timeOfEvent5, link2Id, vehicleId, null);
 		handler.handleEvent(event5);
+		Assert.assertEquals(1.0, handler.getTime2linkIdLeaveCount().get(intervalEndOfEvent5).get(link2Id));
+		Assert.assertEquals(0.0, handler.getWarmEmissionsPerLinkAndTimeInterval().get(intervalEndOfEvent5).get(link2Id).get(WarmPollutant.NMHC));
 		
+		//event 6 contains come pollutants but not all
 		warmEmissions.remove(WarmPollutant.NMHC);
 		warmEmissions.remove(WarmPollutant.NO2);
 		warmEmissions.remove(WarmPollutant.NOX);
-		WarmEmissionEvent event6 = new WarmEmissionEventImpl(endOfTimeIntervall, linkId, vehicleId, warmEmissions);
+		Id linkId3 = new IdImpl("link 3");
+		WarmEmissionEvent event6 = new WarmEmissionEventImpl(simulationEndTime, linkId3, vehicleId, warmEmissions);
 		handler.handleEvent(event6);
-		//assert
+		// check event 6
+		Assert.assertEquals(1.0, handler.getTime2linkIdLeaveCount().get(simulationEndTime).get(linkId));
+		Assert.assertEquals(coValue, handler.getWarmEmissionsPerLinkAndTimeInterval().get(simulationEndTime).get(linkId3).get(WarmPollutant.CO), MatsimTestUtils.EPSILON);
+		Assert.assertEquals(c2Value, handler.getWarmEmissionsPerLinkAndTimeInterval().get(simulationEndTime).get(linkId3).get(WarmPollutant.CO2_TOTAL), MatsimTestUtils.EPSILON);
+		Assert.assertEquals(fcValue, handler.getWarmEmissionsPerLinkAndTimeInterval().get(simulationEndTime).get(linkId3).get(WarmPollutant.FC), MatsimTestUtils.EPSILON);
+		Assert.assertEquals(hcValue, handler.getWarmEmissionsPerLinkAndTimeInterval().get(simulationEndTime).get(linkId3).get(WarmPollutant.HC), MatsimTestUtils.EPSILON);
+		Assert.assertEquals(0.0, handler.getWarmEmissionsPerLinkAndTimeInterval().get(simulationEndTime).get(linkId3).get(WarmPollutant.NMHC), MatsimTestUtils.EPSILON);
+		Assert.assertEquals(0.0, handler.getWarmEmissionsPerLinkAndTimeInterval().get(simulationEndTime).get(linkId3).get(WarmPollutant.NO2), MatsimTestUtils.EPSILON);
+		Assert.assertEquals(0.0, handler.getWarmEmissionsPerLinkAndTimeInterval().get(simulationEndTime).get(linkId3).get(WarmPollutant.NOX), MatsimTestUtils.EPSILON);
+		Assert.assertEquals(pmValue, handler.getWarmEmissionsPerLinkAndTimeInterval().get(simulationEndTime).get(linkId3).get(WarmPollutant.PM), MatsimTestUtils.EPSILON);
+		Assert.assertEquals(soValue, handler.getWarmEmissionsPerLinkAndTimeInterval().get(simulationEndTime).get(linkId3).get(WarmPollutant.SO2), MatsimTestUtils.EPSILON);
 		
 		handler.reset(2);
 		handler.handleEvent(event6);
 		
-		
+		// assert
 		
 		// 'normal', time 0, end of sim, krumme intervallgrenze
 		// warmEmissions = null, teilweise initialisiert, vollstaendig
