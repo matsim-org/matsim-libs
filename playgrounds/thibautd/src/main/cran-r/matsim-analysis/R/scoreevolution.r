6b0ab776eecbeade7cefb2dfd6ca2c29d3364915
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2013-07-17 06:48:57 +0000

    improve labels and appearance
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@25148 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/cran-r/matsim-analysis/R/scoreevolution.r
+++ playgrounds/thibautd/src/main/cran-r/matsim-analysis/R/scoreevolution.r
@@ -6,11 +6,16 @@ plot.score.evolution <-
 			 col.exec="black",
 			 col.min="red",
 			 legend.pos="bottomright",
+			 xlab="Iteration",
+			 ylab="Score",
+			 bty="n",
 			 ...) {
 	attach( dataset )
 	the.lines <- groupId == group
 	plot( iter[ the.lines ] , max[ the.lines ] , type="l" , col=col.max ,
 		 ylim=c(min( min[ the.lines ] ) , max( max[ the.lines ] )),
+		 xlab=xlab, ylab=ylab,
+		 bty=bty,
 		 ... )
 	lines( iter[ the.lines ] , min[ the.lines ] , col=col.min , ... )
 	lines( iter[ the.lines ] , exec[ the.lines ] , col=col.exec , ... )
