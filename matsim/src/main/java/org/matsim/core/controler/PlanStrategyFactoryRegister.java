f6540a07c96593aa39483676abd4ee0e635556ea
mrieser <rieser@gmail.com>
2013-07-19 14:18:47 +0000

    add GPL header
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@25208 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/core/controler/PlanStrategyFactoryRegister.java
+++ matsim/src/main/java/org/matsim/core/controler/PlanStrategyFactoryRegister.java
@@ -1,3 +1,22 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2013 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
 package org.matsim.core.controler;
 
 import java.util.HashMap;
@@ -6,9 +25,9 @@ import java.util.Map;
 import org.matsim.core.replanning.PlanStrategyFactory;
 
 public class PlanStrategyFactoryRegister {
-	
+
 	private Map<String, PlanStrategyFactory> factoryMap = new HashMap<String, PlanStrategyFactory>();
-	
+
 	public PlanStrategyFactory getInstance(String strategyType) {
 		if (!factoryMap.containsKey(strategyType)) {
 			throw new IllegalArgumentException("Plan strategy " + strategyType
