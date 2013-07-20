f3cf476d02816bcfea976620a38a3e559d014c88
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2013-07-20 12:39:29 +0000

    basic social network structure
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@25223 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/thibautd/src/main/java/playground/thibautd/socnetsim/population/SocialNetwork.java
@@ -0,0 +1,52 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * SocialNetwork.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2013 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.thibautd.socnetsim.population;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.matsim.api.core.v01.Id;
+
+import playground.thibautd.utils.MapUtils;
+
+/**
+ * @author thibautd
+ */
+public class SocialNetwork {
+	private final Map<Id, Set<Id>> map = new HashMap<Id, Set<Id>>();
+
+	public void addBidirectionalTie(final Id id1, final Id id2) {
+		addMonodirectionalTie( id1 , id2 );
+		addMonodirectionalTie( id2 , id1 );
+	}
+
+	public void addMonodirectionalTie(
+			final Id ego,
+			final Id alter) {
+		final Set<Id> alters = MapUtils.getSet( ego , map );
+		alters.add( alter );
+	}
+
+	public Set<Id> getAlters(final Id ego) {
+		return map.get( ego );
+	}
+}
+
