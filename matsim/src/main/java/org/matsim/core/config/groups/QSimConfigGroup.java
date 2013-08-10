9f22d74c3fa486422461f983d3a4ae8e1127b481
grether <dominik.grether@gmail.com>
2013-08-10 14:52:02 +0000

    improved documentation
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@25536 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/core/config/groups/QSimConfigGroup.java
+++ matsim/src/main/java/org/matsim/core/config/groups/QSimConfigGroup.java
@@ -206,7 +206,7 @@ public class QSimConfigGroup extends Module implements MobsimConfigGroupI {
 				"factor, and storage capacity factor need to be set with diligence.  Needs to be a vehicular mode to make sense.") ;
 		map.put(INSERTING_WAITING_VEHICLES_BEFORE_DRIVING_VEHICLES, 
 				"decides if waiting vehicles enter the network after or before the already driving vehicles were moved. Default: false") ; 
-		map.put(NODE_OFFSET, "Shortens a link's start and end point in the visualization.");
+		map.put(NODE_OFFSET, "Shortens a link in the visualization, i.e. its start and end point are moved into towards the center. Does not affect traffic flow. ");
 		map.put(LINK_WIDTH, "The (initial) width of the links of the network. Use positive floating point values.");
 		return map ;
 	}
