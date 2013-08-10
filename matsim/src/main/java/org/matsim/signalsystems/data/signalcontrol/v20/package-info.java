41dd2b8ddd716191f8aa63be3bc12ac371ce2eb2
grether <dominik.grether@gmail.com>
2013-08-10 14:23:34 +0000

    fixed typos in comment
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@25530 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/signalsystems/data/signalcontrol/v20/package-info.java
+++ matsim/src/main/java/org/matsim/signalsystems/data/signalcontrol/v20/package-info.java
@@ -6,16 +6,16 @@
  * for all signal control algorithms used.
  * 
  * <p>
- * As a default control algroithm the package can be used to model a fixed-time control behavior. 
+ * As a default control algorithm the package can be used to model a fixed-time control behavior. 
  * Therefore a signal plan is created containing settings for signal groups that specify an onset and  
  * the dropping for a signal group. The plan specifies further  an offset and a cycle time.
  * </p>
  * <p>
- * In general a fixed-time controler of a signal system may have several plans. However there is
+ * In general a fixed-time controller of a signal system may have several plans. However there is
  * no mechanism introduced yet, that implements plan selection, even if the model already has
  * data fields for daytime based selection. Thus always exactly one of the defined plans is used. This 
  * plan is however unspecified, use only one plan per signal system. The next release will
- * support a better behaviour.
+ * support a better behavior.
  * </p>
  * <p>
  * The classes correspond to the data format defined in signalControl_v2.0.xsd for that a reader
