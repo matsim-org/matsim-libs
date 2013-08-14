e56b0876a32084dd577618f1bf292f7df69dd9e7
skull5 <dobler@senozon.com>
2013-08-14 08:34:55 +0000

    Using WithinDayAgentUtils in within-day code.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@25564 a0517364-e873-4167-b1c8-416daa5a6b91

--- contribs/freight/src/main/java/org/matsim/contrib/freight/mobsim/WithinDayActivityReScheduling.java
+++ contribs/freight/src/main/java/org/matsim/contrib/freight/mobsim/WithinDayActivityReScheduling.java
@@ -5,17 +5,20 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Plan;
 import org.matsim.contrib.freight.carrier.Tour.Start;
 import org.matsim.contrib.freight.carrier.Tour.TourActivity;
 import org.matsim.contrib.freight.mobsim.CarrierAgent.CarrierDriverAgent;
 import org.matsim.core.mobsim.framework.MobsimAgent;
+import org.matsim.core.mobsim.framework.PlanAgent;
 import org.matsim.core.mobsim.framework.events.MobsimBeforeSimStepEvent;
 import org.matsim.core.mobsim.framework.listeners.MobsimBeforeSimStepListener;
 import org.matsim.core.mobsim.framework.listeners.MobsimListener;
 import org.matsim.core.mobsim.qsim.InternalInterface;
-import org.matsim.core.mobsim.qsim.agents.ExperimentalBasicWithindayAgent;
+import org.matsim.core.mobsim.qsim.agents.PersonDriverAgentImpl;
+import org.matsim.core.mobsim.qsim.agents.WithinDayAgentUtils;
 import org.matsim.core.mobsim.qsim.interfaces.Netsim;
 import org.matsim.core.population.ActivityImpl;
 
@@ -31,11 +34,14 @@ class WithinDayActivityReScheduling implements MobsimListener, MobsimBeforeSimSt
 	
 	private CarrierAgentTracker carrierAgentTracker;
 	
+	private WithinDayAgentUtils withinDayAgentUtils;
+	
 	WithinDayActivityReScheduling(FreightAgentSource freightAgentSource, InternalInterface internalInterface, CarrierAgentTracker carrierAgentTracker) {
 		super();
 		this.freightAgentSource = freightAgentSource;
 		this.internalInterface = internalInterface;
 		this.carrierAgentTracker = carrierAgentTracker;
+		this.withinDayAgentUtils = new WithinDayAgentUtils();
 	}
 
 	/**
@@ -55,36 +61,37 @@ class WithinDayActivityReScheduling implements MobsimListener, MobsimBeforeSimSt
 		
 	}
 
-	private boolean doReplanning(MobsimAgent pa, Netsim mobsim, double time) {
-		if (!(pa instanceof ExperimentalBasicWithindayAgent)) {
-			logger.error("agent " + pa.getId() + "of wrong type; returning ... ");
+	private boolean doReplanning(MobsimAgent mobsimAgent, Netsim mobsim, double time) {
+		if (!(mobsimAgent instanceof PersonDriverAgentImpl)) {
+			logger.error("agent " + mobsimAgent.getId() + "of wrong type; returning ... ");
 			return false;
 		}
-		ExperimentalBasicWithindayAgent withindayAgent = (ExperimentalBasicWithindayAgent) pa;
-		Plan plan = withindayAgent.getSelectedPlan();
+		PlanAgent planAgent = (PlanAgent) mobsimAgent;
+		Plan plan = planAgent.getSelectedPlan();
+		Id agentId = planAgent.getSelectedPlan().getPerson().getId();
 		if (plan == null) {
 			logger.info(" we don't have a selected plan; returning ... ");
 			return false;
 		}	
 		
-		if (withindayAgent.getCurrentPlanElement() instanceof Activity) {
-			ActivityImpl act = (ActivityImpl) withindayAgent.getCurrentPlanElement();
+		if (planAgent.getCurrentPlanElement() instanceof Activity) {
+			ActivityImpl act = (ActivityImpl) planAgent.getCurrentPlanElement();
 			if(encounteredActivities.contains(act)){
 				return false;
 			}
-			CarrierDriverAgent driver = carrierAgentTracker.getDriver(withindayAgent.getId());
-			TourActivity plannedActivity = (TourActivity) driver.getPlannedTourElement(withindayAgent.getCurrentPlanElementIndex());
+			CarrierDriverAgent driver = carrierAgentTracker.getDriver(agentId);
+			TourActivity plannedActivity = (TourActivity) driver.getPlannedTourElement(this.withinDayAgentUtils.getCurrentPlanElementIndex(mobsimAgent));
 			if(plannedActivity instanceof Start){
 				encounteredActivities.add(act);
 				return false;
 			}
 			else {
 				double newEndTime = Math.max(time, plannedActivity.getTimeWindow().getStart()) + plannedActivity.getDuration();
-				logger.info("[agentId="+ withindayAgent.getId() + "][currentTime="+time+"][actDuration="+plannedActivity.getDuration()+
+				logger.info("[agentId="+ agentId + "][currentTime="+time+"][actDuration="+plannedActivity.getDuration()+
 						"[plannedActEnd="+ act.getEndTime() + "][newActEnd="+newEndTime+"]");
 				act.setEndTime(newEndTime);
-				withindayAgent.calculateAndSetDepartureTime(act);
-				internalInterface.rescheduleActivityEnd(withindayAgent);
+				this.withinDayAgentUtils.calculateAndSetDepartureTime(mobsimAgent, act);
+				internalInterface.rescheduleActivityEnd(mobsimAgent);
 				encounteredActivities.add(act);
 			}
 		} 	
