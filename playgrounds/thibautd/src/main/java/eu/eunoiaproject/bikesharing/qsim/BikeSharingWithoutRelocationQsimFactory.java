69b35bfbb2cbd0357da016c8342b43ba595fc753
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2013-08-30 13:08:15 +0000

    remove unused import
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@25701 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/java/eu/eunoiaproject/bikesharing/qsim/BikeSharingWithoutRelocationQsimFactory.java
+++ playgrounds/thibautd/src/main/java/eu/eunoiaproject/bikesharing/qsim/BikeSharingWithoutRelocationQsimFactory.java
@@ -19,8 +19,6 @@
  * *********************************************************************** */
 package eu.eunoiaproject.bikesharing.qsim;
 
-import org.apache.log4j.Logger;
-
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.config.groups.QSimConfigGroup;
