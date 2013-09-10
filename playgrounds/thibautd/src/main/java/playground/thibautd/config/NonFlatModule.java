2938ee1aacff9210c3874a87e4466b8e18b14cc0
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2013-09-10 14:35:42 +0000

    draft of what would be a "non-flat" config group
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@25775 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/thibautd/src/main/java/playground/thibautd/config/NonFlatModule.java
@@ -0,0 +1,68 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * NonFlatModule.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2013 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.thibautd.config;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.matsim.core.config.Module;
+
+import playground.thibautd.utils.MapUtils;
+
+/**
+ * @author thibautd
+ */
+public class NonFlatModule extends Module {
+	private final Map<String, Collection<Module>> parameterSetsPerType = new HashMap<String, Collection<Module>>();
+
+	public NonFlatModule(final String name) {
+		super(name);
+	}
+
+	/**
+	 * Override if parameter sets of a certain type need a special implementation
+	 */
+	public Module createParameterSet(final String type) {
+		return new Module( type );
+	}
+
+	public void addParameterSet(
+			final String type,
+			final Module set) {
+		MapUtils.getCollection(
+				type,
+				parameterSetsPerType ).add( set );
+	}
+
+	public Collection<Module> getParameterSets(final String type) {
+		final Collection<Module> sets = parameterSetsPerType.get( type );
+		return sets == null ?
+			Collections.<Module>emptySet() :
+			Collections.unmodifiableCollection( sets );
+	}
+
+	public Map<String, Collection<Module>> getParameterSets() {
+		// TODO: immutabilize (including lists)
+		return parameterSetsPerType;
+	}
+}
+
