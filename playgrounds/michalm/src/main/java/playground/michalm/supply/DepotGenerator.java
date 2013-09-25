fa4046b730487eb5b39533a184cb0449197b6c34
michal-m <michal.mac@gmail.com>
2013-09-25 19:44:35 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@25957 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/michalm/src/main/java/playground/michalm/supply/DepotGenerator.java
+++ playgrounds/michalm/src/main/java/playground/michalm/supply/DepotGenerator.java
@@ -46,6 +46,7 @@ public class DepotGenerator
 
         PrintWriter pw = new PrintWriter(new File(depotsFilename));
         pw.println("<?xml version=\"1.0\" ?>");
+        //TODO change the URL
         pw.println("<!DOCTYPE depots SYSTEM \"http://www.man.poznan.pl/~michalm/matsim/depots_v1.dtd\">");
         pw.println();
 
