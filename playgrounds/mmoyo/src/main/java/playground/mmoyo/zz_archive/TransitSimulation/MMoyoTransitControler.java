b86400c0c4e957c328934004d1724d40d92eb32d
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2013-10-10 13:45:50 +0000

    commenting out broken files in "archive" package
    refactoring an "archive" (which I guess it supposed to keep old unused
    code) makes no sense and is even dangerous, as the kept code would not
    reflect the desired state anymore. If this code is actually still used,
    and you do not know how to refactor that, please contact me.
    
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@26141 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/mmoyo/src/main/java/playground/mmoyo/zz_archive/TransitSimulation/MMoyoTransitControler.java
+++ playgrounds/mmoyo/src/main/java/playground/mmoyo/zz_archive/TransitSimulation/MMoyoTransitControler.java
@@ -17,71 +17,71 @@
  *                                                                         *
  * *********************************************************************** */
 
-package playground.mmoyo.zz_archive.TransitSimulation;
-
-import org.matsim.contrib.otfvis.OTFVis;
-import org.matsim.core.config.Config;
-import org.matsim.core.controler.Controler;
-import org.matsim.core.gbl.Gbl;
-import org.matsim.core.mobsim.qsim.QSim;
-import org.matsim.core.mobsim.qsim.QSimFactory;
-import org.matsim.core.population.PopulationFactoryImpl;
-import org.matsim.core.router.util.TravelDisutility;
-import org.matsim.core.router.util.TravelTime;
-import org.matsim.core.scenario.ScenarioImpl;
-import org.matsim.core.scenario.ScenarioLoaderImpl;
-import org.matsim.population.algorithms.PlanAlgorithm;
-import org.matsim.pt.config.TransitConfigGroup;
-import org.matsim.vis.otfvis.OTFClientLive;
-import org.matsim.vis.otfvis.OnTheFlyServer;
-
-public final class MMoyoTransitControler extends Controler {
-	boolean launchOTFDemo=false;
-	private Config config;
-	
-	public MMoyoTransitControler(final ScenarioImpl scenario, boolean launchOTFDemo){
-		super(scenario);
-		this.config = scenario.getConfig();
-		this.setOverwriteFiles(true);   
-		this.launchOTFDemo = launchOTFDemo;
-		throw new RuntimeException(Gbl.CREATE_ROUTING_ALGORITHM_WARNING_MESSAGE) ;
-	}
-	
-	@Override
-	protected void runMobSim() {
-		QSim sim = (QSim) new QSimFactory().createMobsim(this.scenarioData, this.events);
-		
-		OnTheFlyServer server = OTFVis.startServerAndRegisterWithQSim(this.scenarioData.getConfig(), this.scenarioData, events, sim);
-		OTFClientLive.run(this.scenarioData.getConfig(), server);
-
-		sim.run();
-		/*
-		TransitQueueSimulation sim = new TransitQueueSimulation(this.scenarioData, this.events);
-		sim.startOTFServer("livesim");
-		new OnTheFlyClientQuad("rmi:127.0.0.1:4019:" + "livesim").start();
-		sim.run();
-		*/
-	}
-
-//	@Override
-//	public PlanAlgorithm createRoutingAlgorithm() {
-//		return createRoutingAlgorithm(
-//				this.createTravelCostCalculator(),
-//				this.getLinkTravelTimes());
+//package playground.mmoyo.zz_archive.TransitSimulation;
+//
+//import org.matsim.contrib.otfvis.OTFVis;
+//import org.matsim.core.config.Config;
+//import org.matsim.core.controler.Controler;
+//import org.matsim.core.gbl.Gbl;
+//import org.matsim.core.mobsim.qsim.QSim;
+//import org.matsim.core.mobsim.qsim.QSimFactory;
+//import org.matsim.core.population.PopulationFactoryImpl;
+//import org.matsim.core.router.util.TravelDisutility;
+//import org.matsim.core.router.util.TravelTime;
+//import org.matsim.core.scenario.ScenarioImpl;
+//import org.matsim.core.scenario.ScenarioLoaderImpl;
+//import org.matsim.population.algorithms.PlanAlgorithm;
+//import org.matsim.pt.config.TransitConfigGroup;
+//import org.matsim.vis.otfvis.OTFClientLive;
+//import org.matsim.vis.otfvis.OnTheFlyServer;
+//
+//public final class MMoyoTransitControler extends Controler {
+//	boolean launchOTFDemo=false;
+//	private Config config;
+//	
+//	public MMoyoTransitControler(final ScenarioImpl scenario, boolean launchOTFDemo){
+//		super(scenario);
+//		this.config = scenario.getConfig();
+//		this.setOverwriteFiles(true);   
+//		this.launchOTFDemo = launchOTFDemo;
+//		throw new RuntimeException(Gbl.CREATE_ROUTING_ALGORITHM_WARNING_MESSAGE) ;
 //	}
-
-	public PlanAlgorithm createRoutingAlgorithm(final TravelDisutility travelCosts, final TravelTime travelTimes) {
-		return new MMoyoPlansCalcTransitRoute(this.config.plansCalcRoute(), this.network, travelCosts, travelTimes,
-				this.getLeastCostPathCalculatorFactory(), ((PopulationFactoryImpl) this.population.getFactory()).getModeRouteFactory(), this.scenarioData.getTransitSchedule(), new TransitConfigGroup());
-	}
-	
-	public static void main(final String[] args) {
-		if (args.length > 0) {
-			ScenarioLoaderImpl scenarioLoader = ScenarioLoaderImpl.createScenarioLoaderImplAndResetRandomSeed(args[0]); //load from configFile
-			ScenarioImpl scenario = (ScenarioImpl) scenarioLoader.getScenario();
-			scenarioLoader.loadScenario();
-			new MMoyoTransitControler(scenario, true).run();
-		} 
-	}
-	
-}
+//	
+//	@Override
+//	protected void runMobSim() {
+//		QSim sim = (QSim) new QSimFactory().createMobsim(this.scenarioData, this.events);
+//		
+//		OnTheFlyServer server = OTFVis.startServerAndRegisterWithQSim(this.scenarioData.getConfig(), this.scenarioData, events, sim);
+//		OTFClientLive.run(this.scenarioData.getConfig(), server);
+//
+//		sim.run();
+//		/*
+//		TransitQueueSimulation sim = new TransitQueueSimulation(this.scenarioData, this.events);
+//		sim.startOTFServer("livesim");
+//		new OnTheFlyClientQuad("rmi:127.0.0.1:4019:" + "livesim").start();
+//		sim.run();
+//		*/
+//	}
+//
+////	@Override
+////	public PlanAlgorithm createRoutingAlgorithm() {
+////		return createRoutingAlgorithm(
+////				this.createTravelCostCalculator(),
+////				this.getLinkTravelTimes());
+////	}
+//
+//	public PlanAlgorithm createRoutingAlgorithm(final TravelDisutility travelCosts, final TravelTime travelTimes) {
+//		return new MMoyoPlansCalcTransitRoute(this.config.plansCalcRoute(), this.network, travelCosts, travelTimes,
+//				this.getLeastCostPathCalculatorFactory(), ((PopulationFactoryImpl) this.population.getFactory()).getModeRouteFactory(), this.scenarioData.getTransitSchedule(), new TransitConfigGroup());
+//	}
+//	
+//	public static void main(final String[] args) {
+//		if (args.length > 0) {
+//			ScenarioLoaderImpl scenarioLoader = ScenarioLoaderImpl.createScenarioLoaderImplAndResetRandomSeed(args[0]); //load from configFile
+//			ScenarioImpl scenario = (ScenarioImpl) scenarioLoader.getScenario();
+//			scenarioLoader.loadScenario();
+//			new MMoyoTransitControler(scenario, true).run();
+//		} 
+//	}
+//	
+//}
