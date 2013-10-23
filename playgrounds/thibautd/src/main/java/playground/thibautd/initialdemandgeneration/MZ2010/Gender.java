abe8a3345a9cd659429d2ccf6a1b685a0a699fc5
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2013-10-23 09:13:00 +0000

    remove "initialdemandgeneration" package
    this is really too dirty, and unused
    
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@26282 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/java/playground/thibautd/initialdemandgeneration/MZ2010/Gender.java
+++
@@ -1,3 +0,0 @@
-package playground.thibautd.initialdemandgeneration.MZ2010;
-
-public enum Gender { MALE, FEMALE; }
--- playgrounds/thibautd/src/main/java/playground/thibautd/initialdemandgeneration/old/counts/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Contains utilities for creating count data.
- * Consists of small modifications of classes in {@link playground.anhorni.counts},
- * in order to allow counts generation for arbitrary days of week.
- *
- * @author thibautd
- */
-
-package playground.thibautd.initialdemandgeneration.old.counts;
