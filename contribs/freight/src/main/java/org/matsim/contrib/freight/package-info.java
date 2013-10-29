9e70dcef8c2c92990e25e76ddc067ce9ccd7d533
vsp_nagel <2008@kainagel.org>
2013-10-29 11:41:52 +0000

    some small documentation addition
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@26348 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ contribs/freight/src/main/java/org/matsim/contrib/freight/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package that plugs freight algorithms (programmed in external package jsprit) into matsim.
+ * Haven't found out yet how that works.
+ * <p>
+ * Look into controler package for a start. 
+ * 
+ * @author (of documentation) nagel
+  */
+package org.matsim.contrib.freight;
