bdda1d7474153e09013616e9809147385829bdf9
rashid_waraich <rashid_waraich@users.sf.net>
2013-11-04 15:42:21 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@26482 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/main/java/playground/wrashid/parkingSearch/ppSim/jdepSim/searchStrategies/axhausenPolak1989/AxPo1989_Strategy2.java
+++ playgrounds/wrashid/src/main/java/playground/wrashid/parkingSearch/ppSim/jdepSim/searchStrategies/axhausenPolak1989/AxPo1989_Strategy2.java
@@ -17,8 +17,48 @@
  *                                                                         *
  * *********************************************************************** */
 package playground.wrashid.parkingSearch.ppSim.jdepSim.searchStrategies.axhausenPolak1989;
-public class AxPo1989_Strategy2 {
 
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.contrib.parking.lib.GeneralLib;
+import org.matsim.core.population.ActivityImpl;
+
+import playground.wrashid.parkingSearch.ppSim.jdepSim.AgentWithParking;
+import playground.wrashid.parkingSearch.ppSim.jdepSim.searchStrategies.random.RandomStreetParkingSearchBRD;
+
+
+// street before reaching destination, then switch to garage parking then back to street (backup)
+// need multiple strategies for generating good routes to garage parking close-by.
+public class AxPo1989_Strategy2 extends RandomStreetParkingSearchBRD {
+
+	public AxPo1989_Strategy2(double maxDistance, Network network, String name, double distanceToDestinationForStartingRandomSearch) {
+		super(maxDistance, network, name, distanceToDestinationForStartingRandomSearch);
+	}
+
+	@Override
+	public void handleAgentLeg(AgentWithParking aem) {
+		Id personId = aem.getPerson().getId();
+		if (startSearchTime.containsKey(personId)){
+			double searchDuration=getSearchTime(aem);
+			
+			if (searchDuration>5*60){
+				useSpecifiedParkingType.put(personId, "garageParking");
+			}
+			
+			if (searchDuration>15*60){
+				useSpecifiedParkingType.put(personId, "streetParking");
+			}
+		}
+		
+		super.handleAgentLeg(aem);
+	}
+	
+	
+	@Override
+	public void handleParkingDepartureActivity(AgentWithParking aem) {
+		super.handleParkingDepartureActivity(aem);
+		useSpecifiedParkingType.remove(aem.getPerson().getId());
+	}
 
 }
 
