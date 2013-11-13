8c3a39facf409d4e5595b6ad24fa30f944b2c1a9
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2013-11-13 10:12:10 +0000

    module corresponding to the previously introduced algo
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@26618 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/thibautd/src/main/java/playground/thibautd/replanning/ActivitySequenceMutatorModule.java
@@ -0,0 +1,42 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * ActivitySequenceMutatorModule.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2013 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.thibautd.replanning;
+
+import org.matsim.core.gbl.MatsimRandom;
+import org.matsim.core.replanning.modules.AbstractMultithreadedModule;
+import org.matsim.population.algorithms.PlanAlgorithm;
+
+/**
+ * @author thibautd
+ */
+public class ActivitySequenceMutatorModule extends AbstractMultithreadedModule {
+
+	public ActivitySequenceMutatorModule(final int numOfThreads) {
+		super(numOfThreads);
+	}
+
+	@Override
+	public PlanAlgorithm getPlanAlgoInstance() {
+		return new ActivitySequenceMutatorAlgorithm(
+				MatsimRandom.getLocalInstance(),
+				getReplanningContext().getTripRouter().getStageActivityTypes() );
+	}
+}
+
