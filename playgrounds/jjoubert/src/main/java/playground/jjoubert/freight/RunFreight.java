2c81e7494baa4d9e070df2861140ce12412756eb
mrieser <rieser@gmail.com>
2013-11-13 15:04:12 +0000

    delete unused import statement that referred a missing class
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@26650 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/jjoubert/src/main/java/playground/jjoubert/freight/RunFreight.java
+++ playgrounds/jjoubert/src/main/java/playground/jjoubert/freight/RunFreight.java
@@ -33,7 +33,6 @@ import org.matsim.contrib.freight.replanning.selectors.SelectBestPlan;
 import org.matsim.core.config.Config;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.ControlerDefaults;
-import org.matsim.core.controler.ControlerUtils;
 import org.matsim.core.router.util.LeastCostPathCalculator;
 import org.matsim.core.router.util.TravelDisutility;
 import org.matsim.core.router.util.TravelTime;
@@ -55,34 +54,34 @@ final class RunFreight {
 		final Config config = scenario.getConfig();
 		final Controler controler = new Controler(scenario);
 		controler.setOverwriteFiles(true);
-		
+
 		/* Create scoring function factory. */
 		CarrierScoringFunctionFactory scoringFunctionFactory = new CarrierScoringFunctionFactory() {
-			
+
 			@Override
 			public ScoringFunction createScoringFunction(Carrier carrier) {
 				SumScoringFunction sum = new SumScoringFunction();
-				
+
 				/* Add leg and money scoring. */
 				CharyparNagelScoringParameters params = new CharyparNagelScoringParameters(config.planCalcScore());
 				final LegScoring legScoringFunction = new CharyparNagelLegScoring(params , scenario.getNetwork());
 				final MoneyScoring moneyScoringFunction = new CharyparNagelMoneyScoring(params);
 				sum.addScoringFunction(legScoringFunction);
 				sum.addScoringFunction(moneyScoringFunction);
-				
+
 				return sum;
 			}
 		};
-		
+
 		/* Create strategy manager, i.e. replanning modules. */
 		CarrierPlanStrategyManagerFactory strategyManagerFactory = new CarrierPlanStrategyManagerFactory() {
-			
+
 			@Override
 			public CarrierReplanningStrategyManager createStrategyManager(
 					Controler controler) {
-				
+
 				CarrierReplanningStrategyManager strategyManager = new CarrierReplanningStrategyManager();
-				
+
 				/* Create a basic ReRouting module. */
 				TravelTime travelTime = controler.getLinkTravelTimes();
 				TravelDisutility travelCost = ControlerDefaults.createDefaultTravelDisutilityFactory(scenario).createTravelDisutility(travelTime, config.planCalcScore());
@@ -91,18 +90,18 @@ final class RunFreight {
 				CarrierReplanningStrategyModule rerouteModule = new ReRouteVehicles(router , scenario.getNetwork(), travelTime);
 				rerouteStrategy.addModule(rerouteModule);
 				strategyManager.addStrategy(rerouteStrategy, 0.1);
-				
+
 				/* Alternative: do nothing, but select best plan. */
 				CarrierReplanningStrategy selectBestStrategy = new CarrierReplanningStrategy(new SelectBestPlan());
 				strategyManager.addStrategy(selectBestStrategy, 0.9);
-				
+
 				return strategyManager;
 			}
 		};
-		
+
 		CarrierController carrierControlerListener = new CarrierController(carriers, strategyManagerFactory, scoringFunctionFactory);
 		carrierControlerListener.setEnableWithinDayActivityReScheduling(false);
-		
+
 		controler.addControlerListener(carrierControlerListener);
 		controler.run();
 	}
