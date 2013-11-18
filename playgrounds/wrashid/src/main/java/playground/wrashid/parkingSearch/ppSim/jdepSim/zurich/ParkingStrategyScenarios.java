8195317bf687b7a411c26b29a771f90808738b87
rashid_waraich <rashid_waraich@users.sf.net>
2013-11-18 19:36:16 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@26710 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/main/java/playground/wrashid/parkingSearch/ppSim/jdepSim/zurich/ParkingStrategyScenarios.java
+++ playgrounds/wrashid/src/main/java/playground/wrashid/parkingSearch/ppSim/jdepSim/zurich/ParkingStrategyScenarios.java
@@ -292,12 +292,12 @@ public class ParkingStrategyScenarios {
 				strategy.setGroupName("ARD-waiting-S");
 				allStrategies.add(strategy);
 				
-				strategy = new Dummy_ARD_TakeClosestGarageParking(-1, scenario.getNetwork(), "ARD_TakeClosestGarageParking");
-				strategy.setGroupName("ARD_TakeClosestGarageParking");
+				strategy = new Dummy_ARD_TakeClosestGarageParking(-1, scenario.getNetwork(), "ARD-TakeClosestGarageParking-" + i);
+				strategy.setGroupName("ARD-TakeClosestGarageParking");
 				allStrategies.add(strategy);
 				
-				strategy = new Dummy_BRD_TakeClosestGarageParking(-1, scenario.getNetwork(), "BRD_TakeClosestGarageParking", 300.0);
-				strategy.setGroupName("BRD_TakeClosestGarageParking");
+				strategy = new Dummy_BRD_TakeClosestGarageParking(-1, scenario.getNetwork(), "BRD-TakeClosestGarageParking-" + i, 300.0);
+				strategy.setGroupName("BRD-TakeClosestGarageParking");
 				allStrategies.add(strategy);
 				
 				double startStrategyAtDistanceFromDestination = 250;
