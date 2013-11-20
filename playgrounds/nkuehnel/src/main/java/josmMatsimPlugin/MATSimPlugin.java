df3ba8df38dd5b27e7bd161dc777032da5c6a823
nkuehnel <nkuehnel@users.sf.net>
2013-11-20 16:58:30 +0000

    added MAtSim-mapstyle removed default length tag from matsimpreset.xml
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@26760 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/nkuehnel/src/main/java/josmMatsimPlugin/MATSimPlugin.java
+++ playgrounds/nkuehnel/src/main/java/josmMatsimPlugin/MATSimPlugin.java
@@ -18,7 +18,11 @@ import javax.swing.JSeparator;
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.gui.MapFrame;
 import org.openstreetmap.josm.gui.MapView;
+import org.openstreetmap.josm.gui.mappaint.MapPaintStyles;
+import org.openstreetmap.josm.gui.mappaint.mapcss.MapCSSStyleSource;
+import org.openstreetmap.josm.gui.mappaint.xml.XmlStyleSource;
 import org.openstreetmap.josm.gui.preferences.PreferenceSetting;
+import org.openstreetmap.josm.gui.preferences.SourceEntry;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
 import org.openstreetmap.josm.tools.ImageProvider;
@@ -35,82 +39,80 @@ import org.xml.sax.SAXException;
  * @author nkuehnel
  * 
  */
-public class MATSimPlugin extends Plugin
-{
-	private MATSimAction Action;
-	protected static MATSimToggleDialog toggleDialog;
+public class MATSimPlugin extends Plugin {
+    private MATSimAction Action;
+    protected static MATSimToggleDialog toggleDialog;
 
-	public MATSimPlugin(PluginInformation info) throws IOException, SAXException
-	{
-		super(info);
-		Action = new MATSimAction();
-		Main.main.menu.toolsMenu.add(Action);
+    public MATSimPlugin(PluginInformation info) throws IOException,
+	    SAXException {
+	super(info);
+	Action = new MATSimAction();
+	Main.main.menu.toolsMenu.add(Action);
 
-		Reader reader = new InputStreamReader(this
-				.getPluginResourceClassLoader().getResourceAsStream(
-						"resources/matsimPreset.xml"));
-		
-		Collection<TaggingPreset> tps;
-		try
-		{
-			tps = TaggingPresetReader.readAll(reader, true);
-		} catch (SAXException e)
-		{
-			e.printStackTrace();
-			tps = Collections.EMPTY_LIST;
-		}
+	Reader reader = new InputStreamReader(this
+		.getPluginResourceClassLoader().getResourceAsStream(
+			"resources/matsimPreset.xml"));
 
-		for (TaggingPreset tp : tps)
-		{
-			if (!(tp instanceof TaggingPresetSeparator))
-			{
-				Main.toolbar.register(tp);
-			}
-		}
-
-		AutoCompletionManager.cachePresets(tps);
-		HashMap<TaggingPresetMenu, JMenu> submenus = new HashMap<TaggingPresetMenu, JMenu>();
-		for (final TaggingPreset p : tps)
-		{
-			JMenu m = p.group != null ? submenus.get(p.group)
-					: Main.main.menu.presetsMenu;
-			if (p instanceof TaggingPresetSeparator)
-			{
-				m.add(new JSeparator());
-			} else if (p instanceof TaggingPresetMenu)
-			{
-				JMenu submenu = new JMenu(p);
-				submenu.setText(p.getLocaleName());
-				((TaggingPresetMenu) p).menu = submenu;
-				submenus.put((TaggingPresetMenu) p, submenu);
-				m.add(submenu);
-			} else
-			{
-				JMenuItem mi = new JMenuItem(p);
-				mi.setText(p.getLocaleName());
-				m.add(mi);
-			}
-		}
-
+	Collection<TaggingPreset> tps;
+	try {
+	    tps = TaggingPresetReader.readAll(reader, true);
+	} catch (SAXException e) {
+	    e.printStackTrace();
+	    tps = Collections.EMPTY_LIST;
 	}
 
-	/**
-	 * Called when the JOSM map frame is created or destroyed.
-	 */
-	@Override
-	public void mapFrameInitialized(MapFrame oldFrame, MapFrame newFrame)
-	{
-		if (oldFrame == null && newFrame != null)
-		{ // map frame added
-			toggleDialog = new MATSimToggleDialog();
-			Main.map.addToggleDialog(toggleDialog);
-			MapView.addLayerChangeListener(toggleDialog);
-		}
+	for (TaggingPreset tp : tps) {
+	    if (!(tp instanceof TaggingPresetSeparator)) {
+		Main.toolbar.register(tp);
+	    }
 	}
 
-	@Override
-	public PreferenceSetting getPreferenceSetting()
-	{
-		return null;
+	AutoCompletionManager.cachePresets(tps);
+	HashMap<TaggingPresetMenu, JMenu> submenus = new HashMap<TaggingPresetMenu, JMenu>();
+	for (final TaggingPreset p : tps) {
+	    JMenu m = p.group != null ? submenus.get(p.group)
+		    : Main.main.menu.presetsMenu;
+	    if (p instanceof TaggingPresetSeparator) {
+		m.add(new JSeparator());
+	    } else if (p instanceof TaggingPresetMenu) {
+		JMenu submenu = new JMenu(p);
+		submenu.setText(p.getLocaleName());
+		((TaggingPresetMenu) p).menu = submenu;
+		submenus.put((TaggingPresetMenu) p, submenu);
+		m.add(submenu);
+	    } else {
+		JMenuItem mi = new JMenuItem(p);
+		mi.setText(p.getLocaleName());
+		m.add(mi);
+	    }
 	}
+
+    }
+
+    /**
+     * Called when the JOSM map frame is created or destroyed.
+     */
+    @Override
+    public void mapFrameInitialized(MapFrame oldFrame, MapFrame newFrame) {
+	if (oldFrame == null && newFrame != null) { // map frame added
+	    toggleDialog = new MATSimToggleDialog();
+	    Main.map.addToggleDialog(toggleDialog);
+	    MapView.addLayerChangeListener(toggleDialog);
+
+	    URL url = getPluginResourceClassLoader().getResource(
+		    "resources/matsimStyle.mapcss");
+
+	    MapCSSStyleSource style = new MapCSSStyleSource(url.getPath(),
+		    "matsim_style", "MATSim style");
+	    
+	    if (!MapPaintStyles.getStyles().getStyleSources().contains(style)) {
+		MapPaintStyles.addStyle(style);
+	    }
+	}
+    }
+
+    @Override
+    public PreferenceSetting getPreferenceSetting() {
+	return null;
+    }
 }
