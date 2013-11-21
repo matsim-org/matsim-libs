4aa342daefe68b4ee831731723ca8332a487805a
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2013-11-21 13:43:33 +0000

    perform several "laps" in the test
    Finally the test reproduces the problem observed in the ZH Scenario
    (fails when drop-off at the same link as pick-up).
    
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@26772 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/test/java/playground/thibautd/socnetsim/qsim/JointTravelingSimulationTest.java
+++ playgrounds/thibautd/src/test/java/playground/thibautd/socnetsim/qsim/JointTravelingSimulationTest.java
@@ -68,6 +68,8 @@ public class JointTravelingSimulationTest {
 	private static final Logger log =
 		Logger.getLogger(JointTravelingSimulationTest.class);
 
+	private static final int N_LAPS = 5;
+
 	private static final Id ORIGIN_LINK = new IdImpl( "origin" );
 	private static final Id TO_PU_LINK = new IdImpl( "toPu" );
 	private static final Id PU_LINK = new IdImpl( "pu" );
@@ -76,6 +78,7 @@ public class JointTravelingSimulationTest {
 	private static final Id DO_LINK = new IdImpl( "do" );
 	private static final Id TO_DESTINATION_LINK = new IdImpl( "to_destination" );
 	private static final Id DESTINATION_LINK = new IdImpl( "destination" );
+	private static final Id RETURN_LINK = new IdImpl( "return" );
 	
 	@Test
 	public void testAgentsArriveTogetherWithoutDummies() throws Exception {
@@ -111,6 +114,8 @@ public class JointTravelingSimulationTest {
 			final AtomicInteger atDestCount = new AtomicInteger( 0 );
 			events.addHandler( new PersonArrivalEventHandler() {
 				private double arrival = -100;
+				private int nArrival = 0;
+
 				@Override
 				public void reset(final int iteration) {}
 
@@ -120,7 +125,7 @@ public class JointTravelingSimulationTest {
 					log.info( mode+" arrival at "+event.getTime() );
 					if ( mode.equals( JointActingTypes.DRIVER ) ||
 							mode.equals( JointActingTypes.PASSENGER ) ) {
-						if ( arrival < 0 ) {
+						if ( nArrival++ % 3 == 0 ) {
 							arrival = event.getTime();
 							assert arrival > 0;
 						}
@@ -149,12 +154,12 @@ public class JointTravelingSimulationTest {
 
 			Assert.assertEquals(
 					"unexpected number of joint arrivals",
-					3,
+					N_LAPS * 3,
 					arrCount.get());
 
 			Assert.assertEquals(
 					"unexpected number of agents arriving at destination",
-					sc.getPopulation().getPersons().size(),
+					N_LAPS * sc.getPopulation().getPersons().size(),
 					atDestCount.get());
 		}
 	}
@@ -251,112 +256,142 @@ public class JointTravelingSimulationTest {
 		// - same OD served several times by one driver for different passengers
 		// - different drivers serve the same OD
 
-		// driver
-		final Person driver = factory.createPerson( driverId );
-		final Plan driverPlan = factory.createPlan();
-		driverPlan.setPerson( driver );
-		driver.addPlan( driverPlan );
-		sc.getPopulation().addPerson( driver );
+		/* driver */ {
+			final Person driver = factory.createPerson( driverId );
+			final Plan driverPlan = factory.createPlan();
+			driverPlan.setPerson( driver );
+			driver.addPlan( driverPlan );
+			sc.getPopulation().addPerson( driver );
 
-		Activity act = factory.createActivityFromLinkId( "h" , ORIGIN_LINK );
-		act.setEndTime( random.nextDouble() * 12 * 3600 );
-		driverPlan.addActivity( act );
+			for ( int i=0; i < N_LAPS; i++ ) {
+				Activity act = factory.createActivityFromLinkId( "h" , ORIGIN_LINK );
+				act.setEndTime( (i * 12) + random.nextDouble() * 6 * 3600 );
+				driverPlan.addActivity( act );
 
-		Leg l = factory.createLeg( TransportMode.car );
-		l.setRoute( new LinkNetworkRouteImpl( ORIGIN_LINK , toPuRoute , puLink ) );
-		driverPlan.addLeg( l );
+				Leg l = factory.createLeg( TransportMode.car );
+				l.setRoute( new LinkNetworkRouteImpl( ORIGIN_LINK , toPuRoute , puLink ) );
+				driverPlan.addLeg( l );
 
-		if (insertDummyActivities) {
-			act = factory.createActivityFromLinkId(
-						JointActingTypes.INTERACTION,
-						puLink );
+				if (insertDummyActivities) {
+					act = factory.createActivityFromLinkId(
+								JointActingTypes.INTERACTION,
+								puLink );
+					driverPlan.addActivity( act );
+					act.setMaximumDuration( 0 );
+				}
+
+				l = factory.createLeg( JointActingTypes.DRIVER );
+				DriverRoute dRoute =
+					new DriverRoute(
+							puLink,
+							doLink );
+				dRoute.setLinkIds(
+						puLink , 
+						puToDoRoute,
+						doLink);
+				dRoute.addPassenger( passengerId1 );
+				dRoute.addPassenger( passengerId2 );
+				l.setRoute( dRoute );
+				driverPlan.addLeg( l );
+
+				if (insertDummyActivities) {
+					act = factory.createActivityFromLinkId(
+								JointActingTypes.INTERACTION,
+								doLink );
+					driverPlan.addActivity( act );
+					act.setMaximumDuration( 0 );
+				}
+
+				l = factory.createLeg( TransportMode.car );
+				l.setRoute( new LinkNetworkRouteImpl( doLink , doToDestRoute , DESTINATION_LINK ) );
+				driverPlan.addLeg( l );
+
+				final Activity work = factory.createActivityFromLinkId( "w" , DESTINATION_LINK );
+				work.setEndTime( (i * 12) + random.nextDouble() * 12 * 3600 );
+				driverPlan.addActivity( work );
+
+				l = factory.createLeg( TransportMode.car );
+				l.setRoute(
+						new LinkNetworkRouteImpl(
+							DESTINATION_LINK,
+							Collections.singletonList( RETURN_LINK ),
+							ORIGIN_LINK ) );
+				driverPlan.addLeg( l );
+			}
+
+			final Activity act = factory.createActivityFromLinkId( "h" , ORIGIN_LINK );
 			driverPlan.addActivity( act );
-			act.setMaximumDuration( 0 );
 		}
-
-		l = factory.createLeg( JointActingTypes.DRIVER );
-		DriverRoute dRoute =
-			new DriverRoute(
-					puLink,
-					doLink );
-		dRoute.setLinkIds(
-				puLink , 
-				puToDoRoute,
-				doLink);
-		dRoute.addPassenger( passengerId1 );
-		dRoute.addPassenger( passengerId2 );
-		l.setRoute( dRoute );
-		driverPlan.addLeg( l );
-
-		if (insertDummyActivities) {
-			act = factory.createActivityFromLinkId(
-						JointActingTypes.INTERACTION,
-						doLink );
-			driverPlan.addActivity( act );
-			act.setMaximumDuration( 0 );
-		}
-
-		l = factory.createLeg( TransportMode.car );
-		l.setRoute( new LinkNetworkRouteImpl( doLink , doToDestRoute , DESTINATION_LINK ) );
-		driverPlan.addLeg( l );
-
-		act = factory.createActivityFromLinkId( "h" , DESTINATION_LINK );
-		driverPlan.addActivity( act );
 
 		// passengers 
 		for (Id passengerId : new Id[]{ passengerId1 , passengerId2 }) {
 			final Person p1 = factory.createPerson( passengerId );
 			final Plan p1Plan = factory.createPlan();
-			p1Plan.setPerson( driver );
+			p1Plan.setPerson( p1 );
 			p1.addPlan( p1Plan );
 			sc.getPopulation().addPerson( p1 );
 
-			act = factory.createActivityFromLinkId( "h" , ORIGIN_LINK );
-			act.setEndTime( random.nextDouble() * 12 * 3600 );
-			p1Plan.addActivity( act );
-
-			l = factory.createLeg( TransportMode.walk );
-			double tt = random.nextDouble() * 1234;
-			Route walkRoute = new GenericRouteImpl( ORIGIN_LINK , puLink );
-			walkRoute.setTravelTime( tt );
-			l.setTravelTime( tt );
-			l.setRoute( walkRoute );
-			p1Plan.addLeg( l );
-
-			if (insertDummyActivities) {
-				act = factory.createActivityFromLinkId(
-							JointActingTypes.INTERACTION,
-							puLink );
+			for ( int i=0; i < N_LAPS; i++ ) {
+				Activity act = factory.createActivityFromLinkId( "h" , ORIGIN_LINK );
+				act.setEndTime( (i * 12) + random.nextDouble() * 6 * 3600 );
 				p1Plan.addActivity( act );
-				act.setMaximumDuration( 0 );
-			}
 
-			l = factory.createLeg( JointActingTypes.PASSENGER );
-			PassengerRoute pRoute =
-				new PassengerRoute(
-						puLink,
-						doLink );
-			pRoute.setDriverId( driverId );
-			l.setRoute( pRoute );
-			p1Plan.addLeg( l );
+				Leg l = factory.createLeg( TransportMode.walk );
+				double tt = random.nextDouble() * 1234;
+				Route walkRoute = new GenericRouteImpl( ORIGIN_LINK , puLink );
+				walkRoute.setTravelTime( tt );
+				l.setTravelTime( tt );
+				l.setRoute( walkRoute );
+				p1Plan.addLeg( l );
 
-			if (insertDummyActivities) {
-				act = factory.createActivityFromLinkId(
-							JointActingTypes.INTERACTION,
+				if (insertDummyActivities) {
+					act = factory.createActivityFromLinkId(
+								JointActingTypes.INTERACTION,
+								puLink );
+					p1Plan.addActivity( act );
+					act.setMaximumDuration( 0 );
+				}
+
+				l = factory.createLeg( JointActingTypes.PASSENGER );
+				PassengerRoute pRoute =
+					new PassengerRoute(
+							puLink,
 							doLink );
+				pRoute.setDriverId( driverId );
+				l.setRoute( pRoute );
+				p1Plan.addLeg( l );
+
+				if (insertDummyActivities) {
+					act = factory.createActivityFromLinkId(
+								JointActingTypes.INTERACTION,
+								doLink );
+					p1Plan.addActivity( act );
+					act.setMaximumDuration( 0 );
+				}
+
+				l = factory.createLeg( TransportMode.walk );
+				tt = random.nextDouble() * 1234;
+				walkRoute = new GenericRouteImpl( doLink , DESTINATION_LINK );
+				walkRoute.setTravelTime( tt );
+				l.setTravelTime( tt );
+				l.setRoute( walkRoute );
+				p1Plan.addLeg( l );
+
+				act = factory.createActivityFromLinkId( "w" , DESTINATION_LINK );
+				act.setEndTime( (i * 12) + random.nextDouble() * 12 * 3600 );
 				p1Plan.addActivity( act );
-				act.setMaximumDuration( 0 );
+
+				l = factory.createLeg( TransportMode.walk );
+				tt = random.nextDouble() * 1234;
+				walkRoute = new GenericRouteImpl( DESTINATION_LINK , ORIGIN_LINK );
+				walkRoute.setTravelTime( tt );
+				l.setTravelTime( tt );
+				l.setRoute( walkRoute );
+				p1Plan.addLeg( l );
+
 			}
 
-			l = factory.createLeg( TransportMode.walk );
-			tt = random.nextDouble() * 1234;
-			walkRoute = new GenericRouteImpl( doLink , DESTINATION_LINK );
-			walkRoute.setTravelTime( tt );
-			l.setTravelTime( tt );
-			l.setRoute( walkRoute );
-			p1Plan.addLeg( l );
-
-			act = factory.createActivityFromLinkId( "h" , DESTINATION_LINK );
+			final Activity act = factory.createActivityFromLinkId( "h" , ORIGIN_LINK );
 			p1Plan.addActivity( act );
 		}
 
@@ -367,7 +402,8 @@ public class JointTravelingSimulationTest {
 		int c = 0;
 		int d = 0;
 
-		Node node1 = network.getFactory().createNode( new IdImpl( c++ ) , new CoordImpl( 0 , d++ ) );
+		final Node firstNode = network.getFactory().createNode( new IdImpl( c++ ) , new CoordImpl( 0 , d++ ) );
+		Node node1 = firstNode;
 		Node node2 = network.getFactory().createNode( new IdImpl( c++ ) , new CoordImpl( 0 , d++ ) );
 
 		network.addNode( node1 );
@@ -380,6 +416,8 @@ public class JointTravelingSimulationTest {
 			network.addNode( node2 );
 			network.addLink( network.getFactory().createLink( linkId , node1 , node2 ) );
 		}
+
+		network.addLink( network.getFactory().createLink( RETURN_LINK , node2 , firstNode ) );
 	}
 }
 
