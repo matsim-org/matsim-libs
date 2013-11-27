38bb68581af52736d1a38ce3c43989f270d871d1
wdoering <wdoering@users.sf.net>
2013-11-27 15:56:09 +0000

    grips: polygonal selection (minor changes)
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@26902 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ contribs/grips/src/main/java/org/matsim/contrib/grips/model/MultiSelectionInterface.java
@@ -0,0 +1,10 @@
+package org.matsim.contrib.grips.model;
+
+import org.matsim.contrib.grips.model.Constants.SelectionMode;
+
+public interface MultiSelectionInterface {
+	
+	public void setSelectionMode(SelectionMode selectionMode);
+	public SelectionMode getSelectionMode();
+
+}
