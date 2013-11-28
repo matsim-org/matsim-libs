0cf5fde375e6275eb18298aba7d015b825c2c43f
staheale <staheale@users.sf.net>
2013-11-28 10:30:59 +0000

    class to create a location choice strategy.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@26914 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/staheale/src/main/java/playground/staheale/matsim2030/LocationChoiceStrategyFactory.java
@@ -0,0 +1,42 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.staheale.matsim2030;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.replanning.PlanStrategy;
+import org.matsim.core.replanning.PlanStrategyFactory;
+import org.matsim.core.replanning.PlanStrategyImpl;
+import org.matsim.core.replanning.modules.ReRoute;
+import org.matsim.core.replanning.modules.TripsToLegsModule;
+import org.matsim.core.replanning.selectors.RandomPlanSelector;
+
+public class LocationChoiceStrategyFactory implements PlanStrategyFactory {
+
+	@Override
+	public PlanStrategy createPlanStrategy(Scenario scenario, EventsManager eventsManager) {
+		PlanStrategyImpl strategy = new PlanStrategyImpl(new RandomPlanSelector());
+		strategy.addStrategyModule(new TripsToLegsModule(scenario.getConfig()));
+		//strategy.addStrategyModule(new ChangeSingleLegMode(scenario.getConfig()));
+		strategy.addStrategyModule(new ReRoute(scenario));
+		return strategy;
+	}
+
+}
