d307b7c2efa445ad840f745f496e861790c4d39e
dan-ro <daniel_roeder@web.de>
2013-12-02 14:55:28 +0000

    changed outputpath
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@26988 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/vsp/src/main/java/playground/vsp/buildingEnergy/energyCalculation/BuildingEnergyAnalyzer.java
+++ playgrounds/vsp/src/main/java/playground/vsp/buildingEnergy/energyCalculation/BuildingEnergyAnalyzer.java
@@ -161,7 +161,7 @@ public class BuildingEnergyAnalyzer {
 			this.links = reader.getLinkIds();
 		}
 		log.info("writing trip-Data for agents of interest.");
-		reader.getTripsAnalysis().writeResults(outputPath + runId);
+		reader.getTripsAnalysis().writeResults(outputPath);
 		log.info("finished (writing trip-Data for agents of interest).");
 		log.info("finished (running raw analysis for run " + runId + ", iteration " + iter + ").");
 	}
