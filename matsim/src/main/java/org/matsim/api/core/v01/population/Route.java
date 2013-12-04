3f3c1340b5749271d3cbbe51145fcc971ff954f0
michaz <michael.zilske@gmail.com>
2013-12-04 17:11:25 +0000

    removed some comments not reflecting current state of decisions
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@27045 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/api/core/v01/population/Route.java
+++ matsim/src/main/java/org/matsim/api/core/v01/population/Route.java
@@ -29,40 +29,17 @@ import org.matsim.core.api.internal.MatsimPopulationObject;
  */
 public interface Route extends MatsimPopulationObject {
 
-	/**Design thoughts:<ul>
-	 * <li>I am not convinced that this should really be removed.  There is no harm in having an "expected distance".
-	 * The main problem, presumably, is that people confuse "planned" distance and "executed" distance.  The latter needs
-	 * to come from somewhere else (e.g. from RouteUtils.calcDistance()).  kai, jun'11
-	 * <li>I would, in fact, prefer to have this in leg.  (Because then I could do teleportation based on legs.) 
-	 * But on the other hand, I could also live with having it in the route, since it is already there. kai, jun'11  
-	 * </ul> 
-	 * @deprecated  use RouteUtils.calcDistance()  */
-	@Deprecated
 	public double getDistance();
 
-	// once getDistance is removed in the code, remove this as well
 	public void setDistance(final double distance);
 
-	/** Design thoughts:<ul>
-	 * <li>This used to be "deprecated" since I would have preferred to have this in the Leg.  But since the distance is already
-	 * in the Route (which I would like to keep, see above), it maybe makes sense to also leave this here in the Route. 
-	 * kai, jun'11
-	 * </ul>
-	 */
 	public double getTravelTime();
 
-	/** Design thoughts:<ul>
-	 * <li>This used to be "deprecated" since I would have preferred to have this in the Leg.  But since the distance is already
-	 * in the Route (which I would like to keep, see above), it maybe makes sense to also leave this here in the Route. 
-	 * kai, jun'11
-	 * </ul>
-	 */
 	public void setTravelTime(final double travelTime);
 
 	public Id getStartLinkId();
 
 	public Id getEndLinkId();
-
 
 	public void setStartLinkId(final Id linkId);
 
