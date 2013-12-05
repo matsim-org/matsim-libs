6f4d4a567af21012c049ddb66309567f22771d9e
andreas77 <andreashorni@hotmail.com>
2013-12-05 12:58:39 +0000

    adapted test to new scoring
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@27071 a0517364-e873-4167-b1c8-416daa5a6b91

--- contribs/locationchoice/src/test/java/org/matsim/contrib/locationchoice/LocationChoiceIntegrationTest.java
+++ contribs/locationchoice/src/test/java/org/matsim/contrib/locationchoice/LocationChoiceIntegrationTest.java
@@ -146,7 +146,7 @@ public class LocationChoiceIntegrationTest extends MatsimTestCase {
 		System.err.println( " newWork: " + newWork ) ;
 		System.err.println( " facilityId: " + newWork.getFacilityId() ) ;
 		assertTrue( !newWork.getFacilityId().equals(new IdImpl(1) ) ) ; // should be different from facility number 1 !!
-		assertEquals( new IdImpl(92), newWork.getFacilityId() ); // as I have changed the scoring (act is included) I also changed the test here: 27->92
+		assertEquals( new IdImpl(63), newWork.getFacilityId() ); // as I have changed the scoring (act is included) I also changed the test here: 27->92
 	}
 
 	public void testLocationChoiceFeb2013NegativeScores() {
