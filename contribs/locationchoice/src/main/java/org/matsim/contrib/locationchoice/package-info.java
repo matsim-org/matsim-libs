98aae6c93f1de51f07d03bd08dc13e765ee7eabe
andreas77 <andreashorni@hotmail.com>
2013-12-11 10:46:36 +0000

    update parameter documentation
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@27138 a0517364-e873-4167-b1c8-416daa5a6b91

--- contribs/locationchoice/src/main/java/org/matsim/contrib/locationchoice/package-info.java
+++ contribs/locationchoice/src/main/java/org/matsim/contrib/locationchoice/package-info.java
@@ -7,32 +7,29 @@
  * </ul>
  * 
  * <h2>Parameters:<a name="locationchoice_parameters"></a></h2>
- * Two basic versions of the destination choice module exist, one (v0) based on either global random search or local search and Haegerstrand's time geography and the other (v1) based on best response including error terms. v0 includes <i>localSearchRecursive, localSearchSingleAct</i> and global <i>random</i> search to be specified by the <strong><font color="black">algorithm</font></strong> attribute. v1 includes <i>bestResponse</i> also to be specified by the <strong><font color="black">algorithm</font></strong> attribute. 
- * Parameters required for v0 are given in <strong><font color="blue">blue</font></strong>, the ones used in v1 are given in <strong><font color="red">red</font></strong> and parameters applied in both versions are given in <strong>black</strong>.
- * <br/>
+
  * <h3>General Parameters</h3>
  * <ul>
- *  <li><strong><font color="black">algorithm</font></strong><br>
+ *  <li><strong>algorithm</strong><br>
  * 		Type and range: String (localSearchRecursive, localSearchSingleAct, random, bestResponse)<br>
- * 		Default: null<br>
+ * 		Default: bestResponse<br>
  * 		Description: Specifies which version of destination choice module should be applied. <br>
- * 		localSearchRecursive = time geography <br>
- * 		localSearchSingleAct = the same as simple_tg <br>
- * 		random = random mutation <br>
+ * 		localSearchRecursive = time geography (deprecated)<br>
+ * 		localSearchSingleAct = the same as simple_tg (deprecated)<br>
+ * 		random = random mutation (deprecated)<br>
  * 		bestResponse = best response (recommended) destination choice module <br>
  *  </li>
- * 	<li><strong><font color="black">flexible_types</font></strong><br>
+ * 	<li><strong>flexible_types</strong><br>
  * 		Type and range: String <br>
  * 		Default: null <br>
- * 		Description: This parameter globally (for all agents) specifies the activity types for which location choice is performed. For demand v1 instead of x0.5 ... x24, simply x can be given.
+ * 		Description: This parameter globally (for all agents) specifies the activity types for which location choice is performed. For demand v1 instead of x0.5 ... x24, simply x has to be given.
  *  </li>
- *  <li><strong><font color="black">planSelector</font></strong><br>
+ *  <li><strong>planSelector</strong><br>
  * 		Type and range: String (BestScore, SelectExpBeta, ChangeExpBeta, SelectRandom)<br>
  * 		Default: SelectExpBeta <br>
  * 		Description: Specifies which plan should be replanned.
  *  </li>
- * 
- *  <li><strong><font color="red">randomSeed</font></strong><br>
+ *  <li><strong>randomSeed</strong><br>
  * 		Type and range: long <br>
  * 		Default: 221177 <br>
  * 		Description: Initial seed for generation of random error terms
@@ -41,50 +38,30 @@
  * 
  * <h3>Diluted Scenarios</h3>
  * <ul>
- * 	<li><strong><font color="blue">centerNode</font></strong><br>
+ * 	<li><strong>centerNode</strong><br>
  * 		Type and range: String <br> 
  * 		Default: null<br>
- * 		Description: If the locations have to be chosen from an additionally restrained region a center node and a radius can be set. This is only useful for diluted scenarios
+ * 		Description: If the destinations have to be chosen from an additionally restrained region a center node and a radius can be set. This is only useful for diluted scenarios
  * </li>
- *	<li><strong><font color="blue">radius</font></strong><br>
+ *	<li><strong>radius</strong><br>
  *		Type and range: float > 0 <br> 
  *		Default: null<br>
  *		Description: see above (centerNode) </li>
  * </ul>
  * 
- * <h3>Time Geography</h3>
- * <ul>
- * 	<li><strong><font color="blue">recursionTravelSpeedChange</font></strong><br> 
- * 		Type and range: float > 0 <br> 
- *		Default: 0.1 <br>
- *		Description: This factor is used to grow or shrink the approximate set of locations (see above).<br>
- *					 radius<sub>i+1</sub> = ttb / 2 x recurisionTravelSpeed x (1.0 -+ recursionTravelSpeedChange)<sup>i</sup>, with i: recursion number 
- *	</li>
- *	<li><strong><font color="black">recursionTravelSpeed</font></strong><br> 
- *		Type and range: float > 0 <br> 
- *		Default: 8.5 <br>
- *		Description: Initial assumed travel speed for the investigation area in [m/s]. This value is also used to compute the search space for version 1.
- *	</li>
- *	<li><strong><font color="blue">maxRecursions</font></strong><br>
- *		Type and range: int >= 0 <br> 
- *		Default: 1 <br>
- *		Description: Max. number of trials to search for a location according to the travel time budget (if 0 := universal choice set)
- *	</li> 
- * </ul>
- *
  * <h3>Spatial Competition (Capacity Restraint Function)</h3>
  * <ul>
- * 	<li><strong><font color="blue">restraintFcnFactor</font></strong><br>
+ * 	<li><strong>restraintFcnFactor</strong><br>
  * 		Type and range: float >= 0.0 <br> 
  * 		Default: 0.0<br>
  * 		Description: Factor of the fac.cap.restraint function
  * 	</li>
- * 	<li><strong><font color="blue">restraintFcnExp</font></strong><br>
+ * 	<li><strong>restraintFcnExp</strong><br>
  * 		Type and range: float >= 0.0 <br> 
  * 		Default: 0.0<br>
  * 		Description: Exponent of the fac.cap.restraint function
  * 	</li>
- *	<li><strong><font color="blue">scaleFactor</font></strong><br>
+ *	<li><strong>scaleFactor</strong><br>
  * 		Type and range: int > 0 <br> 
  * 		Default: 1<br>
  * 		Description: For sample scenarios, i.e. if 10% population then scaleFactor := 10
@@ -93,26 +70,41 @@
  * 
  * <h3>Utility Function Specification</h3>
  * <ul>
- * 	<li><strong><font color="red">epsilonDistribution</font></strong><br>
+ * 	<li><strong>epsilonDistribution</strong><br>
  * 		Type and range: String (gumbel/gaussian) <br> 
  * 		Default: gumbel <br>
  * 		Description: Either use Normal or Gumbel distribution for error terms
  * 	</li>
- * <li><strong><font color="red">epsilonScaleFactors</font></strong><br>
+ * <li><strong>epsilonScaleFactors</strong><br>
  * 		Type and range: float > array of double separated by ',' <br> 
  * 		Default: null <br>
  * 		Description: Scale the activities epsilons, separated by comma
  * 	</li>
- * <li><strong><font color="red">pkValuesFile</font></strong><br>
+ * <li><strong>pkValuesFile</strong><br>
  * 		Type and range: String <br> 
  * 		Default: null <br>
  * 		Description: specifies the location of the persons k values file (created during previous pre-processing)
  * 	</li>
- * <li><strong><font color="red">fkValuesFile</font></strong><br>
+ * <li><strong>fkValuesFile</strong><br>
  * 		Type and range: String <br> 
  * 		Default: null <br>
  * 		Description: specifies the location of the facilities k values file (created during previous pre-processing)
  * 	</li>
+ * <li><strong>prefsFile</strong><br>
+ * 		Type and range: String <br>
+ * 		Default: null <br>
+ * 		Description: Replaces the deprecated desires data structure. Specifies the agents desired durations. This should go somewhere else, more into the core.
+ *  </li>
+ *  <li><strong>pBetasFileName</strong><br>
+ * 		Type and range: String <br>
+ * 		Default: null <br>
+ * 		Description: Specifies the beta parameters for additional destination attributes specified in fAttributesFileName below
+ *  </li>
+ *  <li><strong>fAttributesFileName</strong><br>
+ * 		Type and range: String <br>
+ * 		Default: null <br>
+ * 		Description: see pBetasFileName
+ *  </li>
  * </ul>
  * 
  * <h3>Search Space Construction</h3>
@@ -124,14 +116,14 @@
  * 		1: Dijkstra trees forward and backwards See working paper Horni,Nagel,Axhausen 2011, page 13<br>
  * 		2: based on travel distances
  * 	</li>
- * <li><strong><font color="red">maxDistanceEpsilon</font></strong><br>
+ * <li><strong>maxDistanceDCScore</strong><br>
  * 		Type and range: float (-1 or > 0.0) <br> 
  * 		Default: -1 <br>
  * 		Description: Maximum search space radius [m]. "-1" means no explicit fixed search space limitation. 
  * 	</li>
- * <li><strong><font color="red">probChoiceSetSize</font></strong><br>
+ * <li><strong>probChoiceSetSize</strong><br>
  * 		Type and range: int > 0 <br> 
- * 		Default: 10 <br>
+ * 		Default: 5 <br>
  * 		Description: Number of alternatives taken into account for the probabilistic choice. See working paper Horni,Nagel,Axhausen 2011, page 13
  * 	</li>
  *  <li><strong><font color="red">probChoiceExponent</font></strong><br>
@@ -139,29 +131,44 @@
  * 		Default: 3.0 <br>
  * 		Description: Weighting of scores in the reduced choice set subject to probabilistic choice. See working paper Horni,Nagel,Axhausen 2011, page 13
  * 	</li>
- * <li><strong><font color="red">maxEpsFile</font></strong><br>
+ * <li><strong>maxDCScoreFile</strong><br>
  * 		Type and range: String <br> 
  * 		Default: null <br>
- * 		Description: specifies the location of the maps eps file (created during pre-processing)
+ * 		Description: specifies the location of the max dc score file (created during pre-processing). If the k values have not changed from one run to another, the max dc score values can be reused and the pre-processing can be skipped.
  * 	</li>
- *  <li><strong><font color="red">idExclusion</font></strong><br>
+ *  <li><strong>idExclusion</strong><br>
  * 		Type and range: int > 0<br> 
  * 		Default: max int <br>
  * 		Description: exclude all agents from destination choice and analysis with id > analysisIdExclusion. (e.g., border crossers)
  * 	</li>
+ * <li><strong>travelSpeed_car</strong><br>
+ * 		Type and range: double > 0.0<br> 
+ * 		Default: 8.5 m/s<br>
+ * 		Description: approximated assumed car travel speed to convert time into distance for search space construction.
+ * 	</li>
+ * <li><strong>travelSpeed_pt</strong><br>
+ * 		Type and range: double > 0.0<br> 
+ * 		Default: 5 m/s<br>
+ * 		Description: approximated assumed pt travel speed to convert time into distance for search space construction.
+ * 	</li>
+ * <li><strong>destinationSamplePercent</strong><br>
+ * 		Type and range: double, 3 possible values: 100.0, 10.0, and 1.0<br> 
+ * 		Default: 100.0<br>
+ * 		Description: specifies how many percent of the destinations in the choice set should be used for the final choice
+ * 	</li>
  * </ul>
  * 
- * * <h3>Analysis</h3>
+ * <h3>Analysis</h3>
  * <ul>
- * 	<li><strong><font color="red">analysisBoundary</font></strong><br>
+ * 	<li><strong>analysisBoundary</strong><br>
  * 		Type and range: double > 0.0<br> 
  * 		Default: 200km <br>
- * 		Description: boundary (radius) of analysis region (used for distance stats creation)
+ * 		Description: maximum distance in distance stats. Greater values go into the last bin.
  * 	</li>
- * <li><strong><font color="red">analysisBinSize</font></strong><br>
+ * <li><strong>analysisBinSize</strong><br>
  * 		Type and range: double > 0.0<br> 
  * 		Default: 20km <br>
- * 		Description: steps (bin sizes) for analysis (used for distance stats creation)
+ * 		Description: steps (bin sizes) for distance stats
  * 	</li>
  * </ul>
  */
