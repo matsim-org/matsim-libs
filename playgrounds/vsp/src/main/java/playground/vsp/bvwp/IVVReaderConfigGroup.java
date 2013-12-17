78c2f3d1b11bcb4b83588398a9590bc8b3c9a163
dan-ro <daniel_roeder@web.de>
2013-12-17 16:49:08 +0000

    Implemented a first draft of IVVReader. Note, currently it's less a reader because it's not adding the data to a data-structure but more used for data-debugging.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@27233 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/vsp/src/main/java/playground/vsp/bvwp/IVVReaderConfigGroup.java
+++ playgrounds/vsp/src/main/java/playground/vsp/bvwp/IVVReaderConfigGroup.java
@@ -28,10 +28,37 @@ public class IVVReaderConfigGroup extends ReflectiveModule {
 	public static final String GROUP_NAME = "IVVReaderConfigGroup" ;
 	
 	// ---
-	private static final String TRAVEL_TIMES_FILE = "travelTimesMatrixFile" ;
-	private String travelTimesMatrixFile ;
-	// ---
-	// next entry
+	// 00
+	/*package*/static final String DEMAND_FILE = "demandMatrixFile" ;
+	private String demandMatrixFile ;
+	
+	// 01
+	/*package*/static final String REMAINING_DEMAND_FILE = "remainingDemandMatrixFile" ;
+	private String demandRemainingMatrixFile ;
+	
+	// 03
+	/*package*/static final String NEW_DEMAND_FILE = "newDemandMatrixFile" ;
+	private String demandNewMatrixFile ;
+	
+	// 04
+	/*package*/static final String DROPPED_DEMAND_FILE = "droppedDemandMatrixFile" ;
+	private String demandDroppedMatrixFile ;
+
+	// 06
+	/*package*/static final String TRAVEL_TIMES_BASE_FILE = "travelTimesBaseCaseMatrixFile" ;
+	private String travelTimesBaseMatrixFile ;
+	
+	// 07
+	/*package*/static final String TRAVEL_TIMES_STUDY_FILE = "travelTimesStudyMatrixFile" ;
+	private String travelTimesStudyMatrixFile ;
+	
+	// 06 - 15
+	/*package*/static final String IMPEDANCE_FILE = "impedanceMatrixFile" ;
+	private String impedanceMatrixFile ;
+	
+	// 16
+	/*package*/static final String IMPEDANCE_SHIFTED_FILE = "impedanceShiftedMatrixFile" ;
+	private String impedanceShiftedMatrixFile ;
 	// ---
 	
 	/**
@@ -48,13 +75,69 @@ public class IVVReaderConfigGroup extends ReflectiveModule {
 	public IVVReaderConfigGroup( boolean storeUnknownParametersAsStrings) {
 		super(GROUP_NAME, storeUnknownParametersAsStrings);
 	}
-	@StringGetter(TRAVEL_TIMES_FILE)
-	public String getTravelTimesMatrixFile() {
-		return travelTimesMatrixFile;
+	
+	@StringGetter(TRAVEL_TIMES_BASE_FILE)
+	public String getTravelTimesBaseMatrixFile() {
+		return travelTimesBaseMatrixFile;
 	}
-	@StringSetter(TRAVEL_TIMES_FILE)
-	public void setTravelTimesMatrixFile(String travelTimesMatrixFile) {
-		this.travelTimesMatrixFile = travelTimesMatrixFile;
+	@StringSetter(TRAVEL_TIMES_BASE_FILE)
+	public void setTravelTimesBaseMatrixFile(String travelTimesMatrixFile) {
+		this.travelTimesBaseMatrixFile = travelTimesMatrixFile;
 	}
-
+	@StringGetter(TRAVEL_TIMES_STUDY_FILE)
+	public String getTravelTimesStudyMatrixFile() {
+		return travelTimesStudyMatrixFile;
+	}
+	@StringSetter(TRAVEL_TIMES_STUDY_FILE)
+	public void setTravelTimesStudyMatrixFile(String travelTimesStudyMatrixFile) {
+		this.travelTimesStudyMatrixFile = travelTimesStudyMatrixFile;
+	}
+	@StringGetter(IMPEDANCE_FILE)
+	public String getImpedanceMatrixFile() {
+		return impedanceMatrixFile;
+	}
+	@StringSetter(IMPEDANCE_FILE)
+	public void setImpedanceMatrixFile(String impedanceMatrixFile) {
+		this.impedanceMatrixFile = impedanceMatrixFile;
+	}
+	@StringGetter(IMPEDANCE_SHIFTED_FILE)
+	public String getImpedanceShiftedMatrixFile() {
+		return impedanceShiftedMatrixFile;
+	}
+	@StringSetter(IMPEDANCE_SHIFTED_FILE)
+	public void setImpedanceShiftedMatrixFile(String impedanceShiftedMatrixFile) {
+		this.impedanceShiftedMatrixFile = impedanceShiftedMatrixFile;
+	}
+	@StringGetter(DEMAND_FILE)
+	public String getDemandMatrixFile() {
+		return demandMatrixFile;
+	}
+	@StringSetter(DEMAND_FILE)
+	public void setDemandMatrixFile(String demandMatrixFile) {
+		this.demandMatrixFile = demandMatrixFile;
+	}
+	@StringGetter(REMAINING_DEMAND_FILE)
+	public String getRemainingDemandMatrixFile() {
+		return demandRemainingMatrixFile;
+	}
+	@StringSetter(REMAINING_DEMAND_FILE)
+	public void setRemainingDemandMatrixFile(String demandRemainingMatrixFile) {
+		this.demandRemainingMatrixFile = demandRemainingMatrixFile;
+	}
+	@StringGetter(NEW_DEMAND_FILE)
+	public String getNewDemandMatrixFile() {
+		return demandNewMatrixFile;
+	}
+	@StringSetter(NEW_DEMAND_FILE)
+	public void setNewDemandMatrixFile(String newDemandMatrixFile) {
+		this.demandNewMatrixFile = newDemandMatrixFile;
+	}
+	@StringGetter(DROPPED_DEMAND_FILE)
+	public String getDroppedDemandMatrixFile() {
+		return demandDroppedMatrixFile;
+	}
+	@StringSetter(DROPPED_DEMAND_FILE)
+	public void setDroppedDemandMatrixFile(String droppedDemandMatrixFile) {
+		this.demandDroppedMatrixFile = droppedDemandMatrixFile;
+	}
 }
