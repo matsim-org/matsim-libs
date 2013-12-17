3b001d9b166a5f9dd409d6833f647aced0911b06
juliakern <juliakern@users.sf.net>
2013-12-17 18:09:39 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@27237 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/juliakern/src/main/java/playground/julia/toi/CreatePlans.java
+++ playgrounds/juliakern/src/main/java/playground/julia/toi/CreatePlans.java
@@ -21,10 +21,20 @@
 package playground.julia.toi;
 
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.population.*;
+import org.matsim.core.basic.v01.IdImpl;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ActivityParams;
@@ -36,9 +46,10 @@ import org.matsim.core.utils.geometry.transformations.TransformationFactory;
 
 
 
-
 public class CreatePlans {
 	
+	static String csvFile = "input/oslo/Stefan_trondheim.csv"; 
+	static String cvsSplitBy = ",";
 
 		private static final Logger logger = Logger.getLogger(CreatePlans.class);
 
@@ -66,6 +77,7 @@ public class CreatePlans {
 			Scenario scenario = ScenarioUtils.createScenario(config);
 			
 			new MatsimNetworkReader(scenario).readFile("input/oslo/trondheim.xml");
+			//new MatsimNetworkReader(scenario).readFile("input/oslo/Matsim_files_1/trondheim_med_omland_4.sbn");
 			Population pop = fillScenario(scenario);
 			
 			Controler controler = new Controler(scenario);
@@ -75,10 +87,106 @@ public class CreatePlans {
 			
 			new PopulationWriter(pop, scenario.getNetwork()).write("input/oslo/plans.xml");
 		}
+		
+		
+		
 		private static Population fillScenario(Scenario scenario) {
 			Population population = scenario.getPopulation();
 			
 			PopulationFactory populationFactory = population.getFactory();
+			
+			BufferedReader br = null;
+			String line = "";
+			Map<Id, ArrayList<String>> person2lines = new HashMap<Id, ArrayList<String>>();
+		 
+			try {
+		 
+				br = new BufferedReader(new FileReader(csvFile));
+				br.readLine(); //skip first line
+				while ((line = br.readLine()) != null) {
+		 
+				    // use comma as separator
+					String[] trip = new String[line.split(cvsSplitBy).length];
+					trip = line.split(cvsSplitBy);
+					Id personId = new IdImpl(trip[0]);
+					if(!person2lines.containsKey(personId)){
+						person2lines.put(personId, new ArrayList<String>());
+					}
+					person2lines.get(personId).add(line);
+				}
+		 
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				if (br != null) {
+					try {
+						br.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		 
+			System.out.println("Done reading csv file.");
+			
+			CoordinateTransformation ct = //TransformationFactory.getCoordinateTransformation("PROJCS[\"ETRS89_UTM_Zone_33\",GEOGCS[\"GCS_ETRS89\",DATUM[\"D_ETRS89\",SPHEROID[\"GRS_1980\",6378137.0,298.257222101]],PRIMEM[\"Greenwich\",0.0],UNIT[\"Degree\",0.0174532925199433]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"False_Easting\",3500000.0],PARAMETER[\"False_Northing\",0.0],PARAMETER[\"Central_Meridian\",15.0],PARAMETER[\"Scale_Factor\",0.9996],PARAMETER[\"Latitude_Of_Origin\",0.0],UNIT[\"Meter\",1.0]]", "EPSG:3395");
+					  TransformationFactory.getCoordinateTransformation(  TransformationFactory.WGS84, TransformationFactory.WGS84  );
+			//TransformationFactory.getCoordinateTransformation(  TransformationFactory.WGS84, TransformationFactory.CH1903_LV03  );
+			
+			for(Id personId : person2lines.keySet()){
+				System.out.println(personId);
+				Person person = populationFactory.createPerson(personId);
+				population.addPerson(person);
+				Plan plan = populationFactory.createPlan();
+				person.addPlan(plan);
+				//TODO sort!
+				//TODO first activity
+				// TODO last activity without endtime
+				String[] last; String[] trip = null;
+				for(String currline: person2lines.get(personId)){
+						trip = currline.split(cvsSplitBy);
+						Coord startCoordinates = scenario.createCoord(Double.parseDouble(trip[5])/10+70000,Double.parseDouble(trip[4])*10-700000);
+						//create activity
+						String actType = "work";
+						if(trip[2].equals("1"))actType="home";
+						Activity act = populationFactory.createActivityFromCoord(actType, startCoordinates);
+						act.setEndTime(getEndTimeInSeconds(trip[11]));	
+						plan.addActivity(act);
+						//create leg
+						plan.addLeg(populationFactory.createLeg("car"));
+						last= trip;
+				}
+				Coord endCoordinates = scenario.createCoord(Double.parseDouble(trip[8])/10+70000, Double.parseDouble(trip[7])*10-700000);
+				Activity lastAct = populationFactory.createActivityFromCoord("home", endCoordinates);
+				plan.addActivity(lastAct);
+			}
+			/*
+			//+++++++++++++++
+			Id personId = new IdImpl(trip[0]);
+			Person person; Plan plan;
+			if(!population.getPersons().containsKey(personId)){
+				//create new person
+				person = populationFactory.createPerson(personId);
+				plan = populationFactory.createPlan();
+				person.addPlan(plan);
+			}else{
+				person = population.getPersons().get(personId);
+				plan = person.getSelectedPlan(); //TODO ist der selected??
+			}
+
+			//add plan element					
+			CoordinateTransformation ct = //TransformationFactory.getCoordinateTransformation("PROJCS[\"ETRS89_UTM_Zone_33\",GEOGCS[\"GCS_ETRS89\",DATUM[\"D_ETRS89\",SPHEROID[\"GRS_1980\",6378137.0,298.257222101]],PRIMEM[\"Greenwich\",0.0],UNIT[\"Degree\",0.0174532925199433]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"False_Easting\",3500000.0],PARAMETER[\"False_Northing\",0.0],PARAMETER[\"Central_Meridian\",15.0],PARAMETER[\"Scale_Factor\",0.9996],PARAMETER[\"Latitude_Of_Origin\",0.0],UNIT[\"Meter\",1.0]]", "EPSG:3395");
+					  TransformationFactory.getCoordinateTransformation(  TransformationFactory.WGS84, TransformationFactory.CH1903_LV03  );
+			 
+			 Coord startCoordinates = scenario.createCoord(Double.parseDouble(trip[5])/10,Double.parseDouble(trip[4])*10);
+			 Coord endCoordinates = scenario.createCoord(Double.parseDouble(trip[8])/10, Double.parseDouble(trip[7])*10);
+			 
+			 String actType = "home"; //TODO erste activity des tages, typen unterscheiden
+			 Activity activity = populationFactory.createActivityFromCoord(actType, ct.transform(endCoordinates));
+			 //TODO
+			//-----------------------
 			Person person = populationFactory .createPerson(scenario.createId("1"));
 			population.addPerson(person);
 			
@@ -102,7 +210,25 @@ public class CreatePlans {
 			 
 			 Activity activity3 = populationFactory.createActivityFromCoord("home", ct.transform(homeCoordinates));
 			 plan.addActivity(activity3);
+			 
+			 //-----
+			  * */
+			  
 			
 			return population;
+		}
+
+
+
+		private static double getEndTimeInSeconds(String string) {
+			String[]time= string.split(" ");
+			Double pm=0.;
+			if(time[1].equals("PM")){
+				pm=43200.; //12*60*60;
+			}
+			time= time[0].split(":");
+			Double timeInSeconds = 3600*Double.parseDouble(time[0])+60*Double.parseDouble(time[1])+Double.parseDouble(time[2])+pm;
+			// TODO Auto-generated method stub
+			return timeInSeconds;
 		}
 }
