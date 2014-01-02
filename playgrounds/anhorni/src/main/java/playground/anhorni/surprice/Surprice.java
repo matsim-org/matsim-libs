10a9344038ec8980d7cce6fcea7976f09044b194
andreas77 <andreashorni@hotmail.com>
2014-01-02 08:44:30 +0000

    calib RW run
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@27305 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/anhorni/src/main/java/playground/anhorni/surprice/Surprice.java
+++ playgrounds/anhorni/src/main/java/playground/anhorni/surprice/Surprice.java
@@ -30,13 +30,13 @@ public class Surprice {
 	public static final String SURPRICE_RUN = "surprice_run";
 	public static final String SURPRICE_PREPROCESS = "surprice_preprocess";
 	
-	public static int numberOfIncomeCategories = 2;
+	public static int numberOfIncomeCategories = 9;
 	
 	// -------------------------------------------------
-	// betas and constants need to be negative!!!			
-	public static double constant_car = 0.0;
-    public static double constant_pt = 0.0;
-    public static double constant_bike = 0.0;
+	// betas need to be negative!!!			
+	public static double constant_car = -2.5; // vorher -2.0
+    public static double constant_pt = -5.0;
+    public static double constant_bike = -7.0;
     public static double constant_walk = 0.0;
      
     // time disutility
@@ -45,42 +45,42 @@ public class Surprice {
 	public static double beta_TT_car_shp = -6.0 * hs;	
 	public static double beta_TT_car_lei = -6.0 * hs; // -1.0
 	
-	public static double beta_TT_pt_com = 0.0 * hs;		
-	public static double beta_TT_pt_shp = 0.0 * hs;
-	public static double beta_TT_pt_lei = 0.0 * hs;
+	public static double beta_TT_pt_com = -6.0 * hs;		
+	public static double beta_TT_pt_shp = -6.0 * hs;
+	public static double beta_TT_pt_lei = -6.0 * hs;
 	
 	public static double beta_TT_bike_com = -6.0 * hs;
-	public static double beta_TT_bike_shp = -3.0 * hs;
-	public static double beta_TT_bike_lei = -1.0 * hs;
+	public static double beta_TT_bike_shp = -6.0 * hs;
+	public static double beta_TT_bike_lei = -6.0 * hs;
 	
-	public static double beta_TT_walk_com = -0.5 * hs;
-	public static double beta_TT_walk_shp = -0.5 * hs;
-	public static double beta_TT_walk_lei = -0.5 * hs;
+	public static double beta_TT_walk_com = -6.0 * hs;
+	public static double beta_TT_walk_shp = -6.0 * hs;
+	public static double beta_TT_walk_lei = -6.0 * hs;
 
 	// distance disutility
 	private static double km = 0.001; // scaling from km to m
-	public static double beta_TD_car_com = 0.0 * km;
-	public static double beta_TD_car_shp = 0.0 * km;
-	public static double beta_TD_car_lei = 0.0 * km;
+	public static double beta_TD_car_com = -0.2 * km;
+	public static double beta_TD_car_shp = -0.2 * km;
+	public static double beta_TD_car_lei = -0.2 * km;
 	
 	public static double beta_TD_pt_com = 0.0;
 	public static double beta_TD_pt_shp = 0.0;
 	public static double beta_TD_pt_lei = 0.0;
 	
-	public static double beta_TD_bike_com = 0.0 * km;
-	public static double beta_TD_bike_shp = 0.0 * km;
-	public static double beta_TD_bike_lei = 0.0 * km;
+	public static double beta_TD_bike_com = -4.0 * km;
+	public static double beta_TD_bike_shp = -4.0 * km;
+	public static double beta_TD_bike_lei = -4.0 * km;
 	
-	public static double beta_TD_walk_com = 0.0 * km;
-	public static double beta_TD_walk_shp = 0.0 * km;
-	public static double beta_TD_walk_lei = 0.0 * km;
+	public static double beta_TD_walk_com = -1.0 * km;
+	public static double beta_TD_walk_shp = -1.0 * km;
+	public static double beta_TD_walk_lei = -1.0 * km;
 	
 	// monetary costs
-	public static double distanceCost_car = 0.0; 
-	public static double constantCost_car = 0.0; // parking
+	public static double distanceCost_car = -0.0005; 
+	public static double constantCost_car = -5.0; // parking
 	
-	public static double distanceCost_pt = 0.0;
-	public static double constantCost_pt = 0.0; // zone tickets
+	public static double distanceCost_pt = -0.0001;
+	public static double constantCost_pt = -5.0; // zone tickets
 	
 	public static double distanceCost_bike = 0.0;
 	public static double constantCost_bike = 0.0;
