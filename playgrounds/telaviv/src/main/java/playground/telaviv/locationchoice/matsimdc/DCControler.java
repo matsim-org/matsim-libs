ddabc5f072f422b307f15492d844a03f9ab53f36
andreas77 <andreashorni@hotmail.com>
2014-01-13 10:14:15 +0000

    blow up DCControler
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@27371 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/telaviv/src/main/java/playground/telaviv/locationchoice/matsimdc/DCControler.java
+++ playgrounds/telaviv/src/main/java/playground/telaviv/locationchoice/matsimdc/DCControler.java
@@ -19,21 +19,57 @@
 
 package playground.telaviv.locationchoice.matsimdc;
 
-import org.matsim.contrib.locationchoice.bestresponse.DestinationChoiceBestResponseContext;
 
-public class DCControler extends org.matsim.contrib.locationchoice.DCControler {
+import org.matsim.contrib.locationchoice.bestresponse.DestinationChoiceBestResponseContext;
+import org.matsim.contrib.locationchoice.bestresponse.DestinationChoiceInitializer;
+import org.matsim.contrib.locationchoice.bestresponse.scoring.DCScoringFunctionFactory;
+import org.matsim.contrib.locationchoice.facilityload.FacilitiesLoadCalculator;
+import org.matsim.core.controler.Controler;
+
+public class DCControler extends Controler {
 	
 	private DestinationChoiceBestResponseContext dcContext;
-				
+	
+	
 	public DCControler(final String[] args) {
-		super(args);
+		super(args);	
 		this.setOverwriteFiles(true);
 		this.init();
 	}
-		
+	 
+	
 	private void init() {
+		/*
+		 * would be muuuuch nicer to have this in DestinationChoiceInitializer, but startupListeners are called after corelisteners are called
+		 * -> scoringFunctionFactory cannot be replaced
+		 */
 		this.dcContext = new DestinationChoiceBestResponseContext(super.getScenario());	
+		/* 
+		 * add ScoringFunctionFactory to controler
+		 *  in this way scoringFunction does not need to create new, identical k-vals by itself    
+		 */
   		DCScoringFunctionFactory dcScoringFunctionFactory = new DCScoringFunctionFactory(this.getConfig(), this, this.dcContext); 	
-		super.setScoringFunctionFactory(dcScoringFunctionFactory);		
+		super.setScoringFunctionFactory(dcScoringFunctionFactory);
+		
+		if (!super.getConfig().locationchoice().getPrefsFile().equals("null") &&
+				!super.getConfig().facilities().getInputFile().equals("null")) {
+			dcScoringFunctionFactory.setUsingConfigParamsForScoring(false);
+		} else {
+			dcScoringFunctionFactory.setUsingConfigParamsForScoring(true);
+			log.info("external prefs are not used for scoring!");
+		}		
+	}
+	
+	protected void loadControlerListeners() {
+		this.dcContext.init(); // this is an ugly hack, but I somehow need to get the scoring function + context into the controler
+
+		this.addControlerListener(new DestinationChoiceInitializer(this.dcContext));
+		
+		if (Double.parseDouble(super.getConfig().locationchoice().getRestraintFcnExp()) > 0.0 &&
+				Double.parseDouble(super.getConfig().locationchoice().getRestraintFcnFactor()) > 0.0) {		
+					this.addControlerListener(new FacilitiesLoadCalculator(this.dcContext.getFacilityPenalties()));
+				}
+		
+		super.loadControlerListeners();
 	}
 }
