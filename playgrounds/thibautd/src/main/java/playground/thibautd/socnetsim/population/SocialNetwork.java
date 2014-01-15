b022692da66cb4f78d30376d3e1e22e1d3c936d3
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2014-01-15 11:29:23 +0000

    add a tag "isReflective" to take advantage of reflectivity to reduce dumped file
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@27458 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/java/playground/thibautd/socnetsim/population/SocialNetwork.java
+++ playgrounds/thibautd/src/main/java/playground/thibautd/socnetsim/population/SocialNetwork.java
@@ -37,12 +37,37 @@ public class SocialNetwork {
 	private final Counter tieCounter = new Counter( "SocialNetwork: (Monodirectional) Tie # " );
 	private final Map<Id, Set<Id>> map = new HashMap<Id, Set<Id>>();
 
+	private final boolean isReflective;
+
+	public SocialNetwork() {
+		this( true );
+	}
+
+	/**
+	 * @param isReflective
+	 * indicates whether social ties are directed.
+	 * tagging a social network as reflective allows to reduce
+	 * the size of the output by half.
+	 */
+	public SocialNetwork(final boolean isReflective) {
+		this.isReflective = isReflective;
+	}
+
 	public void addBidirectionalTie(final Id id1, final Id id2) {
-		addMonodirectionalTie( id1 , id2 );
-		addMonodirectionalTie( id2 , id1 );
+		addTieInternal( id1 , id2 );
+		addTieInternal( id2 , id1 );
 	}
 
 	public void addMonodirectionalTie(
+			final Id ego,
+			final Id alter) {
+		if ( isReflective ) {
+			throw new IllegalStateException( "cannot add a monodirectional tie to a reflective social network." );
+		}
+		addTieInternal( ego , alter );
+	}
+
+	private void addTieInternal(
 			final Id ego,
 			final Id alter) {
 		final Set<Id> alters = MapUtils.getSet( ego , map );
@@ -52,7 +77,17 @@ public class SocialNetwork {
 
 	public Set<Id> getAlters(final Id ego) {
 		final Set<Id> alters = map.get( ego );
-		return alters == null ? Collections.<Id>emptySet() : alters;
+		return alters == null ?
+			Collections.<Id>emptySet() :
+			Collections.unmodifiableSet( alters );
+	}
+
+	public Set<Id> getEgos() {
+		return Collections.unmodifiableSet( map.keySet() );
+	}
+
+	public boolean isReflective() {
+		return isReflective;
 	}
 }
 
