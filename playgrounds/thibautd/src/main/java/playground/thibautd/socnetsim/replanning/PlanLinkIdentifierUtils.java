1fa9b9547795fea2c58a6e6bf67fea01c9345bf3
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2014-01-28 09:16:16 +0000

    a static factory for plan link identifiers
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@27697 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/thibautd/src/main/java/playground/thibautd/socnetsim/replanning/PlanLinkIdentifierUtils.java
@@ -0,0 +1,50 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * PlanLinkIdentifierUtils.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2014 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.thibautd.socnetsim.replanning;
+
+import playground.thibautd.socnetsim.population.SocialNetwork;
+import playground.thibautd.socnetsim.replanning.modules.PlanLinkIdentifier;
+
+/**
+ * @author thibautd
+ */
+public class PlanLinkIdentifierUtils {
+	private PlanLinkIdentifierUtils() {}
+
+	public static PlanLinkIdentifier createDefaultPlanLinkIdentifier() {
+		return new CompositePlanLinkIdentifier(
+				new JointTripsPlanLinkIdentifier(),
+				new VehicularPlanBasedIdentifier() );
+	}
+
+	public static PlanLinkIdentifier createPlanLinkIdentifierForSocialActivities(
+			final String activityType,
+			final SocialNetwork socialNetwork) {
+		final CompositePlanLinkIdentifier id =
+			new CompositePlanLinkIdentifier(
+				new JointTripsPlanLinkIdentifier(),
+				new VehicularPlanBasedIdentifier(),
+				new JoinableActivitiesPlanLinkIdentifier( activityType ) );
+
+		id.addAndComponent( new SocialNetworkPlanLinkIdentifier( socialNetwork ) );
+		return id;
+	}
+}
+
