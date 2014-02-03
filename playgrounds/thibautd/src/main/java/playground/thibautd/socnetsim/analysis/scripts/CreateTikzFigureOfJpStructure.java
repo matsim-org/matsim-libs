aa07aaed62ba14693eb26a2fa04453fd0f8faccd
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2014-02-03 09:29:59 +0000

    move scripts for analysis of joint simulation in their own package
    playground.thibautd.scripts is quide crowded...
    
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@27773 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/java/playground/thibautd/socnetsim/analysis/scripts/CreateTikzFigureOfJpStructure.java
+++ playgrounds/thibautd/src/main/java/playground/thibautd/socnetsim/analysis/scripts/CreateTikzFigureOfJpStructure.java
@@ -17,7 +17,7 @@
  *   See also COPYING, LICENSE and WARRANTY file                           *
  *                                                                         *
  * *********************************************************************** */
-package playground.thibautd.scripts;
+package playground.thibautd.socnetsim.analysis.scripts;
 
 import java.util.ArrayList;
 import java.util.Collections;
--- playgrounds/thibautd/src/main/java/playground/thibautd/socnetsim/analysis/scripts/DumpSizeOfSelectedJointPlans.java
+++ playgrounds/thibautd/src/main/java/playground/thibautd/socnetsim/analysis/scripts/DumpSizeOfSelectedJointPlans.java
@@ -17,7 +17,7 @@
  *   See also COPYING, LICENSE and WARRANTY file                           *
  *                                                                         *
  * *********************************************************************** */
-package playground.thibautd.scripts;
+package playground.thibautd.socnetsim.analysis.scripts;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
