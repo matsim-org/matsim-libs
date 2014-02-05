4803b9310ea6f2c86cafb5e603c7dad7a4a5b215
vsp_nagel <2008@kainagel.org>
2014-02-05 19:26:56 +0000

    kai playground change
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@27832 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/kai/src/main/java/playground/kai/run/KNPlansAnalyzer.java
@@ -0,0 +1,73 @@
+/* *********************************************************************** *
+ * project: org.matsim.*												   *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.kai.run;
+
+import java.util.Formatter;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.scenario.ScenarioUtils;
+
+import playground.southafrica.gauteng.GautengTollStatistics;
+import playground.southafrica.gauteng.GautengTollStatistics.SimplifiedType;
+import playground.southafrica.gauteng.roadpricingscheme.SanralTollFactorOLD;
+import playground.southafrica.gauteng.roadpricingscheme.SanralTollVehicleType;
+import playground.southafrica.gauteng.roadpricingscheme.TollFactorI;
+
+/**
+ * @author nagel
+ *
+ */
+public class KNPlansAnalyzer {
+
+	public static void main(String[] args) {
+
+		Config config = ConfigUtils.createConfig() ;
+
+		config.plans().setInputFile(args[0]); 
+
+		Scenario scenario = ScenarioUtils.loadScenario(config) ;
+
+		TollFactorI tollFactor = new SanralTollFactorOLD() ;
+
+		Map<SimplifiedType,Double> scoreSum = new HashMap<SimplifiedType,Double>() ;
+		Map<SimplifiedType,Double> cnt = new HashMap<SimplifiedType,Double>() ;
+		for ( SimplifiedType stype : SimplifiedType.values() ) {
+			scoreSum.put( stype, 0. ) ;
+			cnt.put( stype, 0. ) ;
+		}
+		for ( Person person : scenario.getPopulation().getPersons().values() ) {
+			SanralTollVehicleType type = tollFactor.typeOf(person.getId()) ;
+			SimplifiedType stype = GautengTollStatistics.simplifiedTypeOf( type ) ;
+			scoreSum.put( stype, scoreSum.get(stype) + person.getSelectedPlan().getScore() ) ;
+			cnt.put( stype,  cnt.get(stype) + 1. ) ;
+		}
+
+		for ( SimplifiedType stype : SimplifiedType.values() ) {
+			if ( cnt.get(stype) > 0. ) {
+				System.out.println( stype.toString() + ": " + scoreSum.get(stype) / cnt.get(stype) ) ;
+			}
+
+		}
+	}
+}
