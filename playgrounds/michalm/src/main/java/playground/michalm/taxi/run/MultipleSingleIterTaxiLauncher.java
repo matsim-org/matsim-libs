11f39776731f45d3a7633b15918f720d62a4cc4a
michal-m <michal.mac@gmail.com>
2014-02-07 11:40:06 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@27863 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/michalm/src/main/java/playground/michalm/taxi/run/MultipleSingleIterTaxiLauncher.java
+++ playgrounds/michalm/src/main/java/playground/michalm/taxi/run/MultipleSingleIterTaxiLauncher.java
@@ -77,20 +77,20 @@ import playground.michalm.taxi.optimizer.TaxiStatsCalculator.TaxiStats;
         boolean warmup;
 
         switch (configIdx) {
-//            case 0:
-//            case 1:
-//            case 3:
-//            case 4:
-//            case 5:
-//            case 6:
-//            case 8:
-//            case 9:
-//            case 10:
-//            case 11:
-//            case 12:
-//            case 13:
-//            case 14:
-//            case 15:
+            case 0:
+            case 1:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
             case 16:
             case 17:
             case 18:
