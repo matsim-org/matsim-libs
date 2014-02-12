7f5eaf9be376bfe7840d7f7230f20287f64da590
skull5 <dobler@senozon.com>
2014-02-12 17:35:51 +0000

    Fixed not compiling playground.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@27940 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/christoph/src/main/java/playground/christoph/strc2014/STRC2014Runner.java
+++ playgrounds/christoph/src/main/java/playground/christoph/strc2014/STRC2014Runner.java
@@ -37,8 +37,8 @@ import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.network.LinkImpl;
+import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.core.utils.misc.NetworkUtils;
 
 import playground.christoph.parking.ParkingTypes;
 import playground.christoph.parking.WithinDayParkingControlerListener;
