bb04e01aca9dad50bc41441cb96f82e8740ccb87
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2014-02-13 13:02:27 +0000

    more tests
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@27945 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/java/playground/thibautd/scripts/ComparePerfQuadTreeCircularElliptical.java
+++ playgrounds/thibautd/src/main/java/playground/thibautd/scripts/ComparePerfQuadTreeCircularElliptical.java
@@ -19,6 +19,9 @@
  * *********************************************************************** */
 package playground.thibautd.scripts;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.apache.log4j.Logger;
 
 import org.matsim.core.utils.collections.QuadTree;
@@ -31,26 +34,92 @@ public class ComparePerfQuadTreeCircularElliptical {
 		Logger.getLogger(ComparePerfQuadTreeCircularElliptical.class);
 
 	public static void main(final String[] args) {
-		final QuadTree<Object> qt = new QuadTree<Object>( 0 , 0 , 1 , 1 );
+		for ( Settings settings : Arrays.asList(
+					new Settings(
+						// few points
+						0, 0, 1,
+						-0.5, 0, 0.5, 0, 2,
+						0.1 ),
+					new Settings(
+						// lots points
+						0, 0, 1,
+						-0.5, 0, 0.5, 0, 2,
+						0.001 ),
+					new Settings(
+						// with useless space
+						0, 0, 0.25,
+						-0.2, 0, 0.2, 0, .45,
+						0.0003 ),
+					new Settings(
+						// with lots ofuseless space
+						0, 0, 0.025,
+						-0.02, 0, 0.02, 0, .045,
+						0.0003 )
+					) ) {
+			log.info( "######################################################################" );
+			log.info( "start new test: "+settings );
+			log.info( "build quad tree" );
+			final QuadTree<Object> qt = new QuadTree<Object>( -1 , -1 , 1 , 1 );
 
-		for ( double x = 0; x <= 1; x += 0.001 ) {
-			for ( double y = 0; y <= 1; y += 0.001 ) {
-				qt.put( x , y , new Object() );
+			for ( double x = 0; x <= 1; x += settings.pointStep ) {
+				for ( double y = 0; y <= 1; y += settings.pointStep ) {
+					qt.put( x , y , new Object() );
+				}
+			}
+			log.info( "build quad tree: DONE" );
+
+			{
+			final double startCircular = System.currentTimeMillis();
+				final Collection<Object> circle = qt.get(
+						settings.centerCircleX,
+						settings.centerCircleY,
+						settings.radiusCircle );
+				final double endCircular = System.currentTimeMillis();
+				log.info( "time circular: "+(endCircular - startCircular)+"ms" );
+				log.info( circle.size()+" elements in disk" );
+			}
+
+			{
+			final double startElliptical = System.currentTimeMillis();
+				final Collection<Object> ellipse = qt.getElliptical(
+						settings.ellipseX1 , settings.ellipseY1 ,
+						settings.ellipseX2 , settings.ellipseY2 ,
+						settings.ellipseDiameter );
+				final double endElliptical = System.currentTimeMillis();
+				log.info( "time elliptical: "+(endElliptical - startElliptical)+"ms" );
+				log.info( ellipse.size()+" elements in ellipse" );
 			}
 		}
+	}
 
-		final double startCircular = System.currentTimeMillis();
-		qt.get( 0.5 , 0.5 , 0.5 );
-		final double endCircular = System.currentTimeMillis();
-		log.info( "time circular: "+(endCircular - startCircular)+"ms" );
+	private static final class Settings {
+		public final double centerCircleX, centerCircleY, radiusCircle;
+		public final double ellipseX1, ellipseY1;
+		public final double ellipseX2, ellipseY2;
+		public final double ellipseDiameter;
+		public final double pointStep;
+		
+		public Settings(double centerCircleX, double centerCircleY,
+				double radiusCircle, double ellipseX1, double ellipseY1,
+				double ellipseX2, double ellipseY2, double ellipseDiameter,
+				double pointStep) {
+			this.centerCircleX = centerCircleX;
+			this.centerCircleY = centerCircleY;
+			this.radiusCircle = radiusCircle;
+			this.ellipseX1 = ellipseX1;
+			this.ellipseY1 = ellipseY1;
+			this.ellipseX2 = ellipseX2;
+			this.ellipseY2 = ellipseY2;
+			this.ellipseDiameter = ellipseDiameter;
+			this.pointStep = pointStep;
+		}
 
-		final double startElliptical = System.currentTimeMillis();
-		qt.getElliptical(
-				0.25 , 0.5 ,
-				0.75 , 0.5 ,
-				1 );
-		final double endElliptical = System.currentTimeMillis();
-		log.info( "time elliptical: "+(endElliptical - startElliptical)+"ms" );
+		@Override
+		public String toString() {
+			return "center( "+centerCircleX+" , "+centerCircleY+" , "+radiusCircle+" ) "+
+				"ellipse( "+ellipseX1+" , "+ellipseY1+" , "+ellipseX2+" , "+ellipseY2+" , "+ellipseDiameter+" ) "+
+				"step="+pointStep;
+		}
 	}
 }
 
