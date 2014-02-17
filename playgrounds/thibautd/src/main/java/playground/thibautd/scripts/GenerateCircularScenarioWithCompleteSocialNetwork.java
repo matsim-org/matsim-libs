452f7488f8b094d25984eb5c9a8d0d4fb6bdaf88
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2014-02-17 15:18:18 +0000

    generate a test scenario for joint location choice
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@27967 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/thibautd/src/main/java/playground/thibautd/scripts/GenerateCircularScenarioWithCompleteSocialNetwork.java
@@ -0,0 +1,255 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * GenerateCircularScenarioWithCompleteSocialNetwork.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2014 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.thibautd.scripts;
+
+import java.io.File;
+
+import java.util.Random;
+
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.network.Node;
+import org.matsim.api.core.v01.population.Activity;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Plan;
+import org.matsim.api.core.v01.population.Population;
+import org.matsim.api.core.v01.population.PopulationFactory;
+import org.matsim.api.core.v01.population.PopulationWriter;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.api.experimental.facilities.ActivityFacilities;
+import org.matsim.core.api.experimental.facilities.ActivityFacility;
+import org.matsim.core.basic.v01.IdImpl;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.facilities.ActivityFacilityImpl;
+import org.matsim.core.facilities.ActivityOption;
+import org.matsim.core.facilities.FacilitiesWriter;
+import org.matsim.core.network.NetworkWriter;
+import org.matsim.core.population.ActivityImpl;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.geometry.CoordImpl;
+import org.matsim.core.utils.misc.Time;
+import org.matsim.population.Desires;
+import org.matsim.utils.objectattributes.ObjectAttributesXmlWriter;
+
+import playground.thibautd.socnetsim.population.SocialNetwork;
+import playground.thibautd.socnetsim.population.SocialNetworkImpl;
+import playground.thibautd.socnetsim.population.SocialNetworkWriter;
+import playground.thibautd.utils.ArgParser;
+import playground.thibautd.utils.CollectionUtils;
+import playground.thibautd.utils.DesiresConverter;
+
+/**
+ * @author thibautd
+ */
+public class GenerateCircularScenarioWithCompleteSocialNetwork {
+	public static void main(final String[] args) {
+		final ArgParser argParser = new ArgParser( args );
+		argParser.setDefaultValue( "--radius" , "10000" );
+		argParser.setDefaultValue( "--nlinks" , "100" );
+		argParser.setDefaultValue( "--nagentsperlink" , "1" );
+		argParser.setDefaultValue( "--nseparationlinks" , "5" );
+
+		final int nLinks = Integer.parseInt( argParser.getValue( "--nlinks" ) );
+		final int nAgentsPerLink = Integer.parseInt( argParser.getValue( "--nagentsperlink" ) );
+		final int nSeparationLinks = Integer.parseInt( argParser.getValue( "--nseparationlinks" ) );
+		final double radius = Integer.parseInt( argParser.getValue( "--radius" ) );
+
+		final String outputDirectory = argParser.getNonSwitchedArgs()[ 0 ];
+		createDirectory( outputDirectory );
+
+		final Scenario scenario = ScenarioUtils.createScenario( ConfigUtils.createConfig() );
+
+		final Network net = scenario.getNetwork();
+		final ActivityFacilities facilities = scenario.getActivityFacilities();
+		final Population population = scenario.getPopulation();
+
+		final double angleStep = 360d / nLinks;
+		Node firstNode = null;
+		Node lastNode = null;
+		for ( int i = 0; i < nLinks; i++ ) {
+			final double angle = i * angleStep;
+			final Node newNode = net.getFactory().createNode(
+					new IdImpl( "node-"+angle ),
+					new CoordImpl(
+						radius * Math.cos( Math.PI * angle / 180 ),
+						radius * Math.sin( Math.PI * angle / 180 ) ) );
+			net.addNode( newNode );
+			if ( firstNode == null ) firstNode = newNode;
+
+			if ( lastNode != null ) {
+				final Link l = createAndAddLink( net , lastNode , newNode );
+				createAndAddLink( net , newNode , lastNode );
+
+				if ( i % nSeparationLinks == 0 ) {
+					createAndAddFacility( facilities , l );
+				}
+			}
+
+			lastNode = newNode;
+		}
+
+		/* connect two ends */ {
+			final Link l = createAndAddLink( net , firstNode , lastNode );
+			createAndAddLink( net , lastNode , firstNode );
+
+			if ( nLinks % nSeparationLinks == 0 ) {
+				createAndAddFacility( facilities , l );
+			}
+		}
+
+		final Random random = new Random( 4567 );
+		final Desires desires = new Desires( null );
+		desires.putActivityDuration( "home" , 14 );
+		desires.putActivityDuration( "leisure" , 10 );
+		for ( ActivityFacility facility : facilities.getFacilities().values() ) {
+			for ( int i = 0; i < nAgentsPerLink; i++ ) {
+				final Person person = population.getFactory().createPerson( new IdImpl( facility.getId()+"-"+i ) );
+				population.addPerson( person );
+				final Plan plan = population.getFactory().createPlan();
+				plan.setPerson( person );
+				person.addPlan( plan );
+
+				plan.addActivity(
+						createActivity(
+							population.getFactory(),
+							"home",
+							facility,
+							8 * 3600d ) );
+				plan.addActivity(
+						createActivity(
+							population.getFactory(),
+							"leisure",
+							CollectionUtils.getRandomElement(
+								random,
+								facilities.getFacilities() ).getValue(),
+							18 * 3600d ) );
+				plan.addActivity(
+						createActivity(
+							population.getFactory(),
+							"home",
+							facility,
+							Time.UNDEFINED_TIME ) );
+
+				population.getPersonAttributes().putAttribute(
+						""+person.getId(),
+						"desires",
+						desires );
+			}
+		}
+
+		final SocialNetwork socialNetwork = createSocialNetwork( population );
+
+		new SocialNetworkWriter( socialNetwork ).write( outputDirectory+"/social_network.xml.gz" );
+		new NetworkWriter( net ).write( outputDirectory+"/network.xml.gz" );
+		new PopulationWriter( population , net ).write( outputDirectory+"/plans.xml.gz" );
+		new FacilitiesWriter( facilities ).write( outputDirectory+"/facilities.xml.gz" );
+
+		final ObjectAttributesXmlWriter attWriter =
+			new ObjectAttributesXmlWriter( population.getPersonAttributes() );
+		attWriter.putAttributeConverter(
+				Desires.class,
+				new DesiresConverter() );
+		attWriter.writeFile( outputDirectory+"/person_attributes.xml.gz" );
+	}
+
+	private static SocialNetwork createSocialNetwork(
+			final Population population) {
+		final Person[] persons =
+			population.getPersons().values().toArray(
+					new Person[ population.getPersons().size() ] );
+
+		final SocialNetwork net = new SocialNetworkImpl( true );
+
+		for ( Person p : persons ) net.addEgo( p.getId() );
+
+		for ( int i=0; i < persons.length; i++ ) {
+			for ( int j=i+1; j < persons.length; j++ ) {
+				net.addBidirectionalTie(
+						persons[ i ].getId(),
+						persons[ j ].getId() );
+			}
+		}
+
+		return net;
+	}
+
+	private static void createDirectory(final String outputDirectory) {
+		final File file = new File( outputDirectory );
+		if ( file.exists() ) {
+			if ( !file.isDirectory() ) {
+				throw new RuntimeException( outputDirectory+" exists and is a file" );
+			}
+			if ( file.listFiles().length > 0 ) {
+				throw new RuntimeException( outputDirectory+" exists and is not empty" );
+			}
+		}
+		else {
+			file.mkdirs();
+		}
+	}
+
+	private static Activity createActivity(
+			final PopulationFactory factory,
+			final String type,
+			final ActivityFacility facility,
+			final double time) {
+		final Activity activity =
+			factory.createActivityFromLinkId(
+					type,
+					facility.getLinkId() );
+		((ActivityImpl) activity).setCoord( facility.getCoord() );
+		((ActivityImpl) activity).setFacilityId( facility.getId() );
+
+		activity.setEndTime( time );
+
+		return activity;
+	}
+
+	private static void createAndAddFacility(
+			final ActivityFacilities facilities,
+			final Link l) {
+		final ActivityFacility facility =
+			facilities.getFactory().createActivityFacility(
+					l.getId(),
+					l.getCoord() );
+		((ActivityFacilityImpl) facility).setLinkId( l.getId() );
+		facilities.addActivityFacility( facility );
+
+		final ActivityOption home = facilities.getFactory().createActivityOption( "home" );
+		facility.addActivityOption( home );
+
+		final ActivityOption leisure = facilities.getFactory().createActivityOption( "leisure" );
+		facility.addActivityOption( leisure );
+	}
+
+	private static Link createAndAddLink( 
+			final Network net,
+			final Node from,
+			final Node to) {
+		final Link l =
+			net.getFactory().createLink(
+				new IdImpl( from.getId() +"^"+ to.getId() ),
+				from,
+				to );
+		net.addLink( l );
+		return l;
+	}
+}
+
