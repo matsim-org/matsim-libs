5f97c636f12fd339e6d001926e4fd3e8dea98f6f
jwjoubert <johan.joubert@up.ac.za>
2014-02-23 06:31:06 +0000

    Adapted controller to run separate files for with, and without toll.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@28019 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/southAfrica/src/main/java/playground/southafrica/gauteng/JWJGautengController_Toll.java
+++ playgrounds/southAfrica/src/main/java/playground/southafrica/gauteng/JWJGautengController_Toll.java
@@ -2,7 +2,7 @@ package playground.southafrica.gauteng;
 
 import playground.southafrica.gauteng.GautengControler_subpopulations.User;
 
-public class JWJGautengController {
+public class JWJGautengController_Toll {
 	
 	static String GAUTENG_PATH = "/Users/jwjoubert/Documents/workspace/data-gauteng/" ;
 
@@ -15,16 +15,16 @@ public class JWJGautengController {
 		
 		String[] args2 = {
 				GAUTENG_PATH + "config/basicConfig.xml", // config
-				GAUTENG_PATH + "population/20140208/gauteng.xml.gz", // pop
+				"output_base/output_plans.xml.gz", // pop
 				GAUTENG_PATH + "population/20140208/gautengAttr.xml.gz", // pop attribs 
 				GAUTENG_PATH + "network/gauteng_20131210_coarseNationalNetwork_clean.xml.gz", // net 
-				GAUTENG_PATH + "toll/gauteng_toll_weekday_Existing_20131211.xml", // toll
+				GAUTENG_PATH + "toll/gauteng_toll_weekday_Combined_20131211.xml", // toll
 				"110", // base VoT
 				"3", // VoT multiplier
 				"8", // number of threads
 				User.johan.toString(),
 //				"/Users/jwjoubert/Documents/Temp/sanral-runs/output/"
-				"./output_base/"
+				"./output_toll/"
 				} ;
 		GautengControler_subpopulations.main(args2);
 	}
