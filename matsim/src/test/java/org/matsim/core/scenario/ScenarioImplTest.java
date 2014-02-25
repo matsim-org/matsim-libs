afc781a0abe079e8f19fd9bb40fb45dda1d91b43
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2014-02-25 13:22:36 +0000

    move pre-existing test methods of ScenarioImpl to the right place
    they were in org.matsim.api.core.v01 while ScenarioImpl is in
    org.matsim.core.scenario
    
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@28075 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/test/java/org/matsim/core/scenario/ScenarioImplTest.java
+++ matsim/src/test/java/org/matsim/core/scenario/ScenarioImplTest.java
@@ -22,6 +22,7 @@ package org.matsim.core.scenario;
 import org.junit.Assert;
 import org.junit.Test;
 
+import org.matsim.api.core.v01.Id;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.households.Households;
@@ -121,5 +122,90 @@ public class ScenarioImplTest {
 		Assert.assertTrue( "schedule said not created" , created );
 		Assert.assertFalse( "schedule said recreated" , recreated );
 	}
+
+	/**
+	 * Tests that {@link ScenarioImpl#createId(String)} returns the same
+	 * Id object for equil Strings.
+	 */
+	@Test
+	public void testCreateId_sameObjectForSameId() {
+		ScenarioImpl s = (ScenarioImpl) ScenarioUtils.createScenario(ConfigUtils.createConfig());
+		String str1 = "9832";
+		String str2 = new String(str1);
+		Assert.assertNotSame(str1, str2);
+		Assert.assertEquals(str1, str2);
+		Id id1 = s.createId(str1);
+		Id id2 = s.createId(str2);
+		Id id3 = s.createId(str1);
+		Assert.assertSame(id1, id2);
+		Assert.assertSame(id1, id3);
+		Assert.assertSame(id2, id3);
+	}
+
+	@Test
+	public void testAddAndGetScenarioElement() {
+		final ScenarioImpl s = (ScenarioImpl) ScenarioUtils.createScenario(ConfigUtils.createConfig());
+
+		final Object element1 = new Object();
+		final String name1 = "peter_parker";
+		final Object element2 = new Object();
+		final String name2 = "popeye";
+
+		s.addScenarioElement( name1 , element1 );
+		s.addScenarioElement( name2 , element2 );
+		Assert.assertSame(
+				"unexpected scenario element",
+				element1,
+				s.getScenarioElement( name1 ) );
+		// just check that it is got, not removed
+		Assert.assertSame(
+				"unexpected scenario element",
+				element1,
+				s.getScenarioElement( name1 ) );
+
+		Assert.assertSame(
+				"unexpected scenario element",
+				element2,
+				s.getScenarioElement( name2 ) );
+
+	}
+
+	@Test
+	public void testCannotAddAnElementToAnExistingName() {
+		final ScenarioImpl s = (ScenarioImpl) ScenarioUtils.createScenario(ConfigUtils.createConfig());
+
+		final String name = "bruce_wayne";
+
+		s.addScenarioElement( name , new Object() );
+		try {
+			s.addScenarioElement( name , new Object() );
+		}
+		catch (IllegalStateException e) {
+			return;
+		}
+		catch (Exception e) {
+			Assert.fail( "wrong exception thrown when trying to add an element for an existing name "+e.getClass().getName() );
+		}
+		Assert.fail( "no exception thrown when trying to add an element for an existing name" );
+	}
+
+	@Test
+	public void testRemoveElement() {
+		final ScenarioImpl s = (ScenarioImpl) ScenarioUtils.createScenario(ConfigUtils.createConfig());
+
+		final Object element = new Object();
+		final String name = "clark_kent";
+
+		s.addScenarioElement( name , element );
+		Assert.assertSame(
+				"unexpected removed element",
+				element,
+				s.removeScenarioElement( name ) );
+		Assert.assertNull(
+				"element was not removed",
+				s.getScenarioElement( name ) );
+
+	}
+
 }
 
