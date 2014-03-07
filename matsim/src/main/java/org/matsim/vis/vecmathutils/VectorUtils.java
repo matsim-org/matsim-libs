b9b158042b3da5d79326b8286fc554ab1e9c312e
tthunig <tthunig@vsp.tu-berlin.de>
2014-03-07 08:29:17 +0000

    forgotten commit. assigns to build #8514
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@28214 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/vis/vecmathutils/VectorUtils.java
+++ matsim/src/main/java/org/matsim/vis/vecmathutils/VectorUtils.java
@@ -26,17 +26,56 @@ import org.matsim.core.utils.collections.Tuple;
 /**
  * Util class containing stateless vecmath utility methods frequently needed for visualization code.
  * @author dgrether
+ * @author tthunig
  * 
  */
 public class VectorUtils {
 
 	/**
-	 * Scales a vector given by start and end point by a factor.
+	 * Scales a vector given by start and end point by a factor. Changes both - start and end coordinate of the vector.
 	 * @param scaleFactor a scale factor in [0..1]
 	 * @return a tuple with the scaled start point as first and the end point as
 	 *         second entry
 	 */
 	public static Tuple<Point2D.Double, Point2D.Double> scaleVector(final Point2D.Double start, final Point2D.Double end,
+			double scaleFactor) {
+		
+		return calculateScaledVector(start, end, scaleFactor);
+	}
+
+	/**
+	 * Scales a vector given by start and end point by a factor. Changes only the start coordinate of the vector.
+	 * @param scaleFactor a scale factor in [0..1]
+	 * @return a tuple with the scaled start point as first and the old end point as
+	 *         second entry
+	 * @deprecated not used yet
+	 */
+	@Deprecated
+	public static Tuple<Point2D.Double, Point2D.Double> scaleVectorStart(final Point2D.Double start, final Point2D.Double end,
+			double scaleFactor) {
+		
+		Tuple<Point2D.Double, Point2D.Double> scaledVector = calculateScaledVector(start, end, scaleFactor);
+		return new Tuple<Point2D.Double, Point2D.Double>(scaledVector.getFirst(), end);
+	}
+	
+	/**
+	 * Scales a vector given by start and end point by a factor. Changes only the end coordinate of the vector.
+	 * @param scaleFactor a scale factor in [0..1]
+	 * @return a tuple with the old start point as first and the scaled end point as
+	 *         second entry
+	 * @deprecated not used yet
+	 */
+	@Deprecated
+	public static Tuple<Point2D.Double, Point2D.Double> scaleVectorEnd(final Point2D.Double start, final Point2D.Double end,
+			double scaleFactor) {
+		
+		Tuple<Point2D.Double, Point2D.Double> scaledVector = calculateScaledVector(start, end, scaleFactor);
+		return new Tuple<Point2D.Double, Point2D.Double>(start, scaledVector.getSecond());
+	}
+	
+	
+	private static Tuple<Point2D.Double, Point2D.Double> calculateScaledVector(
+			final Point2D.Double start, final Point2D.Double end,
 			double scaleFactor) {
 		//norm
 		Point2D.Double.Double delta = new Point2D.Double.Double(end.x - start.x, end.y - start.y);
@@ -50,7 +89,7 @@ public class VectorUtils {
 		Point2D.Double scaledStart = new Point2D.Double(start.x
 				+ (offset * deltaNorm.x), start.y
 				+ (offset * deltaNorm.y));
-		return new Tuple<Point2D.Double, Point2D.Double>(scaledStart, scaledEnd);
+		Tuple<Point2D.Double, Point2D.Double> scaledVector = new Tuple<Point2D.Double, Point2D.Double>(scaledStart, scaledEnd);
+		return scaledVector;
 	}
-
 }
