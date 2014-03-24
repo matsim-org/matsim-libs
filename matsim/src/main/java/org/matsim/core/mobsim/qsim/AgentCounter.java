f658c4f9d1a8f05cbede81d8517e5afa64a22afa
michaz <michael.zilske@gmail.com>
2014-03-24 15:27:39 +0000

    cleanup
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@28374 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/core/mobsim/qsim/AgentCounter.java
+++ matsim/src/main/java/org/matsim/core/mobsim/qsim/AgentCounter.java
@@ -19,74 +19,45 @@
  * *********************************************************************** */
 package org.matsim.core.mobsim.qsim;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 import org.matsim.core.mobsim.qsim.interfaces.AgentCounterI;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 /**
- * This class replaces static functionality of abstract class Simulation.
- * It is responsible for living/lost agent counting.
- *
- * This class is a simple not static replacement of the old static Simulation class.
- * When the agent representation of QSim is implemented we should
- * consider to remove this class completely as it should then no longer be needed. dg May 2010
- *
- * OLD COMMENTS from simulation class
- *
- * 	 * @cdobler
- * Use AtomicIntegers as counting variables. Doing this avoids
- * problems with race conditions in the ParallelQueueSimulation.
- *
- * We should discuss if there is a possibility to assign a
- * Simulation Objects to each thread. Using the AtomicIntegers
- * solves the problems with the race conditions but it is still
- * a bottleneck...
- *
- * --
- * My plan once was to have one SimEngine for each thread, instead of one Simulation per thread. marcel/15feb2010
+ * This class is responsible for living/lost agent counting.
  *
  *
  * @author dgrether
  *
  */
 class AgentCounter implements AgentCounterI {
-	/**
-	 * Number of agents that have not yet reached their final activity location
-	 */
-	private AtomicInteger living = new AtomicInteger(0);
 
-	/**
-	 * Number of agents that got stuck in a traffic jam and where removed from the simulation to solve a possible deadlock
-	 */
-	private AtomicInteger lost = new AtomicInteger(0);
+    /**
+     * Number of agents that have not yet reached their final activity location
+     */
+    private AtomicInteger living = new AtomicInteger(0);
 
-	 void reset() {
-		setLiving(0);
-		resetLost();
-	}
+    /**
+     * Number of agents that got stuck in a traffic jam and were removed from the simulation to solve a possible deadlock
+     */
+    private AtomicInteger lost = new AtomicInteger(0);
 
-	@Override
-	public final int getLiving() {return living.get();	}
+    @Override
+    public final int getLiving() {return living.get();	}
 
-	 final void setLiving(final int count) {living.set(count);}
-	@Override
-	public final boolean isLiving() {return living.get() > 0;	}
-	@Override
-	public final int getLost() {return lost.get();	}
-	@Override
-	public final void incLost() {lost.incrementAndGet(); }
+    @Override
+    public final boolean isLiving() {return living.get() > 0;	}
 
-	 final void incLost(final int count) {lost.addAndGet(count);}
-	private final void resetLost() { lost.set(0); }
+    @Override
+    public final int getLost() {return lost.get();	}
 
-	 final void incLiving() {living.incrementAndGet();}
+    @Override
+    public final void incLost() {lost.incrementAndGet(); }
 
-	 final void incLiving(final int count) {living.addAndGet(count);}
-	@Override
-	public final void decLiving() {living.decrementAndGet();}
-//	@Override
-//	public final void decLiving(final int count) {living.decrementAndGet();}
-//	// yyyyyy does this work (`count' never used)??? kai, feb'12
+    final void incLiving() {living.incrementAndGet();}
+
+    @Override
+    public final void decLiving() {living.decrementAndGet();}
 
 }
