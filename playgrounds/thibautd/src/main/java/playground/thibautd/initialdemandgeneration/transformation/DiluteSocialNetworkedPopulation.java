6f50106191b873048cad54e0f876e2e3a91a4404
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2014-04-04 10:10:53 +0000

    script for dilution of a scenario with social network
    retains the usual agents as well as their alters.
    
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@28507 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/thibautd/src/main/java/playground/thibautd/initialdemandgeneration/transformation/DiluteSocialNetworkedPopulation.java
@@ -0,0 +1,112 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * DiluteSocialNetworkedPopulation.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2014 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.thibautd.initialdemandgeneration.transformation;
+
+import java.io.IOException;
+
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.PopulationWriter;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.network.MatsimNetworkReader;
+import org.matsim.core.population.MatsimPopulationReader;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.geometry.CoordImpl;
+
+import playground.thibautd.socnetsim.population.SocialNetwork;
+import playground.thibautd.socnetsim.population.SocialNetworkReader;
+import playground.thibautd.socnetsim.population.SocialNetworkWriter;
+import playground.thibautd.utils.ArgParser;
+import playground.thibautd.utils.MoreIOUtils;
+
+/**
+ * Given a population for a wide area (say Switzerland), goes through a dilution
+ * process to restrict the scenario to an area of interest (say Zurich).
+ * It does NOT sample the population, it just removes agents considered out
+ * of the area of interest. See {@link SocialNetworkedPopulationDilutionUtils}
+ * for more details on which agents are kept or not.
+ * @author thibautd
+ */
+public class DiluteSocialNetworkedPopulation {
+
+	private static void main(final ArgParser args) throws IOException {
+		args.setDefaultValue( "--xcenter" , "683518.0" );
+		args.setDefaultValue( "--ycenter" , "246836.0" );
+
+		args.setDefaultValue( "--radius" , "30000" );
+
+		args.setDefaultValue( "--netfile" , null );
+		args.setDefaultValue( "--inpopfile" , null );
+		args.setDefaultValue( "--insocnet" , null );
+		args.setDefaultValue( "--outdir" , null );
+
+		final Coord center =
+			new CoordImpl(
+					Double.parseDouble(
+						args.getValue( "--xcenter" ) ),
+					Double.parseDouble(
+						args.getValue( "--ycenter" ) ) );
+
+		final double radius =
+			Double.parseDouble(
+					args.getValue(
+						"--radius" ) );
+
+		final String netfile = args.getValue( "--netfile" );
+		final String inpopfile = args.getValue( "--inpopfile" );
+		final String insocnet = args.getValue( "--insocnet" );
+		final String outdir = args.getValue( "--outdir" );
+
+		MoreIOUtils.initOut( outdir );
+
+		try {
+			final Scenario scenario = ScenarioUtils.createScenario( ConfigUtils.createConfig() );
+
+			new MatsimNetworkReader( scenario ).readFile( netfile );
+			new MatsimPopulationReader( scenario ).readFile( inpopfile );
+			new SocialNetworkReader( scenario ).parse( insocnet );
+
+			SocialNetworkedPopulationDilutionUtils.dilute(
+					scenario,
+					center,
+					radius );
+
+			final String outpopfile = outdir+"/diluted-population.xml.gz";
+			final String outsocnet = outdir+"/diluted-socialnetwork.xml.gz";
+
+			new PopulationWriter(
+					scenario.getPopulation(),
+					scenario.getNetwork() ).write( outpopfile );
+
+			new SocialNetworkWriter(
+					(SocialNetwork)
+						scenario.getScenarioElement(
+							SocialNetwork.ELEMENT_NAME ) ).write( outsocnet );
+		}
+		finally {
+			MoreIOUtils.closeOutputDirLogging();
+		}
+	}
+
+	public static void main(final String[] args) throws IOException {
+		main( new ArgParser( args ) );
+	}
+}
+
---
+++ playgrounds/thibautd/src/main/java/playground/thibautd/initialdemandgeneration/transformation/SocialNetworkedPopulationDilutionUtils.java
@@ -0,0 +1,176 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * SocialNetworkedPopulationDilutionUtils.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2014 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.thibautd.initialdemandgeneration.transformation;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.population.Activity;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Population;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.router.EmptyStageActivityTypes;
+import org.matsim.core.router.TripStructureUtils;
+import org.matsim.core.utils.geometry.CoordUtils;
+
+import playground.thibautd.socnetsim.population.SocialNetwork;
+import playground.thibautd.socnetsim.population.SocialNetworkImpl;
+
+/**
+ * @author thibautd
+ */
+public class SocialNetworkedPopulationDilutionUtils {
+	private static final Logger log =
+		Logger.getLogger(SocialNetworkedPopulationDilutionUtils.class);
+
+	/**
+	 * Dilutes the population of the scenario, by retaining only agents passing
+	 * by the area defined by center and radius, as well as their social contacts.
+	 * TODO: try also only retaining social contacts of agents living or working in the zone
+	 * (not agents passing by).
+	 */
+	public static void dilute(
+			final Scenario scenario,
+			final Coord center,
+			final double radius ) {
+		log.info( "Start dilution with center "+center+" and radius "+radius );
+		final Set<Id> personsToKeep = new HashSet<Id>();
+		fillSetWithIntersectingPersons(
+				personsToKeep,
+				scenario,
+				center,
+				radius );
+		fillSetWithAltersOfSet(
+				personsToKeep,
+				scenario );
+		final Collection<Id> pruned =
+			prunePopulation(
+				scenario,
+				personsToKeep );
+		pruneSocialNetwork( pruned , scenario );
+		log.info( "Finished dilution." );
+	}
+
+	private static void pruneSocialNetwork(
+			final Collection<Id> toPrune,
+			final Scenario scenario) {
+		final SocialNetworkImpl sn = (SocialNetworkImpl)
+			scenario.getScenarioElement( SocialNetwork.ELEMENT_NAME );
+		
+		log.info( "Pruning of the social network begins." );
+		log.info( sn.getEgos().size()+" egos." );
+
+		for ( Id id : toPrune ) sn.removeEgo( id );
+
+		log.info( "Pruning of the social network finished." );
+		log.info( sn.getEgos().size()+" egos remaining." );
+	}
+
+	private static Collection<Id> prunePopulation(
+			final Scenario scenario,
+			final Set<Id> personsToKeep) {
+		final Population population = scenario.getPopulation();
+		log.info( "Actual pruning of the population begins." );
+		log.info( "Population size: "+population.getPersons().size() );
+		log.info( "Remaining persons to keep: "+personsToKeep.size() );
+
+		// XXX this is not guaranteed to remain feasible...
+		final Iterator<Id> popit = population.getPersons().keySet().iterator();
+		final Collection<Id> pruned = new ArrayList<Id>();
+		while ( popit.hasNext() ) {
+			final Id curr = popit.next();
+			if ( !personsToKeep.remove( curr ) ) {
+				popit.remove();
+				pruned.add( curr );
+			}
+		}
+
+		log.info( "Actual pruning of the population finished." );
+		log.info( "Population size: "+population.getPersons().size() );
+		log.info( "Pruned: "+pruned.size() );
+		log.info( "Remaining persons to keep: "+personsToKeep.size() );
+
+		return pruned;
+	}
+
+	private static void fillSetWithAltersOfSet(
+			final Set<Id> personsToKeep,
+			final Scenario scenario) {
+		log.info( "Search for alters of identified persons" ); 
+
+		final SocialNetwork sn = (SocialNetwork)
+			scenario.getScenarioElement( SocialNetwork.ELEMENT_NAME );
+		if ( !sn.isReflective() ) throw new IllegalArgumentException( "results undefined with unreflexive network." );
+
+		final Collection<Id> alters = new ArrayList<Id>();
+		for ( Id ego : personsToKeep ) alters.addAll( sn.getAlters( ego ) );
+
+		personsToKeep.addAll( alters );
+
+		log.info( "Finished search for alters of identified persons" ); 
+		log.info( personsToKeep.size()+" agents identified in total over "+scenario.getPopulation().getPersons().size() );
+	}
+
+	private static void fillSetWithIntersectingPersons(
+			final Set<Id> personsToKeep,
+			final Scenario scenario,
+			final Coord center,
+			final double radius) {
+		log.info( "Search for intersecting persons" );
+		log.warn( "Only using crowfly intersection (not network routes)" );
+
+		for ( Person p : scenario.getPopulation().getPersons().values() ) {
+			if ( accept( p , center , radius ) ) personsToKeep.add( p.getId() );
+		}
+
+		log.info( "Finished search for intersecting persons" );
+		log.info( personsToKeep.size()+" agents identified over "+scenario.getPopulation().getPersons().size() );
+	}
+
+	private static boolean accept(
+			final Person p,
+			final Coord center,
+			final double radius) {
+		Coord lastCoord = null;
+
+		for ( Activity activity : TripStructureUtils.getActivities( p.getSelectedPlan() , EmptyStageActivityTypes.INSTANCE ) ) {
+			if ( activity.getCoord() == null ) throw new NullPointerException( "no coord for activity "+activity+" for person "+p );
+
+			if ( CoordUtils.calcDistance( center , activity.getCoord() ) < radius ) return true;
+
+			if ( lastCoord != null &&
+					CoordUtils.distancePointLinesegment(
+						lastCoord , activity.getCoord(),
+						center ) < radius ) return true;
+			lastCoord = activity.getCoord();
+		}
+
+		return false;
+	}
+}
+
+
