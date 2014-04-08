33d68515c4f1c36f7e79b4a589f4b856a6a52b53
andreas77 <andreashorni@hotmail.com>
2014-04-08 16:09:56 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@28601 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/anhorni/src/main/java/playground/anhorni/rc/WithindayListener.java
+++ playgrounds/anhorni/src/main/java/playground/anhorni/rc/WithindayListener.java
@@ -69,9 +69,10 @@ public class WithindayListener implements StartupListener {
 	public void notifyStartup(StartupEvent event) {
 		log.info("doing within day replanning ...");
 		// initialze within-day module
-		this.withinDayControlerListener.notifyStartup(event);		
-		this.initWithinDayReplanning(this.scenario);
+		this.withinDayControlerListener.notifyStartup(event);	
 		this.addNetworkChange(event.getControler(), links);
+		this.initWithinDayReplanning(this.scenario);
+		
 	}
 	
 	private void initWithinDayReplanning(Scenario scenario) {		
