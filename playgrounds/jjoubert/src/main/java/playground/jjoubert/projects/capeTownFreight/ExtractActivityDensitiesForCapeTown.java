2e7fbbb837febf5abc2ede0ff8202446c76a462c
jwjoubert <johan.joubert@up.ac.za>
2014-04-17 09:15:52 +0000

    Changed activity analysis to be multi-threaded.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@28745 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/jjoubert/src/main/java/playground/jjoubert/projects/capeTownFreight/ExtractActivityDensitiesForCapeTown.java
+++ playgrounds/jjoubert/src/main/java/playground/jjoubert/projects/capeTownFreight/ExtractActivityDensitiesForCapeTown.java
@@ -22,12 +22,18 @@ package playground.jjoubert.projects.capeTownFreight;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
+import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.core.basic.v01.IdImpl;
@@ -56,6 +62,7 @@ import playground.southafrica.utilities.Header;
 import playground.southafrica.utilities.grid.GeneralGrid;
 
 public class ExtractActivityDensitiesForCapeTown {
+	private final static Logger LOG = Logger.getLogger(ExtractActivityDensitiesForCapeTown.class);
 
 	public static void main(String[] args) {
 		Header.printHeader(ExtractActivityDensitiesForCapeTown.class.toString(), args);
@@ -83,61 +90,49 @@ public class ExtractActivityDensitiesForCapeTown {
 		CoordinateTransformation ct = TransformationFactory.getCoordinateTransformation("WGS84", "WGS84_SA_Albers");
 		Counter counter = new Counter("   vehicles # ");
 		
-		Map<String, Integer> zoneCounts = new HashMap<String, Integer>();
-		Map<String, Polygon> hexPolygons = new HashMap<String, Polygon>();
+		/* Set up the multi-threaded infrastructure. */
+		LOG.info("Setting up multi-threaded infrastructure for analyses");
+		ExecutorService threadExecutor = Executors.newFixedThreadPool(numberOfThreads);
+		List<Future<Map<String, Integer>>> jobs = new ArrayList<Future<Map<String,Integer>>>();
 		
-		GeometryFactory gf = new GeometryFactory();
+		LOG.info("Processing vehicle files...");
 		for(File f : vehicleFiles){
-			Id id = new IdImpl(f.getName().substring(0, f.getName().indexOf(".")));
-			/* Read the vehicle file. */
-			DigicoreVehicleReader_v1 dvr = new DigicoreVehicleReader_v1();
-			dvr.parse(f.getAbsolutePath());
-			DigicoreVehicle vehicle = dvr.getVehicle();
-
-			/* Process the activities. */
-			for(DigicoreChain chain : vehicle.getChains()){
-				for(DigicoreActivity act : chain.getAllActivities()){
-					Coord coord = act.getCoord();
-					if(checkInQt(grid, coord)){
-						Point point = gf.createPoint(new Coordinate(coord.getX(), coord.getY()));
-						
-						Tuple<String, Point> tuple = grid.get(coord.getX(), coord.getY());
-						String zone = tuple.getFirst();
-						
-						if(!hexPolygons.containsKey(zone)){
-							/* Create a hexagonal polygon. */
-							double w = 0.5*zoneWidth;
-							double h = Math.sqrt(3.0)/2 * w;
-							double x = tuple.getSecond().getX();
-							double y = tuple.getSecond().getY();
-							Coordinate c1 = new Coordinate(x-w, y);
-							Coordinate c2 = new Coordinate(x-0.5*w, y+h);
-							Coordinate c3 = new Coordinate(x+0.5*w, y+h);
-							Coordinate c4 = new Coordinate(x+w, y);
-							Coordinate c5 = new Coordinate(x+0.5*w, y-h);
-							Coordinate c6 = new Coordinate(x-0.5*w, y-h);
-							Coordinate[] ca = {c1, c2, c3, c4, c5, c6, c1};
-							
-							Polygon hex = gf.createPolygon(ca);
-							hexPolygons.put(zone, hex);
-						}
-						Polygon zonePoly = hexPolygons.get(zone);
-						
-						if(zonePoly.contains(point)){
-							if(!zoneCounts.containsKey(tuple.getFirst())){
-								zoneCounts.put(zone, 1);
-							} else{
-								int oldValue = zoneCounts.get(zone);
-								zoneCounts.put(zone, oldValue+1);
-							}
-						}
-					}
-				}
-			}
-			counter.incCounter();
+			Callable<Map<String, Integer>> job = new VehicleActivityDensityAnalyser(grid, f, counter, zoneWidth);
+			Future<Map<String, Integer>> result = threadExecutor.submit(job);
+			jobs.add(result);
 		}
 		counter.printCounter();
+		LOG.info("Done processing vehicle files...");
 		
+		threadExecutor.shutdown();
+		while(!threadExecutor.isTerminated()){
+		}
+
+		/* Consolidate the output */
+		LOG.info("Consolidating output...");
+		Map<String, Integer> zoneCounts = new HashMap<String, Integer>();
+		for(Future<Map<String, Integer>> future : jobs){
+			/* Get the job's results. */
+			Map<String, Integer> map = null;
+			try {
+				map = future.get();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
+			
+			for(String zone : map.keySet()){
+				if(!zoneCounts.containsKey(zone)){
+					zoneCounts.put(zone, map.get(zone));
+				} else{
+					int oldValue = zoneCounts.get(zone);
+					zoneCounts.put(zone, oldValue + map.get(zone));
+				}
+			}
+		}
+		LOG.info("Done consolidating output...");
+
 		writeCountsToFile(outputFolder + "zoneCounts.csv", zoneCounts);
 		
 		Header.printFooter();
@@ -181,26 +176,88 @@ public class ExtractActivityDensitiesForCapeTown {
 	}
 	
 	
-	private class VehicleActivityDensityAnalyser implements Callable<Map<String,Integer>>{
+	private static class VehicleActivityDensityAnalyser implements Callable<Map<String,Integer>>{
 		private final QuadTree<Tuple<String, Point>> qt;
 		private Map<String, Integer> map;
 		private File file;
+		private final Counter counter;
+		private final double zoneWidth;
 		
-		public VehicleActivityDensityAnalyser(final QuadTree<Tuple<String,Point>> qt, File file) {
+		public VehicleActivityDensityAnalyser(final QuadTree<Tuple<String,Point>> qt, File file, final Counter counter, final double width) {
 			this.qt = qt;
-			this.map = new TreeMap<String, Integer>();
+			this.map = new HashMap<String, Integer>();
 			this.file = file;
+			this.counter = counter;
+			this.zoneWidth = width;
 		}
 
 		@Override
 		public Map<String, Integer> call() throws Exception {
+			Map<String, Polygon> hexPolygons = new HashMap<String, Polygon>();
 			
+			GeometryFactory gf = new GeometryFactory();
 			
-			
-			
+			/* Read the vehicle file. */
+			DigicoreVehicleReader_v1 dvr = new DigicoreVehicleReader_v1();
+			dvr.parse(this.file.getAbsolutePath());
+			DigicoreVehicle vehicle = dvr.getVehicle();
+
+			/* Process the activities. */
+			for(DigicoreChain chain : vehicle.getChains()){
+				for(DigicoreActivity act : chain.getAllActivities()){
+					Coord coord = act.getCoord();
+					if(checkInQt(this.qt, coord)){
+						Point point = gf.createPoint(new Coordinate(coord.getX(), coord.getY()));
+						
+						Tuple<String, Point> tuple = this.qt.get(coord.getX(), coord.getY());
+						String zone = tuple.getFirst();
+						
+						if(!hexPolygons.containsKey(zone)){
+							/* Create a hexagonal polygon. */
+							double w = 0.5*zoneWidth;
+							double h = Math.sqrt(3.0)/2 * w;
+							double x = tuple.getSecond().getX();
+							double y = tuple.getSecond().getY();
+							Coordinate c1 = new Coordinate(x-w, y);
+							Coordinate c2 = new Coordinate(x-0.5*w, y+h);
+							Coordinate c3 = new Coordinate(x+0.5*w, y+h);
+							Coordinate c4 = new Coordinate(x+w, y);
+							Coordinate c5 = new Coordinate(x+0.5*w, y-h);
+							Coordinate c6 = new Coordinate(x-0.5*w, y-h);
+							Coordinate[] ca = {c1, c2, c3, c4, c5, c6, c1};
+							
+							Polygon hex = gf.createPolygon(ca);
+							hexPolygons.put(zone, hex);
+						}
+						Polygon zonePoly = hexPolygons.get(zone);
+						
+						if(zonePoly.contains(point)){
+							if(!map.containsKey(tuple.getFirst())){
+								map.put(zone, 1);
+							} else{
+								int oldValue = map.get(zone);
+								map.put(zone, oldValue+1);
+							}
+						}
+					}
+				}
+			}
+			counter.incCounter();
 			return this.map;
 		}
 		
+		private boolean checkInQt(QuadTree<Tuple<String, Point>> qt, Coord coord){
+			boolean result = false;
+			double x = coord.getX();
+			double y = coord.getY();
+			if(x >= qt.getMinEasting() &&
+					x <= qt.getMaxEasting() &&
+					y >= qt.getMinNorthing() &&
+					y <= qt.getMaxNorthing()){
+				result = true;
+			}
+			return result;
+		}
 	}
 	
 	
