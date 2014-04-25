5270baf57a431c39e46077042ae0e50472cd0813
jbischoff <bischoff@vsp.tu-berlin.de>
2014-04-25 10:46:23 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@28782 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/jbischoff/src/main/java/playground/jbischoff/energy/vehicles/BatteryElectricVehicleImpl.java
+++ playgrounds/jbischoff/src/main/java/playground/jbischoff/energy/vehicles/BatteryElectricVehicleImpl.java
@@ -4,7 +4,15 @@ import org.matsim.contrib.transEnergySim.vehicles.api.BatteryElectricVehicle;
 import org.matsim.contrib.transEnergySim.vehicles.energyConsumption.EnergyConsumptionModel;
 
 public class BatteryElectricVehicleImpl extends BatteryElectricVehicle {
-
+	/**
+	 * 
+	 * a fairly basic implementation of a BEV.
+	 * 
+	 * @param EnergyConsumptionModel
+	 * @param battery capacity (in J)
+	 * 
+	 * 
+	 */
 	public BatteryElectricVehicleImpl(EnergyConsumptionModel ecm, double usableBatteryCapacityInJoules) {
 		this.electricDriveEnergyConsumptionModel=ecm;
 		this.usableBatteryCapacityInJoules=usableBatteryCapacityInJoules;
