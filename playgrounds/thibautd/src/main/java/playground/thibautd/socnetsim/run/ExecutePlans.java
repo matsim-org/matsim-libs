614fc6999083dc0690813438e8665e252b761f3a
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2014-05-05 11:51:18 +0000

    script to re-run the output of a simulation
    useful if one deletes by mistake the events of the last iteration, or to
    reproduce crashes in the QSim
    
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@28856 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/thibautd/src/main/java/playground/thibautd/socnetsim/run/ExecutePlans.java
@@ -0,0 +1,76 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * ExecutePlans.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2014 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.thibautd.socnetsim.run;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.Config;
+
+import playground.thibautd.socnetsim.population.JointPlansConfigGroup;
+import playground.thibautd.utils.ArgParser;
+
+/**
+ * To just execute a plans file, in case you are so stupid you delete
+ * the events file of the last iteration (but nobody would ever do that, right?)
+ * @author thibautd
+ */
+public class ExecutePlans {
+	public static void main(final String[] argParser) {
+		main( new ArgParser( argParser ) );
+	}
+
+	private static enum SimulationType {
+		households, socialnet;
+	}
+
+	private static void main(final ArgParser argParser) {
+		argParser.setDefaultValue( "--folder-for-input" , "-f" , null );
+		argParser.setDefaultValue( "--output-folder" , "-o" , null );
+		argParser.setDefaultValue( "--simulation-type" , "-t" , ""+SimulationType.households );
+
+		final String folder = argParser.getValue( "-f" );
+		final String output = argParser.getValue( "-o" );
+		final SimulationType simType = argParser.getEnumValue( "-t" , SimulationType.class );
+
+		final Config config = RunUtils.loadConfig( folder+"/output_config.xml.gz" );
+
+		config.controler().setOutputDirectory( output );
+		// TODO: use actual iteratio number
+		config.controler().setFirstIteration( 1 );
+		config.controler().setLastIteration( 1 );
+
+		config.plans().setInputFile( folder+"/output_plans.xml.gz" );
+		((JointPlansConfigGroup) config.getModule( JointPlansConfigGroup.GROUP_NAME )).setFileName(
+				folder+"/output_jointPlans.xml.gz" );
+
+		final Scenario scenario = RunUtils.loadScenario( config );
+
+		switch ( simType ) {
+		case households:
+			RunCliquesWithModularStrategies.runScenario( scenario , true );
+			break;
+		case socialnet:
+			RunGenericSocialNetwork.runScenario( scenario , true );
+			break;
+		default:
+			throw new IllegalArgumentException( simType.toString() );
+		}
+	}
+}
+
