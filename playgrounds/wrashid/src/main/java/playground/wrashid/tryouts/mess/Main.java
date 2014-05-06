f803cda5b2622a3e444779ecb4ea23b9d8af7ff9
rashid_waraich <rashid_waraich@users.sf.net>
2014-05-06 13:32:26 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@28877 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/main/java/playground/wrashid/tryouts/mess/Main.java
+++ playgrounds/wrashid/src/main/java/playground/wrashid/tryouts/mess/Main.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Random;
 
+import javax.swing.JFrame;
+
 import net.sourceforge.jeval.EvaluationException;
 import net.sourceforge.jeval.Evaluator;
 
@@ -13,6 +15,7 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.statistics.HistogramDataset;
 import org.jfree.data.statistics.HistogramType;
+import org.math.plot.Plot2DPanel;
 import org.matsim.contrib.parking.lib.GeneralLib;
 import org.matsim.core.utils.collections.QuadTree;
 
@@ -21,18 +24,29 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		double activityDurationInSeconds = 7200;
-		double isMale = 1;
-		double age = 30;
-		double income = 15000;
-		System.out.println("walk time beta :" + -0.108 * 60 * Math.pow(activityDurationInSeconds / 60 / 135, -0.08)
-				* (1 + (0.021 * isMale)) * Math.pow(age / 40.0, 0.236));
-
-		System.out.println("search time beta :" + -0.135 * 60 * Math.pow(activityDurationInSeconds / 60 / 135, -0.246)
-				* (1 + (-0.102 * isMale)));
-
-		System.out.println("cost beta :" + -0.135 *60* Math.pow((income / 7000), -0.1));
-
+		double[] x = { 1, 2, 3, 4, 5, 6 };
+		double[] y = { 45, 89, 6, 32, 63, 12 };
+ 
+		// create your PlotPanel (you can use it as a JPanel)
+		Plot2DPanel plot = new Plot2DPanel();
+ 
+		// define the legend position
+		plot.addLegend("SOUTH");
+ 
+		// add a line plot to the PlotPanel
+		plot.addLinePlot("my plot", x, y);
+ 
+		// put the PlotPanel in a JFrame like a JPanel
+		JFrame frame = new JFrame("a plot panel");
+		frame.setSize(600, 600);
+		frame.setContentPane(plot);
+		frame.setVisible(true);
+		try {
+			plot.toGraphicFile(new File("c:/tmp/abc.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
