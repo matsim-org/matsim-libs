a79b7c000c9af6f76ae4faede5e298a0ae3e58d2
agarwalamit <agarwalamit@users.sf.net>
2014-05-14 14:58:12 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@28969 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/agarwalamit/src/main/java/playground/agarwalamit/siouxFalls/legModeDistributions/LegModeDepartureArrivalTimeDistribution.java
@@ -0,0 +1,191 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2014 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.agarwalamit.siouxFalls.legModeDistributions;
+
+import java.io.BufferedWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Id;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.MatsimConfigReader;
+import org.matsim.core.events.EventsUtils;
+import org.matsim.core.events.MatsimEventsReader;
+import org.matsim.core.events.handler.EventHandler;
+import org.matsim.core.utils.io.IOUtils;
+
+import playground.vsp.analysis.modules.AbstractAnalyisModule;
+
+
+/**
+ * @author amit
+ */
+public class LegModeDepartureArrivalTimeDistribution extends AbstractAnalyisModule {
+
+	private final Logger logger = Logger.getLogger(LegModeDepartureArrivalTimeDistribution.class);
+	private LegModeDepartureArrivalTimeHandler lmdah;
+	private Map<String, Map<Id, double[]>> mode2PersonId2DepartureTime;
+	private Map<String, Map<Id, double[]>> mode2PersonId2ArrivalTime;
+	private List<Integer> timeStepClasses;
+	private List<String> travelModes;
+	private SortedMap<String, Map<Integer, Integer>> mode2DepartureTimeClasses2LegCount;
+	private SortedMap<String, Map<Integer, Integer>> mode2ArrivalTimeClasses2LegCount;
+	private String eventsFile;
+	private String configFile;
+
+	public LegModeDepartureArrivalTimeDistribution(String eventsFile, String configFile) {
+		super(LegModeDepartureArrivalTimeDistribution.class.getSimpleName());
+
+		this.eventsFile = eventsFile;
+		this.configFile=configFile;
+		this.timeStepClasses=new ArrayList<Integer>();
+		this.travelModes = new ArrayList<String>();
+		this.lmdah=new LegModeDepartureArrivalTimeHandler();
+		this.lmdah.reset(0);
+	}
+
+	public static void main(String[] args) {
+		String runDir = "/Users/aagarwal/Desktop/ils4/agarwal/siouxFalls/outputMCOff/";
+		//		String [] runs = {"run33"};
+		String [] runs = {"run105","run106","run107","run108"};
+
+		for(String run:runs){
+			String eventsFile = runDir+run+"/ITERS/it.100/100.events.xml.gz";
+			String configFile = runDir+run+"/output_config.xml.gz";
+			LegModeDepartureArrivalTimeDistribution lmdatd = new LegModeDepartureArrivalTimeDistribution(eventsFile, configFile);
+			lmdatd.preProcessData();
+			lmdatd.postProcessData();
+			lmdatd.writeResults(runDir+"/analysis/"+run);
+		}
+	}
+
+	@Override
+	public List<EventHandler> getEventHandler() {
+		return null;
+	}
+
+	@Override
+	public void preProcessData() {
+		EventsManager manager = EventsUtils.createEventsManager();
+		MatsimEventsReader reader = new MatsimEventsReader(manager);
+		manager.addHandler(this.lmdah);
+		reader.readFile(this.eventsFile);
+	}
+
+	@Override
+	public void postProcessData() {
+		this.mode2PersonId2DepartureTime = this.lmdah.getLegMode2PersonId2DepartureTime();
+		this.mode2PersonId2ArrivalTime=this.lmdah.getLegMode2PersonId2ArrivalTime();
+		initializeTimeStepClasses();
+		getTravelModes();
+		this.mode2DepartureTimeClasses2LegCount = calculateMode2DepOrArrTimeClases2LegCount(this.mode2PersonId2DepartureTime);
+		this.mode2ArrivalTimeClasses2LegCount = calculateMode2DepOrArrTimeClases2LegCount(this.mode2PersonId2ArrivalTime);
+	}
+
+	private SortedMap<String, Map<Integer, Integer>> calculateMode2DepOrArrTimeClases2LegCount (Map<String, Map<Id, double[]>> mode2personId2DepOrArrTime) {
+		SortedMap<String, Map<Integer, Integer>> mode2DepOrArrTime2LegCount= new TreeMap<String, Map<Integer,Integer>>();
+		for(String mode:this.travelModes){
+			SortedMap<Integer, Integer> travelTimeClasses2LegCount = new TreeMap<Integer, Integer>();
+			for(int i=0;i<this.timeStepClasses.size()-1;i++){
+				int legCount =0;
+				for(Id id:mode2personId2DepOrArrTime.get(mode).keySet()){
+					double tt [] = mode2personId2DepOrArrTime.get(mode).get(id);
+					for(double d:tt){
+						if(d > this.timeStepClasses.get(i) && d < this.timeStepClasses.get(i+1)){
+							legCount++;
+						}
+					}
+				}
+				travelTimeClasses2LegCount.put(this.timeStepClasses.get(i+1), legCount);
+			}
+			mode2DepOrArrTime2LegCount.put(mode, travelTimeClasses2LegCount);
+		}
+		return mode2DepOrArrTime2LegCount;
+	}
+
+	@Override
+	public void writeResults(String outputFolder) {
+		BufferedWriter writer = IOUtils.getBufferedWriter(outputFolder+".rLegModeDepartureTimeDistribution.txt");
+		try {
+			writer.write("# \t");
+			for(String mode:this.travelModes){
+				writer.write(mode+"\t");
+			}
+			writer.newLine();
+
+			for(int i=0; i<this.timeStepClasses.size()-1;i++){
+				writer.write(this.timeStepClasses.get(i+1)+"\t");
+				for(String mode :this.travelModes){
+					writer.write(this.mode2DepartureTimeClasses2LegCount.get(mode).get(this.timeStepClasses.get(i+1))+"\t");
+				}
+				writer.newLine();
+			}
+			writer.close();
+		} catch (Exception e) {
+			throw new RuntimeException("Data is not written in File. Reason : "+e);
+		}
+
+		BufferedWriter writer2 = IOUtils.getBufferedWriter(outputFolder+".rLegModeArrivalTimeDistribution.txt");
+		try {
+			writer2.write("# \t");
+			for(String mode:this.travelModes){
+				writer2.write(mode+"\t");
+			}
+			writer2.newLine();
+
+			for(int i=0; i<this.timeStepClasses.size()-1;i++){
+				writer2.write(this.timeStepClasses.get(i+1)+"\t");
+				for(String mode :this.travelModes){
+					writer2.write(this.mode2ArrivalTimeClasses2LegCount.get(mode).get(this.timeStepClasses.get(i+1))+"\t");
+				}
+				writer2.newLine();
+			}
+			writer2.close();
+		} catch (Exception e) {
+			throw new RuntimeException("Data is not written in File. Reason : "+e);
+		}
+
+
+	}
+	private void initializeTimeStepClasses() {
+		double simulationEndTime = getSimulationEndTime();
+
+		for(int endOfTimeStep =0; endOfTimeStep<=(int)simulationEndTime/(2*3600);endOfTimeStep++){
+			this.timeStepClasses.add(endOfTimeStep);
+		}
+
+		logger.info("The following time classes were defined: " + this.timeStepClasses);
+	}
+
+	private double getSimulationEndTime(){
+		Config config = ConfigUtils.createConfig();
+		MatsimConfigReader reader= new MatsimConfigReader(config);
+		reader.readFile(this.configFile);
+		return config.qsim().getEndTime();
+	}
+	private void getTravelModes(){
+		this.travelModes.addAll(this.mode2PersonId2DepartureTime.keySet());
+	}
+}
