abae566ed8269250b214e9c30ed70472e6bdb843
vbemetz <vbemetz@users.sf.net>
2014-05-18 18:31:13 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@29000 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/main/java/playground/wrashid/bsc/vbmh/einzel_klassen_tests/Diagnose.java
+++ playgrounds/wrashid/src/main/java/playground/wrashid/bsc/vbmh/einzel_klassen_tests/Diagnose.java
@@ -4,29 +4,79 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 import playground.wrashid.bsc.vbmh.util.CSVReader;
+import playground.wrashid.bsc.vbmh.util.CSVWriter;
 import playground.wrashid.bsc.vbmh.util.ReadParkhistory;
 import playground.wrashid.bsc.vbmh.util.RemoveDuplicate;
+import playground.wrashid.bsc.vbmh.vmParking.ParkHistoryWriter;
 
 public class Diagnose {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		ReadParkhistory hist = new ReadParkhistory();
-		hist.readXML("parkhistory_200.xml");
-		LinkedList<HashMap<String, String>> list = hist.getAllEventByAttribute("parking", "9000006");
-		ReadParkhistory a = hist.getSubHist(list);
-		LinkedList<HashMap<String, String>> list1 = a.getAllEventByAttribute("spotType", "ev");
-		a=hist.getSubHist(list1);
-		LinkedList<HashMap<String, String>> list2 = a.getAllEventByAttribute("eventtype", "EV_left");
 		
-		for(HashMap<String, String> events:list2){
-			
+		
+		
+		//---------------------
+		int count=0;
+		ReadParkhistory hist = new ReadParkhistory();
+//		hist.readXML("parkhistory_200.xml");
+		if(1==0){
+			hist.readXML("alles_null_sa/parkhistory/parkhistory_200.xml");
+			System.out.println("alles_null gelesen");
+		}
+		if(1==0){
+			hist.readXML("normal_sa/parkhistory/parkhistory_200.xml");
+			System.out.println("normal gelesen");
+		}
+		if(1==0){
+			hist.readXML("erste_min_5_sa/parkhistory/parkhistory_200.xml");
+			System.out.println("erste_min_5 gelesen");
+		}
+		if(1==0){
+			hist.readXML("null_ev_exc_sa/parkhistory/parkhistory_200.xml");
+			System.out.println("ev_exc gelesen");
 		}
 		
-		System.out.println(list2.size());
+		LinkedList<HashMap<String, String>> list = hist.getAllEventByAttribute("eventtype", "occupied");
+		ReadParkhistory a = hist.getSubHist(list);
+//		LinkedList<HashMap<String, String>> list1 = a.getAllEventByAttribute("spotType", "ev");
+//		a=hist.getSubHist(list1);
+//		LinkedList<HashMap<String, String>> list2 = a.getAllEventByAttribute("eventtype", "EV_left");
+		
+//		LinkedList<HashMap<String, String>> hasToCharge = hist.getAllEventByAttribute("eventtype", "Agent_looking_for_parking_has_to_charge");
+//		ReadParkhistory hasTo = new ReadParkhistory();
+//		
+//		hasTo.events=hasToCharge;
+//		
+//		for(HashMap<String, String> events:list){
+//			String id = events.get("person");
+////			for(HashMap<String, String> person:hasToCharge){
+//				if(hasTo.getAllEventByAttribute("person", id)!=null){
+//					System.out.println(events.toString());
+//					count++;
+//				}
+////				if(person.containsValue(id)){
+////					count++;
+////				}
+//			}
+//		}
+		
+		for(HashMap<String, String> event:list){
+			if(event.get("Parkingid").contains("900000")){
+				System.out.println(event.toString());
+			}
+		}
 		
 		
+//		System.out.println(list.size());
+		System.out.println(count);
+		
+		
+		//////----------------------------------------
+//		
+		
+		
+		//--------------------------------------------------
 		
 //		
 //		for (String[] line : walking) {
