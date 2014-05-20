d5c85f1ea5d22a4e2445a701eda1eb0069627453
vsp_nagel <2008@kainagel.org>
2014-05-20 16:03:12 +0000

    kai script change
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@29033 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/southAfrica/src/main/java/playground/southafrica/gauteng/KNGautengController.java
+++ playgrounds/southAfrica/src/main/java/playground/southafrica/gauteng/KNGautengController.java
@@ -16,18 +16,20 @@ public class KNGautengController {
 				, "/Users/nagel/southafrica/gautengRuns/output_base/ITERS/it.600/600.plans.xml.gz"
 				
 				, GAUTENG_PATH + "population/20140208/gautengAttr.xml.gz" // pop attribs 
+
 				, GAUTENG_PATH + "network/gauteng_20131210_coarseNationalNetwork_clean.xml.gz" // net 
 //				, "/Users/nagel/gauteng-kairuns/much_simplified_network.xml.gz"
 
-//				, GAUTENG_PATH + "toll/gauteng_toll_weekday_Existing_20131211.xml" // toll
-				, GAUTENG_PATH + "toll/gauteng_toll_weekday_Combined_20131211.xml" // toll
+				, GAUTENG_PATH + "toll/gauteng_toll_weekday_Existing_20131211.xml" // toll
+//				, GAUTENG_PATH + "toll/gauteng_toll_weekday_Combined_20131211.xml" // toll
 
-				, "110" // base VoT
-				, "4" // VoT multiplier
+				, "55" // base VoT
+				, "8" // VoT multiplier
 				, "1" // number of threads
 				, User.kai.toString()
 				, "./output"
 				, GAUTENG_PATH + "counts/2009/Counts_Thursday_Total.xml.gz"
+				,"/Users/nagel/gauteng-kairuns/additional_config.xml.gz"
 				} ;
 		GautengControler_subpopulations.main(args2);
 	}
