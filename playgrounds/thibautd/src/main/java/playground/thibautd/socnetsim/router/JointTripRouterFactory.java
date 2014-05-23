753d2cf46eb2681e50109a6c47f81addd5c5349e
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2014-05-23 11:57:55 +0000

    remove unsused constructor
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@29075 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/java/playground/thibautd/socnetsim/router/JointTripRouterFactory.java
+++ playgrounds/thibautd/src/main/java/playground/thibautd/socnetsim/router/JointTripRouterFactory.java
@@ -21,12 +21,9 @@ package playground.thibautd.socnetsim.router;
 
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
-import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.population.PopulationFactory;
-import org.matsim.core.config.Config;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.population.PopulationFactoryImpl;
-import org.matsim.core.population.routes.ModeRouteFactory;
 import org.matsim.core.router.TripRouter;
 import org.matsim.core.router.TripRouterFactoryImpl;
 import org.matsim.core.router.TripRouterFactoryInternal;
@@ -34,8 +31,6 @@ import org.matsim.core.router.costcalculators.TravelDisutilityFactory;
 import org.matsim.core.router.util.LeastCostPathCalculatorFactory;
 import org.matsim.core.router.util.TravelTime;
 import org.matsim.pt.router.TransitRouterFactory;
-import org.matsim.pt.transitSchedule.api.TransitSchedule;
-
 import playground.thibautd.socnetsim.population.JointActingTypes;
 
 /**
@@ -53,46 +48,25 @@ public class JointTripRouterFactory implements TripRouterFactoryInternal {
 	}
 
 	public JointTripRouterFactory(
-			final Config config,
-			final Network network,
-			final TravelDisutilityFactory travelDisutilityFactory,
-			final TravelTime travelTime,
-			final LeastCostPathCalculatorFactory leastCostPathAlgoFactory,
-			final PopulationFactory populationFactory,
-			final ModeRouteFactory modeRouteFactory,
-			final TransitRouterFactory transitRouterFactory,
-			final TransitSchedule transitSchedule) {
-		this(
-			new TripRouterFactoryImpl(
-					config,
-					network,
-					travelDisutilityFactory,
-					travelTime,
-					leastCostPathAlgoFactory,
-					populationFactory,
-					modeRouteFactory,
-					transitRouterFactory,
-					transitSchedule),
-			populationFactory);
-	}
-
-	public JointTripRouterFactory(
 			final Scenario scenario,
 			final TravelDisutilityFactory disutilityFactory,
 			final TravelTime travelTime,
 			final LeastCostPathCalculatorFactory leastCostAlgoFactory,
 			final TransitRouterFactory transitRouterFactory) {
-		this( scenario.getConfig(),
-				scenario.getNetwork(),
-				disutilityFactory,
-				travelTime,
-				leastCostAlgoFactory,
-				scenario.getPopulation().getFactory(),
-				((PopulationFactoryImpl) scenario.getPopulation().getFactory()).getModeRouteFactory(),
-				transitRouterFactory,
-				scenario.getConfig().scenario().isUseTransit() ?
-					scenario.getTransitSchedule() :
-					null);
+		this(
+			new TripRouterFactoryImpl(
+					scenario.getConfig(),
+					scenario.getNetwork(),
+					disutilityFactory,
+					travelTime,
+					leastCostAlgoFactory,
+					scenario.getPopulation().getFactory(),
+					((PopulationFactoryImpl) scenario.getPopulation().getFactory()).getModeRouteFactory(),
+					transitRouterFactory,
+					scenario.getConfig().scenario().isUseTransit() ?
+									scenario.getTransitSchedule() :
+									null),
+			scenario.getPopulation().getFactory() );
 	}
 
 	public JointTripRouterFactory(final Controler controler) {
