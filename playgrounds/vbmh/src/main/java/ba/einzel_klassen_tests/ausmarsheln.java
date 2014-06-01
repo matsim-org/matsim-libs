1f6144b7ae04c9537332c1b4bd9ae6b9149fa35c
moritzhpkt <moritzhpkt@users.sf.net>
2014-06-01 12:36:06 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@29176 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/vbmh/src/main/java/ba/einzel_klassen_tests/ausmarsheln.java
@@ -0,0 +1,20 @@
+package ba.einzel_klassen_tests;
+
+import java.io.File;
+
+import javax.xml.bind.JAXB;
+
+import ba.vmParking.ParkingMap;
+
+public class ausmarsheln {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String filename="input/parkings_demo.xml";
+		File file = new File( filename );
+		ParkingMap karte = JAXB.unmarshal( file, ParkingMap.class );
+		System.out.println(karte.getParkings().get(0).type);
+
+	}
+
+}
