28546dd5b8a2e3a9165220849b71ddd83840cbf3
boescpa <boescpa@users.sf.net>
2014-06-05 12:06:10 +0000

    Handles events to create trips.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@29233 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/boescpa/src/main/java/playground/boescpa/topdad/postprocessing/TripHandler.java
@@ -0,0 +1,204 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2014 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.boescpa.topdad.postprocessing;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.events.ActivityStartEvent;
+import org.matsim.api.core.v01.events.PersonArrivalEvent;
+import org.matsim.api.core.v01.events.PersonDepartureEvent;
+import org.matsim.api.core.v01.events.PersonStuckEvent;
+import org.matsim.api.core.v01.events.handler.ActivityStartEventHandler;
+import org.matsim.api.core.v01.events.handler.PersonDepartureEventHandler;
+import org.matsim.api.core.v01.events.handler.PersonArrivalEventHandler;
+import org.matsim.api.core.v01.events.handler.PersonStuckEventHandler;
+
+/**
+ * Handles events to create "trips". 
+ * 
+ * @author boescpa
+ * 
+ * IMPORTANT: This is an adapted version of staheale's class 'TripHandler'.
+ * 
+ */
+public class TripHandler implements PersonDepartureEventHandler, PersonArrivalEventHandler, ActivityStartEventHandler, PersonStuckEventHandler {
+
+	BoxedHashMap<Id,Id> startLink;
+	BoxedHashMap<Id,String> mode;
+	BoxedHashMap<Id,String> purpose;
+	BoxedHashMap<Id,Double> startTime;
+	BoxedHashMap<Id,Id> endLink;
+	BoxedHashMap<Id,Double> endTime;
+
+	List<Id> ptTripList;
+	HashMap<Id,Id> ptStageEndLinkId;
+	HashMap<Id,Double> ptStageEndTime;
+	List<Id> transitWalkList;
+	List<Id> endInitialisedList;
+
+	public BoxedHashMap<Id, Id> getStartLink() {
+		return startLink;
+	}
+
+	public BoxedHashMap<Id, String> getMode() {
+		return mode;
+	}
+	
+	public BoxedHashMap<Id, String> getPurpose() {
+		return purpose;
+	}
+
+	public BoxedHashMap<Id, Double> getStartTime() {
+		return startTime;
+	}
+
+	public BoxedHashMap<Id, Id> getEndLink() {
+		return endLink;
+	}
+
+	public BoxedHashMap<Id, Double> getEndTime() {
+		return endTime;
+	}
+
+	@Override
+	public void reset(int iteration) {
+		startLink = new BoxedHashMap<Id, Id>();
+		mode = new BoxedHashMap<Id, String>();
+		purpose = new BoxedHashMap<Id, String>();
+		startTime = new BoxedHashMap<Id, Double>();
+		endLink = new BoxedHashMap<Id, Id>();
+		endTime = new BoxedHashMap<Id, Double>();
+		ptStageEndLinkId = new HashMap<Id, Id>();
+		ptStageEndTime = new HashMap<Id, Double>();
+		ptTripList = new ArrayList<Id>();
+		transitWalkList = new ArrayList<Id>();
+		endInitialisedList = new ArrayList<Id>();
+	}
+
+	@Override
+	public void handleEvent(PersonArrivalEvent event) {
+		//store endLink and endTime if it's a pt stage
+		if (event.getLegMode().equals("pt") || event.getLegMode().equals("transit_walk")) {
+			ptStageEndLinkId.put(event.getPersonId(), event.getLinkId());
+			ptStageEndTime.put(event.getPersonId(), event.getTime());
+		}
+		//add endLink and endTime if it's not pt stage
+		else {
+			endLink.put(event.getPersonId(), event.getLinkId());
+			endTime.put(event.getPersonId(), event.getTime());
+		}
+	}
+
+	@Override
+	public void handleEvent(PersonDepartureEvent event) {
+		// handle pt trips
+		if (event.getLegMode().equals("pt")) {
+			// do not add a transit walk trip if it's only a stage
+			if (!ptTripList.contains(event.getPersonId())) {
+				startLink.put(event.getPersonId(), event.getLinkId());
+				mode.put(event.getPersonId(), event.getLegMode());
+				startTime.put(event.getPersonId(), event.getTime());
+				// set endLink and endTime to null (in case an agent enters a pt vehicle and the pt vehicle is stuck in the end)
+				if (!endInitialisedList.contains(event.getPersonId())) {
+					endLink.put(event.getPersonId(), null);
+					endTime.put(event.getPersonId(), null);
+					purpose.put(event.getPersonId(), null);
+					endInitialisedList.add(event.getPersonId());
+				}
+				
+			}
+			else {
+				//replace transit walk mode with pt
+				ArrayList<String> personModes = mode.getValues(event.getPersonId());
+				if (!personModes.get(personModes.size()).equals("pt")) {
+					
+					personModes.set((personModes.size()-1), "pt");
+				}
+			}
+
+
+		}
+		// handle transit walk trips
+		else if (event.getLegMode().equals("transit_walk")) {
+			if (!transitWalkList.contains(event.getPersonId())) {
+				transitWalkList.add(event.getPersonId());
+			}
+			// do not add a transit walk trip if it's only a stage
+			if (!ptTripList.contains(event.getPersonId())) {
+				startLink.put(event.getPersonId(), event.getLinkId());
+				mode.put(event.getPersonId(), event.getLegMode());
+				startTime.put(event.getPersonId(), event.getTime());
+				// set endLink and endTime to null (in case an agent enters a pt vehicle and the pt vehicle is stuck in the end)
+				if (!endInitialisedList.contains(event.getPersonId())) {
+					endLink.put(event.getPersonId(), null);
+					endTime.put(event.getPersonId(), null);
+					purpose.put(event.getPersonId(), null);
+					endInitialisedList.add(event.getPersonId());
+				}
+			}
+		}
+		// handle other trips
+		else {
+			startLink.put(event.getPersonId(), event.getLinkId());
+			mode.put(event.getPersonId(), event.getLegMode());
+			startTime.put(event.getPersonId(), event.getTime());
+		}
+
+	}
+
+	@Override
+	public void handleEvent(ActivityStartEvent event) {
+		if (event.getActType().equals("pt interaction")) {
+			// add person to pt trip list
+			if (!ptTripList.contains(event.getPersonId())) {
+				ptTripList.add(event.getPersonId());
+				if (transitWalkList.contains(event.getPersonId())) {
+					transitWalkList.remove(event.getPersonId());
+				}
+			}
+		}
+		else {
+			if (ptTripList.contains(event.getPersonId()) || transitWalkList.contains(event.getPersonId())) {
+				if (endInitialisedList.contains(event.getPersonId())) {
+					endLink.removeLast(event.getPersonId());
+					endTime.removeLast(event.getPersonId());
+					purpose.removeLast(event.getPersonId());
+				}
+				endLink.put(event.getPersonId(), ptStageEndLinkId.get(event.getPersonId()));
+				endTime.put(event.getPersonId(), ptStageEndTime.get(event.getPersonId()));
+				ptTripList.remove(event.getPersonId());
+				transitWalkList.remove(event.getPersonId());
+			}
+			if (endInitialisedList.contains(event.getPersonId())) {
+				endInitialisedList.remove(event.getPersonId());
+			}
+			purpose.put(event.getPersonId(), event.getActType());
+		}
+	}
+
+	@Override
+	public void handleEvent(PersonStuckEvent event) {
+		endLink.put(event.getPersonId(), event.getLinkId());
+		endTime.put(event.getPersonId(), event.getTime());
+		purpose.put(event.getPersonId(), "stuck");
+	}
+}
