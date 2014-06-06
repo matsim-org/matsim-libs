d7016ec0bafd6a5d11f24fd3d0da89997390e26e
boescpa <boescpa@users.sf.net>
2014-06-06 14:31:27 +0000

    Version with distance and time in printTrips.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@29261 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/boescpa/src/main/java/playground/boescpa/topdad/postprocessing/TripProcessing.java
+++ playgrounds/boescpa/src/main/java/playground/boescpa/topdad/postprocessing/TripProcessing.java
@@ -22,8 +22,10 @@ package playground.boescpa.topdad.postprocessing;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-
+import java.util.HashMap;
+import java.util.LinkedList;
 import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.core.utils.io.IOUtils;
@@ -45,10 +47,10 @@ public final class TripProcessing {
 	 * Creates a text file containing all trips based on a given events file.
 	 * 
 	 * The inputs are:
-	 * 	- eventsFile: Path to an events-File, e.g. "run.combined.150.events.xml.gz"
-	 * 	- networkFile: Path to the network-File used for the simulation resulting in the above events-File, e.g. "multimodalNetwork2030final.xml.gz"
-	 * 	- tripFile: Path to the trip-File produced as output, e.g. "trips2030combined.txt"
-	 * 		IMPORTANT: tripFile will be overwritten if already existing.
+	 * 	- tripData: A TripHandler containing the trips read from an events file
+	 * 	- network: The network used for the simulation
+	 * 	- outFile: Path to the File where the trips will be stored
+	 * 		IMPORTANT: outFile will be overwritten if already existing.
 	 * 
 	 * The output format is: agentId, tripStartTime, tripStartLink, tripStartLinkXCoord, tripStartLinkYCoord,
 	 * tripEndLink, tripEndLinkXCoord, tripEndLinkYCoord, mainMode, tripPurpose.
@@ -57,52 +59,36 @@ public final class TripProcessing {
 	 * 
 	 * If a pt trip only contains transit_walk legs, the main mode is transit walk.
 	 */
-	public static void printTrips(TripHandler tripHandler, Network network, String outFile)
-			throws IOException {
-
-		final String header="agentId\tstartTime\tstartLink\tstartXCoord\tstartYCoord\tendTime\tendLink\tendXCoord\tendYCoord\tmode\tpurpose";
-		final BufferedWriter out = IOUtils.getBufferedWriter(outFile); // Path to the trip-File produced as output, e.g. "trips2030combined.txt"
-		out.write(header);
-		out.newLine();
-		log.info("Writing trips file...");
-		for (Id personId : tripHandler.getStartLink().keySet()) {
-			if (!personId.toString().contains("pt")) {
-				ArrayList<Id> startLinks = tripHandler.getStartLink().getValues(personId);
-				ArrayList<String> modes = tripHandler.getMode().getValues(personId);
-				ArrayList<String> purposes = tripHandler.getPurpose().getValues(personId);
-				ArrayList<Double> startTimes = tripHandler.getStartTime().getValues(personId);
-				ArrayList<Id> endLinks = tripHandler.getEndLink().getValues(personId);
-				ArrayList<Double> endTimes = tripHandler.getEndTime().getValues(personId);
-	
-				for (int i = 0; i < startLinks.size(); i++) {
-					if (network.getLinks().get(endLinks.get(i)) != null) {
-						out.write(personId
-							+ "\t"
-							+ startTimes.get(i)
-							+ "\t"
-							+ startLinks.get(i)
-							+ "\t"
-							+ network.getLinks().get(startLinks.get(i)).getCoord().getX()
-							+ "\t"
-							+ network.getLinks().get(startLinks.get(i)).getCoord().getY()
-							+ "\t"
-							+ endTimes.get(i)
-							+ "\t"
-							+ endLinks.get(i)
-							+ "\t"
-							+ network.getLinks().get(endLinks.get(i)).getCoord().getX()
-							+ "\t"
-							+ network.getLinks().get(endLinks.get(i)).getCoord().getY()
-							+ "\t"
-							+ modes.get(i)
-							+ "\t"
-							+ purposes.get(i)							
-							);
-						out.newLine();
-					}
-					// in case there is no end link
-					else {
-						out.write(personId
+	public static void printTrips(TripHandler tripHandler, Network network, String outFile) {
+		try {
+			final String header="agentId\tstartTime\tstartLink\tstartXCoord\tstartYCoord\tendTime\tendLink\tendXCoord\tendYCoord\tmode\tpurpose";
+			final BufferedWriter out = IOUtils.getBufferedWriter(outFile); // Path to the trip-File produced as output, e.g. "trips2030combined.txt"
+			out.write(header);
+			out.newLine();
+			log.info("Writing trips file...");
+			for (Id personId : tripHandler.getStartLink().keySet()) {
+				if (!personId.toString().contains("pt")) {
+					ArrayList<Id> startLinks = tripHandler.getStartLink().getValues(personId);
+					ArrayList<String> modes = tripHandler.getMode().getValues(personId);
+					ArrayList<String> purposes = tripHandler.getPurpose().getValues(personId);
+					ArrayList<Double> startTimes = tripHandler.getStartTime().getValues(personId);
+					ArrayList<Id> endLinks = tripHandler.getEndLink().getValues(personId);
+					ArrayList<Double> endTimes = tripHandler.getEndTime().getValues(personId);
+					ArrayList<LinkedList<Id>> pathList = tripHandler.getPath().getValues(personId);
+		
+					for (int i = 0; i < startLinks.size(); i++) {
+						if (network.getLinks().get(endLinks.get(i)) != null) {
+							// time per mode [minutes]
+							double travelTime = (endTimes.get(i) - startTimes.get(i))/60;
+							
+							// distance per mode [meters]
+							long travelDistance = 0;
+							LinkedList<Id> path = pathList.get(i);
+							for (Id linkId : path) {
+								travelDistance += network.getLinks().get(linkId).getLength();
+							}
+							
+							out.write(personId
 								+ "\t"
 								+ startTimes.get(i)
 								+ "\t"
@@ -116,21 +102,164 @@ public final class TripProcessing {
 								+ "\t"
 								+ endLinks.get(i)
 								+ "\t"
-								+ "null"
+								+ network.getLinks().get(endLinks.get(i)).getCoord().getX()
 								+ "\t"
-								+ "null"
+								+ network.getLinks().get(endLinks.get(i)).getCoord().getY()
 								+ "\t"
 								+ modes.get(i)
 								+ "\t"
-								+ purposes.get(i)							
+								+ purposes.get(i)
+								+ "\t"
+								+ travelDistance
+								+ "\t"
+								+ travelTime
 								);
 							out.newLine();
+						}
+						// in case there is no end link
+						else {
+							out.write(personId
+									+ "\t"
+									+ startTimes.get(i)
+									+ "\t"
+									+ startLinks.get(i)
+									+ "\t"
+									+ network.getLinks().get(startLinks.get(i)).getCoord().getX()
+									+ "\t"
+									+ network.getLinks().get(startLinks.get(i)).getCoord().getY()
+									+ "\t"
+									+ endTimes.get(i)
+									+ "\t"
+									+ endLinks.get(i)
+									+ "\t"
+									+ "null"
+									+ "\t"
+									+ "null"
+									+ "\t"
+									+ modes.get(i)
+									+ "\t"
+									+ purposes.get(i)
+									+ "\t"
+									+ "null"
+									+ "\t"
+									+ "null"
+									);
+								out.newLine();
+						}
+					}
+				}
+			}
+			out.close();
+			log.info("Writing trips file...done.");
+		} catch (IOException e) {
+			log.info("Given trip-file-path not valid. Print trips not successfully executed.");
+		}
+	}
+	
+	
+	/**
+	 * Calculates the total travel distance and travel time per mode
+	 * for a given population based on a given events file.
+	 * 
+	 * The inputs are:
+	 * 	- tripData: A TripHandler containing the trips read from an events file
+	 * 	- network: The network used for the simulation
+	 * 	- outFile: Path to the File where the calculated values will be written
+	 * 		IMPORTANT: outFile will be overwritten if already existing.
+	 * 
+	 * If an agent doesn't finish its trip (endLink = null), this trip is not considered
+	 * for the total travel distances and times.
+	 * 
+	 * If an agent is a pt-driver ("pt" part of id), the agent is not considered in the calculation.
+	 * 
+	 * @return HashMap with (String, key) mode and (Double[], value) [time,distance] per mode 
+	 */
+	public static HashMap<String, Double[]> analyzeTripsTopdad(TripHandler tripData, Network network, String outFile) {
+		HashMap<String, Double> timeMode = new HashMap<String, Double>();
+		HashMap<String, Long> distMode = new HashMap<String, Long>();
+		
+		log.info("Analyzing trips for topdad...");
+		for (Id personId : tripData.getStartLink().keySet()) {
+			if (!personId.toString().contains("pt")) {
+				ArrayList<Id> startLinks = tripData.getStartLink().getValues(personId);
+				ArrayList<String> modes = tripData.getMode().getValues(personId);
+				ArrayList<LinkedList<Id>> pathList = tripData.getPath().getValues(personId);
+				ArrayList<Double> startTimes = tripData.getStartTime().getValues(personId);
+				ArrayList<Id> endLinks = tripData.getEndLink().getValues(personId);
+				ArrayList<Double> endTimes = tripData.getEndTime().getValues(personId);
+				
+				for (int i = 0; i < startLinks.size(); i++) {
+					if (network.getLinks().get(endLinks.get(i)) != null) {
+						String mode = modes.get(i);
+						
+						// time per mode [minutes]
+						double travelTime = (endTimes.get(i) - startTimes.get(i))/60;
+						
+						// distance per mode [meters]
+						long travelDistance = 0;
+						LinkedList<Id> path = pathList.get(i);
+						if (path.size() > 0) {
+							// if a path was recorded, use the actual path for travel-distance calculation
+							for (Id linkId : path) {
+								travelDistance += network.getLinks().get(linkId).getLength();
+							}
+						} else {
+							// if no path available, use euclidian distance as estimation for travel-distance
+							Coord startLink = network.getLinks().get(startLinks.get(i)).getCoord();
+							Coord endLink = network.getLinks().get(endLinks.get(i)).getCoord(); 
+							travelDistance += (long) Math.sqrt(
+									((endLink.getX() - startLink.getX())*(endLink.getX() - startLink.getX()))
+									+ ((endLink.getY() - startLink.getY())*(endLink.getY() - startLink.getY()))); 
+						}
+						
+						
+						// store new values
+						if (timeMode.containsKey(mode)) {
+							travelTime = timeMode.get(mode) + travelTime;
+							travelDistance = distMode.get(mode) + travelDistance;
+						}
+						timeMode.put(mode, travelTime);
+						distMode.put(mode, travelDistance);
 					}
 				}
 			}
 		}
-		out.close();
-		log.info("Writing trips file...done.");
+		
+		// ----------- Write Output -----------
+		// Write logging:
+		log.info("Travel times per mode:");
+		for (String mode : timeMode.keySet()) {
+			log.info("Mode " + mode + ": " + String.valueOf(timeMode.get(mode)) + " min");
+		}
+		log.info("Travel distances per mode:");
+		for (String mode : distMode.keySet()) {
+			log.info("Mode " + mode + ": " + String.valueOf(distMode.get(mode)) + " m");
+		}
+		// Write to file:
+		try {
+			final BufferedWriter out = IOUtils.getBufferedWriter(outFile);
+			out.write("Travel times per mode:"); out.newLine();
+			for (String mode : timeMode.keySet()) {
+				out.write(" - Mode " + mode + ": " + String.valueOf(timeMode.get(mode)) + " min");
+				out.newLine();
+			}
+			out.write("Travel distances per mode:"); out.newLine();
+			for (String mode : distMode.keySet()) {
+				out.write(" - Mode " + mode + ": " + String.valueOf(distMode.get(mode)) + " m");
+				out.newLine();
+			}
+			out.close();
+		} catch (IOException e) {
+			log.info("IOException. Could not write topdad-analysis summary to file.");
+		}
+		
+		log.info("Analyzing trips for topdad...done.");
+		HashMap<String, Double[]> result = new HashMap<String, Double[]>();
+		for (String mode : distMode.keySet()) {
+			Double[] val = {timeMode.get(mode), (double)distMode.get(mode)};
+			result.put(mode, val);
+		}
+		return result;
 	}
 	
 }
