6be47f0339b9018b26a13dcabdf3954b163cd58b
michaz <michael.zilske@gmail.com>
2014-06-10 12:23:16 +0000

    freight cleanup
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@29310 a0517364-e873-4167-b1c8-416daa5a6b91

--- contribs/freight/src/main/java/org/matsim/contrib/freight/controler/CarrierControlerListener.java
+++ contribs/freight/src/main/java/org/matsim/contrib/freight/controler/CarrierControlerListener.java
@@ -49,138 +49,105 @@ import java.util.Map;
 
 /**
  * Controls the workflow of the simulation.
- * 
- * <p>Processes the required actions during the matsim simulation workflow (replanning, scoring, sim). For example, it informs agents to 
- * score their plans when it is scoring time, and it informs them to re-plan, or it injects carriers into the simulation when it is time 
- * to inject them. Currently it is kept to minimum functions, i.e. injecting carrier plans into sim and the possibility 
- * to set custom scoring- and replanning-functionalities. 
- * 
- * @author sschroeder, mzilske
+ * <p/>
+ * <p>Processes the required actions during the matsim simulation workflow (replanning, scoring, sim). For example, it informs agents to
+ * score their plans when it is scoring time, and it informs them to re-plan, or it injects carriers into the simulation when it is time
+ * to inject them. Currently it is kept to minimum functions, i.e. injecting carrier plans into sim and the possibility
+ * to set custom scoring- and replanning-functionalities.
  *
+ * @author sschroeder, mzilske
  */
 
-public class CarrierControlerListener implements StartupListener, ShutdownListener,BeforeMobsimListener, AfterMobsimListener, ScoringListener,
-ReplanningListener, IterationEndsListener {
+public class CarrierControlerListener implements StartupListener, ShutdownListener, BeforeMobsimListener, AfterMobsimListener, ScoringListener,
+        ReplanningListener, IterationEndsListener {
 
 
-	private boolean physicallyEnforceTimeWindowBeginnings = false;
+    private boolean physicallyEnforceTimeWindowBeginnings = false;
 
-	private CarrierScoringFunctionFactory carrierScoringFunctionFactory;
+    private CarrierScoringFunctionFactory carrierScoringFunctionFactory;
 
-	private CarrierPlanStrategyManagerFactory carrierPlanStrategyManagerFactory;
+    private CarrierPlanStrategyManagerFactory carrierPlanStrategyManagerFactory;
 
-	private CarrierAgentTracker carrierAgentTracker;
+    private CarrierAgentTracker carrierAgentTracker;
 
-	private Carriers carriers;
-
-
-	/**
-	 * Constructs a controller with a set of carriers, re-planning capabilities and scoring-functions.
-	 * 
-	 * @param carriers
-	 * @param strategyManagerFactory
-	 * @param scoringFunctionFactory
-	 * 
-	 */
-	public CarrierControlerListener(Carriers carriers, CarrierPlanStrategyManagerFactory strategyManagerFactory, CarrierScoringFunctionFactory scoringFunctionFactory){
-		this.carriers = carriers;
-		this.carrierPlanStrategyManagerFactory = strategyManagerFactory;
-		this.carrierScoringFunctionFactory = scoringFunctionFactory;
-	}
-
-	/**
-	 * Constructs a controller with a carriersPlanFileName, re-planning capabilities and scoring-functions.
-	 * 
-	 * <p>The carriers will then be read and built at the start of matsim.
-	 * 
-	 * @param carrierPlansFilename
-	 * @param strategyManagerFactory
-	 * @param scoringFunctionFactory (not null)
-	 */
-	public CarrierControlerListener(String carrierPlansFilename, CarrierPlanStrategyManagerFactory strategyManagerFactory, CarrierScoringFunctionFactory scoringFunctionFactory){
-		this.carriers = new Carriers();
-		new CarrierPlanXmlReaderV2(carriers).read(carrierPlansFilename);
-		this.carrierPlanStrategyManagerFactory = strategyManagerFactory;
-		this.carrierScoringFunctionFactory = scoringFunctionFactory;
-	}
-
-	public Map<Id,Carrier> getCarriers() {
-		return carriers.getCarriers();
-	}
-
-	public CarrierScoringFunctionFactory getCarrierScoringFunctionFactory() {
-		return carrierScoringFunctionFactory;
-	}
-
-	public CarrierPlanStrategyManagerFactory getCarrierPlanStrategyManagerFactory() {
-		return carrierPlanStrategyManagerFactory;
-	}
-
-	public void setPhysicallyEnforceTimeWindowBeginnings(boolean enableWithinDayReScheduling) {
-		this.physicallyEnforceTimeWindowBeginnings = enableWithinDayReScheduling;
-	}
-
-	@Override
-	public void notifyStartup(StartupEvent event) {}
-
-	@Override
-	public void notifyBeforeMobsim(BeforeMobsimEvent event) {
-		Controler controler = event.getControler();
-		carrierAgentTracker = new CarrierAgentTracker(carriers, event.getControler().getNetwork(), carrierScoringFunctionFactory);
-		FreightQSimFactory mobsimFactory = new FreightQSimFactory(carrierAgentTracker);
-		mobsimFactory.setPhysicallyEnforceTimeWindowBeginnings(physicallyEnforceTimeWindowBeginnings);
-		event.getControler().setMobsimFactory(mobsimFactory);
-		controler.getEvents().addHandler(carrierAgentTracker);
-	}
-
-	@Override
-	public void notifyAfterMobsim(AfterMobsimEvent event) {
-		Controler controler = event.getControler();
-		controler.getEvents().removeHandler(carrierAgentTracker);
-	}
-
-	@Override
-	public void notifyScoring(ScoringEvent event) {
-		carrierAgentTracker.scoreSelectedPlans();
-	}
-
-	@Override
-	public void notifyReplanning(final ReplanningEvent event) {
-		if(carrierPlanStrategyManagerFactory == null){
-			return;
-		}
-		GenericStrategyManager<CarrierPlan> strategyManager = carrierPlanStrategyManagerFactory.createStrategyManager();
-
-//		if ( strategyManager instanceof CarrierReplanningStrategyManager ) {
-//			CarrierReplanningStrategyManager mgr = (CarrierReplanningStrategyManager) strategyManager ;
-//
-//			for (Carrier carrier : carriers.getCarriers().values()) {
-//				if (carrier.getSelectedPlan() == null) {
-//					logger.warn("carrier cannot replan since no selected plan is available");
-//					continue;
-//				}
-//				mgr.nextStrategy(event.getIteration()).run(carrier);
-//			}
-//		} else if ( strategyManager instanceof GenericStrategyManager<?>) {
-			Collection<HasPlansAndId<CarrierPlan>> collection = new ArrayList<HasPlansAndId<CarrierPlan>>() ;
-			for ( Carrier carrier : carriers.getCarriers().values() ) {
-				collection.add(carrier) ;
-			}
-			GenericStrategyManager<CarrierPlan> mgr = (GenericStrategyManager<CarrierPlan>) strategyManager ;
-			mgr.run( collection, null, event.getIteration(), null);
-//		} else {
-//			throw new RuntimeException("registered strategy manager cannot be cast into one of the accepted types") ;
-//		}
+    private Carriers carriers;
 
 
-	}
+    /**
+     * Constructs a controller with a set of carriers, re-planning capabilities and scoring-functions.
+     */
+    public CarrierControlerListener(Carriers carriers, CarrierPlanStrategyManagerFactory strategyManagerFactory, CarrierScoringFunctionFactory scoringFunctionFactory) {
+        this.carriers = carriers;
+        this.carrierPlanStrategyManagerFactory = strategyManagerFactory;
+        this.carrierScoringFunctionFactory = scoringFunctionFactory;
+    }
 
-	@Override
-	public void notifyIterationEnds(IterationEndsEvent event) {}
+    /**
+     * Constructs a controller with a carriersPlanFileName, re-planning capabilities and scoring-functions.
+     * <p/>
+     * <p>The carriers will then be read and built at the start of matsim.
+     */
+    public CarrierControlerListener(String carrierPlansFilename, CarrierPlanStrategyManagerFactory strategyManagerFactory, CarrierScoringFunctionFactory scoringFunctionFactory) {
+        this.carriers = new Carriers();
+        new CarrierPlanXmlReaderV2(carriers).read(carrierPlansFilename);
+        this.carrierPlanStrategyManagerFactory = strategyManagerFactory;
+        this.carrierScoringFunctionFactory = scoringFunctionFactory;
+    }
 
-	@Override
-	public void notifyShutdown(ShutdownEvent event) {
+    public Map<Id, Carrier> getCarriers() {
+        return carriers.getCarriers();
+    }
 
-	}
+    public void setPhysicallyEnforceTimeWindowBeginnings(boolean enableWithinDayReScheduling) {
+        this.physicallyEnforceTimeWindowBeginnings = enableWithinDayReScheduling;
+    }
+
+    @Override
+    public void notifyStartup(StartupEvent event) {
+    }
+
+    @Override
+    public void notifyBeforeMobsim(BeforeMobsimEvent event) {
+        Controler controler = event.getControler();
+        carrierAgentTracker = new CarrierAgentTracker(carriers, event.getControler().getNetwork(), carrierScoringFunctionFactory);
+        FreightQSimFactory mobsimFactory = new FreightQSimFactory(carrierAgentTracker);
+        mobsimFactory.setPhysicallyEnforceTimeWindowBeginnings(physicallyEnforceTimeWindowBeginnings);
+        event.getControler().setMobsimFactory(mobsimFactory);
+        controler.getEvents().addHandler(carrierAgentTracker);
+    }
+
+    @Override
+    public void notifyAfterMobsim(AfterMobsimEvent event) {
+        Controler controler = event.getControler();
+        controler.getEvents().removeHandler(carrierAgentTracker);
+    }
+
+    @Override
+    public void notifyScoring(ScoringEvent event) {
+        carrierAgentTracker.scoreSelectedPlans();
+    }
+
+    @Override
+    public void notifyReplanning(final ReplanningEvent event) {
+        if (carrierPlanStrategyManagerFactory == null) {
+            return;
+        }
+        GenericStrategyManager<CarrierPlan> strategyManager = carrierPlanStrategyManagerFactory.createStrategyManager();
+        Collection<HasPlansAndId<CarrierPlan>> collection = new ArrayList<HasPlansAndId<CarrierPlan>>();
+        for (Carrier carrier : carriers.getCarriers().values()) {
+            collection.add(carrier);
+        }
+        strategyManager.run(collection, null, event.getIteration(), event.getReplanningContext());
+    }
+
+    @Override
+    public void notifyIterationEnds(IterationEndsEvent event) {
+
+    }
+
+    @Override
+    public void notifyShutdown(ShutdownEvent event) {
+
+    }
 
 }
