c2682f15a25734a5b81dc4827711b6097055a999
rashid_waraich <rashid_waraich@users.sf.net>
2014-06-10 10:35:57 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@29299 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ contribs/parking/src/main/java/org/matsim/contrib/parking/parkingChoice/carsharing/ParkingModuleWithFFCarSharing.java
@@ -0,0 +1,27 @@
+package org.matsim.contrib.parking.parkingChoice.carsharing;
+
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
+
+public class ParkingModuleWithFFCarSharing implements ParkingModuleWithFreeFloatingCarSharing {
+
+	@Override
+	public ParkingLinkInfo getNextFreeFloatingVehicle(Coord coord) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ParkingLinkInfo parkFreeFloatingVehicle(Id vehicleId, Coord destCoord) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void resetForNewIterationStart() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
