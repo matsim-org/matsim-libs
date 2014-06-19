bc15118d3ace944feb4aedb01a1e4754fa026ce3
agarwalamit <agarwalamit@users.sf.net>
2014-06-19 17:48:24 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@29406 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/agarwalamit/src/main/java/playground/agarwalamit/siouxFalls/EmissionsAndCongestionsPerTimeBin.java
+++ playgrounds/agarwalamit/src/main/java/playground/agarwalamit/siouxFalls/EmissionsAndCongestionsPerTimeBin.java
@@ -23,6 +23,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.SortedMap;
+import java.util.TreeMap;
 
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Id;
@@ -45,7 +46,7 @@ import playground.agarwalamit.siouxFalls.emissionAnalyzer.PerLinkEmissionData;
 public class EmissionsAndCongestionsPerTimeBin {
 
 	private Logger logger = Logger.getLogger(PerLinkEmissionData.class);
-	private static String outputDir = "/Users/aagarwal/Desktop/ils4/agarwal/siouxFalls/outputMC/run116/";
+	private static String outputDir = "/Users/aagarwal/Desktop/ils4/agarwal/siouxFalls/outputMCOff/run112/";
 	private static String networkFile =outputDir+ "/output_network.xml.gz";
 	private static String configFile = outputDir+"/output_config.xml";
 	private static String emissionEventFile = outputDir+"/ITERS/it.100/100.emission.events.xml.gz";
@@ -54,13 +55,13 @@ public class EmissionsAndCongestionsPerTimeBin {
 
 	Network network;
 	final int noOfTimeBin = 30;
-	public static void main(String[] args) throws IOException {
-		
+	public static void main(String[] args)  {
+
 		EmissionsAndCongestionsPerTimeBin emissionsVsCongestionData = new EmissionsAndCongestionsPerTimeBin();
 		emissionsVsCongestionData.run();
 	}
 
-	private void run() throws IOException {
+	private void run()  {
 
 		BufferedWriter writer1 = IOUtils.getBufferedWriter(outputDir+"/analysis/emissionVsCongestion/100.hourlyDelaysAndEmissionsPerLink.txt");
 
@@ -86,23 +87,73 @@ public class EmissionsAndCongestionsPerTimeBin {
 
 
 
-		writer1.write("time"+"\t"+"linkId"+"\t"+"delays_sec"+"\t"+"CO"+"\t"+"CO2_Total"+"\t"+"FC"+"\t"+"HC"+"\t"+"NMHC"+"\t"+"NO2"+"\t"+"NOX"+"\t"+"PM"+"\t"+"SO2"+"\n");
-		for(double time : time2EmissionsTotalFilled.keySet()){
-			for(Link link : network.getLinks().values()){
+		try {
+			writer1.write("time"+"\t"+"linkId"+"\t"+"delays_sec"+"\t"+"CO"+"\t"+"CO2_Total"+"\t"+"FC"+"\t"+"HC"+"\t"+"NMHC"+"\t"+"NO2"+"\t"+"NOX"+"\t"+"PM"+"\t"+"SO2"+"\n");
 
-				double delay;
-				if(time2linkIdDelays.get(time).get(link.getId())==null) delay = 0.0;
-				else delay = time2linkIdDelays.get(time).get(link.getId());
+			for(double time : time2EmissionsTotalFilled.keySet()){
+				for(Link link : network.getLinks().values()){
 
-				writer1.write(time+"\t"+link.getId().toString()+"\t"+delay+"\t");
+					double delay;
+					if(time2linkIdDelays.get(time).get(link.getId())==null) delay = 0.0;
+					else delay = time2linkIdDelays.get(time).get(link.getId());
 
-				for(String str : (time2EmissionsTotalFilled.get(time)).get(link.getId()).keySet()){
-					writer1.write(time2EmissionsTotalFilled.get(time).get(link.getId()).get(str)+"\t");
-				} 
-				writer1.write("\n");
+					writer1.write(time+"\t"+link.getId().toString()+"\t"+delay+"\t");
+
+					for(String str : (time2EmissionsTotalFilled.get(time)).get(link.getId()).keySet()){
+						writer1.write(time2EmissionsTotalFilled.get(time).get(link.getId()).get(str)+"\t");
+					} 
+					writer1.write("\n");
+				}
+			} 	
+			writer1.close();
+		} catch (IOException e) {
+			throw new RuntimeException("Data is not written in a file. Reason "+e);
+		}
+
+		//writing network delays and emissions per time bin
+		BufferedWriter writer2= IOUtils.getBufferedWriter(outputDir+"/analysis/emissionVsCongestion/100.hourlyNetworkDelaysAndEmissions.txt");
+
+		try{
+			writer2.write("time"+"\t"+"delays_sec"+"\t"+"CO"+"\t"+"CO2_Total"+"\t"+"FC"+"\t"+"HC"+"\t"+"NMHC"+"\t"+"NO2"+"\t"+"NOX"+"\t"+"PM"+"\t"+"SO2"+"\n");
+			for(double time : time2EmissionsTotalFilled.keySet()){
+				double networkDelay = 0;
+				SortedMap<String, Double> networkEmissions = new TreeMap<String, Double>();
+				for(Link link : network.getLinks().values()){
+					double delay;
+					if(time2linkIdDelays.get(time).get(link.getId())==null) delay = 0.0;
+					else delay = time2linkIdDelays.get(time).get(link.getId());
+					networkDelay+=delay;
+
+
+					for(String str : (time2EmissionsTotalFilled.get(time)).get(link.getId()).keySet()){
+						double  emissionSoFar;
+						
+						if(networkEmissions.get(str)!=null){
+							emissionSoFar = networkEmissions.get(str);
+						} else {
+							emissionSoFar =0.;
+						}
+
+						double emissionNow=time2EmissionsTotalFilled.get(time).get(link.getId()).get(str);
+						networkEmissions.put(str, emissionSoFar+emissionNow);
+					}
+				}
+				writer2.write(time+"\t"+networkDelay+"\t");
+				for(String str : networkEmissions.keySet()){
+					writer2.write(networkEmissions.get(str)+"\t");
+				}
+				writer2.newLine();
 			}
-		} 
-		writer1.close();
+
+			writer2.close();
+		}catch (IOException e) {
+			throw new RuntimeException("Data is not written in a file. Reason "+e);
+		}
+
+
+
+
+
 		logger.info("Finished Writing files.");
 	}
 
