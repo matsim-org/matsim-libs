0be60f2378a9ccdfbab656b7873b34a0b424b86b
boescpa <boescpa@users.sf.net>
2014-06-26 08:06:45 +0000

    OSMConverter Main class for boescpa.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@29453 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/boescpa/src/main/java/playground/boescpa/converters/osm/OSMConvMain.java
@@ -0,0 +1,49 @@
+/*
+ * *********************************************************************** *
+ * project: org.matsim.*                                                   *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2014 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** *
+ */
+
+package playground.boescpa.converters.osm;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.network.NetworkWriter;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.pt.transitSchedule.api.TransitScheduleWriter;
+import playground.scnadine.converters.osmPT.Osm2TransitLines;
+
+/**
+ * Run OSM-Converters...
+ *
+ * @author boescpa
+ */
+public class OSMConvMain {
+
+	public static void main(String[] args) {
+		Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+		scenario.getConfig().scenario().setUseTransit(true);
+
+		Osm2TransitLines osm2pt = new Osm2TransitLines(scenario.getTransitSchedule(), scenario.getNetwork());
+		osm2pt.convert(args[0]);
+
+		new NetworkWriter(scenario.getNetwork()).write(args[1]);
+		new TransitScheduleWriter(scenario.getTransitSchedule()).writeFile(args[2]);
+	}
+
+}
