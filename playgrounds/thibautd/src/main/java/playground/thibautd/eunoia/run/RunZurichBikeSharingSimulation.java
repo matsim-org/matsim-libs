641320d663c8b34c09db29b592c37e9b8cb6a3a1
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2014-06-30 15:04:18 +0000

    script to run the ZH Bike Sharing scenario.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@29512 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/thibautd/src/main/java/playground/thibautd/eunoia/run/RunZurichBikeSharingSimulation.java
@@ -0,0 +1,165 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * RunZurichBikeSharingSimulation.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2014 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.thibautd.eunoia.run;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.experimental.ReflectiveModule;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.OutputDirectoryLogging;
+
+import eu.eunoiaproject.bikesharing.framework.BikeSharingConstants;
+import eu.eunoiaproject.bikesharing.framework.qsim.BikeSharingWithoutRelocationQsimFactory;
+import eu.eunoiaproject.bikesharing.framework.router.BikeSharingTripRouterFactory;
+import eu.eunoiaproject.bikesharing.framework.scenario.BikeSharingScenarioUtils;
+import eu.eunoiaproject.bikesharing.scoring.StepBasedFareConfigGroup;
+import eu.eunoiaproject.elevation.scoring.SimpleElevationScorerParameters;
+
+import playground.ivt.matsim2030.Matsim2030Utils;
+
+import playground.thibautd.eunoia.scoring.Matsim2010BikeSharingScoringFunctionFactory;
+
+/**
+ * @author thibautd
+ */
+public class RunZurichBikeSharingSimulation {
+	public static void main(final String[] args) {
+		final String configFile = args[ 0 ];
+
+		OutputDirectoryLogging.catchLogEntries();
+
+		final Config config = BikeSharingScenarioUtils.loadConfig( configFile );
+		Matsim2030Utils.addDefaultGroups( config );
+		config.addModule( new StepBasedFareConfigGroup() );
+		final DenivelationScoringConfigGroup denivelationScoringGroup = new DenivelationScoringConfigGroup();
+		config.addModule( denivelationScoringGroup );
+
+		final RelocationConfigGroup relocationGroup = new RelocationConfigGroup();
+		config.addModule( relocationGroup );
+
+		final Scenario sc = Matsim2030Utils.loadScenario( config );
+		BikeSharingScenarioUtils.loadBikeSharingPart( sc );
+
+		final Controler controler = new Controler( sc );
+
+		// Not sure how well (or bad) this would interact with Bike Sharing...
+		// I expect pretty nasty stuff to silently happen.
+		// Matsim2030Utils.initializeLocationChoice( controler );
+
+		controler.setTripRouterFactory( new BikeSharingTripRouterFactory( sc ) );
+
+		switch ( relocationGroup.getStrategy() ) {
+		case noRelocation:
+			controler.setMobsimFactory( new BikeSharingWithoutRelocationQsimFactory( false ) );
+			break;
+		case systemWideCapacities:
+			controler.setMobsimFactory( new BikeSharingWithoutRelocationQsimFactory( true ) );
+			break;
+		default:
+			throw new RuntimeException();
+		}
+
+		controler.setScoringFunctionFactory(
+				new Matsim2010BikeSharingScoringFunctionFactory(
+					sc,
+					denivelationScoringGroup.getParameters() ) );
+
+		controler.run();
+	}
+
+	private static class RelocationConfigGroup extends ReflectiveModule {
+		public static final String GROUP_NAME = "bikeSharingRedistribution";
+
+		public static enum Strategy {
+			noRelocation, systemWideCapacities;
+		}
+		private Strategy strategy = Strategy.systemWideCapacities;
+
+		public RelocationConfigGroup() {
+			super( GROUP_NAME );
+		}
+
+		@StringGetter( "strategy" )
+		public Strategy getStrategy() {
+			return this.strategy;
+		}
+
+		@StringSetter( "strategy" )
+		public void setStrategy(Strategy strategy) {
+			this.strategy = strategy;
+		}
+	}
+
+	/**
+	 * Better not to use non-flat config for EUNOIA stuff: we kind of need to do
+	 * a very specific config group or use the underscore approach...
+	 */
+	private static class DenivelationScoringConfigGroup extends ReflectiveModule {
+		public static final String GROUP_NAME = "denivelationScoring";
+
+		private double bikeMarginalUtilityOfDenivelation_m = 0;
+		private double walkMarginalUtilityOfDenivelation_m = 0;
+
+		public DenivelationScoringConfigGroup() {
+			super( GROUP_NAME );
+		}
+
+		@StringGetter( "bikeMarginalUtilityOfDenivelation_m" )
+		public double getBikeMarginalUtilityOfDenivelation_m() {
+			return this.bikeMarginalUtilityOfDenivelation_m;
+		}
+
+		@StringSetter( "bikeMarginalUtilityOfDenivelation_m" )
+		public void setBikeMarginalUtilityOfDenivelation_m(
+				final double bikeMarginalUtilityOfDenivelation_m) {
+			this.bikeMarginalUtilityOfDenivelation_m = bikeMarginalUtilityOfDenivelation_m;
+		}
+
+		@StringGetter( "walkMarginalUtilityOfDenivelation_m" )
+		public double getWalkMarginalUtilityOfDenivelation_m() {
+			return this.walkMarginalUtilityOfDenivelation_m;
+		}
+
+		@StringSetter( "walkMarginalUtilityOfDenivelation_m" )
+		public void setWalkMarginalUtilityOfDenivelation_m(
+				final double walkMarginalUtilityOfDenivelation_m) {
+			this.walkMarginalUtilityOfDenivelation_m = walkMarginalUtilityOfDenivelation_m;
+		}
+
+		public SimpleElevationScorerParameters getParameters() {
+			final SimpleElevationScorerParameters params = new SimpleElevationScorerParameters();
+
+			params.addParams(
+					TransportMode.bike,
+					getBikeMarginalUtilityOfDenivelation_m() );
+			params.addParams(
+					BikeSharingConstants.MODE,
+					getBikeMarginalUtilityOfDenivelation_m() );
+
+			params.addParams(
+					TransportMode.walk,
+					getWalkMarginalUtilityOfDenivelation_m() );
+
+			return params;
+		}
+	}
+}
+
---
+++ playgrounds/thibautd/src/main/java/playground/thibautd/eunoia/scoring/Matsim2010BikeSharingScoringFunctionFactory.java
@@ -0,0 +1,87 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * Matsim2010BikeSharingScoring.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2014 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.thibautd.eunoia.scoring;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.Config;
+import org.matsim.core.router.StageActivityTypesImpl;
+import org.matsim.core.scoring.ScoringFunction;
+import org.matsim.core.scoring.ScoringFunctionFactory;
+import org.matsim.core.scoring.SumScoringFunction;
+import org.matsim.pt.PtConstants;
+
+import eu.eunoiaproject.bikesharing.framework.BikeSharingConstants;
+import eu.eunoiaproject.bikesharing.framework.scenario.BikeSharingFacilities;
+import eu.eunoiaproject.bikesharing.scoring.StepBasedFare;
+import eu.eunoiaproject.bikesharing.scoring.StepBasedFareConfigGroup;
+import eu.eunoiaproject.elevation.ElevationProvider;
+import eu.eunoiaproject.elevation.ObjectAttributesBasedElevationProvider;
+import eu.eunoiaproject.elevation.scoring.SimpleElevationScorer;
+import eu.eunoiaproject.elevation.scoring.SimpleElevationScorerParameters;
+
+import playground.ivt.matsim2030.scoring.MATSim2010ScoringFunctionFactory;
+
+/**
+ * @author thibautd
+ */
+public class Matsim2010BikeSharingScoringFunctionFactory implements ScoringFunctionFactory {
+	private final MATSim2010ScoringFunctionFactory delegate;
+	private final ElevationProvider<Id> elevationProvider;
+	private final Config config;
+	private final SimpleElevationScorerParameters params;
+
+	public Matsim2010BikeSharingScoringFunctionFactory(
+			final Scenario scenario,
+			final SimpleElevationScorerParameters params) {
+		this.delegate =
+			new MATSim2010ScoringFunctionFactory(
+					scenario,
+					new StageActivityTypesImpl(
+						PtConstants.TRANSIT_ACTIVITY_TYPE,
+						BikeSharingConstants.INTERACTION_TYPE ) ); 	
+		this.config = scenario.getConfig();
+		this.elevationProvider =
+			new ObjectAttributesBasedElevationProvider(
+					scenario.getActivityFacilities().getFacilityAttributes(),
+					((BikeSharingFacilities)
+					 scenario.getScenarioElement(
+						 BikeSharingFacilities.ELEMENT_NAME )).getFacilitiesAttributes() );
+		this.params = params;
+	}
+
+	@Override
+	public ScoringFunction createNewScoringFunction(final Person person) {
+		final SumScoringFunction function = (SumScoringFunction) delegate.createNewScoringFunction(person);
+		function.addScoringFunction(
+				new StepBasedFare(
+					config.planCalcScore(),
+					(StepBasedFareConfigGroup)
+						config.getModule(
+							StepBasedFareConfigGroup.GROUP_NAME ) ) );
+		function.addScoringFunction(
+				new SimpleElevationScorer(
+					params,
+					elevationProvider ) );
+		return function;
+	}
+}
+
