94a0727c8dcec303b52e7869247da26310d3054b
agarwalamit <agarwalamit@users.sf.net>
2014-08-05 13:12:54 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@29727 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/agarwalamit/src/main/java/playground/agarwalamit/marginalTesting/LinkCongestionInfoExtended.java
@@ -0,0 +1,51 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2014 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.agarwalamit.marginalTesting;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.contrib.emissions.types.WarmPollutant;
+
+import playground.ikaddoura.internalizationCar.LinkCongestionInfo;
+
+/**
+ * @author amit
+ */
+public class LinkCongestionInfoExtended extends LinkCongestionInfo{
+
+	private List<Id> enteringAgents = new ArrayList<Id>();
+	private Map<Id, Double> personId2DelaysToPayFor = new HashMap<Id, Double>();
+	private Map<Id,Map<WarmPollutant, Double>> personId2WarmEmissionsToPayFor = new HashMap<Id, Map<WarmPollutant,Double>>();
+	
+	public List<Id> getEnteringAgents() {
+		return enteringAgents;
+	}
+	
+	public Map<Id , Double> getPersonId2DelaysToPayFor(){
+		return personId2DelaysToPayFor;
+	}
+	
+	public Map<Id,Map<WarmPollutant, Double>> getPersonId2WarmEmissionsToPayFor(){
+		return personId2WarmEmissionsToPayFor;
+	}
+}
