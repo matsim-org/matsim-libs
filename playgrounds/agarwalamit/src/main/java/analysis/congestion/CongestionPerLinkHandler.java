c95814d1acea385e3952e36e92a7da4d03485192
agarwalamit <agarwalamit@users.sf.net>
2014-08-09 19:57:42 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@29751 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/agarwalamit/src/main/java/analysis/congestion/CongestionPerLinkHandler.java
+++ playgrounds/agarwalamit/src/main/java/analysis/congestion/CongestionPerLinkHandler.java
@@ -16,7 +16,7 @@
  *   See also COPYING, LICENSE and WARRANTY file                           *
  *                                                                         *
  * *********************************************************************** */
-package playground.agarwalamit.siouxFalls.congestionAnalyzer;
+package analysis.congestion;
 
 import java.util.HashMap;
 import java.util.Map;
