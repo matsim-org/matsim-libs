756c16e77b1fa2d840d91db040b0f76d858758cf
upnpjdk <upnpjdk@users.sf.net>
2014-08-14 07:23:35 +0000

    Illustrating the svn mechanics.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@29799 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/ndekoker/src/main/java/playground/ndekoker/test/TestHeader.java
@@ -0,0 +1,33 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2013 by the members listed in the COPYING,     *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.ndekoker.test;
+
+import org.apache.log4j.Logger;
+
+
+public class TestHeader {
+	private final static Logger LOG = Logger.getLogger(TestHeader.class);
+
+	public static void main(String[] args) {
+		LOG.info("My first log message.");
+
+	}
+
+}
