a35805b33ab13c865f410c0fd7070a267891f48c
michaz <michael.zilske@gmail.com>
2014-08-26 09:38:18 +0000

    roadpricing tests
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@29910 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ contribs/roadpricing/src/test/java/org/matsim/roadpricing/ModuleTest.java
@@ -0,0 +1,55 @@
+/*
+ *  *********************************************************************** *
+ *  * project: org.matsim.*
+ *  * ModuleTest.java
+ *  *                                                                         *
+ *  * *********************************************************************** *
+ *  *                                                                         *
+ *  * copyright       : (C) 2014 by the members listed in the COPYING, *
+ *  *                   LICENSE and WARRANTY file.                            *
+ *  * email           : info at matsim dot org                                *
+ *  *                                                                         *
+ *  * *********************************************************************** *
+ *  *                                                                         *
+ *  *   This program is free software; you can redistribute it and/or modify  *
+ *  *   it under the terms of the GNU General Public License as published by  *
+ *  *   the Free Software Foundation; either version 2 of the License, or     *
+ *  *   (at your option) any later version.                                   *
+ *  *   See also COPYING, LICENSE and WARRANTY file                           *
+ *  *                                                                         *
+ *  * ***********************************************************************
+ */
+
+package org.matsim.roadpricing;
+
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.matsim.core.config.Config;
+import org.matsim.core.controler.Controler;
+import org.matsim.testcases.MatsimTestUtils;
+
+public class ModuleTest {
+
+    @Rule
+    public MatsimTestUtils utils = new MatsimTestUtils();
+
+    @Test(expected = RuntimeException.class)
+    @Ignore
+    public void testControlerWithoutRoadPricingDoesntWork() {
+        Config config = utils.loadConfig(utils.getClassInputDirectory() + "/config.xml");
+        Controler controler = new Controler(config);
+        controler.run();
+        // config has a roadpricing config group, but controler does not know about
+        // road pricing.
+    }
+
+    @Test
+    public void testControlerWithRoadPricingWorks() {
+        Config config = utils.loadConfig(utils.getClassInputDirectory() + "/config.xml");
+        Controler controler = new Controler(config);
+        controler.addControlerListener(new RoadPricing());
+        controler.run();
+    }
+
+}
