65842a9c7ee20e8bdda4dee17ffa685a42d760ab
vsp_neumann <neumann@senozon.de>
2014-08-26 14:01:22 +0000

    migrated P2 to own contrib
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@29921 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ contribs/minibus/src/main/java/playground/andreas/bvgScoringFunction/TransitControler.java
@@ -0,0 +1,146 @@
+package playground.andreas.bvgScoringFunction;
+
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.contrib.otfvis.OTFVis;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ActivityParams;
+import org.matsim.core.config.groups.QSimConfigGroup;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.events.SynchronizedEventsManagerImpl;
+import org.matsim.core.mobsim.framework.ObservableMobsim;
+import org.matsim.core.mobsim.framework.listeners.MobsimListener;
+import org.matsim.core.mobsim.qsim.ActivityEngine;
+import org.matsim.core.mobsim.qsim.QSim;
+import org.matsim.core.mobsim.qsim.TeleportationEngine;
+import org.matsim.core.mobsim.qsim.agents.AgentFactory;
+import org.matsim.core.mobsim.qsim.agents.PopulationAgentSource;
+import org.matsim.core.mobsim.qsim.agents.TransitAgentFactory;
+import org.matsim.core.mobsim.qsim.pt.ComplexTransitStopHandlerFactory;
+import org.matsim.core.mobsim.qsim.pt.TransitQSimEngine;
+import org.matsim.core.mobsim.qsim.qnetsimengine.DefaultQNetsimEngineFactory;
+import org.matsim.core.mobsim.qsim.qnetsimengine.ParallelQNetsimEngineFactory;
+import org.matsim.core.mobsim.qsim.qnetsimengine.QNetsimEngine;
+import org.matsim.core.mobsim.qsim.qnetsimengine.QNetsimEngineFactory;
+import org.matsim.core.scenario.ScenarioImpl;
+import org.matsim.core.scenario.ScenarioLoaderImpl;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.pt.PtConstants;
+import org.matsim.vis.otfvis.OTFClientLive;
+import org.matsim.vis.otfvis.OnTheFlyServer;
+
+/**
+ * @author aneumann
+ */
+public class TransitControler extends Controler {
+
+	private final static Logger log = Logger.getLogger(TransitControler.class);
+
+	private boolean useOTFVis = false;
+	
+	public TransitControler(Config config) {
+		super(config);
+	}
+	
+	public TransitControler(ScenarioImpl scenario) {
+		super(scenario);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void runMobSim() {
+		
+		log.info("Overriding runMobSim()");
+
+        Scenario sc = this.getScenario();EventsManager eventsManager = this.getEvents();
+
+        QSimConfigGroup conf = sc.getConfig().qsim();
+        if (conf == null) {
+            throw new NullPointerException("There is no configuration set for the QSim. Please add the module 'qsim' to your config file.");
+        }
+
+        // Get number of parallel Threads
+        int numOfThreads = conf.getNumberOfThreads();
+        QNetsimEngineFactory netsimEngFactory;
+        if (numOfThreads > 1) {
+            eventsManager = new SynchronizedEventsManagerImpl(eventsManager);
+            netsimEngFactory = new ParallelQNetsimEngineFactory();
+        } else {
+            netsimEngFactory = new DefaultQNetsimEngineFactory();
+        }
+		QSim qSim1 = new QSim(sc, eventsManager);
+		ActivityEngine activityEngine = new ActivityEngine();
+		qSim1.addMobsimEngine(activityEngine);
+		qSim1.addActivityHandler(activityEngine);
+		QNetsimEngine netsimEngine = netsimEngFactory.createQSimEngine(qSim1);
+		qSim1.addMobsimEngine(netsimEngine);
+		qSim1.addDepartureHandler(netsimEngine.getDepartureHandler());
+		TeleportationEngine teleportationEngine = new TeleportationEngine();
+		qSim1.addMobsimEngine(teleportationEngine);
+        QSim qSim = qSim1;
+        AgentFactory agentFactory;
+            agentFactory = new TransitAgentFactory(qSim);
+            TransitQSimEngine transitEngine = new TransitQSimEngine(qSim);
+            transitEngine.setTransitStopHandlerFactory(new ComplexTransitStopHandlerFactory());
+            qSim.addDepartureHandler(transitEngine);
+            qSim.addAgentSource(transitEngine);
+        PopulationAgentSource agentSource = new PopulationAgentSource(sc.getPopulation(), agentFactory, qSim);
+        qSim.addAgentSource(agentSource);
+
+        QSim simulation = (QSim) qSim;
+
+		transitEngine.setTransitStopHandlerFactory(new ComplexTransitStopHandlerFactory());
+//		this.events.addHandler(new LogOutputEventHandler());
+
+
+		if (simulation instanceof ObservableMobsim){
+			for (MobsimListener l : this.getMobsimListeners()) {
+				((ObservableMobsim)simulation).addQueueSimulationListeners(l);
+			}
+		}
+		if (this.useOTFVis) {
+			OnTheFlyServer server = OTFVis.startServerAndRegisterWithQSim(config,getScenario(), events, simulation);
+			OTFClientLive.run(config, server);
+		}
+		simulation.run();
+	}	
+	
+	void setUseOTFVis(boolean useOTFVis) {
+		this.useOTFVis = useOTFVis;
+	}
+
+	public static void main(final String[] args) {
+		
+		String configFile = args[0];		
+		Config config;
+		
+		// reading the config file:
+		config = ConfigUtils.loadConfig(configFile);
+		
+		// manipulate config
+		// add "pt interaction" cause controler.init() is called too late and in a protected way
+		ActivityParams transitActivityParams = new ActivityParams(PtConstants.TRANSIT_ACTIVITY_TYPE);
+		transitActivityParams.setTypicalDuration(120.0);
+		config.planCalcScore().addActivityParams(transitActivityParams);
+		
+		// reading the scenario (based on the config):
+		ScenarioLoaderImpl scLoader = new ScenarioLoaderImpl(config) ;
+		ScenarioImpl sc = (ScenarioImpl) scLoader.loadScenario() ;
+		
+		TransitControler tc = new TransitControler(sc);
+		tc.setScoringFunctionFactory(new BvgScoringFunctionFactory(config.planCalcScore(), new BvgScoringFunctionConfigGroup(config), tc.getNetwork()));
+		
+
+
+		// Not needed to use own scoring function
+
+		if(args.length > 1 && args[1].equalsIgnoreCase("true")){
+			tc.setUseOTFVis(true);
+		}
+		tc.setOverwriteFiles(true);
+//			tc.setCreateGraphs(false);
+		tc.run();
+		
+	}
+}
