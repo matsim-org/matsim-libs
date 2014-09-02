4faac2efa9b006bd3276353e76e3f92c910a35ea
mrieser <rieser@gmail.com>
2014-09-02 07:24:21 +0000

    MATSIM-169: make Id class generic and add default implementation. Now we can start to use typed Ids!
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@29964 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/api/core/v01/Id.java
+++ matsim/src/main/java/org/matsim/api/core/v01/Id.java
@@ -20,13 +20,90 @@
 
 package org.matsim.api.core.v01;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 
 /**
  * Represents a unique identifier.
  * 
  * Note that Ids should not contain any whitespace characters (spaces, tabs, newlines, ...), 
- * as this may lead to problems when Ids are written to file and read back in. 
+ * as this may lead to problems when Ids are written to file and read back in.
+ * 
+ *  @author mrieser / Senozon AG
  */
-public abstract class Id implements Comparable<Id> {
+public abstract class Id<T> implements Comparable<Id<T>> {
 
+	private final static Map<Class<?>, Map<String, Id<?>>> cache = new ConcurrentHashMap<Class<?>, Map<String, Id<?>>>();
+	
+	/**
+	 * This method supports a cache where ids are stored and re-used per type.   
+	 */
+	public static <T> Id<T> create(final String key, final Class<T> type) {
+		Map<String, Id<?>> map = cache.get(type);
+		if (map == null) {
+			map = new ConcurrentHashMap<String, Id<?>>();
+			cache.put(type, map);
+		}
+		Id<?> id = map.get(key);
+		if (id == null) {
+			id = new IdImpl<T>(key);
+			map.put(key, id);
+		}
+		
+		return (Id<T>) id;
+	}
+	
+	/**
+	 * @return <code>0</code> when the two objects being compared are the same objects, other values according to their ids being compared to each other.
+	 * 
+	 * @throws IllegalArgumentException when the two objects being compared have the same id, but are not the same object because this means they must have different generic types
+	 */
+	@Override
+	public int compareTo(Id<T> o) throws IllegalArgumentException {
+		int res = this.toString().compareTo(o.toString());
+		if (res == 0) {
+			if (equals(o)) {
+				return 0;
+			}
+			throw new IllegalArgumentException("The ids are equal but of different types.");
+		}
+		return res;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		return this == obj;
+		// all other objects have to be different by definition, as long as the cache is correctly implemented
+	}
+
+	
+	/**
+	 * The default implementation to be used for Ids.
+	 * Have this as a separate class instead of integrated into the Id class
+	 * to allow for future optimization of Ids.
+	 * 
+	 * @author mrieser
+	 *
+	 * @param <T>
+	 */
+	private static class IdImpl<T> extends Id<T> {
+
+		private final String id; 
+		
+		/*package*/ IdImpl(final String id) {
+			this.id = id;
+		}
+
+		@Override
+		public int hashCode() {
+			return this.id.hashCode();
+		}
+		
+		@Override
+		public String toString() {
+			return this.id;
+		}
+	}	
+	
 }
---
+++ matsim/src/test/java/org/matsim/api/core/v01/IdTest.java
@@ -0,0 +1,59 @@
+package org.matsim.api.core.v01;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class IdTest {
+
+	@Test
+	public void testConstructor() {
+		Id<TLink> linkId1 = Id.create("1", TLink.class);
+		Id<TLink> linkId2 = Id.create("2", TLink.class);
+		
+		Assert.assertEquals("1", linkId1.toString());
+		Assert.assertEquals("2", linkId2.toString());
+	}
+	
+	@Test
+	public void testObjectIdentity_cache() {
+		Id<TLink> linkId1 = Id.create("1", TLink.class);
+		Id<TLink> linkId2 = Id.create("2", TLink.class);
+		Id<TLink> linkId1again = Id.create("1", TLink.class);
+		
+		Assert.assertTrue(linkId1 == linkId1again);
+		Assert.assertFalse(linkId1 == linkId2);
+	}
+	
+	@Test
+	public void testObjectIdentity_types() {
+		Id<TLink> linkId1 = Id.create("1", TLink.class);
+		Id<TNode> nodeId1 = Id.create("1", TNode.class);
+		
+		Assert.assertFalse((Id) linkId1 == (Id) nodeId1);
+	}
+	
+	@Test
+	public void testCompareTo() {
+		Id<TLink> linkId1 = Id.create("1", TLink.class);
+		Id<TLink> linkId2 = Id.create("2", TLink.class);
+		Id<TLink> linkId1again = Id.create("1", TLink.class);
+		Id<TNode> nodeId1 = Id.create("1", TNode.class);
+		
+		Assert.assertTrue(linkId1.compareTo(linkId2) < 0);
+		Assert.assertTrue(linkId1.compareTo(linkId1) == 0);
+		Assert.assertTrue(linkId1.compareTo(linkId1again) == 0);
+		Assert.assertTrue(linkId2.compareTo(linkId1) > 0);
+		
+		try {
+			Assert.assertTrue(linkId1.compareTo((Id) nodeId1) == 0);
+			Assert.fail("expected exception, got none");
+		} catch (IllegalArgumentException e) {
+			// expected exception
+		}
+	}
+	
+	private static class TLink {}
+	private static class TNode {}
+	
+}
