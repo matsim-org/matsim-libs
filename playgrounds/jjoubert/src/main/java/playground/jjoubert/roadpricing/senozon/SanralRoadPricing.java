a7f6ac92ffa7d928bef959f60550989569f21c1c
jwjoubert <johan.joubert@up.ac.za>
2014-09-04 08:13:07 +0000

    Method name refactored.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@30080 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/jjoubert/src/main/java/playground/jjoubert/roadpricing/senozon/SanralRoadPricing.java
+++ playgrounds/jjoubert/src/main/java/playground/jjoubert/roadpricing/senozon/SanralRoadPricing.java
@@ -57,7 +57,7 @@ public class SanralRoadPricing implements StartupListener, AfterMobsimListener, 
 	@Override
 	public void notifyStartup(final StartupEvent event) {
 		final Controler controler = event.getControler();
-        if (ConfigUtils.addOrGetModule(controler.getConfig(), RoadPricingConfigGroup.GROUP_NAME, RoadPricingConfigGroup.class).isUsingRoadpricing()) {
+        if (ConfigUtils.addOrGetModule(controler.getConfig(), RoadPricingConfigGroup.GROUP_NAME, RoadPricingConfigGroup.class).isUseRoadpricing()) {
 			throw new RuntimeException("roadpricing must not be enabled in config.scenario in order to use special road pricing features!");
 		}
 		// read the road pricing scheme from file
