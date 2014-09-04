849f11f2274925292e30593bfd735bff2dafdccc
vsp_neumann <neumann@senozon.de>
2014-09-04 13:25:38 +0000

    cleanup
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@30112 a0517364-e873-4167-b1c8-416daa5a6b91

--- contribs/minibus/src/main/java/playground/andreas/P2/PControler.java
+++ contribs/minibus/src/main/java/playground/andreas/P2/PControler.java
@@ -21,11 +21,9 @@ package playground.andreas.P2;
 
 import org.apache.log4j.Logger;
 import org.matsim.core.config.Config;
-import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ActivityParams;
+import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.core.config.ConfigUtils;
-import org.matsim.pt.PtConstants;
 
 import playground.andreas.P2.helper.PConfigGroup;
 import playground.andreas.P2.helper.PScenarioImpl;
