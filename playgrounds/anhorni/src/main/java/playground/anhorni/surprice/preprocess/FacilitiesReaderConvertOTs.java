2444a9b95f7bc13bab119473868e1c65032b9199
mrieser <rieser@gmail.com>
2014-09-08 20:34:14 +0000

    all invocations of scenario.createId are gone in playground.anhorni.*
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@30203 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/anhorni/src/main/java/playground/anhorni/surprice/preprocess/FacilitiesReaderConvertOTs.java
+++ playgrounds/anhorni/src/main/java/playground/anhorni/surprice/preprocess/FacilitiesReaderConvertOTs.java
@@ -23,6 +23,7 @@ package playground.anhorni.surprice.preprocess;
 import java.util.Stack;
 
 import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.api.experimental.facilities.ActivityFacilities;
 import org.matsim.core.api.experimental.facilities.ActivityFacilitiesFactory;
@@ -90,7 +91,7 @@ public class FacilitiesReaderConvertOTs extends MatsimXmlParser {
 	}
 	
 	private void startFacility(final Attributes atts) {
-		this.currfacility = this.factory.createActivityFacility(this.scenario.createId(atts.getValue("id")), 
+		this.currfacility = this.factory.createActivityFacility(Id.create(atts.getValue("id"), ActivityFacility.class), 
 				this.scenario.createCoord(Double.parseDouble(atts.getValue("x")), Double.parseDouble(atts.getValue("y"))));
 		this.facilities.addActivityFacility(this.currfacility);
 		((ActivityFacilityImpl) this.currfacility).setDesc(atts.getValue("desc"));
