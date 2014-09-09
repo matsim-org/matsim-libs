581d09e9d96376e3015f16b0487f5057f5dfb52c
vsp_neumann <neumann@senozon.de>
2014-09-09 08:52:00 +0000

    added license
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@30219 a0517364-e873-4167-b1c8-416daa5a6b91

--- contribs/minibus/src/main/java/playground/andreas/P2/performance/PReRouteStrategyModule.java
+++ contribs/minibus/src/main/java/playground/andreas/P2/performance/PReRouteStrategyModule.java
@@ -1,3 +1,22 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2014 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
 package playground.andreas.P2.performance;
 
 import org.apache.log4j.Logger;
@@ -6,6 +25,11 @@ import org.matsim.api.core.v01.population.Plan;
 import org.matsim.api.core.v01.replanning.PlanStrategyModule;
 import org.matsim.core.replanning.ReplanningContext;
 
+/**
+ * 
+ * @author aneumann
+ *
+ */
 public class PReRouteStrategyModule implements PlanStrategyModule{
 	
 	@SuppressWarnings("unused")
