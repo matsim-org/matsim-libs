8359f514af953d0197986866d004f42f430cb32a
michaz <michael.zilske@gmail.com>
2014-09-10 15:55:56 +0000

    removed "knowledges" from a test config
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@30244 a0517364-e873-4167-b1c8-416daa5a6b91

--- contribs/locationchoice/src/test/java/org/matsim/contrib/locationchoice/bestresponse/BestReplyTest.java
+++ contribs/locationchoice/src/test/java/org/matsim/contrib/locationchoice/bestresponse/BestReplyTest.java
@@ -1,11 +1,9 @@
 package org.matsim.contrib.locationchoice.bestresponse;
 
-import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.contrib.locationchoice.DCControler;
 import org.matsim.core.basic.v01.IdImpl;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.scenario.ScenarioImpl;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.testcases.MatsimTestCase;
 
@@ -14,10 +12,8 @@ public class BestReplyTest extends MatsimTestCase {
 	
 	private Scenario scenario;
 	private DestinationChoiceBestResponseContext context;
-	
-	private static final Logger log = Logger.getLogger(BestReplyTest.class);
-	
-	@Override
+
+    @Override
 	protected void setUp() throws Exception {
 		super.setUp();
 		this.init();
@@ -28,13 +24,11 @@ public class BestReplyTest extends MatsimTestCase {
 				context.getPersonsKValuesArray(), context.getFacilitiesKValuesArray(), scenario.getConfig().locationchoice());
 		assertTrue(sampler.sample(context.getFacilityIndex(new IdImpl(1)), context.getPersonIndex(new IdImpl(1))));
 		assertTrue(!sampler.sample(context.getFacilityIndex(new IdImpl(1)), context.getPersonIndex(new IdImpl(2))));
-//		assertTrue(sampler.sample(new IdImpl(1), new IdImpl(1)));
-//		assertTrue(!sampler.sample(new IdImpl(1), new IdImpl(2)));
 	}
 	
 	public void init() {
 		String configFile = this.getPackageInputDirectory() + "/config.xml";
-		this.scenario = (ScenarioImpl) ScenarioUtils.loadScenario(ConfigUtils.loadConfig(configFile));
+		this.scenario = ScenarioUtils.loadScenario(ConfigUtils.loadConfig(configFile));
 		this.context = new DestinationChoiceBestResponseContext(this.scenario);
 		this.context.init();
 	}
