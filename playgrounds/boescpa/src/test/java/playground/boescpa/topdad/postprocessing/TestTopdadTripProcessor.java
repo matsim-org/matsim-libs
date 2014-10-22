0fe84e993374b68456a497dcf1cf6066ca51af44
boescpa <boescpa@users.sf.net>
2014-10-22 17:28:23 +0000

    Corrected for changes in other classes...
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@30711 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/boescpa/src/test/java/playground/boescpa/topdad/postprocessing/TestTopdadTripProcessor.java
+++ playgrounds/boescpa/src/test/java/playground/boescpa/topdad/postprocessing/TestTopdadTripProcessor.java
@@ -87,9 +87,9 @@ public class TestTopdadTripProcessor {
 		Assert.assertEquals("Test: TripProcessing.analyzeTrips - transit_walk mode time sum not as expected.", 13114.93, transit_walk[0], 0.01);
 		// TripProcessing.analyzeTrips - Distance tests
 		Assert.assertEquals("Test: TripProcessing.analyzeTrips - car mode distance sum not as expected.", 7231200.0, car[1], 0);
-		Assert.assertEquals("Test: TripProcessing.analyzeTrips - pt mode distance sum not as expected.", 963017.0, pt[1], 0);
+		Assert.assertEquals("Test: TripProcessing.analyzeTrips - pt mode distance sum not as expected.", 1453968.0, pt[1], 0);
 			// Test above expects calculation of pt distances by euclidian distance. Should this change, this value needs to be readjusted...
-		Assert.assertEquals("Test: TripProcessing.analyzeTrips - transit_walk mode distance sum not as expected.", 430244.0, transit_walk[1], 0);
+		Assert.assertEquals("Test: TripProcessing.analyzeTrips - transit_walk mode distance sum not as expected.", 649569.0, transit_walk[1], 0);
 			// Test above expects calculation of transit_walk distances by euclidian distance. Should this change, this value needs to be readjusted...
 	}
 }
