1de407d8a3ef126f0ffe4ab85c922876df3e3b0d
agarwalamit <agarwalamit@users.sf.net>
2014-10-29 16:53:36 +0000

    bug fix in existing and another class to get distribution of activity duration difference
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@30815 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/agarwalamit/src/main/java/playground/agarwalamit/munich/analysis/ActDurationDiffDistribution.java
@@ -0,0 +1,194 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2014 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.agarwalamit.munich.analysis;
+
+import java.io.BufferedWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.core.utils.io.IOUtils;
+
+import playground.agarwalamit.analysis.ActivityType2DurationHandler;
+import playground.benjamin.scenarios.munich.analysis.filter.PersonFilter;
+
+/**
+ * A class to use along with ActivityType2ActivityDurationDistribution to get the distribution of
+ * activity duration difference.
+ * @author amit
+ */
+public class ActDurationDiffDistribution {
+
+	public ActDurationDiffDistribution(String outputDir, String bau) {
+		this.outputDir = outputDir;
+		this.bau = bau;
+	}
+
+
+	private String outputDir;
+	private String bau;
+	private ActivityType2ActivityDurationDistribution actDistributor;
+	private Map<Id<Person>, Map<String, List<Double>>> personId2ActDurationDiff;
+	private List<Double> allDurDiffs;
+	private List<Integer> timeClasses;
+	private Set<String> actTypes ;
+	private SortedMap<String, SortedMap<Integer, Integer>> actType2ActDurationDiff2LegCount;
+
+	public static void main(String[] args) {
+		String outputDir = "/Users/aagarwal/Desktop/ils4/agarwal/munich/output/1pct/";
+		String bau = "baseCaseCtd";
+		String runCase = "ei";
+		
+		new ActDurationDiffDistribution(outputDir,bau).run(runCase);
+	}
+	
+	private void run (String runCase){
+
+		personId2ActDurationDiff = new HashMap<>();
+		allDurDiffs = new ArrayList<Double>();
+		timeClasses = new ArrayList<Integer>();
+		actType2ActDurationDiff2LegCount = new TreeMap<>();
+		actTypes = new HashSet<>();
+
+		actDistributor = new ActivityType2ActivityDurationDistribution(outputDir);
+
+		Map<Id<Person>, Map<String, List<Double>>> bauPerson2ActDurations = getPersonId2ActType2ActDurations(bau);
+
+		Map<Id<Person>, Map<String, List<Double>>> runCasePerson2ActDurations = getPersonId2ActType2ActDurations(runCase);
+		
+		for (Id<Person> personId : bauPerson2ActDurations.keySet()){
+			Map<String, List<Double>> actType2DurationDiff = new HashMap<>();
+			for(String actType : bauPerson2ActDurations.get(personId).keySet()){
+				actTypes.add(actType);
+				List<Double> actDurDiff = new ArrayList<Double>();
+				for(int i = 0; i< bauPerson2ActDurations.get(personId).get(actType).size();i++){
+					double bauDur = bauPerson2ActDurations.get(personId).get(actType).get(i);
+					double runCaseDur = runCasePerson2ActDurations.get(personId).get(actType).get(i);
+					double durDiff = runCaseDur-bauDur;
+					actDurDiff.add(durDiff);
+					allDurDiffs.add(durDiff);
+					
+				}
+				actType2DurationDiff.put(actType,actDurDiff);
+			}
+			personId2ActDurationDiff.put(personId, actType2DurationDiff);
+		}
+		
+		initializeTimeClasses();
+		getActType2ActDurationDiffDistributionData();
+		
+		
+		String fileName = outputDir+runCase+"/analysis/"+"/actTyp2ActDur_Diff_DistributionDuration.txt";
+		BufferedWriter writer = IOUtils.getBufferedWriter(fileName);
+		try {
+			writer.write("timeIndex \t ");
+			for(String actType : actType2ActDurationDiff2LegCount.keySet()){
+				writer.write(actType+"\t");
+			}
+			writer.newLine();
+
+			for(int i:timeClasses){
+				writer.write(i+"\t");
+				for(String actType : actType2ActDurationDiff2LegCount.keySet()){
+					writer.write(actType2ActDurationDiff2LegCount.get(actType).get(i)+"\t");
+				}
+				writer.newLine();
+			}
+			writer.close();
+		} catch (Exception e) {
+			throw new RuntimeException("Data is not written. Reason - " + e);
+		}
+		ActivityType2DurationHandler.log.info("Data is written to file "+fileName);
+		
+		
+	}
+
+	private Map<Id<Person>, Map<String, List<Double>>> getPersonId2ActType2ActDurations(String run){
+		actDistributor.init(run);
+		return actDistributor.getPersonId2ActivityType2ActivityDurations();
+	}
+	
+	private void getActType2ActDurationDiffDistributionData(){
+		actType2ActDurationDiff2LegCount = new TreeMap<>();
+
+		//initialize
+		for(String actTyp : actTypes){
+			SortedMap<Integer,Integer> time2LegCount = new TreeMap<>();
+			for(int i:timeClasses){
+				time2LegCount.put(i, 0);
+			}
+			actType2ActDurationDiff2LegCount.put(actTyp, time2LegCount);
+		}
+
+		PersonFilter pf = new PersonFilter();
+		for(Id<Person> id : personId2ActDurationDiff.keySet()){
+//			if(pf.isPersonFromMID(id)){
+				Map<String, List<Double>> actTyp2Dur = personId2ActDurationDiff.get(id);
+				for(String actTyp : actTyp2Dur.keySet()){
+					List<Double> durs = actTyp2Dur.get(actTyp);
+					for(double d :durs){
+						for(int i=0;i<timeClasses.size();i++){
+							if(d> timeClasses.get(i)&& d<=timeClasses.get(i+1)){
+								SortedMap<Integer,Integer> time2LegCount = actType2ActDurationDiff2LegCount.get(actTyp);
+								int countSoFar = time2LegCount.get(timeClasses.get(i+1));
+								int newCount = countSoFar+1;
+								time2LegCount.put(timeClasses.get(i+1), newCount);
+							}
+						}
+					}
+				}
+//			}//
+		}
+	}
+	
+	
+	private void initializeTimeClasses(){
+		int endOfTimeClass =0;
+		int classCounter = 0;
+		double minTime = Collections.min(allDurDiffs);
+		double maxTime = Collections.max(allDurDiffs);
+		
+		timeClasses.add(endOfTimeClass);
+
+		while(endOfTimeClass <= maxTime){
+			endOfTimeClass = 100* (int) Math.pow(2, classCounter);
+			classCounter++;
+			timeClasses.add(endOfTimeClass);
+		}
+		
+		endOfTimeClass = 0;
+		classCounter =0;
+		while(endOfTimeClass <= -minTime){
+			endOfTimeClass = 100* (int) Math.pow(2, classCounter);
+			classCounter++;
+			timeClasses.add(-endOfTimeClass);
+		}
+		Collections.sort(timeClasses);
+		ActivityType2DurationHandler.log.info("Following activity duration classes are defined: "+timeClasses.toString());
+	}
+
+}
--- playgrounds/agarwalamit/src/main/java/playground/agarwalamit/munich/analysis/ActivityType2ActivityDurationDistribution.java
+++ playgrounds/agarwalamit/src/main/java/playground/agarwalamit/munich/analysis/ActivityType2ActivityDurationDistribution.java
@@ -90,11 +90,16 @@ public class ActivityType2ActivityDurationDistribution extends AbstractAnalyisMo
 
 	public void run(String [] runCases){
 		for(String runCase:runCases){
+			initializeTimeClasses();
 			init(runCase);
+			String outputConfig = outputDir+runCase+"/output_config.xml";
+			getActType2ActDurationDistributionData();
+			writeResults(outputDir+runCase+"/analysis/");
+			writeTypicalAndMinimumActivityDurations(outputConfig, runCase);
 		}
 	}
 
-	private void init(String runCase){
+	public void init(String runCase){
 		String outputConfig = outputDir+runCase+"/output_config.xml";
 		int lastIt = LoadMyScenarios.getLastIteration(outputConfig);
 		this.eventsFile = outputDir+runCase+"/ITERS/it."+lastIt+"/"+lastIt+".events.xml.gz";
@@ -102,28 +107,36 @@ public class ActivityType2ActivityDurationDistribution extends AbstractAnalyisMo
 		personId2ActDurations = new HashMap<Id<Person>, Map<String,List<Double>>>();
 		actDurHandler = new ActivityType2DurationHandler(this.simEndTime);
 		timeClasses = new ArrayList<Integer>();
+	
 		preProcessData();
 		postProcessData();
-		writeResults(outputDir+runCase+"/analysis/");
-		writeTypicalActivityDurations(outputConfig, runCase);
 	}
 
 	/**
 	 * writes activity type to typical activity duration to file. 
 	 */
-	private void writeTypicalActivityDurations(String configFile,String runCase){
+	private void writeTypicalAndMinimumActivityDurations(String configFile,String runCase){
 
 		Config config = new Config();
 		config.addCoreModules();
 		MatsimConfigReader reader = new MatsimConfigReader(config);
 		reader.readFile(configFile);
 
-		String fileName = outputDir+runCase+"/analysis/actTyp2TypicalActDurations.txt";
+		SortedMap<String, Double> act2TypDur = new TreeMap<>();
+		SortedMap<String, Double> act2MinDur = new TreeMap<>();
+		
+		for (String actTyp :config.planCalcScore().getActivityTypes()){
+			act2TypDur.put(actTyp, config.planCalcScore().getActivityParams(actTyp).getTypicalDuration());
+			act2MinDur.put(actTyp, config.planCalcScore().getActivityParams(actTyp).getMinimalDuration());
+		}
+		
+		String fileName = outputDir+"/analysis/actTyp2TypicalAndMinimumActDurations.txt";
 		BufferedWriter writer = IOUtils.getBufferedWriter(fileName);
 		try {
-			writer.write("actType \t typicalActDuration \n");
-			for (String actTyp :config.planCalcScore().getActivityTypes()){
-				writer.write(actTyp+"\t"+config.planCalcScore().getActivityParams(actTyp).getTypicalDuration()+"\n");
+			writer.write("actType \t typicalActDuration \t minimumActDuration \n");
+			for (String actTyp :act2MinDur.keySet()){
+				writer.write(actTyp+"\t"+act2TypDur.get(actTyp)+"\t"
+			+act2MinDur.get(actTyp)+"\n");
 			}
 			writer.close();
 		} catch (Exception e) {
@@ -134,7 +147,6 @@ public class ActivityType2ActivityDurationDistribution extends AbstractAnalyisMo
 
 	@Override
 	public void preProcessData() {
-		initializeTimeClasses();
 		EventsManager manager = EventsUtils.createEventsManager();
 		MatsimEventsReader reader = new MatsimEventsReader(manager);
 		manager.addHandler(actDurHandler);
@@ -143,18 +155,19 @@ public class ActivityType2ActivityDurationDistribution extends AbstractAnalyisMo
 
 	@Override
 	public List<EventHandler> getEventHandler() {
-		return null;
+		List<EventHandler> eh =  new ArrayList<EventHandler>();
+		eh.add(actDurHandler);
+		return eh;
 	}
 
 	@Override
 	public void postProcessData() {
 		personId2ActDurations = actDurHandler.getPersonId2ActDurations();
-		getActType2ActDurationDistributionData();
 	}
 
 	@Override
 	public void writeResults(String outputFolder) {
-		String fileName = outputFolder+"/urban_actTyp2ActDurDistributionDuration.txt";
+		String fileName = outputFolder+"/actTyp2ActDurDistributionDuration.txt";
 		BufferedWriter writer = IOUtils.getBufferedWriter(fileName);
 		try {
 			writer.write("timeIndex \t ");
@@ -191,7 +204,7 @@ public class ActivityType2ActivityDurationDistribution extends AbstractAnalyisMo
 
 		PersonFilter pf = new PersonFilter();
 		for(Id<Person> id : personId2ActDurations.keySet()){
-			if(pf.isPersonFromMID(id)){
+//			if(pf.isPersonFromMID(id)){
 				Map<String, List<Double>> actTyp2Dur = personId2ActDurations.get(id);
 				for(String actTyp : actTyp2Dur.keySet()){
 					List<Double> durs = actTyp2Dur.get(actTyp);
@@ -206,7 +219,7 @@ public class ActivityType2ActivityDurationDistribution extends AbstractAnalyisMo
 						}
 					}
 				}
-			}
+//			}//
 		}
 	}
 
@@ -220,6 +233,11 @@ public class ActivityType2ActivityDurationDistribution extends AbstractAnalyisMo
 			classCounter++;
 			timeClasses.add(endOfTimeClass);
 		}
-		ActivityType2DurationHandler.log.info("Following activity duration claes are defined: "+timeClasses.toString());
+		ActivityType2DurationHandler.log.info("Following activity duration classes are defined: "+timeClasses.toString());
 	}
+
+	public Map<Id<Person>, Map<String, List<Double>>> getPersonId2ActivityType2ActivityDurations(){
+		return this.personId2ActDurations;
+	}
+
 }
