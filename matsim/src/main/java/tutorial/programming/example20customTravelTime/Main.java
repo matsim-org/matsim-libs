87e9ee114fcbb21b47053a4d220478a9bc7c8929
michaz <michael.zilske@gmail.com>
2014-11-03 13:56:51 +0000

    tutorial.programming.example20customTravelTime
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@30885 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/main/java/tutorial/programming/example20customTravelTime/Main.java
@@ -0,0 +1,68 @@
+/*
+ *  *********************************************************************** *
+ *  * project: org.matsim.*
+ *  * Main.java
+ *  *                                                                         *
+ *  * *********************************************************************** *
+ *  *                                                                         *
+ *  * copyright       : (C) 2014 by the members listed in the COPYING, *
+ *  *                   LICENSE and WARRANTY file.                            *
+ *  * email           : info at matsim dot org                                *
+ *  *                                                                         *
+ *  * *********************************************************************** *
+ *  *                                                                         *
+ *  *   This program is free software; you can redistribute it and/or modify  *
+ *  *   it under the terms of the GNU General Public License as published by  *
+ *  *   the Free Software Foundation; either version 2 of the License, or     *
+ *  *   (at your option) any later version.                                   *
+ *  *   See also COPYING, LICENSE and WARRANTY file                           *
+ *  *                                                                         *
+ *  * ***********************************************************************
+ */
+
+package tutorial.programming.example20customTravelTime;
+
+import org.matsim.analysis.VolumesAnalyzerModule;
+import org.matsim.core.controler.AbstractModule;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.corelisteners.LegHistogramModule;
+import org.matsim.core.controler.corelisteners.LinkStatsModule;
+import org.matsim.core.router.util.TravelTime;
+import org.matsim.core.trafficmonitoring.FreeSpeedTravelTime;
+
+/**
+ * How to tell the Controler *not* to use the Events-collecting
+ * TravelTimeCalculator to provide the TravelTime for the next iteration,
+ * but to use the constant FreeSpeedTravelTime instead.
+ *
+ * @author michaz
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        String configFilename = "examples/equil/config.xml";
+        Controler controler = new Controler(configFilename);
+        controler.setModules(new AbstractModule() {
+            @Override
+            public void install() {
+                // Include everything from ControlerDefaultsModule.java,
+                // but leave out TravelTimeCalculator.
+                // You can just comment out these lines if you don't want them,
+                // these modules are optional.
+                include(new LinkStatsModule());
+                include(new VolumesAnalyzerModule());
+                include(new LegHistogramModule());
+
+                // Because TravelTimeCalculatorModule is left out,
+                // we have to provide a TravelTime.
+                // This line says: Use this thing here as the TravelTime implementation.
+                // Try removing this line: You will get an error because there is no
+                // TravelTime and someone needs it.
+                bindToInstance(TravelTime.class, new FreeSpeedTravelTime());
+            }
+        });
+        controler.run();
+
+    }
+}
