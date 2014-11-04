5a73bc7be51a502094e6ac7f1ede5da22060e9d3
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2014-11-04 15:52:20 +0000

    pass parameters of framework by config group.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@30908 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/java/playground/thibautd/initialdemandgeneration/socnetgen/scripts/RunTRBModel.java
+++ playgrounds/thibautd/src/main/java/playground/thibautd/initialdemandgeneration/socnetgen/scripts/RunTRBModel.java
@@ -19,24 +19,30 @@
  * *********************************************************************** */
 package playground.thibautd.initialdemandgeneration.socnetgen.scripts;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Stack;
 
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.core.basic.v01.IdImpl;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.ConfigWriter;
+import org.matsim.core.config.Module;
 import org.matsim.core.utils.geometry.CoordImpl;
 import org.matsim.core.utils.geometry.CoordUtils;
 import org.matsim.core.utils.io.MatsimXmlParser;
 import org.matsim.core.utils.misc.Counter;
 import org.xml.sax.Attributes;
 
-import playground.ivt.utils.ArgParser;
 import playground.thibautd.initialdemandgeneration.socnetgen.framework.Agent;
 import playground.thibautd.initialdemandgeneration.socnetgen.framework.LockedSocialNetwork;
 import playground.thibautd.initialdemandgeneration.socnetgen.framework.ModelIterator;
 import playground.thibautd.initialdemandgeneration.socnetgen.framework.ModelIteratorFileListener;
 import playground.thibautd.initialdemandgeneration.socnetgen.framework.ModelRunner;
+import playground.thibautd.initialdemandgeneration.socnetgen.framework.SocialNetworkGenerationConfigGroup;
 import playground.thibautd.initialdemandgeneration.socnetgen.framework.SocialPopulation;
 import playground.thibautd.initialdemandgeneration.socnetgen.framework.ThresholdFunction;
 import playground.thibautd.initialdemandgeneration.socnetgen.framework.UtilityFunction;
@@ -51,87 +57,73 @@ public class RunTRBModel {
 	private static final Logger log =
 		Logger.getLogger(RunTRBModel.class);
 
-	private static final double TARGET_NET_SIZE = 22.0;
-	private static final double TARGET_CLUSTERING = 0.206;
-
 	public static void main(final String[] args) {
-		final ArgParser parser = new ArgParser();
+		final String configFile = args[ 0 ];
 
-		parser.setDefaultValue( "-p" , "--population-file" , null );
-		parser.setDefaultValue( "-o" , "--output-dir" , null );
-		parser.setDefaultValue( "-sp" , "--step-primary" , "1" );
-		parser.setDefaultValue( "-ss" , "--step-secondary" , "1" );
+		final SocialNetworkGenerationConfigGroup config = new SocialNetworkGenerationConfigGroup();
+		ConfigUtils.loadConfig( configFile , config );
+		printToLog( config );
 
-		parser.addSwitch( "--null" , "--run-null-model" );
-
-		main( parser.parseArgs( args ) );
-	}
-
-	private static void main(final ArgParser.Args args) {
-		final String populationFile = args.getValue( "-p" );
-		final String outputDirectory = args.getValue( "-o" );
-		final int stepSizePrimary = args.getIntegerValue( "-sp" );
-		final int stepSizeSecondary = args.getIntegerValue( "-ss" );
-		final boolean isNullModel = args.isSwitched( "--null" );
-
-		MoreIOUtils.initOut( outputDirectory );
+		MoreIOUtils.initOut( config.getOutputDirectory() );
 
 		log.info( "################################################################################" );
 		log.info( "###### start socnet gen" );
-		log.info( "###### popfile: "+populationFile );
-		log.info( "###### outputdir: "+outputDirectory );
-		log.info( "###### step size primary: "+stepSizePrimary );
-		log.info( "###### step size secondary: "+stepSizeSecondary );
-		log.info( "###### use null model: "+isNullModel );
+		log.info( "###### popfile: " + config.getInputPopulationFile() );
+		log.info( "###### outputdir: " + config.getOutputDirectory() );
+		log.info( "###### step size primary: " + config.getStepSizePrimary() );
+		log.info( "###### step size secondary: " + config.getStepSizeSecondary() );
 		log.info( "################################################################################" );
 
+		final SocialPopulation<ArentzeAgent> population = parsePopulation( config.getInputPopulationFile() );
 
-		final SocialPopulation<ArentzeAgent> population = parsePopulation( populationFile );
-		
 		final ModelRunner<ArentzeAgent> runner = new ModelRunner<ArentzeAgent>();
 
-		runner.setStepSizePrimary( stepSizePrimary );
-		runner.setStepSizeSecondary( stepSizeSecondary );
+		runner.setStepSizePrimary( config.getStepSizePrimary() );
+		runner.setStepSizeSecondary( config.getStepSizeSecondary() );
 
-		runner.setUtilityFunction(
-				new UtilityFunction<ArentzeAgent>() {
-					@Override
-					public double calcTieUtility(
-							final ArentzeAgent ego,
-							final ArentzeAgent alter) {
-						if ( isNullModel ) return 0;
+		runner.setUtilityFunction( new UtilityFunction<ArentzeAgent>() {
+			@Override
+			public double calcTieUtility( final ArentzeAgent ego , final ArentzeAgent alter ) {
+				final int ageClassDifference = Math.abs( ego.getAgeCategory() - alter.getAgeCategory() );
 
-						final int ageClassDifference = Math.abs( ego.getAgeCategory() - alter.getAgeCategory() );
+				// increase distance by 1 (normally meter) to avoid linking with all agents
+				// living in the same place.
+				// TODO: test sensitivity of the results to this
+				return -1.222 * Math.log( CoordUtils.calcDistance( ego.getCoord(), alter.getCoord() ) + 1 ) + 0.725 * dummy( ego.isMale() == alter.isMale() )
+						+ 0.918 * dummy( ageClassDifference == 0 ) - 0.227 * dummy( ageClassDifference == 2 ) - 1.314 * dummy( ageClassDifference == 3 )
+						- 1.934 * dummy( ageClassDifference == 4 );
+			}
+		} );
 
-						// increase distance by 1 (normally meter) to avoid linking with all agents
-						// living in the same place.
-						// TODO: test sensitivity of the results to this
-						return -1.222 * Math.log( CoordUtils.calcDistance( ego.getCoord() , alter.getCoord() ) + 1 )
-							+0.725 * dummy( ego.isMale() == alter.isMale() )
-							+0.918 * dummy( ageClassDifference == 0 )
-							-0.227 * dummy( ageClassDifference == 2 )
-							-1.314 * dummy( ageClassDifference == 3 )
-							-1.934 * dummy( ageClassDifference == 4 );
-					}
-				});
-		//runner.setThresholds( new ThresholdFunction( 1.735 , 1.835 ) );
-		runner.setThresholds( new ThresholdFunction( -8.8 , 230 ) );
+		runner.setThresholds(
+				new ThresholdFunction(
+					config.getInitialPrimaryThreshold(),
+					config.getInitialSecondaryThreshold() ) );
 
 		final ModelIterator modelIterator = new ModelIterator();
-		final ModelIteratorFileListener listener = new ModelIteratorFileListener( outputDirectory+"/thresholdEvolution.dat" );
+		final ModelIteratorFileListener listener = new ModelIteratorFileListener( config.getOutputDirectory() + "/thresholdEvolution.dat" );
 		//modelIterator.addListener( listener );
 		final LockedSocialNetwork network =
 			modelIterator.iterateModelToTarget(
-				runner,
-				population,
-				TARGET_NET_SIZE,
-				TARGET_CLUSTERING,
-				2);
+					runner,
+					population,
+					config.getTargetDegree(),
+					config.getTargetClustering(),
+					config.getIterationsToTarget() );
 		listener.close();
 
-		new SocialNetworkWriter( network ).write( outputDirectory+"/social-network.xml.gz" );
+		new SocialNetworkWriter( network ).write( config.getOutputDirectory() + "/social-network.xml.gz" );
 
 		MoreIOUtils.closeOutputDirLogging();
+	}
+
+	private static void printToLog( Module... groups ) {
+		final Config config = new Config();
+		for ( Module group : groups ) config.addModule( group );
+
+		final String newline = System.getProperty( "line.separator" );// use native line endings for logfile
+		final StringWriter writer = new StringWriter();
+		new ConfigWriter( config ).writeStream( new PrintWriter( writer ), newline );
 	}
 
 	private static double dummy(final boolean b) {
