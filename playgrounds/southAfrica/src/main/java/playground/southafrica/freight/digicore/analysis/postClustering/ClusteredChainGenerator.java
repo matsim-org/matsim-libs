4f71c707e83aa3572837be5cebc59e029f949e4f
smeintjes <smeintjes@users.sf.net>
2014-11-05 06:02:06 +0000

    Changed clustering parameters to pmin={10,15}, radius={10,15}.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@30912 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/southAfrica/src/main/java/playground/southafrica/freight/digicore/analysis/postClustering/ClusteredChainGenerator.java
+++ playgrounds/southAfrica/src/main/java/playground/southafrica/freight/digicore/analysis/postClustering/ClusteredChainGenerator.java
@@ -75,7 +75,7 @@ public class ClusteredChainGenerator {
 		int idField = Integer.parseInt(args[4]);
 		
 		/* These values should be set following Quintin's Design-of-Experiment inputs. */
-		double[] radii = {30}; //, 10, 15, 20, 25, 30, 35, 40};
+		double[] radii = {10, 15}; //, 10, 15, 20, 25, 30, 35, 40};
 		int[] pmins = {10, 15}; //, 10, 15, 20, 25};
 
 		/* Read the study area from shapefile. This is necessary as we
