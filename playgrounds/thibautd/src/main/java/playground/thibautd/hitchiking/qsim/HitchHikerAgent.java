d1bd161200544438eb11aa9b3904dafbcccf2dca
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2014-11-17 09:29:00 +0000

    make more readable
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@31076 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/java/playground/thibautd/hitchiking/qsim/HitchHikerAgent.java
+++ playgrounds/thibautd/src/main/java/playground/thibautd/hitchiking/qsim/HitchHikerAgent.java
@@ -149,15 +149,10 @@ public class HitchHikerAgent implements MobsimDriverPassengerAgent , HasPerson {
 			analyseCurrentStateAndSetStatus( now );
 		}
 	}
+
 	@Override
 	public boolean isWantingToArriveOnCurrentLink() {
-		// The following is the old condition: Being at the end of the plan means you arrive anyways, no matter if you are on the right or wrong link.
-		// kai, nov'14
-		if ( this.chooseNextLinkId()==null ) {
-			return true ;
-		} else {
-			return false ;
-		}
+		return chooseNextLinkId() == null;
 	}
 
 
