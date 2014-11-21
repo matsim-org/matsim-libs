aeff4471812d6628c9a3c45aae72e15f2dd682ac
vsp_nagel <2008@kainagel.org>
2014-11-21 15:07:29 +0000

    documentation
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@31165 a0517364-e873-4167-b1c8-416daa5a6b91

--- contribs/minibus/src/main/java/playground/andreas/P2/package-info.java
+++ contribs/minibus/src/main/java/playground/andreas/P2/package-info.java
@@ -2,6 +2,17 @@
  * Look into:<ul>
  * <li> {@link playground.andreas.P2.PMain}
  * </ul>
+ * 
+ * Quickstart from command line (without eclipse):<ul>
+ * <li> Create an empty directory and cd into it.
+ * <li> Download MATSim-rXXXXX.jar from http://matsim.org/files/builds/ into that directory.
+ * <li> Download MATSim-libs.zip from ... into that directory and unzip.
+ * <li> Download minibus...zip from ... into that directory and unzip.
+ * <li> Get test scenario from ... .
+ * <li> Type
+ * <code> java -Xmx2000m -cp MATSim-rXXXXX.jar:minibus-0.X.0-SNAPSHOT-rXXXXX.jar playground.andreas.P2.PMain config.xml
+ * </code>
+ * </ul>
  *
  * @author aneumann
  */
