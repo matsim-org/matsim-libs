f264c9b7f9ca9aabde3269ac6c412b12be0d1e3c
vsp_nagel <2008@kainagel.org>
2014-11-26 20:32:43 +0000

    kai playground change
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@31273 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/kai/src/main/java/playground/kai/other/myfilter.sh
+++ playgrounds/kai/src/main/java/playground/kai/other/myfilter.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sed 's/<[a-zA-Z0-9,? ]*>//g' $* | sed 's/, Serializable//' | sed 's/final //g'
+sed 's/, Serializable//' $* | sed 's/final //g'
+
+#sed 's/<[a-zA-Z0-9,? ]*>//g' $* | sed 's/, Serializable//' | sed 's/final //g'
--- playgrounds/kai/src/main/java/playground/kai/test/test2/OurNewTest.java
+++ playgrounds/kai/src/main/java/playground/kai/test/test2/OurNewTest.java
@@ -1,25 +1,6 @@
 package playground.kai.test.test2;
 
-import java.util.ArrayList;
-import java.util.List;
 
-interface Plan {
-}
-
-class PlanImpl implements Plan {
-}
-
-interface Person {
-	public List<? extends Plan> getPlans() ;
-}
-
-class PersonImpl implements Person {
-	List<PlanImpl> plans = new ArrayList<PlanImpl>() ;
-	
-	public List<PlanImpl> getPlans() {
-		return (List<PlanImpl>) plans ;
-	}
-}
 
 
 
@@ -27,18 +8,11 @@ public class OurNewTest {
 	
 	public static void main( String[] args ) {
 		
-		Person person = new PersonImpl() ;
+		Double dbl = null ;
 		
-		Plan plan = new PlanImpl() ;
+		double dbl2 = dbl ;
 		
-		// this does NOT work:
-//		person.getPlans().add( plan ) ;
-	
-		// this works:
-		List<Plan> plans = (List<Plan>) person.getPlans() ;
-		plans.add( plan ) ;
-
-
+		System.out.println( dbl2 );
 		
 	}
 
