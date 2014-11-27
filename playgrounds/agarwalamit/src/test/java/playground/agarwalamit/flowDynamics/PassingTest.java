1a7fa674753b12cc53f86de6cb0cad3686321ec4
vsp_nagel <2008@kainagel.org>
2014-11-27 13:02:22 +0000

    alternative syntax (not working, commented out) for PassingTest
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@31283 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/agarwalamit/src/test/java/playground/agarwalamit/flowDynamics/PassingTest.java
+++ playgrounds/agarwalamit/src/test/java/playground/agarwalamit/flowDynamics/PassingTest.java
@@ -52,6 +52,7 @@ import org.matsim.core.controler.listener.StartupListener;
 import org.matsim.core.events.EventsUtils;
 import org.matsim.core.mobsim.qsim.ActivityEngine;
 import org.matsim.core.mobsim.qsim.QSim;
+import org.matsim.core.mobsim.qsim.QSimFactory;
 import org.matsim.core.mobsim.qsim.TeleportationEngine;
 import org.matsim.core.mobsim.qsim.agents.AgentFactory;
 import org.matsim.core.mobsim.qsim.agents.DefaultAgentFactory;
@@ -232,17 +233,19 @@ public class PassingTest {
 
 	private QSim createQSim (SimpleNetwork net, EventsManager manager){
 		Scenario sc = net.scenario;
-		QSim qSim1 = new QSim(sc, manager);
-		ActivityEngine activityEngine = new ActivityEngine();
-		qSim1.addMobsimEngine(activityEngine);
-		qSim1.addActivityHandler(activityEngine);
-
-		QNetsimEngine netsimEngine = new QNetsimEngine(qSim1);
-		qSim1.addMobsimEngine(netsimEngine);
-		qSim1.addDepartureHandler(netsimEngine.getDepartureHandler());
-		TeleportationEngine teleportationEngine = new TeleportationEngine();
-		qSim1.addMobsimEngine(teleportationEngine);
-		QSim qSim = qSim1;
+//		QSim qSim1 = new QSim(sc, manager);
+//		ActivityEngine activityEngine = new ActivityEngine();
+//		qSim1.addMobsimEngine(activityEngine);
+//		qSim1.addActivityHandler(activityEngine);
+//
+//		QNetsimEngine netsimEngine = new QNetsimEngine(qSim1);
+//		qSim1.addMobsimEngine(netsimEngine);
+//		qSim1.addDepartureHandler(netsimEngine.getDepartureHandler());
+//		TeleportationEngine teleportationEngine = new TeleportationEngine();
+//		qSim1.addMobsimEngine(teleportationEngine);
+//		QSim qSim = qSim1;
+		
+		QSim qSim = (QSim) (new QSimFactory()).createMobsim(sc, manager) ;
 
 		AgentFactory agentFactory = new DefaultAgentFactory(qSim);
 		PopulationAgentSource agentSource = new PopulationAgentSource(sc.getPopulation(), agentFactory, qSim);
