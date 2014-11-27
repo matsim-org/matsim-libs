f47b0dba3f4947bff5baa627a1ead0f2192ca61a
kaddoura <kaddoura@users.sf.net>
2014-11-27 16:23:50 +0000

    second test added for new implementation.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@31293 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/ikaddoura/src/test/java/playground/ikaddoura/noise2/NoiseTest2.java
@@ -0,0 +1,219 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2013 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+/**
+ * 
+ */
+package playground.ikaddoura.noise2;
+
+import java.io.File;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.events.EventsUtils;
+import org.matsim.core.events.MatsimEventsReader;
+import org.matsim.core.events.algorithms.EventWriterXML;
+import org.matsim.core.scenario.ScenarioImpl;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.geometry.CoordImpl;
+import org.matsim.testcases.MatsimTestUtils;
+import org.matsim.vehicles.Vehicle;
+
+/**
+ * @author ikaddoura
+ *
+ */
+
+public class NoiseTest2 {
+	
+	@Rule
+	public MatsimTestUtils testUtils = new MatsimTestUtils();
+	
+	// Tests the NoisSpatialInfo functionality separately for each function
+	@Test
+	public final void test1(){
+		
+		String configFile = testUtils.getPackageInputDirectory() + "NoiseTest2/config1.xml";
+
+		Scenario scenario = ScenarioUtils.loadScenario(ConfigUtils.loadConfig(configFile));
+		NoiseContext noiseContext = new NoiseContext(scenario, new NoiseParameters());
+		
+		noiseContext.initialize();
+		
+		// test the grid of receiver points
+		Assert.assertEquals("wrong number of receiver points", 16, noiseContext.getReceiverPoints().size(), MatsimTestUtils.EPSILON);
+		Assert.assertEquals("wrong coord for receiver point Id '10'", new CoordImpl(500, 100).toString(), noiseContext.getReceiverPoints().get(Id.create(10, ReceiverPoint.class)).getCoord().toString());
+		
+		// test the allocation of receiver point to grid cell
+		Assert.assertEquals("wrong number of grid cells for which receiver points are stored", 9, noiseContext.getZoneTuple2listOfReceiverPointIds().size(), MatsimTestUtils.EPSILON);
+				
+		// test the allocation of activity coordinates to the nearest receiver point
+		Assert.assertEquals("wrong nearest receiver point Id for coord 300/300 (x/y)", "5", noiseContext.getActivityCoord2receiverPointId().get(new CoordImpl(300, 300)).toString());
+		Assert.assertEquals("wrong nearest receiver point Id for coord 150/150 (x/y)", "9", noiseContext.getActivityCoord2receiverPointId().get(new CoordImpl(150, 150)).toString());
+		Assert.assertEquals("wrong nearest receiver point Id for coord 100/100 (x/y)", "8", noiseContext.getActivityCoord2receiverPointId().get(new CoordImpl(100, 100)).toString());
+		Assert.assertEquals("wrong nearest receiver point Id for coord 500/500 (x/y)", "2", noiseContext.getActivityCoord2receiverPointId().get(new CoordImpl(500, 500)).toString());
+					
+		// test the allocation of relevant links to the receiver point
+		Assert.assertEquals("wrong relevant link for receiver point Id '15'", 3, noiseContext.getReceiverPoints().get(Id.create("15", Link.class)).getLinkId2distanceCorrection().size());
+		Assert.assertEquals("wrong relevant link for receiver point Id '15'", 3, noiseContext.getReceiverPoints().get(Id.create("15", Link.class)).getLinkId2angleCorrection().size());
+
+		// test the distance correction term
+		Assert.assertEquals("wrong distance between receiver point Id '8' and link Id '1'", 8.749854822140838, noiseContext.getReceiverPoints().get(Id.create("8", ReceiverPoint.class)).getLinkId2distanceCorrection().get(Id.create("link0", Link.class)), MatsimTestUtils.EPSILON);		
+		
+		// test the angle correction term
+		Assert.assertEquals("wrong immission angle correction for receiver point 14 and link1", -0.8913405699036482, noiseContext.getReceiverPoints().get(Id.create("14", ReceiverPoint.class)).getLinkId2angleCorrection().get(Id.create("link1", Link.class)), MatsimTestUtils.EPSILON);		
+
+		double angle0 = 180.;
+		double immissionCorrection0 = 10 * Math.log10((angle0) / (180));
+		Assert.assertEquals("wrong immission angle correction for receiver point 12 and link5", immissionCorrection0, noiseContext.getReceiverPoints().get(Id.create("12", ReceiverPoint.class)).getLinkId2angleCorrection().get(Id.create("link5", Link.class)), MatsimTestUtils.EPSILON);		
+		
+		double angle = 65.39222026185993;
+		double immissionCorrection = 10 * Math.log10((angle) / (180));
+		Assert.assertEquals("wrong immission angle correction for receiver point 9 and link5", immissionCorrection, noiseContext.getReceiverPoints().get(Id.create("9", ReceiverPoint.class)).getLinkId2angleCorrection().get(Id.create("link5", Link.class)), MatsimTestUtils.EPSILON);		
+
+		// for a visualization of the receiver point 8 and the relevant links, see network file
+		double angle2 = 0.0000000001;
+		double immissionCorrection2 = 10 * Math.log10((angle2) / (180));
+		Assert.assertEquals("wrong immission angle correction for receiver point 8 and link5", immissionCorrection2, noiseContext.getReceiverPoints().get(Id.create("8", ReceiverPoint.class)).getLinkId2angleCorrection().get(Id.create("link5", Link.class)), MatsimTestUtils.EPSILON);
+		
+		double angle3 = 84.28940686250034;
+		double immissionCorrection3 = 10 * Math.log10((angle3) / (180));
+		Assert.assertEquals("wrong immission angle correction for receiver point 8 and link1", immissionCorrection3, noiseContext.getReceiverPoints().get(Id.create("8", ReceiverPoint.class)).getLinkId2angleCorrection().get(Id.create("link1", Link.class)), MatsimTestUtils.EPSILON);
+	
+		double angle4 = 180;
+		double immissionCorrection4 = 10 * Math.log10((angle4) / (180));
+		Assert.assertEquals("wrong immission angle correction for receiver point 8 and link0", immissionCorrection4, noiseContext.getReceiverPoints().get(Id.create("8", ReceiverPoint.class)).getLinkId2angleCorrection().get(Id.create("link0", Link.class)), MatsimTestUtils.EPSILON);
+	}
+	
+	// tests the noise emissions, immissions and exposures
+	@Test
+	public final void test2(){
+		
+		String configFile = testUtils.getPackageInputDirectory() + "NoiseTest2/config2.xml";
+
+		Controler controler = new Controler(configFile);
+		
+		controler.setOverwriteFiles(true);
+		controler.run();
+		
+		// +++++++++++++++++++++++++++++++++++++++++++++++++++
+		
+		String runDirectory = controler.getConfig().controler().getOutputDirectory() + "/";
+		
+		Config config = ConfigUtils.createConfig();
+		config.network().setInputFile(runDirectory + "output_network.xml.gz");
+		config.plans().setInputFile(runDirectory + "output_plans.xml.gz");
+		config.controler().setOutputDirectory(runDirectory);
+		config.controler().setLastIteration(controler.getConfig().controler().getLastIteration());
+		
+		NoiseParameters noiseParameters = new NoiseParameters();
+		noiseParameters.setReceiverPointGap(250.);
+		noiseParameters.setScaleFactor(1.);
+		
+		String[] consideredActivities = {"home", "work"};
+		noiseParameters.setConsideredActivities(consideredActivities);
+		
+		ScenarioImpl scenario = (ScenarioImpl) ScenarioUtils.loadScenario(config);
+		
+		String outputFilePath = runDirectory + "analysis_it." + config.controler().getLastIteration() + "/";
+		File file = new File(outputFilePath);
+		file.mkdirs();
+		
+		EventsManager events = EventsUtils.createEventsManager();
+		
+		EventWriterXML eventWriter = new EventWriterXML(outputFilePath + config.controler().getLastIteration() + ".events_NoiseImmission_Offline.xml.gz");
+		events.addHandler(eventWriter);
+			
+		NoiseContext noiseContext = new NoiseContext(scenario, noiseParameters);
+		noiseContext.initialize();
+		NoiseWriter2.writeReceiverPoints(noiseContext, outputFilePath + "/receiverPoints/");
+		
+		NoiseTimeTracker timeTracker = new NoiseTimeTracker(noiseContext, events, outputFilePath);
+		events.addHandler(timeTracker);
+				
+		MatsimEventsReader reader = new MatsimEventsReader(events);
+		reader.readFile(runDirectory + "ITERS/it." + config.controler().getLastIteration() + "/" + config.controler().getLastIteration() + ".events.xml.gz");
+		
+		timeTracker.computeFinalTimeInterval();
+
+		eventWriter.closeFile();
+		
+		// +++++++++++++++++++++++++++++++++++++++++++++++++++
+		
+		// TODO: test emissions per link and time
+		// TODO: test immissions and damages per receiver point and time
+
+		boolean tested = false;
+		int counter = 0;
+		for (NoiseEventCaused event : timeTracker.getNoiseEventsCaused()) {
+			tested = true;
+
+			if (event.getTime() == 11 * 3600. && event.getLinkId().toString().equals(Id.create("linkA5", Link.class).toString()) && event.getCausingVehicleId().toString().equals((Id.create("person_car_test1", Vehicle.class).toString()))) {
+				Assert.assertEquals("wrong cost per car for the given link and time interval", 0.0328089315079348, event.getAmount(), MatsimTestUtils.EPSILON);
+				counter++;
+			} else if (event.getTime() == 11 * 3600. && event.getLinkId().toString().equals(Id.create("linkA5", Link.class).toString()) && event.getCausingVehicleId().toString().equals((Id.create("person_car_test2", Vehicle.class).toString()))) {
+				Assert.assertEquals("wrong cost per car for the given link and time interval", 0.0328089315079348, event.getAmount(), MatsimTestUtils.EPSILON);
+				counter++;
+			} else if (event.getTime() == 11 * 3600. && event.getLinkId().toString().equals(Id.create("link2", Link.class).toString()) && event.getCausingVehicleId().toString().equals((Id.create("person_car_test1", Vehicle.class).toString()))) {
+				Assert.assertEquals("wrong cost per car for the given link and time interval", 3.992732562920194E-4, event.getAmount(), MatsimTestUtils.EPSILON);
+				counter++;
+			} else if (event.getTime() == 11 * 3600. && event.getLinkId().toString().equals(Id.create("link2", Link.class).toString()) && event.getCausingVehicleId().toString().equals((Id.create("person_car_test2", Vehicle.class).toString()))) {
+				Assert.assertEquals("wrong cost per car for the given link and time interval", 3.992732562920194E-4, event.getAmount(), MatsimTestUtils.EPSILON);
+				counter++;
+			} else {
+				Assert.assertEquals("There should either be no further events, or the amount should be zero.", 0., event.getAmount(), MatsimTestUtils.EPSILON);
+			}
+		}		
+		Assert.assertTrue("No event found to be tested.", tested);
+		Assert.assertEquals("Wrong number of total events.", 4, counter, MatsimTestUtils.EPSILON);
+		
+		boolean tested2 = false;
+		int counter2 = 0;
+		for (NoiseEventAffected event : timeTracker.getNoiseEventsAffected()) {
+			tested2 = true;
+
+			if (event.getTime() == 11 * 3600. && event.getrReceiverPointId().toString().equals(Id.create("16", ReceiverPoint.class).toString()) && event.getAffectedAgentId().toString().equals((Id.create("person_car_test1", Person.class).toString())) && event.getActType().equals("work") ) {
+				Assert.assertEquals("wrong cost per car for the given link and time interval", 0.020745817449213576, event.getAmount(), MatsimTestUtils.EPSILON);
+				counter2++;
+			} else if (event.getTime() == 11 * 3600. && event.getrReceiverPointId().toString().equals(Id.create("16", ReceiverPoint.class).toString()) && event.getAffectedAgentId().toString().equals((Id.create("person_car_test2", Person.class).toString())) && event.getActType().equals("work")) {
+				Assert.assertEquals("wrong cost per car for the given link and time interval", 0.017444990107520864, event.getAmount(), MatsimTestUtils.EPSILON);
+				counter2++;
+			} else if (event.getTime() == 11 * 3600. && event.getrReceiverPointId().toString().equals(Id.create("16", ReceiverPoint.class).toString()) && event.getAffectedAgentId().toString().equals((Id.create("person_car_test3", Person.class).toString())) && event.getActType().equals("home")) {
+				Assert.assertEquals("wrong cost per car for the given link and time interval", 0.028225601971719153, event.getAmount(), MatsimTestUtils.EPSILON);
+				counter2++;
+			} else {
+				Assert.assertEquals("There should either be no further events, or the amount should be zero.", 0., event.getAmount(), MatsimTestUtils.EPSILON);
+			}
+			
+		}		
+		Assert.assertTrue("No event found to be tested.", tested2);
+		Assert.assertEquals("Wrong number of total events.", 3, counter2, MatsimTestUtils.EPSILON);
+		
+	 }
+	
+}
