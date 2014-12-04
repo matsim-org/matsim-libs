3341ef25cb6ecabbff4da0ef4e24515619a1f1af
dhosse <dhosse@users.sf.net>
2014-12-04 13:01:46 +0000

    tests mit betroffenen personen, emissionen, immissionen und damage hinzugefügt
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@31381 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/ikaddoura/src/test/java/playground/ikaddoura/noise2/NoiseTest.java
+++ playgrounds/ikaddoura/src/test/java/playground/ikaddoura/noise2/NoiseTest.java
@@ -22,15 +22,28 @@
  */
 package playground.ikaddoura.noise2;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
+import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.events.ActivityEndEvent;
+import org.matsim.api.core.v01.events.ActivityStartEvent;
+import org.matsim.api.core.v01.events.handler.ActivityEndEventHandler;
+import org.matsim.api.core.v01.events.handler.ActivityStartEventHandler;
 import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.population.Activity;
+import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
@@ -38,9 +51,12 @@ import org.matsim.core.controler.Controler;
 import org.matsim.core.events.EventsUtils;
 import org.matsim.core.events.MatsimEventsReader;
 import org.matsim.core.events.algorithms.EventWriterXML;
+import org.matsim.core.population.MatsimPopulationReader;
 import org.matsim.core.scenario.ScenarioImpl;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.geometry.CoordImpl;
+import org.matsim.core.utils.io.IOUtils;
+import org.matsim.core.utils.misc.Time;
 import org.matsim.testcases.MatsimTestUtils;
 import org.matsim.vehicles.Vehicle;
 
@@ -165,7 +181,55 @@ public class NoiseTest {
 		
 		NoiseTimeTracker timeTracker = new NoiseTimeTracker(noiseContext, events, outputFilePath);
 		events.addHandler(timeTracker);
+		
+		final double endTime = 39600;
+		final Scenario sc = scenario;
+		final Map<Id<Person>,Double> homeActivityStartEvents = new HashMap<Id<Person>,Double>();
+		final Map<Id<Person>,Double> workActivityStartEvents = new HashMap<Id<Person>,Double>();
+		final Map<Id<Person>,Double> homeActivityEndEvents = new HashMap<Id<Person>,Double>();
+		final Map<Id<Person>,Double> workActivityEndEvents = new HashMap<Id<Person>,Double>();
+		final Map<Id<Person>,Coord> coordsHome = new HashMap<Id<Person>, Coord>();
+		final Map<Id<Person>,Coord> coordsWork = new HashMap<Id<Person>, Coord>();
+		
+		events.addHandler(new ActivityStartEventHandler() {
+			
+			@Override
+			public void reset(int iteration) {
 				
+			}
+			
+			@Override
+			public void handleEvent(ActivityStartEvent event) {
+					if(event.getActType().equals("home")){
+						homeActivityStartEvents.put(event.getPersonId(), 0.);
+						coordsHome.put(event.getPersonId(), sc.getNetwork().getLinks().get(event.getLinkId()).getCoord());
+					}
+					if(event.getActType().equals("work")){
+						workActivityStartEvents.put(event.getPersonId(), event.getTime());
+						coordsWork.put(event.getPersonId(), sc.getNetwork().getLinks().get(event.getLinkId()).getCoord());
+					}
+			}
+		});
+		
+		events.addHandler(new ActivityEndEventHandler() {
+			
+			@Override
+			public void reset(int iteration) {
+				
+			}
+			
+			@Override
+			public void handleEvent(ActivityEndEvent event) {
+				if(event.getTime() <= endTime && event.getTime() >= endTime - 3600){
+					if(event.getActType().equals("home")){
+						homeActivityEndEvents.put(event.getPersonId(), event.getTime());
+					} else if(event.getActType().equals("work")){
+						workActivityEndEvents.put(event.getPersonId(), event.getTime());
+					}
+				}
+			}
+		});
+		
 		MatsimEventsReader reader = new MatsimEventsReader(events);
 		reader.readFile(runDirectory + "ITERS/it." + config.controler().getLastIteration() + "/" + config.controler().getLastIteration() + ".events.xml.gz");
 		
@@ -175,8 +239,281 @@ public class NoiseTest {
 		
 		// +++++++++++++++++++++++++++++++++++++++++++++++++++
 		
-		// TODO: test emissions per link and time
-		// TODO: test immissions and damages per receiver point and time
+		// test considered agent units
+		
+		String separator = ";";
+		String line = null;
+		
+		String pathToConsideredAgentUnitsFile = runDirectory + "analysis_it.0/consideredAgentUnits/0.consideredAgentUnits_" + Double.toString(endTime) + ".csv";
+		
+		Map<Id<ReceiverPoint>, Double> consideredAgentsPerReceiverPoint = new HashMap<Id<ReceiverPoint>, Double>();
+		Map<String, Integer> idxFromKey = new ConcurrentHashMap<String, Integer>();
+		
+		BufferedReader br = IOUtils.getBufferedReader(pathToConsideredAgentUnitsFile);
+		
+		Scenario justForTesting = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+		MatsimPopulationReader populationReader = new MatsimPopulationReader(justForTesting);
+		populationReader.readFile("test/output/playground/ikaddoura/noise2/noiseTest/test2/output_plans.xml.gz");
+		
+		try {
+			
+			line = br.readLine();
+			
+			String[] keys = line.split(separator);
+			for(int i = 0; i < keys.length; i++){
+				idxFromKey.put(keys[i], i);
+			}
+			
+			int idxReceiverPointId = idxFromKey.get("Receiver Point Id");
+			int idxConsideredAgentUnits = idxFromKey.get("Considered Agent Units " + Time.writeTime(endTime, Time.TIMEFORMAT_HHMMSS));
+			
+			while((line = br.readLine()) != null){
+				
+				keys = line.split(separator);
+				consideredAgentsPerReceiverPoint.put(Id.create(keys[idxReceiverPointId], ReceiverPoint.class), Double.parseDouble(keys[idxConsideredAgentUnits]));
+				
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		Map<Id<ReceiverPoint>, Double> affectedPersonsPerReceiverPoint = new HashMap<Id<ReceiverPoint>, Double>();
+
+		for(Id<Person> personId : noiseContext.getScenario().getPopulation().getPersons().keySet()){
+			
+			double affectedPersons = 0.;
+			
+			double startHome = 0.;
+			double endHome = homeActivityEndEvents.containsKey(personId) ? homeActivityEndEvents.get(personId) : 30.*3600;
+			double startWork = workActivityStartEvents.containsKey(personId) ? workActivityStartEvents.get(personId) : 0.;
+			double endWork = workActivityEndEvents.containsKey(personId) ? workActivityEndEvents.get(personId) : 30.*3600;
+			
+			PersonActivityInfo actInfo = new PersonActivityInfo();
+			actInfo.setActivityType("home");
+			actInfo.setStartTime(startHome);
+			actInfo.setEndTime(endHome);
+			
+			double unitsThisPersonActivityInfo = actInfo.getDurationWithinInterval(endTime, noiseContext.getNoiseParams().getTimeBinSizeNoiseComputation()) / noiseContext.getNoiseParams().getTimeBinSizeNoiseComputation();
+			affectedPersons = ( unitsThisPersonActivityInfo * noiseContext.getNoiseParams().getScaleFactor() );
+			
+			Coord home = ((Activity)noiseContext.getScenario().getPopulation().getPersons().get(personId).getSelectedPlan().getPlanElements().get(0)).getCoord();
+			
+			Id<ReceiverPoint> rpId = noiseContext.getActivityCoord2receiverPointId().get(home);
+			
+			if(!affectedPersonsPerReceiverPoint.containsKey(rpId)){
+				affectedPersonsPerReceiverPoint.put(rpId, affectedPersons);
+			} else{
+				double n = affectedPersonsPerReceiverPoint.get(rpId);
+				affectedPersonsPerReceiverPoint.put(rpId, n + affectedPersons);
+			}
+			
+			if(startWork > 0){
+			
+			actInfo = new PersonActivityInfo();
+			actInfo.setActivityType("work");
+			actInfo.setStartTime(startWork);
+			actInfo.setEndTime(endWork);
+			
+			unitsThisPersonActivityInfo = actInfo.getDurationWithinInterval(endTime, noiseContext.getNoiseParams().getTimeBinSizeNoiseComputation()) / noiseContext.getNoiseParams().getTimeBinSizeNoiseComputation();
+			affectedPersons = ( unitsThisPersonActivityInfo * noiseContext.getNoiseParams().getScaleFactor() );
+			
+			Coord work = ((Activity)noiseContext.getScenario().getPopulation().getPersons().get(personId).getSelectedPlan().getPlanElements().get(2)).getCoord();
+			
+			rpId = noiseContext.getActivityCoord2receiverPointId().get(work);
+			
+			if(!affectedPersonsPerReceiverPoint.containsKey(rpId)){
+				affectedPersonsPerReceiverPoint.put(rpId, affectedPersons);
+			} else{
+				double n = affectedPersonsPerReceiverPoint.get(rpId);
+				affectedPersonsPerReceiverPoint.put(rpId, n + affectedPersons);
+			}
+			}
+			
+		}
+		
+		for(Id<ReceiverPoint> receiverPointId : affectedPersonsPerReceiverPoint.keySet()){
+			
+			Assert.assertEquals(consideredAgentsPerReceiverPoint.get(receiverPointId), affectedPersonsPerReceiverPoint.get(receiverPointId), MatsimTestUtils.EPSILON);
+			
+		}
+		
+		// +++++++++++++++++++++++++++++++++++++++++++++++++++
+		// test emissions per link and time
+		
+		line = null;
+		
+		String pathToEmissionsFile = runDirectory + "analysis_it.0/emissions/0.emission_" + Double.toString(endTime) + ".csv";
+		
+		Map<Id<Link>, Double> emissionsPerLink = new HashMap<Id<Link>, Double>();
+		idxFromKey = new ConcurrentHashMap<String, Integer>();
+		
+		br = IOUtils.getBufferedReader(pathToEmissionsFile);
+		
+		try {
+			
+			line = br.readLine();
+			String[] keys = line.split(separator);
+			for(int i = 0; i < keys.length; i++){
+				idxFromKey.put(keys[i], i);
+			}
+			
+			int idxLinkId = idxFromKey.get("Link Id");
+			int idxNoiseEmission = idxFromKey.get("Noise Emission " + Time.writeTime(endTime, Time.TIMEFORMAT_HHMMSS));
+			
+			while((line = br.readLine()) != null){
+				
+				keys = line.split(separator);
+				emissionsPerLink.put(Id.create(keys[idxLinkId], Link.class), Double.parseDouble(keys[idxNoiseEmission]));
+				
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		Map<Id<Link>, Integer> amountOfVehiclesPerLink = new HashMap<Id<Link>, Integer>();
+		
+		for(NoiseEventCaused event: timeTracker.getNoiseEventsCaused()){
+			
+			if(event.getTime() >= endTime - 3600 && event.getTime() <= endTime){
+				
+				Id<Link> linkId = event.getLinkId();
+				int amount = 1;
+				
+				if(amountOfVehiclesPerLink.containsKey(linkId)){
+					
+					amount = amountOfVehiclesPerLink.get(linkId) + 1;
+					amountOfVehiclesPerLink.put(linkId, amount);
+					
+				} else{
+					
+					amountOfVehiclesPerLink.put(linkId, amount);
+					
+				}
+				
+			}
+			
+		}
+		
+		for(Id<Link> linkId : amountOfVehiclesPerLink.keySet()){
+
+			double vCar = (noiseContext.getScenario().getNetwork().getLinks().get(linkId).getFreespeed()) * 3.6;
+			double vHdv = vCar;
+			double p = 0;
+			int n = amountOfVehiclesPerLink.size();
+			
+			double mittelungspegel = NoiseEquations.calculateMittelungspegelLm(n, p);
+			double Dv = NoiseEquations.calculateGeschwindigkeitskorrekturDv(vCar, vHdv, p);
+			double noiseEmission = mittelungspegel + Dv;
+			
+			Assert.assertEquals(emissionsPerLink.get(linkId), noiseEmission, MatsimTestUtils.EPSILON);
+			
+		}
+		
+		// +++++++++++++++++++++++++++++++++++++++++++++++++++
+		// test immissions and damages per receiver point and time
+		
+		line = null;
+		
+		String pathToImmissionsFile = runDirectory + "analysis_it.0/immissions/0.immission_" + Double.toString(endTime) + ".csv";
+		
+		Map<Id<ReceiverPoint>, Double> immissionPerReceiverPointId = new HashMap<Id<ReceiverPoint>, Double>();
+		
+		idxFromKey = new ConcurrentHashMap<String, Integer>();
+		
+		br = IOUtils.getBufferedReader(pathToImmissionsFile);
+		
+		try {
+			
+			line = br.readLine();
+			
+			String[] keys = line.split(separator);
+			for(int i = 0; i < keys.length; i++){
+				idxFromKey.put(keys[i], i);
+			}
+			
+			int idxReceiverPointId = idxFromKey.get("Receiver Point Id");
+			int idxImmission = idxFromKey.get("Immission " + Time.writeTime(endTime, Time.TIMEFORMAT_HHMMSS));
+			
+			while((line = br.readLine()) != null){
+				
+				keys = line.split(separator);
+				immissionPerReceiverPointId.put(Id.create(keys[idxReceiverPointId], ReceiverPoint.class), Double.parseDouble(keys[idxImmission]));
+				
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		for(ReceiverPoint rp : noiseContext.getReceiverPoints().values()){
+			
+			Map<Id<Link>, Double> linkId2IsolatedImmission = new HashMap<Id<Link>, Double>();
+			
+			for(Id<Link> linkId : rp.getLinkId2distanceCorrection().keySet()){
+				
+				double noiseImmission = 0;
+				
+				if(emissionsPerLink.get(linkId) > 0){
+					
+					noiseImmission = emissionsPerLink.get(linkId) + rp.getLinkId2distanceCorrection().get(linkId) + rp.getLinkId2angleCorrection().get(linkId);
+					
+					if(noiseImmission < 0) noiseImmission = 0;
+					
+				}
+				
+				linkId2IsolatedImmission.put(linkId, noiseImmission);
+				
+			}
+			
+			Assert.assertEquals(immissionPerReceiverPointId.get(rp.getId()), NoiseEquations.calculateResultingNoiseImmission(linkId2IsolatedImmission.values()), MatsimTestUtils.EPSILON);
+				
+		}
+		
+		line = null;
+		
+		String pathToDamagesFile = runDirectory + "analysis_it.0/damages/0.damages_" + Double.toString(endTime) + ".csv";
+		
+		Map<Id<ReceiverPoint>, Double> damagesPerReceiverPointId = new HashMap<Id<ReceiverPoint>, Double>();
+		
+		idxFromKey = new ConcurrentHashMap<String, Integer>();
+		
+		br = IOUtils.getBufferedReader(pathToDamagesFile);
+		
+		try {
+			
+			line = br.readLine();
+			
+			String[] keys = line.split(separator);
+			for(int i = 0; i < keys.length; i++){
+				idxFromKey.put(keys[i], i);
+			}
+			
+			int idxReceiverPointId = idxFromKey.get("Receiver Point Id");
+			int idxDamages = idxFromKey.get("Damages " + Time.writeTime(endTime, Time.TIMEFORMAT_HHMMSS));
+			
+			while((line = br.readLine()) != null){
+				
+				keys = line.split(separator);
+				damagesPerReceiverPointId.put(Id.create(keys[idxReceiverPointId], ReceiverPoint.class), Double.parseDouble(keys[idxDamages]));
+				
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		for(ReceiverPoint rp : noiseContext.getReceiverPoints().values()){
+			
+			double noiseImmission = immissionPerReceiverPointId.get(rp.getId());
+			double affectedAgentUnits = consideredAgentsPerReceiverPoint.get(rp.getId());
+			
+			Assert.assertEquals(damagesPerReceiverPointId.get(rp.getId()), NoiseEquations.calculateDamageCosts(noiseImmission, affectedAgentUnits, endTime, noiseParameters.getAnnualCostRate(), noiseParameters.getTimeBinSizeNoiseComputation()), MatsimTestUtils.EPSILON);
+			
+		}
+		
+		// +++++++++++++++++++++++++++++++++++++++++++++++++++
 
 		boolean tested = false;
 		int counter = 0;
