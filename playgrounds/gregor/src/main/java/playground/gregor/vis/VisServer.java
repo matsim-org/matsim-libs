557917388e711a723fd69e06c7a0caa30baaad95
laemmel <gregor.laemmel@gmail.com>
2014-12-08 20:32:22 +0000

    replacement for EventsBasedVisDebugger new version is based on a client-server architecture. It uses google protocol buffers for serialization and protobuf-rpc-duplex for communication
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@31450 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/gregor/src/main/java/playground/gregor/vis/VisServer.java
+++ playgrounds/gregor/src/main/java/playground/gregor/vis/VisServer.java
@@ -19,31 +19,100 @@
 
 package playground.gregor.vis;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Node;
 
 import playground.gregor.proto.ProtoFrame;
-import playground.gregor.proto.ProtoFrame.Frame;
-import playground.gregor.proto.ProtoFrame.Frame.Event;
-import playground.gregor.proto.ProtoFrame.Frame.Event.Type;
+import playground.gregor.proto.ProtoScenario;
+import playground.gregor.proto.ProtoScenario.Scenario.Network.Builder;
+
+import com.googlecode.protobuf.pro.duplex.PeerInfo;
+import com.googlecode.protobuf.pro.duplex.execute.RpcServerCallExecutor;
+import com.googlecode.protobuf.pro.duplex.execute.ThreadPoolCallExecutor;
+import com.googlecode.protobuf.pro.duplex.server.DuplexTcpServerPipelineFactory;
+import com.googlecode.protobuf.pro.duplex.util.RenamingThreadFactoryProxy;
 
 public class VisServer {
 
-	public static void main(String[] args) throws IOException {
+	private final BlockingVisServiceImpl service;
+	private Scenario sc;
+	public playground.gregor.proto.ProtoScenario.Scenario scenario;
 
-		Event e = ProtoFrame.Frame.Event.newBuilder()
-				.setEvntType(Type.LINK_INF).build();
-		Frame f = ProtoFrame.Frame.newBuilder().addEvnt(e).setTime(0).build();
+	public VisServer(Scenario sc, VisRequestHandler vrh) {
+		this.sc = sc;
+		initScenario();
+		this.service = new BlockingVisServiceImpl(this, vrh);
+		initServer();
+	}
 
-		FileOutputStream output = new FileOutputStream(
-				"/Users/laemmel/tmp/frame.ser");
-		f.writeTo(output);
-		output.close();
+	private void initScenario() {
+		Builder net = ProtoScenario.Scenario.Network.newBuilder();
+		List<ProtoScenario.Scenario.Network.Node> nodes = new ArrayList<>();
+		playground.gregor.proto.ProtoScenario.Scenario.Network.Node.Builder nb = ProtoScenario.Scenario.Network.Node
+				.newBuilder();
+		for (Node n : this.sc.getNetwork().getNodes().values()) {
+			nodes.add(nb.setId(n.getId().toString()).setX(n.getCoord().getX())
+					.setY(n.getCoord().getY()).build());
+		}
+		List<ProtoScenario.Scenario.Network.Link> links = new ArrayList<>();
+		playground.gregor.proto.ProtoScenario.Scenario.Network.Link.Builder lb = ProtoScenario.Scenario.Network.Link
+				.newBuilder();
+		for (Link l : this.sc.getNetwork().getLinks().values()) {
+			links.add(lb.setCapacity(l.getCapacity())
+					.setFromNodeId(l.getFromNode().getId().toString())
+					.setToNodeId(l.getToNode().getId().toString())
+					.setId(l.getId().toString()).setLanes(l.getNumberOfLanes())
+					.setLength(l.getLength()).setFreespeed(l.getFreespeed())
+					.build());
+		}
+		net.addAllNodes(nodes).addAllLinks(links);
+		this.scenario = ProtoScenario.Scenario.newBuilder()
+				.setCrs(this.sc.getConfig().global().getCoordinateSystem())
+				.setNet(net.build()).build();
 
-		Frame f2 = Frame.parseFrom(new FileInputStream(
-				"/Users/laemmel/tmp/frame.ser"));
-		System.out.println(f2);
+	}
+
+	private void initServer() {
+
+		PeerInfo server = new PeerInfo("localhost", 9090);
+
+		RpcServerCallExecutor executor = new ThreadPoolCallExecutor(3, 200);
+
+		DuplexTcpServerPipelineFactory serverFactory = new DuplexTcpServerPipelineFactory(
+				server);
+		serverFactory.setRpcServerCallExecutor(executor);
+
+		ServerBootstrap bootstrap = new ServerBootstrap();
+		bootstrap.group(
+				new NioEventLoopGroup(0, new RenamingThreadFactoryProxy("boss",
+						Executors.defaultThreadFactory())),
+				new NioEventLoopGroup(0, new RenamingThreadFactoryProxy(
+						"worker", Executors.defaultThreadFactory())));
+		bootstrap.channel(NioServerSocketChannel.class);
+		bootstrap.childHandler(serverFactory);
+		bootstrap.localAddress(server.getPort());
+		bootstrap.option(ChannelOption.SO_SNDBUF, 1048576);
+		bootstrap.option(ChannelOption.SO_RCVBUF, 1048576);
+		bootstrap.childOption(ChannelOption.SO_RCVBUF, 1048576);
+		bootstrap.childOption(ChannelOption.SO_SNDBUF, 1048576);
+		bootstrap.option(ChannelOption.TCP_NODELAY, true);
+
+		serverFactory.getRpcServiceRegistry().registerService(
+				ProtoFrame.FrameServerService
+						.newReflectiveBlockingService(this.service));
+
+		bootstrap.bind();
+
 	}
 
 }
