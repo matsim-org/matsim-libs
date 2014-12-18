f865e9a1562ece6e03e4296b3a7521e8f4864c63
rashid_waraich <rashid_waraich@users.sf.net>
2014-12-18 10:02:35 +0000

    performance improvement writing file
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@31568 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/main/java/playground/wrashid/ABMT/vehicleShare/WriteStatistics.java
+++ playgrounds/wrashid/src/main/java/playground/wrashid/ABMT/vehicleShare/WriteStatistics.java
@@ -4,9 +4,12 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 import org.matsim.api.core.v01.Id;
 import org.matsim.contrib.parking.lib.DebugLib;
+import org.matsim.contrib.parking.lib.GeneralLib;
 import org.matsim.core.controler.events.AfterMobsimEvent;
 import org.matsim.core.controler.events.IterationEndsEvent;
 import org.matsim.core.controler.listener.AfterMobsimListener;
@@ -19,16 +22,21 @@ public class WriteStatistics implements IterationEndsListener {
 
 	@Override
 	public void notifyIterationEnds(IterationEndsEvent event) {
+		ArrayList<String> list=new ArrayList<String>();
 		
-		
-		String filename = event.getControler().getControlerIO().getIterationFilename(event.getIteration(), "statistics.txt");
+		String fileName = event.getControler().getControlerIO().getIterationFilename(event.getIteration(), "statistics.txt");
 
-		try(PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(filename, true)))) {
-		    writer.println("Person \t hasEV \t distTrav \t toll \t tollEntry \t tollExit");
-		    writer.close();
-		}catch (IOException e) {
-		    System.out.println("Unable to write statistics");
-		}		
+		list.add("Person \t hasEV \t distTrav \t toll \t tollEntry \t tollExit");
+		
+//		try(PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(filename, true)))) {
+//		    writer.println("Person \t hasEV \t distTrav \t toll \t tollEntry \t tollExit");
+//		    writer.close();
+//		}catch (IOException e) {
+//		    System.out.println("Unable to write statistics");
+//		}		
+//		
+		
+		
 		
 		
 		for (Id id : DistanceTravelledWithCar.distanceTravelled.keySet()){
@@ -38,15 +46,31 @@ public class WriteStatistics implements IterationEndsListener {
 				DebugLib.stopSystemWithError();
 			}
 			
-			try(PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(filename, true)))) {
-			    writer.println(id + "\t" + VehicleInitializer.personHasElectricVehicle.get(id) + "\t" + DistanceTravelledWithCar.distanceTravelled.get(id) + "\t" + TollsManager.tollDisutilities.get(id) + "\t" + TollsManager.tollTimeOfEntry.get(id) + "\t" + TollsManager.tollTimeOfExit.get(id));
-			    writer.close();
-			}catch (IOException e) {
-			    System.out.println("Unable to write statistics");
-			}
+//			try(PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(filename, true)))) {
+//			    //writer.println(id + "\t" + VehicleInitializer.personHasElectricVehicle.get(id) + "\t" + DistanceTravelledWithCar.distanceTravelled.get(id) + "\t" + TollsManager.tollDisutilities.get(id) + "\t" + TollsManager.tollTimeOfEntry.get(id) + "\t" + TollsManager.tollTimeOfExit.get(id));
+//			    //writer.close();
+//			}catch (IOException e) {
+//			    System.out.println("Unable to write statistics");
+//			}
+//			
 			
+			StringBuilder sb=new StringBuilder();
+			sb.append(id);
+			sb.append("\t");
+			sb.append(VehicleInitializer.personHasElectricVehicle.get(id));
+			sb.append("\t");
+			sb.append(DistanceTravelledWithCar.distanceTravelled.get(id));
+			sb.append("\t");
+			sb.append(TollsManager.tollDisutilities.get(id));
+			sb.append("\t");
+			sb.append(TollsManager.tollTimeOfEntry.get(id));
+			sb.append("\t");
+			sb.append(TollsManager.tollTimeOfExit.get(id));
 			
+			list.add(sb.toString());
 		}
+		
+		GeneralLib.writeList(list, fileName);
 		
 		VehicleInitializer.prepareForNewIteration();
 		
