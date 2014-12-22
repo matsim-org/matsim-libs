44b03ca6bf9ae0aafb1226c87417b883da79f447
oblonski <oblonski@users.sf.net>
2014-12-22 09:49:57 +0000

    test commit - change path
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@31592 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/sschroeder/src/main/java/util/NetVis.java
+++ playgrounds/sschroeder/src/main/java/util/NetVis.java
@@ -4,6 +4,6 @@ import org.matsim.contrib.otfvis.OTFVis;
 
 public class NetVis {
 	public static void main(String[] args) {
-		OTFVis.playNetwork("sensitivity/network.xml");
+		OTFVis.playNetwork("input/grid10.xml");
 	}
 }
