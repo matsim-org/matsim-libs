eebdceca65dc694d2a034e2d843f420e89e08d00
vsp_nagel <2008@kainagel.org>
2014-12-30 14:34:04 +0000

    moving a test case to where (I think) it belongs
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@31627 a0517364-e873-4167-b1c8-416daa5a6b91

--- contribs/matrixbasedptrouter/src/test/java/org/matsim/contrib/matrixbasedptrouter/TestQuadTree.java
+++ contribs/matrixbasedptrouter/src/test/java/org/matsim/contrib/matrixbasedptrouter/TestQuadTree.java
@@ -20,29 +20,35 @@
 /**
  * 
  */
-package org.matsim.contrib.matsim4urbansim.pt;
+package org.matsim.contrib.matrixbasedptrouter;
 
 import java.util.Collection;
 import java.util.Iterator;
 
+import junit.framework.Assert;
+
+import org.junit.Test;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.core.utils.collections.QuadTree;
 import org.matsim.core.utils.geometry.CoordImpl;
 
 /**
- * @author thomas
+ * I <i>think</i> that this tests if the centrally provided quad tree provides expected results.  kai, dec'14
+ * 
+ * @author (of original material) thomas
+ * @author (of additional material) nagel
  *
  */
-public class TransitCoordinates {
-	
+public class TestQuadTree {
+
 	private Coord home, work;
 	private Coord pt1a, pt1b, pt2a, pt2b, pt3a, pt3b;
 	private QuadTree<String> qTree;
-	
-	public TransitCoordinates(){
+
+	public TestQuadTree(){
 		init();
 	}
-	
+
 	/**
 	 * 		 	pt1
 	 * 			|
@@ -60,55 +66,74 @@ public class TransitCoordinates {
 	 * 
 	 */
 	private void init(){
-		
+
 		home = new CoordImpl(150., 500.);
 		work = new CoordImpl(900., 100.);
-		
+
 		pt1a = new CoordImpl(200., 550.);
 		pt1b = new CoordImpl(200., 900.);
 		pt2a = new CoordImpl(100., 450.);
 		pt2b = new CoordImpl(900., 450.);
 		pt3a = new CoordImpl(150., 420.);
 		pt3b = new CoordImpl(880., 100.);
-		
+
 		double minX = 0.;
 		double maxX = 1000.;
 		double minY = 0.;
 		double maxY = 1000.;
-		
+
 		qTree= new QuadTree<String>(minX, minY, maxX, maxY);
-		
+
 		qTree.put(pt1a.getX(), pt1a.getY(), "pt1a");
 		qTree.put(pt1b.getX(), pt1b.getY(), "pt1b");
-		
+
 		qTree.put(pt2a.getX(), pt2a.getY(), "pt2a");
 		qTree.put(pt2b.getX(), pt2b.getY(), "pt2b");
-		
+
 		qTree.put(pt3a.getX(), pt3a.getY(), "pt3a");
 		qTree.put(pt3b.getX(), pt3b.getY(), "pt3b");
 	}
-	
+
 	private void determineNearestPtStation(){
-		
+
 		double distance = 100.;
-		
-		Collection<String> ptColHome = qTree.get(home.getX(), home.getY(), distance);
-		Iterator<String> homeIt = ptColHome.iterator();
-		System.out.println("Pt stations in " + distance + "m distance to home location:");
-		while(homeIt.hasNext())
-			System.out.println(homeIt.next());
-		
-		Collection<String> ptColWork = qTree.get(work.getX(), work.getY(), distance);
-		Iterator<String> workIt = ptColWork.iterator();
-		System.out.println("Pt stations in " + distance + "m distance to work location:");
-		while(workIt.hasNext())
-			System.out.println(workIt.next());
+
+		{		
+			Collection<String> ptColHome = qTree.get(home.getX(), home.getY(), distance);
+			Iterator<String> homeIt = ptColHome.iterator();
+			System.out.println("Pt stations in " + distance + "m distance to home location:");
+			StringBuilder strb = new StringBuilder() ;
+			while(homeIt.hasNext()) {
+				strb.append( homeIt.next() );
+				strb.append( ' ' );
+			}
+			System.out.println( strb );
+			Assert.assertEquals( "pt1a pt2a pt3a ", strb.toString() );
+		}
+		{
+			Collection<String> ptColWork = qTree.get(work.getX(), work.getY(), distance);
+			Iterator<String> workIt = ptColWork.iterator();
+			System.out.println("Pt stations in " + distance + "m distance to work location:");
+			StringBuilder strb = new StringBuilder() ;
+			while(workIt.hasNext()) {
+				strb.append(workIt.next() );
+				strb.append( ' ') ;
+			}
+			System.out.println( strb );
+			Assert.assertEquals( "pt3b ", strb.toString() );
+		}
 	}
-	
-	public static void main(String args[]){
-		
-		TransitCoordinates tc = new TransitCoordinates();
+
+	@Test
+	public void test() {
+		TestQuadTree tc = new TestQuadTree();
 		tc.determineNearestPtStation();
-		
+	}
+
+	public static void main(String args[]){
+
+		TestQuadTree tc = new TestQuadTree();
+		tc.determineNearestPtStation();
+
 	}
 }
