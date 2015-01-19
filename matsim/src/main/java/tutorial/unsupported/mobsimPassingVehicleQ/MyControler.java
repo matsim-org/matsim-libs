bca2516bb1f943db9416bd61e1057baf4f4d623f
michaz <michael.zilske@gmail.com>
2015-01-19 17:32:40 +0000

    updated passing vehicle queue example to the fact that most of it is now configurable in config.xml
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@31779 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/tutorial/unsupported/mobsimPassingVehicleQ/MyControler.java
+++ matsim/src/main/java/tutorial/unsupported/mobsimPassingVehicleQ/MyControler.java
@@ -1,9 +1,7 @@
 package tutorial.unsupported.mobsimPassingVehicleQ;
 
-import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
-import org.matsim.api.core.v01.network.Link;
-import org.matsim.api.core.v01.network.Node;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
@@ -15,8 +13,16 @@ import org.matsim.core.mobsim.qsim.ActivityEngine;
 import org.matsim.core.mobsim.qsim.QSim;
 import org.matsim.core.mobsim.qsim.QSimFactory;
 import org.matsim.core.mobsim.qsim.TeleportationEngine;
-import org.matsim.core.mobsim.qsim.qnetsimengine.*;
+import org.matsim.core.mobsim.qsim.agents.AgentFactory;
+import org.matsim.core.mobsim.qsim.agents.DefaultAgentFactory;
+import org.matsim.core.mobsim.qsim.agents.PopulationAgentSource;
+import org.matsim.core.mobsim.qsim.qnetsimengine.QNetsimEngine;
 import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.vehicles.VehicleType;
+import org.matsim.vehicles.VehicleUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Example to show how the standard queue can be replaced by something else.  Search for PassingVehicleQ in the code below.
@@ -30,11 +36,11 @@ import org.matsim.core.scenario.ScenarioUtils;
 class MyControler {
 
 	public static void main ( String[] args ) {
-		Logger.getLogger("blabla").warn("here") ;
 
 		// prepare the config:
 		@SuppressWarnings("unchecked")
 		Config config = ConfigUtils.loadConfig( args[0] ) ;
+        config.qsim().setLinkDynamics(QSimConfigGroup.LinkDynamics.PassingQ.toString());
 		config.vspExperimental().setRemovingUnneccessaryPlanAttributes(true) ;
 
 		// prepare the scenario
@@ -68,61 +74,47 @@ class MyControler {
 			// construct the QSim:
 			QSim qSim = new QSim(sc, eventsManager);
 
-			// add the actsim engine:
+			// add the activity engine:
 			ActivityEngine activityEngine = new ActivityEngine();
 			qSim.addMobsimEngine(activityEngine);
 			qSim.addActivityHandler(activityEngine);
 
 			// add the netsim engine:
-			NetsimNetworkFactory<QNode, QLinkImpl> netsimNetworkFactory = new NetsimNetworkFactory<QNode, QLinkImpl>() {
-				@Override
-				public QLinkImpl createNetsimLink(final Link link, final QNetwork network, final QNode toQueueNode) {
-					// !! this is the important line (note the PassingVehicleQ) !!:
-					return new QLinkImpl(link, network, toQueueNode, new PassingVehicleQ());
-				}
-				@Override
-				public QNode createNetsimNode(final Node node, QNetwork network) {
-					return new QNode(node, network);
-				}
-			};
-			QNetsimEngine netsimEngine = new QNetsimEngine(qSim, netsimNetworkFactory) ;
+			QNetsimEngine netsimEngine = new QNetsimEngine(qSim) ;
 			qSim.addMobsimEngine(netsimEngine);
 			qSim.addDepartureHandler(netsimEngine.getDepartureHandler());
 
 			TeleportationEngine teleportationEngine = new TeleportationEngine();
 			qSim.addMobsimEngine(teleportationEngine);
 
-			// The following is only necessary if you want to add vehicles of different length.
-//			AgentFactory agentFactory;
-//
-//			agentFactory = new DefaultAgentFactory(qSim);
-//
-//			PopulationAgentSource agentSource = new PopulationAgentSource(sc.getPopulation(), agentFactory, qSim);
-//			Map<String, VehicleType> modeVehicleTypes = new HashMap<String, VehicleType>();
-//
-//			VehicleType car = VehicleUtils.getFactory().createVehicleType(new IdImpl("car"));
-//			car.setMaximumVelocity(60.0/3.6);
-//			car.setPcuEquivalents(1.0);
-//			modeVehicleTypes.put("car", car);
-//
-//			VehicleType bike = VehicleUtils.getFactory().createVehicleType(new IdImpl("bike"));
-//			bike.setMaximumVelocity(60.0/3.6);
-//			bike.setPcuEquivalents(0.25);
-//			modeVehicleTypes.put("bike", bike);
-//
-//			VehicleType bicycles = VehicleUtils.getFactory().createVehicleType(new IdImpl("bicycle"));
-//			bicycles.setMaximumVelocity(15.0/3.6);
-//			bicycles.setPcuEquivalents(0.05);
-//			modeVehicleTypes.put("bicycle", bicycles);
-//
-//			VehicleType walks = VehicleUtils.getFactory().createVehicleType(new IdImpl("walk"));
-//			walks.setMaximumVelocity(1.5);
-//			walks.setPcuEquivalents(0.10);  			// assumed pcu for walks is 0.1
-//			modeVehicleTypes.put("walk", walks);
-//
-//			agentSource.setModeVehicleTypes(modeVehicleTypes);
-//
-//			qSim.addAgentSource(agentSource);
+			AgentFactory agentFactory = new DefaultAgentFactory(qSim);
+
+			PopulationAgentSource agentSource = new PopulationAgentSource(sc.getPopulation(), agentFactory, qSim);
+			Map<String, VehicleType> modeVehicleTypes = new HashMap<>();
+
+			VehicleType car = VehicleUtils.getFactory().createVehicleType(Id.create("car", VehicleType.class));
+			car.setMaximumVelocity(60.0/3.6);
+			car.setPcuEquivalents(1.0);
+			modeVehicleTypes.put("car", car);
+
+			VehicleType bike = VehicleUtils.getFactory().createVehicleType(Id.create("bike", VehicleType.class));
+			bike.setMaximumVelocity(60.0/3.6);
+			bike.setPcuEquivalents(0.25);
+			modeVehicleTypes.put("bike", bike);
+
+			VehicleType bicycles = VehicleUtils.getFactory().createVehicleType(Id.create("bicycle", VehicleType.class));
+			bicycles.setMaximumVelocity(15.0/3.6);
+			bicycles.setPcuEquivalents(0.05);
+			modeVehicleTypes.put("bicycle", bicycles);
+
+			VehicleType walks = VehicleUtils.getFactory().createVehicleType(Id.create("walk", VehicleType.class));
+			walks.setMaximumVelocity(1.5);
+			walks.setPcuEquivalents(0.10);  			// assumed pcu for walks is 0.1
+			modeVehicleTypes.put("walk", walks);
+
+			agentSource.setModeVehicleTypes(modeVehicleTypes);
+
+			qSim.addAgentSource(agentSource);
 
 			return qSim ;
 		}
