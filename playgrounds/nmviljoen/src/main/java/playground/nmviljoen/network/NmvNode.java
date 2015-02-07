5b13f83153fe3c21fb023b932d33614468d0bfce
jwjoubert <johan.joubert@up.ac.za>
2015-02-07 12:43:04 +0000

    Overriding equals() method.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@31991 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/nmviljoen/src/main/java/playground/nmviljoen/network/NmvNode.java
+++ playgrounds/nmviljoen/src/main/java/playground/nmviljoen/network/NmvNode.java
@@ -1,5 +1,9 @@
 package playground.nmviljoen.network;
 
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.apache.log4j.Logger;
 
 public class NmvNode implements Comparable<NmvNode>{
 	public String intID;
@@ -35,4 +39,33 @@ public class NmvNode implements Comparable<NmvNode>{
 		return this.getId().compareTo(o.getId().toString());
 	}
 	
+	@Override
+	public boolean equals(Object o){
+		return this.getId().equalsIgnoreCase(((NmvNode)o).getId());
+	}
+	
+	public static void main(String[] args){
+		Logger log = Logger.getLogger(NmvNode.class);
+		NmvNode n1 = new NmvNode("1", "1", 0.0, 0.0);
+		NmvNode n2 = new NmvNode("2", "2", 0.0, 0.0);
+		NmvNode n11 = new NmvNode("1", "1", 0.0, 0.0);
+		log.info("n1.compareTo(n2): " + n1.compareTo(n2));
+		log.info("n1.compareTo(n3): " + n1.compareTo(n11));
+		log.info("n2.compareTo(n3): " + n2.compareTo(n11));
+		
+		NmvNode n3 = new NmvNode("3", "3", 0.0, 0.0);
+		Map<NmvNode, Double> map = new TreeMap<NmvNode, Double>();
+		map.put(n1, 1.0);
+		map.put(n2, 2.0);
+		map.put(n3, 3.0);
+		String s = "";
+		for(NmvNode n : map.keySet()){
+			s += " ... " + n.getId();
+		}
+		log.info(s);
+		
+		log.info("Is n1 in there? " + map.containsKey(n11));
+		log.info("Is n1 and n11 the same? " + n1.equals(n11));
+	}
+
 }
