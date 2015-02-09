fb1af14df0945fdb3bec5a2fe23246ee4365bc65
jbischoff <bischoff@vsp.tu-berlin.de>
2015-02-09 15:16:31 +0000

    added class to run otfvis in accordance with naming convention
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@32020 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ contribs/otfvis/src/main/java/org/matsim/contrib/otfvis/RunOTFVis.java
@@ -0,0 +1,34 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * OTFVis.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008, 2009 by the members listed in the COPYING,  *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.contrib.otfvis;
+/**
+ * A generic starter for the OnTheFly Visualizer that supports
+ * MATSim Visualizer Files (MVI) and MATSim Config Files for the live simulation and visualization.
+ *
+ * 
+ */
+public class RunOTFVis {
+
+	public static void main(String[] args) {
+		OTFVis.main(args);
+	}
+
+}
