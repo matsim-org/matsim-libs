c29af0e35a306f785566aa7ea29c30d5ef0fe36a
michal-m <michal.mac@gmail.com>
2015-02-12 21:21:58 +0000

    swing/chart utils added
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@32130 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ contribs/dvrp/src/main/java/org/matsim/contrib/dvrp/util/chart/SwingUtils.java
@@ -0,0 +1,36 @@
+package org.matsim.contrib.dvrp.util.chart;
+
+import java.awt.Window;
+
+import javax.swing.SwingUtilities;
+
+
+/**
+ * @author michalm
+ */
+public class SwingUtils
+{
+    public static void showWindow(final Window window, boolean modal)
+    {
+        Runnable r = new Runnable() {
+            public void run()
+            {
+                window.pack();
+                window.setVisible(true);
+            }
+        };
+
+        if (modal) {
+            try {
+                SwingUtilities.invokeAndWait(r);
+                window.dispose();
+            }
+            catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        else {
+            SwingUtilities.invokeLater(r);
+        }
+    }
+}
