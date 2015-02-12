fce1311000f935a060d64019b5fb5215194f36dc
artemc <chakirov@ivt.baug.ethz.ch>
2015-02-12 03:36:36 +0000

    added smartCardAnalysisTools
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@32099 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/artemc/src/test/java/playground/artemc/heterogeneity/scoring/HeterogeneousCharyparNagelScoringFunctionForAnalysisFactoryTest.java
@@ -0,0 +1,13 @@
+package playground.artemc.heterogeneity.scoring;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HeterogeneousCharyparNagelScoringFunctionForAnalysisFactoryTest {
+	
+	@Test
+	public void testAdjustParametersForHeterogeneity() throws Exception {
+
+	}
+}
