bf609bce69eada728356b2249c8d816c241acfd8
michaz <michael.zilske@gmail.com>
2015-02-13 14:24:22 +0000

    removed old import
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@32148 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/artemc/src/main/java/playground/artemc/scenarioTools/StructuresToFacilities.java
+++ playgrounds/artemc/src/main/java/playground/artemc/scenarioTools/StructuresToFacilities.java
@@ -3,14 +3,12 @@ package playground.artemc.scenarioTools;
 import com.mysql.jdbc.Statement;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
-
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.api.experimental.facilities.ActivityFacility;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.facilities.*;
 import org.matsim.core.network.NetworkImpl;
 import org.matsim.core.network.NetworkReaderMatsimV1;
 import org.matsim.core.network.NetworkUtils;
@@ -18,15 +16,9 @@ import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.collections.Tuple;
 import org.matsim.core.utils.geometry.CoordImpl;
 import org.matsim.core.utils.gis.ShapeFileReader;
-import org.matsim.facilities.ActivityFacilitiesImpl;
-import org.matsim.facilities.ActivityFacilityImpl;
-import org.matsim.facilities.ActivityOption;
-import org.matsim.facilities.ActivityOptionImpl;
-import org.matsim.facilities.FacilitiesWriter;
-import org.matsim.facilities.OpeningTimeImpl;
+import org.matsim.facilities.*;
 import org.opengis.feature.Property;
 import org.opengis.feature.simple.SimpleFeature;
-
 import playground.artemc.utils.DataBaseAdmin;
 
 import java.io.File;
