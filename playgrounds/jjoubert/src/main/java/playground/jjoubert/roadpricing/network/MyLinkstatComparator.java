c783aa4a37cb83294ad7a61f72a751f2a384a85e
vsp_nagel <2008@kainagel.org>
2015-03-02 07:01:09 +0000

    changing wrong import for Link class
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@32333 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/jjoubert/src/main/java/playground/jjoubert/roadpricing/network/MyLinkstatComparator.java
+++ playgrounds/jjoubert/src/main/java/playground/jjoubert/roadpricing/network/MyLinkstatComparator.java
@@ -31,13 +31,12 @@ import java.util.TreeMap;
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.network.NetworkReaderMatsimV1;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.io.IOUtils;
-
-import com.sun.syndication.feed.atom.Link;
 
 public class MyLinkstatComparator {
 	private final static Logger log = Logger.getLogger(MyLinkstatComparator.class);
