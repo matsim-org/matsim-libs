8ad654507f55909b57634531c9b1dd79157cdf8d
vsp_nagel <2008@kainagel.org>
2015-03-02 07:02:02 +0000

    chaning wrong import for Person class
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@32334 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/wrashid/src/main/java/playground/wrashid/thelma/y2030/psl/FilteroutAllEventsAbroad.java
+++ playgrounds/wrashid/src/main/java/playground/wrashid/thelma/y2030/psl/FilteroutAllEventsAbroad.java
@@ -28,6 +28,7 @@ import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.population.Person;
 import org.matsim.contrib.parking.lib.GeneralLib;
 import org.matsim.contrib.parking.lib.obj.Matrix;
 import org.matsim.core.utils.gis.ShapeFileReader;
@@ -36,7 +37,6 @@ import org.opengis.feature.simple.SimpleFeature;
 import playground.wrashid.lib.tools.kml.BasicPointVisualizer;
 import playground.wrashid.lib.tools.kml.Color;
 
-import com.sun.syndication.feed.atom.Person;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 
