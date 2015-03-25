dd781a62e3fdcb85e2bc6f83624a3a52d339eeb1
laemmel <gregor.laemmel@gmail.com>
2015-03-25 17:42:03 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@32533 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/lcrociani/src/main/java/pedCA/utility/Constants.java
+++ playgrounds/lcrociani/src/main/java/pedCA/utility/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	public static final double SQRT2 = Math.sqrt(2);
 	
 	//Constant for the random seed
-	public static final long RANDOM_SEED = 24;
+	public static final long RANDOM_SEED = 42;
 	
 	//Constants for environment
 	public static final int ENV_OBSTACLE = -1;
