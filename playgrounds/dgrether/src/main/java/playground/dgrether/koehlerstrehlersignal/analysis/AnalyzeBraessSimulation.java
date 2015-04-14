aa7332725f40aea85046cdb2efa4ce962383e4be
tthunig <tthunig@vsp.tu-berlin.de>
2015-04-14 11:29:32 +0000

    changed structure of analyzing a braess simulation
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@32660 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/dgrether/src/main/java/playground/dgrether/koehlerstrehlersignal/analysis/AnalyzeBraessSimulation.java
+++ playgrounds/dgrether/src/main/java/playground/dgrether/koehlerstrehlersignal/analysis/AnalyzeBraessSimulation.java
@@ -16,7 +16,6 @@ import org.matsim.core.events.MatsimEventsReader;
 
 import playground.dgrether.DgPaths;
 import playground.dgrether.analysis.RunResultsLoader;
-import playground.dgrether.koehlerstrehlersignal.analysis.DgAnalyseCottbusKS2010.RunInfo;
 
 /**
  * Class to start the analysis of a MATSim simulation of Braess' example.
@@ -31,7 +30,7 @@ public class AnalyzeBraessSimulation {
 	// input and output information
 	private String runDirectory;
 	private String outputDir;
-	private RunInfo ri;
+	private int lastIteration;
 
 	// fields for results
 	private double totalTT;
@@ -39,16 +38,24 @@ public class AnalyzeBraessSimulation {
 	private double[] avgRouteTTs;
 	private int[] routeUsers;
 
-	public AnalyzeBraessSimulation(String runDirectory, RunInfo ri,
+	public AnalyzeBraessSimulation(String runDirectory, int lastIteration,
 			String outputDir) {
 		this.runDirectory = runDirectory;
 		this.outputDir = outputDir;
-		this.ri = ri;
+		this.lastIteration = lastIteration;
+	}
+	
+	/**
+	 * starts the analysis without using the main method of this class
+	 */
+	public void analyze() {
+		calculateResults();
+		writeResults();
 	}
 
 	private void calculateResults() {
-		RunResultsLoader runDir = new RunResultsLoader(runDirectory, ri.runId);
-		String eventsFilename = runDir.getEventsFilename(ri.iteration);
+		RunResultsLoader runDir = new RunResultsLoader(runDirectory, null);
+		String eventsFilename = runDir.getEventsFilename(lastIteration);
 
 		EventsManager eventsManager = new EventsManagerImpl();
 		BraessRouteDistributionAndTT handler = new BraessRouteDistributionAndTT();
@@ -79,7 +86,6 @@ public class AnalyzeBraessSimulation {
 		String header = "run\ttotal tt[s]\t#users up\t#users mid\t#users low\tavg tt[s] up\tavg tt[s] mid\tavg tt[s] low\ttotal tt[s] up\ttotal tt[s] mid\ttotal tt[s] low";
 		stream.println(header);
 		StringBuffer line = new StringBuffer();
-		line.append(ri.remark);
 		line.append("\t");
 		line.append(totalTT);
 		for (int i = 0; i < 3; i++) {
@@ -101,11 +107,55 @@ public class AnalyzeBraessSimulation {
 		log.info("output written to " + outputDir + "results.txt");
 	}
 
+	
+	/* -------------------- static methods ------------------- */
+	
 	/**
-	 * @param args not used
+	 * start to analyze a braess simulation.
+	 * 
+	 * if args contains information, this tool will analyze the simulation in the
+	 * run directory given from the first entry in the iteration number given in 
+	 * the second entry.
+	 * 
+	 * if args is empty, the simulation will be started via code configuration
+	 * 
+	 * @param args information for the run to analyze 
 	 */
 	public static void main(String[] args) {
 		
+		if (args == null || args.length == 0){
+			AnalyzeBraessSimulation.runFromCode();
+		}
+		else {
+			AnalyzeBraessSimulation.runFromArgs(args);
+		}
+		
+	}
+
+	/**
+	 * starts the analysis with the given input
+	 * 
+	 * @param args
+	 */
+	private static void runFromArgs(String[] args) {
+		String runDirectory = args[0];
+		String outputDir = runDirectory + "analysis/";
+		new File(outputDir).mkdir();
+
+		int lastIteration = Integer.parseInt(args[1]);
+		
+		AnalyzeBraessSimulation analyzer = new AnalyzeBraessSimulation(
+				runDirectory, lastIteration, outputDir);
+		analyzer.calculateResults();
+		analyzer.writeResults();
+	}
+
+	/**
+	 * starts the analysis with a code configuration.
+	 * please adapt your run properties here.
+	 * 
+	 */
+	private static void runFromCode() {
 		List<String> coordNames = new ArrayList<>();
 		coordNames.add("minCoord");
 		coordNames.add("greenWaveZ");
@@ -134,23 +184,18 @@ public class AnalyzeBraessSimulation {
 				String outputDir = runDirectory + "analysis/";
 				new File(outputDir).mkdir();
 
-				RunInfo ri = new RunInfo();
-				ri.runId = null;
+				int lastIteration;
 				if (coordName == "basecase")
-					ri.iteration = 100;
+					lastIteration = 100;
 				else
-					ri.iteration = 200;
-				ri.baseCase = true;
-				ri.remark = coordName;
+					lastIteration = 200;
 
 				AnalyzeBraessSimulation analyzer = new AnalyzeBraessSimulation(
-						runDirectory, ri, outputDir);
+						runDirectory, lastIteration, outputDir);
 				analyzer.calculateResults();
 				analyzer.writeResults();
 				// TODO latex writer bei LatexResultsWriter abgucken
 			}
 		}
-		
 	}
-
 }
