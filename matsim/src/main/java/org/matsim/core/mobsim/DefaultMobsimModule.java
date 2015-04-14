5de06e43e9493cb4626255148ce91f076153b9e6
michaz <michael.zilske@gmail.com>
2015-04-14 17:49:08 +0000

    DefaultMobsimModule must be included everywhere.. commented out Signals because it is missing right now. :)
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@32685 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/main/java/org/matsim/core/mobsim/DefaultMobsimModule.java
@@ -0,0 +1,39 @@
+/*
+ *  *********************************************************************** *
+ *  * project: org.matsim.*
+ *  * DefaultMobsimModule.java
+ *  *                                                                         *
+ *  * *********************************************************************** *
+ *  *                                                                         *
+ *  * copyright       : (C) 2015 by the members listed in the COPYING, *
+ *  *                   LICENSE and WARRANTY file.                            *
+ *  * email           : info at matsim dot org                                *
+ *  *                                                                         *
+ *  * *********************************************************************** *
+ *  *                                                                         *
+ *  *   This program is free software; you can redistribute it and/or modify  *
+ *  *   it under the terms of the GNU General Public License as published by  *
+ *  *   the Free Software Foundation; either version 2 of the License, or     *
+ *  *   (at your option) any later version.                                   *
+ *  *   See also COPYING, LICENSE and WARRANTY file                           *
+ *  *                                                                         *
+ *  * ***********************************************************************
+ */
+
+package org.matsim.core.mobsim;
+
+import org.matsim.core.config.groups.ControlerConfigGroup;
+import org.matsim.core.controler.AbstractModule;
+import org.matsim.core.mobsim.jdeqsim.JDEQSimulation;
+import org.matsim.core.mobsim.qsim.QSimProvider;
+
+public class DefaultMobsimModule extends AbstractModule {
+    @Override
+    public void install() {
+        if (getConfig().controler().getMobsim().equals(ControlerConfigGroup.MobsimType.qsim.toString())) {
+            bindMobsim().toProvider(QSimProvider.class);
+        } else if (getConfig().controler().getMobsim().equals(ControlerConfigGroup.MobsimType.JDEQSim.toString())) {
+            bindMobsim().to(JDEQSimulation.class);
+        }
+    }
+}
