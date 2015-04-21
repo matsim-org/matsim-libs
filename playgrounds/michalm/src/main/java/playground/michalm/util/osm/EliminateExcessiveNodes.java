36cecfc59f0fc0c4580efe4e0f7e16230ade37d5
michal-m <michal.mac@gmail.com>
2015-04-21 21:11:50 +0000

    fixed problem with compilation
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@32773 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/michalm/src/main/java/playground/michalm/util/osm/EliminateExcessiveNodes.java
+++ playgrounds/michalm/src/main/java/playground/michalm/util/osm/EliminateExcessiveNodes.java
@@ -25,6 +25,7 @@ import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.*;
 import org.matsim.contrib.dvrp.run.VrpConfigUtils;
 import org.matsim.core.network.*;
+import org.matsim.core.network.NetworkWriter;
 import org.matsim.core.scenario.ScenarioUtils;
 
 
