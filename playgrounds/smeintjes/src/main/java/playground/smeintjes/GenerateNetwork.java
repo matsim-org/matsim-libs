3c3b7d9388688794ca9f1db1db40aed16098e164
smeintjes <smeintjes@users.sf.net>
2015-04-22 08:44:41 +0000

    Created (empty) class to generate Jung networks.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@32778 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/smeintjes/src/main/java/playground/smeintjes/GenerateNetwork.java
@@ -0,0 +1,22 @@
+package playground.smeintjes;
+
+import 
+
+/**
+ * Class to generate multiple networks of a predetermined size.
+ * Output is a text file with one edge per line, written as 
+ * "sourceNode destinationNode" (only a space between the two nodes).  
+ * @author sumarie
+ *
+ */
+public class GenerateNetwork {
+
+	/** 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
