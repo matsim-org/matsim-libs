db832dbc021d85007672751cc9de1071bcd1acb3
michaz <michael.zilske@gmail.com>
2015-04-27 18:20:25 +0000

    remove freight chains from project, to be hosted separately
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@32854 a0517364-e873-4167-b1c8-416daa5a6b91

--- contribs/freightChainsFromTravelDiaries/src/main/java/org/matsim/contrib/freightChainsFromTravelDiaries/pwvm/Pwvm_Purpose.java
+++
@@ -1,8 +0,0 @@
-package org.matsim.contrib.freightChainsFromTravelDiaries.pwvm;
-
-public enum Pwvm_Purpose {
-	RUECKFAHRT_ZUM_BETRIEB,
-	RUECKFAHRT_NACH_HAUSE,
-	PERSONENWIRTSCHAFTS_VERKEHR,
-	NICHT_PERSONENWIRTSCHAFTS_VERKEHR;
-}
