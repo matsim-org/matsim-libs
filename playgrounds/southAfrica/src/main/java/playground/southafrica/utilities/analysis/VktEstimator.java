5ece6f17eee0bc573722d257673fb4b931dff1bc
jwjoubert <johan.joubert@up.ac.za>
2015-04-29 14:40:17 +0000

    Outstanding classes and changes for the City of Cape Town analyses.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@32896 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/southAfrica/src/main/java/playground/southafrica/utilities/analysis/VktEstimator.java
+++ playgrounds/southAfrica/src/main/java/playground/southafrica/utilities/analysis/VktEstimator.java
@@ -20,17 +20,27 @@
 
 package playground.southafrica.utilities.analysis;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.Plan;
 import org.matsim.api.core.v01.population.PlanElement;
+import org.matsim.api.core.v01.population.Route;
+import org.matsim.core.population.routes.GenericRoute;
+import org.matsim.core.population.routes.NetworkRoute;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.MultiLineString;
 import com.vividsolutions.jts.geom.Point;
 
 /**
@@ -43,13 +53,73 @@ public class VktEstimator {
 	final private static Logger LOG = Logger.getLogger(VktEstimator.class);
 	final static Double DISTANCE_MULTIPLIER = 1.3;
 
+	/**
+	 * Only the {@link Leg}s <i>with</i> {@link Route}s are considered.
+	 * 
+	 * @param network
+	 * @param plan
+	 * @param geom
+	 * @return
+	 */
+	public static double estimateVktFromLegs(Network network, Plan plan, Geometry geom){
+		double vkt = 0.0;
+		
+		GeometryFactory gf = new GeometryFactory();
+		
+		/* Set up a cached map of link distances inside the geometry. */
+		Map<Id<Link>, Double> linkMap = new HashMap<>();
+		
+		for(PlanElement pe : plan.getPlanElements()){
+			if(pe instanceof Leg){
+				Leg leg = (Leg)pe;
+				Route route = leg.getRoute();
+				if(route != null && route instanceof GenericRoute){
+					GenericRoute gr = (GenericRoute)route;
+					String[] sa = gr.getRouteDescription().split(" ");
+					for(String s : sa){
+						Id<Link> linkId = Id.createLinkId(s);
+						if(linkMap.containsKey(linkId)){
+							vkt += linkMap.get(linkId);
+						} else{
+							/* Calculate the distance of a link inside the geometry. */  
+							Link link = network.getLinks().get(linkId);
+							Coord c1 = link.getFromNode().getCoord();
+							Coordinate cc1 = new Coordinate(c1.getX(), c1.getY());
+							
+							Coord c2 = link.getToNode().getCoord();
+							Coordinate cc2 = new Coordinate(c2.getX(), c2.getY());
+							
+							Coordinate[] ca = {cc1, cc2};
+							LineString ls = gf.createLineString(ca);
+							
+							Geometry intersection = geom.intersection(ls);
+							if(intersection instanceof LineString){
+								linkMap.put(linkId, ((LineString)intersection).getLength());
+							} else if(intersection instanceof MultiLineString){
+								linkMap.put(linkId, ((MultiLineString)intersection).getLength());
+							}else{
+								LOG.warn("Don't know what to do if the intersection is of type " + intersection.getClass().getName());
+								LOG.warn("Using zero distance.");
+								linkMap.put(linkId, 0.0);
+							}
+							vkt += linkMap.get(linkId);
+						}
+					}
+				}
+			}
+		}
+		
+		return vkt/1000.0;
+	}
 	
 	/**
 	 * Estimates the vehicle kilometres travelled (vkt) of a {@link Plan} 
 	 * within a given {@link Geometry}. The distance is calculated as the 
 	 * overlap/intersection between the straight-line segments of the plan and 
 	 * the geometry, and then multiplied by the (currently hard coded) distance
-	 * multiplier.  
+	 * multiplier. If the {@link Plan} contains {@link Leg}s with routes, then
+	 * the vkt returned is the average of the inter-activity distance 
+	 * (weighted my the multiplier) and the observed route distance. 
 	 *  
 	 * @param plan
 	 * @param geom
@@ -136,7 +206,7 @@ public class VktEstimator {
 		} else{
 			vkt = (vktLeg + vktAct)/2.0;
 		}
-		return vkt;
+		return vkt/1000.0;
 	}
 
 }
