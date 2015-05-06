62447f09f9bcde8d2aa9fdb5ad7764802caee1b2
pieterfourie <fourie@ivt.baug.ethz.ch>
2015-05-06 07:51:07 +0000

    psim controler updated
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@32981 a0517364-e873-4167-b1c8-416daa5a6b91

--- contribs/pseudosimulation/src/main/java/org/matsim/contrib/pseudosimulation/PSimControler.java
+++ contribs/pseudosimulation/src/main/java/org/matsim/contrib/pseudosimulation/PSimControler.java
@@ -1,3 +1,24 @@
+/*
+ *   *********************************************************************** *
+ *   project: org.matsim.*
+ *                                                                           *
+ *   *********************************************************************** *
+ *                                                                           *
+ *   copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                     LICENSE and WARRANTY file.                            *
+ *   email           : info at matsim dot org                                *
+ *                                                                           *
+ *   *********************************************************************** *
+ *                                                                           *
+ *     This program is free software; you can redistribute it and/or modify  *
+ *     it under the terms of the GNU General Public License as published by  *
+ *     the Free Software Foundation; either version 2 of the License, or     *
+ *     (at your option) any later version.                                   *
+ *     See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                           *
+ *  * ***********************************************************************
+ */
+
 package org.matsim.contrib.pseudosimulation;
 
 import org.apache.commons.cli.*;
@@ -44,6 +65,12 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
+/**
+ * @author pieterfourie
+ *
+ * A controler that alternates between the QSim and PSim for the mobility simulation.
+ * Run this class with no arguments to get printed help listing current command line options.
+ */
 public class PSimControler {
     private final Config config;
     private final Scenario scenario;
