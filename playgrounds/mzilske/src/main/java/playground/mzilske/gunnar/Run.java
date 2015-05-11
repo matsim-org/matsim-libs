1566973251524d43f874c1fbee0612309d8fc89e
michaz <michael.zilske@gmail.com>
2015-05-11 18:51:19 +0000

    note to myself trying to understand gunnar's new stuff. .)
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@33091 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/mzilske/src/main/java/playground/mzilske/gunnar/Run.java
@@ -0,0 +1,131 @@
+/*
+ *  *********************************************************************** *
+ *  * project: org.matsim.*
+ *  * Run.java
+ *  *                                                                         *
+ *  * *********************************************************************** *
+ *  *                                                                         *
+ *  * copyright       : (C) 2015 by the members listed in the COPYING, *
+ *  *                   LICENSE and WARRANTY file.                            *
+ *  * email           : info at matsim dot org                                *
+ *  *                                                                         *
+ *  * *********************************************************************** *
+ *  *                                                                         *
+ *  *   This program is free software; you can redistribute it and/or modify  *
+ *  *   it under the terms of the GNU General Public License as published by  *
+ *  *   the Free Software Foundation; either version 2 of the License, or     *
+ *  *   (at your option) any later version.                                   *
+ *  *   See also COPYING, LICENSE and WARRANTY file                           *
+ *  *                                                                         *
+ *  * ***********************************************************************
+ */
+
+package playground.mzilske.gunnar;
+
+import floetteroed.utilities.math.Vector;
+import optdyts.DecisionVariable;
+import optdyts.ObjectiveFunction;
+import optdyts.SimulatorState;
+import optdyts.algorithms.DecisionVariableSetEvaluator;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class Run {
+
+    public static void main(String[] args) {
+        new Run().run();
+    }
+
+    double statex, statey = 0.0;
+
+    private void run() {
+        ObjectiveFunction<MySimulatorState> objectiveFunction = new ObjectiveFunction<MySimulatorState>() {
+            double targetx = 20;
+            double targety = -37;
+
+            @Override
+            public double evaluateState(MySimulatorState state) {
+                double dx2 = (targetx - state.x) * (targetx - state.x);
+                double dy2 = (targety - state.y) * (targety - state.y);
+                return Math.sqrt(dx2 + dy2);
+            }
+        };
+        DecisionVariableSetEvaluator<MySimulatorState, DecisionVariable> decisionVariableSetEvaluator
+                = new DecisionVariableSetEvaluator<>(new HashSet<>(Arrays.asList(
+                new DecisionVariable() {
+                    @Override
+                    public void implementInSimulation() {
+                        statex = (0.9 * statex) + (0.1 * 100) ;
+                        statey = (0.9 * statey )+ (0.1 * 100) ;
+                    }
+                }, new DecisionVariable() {
+                    @Override
+                    public void implementInSimulation() {
+                        statex = (0.9 * statex) + (0.1 * 20) ;
+                        statey = (0.9 * statey )+ (0.1 * -37);
+                    }
+                }, new DecisionVariable() {
+                    @Override
+                    public void implementInSimulation() {
+                        statex = (0.9 * statex) + (0.1 * 0) ;
+                        statey = (0.9 * statey )+ (0.1 * 0) ;
+                    }
+                }, new DecisionVariable() {
+                    @Override
+                    public void implementInSimulation() {
+                        statex = (0.9 * statex) + (0.1 * -37);
+                        statey = (0.9 * statey )+ (0.1 * 20);
+                    }
+                }, new DecisionVariable() {
+                    @Override
+                    public void implementInSimulation() {
+                        statex = (0.9 * statex) + (0.1 * 21);
+                        statey = (0.9 * statey )+ (0.1 * -38);
+                    }
+                })),
+                objectiveFunction,
+                2 * (1.0 / 12.0),
+                1.);
+        while (true) {
+            System.out.println("implement");
+            decisionVariableSetEvaluator.implementNextDecisionVariable();
+            System.out.println("register");
+            decisionVariableSetEvaluator.registerState(new MySimulatorState(statex + Math.random(), statey + Math.random()));
+            System.out.printf("%f,%f", statex, statey);
+        }
+    }
+
+    static class MySimulatorState implements SimulatorState<MySimulatorState> {
+
+        double x, y;
+
+        public MySimulatorState(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public MySimulatorState deepCopy() {
+            return new MySimulatorState(x, y);
+        }
+
+        @Override
+        public void takeOverConvexCombination(List<MySimulatorState> states, List<Double> weights) {
+            x = 0;
+            y = 0;
+            for (int i = 0; i < states.size(); i++) {
+                x += states.get(i).x * weights.get(i);
+                y += states.get(i).y * weights.get(i);
+            }
+        }
+
+        @Override
+        public Vector getReferenceToVectorRepresentation() {
+            return new Vector(new double[]{x, y});
+        }
+    }
+
+
+}
