35176e21fe324e95ca24fafaf41329c2c1db1cd8
dhosse <dhosse@users.sf.net>
2015-05-11 14:39:58 +0000

    added a method to create network from osm file
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@33085 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/dhosse/src/main/java/playground/dhosse/cl/NetConverter.java
+++ playgrounds/dhosse/src/main/java/playground/dhosse/cl/NetConverter.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.geotools.feature.simple.SimpleFeatureBuilder;
 import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
 import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.network.Node;
@@ -15,12 +16,17 @@ import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.api.core.v01.population.Population;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.network.NetworkWriter;
 import org.matsim.core.network.NodeImpl;
+import org.matsim.core.network.algorithms.NetworkCleaner;
+import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.geometry.CoordinateTransformation;
 import org.matsim.core.utils.geometry.transformations.TransformationFactory;
 import org.matsim.core.utils.gis.ShapeFileWriter;
 import org.matsim.core.utils.io.IOUtils;
+import org.matsim.core.utils.io.OsmNetworkReader;
 import org.opengis.feature.simple.SimpleFeature;
 
 import com.vividsolutions.jts.geom.Coordinate;
@@ -29,6 +35,20 @@ import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.Point;
 
 public class NetConverter {
+	
+	public void createNetwork(String osmFile, String outputFile){
+		
+		Config config = ConfigUtils.createConfig();
+		Scenario scenario = ScenarioUtils.createScenario(config);
+		Network network = scenario.getNetwork();
+		CoordinateTransformation ct = TransformationFactory.getCoordinateTransformation(TransformationFactory.WGS84, "EPSG:32719");
+		OsmNetworkReader onr = new OsmNetworkReader(network, ct);
+		onr.parse(osmFile);
+		new NetworkCleaner().run(network);
+		
+		new NetworkWriter(network).write(outputFile);
+		
+	}
 	
 	public void convertCoordinates(Network net, String outputFile){
 		
