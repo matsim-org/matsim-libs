c6c8dd2ce9075ac7ee87694717fbad7e23a16a28
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2015-05-20 11:18:45 +0000

    move analysis scripts with dependencies outside of socnetsim
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@33181 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/thibautd/src/main/java/playground/thibautd/socnetsimusages/analysis/ConvertSocialNetworkToGDF.java
+++ playgrounds/thibautd/src/main/java/playground/thibautd/socnetsimusages/analysis/ConvertSocialNetworkToGDF.java
@@ -17,7 +17,7 @@
  *   See also COPYING, LICENSE and WARRANTY file                           *
  *                                                                         *
  * *********************************************************************** */
-package playground.thibautd.socnetsim.usage.analysis.scripts;
+package playground.thibautd.socnetsimusages.analysis;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
