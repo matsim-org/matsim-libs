f3549e72b62c11e24353fb4d6447a32d1d8b651d
kaddoura <kaddoura@users.sf.net>
2015-05-22 13:42:47 +0000

    adding a test for the advanced pricing handler
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@33229 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/vsp/src/test/java/playground/vsp/congestion/AdvancedMarginalCongestionPricingTest.java
@@ -0,0 +1,339 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2015 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+/**
+ * 
+ */
+package playground.vsp.congestion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.events.PersonMoneyEvent;
+import org.matsim.api.core.v01.events.handler.PersonMoneyEventHandler;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
+import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ActivityParams;
+import org.matsim.core.controler.AbstractModule;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.OutputDirectoryHierarchy;
+import org.matsim.core.population.ActivityImpl;
+import org.matsim.core.scenario.ScenarioImpl;
+import org.matsim.core.scoring.functions.CharyparNagelScoringParameters;
+import org.matsim.testcases.MatsimTestUtils;
+import org.matsim.vis.otfvis.OTFFileWriterFactory;
+
+import playground.vsp.congestion.controler.AdvancedMarginalCongestionPricingContolerListener;
+import playground.vsp.congestion.events.CongestionEvent;
+import playground.vsp.congestion.handlers.CongestionEventHandler;
+import playground.vsp.congestion.handlers.CongestionHandlerImplV3;
+import playground.vsp.congestion.handlers.MarginalSumScoringFunction;
+import playground.vsp.congestion.handlers.TollHandler;
+import playground.vsp.congestion.routing.TollDisutilityCalculatorFactory;
+
+/**
+ * @author ikaddoura
+ *
+ */
+public class AdvancedMarginalCongestionPricingTest {
+	
+	@Rule
+	public MatsimTestUtils testUtils = new MatsimTestUtils();
+	
+	// test normal activities
+	@Test
+	public final void test0a(){
+		
+		PlanCalcScoreConfigGroup plansCalcScoreConfigGroup = new PlanCalcScoreConfigGroup();
+		ActivityParams activityParams = new ActivityParams("work");
+		activityParams.setTypicalDuration(6 * 3600.);
+		activityParams.setOpeningTime(7 * 3600.);
+		activityParams.setClosingTime(18 * 3600.);
+		
+		plansCalcScoreConfigGroup.addActivityParams(activityParams);
+		plansCalcScoreConfigGroup.setEarlyDeparture_utils_hr(0.);
+		plansCalcScoreConfigGroup.setLateArrival_utils_hr(0.);
+		plansCalcScoreConfigGroup.setMarginalUtlOfWaiting_utils_hr(0.);
+		plansCalcScoreConfigGroup.setPerforming_utils_hr(6.);
+		CharyparNagelScoringParameters params = new CharyparNagelScoringParameters(plansCalcScoreConfigGroup);
+
+		MarginalSumScoringFunction marginaSumScoringFunction = new MarginalSumScoringFunction();
+		
+		Id<Link> linkId = null;
+		
+		// test if zero delay results in zero activity delay disutility
+		ActivityImpl activity1 = new ActivityImpl("work", linkId);
+		activity1.setStartTime(10 * 3600.);
+		activity1.setEndTime(16 * 3600.);
+		double delay1 = 0 * 3600.;
+		double activityDelayDisutility1 = marginaSumScoringFunction.getNormalActivityDelayDisutility(params, activity1, delay1);
+		Assert.assertEquals("Wrong disutility from starting an activity with a delay (arriving later at the activity location).", 0., activityDelayDisutility1, MatsimTestUtils.EPSILON);
+
+		// test if a delay results in zero activity delay disutility if the agent would have arrived to late at the activity location anyway
+		ActivityImpl activity2 = new ActivityImpl("work", linkId);
+		activity2.setStartTime(19 * 3600.);
+		activity2.setEndTime(20 * 3600.);
+		double delay2 = 0.5 * 3600.;
+		double activityDelayDisutility2 = marginaSumScoringFunction.getNormalActivityDelayDisutility(params, activity2, delay2);
+		Assert.assertEquals("Wrong disutility from starting an activity with a delay (arriving later at the activity location).", 0., activityDelayDisutility2, MatsimTestUtils.EPSILON);
+	
+		// test if a delay results in zero activity delay disutility if the agent would have arrived to early at the activity location anyway
+		ActivityImpl activity3 = new ActivityImpl("work", linkId);
+		activity3.setStartTime(4 * 3600.);
+		activity3.setEndTime(5 * 3600.);
+		double delay3 = 0.5 * 3600.;
+		double activityDelayDisutility3 = marginaSumScoringFunction.getNormalActivityDelayDisutility(params, activity3, delay3);
+		Assert.assertEquals("Wrong disutility from starting an activity with a delay (arriving later at the activity location).", 0., activityDelayDisutility3, MatsimTestUtils.EPSILON);
+		
+		// test if a delay results in the right activity delay disutility if the agent would have had more time to perform the activity
+		ActivityImpl activity4 = new ActivityImpl("work", linkId);
+		activity4.setStartTime(10 * 3600.);
+		activity4.setEndTime(16 * 3600.);
+		double delay4 = 1 * 3600.;
+		double activityDelayDisutility4 = marginaSumScoringFunction.getNormalActivityDelayDisutility(params, activity4, delay4);
+		// 6 hours --> 65.549424473781 utils
+		// 5 hours --> 60 utils
+		Assert.assertEquals("Wrong disutility from starting an activity with a delay (arriving later at the activity location).", 5.549424473781310, activityDelayDisutility4, MatsimTestUtils.EPSILON);
+	}
+	
+	// test overnight activities with first and last activity of the same type
+	@Test
+	public final void test0b(){
+		
+		PlanCalcScoreConfigGroup plansCalcScoreConfigGroup = new PlanCalcScoreConfigGroup();
+		ActivityParams activityParams = new ActivityParams("overnightActivity");
+		activityParams.setTypicalDuration(12 * 3600.);
+		
+		plansCalcScoreConfigGroup.addActivityParams(activityParams);
+		plansCalcScoreConfigGroup.setEarlyDeparture_utils_hr(0.);
+		plansCalcScoreConfigGroup.setLateArrival_utils_hr(0.);
+		plansCalcScoreConfigGroup.setMarginalUtlOfWaiting_utils_hr(0.);
+		plansCalcScoreConfigGroup.setPerforming_utils_hr(6.);
+		CharyparNagelScoringParameters params = new CharyparNagelScoringParameters(plansCalcScoreConfigGroup);
+
+		MarginalSumScoringFunction marginaSumScoringFunction = new MarginalSumScoringFunction();
+		
+		Id<Link> linkId = null;
+		
+		ActivityImpl activity1 = new ActivityImpl("overnightActivity", linkId);
+		activity1.setEndTime(7 * 3600.);
+		ActivityImpl activity2 = new ActivityImpl("overnightActivity", linkId);
+		activity2.setStartTime(18 * 3600.);
+		
+		// test if zero delay results in zero activity delay disutility
+		double delay1 = 0 * 3600.;
+		double activityDelayDisutility1 = marginaSumScoringFunction.getOvernightActivityDelayDisutility(params, activity1, activity2, delay1);
+		// 6 + 7 hours --> 65.763074952494600 utils
+		// 6 + 7 hours --> 65.763074952494600 utils
+		Assert.assertEquals("Wrong disutility from starting an activity with a delay (arriving later at the activity location).", 0., activityDelayDisutility1, MatsimTestUtils.EPSILON);
+	
+		// test if a delay results in the right activity delay disutility
+		double delay2 = 1 * 3600.;
+		double activityDelayDisutility2 = marginaSumScoringFunction.getOvernightActivityDelayDisutility(params, activity1, activity2, delay2);
+		// 6 + 7 hours --> 65.763074952494600 utils
+		// 7 + 7 hours --> 71.098848947562600 utils
+		Assert.assertEquals("Wrong disutility from starting an activity with a delay (arriving later at the activity location).", 5.335773995067980, activityDelayDisutility2, MatsimTestUtils.EPSILON);
+	}
+	
+	// test overnight activities with first and last activity of different types
+	@Test
+	public final void test0c(){
+		
+		PlanCalcScoreConfigGroup plansCalcScoreConfigGroup = new PlanCalcScoreConfigGroup();
+		
+		ActivityParams activityParams1 = new ActivityParams("firstActivityType");
+		activityParams1.setTypicalDuration(12 * 3600.);
+		
+		ActivityParams activityParams2 = new ActivityParams("lastActivityType");
+		activityParams2.setTypicalDuration(12 * 3600.);
+		
+		plansCalcScoreConfigGroup.addActivityParams(activityParams1);
+		plansCalcScoreConfigGroup.addActivityParams(activityParams2);
+
+		plansCalcScoreConfigGroup.setEarlyDeparture_utils_hr(0.);
+		plansCalcScoreConfigGroup.setLateArrival_utils_hr(0.);
+		plansCalcScoreConfigGroup.setMarginalUtlOfWaiting_utils_hr(0.);
+		plansCalcScoreConfigGroup.setPerforming_utils_hr(6.);
+		CharyparNagelScoringParameters params = new CharyparNagelScoringParameters(plansCalcScoreConfigGroup);
+
+		MarginalSumScoringFunction marginaSumScoringFunction = new MarginalSumScoringFunction();
+		
+		Id<Link> linkId = null;
+		
+		ActivityImpl activity1 = new ActivityImpl("firstActivityType", linkId);
+		activity1.setEndTime(7 * 3600.);
+		ActivityImpl activity2 = new ActivityImpl("lastActivityType", linkId);
+		activity2.setStartTime(18 * 3600.);
+		
+		// test if zero delay results in zero activity delay disutility
+		double delay1 = 0 * 3600.;
+		double activityDelayDisutility1 = marginaSumScoringFunction.getOvernightActivityDelayDisutility(params, activity1, activity2, delay1);
+		// 6 --> 10.0934029996839 utils + 7 --> 21.1922519472465 utils = 31.285654946930 utils
+		// 6 --> 10.0934029996839 utils + 7 --> 21.1922519472465 utils = 31.285654946930 utils
+		Assert.assertEquals("Wrong disutility from starting an activity with a delay (arriving later at the activity location).", 0., activityDelayDisutility1, MatsimTestUtils.EPSILON);
+	
+		// test if a delay results in the right activity delay disutility
+		double delay2 = 1 * 3600.;
+		double activityDelayDisutility2 = marginaSumScoringFunction.getOvernightActivityDelayDisutility(params, activity1, activity2, delay2);
+		// 6 --> 10.0934029996839 utils + 7 --> 21.1922519472465 utils = 31.285654946930 utils
+		// 7 --> 21.1922519472465 utils + 7 --> 21.1922519472465 utils = 42.3845038944931 utils
+		Assert.assertEquals("Wrong disutility from starting an activity with a delay (arriving later at the activity location).", 11.0988489475631, activityDelayDisutility2, MatsimTestUtils.EPSILON);
+	}	
+	
+	// test if the delayed arrival at a normal activity (not the first or last activity) results in the right monetary amount
+	@Test
+	public final void test1(){
+		
+		String configFile = testUtils.getPackageInputDirectory() + "AdvancedMarginalCongestionPricingTest/config1.xml";
+
+		Controler controler = new Controler(configFile);
+		
+		EventsManager events = controler.getEvents();
+				
+		final List<CongestionEvent> congestionEvents = new ArrayList<CongestionEvent>();
+		final List<PersonMoneyEvent> moneyEvents = new ArrayList<PersonMoneyEvent>();
+
+		events.addHandler( new CongestionEventHandler() {
+
+			@Override
+			public void reset(int iteration) {				
+			}
+
+			@Override
+			public void handleEvent(CongestionEvent event) {
+				congestionEvents.add(event);
+			}	
+		});
+				
+		events.addHandler( new PersonMoneyEventHandler() {
+
+			@Override
+			public void reset(int iteration) {				
+			}
+
+			@Override
+			public void handleEvent(PersonMoneyEvent event) {
+				moneyEvents.add(event);
+			}	
+		});
+		
+		TollHandler tollHandler = new TollHandler(controler.getScenario());
+		final TollDisutilityCalculatorFactory tollDisutilityCalculatorFactory = new TollDisutilityCalculatorFactory(tollHandler);
+		controler.addOverridingModule(new AbstractModule() {
+			@Override
+			public void install() {
+				bindTravelDisutilityFactory().toInstance(tollDisutilityCalculatorFactory);
+			}
+		});
+
+		controler.addControlerListener(new AdvancedMarginalCongestionPricingContolerListener(controler.getScenario(), tollHandler, new CongestionHandlerImplV3(controler.getEvents(), (ScenarioImpl) controler.getScenario())));
+
+		controler.addSnapshotWriterFactory("otfvis", new OTFFileWriterFactory());	
+		controler.getConfig().controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.overwriteExistingFiles);
+		controler.run();
+		
+		// test if the delay is 2 seconds
+		double delay = congestionEvents.get(0).getDelay();
+		Assert.assertEquals("Wrong delay.", 2.0, delay, MatsimTestUtils.EPSILON);
+
+		double amountFromEvent = moneyEvents.get(0).getAmount();
+		double tripDelayDisutility = delay / 3600. * controler.getConfig().planCalcScore().getTraveling_utils_hr() * (-1);
+		// with delay --> 70.570685898554200
+		// without delay --> 70.573360291244900
+		double activityDelayDisutility = 70.573360291244900 - 70.570685898554200;
+		double amount = (-1) * (activityDelayDisutility + tripDelayDisutility) / controler.getConfig().planCalcScore().getMarginalUtilityOfMoney();
+		Assert.assertEquals("Wrong amount.", amount, amountFromEvent, MatsimTestUtils.EPSILON);	
+	 }
+	
+	// test if a delayed arrival at the last activity results in the right monetary amount
+	@Test
+	public final void test2(){
+		
+		String configFile = testUtils.getPackageInputDirectory() + "AdvancedMarginalCongestionPricingTest/config2.xml";
+
+		Controler controler = new Controler(configFile);
+		
+		EventsManager events = controler.getEvents();
+				
+		final List<CongestionEvent> congestionEvents = new ArrayList<CongestionEvent>();
+		final List<PersonMoneyEvent> moneyEvents = new ArrayList<PersonMoneyEvent>();
+
+		events.addHandler( new CongestionEventHandler() {
+
+			@Override
+			public void reset(int iteration) {				
+			}
+
+			@Override
+			public void handleEvent(CongestionEvent event) {
+				congestionEvents.add(event);
+			}	
+		});
+				
+		events.addHandler( new PersonMoneyEventHandler() {
+
+			@Override
+			public void reset(int iteration) {				
+			}
+
+			@Override
+			public void handleEvent(PersonMoneyEvent event) {
+				moneyEvents.add(event);
+			}	
+		});
+		
+		TollHandler tollHandler = new TollHandler(controler.getScenario());
+		final TollDisutilityCalculatorFactory tollDisutilityCalculatorFactory = new TollDisutilityCalculatorFactory(tollHandler);
+		controler.addOverridingModule(new AbstractModule() {
+			@Override
+			public void install() {
+				bindTravelDisutilityFactory().toInstance(tollDisutilityCalculatorFactory);
+			}
+		});
+
+		controler.addControlerListener(new AdvancedMarginalCongestionPricingContolerListener(controler.getScenario(), tollHandler, new CongestionHandlerImplV3(controler.getEvents(), (ScenarioImpl) controler.getScenario())));
+
+		controler.addSnapshotWriterFactory("otfvis", new OTFFileWriterFactory());	
+		controler.getConfig().controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.overwriteExistingFiles);
+		controler.run();
+				
+		// test if the delay is 2 seconds
+		double delay = congestionEvents.get(0).getDelay();
+		Assert.assertEquals("Wrong delay.", 2.0, delay, MatsimTestUtils.EPSILON);
+
+		double amountFromEvent = moneyEvents.get(0).getAmount();
+		double tripDelayDisutility = delay / 3600. * controler.getConfig().planCalcScore().getTraveling_utils_hr() * (-1);
+		
+		// home duration morning: 28800.
+		// home duration evening with delay: (24 * 3600.) - 57705.
+		// home duration evening without delay: (24 * 3600.) - 57703.
+		
+		// with delay --> 80.581739442040600
+		// without delay --> 80.584243964094500
+				
+		double activityDelayDisutility = 80.584243964094500 - 80.581739442040600;
+		double amount = (-1) * (activityDelayDisutility + tripDelayDisutility) / controler.getConfig().planCalcScore().getMarginalUtilityOfMoney();
+		Assert.assertEquals("Wrong amount.", amount, amountFromEvent, MatsimTestUtils.EPSILON);	
+	 }	
+}
