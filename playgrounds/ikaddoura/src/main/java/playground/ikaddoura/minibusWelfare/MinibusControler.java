46ba23840cd5fe146234a5e2d4d336c3675fb7cf
dhosse <dhosse@users.sf.net>
2015-06-02 12:59:40 +0000

    added a for loop that sets the freespeed in the test network to 20/12 m/s (in order to make the example work)
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@33345 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/ikaddoura/src/main/java/playground/ikaddoura/minibusWelfare/MinibusControler.java
+++ playgrounds/ikaddoura/src/main/java/playground/ikaddoura/minibusWelfare/MinibusControler.java
@@ -23,10 +23,13 @@
 package playground.ikaddoura.minibusWelfare;
 
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.network.Link;
 import org.matsim.contrib.minibus.PConfigGroup;
 import org.matsim.contrib.minibus.hook.PModule;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.PlansCalcRouteConfigGroup.ModeRoutingParams;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
 import org.matsim.core.scenario.ScenarioUtils;
@@ -38,18 +41,27 @@ import org.matsim.core.scenario.ScenarioUtils;
 public class MinibusControler {
 
 	private static String configFile = "/Users/ihab/Documents/workspace/shared-svn/projects/paratransit/paratransitWelfareExample/config.xml";
+//	private static String configFile = "C:/Users/Daniel/Documents/work/shared-svn/projects/paratransit/paratransitWelfareExample/config.xml";
 
 	public static void main(String[] args) {
 		
 		Config config = ConfigUtils.loadConfig( configFile, new PConfigGroup() ) ;
 
+		ModeRoutingParams pars = new ModeRoutingParams(TransportMode.walk);
+		pars.setTeleportedModeSpeed(3.9 / 3.6);
+		config.plansCalcRoute().addModeRoutingParams(pars);
+		
 		Scenario scenario = ScenarioUtils.loadScenario(config);
+		
+		for(Link link : scenario.getNetwork().getLinks().values()){
+			link.setFreespeed(20.0/12.0);
+		}
 		
 		Controler controler = new Controler(scenario);
 		controler.getConfig().controler().setCreateGraphs(false);
 		controler.getConfig().controler().setOverwriteFileSetting(
 				OutputDirectoryHierarchy.OverwriteFileSetting.overwriteExistingFiles );
-
+		
 		PModule builder = new PModule() ;
 		builder.configureControler(controler);
 
