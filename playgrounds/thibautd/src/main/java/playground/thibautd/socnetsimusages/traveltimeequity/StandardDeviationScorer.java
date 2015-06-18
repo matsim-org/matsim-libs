cb1ff8aff8a20dd49fd21e976c09c691a4d3add8
thibautd <thibaut.dubernet@ivt.baug.ethz.ch>
2015-06-18 14:49:54 +0000

    try including an "equity" scoring
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@33485 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/thibautd/src/main/java/playground/thibautd/socnetsimusages/traveltimeequity/StandardDeviationScorer.java
@@ -0,0 +1,159 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2013 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.thibautd.socnetsimusages.traveltimeequity;
+
+import gnu.trove.list.array.TDoubleArrayList;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.events.ActivityEndEvent;
+import org.matsim.api.core.v01.events.ActivityStartEvent;
+import org.matsim.api.core.v01.events.Event;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.contrib.socnetsim.framework.events.CourtesyEvent;
+import org.matsim.core.scoring.SumScoringFunction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author thibautd
+ */
+public class StandardDeviationScorer implements SumScoringFunction.ArbitraryEventScoring {
+	private final TravelTimesRecord travelTimesRecord;
+	private final String activityType;
+
+	private boolean isInActivity = false;
+
+	private final List<ActivityValues> activityValues = new ArrayList<>( 2 );
+	private ActivityValues currentActivityValues = null;
+	private final double betaStdDev;
+
+	public StandardDeviationScorer(
+			final TravelTimesRecord travelTimesRecord,
+			final String activityType,
+			final double betaStdDev) {
+		this.travelTimesRecord = travelTimesRecord;
+		this.activityType = activityType;
+		this.betaStdDev = betaStdDev;
+	}
+
+	@Override
+	public void handleEvent(final Event event) {
+		if ( (event instanceof CourtesyEvent) ) handleCourtesyEvent( (CourtesyEvent) event );
+		if ( (event instanceof ActivityStartEvent) ) handleActivityStartEvent((ActivityStartEvent) event);
+		if ( (event instanceof ActivityEndEvent) ) handleActivityEndEvent((ActivityEndEvent) event);
+	}
+
+	private void handleActivityStartEvent(final ActivityStartEvent event) {
+		if ( event.getActType().equals( activityType ) ) {
+			isInActivity = true;
+			currentActivityValues = new ActivityValues();
+			activityValues.add( currentActivityValues );
+
+			currentActivityValues.addArrival( event.getPersonId() , event.getTime() );
+		}
+	}
+
+	private void handleActivityEndEvent(final ActivityEndEvent event) {
+		isInActivity = false;
+
+		if ( currentActivityValues != null ) {
+			currentActivityValues.addDeparture( event.getPersonId() , event.getTime() );
+			currentActivityValues = null;
+		}
+	}
+
+	private void handleCourtesyEvent(final CourtesyEvent event) {
+		if ( !isInActivity ) return;
+
+		switch ( event.getType() ) {
+			case sayHelloEvent:
+				currentActivityValues.addArrival( event.getAlterId() , event.getTime() );
+				break;
+			case sayGoodbyeEvent:
+				currentActivityValues.addDeparture(event.getAlterId(), event.getTime());
+				break;
+			default:
+				throw new RuntimeException( event.getType()+" not recognized!?" );
+		}
+	}
+
+	@Override
+	public void finish() {}
+
+	@Override
+	public double getScore() {
+		double sumStdDevs = 0;
+		for ( ActivityValues values : activityValues ) sumStdDevs += values.calcStdDev();
+		return betaStdDev * sumStdDevs;
+	}
+
+	private class ActivityValues {
+		private final TDoubleArrayList arrivalTimes = new TDoubleArrayList();
+		private final List<Id<Person>> arrivalPersons = new ArrayList<>();
+
+		private final TDoubleArrayList departureTimes = new TDoubleArrayList();
+		private final List<Id<Person>> departurePersons = new ArrayList<>();
+
+		public void addArrival( final Id<Person> person , final double time ) {
+			arrivalPersons.add( person );
+			arrivalTimes.add( time );
+		}
+
+		public void addDeparture( final Id<Person> person , final double time ) {
+			departurePersons.add( person );
+			departureTimes.add( time );
+		}
+
+		public double calcStdDev() {
+			assert departureTimes.size() == arrivalTimes.size();
+			final TDoubleArrayList travelTimes = new TDoubleArrayList( arrivalTimes.size() * 2 );
+
+			assert arrivalTimes.size() == arrivalPersons.size();
+			for ( int i=0; i < arrivalTimes.size(); i++ ) {
+				travelTimes.add(
+						travelTimesRecord.getTravelTimeBefore(
+								arrivalPersons.get( i ),
+								arrivalTimes.get( i ) ) );
+			}
+
+			assert departureTimes.size() == departurePersons.size();
+			for ( int i=0; i < departureTimes.size(); i++ ) {
+				travelTimes.add(
+						travelTimesRecord.getTravelTimeBefore(
+								departurePersons.get( i ),
+								departureTimes.get( i ) ) );
+			}
+
+			return calcStdDev( travelTimes );
+		}
+
+		private double calcStdDev( final TDoubleArrayList travelTimes ) {
+			double avg = 0;
+			for ( double tt : travelTimes.toArray() ) avg += tt;
+			avg /= travelTimes.size();
+
+			double stdDev = 0;
+			for ( double tt : travelTimes.toArray() ) stdDev += Math.abs( tt - avg );
+			stdDev /= travelTimes.size();
+
+			return stdDev;
+		}
+	}
+
+}
