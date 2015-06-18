1869ebff5eb955d4896edf5fe24181e31553f15f
jbischoff <bischoff@vsp.tu-berlin.de>
2015-06-18 15:45:14 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@33493 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/jbischoff/src/main/java/playground/jbischoff/taxi/berlin/converters/LORUtil.java
@@ -0,0 +1,39 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * RunEmissionToolOffline.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2009 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.jbischoff.taxi.berlin.converters;
+
+
+/**
+ * @author jbischoff
+ *
+ */
+public class LORUtil {
+
+		
+	static boolean isInBerlin(String zone){
+		
+
+		if (zone.length()!=8) 
+			throw new IllegalStateException();
+		if (zone.startsWith("120"))
+		return false;
+		else return true;
+	}
+}
