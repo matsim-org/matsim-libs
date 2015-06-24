84152f75739fe366ec21aafdde5a1ee8a2068654
agarwalamit <agarwalamit@users.sf.net>
2015-06-24 14:48:12 +0000

    CausedDelayHandler as a separate class
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@33574 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/agarwalamit/src/main/java/playground/agarwalamit/analysis/congestion/CausedDelayAnalyzer.java
+++ playgrounds/agarwalamit/src/main/java/playground/agarwalamit/analysis/congestion/CausedDelayAnalyzer.java
@@ -18,24 +18,18 @@
  * *********************************************************************** */
 package playground.agarwalamit.analysis.congestion;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.SortedMap;
-import java.util.TreeMap;
 
-import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Link;
-import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.events.EventsUtils;
 
-import playground.agarwalamit.munich.ExtendedPersonFilter;
-import playground.vsp.congestion.events.CongestionEvent;
 import playground.vsp.congestion.events.CongestionEventsReader;
-import playground.vsp.congestion.handlers.CongestionEventHandler;
 
 /**
  * Calculates the delay caused for each person by reading the marginal congestion events.
@@ -80,107 +74,8 @@ public class CausedDelayAnalyzer {
 	public SortedMap<Double, Map<Id<Link>, Integer>> getTimeBin2Link2PersonCount(){
 		return handler.getTimeBin2Link2PersonCount();
 	}
-
-
-	public class CausedDelayHandler implements CongestionEventHandler {
-
-		public CausedDelayHandler(Scenario scenario, int noOfTimeBin) {
-			double simulatioEndTime = scenario.getConfig().qsim().getEndTime();
-			this.timeBinSize = simulatioEndTime /noOfTimeBin;
-			this.network = scenario.getNetwork();
-
-			initialize(noOfTimeBin, scenario);
-		}
-		
-		public CausedDelayHandler(Scenario scenario, int noOfTimeBin, boolean sortingForInsideMunich) {
-			double simulatioEndTime = scenario.getConfig().qsim().getEndTime();
-			this.timeBinSize = simulatioEndTime /noOfTimeBin;
-			this.network = scenario.getNetwork();
-			this.isSortingForInsideMunich = sortingForInsideMunich;
-			
-			initialize(noOfTimeBin, scenario);
-		}
-		
-		private void initialize(int noOfTimeBin, Scenario scenario) {
-			for (int i=0;i<noOfTimeBin;i++){
-				this.timeBin2Link2DelayCaused.put(this.timeBinSize*(i+1), new HashMap<Id<Link>,Double>());
-				this.timeBin2Person2DelayCaused.put(this.timeBinSize*(i+1), new HashMap<Id<Person>,Double>());
-				this.timeBin2Link2PersonCount.put(this.timeBinSize*(i+1), new HashMap<Id<Link>,Integer>());
-
-				Map<Id<Link>,Double> link2del = this.timeBin2Link2DelayCaused.get(this.timeBinSize*(i+1));
-				Map<Id<Link>, Integer> link2CausingPersonCount = this.timeBin2Link2PersonCount.get(this.timeBinSize*(i+1));
-				for (Id<Link> linkId : this.network.getLinks().keySet()){
-					link2del.put(linkId, 0.);
-					link2CausingPersonCount.put(linkId, 0);
-				}
-				
-				Map<Id<Person>,Double> person2del = this.timeBin2Person2DelayCaused.get(this.timeBinSize*(i+1));				
-				for (Id<Person> personId : scenario.getPopulation().getPersons().keySet()){
-					person2del.put(personId, 0.);
-				}
-			}
-		}
-
-		private boolean isSortingForInsideMunich = false;
-		private Map<Id<Person>, Double> personId2DelayCaused = new HashMap<>();
-		private Map<Id<Link>, Double> linkId2DelayCaused = new HashMap<>();
-		private final double timeBinSize;
-		private SortedMap<Double,Map<Id<Link>,Double>> timeBin2Link2DelayCaused = new TreeMap<Double, Map<Id<Link>,Double>>();
-		private SortedMap<Double,Map<Id<Link>,Integer>> timeBin2Link2PersonCount = new TreeMap<Double, Map<Id<Link>,Integer>>();
-		private SortedMap<Double,Map<Id<Person>,Double>> timeBin2Person2DelayCaused = new TreeMap<Double, Map<Id<Person>,Double>>();
-		private Network network;
-		private final ExtendedPersonFilter pf = new ExtendedPersonFilter();
-		
-		@Override
-		public void reset(int iteration) {
-			this.personId2DelayCaused.clear();
-			this.linkId2DelayCaused.clear();
-			this.timeBin2Link2DelayCaused.clear();
-			this.timeBin2Person2DelayCaused.clear();
-			this.timeBin2Link2PersonCount.clear();
-		}
-
-		@Override
-		public void handleEvent(CongestionEvent event) {
-			
-			Double time = event.getTime(); 
-			if(time ==0.0) time = this.timeBinSize;
-			double endOfTimeInterval = 0.0;
-			endOfTimeInterval = Math.ceil(time/this.timeBinSize)*this.timeBinSize;
-			if(endOfTimeInterval<=0.0)endOfTimeInterval=this.timeBinSize;
-
-			Id<Link> linkId = event.getLinkId();
-			
-			Coord linkCoord = this.network.getLinks().get(linkId).getCoord();
-			if(isSortingForInsideMunich && (!pf.isCellInsideMunichCityArea(linkCoord))) return;
-			
-			Id<Person> causingAgentId = event.getCausingAgentId();
-
-			//person count
-			Map<Id<Link>, Integer> link2PersonCount = this.timeBin2Link2PersonCount.get(endOfTimeInterval);
-			link2PersonCount.put(linkId,link2PersonCount.get(linkId)+1);
-			
-			//causing person delay
-			Map<Id<Person>,Double> causingPerson2delay = this.timeBin2Person2DelayCaused.get(endOfTimeInterval);
-			causingPerson2delay.put(causingAgentId, causingPerson2delay.get(causingAgentId)+event.getDelay());
-			
-			//link delays
-			Map<Id<Link>,Double> link2delay = this.timeBin2Link2DelayCaused.get(endOfTimeInterval);
-			link2delay.put(linkId, link2delay.get(linkId)+event.getDelay());
-		}
-
-		public SortedMap<Double, Map<Id<Link>, Double>> getTimeBin2Link2Delay() {
-			return timeBin2Link2DelayCaused;
-		}
-
-		public SortedMap<Double, Map<Id<Link>, Integer>> getTimeBin2Link2PersonCount() {
-			return timeBin2Link2PersonCount;
-		}
-
-		public SortedMap<Double, Map<Id<Person>, Double>> getTimeBin2CausingPerson2Delay() {
-			return timeBin2Person2DelayCaused;
-		}
-
+	
+	public SortedMap<Double,List<Id<Person>>> getTimeBin2ListOfTollPayers() {
+		return handler.getTimeBin2ListOfTollPayers();
 	}
-
 }
