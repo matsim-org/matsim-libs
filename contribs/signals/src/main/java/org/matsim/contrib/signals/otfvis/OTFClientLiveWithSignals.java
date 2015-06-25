19a36b7783331474eb0eb1c515540216297a3681
michaz <michael.zilske@gmail.com>
2015-06-25 09:41:32 +0000

    partially repair singals example - but id is still not serializable.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@33590 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ contribs/signals/src/main/java/org/matsim/contrib/signals/otfvis/OTFClientLiveWithSignals.java
@@ -0,0 +1,160 @@
+/*
+ *  *********************************************************************** *
+ *  * project: org.matsim.*
+ *  * OTFClientLive.java
+ *  *                                                                         *
+ *  * *********************************************************************** *
+ *  *                                                                         *
+ *  * copyright       : (C) 2015 by the members listed in the COPYING, *
+ *  *                   LICENSE and WARRANTY file.                            *
+ *  * email           : info at matsim dot org                                *
+ *  *                                                                         *
+ *  * *********************************************************************** *
+ *  *                                                                         *
+ *  *   This program is free software; you can redistribute it and/or modify  *
+ *  *   it under the terms of the GNU General Public License as published by  *
+ *  *   the Free Software Foundation; either version 2 of the License, or     *
+ *  *   (at your option) any later version.                                   *
+ *  *   See also COPYING, LICENSE and WARRANTY file                           *
+ *  *                                                                         *
+ *  * ***********************************************************************
+ */
+
+package org.matsim.contrib.signals.otfvis;
+
+import org.jdesktop.swingx.mapviewer.DefaultTileFactory;
+import org.jdesktop.swingx.mapviewer.TileFactory;
+import org.jdesktop.swingx.mapviewer.TileFactoryInfo;
+import org.jdesktop.swingx.mapviewer.wms.WMSService;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.lanes.otfvis.drawer.OTFLaneSignalDrawer;
+import org.matsim.lanes.otfvis.io.OTFLaneReader;
+import org.matsim.lanes.otfvis.io.OTFLaneWriter;
+import org.matsim.pt.otfvis.FacilityDrawer;
+import org.matsim.vis.otfvis.OTFClient;
+import org.matsim.vis.otfvis.OTFClientControl;
+import org.matsim.vis.otfvis.OTFVisConfigGroup;
+import org.matsim.vis.otfvis.caching.SimpleSceneLayer;
+import org.matsim.vis.otfvis.data.OTFClientQuadTree;
+import org.matsim.vis.otfvis.data.OTFConnectionManager;
+import org.matsim.vis.otfvis.data.OTFServerQuadTree;
+import org.matsim.vis.otfvis.data.fileio.SettingsSaver;
+import org.matsim.vis.otfvis.gui.OTFHostControlBar;
+import org.matsim.vis.otfvis.gui.OTFQueryControl;
+import org.matsim.vis.otfvis.gui.OTFQueryControlToolBar;
+import org.matsim.vis.otfvis.handler.OTFAgentsListHandler;
+import org.matsim.vis.otfvis.handler.OTFLinkAgentsHandler;
+import org.matsim.vis.otfvis.interfaces.OTFServer;
+import org.matsim.vis.otfvis.opengl.drawer.OTFOGLDrawer;
+import org.matsim.vis.otfvis.opengl.layer.OGLSimpleQuadDrawer;
+import org.matsim.vis.otfvis.opengl.layer.OGLSimpleStaticNetLayer;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class OTFClientLiveWithSignals {
+
+	public static void run(final Config config, final OTFServer server) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				OTFConnectionManager connectionManager = new OTFConnectionManager();
+				connectionManager.connectLinkToWriter(OTFLinkAgentsHandler.Writer.class);
+				connectionManager.connectWriterToReader(OTFLinkAgentsHandler.Writer.class, OTFLinkAgentsHandler.class);
+				connectionManager.connectReaderToReceiver(OTFLinkAgentsHandler.class, OGLSimpleQuadDrawer.class);
+				connectionManager.connectReceiverToLayer(OGLSimpleQuadDrawer.class, OGLSimpleStaticNetLayer.class);
+				connectionManager.connectWriterToReader(OTFAgentsListHandler.Writer.class, OTFAgentsListHandler.class);
+				
+				if (config.scenario().isUseTransit()) {
+					connectionManager.connectWriterToReader(FacilityDrawer.Writer.class, FacilityDrawer.Reader.class);
+					connectionManager.connectReaderToReceiver(FacilityDrawer.Reader.class, FacilityDrawer.DataDrawer.class);
+					connectionManager.connectReceiverToLayer(FacilityDrawer.DataDrawer.class, SimpleSceneLayer.class);
+				}
+				
+				if (config.scenario().isUseLanes() && (!config.scenario().isUseSignalSystems())) {
+					connectionManager.connectWriterToReader(OTFLaneWriter.class, OTFLaneReader.class);
+					connectionManager.connectReaderToReceiver(OTFLaneReader.class, OTFLaneSignalDrawer.class);
+					connectionManager.connectReceiverToLayer(OTFLaneSignalDrawer.class, SimpleSceneLayer.class);
+				} else if (config.scenario().isUseSignalSystems()) {
+					connectionManager.connectWriterToReader(OTFSignalWriter.class, OTFSignalReader.class);
+					connectionManager.connectReaderToReceiver(OTFSignalReader.class, OTFLaneSignalDrawer.class);
+					connectionManager.connectReceiverToLayer(OTFLaneSignalDrawer.class, SimpleSceneLayer.class);
+				}
+				OTFClient otfClient = new OTFClient();
+				otfClient.setServer(server);
+				SettingsSaver saver = new SettingsSaver("otfsettings");
+				OTFVisConfigGroup visconf = saver.tryToReadSettingsFile();
+				if (visconf == null) {
+					visconf = server.getOTFVisConfig();
+				}
+				OTFClientControl.getInstance().setOTFVisConfig(visconf); // has to be set before OTFClientQuadTree.getConstData() is invoked!
+				OTFServerQuadTree serverQuadTree = server.getQuad(connectionManager);
+				OTFClientQuadTree clientQuadTree = serverQuadTree.convertToClient(server, connectionManager);
+				clientQuadTree.getConstData();
+				OTFHostControlBar hostControlBar = otfClient.getHostControlBar();
+				OTFOGLDrawer mainDrawer = new OTFOGLDrawer(clientQuadTree, hostControlBar, ConfigUtils.addOrGetModule(config, OTFVisConfigGroup.GROUP_NAME, OTFVisConfigGroup.class));
+				OTFQueryControl queryControl = new OTFQueryControl(server, hostControlBar, visconf);
+				OTFQueryControlToolBar queryControlBar = new OTFQueryControlToolBar(queryControl, visconf);
+				queryControl.setQueryTextField(queryControlBar.getTextField());
+				otfClient.getContentPane().add(queryControlBar, BorderLayout.SOUTH);
+				mainDrawer.setQueryHandler(queryControl);
+				otfClient.addDrawerAndInitialize(mainDrawer, saver);
+				if (ConfigUtils.addOrGetModule(config, OTFVisConfigGroup.GROUP_NAME, OTFVisConfigGroup.class).isMapOverlayMode()) {
+					TileFactory tf;
+					if (ConfigUtils.addOrGetModule(config, OTFVisConfigGroup.GROUP_NAME, OTFVisConfigGroup.class).getMapBaseURL().isEmpty()) {
+						assertZoomLevel17(config);
+						tf = osmTileFactory();
+					} else {
+						WMSService wms = new WMSService(ConfigUtils.addOrGetModule(config, OTFVisConfigGroup.GROUP_NAME, OTFVisConfigGroup.class).getMapBaseURL(), ConfigUtils.addOrGetModule(config, OTFVisConfigGroup.GROUP_NAME, OTFVisConfigGroup.class).getMapLayer());
+						tf = new OTFVisWMSTileFactory(wms, ConfigUtils.addOrGetModule(config, OTFVisConfigGroup.GROUP_NAME, OTFVisConfigGroup.class).getMaximumZoom());
+					}
+					otfClient.addMapViewer(tf);
+				}
+                otfClient.pack();
+				otfClient.setVisible(true);
+			}
+		});
+	}
+	
+	private static void assertZoomLevel17(Config config) {
+		if(ConfigUtils.addOrGetModule(config, OTFVisConfigGroup.GROUP_NAME, OTFVisConfigGroup.class).getMaximumZoom() != 17) {
+			throw new RuntimeException("The OSM layer only works with maximumZoomLevel = 17. Please adjust your config.");
+		}
+	}
+	
+	private static TileFactory osmTileFactory() {
+		final int max=17;
+		TileFactoryInfo info = new TileFactoryInfo(0, 17, 17,
+				256, true, true,
+				"http://tile.openstreetmap.org",
+				"x","y","z") {
+			@Override
+			public String getTileUrl(int x, int y, int zoom) {
+				zoom = max-zoom;
+				String url = this.baseURL +"/"+zoom+"/"+x+"/"+y+".png";
+				return url;
+			}
+
+		};
+		TileFactory tf = new DefaultTileFactory(info);
+		return tf;
+	}
+	
+	private static class OTFVisWMSTileFactory extends DefaultTileFactory {
+		public OTFVisWMSTileFactory(final WMSService wms, final int maxZoom) {
+			super(new TileFactoryInfo(0, maxZoom, maxZoom, 
+					256, true, true, // tile size and x/y orientation is r2l & t2b
+					"","x","y","zoom") {
+				@Override
+				public String getTileUrl(int x, int y, int zoom) {
+					int zz = maxZoom - zoom;
+					int z = (int)Math.pow(2,(double)zz-1);
+					return wms.toWMSURL(x-z, z-1-y, zz, getTileSize(zoom));
+				}
+
+			});
+		}
+	}
+
+}
---
+++ contribs/signals/src/main/java/org/matsim/contrib/signals/otfvis/OTFVisWithSignals.java
@@ -0,0 +1,108 @@
+/*
+ *  *********************************************************************** *
+ *  * project: org.matsim.*
+ *  * OTFVisWithSignals.java
+ *  *                                                                         *
+ *  * *********************************************************************** *
+ *  *                                                                         *
+ *  * copyright       : (C) 2015 by the members listed in the COPYING, *
+ *  *                   LICENSE and WARRANTY file.                            *
+ *  * email           : info at matsim dot org                                *
+ *  *                                                                         *
+ *  * *********************************************************************** *
+ *  *                                                                         *
+ *  *   This program is free software; you can redistribute it and/or modify  *
+ *  *   it under the terms of the GNU General Public License as published by  *
+ *  *   the Free Software Foundation; either version 2 of the License, or     *
+ *  *   (at your option) any later version.                                   *
+ *  *   See also COPYING, LICENSE and WARRANTY file                           *
+ *  *                                                                         *
+ *  * ***********************************************************************
+ */
+
+package org.matsim.contrib.signals.otfvis;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.contrib.signals.builder.FromDataBuilder;
+import org.matsim.contrib.signals.data.SignalsData;
+import org.matsim.contrib.signals.data.signalgroups.v20.SignalGroupsData;
+import org.matsim.contrib.signals.data.signalsystems.v20.SignalSystemsData;
+import org.matsim.contrib.signals.mobsim.QSimSignalEngine;
+import org.matsim.contrib.signals.mobsim.SignalEngine;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.events.EventsUtils;
+import org.matsim.core.mobsim.qsim.QSim;
+import org.matsim.core.mobsim.qsim.QSimUtils;
+import org.matsim.core.mobsim.qsim.pt.TransitQSimEngine;
+import org.matsim.core.mobsim.qsim.pt.TransitStopAgentTracker;
+import org.matsim.lanes.data.v20.LaneDefinitions20;
+import org.matsim.lanes.otfvis.io.OTFLaneWriter;
+import org.matsim.pt.otfvis.FacilityDrawer;
+import org.matsim.pt.transitSchedule.api.TransitSchedule;
+import org.matsim.vis.otfvis.*;
+import org.matsim.vis.snapshotwriters.AgentSnapshotInfoFactory;
+
+public class OTFVisWithSignals {
+
+	public static void playScenario(Scenario scenario){
+		EventsManager events = EventsUtils.createEventsManager();
+		QSim qSim = QSimUtils.createDefaultQSim(scenario, events);
+		if (scenario.getConfig().scenario().isUseSignalSystems()){
+			SignalEngine engine = new QSimSignalEngine(new FromDataBuilder(scenario, events).createAndInitializeSignalSystemsManager());
+			qSim.addQueueSimulationListeners(engine);
+		}
+
+		OnTheFlyServer server = startServerAndRegisterWithQSim(scenario.getConfig(), scenario, events, qSim);
+		OTFClientLiveWithSignals.run(scenario.getConfig(), server);
+
+		qSim.run();
+	}
+
+	public static OnTheFlyServer startServerAndRegisterWithQSim(Config config, Scenario scenario, EventsManager events, QSim qSim) {
+		OnTheFlyServer server = OnTheFlyServer.createInstance(scenario, events);
+
+		// this is the start/stop facility, which may be used outside of otfvis:
+		PlayPauseMobsimListener playPauseMobsimListener = new PlayPauseMobsimListener();
+		server.setListener( playPauseMobsimListener ) ;
+		qSim.addQueueSimulationListeners(playPauseMobsimListener);
+
+		// this is to trigger otfvis-specific cleanup (quite possibly not needed):
+		qSim.addQueueSimulationListeners( new OTFVisMobsimListener(server) );
+
+		server.setSimulation(qSim);
+
+		if (config.scenario().isUseTransit()) {
+
+			Network network = scenario.getNetwork();
+			TransitSchedule transitSchedule = scenario.getTransitSchedule();
+			TransitQSimEngine transitEngine = qSim.getTransitEngine();
+			TransitStopAgentTracker agentTracker = transitEngine.getAgentTracker();
+			AgentSnapshotInfoFactory snapshotInfoFactory = qSim.getVisNetwork().getAgentSnapshotInfoFactory();
+			FacilityDrawer.Writer facilityWriter = new FacilityDrawer.Writer(network, transitSchedule, agentTracker, snapshotInfoFactory);
+			server.addAdditionalElement(facilityWriter);
+		}
+
+		if (config.scenario().isUseLanes() && (!config.scenario().isUseSignalSystems())) {
+			ConfigUtils.addOrGetModule(config, OTFVisConfigGroup.GROUP_NAME, OTFVisConfigGroup.class).setScaleQuadTreeRect(true);
+			OTFLaneWriter otfLaneWriter = new OTFLaneWriter(qSim.getVisNetwork(), (LaneDefinitions20) scenario.getScenarioElement(LaneDefinitions20.ELEMENT_NAME), scenario.getConfig());
+			server.addAdditionalElement(otfLaneWriter);
+		} else if (config.scenario().isUseSignalSystems()) {
+			ConfigUtils.addOrGetModule(config, OTFVisConfigGroup.GROUP_NAME, OTFVisConfigGroup.class).setScaleQuadTreeRect(true);
+			SignalGroupStateChangeTracker signalTracker = new SignalGroupStateChangeTracker();
+			events.addHandler(signalTracker);
+			SignalsData signalsData = (SignalsData) scenario.getScenarioElement(SignalsData.ELEMENT_NAME);
+			LaneDefinitions20 laneDefs = (LaneDefinitions20) scenario.getScenarioElement(LaneDefinitions20.ELEMENT_NAME);
+			SignalSystemsData systemsData = signalsData.getSignalSystemsData();
+			SignalGroupsData groupsData = signalsData.getSignalGroupsData();
+			OTFSignalWriter otfSignalWriter = new OTFSignalWriter(qSim.getVisNetwork(), laneDefs, scenario.getConfig(), systemsData, groupsData , signalTracker);
+			server.addAdditionalElement(otfSignalWriter);
+		}
+		server.pause();
+		return server;
+	}
+
+
+}
