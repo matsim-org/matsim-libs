0c944a8a7507beb15738e6b8250333dedfa09994
tthunig <tthunig@vsp.tu-berlin.de>
2015-07-02 10:10:54 +0000

    comment
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@33743 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/lanes/data/v20/Lane.java
+++ matsim/src/main/java/org/matsim/lanes/data/v20/Lane.java
@@ -7,16 +7,25 @@ import java.util.List;
 
 /**
  * Models a lane on a link.
- *
- * The position is specified via the distance the lane starts measured from the downstream node
- * of the link and the ids of the downstream lanes. If the distance to the downstream node equals the link length, this
- * implies that this is the most upstream (first) lane on a link. More than one first lane is permitted. If the first or
- * any other lane shall model more than one lane that exists in the reality to be modeled, increase the number of represented lanes attribute.
- *
- * For an easy modeling use the v11 lane model and the conversion class in the package.
- *
+ * 
+ * The position is specified via the distance the lane starts measured from the
+ * downstream node of the link and the ids of the downstream lanes. If the
+ * distance to the downstream node equals the link length, this implies that
+ * this is the most upstream (first) lane on a link. More than one first lane is
+ * permitted. If the first or any other lane shall model more than one lane that
+ * exists in the reality to be modeled, increase the number of represented lanes
+ * attribute.
+ * 
+ * The alignment is used to visualize parallel lanes in the correct order. The
+ * higher the alignment the more right the lane is visualized on a link. Lanes
+ * leading straight on are assigned with an alignment of zero, lanes leading
+ * left with an negative and lanes leading right with an positive alignment.
+ * 
+ * For an easy modeling use the v11 lane model and the conversion class in the
+ * package.
+ * 
  * @author dgrether
- *
+ * 
  */
 public interface Lane {
 
