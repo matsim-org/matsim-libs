6505c7ae097024d834f27041db698bdeadb3a41e
gthunig <gthunig@users.sf.net>
2015-07-03 12:20:08 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@33763 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ playgrounds/vspshk/src/main/java/playground/gthunig/utils/TimeWatch.java
@@ -0,0 +1,34 @@
+package playground.gthunig.utils;
+
+import java.util.concurrent.TimeUnit;
+
+public class TimeWatch {    
+    
+	long starts;
+
+    public static TimeWatch start() {
+        return new TimeWatch();
+    }
+
+    private TimeWatch() {
+        reset();
+    }
+
+    public TimeWatch reset() {
+        starts = System.currentTimeMillis();
+        return this;
+    }
+
+    public long time() {
+        long ends = System.currentTimeMillis();
+        return ends - starts;
+    }
+
+    public long time(TimeUnit unit) {
+        return unit.convert(time(), TimeUnit.MILLISECONDS);
+    }
+    
+    public long timeInSec() {
+    	return time(TimeUnit.SECONDS);
+    }
+}
---
+++ playgrounds/vspshk/src/main/java/playground/gthunig/utils/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author gthunig
+ *	Helpful Utilities someone(probably me) might to reuse
+ */
+package playground.gthunig.utils;
