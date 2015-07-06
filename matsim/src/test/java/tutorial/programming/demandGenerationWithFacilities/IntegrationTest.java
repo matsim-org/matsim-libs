083151523965abf38b35d0fc24e6600900f232ad
vsp_nagel <2008@kainagel.org>
2015-07-06 15:56:28 +0000

    integration test around tutorial
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@33798 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ matsim/src/test/java/tutorial/programming/demandGenerationWithFacilities/IntegrationTest.java
@@ -0,0 +1,65 @@
+/* *********************************************************************** *
+ * project: org.matsim.*												   *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package tutorial.programming.demandGenerationWithFacilities;
+
+import junit.framework.Assert;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.matsim.testcases.MatsimTestUtils;
+
+/**
+ * @author nagel
+ *
+ */
+public class IntegrationTest {
+	
+	@Rule public MatsimTestUtils utils = new MatsimTestUtils() ;
+
+	@SuppressWarnings("static-method")
+	@Test
+	public final void test() {
+		
+		try {
+			RunCreateFacilities.main(null);
+		} catch ( Exception eee ) {
+			eee.printStackTrace(); 
+			Assert.fail();
+		}
+		
+
+		try {
+			RunCreateNetwork.main(null);
+		} catch ( Exception eee ) {
+			eee.printStackTrace(); 
+			Assert.fail();
+		}
+
+		try {
+			RunCreatePopulationAndDemand.main(null);
+		} catch ( Exception eee ) {
+			eee.printStackTrace(); 
+			Assert.fail();
+		}
+
+		// The above test only tests if it runs, not if the output is reasonable.  Please go ahead and improve this. kai, jul'15
+		
+	}
+
+}
