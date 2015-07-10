810238525d24bd194aaf9bb87d236682fefe63b8
mrieser <rieser@gmail.com>
2015-07-10 09:47:54 +0000

    fix compilation error for nightly-builds. Nightly builds enforce that org.matsim.* does not use any class from tutorial.*, thus remove the import statement but use the fully qualified class name in Javadoc.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@33846 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/core/router/util/TravelTime.java
+++ matsim/src/main/java/org/matsim/core/router/util/TravelTime.java
@@ -24,15 +24,10 @@ import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.vehicles.Vehicle;
 
-import tutorial.programming.example20customTravelTime.RunCustomTravelTimeExample;
-
-
-
-
 /**
  * A simple interface to retrieve the travel time on links.
  * <br/>
- * For an example of how to replace this, see {@link RunCustomTravelTimeExample }.
+ * For an example of how to replace this, see {@link tutorial.programming.example20customTravelTime.RunCustomTravelTimeExample }.
  *
  * @author mrieser
  */
