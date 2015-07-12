dfdc14f550d6fb117bd93278ace737e9c1d93b59
gunfloe <gunnar.floetteroed@abe.kth.se>
2015-07-12 08:52:27 +0000

    git-svn-id: file:///srv/local/data/cleanRepo/trunk@33856 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/gunnar/src/main/java/kai/expectedUtls/KNExpectedUtilities.java
+++ playgrounds/gunnar/src/main/java/kai/expectedUtls/KNExpectedUtilities.java
@@ -40,8 +40,8 @@ class KNExpectedUtilities {
 		GumbelDistribution gmb = new GumbelDistribution( EULER, beta ) ;
 
 		System.out.println( "mean: " + gmb.getNumericalMean() ) ;
-		System.out.println( " variance: " + gmb.getNumericalVariance() ) ;
-
+		System.out.println( "variance: " + gmb.getNumericalVariance() ) ;
+		
 		final int NNN = 100 ;
 
 		List<Double> vvv = new ArrayList<>() ;
@@ -53,47 +53,101 @@ class KNExpectedUtilities {
 		}
 
 		final double N_DRAWS = 10000. ;
-		double utlsSum = 0. ;
-		double diff = 0 ;
-		double sumEta = 0. ;
+		double expectedUtlSum = 0. ;
+		double receivedUtlSum = 0. ;
+		double gammaSum = 0.0;
+		double logsumSum = 0.0;
+		// double diff = 0 ;
+		// double sumEta = 0. ;
 
+		// initialize eta process
+		final double etaInertia = 0.5; // creates correlation
+		System.out.println("eta inertia = " + etaInertia);
+		List<Double> eta = new ArrayList<Double>();		
+		for (int ii = 0; ii < NNN; ii++) {
+			eta.add(fact * gmb.sample());
+		}
+		
 		for ( int jj=0 ; jj<N_DRAWS ; jj++ ) {
 			// average over N_DRAWS:
 			
-			List<Double> vvPlsEps = new ArrayList<>() ;
-			List<Double> vvPlsEpsPlsEta = new ArrayList<>() ;
+			List<Double> vvPlsEps = new ArrayList<>() ;			
 			for ( Double vv : vvv ) {
 				// these are the V_i + eps_i:
 				final double vvPlsEp = vv + gmb.sample();
-				vvPlsEps.add( vvPlsEp  ) ;
-				
-				// these are the V_i + eps_i + eta_i:
-				vvPlsEpsPlsEta.add( vvPlsEp + fact * gmb.sample() );
+				vvPlsEps.add( vvPlsEp  ) ;				
 			}
-			
+
+			// these are the V_i + eps_i + eta_i:				
+			List<Double> vvPlsEpsPlsEta = new ArrayList<>() ;
+			for (int ii = 0; ii < NNN; ii++) {
+				vvPlsEpsPlsEta.add( vvPlsEps.get(ii) + eta.get(ii) );				
+			}			
+
+			// compute this iteration's logsum given the current eta,
+			// for "wartezimmer" averaging
+			double sum = 0. ;
+			for (int ii = 0; ii < NNN; ii++) {
+				sum += FastMath.exp( beta * vvv.get(ii) + eta.get(ii) ) ;
+			}
+			logsumSum += FastMath.log( sum )/beta;			
+
 			// this computes the index where V_i + eps_i + eta_i is max:
 			final Double scoreThatAgentHopesFor = Collections.max( vvPlsEpsPlsEta );
 			int index = vvPlsEpsPlsEta.indexOf( scoreThatAgentHopesFor ) ;
+			expectedUtlSum += scoreThatAgentHopesFor;
+						
+			// create new etas (to be experienced; also the basis for the
+			// next iterations decision)
+			List<Double> newEta = new ArrayList<Double>();		
+			for (int ii = 0; ii < NNN; ii++) {
+				newEta.add(etaInertia * eta.get(ii) + (1.0 - etaInertia) * fact * gmb.sample());
+			}
 			
-			// use V_i + eps_i from this and add another eta_i (this is the utl the agent will receive) and average over that:
-			final double actualScore = vvPlsEps.get(index)  + fact * gmb.sample();
-			utlsSum += actualScore ;
+			// use V_i + eps_i from this and add the new eta_i (this is the utl the agent will receive) and average over that:
+			final double actualScore = vvPlsEps.get(index) + newEta.get(index);			
+			receivedUtlSum += actualScore ;
+			
+			// This is the correction. It does not become zero for reasons
+			// explained via Email. It is straightforward to compute from
+			// "MATSim-variables" as (the received score of the selected plan)
+			// minus (the score of the selected plan on which the choice was 
+			// based).
+			final double gamma = actualScore - scoreThatAgentHopesFor;
+			gammaSum += gamma;
+			
+			// take over etas
+			eta = newEta;
 			
 			// collect some other quantities as candidates for correction:
-			sumEta += scoreThatAgentHopesFor - vvPlsEps.get(index) ;
-			diff += scoreThatAgentHopesFor - actualScore ;
+			// sumEta += scoreThatAgentHopesFor - vvPlsEps.get(index) ;
+			// diff += scoreThatAgentHopesFor - actualScore ;
 		}
 
-		System.out.println( "numerical max is: " + utlsSum/N_DRAWS ) ;
-		System.out.println( "diff is: " + diff/N_DRAWS ) ;
-		System.out.println( "av eta (before) is: " + sumEta/N_DRAWS ) ;
+		System.out.println();
+		System.out.println("received: " + receivedUtlSum / N_DRAWS);
+		System.out.println("correction: " + gammaSum / N_DRAWS);
+
+		System.out.println("\navailable if one knew the epsilons:");
+		System.out.println("expected (numerical): " + expectedUtlSum / N_DRAWS);
+		System.out.println("expected (numerical, corrected): "
+				+ (expectedUtlSum / N_DRAWS + gammaSum / N_DRAWS));
 		
-		double sum = 0. ;
-		for ( Double vv : vvv ) {
-			sum += FastMath.exp( beta * vv ) ;
-		}
+		System.out.println("\navailable even if one does not know the epsilons:");
+		System.out.println("expected (analytical): " + logsumSum / N_DRAWS);
+		System.out.println("expected (analytical, corrected): "
+				+ (logsumSum / N_DRAWS + gammaSum / N_DRAWS));
+		
+		// System.out.println( "numerical max is: " + receivedUtlSum/N_DRAWS ) ;
+		// System.out.println( "diff is: " + diff/N_DRAWS ) ;
+		// System.out.println( "av eta (before) is: " + sumEta/N_DRAWS ) ;
 
-		System.out.println( "logsum is: " + FastMath.log( sum )/beta ) ;
+		// double sum = 0. ;
+		// for ( Double vv : vvv ) {
+		// sum += FastMath.exp( beta * vv ) ;
+		// }
+		//
+		//		System.out.println( "logsum is: " + FastMath.log( sum )/beta ) ;
 
 
 	}
