3904e31df3223ca451816932e63345bde623664b
vsp_nagel <2008@kainagel.org>
2015-07-15 13:26:05 +0000

    expected utilities example
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@33905 a0517364-e873-4167-b1c8-416daa5a6b91

--- playgrounds/gunnar/src/main/java/kai/expectedUtls/KNExpectedUtilities.java
+++ playgrounds/gunnar/src/main/java/kai/expectedUtls/KNExpectedUtilities.java
@@ -32,39 +32,41 @@ import org.apache.commons.math3.util.FastMath;
 class KNExpectedUtilities {
 
 	public static void main(String[] args) {
-		final double fact = 1. ; // weight of eta
-
 		final double EULER = - FastMath.PI / (2 * FastMath.E);
 
+		final double fact = 1. ; // weight of eta
+		final double etaInertia = 0.0; // creates correlation
 		final double beta = 1. ;
+
+		final int N_ALTERNATIVES = 100 ;
+
+		final double N_DRAWS = 10000. ;
+
 		GumbelDistribution gmb = new GumbelDistribution( EULER, beta ) ;
 
 		System.out.println( "mean: " + gmb.getNumericalMean() ) ;
 		System.out.println( "variance: " + gmb.getNumericalVariance() ) ;
 		
-		final int NNN = 100 ;
-
 		List<Double> vvv = new ArrayList<>() ;
-		for ( int ii=0 ; ii<NNN ; ii++ ) {
+		for ( int ii=0 ; ii<N_ALTERNATIVES ; ii++ ) {
 
 			// generate the V_i (they are all zero):
 			vvv.add( 0. ) ;
 
 		}
 
-		final double N_DRAWS = 10000. ;
 		double expectedUtlSum = 0. ;
 		double receivedUtlSum = 0. ;
+		double receivedUtlSum2 = 0. ;
 		double gammaSum = 0.0;
 		double logsumSum = 0.0;
 		// double diff = 0 ;
 		// double sumEta = 0. ;
 
 		// initialize eta process
-		final double etaInertia = 0.5; // creates correlation
 		System.out.println("eta inertia = " + etaInertia);
 		List<Double> eta = new ArrayList<Double>();		
-		for (int ii = 0; ii < NNN; ii++) {
+		for (int ii = 0; ii < N_ALTERNATIVES; ii++) {
 			eta.add(fact * gmb.sample());
 		}
 		
@@ -80,14 +82,14 @@ class KNExpectedUtilities {
 
 			// these are the V_i + eps_i + eta_i:				
 			List<Double> vvPlsEpsPlsEta = new ArrayList<>() ;
-			for (int ii = 0; ii < NNN; ii++) {
+			for (int ii = 0; ii < N_ALTERNATIVES; ii++) {
 				vvPlsEpsPlsEta.add( vvPlsEps.get(ii) + eta.get(ii) );				
 			}			
 
 			// compute this iteration's logsum given the current eta,
 			// for "wartezimmer" averaging
 			double sum = 0. ;
-			for (int ii = 0; ii < NNN; ii++) {
+			for (int ii = 0; ii < N_ALTERNATIVES; ii++) {
 				sum += FastMath.exp( beta * vvv.get(ii) + eta.get(ii) ) ;
 			}
 			logsumSum += FastMath.log( sum )/beta;			
@@ -96,17 +98,24 @@ class KNExpectedUtilities {
 			final Double scoreThatAgentHopesFor = Collections.max( vvPlsEpsPlsEta );
 			int index = vvPlsEpsPlsEta.indexOf( scoreThatAgentHopesFor ) ;
 			expectedUtlSum += scoreThatAgentHopesFor;
+			
+			// decision based on average score:
+			final Double averageScore = Collections.max( vvPlsEps ) ;
+			int index2 = vvPlsEps.indexOf( averageScore ) ;
 						
 			// create new etas (to be experienced in the mobsim; these will then
 			// also be the basis for the next iteration's decision)
 			List<Double> newEta = new ArrayList<Double>();		
-			for (int ii = 0; ii < NNN; ii++) {
+			for (int ii = 0; ii < N_ALTERNATIVES; ii++) {
 				newEta.add(etaInertia * eta.get(ii) + (1.0 - etaInertia) * fact * gmb.sample());
 			}
 			
 			// use V_i + eps_i from this and add the new eta_i (this is the utl the agent will receive) and average over that:
 			final double actualScore = vvPlsEps.get(index) + newEta.get(index);			
 			receivedUtlSum += actualScore ;
+			
+			final double actualScore2 = vvPlsEps.get(index2) + newEta.get(index2) ;
+			receivedUtlSum2 += actualScore2 ;
 			
 			// This is the correction. It does not become zero for reasons
 			// explained via Email. Using only directly available "MATSim-variables".
@@ -128,14 +137,17 @@ class KNExpectedUtilities {
 		System.out.println("correction: " + gammaSum / N_DRAWS);
 
 		System.out.println("\navailable if one knew the epsilons:");
-		System.out.println("expected (numerical): " + expectedUtlSum / N_DRAWS);
-		System.out.println("expected (numerical, corrected): "
-				+ (expectedUtlSum / N_DRAWS + gammaSum / N_DRAWS));
+		System.out.println("expected (from frozen epsilons): " + expectedUtlSum / N_DRAWS);
+		System.out.println("expected (from frozen epsilons) - correction: "
+				+ (expectedUtlSum / N_DRAWS + gammaSum / N_DRAWS) + " (same as above under _received_)");
 		
 		System.out.println("\navailable even if one does not know the epsilons:");
-		System.out.println("expected (analytical): " + logsumSum / N_DRAWS);
-		System.out.println("expected (analytical, corrected): "
+		System.out.println("expected (from logsum): " + logsumSum / N_DRAWS);
+		System.out.println("expected (from logsum) - correction: "
 				+ (logsumSum / N_DRAWS + gammaSum / N_DRAWS));
+		
+		System.out.println("\ndecision based on average score:") ;
+		System.out.println("average score: " + receivedUtlSum2 / N_DRAWS ) ;
 		
 		// System.out.println( "numerical max is: " + receivedUtlSum/N_DRAWS ) ;
 		// System.out.println( "diff is: " + diff/N_DRAWS ) ;
