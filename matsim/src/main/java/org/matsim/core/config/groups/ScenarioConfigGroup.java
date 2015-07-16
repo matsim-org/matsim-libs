61557f50989a508167af2cc4ec3159c133fbec9e
vsp_nagel <2008@kainagel.org>
2015-07-16 09:55:15 +0000

    tried to be a bit more helpful with throwing exceptions at the right place, but that fails some tests.  Now hopefully a compromise ...
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@33920 a0517364-e873-4167-b1c8-416daa5a6b91

--- matsim/src/main/java/org/matsim/core/config/groups/ScenarioConfigGroup.java
+++ matsim/src/main/java/org/matsim/core/config/groups/ScenarioConfigGroup.java
@@ -21,6 +21,7 @@ package org.matsim.core.config.groups;
 
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.matsim.core.config.ReflectiveConfigGroup;
 
 /**
@@ -28,7 +29,6 @@ import org.matsim.core.config.ReflectiveConfigGroup;
  *
  */
 public final class ScenarioConfigGroup extends ReflectiveConfigGroup {
-
 	public static final String GROUP_NAME = "scenario";
 
 	private static final String USE_LANES = "useLanes";
@@ -36,15 +36,10 @@ public final class ScenarioConfigGroup extends ReflectiveConfigGroup {
 	private static final String USE_TRANSIT = "useTransit";
 	private static final String USE_VEHICLES = "useVehicles";
 	
-	private static final String message = "This switch is no longer operational.  The  file is loaded if the file name"
-			+ " is different from null.  If you needed this for the creation of the container, use the ScenarioBuilder in "
-			+ "ScenarioUtils.  Note that loading the file does not mean that it is used anywhere; such functionality needs to be "
-			+ "switched on elsewhere (e.g. in qsim, in transit, ...).  If this does not work for you, please let us know. kai, jun'15";
-
-
+	private static final Logger log = Logger.getLogger( ScenarioConfigGroup.class ) ;
+	
 	private boolean useLanes = false;
 	private boolean useHouseholds = false;
-
 
 	public ScenarioConfigGroup() {
 		super(GROUP_NAME);
@@ -55,7 +50,7 @@ public final class ScenarioConfigGroup extends ReflectiveConfigGroup {
 		Map<String,String> map = super.getComments();
 		map.put(USE_LANES, "Set this parameter to true if lanes should be used, false if not.");
 		map.put(USE_HOUSEHOLDS, "Set this parameter to true if households should be used, false if not.");
-//		map.put(USE_TRANSIT, "Set this parameter to true if transit should be simulated, false if not.");
+		map.put(USE_TRANSIT, "Deprecated, to not use.  See transit section of config file.") ; // since jul'15
 		return map;
 	}
 
@@ -79,30 +74,54 @@ public final class ScenarioConfigGroup extends ReflectiveConfigGroup {
 		this.useHouseholds = b;
 	}
 
-	@SuppressWarnings("static-method")
+	private boolean failConsistencyCheck = false ;
+
 	@Deprecated // since jul'15
 	@StringSetter( USE_VEHICLES )
-	public void setUseVehicles(@SuppressWarnings("unused") final boolean b) {
-		throw new RuntimeException( message ) ;
+	public void setUseVehicles(@SuppressWarnings("unused") final Boolean b) {
+		log.fatal( getMessage( USE_VEHICLES ) ) ;
+		this.failConsistencyCheck = true ;
+//		throw new RuntimeException( getMessage( USE_VEHICLES ) ) ;
 	}
 	@Deprecated // since jul'15
 	@StringGetter( USE_VEHICLES )
-	public static boolean getUseVehicles() {
-		throw new RuntimeException( message ) ;
+	public Boolean getUseVehicles() {
+//		log.fatal( getMessage( USE_VEHICLES ) ) ;
+//		this.failConsistencyCheck = true ;
+//		throw new RuntimeException( getMessage( USE_VEHICLES ) ) ;
+		return null ;
 	}
 	
-	@SuppressWarnings("static-method")
 	@StringSetter( USE_TRANSIT )
 	@Deprecated // since jul'15
-	public void setUseTransit(final boolean b) {
-		throw new RuntimeException("The " + USE_TRANSIT + " switch has moved to the transit section of the config file." ) ;
+	public void setUseTransit(@SuppressWarnings("unused") final Boolean b) {
+		log.fatal("The " + USE_TRANSIT + " switch has moved to the transit section of the config file." ) ;
+		this.failConsistencyCheck = true ;
+//		throw new RuntimeException("The " + USE_TRANSIT + " switch has moved to the transit section of the config file." ) ;
 	}
-	@SuppressWarnings("static-method")
 	@StringGetter( USE_TRANSIT )
 	@Deprecated // since jul'15
-	public boolean getUseTransit(final boolean b) {
-		throw new RuntimeException("The " + USE_TRANSIT + " switch has moved to the transit section of the config file." ) ;
+	public Boolean getUseTransit() {
+//		log.fatal("The " + USE_TRANSIT + " switch has moved to the transit section of the config file." ) ;
+//		this.failConsistencyCheck = true ;
+//		throw new RuntimeException("The " + USE_TRANSIT + " switch has moved to the transit section of the config file." ) ;
+		return null ;
 	}
 	
+	private static String getMessage( String module ) {
+		return "The " + module + " switch is no longer operational.  The file is loaded if the file name"
+				+ " is different from null.  If you needed this for the creation of the container, use the ScenarioBuilder in "
+				+ "ScenarioUtils.  Note that loading the file does not mean that it is used anywhere; such functionality needs to be "
+				+ "switched on elsewhere (e.g. in qsim, in transit, ...).  If this does not work for you, please let us know. kai, jun'15";
+	}
+	
+	@Override
+	protected void checkConsistency() {
+		if ( this.failConsistencyCheck ) {
+			throw new RuntimeException( "trying to use a switch that is no longer operational.  cannot fail this right away because some "
+					+ "tests will fail.  Check error messages to find cause of problem") ;
+		}
+
+}
 
 }
