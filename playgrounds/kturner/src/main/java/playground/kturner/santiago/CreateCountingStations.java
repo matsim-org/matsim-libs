5f2840c9acd2aafa56d1dfa7cf81381fcedddac2
Kai Turner <kturner@mailbox.tu-berlin.de>
2015-08-19 11:06:04 +0200

    corrected in-output paths, now both types of counts (veh, pers) were created in one run

--- playgrounds/kturner/src/main/java/playground/kturner/santiago/CreateCountingStations.java
+++ playgrounds/kturner/src/main/java/playground/kturner/santiago/CreateCountingStations.java
@@ -3,49 +3,55 @@ package playground.kturner.santiago;
 import java.io.File;
 import java.text.DecimalFormat;
 
-public class RunCL_KT {
-	
-	//Input Dir KT: 
-			final static String INPUT_DIR = "F:/Arbeit-VSP/Santiago/Kai_und_Daniel/Counts/" ;
-			final static String OUTPUT_DIR = "F:/Arbeit-VSP/Santiago/Kai_und_Daniel/Counts/" ;
-			
-//			//Input Dir DH: 
-//			private static final String INPUT_DIR = "" ;
-//			private static final String OUTPUT_DIR = "" ;
+public class CreateCountingStations {
 
-			//Filename without extention
-			
-			final static String CSIdFILE_NAME = "CSId-LinkId_merged";
-//			final static String CSDATAFILE_NAME = "T_FLUJO_TASA";//Count of Vehicle
-//			final static String COUNTFILE_NAME = "counts_merged_VEH" ;	//Output-Countfile_name
-			final static String CSDATAFILE_NAME = "T_VIAJESTEMP"; //Number of Persons = counts per Vehicle * Factor (#Persons / vehicle)
-			final static String COUNTFILE_NAME = "counts_merged_PERS" ;	//Output-Countfile_name
-//			private static final String CONFIGFILE_NAME = "config";
-			
-			//File
-			final static String CSIdFILE = INPUT_DIR + CSIdFILE_NAME + ".csv" ;
-			final static String CSDATAFILE = INPUT_DIR + CSDATAFILE_NAME + ".csv" ;
-			
-	
+	private static final String svnWorkingDir = "../../shared-svn/"; 	//Path: KT (SVN-checkout)
+	private static final String workingDirInputFiles = svnWorkingDir + "Kai_und_Daniel/inputFromElsewhere/counts/" ;
+	private static final String outputDir = svnWorkingDir + "Kai_und_Daniel/inputForMATSim/creationResults/counts/" ; //outputDir of this class -> input for Matsim (KT)
+
+	//Position (linkId) of Counting Stations
+	private static final String CSIdFILE_NAME = "CSId-LinkId_merged";		
+	private static final String CSIdFILE = workingDirInputFiles + CSIdFILE_NAME + ".csv" ;
+
+
 	public static void main(String args[]){
-		
-		//Erstellt das angegebene Verzeichnis. Falls es bereits exisitert, geschieht nichts
-		File file = new File(OUTPUT_DIR);
-		System.out.println("Verzeichnis " + file + " erstellt: "+ file.mkdirs());	
-	
 
+		File file = new File(outputDir);
+		System.out.println("Verzeichnis " + file + " erstellt: "+ file.mkdirs());	
+
+		String csDataFile_Name;
+		String countfile_Name;
+		String csDataFile;
+
+		//#Counts in persons
+		csDataFile_Name = "T_VIAJESTEMP"; //Number of Persons = counts per Vehicle * Factor (#Persons / vehicle)
+		csDataFile = workingDirInputFiles + csDataFile_Name + ".csv" ;
+		countfile_Name = "counts_merged_PERS" ;	//Output-Countfile_name
+
+		createCsFiles(countfile_Name, csDataFile);
+
+		//#Counts in vehicles
+		csDataFile_Name = "T_FLUJO_TASA"; //Number of Persons = counts per Vehicle * Factor (#Persons / vehicle)
+		csDataFile = workingDirInputFiles + csDataFile_Name + ".csv" ;
+		countfile_Name = "counts_merged_VEH" ;	//Output-Countfile_name
+
+		createCsFiles(countfile_Name, csDataFile);
+
+		System.out.println("#### Done.");
+	}
+
+	private static void createCsFiles(String countfile_Name, String csDataFile) {
 		for (int i=1; i<=13; i++){
 			String vehCat = "C"+ String.format("%2s", new DecimalFormat("00").format(i));
 			System.out.println(vehCat);
-			final String COUNTFILE = OUTPUT_DIR + COUNTFILE_NAME + "_" + vehCat + ".xml" ;	//Output-Countfile
-			CSVToCountingStations converter = new CSVToCountingStations(COUNTFILE, CSIdFILE, CSDATAFILE, vehCat);
+			String countfile = outputDir + countfile_Name + "_" + vehCat + ".xml" ;	//Output-Countfile
+			CSVToCountingStations converter = new CSVToCountingStations(countfile, CSIdFILE, csDataFile, vehCat);
 			converter.createIds();
 			converter.readCountsFromCSV();
 			converter.writeCountsToFile();
 		}
-		System.out.println("#### Done.");
 	}
-	
+
 
 
 }
