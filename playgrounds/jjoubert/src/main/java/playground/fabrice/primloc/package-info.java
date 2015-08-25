1e8702c932dc003e3a091f5b971cc4f7e6e67934
Michael Zilske <michael.zilske@tu-berlin.de>
2015-08-25 09:56:53 +0200

    resurrect classes from fabrice playground to jjoubert playground

---
+++ playgrounds/jjoubert/src/main/java/playground/fabrice/primloc/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package to run an Iterative Proportional Fitting that in addition fits against a travel cost distribution.
+ * 
+ * Some information is at the beginning of some classes, and in some of the test cases.
+ * 
+ * 
+ */
+package playground.fabrice.primloc;
