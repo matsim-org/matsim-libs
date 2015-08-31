3962c98b2eec4a858e1b66716929a5f460af1f5f
Amit Agarwal <amit.agarwal@campus.tu-berlin.de>
2015-08-31 14:58:03 +0200

    laemmel2011Diss approach

---
+++ playgrounds/agarwalamit/src/main/java/playground/agarwalamit/congestionPricing/GLKN/CongestionHandlerImplV7.java
@@ -0,0 +1,181 @@
+package playground.agarwalamit.congestionPricing.GLKN;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.events.LinkEnterEvent;
+import org.matsim.api.core.v01.events.LinkLeaveEvent;
+import org.matsim.api.core.v01.events.PersonArrivalEvent;
+import org.matsim.api.core.v01.events.PersonDepartureEvent;
+import org.matsim.api.core.v01.events.PersonStuckEvent;
+import org.matsim.api.core.v01.events.handler.LinkEnterEventHandler;
+import org.matsim.api.core.v01.events.handler.LinkLeaveEventHandler;
+import org.matsim.api.core.v01.events.handler.PersonArrivalEventHandler;
+import org.matsim.api.core.v01.events.handler.PersonDepartureEventHandler;
+import org.matsim.api.core.v01.events.handler.PersonStuckEventHandler;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.core.api.experimental.events.EventsManager;
+
+import playground.vsp.congestion.LinkCongestionInfo;
+import playground.vsp.congestion.events.CongestionEvent;
+
+public class CongestionHandlerImplV7 implements PersonDepartureEventHandler,
+LinkEnterEventHandler, LinkLeaveEventHandler, PersonStuckEventHandler, PersonArrivalEventHandler {
+
+	private final Logger log = Logger.getLogger(CongestionHandlerImplV7.class);
+
+	public CongestionHandlerImplV7(EventsManager events, Scenario scenario){
+		this.events = events;
+		this.scenario = scenario;
+
+		this.congestedModes.addAll(this.scenario.getConfig().qsim().getMainModes());
+		if (congestedModes.size()>1) throw new RuntimeException("Mixed traffic is not tested yet.");
+
+		storeLinkInfo();
+	}
+
+	private void storeLinkInfo(){
+		for(Link l : this.scenario.getNetwork().getLinks().values()){
+			LinkCongestionInfo lci = new LinkCongestionInfo();
+			lci.setLinkId(l.getId());
+			double flowCapacity_CapPeriod = l.getCapacity() * this.scenario.getConfig().qsim().getFlowCapFactor();
+			double marginalDelay_sec = ((1 / (flowCapacity_CapPeriod / this.scenario.getNetwork().getCapacityPeriod()) ) );
+			lci.setMarginalDelayPerLeavingVehicle(marginalDelay_sec);
+			link2LinkCongestionInfo.put(l.getId(), lci);
+		}
+	}
+
+	private EventsManager events ;
+	private Scenario scenario;
+	private final List<String> congestedModes = new ArrayList<String>();
+	private Map<Id<Link>,LinkCongestionInfo> link2LinkCongestionInfo = new HashMap<Id<Link>, LinkCongestionInfo>();
+	private double totalDelay = 0;
+	private double totalInternalizedDelay = 0;
+
+	@Override
+	public void reset(int iteration) {
+		link2LinkCongestionInfo.clear();
+		storeLinkInfo();
+	}
+
+	@Override
+	public void handleEvent(PersonStuckEvent event) {
+		log.warn("An agent is stucking. No garantee for right calculation of external congestion effects "
+				+ "because there are no linkLeaveEvents for stucked agents.: \n" + event.toString());
+	}
+
+	@Override
+	public void handleEvent(LinkLeaveEvent event) {
+
+		Id<Person> pId = Id.createPersonId(event.getVehicleId());
+		Id<Link> linkId = event.getLinkId();
+		double linkLeaveTime = event.getTime();
+
+		LinkCongestionInfo lci = link2LinkCongestionInfo.get(linkId);
+		double freeSpeedLeaveTime = lci.getPersonId2freeSpeedLeaveTime().get(pId);
+
+		double delay = linkLeaveTime - freeSpeedLeaveTime;
+
+		if(delay > 0.){
+			totalDelay += delay;
+			lci.getLeavingAgents().add(pId);
+
+			boolean isThisLastAgentOnLink = lci.getEnteringAgents().size() - 1 == 0 ? true :false;
+
+			if( isThisLastAgentOnLink ) { // last agent on the link is delayed, thus, queue will dissolve immediately.
+
+				throwCongestionEvents(event);
+
+			} else { // check for the headway i.e. if more agents will be queued, continue
+				// nextFreeSpeedLinkLeaveTime - current leave time < timeHeadway ==> charge later.
+				Id<Person> nextAgent = lci.getEnteringAgents().get(1);
+				double nextFreeSpeedLinkLeaveTime = lci.getPersonId2freeSpeedLeaveTime().get(nextAgent); 
+
+				if( nextFreeSpeedLinkLeaveTime - linkLeaveTime >= lci.getMarginalDelayPerLeavingVehicle_sec() ){
+
+					throwCongestionEvents(event);
+
+				} else { //charge later
+					
+				}
+			}
+		} else {
+			lci.getPersonId2freeSpeedLeaveTime().remove(pId);
+		}
+
+		lci.getEnteringAgents().remove(pId);
+		
+	}
+
+	@Override
+	public void handleEvent(LinkEnterEvent event) {
+		Id<Person> personId = Id.createPersonId(event.getVehicleId());
+		LinkCongestionInfo lci = this.link2LinkCongestionInfo.get(event.getLinkId());
+
+		Link link = scenario.getNetwork().getLinks().get(event.getLinkId());
+		double minLinkTravelTime = Math.floor(link.getLength()/link.getFreespeed());
+
+		lci.getPersonId2freeSpeedLeaveTime().put(personId, event.getTime()+ minLinkTravelTime + 1.0);
+		lci.getEnteringAgents().add(personId);
+		lci.getPersonId2linkEnterTime().put(personId, event.getTime());
+	}
+
+	@Override
+	public void handleEvent(PersonDepartureEvent event) {
+
+		String travelMode = event.getLegMode();
+		if(congestedModes.contains(travelMode)){
+			LinkCongestionInfo lci = this.link2LinkCongestionInfo.get(event.getLinkId());
+			lci.getPersonId2freeSpeedLeaveTime().put(event.getPersonId(), event.getTime() + 1);
+			lci.getEnteringAgents().add(event.getPersonId());
+			lci.getPersonId2linkEnterTime().put(event.getPersonId(), event.getTime());
+		}
+
+	}
+
+	@Override
+	public void handleEvent(PersonArrivalEvent event) {
+		String travelMode = event.getLegMode();
+
+		if(congestedModes.contains(travelMode)){
+			LinkCongestionInfo lci = this.link2LinkCongestionInfo.get(event.getLinkId());
+			lci.getEnteringAgents().remove(event.getPersonId());
+			lci.getPersonId2freeSpeedLeaveTime().remove(event.getPersonId());
+		}
+
+	}
+
+	private void throwCongestionEvents(LinkLeaveEvent event) {
+		//charge here
+		LinkCongestionInfo lci = this.link2LinkCongestionInfo.get(event.getLinkId());
+		List<Id<Person>> leavingAgents = new ArrayList<Id<Person>>(lci.getLeavingAgents());
+		Id<Person> nullAffectedAgent = Id.createPersonId("NullAgent");
+
+		double queueDissolveTime = event.getTime() /*+ lci.getMarginalDelayPerLeavingVehicle_sec()*/; // not yet sure, if this is right.
+		
+		for(Id<Person> person : leavingAgents){
+			double delayToPayFor = queueDissolveTime - lci.getPersonId2freeSpeedLeaveTime().get(person);
+			this.totalInternalizedDelay += delayToPayFor;
+			CongestionEvent congestionEvent = new CongestionEvent(event.getTime(), "GL_Approach", person, nullAffectedAgent, delayToPayFor, event.getLinkId(), lci.getPersonId2linkEnterTime().get(person));
+			this.events.processEvent(congestionEvent);
+			lci.getPersonId2freeSpeedLeaveTime().remove(person);
+		}
+
+		lci.getLeavingAgents().clear();
+	}
+
+	public double getTotalDelay() {
+		return totalDelay;
+	}
+
+	public double getTotalInternalizedDelay() {
+		return totalInternalizedDelay;
+	}
+
+}
