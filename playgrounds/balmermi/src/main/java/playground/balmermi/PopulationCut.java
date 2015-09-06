4d9c229c6d2d91a9fce3e754a63a9bedac89e75e
Michael Zilske <michael.zilske@tu-berlin.de>
2015-09-06 09:00:49 +0200

    Coord

--- playgrounds/balmermi/src/main/java/playground/balmermi/PopulationCut.java
+++ playgrounds/balmermi/src/main/java/playground/balmermi/PopulationCut.java
@@ -20,21 +20,21 @@
 
 package playground.balmermi;
 
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
-
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.Plan;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.gbl.MatsimRandom;
 import org.matsim.core.population.PlanImpl;
-import org.matsim.core.population.PopulationWriter;
-import org.matsim.core.scenario.ScenarioImpl;
-import org.matsim.core.scenario.ScenarioLoaderImpl;
-import org.matsim.core.utils.geometry.CoordImpl;
+import org.matsim.core.scenario.ScenarioUtils;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 public class PopulationCut {
 
@@ -46,8 +46,8 @@ public class PopulationCut {
 		Scenario scenario = ScenarioUtils.createScenario(config);
 		ScenarioUtils.loadScenario(scenario);
 		
-		Coord min = new CoordImpl(args[1],args[2]);
-		Coord max = new CoordImpl(args[3],args[4]);
+		Coord min = new Coord(Double.parseDouble(args[1]),Double.parseDouble(args[2]));
+		Coord max = new Coord(Double.parseDouble(args[3]),Double.parseDouble(args[4]));
 
 		System.out.println("Removing persons... " + (new Date()));
 		Set<Id> toRemove = new HashSet<Id>();
