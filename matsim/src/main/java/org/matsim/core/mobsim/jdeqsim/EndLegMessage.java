9658c403d967b22b19f0246353e441a321179657
Kai Nagel <2008@kainagel.org>
2015-09-07 20:55:24 +0200

    Link2WaitEvent --> VehicleLeavesTrafficEvent

--- matsim/src/main/java/org/matsim/core/mobsim/jdeqsim/EndLegMessage.java
+++ matsim/src/main/java/org/matsim/core/mobsim/jdeqsim/EndLegMessage.java
@@ -25,17 +25,14 @@ import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.events.ActivityStartEvent;
 import org.matsim.api.core.v01.events.Event;
-import org.matsim.api.core.v01.events.Link2WaitEvent;
 import org.matsim.api.core.v01.events.LinkEnterEvent;
 import org.matsim.api.core.v01.events.PersonArrivalEvent;
-import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.events.VehicleLeavesTrafficEvent;
 import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Leg;
-import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.Plan;
 import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.core.config.groups.PlansConfigGroup;
-import org.matsim.core.config.groups.PlansConfigGroup.ActivityDurationInterpretation;
 import org.matsim.core.mobsim.qsim.agents.ActivityDurationUtils;
 import org.matsim.core.population.ActivityImpl;
 
@@ -115,7 +112,7 @@ public class EndLegMessage extends EventMessage {
 
 		// schedule VehicleLeavesTrafficEvent
 		Id<org.matsim.vehicles.Vehicle> vehicleId = Id.create( this.vehicle.getOwnerPerson().getId() , org.matsim.vehicles.Vehicle.class ) ;
-		event = new Link2WaitEvent(this.getMessageArrivalTime(), this.vehicle.getOwnerPerson().getId(), this.vehicle.getCurrentLinkId(), 
+		event = new VehicleLeavesTrafficEvent(this.getMessageArrivalTime(), this.vehicle.getOwnerPerson().getId(), this.vehicle.getCurrentLinkId(), 
 				vehicleId, this.vehicle.getCurrentLeg().getMode() );
 		SimulationParameters.getProcessEventThread().processEvent(event);
 
