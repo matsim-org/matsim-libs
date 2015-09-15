856e88ca3aca78660802b318074955bdf073feb1
Amit Agarwal <amit.agarwal@campus.tu-berlin.de>
2015-09-15 11:49:41 +0200

    just some comments

--- playgrounds/vsp/src/main/java/playground/vsp/congestion/handlers/CongestionInternalization.java
+++ playgrounds/vsp/src/main/java/playground/vsp/congestion/handlers/CongestionInternalization.java
@@ -1,20 +1,52 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2013 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
 package playground.vsp.congestion.handlers;
 
 import org.matsim.api.core.v01.events.LinkLeaveEvent;
 
+/**
+ * @author ikaddoura
+ *
+ */
 public interface CongestionInternalization {
 	
-	// This is the core method which can be implemented in different ways
-	// in order to change the logic how to internalize delays.
+	/**
+	 * <p> This is the core method which can be implemented in different ways
+	 * in order to change the logic how to internalize delays.
+	 */
 	public void calculateCongestion(LinkLeaveEvent event);
 
-	// the total delay calculated as 'link leave time minus freespeed leave time'
+	/**
+	 * <p> The total delay calculated as 'link leave time minus freespeed leave time'
+	 */
 	public double getTotalDelay();
 	
-	// the total delay which is internalized, i.e. allocated to causing agents
+	
+	/**
+	 * The total delay which is internalized, i.e. allocated to causing agents
+	 */
 	public double getTotalInternalizedDelay();
 	
-	// writes the basic information to a file
+	/**
+	 * Writes the basic information to a file
+	 */
 	public void writeCongestionStats(String fileName);
 	
 }
