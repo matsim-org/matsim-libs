36e96f36d4ad3cd9b9344123114318e5c8dd1aad
ikaddoura <kaddoura@vsp.tu-berlin.de>
2015-09-21 16:04:56 +0200

    adding a test method to compare the outcome with the default router

--- playgrounds/ikaddoura/src/test/java/playground/ikaddoura/router/VTTSspecificRouterTest.java
+++ playgrounds/ikaddoura/src/test/java/playground/ikaddoura/router/VTTSspecificRouterTest.java
@@ -63,7 +63,8 @@ public class VTTSspecificRouterTest {
 	/**
 	 * There are two agents. One agent is more pressed for time than the other one which results in different VTTS.
 	 * There are two routes. One expensive but fast route. One cheap but slow route.
-	 * Now see which route is chosen by which agent.
+	 * 
+	 * Accounting for the different VTTS results in a different outcome than assuming the default VTTS (as it is the case in the default router).
 	 * 
 	 */
 	@Test
@@ -149,4 +150,78 @@ public class VTTSspecificRouterTest {
 			}
 		}		
 	}
+	
+	/**
+	 * Now do same as before but use the default router...
+	 * 
+	 */
+	@Test
+	public final void test2(){
+				
+		final String configFile1 = testUtils.getPackageInputDirectory() + "vttsSpecificRouter/configVTTS.xml";
+		final Controler controler = new Controler(configFile1);
+				
+		final Map<Id<Vehicle>, Set<Id<Link>>> vehicleId2linkIds = new HashMap<>();
+
+		controler.addControlerListener( new StartupListener() {
+
+			@Override
+			public void notifyStartup(StartupEvent event) {
+				event.getControler().getEvents().addHandler(new LinkLeaveEventHandler() {
+					
+					@Override
+					public void reset(int iteration) {
+						vehicleId2linkIds.clear();
+					}
+					
+					@Override
+					public void handleEvent(LinkLeaveEvent event) {
+						if (vehicleId2linkIds.containsKey(event.getVehicleId())) {
+							vehicleId2linkIds.get(event.getVehicleId()).add(event.getLinkId());
+						} else {
+							Set<Id<Link>> linkIds = new HashSet<Id<Link>>();
+							linkIds.add(event.getLinkId());
+							vehicleId2linkIds.put(event.getVehicleId(), linkIds);
+						}
+					}
+				});		
+			}		
+		});
+		
+		
+		controler.addOverridingModule(new OTFVisModule());
+		controler.getConfig().controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
+		controler.run();
+				
+		// high speed and costly route: 15 km; 50 km/h (travel time: 0.3h)
+		// for both persons: 12 EUR/km * 0.3 km + 15 km * 0.002 EUR/m --> 33.6 EUR
+		
+		// low speed and cheap route: 10 km; 10 km/h (travel time: 1h)
+		// for both person: 12 * 1 + 10 * 2 --> 32 EUR
+		
+		// ==> both persons will choose the low speed but cheap route (32 < 33.6)
+		
+		Id<Link> shortDistanceLongTimeLinkId = Id.createLinkId("link_3_6");
+		
+		for (Id<Vehicle> id : vehicleId2linkIds.keySet()) {
+
+			if (id.toString().contains("highVTTS")) {
+				
+				// both persons use the slow but cheap route
+				Assert.equals(true, vehicleId2linkIds.get(id).contains(shortDistanceLongTimeLinkId));
+				
+//				System.out.println(vehicleId2linkIds.get(id).toString());
+			}
+			
+			if (id.toString().contains("lowVTTS")) {
+
+				// both persons use the slow but cheap route
+				Assert.equals(true, vehicleId2linkIds.get(id).contains(shortDistanceLongTimeLinkId));
+				
+//				System.out.println(vehicleId2linkIds.get(id).toString());
+			}
+		}		
+	}
+	
+	
 }
