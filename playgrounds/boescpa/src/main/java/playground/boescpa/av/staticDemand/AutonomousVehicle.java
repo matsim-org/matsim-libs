4fd79948e4e8363f724f5eaa97b823f94bb4abd7
boescpa <patrick.boesch@ivt.baug.ethz.ch>
2015-09-24 15:06:42 +0200

    Updating playground boescpa to local version.

--- playgrounds/boescpa/src/main/java/playground/boescpa/av/staticDemand/AutonomousVehicle.java
+++ playgrounds/boescpa/src/main/java/playground/boescpa/av/staticDemand/AutonomousVehicle.java
@@ -32,44 +32,137 @@ import org.matsim.core.utils.geometry.CoordUtils;
  */
 public class AutonomousVehicle {
 
-	private Coord myPosition;
-	private int arrivalTime;
+    private Coord myPosition;
+    private int arrivalTime;
+    private int lastArrivalTime;
 
-	protected AutonomousVehicle(Coord initialCoords) {
-		myPosition = initialCoords;
-		arrivalTime = -1;
-	}
+    protected AutonomousVehicle(Coord initialCoords) {
+        myPosition = initialCoords;
+        arrivalTime = -1;
+        lastArrivalTime = 0;
+    }
 
-	public Coord getMyPosition() {
-		return new Coord(myPosition.getX(), myPosition.getY());
-	}
+    public Coord getMyPosition() {
+        return CoordUtils.createCoord(myPosition.getX(), myPosition.getY());
+    }
 
-	/**
-	 * Moves AV to the new position and returns the travel time.
-	 * @param newPosition
-	 * @return travelTime
-	 */
-	public double moveTo(Coord newPosition) {
-		double distance = CoordUtils.calcDistance(myPosition, newPosition) * Constants.getBeelineFactorStreet();
-		myPosition = newPosition;
-		return distance / Constants.getAvSpeed();
-	}
+    /**
+     * Moves AV to the new position and returns the travel time.
+     * @param newPosition
+     * @return travelTime
+     */
+    public double moveTo(Coord newPosition) {
+        if (Double.isNaN(newPosition.getX()) || Double.isNaN(newPosition.getY())) {
+            throw new IllegalArgumentException("NaN-Coordinates as new position!");
+        }
+        double distance = CoordUtils.calcDistance(myPosition, newPosition) * Constants.BEELINE_FACTOR_STREET;
+        myPosition = newPosition;
+        return distance / Constants.AV_SPEED;
+    }
 
-	/**
-	 * If the vehicle is in use, get time when it's free again.
-	 *
-	 * @return -1 if not in use, the arrival time else.
-	 */
-	public int getArrivalTime() {
-		return arrivalTime;
-	}
+    /**
+     * If the vehicle is in use, get time when it's free again.
+     *
+     * @return -1 if not in use, the arrival time else.
+     */
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
 
-	public void setArrivalTime(int arrivalTime) {
-		if (arrivalTime > 0 || arrivalTime == -1) {
-			this.arrivalTime = arrivalTime;
-		} else {
-			throw new IllegalArgumentException("illegal arrival time");
-		}
-	}
+    public void setArrivalTime(int arrivalTime) {
+        if (arrivalTime > 0) {
+            this.arrivalTime = arrivalTime;
+            this.lastArrivalTime = arrivalTime;
+        } else {
+            throw new IllegalArgumentException("illegal arrival time");
+        }
+    }
 
+    public void resetArrivalTime() {
+        this.arrivalTime = -1;
+    }
+
+    public int getLastArrivalTime() {
+        return lastArrivalTime;
+    }
+
+    // ------ Stats ------
+
+    private int numberOfServices = 0;
+    private double totalAccessTime = 0;
+    private double totalAccessDistance = 0;
+    private double totalServiceTime = 0;
+    private double totalServiceDistance = 0;
+    private double totalWaitingTimeForAgents = 0;
+    //private double maxWaitingTimeForAgent = 0;
+
+    public void incNumberOfServices() {
+        numberOfServices++;
+    }
+
+    public void incAccessTime(double accessTime) {
+        if (accessTime >= 0) {
+            totalAccessTime += accessTime;
+        } else {
+            throw new IllegalArgumentException("Negative access time!");
+        }
+    }
+
+    public void incAccessDistance(double accessDistance) {
+        if (accessDistance >= 0) {
+            totalAccessDistance += accessDistance;
+        } else {
+            throw new IllegalArgumentException("Negative access distance!");
+        }
+    }
+
+    public void incServiceTime(double serviceTime) {
+        if (serviceTime >= 0) {
+            totalServiceTime += serviceTime;
+        } else {
+            throw new IllegalArgumentException("Negative service time!");
+        }
+    }
+
+    public void incServiceDistance(long serviceDistance) {
+        if (serviceDistance >= 0) {
+            totalServiceDistance += serviceDistance;
+        } else {
+            throw new IllegalArgumentException("Negative service distance!");
+        }
+    }
+
+    public void incWaitingTime(double waitingTimeForAgent) {
+        if (waitingTimeForAgent >= 0) {
+            totalWaitingTimeForAgents += waitingTimeForAgent;
+			/*if (waitingTimeForAgent > maxWaitingTimeForAgent) {
+				maxWaitingTimeForAgent = waitingTimeForAgent;
+			}*/
+        } else {
+            throw new IllegalArgumentException("Negative waiting time for agent!");
+        }
+    }
+
+    public static String getStatsDescr() {
+        return "numberOfServices"
+                + Stats.delimiter + "totalAccessDistance"
+                + Stats.delimiter + "totalServiceDistance"
+                + Stats.delimiter + "totalAccessTime"
+                + Stats.delimiter + "totalWaitingTimeForAgents"
+                + Stats.delimiter + "totalServiceTime"
+                + Stats.delimiter + "idleTime";
+        //+ Stats.delimiter + "maxWaitingTimeForAgent";
+
+    }
+
+    public String getStats() {
+        return numberOfServices
+                + Stats.delimiter + totalAccessDistance
+                + Stats.delimiter + totalServiceDistance
+                + Stats.delimiter + totalAccessTime
+                + Stats.delimiter + totalWaitingTimeForAgents
+                + Stats.delimiter + totalServiceTime
+                + Stats.delimiter + (Constants.TOTAL_SIMULATION_TIME - totalAccessTime - totalServiceTime - totalWaitingTimeForAgents);
+        //+ Stats.delimiter + maxWaitingTimeForAgent;
+    }
 }
