1b120995b9a748141df6e2906e174b20e4cc0a89
Theresa Thunig <thunig@vsp.tu-berlin.de>
2015-09-25 14:40:37 +0200

    remove some usages of getPersonId()

--- playgrounds/dgrether/src/main/java/playground/dgrether/koehlerstrehlersignal/analysis/ReadRoutesFromEvents.java
+++ playgrounds/dgrether/src/main/java/playground/dgrether/koehlerstrehlersignal/analysis/ReadRoutesFromEvents.java
@@ -5,17 +5,22 @@ package playground.dgrether.koehlerstrehlersignal.analysis;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.events.LinkEnterEvent;
 import org.matsim.api.core.v01.events.PersonDepartureEvent;
+import org.matsim.api.core.v01.events.PersonEntersVehicleEvent;
 import org.matsim.api.core.v01.events.handler.LinkEnterEventHandler;
 import org.matsim.api.core.v01.events.handler.PersonDepartureEventHandler;
+import org.matsim.api.core.v01.events.handler.PersonEntersVehicleEventHandler;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.population.Person;
+import org.matsim.vehicles.Vehicle;
 
 /**
  * Class to read selected MATSim routes per agent from the events file 
@@ -24,11 +29,13 @@ import org.matsim.api.core.v01.population.Person;
  * 
  * @author tthunig
  */
-public class ReadRoutesFromEvents implements LinkEnterEventHandler, PersonDepartureEventHandler{
+public class ReadRoutesFromEvents implements LinkEnterEventHandler, PersonEntersVehicleEventHandler, PersonDepartureEventHandler{
 
 	private static final Logger log = Logger.getLogger(ReadRoutesFromEvents.class);
 	
 	private Map<Id<Person>, List<Id<Link>>> matsimRoutes;
+
+	private Map<Id<Vehicle>, Set<Id<Person>>> vehicleId2PassengerIds = new HashMap<>();
 	
 	public ReadRoutesFromEvents() {
 		this.reset(0);
@@ -40,32 +47,39 @@ public class ReadRoutesFromEvents implements LinkEnterEventHandler, PersonDepart
 	}
 
 	@Override
-	public void handleEvent(LinkEnterEvent event) {
-		Id<Person> personId = event.getPersonId();
-		if (!this.matsimRoutes.containsKey(personId)){
-			log.error("Person wasn't seen before, although they has to departure before their first LinkEnterEvent.");
-		}
-		// add the link to the route
-		this.matsimRoutes.get(personId).add(event.getLinkId());
-	}
-
-	public Map<Id<Person>, List<Id<Link>>> getMatsimRoutes() {
-		return this.matsimRoutes;
-	}
-
-	@Override
 	public void handleEvent(PersonDepartureEvent event) {
 		Id<Person> personId = event.getPersonId();
 		if (!this.matsimRoutes.containsKey(personId)){
 			this.matsimRoutes.put(personId, new ArrayList<Id<Link>>());
 		}
 		else{
-			log.error("Person was already seen, although it's their DepartureEvent.");
+			log.error("Person was already seen.");
 		}
 		// add the link to the route
 		this.matsimRoutes.get(personId).add(event.getLinkId());
 	}
-	
-	
+
+	@Override
+	public void handleEvent(PersonEntersVehicleEvent event) {
+		if (!vehicleId2PassengerIds.containsKey(event.getVehicleId())){
+			vehicleId2PassengerIds.put(event.getVehicleId(), new HashSet<Id<Person>>());
+		}
+		vehicleId2PassengerIds.get(event.getVehicleId()).add(event.getPersonId());
+	}
+
+	@Override
+	public void handleEvent(LinkEnterEvent event) {
+		for (Id<Person> passengerId : vehicleId2PassengerIds.get(event.getVehicleId())){
+			if (!this.matsimRoutes.containsKey(passengerId)){
+				log.error("Person wasn't seen before.");
+			}
+			// add the link to the route
+			this.matsimRoutes.get(passengerId).add(event.getLinkId());
+		}		
+	}
+
+	public Map<Id<Person>, List<Id<Link>>> getMatsimRoutes() {
+		return this.matsimRoutes;
+	}
 	
 }
