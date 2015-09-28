2e726be907c68f39cfcfa6835cf838190aca6b48
Thibaut Dubernet <thibaut.dubernet@ivt.baug.ethz.ch>
2015-09-28 13:19:33 +0200

    activity histograms --- similar to leg histogram
    dodgy because of first activity, but enough to get an idea of what
    happens for now

--- playgrounds/thibautd/src/main/java/playground/thibautd/socnetsimusages/run/RunSocialScenarioWithEquity.java
+++ playgrounds/thibautd/src/main/java/playground/thibautd/socnetsimusages/run/RunSocialScenarioWithEquity.java
@@ -43,6 +43,7 @@ import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryLogging;
 import org.matsim.core.network.NetworkImpl;
 import org.matsim.facilities.algorithms.WorldConnectLocations;
+import playground.ivt.analysis.IvtAnalysisModule;
 import playground.ivt.matsim2030.Matsim2030Utils;
 import playground.ivt.matsim2030.generation.ScenarioMergingConfigGroup;
 import playground.thibautd.initialdemandgeneration.transformation.SocialNetworkedPopulationDilutionUtils;
@@ -90,6 +91,7 @@ public class RunSocialScenarioWithEquity {
 						install(new JointTripsModule());
 						install(new SocialNetworkModule());
 						install(new EquityStrategiesModule());
+						install(new IvtAnalysisModule() );
 					}
 				});
 		controller.addOverridingModule(
