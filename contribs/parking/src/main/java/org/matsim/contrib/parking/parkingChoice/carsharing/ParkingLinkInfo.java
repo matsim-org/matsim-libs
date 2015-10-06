cba28addfd343291a77995257fa2edac790c95fe
balacmi <milos.balac@ivt.baug.ethz.ch>
2015-10-06 13:34:33 +0200

    Changes to carsharing contrib including:
    -walking leg at the end of the freefloating rental
    
    -in routing all carsharing trips are now consisting of only one leg
    
    -during the simulation carsharing legs are disassembled into the
    components like walk or car, leading to easier implementation of the
    multimodal trips.
    
    Parking contrib:
    
    -updates inside of the pairing modules with carsharing
    
    wrashid playground:
    
    -carsharing updates, fixing some bugs

--- contribs/parking/src/main/java/org/matsim/contrib/parking/parkingChoice/carsharing/ParkingLinkInfo.java
+++ contribs/parking/src/main/java/org/matsim/contrib/parking/parkingChoice/carsharing/ParkingLinkInfo.java
@@ -2,6 +2,7 @@ package org.matsim.contrib.parking.parkingChoice.carsharing;
 
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Link;
+import org.matsim.contrib.parking.PC2.infrastructure.PC2Parking;
 import org.matsim.vehicles.Vehicle;
 
 public class ParkingLinkInfo {
@@ -11,8 +12,24 @@ public class ParkingLinkInfo {
 		this.linkId = linkId;
 	}
 
+	public ParkingLinkInfo(Id<Vehicle> vehicleId, Id<Link> linkId, PC2Parking parking) {
+		super();
+		this.vehicleId = vehicleId;
+		this.linkId = linkId;
+		this.parking = parking;
+	}
+
 	Id<Vehicle> vehicleId;
 	Id<Link> linkId;
+	PC2Parking parking;
+
+	public PC2Parking getParking() {
+		return parking;
+	}
+
+	public void setParking(PC2Parking parking) {
+		this.parking = parking;
+	}
 
 	public Id<Vehicle> getVehicleId() {
 		return vehicleId;
