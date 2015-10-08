5083ddee3a66b5b186549a5d21c7a36f66797863
gunnar.floetteroed@abe.kth.se <gunnar.floetteroed@abe.kth.se>
2015-10-08 15:50:30 +0200

    continued with opdyts

---
+++ playgrounds/gunnar/src/main/java/floetteroed/opdyts/trajectorysampling/SequentialTrajectorySampler.java
@@ -0,0 +1,114 @@
+/*
+ * Opdyts - Optimization of dynamic traffic simulations
+ *
+ * Copyright 2015 Gunnar Flötteröd
+ * 
+ *
+ * This file is part of Opdyts.
+ *
+ * Opdyts is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Opdyts is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Opdyts.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * contact: gunnar.floetteroed@abe.kth.se
+ *
+ */
+package floetteroed.opdyts.trajectorysampling;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import floetteroed.opdyts.DecisionVariable;
+import floetteroed.opdyts.SimulatorState;
+
+/**
+ * 
+ * @author Gunnar Flötteröd
+ *
+ */
+public class SequentialTrajectorySampler implements TrajectorySampler {
+
+	// -------------------- MEMBERS --------------------
+
+	private final LinkedList<SingleTrajectorySampler> untriedSingleTrajectorySamplers;
+
+	private final LinkedList<SingleTrajectorySampler> processedSingleTrajectorySamplers;
+
+	// -------------------- CONSTRUCTION --------------------
+
+	public SequentialTrajectorySampler(
+			final List<? extends SingleTrajectorySampler> singleTrajectorySamplers) {
+		this.untriedSingleTrajectorySamplers = new LinkedList<SingleTrajectorySampler>(
+				singleTrajectorySamplers);
+		this.processedSingleTrajectorySamplers = new LinkedList<SingleTrajectorySampler>();
+	}
+
+	// -------------------- GETTERS --------------------
+
+	public List<SingleTrajectorySampler> getProcessedSingleTrajectorySamplersView() {
+		return Collections
+				.unmodifiableList(this.processedSingleTrajectorySamplers);
+	}
+
+	public int getUntriedTrajectorySamplerCnt() {
+		return this.untriedSingleTrajectorySamplers.size();
+	}
+
+	// --------------- IMPLEMENTATION OF TrajectorySampler ---------------
+
+	@Override
+	public void initialize() {
+		this.processedSingleTrajectorySamplers
+				.addFirst(this.untriedSingleTrajectorySamplers.removeLast());
+	}
+
+	@Override
+	public boolean foundSolution() {
+		return this.processedSingleTrajectorySamplers.getFirst()
+				.foundSolution();
+	}
+
+	@Override
+	public DecisionVariable getCurrentDecisionVariable() {
+		return this.processedSingleTrajectorySamplers.getFirst()
+				.getCurrentDecisionVariable();
+	}
+
+	@Override
+	public void afterIteration(final SimulatorState newState) {
+		this.processedSingleTrajectorySamplers.getFirst().afterIteration(
+				newState);
+	}
+
+	@Override
+	public Map<DecisionVariable, Double> getDecisionVariable2finalObjectiveFunctionValue() {
+		final Map<DecisionVariable, Double> result = new TreeMap<DecisionVariable, Double>();
+		for (SingleTrajectorySampler singleSampler : this.processedSingleTrajectorySamplers) {
+			result.putAll(singleSampler
+					.getDecisionVariable2finalObjectiveFunctionValue());
+		}
+		return result;
+	}
+
+	@Override
+	public int getTotalTransitionCnt() {
+		int result = 0;
+		for (SingleTrajectorySampler singleSampler : this.processedSingleTrajectorySamplers) {
+			result += singleSampler
+					.getTotalTransitionCnt();
+		}
+		return result;
+	}
+}
---
+++ playgrounds/gunnar/src/main/java/floetteroed/opdyts/trajectorysampling/SingleTrajectorySampler.java
@@ -0,0 +1,131 @@
+/*
+ * Opdyts - Optimization of dynamic traffic simulations
+ *
+ * Copyright 2015 Gunnar Flötteröd
+ * 
+ *
+ * This file is part of Opdyts.
+ *
+ * Opdyts is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Opdyts is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Opdyts.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * contact: gunnar.floetteroed@abe.kth.se
+ *
+ */
+package floetteroed.opdyts.trajectorysampling;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import floetteroed.opdyts.DecisionVariable;
+import floetteroed.opdyts.ObjectBasedObjectiveFunction;
+import floetteroed.opdyts.SimulatorState;
+import floetteroed.opdyts.VectorBasedObjectiveFunction;
+import floetteroed.opdyts.convergencecriteria.ConvergenceCriterion;
+
+/**
+ * 
+ * @author Gunnar Flötteröd
+ *
+ */
+public class SingleTrajectorySampler implements TrajectorySampler {
+
+	// -------------------- MEMBERS --------------------
+
+	private final DecisionVariable decisionVariable;
+
+	private final ObjectBasedObjectiveFunction objectBasedObjectiveFunction;
+
+	private final VectorBasedObjectiveFunction vectorBasedObjectiveFunction;
+
+	private final ConvergenceCriterion convergenceCriterion;
+
+	private SimulatorState fromState = null;
+
+	private TransitionSequence transitionSequence = null;
+
+	// -------------------- CONSTRUCTION --------------------
+
+	public SingleTrajectorySampler(final DecisionVariable decisionVariable,
+			final ObjectBasedObjectiveFunction objectBasedObjectiveFunction,
+			final ConvergenceCriterion convergenceCriterion) {
+		this.decisionVariable = decisionVariable;
+		this.objectBasedObjectiveFunction = objectBasedObjectiveFunction;
+		this.vectorBasedObjectiveFunction = null;
+		this.convergenceCriterion = convergenceCriterion;
+	}
+
+	public SingleTrajectorySampler(final DecisionVariable decisionVariable,
+			final VectorBasedObjectiveFunction vectorBasedObjectiveFunction,
+			final ConvergenceCriterion convergenceCriterion) {
+		this.decisionVariable = decisionVariable;
+		this.objectBasedObjectiveFunction = null;
+		this.vectorBasedObjectiveFunction = vectorBasedObjectiveFunction;
+		this.convergenceCriterion = convergenceCriterion;
+	}
+
+	// --------------- IMPLEMENTATION OF TrajectorySampler ---------------
+
+	@Override
+	public boolean foundSolution() {
+		return this.convergenceCriterion.isConverged();
+	}
+
+	@Override
+	public Map<DecisionVariable, Double> getDecisionVariable2finalObjectiveFunctionValue() {
+		final Map<DecisionVariable, Double> result = new LinkedHashMap<>();
+		if (this.convergenceCriterion.isConverged()) {
+			result.put(this.decisionVariable,
+					this.convergenceCriterion.getFinalObjectiveFunctionValue());
+		}
+		return result;
+	}
+
+	@Override
+	public DecisionVariable getCurrentDecisionVariable() {
+		return this.decisionVariable;
+	}
+
+	@Override
+	public void initialize() {
+		this.decisionVariable.implementInSimulation();
+	}
+
+	@Override
+	public void afterIteration(SimulatorState newState) {
+		if (this.fromState != null) {
+			if (this.transitionSequence == null) {
+				this.transitionSequence = new TransitionSequence(
+						this.fromState, this.decisionVariable, newState,
+						this.objectBasedObjectiveFunction,
+						this.vectorBasedObjectiveFunction);
+			} else {
+				this.transitionSequence.addTransition(this.fromState,
+						this.decisionVariable, newState,
+						this.objectBasedObjectiveFunction,
+						this.vectorBasedObjectiveFunction);
+			}
+			this.convergenceCriterion.evaluate(this.transitionSequence);
+		}
+		this.fromState = newState;
+	}
+
+	@Override
+	public int getTotalTransitionCnt() {
+		if (this.transitionSequence != null) {
+			return this.transitionSequence.size();
+		} else {
+			return 0;
+		}
+	}
+}
