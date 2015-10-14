5776a536314a3ccae3466a3103f238b9407eb10f
Thibaut Dubernet <thibaut.dubernet@ivt.baug.ethz.ch>
2015-10-14 17:10:17 +0200

    re-update error message to make it really meaningful
    here, it was implying one can only change this setting using
    programming.

--- matsim/src/main/java/org/matsim/core/controler/OutputDirectoryHierarchy.java
+++ matsim/src/main/java/org/matsim/core/controler/OutputDirectoryHierarchy.java
@@ -169,7 +169,8 @@ public class OutputDirectoryHierarchy {
 								"The output directory " + outputPath
 								+ " already exists and is not empty!"
 								+ " Please either delete or empty the directory or"
-								+ " configure the controler via setOverwriteFileSetting().");
+								+ " configure the controler via setOverwriteFileSetting()"
+								+ " or the \"overwriteFiles\" parameter of the \"controler\" config group.");
 					case overwriteExistingFiles:
 						System.out.flush();
 						log.warn("###########################################################");
