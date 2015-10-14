ed6430f4cc37b530a17e4fc9d0eecde33d69a0ae
Amit Agarwal <amit.agarwal@campus.tu-berlin.de>
2015-10-14 21:33:58 +0200

    an assert statement to check the number of stuck events.

--- playgrounds/agarwalamit/src/test/java/playground/agarwalamit/flowDynamics/HolesInOTFVisTest.java
+++ playgrounds/agarwalamit/src/test/java/playground/agarwalamit/flowDynamics/HolesInOTFVisTest.java
@@ -18,15 +18,20 @@
  * *********************************************************************** */
 package playground.agarwalamit.flowDynamics;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.events.PersonStuckEvent;
+import org.matsim.api.core.v01.events.handler.PersonStuckEventHandler;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Node;
 import org.matsim.api.core.v01.population.Activity;
@@ -47,6 +52,7 @@ import org.matsim.core.network.NetworkImpl;
 import org.matsim.core.population.routes.LinkNetworkRouteFactory;
 import org.matsim.core.population.routes.NetworkRoute;
 import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.testcases.MatsimTestUtils;
 import org.matsim.vis.otfvis.OTFClientLive;
 import org.matsim.vis.otfvis.OTFVisConfigGroup;
 import org.matsim.vis.otfvis.OnTheFlyServer;
@@ -86,8 +92,20 @@ public class HolesInOTFVisTest {
 
 		}
 
+		final List<PersonStuckEvent> stuckEvents = new ArrayList<PersonStuckEvent>();
 		EventsManager manager = EventsUtils.createEventsManager();
-
+		manager.addHandler(new PersonStuckEventHandler() {
+			
+			@Override
+			public void reset(int iteration) {
+			}
+			
+			@Override
+			public void handleEvent(PersonStuckEvent event) {
+				stuckEvents.add(event);
+			}
+		});
+		
 		QSim qSim = QSimUtils.createDefaultQSim(sc, manager);
 		
 		if ( useOTFVis ) {
@@ -99,8 +117,9 @@ public class HolesInOTFVisTest {
 			OnTheFlyServer server = OTFVis.startServerAndRegisterWithQSim(sc.getConfig(), sc, manager, qSim);
 			OTFClientLive.run(sc.getConfig(), server);
 		}
-		
 		qSim.run();
+		
+		Assert.assertEquals("There should not be any stuck events.", 0, stuckEvents.size(), MatsimTestUtils.EPSILON);
 	}
 
 	private static final class SimpleNetwork{
@@ -121,6 +140,7 @@ public class HolesInOTFVisTest {
 			config.qsim().setStorageCapFactor(0.05);
 			
 			config.qsim().setStuckTime(24*3600); // in order to let agents wait instead of forced entry.
+			config.qsim().setEndTime(01*3600);
 
 			config.qsim().setTrafficDynamics(TrafficDynamics.withHoles);
 			config.qsim().setSnapshotStyle(SnapshotStyle.withHoles);
