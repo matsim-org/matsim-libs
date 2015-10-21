82e52766383021c020cd8a3e27c44e29257c59c1
boescpa <patrick.boesch@ivt.baug.ethz.ch>
2015-10-21 10:21:15 +0200

    Aggregating Facilities...

--- playgrounds/boescpa/src/main/java/playground/boescpa/baseline/preparation/FacilitiesFromBZ12.java
+++ playgrounds/boescpa/src/main/java/playground/boescpa/baseline/preparation/FacilitiesFromBZ12.java
@@ -7,9 +7,7 @@ import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.facilities.*;
 import playground.boescpa.lib.obj.CSVReader;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -24,10 +22,10 @@ public class FacilitiesFromBZ12 {
         final String activityType = args[1];
         final String pathToOutputFacilities = args[2];
 
-        final boolean publicFacilities = false;
+        final boolean publicFacilities = true;
 
         final ActivityFacilitiesFactoryImpl factory = new ActivityFacilitiesFactoryImpl();
-        final ActivityFacilities facilities = FacilitiesUtils.createActivityFacilities();
+        final Map<String, TempFacility> tempFacilities = new HashMap<>();
         int idNumber = 0;
 
         CSVReader reader = new CSVReader(pathToCSV);
@@ -36,37 +34,62 @@ public class FacilitiesFromBZ12 {
         while (newLine != null) {
 
             // EMPFTE;METER_X;METER_Y;NOGA_CD_2008_6;KATEGORIE;NOGA_TAG;CAPACITY;OPEN_FROM;OPEN_TO;METER_X_GERUNDET;METER_Y_GERUNDET
-            String idString;
             String desc;
             if (!publicFacilities) {
-                //idString = newLine[4];
-                desc = newLine[4]; //5];
+                desc = newLine[4];
             } else {
-                //idString = activityType;
                 desc = null;
             }
-            idString = String.format("%06d", ++idNumber); //idString + "_" + String.format("%06d", ++idNumber);
-            Double capacity = Double.parseDouble(newLine[6]);
-            Double openFrom = Double.parseDouble(newLine[7]);
-            Double openTill = Double.parseDouble(newLine[8]);
-            Double xCoord = Double.parseDouble(newLine[9]);
-            Double yCoord = Double.parseDouble(newLine[10]);
+            double capacity = Double.parseDouble(newLine[6]);
+            double openFrom = Double.parseDouble(newLine[7]);
+            double openTill = Double.parseDouble(newLine[8]);
+            double xCoord = Double.parseDouble(newLine[9]);
+            double yCoord = Double.parseDouble(newLine[10]);
 
             // new facility
-            ActivityFacilityImpl newFacility = (ActivityFacilityImpl) factory.createActivityFacility(
-                    Id.create(idString, ActivityFacility.class),
-                    new Coord(xCoord,yCoord));
-            newFacility.setDesc(desc);
-            // new activity
-            ActivityOption newActivity = factory.createActivityOption(activityType);
-            newActivity.setCapacity(capacity);
-            newActivity.addOpeningTime(new OpeningTimeImpl(openFrom, openTill));
-
-            // add new facility and activity
-            newFacility.addActivityOption(newActivity);
-            facilities.addActivityFacility(newFacility);
+            TempFacility tempFacility;
+            String coordKey = xCoord + "_" + yCoord;
+            if (tempFacilities.containsKey(coordKey)) {
+                tempFacility = tempFacilities.get(coordKey);
+            } else {
+                tempFacility = new TempFacility(xCoord, yCoord);
+                tempFacilities.put(coordKey, tempFacility);
+            }
+            tempFacility.capacity += capacity;
+            tempFacility.openFrom = tempFacility.openFrom < openFrom ? tempFacility.openFrom : openFrom;
+            tempFacility.openTill = tempFacility.openTill > openTill ? tempFacility.openTill : openTill;
+            if (desc != null) {
+                if (!tempFacility.descs.keySet().contains(desc)) {
+                    tempFacility.descs.put(desc, 0l);
+                }
+                tempFacility.descs.put(desc, tempFacility.descs.get(desc) + (long) capacity);
+            }
 
             newLine = reader.readLine(); // next line
+        }
+
+        // Create actual facilities:
+        final ActivityFacilities facilities = FacilitiesUtils.createActivityFacilities();
+        for (TempFacility tempFacility : tempFacilities.values()) {
+            ActivityFacilityImpl newFacility = (ActivityFacilityImpl) factory.createActivityFacility(
+                    Id.create(String.format("%06d", ++idNumber), ActivityFacility.class),
+                    tempFacility.coords);
+            // Create description
+            String finalDesc = "";
+            for (String desc : tempFacility.descs.keySet()) {
+                finalDesc = finalDesc + desc + " " + tempFacility.descs.get(desc) + "; ";
+            }
+            if (finalDesc.length() > 1) {
+                finalDesc = finalDesc.substring(0, finalDesc.lastIndexOf(";"));
+                newFacility.setDesc(finalDesc);
+            }
+            // Create activities
+            ActivityOption newActivity = factory.createActivityOption(activityType);
+            newActivity.setCapacity(tempFacility.capacity);
+            newActivity.addOpeningTime(new OpeningTimeImpl(tempFacility.openFrom, tempFacility.openTill));
+            newFacility.addActivityOption(newActivity);
+            // add new facility and activity
+            facilities.addActivityFacility(newFacility);
         }
 
         // Write facilities
@@ -82,15 +105,16 @@ public class FacilitiesFromBZ12 {
         facilitiesReader.readFile(pathToFile);
     }
 
-    /*private class TempFacility {
+    private static class TempFacility {
         Coord coords;
-        List<ActivityOption> activities;
-        Map<String, Double> descs;
+        double capacity = 0;
+        double openFrom = Double.MAX_VALUE;
+        double openTill = Double.MIN_VALUE;
+        Map<String, Long> descs;
 
         TempFacility(double xCoord, double yCoord) {
             this.coords = new Coord(xCoord, yCoord);
-            activities = new ArrayList<>();
             descs = new HashMap<>();
         }
-    }*/
+    }
 }
