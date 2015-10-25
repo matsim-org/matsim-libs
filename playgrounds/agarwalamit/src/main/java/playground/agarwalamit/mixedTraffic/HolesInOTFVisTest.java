be658e5a31b90662d86ad1342ed4108c8fa31b6d
Amit Agarwal <amit.agarwal@campus.tu-berlin.de>
2015-10-25 20:55:32 +0100

    minor change

--- playgrounds/agarwalamit/src/main/java/playground/agarwalamit/mixedTraffic/HolesInOTFVisTest.java
+++ playgrounds/agarwalamit/src/main/java/playground/agarwalamit/mixedTraffic/HolesInOTFVisTest.java
@@ -24,7 +24,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.junit.Assert;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
