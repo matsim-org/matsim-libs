8a169e818dee730f573e39031fc1a6a6f75a4c99
Kai Nagel <2008@kainagel.org>
2015-10-28 20:31:25 +0100

    hopefully fix failing compile by (1) retrofitting something that was not yet there during rename; (2) move a test to another package to get around an access reduction

--- playgrounds/hbecker/src/main/java/sharedmobility/CSMembershipAllocator.java
+++ playgrounds/hbecker/src/main/java/sharedmobility/CSMembershipAllocator.java
@@ -5,8 +5,7 @@ import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.network.MatsimNetworkReader;
 import org.matsim.core.population.MatsimPopulationReader;
 import org.matsim.core.population.PopulationReader;
-import org.matsim.core.population.PopulationWriter;
-import org.matsim.core.scenario.ScenarioImpl;
+import org.matsim.core.scenario.MutableScenario;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.utils.objectattributes.ObjectAttributes;
 import org.matsim.utils.objectattributes.ObjectAttributesXmlReader;
@@ -21,7 +20,7 @@ public class CSMembershipAllocator {
 		
 		new ObjectAttributesXmlReader(bla).parse(args[0]);		
 			
-		ScenarioImpl scenario = (ScenarioImpl) ScenarioUtils.createScenario(ConfigUtils.createConfig());
+		MutableScenario scenario = (MutableScenario) ScenarioUtils.createScenario(ConfigUtils.createConfig());
 		PopulationReader populationReader = new MatsimPopulationReader(scenario);
 		MatsimNetworkReader networkReader = new MatsimNetworkReader(scenario);
 		networkReader.readFile(args[1]);
