32c8bd2fbb32f39d425e3087bb7f6eb9ce0340f1
Amit Agarwal <amit.agarwal@campus.tu-berlin.de>
2015-10-28 09:09:27 +0100

    using enum EmissionCostFactors

--- playgrounds/benjamin/src/main/java/playground/benjamin/scenarios/munich/exposure/EmissionResponsibilityCostModule.java
+++ playgrounds/benjamin/src/main/java/playground/benjamin/scenarios/munich/exposure/EmissionResponsibilityCostModule.java
@@ -27,6 +27,8 @@ import org.matsim.api.core.v01.network.Link;
 import org.matsim.contrib.emissions.types.ColdPollutant;
 import org.matsim.contrib.emissions.types.WarmPollutant;
 
+import playground.benjamin.internalization.EmissionCostFactors;
+
 
 /**
  * @author benjamin
@@ -38,13 +40,6 @@ public class EmissionResponsibilityCostModule {
 	private final double emissionCostFactor;
 	private boolean considerCO2Costs = false;
 	
-	/*Values taken from IMPACT (Maibach et al.(2008))*/
-	private final double EURO_PER_GRAMM_NOX = 9600. / (1000. * 1000.);
-	private final double EURO_PER_GRAMM_NMVOC = 1700. / (1000. * 1000.);
-	private final double EURO_PER_GRAMM_SO2 = 11000. / (1000. * 1000.);
-	private final double EURO_PER_GRAMM_PM2_5_EXHAUST = 384500. / (1000. * 1000.);
-	private final double EURO_PER_GRAMM_CO2 = 70. / (1000. * 1000.);
-
 	private ResponsibilityGridTools responsibilityGridTools;
 
 	
@@ -74,30 +69,12 @@ public class EmissionResponsibilityCostModule {
 		double warmEmissionCosts = 0.0;
 		
 		for(WarmPollutant wp : warmEmissions.keySet()){
-			if(wp.equals(WarmPollutant.NOX)) {
-				double noxCosts = warmEmissions.get(wp) * EURO_PER_GRAMM_NOX;
-				warmEmissionCosts += noxCosts;
-//				logger.warn("emissions: " + warmEmissions.get(wp) + "\t noxCosts: " + noxCosts);
-			} else if(wp.equals(WarmPollutant.NMHC)) {
-				double nmhcCosts = warmEmissions.get(wp) * EURO_PER_GRAMM_NMVOC;
-				warmEmissionCosts += nmhcCosts;
-//				logger.warn("emissions: " + warmEmissions.get(wp) + "\t nmhcCosts: " + nmhcCosts);
-			} else if(wp.equals(WarmPollutant.SO2)) {
-				double so2Costs = warmEmissions.get(wp) * EURO_PER_GRAMM_SO2;
-				warmEmissionCosts += so2Costs;
-//				logger.warn("emissions: " + warmEmissions.get(wp) + "\t so2Costs: " + so2Costs);
-			} else if(wp.equals(WarmPollutant.PM)) {
-				double pmCosts = warmEmissions.get(wp) * EURO_PER_GRAMM_PM2_5_EXHAUST;
-				warmEmissionCosts += pmCosts;
-//				logger.warn("emissions: " + warmEmissions.get(wp) + "\t  pmCosts: " + pmCosts);
-			} else if(wp.equals(WarmPollutant.CO2_TOTAL)){
-				if(this.considerCO2Costs) {
-					double co2Costs = warmEmissions.get(wp) * EURO_PER_GRAMM_CO2;
-					warmEmissionCosts += co2Costs;
-//					logger.warn("emissions: " + warmEmissions.get(wp) + "\t co2Costs: " + co2Costs);
-				} else ; //do nothing
+			if(wp.equals(WarmPollutant.CO2_TOTAL) && ! considerCO2Costs) {
+				// do nothing
+			} else {
+				double costFactor = EmissionCostFactors.getCostFactor(wp.toString());
+				warmEmissionCosts += warmEmissions.get(wp) * costFactor ;
 			}
-			else ; //do nothing
 		}
 		//following log statment increases size of the logFile thus commented. amit, Oct'15
 //		logger.info("warm emission costs" + warmEmissionCosts);
@@ -114,10 +91,12 @@ public class EmissionResponsibilityCostModule {
 		double coldEmissionCosts = 0.0;
 		
 		for(ColdPollutant cp : coldEmissions.keySet()){
-			if(cp.equals(ColdPollutant.NOX)) coldEmissionCosts += coldEmissions.get(cp) * EURO_PER_GRAMM_NOX;
-			else if(cp.equals(ColdPollutant.NMHC)) coldEmissionCosts += coldEmissions.get(cp) * EURO_PER_GRAMM_NMVOC;
-			else if(cp.equals(ColdPollutant.PM)) coldEmissionCosts += coldEmissions.get(cp) * EURO_PER_GRAMM_PM2_5_EXHAUST;
-			else ; //do nothing
+			if(cp.equals(WarmPollutant.CO2_TOTAL) && ! considerCO2Costs) {
+				// do nothing
+			} else {
+				double costFactor = EmissionCostFactors.getCostFactor(cp.toString());
+				coldEmissionCosts += coldEmissions.get(cp) * costFactor ;
+			}
 		}
 		// relative density = person minutes of resp. cell and time bin / average person minutes of all cells from this time bin
 		Double relativeDensity = responsibilityGridTools.getFactorForLink(linkId, time);
