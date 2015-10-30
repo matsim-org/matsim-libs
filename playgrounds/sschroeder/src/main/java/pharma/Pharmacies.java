6a82f3e0df024d4acd99a12d31e6e5c073919004
oblonski <jsprit.vehicle.routing@gmail.com>
2015-10-30 17:38:04 +0100

    add geojson pharmacy reader

--- playgrounds/sschroeder/src/main/java/pharma/Pharmacies.java
+++ playgrounds/sschroeder/src/main/java/pharma/Pharmacies.java
@@ -3,8 +3,14 @@ package pharma;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
+import org.matsim.core.utils.geometry.transformations.GeotoolsTransformation;
+import org.matsim.core.utils.geometry.transformations.TransformationFactory;
 import org.matsim.facilities.ActivityFacilities;
+import org.matsim.facilities.ActivityFacility;
 import org.matsim.facilities.FacilitiesUtils;
+import org.matsim.facilities.FacilitiesWriter;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,23 +24,40 @@ public class Pharmacies {
 
         ActivityFacilities facts = FacilitiesUtils.createActivityFacilities("facilities");
 
+        GeotoolsTransformation transformation = new GeotoolsTransformation(TransformationFactory.WGS84,TransformationFactory.DHDN_GK4);
+
+
         ObjectMapper mapper = new ObjectMapper();
         JsonParser jParser = mapper.getFactory().createParser(new File("/Users/schroeder/DLR/Pharma/data/pharmacies.geojson"));
         JsonNode root = mapper.readTree(jParser);
 
         JsonNode features = root.findPath("features");
+        int facilityId = 1;
         for(JsonNode feature : features){
             JsonNode coordinates = feature.findPath("coordinates");
             if(coordinates.isArray()){
                 if(coordinates.get(0).isArray()){
-                    System.out.println(coordinates.get(0).get(0));
+                    double x = coordinates.get(0).get(0).get(0).asDouble();
+                    double y = coordinates.get(0).get(0).get(1).asDouble();
+                    Coord coord = new Coord(x,y);
+                    ActivityFacility fac = facts.getFactory().createActivityFacility(Id.create(facilityId, ActivityFacility.class),transformation.transform(coord));
+                    facts.addActivityFacility(fac);
+                    facilityId++;
                 }
                 else {
-                    System.out.println(coordinates);
+                    double x = coordinates.get(0).asDouble();
+                    double y = coordinates.get(1).asDouble();
+                    Coord coord = new Coord(x,y);
+                    ActivityFacility fac = facts.getFactory().createActivityFacility(Id.create(facilityId, ActivityFacility.class),transformation.transform(coord));
+                    facts.addActivityFacility(fac);
+                    facilityId++;
+//                    System.out.println(coordinates);
                 }
 
             }
         }
+
+        new FacilitiesWriter(facts).write("output/pharmacies.xml");
 
     }
 }
