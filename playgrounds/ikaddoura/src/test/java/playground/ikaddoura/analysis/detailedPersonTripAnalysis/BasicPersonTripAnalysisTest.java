b5c36630c5c45b9b072cb373cfa6ec5f81714aea
ikaddoura <kaddoura@vsp.tu-berlin.de>
2015-11-02 13:46:10 +0100

    minor change

--- playgrounds/ikaddoura/src/test/java/playground/ikaddoura/analysis/detailedPersonTripAnalysis/BasicPersonTripAnalysisTest.java
+++ playgrounds/ikaddoura/src/test/java/playground/ikaddoura/analysis/detailedPersonTripAnalysis/BasicPersonTripAnalysisTest.java
@@ -33,7 +33,7 @@ public class BasicPersonTripAnalysisTest {
 	
 	private static final Logger log = Logger.getLogger(PersonTripAnalysisMain.class);
 
-	private static final boolean printResults = true;
+	private static final boolean printResults = false;
 	
 	@Rule public MatsimTestUtils utils = new MatsimTestUtils();
 	
