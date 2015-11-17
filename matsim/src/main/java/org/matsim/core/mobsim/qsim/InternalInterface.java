f093acb1573f390038848f5b68ed38e1f489cb14
Kai Nagel <2008@kainagel.org>
2015-11-17 13:57:47 +0100

    small amounts of cleanups (making methods static if possible, formatting)

--- matsim/src/main/java/org/matsim/core/mobsim/qsim/InternalInterface.java
+++ matsim/src/main/java/org/matsim/core/mobsim/qsim/InternalInterface.java
@@ -23,7 +23,6 @@ import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.mobsim.framework.MobsimAgent;
-import org.matsim.core.mobsim.framework.ObservableMobsim;
 import org.matsim.core.mobsim.qsim.interfaces.Netsim;
 
 /**
