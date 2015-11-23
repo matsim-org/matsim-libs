519d6126c919a89e3435ce5c466948336548426b
Thibaut Dubernet <thibaut.dubernet@ivt.baug.ethz.ch>
2015-11-23 15:37:54 +0100

    bugfix

--- playgrounds/thibautd/src/main/java/playground/thibautd/maxess/nestedlogitaccessibility/writers/BasicPersonAccessibilityWriter.java
+++ playgrounds/thibautd/src/main/java/playground/thibautd/maxess/nestedlogitaccessibility/writers/BasicPersonAccessibilityWriter.java
@@ -20,6 +20,7 @@ package playground.thibautd.maxess.nestedlogitaccessibility.writers;
 
 import gnu.trove.iterator.TObjectDoubleIterator;
 import gnu.trove.map.TObjectDoubleMap;
+import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
@@ -28,6 +29,7 @@ import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.api.internal.MatsimWriter;
 import org.matsim.core.utils.io.IOUtils;
 import org.matsim.core.utils.io.UncheckedIOException;
+import org.matsim.core.utils.misc.Counter;
 import org.matsim.facilities.ActivityFacility;
 
 import java.io.BufferedWriter;
@@ -37,6 +39,7 @@ import java.io.IOException;
  * @author thibautd
  */
 public class BasicPersonAccessibilityWriter implements MatsimWriter {
+	private static final Logger log = Logger.getLogger( BasicPersonAccessibilityWriter.class );
 	private final TObjectDoubleMap<Id<Person>> accessibilityPerPerson;
 	private final Scenario scenario;
 
@@ -49,12 +52,16 @@ public class BasicPersonAccessibilityWriter implements MatsimWriter {
 
 	@Override
 	public void write( final String filename ) {
+		log.info( "Write accessibility per person to file "+filename );
+		final Counter lineCounter = new Counter( "write accessibility for person # " );
 		try ( final BufferedWriter writer = IOUtils.getBufferedWriter( filename ) ) {
 			writer.write( "person_id\tfacility_id\tx\ty\taccessibility" );
 
 			for ( TObjectDoubleIterator<Id<Person>> it = accessibilityPerPerson.iterator();
-						it.hasNext();
-						it.advance() ) {
+						it.hasNext(); ) {
+				// need to go here and not in "for", because need to "advance" before first element...
+				it.advance();
+				lineCounter.incCounter();
 				final Person person = scenario.getPopulation().getPersons().get( it.key() );
 				final Activity firstActivity = (Activity) person.getSelectedPlan().getPlanElements().get( 0 );
 				final Id<ActivityFacility> facilityId = firstActivity.getFacilityId();
@@ -69,6 +76,8 @@ public class BasicPersonAccessibilityWriter implements MatsimWriter {
 				writer.write( coord.getX()+"\t"+coord.getY()+"\t" );
 				writer.write( accessibility+"" );
 			}
+			lineCounter.printCounter();
+			log.info( "Write accessibility per person to file "+filename+" : DONE" );
 		}
 		catch ( IOException e ) {
 			throw new UncheckedIOException( e );
