64c050fba2a60591e99683f62f50408a2d2451f0
jwjoubert <johan.joubert@up.ac.za>
2015-11-27 11:23:37 +0200

    Updates on Nadia's classes to address memory leak, and run on server.

--- playgrounds/nmviljoen/src/main/java/playground/nmviljoen/network/maliklab/MalikLabExperiments.java
+++ playgrounds/nmviljoen/src/main/java/playground/nmviljoen/network/maliklab/MalikLabExperiments.java
@@ -23,93 +23,118 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+import org.apache.log4j.Logger;
+
+import edu.uci.ics.jung.graph.DirectedGraph;
 import playground.nmviljoen.network.JungCentrality;
 import playground.nmviljoen.network.JungClusters;
 import playground.nmviljoen.network.JungGraphDistance;
 import playground.nmviljoen.network.NmvLink;
 import playground.nmviljoen.network.NmvNode;
 import playground.nmviljoen.network.generator.ShortestPath;
-import edu.uci.ics.jung.graph.DirectedGraph;
+import playground.southafrica.utilities.Header;
 
 public class MalikLabExperiments {
+	private final static Logger LOG = Logger.getLogger(MalikLabExperiments.class);
 	
-	static DirectedGraph<NmvNode, NmvLink> myGraphGrid; 
-	static DirectedGraph<NmvNode, NmvLink> myGraphMalik;
-	static DirectedGraph<NmvNode, NmvLink> myGraphGhost;
-	public MalikLabExperiments() {       
-	}
-	public static void main(String[] args) throws FileNotFoundException{
-		int runs = 3;
-		for (int y = 3; y<=runs;y++){
-			String path = args[0];
-			String sim =args[1];
-			
-			//if it's BASE
-//			path = path+y+"/";
-			
-			//if it's a simulation
-			path = path+y+"/"+sim+y+"/";
-			
-			myGraphMalik = TriGraphConstructor.constructMalikGraph(path);
-			System.out.println("Malik Graph created");
-//			myGraphGrid = TriGraphConstructor.constructGridGraph(path);
-			myGraphGrid = TriGraphConstructor.constructGridGraphSim(path);
-			System.out.println("Grid Graph created");
-//			int[][] assocList = TriGraphConstructor.layerMalik(path,myGraphMalik,myGraphGrid);
-			int[][] assocList = TriGraphConstructor.layerMalikSim(path);
-			
-			myGraphGhost = TriGraphConstructor.constructGhostGraph(path, assocList);
-			System.out.println("The ghost lives");
-			
-			LinkedList<NmvLink> linkListGrid = new LinkedList<NmvLink>(myGraphGrid.getEdges());
-			ArrayList<NmvNode> nodeListGrid = new ArrayList<NmvNode>(myGraphGrid.getVertices());
-			LinkedList<NmvLink> linkListGhost = new LinkedList<NmvLink>(myGraphGhost.getEdges());
-			ArrayList<NmvNode> nodeListGhost = new ArrayList<NmvNode>(myGraphGhost.getVertices());
-			LinkedList<NmvLink> linkListMalik = new LinkedList<NmvLink>(myGraphMalik.getEdges());
-			ArrayList<NmvNode> nodeListMalik = new ArrayList<NmvNode>(myGraphMalik.getVertices());
-			String shortFile=path+"MalikShortPathStat.csv";
-			ShortestPath.collectShortest(myGraphGrid, myGraphMalik, linkListGrid, linkListMalik, nodeListGrid, nodeListMalik, assocList, shortFile);
-			
-			//Grid graph metrics
-			String Gridpath=path+"GridGraph";
-			
-			//Centrality scores
-			
-			JungCentrality.calculateAndWriteUnweightedBetweenness(myGraphGrid,Gridpath+"unweightedNodeBetweenness.csv", Gridpath+"unweightedEdgeBetweenness.csv",nodeListGrid, linkListGrid);
-			JungCentrality.calculateAndWriteUnweightedCloseness(myGraphGrid, Gridpath+"unweightedCloseness.csv", nodeListGrid);
-			JungCentrality.calculateAndWriteUnweightedEigenvector(myGraphGrid, Gridpath+"unweightedEigen.csv", nodeListGrid);
-			JungCentrality.calculateAndWriteDegreeCentrality(myGraphGrid, Gridpath+"Degree.csv", nodeListGrid, linkListGrid);
-
-			//Clustering
-			JungClusters.calculateAndWriteClusteringCoefficient(myGraphGrid, Gridpath+"clusterCoeff.csv");
-			JungClusters.calculateAndWriteWeakComponents(myGraphGrid, Gridpath+"weakComp.csv");
-			JungClusters.calculateAndWriteTriadicCensus(myGraphGrid, Gridpath+"triadCensus.csv");
-
-			//Graph distance
-			JungGraphDistance.calculateAndWriteUnweightedDistances(myGraphGrid, path+"unweightedDist.csv");
-			
-			//Ghost graph metrics
-			String Ghostpath=path+"GhostGraph";
-			
-			//Centrality scores
-			
-			JungCentrality.calculateAndWriteUnweightedBetweenness(myGraphGhost,Ghostpath+"unweightedNodeBetweenness.csv", Ghostpath+"unweightedEdgeBetweenness.csv",nodeListGhost, linkListGhost);
-			JungCentrality.calculateAndWriteUnweightedCloseness(myGraphGhost, Ghostpath+"unweightedCloseness.csv", nodeListGhost);
-			JungCentrality.calculateAndWriteUnweightedEigenvector(myGraphGhost, Ghostpath+"unweightedEigen.csv", nodeListGhost);
-			JungCentrality.calculateAndWriteDegreeCentrality(myGraphGhost, Ghostpath+"Degree.csv", nodeListGhost, linkListGhost);
-
-			//Clustering
-			JungClusters.calculateAndWriteClusteringCoefficient(myGraphGhost, Ghostpath+"clusterCoeff.csv");
-			JungClusters.calculateAndWriteWeakComponents(myGraphGhost, Ghostpath+"weakComp.csv");
-			JungClusters.calculateAndWriteTriadicCensus(myGraphGhost, Ghostpath+"triadCensus.csv");
-
-			//Graph distance
-			JungGraphDistance.calculateAndWriteUnweightedDistances(myGraphGhost, Ghostpath+"unweightedDist.csv");
+	private DirectedGraph<NmvNode, NmvLink> myGraphGrid; 
+	private DirectedGraph<NmvNode, NmvLink> myGraphMalik;
+	private DirectedGraph<NmvNode, NmvLink> myGraphGhost;
+	private String path;
+	private int[][] assocList;
+	
+	public MalikLabExperiments(String path, int ModeSwitch, int SimDim) {       
+		this.path = path;
+		if (ModeSwitch == 0){
+			this.myGraphMalik = TriGraphConstructor.constructMalikGraph(path);
+			LOG.info("Malik Graph created");
+			this.myGraphGrid = TriGraphConstructor.constructGridGraph(path);
+			LOG.info("Grid Graph created");
+			assocList = TriGraphConstructor.layerMalik(path,myGraphMalik,myGraphGrid);
+			this.myGraphGhost = TriGraphConstructor.constructGhostGraph(path, assocList );
+			LOG.info("The ghost lives");
+		}else{
+			this.myGraphMalik = TriGraphConstructor.constructMalikGraph(path);
+			LOG.info("Malik Graph created");
+			this.myGraphGrid = TriGraphConstructor.constructGridGraphSim(path,SimDim);
+			LOG.info("Grid Graph created");
+			assocList = TriGraphConstructor.layerMalikSim(path);
+			this.myGraphGhost = TriGraphConstructor.constructGhostGraph(path, assocList );
+			LOG.info("The ghost lives");	
 		}
 		
+	}
+	
+	
+	public void runThisSpecificExperiment(){
+		LinkedList<NmvLink> linkListGrid = new LinkedList<NmvLink>(myGraphGrid.getEdges());
+		ArrayList<NmvNode> nodeListGrid = new ArrayList<NmvNode>(myGraphGrid.getVertices());
+		LinkedList<NmvLink> linkListGhost = new LinkedList<NmvLink>(myGraphGhost.getEdges());
+		ArrayList<NmvNode> nodeListGhost = new ArrayList<NmvNode>(myGraphGhost.getVertices());
+		LinkedList<NmvLink> linkListMalik = new LinkedList<NmvLink>(myGraphMalik.getEdges());
+		ArrayList<NmvNode> nodeListMalik = new ArrayList<NmvNode>(myGraphMalik.getVertices());
+		String shortFile=path+"MalikShortPathStat.csv";
+		ShortestPath.collectShortest(myGraphGrid, myGraphMalik, linkListGrid, linkListMalik, nodeListGrid, nodeListMalik, assocList, shortFile);
 		
+		//Grid graph metrics
+		String Gridpath=path+"GridGraph";
+		
+		//Centrality scores
+		
+		JungCentrality.calculateAndWriteUnweightedBetweenness(myGraphGrid,Gridpath+"unweightedNodeBetweenness.csv", Gridpath+"unweightedEdgeBetweenness.csv",nodeListGrid, linkListGrid);
+		JungCentrality.calculateAndWriteUnweightedCloseness(myGraphGrid, Gridpath+"unweightedCloseness.csv", nodeListGrid);
+		JungCentrality.calculateAndWriteUnweightedEigenvector(myGraphGrid, Gridpath+"unweightedEigen.csv", nodeListGrid);
+		JungCentrality.calculateAndWriteDegreeCentrality(myGraphGrid, Gridpath+"Degree.csv", nodeListGrid, linkListGrid);
+
+		//Clustering
+		JungClusters.calculateAndWriteClusteringCoefficient(myGraphGrid, Gridpath+"clusterCoeff.csv");
+		JungClusters.calculateAndWriteWeakComponents(myGraphGrid, Gridpath+"weakComp.csv");
+		JungClusters.calculateAndWriteTriadicCensus(myGraphGrid, Gridpath+"triadCensus.csv");
+
+		//Graph distance
+		JungGraphDistance.calculateAndWriteUnweightedDistances(myGraphGrid, path+"unweightedDist.csv");
+		
+		//Ghost graph metrics
+		String Ghostpath=path+"GhostGraph";
+		
+		//Centrality scores
+		
+		JungCentrality.calculateAndWriteUnweightedBetweenness(myGraphGhost,Ghostpath+"unweightedNodeBetweenness.csv", Ghostpath+"unweightedEdgeBetweenness.csv",nodeListGhost, linkListGhost);
+		JungCentrality.calculateAndWriteUnweightedCloseness(myGraphGhost, Ghostpath+"unweightedCloseness.csv", nodeListGhost);
+		JungCentrality.calculateAndWriteUnweightedEigenvector(myGraphGhost, Ghostpath+"unweightedEigen.csv", nodeListGhost);
+		JungCentrality.calculateAndWriteDegreeCentrality(myGraphGhost, Ghostpath+"Degree.csv", nodeListGhost, linkListGhost);
+
+		//Clustering
+		JungClusters.calculateAndWriteClusteringCoefficient(myGraphGhost, Ghostpath+"clusterCoeff.csv");
+		JungClusters.calculateAndWriteWeakComponents(myGraphGhost, Ghostpath+"weakComp.csv");
+		JungClusters.calculateAndWriteTriadicCensus(myGraphGhost, Ghostpath+"triadCensus.csv");
+
+		//Graph distance
+		JungGraphDistance.calculateAndWriteUnweightedDistances(myGraphGhost, Ghostpath+"unweightedDist.csv");
+		
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException{
+		Header.printHeader(MalikLabExperiments.class.toString(), args);
+		
+		int ModeSwitch = Integer.parseInt(args[2]); //if ModeSwitch = 0 then it's a base run; if =1 then it's a simulation
+		int runStart = Integer.parseInt(args[3]);
+		int runEnd = Integer.parseInt(args[4]);
+		int SimDim = Integer.parseInt(args[5]);
+		for (int y = runStart; y<=runEnd;y++){
+			String path = args[0];
+			String sim =args[1];
+			if(ModeSwitch==0){
+				path = path+y+"/";
+			}else{
+				path = path+y+"/"+sim+y+"/";
+			}
 			
+			MalikLabExperiments mle = new MalikLabExperiments(path, ModeSwitch, SimDim);
+			mle.runThisSpecificExperiment();
+		}
 		
+		Header.printFooter();
 	}
 
 }
