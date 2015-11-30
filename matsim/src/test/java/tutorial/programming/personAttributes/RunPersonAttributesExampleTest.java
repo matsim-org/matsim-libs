0dc82755822e3ddd8195a90939017107b3c63903
tschlenther <schlenther@vsp.tu-berlin.de>
2015-11-30 15:11:39 +0100

    added RunPersonAttributesExampleTest

---
+++ matsim/src/test/java/tutorial/programming/personAttributes/RunPersonAttributesExampleTest.java
@@ -0,0 +1,46 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * RunPersonAttributesExample.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2009 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+/**
+ * 
+ */
+package tutorial.programming.personAttributes;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.matsim.testcases.MatsimTestUtils;
+
+public class RunPersonAttributesExampleTest {
+
+	@Rule public MatsimTestUtils utils = new MatsimTestUtils() ;
+
+	@Test
+	public void test(){
+		try{
+		RunPersonAttributesExample.main(null);
+		}
+		catch(Exception e){
+			Assert.fail(e.toString());
+		}
+	}
+	
+	
+}
