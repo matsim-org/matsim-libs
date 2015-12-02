2f256e0f0811dc4737d69f22783808e04ecd5b34
boescpa <patrick.boesch@ivt.baug.ethz.ch>
2015-12-02 12:55:04 +0100

    Added ScenarioTest.

---
+++ playgrounds/boescpa/src/test/java/playground/boescpa/ivtBaseline/preparation/TestRunLocationChoice.java
@@ -0,0 +1,177 @@
+/*
+ * *********************************************************************** *
+ * project: org.matsim.*                                                   *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2015 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** *
+ */
+
+package playground.boescpa.ivtBaseline.preparation;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.PlanElement;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.ConfigWriter;
+import org.matsim.core.network.MatsimNetworkReader;
+import org.matsim.core.population.ActivityImpl;
+import org.matsim.core.population.MatsimPopulationReader;
+import org.matsim.core.population.PopulationWriter;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.facilities.*;
+import org.matsim.testcases.MatsimTestUtils;
+import org.matsim.utils.objectattributes.ObjectAttributes;
+import org.matsim.utils.objectattributes.ObjectAttributesXmlWriter;
+import playground.boescpa.analysis.scenarioAnalyzer.ScenarioAnalyzer;
+import playground.boescpa.analysis.spatialCutters.NoCutter;
+import playground.boescpa.lib.tools.fileCreation.F2LCreator;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author boescpa
+ */
+public class TestRunLocationChoice {
+
+	//private ScenarioAnalyzer scenarioAnalyzer;
+
+	@Rule
+	public MatsimTestUtils utils = new MatsimTestUtils();
+
+	@Before
+	public void prepareTests() {
+
+		final String pathToNetwork = "test/scenarios/pt-tutorial/multimodalnetwork.xml";
+		final String pathToInitialPopulation = utils.getClassInputDirectory() + "population.xml";
+		final String pathToPopulation = utils.getOutputDirectory() + "population.xml";
+		final String pathToPrefs = utils.getOutputDirectory() + "prefs.xml";
+		final String pathToFacilities = utils.getOutputDirectory() + "facilities.xml";
+		final String pathToF2L = utils.getOutputDirectory() + "f2l.f2l";
+		final String pathToConfig = utils.getOutputDirectory() + "config.xml";
+		final String pathToSchedule = "test/scenarios/pt-tutorial/transitschedule.xml";
+		final String pathToVehicles = "test/scenarios/pt-tutorial/transitVehicles.xml";
+
+		Scenario tempScenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+		new MatsimNetworkReader(tempScenario).readFile(pathToNetwork);
+		new MatsimPopulationReader(tempScenario).readFile(pathToInitialPopulation);
+		createPrefs(tempScenario, pathToPrefs);
+		createFacilities(tempScenario, pathToFacilities);
+		F2LCreator.createF2L(tempScenario, pathToF2L);
+		new PopulationWriter(tempScenario.getPopulation()).write(pathToPopulation);
+
+		// create config
+		String[] argsConfig = {pathToConfig, "100"};
+		ChooseSecondaryFacilitiesConfigCreator.main(argsConfig);
+		Config config = ConfigUtils.loadConfig(pathToConfig);
+		config.setParam("controler", "outputDirectory", utils.getOutputDirectory() + "output/");
+			// Activate transit
+		config.setParam("transit", "useTransit", "false");
+			// Set files
+		config.setParam("facilities", "inputFacilitiesFile", pathToFacilities);
+		config.setParam("f2l", "inputF2LFile", pathToF2L);
+		config.setParam("households", "inputFile", "null");
+		config.setParam("households", "inputHouseholdAttributesFile", "null");
+		config.setParam("network", "inputNetworkFile", pathToNetwork);
+		config.setParam("plans", "inputPersonAttributesFile", pathToPrefs);
+		config.setParam("plans", "inputPlansFile", pathToPopulation);
+		config.setParam("transit", "transitScheduleFile", pathToSchedule);
+		config.setParam("transit", "vehiclesFile", pathToVehicles);
+			// handle location choice
+		config.setParam("locationchoice", "prefsFile", pathToPrefs);
+		config.setParam("locationchoice", "flexible_types", "shop");
+		config.setParam("locationchoice", "epsilonScaleFactors", "0.3");
+			// Set threads to 1
+		config.setParam("global", "numberOfThreads", "1");
+		config.setParam("parallelEventHandling", "numberOfThreads", "1");
+		config.setParam("qsim", "numberOfThreads", "1");
+		new ConfigWriter(config).write(pathToConfig);
+
+		String[] argsSim = {pathToConfig};
+		RunLocationChoice.main(argsSim);
+
+		/*final Scenario scenario = ScenarioUtils.loadScenario(config);
+		final Controler controler = new Controler(scenario);
+		controler.run();
+
+		// Get network and events file
+		Network network = scenario.getNetwork();
+		String eventFile = this.utils.getOutputDirectory() + "ITERS/it.0/0.events.xml.gz";
+
+		// Analyze the events:
+		ScenarioAnalyzerEventHandler[] handlers = {
+				new AgentCounter(network),
+				new TripAnalyzer(network),
+				new TripActivityCrosscorrelator(network),
+				new MFDCreator(network)
+		};
+		scenarioAnalyzer = new ScenarioAnalyzer(eventFile, 1, handlers);
+		scenarioAnalyzer.analyzeScenario();*/
+	}
+
+	@Test
+	public void testAnalyzer() {
+		// Return the results:
+		//scenarioAnalyzer.createResults(utils.getOutputDirectory() + "scenarioAnalyzerResults.txt", new NoCutter());
+	}
+
+
+	private void createPrefs(Scenario tempScenario, String pathToPrefsFile) {
+		ObjectAttributes prefs = PrefsCreator.createPrefsBasedOnPlans(tempScenario.getPopulation());
+		ObjectAttributesXmlWriter attributesXmlWriterWriter = new ObjectAttributesXmlWriter(prefs);
+		attributesXmlWriterWriter.writeFile(pathToPrefsFile);
+	}
+
+	private void createFacilities(Scenario tempScenario, String pathToFacilitiesFile) {
+		int facilityId = 0;
+		Map<Coord, Id<ActivityFacility>> facilities = new HashMap<>();
+		ActivityFacilities activityFacilities = tempScenario.getActivityFacilities();
+		for (Person person : tempScenario.getPopulation().getPersons().values()) {
+			if (person.getSelectedPlan() != null) {
+				List<PlanElement> plan = person.getSelectedPlan().getPlanElements();
+				for (PlanElement planElement : plan) {
+					if (planElement instanceof ActivityImpl) {
+						ActivityImpl act = (ActivityImpl) planElement;
+						if (!facilities.containsKey(act.getCoord())) {
+							ActivityFacility activityFacility =
+									activityFacilities.getFactory().createActivityFacility(Id.create(facilityId++, ActivityFacility.class), act.getCoord());
+							activityFacilities.addActivityFacility(activityFacility);
+							facilities.put(act.getCoord(), activityFacility.getId());
+						}
+						ActivityFacility activityFacility = activityFacilities.getFacilities().get(facilities.get(act.getCoord()));
+						if (!activityFacility.getActivityOptions().containsKey(act.getType())) {
+							ActivityOption activityOption =
+									activityFacilities.getFactory().createActivityOption(act.getType());
+							if (!act.getType().equals("home")) {
+								activityOption.addOpeningTime(new OpeningTimeImpl(21600, act.getEndTime()));
+							}
+							activityFacility.addActivityOption(activityOption);
+						}
+						act.setFacilityId(activityFacility.getId());
+					}
+				}
+			}
+		}
+		new FacilitiesWriter(tempScenario.getActivityFacilities()).write(pathToFacilitiesFile);
+	}
+}
