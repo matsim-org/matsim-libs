6f0aedda8be7f1538fe7b5d5228e331767e846e5
Theresa Thunig <thunig@vsp.tu-berlin.de>
2015-12-09 15:44:41 +0100

    expand the test that fixes the matsim behavior in braess's scenario

--- playgrounds/tthunig/src/test/java/scenarios/braess/FixBraessBehaviorTest.java
+++ playgrounds/tthunig/src/test/java/scenarios/braess/FixBraessBehaviorTest.java
@@ -21,6 +21,7 @@
  */
 package scenarios.braess;
 
+import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Rule;
@@ -33,16 +34,23 @@ import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ActivityParams;
 import org.matsim.core.config.groups.StrategyConfigGroup.StrategySettings;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
+import org.matsim.core.events.handler.EventHandler;
 import org.matsim.core.replanning.DefaultPlanStrategiesModule.DefaultSelector;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.testcases.MatsimTestUtils;
 
+import playground.artemc.socialCost.SocialCostController.Initializer;
 import playground.vsp.congestion.controler.MarginalCongestionPricingContolerListener;
+import playground.vsp.congestion.handlers.CongestionHandlerImplV3;
 import playground.vsp.congestion.handlers.CongestionHandlerImplV4;
+import playground.vsp.congestion.handlers.CongestionHandlerImplV8;
+import playground.vsp.congestion.handlers.CongestionHandlerImplV9;
 import playground.vsp.congestion.handlers.TollHandler;
 import scenarios.analysis.TtAbstractAnalysisTool;
 import scenarios.analysis.TtListenerToBindAndWriteAnalysis;
 import scenarios.braess.analysis.TtAnalyzeBraess;
+import scenarios.braess.run.RunBraessSimulation;
+import scenarios.braess.run.RunBraessSimulation.PricingType;
 
 /**
  * Test to fix the route distribution and travel times in Braess's scenario.
@@ -53,46 +61,22 @@ import scenarios.braess.analysis.TtAnalyzeBraess;
  * @author tthunig
  *
  */
-public class FixBraessBehaviorTest{
+public final class FixBraessBehaviorTest{
+	
+	private static final Logger log = Logger
+			.getLogger(FixBraessBehaviorTest.class);
 	
 	@Rule
 	public MatsimTestUtils testUtils = new MatsimTestUtils();
 	
 	@Test
 	public void testBraessWoPricing() {
-		
-		Config config = defineConfig();
-		Scenario scenario = ScenarioUtils.loadScenario(config);
-
-		Controler controler = new Controler(scenario);
-		 		 
-		// add a controller listener to analyze results
-		TtAbstractAnalysisTool handler = new TtAnalyzeBraess();
-		controler.addControlerListener(new TtListenerToBindAndWriteAnalysis(scenario, handler));
-			
-		controler.run();		
-		
-		// test route distribution
-		int agentsOnMiddleRoute = handler.getRouteUsers()[1];
-		// was 1983 in sept'15 - why?
-		Assert.assertEquals("The number of agents on the middle route has changed to previous MATSim behavior.", 1978, agentsOnMiddleRoute, 5);
-		int agentsOnLowerRoute = handler.getRouteUsers()[2];
-		// was 8 in sept'15 - why?
-		Assert.assertEquals("The number of agents on the lower route has changed to previous MATSim behavior.", 11, agentsOnLowerRoute, 3);
-		int agentsOnUpperRoute = handler.getRouteUsers()[0];
-		// was 9 in sept'15 - why?
-		Assert.assertEquals("The number of agents on the upper route has changed to previous MATSim behavior.", 11, agentsOnUpperRoute, 2);
-		
-		// test total travel time
-		double totalTT = handler.getTotalTT();
-		// was 3951597 in sept'15 - why?
-		Assert.assertEquals("The total travel time has changed to previous MATSim behavior.", 3949870, totalTT, 2000);		
+		fixRouteDistributionAndTT(RunBraessSimulation.PricingType.NONE, 11, 1978, 11, 3949870);
 	}
-	
-	// TODO test other congestion versions too (v3, v8, v9)
+
 	@Test
 	public void testV3() {
-		Assert.fail("Not yet implemented.");
+		fixRouteDistributionAndTT(RunBraessSimulation.PricingType.V3, 455, 1126, 419, 3153112);
 	}
 	
 	/* V4 throws a runtime exception: 
@@ -102,26 +86,79 @@ public class FixBraessBehaviorTest{
 	@Test
 	@Ignore
 	public void testV4() {
-		// prepare config and scenario
+		fixRouteDistributionAndTT(RunBraessSimulation.PricingType.V4, 445, 1021, 534, 2985780);
+	}
+
+	@Test
+	public void testV8() {
+		fixRouteDistributionAndTT(RunBraessSimulation.PricingType.V8, 566, 945, 489, 2825445);
+	}
+	
+	@Test
+	public void testV9() {
+		fixRouteDistributionAndTT(RunBraessSimulation.PricingType.V9, 619, 843, 538, 2830500);
+	}
+	
+	@Test
+	@Ignore
+	public void testFlowbased() {
+		Assert.fail("Not yet implemented.");
+//		fixRouteDistributionAndTT(RunBraessSimulation.PricingType.FLOWBASED, , , , );
+	}
+	
+	private void fixRouteDistributionAndTT(RunBraessSimulation.PricingType pricingType, int expectedNOAgentsOnUpperRoute,
+			int expectedNOAgentsOnMiddleRoute, int expectedNOAgentsOnLowerRoute, double expectedTotalTT){
+
 		Config config = defineConfig();
 		Scenario scenario = ScenarioUtils.loadScenario(config);
 
-		// prepare the controller
 		Controler controler = new Controler(scenario);
-
-		// add tolling
-		TollHandler tollHandler = new TollHandler(scenario);
-
-		controler.addControlerListener(
-				new MarginalCongestionPricingContolerListener(
-						controler.getScenario(), tollHandler, 
-						new CongestionHandlerImplV4(controler.getEvents(), 
-								controler.getScenario())));
-
-		// run the simulation
-		controler.run();
 		
-		// TODO test whether total travel time, route distribution ... has changed compared to previous MATSim behavior.
+		if (!pricingType.equals(PricingType.NONE) && !pricingType.equals(PricingType.FLOWBASED)) {
+			// add tolling
+			TollHandler tollHandler = new TollHandler(scenario);
+			// choose the correct congestion handler and add it
+			EventHandler congestionHandler = null;
+			switch (pricingType) {
+			case V3:
+				congestionHandler = new CongestionHandlerImplV3(controler.getEvents(), controler.getScenario());
+				break;
+			case V4:
+				congestionHandler = new CongestionHandlerImplV4(controler.getEvents(), controler.getScenario());
+				break;
+			case V8:
+				congestionHandler = new CongestionHandlerImplV8(controler.getEvents(), controler.getScenario());
+				break;
+			case V9:
+				congestionHandler = new CongestionHandlerImplV9(controler.getEvents(), controler.getScenario());
+				break;
+			default:
+				break;
+			}
+			controler.addControlerListener(new MarginalCongestionPricingContolerListener(controler.getScenario(), tollHandler, congestionHandler));
+		} else if (pricingType.equals(PricingType.FLOWBASED)) {
+			Initializer initializer = new Initializer();
+			controler.addControlerListener(initializer);		
+		}
+					
+		// add a controller listener to analyze results
+		TtAbstractAnalysisTool handler = new TtAnalyzeBraess();
+		controler.addControlerListener(new TtListenerToBindAndWriteAnalysis(scenario, handler));
+			
+		controler.run();		
+		
+		// test route distribution
+		int agentsOnUpperRoute = handler.getRouteUsers()[0];
+		int agentsOnMiddleRoute = handler.getRouteUsers()[1];
+		int agentsOnLowerRoute = handler.getRouteUsers()[2];
+		log.info("Route distribution: " + agentsOnUpperRoute + ", " + agentsOnMiddleRoute + ", " + agentsOnLowerRoute);
+		Assert.assertEquals("The number of agents on the upper route has changed to previous MATSim behavior.", expectedNOAgentsOnUpperRoute, agentsOnUpperRoute);
+		Assert.assertEquals("The number of agents on the middle route has changed to previous MATSim behavior.", expectedNOAgentsOnMiddleRoute, agentsOnMiddleRoute);
+		Assert.assertEquals("The number of agents on the lower route has changed to previous MATSim behavior.", expectedNOAgentsOnLowerRoute, agentsOnLowerRoute);
+		
+		// test total travel time
+		double totalTT = handler.getTotalTT();
+		Assert.assertEquals("The total travel time has changed to previous MATSim behavior.", expectedTotalTT, totalTT, MatsimTestUtils.EPSILON);
 	}
 
 	private Config defineConfig() {
