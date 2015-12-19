d447ccfe3ca02d7cb02eacd15cb55363ef975ecf
gunnar.floetteroed@abe.kth.se <gunnar.floetteroed@abe.kth.se>
2015-12-19 12:12:02 +0100

    resolving merge errors

--- playgrounds/gunnar/src/main/java/opdytsintegration/OccupancyAnalyzer.java
+++ playgrounds/gunnar/src/main/java/opdytsintegration/OccupancyAnalyzer.java
@@ -18,6 +18,7 @@ import org.matsim.api.core.v01.events.handler.VehicleAbortsEventHandler;
 import org.matsim.api.core.v01.events.handler.VehicleEntersTrafficEventHandler;
 import org.matsim.api.core.v01.events.handler.VehicleLeavesTrafficEventHandler;
 import org.matsim.api.core.v01.network.Link;
+import org.matsim.vehicles.Vehicle;
 
 import floetteroed.utilities.DynamicData;
 
@@ -157,55 +158,56 @@ public class OccupancyAnalyzer implements LinkLeaveEventHandler,
 				binSize_s, binCnt);
 
 		final Id<Link> id1 = Id.createLinkId("1");
+		final Id<Vehicle> veh1 = Id.createVehicleId("1");
 
 		// [0,10): avg. occupancy = 0, last occupancy = 0
 
-		analyzer.handleEvent(new LinkEnterEvent(10.0, null, id1));
+		analyzer.handleEvent(new LinkEnterEvent(10.0, veh1, id1));
 		// time = 10, in = 1, out = 0
 		analyzer.handleEvent(new VehicleEntersTrafficEvent(10.0, null, id1,
-				null, null, 0.0));
+				veh1, null, 0.0));
 		// time = 10, in = 2, out = 0
-		analyzer.handleEvent(new LinkEnterEvent(14.0, null, id1));
+		analyzer.handleEvent(new LinkEnterEvent(14.0, veh1, id1));
 		// time = 14, in = 3, out = 0
-		analyzer.handleEvent(new LinkLeaveEvent(19.0, null, id1));
+		analyzer.handleEvent(new LinkLeaveEvent(19.0, veh1, id1));
 		// time = 19, in = 3, out = 1
 
 		// [10,20): avg. occupancy = 2.5, last occupancy = 2
 
 		analyzer.handleEvent(new VehicleEntersTrafficEvent(20.0, null, id1,
-				null, "car", 0.0));
+				veh1, "car", 0.0));
 		// time = 20, in = 4, out = 1
-		analyzer.handleEvent(new LinkEnterEvent(29.99, null, id1));
+		analyzer.handleEvent(new LinkEnterEvent(29.99, veh1, id1));
 		// time = 29, in = 5, out = 1
 
 		// [20,30): avg. occupancy = 3.1, last occupancy = 4
 
 		analyzer.handleEvent(new VehicleLeavesTrafficEvent(30.0, null, id1,
-				null, "car", 0.0));
+				veh1, "car", 0.0));
 		// time = 30, in = 5, out = 2
-		analyzer.handleEvent(new LinkLeaveEvent(30.0, null, id1));
+		analyzer.handleEvent(new LinkLeaveEvent(30.0, veh1, id1));
 		// time = 30, in = 5, out = 3
-		analyzer.handleEvent(new LinkEnterEvent(30.0, null, id1));
+		analyzer.handleEvent(new LinkEnterEvent(30.0, veh1, id1));
 		// time = 30, in = 6, out = 3
 		analyzer.handleEvent(new VehicleLeavesTrafficEvent(39.99, null, id1,
-				null, "car", 0.0));
+				veh1, "car", 0.0));
 		analyzer.handleEvent(new VehicleLeavesTrafficEvent(39.999, null, id1,
-				null, "car", 0.0));
+				veh1, "car", 0.0));
 		analyzer.handleEvent(new VehicleLeavesTrafficEvent(39.9999, null, id1,
-				null, "car", 0.0));
+				veh1, "car", 0.0));
 		// time = 39, in = 6, out = 6
 
 		// [30,39): avg. occupancy: 0.9*3+0.1*0=2.7, last occupancy = 0
 
-		analyzer.handleEvent(new LinkEnterEvent(40.0, null, id1));
-		analyzer.handleEvent(new LinkEnterEvent(40.0, null, id1));
-		analyzer.handleEvent(new LinkLeaveEvent(40.0, null, id1));
+		analyzer.handleEvent(new LinkEnterEvent(40.0, veh1, id1));
+		analyzer.handleEvent(new LinkEnterEvent(40.0, veh1, id1));
+		analyzer.handleEvent(new LinkLeaveEvent(40.0, veh1, id1));
 
 		// time = 40, in = 8, out = 7
 
 		// [40,49): avg. occupancy: 1.0, last occupancy = 0
 
-		analyzer.handleEvent(new LinkLeaveEvent(100.0, null, id1));
+		analyzer.handleEvent(new LinkLeaveEvent(100.0, veh1, id1));
 
 		// time = 100, in = 8, out = 8
 
