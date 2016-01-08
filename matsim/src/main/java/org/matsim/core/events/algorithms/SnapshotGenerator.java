b614f4a7de713121b851f90a4ea56ac1daf62681
Amit Agarwal <amit.agarwal@campus.tu-berlin.de>
2016-01-08 15:07:46 +0100

    free speed travel time should be same as queue model + 1 extra second because, the positions are assigned based on it.

--- matsim/src/main/java/org/matsim/core/events/algorithms/SnapshotGenerator.java
+++ matsim/src/main/java/org/matsim/core/events/algorithms/SnapshotGenerator.java
@@ -258,7 +258,7 @@ public class SnapshotGenerator implements PersonDepartureEventHandler, PersonArr
 			this.waitingQueue = new ArrayList<EventAgent>();
 			this.buffer = new ArrayList<EventAgent>();
 			this.euklideanDist = CoordUtils.calcDistance(link2.getFromNode().getCoord(), link2.getToNode().getCoord());
-			this.freespeedTravelTime = this.link.getLength() / this.link.getFreespeed();
+			this.freespeedTravelTime = Math.ceil( this.link.getLength() / this.link.getFreespeed() ) + 1; 
 			this.timeCap = this.link.getCapacity() * capCorrectionFactor;
 			this.storageCapFactor = storageCapFactor;
 			this.inverseTimeCap = 1.0 / this.timeCap;
