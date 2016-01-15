22fc44f605e004a6685bbe8afc5cb17fa368b770
smetzler <simon.metzler@mailbox.tu-berlin.de>
2016-01-15 15:25:41 +0100

    xml parser bike implementation to links

--- playgrounds/smetzler/src/main/java/playground/smetzler/bike/BikeLinkImpl.java
+++ playgrounds/smetzler/src/main/java/playground/smetzler/bike/BikeLinkImpl.java
@@ -13,12 +13,30 @@ public class BikeLinkImpl extends LinkImpl {
 
 	protected BikeLinkImpl(Id<Link> id, Node from, Node to, Network network, double length, double freespeed,
 			double capacity, double lanes, String cycleway, String cyclewaySurface) {
-		super(id, from, to, network, length, freespeed, capacity, lanes); //cycleway, cyclewaySurface ???
+		super(id, from, to, network, length, freespeed, capacity, lanes);
 		
 		this.cycleway = cycleway;
 		this.cyclewaySurface = cyclewaySurface;
 		// TODO Auto-generated constructor stub
 	}
+	
+	
+//	@Override
+//	public String toString() {
+//		return super.toString() +
+//		"[id=" + this.getId() + "]" +
+//		"[from_id=" + this.from.getId() + "]" +
+//		"[to_id=" + this.to.getId() + "]" +
+//		"[length=" + this.length + "]" +
+//		"[freespeed=" + this.freespeed + "]" +
+//		"[capacity=" + this.capacity + "]" +
+//		"[permlanes=" + this.nofLanes + "]" +
+//		"[origid=" + this.origid + "]" +
+//		"[type=" + this.type + "]";
+//		"[cycleway=" + this.cycleway + "]";
+//		"[cyclewaySurface=" + this.cyclewaySurface + "]";
+//		
+//	}
 
 }
 
