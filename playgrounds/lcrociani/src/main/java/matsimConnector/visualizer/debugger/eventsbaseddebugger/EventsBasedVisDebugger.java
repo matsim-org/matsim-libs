813cf3003d3e21ffc26049b22f6ccc372dc7507e
laemmel <gregor.laemmel@gmail.com>
2016-01-25 11:35:18 +0100

    removed jar files + modified pom

--- playgrounds/lcrociani/src/main/java/matsimConnector/visualizer/debugger/eventsbaseddebugger/EventsBasedVisDebugger.java
+++ playgrounds/lcrociani/src/main/java/matsimConnector/visualizer/debugger/eventsbaseddebugger/EventsBasedVisDebugger.java
@@ -27,9 +27,9 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.OverlayLayout;
+import javax.swing.*;
+import java.awt.*;
+
 
 import matsimConnector.utility.Constants;
 
