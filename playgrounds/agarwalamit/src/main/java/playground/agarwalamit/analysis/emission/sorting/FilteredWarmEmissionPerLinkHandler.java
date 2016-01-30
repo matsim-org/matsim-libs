4934332c29e41157610234896042b5d9516d9588
Amit Agarwal <amit.agarwal@campus.tu-berlin.de>
2016-01-30 12:23:59 +0100

    a set up for filtering area and person.

--- playgrounds/agarwalamit/src/main/java/playground/agarwalamit/analysis/emission/sorting/FilteredWarmEmissionPerLinkHandler.java
+++ playgrounds/agarwalamit/src/main/java/playground/agarwalamit/analysis/emission/sorting/FilteredWarmEmissionPerLinkHandler.java
@@ -18,52 +18,97 @@
  * *********************************************************************** */
 package playground.agarwalamit.analysis.emission.sorting;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.events.VehicleEntersTrafficEvent;
+import org.matsim.api.core.v01.events.VehicleLeavesTrafficEvent;
+import org.matsim.api.core.v01.events.handler.VehicleEntersTrafficEventHandler;
+import org.matsim.api.core.v01.events.handler.VehicleLeavesTrafficEventHandler;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.population.Person;
 import org.matsim.contrib.emissions.events.WarmEmissionEvent;
 import org.matsim.contrib.emissions.events.WarmEmissionEventHandler;
 import org.matsim.contrib.emissions.types.WarmPollutant;
+import org.matsim.core.events.algorithms.Vehicle2DriverEventHandler;
 import org.matsim.core.utils.gis.ShapeFileReader;
 import org.opengis.feature.simple.SimpleFeature;
 
+import playground.agarwalamit.munich.utils.ExtendedPersonFilter;
 import playground.agarwalamit.utils.GeometryUtils;
+import playground.benjamin.scenarios.munich.analysis.filter.UserGroup;
 import playground.benjamin.scenarios.munich.analysis.nectar.EmissionsPerLinkWarmEventHandler;
 
 /**
  * @author amit
  */
 
-public class FilteredWarmEmissionPerLinkHandler implements WarmEmissionEventHandler {
+public class FilteredWarmEmissionPerLinkHandler implements WarmEmissionEventHandler, VehicleEntersTrafficEventHandler, VehicleLeavesTrafficEventHandler {
+	private static final Logger LOGGER = Logger.getLogger(FilteredWarmEmissionPerLinkHandler.class.getName());
+	
 	private final EmissionsPerLinkWarmEventHandler delegate;
+	private final Vehicle2DriverEventHandler veh2DriverDelegate = new Vehicle2DriverEventHandler();
+	private final ExtendedPersonFilter pf = new ExtendedPersonFilter();
 	private final Collection<SimpleFeature> features ;
 	private Network network;
-	private final boolean isSorting;
+	private boolean isSortingForArea = false;
+	private final UserGroup ug ;
 
-	public FilteredWarmEmissionPerLinkHandler (final double simulationEndTime, final int noOfTimeBins, final String shapeFile, final Network network){
-		delegate = new EmissionsPerLinkWarmEventHandler(simulationEndTime,noOfTimeBins);
-		features = new ShapeFileReader().readFileAndInitialize(shapeFile);
+	public FilteredWarmEmissionPerLinkHandler (final double simulationEndTime, final int noOfTimeBins, final String shapeFile, 
+			final Network network, final UserGroup userGroup){
+		this.delegate = new EmissionsPerLinkWarmEventHandler(simulationEndTime,noOfTimeBins);
+		this.features = new ShapeFileReader().readFileAndInitialize(shapeFile);
 		this.network = network;
-		isSorting = true;
+		this.isSortingForArea = true;
+		this.ug=userGroup;
+		LOGGER.info("Area and user group filtering is used, links fall inside the given shape and belongs to the given user group will be considered.");
+	}
+	
+	public FilteredWarmEmissionPerLinkHandler (final double simulationEndTime, final int noOfTimeBins, final UserGroup userGroup){
+		this(simulationEndTime,noOfTimeBins,null,null,userGroup);
+		LOGGER.info("Usergroup filtering is used, result will include all links but persons from given user group only.");
+		LOGGER.warn( "This could be achieved from the other class \"EmissionsPerPersonPerUserGroup\", alternatively verify your results with the other class.");
+	}
+	
+	
+	public FilteredWarmEmissionPerLinkHandler (final double simulationEndTime, final int noOfTimeBins, final String shapeFile, final Network network){
+		this(simulationEndTime,noOfTimeBins,shapeFile,network,null);
+		LOGGER.info("Area filtering is used, result will include links falls inside given shape and all persons.");
 	}
 	
 	public FilteredWarmEmissionPerLinkHandler (final double simulationEndTime, final int noOfTimeBins){
-		delegate = new EmissionsPerLinkWarmEventHandler(simulationEndTime,noOfTimeBins);
-		features = new ArrayList<>();
-		isSorting = false;
+		this(simulationEndTime,noOfTimeBins,null,null);
+		LOGGER.info("No filtering is used, result will include all links, all persons.");
 	}
 
 	@Override
 	public void handleEvent(WarmEmissionEvent event) {
-		if(isSorting) {
-			Link link = network.getLinks().get(event.getLinkId());
-			if(GeometryUtils.isLinkInsideCity(features, link) ) delegate.handleEvent(event);
-		} else {
-			delegate.handleEvent(event);
+		
+		if( this.ug != null ) {
+			if(isSortingForArea) { // filtering for area only
+				Link link = network.getLinks().get(event.getLinkId());
+				if(GeometryUtils.isLinkInsideCity(features, link) ) {
+					delegate.handleEvent(event);
+				}
+			} else { // no filtering at all
+				delegate.handleEvent(event);
+			}
+		} else { 
+			if (isSortingForArea) { // filtering for both
+				Link link = network.getLinks().get(event.getLinkId());
+				Id<Person> driverId = this.veh2DriverDelegate.getDriverOfVehicle(event.getVehicleId());
+				 if ( this.pf.isPersonIdFromUserGroup(driverId, ug)  && GeometryUtils.isLinkInsideCity(features, link) ) {
+					 delegate.handleEvent(event);
+				 }
+			} else { // filtering for user group only
+				Id<Person> driverId = this.veh2DriverDelegate.getDriverOfVehicle(event.getVehicleId());
+				 if ( this.pf.isPersonIdFromUserGroup(driverId, ug)  ) {
+					 delegate.handleEvent(event);
+				 }
+			}
 		}
 	}
 
@@ -78,5 +123,15 @@ public class FilteredWarmEmissionPerLinkHandler implements WarmEmissionEventHand
 	@Override
 	public void reset(int iteration) {
 		delegate.reset(iteration);
+		veh2DriverDelegate.reset(iteration);
+	}
+	@Override
+	public void handleEvent(VehicleLeavesTrafficEvent event) {
+		veh2DriverDelegate.handleEvent(event);
+	}
+
+	@Override
+	public void handleEvent(VehicleEntersTrafficEvent event) {
+		veh2DriverDelegate.handleEvent(event);
 	}
 }
