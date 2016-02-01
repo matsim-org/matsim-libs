4b4ae71f9b3c9318d6d367b99944445110cb555c
jwjoubert <johan.joubert@up.ac.za>
2016-02-01 11:28:36 +0200

    Adapted network splitter to write output.

--- playgrounds/jjoubert/src/main/java/playground/jjoubert/projects/gfipQueuePassing/GfipNetworkSplitter.java
+++ playgrounds/jjoubert/src/main/java/playground/jjoubert/projects/gfipQueuePassing/GfipNetworkSplitter.java
@@ -31,6 +31,7 @@ import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.network.MatsimNetworkReader;
+import org.matsim.core.network.NetworkWriter;
 import org.matsim.core.scenario.ScenarioUtils;
 
 import playground.southafrica.utilities.Header;
@@ -55,33 +56,38 @@ public class GfipNetworkSplitter {
 		
 		/* Parse the original network. */
 		String networkFolder = args[0];
-		String network = args[1];
+		networkFolder += networkFolder.endsWith("/") ? "" : "/";
+		String inputNetwork = args[1];
+		String outputNetwork = args[2];
 		Scenario sc = ScenarioUtils.createScenario(ConfigUtils.createConfig());
-		new MatsimNetworkReader(sc.getNetwork()).parse(networkFolder + (networkFolder.endsWith("/") ? "" : "/") + network);
+		new MatsimNetworkReader(sc.getNetwork()).parse(networkFolder + inputNetwork);
 		
 		List<Double> thresholds = new ArrayList<>();
 		for(int i = 2; i < args.length; i++){
 			thresholds.add(Double.parseDouble(args[i]));
 		}
 
+		Network newNetwork = null;
 		for(double d : thresholds){
-			splitNetwork(sc.getNetwork(), d);
+			newNetwork = splitNetwork(sc.getNetwork(), d);
+			new NetworkWriter(newNetwork).write(networkFolder + outputNetwork);
 		}
 		
 		Header.printFooter();
 	}
 	
 	
-	public static void splitNetwork(Network original, double threshold){
+	public static Network splitNetwork(Network original, double threshold){
 		LOG.info("------------------------------------------------------------------------");
 		LOG.info("Splitting the links of a network: Threshold " + threshold + "...");
 		LOG.info("Original network:");
 		LOG.info("  Nodes: " + original.getNodes().size());
 		LOG.info("  Links: " + original.getLinks().size());
 		
-		LongLinkSplitter.splitNetwork(original, threshold, false);
+		Network newNetwork = LongLinkSplitter.splitNetwork(original, threshold, false);
 		LOG.info("------------------------------------------------------------------------");
 		LOG.info("Done splitting links.");
+		return newNetwork;
 	}
 
 }
