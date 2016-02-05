099916381cbf2919353c4c6e9e03165df5c7e7ea
smetzler <simon.metzler@mailbox.tu-berlin.de>
2016-02-05 16:56:07 +0100

    parsing elevation data works so far

--- playgrounds/smetzler/src/main/java/playground/smetzler/parseElevationData/ParseEleDataFromGeoTiff.java
+++ playgrounds/smetzler/src/main/java/playground/smetzler/parseElevationData/ParseEleDataFromGeoTiff.java
@@ -1,31 +1,37 @@
 package playground.smetzler.parseElevationData;
 
-import java.awt.image.Raster;
-import java.io.File;
-import java.io.IOException;
-
 import org.geotools.coverage.grid.GridCoordinates2D;
 import org.geotools.coverage.grid.GridCoverage2D;
 import org.geotools.coverage.grid.GridGeometry2D;
-import org.geotools.gce.geotiff.GeoTiffFormat;
 import org.geotools.gce.geotiff.GeoTiffReader;
 import org.geotools.geometry.DirectPosition2D;
 
+import java.awt.image.Raster;
+import java.io.File;
+
+//to-do
+// pruefe ausmaße der inputdatei und und gib warning wenn anfragelocation ausserhalb ist
+
 public class ParseEleDataFromGeoTiff {
 
-	    private final static String url = "jdbc:postgresql://localhost/bag";
 	    private static GridCoverage2D grid;
 	    private static Raster gridData;
 
 	 
 	    public static void main(String[] args) throws Exception {
 	        initTif();
-	        System.out.println(getValue(13.45, 52.33));
-//	        loadData();
+	        System.out.println("teufelsberg " + getValue(13.2407,52.4971));
+	        System.out.println("tempelhofer feld " + getValue(13.3989,52.4755));
+	        System.out.println("mueggelsee " + getValue(13.6354,52.4334));
+	        System.out.println("mueggelberg " + getValue(13.64048,52.41594));
+	        
+	        System.out.println("alexanderplatz " + getValue(13.40993,52.52191));	        
+	        System.out.println("der kreuzberg " + getValue(13.379491,52.487610));
+	        
 	    }
-	    
 	 
 	    private static void initTif() throws Exception {
+	    	// download data from http://earthexplorer.usgs.gov/ (login in required)
 	        File tiffFile = new File("../../../../13.Sem - Uni WS 15-16/Masterarbeit/netzwerk/elevation_berlin/n52_e013_1arc_v3.tif");
 	        GeoTiffReader reader = new GeoTiffReader(tiffFile);
 	 
@@ -41,91 +47,16 @@ public class ParseEleDataFromGeoTiff {
 	        GridCoordinates2D posGrid = gg.worldToGrid(posWorld);
 	 
 	        // envelope is the size in the target projection
-	        double[] pixel=new double[1];
+	        double[] pixel= new double[1];
 	        double[] data = gridData.getPixel(posGrid.x, posGrid.y, pixel);
 	        return data[0];
 	    }
-	 
-//	    private static void loadData() throws Exception {
-//	        Connection conn = DriverManager.getConnection(url);
-//	        QueryRunner runner = new QueryRunner();
-//	        final Map<Long, Double> map = new HashMap<Long, Double>();
-//	        ResultSetHandler handler = new ResultSetHandler() {
-//	 
-//	            @Override
-//	            public Object handle(ResultSet resultSet) throws SQLException {
-//	                while (resultSet.next()) {
-//	                    String point = resultSet.getString("point");
-//	                    double x = Double.parseDouble(point.substring(
-//	                            point.indexOf('(') + 1,
-//	                            point.indexOf(' ')
-//	                    ));
-//	 
-//	                    double y = Double.parseDouble(point.substring(
-//	                            point.indexOf(' ') + 1,
-//	                            point.indexOf(')')
-//	                    ));
-//	 
-//	                    try {
-//	                        double hoogte = getValue(x, y);
-//	                        map.put(resultSet.getLong("gid"),hoogte);
-//	                    } catch (Exception e) {
-//	                        e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-//	                    }
-//	                }
-//	                return null;  //To change body of implemented methods use File | Settings | File Templates.
-//	            }
-//	        };
-//	 
-//	        runner.query(conn, "SELECT gid, ST_AsText(ST_Centroid(geovlak)) as point \n" +
-//	                "FROM bag8mrt2014.pand\n" +
-//	                "WHERE geovlak && ST_MakeEnvelope(130153, 408769,132896, 410774, 28992) ORDER by gid ;", handler);
-//	 
-//	        int count = 0;
-//	        for (Long key : map.keySet()) {
-//	 
-//	            System.out.println("Inserting for key = " + key + " value: " + map.get(key));
-//	            int col = runner.update(conn, "UPDATE bag8mrt2014.pand SET hoogte= ? where gid = ?",
-//	                    map.get(key), key);
-//	 
-//	            count++;
-//	 
-//	            if (count%100 == 0) {
-//	                System.out.println("count = " + count);
-//	            }
-//	        }
-//	    
-	    
+	     
 	}
 	
 	
 	
 	
-	
 
-	
-//	public GridCoverage2D getGridCoverage() {
-//	    GeoTiffFormat format = new GeoTiffFormat();
-//	    GeoTiffReader reader = null;
-//	    GridCoverage2D coverage = null;
-//
-//	    String inputTIFF = "../../../../13.Sem - Uni WS 15-16/Masterarbeit/netzwerk/skalitzer/skalitzer_OSM.xml";
-//	    
-//	    try {
-//	        if (inputTIFF == null) {
-//	            throw new RuntimeException("Path not set");
-//	        }
-//	        
-//	        reader = format.getReader(inputTIFF);
-//	        coverage = reader.read(null);
-//	    } catch (IOException e) {
-//	        throw new RuntimeException("Error getting coverage automatically. ", e);
-//	    }
-//
-//	    return coverage;
-//	}
-//	
-//
-//}
 
 
