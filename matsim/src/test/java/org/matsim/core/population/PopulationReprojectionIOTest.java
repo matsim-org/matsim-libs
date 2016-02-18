85eb78dd371beaa20acd99b6be618e15026e415b
Thibaut Dubernet <thibaut.dubernet@ivt.baug.ethz.ch>
2016-02-18 11:40:03 +0100

    MATSIM-374: test config-specified reprojection for population

--- matsim/src/test/java/org/matsim/core/population/PopulationReprojectionIOTest.java
+++ matsim/src/test/java/org/matsim/core/population/PopulationReprojectionIOTest.java
@@ -11,12 +11,15 @@ import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.Plan;
 import org.matsim.api.core.v01.population.Population;
+import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
 import org.matsim.core.network.MatsimNetworkReader;
 import org.matsim.core.router.EmptyStageActivityTypes;
 import org.matsim.core.router.TripStructureUtils;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.geometry.CoordinateTransformation;
+import org.matsim.core.utils.geometry.transformations.TransformationFactory;
 import org.matsim.testcases.MatsimTestUtils;
 
 import java.util.Iterator;
@@ -121,6 +124,110 @@ public class PopulationReprojectionIOTest {
 		new MatsimPopulationReader(reprojectedScenario).readFile(testFile);
 
 		assertPopulationCorrectlyTransformed( originalScenario.getPopulation() , reprojectedScenario.getPopulation() );
+	}
+
+	@Test
+	public void testWithControlerAndConfigParameters() {
+		// accept a rounding error of 1 cm.
+		// this is used both to compare equality and non-equality, so the more we accept difference between input
+		// and output coordinates, the more we require the internally reprojected coordinates to be different.
+		// It is thus OK to use a reasonably "high" tolerance compared to usual double comparisons.
+		final double epsilon = 0.01;
+
+		// read test population
+		final Scenario originalScenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+		// necessary for v4...
+		new MatsimNetworkReader(originalScenario.getNetwork()).readFile(NET_FILE);
+		new MatsimPopulationReader(originalScenario).readFile(BASE_FILE);
+		final Config config = ConfigUtils.createConfig();
+
+		// specify config
+		// need to reproject network as well...
+		config.network().setInputCRS( TransformationFactory.DHDN_GK4 );
+		config.plans().setInputCRS( TransformationFactory.DHDN_GK4 );
+		// web mercator. This would be a pretty silly choice for simulation,
+		// but does not matter for tests. Just makes sure that (almost) every
+		// coordinate can be projected
+		config.global().setCoordinateSystem(  "EPSG:3857" );
+
+		config.network().setInputFile( NET_FILE );
+		config.plans().setInputFile( BASE_FILE );
+
+		// TODO: test also with loading from Controler C'tor?
+		final Scenario scenario = ScenarioUtils.loadScenario( config );
+		for ( Id<Person> id : originalScenario.getPopulation().getPersons().keySet() ) {
+			final Person originalPerson = originalScenario.getPopulation().getPersons().get( id );
+			final Person internalPerson = scenario.getPopulation().getPersons().get( id );
+
+			final List<Activity> originalActivities = TripStructureUtils.getActivities( originalPerson.getSelectedPlan() , EmptyStageActivityTypes.INSTANCE );
+			final List<Activity> reprojectedActivities = TripStructureUtils.getActivities( internalPerson.getSelectedPlan() , EmptyStageActivityTypes.INSTANCE );
+
+			Assert.assertEquals(
+					"unexpected number of activities in reprojected plan",
+					originalActivities.size(),
+					reprojectedActivities.size() );
+
+			final Iterator<Activity> originalIterator = originalActivities.iterator();
+			final Iterator<Activity> reprojectedIterator = reprojectedActivities.iterator();
+
+			while ( originalIterator.hasNext() ) {
+				final Activity o = originalIterator.next();
+				final Activity r = reprojectedIterator.next();
+
+				Assert.assertNotEquals(
+						"No coordinates transform performed!",
+						o.getCoord().getX(),
+						r.getCoord().getX(),
+						epsilon );
+				Assert.assertNotEquals(
+						"No coordinates transform performed!",
+						o.getCoord().getY(),
+						r.getCoord().getY(),
+						epsilon );
+			}
+		}
+
+		// do not perform ANY mobsim run
+		config.controler().setLastIteration( -1 );
+		final String outputDirectory = utils.getOutputDirectory()+"/output/";
+		config.controler().setOutputDirectory( outputDirectory );
+		final Controler controler = new Controler( scenario );
+		controler.run();
+
+		final Scenario dumpedScenario = ScenarioUtils.createScenario( config );
+		new MatsimPopulationReader( dumpedScenario ).readFile( outputDirectory+"/output_plans.xml.gz" );
+
+		for ( Id<Person> id : originalScenario.getPopulation().getPersons().keySet() ) {
+			final Person originalPerson = originalScenario.getPopulation().getPersons().get( id );
+			final Person dumpedPerson = dumpedScenario.getPopulation().getPersons().get( id );
+
+			final List<Activity> originalActivities = TripStructureUtils.getActivities( originalPerson.getSelectedPlan() , EmptyStageActivityTypes.INSTANCE );
+			final List<Activity> reprojectedActivities = TripStructureUtils.getActivities( dumpedPerson.getSelectedPlan() , EmptyStageActivityTypes.INSTANCE );
+
+			Assert.assertEquals(
+					"unexpected number of activities in reprojected plan",
+					originalActivities.size(),
+					reprojectedActivities.size() );
+
+			final Iterator<Activity> originalIterator = originalActivities.iterator();
+			final Iterator<Activity> reprojectedIterator = reprojectedActivities.iterator();
+
+			while ( originalIterator.hasNext() ) {
+				final Activity o = originalIterator.next();
+				final Activity r = reprojectedIterator.next();
+
+				Assert.assertEquals(
+						"coordinates were not reprojected for dump",
+						o.getCoord().getX(),
+						r.getCoord().getX(),
+						epsilon );
+				Assert.assertEquals(
+						"coordinates were not reprojected for dump",
+						o.getCoord().getY(),
+						r.getCoord().getY(),
+						epsilon );
+			}
+		}
 	}
 
 	public void testConversionAtInput(final String inputFile) {
