35e499b812d031521fb787bd43e349ed30a45848
Thibaut Dubernet <thibaut.dubernet@ivt.baug.ethz.ch>
2016-02-18 12:38:07 +0100

    MATSIM-374: test config-specified reprojection for transit schedule

--- matsim/src/test/java/org/matsim/pt/transitSchedule/TransitScheduleReprojectionIOTest.java
+++ matsim/src/test/java/org/matsim/pt/transitSchedule/TransitScheduleReprojectionIOTest.java
@@ -6,10 +6,15 @@ import org.junit.Test;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.geometry.CoordinateTransformation;
+import org.matsim.core.utils.geometry.transformations.TransformationFactory;
+import org.matsim.facilities.ActivityFacility;
 import org.matsim.pt.transitSchedule.api.TransitSchedule;
+import org.matsim.pt.transitSchedule.api.TransitScheduleReader;
 import org.matsim.pt.transitSchedule.api.TransitStopFacility;
 import org.matsim.testcases.MatsimTestUtils;
 
@@ -45,6 +50,71 @@ public class TransitScheduleReprojectionIOTest {
 		new TransitScheduleReaderV1( reprojectedScenario ).readFile( file );
 
 		assertCorrectlyReprojected( originalScenario.getTransitSchedule() , reprojectedScenario.getTransitSchedule() );
+	}
+
+	@Test
+	public void testWithControlerAndConfigParameters() {
+		// accept a rounding error of 1 cm.
+		// this is used both to compare equality and non-equality, so the more we accept difference between input
+		// and output coordinates, the more we require the internally reprojected coordinates to be different.
+		// It is thus OK to use a reasonably "high" tolerance compared to usual double comparisons.
+		final double epsilon = 0.01;
+
+		final Scenario originalScenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+		new TransitScheduleReader( originalScenario ).readFile( TEST_SCHEDULE );
+
+		final Config config = ConfigUtils.createConfig();
+		config.transit().setTransitScheduleFile( TEST_SCHEDULE );
+
+		config.transit().setUseTransit( true );
+		config.transit().setInputScheduleCRS( TransformationFactory.CH1903_LV03_GT );
+		// web mercator. This would be a pretty silly choice for simulation,
+		// but does not matter for tests. Just makes sure that (almost) every
+		// coordinate can be projected
+		config.global().setCoordinateSystem(  "EPSG:3857" );
+
+		// TODO: test also with loading from Controler C'tor?
+		final Scenario scenario = ScenarioUtils.loadScenario( config );
+		for ( Id<TransitStopFacility> id : originalScenario.getTransitSchedule().getFacilities().keySet() ) {
+			final Coord originalCoord = originalScenario.getTransitSchedule().getFacilities().get( id ).getCoord();
+			final Coord internalCoord = scenario.getTransitSchedule().getFacilities().get( id ).getCoord();
+
+			Assert.assertNotEquals(
+					"No coordinates transform performed!",
+					originalCoord.getX(),
+					internalCoord.getX(),
+					epsilon );
+			Assert.assertNotEquals(
+					"No coordinates transform performed!",
+					originalCoord.getY(),
+					internalCoord.getY(),
+					epsilon );
+		}
+
+		config.controler().setLastIteration( -1 );
+		final String outputDirectory = utils.getOutputDirectory()+"/output/";
+		config.controler().setOutputDirectory( outputDirectory );
+		final Controler controler = new Controler( scenario );
+		controler.run();
+
+		final Scenario dumpedScenario = ScenarioUtils.createScenario( config );
+		new TransitScheduleReader( dumpedScenario ).readFile( outputDirectory+"/output_transitSchedule.xml.gz" );
+
+		for ( Id<TransitStopFacility> id : originalScenario.getTransitSchedule().getFacilities().keySet() ) {
+			final Coord originalCoord = originalScenario.getTransitSchedule().getFacilities().get( id ).getCoord();
+			final Coord dumpedCoord = dumpedScenario.getTransitSchedule().getFacilities().get( id ).getCoord();
+
+			Assert.assertEquals(
+					"coordinates were not reprojected for dump",
+					originalCoord.getX(),
+					dumpedCoord.getX(),
+					epsilon );
+			Assert.assertEquals(
+					"coordinates were not reprojected for dump",
+					originalCoord.getY(),
+					dumpedCoord.getY(),
+					epsilon );
+		}
 	}
 
 	private void assertCorrectlyReprojected(
