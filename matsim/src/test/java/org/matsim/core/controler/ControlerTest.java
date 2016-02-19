80bef42b266e07134923c8a7c8d9d10a63e8a42f
Kai Nagel <2008@kainagel.org>
2016-02-19 12:15:58 +0100

    test result depends on a config setting

--- matsim/src/test/java/org/matsim/core/controler/ControlerTest.java
+++ matsim/src/test/java/org/matsim/core/controler/ControlerTest.java
@@ -177,9 +177,14 @@ public class ControlerTest {
 		controler.run();
 
 		// test if we got the right result
-		// the actual result is 151sec, not 150, as each vehicle "loses" 1sec in the buffer
-		assertEquals("TravelTimeCalculator has wrong result",
-				151.0, controler.getLinkTravelTimes().getLinkTravelTime(f.link2, 7*3600, null, null), 0.0);
+		if ( config.qsim().isUsingFastCapacityUpdate() ) {
+			// the actual result is 151sec, not 150, as each vehicle "loses" 1sec in the buffer
+			assertEquals("TravelTimeCalculator has wrong result",
+					150.5, controler.getLinkTravelTimes().getLinkTravelTime(f.link2, 7*3600, null, null), 0.0);
+		} else {
+			assertEquals("TravelTimeCalculator has wrong result",
+					151.0, controler.getLinkTravelTimes().getLinkTravelTime(f.link2, 7*3600, null, null), 0.0);
+		}
 
 		// now test that the ReRoute-Strategy also knows about these travel times...
 		config.controler().setLastIteration(1);
@@ -195,8 +200,13 @@ public class ControlerTest {
 		controler.run();
 
 		// test that the plans have the correct times
-		assertEquals("ReRoute seems to have wrong travel times.",
-				151.0, ((Leg) (person1.getPlans().get(1).getPlanElements().get(1))).getTravelTime(), 0.0);
+		if ( config.qsim().isUsingFastCapacityUpdate() ) {
+			assertEquals("ReRoute seems to have wrong travel times.",
+					150.0, ((Leg) (person1.getPlans().get(1).getPlanElements().get(1))).getTravelTime(), 0.0);
+		} else {
+			assertEquals("ReRoute seems to have wrong travel times.",
+					151.0, ((Leg) (person1.getPlans().get(1).getPlanElements().get(1))).getTravelTime(), 0.0);
+		}
 	}
 
 	/**
