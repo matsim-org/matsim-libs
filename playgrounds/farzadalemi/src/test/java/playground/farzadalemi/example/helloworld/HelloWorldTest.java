68f7882c23a933a8de6b335ca2b6ab67bf028491
boescpa <patrick.boesch@ivt.baug.ethz.ch>
2016-02-19 16:35:29 +0100

    Network converter template...

--- playgrounds/farzadalemi/src/test/java/playground/farzadalemi/example/helloworld/HelloWorldTest.java
+++ playgrounds/farzadalemi/src/test/java/playground/farzadalemi/example/helloworld/HelloWorldTest.java
@@ -16,7 +16,7 @@
  *   See also COPYING, LICENSE and WARRANTY file                           *
  *                                                                         *
  * *********************************************************************** */
-package org.matsim.example;
+package playground.farzadalemi.example.helloworld;
 
 import org.apache.log4j.Logger;
 import org.junit.Assert;
