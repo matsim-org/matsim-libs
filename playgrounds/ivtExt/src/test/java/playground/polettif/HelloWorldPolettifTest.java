f86b620c47b6a439b43b3e02c8c5c5131718a74d
polettif <polettif@student.ethz.ch>
2016-02-24 16:05:25 +0100

    examples for polettif

---
+++ playgrounds/ivtExt/src/test/java/playground/polettif/HelloWorldPolettifTest.java
@@ -0,0 +1,56 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2016 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+
+package playground.polettif;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.OutputDirectoryHierarchy;
+import org.matsim.core.scenario.ScenarioUtils;
+
+/**
+ * Created by polettif on 24.02.2016.
+ */
+public class HelloWorldPolettifTest {
+	
+	@Test
+	public final void testMain() {
+		try {
+			Config config = ConfigUtils.createConfig() ;
+			config.controler().setLastIteration(1);
+			config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
+
+			Scenario scenario = ScenarioUtils.loadScenario(config) ;
+
+			Controler controler = new Controler( scenario ) ;
+
+			controler.run();
+		} catch ( Exception ee ) {
+			Logger.getLogger(this.getClass()).fatal("there was an exception: \n" + ee ) ;
+
+			// if one catches an exception, then one needs to explicitly fail the test:
+			Assert.fail();
+		}
+	}
+}
