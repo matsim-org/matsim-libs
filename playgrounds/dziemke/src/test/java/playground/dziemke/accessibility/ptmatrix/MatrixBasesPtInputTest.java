5da462d5b7643d6c8289fdf21846d95ad5cfe36f
gabrielthunig <gabriel.thunig@web.de>
2016-02-25 15:16:33 +0100

    refactored "calcDistance" added a new link with the depending nodes to the pt network and cloned it from matsim-core adjusted the test

--- playgrounds/dziemke/src/test/java/playground/dziemke/accessibility/ptmatrix/MatrixBasesPtInputTest.java
+++ playgrounds/dziemke/src/test/java/playground/dziemke/accessibility/ptmatrix/MatrixBasesPtInputTest.java
@@ -35,17 +35,14 @@ import org.matsim.pt.transitSchedule.api.TransitScheduleReader;
 import org.matsim.pt.transitSchedule.api.TransitStopFacility;
 import org.matsim.testcases.MatsimTestUtils;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @author dziemke
+ * @author dziemke, gthunig
  */
 public class MatrixBasesPtInputTest {
 
@@ -56,8 +53,10 @@ public class MatrixBasesPtInputTest {
 
 	@Test
 	public final void test() {
-		String transitScheduleFile = "../../matsim/examples/pt-tutorial/transitschedule.xml";
-		String networkFile = "../../matsim/examples/pt-tutorial/multimodalnetwork.xml";
+//		String transitScheduleFile = "../../matsim/examples/pt-tutorial/transitschedule.xml";
+		String transitScheduleFile = "examples/pt-tutorial/transitschedule.xml";
+//		String /networkFile = "../../matsim/examples/pt-tutorial/multimodalnetwork.xml";
+		String networkFile = "examples/pt-tutorial/multimodalnetwork.xml";
 		String outputRoot = utils.getOutputDirectory();
         System.out.println("outputRoot = " + outputRoot);
 
@@ -107,6 +106,8 @@ public class MatrixBasesPtInputTest {
         ptStopsCompare.add(new String[]{"2b", "2050.0", "2960.0"});
         ptStopsCompare.add(new String[]{"1", "1050.0", "1050.0"});
         ptStopsCompare.add(new String[]{"3", "3950.0", "1050.0"});
+        ptStopsCompare.add(new String[]{"4", "2050.0", "3050.0"});
+        ptStopsCompare.add(new String[]{"5" ,"2050.0", "3950.0"});
         ptStopsCompare.add(new String[]{"2a", "2050.0", "2940.0"});
         Assert.assertNotNull(ptStops);
         for (String[] line : ptStops) {
@@ -123,18 +124,38 @@ public class MatrixBasesPtInputTest {
         tdmCompare.add(new String[]{"2b", "2b", "0.0"});
         tdmCompare.add(new String[]{"2b", "1", "2413.0"});
         tdmCompare.add(new String[]{"2b", "3", "3639.0"});
+        tdmCompare.add(new String[]{"2b", "4", "117.0"});
+        tdmCompare.add(new String[]{"2b", "5", "1017.0"});
         tdmCompare.add(new String[]{"2b", "2a", "26.0"});
         tdmCompare.add(new String[]{"1", "2b", "2805.721029168215"});
         tdmCompare.add(new String[]{"1", "1", "0.0"});
         tdmCompare.add(new String[]{"1", "3", "6000.0"});
+        tdmCompare.add(new String[]{"1", "4", "2922.721029168215"});
+        tdmCompare.add(new String[]{"1", "5", "3822.721029168215"});
         tdmCompare.add(new String[]{"1", "2a", "2779.721029168215"});
         tdmCompare.add(new String[]{"3", "2b", "3502.31195069771"});
         tdmCompare.add(new String[]{"3", "1", "6000.0"});
         tdmCompare.add(new String[]{"3", "3", "0.0"});
+        tdmCompare.add(new String[]{"3", "4", "3619.31195069771"});
+        tdmCompare.add(new String[]{"3", "5", "4519.31195069771"});
         tdmCompare.add(new String[]{"3", "2a", "3528.31195069771"});
+        tdmCompare.add(new String[]{"4", "2b", "117.0"});
+        tdmCompare.add(new String[]{"4", "1", "2530.0"});
+        tdmCompare.add(new String[]{"4", "3", "3756.0"});
+        tdmCompare.add(new String[]{"4", "4", "0.0"});
+        tdmCompare.add(new String[]{"4", "5", "900.0"});
+        tdmCompare.add(new String[]{"4", "2a", "143.0"});
+        tdmCompare.add(new String[]{"5", "2b", "1017.0"});
+        tdmCompare.add(new String[]{"5", "1", "3430.0"});
+        tdmCompare.add(new String[]{"5", "3", "4656.0"});
+        tdmCompare.add(new String[]{"5", "4", "900.0"});
+        tdmCompare.add(new String[]{"5", "5", "0.0"});
+        tdmCompare.add(new String[]{"5", "2a", "1043.0"});
         tdmCompare.add(new String[]{"2a", "2b", "26.0"});
         tdmCompare.add(new String[]{"2a", "1", "2439.0"});
         tdmCompare.add(new String[]{"2a", "3", "3613.0"});
+        tdmCompare.add(new String[]{"2a", "4", "143.0"});
+        tdmCompare.add(new String[]{"2a", "5", "1043.0"});
         tdmCompare.add(new String[]{"2a", "2a", "0.0"});
         Assert.assertNotNull(tdm);
         for (String[] line : tdm) {
@@ -151,18 +172,38 @@ public class MatrixBasesPtInputTest {
         ttmCompare.add(new String[]{"2b", "2b", "0.0"});
         ttmCompare.add(new String[]{"2b", "1", "540.0"});
         ttmCompare.add(new String[]{"2b", "3", "539.9999999999993"});
+        ttmCompare.add(new String[]{"2b", "4", "140.4"});
+        ttmCompare.add(new String[]{"2b", "5", "1079.9999999999986"});
         ttmCompare.add(new String[]{"2b", "2a", "31.200000000000003"});
         ttmCompare.add(new String[]{"1", "2b", "231.2"});
         ttmCompare.add(new String[]{"1", "1", "0.0"});
         ttmCompare.add(new String[]{"1", "3", "540.0"});
+        ttmCompare.add(new String[]{"1", "4", "371.6"});
+        ttmCompare.add(new String[]{"1", "5", "1080.0000000000014"});
         ttmCompare.add(new String[]{"1", "2a", "200.0"});
         ttmCompare.add(new String[]{"3", "2b", "300.0"});
         ttmCompare.add(new String[]{"3", "1", "540.0"});
         ttmCompare.add(new String[]{"3", "3", "0.0"});
+        ttmCompare.add(new String[]{"3", "4", "440.4"});
+        ttmCompare.add(new String[]{"3", "5", "1079.9999999999986"});
         ttmCompare.add(new String[]{"3", "2a", "331.2"});
+        ttmCompare.add(new String[]{"4", "2b", "140.4"});
+        ttmCompare.add(new String[]{"4", "1", "539.9999999999985"});
+        ttmCompare.add(new String[]{"4", "3", "540.0000000000015"});
+        ttmCompare.add(new String[]{"4", "4", "0.0"});
+        ttmCompare.add(new String[]{"4", "5", "180.0"});
+        ttmCompare.add(new String[]{"4", "2a","171.60000000000002"});
+        ttmCompare.add(new String[]{"5", "2b", "530.4"});
+        ttmCompare.add(new String[]{"5", "1", "1439.9999999999986"});
+        ttmCompare.add(new String[]{"5", "3", "1440.0000000000014"});
+        ttmCompare.add(new String[]{"5", "4", "390.0"});
+        ttmCompare.add(new String[]{"5", "5", "0.0"});
+        ttmCompare.add(new String[]{"5", "2a", "561.6"});
         ttmCompare.add(new String[]{"2a", "2b", "31.200000000000003"});
         ttmCompare.add(new String[]{"2a", "1", "539.9999999999993"});
         ttmCompare.add(new String[]{"2a", "3", "540.0"});
+        ttmCompare.add(new String[]{"2a", "4", "171.60000000000002"});
+        ttmCompare.add(new String[]{"2a", "5", "1080.0000000000014"});
         ttmCompare.add(new String[]{"2a", "2a", "0.0"});
         Assert.assertNotNull(ttm);
         for (String[] line : ttm) {
