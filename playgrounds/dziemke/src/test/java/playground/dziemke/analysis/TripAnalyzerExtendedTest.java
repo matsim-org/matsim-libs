4c0be922ca18db02d043f5ee0cde3459aff7f5e9
gabriel <gabriel.thunig@web.de>
2016-03-30 15:31:04 +0200

    created TripAnalyzerExtendedTest

---
+++ playgrounds/dziemke/src/test/java/playground/dziemke/analysis/TripAnalyzerExtendedTest.java
@@ -0,0 +1,22 @@
+package playground.dziemke.analysis;
+
+import org.apache.log4j.Logger;
+import org.junit.Rule;
+import org.junit.Test;
+import org.matsim.testcases.MatsimTestUtils;
+
+/**
+ * @author gthunig
+ */
+public class TripAnalyzerExtendedTest {
+
+    @Rule
+    public MatsimTestUtils utils = new MatsimTestUtils();
+
+    private static final Logger log = Logger.getLogger(TripAnalyzerExtendedTest.class);
+
+    @Test
+    public final void testTripAnalyzerExtended() {
+
+    }
+}
