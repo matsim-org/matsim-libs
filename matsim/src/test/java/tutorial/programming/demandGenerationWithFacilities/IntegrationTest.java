2d5f6e6da92e7cf9061590067dc567e40fa12fa1
Kai Nagel <2008@kainagel.org>
2016-04-01 12:57:31 +0200

    modernize an Assert import

--- matsim/src/test/java/tutorial/programming/demandGenerationWithFacilities/IntegrationTest.java
+++ matsim/src/test/java/tutorial/programming/demandGenerationWithFacilities/IntegrationTest.java
@@ -18,8 +18,7 @@
  * *********************************************************************** */
 package tutorial.programming.demandGenerationWithFacilities;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.matsim.testcases.MatsimTestUtils;
