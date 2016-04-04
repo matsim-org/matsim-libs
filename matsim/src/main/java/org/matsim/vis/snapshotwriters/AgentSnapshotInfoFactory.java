974d84b22194a689603dac7681044b029e316923
Kai Nagel <2008@kainagel.org>
2016-04-04 13:16:36 +0200

    remove a class from public scope by nesting it into another class that was the sole user of it

--- matsim/src/main/java/org/matsim/vis/snapshotwriters/AgentSnapshotInfoFactory.java
+++ matsim/src/main/java/org/matsim/vis/snapshotwriters/AgentSnapshotInfoFactory.java
@@ -25,6 +25,7 @@ import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.gbl.Gbl;
+import org.matsim.vis.snapshotwriters.AgentSnapshotInfo.AgentState;
 
 /**
  * translation of physical position (e.g. odometer distance on link, lane) into visualization position
@@ -138,6 +139,103 @@ public class AgentSnapshotInfoFactory {
 				- Math.cos(theta) * lanePosition  );
 		
 		info.setAzimuth( theta / TWO_PI * 360. ) ;
+	}
+
+	/**
+	 * A helper class to store information about agents (id, position, speed), mainly used to create
+	 * {@link SnapshotWriter snapshots}.  It also provides a way to convert graph coordinates (linkId, offset) into
+	 * Euclidean coordinates.  Also does some additional coordinate shifting (e.g. to the "right") to improve visualization.
+	 * In contrast to earlier versions of this comment, it does _not_ define a physical position of particles in the queue model;
+	 * that functionality needs to be provided elsewhere.
+	 *
+	 * @author mrieser, knagel
+	 */
+	 private static class PositionInfo implements AgentSnapshotInfo {
+
+		private Id<Person> agentId = null;
+		private double easting = Double.NaN;
+		private double northing = Double.NaN;
+		private double azimuth = Double.NaN;
+		private double colorValue = 0;
+		private AgentState agentState = null;
+		private Id<Link> linkId = null;
+		private int user = 0;
+
+		/* package-private */ PositionInfo() { }
+
+		@Override
+		public final Id<Person> getId() {
+			return this.agentId;
+		}
+		public final void setId( Id<Person> tmp ) {
+			this.agentId = tmp ;
+		}
+
+		@Override
+		public final double getEasting() {
+			return this.easting;
+		}
+		public final void setEasting( double tmp ) {
+			this.easting = tmp ;
+		}
+
+		@Override
+		public final double getNorthing() {
+			return this.northing;
+		}
+		public final void setNorthing( double tmp ) {
+			this.northing = tmp ;
+		}
+
+		@Override
+		public final double getAzimuth() {
+			return this.azimuth;
+		}
+		public final void setAzimuth( double tmp ) {
+			this.azimuth = tmp ;
+		}
+
+		@Override
+		public final double getColorValueBetweenZeroAndOne() {
+			return this.colorValue;
+		}
+		@Override
+		public final void setColorValueBetweenZeroAndOne( double tmp ) {
+			this.colorValue = tmp ;
+		}
+
+		@Override
+		public final AgentState getAgentState(){
+			return this.agentState;
+		}
+		@Override
+		public final void setAgentState( AgentState state ) {
+			this.agentState = state ;
+		}
+
+		public final Id<Link> getLinkId() {
+			return this.linkId;
+		}
+		public final void setLinkId( Id<Link> tmp ) {
+			this.linkId = tmp ;
+		}
+
+		@Override
+		public int getUserDefined() {
+			return this.user;
+		}
+		@Override
+		public void setUserDefined( int tmp ) {
+			this.user = tmp ;
+		}
+
+		@Override
+		public String toString() {
+			return "PositionInfo; agentId: " + this.agentId.toString()
+			+ " easting: " + this.easting
+			+ " northing: " + this.northing ;
+		}
+
 	}
 
 }
