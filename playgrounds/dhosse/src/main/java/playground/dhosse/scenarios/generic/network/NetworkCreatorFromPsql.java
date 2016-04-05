8a94269a3722a00c6473af7a2165beb42ef7a3ab
dhosse <dhosse08@gmail.com>
2016-04-05 12:08:58 +0200

    changes after a first test run

--- playgrounds/dhosse/src/main/java/playground/dhosse/scenarios/generic/network/NetworkCreatorFromPsql.java
+++ playgrounds/dhosse/src/main/java/playground/dhosse/scenarios/generic/network/NetworkCreatorFromPsql.java
@@ -11,13 +11,17 @@ import java.util.Map;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.network.Node;
 import org.matsim.core.network.LinkImpl;
+import org.matsim.core.network.NetworkImpl;
 import org.matsim.core.network.NetworkWriter;
+import org.matsim.core.network.algorithms.NetworkCleaner;
+import org.matsim.core.network.algorithms.NetworkSimplifier;
 import org.matsim.core.utils.geometry.CoordUtils;
 import org.matsim.core.utils.geometry.CoordinateTransformation;
 import org.matsim.core.utils.geometry.geotools.MGC;
@@ -48,8 +52,8 @@ public class NetworkCreatorFromPsql {
 	private static final String TAG_HIGHWAY = "highway";
 	private static final String TAG_ID = "osm_id";
 	private static final String TAG_JUNCTION = "junction";
-	private static final String TAG_LANES = "lanes";
-	private static final String TAG_MAXSPEED = "maxspeed";
+//	private static final String TAG_LANES = "lanes"; TODO
+//	private static final String TAG_MAXSPEED = "maxspeed"; TODO
 	private static final String TAG_ONEWAY = "oneway";
 
 	private static final String MOTORWAY = "motorway";
@@ -71,27 +75,44 @@ public class NetworkCreatorFromPsql {
 	private final Scenario scenario;
 	private final CoordinateTransformation transform;
 	private final Configuration configuration;
-	private final boolean useDefaults;
 	private final boolean scaleMaxSpeed;
+	private final boolean cleanNetwork;
+	private final boolean simplifyNetworK;
 	
 	//TODO what can you modify?
 	static enum modification{};
 
-	public NetworkCreatorFromPsql(final Scenario scenario, Configuration configuration, boolean useHighwayDefaults, boolean scaleMaxSpeed){
+	public NetworkCreatorFromPsql(final Scenario scenario, Configuration configuration, boolean scaleMaxSpeed){
+		
+		this(scenario, configuration, scaleMaxSpeed, false, false);
+		
+	};
+	
+	public NetworkCreatorFromPsql(final Scenario scenario, Configuration configuration, boolean scaleMaxSpeed, boolean cleanNetwork){
+		
+		this(scenario, configuration, scaleMaxSpeed, cleanNetwork, false);
+		
+	}
+	
+	public NetworkCreatorFromPsql(final Scenario scenario, Configuration configuration, boolean scaleMaxSpeed, boolean cleanNetwork,
+			boolean simplifyNetwork){
 		
 		this.scenario = scenario;
 		this.transform = TransformationFactory.getCoordinateTransformation(TransformationFactory.WGS84, configuration.getCrs());
 		this.configuration = configuration;
-		this.useDefaults = useHighwayDefaults;
 		this.scaleMaxSpeed = scaleMaxSpeed;
+		this.cleanNetwork = cleanNetwork;
+		this.simplifyNetworK = simplifyNetwork;
 		
-	};
+	}
+	
+	
 	
 	private Map<String, HighwayDefaults> highwayDefaults = new HashMap<String, HighwayDefaults>();
 	
 	public void create(){
 		
-		if(useDefaults || this.highwayDefaults.size() < 1){
+		if(this.highwayDefaults.size() < 1){
 			
 			this.setHighwayDefaults(MOTORWAY, 1.0, 120/3.6, 1.0, 2000.0, true);
 			this.setHighwayDefaults(MOTORWAY_LINK, 1,  80.0/3.6, 1.0, 1500, true);
@@ -123,10 +144,10 @@ public class NetworkCreatorFromPsql {
 			if(connection != null){
 				
 				log.info("Connection establised.");
-				
+	
 				Statement statement = connection.createStatement();
-				ResultSet result = statement.executeQuery("select osm_id, access, highway, junction, lanes, maxspeed, oneway,"
-						+ " st_astext(way) from osm.osm_line");
+				ResultSet result = statement.executeQuery("select osm_id, access, highway, junction, oneway,"
+						+ " st_astext(way) from osm.osm_line;");
 				
 				while(result.next()){
 					
@@ -135,8 +156,8 @@ public class NetworkCreatorFromPsql {
 					entry.accessTag = result.getString(TAG_ACCESS);
 					entry.highwayTag = result.getString(TAG_HIGHWAY);
 					entry.junctionTag = result.getString(TAG_JUNCTION);
-					entry.lanesTag = result.getString(TAG_LANES);
-					entry.maxspeedTag = result.getString(TAG_MAXSPEED);
+//					entry.lanesTag = result.getString(TAG_LANES);
+//					entry.maxspeedTag = result.getString(TAG_MAXSPEED);
 					entry.onewayTag = result.getString(TAG_ONEWAY);
 					entry.geometry = wktReader.read(result.getString(TAG_GEOMETRY));
 					wayEntries.add(entry);
@@ -152,7 +173,19 @@ public class NetworkCreatorFromPsql {
 			
 			processWayEntries(wayEntries);
 			
-			new NetworkWriter(scenario.getNetwork()).write(configuration.getWorkingDirectory() + "network.xml.gz");
+			if(this.simplifyNetworK){
+				
+				new NetworkSimplifier().run(scenario.getNetwork());
+				
+			}
+			
+			if(this.cleanNetwork){
+			
+				new NetworkCleaner().run(scenario.getNetwork());
+				
+			}
+			
+			new NetworkWriter(scenario.getNetwork()).write(configuration.getWorkingDirectory() + "network_cleaned.xml.gz");
 			
 		} catch (InstantiationException | IllegalAccessException
 				| ClassNotFoundException | SQLException | ParseException e) {
@@ -200,25 +233,20 @@ public class NetworkCreatorFromPsql {
 			if(coordinates.length > 1){
 				
 				Coordinate from = coordinates[0];
-				Coordinate to = coordinates[coordinates.length - 1];
+//				Coordinate to = coordinates[coordinates.length - 1];
 				double length = 0.;
 				Coordinate lastTo = from;
 				
-				for(int i = 0; i < coordinates.length; i++){
+				for(int i = 1; i < coordinates.length; i++){
 					
 					Coordinate next = coordinates[i];
 					
-					if(!next.equals(to)){
-						
-						length += CoordUtils.calcEuclideanDistance(MGC.coordinate2Coord(lastTo), MGC.coordinate2Coord(next));
-						
-					}
+					length = CoordUtils.calcEuclideanDistance(MGC.coordinate2Coord(lastTo), MGC.coordinate2Coord(next));
+					createLink(entry, length, lastTo, next);
 					
 					lastTo = next;
 					
 				}
-				
-				createLink(entry, length);
 				
 			}
 			
@@ -230,10 +258,9 @@ public class NetworkCreatorFromPsql {
 		log.info("MATSim links: " + scenario.getNetwork().getLinks().size());
 		
 	}
-
-	//basically a copy from OsmNetworkReader
-	private void createLink(WayEntry entry, double length){
 	
+	private void createLink(WayEntry entry, double length, Coordinate from, Coordinate to){
+		
 		HighwayDefaults defaults = this.highwayDefaults.get(entry.highwayTag);
 		
 		//if there are defaults for the highway type of the current way, we proceed
@@ -290,7 +317,8 @@ public class NetworkCreatorFromPsql {
 				
 			}
 			
-			if(entry.highwayTag.equalsIgnoreCase("trunk") || entry.highwayTag.equalsIgnoreCase("primary") || entry.highwayTag.equalsIgnoreCase("secondary")){
+			if(entry.highwayTag.equalsIgnoreCase("trunk") || entry.highwayTag.equalsIgnoreCase("primary") ||
+					entry.highwayTag.equalsIgnoreCase("secondary")){
 	            
 				if((oneway || onewayReverse) && lanesPerDirection == 1.0){
 	            
@@ -330,16 +358,15 @@ public class NetworkCreatorFromPsql {
 				freespeed *= freespeedFactor;
 				
 			}
+
+			Coord fromCoord = this.transform.transform(MGC.coordinate2Coord(from));
+			Node closestFromNode = ((NetworkImpl)scenario.getNetwork()).getNearestNode(fromCoord);
+			Node fromNode = setNode(fromCoord, closestFromNode);
 			
-			Node fromNode = scenario.getNetwork().getFactory().createNode(Id.createNodeId(nodeCounter),
-					this.transform.transform(MGC.coordinate2Coord(entry.geometry.getCoordinates()[0])));
-			scenario.getNetwork().addNode(fromNode);
-			nodeCounter++;
+			Coord toCoord = this.transform.transform(MGC.coordinate2Coord(to));
+			Node closestToNode = ((NetworkImpl)scenario.getNetwork()).getNearestNode(toCoord);
 			
-			Node toNode = scenario.getNetwork().getFactory().createNode(Id.createNodeId(nodeCounter),
-					this.transform.transform(MGC.coordinate2Coord(entry.geometry.getCoordinates()[entry.geometry.getCoordinates().length - 1])));
-			scenario.getNetwork().addNode(toNode);
-			nodeCounter++;
+			Node toNode = setNode(toCoord, closestToNode);
 			
 			String origId = entry.osmId;
 			
@@ -384,6 +411,189 @@ public class NetworkCreatorFromPsql {
 			}
 			
 		}
+		
+	}
+
+	//basically a copy from OsmNetworkReader
+//	private void createLink(WayEntry entry, double length){
+//	
+//		HighwayDefaults defaults = this.highwayDefaults.get(entry.highwayTag);
+//		
+//		//if there are defaults for the highway type of the current way, we proceed
+//		//else the way is simply skipped
+//		if(defaults != null){
+//			
+//			//set all values to default
+//			double freespeed = defaults.freespeed;
+//			double freespeedFactor = defaults.freespeedFactor;
+//			double lanesPerDirection = defaults.lanesPerDirection;
+//			double laneCapacity = defaults.laneCapacity;
+//			boolean oneway = defaults.oneway;
+//			boolean onewayReverse = false;
+//
+//			//freespeed tag
+//			String freespeedTag = entry.maxspeedTag;
+//			if(freespeedTag != null){
+//				
+//				try{
+//					
+//					freespeed = Double.parseDouble(freespeedTag) / 3.6;
+//					
+//				} catch(NumberFormatException e){
+//					
+//					e.printStackTrace();
+//					
+//				}
+//				
+//			}
+//			
+//			if("roundabout".equals(entry.junctionTag)){
+//				
+//				oneway = true;
+//				
+//			}
+//			
+//			//oneway tag
+//			if(entry.onewayTag != null){
+//
+//				if(entry.onewayTag.equals("yes") || entry.onewayTag.equals("true") || entry.onewayTag.equals("1")){
+//					
+//					oneway = true;
+//					
+//				} else if(entry.onewayTag.equals("no")){
+//					
+//					oneway = false;
+//					
+//				} else if(entry.onewayTag.equals("-1")){
+//					
+//					onewayReverse = true;
+//					oneway = false;
+//					
+//				}
+//				
+//			}
+//			
+//			if(entry.highwayTag.equalsIgnoreCase("trunk") || entry.highwayTag.equalsIgnoreCase("primary") ||
+//					entry.highwayTag.equalsIgnoreCase("secondary")){
+//	            
+//				if((oneway || onewayReverse) && lanesPerDirection == 1.0){
+//	            
+//					lanesPerDirection = 2.0;
+//					
+//	            }
+//				
+//			}
+//			
+//			String lanesTag = entry.lanesTag;
+//			if(lanesTag != null){
+//				
+//				try {
+//					double totalNofLanes = Double.parseDouble(lanesTag);
+//					if (totalNofLanes > 0) {
+//						lanesPerDirection = totalNofLanes;
+//
+//			            if (!oneway && !onewayReverse) {
+//			            	
+//			                lanesPerDirection /= 2.;
+//			                
+//			            }
+//					}
+//				
+//				} catch (Exception e) {
+//					
+//					e.printStackTrace();
+//				
+//				}
+//				
+//			}
+//			
+//			double capacity = lanesPerDirection * laneCapacity;
+//			
+//			if(this.scaleMaxSpeed){
+//				
+//				freespeed *= freespeedFactor;
+//				
+//			}
+//
+//			Coord from = this.transform.transform(MGC.coordinate2Coord(entry.geometry.getCoordinates()[0]));
+//			Node closestFromNode = ((NetworkImpl)scenario.getNetwork()).getNearestNode(from);
+//			Node fromNode = setNode(from, closestFromNode);
+//			
+//			Coord to = this.transform.transform(MGC.coordinate2Coord(entry.geometry.getCoordinates()[entry.geometry.getCoordinates().length - 1]));
+//			Node closestToNode = ((NetworkImpl)scenario.getNetwork()).getNearestNode(to);
+//			
+//			Node toNode = setNode(to, closestToNode);
+//			
+//			String origId = entry.osmId;
+//			
+//			if(!onewayReverse){
+//				
+//				Link link = scenario.getNetwork().getFactory().createLink(Id.createLinkId(linkCounter), fromNode, toNode);
+//				link.setCapacity(capacity);
+//				link.setFreespeed(freespeed);
+//				link.setLength(length);
+//				link.setNumberOfLanes(lanesPerDirection);
+//				
+//				if(link instanceof LinkImpl){
+//					
+//					((LinkImpl)link).setOrigId(origId);
+//					((LinkImpl)link).setType(entry.highwayTag);
+//					
+//				}
+//				
+//				scenario.getNetwork().addLink(link);
+//				linkCounter++;
+//				
+//			}
+//			
+//			if(!oneway){
+//				
+//				Link link = scenario.getNetwork().getFactory().createLink(Id.createLinkId(linkCounter), toNode, fromNode);
+//				link.setCapacity(capacity);
+//				link.setFreespeed(freespeed);
+//				link.setLength(length);
+//				link.setNumberOfLanes(lanesPerDirection);
+//				
+//				if(link instanceof LinkImpl){
+//					
+//					((LinkImpl)link).setOrigId(origId);
+//					((LinkImpl)link).setType(entry.highwayTag);
+//					
+//				}
+//				
+//				scenario.getNetwork().addLink(link);
+//				linkCounter++;
+//				
+//			}
+//			
+//		}
+//		
+//	}
+	
+	private Node setNode(Coord coord, Node closestNode){
+		
+		Node node = null;
+		
+		if(closestNode != null){
+		
+			if(CoordUtils.calcEuclideanDistance(coord, closestNode.getCoord()) == 0){
+				
+				node = closestNode;
+				
+				
+			} else {
+				node = scenario.getNetwork().getFactory().createNode(Id.createNodeId(nodeCounter), coord);
+				scenario.getNetwork().addNode(node);
+				nodeCounter++;
+			}
+			
+		} else {
+			node = scenario.getNetwork().getFactory().createNode(Id.createNodeId(nodeCounter), coord);
+			scenario.getNetwork().addNode(node);
+			nodeCounter++;
+		}
+		
+		return node;
 		
 	}
 
