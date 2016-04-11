b5b776f9d8926c95a35856e1a9bd3f6a2822a463
wrashid <sfA1kewo>
2016-04-11 16:32:22 -0700

    renaming variable

--- contribs/parking/src/main/java/org/matsim/contrib/parking/lib/obj/SortableMapObject.java
+++ contribs/parking/src/main/java/org/matsim/contrib/parking/lib/obj/SortableMapObject.java
@@ -3,33 +3,33 @@ package org.matsim.contrib.parking.lib.obj;
 public class SortableMapObject<Key> implements Comparable<SortableMapObject> {
 
 	private final Key key;
-	private final double score;
+	private final double weight;
 
 	/**
-	 * attention: it gives you the smaller number. if you are using negative
-	 * scores and need the best score, use multiply by -1.0, before insertion
+	 * attention: it gives you the smaller number. If you are using negative
+	 * weights and need the best score, use multiply by -1.0, before insertion
 	 * 
 	 * @param key
-	 * @param score
+	 * @param weight
 	 */
-	public SortableMapObject(Key key, double score) {
+	public SortableMapObject(Key key, double weight) {
 		this.key = key;
-		this.score = score;
+		this.weight = weight;
 	}
 
 	@Override
 	public int compareTo(SortableMapObject other) {
-		if (getScore() > other.getScore())
+		if (getWeight() > other.getWeight())
 			return 1;
 
-		if (getScore() < other.getScore())
+		if (getWeight() < other.getWeight())
 			return -1;
 
 		return 0;
 	}
 
-	public double getScore() {
-		return score;
+	public double getWeight() {
+		return weight;
 	}
 
 	public Key getKey() {
