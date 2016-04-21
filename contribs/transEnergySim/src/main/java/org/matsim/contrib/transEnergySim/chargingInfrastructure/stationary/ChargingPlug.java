7b57e30c274430be9a9b72290cc31eb5c6bac053
wrashid <sfA1kewo>
2016-04-21 14:21:12 -0700

    moving methods between interfaces charging infrastructure (refactoring)

--- contribs/transEnergySim/src/main/java/org/matsim/contrib/transEnergySim/chargingInfrastructure/stationary/ChargingPlug.java
+++ contribs/transEnergySim/src/main/java/org/matsim/contrib/transEnergySim/chargingInfrastructure/stationary/ChargingPlug.java
@@ -17,19 +17,11 @@ public interface ChargingPlug extends Identifiable<ChargingPlug> {
 	ChargingPlugStatus getChargingPlugStatus();
 	
 	Collection<ChargingLevel> getAvailableChargingLevels();
-	
-	// We assume that one charging plug can potentially serve multiple parking spots located adjacent to it. But only one parked car at a time can use it.
-	// For example in the scenarios it could be assumed that the charger is released when charging is finished (e.g. electronic unlock - chargingPlugStatus=AVAILABLE).
-	double getNumberOfAvailableParkingSpots();
 		
 	void plugVehicle(VehicleWithBattery vehicle, double time);
 	void unPlugVehicle(VehicleWithBattery vehicle, double time);
-	
-	void registerVehicleArrival(double time, Id<Vehicle> vehicleId); 
-	void registerVehicleDeparture(double time, Id<Vehicle> vehicleId);
+
 	
 	void selectChargingLevel(ChargingLevel chargingLevel);
-	
-
 	
 }
--- contribs/transEnergySim/src/main/java/org/matsim/contrib/transEnergySim/chargingInfrastructure/stationary/ChargingPoint.java
+++ contribs/transEnergySim/src/main/java/org/matsim/contrib/transEnergySim/chargingInfrastructure/stationary/ChargingPoint.java
@@ -3,6 +3,7 @@ package org.matsim.contrib.transEnergySim.chargingInfrastructure.stationary;
 import java.util.Collection;
 
 import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Identifiable;
 /**
 @author rashid_waraich, colin_sheppard
@@ -20,6 +21,14 @@ public interface ChargingPoint extends Identifiable<ChargingPoint> {
 	Collection<ChargingPlug> getAllChargingPlugs();
 	
 	void addChargingPlug(ChargingPlug chargingPlug);
+	
+	// We assume that one charging plug can potentially serve multiple parking spots located adjacent to it. But only one parked car at a time can use it.
+	// For example in the scenarios it could be assumed that the charger is released when charging is finished (e.g. electronic unlock - chargingPlugStatus=AVAILABLE).
+	double getNumberOfAvailableParkingSpots();
+	
+	
+	void registerVehicleArrival(double time, Id<Vehicle> vehicleId); 
+	void registerVehicleDeparture(double time, Id<Vehicle> vehicleId);
 	
 	
 }
