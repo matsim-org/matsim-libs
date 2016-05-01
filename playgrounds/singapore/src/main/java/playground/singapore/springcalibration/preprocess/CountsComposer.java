62d9f9b264edececf60e98ca73ac6f6535da4f9a
Andreas Horni <andreashorni@hotmail.com>
2016-05-01 18:51:53 +0200

    counts analysis

--- playgrounds/singapore/src/main/java/playground/singapore/springcalibration/preprocess/CountsComposer.java
+++ playgrounds/singapore/src/main/java/playground/singapore/springcalibration/preprocess/CountsComposer.java
@@ -45,6 +45,9 @@ public class CountsComposer {
 	
 	
 	public void run(String path, String networkfile, double scale) {
+		double buses = 0;
+		double cars = 0;
+		double freight = 0;
 		
 		this.scaleFactor = scale;
 		
@@ -58,19 +61,19 @@ public class CountsComposer {
 		
 		for (Id<Link> countedlink : buscounts.getCounts().keySet()) {
 			Count<Link> count = buscounts.getCount(countedlink);
-			this.addCountValues(count, "bus");
+			buses += this.addCountValues(count, "bus");
 		}
 		for (Id<Link> countedlink : carcounts.getCounts().keySet()) {
 			Count<Link> count = carcounts.getCount(countedlink);
-			this.addCountValues(count, TransportMode.car);
+			cars += this.addCountValues(count, TransportMode.car);
 		}
 		for (Id<Link> countedlink : hgvcounts.getCounts().keySet()) {
 			Count<Link> count = hgvcounts.getCount(countedlink);
-			this.addCountValues(count, "freight");
+			freight += this.addCountValues(count, "freight");
 		}
 		for (Id<Link> countedlink : lgvcounts.getCounts().keySet()) {
 			Count<Link> count = lgvcounts.getCount(countedlink);
-			this.addCountValues(count, "freight");
+			freight += this.addCountValues(count, "freight");
 		}
 		for (Id<Link> countedlink : taxicounts.getCounts().keySet()) {
 			Count<Link> count = taxicounts.getCount(countedlink);
@@ -78,10 +81,12 @@ public class CountsComposer {
 		}
 
 		log.info("Writing counts file to " + path + this.composedfile + ".xml");
+		log.info("Cars: " + cars + " buses: " + buses + " freight: " + freight);
 		new CountsWriter(composedcounts).write(path + this.composedfile + ".xml");		
 	}
 	
-	private void addCountValues(Count<Link> countstoadd, String mode) {
+	private double addCountValues(Count<Link> countstoadd, String mode) {
+				
 		double scale = 1.0;
 		if (mode.equals("bus"))  scale = this.scaleFactor;
 						
@@ -89,7 +94,8 @@ public class CountsComposer {
 		if (composedCount == null) {
 			composedCount = this.composedcounts.createAndAddCount(countstoadd.getLocId(), countstoadd.getCsId());
 		}
-			
+		
+		double totalCount = 0;
 		for (int h = 1; h <= 24; h++) {
 			double countval2add = 0.0;
 			Volume volume2add = countstoadd.getVolume(h);
@@ -98,11 +104,14 @@ public class CountsComposer {
 			double oldcountval = 0.0;
 			Volume oldVolume = composedCount.getVolume(h);
 			if (oldVolume != null) oldcountval = oldVolume.getValue();
-						
+			
+			totalCount += countval2add;
+			
 			double newCount = scale * countval2add + oldcountval;
 			
 			if (volume2add != null || oldVolume != null) composedCount.createVolume(h, newCount);		
 		}
+		return totalCount;
 	}
 	
 }
