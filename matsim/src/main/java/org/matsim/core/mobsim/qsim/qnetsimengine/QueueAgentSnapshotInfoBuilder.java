edb1bf29a47bb82f6f6b8d434378c8e726f90874
Amit Agarwal <amit.agarwal@campus.tu-berlin.de>
2016-05-15 19:27:52 +0200

    first veh can now attain a pos == curvedLength

--- matsim/src/main/java/org/matsim/core/mobsim/qsim/qnetsimengine/QueueAgentSnapshotInfoBuilder.java
+++ matsim/src/main/java/org/matsim/core/mobsim/qsim/qnetsimengine/QueueAgentSnapshotInfoBuilder.java
@@ -68,6 +68,7 @@ class QueueAgentSnapshotInfoBuilder extends AbstractAgentSnapshotInfoBuilder {
 	public double calculateOdometerDistanceFromFromNode(double curvedLength, double spacing,
 			 double lastDistanceFromFNode, double now, double freespeedTraveltime, double remainingTravelTime) {
 		double distanceFromFNode ;
+		boolean isFirstVehicle = false;
 		
 		if (freespeedTraveltime == 0.0){
 			distanceFromFNode = 0. ;
@@ -83,17 +84,21 @@ class QueueAgentSnapshotInfoBuilder extends AbstractAgentSnapshotInfoBuilder {
 		
 		if (Double.isNaN(lastDistanceFromFNode)) {
 			// (non-object-oriented way of "null" (?))
-			
+			isFirstVehicle = true;
 			lastDistanceFromFNode = curvedLength ;
 		}
 
-		if (distanceFromFNode >= lastDistanceFromFNode - spacing ) { 
+		if (isFirstVehicle && distanceFromFNode >= lastDistanceFromFNode) {
+			// first vehicle can be at the end of the link. amit May 2016
+			// == --> if remainingTravelTime == 0 ; >= remaining travel time is zero or negative. 
+			distanceFromFNode = lastDistanceFromFNode;
+		} else if (distanceFromFNode >= lastDistanceFromFNode - spacing ) {  
 			/* vehicle is already in queue or has to stay behind another vehicle
 			 * -> position it directly after the last position
 			 */
 			distanceFromFNode = lastDistanceFromFNode - spacing;
 		}
-		
+
 		//else just do nothing anymore
 		return distanceFromFNode;
 	}
