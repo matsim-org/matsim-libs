fb419578b0b6120c8bbecfe3e6c1987115a6ca5a
gabriel.thunig <gabriel.thunig@vsp.tu-berlin.de>
2016-06-20 18:11:33 +0200

    add ratio test for stop criterion but set it to ignore for 1 in 100 test could fail

--- matsim/src/test/java/org/matsim/pt/router/TransitLeastCostPathTreeTest.java
+++ matsim/src/test/java/org/matsim/pt/router/TransitLeastCostPathTreeTest.java
@@ -1,25 +1,29 @@
 package org.matsim.pt.router;
 
+import org.hamcrest.Matchers;
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Node;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.network.NetworkImpl;
 import org.matsim.core.router.util.LeastCostPathCalculator.Path;
+import org.matsim.core.router.util.TravelTime;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.pt.router.PreparedTransitSchedule;
-import org.matsim.pt.router.TransitRouterConfig;
-import org.matsim.pt.router.TransitRouterNetwork;
-import org.matsim.pt.router.TransitRouterNetworkTravelTimeAndDisutility;
-import org.matsim.pt.router.TransitLeastCostPathTree;
+import org.matsim.pt.router.TransitLeastCostPathTree.InitialNode;
 import org.matsim.pt.transitSchedule.api.TransitSchedule;
 import org.matsim.pt.transitSchedule.api.TransitScheduleReader;
 import org.matsim.testcases.MatsimTestUtils;
+import org.matsim.vehicles.Vehicle;
+import static org.hamcrest.Matchers.*;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -34,10 +38,8 @@ public class TransitLeastCostPathTreeTest {
     private TransitRouterNetworkTravelTimeAndDisutility travelDisutility;
 
     /**
-     * Gets executed before each Method.
      * Instantiates a new TransitLeastCostPathTree object with a sample transitSchedule and default configuration.
      */
-    @Before
     public void instantiateNetworkAndTravelDisutility() {
         String transitScheduleFile = "examples/pt-tutorial/transitschedule.xml";
 
@@ -59,6 +61,7 @@ public class TransitLeastCostPathTreeTest {
      */
     @Test
     public void TestNetworkAndTravelDisutilityInstantiated() {
+        instantiateNetworkAndTravelDisutility();
         Assert.assertNotNull(network);
         Assert.assertNotNull(travelDisutility);
     }
@@ -70,13 +73,13 @@ public class TransitLeastCostPathTreeTest {
      * @param departureTime The time the person departures at the origin.
      * @return the next transitNode.
      */
-    private Map<Node, TransitLeastCostPathTree.InitialNode> locateWrappedNearestTransitNode(Person person, Coord coord, double departureTime) {
+    private Map<Node, InitialNode> locateWrappedNearestTransitNode(Person person, Coord coord, double departureTime) {
         TransitRouterNetwork.TransitRouterNetworkNode nearestNode = network.getNearestNode(coord);
-        Map<Node, TransitLeastCostPathTree.InitialNode> wrappedNearestNodes = new LinkedHashMap<>();
+        Map<Node, InitialNode> wrappedNearestNodes = new LinkedHashMap<>();
         Coord toCoord = nearestNode.stop.getStopFacility().getCoord();
         double initialTime = travelDisutility.getTravelTime(person, coord, toCoord);
         double initialCost = travelDisutility.getTravelDisutility(person, coord, toCoord);
-        wrappedNearestNodes.put(nearestNode, new TransitLeastCostPathTree.InitialNode(initialCost, initialTime + departureTime));
+        wrappedNearestNodes.put(nearestNode, new InitialNode(initialCost, initialTime + departureTime));
         return wrappedNearestNodes;
     }
 
@@ -85,11 +88,12 @@ public class TransitLeastCostPathTreeTest {
      */
     @Test
     public void TestValidRouting() {
+        instantiateNetworkAndTravelDisutility();
         Coord fromCoord = new Coord(1050d, 1050d);
-        Map<Node, TransitLeastCostPathTree.InitialNode> wrappedFromNodes = this.locateWrappedNearestTransitNode(null, fromCoord, 28800);
+        Map<Node, InitialNode> wrappedFromNodes = this.locateWrappedNearestTransitNode(null, fromCoord, 28800);
 		TransitLeastCostPathTree tree = new TransitLeastCostPathTree(network, travelDisutility, travelDisutility, wrappedFromNodes, null);
         Coord toCoord = new Coord(3950d, 1050d);
-        Map<Node, TransitLeastCostPathTree.InitialNode> wrappedToNodes = this.locateWrappedNearestTransitNode(null, toCoord, 28800);
+        Map<Node, InitialNode> wrappedToNodes = this.locateWrappedNearestTransitNode(null, toCoord, 28800);
         Path path = tree.getPath(wrappedToNodes);
         Assert.assertNotNull(path);
         double pathCost = path.travelCost;
@@ -103,11 +107,12 @@ public class TransitLeastCostPathTreeTest {
      */
     @Test
     public void TestValidRoutingWithInterchange() {
+        instantiateNetworkAndTravelDisutility();
         Coord fromCoord = new Coord(1050d, 1050d);
-        Map<Node, TransitLeastCostPathTree.InitialNode> wrappedFromNodes = this.locateWrappedNearestTransitNode(null, fromCoord, 28800);
+        Map<Node, InitialNode> wrappedFromNodes = this.locateWrappedNearestTransitNode(null, fromCoord, 28800);
 		TransitLeastCostPathTree tree = new TransitLeastCostPathTree(network, travelDisutility, travelDisutility, wrappedFromNodes, null);
         Coord toCoord = new Coord(2050d, 2960d);
-        Map<Node, TransitLeastCostPathTree.InitialNode> wrappedToNodes = this.locateWrappedNearestTransitNode(null, toCoord, 28800);
+        Map<Node, InitialNode> wrappedToNodes = this.locateWrappedNearestTransitNode(null, toCoord, 28800);
         Path path = tree.getPath(wrappedToNodes);
         Assert.assertNotNull(path);
         double pathCost = path.travelCost;
@@ -116,4 +121,98 @@ public class TransitLeastCostPathTreeTest {
         Assert.assertEquals(231.20000000000073d, pathTime, MatsimTestUtils.EPSILON);
     }
 
+    @Ignore
+    @Test
+    public void TestSpeedImprovementOnStopCriterion() {
+        Fixture f = new Fixture();
+        TestTimeCost tc = new TestTimeCost();
+        for (int i = 0; i < f.count-1; i++) {
+            tc.setData(Id.create(i, Link.class), 1.0, 1.0);
+        }
+        Map<Node, InitialNode> fromNodes = new HashMap<>();
+        fromNodes.put(f.network.getNodes().get(Id.create(0, Node.class)), new InitialNode(0.0, 0.0));
+        Map<Node, InitialNode> toNodes = new HashMap<>();
+        toNodes.put(f.network.getNodes().get(Id.create(1, Node.class)), new InitialNode(0.0, 0.0));
+        for (Node node : fromNodes.keySet()) {
+            System.out.println("From Node = " + node.getCoord());
+        }
+        for (Node node : toNodes.keySet()) {
+            System.out.println("To Node = " + node.getCoord());
+        }
+        long startTime = System.currentTimeMillis();
+        new TransitLeastCostPathTree(f.network, tc, tc, fromNodes, null);
+        long endTime = System.currentTimeMillis();
+        long elapsedTimeWithoutStopCreterion = (endTime - startTime);
+        startTime = System.currentTimeMillis();
+        new TransitLeastCostPathTree(f.network, tc, tc, fromNodes, toNodes, null);
+        endTime = System.currentTimeMillis();
+        long elapsedTimeWithStopCreterion = (endTime - startTime);
+        Double bareRatio = (double)elapsedTimeWithoutStopCreterion / (double)elapsedTimeWithStopCreterion;
+        System.out.println("bareRatio = " + bareRatio);
+        int ratio = (int) ((bareRatio) * 10);
+        System.out.println("Time without stop criterion = " + elapsedTimeWithoutStopCreterion);
+        System.out.println("Time with stop criterion = " + elapsedTimeWithStopCreterion);
+        System.out.println("ratio = " + ratio);
+        Assert.assertThat("Bad ratio",
+                ratio,
+                greaterThan(15));
+    }
+
+    /**
+     * Creates a simple network to be used in TestSpeedImprovementOnStopCriterion.
+     *
+     * <pre>
+     *   (0)--(1)--(2)--...--(9997)--(9998)--(9999)
+     * </pre>
+     *
+     * @author gthunig
+     */
+	/*package*/ static class Fixture {
+        /*package*/ NetworkImpl network;
+
+        int count = 10000;
+
+        public Fixture() {
+            this.network = NetworkImpl.createNetwork();
+            Node linkStartNode = this.network.createAndAddNode(Id.create(0, Node.class), new Coord((double) 0, (double) 0));
+            for (int i = 1; i < count; i++) {
+                Node linkEndNode = this.network.createAndAddNode(Id.create(i, Node.class), new Coord((double) i, (double) 0));
+                this.network.createAndAddLink(Id.create(i-1, Link.class), linkStartNode, linkEndNode, 1000.0, 10.0, 2000.0, 1);
+                linkStartNode = linkEndNode;
+            }
+
+        }
+    }
+
+    /*package*/ static class TestTimeCost implements TravelTime, TransitTravelDisutility {
+
+        private final Map<Id<Link>, Double> travelTimes = new HashMap<>();
+        private final Map<Id<Link>, Double> travelCosts = new HashMap<>();
+
+        public void setData(final Id<Link> id, final double travelTime, final double travelCost) {
+            this.travelTimes.put(id, travelTime);
+            this.travelCosts.put(id, travelCost);
+        }
+
+        @Override
+        public double getLinkTravelTime(final Link link, final double time, Person person, Vehicle vehicle) {
+            return this.travelTimes.get(link.getId());
+        }
+
+        @Override
+        public double getLinkTravelDisutility(final Link link, final double time, final Person person, final Vehicle vehicle, final CustomDataManager dataManager) {
+            return this.travelCosts.get(link.getId());
+        }
+
+        @Override
+        public double getTravelTime(Person person, Coord coord, Coord toCoord) {
+            return 0;
+        }
+
+        @Override
+        public double getTravelDisutility(Person person, Coord coord, Coord toCoord) {
+            return 0;
+        }
+
+    }
 }
