bcd64ae41331cec28618728dd490a1cdd0e43a47
ikaddoura <kaddoura@vsp.tu-berlin.de>
2016-06-22 14:32:40 +0200

    adding a decongestion pricing approach

---
+++ playgrounds/ikaddoura/src/main/java/playground/ikaddoura/decongestion/DecongestionRun.java
@@ -0,0 +1,114 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * TestControler.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2011 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+/**
+ * 
+ */
+package playground.ikaddoura.decongestion;
+
+
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.AbstractModule;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.OutputDirectoryHierarchy;
+import org.matsim.core.scenario.ScenarioUtils;
+
+import playground.ikaddoura.analysis.detailedPersonTripAnalysis.PersonTripBasicAnalysisMain;
+import playground.ikaddoura.decongestion.data.DecongestionInfo;
+import playground.ikaddoura.decongestion.routing.TollTimeDistanceTravelDisutilityFactory;
+
+/**
+ * Starts an interval-based decongestion pricing simulation run.
+ * 
+ * @author ikaddoura
+ *
+ */
+public class DecongestionRun {
+
+	private static final Logger log = Logger.getLogger(DecongestionRun.class);
+
+	private static String configFile;
+	private static String outputDirectory;
+	
+	public static void main(String[] args) throws IOException {
+		log.info("Starting simulation run with the following arguments:");
+		
+		if (args.length > 0) {
+
+			configFile = args[0];		
+			log.info("config file: "+ configFile);
+			
+			outputDirectory = args[1];		
+			log.info("output directory: "+ outputDirectory);
+
+		} else {
+			configFile = "../../../runs-svn/decongestion/input/config.xml";
+			outputDirectory = "../../../runs-svn/decongestion/output/decongestion_1000it_60_50_0.05/";
+//			outputDirectory = "../../../runs-svn/decongestion/output/baseCase/";
+		}
+		
+		DecongestionRun main = new DecongestionRun();
+		main.run();
+		
+	}
+
+	private void run() {
+
+		Config config = ConfigUtils.loadConfig(configFile);	
+		config.controler().setOutputDirectory(outputDirectory);
+		Scenario scenario = ScenarioUtils.loadScenario(config);
+		
+		Controler controler = new Controler(scenario);
+				
+		final DecongestionInfo info = new DecongestionInfo(scenario);
+
+		// decongestion pricing
+		final DecongestionControlerListener decongestion = new DecongestionControlerListener(info);		
+		controler.addOverridingModule(new AbstractModule() {
+			@Override
+			public void install() {
+				this.addControlerListenerBinding().toInstance(decongestion);
+			}
+		});
+		
+		// toll-adjusted routing
+		final TollTimeDistanceTravelDisutilityFactory travelDisutilityFactory = new TollTimeDistanceTravelDisutilityFactory(info, config.planCalcScore());
+		travelDisutilityFactory.setSigma(0.);
+		controler.addOverridingModule(new AbstractModule(){
+			@Override
+			public void install() {
+				this.bindCarTravelDisutilityFactory().toInstance( travelDisutilityFactory );
+			}
+		}); 
+				
+        controler.getConfig().controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
+		controler.run();
+		
+		log.info("Analyzing the final iteration...");
+		PersonTripBasicAnalysisMain analysis = new PersonTripBasicAnalysisMain(scenario.getConfig().controler().getOutputDirectory());
+		analysis.run();
+	}
+}
+
---
+++ playgrounds/ikaddoura/src/main/java/playground/ikaddoura/decongestion/data/DecongestionInfo.java
@@ -0,0 +1,88 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2015 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.ikaddoura.decongestion.data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.vehicles.Vehicle;
+
+/**
+ * Stores the information which is requried during the computation of decongestion prices
+ * Contains the input parameters, e.g. how often the interval-based output is written out, the number of iterations after which the price should be updated, ...
+ * 
+ * @author ikaddoura
+ */
+
+public class DecongestionInfo {
+	
+	private final int WRITE_OUTPUT_ITERATION = 10;
+	private final int UPDATE_PRICE_INTERVAL = 50;
+	private final double TOLERATED_AVERAGE_DELAY_SEC = 1.;
+	private final double TOLL_ADJUSTMENT_RATE = 0.05;
+	private final double FRACTION_OF_ITERATIONS_TO_END_PRICE_ADJUSTMENT = 1.0;
+	
+	private final Scenario scenario;
+	
+	private final Map<Id<Link>, LinkInfo> linkId2info = new HashMap<>();
+	private final Map<Id<Vehicle>, Id<Person>> vehicleId2personId = new HashMap<>();
+
+	public DecongestionInfo(Scenario scenario) {
+		this.scenario = scenario;
+	}
+
+	public int getWRITE_OUTPUT_ITERATION() {
+		return WRITE_OUTPUT_ITERATION;
+	}
+
+	public int getUPDATE_PRICE_INTERVAL() {
+		return UPDATE_PRICE_INTERVAL;
+	}
+	
+	public Scenario getScenario() {
+		return scenario;
+	}
+	
+	public Map<Id<Vehicle>, Id<Person>> getVehicleId2personId() {
+		return vehicleId2personId;
+	}
+	
+	public Map<Id<Link>, LinkInfo> getlinkInfos() {
+		return linkId2info;
+	}
+
+	public double getTOLERATED_AVERAGE_DELAY_SEC() {
+		return TOLERATED_AVERAGE_DELAY_SEC;
+	}
+
+	public double getTOLL_ADJUSTMENT_RATE() {
+		return TOLL_ADJUSTMENT_RATE;
+	}
+
+	public double getFRACTION_OF_ITERATIONS_TO_END_PRICE_ADJUSTMENT() {
+		return FRACTION_OF_ITERATIONS_TO_END_PRICE_ADJUSTMENT;
+	}
+
+}
+
