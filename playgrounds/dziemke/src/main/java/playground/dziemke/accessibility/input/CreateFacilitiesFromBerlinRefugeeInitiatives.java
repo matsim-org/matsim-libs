db2e7749c4bb9c8104ff301e0b64205cf8786f49
dziemke <ziemke@vsp.tu-berlin.de>
2016-06-24 16:19:20 +0200

    revise facility crwaotr classes and put them in one package

---
+++ playgrounds/dziemke/src/main/java/playground/dziemke/accessibility/input/CreateFacilitiesFromBerlinRefugeeInitiatives.java
@@ -0,0 +1,130 @@
+/* *********************************************************************** *
+ * project: org.matsim.*												   *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package playground.dziemke.accessibility.input;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
+import org.matsim.core.utils.geometry.CoordUtils;
+import org.matsim.core.utils.geometry.CoordinateTransformation;
+import org.matsim.core.utils.geometry.transformations.TransformationFactory;
+import org.matsim.facilities.ActivityFacilities;
+import org.matsim.facilities.ActivityFacilitiesFactory;
+import org.matsim.facilities.ActivityFacilitiesFactoryImpl;
+import org.matsim.facilities.ActivityFacility;
+import org.matsim.facilities.ActivityOption;
+import org.matsim.facilities.FacilitiesUtils;
+import org.matsim.facilities.FacilitiesWriter;
+
+/**
+ * @author gthunig, dziemke
+ *
+ * This class searches for data about refugee initiatives on the berlin.de website and creates facilities based on them and
+ * then writes them into a facilities file.
+ */
+public class CreateFacilitiesFromBerlinRefugeeInitiatives {
+    private static final Logger LOG = Logger.getLogger(CreateFacilitiesFromBerlinRefugeeInitiatives.class);
+
+    public static void main(String[] args) {
+    	
+    	String urlString = "https://www.berlin.de/fluechtlinge/berlin-engagiert-sich/berliner-initiativen/";
+        String facilitiesFile = "../../../shared-svn/projects/accessibility_berlin/refugee/initiativen_2.xml";
+        String facilitiesFileDescription = "Refugee-relevant facilities in Berlin";
+    	
+    	String inputCRS = "EPSG:4326";
+		String outputCRS = TransformationFactory.DHDN_GK4;
+    	
+    	CoordinateTransformation ct = TransformationFactory.getCoordinateTransformation(inputCRS, outputCRS);
+    	
+    	ActivityFacilities activityFacilities = FacilitiesUtils.createActivityFacilities(facilitiesFileDescription);
+    	
+        try {
+            URL url = new URL(urlString);
+            try {
+                InputStreamReader inputStreamReader = new InputStreamReader(url.openStream());
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                LOG.info("Reading " + url);
+
+                String line;
+                try {
+                    while ((line = bufferedReader.readLine()) != null) {
+                        if (line.contains("mapOptions.markerList.push({")) {
+                            String title = null;
+                            Coord coord = null;
+                            String[] lineArray;
+                            line = bufferedReader.readLine();
+                            if (line.contains("coordinates")) {
+                                lineArray = line.split("'");
+                                String[] coordStrings = lineArray[1].split(",");
+                                double lat = Double.parseDouble(coordStrings[0]);
+                                double lon = Double.parseDouble(coordStrings[1]);
+                                coord = CoordUtils.createCoord(lat, lon);
+                            }
+                            bufferedReader.readLine();
+                            bufferedReader.readLine();
+                            bufferedReader.readLine();
+                            bufferedReader.readLine();
+                            line = bufferedReader.readLine();
+                            if (line.contains("title")) {
+                                lineArray = line.split("'");
+                                title = lineArray[1];
+                            }
+                            if (title != null && coord != null) {
+                            	ActivityFacility activityFacility = createFacility(title, coord, facilitiesFile, ct);
+                            	activityFacilities.addActivityFacility(activityFacility);
+                            }
+                        }
+                    }
+                } catch (IOException e2) {
+                    e2.printStackTrace();
+                }
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        writeFacilitiesFile(activityFacilities, facilitiesFile);
+    }
+
+    private static ActivityFacility createFacility(String title, Coord coord, String outputFile, CoordinateTransformation ct) {
+        	Id<ActivityFacility> id = Id.create(title , ActivityFacility.class);
+    		Coord transformedCoord = ct.transform(coord);
+        	
+        	ActivityFacilitiesFactory activityFacilitiesFactory = new ActivityFacilitiesFactoryImpl();
+    		ActivityFacility activityFacility = activityFacilitiesFactory.createActivityFacility(id, transformedCoord);
+    		ActivityOption activityOption = activityFacilitiesFactory.createActivityOption("Refugee_Initiative");
+    		activityFacility.addActivityOption(activityOption);
+        return activityFacility;
+    }
+
+    
+    private static void writeFacilitiesFile(ActivityFacilities activityFacilities, String facilitiesOutputFile) {
+		FacilitiesWriter facilitiesWriter = new FacilitiesWriter(activityFacilities);
+		facilitiesWriter.write(facilitiesOutputFile);
+		LOG.info("Facility file written.");
+	}
+}
