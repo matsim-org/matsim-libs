6dc1ca5394b43733f9172b0659b241fbf0743966
Amit Agarwal <amit.agarwal@campus.tu-berlin.de>
2016-06-28 22:50:12 +0200

    counts merger

---
+++ playgrounds/agarwalamit/src/main/java/playground/agarwalamit/mixedTraffic/patnaIndia/input/combined/JointCountsGenerator.java
@@ -0,0 +1,48 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2016 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.agarwalamit.mixedTraffic.patnaIndia.input.combined;
+
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.counts.Counts;
+import org.matsim.counts.CountsReaderMatsimV1;
+import org.matsim.counts.CountsWriter;
+
+import playground.agarwalamit.mixedTraffic.patnaIndia.utils.PatnaUtils;
+
+/**
+ * @author amit
+ */
+
+public class JointCountsGenerator {
+	
+	private static final String urbanCountFile = PatnaUtils.INPUT_FILES_DIR+"/simulationInputs/urban/urbanCounts_excl_rckw_osmNetwork.xml.gz";
+	private static final String externalCountFile = PatnaUtils.INPUT_FILES_DIR+"/simulationInputs/external/outerCordonCounts_10pct_OC1Excluded.xml.gz";
+	private static final String jointCountFile = PatnaUtils.INPUT_FILES_DIR+"/simulationInputs/joint/joint_counts_osmNetwork.xml.gz";
+	
+	public static void main(String[] args) {
+
+		Counts<Link> counts = new Counts<>();
+		CountsReaderMatsimV1 reader = new CountsReaderMatsimV1(counts);
+		reader.parse(urbanCountFile);
+		reader.parse(externalCountFile);
+		
+		new CountsWriter(counts).write(jointCountFile);
+	}
+}
