8955c8237a400105139be2f11bcb12fd2a65a808
jfbischoff <bischoff@vsp.tu-berlin.de>
2016-06-28 14:31:02 +0200

    updated the examples

--- playgrounds/jbischoff/src/main/java/playground/jbischoff/taxibus/algorithm/scheduler/TaxibusScheduler.java
+++ playgrounds/jbischoff/src/main/java/playground/jbischoff/taxibus/algorithm/scheduler/TaxibusScheduler.java
@@ -163,12 +163,12 @@ public class TaxibusScheduler {
 		Schedule<TaxibusTask> bestSched = (Schedule<TaxibusTask>) best.vehicle.getSchedule();
 
 		TaxibusTask lastTask = Schedules.getLastTask(bestSched);
-		log.info("Bus " + best.vehicle.getId() + " Scheduled Route");
-		for (VrpPathWithTravelData path : best.path) {
-			log.info(path.getFromLink().getId() + " to " + path.getToLink().getId());
-		}
-		log.info("End of route");
-		log.info("scheduled to bus: " + best.requests);
+//		log.info("Bus " + best.vehicle.getId() + " Scheduled Route");
+//		for (VrpPathWithTravelData path : best.path) {
+//			log.info(path.getFromLink().getId() + " to " + path.getToLink().getId());
+//		}
+//		log.info("End of route");
+//		log.info("scheduled to bus: " + best.requests);
 		Iterator<VrpPathWithTravelData> iterator = best.path.iterator();
 		double lastEndTime;
 		VrpPathWithTravelData path;
