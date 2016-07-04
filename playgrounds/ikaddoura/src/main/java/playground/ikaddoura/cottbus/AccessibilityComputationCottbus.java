2749ce83fae5f46c2a3f0be963339a96f9195e17
gabriel.thunig <gabriel.thunig@vsp.tu-berlin.de>
2016-07-04 16:09:23 +0200

    refactor double[] -> envelope

--- playgrounds/ikaddoura/src/main/java/playground/ikaddoura/cottbus/AccessibilityComputationCottbus.java
+++ playgrounds/ikaddoura/src/main/java/playground/ikaddoura/cottbus/AccessibilityComputationCottbus.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import javax.inject.Inject;
 import javax.inject.Provider;
 
+import com.vividsolutions.jts.geom.Envelope;
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.contrib.accessibility.AccessibilityCalculator;
@@ -48,7 +49,7 @@ public class AccessibilityComputationCottbus {
 		Integer range = 9;
 		int symbolSize = 110;
 		int populationThreshold = (int) (200 / (1000/cellSize * 1000/cellSize));
-		double[] mapViewExtent = {447000,5729000,461000,5740000};
+		Envelope envelope = new Envelope(447000,5729000,461000,5740000);
 		
 		Config config = ConfigUtils.loadConfig(runOutputFolder + "config.xml", new AccessibilityConfigGroup());
 		
@@ -128,7 +129,7 @@ public class AccessibilityComputationCottbus {
 				String actSpecificWorkingDirectory = workingDirectory + actType + "/";
 
 				for ( Modes4Accessibility mode : Modes4Accessibility.values()) {
-					VisualizationUtils.createQGisOutput(actType, mode, mapViewExtent, workingDirectory, crs, includeDensityLayer,
+					VisualizationUtils.createQGisOutput(actType, mode, envelope, workingDirectory, crs, includeDensityLayer,
 							lowerBound, upperBound, range, symbolSize, populationThreshold);
 					VisualizationUtils.createSnapshot(actSpecificWorkingDirectory, mode, osName);
 				}
