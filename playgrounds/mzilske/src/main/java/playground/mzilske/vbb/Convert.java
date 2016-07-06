7500d59d817da9160718cda3c50732191bfd0ba2
Michael Zilske <michael.zilske@tu-berlin.de>
2016-07-06 18:04:23 +0200

    Create a few random transit users to visualize VBB.

--- playgrounds/mzilske/src/main/java/playground/mzilske/vbb/Convert.java
+++ playgrounds/mzilske/src/main/java/playground/mzilske/vbb/Convert.java
@@ -17,6 +17,8 @@ import org.matsim.vehicles.VehicleWriterV1;
 import org.matsim.vis.otfvis.OTFVisConfigGroup;
 import org.matsim.vis.otfvis.OTFVisConfigGroup.ColoringScheme;
 
+import java.time.LocalDate;
+
 public class Convert {
 	
 
@@ -29,7 +31,6 @@ public class Convert {
 
 	private void convert() {
 		final Scenario scenario = readScenario();
-		// new NetworkCleaner().run(scenario.getNetwork());
 		System.out.println("Scenario has " + scenario.getNetwork().getLinks().size() + " links.");
 		scenario.getConfig().controler().setMobsim("qsim");
 		scenario.getConfig().qsim().setSnapshotStyle( SnapshotStyle.queue ) ;;
@@ -38,34 +39,21 @@ public class Convert {
 		ConfigUtils.addOrGetModule(scenario.getConfig(), OTFVisConfigGroup.GROUP_NAME, OTFVisConfigGroup.class).setColoringScheme(ColoringScheme.gtfs);
 		ConfigUtils.addOrGetModule(scenario.getConfig(), OTFVisConfigGroup.GROUP_NAME, OTFVisConfigGroup.class).setDrawTransitFacilities(false);
 		scenario.getConfig().transitRouter().setMaxBeelineWalkConnectionDistance(1.0);
-//		for (TransitStopFacility facility : scenario.getTransitSchedule().getFacilities().values()) {
-//			if (scenario.getNetwork().getLinks().get(facility.getId()) == null) {
-//				throw new RuntimeException();
-//			}
-//		}
 		new NetworkWriter(scenario.getNetwork()).write("/Users/zilske/gtfs-bvg/network.xml");
 		new TransitScheduleWriter(scenario.getTransitSchedule()).writeFile("/Users/zilske/gtfs-bvg/transit-schedule.xml");
-		new VehicleWriterV1(((MutableScenario) scenario).getTransitVehicles()).writeFile("/Users/zilske/gtfs-bvg/transit-vehicles.xml");		
+		new VehicleWriterV1(scenario.getTransitVehicles()).writeFile("/Users/zilske/gtfs-bvg/transit-vehicles.xml");
 	}
 	
 	private static Scenario readScenario() {
-		// GtfsConverter gtfs = new GtfsConverter("/Users/zilske/Documents/torino", new GeotoolsTransformation("WGS84", CRS));
 		Config config = ConfigUtils.createConfig();
 		config.global().setCoordinateSystem(CRS);
 		config.controler().setLastIteration(0);
-		config.scenario().setUseVehicles(true);
 		config.transit().setUseTransit(true);
 		Scenario scenario = ScenarioUtils.createScenario(config);
-		// GtfsConverter gtfs = new GtfsConverter("/Users/zilske/gtfs-bvg", scenario, new GeotoolsTransformation("WGS84", CRS));
-		GtfsConverter gtfs = new GtfsConverter(GTFSFeed.fromFile("/Users/zilske/gtfs-bvg"), scenario, new CoordinateTransformation() {
-			
-			@Override
-			public Coord transform(Coord coord) {
-				return coord;
-			}
-		});
-//		gtfs.setDate(20111015);
+		GtfsConverter gtfs = new GtfsConverter(GTFSFeed.fromFile("/Users/zilske/gtfs-bvg"), scenario, coord -> coord);
+		gtfs.setDate(LocalDate.of(2016, 7, 6));
 		gtfs.convert();
+
 		return scenario;
 	}
 
