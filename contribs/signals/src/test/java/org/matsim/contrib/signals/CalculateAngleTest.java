ff863cb62e81ca53be5cf975ef5bbd2a5c5a2766
Michael Zilske <michael.zilske@tu-berlin.de>
2016-07-09 19:44:46 +0200

    Fix paths in signals.

--- contribs/signals/src/test/java/org/matsim/contrib/signals/CalculateAngleTest.java
+++ contribs/signals/src/test/java/org/matsim/contrib/signals/CalculateAngleTest.java
@@ -40,7 +40,7 @@ public class CalculateAngleTest {
 	public void testGetLeftLane() {
 		Config conf = utils.loadConfig(utils.getClassInputDirectory() + "config.xml");
 		MutableScenario scenario = (MutableScenario) ScenarioUtils.createScenario(conf);
-		new MatsimNetworkReader(scenario.getNetwork()).readFile(conf.network().getInputFile());
+		new MatsimNetworkReader(scenario.getNetwork()).parse(conf.network().getInputFileURL(conf.getContext()));
 
 		Assert.assertEquals("Has to be 'null', since there is no other way back but Link 11.",
 				null, NetworkUtils.getLeftLane(scenario.getNetwork().getLinks().get(Id.create("1", Link.class))));
