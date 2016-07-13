11acbc5aecce66ce688c7dba075524c387078a7c
dziemke <ziemke@vsp.tu-berlin.de>
2016-07-13 17:31:22 +0200

    revise plotting helper class

--- playgrounds/dziemke/src/main/java/playground/dziemke/analysis/GnuplotUtils.java
+++ playgrounds/dziemke/src/main/java/playground/dziemke/analysis/GnuplotUtils.java
@@ -27,20 +27,20 @@ public class GnuplotUtils {
 		try {
 			ProcessBuilder processBuilder = null;
 			
-			// if OS is Windows --- example (daniel r) // os.arch=amd64 // os.name=Windows 7 // os.version=6.1
-			if ( osName.contains("Win") || osName.contains("win")) {
-				processBuilder = new ProcessBuilder( "cmd", "/c", "cd", pathToSpecificAnalysisDir, "&", "gnuplot", relativePathToGnuplotScript);
+			// If OS is Windows --- example (daniel r) // os.arch=amd64 // os.name=Windows 7 // os.version=6.1
+			if (osName.contains("Win") || osName.contains("win")) {
+				processBuilder = new ProcessBuilder( "cmd", "/c", "cd", pathToSpecificAnalysisDir, "&", "gnuplot", relativePathToGnuplotScript, allArgumentsAsString);
 			
-				// if OS is Macintosh --- example (dominik) // os.arch=x86_64 // os.name=Mac OS X // os.version=10.10.2
+			// If OS is Macintosh --- example (dominik) // os.arch=x86_64 // os.name=Mac OS X // os.version=10.10.2
 			} else if ( osName.contains("Mac") || osName.contains("mac") ) {
 				processBuilder = new ProcessBuilder("bash", "-c", "(cd " + pathToSpecificAnalysisDir + " && /usr/local/bin/gnuplot -c " + relativePathToGnuplotScript + " " + allArgumentsAsString + ")");
 			
-				// if OS is Linux --- example (benjamin) // os.arch=amd64 // os.name=Linux	// os.version=3.13.0-45-generic
+			// If OS is Linux --- example (benjamin) // os.arch=amd64 // os.name=Linux	// os.version=3.13.0-45-generic
 			} else if ( osName.contains("Lin") || osName.contains("lin") ) {
 				log.warn("This implemenation has not yet been tested for Linux. Please report if you use this.");
 				processBuilder = new ProcessBuilder("bash", "-c", "(cd " + pathToSpecificAnalysisDir + " && /usr/local/bin/gnuplot " + relativePathToGnuplotScript + ")");
 			
-				// if OS is neither Win nor Mac nor Linux
+			// If OS is neither Win nor Mac nor Linux
 			} else {
 				log.error("Not implemented for os.arch=" + System.getProperty("os.arch") );
 			}
