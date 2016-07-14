4e0cf713a73e47e47954172cafbef7e60eb25806
gabriel.thunig <gabriel.thunig@vsp.tu-berlin.de>
2016-07-14 14:04:30 +0200

    works only with one argument so the method is now also only accepting one

--- playgrounds/dziemke/src/main/java/playground/dziemke/analysis/GnuplotUtils.java
+++ playgrounds/dziemke/src/main/java/playground/dziemke/analysis/GnuplotUtils.java
@@ -1,39 +1,60 @@
 package playground.dziemke.analysis;
 
+import org.apache.log4j.Logger;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-import org.apache.log4j.Logger;
 
 /**
  * @author dziemke, tthunig
  */
 public class GnuplotUtils {
 	public static final Logger log = Logger.getLogger(GnuplotUtils.class);
-	
-	public static void runGnuplotScript(String pathToSpecificAnalysisDir, String relativePathToGnuplotScript, String... gnuplotArguments){
+
+	public static void main(String[] args) {
+		String pathToSpecificAnalysisDir = "../../../../Desktop/gnuplottest/";
+		String relativePathToGnuplotScript = "test.gnu";
+		GnuplotUtils.runGnuplotScript(pathToSpecificAnalysisDir, relativePathToGnuplotScript, "one");
+	}
+
+	public static void runGnuplotScript(String pathToSpecificAnalysisDir, String relativePathToGnuplotScript, String gnuplotArgument){
+		/*
+			right now this only works with one argument
+			if you need to pass more then one change "String gnuplotArgument" to "String... gnuplotArguments"
+			also you have to think about a way to pass the multiple arguments dynamicly to the ProcessBuilder
+			(you could also just implement the specific case for yourself)
+
+			a fine testscript for gnuplot would be:
+				#!/usr/local/bin/gnuplot --persist
+				print "script name        : ", ARG0
+				print "first argument     : ", ARG1
+				print "second argument    : ", ARG2
+				print "third argument     : ", ARG3
+				print "number of arguments: ", ARGC
+		*/
 		log.info("Analysis directory is = " + pathToSpecificAnalysisDir);
 		log.info("Relative path from analysis directory to directory with gnuplot script is = " + relativePathToGnuplotScript);
 		
 		String osName = System.getProperty("os.name");
 		log.info("OS name is = " + osName);
 		
-		String allArgumentsAsString = "";
+		/*String allArgumentsAsString = "";
 		for (int i=0; i < gnuplotArguments.length ; i++) {
-			allArgumentsAsString.concat(gnuplotArguments[i] + " ");
-		}
-		
+			allArgumentsAsString = allArgumentsAsString.concat(gnuplotArguments[i] + " ");
+		}*/
+
 		try {
 			ProcessBuilder processBuilder = null;
 			
 			// If OS is Windows --- example (daniel r) // os.arch=amd64 // os.name=Windows 7 // os.version=6.1
 			if (osName.contains("Win") || osName.contains("win")) {
-				processBuilder = new ProcessBuilder( "cmd", "/c", "cd", pathToSpecificAnalysisDir, "&", "gnuplot", relativePathToGnuplotScript, allArgumentsAsString);
+				String[] args = {"one", "two", "three", "four", "five"};
+				processBuilder = new ProcessBuilder( "cmd", "/c", "cd", pathToSpecificAnalysisDir, "&", "gnuplot", "-c", relativePathToGnuplotScript, gnuplotArgument);
 			
 			// If OS is Macintosh --- example (dominik) // os.arch=x86_64 // os.name=Mac OS X // os.version=10.10.2
 			} else if ( osName.contains("Mac") || osName.contains("mac") ) {
-				processBuilder = new ProcessBuilder("bash", "-c", "(cd " + pathToSpecificAnalysisDir + " && /usr/local/bin/gnuplot -c " + relativePathToGnuplotScript + " " + allArgumentsAsString + ")");
+				processBuilder = new ProcessBuilder("bash", "-c", "(cd " + pathToSpecificAnalysisDir + " && /usr/local/bin/gnuplot -c " + relativePathToGnuplotScript + " " + gnuplotArgument + ")");
 			
 			// If OS is Linux --- example (benjamin) // os.arch=amd64 // os.name=Linux	// os.version=3.13.0-45-generic
 			} else if ( osName.contains("Lin") || osName.contains("lin") ) {
