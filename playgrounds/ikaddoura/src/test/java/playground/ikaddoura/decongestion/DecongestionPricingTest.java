f77194569198b9822880c0961213ca6bbcf4bfd1
ikaddoura <kaddoura@vsp.tu-berlin.de>
2016-07-21 11:58:30 +0200

    adding a (regression) test for each controller

--- playgrounds/ikaddoura/src/test/java/playground/ikaddoura/decongestion/DecongestionPricingTest.java
+++ playgrounds/ikaddoura/src/test/java/playground/ikaddoura/decongestion/DecongestionPricingTest.java
@@ -53,10 +53,7 @@ public class DecongestionPricingTest {
 	public MatsimTestUtils testUtils = new MatsimTestUtils();
 
 	/**
-	 * There are two agents. One agent is more pressed for time than the other one which results in different VTTS.
-	 * There are two routes. One expensive but fast route. One cheap but slow route.
-	 * 
-	 * Accounting for the different VTTS results in a different outcome than assuming the default VTTS (as it is the case in the default router).
+	 * Tests the PID controller
 	 * 
 	 */
 	@Test
@@ -89,9 +86,54 @@ public class DecongestionPricingTest {
         double tt2 = controler.getLinkTravelTimes().getLinkTravelTime(scenario.getNetwork().getLinks().get(Id.createLinkId("link12")), 7 * 3600 + 35 * 60 + 5., null, null);
         double tt3 = controler.getLinkTravelTimes().getLinkTravelTime(scenario.getNetwork().getLinks().get(Id.createLinkId("link12")), 7 * 3600 + 35 * 60 + 10., null, null);
 
-		Assert.assertEquals("Wrong travel time. The run output seems to have changed.", 2652.732890, tt1, MatsimTestUtils.EPSILON);
+		Assert.assertEquals("Wrong travel time. The run output seems to have changed.", 2652.7328901056, tt1, MatsimTestUtils.EPSILON);
 		Assert.assertEquals("Wrong travel time. The run output seems to have changed.", 3847.328125, tt2, MatsimTestUtils.EPSILON);
 		Assert.assertEquals("Wrong travel time. The run output seems to have changed.", 3847.328125, tt3, MatsimTestUtils.EPSILON);
+		
+		double avgScore = controler.getScoreStats().getHistory()[3][config.controler().getLastIteration() - config.controler().getFirstIteration()];
+		Assert.assertEquals("Wrong average executed score. The run output seems to have changed.", -11787.72560635766, avgScore, MatsimTestUtils.EPSILON);		
+	}
+	
+	/**
+	 * Tests the BangBang controller
+	 * 
+	 */
+	@Test
+	public final void test2() {
+		
+		System.out.println(testUtils.getPackageInputDirectory());
+		
+		final String configFile = testUtils.getPackageInputDirectory() + "/config.xml";
+		
+		final DecongestionConfigGroup decongestionSettings = new DecongestionConfigGroup();
+		decongestionSettings.setTOLLING_APPROACH(TollingApproach.BangBang);
+		decongestionSettings.setWRITE_OUTPUT_ITERATION(1);
+		decongestionSettings.setFRACTION_OF_ITERATIONS_TO_END_PRICE_ADJUSTMENT(1.0);
+		decongestionSettings.setFRACTION_OF_ITERATIONS_TO_START_PRICE_ADJUSTMENT(0.0);
+		
+		Config config = ConfigUtils.loadConfig(configFile);
+
+		String outputDirectory = testUtils.getOutputDirectory() + "/";
+		config.controler().setOutputDirectory(outputDirectory);
+		final Scenario scenario = ScenarioUtils.loadScenario(config);
+				
+		final DecongestionInfo info = new DecongestionInfo(scenario, decongestionSettings);
+		final Decongestion decongestion = new Decongestion(info);
+		
+		final Controler controler = decongestion.getControler();
+        controler.getConfig().controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
+        controler.run();   
+		
+        double tt1 = controler.getLinkTravelTimes().getLinkTravelTime(scenario.getNetwork().getLinks().get(Id.createLinkId("link12")), 7 * 3600 + 63, null, null);
+        double tt2 = controler.getLinkTravelTimes().getLinkTravelTime(scenario.getNetwork().getLinks().get(Id.createLinkId("link12")), 7 * 3600 + 35 * 60 + 5., null, null);
+        double tt3 = controler.getLinkTravelTimes().getLinkTravelTime(scenario.getNetwork().getLinks().get(Id.createLinkId("link12")), 7 * 3600 + 35 * 60 + 10., null, null);
+
+		Assert.assertEquals("Wrong travel time. The run output seems to have changed.", 3429.57425742574, tt1, MatsimTestUtils.EPSILON);
+		Assert.assertEquals("Wrong travel time. The run output seems to have changed.", 5343.2, tt2, MatsimTestUtils.EPSILON);
+		Assert.assertEquals("Wrong travel time. The run output seems to have changed.", 5343.2, tt3, MatsimTestUtils.EPSILON);
+		
+		double avgScore = controler.getScoreStats().getHistory()[3][config.controler().getLastIteration() - config.controler().getFirstIteration()];
+		Assert.assertEquals("Wrong average executed score. The run output seems to have changed.", -55.08439467592601, avgScore, MatsimTestUtils.EPSILON);		
 	}
 	
 }
