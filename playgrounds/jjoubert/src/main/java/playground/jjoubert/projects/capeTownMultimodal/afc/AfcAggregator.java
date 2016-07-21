d76944a52860f02400add5d552ec45d0049d2592
Johan W Joubert <johan.joubert@up.ac.za>
2016-07-21 08:55:51 +0200

    Classes to run a 3D version of the equil network

--- playgrounds/jjoubert/src/main/java/playground/jjoubert/projects/capeTownMultimodal/afc/AfcAggregator.java
+++ playgrounds/jjoubert/src/main/java/playground/jjoubert/projects/capeTownMultimodal/afc/AfcAggregator.java
@@ -36,6 +36,7 @@ import org.apache.log4j.Logger;
 import org.matsim.core.utils.io.IOUtils;
 import org.matsim.core.utils.misc.Counter;
 
+import edu.uci.ics.jung.graph.util.Pair;
 import playground.southafrica.utilities.Header;
 
 /**
@@ -46,6 +47,7 @@ import playground.southafrica.utilities.Header;
  */
 public class AfcAggregator {
 	final private static Logger LOG = Logger.getLogger(AfcAggregator.class);
+	private static Map<String, Integer> gtfsStopMap;
 	
 	/**
 	 * @param args
@@ -55,8 +57,11 @@ public class AfcAggregator {
 		String input = args[0];
 		int seconds = Integer.parseInt(args[1]);
 		String output = args[2];
+		String odDurations = args[3];
+
+		gtfsStopMap = AfcUtils.parseStopIdFromGtfs(args[4]);
 		
-		processInput(input, seconds, output);
+		processInput(input, seconds, output, odDurations);
 		
 		Header.printFooter();
 	}
@@ -66,7 +71,7 @@ public class AfcAggregator {
 	}
 	
 	
-	public static void processInput(String input, int binSize, String output){
+	public static void processInput(String input, int binSize, String output, String odFile){
 		LOG.info("Processing input file " + input);
 		int unknownLeavers = 0;
 		Map<String, Integer> mapBoarding = new TreeMap<String, Integer>();
@@ -74,61 +79,117 @@ public class AfcAggregator {
 		Map<String, Integer> mapConnecting = new TreeMap<String, Integer>();
 		
 		Map<String,Integer> mapPerson = new HashMap<String, Integer>();
+		Map<String,Integer> mapLocation = new HashMap<>();
 		
 		Map<String, List<Integer>> mapDuration = new TreeMap<>();
+		List<String> odPairDurations = new ArrayList<>();
+		Map<Integer, String> stopMap = new HashMap<>();
+		
 		
 		BufferedReader br = IOUtils.getBufferedReader(input);
 		Counter counter = new Counter("  lines # ");
+		int alightAtZeroCost = 0;
+		int alightAtCost = 0;
+		int fullChains = 0;
+		int staffTransactions = 0;
+		
 		try{
 			String line = br.readLine(); /*Header. */
 			while((line = br.readLine()) != null){
 				String[] sa = line.split(",");
 				
 				String id = sa[0];
-				String type = sa[6];
-				String time = String.format("%02d%02d", 
-						Integer.parseInt(sa[9]), 
-						Integer.parseInt(sa[12]));
-				int min = Integer.parseInt(sa[9])*60 + Integer.parseInt(sa[12]);
-				int bin = (int) Math.ceil(((double)min)/((double)binSize));
-				String theBin = String.format("%04d", bin);
-				if(!mapBoarding.containsKey(theBin)){
-					mapBoarding.put(theBin, 0);
-					mapAlighting.put(theBin, 0);
-					mapConnecting.put(theBin, 0);
-					mapDuration.put(theBin, new ArrayList<Integer>());
+				int station = Integer.parseInt(sa[4]);
+				if(!stopMap.containsKey(station)){
+					String stationName = sa[5];
+					stopMap.put(station, stationName);
 				}
 				
-				if(type.equalsIgnoreCase("1st boarding")){
+				/* Only consider cards numbers that are longer than 8 digits
+				 * as that will ignore staff cards. */
+				if(id.length() > 8){
+					String type = sa[6];
+					double amount = Double.parseDouble(sa[11]);
+					String time = String.format("%02d:%02d", 
+							Integer.parseInt(sa[9]), 
+							Integer.parseInt(sa[12]));
+					int min = Integer.parseInt(sa[9])*60 + Integer.parseInt(sa[12]);
+					int bin = (int) Math.ceil(((double)min)/((double)binSize));
+					String theBin = String.format("%04d", bin);
+					if(!mapBoarding.containsKey(theBin)){
+						mapBoarding.put(theBin, 0);
+						mapAlighting.put(theBin, 0);
+						mapConnecting.put(theBin, 0);
+						mapDuration.put(theBin, new ArrayList<Integer>());
+					}
 					
-					if(!mapPerson.containsKey(id)){
-						mapBoarding.put(theBin, mapBoarding.get(theBin)+1);
-						mapPerson.put(id, min);
-					} else{
+					if(type.equalsIgnoreCase("1st boarding")){
+						if(!mapPerson.containsKey(id)){
+							mapPerson.put(id, min);
+							mapLocation.put(id, station);
+							mapBoarding.put(theBin, mapBoarding.get(theBin)+1);
+						} else{
 //						LOG.warn("Card " + id + " already in the system.");
-					}
-				} else if (type.equalsIgnoreCase("Connection")){
-					
-					if(!mapPerson.containsKey(id)){
-						mapConnecting.put(theBin, mapConnecting.get(theBin)+1);
-						mapPerson.put(id, min);
-					} else{
+						}
+					} else if (type.equalsIgnoreCase("Connection")){
+						if(!mapPerson.containsKey(id)){
+							mapPerson.put(id, min);
+							mapLocation.put(id, station);
+							mapConnecting.put(theBin, mapConnecting.get(theBin)+1);
+						} else{
 //						LOG.warn("Card " + id + " already in the system.");
-					}
-				} else if (type.equalsIgnoreCase("Alighting")){
-					
-					if(!mapPerson.containsKey(id)){
-						LOG.warn("Unknown person " + id + " alighting.");
-						unknownLeavers++;
+						}
+					} else if (type.equalsIgnoreCase("Alighting")){
+						if( amount > 0){
+							/* It is really getting out of the system. */
+							alightAtCost++;
+						} else{
+							/* It is alighting and will probably connect again. */
+							alightAtZeroCost++;
+						}
+						if(!mapPerson.containsKey(id)){
+							LOG.warn("Unknown person " + id + " alighting.");
+							unknownLeavers++;
+						} else{
+							mapAlighting.put(theBin, mapAlighting.get(theBin)+1);
+							
+							/* Calculate the duration. */
+							int duration = min - mapPerson.get(id);
+							mapDuration.get(theBin).add(duration);
+							mapPerson.remove(id);
+							
+							/* Determine the OD pair. */
+							int o = mapLocation.get(id);
+							String oName = stopMap.get(o);
+							int oGtfs = 0;
+							if(gtfsStopMap.containsKey(oName)){
+								oGtfs = gtfsStopMap.get(oName);
+							} else{
+								oGtfs = AfcUtils.getGtfsStationIdFromName(oName);
+							}
+							
+							int d = station;
+							String dName = stopMap.get(d);
+							int dGtfs = 0;
+							if(gtfsStopMap.containsKey(dName)){
+								dGtfs = gtfsStopMap.get(dName);
+							} else{
+								dGtfs = AfcUtils.getGtfsStationIdFromName(dName);
+							}
+							String entry = String.format("%s,%d,%s,%d,%s,%s,%d", 
+									id, oGtfs, oName, dGtfs, dName, time, duration);
+							odPairDurations.add(entry);
+							
+							fullChains++;
+						}
 					} else{
-						mapAlighting.put(theBin, mapAlighting.get(theBin)+1);
-
-						int duration = min - mapPerson.get(id);
-						mapDuration.get(theBin).add(duration);
-						mapPerson.remove(id);
+						LOG.error("What is this person doing?!");
+						throw new RuntimeException("Cannot handle transaction type '" + type + "'.");
 					}
+					
+				} else{
+					staffTransactions++;
 				}
-				
 				counter.incCounter();
 			}
 		} catch (IOException e) {
@@ -146,6 +207,10 @@ public class AfcAggregator {
 		LOG.info("Done processing.");
 		LOG.info("Number of people alighting without boarding: " + unknownLeavers);
 		LOG.info("Number of people still in system: " + mapPerson.size());
+		LOG.info("Number of alightings with zero cost: " + alightAtZeroCost);
+		LOG.info("Number of alightings with cost: " + alightAtCost);
+		LOG.info("Number of complete chains: " + fullChains);
+		LOG.info("Number of staff transactions: " + staffTransactions);
 		
 		LOG.info("Writing bin data to file...");
 		BufferedWriter bw = IOUtils.getBufferedWriter(output);
@@ -167,8 +232,7 @@ public class AfcAggregator {
 						total += (double)i;
 					}
 					avg = total / (double)list.size();
-				} 		
-				
+				} 			
 				
 				bw.write(String.format("%d,%d,%d,%d,%.2f,%.2f,%.2f\n", 
 						Integer.parseInt(s),
@@ -189,6 +253,35 @@ public class AfcAggregator {
 			}
 		}
 		LOG.info("Done writing output.");
+		
+		LOG.info("Writing stuck-in-system persons.");
+		for(String id : mapPerson.keySet()){
+			LOG.info("  " + id);
+		}
+		
+		LOG.info("Writing OD durations to file.");
+		bw = IOUtils.getBufferedWriter(odFile);
+		try{
+			bw.write("id,o,oName,d,dName,time,dur");
+			bw.newLine();
+			for(String s : odPairDurations){
+				bw.write(s);
+				bw.newLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Cannot write to " + odFile);
+		} finally{
+			try {
+				bw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+				throw new RuntimeException("Cannot close " + odFile);
+			}
+		}
+		
+		
+		LOG.info("Done writing OD durations.");
 	}
 	
 	
