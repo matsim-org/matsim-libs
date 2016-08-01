a451c41b0808a4b7724416482657041fda70bdbe
Andreas Neumann <neumann@senozon.de>
2016-08-01 16:40:45 +0200

    made this standard csv added similar analysis for taxi vehicles

--- playgrounds/vsp/src/main/java/playground/vsp/analysis/modules/taxiTravelStats/TaxiTravelStatsAnalyzer.java
+++ playgrounds/vsp/src/main/java/playground/vsp/analysis/modules/taxiTravelStats/TaxiTravelStatsAnalyzer.java
@@ -17,14 +17,13 @@
  *                                                                         *
  * *********************************************************************** */
 
-package playground.vsp.analysis.modules.ptTravelStats;
+package playground.vsp.analysis.modules.taxiTravelStats;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
 
@@ -40,14 +39,13 @@ import org.matsim.counts.Volume;
 import playground.vsp.analysis.modules.AbstractAnalysisModule;
 
 /**
- * Writes counts data for pax, paxMeter, transit vehicle capacity, capacityMeter, and number of transit vehicles each per link and mode.
- * Note: In order to have statistics for different transit modes, e.g. subway and bus, you have to set different modes in the transit schedule before running this analysis.
- * Implement your own {@linkplain PtMode2LineSetter} for this.
+ * Writes counts data for pax, paxMeter, transit vehicle capacity, capacityMeter, and number of taxi cabs each per link and mode.
  * 
- * @author aneumann, sfuerbas
+ * @author aneumann
  */
-public class TravelStatsAnalyzer extends AbstractAnalysisModule {
+public class TaxiTravelStatsAnalyzer extends AbstractAnalysisModule {
 
+	public final static String TAXI_PREFIX = "taxi";
 	public final static String CAPACITY = "capacity";
 	public final static String CAPACITY_METER = "capacityMeter";
 	public final static String PAX = "pax";
@@ -55,16 +53,16 @@ public class TravelStatsAnalyzer extends AbstractAnalysisModule {
 	public final static String VEHICLE = "vehicle";
 	public final static String OCCUPANCY = "occupancy";
 	
-	private final static Logger log = Logger.getLogger(TravelStatsAnalyzer.class);
+	private final static Logger log = Logger.getLogger(TaxiTravelStatsAnalyzer.class);
 	private final String separator = ";";
 	private final String header = "linkId" + separator + "x1" + separator + "y1" + separator + "x2" + separator + "y2" + separator + "total";
 	private Scenario scenario;
-	private TravelStatsHandler handler;
+	private TaxiTravelStatsHandler handler;
 	
-	public TravelStatsAnalyzer(Scenario scenario, Double interval) {
-		super(TravelStatsAnalyzer.class.getSimpleName());
+	public TaxiTravelStatsAnalyzer(Scenario scenario, Double interval) {
+		super(TaxiTravelStatsAnalyzer.class.getSimpleName());
 		this.scenario = scenario;
-		this.handler = new TravelStatsHandler(scenario, interval);
+		this.handler = new TaxiTravelStatsHandler(scenario, interval);
 	}
 
 	@Override
@@ -87,43 +85,42 @@ public class TravelStatsAnalyzer extends AbstractAnalysisModule {
 	@Override
 	public void writeResults(String outputFolder) {
 		
-		HashMap<String, Counts<Link>> mode2countsMap;
+		Counts<Link> mode2countsMap;
 		String countsName;
 		
-		mode2countsMap = this.handler.getMode2CountsCapacity();
-		countsName = TravelStatsAnalyzer.CAPACITY;
+		mode2countsMap = this.handler.getCountsCapacity();
+		countsName = TaxiTravelStatsAnalyzer.CAPACITY;
 		writeCounts(outputFolder, mode2countsMap, countsName);
 		
-		mode2countsMap = this.handler.getMode2CountsCapacity_m();
-		countsName = TravelStatsAnalyzer.CAPACITY_METER;
+		mode2countsMap = this.handler.getCountsCapacity_m();
+		countsName = TaxiTravelStatsAnalyzer.CAPACITY_METER;
 		writeCounts(outputFolder, mode2countsMap, countsName);
 		
-		mode2countsMap = this.handler.getMode2CountsPax();
-		countsName = TravelStatsAnalyzer.PAX;
+		mode2countsMap = this.handler.getCountsPax();
+		countsName = TaxiTravelStatsAnalyzer.PAX;
 		writeCounts(outputFolder, mode2countsMap, countsName);
 		
-		mode2countsMap = this.handler.getMode2CountsPax_m();
-		countsName = TravelStatsAnalyzer.PAX_METER;
+		mode2countsMap = this.handler.getCountsPax_m();
+		countsName = TaxiTravelStatsAnalyzer.PAX_METER;
 		writeCounts(outputFolder, mode2countsMap, countsName);
 		
-		mode2countsMap = this.handler.getMode2CountsVehicles();
-		countsName = TravelStatsAnalyzer.VEHICLE;
+		mode2countsMap = this.handler.getCountsVehicles();
+		countsName = TaxiTravelStatsAnalyzer.VEHICLE;
 		writeCounts(outputFolder, mode2countsMap, countsName);
 		
-		this.writeOccupancy(outputFolder, this.handler.getMode2CountsPax(), this.handler.getMode2CountsCapacity());
+		this.writeOccupancy(outputFolder, this.handler.getCountsPax(), this.handler.getCountsCapacity());
 	}
 
 	/**
 	 * Write the counts data to file
 	 * 
 	 * @param outputFolder
-	 * @param mode2countsMap
+	 * @param countsMap
 	 * @param countsName
 	 */
-	private void writeCounts(String outputFolder, HashMap<String, Counts<Link>> mode2countsMap, String countsName) {
-		for (Entry<String, Counts<Link>> mode2Counts : mode2countsMap.entrySet()) {
+	private void writeCounts(String outputFolder, Counts<Link> countsMap, String countsName) {
 			
-			String fileName = outputFolder + this.createFilename(mode2Counts.getKey(), countsName);
+			String fileName = outputFolder + this.createFilename(TAXI_PREFIX, countsName);
 
 			try {
 				BufferedWriter bw = new BufferedWriter(new FileWriter(new File(fileName)));
@@ -132,7 +129,7 @@ public class TravelStatsAnalyzer extends AbstractAnalysisModule {
 					bw.write(separator + i);
 				}
 				
-				for (Entry<Id<Link>, Count<Link>> linkId2Count : mode2Counts.getValue().getCounts().entrySet()) {
+				for (Entry<Id<Link>, Count<Link>> linkId2Count : countsMap.getCounts().entrySet()) {
 					// write one line for each count station registered - links without any count will not be written to file
 					bw.newLine();
 					bw.write(linkId2Count.getKey().toString());
@@ -165,7 +162,6 @@ public class TravelStatsAnalyzer extends AbstractAnalysisModule {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}		
-		}
 	}
 	
 	/**
@@ -177,10 +173,9 @@ public class TravelStatsAnalyzer extends AbstractAnalysisModule {
 	 * @param paxCounts
 	 * @param capCounts
 	 */
-	private void writeOccupancy(String outputFolder, HashMap<String, Counts<Link>> paxCounts, HashMap<String, Counts<Link>> capCounts) {
-		for (Entry<String, Counts<Link>> mode2CapCounts : capCounts.entrySet()) {
+	private void writeOccupancy(String outputFolder, Counts<Link> paxCounts, Counts<Link> capCounts) {
 			
-			String fileName = outputFolder + this.createFilename(mode2CapCounts.getKey(), TravelStatsAnalyzer.OCCUPANCY);
+			String fileName = outputFolder + this.createFilename(TAXI_PREFIX, TaxiTravelStatsAnalyzer.OCCUPANCY);
 
 			try {
 				BufferedWriter bw = new BufferedWriter(new FileWriter(new File(fileName)));
@@ -189,7 +184,7 @@ public class TravelStatsAnalyzer extends AbstractAnalysisModule {
 					bw.write(separator + i);
 				}
 				
-				for (Entry<Id<Link>, Count<Link>> linkId2Count : mode2CapCounts.getValue().getCounts().entrySet()) {
+				for (Entry<Id<Link>, Count<Link>> linkId2Count : capCounts.getCounts().entrySet()) {
 					// write one line for each count station registered - links without any count will not be written to file
 					bw.newLine();
 					bw.write(linkId2Count.getKey().toString());
@@ -206,7 +201,7 @@ public class TravelStatsAnalyzer extends AbstractAnalysisModule {
 					}
 					
 					double paxSum = 0.0;
-					for (Volume volume : paxCounts.get(mode2CapCounts.getKey()).getCounts().get(linkId2Count.getKey()).getVolumes().values()) {
+					for (Volume volume : paxCounts.getCounts().get(linkId2Count.getKey()).getVolumes().values()) {
 						paxSum += volume.getValue();
 					}
 					
@@ -223,8 +218,8 @@ public class TravelStatsAnalyzer extends AbstractAnalysisModule {
 						}
 						
 						double paxValue = 0.0;
-						if(paxCounts.get(mode2CapCounts.getKey()).getCounts().get(linkId2Count.getKey()).getVolume(i) != null){
-							paxValue = paxCounts.get(mode2CapCounts.getKey()).getCounts().get(linkId2Count.getKey()).getVolume(i).getValue();
+						if(paxCounts.getCounts().get(linkId2Count.getKey()).getVolume(i) != null){
+							paxValue = paxCounts.getCounts().get(linkId2Count.getKey()).getVolume(i).getValue();
 						}
 						
 						if (capValue == 0.0) {
@@ -242,7 +237,6 @@ public class TravelStatsAnalyzer extends AbstractAnalysisModule {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}		
-		}
 	}
 	
 	private String createFilename(String mode, String identifier){
