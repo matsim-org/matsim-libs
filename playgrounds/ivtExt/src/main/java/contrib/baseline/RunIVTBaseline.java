d621cbb5fb5522a006c370d24dc0eae5d1185e2c
boescpa <patrick.boesch@ivt.baug.ethz.ch>
2016-08-23 10:04:52 +0200

    Debugging and updating.

--- playgrounds/ivtExt/src/main/java/contrib/baseline/RunIVTBaseline.java
+++ playgrounds/ivtExt/src/main/java/contrib/baseline/RunIVTBaseline.java
@@ -1,14 +1,13 @@
 package contrib.baseline;
 
+import contrib.baseline.counts.*;
+import com.google.inject.name.Names;
 import contrib.baseline.lib.F2LConfigGroup;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.controler.Controler;
-import org.matsim.core.controler.MatsimServices;
-import org.matsim.core.controler.OutputDirectoryHierarchy;
-import org.matsim.core.controler.OutputDirectoryLogging;
+import org.matsim.core.controler.*;
 import org.matsim.core.router.StageActivityTypesImpl;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.facilities.ActivityFacilities;
@@ -27,36 +26,61 @@ import playground.ivt.replanning.BlackListedTimeAllocationMutatorStrategyModule;
 public class RunIVTBaseline {
 
     public static void main(String[] args) {
-        final String configFile = args[0];
+		final String configFile = args[0];
+		final String pathToPTLinksMonitorList = args[1];
+		final String pathToPTStationsMonitorList = args[2];
+		/*final String pathToStreetLinksDailyToMonitor = args[3];
+		final String pathToStreetLinksHourlyToMonitor = args[4];*/
 
-        // This allows to get a log file containing the log messages happening
-        // before controler init.
-        OutputDirectoryLogging.catchLogEntries();
+		// This allows to get a log file containing the log messages happening
+		// before controler init.
+		OutputDirectoryLogging.catchLogEntries();
 
-        // It is suggested to use the config created by playground/boescpa/baseline/ConfigCreator.java.
-        final Config config = ConfigUtils.loadConfig(configFile, new BlackListedTimeAllocationMutatorConfigGroup(), new F2LConfigGroup());
+		// It is suggested to use the config created by playground/boescpa/baseline/ConfigCreator.java.
+		final Config config = ConfigUtils.loadConfig(configFile, new BlackListedTimeAllocationMutatorConfigGroup(), new F2LConfigGroup());
+		config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.failIfDirectoryExists);
+		final Scenario scenario = ScenarioUtils.loadScenario(config);
+		final Controler controler = new Controler(scenario);
 
-        final Scenario scenario = ScenarioUtils.loadScenario(config);
-        final Controler controler = new Controler(scenario);
-
-        controler.getConfig().controler().setOverwriteFileSetting(
-                OutputDirectoryHierarchy.OverwriteFileSetting.failIfDirectoryExists);
-
-        connectFacilitiesWithNetwork(controler);
+		connectFacilitiesWithNetwork(controler);
 
 		// We use a time allocation mutator that allows to exclude certain activities.
 		controler.addOverridingModule(new BlackListedTimeAllocationMutatorStrategyModule());
 		// We use a specific scoring function, that uses individual preferences for activity durations.
 		controler.setScoringFunctionFactory(
-                new IVTBaselineScoringFunctionFactory(controler.getScenario(),
-                        new StageActivityTypesImpl(PtConstants.TRANSIT_ACTIVITY_TYPE)));
+				new IVTBaselineScoringFunctionFactory(controler.getScenario(),
+						new StageActivityTypesImpl(PtConstants.TRANSIT_ACTIVITY_TYPE)));
 
-        controler.run();
+		// Add PT-Counts creator:
+		controler.addOverridingModule(new AbstractModule() {
+			@Override
+			public void install() {
+				this.addControlerListenerBinding().to(CountsIVTBaseline.class);
+				this.bind(PTLinkCountsEventHandler.class);
+				bind(String.class)
+						.annotatedWith(Names.named("pathToPTLinksToMonitor"))
+						.toInstance(pathToPTLinksMonitorList);
+				this.bind(PTStationCountsEventHandler.class);
+				bind(String.class)
+						.annotatedWith(Names.named("pathToPTStationsToMonitor"))
+						.toInstance(pathToPTStationsMonitorList);
+				/*this.bind(StreetLinkDailyCountsEventHandler.class);
+				bind(String.class)
+						.annotatedWith(Names.named("pathToStreetLinksDailyToMonitor"))
+						.toInstance(pathToStreetLinksDailyToMonitor);
+				this.bind(StreetLinkHourlyCountsEventHandler.class);
+				bind(String.class)
+						.annotatedWith(Names.named("pathToStreetLinksHourlyToMonitor"))
+						.toInstance(pathToStreetLinksHourlyToMonitor);*/
+			}
+		});
+
+		controler.run();
     }
 
     public static void connectFacilitiesWithNetwork(MatsimServices controler) {
         ActivityFacilities facilities = controler.getScenario().getActivityFacilities();
-        Network network = (Network) controler.getScenario().getNetwork();
+        Network network = controler.getScenario().getNetwork();
         WorldConnectLocations wcl = new WorldConnectLocations(controler.getConfig());
         wcl.connectFacilitiesWithLinks(facilities, network);
     }
