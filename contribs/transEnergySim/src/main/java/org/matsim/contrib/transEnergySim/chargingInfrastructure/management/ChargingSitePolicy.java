6820c9c8999fb064ea0af5cd6c31522c24cfbb1d
Colin Sheppard <colinthegaunt@gmail.com>
2016-08-24 17:30:19 -0700

    changes to refine cost calculations

--- contribs/transEnergySim/src/main/java/org/matsim/contrib/transEnergySim/chargingInfrastructure/management/ChargingSitePolicy.java
+++ contribs/transEnergySim/src/main/java/org/matsim/contrib/transEnergySim/chargingInfrastructure/management/ChargingSitePolicy.java
@@ -2,10 +2,11 @@ package org.matsim.contrib.transEnergySim.chargingInfrastructure.management;
 
 import org.matsim.contrib.transEnergySim.chargingInfrastructure.stationary.ChargingLevel;
 import org.matsim.contrib.transEnergySim.chargingInfrastructure.stationary.ChargingPlugType;
+import org.matsim.contrib.transEnergySim.vehicles.api.VehicleWithBattery;
 
 public interface ChargingSitePolicy {
 	double getParkingCost(double time, double duration);
 	
-	double getChargingCost(double time, double duration, ChargingPlugType plugType);
+	double getChargingCost(double time, double duration, ChargingPlugType plugType, VehicleWithBattery vehicle);
 }
 
