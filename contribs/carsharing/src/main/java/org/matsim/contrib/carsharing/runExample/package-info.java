144f9175ca60a72f0d2881ae54a8cb18e71a76c9
balacmi <milos.balac@ivt.baug.ethz.ch>
2016-09-02 10:29:31 +0200

    Added package info for the example run

---
+++ contribs/carsharing/src/main/java/org/matsim/contrib/carsharing/runExample/package-info.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+/**
+ * @author balacm
+ *
+ *
+ * <h2>Usage</h2>
+ * An example input files can be found in the input folder of the test folder for carsharing. 
+ * <p>
+ * Main method of the RunExample class takes only the config file as an input in order to run the carsharing module.
+ * <p>
+ * 
+ * <h2>Input files</h2>
+ * 
+ *  Necessary input files include:
+ *  <ul>
+ *  <li>Location of stations (or vehicles), per company, Example can be seen in CarsharingStations.xml </li>
+ *  <li>Membership information per person, per company per carsharing type. Example can be seen in CSMembership.xml </li>
+ *  <li>Config file where we set up the paths to the above mentioned input files and whether or not specific carsharing options are used.
+ *  <p> Additionally in the config file we have to specify carsharing specific replanning strategies. An example can be seen in config.xml. </li>
+ *  </ul>
+ *  
+ *  
+ */
+package org.matsim.contrib.carsharing.runExample;
