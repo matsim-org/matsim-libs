37989b76d5e3674b02c91581677b3e26fd23bb09
Sebastian Hörl <hoerl.sebastian@gmail.com>
2016-09-09 17:49:22 +0200

    single taxi, two sequential pickups

--- playgrounds/ivtExt/src/main/java/playground/sebhoerl/avtaxi/optimizer/AVTaxiOptimizerFactory.java
+++ playgrounds/ivtExt/src/main/java/playground/sebhoerl/avtaxi/optimizer/AVTaxiOptimizerFactory.java
@@ -3,11 +3,22 @@ package playground.sebhoerl.avtaxi.optimizer;
 import org.matsim.contrib.taxi.optimizer.TaxiOptimizer;
 import org.matsim.contrib.taxi.optimizer.TaxiOptimizerContext;
 import org.matsim.contrib.taxi.optimizer.TaxiOptimizerFactory;
+import org.matsim.contrib.taxi.run.TaxiConfigGroup;
+import org.matsim.contrib.taxi.scheduler.TaxiSchedulerParams;
 import org.matsim.core.config.ConfigGroup;
 
+import com.google.inject.Inject;
+
 public class AVTaxiOptimizerFactory implements TaxiOptimizerFactory {
+	final private TaxiSchedulerParams params;
+	
+	@Inject
+	public AVTaxiOptimizerFactory(TaxiConfigGroup config) {
+		this.params = new TaxiSchedulerParams(config);
+	}
+	
 	@Override
 	public TaxiOptimizer createTaxiOptimizer(TaxiOptimizerContext optimContext, ConfigGroup optimizerConfigGroup) {
-		return new AVTaxiOptimizer(optimContext);
+		return new AVTaxiOptimizer(optimContext, params);
 	}
 }
