701f9d45dede487f0eda4c485256bb823eab9c93
Johan W Joubert <johan.joubert@up.ac.za>
2016-09-20 17:26:30 +0200

    Clean up import statements

--- playgrounds/nmviljoen/src/main/java/playground/nmviljoen/digicore/EdgeListGenerator.java
+++ playgrounds/nmviljoen/src/main/java/playground/nmviljoen/digicore/EdgeListGenerator.java
@@ -30,7 +30,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Coord;
