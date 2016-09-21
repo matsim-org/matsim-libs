ca039d628947028d2360a126cb638d5f10723d75
Michael Zilske <michael.zilske@tu-berlin.de>
2016-09-21 12:35:38 +0200

    Find correct CS for cemdap-scenario ... it's actually in the config file

--- playgrounds/mzilske/src/main/java/playground/mzilske/sotm/ConvertAndViewNetwork.java
+++ playgrounds/mzilske/src/main/java/playground/mzilske/sotm/ConvertAndViewNetwork.java
@@ -4,20 +4,22 @@ import org.matsim.api.core.v01.Scenario;
 import org.matsim.contrib.otfvis.OTFVis;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.network.io.MatsimNetworkReader;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.geometry.transformations.TransformationFactory;
+import org.matsim.core.utils.io.OsmNetworkReader;
 import org.matsim.vis.otfvis.OTFVisConfigGroup;
 
-public class RunCemdapBerlin {
+public class ConvertAndViewNetwork {
 
 	public static void main(String[] args) {
-		Config config = ConfigUtils.loadConfig("/Users/michaelzilske/wurst/berlin-1pct/config_be_1pct.xml");
+		Config config = ConfigUtils.createConfig();
 		OTFVisConfigGroup otfVisConfigGroup = ConfigUtils.addOrGetModule(config, OTFVisConfigGroup.GROUP_NAME, OTFVisConfigGroup.class);
 		otfVisConfigGroup.setMapOverlayMode(true);
-		Scenario scenario = ScenarioUtils.loadScenario(config);
-
+		config.global().setCoordinateSystem("EPSG:31468");
+		Scenario scenario = ScenarioUtils.createScenario(config);
+		new OsmNetworkReader(scenario.getNetwork(), TransformationFactory.getCoordinateTransformation("WGS84", "EPSG:31468"), true).parse("/Users/michaelzilske/wurst/hermann.osm.xml");
 		OTFVis.playScenario(scenario);
-
 	}
 
 }
--- playgrounds/mzilske/src/main/java/playground/mzilske/sotm/ViewNetwork.java
+++ playgrounds/mzilske/src/main/java/playground/mzilske/sotm/ViewNetwork.java
@@ -1,23 +1,26 @@
 package playground.mzilske.sotm;
 
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Network;
 import org.matsim.contrib.otfvis.OTFVis;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.network.NetworkUtils;
+import org.matsim.core.network.io.MatsimNetworkReader;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.geometry.transformations.TransformationFactory;
 import org.matsim.vis.otfvis.OTFVisConfigGroup;
 
-public class RunCemdapBerlin {
+public class ViewNetwork {
 
 	public static void main(String[] args) {
-		Config config = ConfigUtils.loadConfig("/Users/michaelzilske/wurst/berlin-1pct/config_be_1pct.xml");
+		Config config = ConfigUtils.createConfig();
 		OTFVisConfigGroup otfVisConfigGroup = ConfigUtils.addOrGetModule(config, OTFVisConfigGroup.GROUP_NAME, OTFVisConfigGroup.class);
 		otfVisConfigGroup.setMapOverlayMode(true);
-		Scenario scenario = ScenarioUtils.loadScenario(config);
-
+		config.global().setCoordinateSystem(TransformationFactory.DHDN_GK4);
+		Scenario scenario = ScenarioUtils.createScenario(config);
+		new MatsimNetworkReader(scenario.getNetwork()).readFile("/Users/michaelzilske/wurst/hermann-wurst.xml");
 		OTFVis.playScenario(scenario);
-
 	}
 
 }
