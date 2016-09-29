787ae4eacd272673101a6193c49d4bc749ffbdf4
Amit Agarwal <amit.agarwal@campus.tu-berlin.de>
2016-09-29 22:19:00 +0200

    equil-mixed traffic can be run directly from config

--- matsim/src/test/java/tutorial/config/ExamplesByConfigfileTest.java
+++ matsim/src/test/java/tutorial/config/ExamplesByConfigfileTest.java
@@ -21,14 +21,11 @@ package tutorial.config;
 import java.io.File;
 import java.util.Arrays;
 import java.util.Collection;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 import org.matsim.core.utils.io.IOUtils;
-
-import tutorial.config.RunFromConfigfileExample;
 
 /**
  * @author nagel
@@ -45,15 +42,15 @@ public class ExamplesByConfigfileTest {
 	
 	@Parameters(name = "{index}: configFilename == {0};")
 	public static Collection<Object[]> createTests() {
-		int nConfigFiles = 5 ;
+		int nConfigFiles = 6 ;
 		Object [][] files2run = new Object [nConfigFiles][1]; 
 		files2run[0] = new Object [] {"examples/equil/config.xml"};
 		files2run[1] = new Object [] {"examples/equil-extended/config.xml"};
 		files2run[2] = new Object [] {"examples/tutorial/config/example1-config.xml"} ;
 		files2run[3] = new Object [] {"examples/tutorial/config/example5-config.xml"} ;
 		files2run[4] = new Object [] {"examples/tutorial/config/example5trips-config.xml"} ;
+		files2run[5] = new Object [] {"examples/equil-mixedTraffic/config.xml"};
 //		files2run[5] = new Object [] {"examples/equil-extended/config-with-roadpricing.xml"}; // yyyy throws exception; should be investigated and fixed.  (This can not work from core matsim after roadpricing was moved into a contrib!) kai, sep'16
-//		files2run[3] = new Object [] {"examples/equil-mixedTraffic/config.xml"}; // yyyy throws exception; should be investigated and fixed.  kai, sep'16
 //		files2run[5] = new Object [] {"examples/tutorial/config/example2-config.xml"} ; // yyyy throws exception; should be investigated and fixed.  kai, sep'16
 //		files2run[8] = new Object [] {"examples/tutorial/config/externalReplanning.xml"} ; // yyyy throws exception; should be investigated and fixed.  kai, sep'16 
 //		files2run[2] = new Object [] {"examples/equil-extended/config-with-network-change-events.xml"}; // yyyy runs forever; should be investigated and fixed.  kai, sep'16
