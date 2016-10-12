90303a6151596f8f2607779ebaeed53a561dd095
ikaddoura <kaddoura@vsp.tu-berlin.de>
2016-10-12 10:44:40 +0200

    some incident replanning adjustments

--- playgrounds/ikaddoura/src/main/java/playground/ikaddoura/incidents/incidentWithinDayReplanning/IncidentWithinDayReplanning.java
+++ playgrounds/ikaddoura/src/main/java/playground/ikaddoura/incidents/incidentWithinDayReplanning/IncidentWithinDayReplanning.java
@@ -29,6 +29,7 @@ import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.PopulationWriter;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.AbstractModule;
@@ -49,11 +50,18 @@ public class IncidentWithinDayReplanning {
 
 // ############################################################################################################################################
 
-	private static String baseCaseOutputDirectory = "../../../runs-svn/incidents/output/baseCase/";
-	private static String withinDayReplanningOutputDirectory = "../../../runs-svn/incidents/output/2016-03-15_withinDayReplanning/";
-	private static String networkChangeEventsFile = "../../../runs-svn/incidents/input/networkChangeEvents_2016-03-15.xml.gz";
+	private static String day = "2016-03-15";
+
+	private static String populationFile = "../../../runs-svn/incidents/input/bvg.run189.10pct.100.plans.selected.genericPt_baseCase100it_output_plans.xml.gz";
+	private static String networkFile = "../../../runs-svn/incidents/input/network.xml";
+	private static String configFile = "../../../runs-svn/incidents/input/baseCase_output_config_updated.xml";
+	private static String networkChangeEventsFile = "../../../runs-svn/incidents/input/networkChangeEvents_" + day + ".xml.gz";
+	private static String runOutputBaseDirectory = "../../../runs-svn/incidents/output/";
+		
+	private static final boolean reducePopulationToAffectedAgents = true;
+	private static final String reducedPopulationFile = "../../../runs-svn/incidents/input/" + day + "_reduced-population.xml.gz";
 	
-	private static final boolean reducePopulationToAffectedAgents = false;
+	private static final boolean applyNetworkChangeEventsAndRunWithinDayReplanning = false;
 		
 // ############################################################################################################################################
 	
@@ -63,14 +71,7 @@ public class IncidentWithinDayReplanning {
 		
 		if (args.length > 0) {
 			
-			baseCaseOutputDirectory = args[0];		
-			log.info("base case output directory: "+ baseCaseOutputDirectory);
-
-			withinDayReplanningOutputDirectory = args[1];		
-			log.info("within-day replanning output directory: "+ withinDayReplanningOutputDirectory);
-
-			networkChangeEventsFile = args[2];		
-			log.info("network change events file: "+ networkChangeEventsFile);
+			// TODO
 			
 		}
 		
@@ -81,60 +82,76 @@ public class IncidentWithinDayReplanning {
 	private void run() {
 		OutputDirectoryLogging.catchLogEntries();
 		try {
-			OutputDirectoryLogging.initLoggingWithOutputDirectory(withinDayReplanningOutputDirectory);
+			OutputDirectoryLogging.initLoggingWithOutputDirectory(runOutputBaseDirectory);
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
 		
-		final Config config = ConfigUtils.loadConfig(baseCaseOutputDirectory + "output_config.xml.gz");
-		
-		// consider the network change events
-		config.network().setInputFile(baseCaseOutputDirectory + "output_network.xml.gz");
-		config.network().setChangeEventsInputFile(networkChangeEventsFile);
-		config.network().setTimeVariantNetwork(true);
-		
-		config.plans().setInputFile(baseCaseOutputDirectory + "output_plans.xml.gz");
+		final Config config = ConfigUtils.loadConfig(configFile);
+		config.network().setInputFile(networkFile);
 		config.plans().setRemovingUnneccessaryPlanAttributes(true);
-		
 		config.controler().setFirstIteration(0);
 		config.controler().setLastIteration(0);
-		config.controler().setOutputDirectory(withinDayReplanningOutputDirectory);
+				
+		if (applyNetworkChangeEventsAndRunWithinDayReplanning) {
+			
+			config.controler().setOutputDirectory(runOutputBaseDirectory + day + "_within-day-replanning" + "/");
+			
+			// consider the network change events
+			config.network().setChangeEventsInputFile(networkChangeEventsFile);
+			config.network().setTimeVariantNetwork(true);
+			
+			config.plans().setInputFile(populationFile);
+			
+		} else {
+			config.controler().setOutputDirectory(runOutputBaseDirectory + day + "_baseCase" + "/");
+
+			if (reducePopulationToAffectedAgents) {
+				// use the reduced population
+				config.plans().setInputFile(reducedPopulationFile);
+			} else {
+				config.plans().setInputFile(populationFile);
+			}
+		}
 				
 		final Scenario scenario = ScenarioUtils.loadScenario(config);
-				
-		if (reducePopulationToAffectedAgents) {
-			
-			log.info("Reducing the population size from " + scenario.getPopulation().getPersons().size() + "...");
 
-			Set<Id<Link>> incidentLinkIds = NetworkChangeEventsUtils.getIncidentLinksFromNetworkChangeEventsFile(scenario);
-			Set<Id<Person>> personIdsToKeepInPopulation = NetworkChangeEventsUtils.getPersonIDsOfAgentsDrivingAlongSpecificLinks(scenario, incidentLinkIds);
-			NetworkChangeEventsUtils.filterPopulation(scenario, personIdsToKeepInPopulation);
-
-			log.info("... to " + scenario.getPopulation().getPersons().size() + " agents (= those agents driving along incident links).");
-
-		}
-
-		// start within day replanning
-		
 		final Controler controler = new Controler(scenario);
 		controler.getConfig().controler().setOverwriteFileSetting(OverwriteFileSetting.overwriteExistingFiles);
 		
-		Set<String> analyzedModes = new HashSet<>();
-		analyzedModes.add(TransportMode.car);
-		final TravelTimeCollector travelTime = new TravelTimeCollector(controler.getScenario(), analyzedModes);
-						
-		controler.addOverridingModule( new AbstractModule() {
-			@Override public void install() {
-				
-				this.bind(IncidentBestRouteMobsimListener.class).asEagerSingleton();
-				this.addMobsimListenerBinding().to(IncidentBestRouteMobsimListener.class);
-				this.addControlerListenerBinding().to(IncidentBestRouteMobsimListener.class);
+		if (applyNetworkChangeEventsAndRunWithinDayReplanning) {
+			
+			if (reducePopulationToAffectedAgents) {
+				log.info("Reducing the population size from " + scenario.getPopulation().getPersons().size() + "...");
 
-				this.bind(TravelTime.class).toInstance(travelTime);
-				this.addEventHandlerBinding().toInstance(travelTime);
-				this.addMobsimListenerBinding().toInstance(travelTime);
+				Set<Id<Link>> incidentLinkIds = NetworkChangeEventsUtils.getIncidentLinksFromNetworkChangeEventsFile(scenario);
+				Set<Id<Person>> personIdsToKeepInPopulation = NetworkChangeEventsUtils.getPersonIDsOfAgentsDrivingAlongSpecificLinks(scenario, incidentLinkIds);
+				NetworkChangeEventsUtils.filterPopulation(scenario, personIdsToKeepInPopulation);
+
+				log.info("... to " + scenario.getPopulation().getPersons().size() + " agents (= those agents driving along incident links).");
+				PopulationWriter writer = new PopulationWriter(scenario.getPopulation());
+				writer.write(reducedPopulationFile);
 			}
-		}) ;
+			
+			// start within day replanning
+
+			Set<String> analyzedModes = new HashSet<>();
+			analyzedModes.add(TransportMode.car);
+			final TravelTimeCollector travelTime = new TravelTimeCollector(controler.getScenario(), analyzedModes);
+							
+			controler.addOverridingModule( new AbstractModule() {
+				@Override public void install() {
+					
+					this.bind(IncidentBestRouteMobsimListener.class).asEagerSingleton();
+					this.addMobsimListenerBinding().to(IncidentBestRouteMobsimListener.class);
+					this.addControlerListenerBinding().to(IncidentBestRouteMobsimListener.class);
+
+					this.bind(TravelTime.class).toInstance(travelTime);
+					this.addEventHandlerBinding().toInstance(travelTime);
+					this.addMobsimListenerBinding().toInstance(travelTime);
+				}
+			}) ;
+		}
 				
 		controler.run();		
 	}
