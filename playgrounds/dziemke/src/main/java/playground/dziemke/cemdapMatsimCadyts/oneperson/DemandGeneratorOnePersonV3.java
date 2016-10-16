df10f8088bcc3e80b25db092a0f5b723dec0a9ae
dziemke <ziemke@vsp.tu-berlin.de>
2016-10-16 23:38:29 +0200

    minor revisions

--- playgrounds/dziemke/src/main/java/playground/dziemke/cemdapMatsimCadyts/oneperson/DemandGeneratorOnePersonV3.java
+++ playgrounds/dziemke/src/main/java/playground/dziemke/cemdapMatsimCadyts/oneperson/DemandGeneratorOnePersonV3.java
@@ -1,3 +1,21 @@
+/* *********************************************************************** *
+ * project: org.matsim.*												   *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
 package playground.dziemke.cemdapMatsimCadyts.oneperson;
 
 import java.io.BufferedWriter;
@@ -18,6 +36,9 @@ import playground.dziemke.cemdapMatsimCadyts.CommuterRelation;
 import playground.dziemke.utils.LogToOutputSaver;
 import playground.dziemke.utils.TwoAttributeShapeReader;
 
+/**
+ * @author dziemke
+ */
 public class DemandGeneratorOnePersonV3 {
 	private static final Logger log = Logger.getLogger(DemandGeneratorOnePersonV3.class);
 
