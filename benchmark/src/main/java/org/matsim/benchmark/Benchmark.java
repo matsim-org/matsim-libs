2fa1bd2c99eea82077b7c212e0c5c273e84fcdb4
Michael Zilske <michael.zilske@tu-berlin.de>
2016-10-17 18:59:10 +0200

    Benchmark is its own artifact

---
+++ benchmark/src/main/java/org/matsim/benchmark/Benchmark.java
@@ -0,0 +1,39 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2010 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.benchmark;
+
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.utils.io.IOUtils;
+import org.matsim.examples.ExamplesUtils;
+
+import java.io.IOException;
+
+public class Benchmark {
+
+	public static void main(String[] args) throws IOException {
+		Config config = ConfigUtils.loadConfig(IOUtils.newUrl(ExamplesUtils.getTestScenarioURL("benchmark"), "config.xml"));
+		Controler ctl = new Controler(config);
+        ctl.getConfig().controler().setCreateGraphs(false);
+        ctl.run();
+	}
+
+}
