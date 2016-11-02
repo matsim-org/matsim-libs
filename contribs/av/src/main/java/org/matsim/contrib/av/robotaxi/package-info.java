1c8c4f648caa4b199a9d7fd01481197f847c2c5b
jfbischoff <bischoff@vsp.tu-berlin.de>
2016-11-02 17:29:49 +0100

    AV Contrib, first additions

---
+++ contribs/av/src/main/java/org/matsim/contrib/av/robotaxi/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The contents of this packages provides a tool set to simulate large-scale taxi simulations with huge fleets (100.000+ are possible).
+ * For a use case and reference, see 
+ * J. Bischoff, M. Maciejewski, Simulation of City-wide Replacement of Private Cars with Autonomous Taxis in Berlin, 
+ * Procedia Computer Science, Volume 83, 2016, Pages 237-244, ISSN 1877-0509, 
+ * <a href="http://dx.doi.org/10.1016/j.procs.2016.04.121">http://dx.doi.org/10.1016/j.procs.2016.04.121</a>
+ */
+package org.matsim.contrib.av.robotaxi;
