fb5c3362f129100fea6a36c6b1c0bd060e731c0f
leocamusc <leocamusc@gmail.com>
2016-11-09 08:37:00 -0300

    cleaning

--- playgrounds/santiago/src/main/java/playground/santiago/analysis/SantiagoAnalysis.java
+++ playgrounds/santiago/src/main/java/playground/santiago/analysis/SantiagoAnalysis.java
@@ -11,30 +11,28 @@ import playground.agarwalamit.utils.LoadMyScenarios;
 import playground.benjamin.scenarios.munich.analysis.filter.UserGroup;
 import playground.benjamin.scenarios.munich.analysis.modular.legModeDistanceDistribution.RunLegModeDistanceDistribution;
 
+/** 
+ * 
+ * Class to write ALL THE NECESSARY ANALYSIS FILES
+ * 
+ */
+
 public class SantiagoAnalysis {
 	
-	private static final String RUN_DIR = "../../../runs-svn/santiago/baseCase10pct/";
-	private static final String OUTPUT_FOLDER = RUN_DIR + "output/";	
+	private static final String CASE_NAME = "baseCase10pct";
+	
+	private static final String RUN_DIR = "../../../runs-svn/santiago/" + CASE_NAME + "/";
+	private static final String OUTPUT_FOLDER = RUN_DIR + "output_sim7/";	
 	private static final String ANALYSIS_DIR = OUTPUT_FOLDER + "analysis/";
 	
-	
-
-//	private static final String OUTPUT_TEMP = "../../../runs-svn/santiago/baseCase10pct/OLD/outputSim1/";
-//	private static final String ANALYSIS_DIR_TEMP = OUTPUT_TEMP + "analysis/";
-	
-	private static final String IT_NUMBER = "0";
+	private static final String IT_NUMBER = "100";
 	private static final String IT_FOLDER = OUTPUT_FOLDER + "ITERS/it." + IT_NUMBER + "/";	
 	
-//	private static final String IT_FOLDER_TEMP = OUTPUT_TEMP + "ITERS/it." + IT_NUMBER + "/";
+	private static final String EVENTS = IT_FOLDER + IT_NUMBER +".events.xml.gz";	
+	private static final String PLANS = IT_FOLDER + IT_NUMBER +".plans.xml.gz";
+	private static final String CONFIG_FILE = RUN_DIR + "config_" + CASE_NAME + "_sim7.xml";
 	
-	
-	private static final String EVENTS = IT_FOLDER + IT_NUMBER +".events.xml.gz";
-	
-//	private static final String EVENTS_TEMP = IT_FOLDER_TEMP + IT_NUMBER +".events.xml.gz";
-	
-//	private static final String PLANS = IT_FOLDER + IT_NUMBER +".plans.xml.gz";
-//	private static final String CONFIG_FILE = RUN_DIR + "config_baseCase1pct.xml";
-//	private static final UserGroup USER_GROUP = null;
+	private static final UserGroup USER_GROUP = null;
 	
 	
 	private static void createDir(File file) {
@@ -44,30 +42,18 @@ public class SantiagoAnalysis {
 	
 	public static void writeModalShare(){
 			
-		File analysisDir = new File(ANALYSIS_DIR);
-		
-//		File analysisDir = new File(ANALYSIS_DIR_TEMP);
-		
-		
+		File analysisDir = new File(ANALYSIS_DIR);		
 		String eventsFile = EVENTS;	
-//		String eventsFile = EVENTS_TEMP;
+
+		if(!analysisDir.exists()) createDir(analysisDir);	
 		
-		
-//		String plansFile = PLANS;
-		if(!analysisDir.exists()) createDir(analysisDir);			
-			String outputFile = ANALYSIS_DIR +"modalSplit_It"+ IT_NUMBER  + ".txt";
+		String outputFile = ANALYSIS_DIR +"modalSplit_It"+ IT_NUMBER  + ".txt";
+
 			
-//			String outputFile = ANALYSIS_DIR_TEMP +"modalSplit_It"+ IT_NUMBER  + ".txt";
-			
-			ModalShareFromEvents msc = new ModalShareFromEvents(eventsFile);
-			msc.run();
-			msc.writeResults(outputFile);
-			
-//			Scenario sc = LoadMyScenarios.loadScenarioFromPlans(plansFile);
-//			ModalShareFromPlans msg = new ModalShareFromPlans(sc.getPopulation());
-//			msg.run();
-//			msg.writeResults(outputFile);
-			
+		ModalShareFromEvents msc = new ModalShareFromEvents(eventsFile);
+		msc.run();
+		msc.writeResults(outputFile);
+
 			
 
 	}
@@ -82,17 +68,18 @@ public class SantiagoAnalysis {
 //			mtta.writeResults(outputFile);
 //		}
 //	
-//	public static void writeModeDistanceDistribution(){
-//	RunLegModeDistanceDistribution rlmdd = new RunLegModeDistanceDistribution(OUTPUT_FOLDER, CONFIG_FILE, IT_NUMBER, USER_GROUP);
-//	rlmdd.run();
-//	}
-//	
+	public static void writeModeDistanceDistribution(){
+	RunLegModeDistanceDistribution rlmdd = new RunLegModeDistanceDistribution(OUTPUT_FOLDER, CONFIG_FILE, IT_NUMBER, USER_GROUP);	
+	rlmdd.run();
+	}
+	
 	
 	
 	public static void main (String[]arg){
-		writeModalShare();
+//		writeModalShare();
+		writeModeDistanceDistribution();
 //		writeModalTravelTimes();
-//		writeModeDistanceDistribution();
+
 	}
 	
 
