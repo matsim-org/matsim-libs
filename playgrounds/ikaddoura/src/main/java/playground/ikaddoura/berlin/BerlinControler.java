97465f947feb8ab62b556863ac4d44f77d6d7162
ikaddoura <kaddoura@vsp.tu-berlin.de>
2016-11-14 11:42:17 +0100

    minor changes; adding different constructors

--- playgrounds/ikaddoura/src/main/java/playground/ikaddoura/berlin/BerlinControler.java
+++ playgrounds/ikaddoura/src/main/java/playground/ikaddoura/berlin/BerlinControler.java
@@ -19,12 +19,20 @@
 
 package playground.ikaddoura.berlin;
 
+import java.io.IOException;
+
+import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ActivityParams;
 import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.TypicalDurationScoreComputation;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
+import org.matsim.core.scenario.ScenarioUtils;
+
+import playground.ikaddoura.analysis.detailedPersonTripAnalysis.PersonTripCongestionNoiseAnalysisMain;
+import playground.ikaddoura.analysis.pngSequence2Video.MATSimVideoUtils;
+import playground.ikaddoura.integrationCNE.CNEIntegration;
 
 /**
 * @author ikaddoura
@@ -32,24 +40,26 @@ import org.matsim.core.controler.OutputDirectoryHierarchy;
 
 public class BerlinControler {
 	
-// 	run0
-//	final static String configFile = "../../../public-svn/matsim/scenarios/countries/de/berlin/car-traffic-only-1pct-2014-08-01/config_be_1pct_ik.xml";
-	
-	final private String configFile = "../../../runs-svn/berlin_car-traffic-only-1pct-2014-08-01/run1/input/config1.xml";
-	final private String outputDirectory = "../../../runs-svn/berlin_car-traffic-only-1pct-2014-08-01/run1/output/";
+// 	base case
+	final static String configFile = "../../../runs-svn/berlin_car-traffic-only-1pct-2014-08-01/baseCase/input/config.xml";
+
+	// only reroute - base case continued / pricing 
+//	final private String configFile = "../../../runs-svn/berlin_car-traffic-only-1pct-2014-08-01/onlyReRoute/input/config_test.xml";
+//	final private String outputDirectory = "../../../runs-svn/berlin_car-traffic-only-1pct-2014-08-01/onlyReRoute/output_QBPV3/";
 	
 	final private int activityDurationHRS = 1;
+	final private boolean pricing = false;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		BerlinControler berlin = new BerlinControler();
 		berlin.run();		
 	}
 
-	private void run() {
+	private void run() throws IOException {
 		
 		Config config = ConfigUtils.loadConfig(configFile);
-		config.controler().setOutputDirectory(outputDirectory);
+		final String outputDirectory = config.controler().getOutputDirectory();
 		
 		// home
 		for (int n = 1; n <= 24 ; n = n + this.activityDurationHRS) {
@@ -95,12 +105,24 @@ public class BerlinControler {
 			config.planCalcScore().addActivityParams(params);
 		}
 		
-		Controler controler = new Controler(config);
+		Scenario scenario = ScenarioUtils.loadScenario(config);
+		Controler controler = new Controler(scenario);
+				
+		if (pricing) {
+			CNEIntegration cne = new CNEIntegration(controler);
+			cne.setCongestionPricing(true);
+			controler = cne.prepareControler();
+		}
+		
 		controler.getConfig().controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.overwriteExistingFiles );
-		
-		// TODO: add some analysis
-		
 		controler.run();
+		
+		// analysis
+		
+		PersonTripCongestionNoiseAnalysisMain analysis = new PersonTripCongestionNoiseAnalysisMain(controler.getConfig().controler().getOutputDirectory());
+		analysis.run();
+		
+		MATSimVideoUtils.createLegHistogramVideo(outputDirectory);
 	}
 
 }
