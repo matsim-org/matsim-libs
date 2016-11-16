5200e98785528dbf5595b9e38ff4cdb25b082e2b
Thibaut Dubernet <thibaut.dubernet@ivt.baug.ethz.ch>
2016-11-16 15:03:57 +0100

    test and bugfix

--- playgrounds/thibautd/src/test/java/playground/thibautd/analysis/socialchoicesetconstraints/UtilsTest.java
+++ playgrounds/thibautd/src/test/java/playground/thibautd/analysis/socialchoicesetconstraints/UtilsTest.java
@@ -56,11 +56,73 @@ public class UtilsTest {
 				5, cliques.getNCliquesOfSize( random , Integer.MAX_VALUE , 3 ).size() );
 	}
 
+	@Test
+	public void testCliquesOfSizeSubsample() {
+		final Utils.AllCliques cliques = new Utils.AllCliques();
+
+		cliques.addClique( clique( 1 , 3 ) );
+		cliques.addClique( clique( 4 , 7 ) );
+
+		Assert.assertEquals(
+				"unexpected max clique size",
+				4, cliques.getMaxSize() );
+
+		final Random random = new Random( 123 );
+
+		Assert.assertEquals(
+				"unexpected number of cliques of size 1",
+				5, cliques.getNCliquesOfSize( random , 5 , 1 ).size() );
+
+		Assert.assertEquals(
+				"unexpected number of cliques of size 3",
+				4, cliques.getNCliquesOfSize( random , 4 , 3 ).size() );
+	}
+
 	private static Set<Id<Person>> clique( final int min, final int max ) {
 		final Set<Id<Person>> set = new HashSet<>();
 		for ( int i=min; i <= max; i++ ) {
 			set.add( Id.createPersonId( i ) );
 		}
 		return set;
+	}
+
+	@Test
+	public void testSparseLongVector() {
+		final Utils.SparseLongVector v = new Utils.SparseLongVector();
+
+		v.swap( 0 , 0 );
+
+		Assert.assertArrayEquals(
+				"unexpected swapped array",
+				new long[]{0,1,2,3,4},
+				v.toArray( 5 ) );
+
+		v.swap( 1 , 3 );
+
+		Assert.assertArrayEquals(
+				"unexpected swapped array",
+				new long[]{0,3,2,1,4},
+				v.toArray( 5 ) );
+
+		v.swap( 2 , 3 );
+
+		Assert.assertArrayEquals(
+				"unexpected swapped array",
+				new long[]{0,3,1,2,4},
+				v.toArray( 5 ) );
+
+		v.swap( 3 , 4 );
+
+		Assert.assertArrayEquals(
+				"unexpected swapped array",
+				new long[]{0,3,1,4,2},
+				v.toArray( 5 ) );
+
+		v.swap( 4 , 4 );
+
+		Assert.assertArrayEquals(
+				"unexpected swapped array",
+				new long[]{0,3,1,4,2},
+				v.toArray( 5 ) );
 	}
 }
