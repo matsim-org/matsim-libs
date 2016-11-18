6918f873eeb96aec74b4385fa541bedcf07dae74
kainagel <nagel@vsp.tu-berlin.de>
2016-11-18 16:46:48 +0100

    kai playground change

--- playgrounds/kairuns/src/main/java/playground/kairuns/run/KNTransitControler.java
+++ playgrounds/kairuns/src/main/java/playground/kairuns/run/KNTransitControler.java
@@ -18,45 +18,28 @@
  *                                                                         *
  * *********************************************************************** */
 
-package playground.kai.otfvis;
+package playground.kairuns.run;
 
-import com.google.inject.Provider;
-
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map.Entry;
 
-import javax.inject.Inject;
-
-import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.api.core.v01.population.Population;
-import org.matsim.contrib.otfvis.OTFVis;
-import org.matsim.contrib.otfvis.OTFVisFileWriterModule;
 import org.matsim.contrib.otfvis.OTFVisLiveModule;
-import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.config.Config;
-import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.config.ConfigReader;
+import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ActivityParams;
 import org.matsim.core.config.groups.QSimConfigGroup;
-import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
-import org.matsim.core.mobsim.framework.Mobsim;
-import org.matsim.core.mobsim.framework.MobsimFactory;
-import org.matsim.core.mobsim.qsim.QSim;
-import org.matsim.core.mobsim.qsim.QSimUtils;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.vis.otfvis.OTFClientLive;
 import org.matsim.vis.otfvis.OTFVisConfigGroup;
-import org.matsim.vis.otfvis.OnTheFlyServer;
 
-public class KNTransitControler {
+class KNTransitControler {
 
 	private static boolean useTransit = true ;
 	private static boolean useOTFVis = true ;
