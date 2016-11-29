16eb4d0752daf9928ee8ba82573e301f3c19c575
Thibaut Dubernet <thibaut.dubernet@ivt.baug.ethz.ch>
2016-11-29 15:39:19 +0100

    add "alone" and "out of home" options

--- playgrounds/thibautd/src/main/java/playground/thibautd/negotiation/locationnegotiation/LocationAlternativesGenerator.java
+++ playgrounds/thibautd/src/main/java/playground/thibautd/negotiation/locationnegotiation/LocationAlternativesGenerator.java
@@ -20,37 +20,56 @@ package playground.thibautd.negotiation.locationnegotiation;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.Population;
 import org.matsim.contrib.socnetsim.framework.population.SocialNetwork;
+import org.matsim.contrib.socnetsim.utils.QuadTreeRebuilder;
+import org.matsim.core.utils.collections.QuadTree;
+import org.matsim.facilities.ActivityFacilities;
+import org.matsim.facilities.ActivityFacility;
 import playground.thibautd.negotiation.framework.AlternativesGenerator;
 import playground.thibautd.negotiation.framework.NegotiationAgent;
+import playground.thibautd.negotiation.framework.PropositionUtility;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.stream.Collectors;
 
 /**
  * @author thibautd
  */
 @Singleton
 public class LocationAlternativesGenerator implements AlternativesGenerator<LocationProposition> {
+	private static final int N_OUT_OF_HOME = 10;
+	private static final double OUT_OF_HOME_RADIUS_M = 30 * 1000;
+
 	private final SocialNetwork socialNetwork;
 	private final Population population;
 	private final RandomSeedHelper seeds;
 	private final LocationHelper locations;
+	private final PropositionUtility<LocationProposition> utility;
+	private final QuadTree<ActivityFacility> facilities;
 
 	@Inject
 	public LocationAlternativesGenerator(
 			final SocialNetwork socialNetwork,
 			final Population population,
+			final ActivityFacilities facilities,
 			final RandomSeedHelper seeds,
-			final LocationHelper locations ) {
+			final LocationHelper locations,
+			final PropositionUtility<LocationProposition> utility ) {
 		this.socialNetwork = socialNetwork;
 		this.population = population;
 		this.seeds = seeds;
 		this.locations = locations;
+		this.utility = utility;
+
+		final QuadTreeRebuilder<ActivityFacility> qt = new QuadTreeRebuilder<>();
+		facilities.getFacilities().values().forEach( f -> qt.put( f.getCoord() , f ) );
+		this.facilities = qt.getQuadTree();
 	}
 
 	@Override
@@ -58,7 +77,12 @@ public class LocationAlternativesGenerator implements AlternativesGenerator<Loca
 		final Collection<Id<Person>> alterIds = socialNetwork.getAlters( agent.getId() );
 		final Person ego = population.getPersons().get( agent.getId() );
 
-		final Collection<LocationProposition> propositions = new ArrayList<>( 2 * alterIds.size() );
+		final Collection<LocationProposition> propositions =
+				new ArrayList<>(
+						2 * alterIds.size() + // visits
+								1 + // alone at home
+								N_OUT_OF_HOME * alterIds.size() ); // out of home
+		// visits
 		for ( Id<Person> alter : alterIds ) {
 			propositions.add(
 					new LocationProposition(
@@ -72,9 +96,35 @@ public class LocationAlternativesGenerator implements AlternativesGenerator<Loca
 							locations.getHomeLocation( population.getPersons().get( alter ) ) ) );
 		}
 
-		// TODO sample possible out-of-home locations
+		// alone at home
+		propositions.add(
+				new LocationProposition(
+						ego.getId() ,
+						Collections.emptyList(),
+						locations.getHomeLocation( ego ) ) );
+
+		// out-of-home locations
+		propositions.addAll( generateOutOfHome( agent , alterIds ) );
 
 		return propositions;
+	}
+
+	private Collection<LocationProposition> generateOutOfHome(
+			final NegotiationAgent<LocationProposition> agent,
+			final Collection<Id<Person>> alters ) {
+		final Coord home = locations.getHomeLocation( agent.getId() ).getCoord();
+		final Collection<ActivityFacility> close = facilities.getDisk( home.getX() , home.getY() , OUT_OF_HOME_RADIUS_M );
+
+		return close.stream()
+				.sorted()
+				.limit( N_OUT_OF_HOME )
+				.flatMap( facility ->
+						alters.stream().map( alter ->
+								new LocationProposition(
+										agent.getId(),
+										Collections.singleton( alter ),
+										facility ) ) )
+				.collect( Collectors.toList() );
 	}
 
 }
