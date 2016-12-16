61dcec28fb732c0efb38a5800135bb74e9f5e292
ikaddoura <kaddoura@vsp.tu-berlin.de>
2016-12-16 15:58:35 +0100

    making the blend factor adjustable

--- playgrounds/ikaddoura/src/main/java/playground/ikaddoura/BerlinControler.java
+++ playgrounds/ikaddoura/src/main/java/playground/ikaddoura/BerlinControler.java
@@ -19,6 +19,7 @@
 
 package playground.ikaddoura;
 
+import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -28,9 +29,12 @@ import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
+import org.matsim.core.router.costcalculators.RandomizingTimeDistanceTravelDisutilityFactory;
 import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.io.IOUtils;
 
 import playground.ikaddoura.agentSpecificActivityScheduling.AgentSpecificActivityScheduling;
 import playground.ikaddoura.analysis.detailedPersonTripAnalysis.PersonTripCongestionNoiseAnalysisMain;
@@ -39,8 +43,12 @@ import playground.ikaddoura.decongestion.Decongestion;
 import playground.ikaddoura.decongestion.DecongestionConfigGroup;
 import playground.ikaddoura.decongestion.DecongestionConfigGroup.TollingApproach;
 import playground.ikaddoura.decongestion.data.DecongestionInfo;
-import playground.ikaddoura.integrationCNE.CNEIntegration;
-import playground.ikaddoura.integrationCNE.CNEIntegration.CongestionTollingApproach;
+import playground.vsp.congestion.controler.AdvancedMarginalCongestionPricingContolerListener;
+import playground.vsp.congestion.handlers.CongestionHandlerImplV10;
+import playground.vsp.congestion.handlers.CongestionHandlerImplV3;
+import playground.vsp.congestion.handlers.CongestionHandlerImplV9;
+import playground.vsp.congestion.handlers.TollHandler;
+import playground.vsp.congestion.routing.CongestionTollTimeDistanceTravelDisutilityFactory;
 
 /**
 * @author ikaddoura
@@ -58,10 +66,13 @@ public class BerlinControler {
 	private static double tolerance;
 	
 	private static PricingApproach pricingApproach;
+	private static double blendFactor;
 	
 	private enum PricingApproach {
-		NoPricing, PID, QBPV3, QBPV9
+		NoPricing, PID, QBPV3, QBPV9, QBPV10
 	}
+	
+	private final double sigma = 0.;
 		
 	public static void main(String[] args) throws IOException {
 
@@ -89,12 +100,17 @@ public class BerlinControler {
 				pricingApproach = PricingApproach.QBPV3;
 			} else if (congestionTollingApproachString.equals(PricingApproach.QBPV9.toString())) {
 				pricingApproach = PricingApproach.QBPV9;
+			} else if (congestionTollingApproachString.equals(PricingApproach.QBPV10.toString())) {
+				pricingApproach = PricingApproach.QBPV10;
 			} else if (congestionTollingApproachString.equals(PricingApproach.PID.toString())) {
 				pricingApproach = PricingApproach.PID;
 			} else {
 				throw new RuntimeException("Unknown congestion pricing approach. Aborting...");
 			}
 			log.info("pricingApproach: " + pricingApproach);
+			
+			blendFactor = Double.parseDouble(args[6]);
+			log.info("blendFactor: "+ blendFactor);
 			 
 		} else {
 			
@@ -105,7 +121,8 @@ public class BerlinControler {
 			activityDurationBin = 3600.;
 			tolerance = 0.;
 			
-			pricingApproach = PricingApproach.NoPricing;
+			pricingApproach = PricingApproach.QBPV10;
+			blendFactor = 0.1;
 		}
 		
 		BerlinControler berlin = new BerlinControler();
@@ -122,7 +139,7 @@ public class BerlinControler {
 		
 		String outputDirectory = outputBaseDirectory + "perf" + config.planCalcScore().getPerforming_utils_hr()
 					+ "_lateArrival" + config.planCalcScore().getLateArrival_utils_hr() + "_asas-" + String.valueOf(agentSpecificActivityScheduling) 
-					+ "_actDurBin" +  activityDurationBin + "_tolerance" + tolerance + "_pricing-" + pricingApproach.toString() + "_" + dateTime + "/";
+					+ "_actDurBin" +  activityDurationBin + "_tolerance" + tolerance + "_pricing-" + pricingApproach.toString() + "_tollBlendFactor" + blendFactor + "_" + dateTime + "/";
 		
 		config.controler().setOutputDirectory(outputDirectory);
 		Scenario scenario = ScenarioUtils.loadScenario(config);
@@ -142,33 +159,83 @@ public class BerlinControler {
 			double kp = 2 *
 					((config.planCalcScore().getPerforming_utils_hr() - config.planCalcScore().getModes().get(TransportMode.car).getMarginalUtilityOfTraveling())
 							/ config.planCalcScore().getMarginalUtilityOfMoney()) / 3600.;
-			log.info("kp: " + kp);
+			log.info("Kp: " + kp);
 			
 			final DecongestionConfigGroup decongestionSettings = new DecongestionConfigGroup();
 			decongestionSettings.setTOLLING_APPROACH(TollingApproach.PID);
 			decongestionSettings.setKp(kp);
 			decongestionSettings.setKi(0.);
 			decongestionSettings.setKd(0.);
-			decongestionSettings.setTOLL_BLEND_FACTOR(0.1);
-			decongestionSettings.setMsa(true);
+			
+			if (blendFactor == 0.) {
+				log.info("blend factor is ignored. Using MSA instead.");
+				decongestionSettings.setMsa(true);
+			} else {
+				log.info("Using blend factor " + blendFactor);
+				decongestionSettings.setTOLL_BLEND_FACTOR(blendFactor);
+			}
 			decongestionSettings.setRUN_FINAL_ANALYSIS(false);
 			decongestionSettings.setWRITE_LINK_INFO_CHARTS(false);
 			
 			final DecongestionInfo info = new DecongestionInfo(controler.getScenario(), decongestionSettings);
 			final Decongestion decongestion = new Decongestion(controler, info);
+			decongestion.setSigma(sigma);
 			controler = decongestion.getControler();
 				
 		} else if (pricingApproach.toString().equals(PricingApproach.QBPV3.toString())) {
-			CNEIntegration cne = new CNEIntegration(controler);
-			cne.setCongestionTollingApproach(CongestionTollingApproach.QBPV3);
-			cne.setCongestionPricing(true);
-			controler = cne.prepareControler();
+			
+			final TollHandler congestionTollHandlerQBP = new TollHandler(controler.getScenario());
+			final CongestionTollTimeDistanceTravelDisutilityFactory factory = new CongestionTollTimeDistanceTravelDisutilityFactory(
+					new RandomizingTimeDistanceTravelDisutilityFactory(TransportMode.car, controler.getConfig().planCalcScore()),
+					congestionTollHandlerQBP, controler.getConfig().planCalcScore()
+				);
+			factory.setSigma(sigma);
+			factory.setBlendFactor(blendFactor);
+			
+			controler.addOverridingModule(new AbstractModule(){
+				@Override
+				public void install() {
+					this.bindCarTravelDisutilityFactory().toInstance( factory );
+				}
+			}); 
+			controler.addControlerListener(new AdvancedMarginalCongestionPricingContolerListener(controler.getScenario(), congestionTollHandlerQBP, new CongestionHandlerImplV3(controler.getEvents(), controler.getScenario())));
 			
 		} else if (pricingApproach.toString().equals(PricingApproach.QBPV9.toString())) {
-			CNEIntegration cne = new CNEIntegration(controler);
-			cne.setCongestionTollingApproach(CongestionTollingApproach.QBPV9);
-			cne.setCongestionPricing(true);
-			controler = cne.prepareControler();
+			
+			final TollHandler congestionTollHandlerQBP = new TollHandler(controler.getScenario());
+			final CongestionTollTimeDistanceTravelDisutilityFactory factory = new CongestionTollTimeDistanceTravelDisutilityFactory(
+					new RandomizingTimeDistanceTravelDisutilityFactory(TransportMode.car, controler.getConfig().planCalcScore()),
+					congestionTollHandlerQBP, controler.getConfig().planCalcScore()
+				);
+			factory.setSigma(sigma);
+			factory.setBlendFactor(blendFactor);
+
+			controler.addOverridingModule(new AbstractModule(){
+				@Override
+				public void install() {
+					this.bindCarTravelDisutilityFactory().toInstance( factory );
+				}
+			}); 
+			controler.addControlerListener(new AdvancedMarginalCongestionPricingContolerListener(controler.getScenario(), congestionTollHandlerQBP, new CongestionHandlerImplV9(controler.getEvents(), controler.getScenario())));
+			
+		} else if (pricingApproach.toString().equals(PricingApproach.QBPV10.toString())) {
+			
+			final TollHandler congestionTollHandlerQBP = new TollHandler(controler.getScenario());
+			final CongestionTollTimeDistanceTravelDisutilityFactory factory = new CongestionTollTimeDistanceTravelDisutilityFactory(
+					new RandomizingTimeDistanceTravelDisutilityFactory(TransportMode.car, controler.getConfig().planCalcScore()),
+					congestionTollHandlerQBP, controler.getConfig().planCalcScore()
+				);
+			factory.setSigma(sigma);
+			factory.setBlendFactor(blendFactor);
+			
+			controler.addOverridingModule(new AbstractModule(){
+				@Override
+				public void install() {
+					this.bindCarTravelDisutilityFactory().toInstance( factory );
+				}
+			}); 
+			controler.addControlerListener(new AdvancedMarginalCongestionPricingContolerListener(controler.getScenario(), congestionTollHandlerQBP, new CongestionHandlerImplV10(controler.getEvents(), controler.getScenario())));
+			
 		} else {
 			throw new RuntimeException("Unknown congestion pricing approach. Aborting...");
 		}
@@ -182,6 +249,16 @@ public class BerlinControler {
 		analysis.run();
 		
 		MATSimVideoUtils.createLegHistogramVideo(controler.getConfig().controler().getOutputDirectory());
+		
+		// delete unnecessary iterations folder here.
+		int firstIt = controler.getConfig().controler().getFirstIteration();
+		int lastIt = controler.getConfig().controler().getLastIteration();
+		String OUTPUT_DIR = controler.getConfig().controler().getOutputDirectory();
+		for (int index = firstIt+1; index < lastIt; index ++){
+			String dirToDel = OUTPUT_DIR+"/ITERS/it."+index;
+			log.info("Deleting the directory "+dirToDel);
+			IOUtils.deleteDirectory(new File(dirToDel),false);
+		}
 	}
 
 }
