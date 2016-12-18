0305255b1e11b939a6ee5a21d7872ec09aa23efb
dziemke <ziemke@vsp.tu-berlin.de>
2016-12-18 21:55:41 +0100

    revise simple qgis output creator class

--- playgrounds/dziemke/src/main/java/playground/dziemke/accessibility/CreateQGisVisualsForAccessibiliyComputation.java
+++ playgrounds/dziemke/src/main/java/playground/dziemke/accessibility/CreateQGisVisualsForAccessibiliyComputation.java
@@ -1,14 +1,13 @@
 package playground.dziemke.accessibility;
 
-import com.vividsolutions.jts.geom.Envelope;
+import java.util.Arrays;
+import java.util.List;
 
-import org.matsim.api.core.v01.network.Network;
-import org.matsim.contrib.accessibility.Modes4Accessibility;
+import org.matsim.api.core.v01.TransportMode;
 import org.matsim.contrib.accessibility.utils.VisualizationUtils;
-import org.matsim.contrib.matrixbasedptrouter.utils.BoundingBox;
-import org.matsim.core.network.NetworkUtils;
-import org.matsim.core.network.io.MatsimNetworkReader;
 import org.matsim.core.utils.geometry.transformations.TransformationFactory;
+
+import com.vividsolutions.jts.geom.Envelope;
 
 /**
  * This is a starter class to create visual QGis-based output from a readily-done accessibility computation
@@ -19,41 +18,25 @@ import org.matsim.core.utils.geometry.transformations.TransformationFactory;
 public class CreateQGisVisualsForAccessibiliyComputation {
 
 	public static void main(String[] args) {
-		// Input and output
-		String workingDirectory = "../../../shared-svn/projects/maxess/data/nmb/output/34a/";
-		String networkFile = "../../../../Workspace/data/accessibility/nairobi/network/2015-11-05_kibera_paths_detailed.xml";
+		String workingDirectory = "../../../shared-svn/projects/maxess/data/nmb/output/17neuRestrictedFile/";
+		int cellSize = 500;
+		final List<String> activityTypes = Arrays.asList(new String[]{"composite"});
+		final List<String> modes = Arrays.asList(new String[]{TransportMode.car, TransportMode.bike, TransportMode.walk, "freespeed"});
 
-		// Parameters
-		String actType = "s";
-//		String crs = "EPSG:21037";
-		String crs = TransformationFactory.WGS84_SA_Albers;
-		
-		// QGis
-		boolean includeDensityLayer = true;
-		Double lowerBound = 0.;
-		Double upperBound = 3.5;
-		Integer range = 9;
-		int symbolSize = 1010;
-		int cellSize = 1000;
-		int populationThreshold = (int) (200 / (1000/cellSize * 1000/cellSize));
-		Envelope envelope = new Envelope(115000,-3718000,161000,-3679000); // NMB
+		final boolean includeDensityLayer = true;
+		final Integer range = 9; // In the current implementation, this must always be 9
+		final Double lowerBound = 0.5; // (upperBound - lowerBound) ideally nicely divisible by (range - 2)
+		final Double upperBound = 4.;
+		final int populationThreshold = (int) (50 / (1000/cellSize * 1000/cellSize));
 
-		// Set mapViewExtent by bounding box around network
-//		Network network = NetworkUtils.createNetwork();
-//		MatsimNetworkReader networkReader = new MatsimNetworkReader(network);
-//		networkReader.readFile(networkFile);
-//		BoundingBox boundingBox = BoundingBox.createBoundingBox(network);
-//		double[] mapViewExtent = {boundingBox.getXMin(), boundingBox.getYMin(), boundingBox.getXMax(), boundingBox.getYMax()};
-		
-		// create...
 		String osName = System.getProperty("os.name");
-		String actSpecificWorkingDirectory =  workingDirectory + actType + "/";
-		
-		for ( Modes4Accessibility modeOld : Modes4Accessibility.values()) {
-			String mode = modeOld.toString();
-			VisualizationUtils.createQGisOutput(actType, mode, envelope, workingDirectory, crs, includeDensityLayer,
-					lowerBound, upperBound, range, symbolSize, populationThreshold);
-			VisualizationUtils.createSnapshot(actSpecificWorkingDirectory, mode, osName);
-		}
+		for (String actType : activityTypes) {
+			String actSpecificWorkingDirectory = workingDirectory + actType + "/";
+			for (String mode : modes) {
+				VisualizationUtils.createQGisOutput(actType, mode, new Envelope(100000,180000,-3720000,-3675000), workingDirectory, TransformationFactory.WGS84_SA_Albers, includeDensityLayer,
+						lowerBound, upperBound, range, cellSize, populationThreshold);
+				VisualizationUtils.createSnapshot(actSpecificWorkingDirectory, mode, osName);
+			}
+		}  
 	}
 }
