34b5c3663e9d0928b2fedc27cf9d1d09c1018790
Johan W Joubert <johan.joubert@up.ac.za>
2017-01-04 10:34:27 +0200

    When comparing activity chain counts, a comparative output file is written.
    The output file columns are currently, unfortunately, hard coded. This should be fine, though, as the class is performing a very specific comparison.

--- playgrounds/southAfrica/src/main/java/playground/southafrica/freight/digicore/analysis/chain/ChainCounter.java
+++ playgrounds/southAfrica/src/main/java/playground/southafrica/freight/digicore/analysis/chain/ChainCounter.java
@@ -23,9 +23,21 @@
  */
 package playground.southafrica.freight.digicore.analysis.chain;
 
-import org.apache.log4j.Logger;
-import org.matsim.core.utils.misc.Counter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Id;
+import org.matsim.core.utils.io.IOUtils;
+import org.matsim.core.utils.misc.Counter;
+import org.matsim.vehicles.Vehicle;
+
+import playground.southafrica.freight.digicore.containers.DigicoreChain;
 import playground.southafrica.freight.digicore.containers.DigicoreVehicle;
 import playground.southafrica.freight.digicore.containers.DigicoreVehicles;
 import playground.southafrica.freight.digicore.io.DigicoreVehiclesReader;
@@ -43,6 +55,13 @@ import playground.southafrica.utilities.Header;
 public class ChainCounter {
 	final private static Logger LOG = Logger.getLogger(ChainCounter.class);
 	
+	final static String[] months = {
+			"201306", "201307", "201308", "201309", "201310", "201311", 
+			"201312", "201401", "201402", "201403", "201404", "201405"};
+
+	private static Map<Id<Vehicle>, Map<String, Integer>> monthlyMap = new TreeMap<>();
+	private static Map<Id<Vehicle>, Map<String, Integer>> yearlyMap = new TreeMap<>();
+	
 	/** Hide constructor */
 	private ChainCounter() {
 	}
@@ -54,82 +73,21 @@ public class ChainCounter {
 		checkMonthByMonth(path);
 		checkWholeYear(path);
 		
+		writeChainCountComparison(path);
+		
 		Header.printFooter();
 	}
 	
 	private static void checkMonthByMonth(String path){
 		int chains = 0;
-		DigicoreVehicles vehicles = new DigicoreVehicles();
-		new DigicoreVehiclesReader(vehicles).readFile(path + "digicoreVehicles_201306.xml.gz");
-		int count = countChains(vehicles);
-		LOG.info("201306: " + count);
-		chains += count;
+		DigicoreVehicles vehicles = null;
 		
-		vehicles = null; vehicles = new DigicoreVehicles();
-		new DigicoreVehiclesReader(vehicles).readFile(path + "digicoreVehicles_201307.xml.gz");
-		count = countChains(vehicles);
-		LOG.info("201307: " + count);
-		chains += count;
-		
-		vehicles = null; vehicles = new DigicoreVehicles();
-		new DigicoreVehiclesReader(vehicles).readFile(path + "digicoreVehicles_201308.xml.gz");
-		count = countChains(vehicles);
-		LOG.info("201308: " + count);
-		chains += count;
-		
-		vehicles = null; vehicles = new DigicoreVehicles();
-		new DigicoreVehiclesReader(vehicles).readFile(path + "digicoreVehicles_201309.xml.gz");
-		count = countChains(vehicles);
-		LOG.info("201309: " + count);
-		chains += count;
-		
-		vehicles = null; vehicles = new DigicoreVehicles();
-		new DigicoreVehiclesReader(vehicles).readFile(path + "digicoreVehicles_201310.xml.gz");
-		count = countChains(vehicles);
-		LOG.info("201310: " + count);
-		chains += count;
-		
-		vehicles = null; vehicles = new DigicoreVehicles();
-		new DigicoreVehiclesReader(vehicles).readFile(path + "digicoreVehicles_201311.xml.gz");
-		count = countChains(vehicles);
-		LOG.info("201311: " + count);
-		chains += count;
-		
-		vehicles = null; vehicles = new DigicoreVehicles();
-		new DigicoreVehiclesReader(vehicles).readFile(path + "digicoreVehicles_201312.xml.gz");
-		count = countChains(vehicles);
-		LOG.info("201312: " + count);
-		chains += count;
-		
-		vehicles = null; vehicles = new DigicoreVehicles();
-		new DigicoreVehiclesReader(vehicles).readFile(path + "digicoreVehicles_201401.xml.gz");
-		count = countChains(vehicles);
-		LOG.info("201401: " + count);
-		chains += count;
-		
-		vehicles = null; vehicles = new DigicoreVehicles();
-		new DigicoreVehiclesReader(vehicles).readFile(path + "digicoreVehicles_201402.xml.gz");
-		count = countChains(vehicles);
-		LOG.info("201402: " + count);
-		chains += count;
-		
-		vehicles = null; vehicles = new DigicoreVehicles();
-		new DigicoreVehiclesReader(vehicles).readFile(path + "digicoreVehicles_201403.xml.gz");
-		count = countChains(vehicles);
-		LOG.info("201403: " + count);
-		chains += count;
-		
-		vehicles = null; vehicles = new DigicoreVehicles();
-		new DigicoreVehiclesReader(vehicles).readFile(path + "digicoreVehicles_201404.xml.gz");
-		count = countChains(vehicles);
-		LOG.info("201404: " + count);
-		chains += count;
-		
-		vehicles = null; vehicles = new DigicoreVehicles();
-		new DigicoreVehiclesReader(vehicles).readFile(path + "digicoreVehicles_201405.xml.gz");
-		count = countChains(vehicles);
-		LOG.info("201405: " + count);
-		chains += count;
+		for(String month : months){
+			vehicles = null; vehicles = new DigicoreVehicles();
+			new DigicoreVehiclesReader(vehicles).readFile(path + "digicoreVehicles_" + month + ".xml.gz");
+			int count = countChains(vehicles, monthlyMap);
+			LOG.info("Number of chains for " + month + ": " + count);
+		}
 
 		LOG.info("Total number of chains on month-by-month basis: " + chains);
 	}
@@ -138,18 +96,120 @@ public class ChainCounter {
 		int chains = 0;
 		DigicoreVehicles vehicles = new DigicoreVehicles();
 		new DigicoreVehiclesReader(vehicles).readFile(path + "digicoreVehicles_201306_201405.xml.gz");
-		chains += countChains(vehicles);
+		chains += countChains(vehicles, yearlyMap);
 		LOG.info("Total number of chains on yearly basis: " + chains);
 	}
 	
-	private static int countChains(DigicoreVehicles vehicles){
+	private static int countChains(DigicoreVehicles vehicles, 
+			Map<Id<Vehicle>, Map<String, Integer>> map){
+		
 		int total = 0;
 		Counter counter = new Counter("  vehicles # ");
 		for(DigicoreVehicle vehicle : vehicles.getVehicles().values()){
+			if(!map.containsKey(vehicle.getId())){
+				map.put(vehicle.getId(), new TreeMap<>());
+			}
+			Map<String, Integer> thisMap = map.get(vehicle.getId());
+			
+			for(DigicoreChain chain : vehicle.getChains()){
+				int chainYear = chain.getFirstMajorActivity().getEndTimeGregorianCalendar().get(Calendar.YEAR);
+				int chainMonth = chain.getFirstMajorActivity().getEndTimeGregorianCalendar().get(Calendar.MONTH)+1;
+				String monthString = String.format("%d%02d", chainYear, chainMonth);
+				if(!thisMap.containsKey(monthString)){
+					thisMap.put(monthString, 1);
+				} else{
+					int oldValue = thisMap.get(monthString);
+					thisMap.put(monthString, oldValue + 1);
+				}
+			}
+			
 			total += vehicle.getChains().size();
 			counter.incCounter();
 		}
 		counter.printCounter();
+		
 		return total;
 	}
+	
+	private static void writeChainCountComparison(String path){
+		LOG.info("Writing chain count comparison to file...");
+		
+		/* Get a consolidated list of vehicle IDs. */
+		List<Id<Vehicle>> vIds = new ArrayList<Id<Vehicle>>();
+		for(Id<Vehicle> vId : monthlyMap.keySet()){
+			if(!vIds.contains(vId)){
+				vIds.add(vId);
+			}
+		}
+		for(Id<Vehicle> vId : yearlyMap.keySet()){
+			if(!vIds.contains(vId)){
+				vIds.add(vId);
+			}
+		}
+		
+		BufferedWriter bw = IOUtils.getBufferedWriter(path + "chainCountComparison.csv.gz");
+		try{
+			/* Header */
+			bw.write("vid");
+			for(int i = 0; i < months.length; i++){
+				bw.write(",");
+				bw.write("m");
+				bw.write(months[i]);
+			}
+			for(int i = 0; i < months.length; i++){
+				bw.write(",");
+				bw.write("y");
+				bw.write(months[i]);
+			}
+			bw.newLine();
+			
+			/* Write each vehicle's number of chains in each of the considered 
+			 * months. */
+			for(Id<Vehicle> vid : vIds){
+				bw.write(vid.toString());
+				
+				/* Monthly. */
+				for(String month : months){
+					int count = 0;
+					if(monthlyMap.containsKey(vid)){
+						if(monthlyMap.get(vid).containsKey(month)){
+							count = monthlyMap.get(vid).get(month);
+						}
+					}
+					bw.write(String.format(",%d", count));
+				}
+				
+				/* Yearly. */
+				for(String month : months){
+					int count = 0;
+					if(yearlyMap.containsKey(vid)){
+						if(yearlyMap.get(vid).containsKey(month)){
+							count = yearlyMap.get(vid).get(month);
+						}
+					}
+					bw.write(String.format(",%d", count));
+				}
+				
+				bw.newLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Cannot write to " + bw.toString());
+		} finally{
+			try {
+				bw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+				throw new RuntimeException("Cannot close " + bw.toString());
+			}
+		}
+		LOG.info("Done writing chain count comparison to file.");
+	}
+	
+	
+	
+	
+	
+	
+	
 }
