54b30df2fe7d1a9cca21482123ebc37cba533188
gabriel.thunig <gabriel.thunig@vsp.tu-berlin.de>
2017-01-10 13:26:18 +0100

    make file cluster-runable

--- playgrounds/vspshk/src/main/java/playground/gleich/av_bus/prepareScenario/ExtractAgentsInArea.java
+++ playgrounds/vspshk/src/main/java/playground/gleich/av_bus/prepareScenario/ExtractAgentsInArea.java
@@ -1,34 +1,28 @@
 package playground.gleich.av_bus.prepareScenario;
 
-import java.util.HashSet;
-import java.util.Set;
-
+import com.vividsolutions.jts.geom.Geometry;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Link;
-//import org.matsim.api.core.v01.network.Network;
-//import org.matsim.api.core.v01.network.Node;
-import org.matsim.api.core.v01.population.Activity;
-import org.matsim.api.core.v01.population.Leg;
-import org.matsim.api.core.v01.population.Person;
-import org.matsim.api.core.v01.population.Plan;
-import org.matsim.api.core.v01.population.PlanElement;
-import org.matsim.api.core.v01.population.PopulationWriter;
+import org.matsim.api.core.v01.population.*;
 import org.matsim.core.config.ConfigUtils;
-//import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.network.io.MatsimNetworkReader;
-//import org.matsim.core.network.io.NetworkWriter;
 import org.matsim.core.population.io.PopulationReader;
 import org.matsim.core.population.routes.NetworkRoute;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.geometry.geotools.MGC;
 import org.matsim.pt.router.TransitActsRemover;
-
-import com.vividsolutions.jts.geom.Geometry;
-
 import playground.gleich.av_bus.FilePaths;
 import playground.jbischoff.utils.JbUtils;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//import org.matsim.api.core.v01.network.Network;
+//import org.matsim.api.core.v01.network.Node;
+//import org.matsim.core.network.NetworkUtils;
+//import org.matsim.core.network.io.NetworkWriter;
 
 /**
  * @author gleich
@@ -38,23 +32,33 @@ import playground.jbischoff.utils.JbUtils;
  */
 public class ExtractAgentsInArea {
 	
-	private final String inputNetworkPath = FilePaths.PATH_NETWORK_BERLIN_100PCT;
-	private final String inputPopulationPath = FilePaths.PATH_POPULATION_BERLIN_100PCT_UNFILTERED;
-	private final String studyAreaShpPath = FilePaths.PATH_STUDY_AREA_SHP;
-	private final String studyAreaShpKey = FilePaths.STUDY_AREA_SHP_KEY;
-	private final String studyAreaShpElement = FilePaths.STUDY_AREA_SHP_ELEMENT;
-	private final String outputPopulationPath = FilePaths.PATH_POPULATION_BERLIN_100PCT_FILTERED;
+	private static String inputNetworkPath = FilePaths.PATH_NETWORK_BERLIN_100PCT;
+	private static String inputPopulationPath = FilePaths.PATH_POPULATION_BERLIN_100PCT_UNFILTERED;
+	private static String studyAreaShpPath = FilePaths.PATH_STUDY_AREA_SHP;
+	private static String studyAreaShpKey = FilePaths.STUDY_AREA_SHP_KEY;
+	private static String studyAreaShpElement = FilePaths.STUDY_AREA_SHP_ELEMENT;
+	private static String outputPopulationPath = FilePaths.PATH_POPULATION_BERLIN_100PCT_FILTERED;
 	private Scenario inputScenario;
 	private Scenario outputScenario;
 //	private Network networkEnclosedInStudyArea;
-	private Set<Id<Link>> linksInArea = new HashSet<Id<Link>>();
+	private Set<Id<Link>> linksInArea = new HashSet<>();
 	private Geometry geometryStudyArea;
 	
 	public static void main(String[] args) {
+
+		if (args.length != 0) {
+			inputNetworkPath = args[0];
+			inputPopulationPath = args[1];
+			studyAreaShpPath = args[2];
+			studyAreaShpKey = args[3];
+			studyAreaShpElement = args[4];
+			outputPopulationPath = args[5];
+		}
+
 		(new ExtractAgentsInArea()).run();
 	}
 	
-	void run(){
+	private void run(){
 		initialize();
 		System.out.println("initialize done");
 		selectAgentsByActivitiesInArea();
