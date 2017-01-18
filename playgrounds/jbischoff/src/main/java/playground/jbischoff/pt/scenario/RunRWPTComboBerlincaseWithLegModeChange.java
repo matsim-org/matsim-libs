b625f0cb436bcb66641d2979f0b3d3c6f05e6b9a
jfbischoff <bischoff@vsp.tu-berlin.de>
2017-01-18 08:01:17 +0100

    RunRWPTComboBerlincaseWithLegModeChange changes

--- playgrounds/jbischoff/src/main/java/playground/jbischoff/pt/scenario/RunRWPTComboBerlincaseWithLegModeChange.java
+++ playgrounds/jbischoff/src/main/java/playground/jbischoff/pt/scenario/RunRWPTComboBerlincaseWithLegModeChange.java
@@ -26,6 +26,8 @@ import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.Plan;
+import org.matsim.contrib.av.robotaxi.scoring.TaxiFareConfigGroup;
+import org.matsim.contrib.av.robotaxi.scoring.TaxiFareHandler;
 import org.matsim.contrib.dvrp.data.file.VehicleReader;
 import org.matsim.contrib.dvrp.trafficmonitoring.VrpTravelTimeModules;
 import org.matsim.contrib.dynagent.run.DynQSimModule;
@@ -70,12 +72,12 @@ import playground.jbischoff.pt.strategy.ChangeSingleLegModeWithPredefinedFromMod
 public class RunRWPTComboBerlincaseWithLegModeChange {
 	public static void main(String[] args) {
 
-		// if (args.length!=1){
-		// throw new RuntimeException("Wrong arguments");
-		// }
+//		 if (args.length!=1){
+//		 throw new RuntimeException("Wrong arguments");
+//		 }
 		String configfile = "C:/Users/Joschka/Documents/shared-svn/studies/jbischoff/multimodal/berlin/input/modechoice/config_modechoice.xml";
 
-		Config config = ConfigUtils.loadConfig(configfile, new TaxiConfigGroup());
+		Config config = ConfigUtils.loadConfig(configfile, new TaxiConfigGroup(), new TaxiFareConfigGroup());
 		config.controler().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
 
 		TaxiConfigGroup taxiCfg = TaxiConfigGroup.get(config);
@@ -86,8 +88,8 @@ public class RunRWPTComboBerlincaseWithLegModeChange {
 
 		// special, second transit schedule for our special pt mode, i.e. only
 		// railway lines
-		new TransitScheduleReader(scenario2).readFile(
-				"C:/Users/Joschka/Documents/shared-svn/studies/jbischoff/multimodal/berlin/input/modechoice/scrappedSchedule.xml");
+		String scrappedScheduleFile = config.transit().getTransitScheduleFileURL(config.getContext()).getFile().split(".xml")[0]+"_va.xml"; 
+		new TransitScheduleReader(scenario2).readFile(scrappedScheduleFile);
 		final TransitSchedule schedule = scenario2.getTransitSchedule();
 
 		VariableAccessModeConfigGroup walk = new VariableAccessModeConfigGroup();
@@ -98,7 +100,7 @@ public class RunRWPTComboBerlincaseWithLegModeChange {
 		VariableAccessModeConfigGroup taxi = new VariableAccessModeConfigGroup();
 		taxi.setDistance(20000);
 		taxi.setTeleported(false);
-		taxi.setMode("car");
+		taxi.setMode("taxi");
 		VariableAccessConfigGroup vacfg = new VariableAccessConfigGroup();
 		vacfg.setAccessModeGroup(taxi);
 		vacfg.setAccessModeGroup(walk);
@@ -119,6 +121,8 @@ public class RunRWPTComboBerlincaseWithLegModeChange {
 
 			@Override
 			public void install() {
+				addEventHandlerBinding().to(TaxiFareHandler.class).asEagerSingleton();
+
 				bind(TransitSchedule.class).annotatedWith(Names.named("variableAccess")).toInstance(schedule);
 				addRoutingModuleBinding("avpt").toProvider(VariableAccessTransit.class);
 
