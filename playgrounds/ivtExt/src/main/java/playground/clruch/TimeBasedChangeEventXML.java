19eab99d7276f746305469180c7f8fb096886edf
clruch <clruch@idsc.mavt.ethz.ch>
2017-01-30 20:13:23 +0100

    initial commit

---
+++ playgrounds/ivtExt/src/main/java/playground/clruch/TimeBasedChangeEventXML.java
@@ -0,0 +1,82 @@
+package playground.clruch;
+
+import org.jdom.Attribute;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * Created by Claudio on 1/26/2017.
+ */
+public class TimeBasedChangeEventXML extends AbstractEventXML {
+    @Override
+    public void generate(Map<String, NavigableMap<Double, Integer>> waitStepFctn, File file) {
+        // from the event file extract requests of AVs and arrivals of AVs at customers
+        // calculate data in the form <time,ID,change>
+        // save as XML file
+        try {
+            Element SimulationResult = new Element("SimulationResult");
+            Document doc = new Document(SimulationResult);
+            doc.setRootElement(SimulationResult);
+
+
+            Set<String> s = waitStepFctn.keySet();
+            Iterator<String> e = s.iterator();
+            SortedMap<Double, List<IdNumCust>> visualizationEvents = new TreeMap<>();
+
+
+            // iterate through all stations and record events of customer number changes
+            while (e.hasNext()) {
+                String statID = (String) e.next();
+                // for the node, extract the step function of numWaitCusotmer changes
+                NavigableMap<Double, Integer> StepFctn = waitStepFctn.get(statID);
+                for (Double timeVal : StepFctn.keySet()) {
+                    // check if the time is already recorded
+                    if (!visualizationEvents.containsKey(timeVal)) {
+                        visualizationEvents.put(timeVal, new ArrayList<>());
+                    }
+                    // extract the list and add the event
+                    IdNumCust temp = new IdNumCust();
+                    temp.id = statID;
+                    temp.numberCust = StepFctn.get(timeVal);
+                    visualizationEvents.get(timeVal).add(temp);
+                }
+            }
+
+
+            // iterate through the new list and output in the XML file
+            Set<Double> s2 = visualizationEvents.keySet();
+            System.out.println("Times with events:");
+            for (double timeVal : s2) {
+                Element node = new Element("time");
+                node.setAttribute(new Attribute("seconds", Double.toString(timeVal)));
+                List<IdNumCust> tempList = visualizationEvents.get(timeVal);
+                for (IdNumCust idNumCust : tempList) {
+                    Element waitChange = new Element("waitChange");
+                    waitChange.setAttribute("id", idNumCust.id);
+                    waitChange.setAttribute("numCustWait", Integer.toString(idNumCust.numberCust));
+                    node.addContent(waitChange);
+                }
+                doc.getRootElement().addContent(node);
+            }
+
+
+            // new XMLOutputter().output(doc, System.out);
+            XMLOutputter xmlOutput = new XMLOutputter();
+
+            // display nice nice
+            xmlOutput.setFormat(Format.getPrettyFormat());
+            xmlOutput.output(doc, new FileWriter(file));
+
+            System.out.println("File Saved!");
+        } catch (IOException io) {
+            System.out.println(io.getMessage());
+        }
+    }
+}
---
+++ playgrounds/ivtExt/src/main/java/playground/clruch/utils/GZHandler.java
@@ -0,0 +1,33 @@
+package playground.clruch.utils;
+
+import java.io.*;
+import java.util.zip.GZIPInputStream;
+
+/**
+ * Created by Claudio on 1/26/2017.
+ */
+public class GZHandler {
+    /**
+     * Function to take a .gz file and extract the content
+     *
+     * @param source      a .gz file
+     * @param destination the corresponding unzipped file
+     */
+    public static void extract(File source, File destination) throws IOException {
+
+        System.out.println("Opening the gzip file.......................... : " + source.toString());
+        GZIPInputStream gzipInputStream = new GZIPInputStream(new FileInputStream(source.toString()));
+        OutputStream out = new FileOutputStream(destination);
+
+        System.out.println("Transferring bytes from the compressed file to the output file........: Transfer successful");
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = gzipInputStream.read(buf)) > 0) {
+            out.write(buf, 0, len);
+        }
+
+        System.out.println("The file and stream is ......closing.......... : closed");
+        gzipInputStream.close();
+        out.close();
+    }
+}
