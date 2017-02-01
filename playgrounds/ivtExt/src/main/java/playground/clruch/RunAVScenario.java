16e60d1175d97c9c9f87a6df53c1a5fa92999edf
clruch <clruch@idsc.mavt.ethz.ch>
2017-02-01 15:11:33 +0100

    RunAVScenario

--- playgrounds/ivtExt/src/main/java/playground/clruch/RunAVScenario.java
+++ playgrounds/ivtExt/src/main/java/playground/clruch/RunAVScenario.java
@@ -5,6 +5,7 @@ package playground.clruch;
 import java.io.File;
 import java.net.MalformedURLException;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Network;
 import org.matsim.contrib.dvrp.trafficmonitoring.VrpTravelTimeModules;
 import org.matsim.contrib.dynagent.run.DynQSimModule;
 import org.matsim.core.config.Config;
@@ -19,14 +20,16 @@ import javax.inject.*;
 
 
 public class RunAVScenario {
+    public static Network NETWORKINSTANCE;
+
     public static void main(String[] args) throws MalformedURLException {
         File configFile = new File(args[0]);
 
         Config config = ConfigUtils.loadConfig(configFile.toString(), new AVConfigGroup());
         Scenario scenario = ScenarioUtils.loadScenario(config);
+        NETWORKINSTANCE = scenario.getNetwork();
 
         System.out.println("Population size:" + scenario.getPopulation().getPersons().values().size());
-
 
         Controler controler = new Controler(scenario);
         controler.addOverridingModule(VrpTravelTimeModules.createTravelTimeEstimatorModule(0.05));
