a760be5946b668a67865420dde1add0f9dec4b94
clruch <clruch@idsc.mavt.ethz.ch>
2017-02-08 11:47:22 +0100

    intermediate commit

--- playgrounds/ivtExt/src/main/java/playground/clruch/export/AbstractEventXML.java
+++ playgrounds/ivtExt/src/main/java/playground/clruch/export/AbstractEventXML.java
@@ -8,6 +8,17 @@ import java.util.NavigableMap;
  * Created by Claudio on 1/26/2017.
  */
 abstract class AbstractEventXML<Type> {
-    public abstract void generate(Map<String, NavigableMap<Double, Type>> waitStepFctn, File file);
+    String name1;
+    String name2;
+
+    AbstractEventXML(String name1in, String name2in){
+        name1 = name1in;
+        name2 = name2in;
+    }
+
+    public void generate(Map<String, NavigableMap<Double, Type>> waitStepFctn, File file){
+
+    };
+//    public abstract void generate(Map<String, NavigableMap<Double, Type>> waitStepFctn, File file);
 
 }
