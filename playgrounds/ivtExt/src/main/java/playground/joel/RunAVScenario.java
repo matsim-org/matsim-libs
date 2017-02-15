16ea55dfedba26c4c8836cb09fb105f0f872ea20
JoelGaechter <joelgaechter@bluewin.ch>
2017-02-15 12:01:13 +0100

    version with local paths

--- playgrounds/ivtExt/src/main/java/playground/joel/RunAVScenario.java
+++ playgrounds/ivtExt/src/main/java/playground/joel/RunAVScenario.java
@@ -1,7 +1,44 @@
 package playground.joel;
 
-/**
- * Created by Joel on 06.02.2017.
- */
+
+
+import java.io.File;
+import java.net.MalformedURLException;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.contrib.dvrp.trafficmonitoring.VrpTravelTimeModules;
+import org.matsim.contrib.dynagent.run.DynQSimModule;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.scenario.ScenarioUtils;
+import playground.sebhoerl.avtaxi.framework.AVConfigGroup;
+import playground.sebhoerl.avtaxi.framework.AVModule;
+import playground.sebhoerl.avtaxi.framework.AVQSimProvider;
+
+import javax.inject.*;
+
+
 public class RunAVScenario {
+    public static Network NETWORKINSTANCE;
+
+    public static void main(String[] args) throws MalformedURLException {
+        //File configFile = new File(args[0]);
+        File configFile = new File("C:/Users/Joel/Documents/Studium/ETH/Bachelorarbeit/Simulation_Data/2017_02_07_Sioux_onlyUnitCapacityAVs/av_config.xml");
+
+        Config config = ConfigUtils.loadConfig(configFile.toString(), new AVConfigGroup());
+        Scenario scenario = ScenarioUtils.loadScenario(config);
+        NETWORKINSTANCE = scenario.getNetwork();
+
+        System.out.println("Population size:" + scenario.getPopulation().getPersons().values().size());
+
+        Controler controler = new Controler(scenario);
+        controler.addOverridingModule(VrpTravelTimeModules.createTravelTimeEstimatorModule(0.05));
+        controler.addOverridingModule(new DynQSimModule<>(AVQSimProvider.class));
+        controler.addOverridingModule(new AVModule());
+
+        controler.run();
+
+        System.out.println("RunAVScenario finished!");
+    }
 }
