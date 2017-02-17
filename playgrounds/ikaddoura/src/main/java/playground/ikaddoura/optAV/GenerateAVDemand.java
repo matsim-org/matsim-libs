5730b204f115461780d241623ccc831a2b04b581
ikaddoura <kaddoura@vsp.tu-berlin.de>
2017-02-17 15:26:02 +0100

    minor

--- playgrounds/ikaddoura/src/main/java/playground/ikaddoura/optAV/GenerateAVDemand.java
+++ playgrounds/ikaddoura/src/main/java/playground/ikaddoura/optAV/GenerateAVDemand.java
@@ -45,10 +45,11 @@ public class GenerateAVDemand {
 
 	private static final Logger log = Logger.getLogger(GenerateAVDemand.class);
 	
-	private static final double taxiTripShare = 0.1;
+	private static final double taxiTripShare = 0.5;
 	private static final String inputPlansFile = "/Users/ihab/Documents/workspace/public-svn/matsim/scenarios/countries/de/berlin/car-traffic-only-10pct-2016-04-21/run_194c.150.plans_selected.xml.gz";
-	private static final String outputPlansFile = "/Users/ihab/Documents/workspace/runs-svn/optAV/input/run_194c.150.plans_selected_BerlinArea_taxiTripShare_" + taxiTripShare + ".xml.gz";
-
+//	private static final String outputPlansFile = "/Users/ihab/Documents/workspace/runs-svn/optAV/input/run_194c.150.plans_selected_BerlinArea_taxiTripShare_" + taxiTripShare + ".xml.gz";
+	private static final String outputPlansFile = "/Users/ihab/Documents/workspace/runs-svn/optAV/input/run_194c.150.plans_selected_taxiTripShare_" + taxiTripShare + ".xml.gz";
+	
 	private static final double minX = 4554761.;
 	private static final double minY = 5793603.;
 	private static final double maxX = 4631345.;
@@ -62,8 +63,8 @@ public class GenerateAVDemand {
 	public static void main(String[] args) {
 			
 		GenerateAVDemand generateAVDemand = new GenerateAVDemand();
-//		generateAVDemand.createTaxiTripsForAllAgents();	
-		generateAVDemand.createTaxiTripsForAgentsWithAllTripsInSpecificArea();
+		generateAVDemand.createTaxiTripsForAllAgents();	
+//		generateAVDemand.createTaxiTripsForAgentsWithAllTripsInSpecificArea();
 	}
 
 	private void createTaxiTripsForAgentsWithAllTripsInSpecificArea() {
