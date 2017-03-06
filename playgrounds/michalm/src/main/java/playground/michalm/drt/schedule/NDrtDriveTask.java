80a7b88356eac525b30ea9866161451576ff77a7
michalmac <michal.mac@gmail.com>
2017-03-06 21:00:15 +0100

    giving up taxibus, newDRT introduced

---
+++ playgrounds/michalm/src/main/java/playground/michalm/drt/schedule/NDrtDriveTask.java
@@ -0,0 +1,51 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2015 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.michalm.drt.schedule;
+
+import java.util.*;
+
+import org.matsim.contrib.dvrp.path.VrpPathWithTravelData;
+import org.matsim.contrib.dvrp.schedule.DriveTaskImpl;
+
+import playground.michalm.drt.data.NDrtRequest;
+
+/**
+ * @author michalm
+ */
+public class NDrtDriveTask extends DriveTaskImpl implements NDrtTask {
+	private final Set<NDrtRequest> onBoardRequests = new HashSet<>();
+
+	public NDrtDriveTask(VrpPathWithTravelData path) {
+		super(path);
+	}
+
+	@Override
+	public NDrtTaskType getDrtTaskType() {
+		return NDrtTaskType.DRIVE;
+	}
+
+	public Set<NDrtRequest> getOnBoardRequests() {
+		return Collections.unmodifiableSet(onBoardRequests);
+	}
+
+	public boolean isEmpty() {
+		return onBoardRequests.isEmpty();
+	}
+}
