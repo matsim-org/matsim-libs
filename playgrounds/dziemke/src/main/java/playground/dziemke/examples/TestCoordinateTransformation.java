69adcee0d2da37b0aed405fa0fc979df323b13de
dominik <dominik@192.168.35.103>
2017-03-06 18:18:19 +0100

    tidying up

--- playgrounds/dziemke/src/main/java/playground/dziemke/examples/TestCoordinateTransformation.java
+++ playgrounds/dziemke/src/main/java/playground/dziemke/examples/TestCoordinateTransformation.java
@@ -14,7 +14,8 @@ public class TestCoordinateTransformation {
 //				TransformationFactory.DHDN_GK4, TransformationFactory.WGS84);
 		CoordinateTransformation transformation = TransformationFactory.getCoordinateTransformation
 //				("EPSG:4326", "EPSG:31468");
-				("EPSG:4326", "EPSG:21037");
+//				("EPSG:4326", "EPSG:21037");
+				("EPSG:4326", "EPSG:26911");
 //				("EPSG:3006", "EPSG:4326");
 //				(TransformationFactory.WGS84_SA_Albers, "EPSG:4326");
 
@@ -23,19 +24,21 @@ public class TestCoordinateTransformation {
 //		Coord originalCoord2 = new Coord(13.718465, 52.648131); // Berlin upper right
 //		Coord originalCoord1 = new Coord(150583.9441831379,-3699678.99131796); // somewhere in NMB in SA_Albers
 //		Coord originalCoord2 = new Coord(171583.944, y);
-//		Coord originalCoord1 = new Coord(33.88, 5.000); // Kenya northwest corner
-//		Coord originalCoord2 = new Coord(41.88, -4.75); // Kenya southeast corner
-		Coord originalCoord1 = new Coord(36.7715, -1.3055); // Kibera northwest corner
-		Coord originalCoord2 = new Coord(36.8014, -1.3198); // Kibera southeast corner
+//		Coord coordNE = new Coord(41.88, 5.000); // Kenya northeast corner
+//		Coord coordSW = new Coord(33.88, -4.75); // Kenya southwest corner 
+//		Coord coordNE = new Coord(36.8014, -1.3055); // Kibera northeast corner 
+//		Coord coordSW = new Coord(36.7715, -1.3198); // Kibera southwest corner 
+		Coord coordNE = new Coord(-119.55, 34.45); // Santa Barbara northeast corner
+		Coord coordSW = new Coord(-119.90, 34.38); // Santa Barbara southwest corner
 		
-		Coord convertedCoord1 = transformation.transform(originalCoord1);
-		Coord convertedCoord2 = transformation.transform(originalCoord2);
+		Coord convertedCoordNE = transformation.transform(coordNE);
+		Coord convertedCoordSW = transformation.transform(coordSW);
 		
 		System.out.println("###########################################################################");
-		System.out.println("originalCoord1: " + originalCoord1);
-		System.out.println("originalCoord2: " + originalCoord2);
-		System.out.println("convertedCoord1: " + convertedCoord1);
-		System.out.println("convertedCoord2: " + convertedCoord2);
+		System.out.println("coordNE: " + coordNE);
+		System.out.println("coordSW: " + coordSW);
+		System.out.println("convertedCoordNE: " + convertedCoordNE);
+		System.out.println("convertedCoordSW: " + convertedCoordSW);
 		System.out.println("###########################################################################");
 	}
 }
