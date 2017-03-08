0f0b9ff50c560c6e7213d758ceabc9c4fcb9e9bf
Amit Agarwal <amit.agarwal@campus.tu-berlin.de>
2017-03-08 20:44:48 +0100

    vsp log statements only if traffic dynamics is queue

--- matsim/src/main/java/org/matsim/core/config/consistency/VspConfigConsistencyCheckerImpl.java
+++ matsim/src/main/java/org/matsim/core/config/consistency/VspConfigConsistencyCheckerImpl.java
@@ -234,6 +234,7 @@ public final class VspConfigConsistencyCheckerImpl implements ConfigConsistencyC
 		}
 		switch( config.qsim().getTrafficDynamics() ) {
 		case withHoles:
+		case KWM:
 			break;
 		case queue:
 		default:
