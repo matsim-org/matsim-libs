66faa0d4a9b8b0c404092f02cf6cb8af529bf403
sergio537 <sergio537@gmail.com>
2017-03-08 17:28:24 +0800

    Updated implementation of Seepage mode

--- playgrounds/sergioo/src/main/java/playground/sergioo/mixedTraffic2017/RunControler.java
+++ playgrounds/sergioo/src/main/java/playground/sergioo/mixedTraffic2017/RunControler.java
@@ -1,12 +1,11 @@
 package playground.sergioo.mixedTraffic2017;
 
-import com.google.inject.Provider;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
-import org.matsim.core.mobsim.framework.Mobsim;
-import playground.sergioo.weeklySimulation.scenario.ScenarioUtils;
+import org.matsim.core.mobsim.qsim.*;
+import org.matsim.core.scenario.ScenarioUtils;
 
 /**
  * Created by sergioo on 24/2/17.
@@ -15,17 +14,12 @@ public class RunControler {
     public static void main(String[] args) {
         final Scenario scenario = ScenarioUtils.loadScenario(ConfigUtils.loadConfig(args[0]));
         final Controler controler = new Controler(scenario);
-        /*controler.addOverridingModule(new AbstractModule() {
+        controler.addOverridingModule(new AbstractModule() {
             @Override
             public void install() {
-            bindMobsim().toProvider(new Provider<Mobsim>() {
-                @Override
-                public Mobsim get() {
-                    return new
-                }
-            });
+                install(new QSimModule());
             }
-        });*/
+        });
         controler.run();
     }
 }
