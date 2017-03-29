d30a8feec3d1ca8e149cacd489f2f4a180bf4a9b
Jan Hakenberg <jan.hakenberg@gmail.com>
2017-03-29 05:11:10 +0200

    reset to basel coordinates...

--- playgrounds/ivtExt/src/main/java/playground/clruch/ScenarioViewer.java
+++ playgrounds/ivtExt/src/main/java/playground/clruch/ScenarioViewer.java
@@ -10,11 +10,11 @@ import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.geometry.CoordinateTransformation;
+import org.matsim.core.utils.geometry.transformations.CH1903LV03PlustoWGS84;
 
 import playground.clruch.gfx.MatsimJMapViewer;
 import playground.clruch.gfx.MatsimStaticDatabase;
 import playground.clruch.gfx.MatsimViewerFrame;
-import playground.clruch.gfx.helper.SiouxFallstoWGS84;
 import playground.sebhoerl.avtaxi.framework.AVConfigGroup;
 
 /**
@@ -51,8 +51,8 @@ public class ScenarioViewer {
         System.out.println(bb[0] + " " + bb[1] + " " + bb[2] + " " + bb[3]);
 
         CoordinateTransformation ct;
-        // ct = new CH1903LV03PlustoWGS84(); // <- switzerland
-        ct = new SiouxFallstoWGS84(); // <- sioux falls
+        ct = new CH1903LV03PlustoWGS84(); // <- switzerland
+        // ct = new SiouxFallstoWGS84(); // <- sioux falls
 
         MatsimStaticDatabase.initializeSingletonInstance(network, ct);
 
@@ -62,11 +62,11 @@ public class ScenarioViewer {
         MatsimViewerFrame matsimViewer = new MatsimViewerFrame(matsimJMapViewer);
 
         // basel
-        // matsimViewer.setDisplayPosition(47.55814, 7.58769, 11);
+        matsimViewer.setDisplayPosition(47.55814, 7.58769, 11);
 
         // sioux falls
         // TODO obtain center from db
-        matsimViewer.setDisplayPosition(43.54469101104898, -96.72376155853271, 13);
+        // matsimViewer.setDisplayPosition(43.54469101104898, -96.72376155853271, 13);
 
         matsimViewer.jFrame.setSize(800, 900);
         matsimViewer.jFrame.setVisible(true);
