6d5a9e5f4a3a47c6e48993609d8f5948b1df708a
jfbischoff <bischoff@vsp.tu-berlin.de>
2017-04-04 14:33:10 +0200

    minor

--- playgrounds/jbischoff/src/main/java/playground/jbischoff/sharedTaxiBerlin/run/RunSharedTaxiBatch.java
+++ playgrounds/jbischoff/src/main/java/playground/jbischoff/sharedTaxiBerlin/run/RunSharedTaxiBatch.java
@@ -42,18 +42,21 @@ public class RunSharedTaxiBatch {
 
 	public static void main(String[] args) {
 
-		int capacity = 1;
-		for (int i = 100; i<150; i=i+25){
-			String runId = "v"+i+"c"+capacity;
+		int capacity[] = {1,2,3,4,6};
+		for (int c = 0; c<capacity.length; c++){
+		for (int i = 25; i<150; i=i+25){
+			String runId = "v"+i+"c"+capacity[c];
 			String configFile = "../../../shared-svn/projects/bvg_sharedTaxi/input/config.xml";
 			Config config = ConfigUtils.loadConfig(configFile, new DvrpConfigGroup(), new DrtConfigGroup(),
 					new OTFVisConfigGroup(), new TaxiFareConfigGroup());
 			DrtConfigGroup drt = (DrtConfigGroup) config.getModules().get(DrtConfigGroup.GROUP_NAME);
 			drt.setEstimatedBeelineDistanceFactor(1.5);
-			drt.setVehiclesFile("vehicles_net_bvg/cap_"+capacity+"/taxis_"+i+".xml.gz");
+			drt.setVehiclesFile("vehicles_net_bvg/cap_"+capacity[c]+"/taxis_"+i+".xml.gz");
+			drt.setNumberOfThreads(7);
 			config.controler().setRunId(runId);
-			config.controler().setOutputDirectory("D:/runs-svn/bvg_sharedTaxi/demand01/"+runId);
+			config.controler().setOutputDirectory("D:/runs-svn/bvg_sharedTaxi/demand01/c"+capacity[c]+"/"+runId);
 			DrtControlerCreator.createControler(config, false).run();
+		}
 		}
 		
 	}
