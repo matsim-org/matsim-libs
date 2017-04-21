109cf9f74855c57e4054d756c892291751ad8b67
kainagel <nagel@vsp.tu-berlin.de>
2017-04-21 19:26:51 +0200

    fix failing emissions test

--- contribs/emissions/src/main/java/org/matsim/contrib/emissions/utils/EmissionsConfigGroup.java
+++ contribs/emissions/src/main/java/org/matsim/contrib/emissions/utils/EmissionsConfigGroup.java
@@ -50,7 +50,7 @@ extends ReflectiveConfigGroup
 	private static final String USING_VEHICLE_TYPE_ID_AS_VEHICLE_DESCRIPTION = "isUsingVehicleTypeIdAsVehicleDescription";
 	private boolean isUsingVehicleIdAsVehicleDescription = false;
 
-	private static final String IGNORING_EMISSIONS_FROM_EVENTS_FILE = "isWritingEmissionsEvents";
+	private static final String WRITING_EMISSIONS_EVENTS = "isWritingEmissionsEvents";
 	private boolean isWritingEmissionsEvents = true;
 
 	private static final String EMISSION_EFFICIENCY_FACTOR = "emissionEfficiencyFactor";
@@ -79,7 +79,7 @@ extends ReflectiveConfigGroup
 			"The emission specifications of a vehicle type should be surrounded by emission specification markers i.e."+
 			EmissionSpecificationMarker.BEGIN_EMISSIONS + " and " + EmissionSpecificationMarker.END_EMISSIONS + "." ;
 
-	static final String IGNORING_EMISSIONS_FROM_EVENTS_FILE_CMT = "if true, emission events will not appear in the events file.";
+	static final String WRITING_EMISSIONS_EVENTS_CMT = "if false, emission events will not appear in the events file.";
 
 	static final String EMISSION_EFFICIENCY_FACTOR_CMT = "A factor to include efficiency of the vehicles; the factor is applied to the whole fleet. ";
 
@@ -106,7 +106,7 @@ extends ReflectiveConfigGroup
 
 		map.put(USING_VEHICLE_TYPE_ID_AS_VEHICLE_DESCRIPTION, USING_VEHICLE_TYPE_ID_AS_VEHICLE_DESCRIPTION_CMT);
 
-		map.put(IGNORING_EMISSIONS_FROM_EVENTS_FILE, IGNORING_EMISSIONS_FROM_EVENTS_FILE_CMT);
+		map.put(WRITING_EMISSIONS_EVENTS, WRITING_EMISSIONS_EVENTS_CMT);
 
 		map.put(EMISSION_EFFICIENCY_FACTOR, EMISSION_EFFICIENCY_FACTOR_CMT);
 
@@ -226,15 +226,15 @@ extends ReflectiveConfigGroup
 		isUsingVehicleIdAsVehicleDescription = usingVehicleIdAsVehicleDescription;
 	}
 
-	@StringGetter(IGNORING_EMISSIONS_FROM_EVENTS_FILE)
+	@StringGetter(WRITING_EMISSIONS_EVENTS)
 	public boolean isWritingEmissionsEvents() {
 		return isWritingEmissionsEvents;
 	}
 
 	/**
-	 * @param ignoringEmissionsFromEventsFile -- {@value #IGNORING_EMISSIONS_FROM_EVENTS_FILE_CMT}
+	 * @param ignoringEmissionsFromEventsFile -- {@value #WRITING_EMISSIONS_EVENTS_CMT}
 	 */
-	@StringSetter(IGNORING_EMISSIONS_FROM_EVENTS_FILE)
+	@StringSetter(WRITING_EMISSIONS_EVENTS)
 	public void setIgnoringEmissionsFromEventsFile(boolean ignoringEmissionsFromEventsFile) {
 		isWritingEmissionsEvents = ignoringEmissionsFromEventsFile;
 	}
