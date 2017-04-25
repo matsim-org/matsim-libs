0eca5b6cf4851cf4d92385525a4bdcebbe2dcd38
Theresa Thunig <thunig@vsp.tu-berlin.de>
2017-04-25 11:52:08 +0200

    make TtTotalDelay injectable

--- playgrounds/dgrether/src/main/java/playground/dgrether/koehlerstrehlersignal/analysis/TtTotalDelay.java
+++ playgrounds/dgrether/src/main/java/playground/dgrether/koehlerstrehlersignal/analysis/TtTotalDelay.java
@@ -44,6 +44,8 @@ import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.vehicles.Vehicle;
 
+import com.google.inject.Inject;
+
 
 /**
  * Determines delay of all agents inside a given subnetwork.
