85793d5af3d0e5403ff905b623b9947f5fffdca5
clruch <claudio.ruch@gmail.com>
2017-05-06 12:48:16 +0200

    various small corrections added

--- playgrounds/ivtExt/src/main/java/playground/clruch/dispatcher/utils/TravelData.java
+++ playgrounds/ivtExt/src/main/java/playground/clruch/dispatcher/utils/TravelData.java
@@ -1,25 +1,15 @@
 package playground.clruch.dispatcher.utils;
 
-import static org.matsim.pt.PtConstants.TRANSIT_ACTIVITY_TYPE;
-
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-
-import ch.ethz.idsc.tensor.alg.Array;
-import ch.ethz.idsc.tensor.alg.Dimensions;
-import ch.ethz.idsc.tensor.io.Pretty;
 
 import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
-import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.population.Activity;
@@ -28,14 +18,15 @@ import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.Plan;
 import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.api.core.v01.population.Population;
-import org.matsim.api.core.v01.population.Route;
-import org.matsim.utils.objectattributes.attributable.Attributes;
 
 import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
 import ch.ethz.idsc.tensor.ZeroScalar;
+import ch.ethz.idsc.tensor.alg.Array;
+import ch.ethz.idsc.tensor.alg.Dimensions;
+import ch.ethz.idsc.tensor.alg.Transpose;
 import playground.clruch.netdata.VirtualNetwork;
 import playground.clruch.netdata.VirtualNode;
 import playground.clruch.utils.GlobalAssert;
@@ -44,8 +35,9 @@ import playground.clruch.utils.GlobalAssert;
  * Created by Claudio on 3/19/2017.
  */
 public class TravelData {
-    Tensor lambda;
-    Tensor pij;
+    Tensor lambda; // tensor of dimension (numberofTimeSteps, numberVirtualNodes) that contains mean arrival rates per virtual Node in timestep
+    Tensor pij; // tensor of dimension (numberofTimeSteps, numberVirtualNodes, numberVirtualNodes) pij(t,i,j) probability of moving from i to j in
+                // timestep t
     Tensor alpha_ij;
     private int numberTimeSteps;
     private final int dt; // used as lookup
@@ -57,18 +49,22 @@ public class TravelData {
     /**
      * Constructor for TravelData object creating historic travel information based on virtualNetwork and population
      * 
-     * @param virtualNetwork
+     * @param virtualNetworkIn
      * @param population
      * @param dtIn
      *            time step for calculation
      */
-    public TravelData(VirtualNetwork virtualNetwork, Network network, Population population, int dtIn) {
+    public TravelData(VirtualNetwork virtualNetworkIn, Network network, Population population, int dtIn) {
+        System.out.println("reading travel data for population of size" + population.getPersons().size());
         populationSize = population.getPersons().size();
+        virtualNetwork = virtualNetworkIn;
+        
+        
         // ensure that dayduration / timeInterval is integer value
         dt = greatestNonRestDt(dtIn, dayduration);
         numberTimeSteps = dayduration / dt;
         System.out.println("Number of time steps = " + numberTimeSteps);
-        System.out.println("dtSeconds = " + dt);
+        System.out.println("dt = " + dt);
         GlobalAssert.that(dayduration % dt == 0);
 
         // create lambdas and pij
@@ -117,7 +113,6 @@ public class TravelData {
             }
         }
         // norm pij such that it is row stochastic, i.e. sum(p_ij)_j = 1 for all i and all time indexes
-        System.out.println(Pretty.of(pij));
         for (int t = 0; t < numberTimeSteps; ++t) {
             for (int i = 0; i < virtualNetwork.getvNodesCount(); ++i) {
                 Scalar sum = RealScalar.of(0.0);
@@ -134,9 +129,13 @@ public class TravelData {
                 }
             }
         }
-        System.out.println(Pretty.of(pij));
+        checkConsistency();
+        // TODO how to get dimensions of Tensor?
+        System.out.println("successfully created lambda matrix of size " + lambda.length() + " x " + Transpose.of(lambda).length());
+        System.out.println("successfully created pij matrix of size " + "?" + " x " + "?"+ " x "+ "?");
     }
 
+    @Deprecated
     public TravelData(VirtualNetwork virtualNetworkIn, File lambdaFile, File pijFile, File alphaijFile, long populationSize, int rebalancingPeriod)
             throws JDOMException, IOException {
         virtualNetwork = virtualNetworkIn;
@@ -201,6 +200,7 @@ public class TravelData {
         return hours * 60 * 60 + minutes * 60 + seconds;
     }
 
+    @Deprecated
     private Scalar getLambdafromFile(int row, int col, Element rootNode) {
         List<Element> children = rootNode.getChildren();
 
@@ -215,6 +215,7 @@ public class TravelData {
         return RealScalar.of(Double.parseDouble(timeatvNode.getAttribute("lambda").getValue()));
     }
 
+    @Deprecated
     private Scalar getpijfromFile(int from, int to, int timestep, Element rootNode) {
         List<Element> children = rootNode.getChildren();
 
@@ -235,6 +236,7 @@ public class TravelData {
         return ZeroScalar.get();
     }
 
+    @Deprecated
     private Scalar getalphaijfromFile(int from, int to, int timestep, Element rootNode) {
         List<Element> children = rootNode.getChildren();
 
@@ -309,6 +311,24 @@ public class TravelData {
         if (length % dt == 0)
             return dt;
         return greatestNonRestDt(dt - 1, length);
+    }
+
+    
+    /**
+     * Perform consistency checks after completion of constructor operations. 
+     */
+    private void checkConsistency() {
+        for (int t = 0; t < numberTimeSteps; ++t) {
+            for (int i = 0; i < virtualNetwork.getvNodesCount(); ++i) {
+                Scalar sum = RealScalar.of(0.0);
+                for (int j = 0; j < virtualNetwork.getvNodesCount(); ++j) {
+                    Scalar val = (Scalar) pij.get(t, i, j);
+                    sum = sum.add(val);
+                }
+                int sumInt = sum.number().intValue();
+                GlobalAssert.that(sumInt == 1 || sumInt == 0);
+            }
+        }
     }
 
 }
