623205714c031d307e5fb3071f0786677bc007a9
Amit Agarwal <amit.agarwal@campus.tu-berlin.de>
2017-05-07 12:42:33 +0200

    creating/adding vehicles in perpare for sim than in population agent source. However, all tests which runs mobsim will fail.

--- matsim/src/main/java/org/matsim/core/controler/PrepareForSimImpl.java
+++ matsim/src/main/java/org/matsim/core/controler/PrepareForSimImpl.java
@@ -1,26 +1,33 @@
 package org.matsim.core.controler;
 
 
+import java.util.HashMap;
 import java.util.HashSet;
-
+import java.util.Map;
 import javax.inject.Inject;
 import javax.inject.Provider;
-
 import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Network;
-import org.matsim.api.core.v01.population.Population;
+import org.matsim.api.core.v01.population.*;
 import org.matsim.core.config.groups.GlobalConfigGroup;
+import org.matsim.core.config.groups.QSimConfigGroup;
+import org.matsim.core.gbl.Gbl;
 import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.network.algorithms.TransportModeNetworkFilter;
 import org.matsim.core.population.algorithms.AbstractPersonAlgorithm;
 import org.matsim.core.population.algorithms.ParallelPersonAlgorithmRunner;
 import org.matsim.core.population.algorithms.PersonPrepareForSim;
+import org.matsim.core.population.routes.NetworkRoute;
 import org.matsim.core.router.PlanRouter;
 import org.matsim.core.router.TripRouter;
 import org.matsim.core.scenario.Lockable;
 import org.matsim.facilities.ActivityFacilities;
+import org.matsim.vehicles.Vehicle;
+import org.matsim.vehicles.VehicleType;
+import org.matsim.vehicles.VehicleUtils;
 
 class PrepareForSimImpl implements PrepareForSim {
 
@@ -32,15 +39,20 @@ class PrepareForSimImpl implements PrepareForSim {
 	private final Population population;
 	private final ActivityFacilities activityFacilities;
 	private final Provider<TripRouter> tripRouterProvider;
+	private final QSimConfigGroup qSimConfigGroup;
+
+	private final Map<String, VehicleType> modeVehicleTypes;
 
 	@Inject
-	PrepareForSimImpl(GlobalConfigGroup globalConfigGroup, Scenario scenario, Network network, Population population, ActivityFacilities activityFacilities, Provider<TripRouter> tripRouterProvider) {
+	PrepareForSimImpl(GlobalConfigGroup globalConfigGroup, Scenario scenario, Network network, Population population, ActivityFacilities activityFacilities, Provider<TripRouter> tripRouterProvider, QSimConfigGroup qSimConfigGroup) {
 		this.globalConfigGroup = globalConfigGroup;
 		this.scenario = scenario;
 		this.network = network;
 		this.population = population;
 		this.activityFacilities = activityFacilities;
 		this.tripRouterProvider = tripRouterProvider;
+		this.qSimConfigGroup = qSimConfigGroup;
+		this.modeVehicleTypes = new HashMap<>();
 	}
 
 
@@ -63,8 +75,42 @@ class PrepareForSimImpl implements PrepareForSim {
 			net = network;
 		}
 
-		// vehicles should be created before a route is created. Amit May'17
+		// vehicles should be created (if not available) before a route is created. Amit May'17
+		storeMode2VehicleType();
 
+		for(Person person : scenario.getPopulation().getPersons().values()) {
+			Plan plan = person.getSelectedPlan();
+			Map<String,Id<Vehicle>> seenModes = new HashMap<>();
+			for(PlanElement planElement : plan.getPlanElements()) {
+				if( planElement instanceof Leg) {
+					Leg leg =  (Leg) planElement;
+					if (qSimConfigGroup.getMainModes().contains(leg.getMode())) {// only simulated modes get vehicles
+						NetworkRoute route = (NetworkRoute) leg.getRoute();
+						Id<Vehicle> vehicleId = null ;
+						if (route != null) {
+							vehicleId = route.getVehicleId(); // may be null!
+						}
+
+						if (!seenModes.keySet().contains(leg.getMode())) { // create one vehicle per simulated mode, put it on the home location
+
+							if (vehicleId == null) {
+								vehicleId = createAutomaticVehicleId(person, leg, route);
+							}
+
+							// so here we have a vehicle id, now try to find or create a physical vehicle:
+							Vehicle vehicle = createVehicle(leg, vehicleId);
+							seenModes.put(leg.getMode(),vehicleId);
+						} else {
+							if (vehicleId==null && route!=null) {
+								vehicleId = seenModes.get(leg.getMode());
+								route.setVehicleId( vehicleId );
+							}
+						}
+
+					}
+				}
+			}
+		}
 
 
 		// make sure all routes are calculated.
@@ -91,5 +137,82 @@ class PrepareForSimImpl implements PrepareForSim {
 		
 		// (yyyy means that if someone replaces prepareForSim and does not add the above lines, the containers are not locked.  kai, nov'16)
 
+	}
+
+	private void storeMode2VehicleType(){
+		switch ( this.qSimConfigGroup.getVehiclesSource() ) {
+			case defaultVehicle:
+				for (String mode : this.qSimConfigGroup.getMainModes()) {
+					// initialize each mode with default vehicle type:
+					modeVehicleTypes.put(mode, VehicleUtils.getDefaultVehicleType());
+				}
+				break;
+			case modeVehicleTypesFromVehiclesData:
+				for (String mode : qSimConfigGroup.getMainModes()) {
+					VehicleType vehicleType = scenario.getVehicles().getVehicleTypes().get( Id.create(mode, VehicleType.class) ) ;
+					Gbl.assertNotNull(vehicleType);
+					modeVehicleTypes.put(mode, vehicleType );
+				}
+				break;
+			case fromVehiclesData:
+				// don't do anything
+				break;
+			default:
+				throw new RuntimeException("not implemented yet.");
+		}
+	}
+
+	private Vehicle createVehicle(Leg leg, Id<Vehicle> vehicleId) {
+		// try to get vehicle from the vehicles container:
+		Vehicle vehicle = scenario.getVehicles().getVehicles().get(vehicleId);
+
+		if ( vehicle==null ) {
+			// if it was not found, next step depends on config:
+			switch ( qSimConfigGroup.getVehiclesSource() ) {
+				case defaultVehicle:
+					// create vehicle but don't add it to the container. Amit Apr'17
+					vehicle = VehicleUtils.getFactory().createVehicle(vehicleId, modeVehicleTypes.get(leg.getMode()));
+					break;
+				case modeVehicleTypesFromVehiclesData:
+					// if config says mode vehicles, then create and add it:
+					vehicle = VehicleUtils.getFactory().createVehicle(vehicleId, modeVehicleTypes.get(leg.getMode()));
+					scenario.getVehicles().addVehicle(vehicle);
+					break;
+				case fromVehiclesData:
+					// otherwise complain:
+					throw new IllegalStateException("Expecting a vehicle id which is missing in the vehicles database: " + vehicleId);
+				default:
+					// also complain when someone added another config option here:
+					throw new RuntimeException("not implemented") ;
+			}
+		}
+		return vehicle;
+	}
+
+	private Id<Vehicle> createAutomaticVehicleId(Person p, Leg leg, NetworkRoute route) {
+		Id<Vehicle> vehicleId ;
+		if (qSimConfigGroup.getUsePersonIdForMissingVehicleId()) {
+
+			switch (qSimConfigGroup.getVehiclesSource()) {
+				case defaultVehicle:
+				case fromVehiclesData:
+					vehicleId = Id.createVehicleId(p.getId());
+					break;
+				case modeVehicleTypesFromVehiclesData:
+					if(!leg.getMode().equals(TransportMode.car)) {
+						String vehIdString = p.getId().toString() + "_" + leg.getMode() ;
+						vehicleId = Id.create(vehIdString, Vehicle.class);
+					} else {
+						vehicleId = Id.createVehicleId(p.getId());
+					}
+					break;
+				default:
+					throw new RuntimeException("not implemented") ;
+			}
+			if(route!=null) route.setVehicleId(vehicleId);
+		} else {
+			throw new IllegalStateException("Found a network route without a vehicle id.");
+		}
+		return vehicleId;
 	}
 }
