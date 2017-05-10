a5ca791148c5f784f231790ba84c797e110990c7
clruch <claudio.ruch@gmail.com>
2017-05-10 08:44:28 +0200

    added AVTravelTimeModule() of Sebastian to avoid congestion in AV scenarios, not tested.

---
+++ playgrounds/ivtExt/src/main/java/playground/clruch/traveltimetracker/AVTravelTime.java
@@ -0,0 +1,35 @@
+package playground.clruch.traveltimetracker;
+
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.core.router.util.TravelTime;
+import org.matsim.vehicles.Vehicle;
+
+public class AVTravelTime implements TravelTime {
+    final private AVTravelTimeTracker travelTimeTracker;
+    final private TravelTime delegate;
+
+    final private double maximumInterpolationTime = 300.0;
+    final private double exponent = 1.0;
+
+    public AVTravelTime(AVTravelTimeTracker travelTimeTracker, TravelTime delegate) {
+        this.delegate = delegate;
+        this.travelTimeTracker = travelTimeTracker;
+    }
+
+    @Override
+    public double getLinkTravelTime(Link link, double time, Person person, Vehicle vehicle) {
+        AVTravelTimeTracker.LinkTravelTime travelTime = travelTimeTracker.getLinkTravelTime(link.getId());
+        double delegateTravelTime = delegate.getLinkTravelTime(link, time, person, vehicle);
+
+        if (travelTime.updateTime > time - maximumInterpolationTime) {
+            return interpolate(delegateTravelTime, travelTime.travelTime, (time - travelTime.updateTime) / maximumInterpolationTime);
+        } else {
+            return delegateTravelTime;
+        }
+    }
+
+    private double interpolate(double freespeedTravelTime, double measuredTravelTime, double relativeElapsedTime) {
+        return Math.pow(1.0 - relativeElapsedTime, exponent) * measuredTravelTime + Math.pow(relativeElapsedTime, exponent) * freespeedTravelTime;
+    }
+}
---
+++ playgrounds/ivtExt/src/main/java/playground/clruch/traveltimetracker/AVTravelTimeTracker.java
@@ -0,0 +1,61 @@
+package playground.clruch.traveltimetracker;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.events.LinkEnterEvent;
+import org.matsim.api.core.v01.events.LinkLeaveEvent;
+import org.matsim.api.core.v01.events.VehicleLeavesTrafficEvent;
+import org.matsim.api.core.v01.events.handler.LinkEnterEventHandler;
+import org.matsim.api.core.v01.events.handler.LinkLeaveEventHandler;
+import org.matsim.api.core.v01.events.handler.VehicleLeavesTrafficEventHandler;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.vehicles.Vehicle;
+
+public class AVTravelTimeTracker implements LinkEnterEventHandler, LinkLeaveEventHandler, VehicleLeavesTrafficEventHandler {
+    final private Map<Id<Vehicle>, Double> enterTimes = new HashMap<>();
+    final private Map<Id<Link>, LinkTravelTime> travelTimes = new HashMap<>();
+
+    public class LinkTravelTime {
+        public double travelTime = Double.NaN;
+        public double updateTime = Double.NaN;
+    }
+
+    @Override
+    public void handleEvent(LinkEnterEvent event) {
+        enterTimes.put(event.getVehicleId(), event.getTime());
+    }
+
+    @Override
+    public void handleEvent(LinkLeaveEvent event) {
+        Double enterTime = enterTimes.remove(event.getVehicleId());
+
+        if (enterTime != null) {
+            LinkTravelTime travelTime = travelTimes.get(event.getLinkId());
+            if (travelTime == null) travelTime = new LinkTravelTime();
+
+            travelTime.travelTime = event.getTime() - enterTime;
+            travelTime.updateTime = event.getTime();
+
+            travelTimes.put(event.getLinkId(), travelTime);
+        }
+    }
+
+    @Override
+    public void handleEvent(VehicleLeavesTrafficEvent event) {
+        enterTimes.remove(event.getVehicleId());
+    }
+
+    @Override
+    public void reset(int iteration) {
+        for (LinkTravelTime travelTime : travelTimes.values()) {
+            travelTime.updateTime = Double.NaN;
+        }
+    }
+
+    public LinkTravelTime getLinkTravelTime(Id<Link> linkId) {
+        LinkTravelTime travelTime = travelTimes.get(linkId);
+        return (travelTime == null) ? new LinkTravelTime() : travelTime;
+    };
+}
