3d2c6a1fd5f4294ab41c4ac67c0ab666d1e3ce38
Nico <nkuehnel2@gmail.com>
2017-06-06 13:47:54 +0200

    laemmer updates

--- playgrounds/tthunig/src/main/java/signals/advancedPlanbased/AdvancedPlanBasedSignalSystemController.java
+++ playgrounds/tthunig/src/main/java/signals/advancedPlanbased/AdvancedPlanBasedSignalSystemController.java
@@ -4,6 +4,7 @@ import com.google.inject.Provider;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.contrib.signals.model.*;
+import org.matsim.lanes.data.Lane;
 import playground.dgrether.koehlerstrehlersignal.analysis.TtTotalDelay;
 import signals.Analyzable;
 import signals.sensor.LinkSensorManager;
@@ -59,9 +60,16 @@ public class AdvancedPlanBasedSignalSystemController implements SignalController
     @Override
     public void simulationInitialized(double simStartTimeSeconds) {
         delegate.simulationInitialized(simStartTimeSeconds);
-        for (Signal signal : system.getSignals().values()) {
-            Id<Link> linkId = signal.getLinkId();
-            this.sensorManager.registerNumberOfCarsInDistanceMonitoring(linkId, 0.);
+        for (SignalGroup group : this.system.getSignalGroups().values()) {
+            for (Signal signal : group.getSignals().values()) {
+                if (signal.getLaneIds() != null && !(signal.getLaneIds().isEmpty())) {
+                    for (Id<Lane> laneId : signal.getLaneIds()) {
+                        this.sensorManager.registerNumberOfCarsOnLaneInDistanceMonitoring(signal.getLinkId(), laneId, 0.);
+                    }
+                }
+                //always register link in case only one lane is specified (-> no LaneEnter/Leave-Events?)
+                this.sensorManager.registerNumberOfCarsInDistanceMonitoring(signal.getLinkId(), 0.);
+            }
         }
     }
 
@@ -77,9 +85,7 @@ public class AdvancedPlanBasedSignalSystemController implements SignalController
         builder.append("total_delay;");
         for (SignalGroup group : this.system.getSignalGroups().values()) {
             builder.append("state_group_" + group.getId() + ";");
-        }
-        for (Signal signal : system.getSignals().values()) {
-            builder.append("n_signal_" + signal.getId() + ";");
+            builder.append("n_group_" + group.getId() + ";");
         }
         return builder.toString();
     }
@@ -94,9 +100,17 @@ public class AdvancedPlanBasedSignalSystemController implements SignalController
             } else {
                 builder.append("null;");
             }
-        }
-        for (Signal signal : system.getSignals().values()) {
-            builder.append(sensorManager.getNumberOfCarsInDistance(signal.getLinkId(), 0., now) + ";");
+            int totalN = 0;
+            for (Signal signal : group.getSignals().values()) {
+                if (signal.getLaneIds() != null && !signal.getLaneIds().isEmpty() && signal.getLaneIds().size() > 1) {
+                    for (Id<Lane> laneId : signal.getLaneIds()) {
+                        totalN += sensorManager.getNumberOfCarsInDistanceOnLane(signal.getLinkId(), laneId, 0.,now);
+                    }
+                } else {
+                    totalN += sensorManager.getNumberOfCarsInDistance(signal.getLinkId(), 0., now);
+                }
+            }
+            builder.append(totalN + ";");
         }
         return builder.toString();
     }
