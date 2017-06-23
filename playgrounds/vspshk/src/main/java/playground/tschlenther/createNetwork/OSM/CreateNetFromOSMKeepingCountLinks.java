ca7d0d5d75c4afef01da5bd6a9a225b2b381c4c6
Tilmann Schlenther <schlenther@vsp.tu-berlin.de>
2017-06-23 15:52:51 +0200

    some more logging in OsmNetworkReader and other minor changes

--- playgrounds/vspshk/src/main/java/playground/tschlenther/createNetwork/OSM/CreateNetFromOSMKeepingCountLinks.java
+++ playgrounds/vspshk/src/main/java/playground/tschlenther/createNetwork/OSM/CreateNetFromOSMKeepingCountLinks.java
@@ -24,7 +24,9 @@ package playground.tschlenther.createNetwork.OSM;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
@@ -35,6 +37,9 @@ import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.geometry.CoordinateTransformation;
 import org.matsim.core.utils.geometry.transformations.TransformationFactory;
 import org.matsim.core.utils.io.OsmNetworkReader;
+import org.matsim.core.utils.io.tabularFileParser.TabularFileHandler;
+import org.matsim.core.utils.io.tabularFileParser.TabularFileParser;
+import org.matsim.core.utils.io.tabularFileParser.TabularFileParserConfig;
 
 /**
  * @author Work
@@ -42,15 +47,16 @@ import org.matsim.core.utils.io.OsmNetworkReader;
  */
 public class CreateNetFromOSMKeepingCountLinks {
 
-	private static final String INPUT_OSMFILE = "C:/Users/Work/VSP/OSM/Reinickendorf_Flottenstr.osm";
-	private static final String OUTPUT_NETWORK = "C:/Users/Work/VSP/OSM/test_DontKeepPaths_fast_firstKeep1NodeThanSimplify.xml";
+	private static final String INPUT_OSMFILE = "C:/Users/Work/svn/shared-svn/studies/fzwick/berlinHighways.osm";
+	private static final String INPUT_COUNT_NODES= "C:/Users/Work/svn/shared-svn/studies/countries/de/berlin_scenario_2016/network_counts/counts-osm-mapping/2017-06-17/OSM-nodes.csv";
+	private static final String OUTPUT_NETWORK = "C:/Users/Work/VSP/OSM/testFull.xml";
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		readAndWrite();
 	}
-
+	
 	
 	private static void readAndWrite(){
 	
@@ -66,20 +72,63 @@ public class CreateNetFromOSMKeepingCountLinks {
 		onr.setKeepPaths(false);
 		
 		Set<Long> nodeIDsToKeep = new HashSet<Long>();
-		nodeIDsToKeep.add(1184135011l);
+
+		nodeIDsToKeep = readNodeIDs(INPUT_COUNT_NODES, "\t");
+		
 		onr.setNodeIDsToKeep(nodeIDsToKeep);
 		
 		onr.parse(INPUT_OSMFILE);
+		
+		
+		/*
+		 * simplify network: merge links that are shorter than the given threshold
+		 */
+		NetworkSimplifier simp = new NetworkSimplifier();
+		simp.setNodesNotToMerge(nodeIDsToKeep);
+		simp.run(network);
+		
+		new NetworkCleaner().run(network);
 		
 		/*
 		 * Clean the Network. Cleaning means removing disconnected components, so that afterwards there is a route from every link
 		 * to every other link. This may not be the case in the initial network converted from OpenStreetMap.
 		 */
-		new NetworkSimplifier().run(network);
 		
 		new NetworkWriter(network).write(OUTPUT_NETWORK);
 		
 	}
 	
 	
+	/**
+	 * expects a path to csv file that has the following structure: <br><br>
+	 * 
+	 * <i>COUNT-ID {@linkplain delimiter} OSM_FROMNODE_ID {@linkplain delimiter} OSM_TONODE_ID </i><br><br>
+	 * 
+	 * It is assumed that the csv file contains a header line.
+	 * returns a set of all mentioned osm-node-ids.
+	 * 
+	 * @param pathToCSVFile
+	 * @return
+	 */
+	private static Set<Long> readNodeIDs(String pathToCSVFile, String delimiter){
+		Set<Long> allNodeIDs = new HashSet<Long>();
+		
+		TabularFileParserConfig config = new TabularFileParserConfig();
+	    config.setDelimiterTags(new String[] {delimiter});
+	    config.setFileName(pathToCSVFile);
+	    new TabularFileParser().parse(config, new TabularFileHandler() {
+	    	boolean header = true;
+			@Override
+			public void startRow(String[] row) {
+				if(!header){
+					allNodeIDs.add( Long.parseLong(row[1]) );
+					allNodeIDs.add( Long.parseLong(row[2]) );
+				}
+				header = false;				
+			}
+		});
+
+	    return allNodeIDs;
+		
+	}
 }
