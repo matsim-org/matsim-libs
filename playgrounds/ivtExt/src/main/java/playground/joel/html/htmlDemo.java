6ca5d3cc62eb3e714f6e3a2f29bf65665222ec66
JoelGaechter <joelgaechter@bluewin.ch>
2017-06-27 12:20:51 +0200

    html wip

--- playgrounds/ivtExt/src/main/java/playground/joel/html/htmlDemo.java
+++ playgrounds/ivtExt/src/main/java/playground/joel/html/htmlDemo.java
@@ -14,14 +14,48 @@ public class htmlDemo {
         file.mkdir();
 
         // write document
+        // -------------------------------------------------------------------------------------------------------------
+        // set up
         htmlUtils.html();
-        htmlUtils.title("HTML Demo");
+        // specific formatting for the entire document
+        htmlUtils.insertCSS( //
+                "p {text-align: center;", //
+                    "width: 40%;", //
+                    "padding: 20px;", //
+                    "margin: 10px;", //
+                    "float: left;", //
+                    "border: 1px solid black;}", //
+                "img {float: right;", //
+                    "padding: 20px;}", //
+                "pre {float: left;}", //
+                "table {float: right;}" //
+        );
+        // begin of body
         htmlUtils.body();
+        // set title in header and as tab title
+        htmlUtils.title("HTML Demo");
+        // text displayed line by line
         htmlUtils.insertText("1st line", "2nd line", "3rd line", "and so forth");
+        htmlUtils.insertImg("data/binnedWaitingTimes.png", 400, 300);
+        htmlUtils.newLine();
+        // text writen in this exact manner
+        htmlUtils.insertText("1st line of preformatted text\n" + //
+                "2nd line of preformatted text\n" + //
+                "3rd line and \t so \t forth");
+        htmlUtils.insertTable(3,2, true, "h1", "h2", "h3", "c1", "c2", "c3");
+        htmlUtils.newLine();
+        htmlUtils.insertList("element 1", "element 2", "element 3", "element 3");
+        // begin of footer
+        htmlUtils.footer();
+        htmlUtils.insertLink("http://www.idsc.ethz.ch/", "www.idsc.ethz.ch");
+        // end of footer
+        htmlUtils.footer();
+        // end of body
         htmlUtils.body();
         htmlUtils.html();
 
         // save document
+        // -------------------------------------------------------------------------------------------------------------
         try {
             htmlUtils.saveFile(fileName);
         } catch (Exception e) {
--- playgrounds/ivtExt/src/main/java/playground/joel/html/htmlUtils.java
+++ playgrounds/ivtExt/src/main/java/playground/joel/html/htmlUtils.java
@@ -11,19 +11,25 @@ public abstract class htmlUtils {
     public static StringBuilder stringBuilder = new StringBuilder();
     public static boolean html = false;
     public static boolean head = false;
+    public static boolean header = false;
     public static boolean body = false;
+    public static boolean paragraph = false;
+    public static boolean footer = false;
+    public static boolean style = false;
 
     /**
      * called at the begin and end of document
      */
     public static void html() {
-        if (html) {
-            stringBuilder.append("</html>");
-            html = false;
-        }
+        if (html) {stringBuilder.append("</html>"); html = false;}
         else {
-            stringBuilder.append("<html>");
-            html = true;
+            stringBuilder.append("<html>"); html = true;
+            htmlUtils.insertCSS("header, footer {font-family: arial;", //
+                    "background-color: #000099;", //
+                    "color: white;", //
+                    "padding: 20px;}", //
+                    "body {font-family: times;", //
+                    "line-height: 1.5}");
         }
     }
 
@@ -31,28 +37,52 @@ public abstract class htmlUtils {
      * called at the begin and end of head
      */
     public static void head() {
-        if (head) {
-            stringBuilder.append("</head>");
-            head = false;
-        }
-        else {
-            stringBuilder.append("<head>");
-            head = true;
-        }
+        if (head) {stringBuilder.append("</head>"); head = false;}
+        else {stringBuilder.append("<head>"); head = true;}
+    }
+
+    /**
+     * called at the begin and end of header
+     */
+    public static void header() {
+        if (header) {stringBuilder.append("</header>"); header = false;}
+        else {stringBuilder.append("<header>"); header = true;}
     }
 
     /**
      * called at the begin and end of document
      */
     public static void body() {
-        if (body) {
-            stringBuilder.append("</body>");
-            body = false;
-        }
-        else {
-            stringBuilder.append("<body>");
-            body = true;
-        }
+        if (body) {stringBuilder.append("</body>"); body = false;}
+        else {stringBuilder.append("<body>"); body = true;}
+    }
+
+    /**
+     * called at the begin and end of paragraph
+     */
+    public static void paragraph() {
+        if (paragraph) {stringBuilder.append("</p>"); paragraph = false;}
+        else {stringBuilder.append("<p>"); paragraph = true;}
+    }
+
+    public static void newLine() {
+        stringBuilder.append("<br style=\"clear:both\">");
+    }
+
+    /**
+     * called at the begin and end of footer
+     */
+    public static void footer() {
+        if (footer) {stringBuilder.append("</footer>"); header = false;}
+        else {stringBuilder.append("<footer>"); footer = true;}
+    }
+
+    /**
+     * called at the begin and end of CSS
+     */
+    public static void style() {
+        if (style) {stringBuilder.append("</style>"); style = false;}
+        else {stringBuilder.append("<style>"); style = true;}
     }
 
     /**
@@ -72,9 +102,9 @@ public abstract class htmlUtils {
      * @param title
      */
     public static void insertTitle(String title) {
-        head();
+        header();
         stringBuilder.append("<h1>" + title + "</h1>");
-        head();
+        header();
     }
 
     public static void title(String title) {
@@ -82,15 +112,73 @@ public abstract class htmlUtils {
         setTitle(title);
     }
 
-    public static void insertText(String... line) {
-        for (int i = 0; i < line.length; i++) {
-            stringBuilder.append("<p>" + line[i] + "</p>");
+    public static void insertText(String... lines) {
+        paragraph();
+        for (int i = 0; i < lines.length; i++)
+            stringBuilder.append(lines[i] + "<br>");
+        paragraph();
+    }
+
+    public static void insertText(String text) {
+        stringBuilder.append("<pre>" + text + "</pre>");
+    }
+
+    public static void insertLink(String url, String link) {
+        htmlUtils.insertCSS("a {font-family: arial;}");
+        if (footer || header) htmlUtils.insertCSS("a {color: white;}");
+        stringBuilder.append("<a href=\"" + url + "\"> <b>" + link + "</b></a>");
+    }
+
+    public static void insertCSS(String... line) {
+        head();
+        style();
+        for (int i = 0; i < line.length; i++)
+            stringBuilder.append(line[i]);
+        style();
+        head();
+    }
+
+    public static void insertImg(String relPath) {
+        insertImg(relPath, 800, 600);
+    }
+
+    public static void insertImg(String relPath, int width, int heigth) {
+        stringBuilder.append("<img src=" + relPath + " alt=\"image not found\" style=\"width:" + //
+                width + "px;height:" + heigth + "px;\">");
+    }
+
+    /**
+     * first elements are automatically set as headings
+     *
+     * @param columns
+     * @param rows
+     * @param borders
+     * @param content
+     */
+    public static void insertTable(int columns, int rows, boolean borders, String... content) {
+        if (content.length != columns*rows) stringBuilder.append("unable to create table");
+        else {
+            if (borders) insertCSS("th, td {border: 1px solid black;}");
+            insertCSS("th, td {padding: 5px; margin: 5px}");
+            stringBuilder.append("<table style=\"border-collapse: collapse;\">");
+            for (int i = 0; i < rows; i++) {
+                stringBuilder.append("<tr>");
+                for (int j = 0; j < columns; j++) {
+                    if (i == 0) stringBuilder.append("<th>" + content[j] + "</th>");
+                    else stringBuilder.append("<td>" + content[i*columns + j] + "</td>");
+                }
+                stringBuilder.append("</tr>");
+            }
+            stringBuilder.append("</table>");
         }
     }
 
-
-
-
+    public static void insertList(String... lines) {
+        stringBuilder.append("<ul style=\"list-style-type:disc\">");
+        for (int i = 0; i < lines.length; i++)
+            stringBuilder.append("<li>" + lines[i] + "</li>");
+        stringBuilder.append("</ul>");
+    }
 
     public static void saveFile(String fileName) throws IOException {
         File file = new File("output", fileName);
