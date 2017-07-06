05b85b5d5578c8b5ab2b6710b94dd3a1eeacd41d
clruch <claudio.ruch@gmail.com>
2017-07-06 17:29:39 +0200

    various improvements together with Sebastian.

--- playgrounds/ivtExt/src/main/java/playground/clruch/ScenarioPreparer.java
+++ playgrounds/ivtExt/src/main/java/playground/clruch/ScenarioPreparer.java
@@ -47,16 +47,16 @@ public class ScenarioPreparer {
 
         // BEGIN: CUSTOMIZE -----------------------------------------------
         // set manually depending on the scenario:
-        final int maxPopulationSize = 1000;
+        final int maxPopulationSize = 80000;
         final int numVirtualNodes = 40;
-        final int dtTravelData = 300;
+        final int dtTravelData = 600;
         final boolean completeGraph = true;
 
         // LocationSpec object to specify city location and center, radius for cutting
         LocationSpec ls = LocationSpec.ZURICH_CITY;
 
         final boolean populationeliminateFreight = true;
-        final boolean populationeliminateWalking = true;
+        final boolean populationeliminateWalking = false;
         final boolean populationchangeModeToAV = true; // TODO check if this is still needed !?
 
         // output file names
