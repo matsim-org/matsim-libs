0ba81dbde3afac51601fccaec91fa1c75a5f3219
clruch <claudio.ruch@gmail.com>
2017-07-10 23:20:36 +0200

    fixed bug that caused negative rebalancing (LP solver numerical errors).

--- playgrounds/ivtExt/src/main/java/playground/clruch/traveldata/TravelData.java
+++ playgrounds/ivtExt/src/main/java/playground/clruch/traveldata/TravelData.java
@@ -176,7 +176,22 @@ public class TravelData implements Serializable {
 
             // ensure positivity of solution (small negative values possible due to solver
             // accuracy)
-            rebalancingRate.flatten(-1).forEach(v -> GlobalAssert.that(v.Get().number().doubleValue() > -10E-7));
+            
+            // ensure positivity of solution (small negative values possible due to solver
+            // accuracy)
+            for (int i = 0; i < Dimensions.of(rebalancingRate).get(0); ++i) {
+                for (int j = 0; j < Dimensions.of(rebalancingRate).get(1); ++j) {
+                    if (rebalancingRate.Get(i, j).number().doubleValue() < 0) {
+                        GlobalAssert.that(rebalancingRate.Get(i, j).number().doubleValue() > 10E-7);
+                        rebalancingRate.set(RealScalar.of(0.0), i, j);
+                    }
+                }
+            }
+
+            // rebalancingRate.flatten(-1).forEach(v -> GlobalAssert.that(v.Get().number().doubleValue() > -10E-7));
+            
+            
+            
 
             alphaijPSF.set(rebalancingRate, t);
 
