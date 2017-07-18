a8143b0bb15ee5ceb07e9c43c1f98b77ec6f3e62
clruch <claudio.ruch@gmail.com>
2017-07-18 14:22:44 +0200

    fixed MeanValueAnalysis.

--- playgrounds/ivtExt/src/main/java/playground/joel/analysis/MeanValueAnalysis.java
+++ playgrounds/ivtExt/src/main/java/playground/joel/analysis/MeanValueAnalysis.java
@@ -1,58 +1,57 @@
 package playground.joel.analysis;
 
-import ch.ethz.idsc.tensor.*;
-import ch.ethz.idsc.tensor.alg.Array;
-import ch.ethz.idsc.tensor.alg.Dimensions;
-import ch.ethz.idsc.tensor.red.Total;
+import ch.ethz.idsc.tensor.RealScalar;
+import ch.ethz.idsc.tensor.Scalar;
+import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.Tensors;
 import ch.ethz.idsc.tensor.sca.InvertUnlessZero;
 import playground.clruch.utils.GlobalAssert;
 
 /**
- * Created by Joel on 11.07.2017.
+ * @author Claudio Ruch Performns mean value analysis for a system with i nodes, relative throughputs pii, arrival rates mui
  */
 public class MeanValueAnalysis {
-    final int maxTokens;
-    final int size;
-    final Tensor mi;
-    final Tensor pi;
+    final int maxAgents;
+    final int numNodes;
+    final Tensor mui;
+    final Tensor pii;
+    final Tensor muiInv;
 
-    Tensor W = Tensors.empty();
+    Tensor W;
     Tensor L;
 
-    public MeanValueAnalysis(int maxTokensIn, Tensor miIn, Tensor piIn) {
-        maxTokens = maxTokensIn;
-        mi = miIn;
-        pi = piIn;
-        GlobalAssert.that(mi.length() == pi.length());
-        size = mi.length();
-        W = Array.zeros(maxTokens, size);
-        L = Array.zeros(maxTokens, size);
+    public MeanValueAnalysis(int maxAgentsIn, Tensor muiIn, Tensor piiIn) {
+        maxAgents = maxAgentsIn;
+        mui = muiIn;
+        pii = piiIn;
+        muiInv = InvertUnlessZero.of(mui);
+        GlobalAssert.that(mui.length() == pii.length());
+        numNodes = mui.length();
+        W = Tensors.matrix((i, j) -> RealScalar.ZERO, maxAgentsIn + 1, numNodes);
+        L = Tensors.matrix((i, j) -> RealScalar.ZERO, maxAgentsIn + 1, numNodes);
+        perform();
     }
 
-    public void perform() {
+    private void perform() {
         // L_i(0) = 0 was initialized in the constructor
-        for (int tokens = 1; tokens < maxTokens; tokens++) {
-            updateW(tokens);
-            updateL(tokens);
+        // W_i(0) = 0 was initialized in the constructor
+        for (int agents = 1; agents <= maxAgents; ++agents) {
+            updateW(agents);
+            updateL(agents);
         }
     }
 
-    private void updateW(int tokens) {
-        Tensor Wt = L.get(tokens - 1).add(Array.of(e -> RealScalar.of(1), size)).pmul(mi.map(InvertUnlessZero.function));
-        W.set(Wt, tokens);
-        System.out.println("Wt " + Dimensions.of(Wt)+ " = " + Dimensions.of(Wt));
-        System.out.println("W " + Dimensions.of(W)+ " = " + Dimensions.of(W));
+    private void updateW(int agents) {
+        Tensor OnePlusLi = (L.get(agents - 1)).add(Tensors.vector(i_ -> RealScalar.ONE, numNodes));
+        W.set(muiInv.pmul(OnePlusLi), agents);
     }
 
-    private void updateL(int tokens) {
-        Tensor piMulW = W.get(tokens).pmul(pi);
-        Scalar norm = Total.of(piMulW).map(InvertUnlessZero.function).Get();
-        Tensor Lt = piMulW.multiply(RealScalar.of(tokens)).multiply(norm);
-        L.set(Lt, tokens);
-        System.out.println("Lt " + Dimensions.of(Lt)+ " = " + Dimensions.of(Lt));
-        System.out.println("L " + Dimensions.of(L)+ " = " + Dimensions.of(L));
-    }
+    private void updateL(int agents) {
+        Tensor numerator = (W.get(agents).pmul(pii)).multiply(RealScalar.of(agents));
+        Tensor denominator = mui.dot(W.get(agents));
+        L.set(numerator.multiply((Scalar) InvertUnlessZero.of(denominator)), agents);
 
+    }
 
     // external getter methods
     public Tensor getW() {
