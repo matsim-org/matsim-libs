5fb663bca5230af5d8d16fd011f5149af5f7c001
clruch <claudio.ruch@gmail.com>
2017-07-27 15:57:41 +0200

    inermediate solution, need to look at case with disjoint subcycles.

--- playgrounds/ivtExt/src/main/java/playground/joel/analysis/PerformanceFleetSizeCalculatorClean.java
+++ playgrounds/ivtExt/src/main/java/playground/joel/analysis/PerformanceFleetSizeCalculatorClean.java
@@ -3,17 +3,38 @@
  */
 package playground.joel.analysis;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.scenario.ScenarioUtils;
 
+import ch.ethz.idsc.tensor.RealScalar;
+import ch.ethz.idsc.tensor.Scalar;
+import ch.ethz.idsc.tensor.Scalars;
 import ch.ethz.idsc.tensor.Tensor;
-import ch.ethz.idsc.tensor.io.Pretty;
+import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.alg.Array;
+import ch.ethz.idsc.tensor.alg.Dimensions;
+import ch.ethz.idsc.tensor.alg.Transpose;
+import ch.ethz.idsc.tensor.red.Norm;
+import ch.ethz.idsc.tensor.red.Total;
+import ch.ethz.idsc.tensor.sca.N;
+import ch.ethz.idsc.tensor.sca.Round;
+import playground.clruch.netdata.VirtualLink;
 import playground.clruch.netdata.VirtualNetwork;
 import playground.clruch.netdata.VirtualNetworkGet;
+import playground.clruch.netdata.VirtualNode;
+import playground.clruch.tensorUtils.TensorOperations;
 import playground.clruch.traveldata.TravelData;
 import playground.clruch.traveldata.TravelDataGet;
 import playground.clruch.traveldata.TravelDataUtils;
@@ -28,7 +49,9 @@ public class PerformanceFleetSizeCalculatorClean {
     final int timeSteps;
     final int dt;
     final int maxVehicles;
-    final int vehicleSteps = 20;
+    final int vehicleSteps = 50;
+    final double PEAKPERCENTAGE = 0.7;
+    final int avSpeed = 33;
     final int vehicleBins;
     final int numVNode;
     final int numVLink;
@@ -57,20 +80,78 @@ public class PerformanceFleetSizeCalculatorClean {
 
     void calcAvailab() throws InterruptedException {
 
-        for (int i = 0; i < timeSteps; ++i) {
+        Set<Integer> offPeakSteps = new HashSet();
+        for (int i = 0; i < timeSteps; ++i)
+            offPeakSteps.add(i);
+        
+        Set<Integer> peakSteps = PerformanceFleetSizeUtils.calcPeakSteps(virtualNetwork, travelData, PEAKPERCENTAGE);
+        peakSteps.stream().forEach(v->offPeakSteps.remove(v));
+        
+        System.out.println("peak steps: " + peakSteps);
+        System.out.println("offpeak steps: " + offPeakSteps);
+        
+        
+
+        Tensor a = Array.zeros(numVNode, timeSteps, vehicleBins + 1);
+        Tensor atemp = Array.zeros(timeSteps, vehicleBins + 1, numVNode); // Array.zeros(numberTimeSteps,
+        for (int i = 24; i < timeSteps; ++i) {
+            System.out.println("i = "+ i);
+            // 1) calculate the throughput of the network
+            Tensor flowReb = travelData.getAlphaijPSFforTime(i * dt);
+            Tensor flowCust = travelData.getlambdaijPSFforTime(i * dt);
+            Tensor flowTot = flowCust.add(flowReb);
+            // TODO look at flowTot and see if some properties can be asserted.
+            Tensor flowTotLarge = flowTot.multiply(RealScalar.of(1000000)).map(Round.FUNCTION);
+            Tensor pij = TensorOperations.normToRowStochastic(flowTotLarge);
+            Tensor tpStation = ThroughputCalculator.getRelativeThroughputOfi(pij);
+            System.out.println("tpStation = " + tpStation.map(Round._2));
+            Tensor tpStationTot = ThroughputCalculator.combineDisjoint(tpStation);
             
-            Tensor pij = travelData.getpijPSFforTime(i * dt);
-            System.out.println("pij = " + Pretty.of(pij));
+            System.out.println("tpStationTot = " + tpStationTot.map(Round._2));
+            Tensor tp = PerformanceFleetSizeUtils.calctpTot(tpStationTot, pij);
+            System.out.println("tp = " + tp.map(Round._2));
 
-            Tensor tp = ThroughputCalculator.getRelativeThroughputOfi(travelData.getpijPSFforTime(i * dt));
-            System.out.println("tp = " + Pretty.of(tp));
+            // 2) calculate the service rates
+            Tensor srStations = Total.of(Transpose.of(flowTot));
+            Tensor srTotVehicles = Tensors.empty();
+            for (int j = 0; j <= maxVehicles; ++j) {
+                Tensor lineupd = srStations.copy();
+                Tensor roadServ = PerformanceFleetSizeUtils.calcSRRoad(j, virtualNetwork, avSpeed);
+                roadServ.flatten(-1).forEach(v -> lineupd.append(v));
+                srTotVehicles.append(lineupd);
+            }
+            // srTotVehicles = srTotVehicles.multiply(RealScalar.of(1000000)).map(Round.FUNCTION);
+            // srTotVehicles = srTotVehicles.multiply(RealScalar.of(1000000).invert());
+
+            // 3) perform mean value analysis
+            Tensor srTotVehiclesNum = N.of(srTotVehicles);
+            Tensor tpNum = N.of(tp);
+            MeanValueAnalysis mva = new MeanValueAnalysis(srTotVehiclesNum, tpNum, numVNode);
+
+            System.out.println("tps = " + tpStationTot.get(0).map(Round._2));
+            System.out.println("as_ = " + mva.getA(maxVehicles).extract(0, numVNode).map(Round._2));
+
+            // 4) save results
+            Tensor mat = Tensors.empty();
+            for (int vehBin = 0; vehBin <= vehicleBins; ++vehBin) {
+                Tensor AvehRef = mva.getA(vehBin * vehicleSteps);
+                mat.append(AvehRef.extract(0, numVNode));
+            }
+            atemp.set(mat, i);
         }
+        a = Transpose.of(atemp, 1, 2, 0);
+        Tensor meanByVehiclesPeak = PerformanceFleetSizeUtils.calcMeanByVehicles(a, peakSteps);
+        Tensor meanByVehiclesOffPeak = PerformanceFleetSizeUtils.calcMeanByVehicles(a, offPeakSteps);
 
-        // Tensor srVeh = Tensors.empty();
-        // Tensor tp = Tensors.empty();
-        // MeanValueAnalysis mva = new MeanValueAnalysis(srVeh, tp,
-        // virtualNetwork.getvNodesCount());
-
+        try {
+            AnalyzeAll.saveFile(a, "availabilitiesFull");
+            AnalyzeAll.saveFile(meanByVehiclesOffPeak, "availabilitiesOffPeak");
+            AnalyzeAll.saveFile(meanByVehiclesPeak, "availabilitiesPeak");
+            PerformanceFleetSizeUtils.plot(meanByVehiclesOffPeak, meanByVehiclesPeak, vehicleSteps);
+        } catch (Exception e) {
+            System.out.println("Error saving the availabilities");
+            e.printStackTrace(System.out);
+        }
     }
 
 }
---
+++ playgrounds/ivtExt/src/main/java/playground/joel/analysis/PerformanceFleetSizeUtils.java
@@ -0,0 +1,184 @@
+/**
+ * 
+ */
+package playground.joel.analysis;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import ch.ethz.idsc.tensor.RealScalar;
+import ch.ethz.idsc.tensor.Scalar;
+import ch.ethz.idsc.tensor.Scalars;
+import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.alg.Array;
+import ch.ethz.idsc.tensor.alg.Dimensions;
+import ch.ethz.idsc.tensor.red.Norm;
+import ch.ethz.idsc.tensor.red.Total;
+import playground.clruch.netdata.VirtualLink;
+import playground.clruch.netdata.VirtualNetwork;
+import playground.clruch.netdata.VirtualNode;
+import playground.clruch.traveldata.TravelData;
+
+/** @author Claudio Ruch */
+public enum PerformanceFleetSizeUtils {
+    ;
+
+    /* package */ static Set<Integer> calcPeakSteps(VirtualNetwork virtualNetwork, TravelData tData, double PEAKPERCENTAGE) {
+        int numberTimeSteps = tData.getNumbertimeSteps();
+        int dt = tData.getdt();
+
+        // list arrivals per time step
+        List<Double> arrivalNumbers = new ArrayList<>();
+        for (int k = 0; k < numberTimeSteps; ++k) {
+            Tensor lambdaii = tData.getLambdaPSFforTime(k * dt);
+            Tensor totalArrivalRate = Total.of(lambdaii);
+            RealScalar arrivals = (RealScalar) totalArrivalRate.multiply(RealScalar.of(dt));
+            arrivalNumbers.add(arrivals.number().doubleValue());
+        }
+
+        double maxArrival = Collections.max(arrivalNumbers);
+
+        Set<Integer> peakSteps = new HashSet<Integer>();
+
+        for (int k = 0; k < numberTimeSteps; ++k) {
+            if (arrivalNumbers.get(k) > PEAKPERCENTAGE * maxArrival) {
+                peakSteps.add(k);
+
+            }
+        }
+
+        return peakSteps;
+
+    }
+
+    /* package */ static Tensor calctpTot(Tensor tpStation, Tensor pij) {
+        int numVNode = Dimensions.of(pij).get(0);
+
+        Tensor tpRoad = Array.zeros(numVNode, numVNode);
+        for (int i = 0; i < numVNode; ++i) {
+            for (int j = 0; j < numVNode; ++j) {
+                Scalar tpstati = tpStation.Get(0, i);
+                Scalar pijel = pij.Get(i, j);
+                Scalar tpij = pijel.multiply(tpstati);
+                tpRoad.set(tpij, i, j);
+            }
+        }
+
+        Tensor tp = tpStation.get(0);
+
+        for (int i = 0; i < numVNode; ++i) {
+            for (int j = 0; j < numVNode; ++j) {
+                if (i != j) {
+                    tp.append(tpRoad.Get(i, j));
+                }
+            }
+        }
+
+        return tp;
+
+    }
+
+    /* package */static Tensor calcSRRoad(int numVehicles, VirtualNetwork virtualNetwork, int avSpeed) {
+        int numVNode = virtualNetwork.getvNodesCount();
+        Tensor serviceRateRoads = Array.zeros(numVNode, numVNode);
+        for (int i = 0; i < numVNode; ++i) {
+            for (int j = 0; j < numVNode; ++j) {
+                VirtualNode from = virtualNetwork.getVirtualNode(i);
+                VirtualNode to = virtualNetwork.getVirtualNode(j);
+                Scalar serviceRate = RealScalar.ZERO;
+                if (i != j) {
+                    VirtualLink vLink = virtualNetwork.getVirtualLink(from, to);
+                    serviceRate = RealScalar.of(vLink.getTtime());
+                }
+                serviceRateRoads.set(serviceRate, i, j);
+            }
+        }
+
+        Tensor serviceRateRoadsVector = Tensors.empty();
+
+        for (int i = 0; i < Dimensions.of(serviceRateRoads).get(0); ++i) {
+            for (int j = 0; j < Dimensions.of(serviceRateRoads).get(1); ++j) {
+                if (i != j) {
+                    serviceRateRoadsVector.append(serviceRateRoads.Get(i, j));
+                }
+            }
+        }
+
+        return serviceRateRoadsVector.multiply(RealScalar.of(numVehicles)).multiply(RealScalar.of(avSpeed).invert());
+
+    }
+
+    /* package */ static Tensor calcMeanByVehicles(Tensor a, Set<Integer> steps) {
+        int numVNode = Dimensions.of(a).get(0);
+        int vehicleBins = Dimensions.of(a).get(2);
+
+        Tensor meanByVehicles = Array.zeros(vehicleBins);
+        int numElem = 0;
+        for (int i = 0; i < numVNode; ++i) {
+            System.out.println("---");
+            for (int j : steps) {
+                Tensor aByVehicle = a.get(i, j);
+                if (Scalars.lessThan(RealScalar.of(0.001), Norm._1.of(aByVehicle))) {
+                    meanByVehicles = meanByVehicles.add(aByVehicle);
+                    ++numElem;
+                }
+            }
+        }
+
+        meanByVehicles = meanByVehicles.multiply(RealScalar.of(1.0 / numElem));
+        return meanByVehicles;
+
+    }
+
+    /* package */ static void plot(Tensor values, Tensor valuesPeak, int vehicleSteps) throws Exception {
+        XYSeries series = new XYSeries("Availability");
+        for (int i = 0; i < Dimensions.of(values).get(0); ++i) {
+            series.add(i * vehicleSteps, values.Get(i).number().doubleValue());
+        }
+        XYSeries seriesPeak = new XYSeries("Availability Peak");
+        for (int i = 0; i < Dimensions.of(values).get(0); ++i) {
+            seriesPeak.add(i * vehicleSteps, valuesPeak.Get(i).number().doubleValue());
+        }
+
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(series);
+        dataset.addSeries(seriesPeak);
+
+        JFreeChart timechart = ChartFactory.createXYLineChart("Vehicle Availability", "Number of Vehicles", //
+                "Availability", dataset);
+
+        // range and colors of the background/grid
+        timechart.getPlot().setBackgroundPaint(Color.white);
+        timechart.getXYPlot().setRangeGridlinePaint(Color.lightGray);
+        timechart.getXYPlot().setDomainGridlinePaint(Color.lightGray);
+
+        // line thickness
+        for (int k = 0; k < values.length(); k++) {
+            timechart.getXYPlot().getRenderer().setSeriesStroke(k, new BasicStroke(2.0f));
+        }
+
+        // set text fonts
+        timechart.getTitle().setFont(DiagramCreator.titleFont);
+        timechart.getXYPlot().getDomainAxis().setLabelFont(DiagramCreator.axisFont);
+        timechart.getXYPlot().getRangeAxis().setLabelFont(DiagramCreator.axisFont);
+        timechart.getXYPlot().getDomainAxis().setTickLabelFont(DiagramCreator.tickFont);
+        timechart.getXYPlot().getRangeAxis().setTickLabelFont(DiagramCreator.tickFont);
+
+        // save plot as png
+        int width = 1000; // Width of the image
+        int height = 750; // Height of the image
+        DiagramCreator.savePlot(AnalyzeAll.RELATIVE_DIRECTORY, "availbilitiesByNumberVehicles", timechart, width, height);
+    }
+
+}
