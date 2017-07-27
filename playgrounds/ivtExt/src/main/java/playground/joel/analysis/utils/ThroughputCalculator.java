1543ff0fe2addd6ad52eef0d1ce004153d4c50eb
clruch <claudio.ruch@gmail.com>
2017-07-27 11:54:23 +0200

    catched all special cases in throughputCalculator.

--- playgrounds/ivtExt/src/main/java/playground/joel/analysis/utils/ThroughputCalculator.java
+++ playgrounds/ivtExt/src/main/java/playground/joel/analysis/utils/ThroughputCalculator.java
@@ -7,10 +7,13 @@ import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Scalars;
 import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.alg.Array;
 import ch.ethz.idsc.tensor.alg.Dimensions;
 import ch.ethz.idsc.tensor.alg.Transpose;
 import ch.ethz.idsc.tensor.mat.IdentityMatrix;
 import ch.ethz.idsc.tensor.mat.NullSpace;
+import ch.ethz.idsc.tensor.red.Norm;
 import ch.ethz.idsc.tensor.red.Total;
 import ch.ethz.idsc.tensor.sca.Chop;
 import ch.ethz.idsc.tensor.sca.Round;
@@ -23,7 +26,8 @@ public enum ThroughputCalculator {
 
     /** @param pij
      *            row-stochastic matrix where M(i,j) is transition probability from i to j
-     * @return relative throughputs normed and positive
+     * @return relative throughputs normed and positive, if pij == 0, then zero is returned, if the matrix has no
+     * steady-state throughput, zero is returned
      * @throws InterruptedException */
 
     public static Tensor getRelativeThroughputOfi(Tensor pijInput) throws InterruptedException {
@@ -34,6 +38,14 @@ public enum ThroughputCalculator {
         // transform to rational form
         pij = pij.multiply(RealScalar.of(1000000)).map(Round.FUNCTION);
         pij = TensorOperations.normToRowStochastic(pij);
+        
+        // ensure positivity of input
+        pij.flatten(-1).forEach(v -> GlobalAssert.that(Scalars.lessEquals(RealScalar.ZERO, (Scalar) v)));
+
+        // if pij == ZERO, return zero throughput
+        Scalar tot = (Scalar) Total.of(Total.of(pij));
+        if(Scalars.isZero(tot)) return Array.zeros(1,size);
+
 
         // ensure input is row-stochastic
         Scalar totalofRows = (Scalar) Total.of(Total.of(pij));
@@ -42,16 +54,21 @@ public enum ThroughputCalculator {
         } catch (Exception e) {
             System.err.println("INFO: input of throughputCalculator not row stochastic.");
         }
-
-        // ensure positivity of input
-        pij.flatten(-1).forEach(v -> GlobalAssert.that(Scalars.lessEquals(RealScalar.ZERO, (Scalar) v)));
+        
 
         // calculate throughput
         Tensor IminusPij = IdentityMatrix.of(size).subtract(Transpose.of(pij));
         Tensor tp = NullSpace.of(IminusPij);
+        
+        if(Dimensions.of(tp).get(0) == 0){            
+            System.err.println("INFO: no throughput found beause pij has full rank, outputting zero");
+            return Array.zeros(1,size);
+            
+        }
 
         // ensure positivity of solution
         tp.flatten(-1).forEach(v -> GlobalAssert.that(Scalars.lessEquals(RealScalar.ZERO, (Scalar) v)));
+        
 
         // return normed vector
         return TensorOperations.normToRowStochastic(tp);
