eb1da30dfdc419f6f3122fd7d9373b9d8d76a1a5
dziemke <ziemke@vsp.tu-berlin.de>
2017-08-01 23:57:42 +0200

    bicycle test

--- contribs/bicycle/src/main/java/org/matsim/contrib/bicycle/run/BicycleTravelDisutility.java
+++ contribs/bicycle/src/main/java/org/matsim/contrib/bicycle/run/BicycleTravelDisutility.java
@@ -23,6 +23,7 @@ import java.util.Random;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
+import org.matsim.core.gbl.MatsimRandom;
 import org.matsim.core.router.util.TravelDisutility;
 import org.matsim.utils.objectattributes.ObjectAttributes;
 import org.matsim.utils.objectattributes.ObjectAttributesXmlReader;
@@ -212,10 +213,10 @@ public class BicycleTravelDisutility implements TravelDisutility {
 		//add disutility for slope here, makes sense for hilly cities, but left aside for berlin
 
 		// adding a randomfactor to disutility calculation
-		Random r = new Random();
+		Random random = MatsimRandom.getLocalInstance(); // Make sure that stream of random variables is reproducible. dz, aug'17
 		double standardDeviation = 0.2;
 		int mean = 1;
-		double randomfactor = r.nextGaussian() * standardDeviation + mean;
+		double randomfactor = random.nextGaussian() * standardDeviation + mean;
 		// yyyyyy in the randomized toll disutility this is LOGnormal.  Should be made consistent, or an argument provided why in the different cases different
 		// mathematical forms make sense.  kai, feb'17
 
