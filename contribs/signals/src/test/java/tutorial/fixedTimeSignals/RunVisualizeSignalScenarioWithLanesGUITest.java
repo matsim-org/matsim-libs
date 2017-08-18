ae1116a9ba7f012590456a4602bbf84e81c4b714
jfbischoff <bischoff@vsp.tu-berlin.de>
2017-08-18 08:58:32 +0200

    Setting RunVisualizeSignalScenarioWithLanesGUITest to ignore

--- contribs/signals/src/test/java/tutorial/fixedTimeSignals/RunVisualizeSignalScenarioWithLanesGUITest.java
+++ contribs/signals/src/test/java/tutorial/fixedTimeSignals/RunVisualizeSignalScenarioWithLanesGUITest.java
@@ -1,10 +1,11 @@
 package tutorial.fixedTimeSignals;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class RunVisualizeSignalScenarioWithLanesGUITest {
-
+	
 	public static void main(String[] args) {
 		try {
 			VisualizeSignalScenarioWithLanes.main(new String[]{});
@@ -13,7 +14,7 @@ public class RunVisualizeSignalScenarioWithLanesGUITest {
 			Assert.fail("something went wrong") ;
 		}
 	}
-	
+	@Ignore("This does not run on the server.")
 	@Test
 	public void testSignalExampleVisualization(){
 		main(null);
