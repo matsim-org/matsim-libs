7952c24a2be98c01b403641ae6f0545d82df642c
clruch <claudio.ruch@gmail.com>
2017-08-25 18:07:56 +0200

    one single DefaultOptions file.

--- playgrounds/ivtExt/src/main/java/playground/clruch/DefaultOptions.java
+++ playgrounds/ivtExt/src/main/java/playground/clruch/DefaultOptions.java
@@ -8,66 +8,31 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-import playground.clruch.data.LocationSpec;
-
+//TODO comment this file
 /** @author Claudio Ruch */
 public enum DefaultOptions {
     ;
 
-    /** "av_config_full.xml" is a MATSim config file containing references for the full population and network which
-     * are not yet cut.
-     * "waitForClients" boolean value false / true that indicates if the server should wait before starting for a
-     * viewer to connect.
-     * 
-     * @return */
-    public static Properties getPreparerDefault() {
+    public static Properties getDefault() {
         Properties returnP = new Properties();
-        returnP.setProperty("av_config_full.xml", "/home/clruch/Simulations/2017_07_13_Sioux_TestBed/av_config_full.xml");
+        returnP.setProperty("fullConfig", "av_config_full.xml");
+        returnP.setProperty("simuConfig", "av_config.xml");
         returnP.setProperty("maxPopulationSize", "5000");
         returnP.setProperty("numVirtualNodes", "40");
         returnP.setProperty("dtTravelData", "900");
         returnP.setProperty("completeGraph", "true");
-
         returnP.setProperty("LocationSpec", "SIOUXFALLS_CITY");
         returnP.setProperty("populationeliminateFreight", "false");
         returnP.setProperty("populationeliminateWalking", "false");
         returnP.setProperty("populationchangeModeToAV", "true");
-        return returnP;
-    }
-
-    public static void savePreparerDefault() throws IOException {
-        saveProperties(getPreparerDefault(), "DefaultPreparerOptions");
-    }
-
-    /** "av_config.xml" is a MATSim config file
-     * "waitForClients" boolean value false / true that indicates if the server should wait before starting for a
-     * viewer to connect.
-     * 
-     * @return */
-    public static Properties getServerDefault() {
-        Properties returnP = new Properties();
-        returnP.setProperty("av_config.xml", "/home/clruch/Simulations/2017_07_13_Sioux_TestBed/av_config.xml");
         returnP.setProperty("waitForClients", "false");
-        return returnP;
-    }
-
-    public static void saveServerDefault() throws IOException {
-        saveProperties(getServerDefault(), "DefaultServerOptions");
-    }
-
-    /** "av_config.xml" is a MATSim config file
-     * "ReferenceFrame" indicates the coordinate system, reference frame to be used, e.g. SWITZERLAND, SIOUXFALLS,...
-     * 
-     * @return */
-    public static Properties getViewerDefault() {
-        Properties returnP = new Properties();
         returnP.setProperty("av_config.xml", "/home/clruch/Simulations/2017_07_13_Sioux_TestBed/av_config.xml");
         returnP.setProperty("ReferenceFrame", "SIOUXFALLS");
         return returnP;
     }
 
-    public static void saveViewerDefault() throws IOException {
-        saveProperties(getViewerDefault(), "DefaultViewerOptions");
+    public static void saveDefault() throws IOException {
+        saveProperties(getDefault(), "DefaultPreparerOptions");
     }
 
     private static void saveProperties(Properties prop, String filename) throws IOException {
