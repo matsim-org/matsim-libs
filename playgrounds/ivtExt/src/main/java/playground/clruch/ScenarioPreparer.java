7afaf07baaca92f94f4ba2ce8c8ef081b2f45074
clruch <claudio.ruch@gmail.com>
2017-08-26 15:36:15 +0200

    removed imports.

--- playgrounds/ivtExt/src/main/java/playground/clruch/ScenarioPreparer.java
+++ playgrounds/ivtExt/src/main/java/playground/clruch/ScenarioPreparer.java
@@ -1,7 +1,6 @@
 package playground.clruch;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.Properties;
