d274446b347295a99ee53ff0812f7866d38146c0
dziemke <ziemke@vsp.tu-berlin.de>
2017-08-28 00:59:13 +0200

    correct bicycle scoring

--- contribs/bicycle/src/main/java/org/matsim/contrib/bicycle/run/BicycleScoring.java
+++ contribs/bicycle/src/main/java/org/matsim/contrib/bicycle/run/BicycleScoring.java
@@ -21,6 +21,7 @@ package org.matsim.contrib.bicycle.run;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.events.Event;
@@ -37,9 +38,13 @@ import org.matsim.vehicles.Vehicle;
  * @author dziemke
  */
 public class BicycleScoring implements SumScoringFunction.ArbitraryEventScoring {
+	private static final Logger LOG = Logger.getLogger(BicycleScoring.class);
+	
 	private Scenario scenario;
 	private BicycleTravelDisutility bicycleTravelDisutility;
 	private Vehicle2DriverEventHandler delegate = new Vehicle2DriverEventHandler();
+	private Map<Id<Vehicle>,Id<Link>> previousLinks = new HashMap<>();
+	private Map<Id<Vehicle>,Double> previousLinkRelativePositions = new HashMap<>();
 	private Map<Id<Vehicle>,Double> previousLinkEnterTimes = new HashMap<>();
 	private double score;
 	
@@ -58,21 +63,27 @@ public class BicycleScoring implements SumScoringFunction.ArbitraryEventScoring 
 	@Override
 	public void handleEvent(Event event) {
 		if (event instanceof VehicleEntersTrafficEvent) {
+			LOG.warn(event.toString());
 			VehicleEntersTrafficEvent vehEvent = (VehicleEntersTrafficEvent) event;
-
+			
 			// Establish connection between driver and vehicle
 			delegate.handleEvent(vehEvent);
 			
+			Id<Vehicle> vehId = vehEvent.getVehicleId();
+
 			// No LinkEnterEvent on first link of a leg
-			previousLinkEnterTimes.put(vehEvent.getVehicleId(), vehEvent.getTime());
+			previousLinks.put(vehId, vehEvent.getLinkId());
+			previousLinkRelativePositions.put(vehId, vehEvent.getRelativePositionOnLink());
+			previousLinkEnterTimes.put(vehId, vehEvent.getTime());
 		}
 		if (event instanceof VehicleLeavesTrafficEvent) {
+			LOG.warn(event.toString());
 			VehicleLeavesTrafficEvent vehEvent = (VehicleLeavesTrafficEvent) event;
 			
 			Id<Vehicle> vehId = vehEvent.getVehicleId();
 			double enterTime = previousLinkEnterTimes.get(vehId);
 			double travelTime = vehEvent.getTime() - enterTime;
-			calculateScoreForPreviousLink(vehEvent.getLinkId(), enterTime, vehId, travelTime);
+			calculateScoreForPreviousLink(vehEvent.getLinkId(), enterTime, vehId, travelTime, previousLinkRelativePositions.get(vehId));
 			
 			// End connection between driver and vehicle
 			delegate.handleEvent(vehEvent);
@@ -80,24 +91,32 @@ public class BicycleScoring implements SumScoringFunction.ArbitraryEventScoring 
 		if ( event instanceof LinkEnterEvent ) {
 			// This only works because setPassLinkEventsToPerson is activated (via ScoringFunctionsForPopulation)
 			// Otherwise ArbitraryEventScoring only handles events that are instance of HasPersonId, which is not the case for LinkEnterEvents
-			System.err.println("New link enter event: " + event.toString());
+			LOG.warn(event.toString());
 			LinkEnterEvent linkEnterEvent = (LinkEnterEvent) event;
 			
 			Id<Vehicle> vehId = linkEnterEvent.getVehicleId();
 			double enterTime = previousLinkEnterTimes.get(vehId);
 			double travelTime = linkEnterEvent.getTime() - enterTime;
-			calculateScoreForPreviousLink(linkEnterEvent.getLinkId(), enterTime, vehId, travelTime);
+			calculateScoreForPreviousLink(previousLinks.get(vehId), enterTime, vehId, travelTime, previousLinkRelativePositions.get(vehId));
 			
+			previousLinks.put(vehId, linkEnterEvent.getLinkId());
+			previousLinkRelativePositions.put(vehId, 0.);
 			previousLinkEnterTimes.put(vehId, linkEnterEvent.getTime());
 		}	
 	}
 	
-	private void calculateScoreForPreviousLink(Id<Link> linkId, Double enterTime, Id<Vehicle> vehId, double travelTime) {
-		Link link = scenario.getNetwork().getLinks().get(linkId);
-		Person person = scenario.getPopulation().getPersons().get(delegate.getDriverOfVehicle(vehId));
-		Vehicle vehicle = scenario.getVehicles().getVehicles().get(vehId);
-		
-		this.score += bicycleTravelDisutility.getTravelDisutilityBasedOnTTime(link, enterTime, person, vehicle, travelTime);
-		System.out.println("score = " + score + " -- linkId = " + link.getId() + " -- enterTime = " + enterTime + " -- personId = " + person.getId() + " -- travelTime = " + travelTime);
+	private void calculateScoreForPreviousLink(Id<Link> linkId, Double enterTime, Id<Vehicle> vehId, double travelTime, double relativeLinkEnterPosition) {
+		if (relativeLinkEnterPosition != 1.0) {
+			Link link = scenario.getNetwork().getLinks().get(linkId);
+			Person person = scenario.getPopulation().getPersons().get(delegate.getDriverOfVehicle(vehId));
+			Vehicle vehicle = scenario.getVehicles().getVehicles().get(vehId);
+			
+			this.score += bicycleTravelDisutility.getTravelDisutilityBasedOnTTime(link, enterTime, person, vehicle, travelTime);
+			LOG.info("score = " + score + " -- linkId = " + link.getId() + " -- enterTime = " + enterTime + " -- personId = " + person.getId() + " -- travelTime = " + travelTime);
+		}
+		else {
+			// If agent was already at the end of the link and thus did not travel on it, do nothing
+		}
+		// TODO Use relative position in a more sophisticated way.
 	}
 }
