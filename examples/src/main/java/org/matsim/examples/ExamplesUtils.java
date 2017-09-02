4039c9ada0d38d6bc7baf9da4b0969c7fd36aed5
kainagel <nagel@vsp.tu-berlin.de>
2017-09-02 12:27:24 +0200

    add explanatory comment

--- examples/src/main/java/org/matsim/examples/ExamplesUtils.java
+++ examples/src/main/java/org/matsim/examples/ExamplesUtils.java
@@ -5,5 +5,6 @@ import java.net.URL;
 public class ExamplesUtils {
     public static URL getTestScenarioURL(String name) {
         return ExamplesUtils.class.getResource("/test/scenarios/"+ name +"/");
+        // note that this works because it is entered as such in the pom.xml.  kai, sep'17
     }
 }
