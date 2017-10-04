78b1cef100306b6ea7ecc189ecda374b95befa87
clruch <claudio.ruch@gmail.com>
2017-10-04 18:12:18 +0200

    wip hopefully it works

--- playgrounds/ivtExt/src/main/java/playground/clruch/SequentialScenarioServer.java
+++ playgrounds/ivtExt/src/main/java/playground/clruch/SequentialScenarioServer.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.apache.commons.io.FileUtils;
 import org.jdom.Attribute;
 import org.jdom.Document;
 import org.jdom.Element;
@@ -28,29 +27,22 @@ public class SequentialScenarioServer {
     public static void main(String[] args) throws MalformedURLException, Exception {
 
         final int iterations = 5;
-        String rawFolderName = "2017_10_04_SiouxFareDataRaw";
-        File rawFolder = new File(rawFolderName);
+        File rawFolder = new File("").getCanonicalFile();
         double[] fareRatios = { 0.1, 0.5, 1.0, 2.0, 10.0 };
 
         // copy the raw folder name including changed settings
         for (int i = 0; i < iterations; ++i) {
-            File simFolder = new File(rawFolderName + "_Iteration_" + Integer.toString(i + 1));
+            System.out.println("working in the directory " + rawFolder.getAbsolutePath());
 
-            // copy the raw folder
-            System.out.println("creating new simulation folder "+ simFolder.getAbsolutePath());
-            FileUtils.copyDirectory(rawFolder, simFolder);
+            // modify the av.xml file
+            changeFareRatioTo(fareRatios[i], rawFolder);
 
-            // change the respective setting
+            // set the output-directory correctly
+            changeOutputDirectoryTo("output" + i, rawFolder);
 
-            changeFareRatioTo(fareRatios[i], simFolder);
-
-            // simulate the folder
-            String userDir = System.getProperty("user.dir");
-            System.setProperty( "user.dir", simFolder.getAbsolutePath());
-            System.out.println("now in the working directory: " + (new File("").toString()));
-            Thread.sleep(5000);
+            // simulate
             ScenarioServer.simulate();
-            System.setProperty("user.dir", userDir);
+
         }
 
     }
@@ -60,6 +52,8 @@ public class SequentialScenarioServer {
         System.out.println("changing fare ratio to " + fareRatio);
 
         File xmlFile = new File(simFolder, "av.xml");
+        
+        System.out.println("looking for av.xml file at " +  xmlFile.getAbsolutePath());
 
         GlobalAssert.that(xmlFile.exists());
 
@@ -68,7 +62,7 @@ public class SequentialScenarioServer {
         builder.setFeature("http://xml.org/sax/features/validation", false);
         builder.setFeature("http://apache.org/xml/features/nonvalidating/load-dtd-grammar", false);
         builder.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
-        
+
         Document doc = (Document) builder.build(xmlFile);
         Element rootNode = doc.getRootElement();
         Element operator = rootNode.getChild("operator");
@@ -91,4 +85,112 @@ public class SequentialScenarioServer {
 
     }
 
+    private static void changeOutputDirectoryTo(String name, File simFolder) //
+            throws ParserConfigurationException, JDOMException, IOException {
+
+        System.out.println("changing output directory to " + name);
+
+        File xmlFile = new File(simFolder, "av_config.xml");
+        GlobalAssert.that(xmlFile.exists());
+
+        SAXBuilder builder = new SAXBuilder();
+        builder.setValidation(false);
+        builder.setFeature("http://xml.org/sax/features/validation", false);
+        builder.setFeature("http://apache.org/xml/features/nonvalidating/load-dtd-grammar", false);
+        builder.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+
+        Document doc = (Document) builder.build(xmlFile);
+        Element rootNode = doc.getRootElement();
+
+        List<Element> children = rootNode.getChildren();
+
+        for (Element child : children) {
+            if (child.getAttributeValue("name").equals("controler")) {
+                List<Element> cchildren = child.getChildren();
+                for (Element cchild : cchildren) {
+                    if (cchild.getAttributeValue("name").equals("outputDirectory")) {
+                        List<Attribute> theAttributes = cchild.getAttributes();
+                        if (theAttributes.get(0).getValue().equals("outputDirectory")) {
+                            theAttributes.get(1).setValue(name);
+                        }
+                    }
+                }
+            }
+        }
+        XMLOutputter xmlOutput = new XMLOutputter();
+        xmlOutput.setFormat(Format.getPrettyFormat());
+        xmlOutput.output(doc, new FileWriter(xmlFile));
+
+    }
+
 }
+
+// public class SequentialScenarioServer {
+//
+// public static void main(String[] args) throws MalformedURLException, Exception {
+//
+// final int iterations = 5;
+// String rawFolderName = "2017_10_04_SiouxFareDataRaw";
+// File rawFolder = new File(rawFolderName);
+// double[] fareRatios = { 0.1, 0.5, 1.0, 2.0, 10.0 };
+//
+// // copy the raw folder name including changed settings
+// for (int i = 0; i < iterations; ++i) {
+// File simFolder = new File(rawFolderName + "_Iteration_" + Integer.toString(i + 1));
+//
+// // copy the raw folder
+// System.out.println("creating new simulation folder "+ simFolder.getAbsolutePath());
+// FileUtils.copyDirectory(rawFolder, simFolder);
+//
+// // change the respective setting
+//
+// changeFareRatioTo(fareRatios[i], simFolder);
+//
+// // simulate the folder
+// String userDir = System.getProperty("user.dir");
+// System.setProperty( "user.dir", simFolder.getAbsolutePath());
+// System.out.println("now in the working directory: " + (new File("").toString()));
+// Thread.sleep(5000);
+// ScenarioServer.simulate();
+// System.setProperty("user.dir", userDir);
+// }
+//
+// }
+//
+// private static void changeFareRatioTo(double fareRatio, File simFolder) //
+// throws ParserConfigurationException, JDOMException, IOException {
+// System.out.println("changing fare ratio to " + fareRatio);
+//
+// File xmlFile = new File(simFolder, "av.xml");
+//
+// GlobalAssert.that(xmlFile.exists());
+//
+// SAXBuilder builder = new SAXBuilder();
+// builder.setValidation(false);
+// builder.setFeature("http://xml.org/sax/features/validation", false);
+// builder.setFeature("http://apache.org/xml/features/nonvalidating/load-dtd-grammar", false);
+// builder.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+//
+// Document doc = (Document) builder.build(xmlFile);
+// Element rootNode = doc.getRootElement();
+// Element operator = rootNode.getChild("operator");
+// Element dispatcher = operator.getChild("dispatcher");
+// List<Element> children = dispatcher.getChildren();
+//
+// for (Element element : children) {
+// List<Attribute> theAttributes = element.getAttributes();
+//
+// if (theAttributes.get(0).getValue().equals("fareRatioMultiply")) {
+// theAttributes.get(1).setValue(Double.toString(fareRatio));
+//
+// }
+//
+// }
+//
+// XMLOutputter xmlOutput = new XMLOutputter();
+// xmlOutput.setFormat(Format.getPrettyFormat());
+// xmlOutput.output(doc, new FileWriter(xmlFile));
+//
+// }
+//
+// }
