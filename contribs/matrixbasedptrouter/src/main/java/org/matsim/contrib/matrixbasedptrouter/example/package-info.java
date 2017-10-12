e72c86a6c7f3775823d1e42104669c645e1d299b
clruch <claudio.ruch@gmail.com>
2017-10-12 14:54:34 +0200

    removed unnecessary playgrounds.

--- contribs/matrixbasedptrouter/src/main/java/org/matsim/contrib/matrixbasedptrouter/example/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- *  contains a basic example that should help to get started 
- */
-/**
- * @author jbischoff
- *
- */
-package org.matsim.contrib.matrixbasedptrouter.example;
--- contribs/taxi/src/main/java/org/matsim/contrib/drt/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * 
- */
-/**
- * Generalisable DRT Tasks for taxi-like services with more than one passenger.
- */
-package org.matsim.contrib.drt;
