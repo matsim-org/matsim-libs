6c44f30e386823ab60c148ef8f3b7a6fc7327298
clruch <claudio.ruch@gmail.com>
2017-10-12 16:09:10 +0200

    intermediate status, not yet working...

--- playgrounds/ivtExt/src/main/java/playground/clruch/prep/timeinvariant/DemoInvariantPop.java
+++ playgrounds/ivtExt/src/main/java/playground/clruch/prep/timeinvariant/DemoInvariantPop.java
@@ -10,9 +10,11 @@ import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.population.Population;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.population.io.PopulationWriter;
 import org.matsim.core.scenario.ScenarioUtils;
 
 import ch.ethz.idsc.queuey.datalys.MultiFileTools;
+import ch.ethz.idsc.queuey.util.GZHandler;
 import playground.clruch.ScenarioOptions;
 import playground.clruch.utils.PropertiesExt;
 
@@ -21,17 +23,25 @@ public class DemoInvariantPop {
 
     public static void main(String[] args) throws IOException {
         final int sTime = 27900;
-        final int duration = 60 * 60;
+        final int duration = 3600;
+        final String POPULATIONUPDATEDNAME = "populationMorning";
 
         // demo
         File workingDirectory = MultiFileTools.getWorkingDirectory();
         PropertiesExt simOptions = PropertiesExt.wrap(ScenarioOptions.load(workingDirectory));
-        File configFile = new File(workingDirectory, simOptions.getString("simuConfig"));
+        File configFile = new File(workingDirectory, simOptions.getString("fullConfig"));
         Config config = ConfigUtils.loadConfig(configFile.toString());
         Scenario scenario = ScenarioUtils.loadScenario(config);
         Population population = scenario.getPopulation();
 
         Population populationInvariant = TimeInvariantPopulation.at(sTime, duration, population);
+
+        // write the modified population to file
+        final File fileExportGz = new File(workingDirectory, POPULATIONUPDATEDNAME + ".xml.gz");
+        final File fileExport = new File(workingDirectory, POPULATIONUPDATEDNAME + ".xml");
+        PopulationWriter pw = new PopulationWriter(populationInvariant);
+        pw.write(fileExportGz.toString());
+        GZHandler.extract(fileExportGz, fileExport);
 
     }
 
--- playgrounds/ivtExt/src/main/java/playground/clruch/prep/timeinvariant/TimeInvariantPopulation.java
+++ playgrounds/ivtExt/src/main/java/playground/clruch/prep/timeinvariant/TimeInvariantPopulation.java
@@ -3,6 +3,16 @@
  */
 package playground.clruch.prep.timeinvariant;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.population.Leg;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Plan;
+import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.api.core.v01.population.Population;
 
 /** @author Claudio Ruch */
@@ -10,7 +20,46 @@ public enum TimeInvariantPopulation {
     ;
 
     public static Population at(int time, int duration, Population population) {
-        System.out.println("calc. time-invariant pop. from " + time + " to " + time + duration);
+        System.out.println("calc. time-invariant pop. from " + time + " to " + (time + duration));
+
+        // 1) get all trips that occur during timeslice
+        Map<Id<Person>, Person> people = (Map<Id<Person>, Person>) population.getPersons();
+
+        HashMap<Person, HashSet<Plan>> toRemove = new HashMap<>();
+        for(Person person : population.getPersons().values()){
+            toRemove.put(person, new HashSet<>());
+        }
+        
+        
+
+        for (Person person : people.values()) {
+
+            List<Plan> plans = (List<Plan>) person.getPlans();
+
+            for (Plan plan : plans) {
+                for (PlanElement planElement : plan.getPlanElements()) {
+                    if (planElement instanceof Leg) {
+                        Leg leg = (Leg) planElement;
+                        double depTime = leg.getDepartureTime();
+                        System.out.println("depTime: " +  depTime);
+                        boolean inWindow = depTime >= time && depTime <= time + duration;
+                        if (!inWindow) {
+                            toRemove.get(person).add(plan);
+                        }else{
+                            System.out.println("the plan can stay, it is in the window");
+                        }
+                    }
+                }
+            }
+        }
+
+        for(Person person : people.values()){
+            for(Plan plan : toRemove.get(person)){
+                person.removePlan(plan);
+            }
+        }
+        
+
         return population;
     }
 
