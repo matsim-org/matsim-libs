8cf4a34fb20f4e0bcfed480b5c81ca9b65e4e3a6
anapelaez <anape@student.ethz.ch>
2017-10-19 17:17:22 +0200

    work in progres wip

--- playgrounds/ivtExt/src/main/java/playground/clruch/io/fleet/StandaloneFleetConverter.java
+++ playgrounds/ivtExt/src/main/java/playground/clruch/io/fleet/StandaloneFleetConverter.java
@@ -3,8 +3,18 @@
 package playground.clruch.io.fleet;
 
 import java.io.File;
+import java.util.List;
 
+import org.matsim.api.core.v01.network.Network;
+
+import ch.ethz.idsc.queuey.datalys.MultiFileReader;
+import ch.ethz.idsc.queuey.datalys.MultiFileTools;
 import ch.ethz.idsc.queuey.util.GlobalAssert;
+
+import playground.clruch.data.ReferenceFrame;
+import playground.clruch.net.MatsimStaticDatabase;
+import playground.clruch.net.StorageUtils;
+import playground.clruch.utils.NetworkLoader;
 
 /** @author Claudio Ruch */
 enum StandaloneFleetConverter {
@@ -14,37 +24,38 @@ enum StandaloneFleetConverter {
         // selection of reference frame, file
         File directory = //
                 new File("/home/anape/Downloads/SanFranciscoTaxi/data");
-        
         GlobalAssert.that(directory.exists());
         GlobalAssert.that(directory.isDirectory());
-        
-        File[] files = directory.listFiles();
-        
-        
 
-        
-        for(File file: files) {
-        	System.out.println("loaded file: " + file.getAbsolutePath());
+        List<File> trailFiles = new MultiFileReader(directory).getFolderFiles();
+
+        for (File file : trailFiles) {
+            System.out.println("loaded file: " + file.getAbsolutePath());
         }
+
+        File workingDirectory = MultiFileTools.getWorkingDirectory();
+        File outputDirectory = new File(workingDirectory, "output");
+
+        StorageUtils storageUtils = new StorageUtils(outputDirectory);
+        ReferenceFrame referenceFrame = ReferenceFrame.SWITZERLAND;
+
+        DayTaxiRecord dayTaxiRecord = new DayTaxiRecord();
         
+        // File file = new File("/media/datahaki/media/ethz/taxi", "2017-06-27 - GPS
+        //List<File> trailFiles = (new MultiFileReader(directory, "Fahrtstrecken")).getFolderFiles();
         
+        // extract data from file and put into dayTaxiRecord
+        CsvFleetReader reader = new CsvFleetReader(dayTaxiRecord);
+        reader.populateFrom(trailFiles);
         
-        
-//        File outputDirectory = new File(args[0], "output");
-//        StorageUtils storageUtils = new StorageUtils(outputDirectory);
-//        ReferenceFrame referenceFrame = ReferenceFrame.SWITZERLAND;
-//        // File file = new File("/media/datahaki/media/ethz/taxi", "2017-06-27 - GPS Fahrtstrecken-Protokoll.csv");
-//        List<File> trailFiles = (new MultiFileReader(directory, "Fahrtstrecken")).getFolderFiles();
-//        DayTaxiRecord dayTaxiRecord = new DayTaxiRecord();
-//        // extract data from file and put into dayTaxiRecord
-//        CsvFleetReader reader = new CsvFleetReader(dayTaxiRecord);
-//        reader.populateFrom(trailFiles);
-//        Network network = NetworkLoader.loadNetwork(new File(args[0]));
-//
-//        // STEP 2: DayTaxiRecord to MATSimStaticDatabase
-//        MatsimStaticDatabase.initializeSingletonInstance(network, referenceFrame);
-//        // generate sim objects and store
-//        SimulationFleetDump.of(dayTaxiRecord, network, MatsimStaticDatabase.INSTANCE, storageUtils);
+        // =================================================
+        // TODO from here onwards you will need a San Francisco network... 
+        Network network = NetworkLoader.loadNetwork(new File(args[0]));
+
+        // STEP 2: DayTaxiRecord to MATSimStaticDatabase
+        MatsimStaticDatabase.initializeSingletonInstance(network, referenceFrame);
+        // generate sim objects and store
+        SimulationFleetDump.of(dayTaxiRecord, network, MatsimStaticDatabase.INSTANCE, storageUtils);
 
     }
 
