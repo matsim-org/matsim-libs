c9776677ecf2d9108fc068e2de00fdb34b22ac5c
clruch <claudio.ruch@gmail.com>
2017-10-24 16:05:42 +0200

    jan and claudio deleted everything because of some Scandinavian...

--- playgrounds/ivtExt/src/main/java/playground/sebhoerl/mexec/data/ControllerData.java
+++
@@ -1,7 +0,0 @@
-package playground.sebhoerl.mexec.data;
-
-public class ControllerData {
-    public String id;
-    public String className;
-    public String classPath;
-}
--- playgrounds/ivtExt/src/main/java/playground/sebhoerl/mexec_opdyts/optimization/IterationEventHandler.java
+++
@@ -1,7 +0,0 @@
-package playground.sebhoerl.mexec_opdyts.optimization;
-
-import org.matsim.core.events.handler.EventHandler;
-
-public interface IterationEventHandler extends EventHandler {
-    double getValue();
-}
