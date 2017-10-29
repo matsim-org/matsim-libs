4e20c36eb010234c4500fcb114cb74cdd784846a
kainagel <nagel@vsp.tu-berlin.de>
2017-10-29 21:03:13 +1100

    fix test; change from *IT to *Test

--- code-examples/src/test/java/tutorial/mobsim/mobsimPassingVehicleQ/RunMobsimWithMultipleModeVehiclesExampleTest.java
+++ code-examples/src/test/java/tutorial/mobsim/mobsimPassingVehicleQ/RunMobsimWithMultipleModeVehiclesExampleTest.java
@@ -6,20 +6,21 @@ import org.junit.Test;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.utils.io.IOUtils;
+import org.matsim.core.utils.io.UncheckedIOException;
 
-public class RunMobsimWithMultipleModeVehiclesExampleIT {
+public class RunMobsimWithMultipleModeVehiclesExampleTest {
 
 	@Test
 	public void test() {
 		// using an appropriate config file, that is included in the repository.
 		// unfortunately, the test now depends on this file. if someone removes/changes it, problems might occur.
-		String[] args = {"examples/tutorial/config/example5-config.xml"};
+		String[] args = {"scenarios/equil/example5-config.xml"};
 		Config config = ConfigUtils.loadConfig( args[0] ) ;
 		String pathname = config.getParam("controler", "outputDirectory");
 
 		try {
 			IOUtils.deleteDirectoryRecursively(new File(pathname).toPath());
-		} catch (IllegalArgumentException ee) {
+		} catch (UncheckedIOException ee) {
 			// (maybe the directory is left over from somewhere else; do nothing)
 		}
 
