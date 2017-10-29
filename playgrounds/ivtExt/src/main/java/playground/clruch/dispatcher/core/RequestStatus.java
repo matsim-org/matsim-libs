d12adc11f83625e65eace4ce6158cf55be724dbc
Andreas Aumiller <andy2804@github.com>
2017-10-29 18:04:37 +0100

    Introducing RequestStatus and Request Handler class WIP

---
+++ playgrounds/ivtExt/src/main/java/playground/clruch/dispatcher/core/RequestStatus.java
@@ -0,0 +1,27 @@
+package playground.clruch.dispatcher.core;
+
+/** @author Andreas Aumiller */
+// TODO check if useful.
+public enum RequestStatus {
+    REQUEST("req", "taxi requested"), //
+    PICKUP("pu", "pickup"), //
+    DROPOFF("do", "dropoff"), //
+    CANCELED("can", "request canceled"), //
+    FREE("noc", "no customer"), //
+    ;
+
+    public final String xmlTag;
+    public final String description;
+
+    RequestStatus(String xmlTag, String description) {
+        this.xmlTag = xmlTag;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return this.xmlTag;
+    }
+
+    
+}
