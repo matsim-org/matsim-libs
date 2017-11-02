59a7b6dcb8ef06fef65fd2253f7c5054fb3227da
anapelaez <anape@student.ethz.ch>
2017-11-02 16:51:54 +0100

    wip, we continue on claudios computer

--- playgrounds/ivtExt/src/main/java/playground/clruch/io/fleet/StandaloneFleetConverter.java
+++ playgrounds/ivtExt/src/main/java/playground/clruch/io/fleet/StandaloneFleetConverter.java
@@ -3,14 +3,12 @@
 package playground.clruch.io.fleet;
 
 import java.io.File;
-import java.util.List;
 
 import org.matsim.api.core.v01.network.Network;
 
-import ch.ethz.idsc.queuey.datalys.MultiFileReader;
-import playground.clruch.data.ReferenceFrame;
-import playground.clruch.net.MatsimStaticDatabase;
-import playground.clruch.net.StorageUtils;
+import ch.ethz.idsc.queuey.datalys.MultiFileTools;
+import ch.ethz.idsc.queuey.util.GlobalAssert;
+
 import playground.clruch.utils.NetworkLoader;
 
 /** @author Claudio Ruch */
@@ -23,38 +21,45 @@ enum StandaloneFleetConverter {
 
         // File directory = //
         // new File("/home/andya/Desktop/idsc_st/10_Daten/2017-10-11_ZurichNew");
-        
-        File simulationDirectory = new File(args[0]);
-        File networkFile = new File(simulationDirectory, "trb_config.xml");
-        System.out.println("INFO working folder: " + simulationDirectory.getAbsolutePath());
-        System.out.println("INFO network file: " + networkFile.getAbsolutePath());
-        ReferenceFrame referenceFrame = ReferenceFrame.SWITZERLAND;
-        List<File> trailFiles = (new MultiFileReader(simulationDirectory, "Fahrtstrecken")).getFolderFiles();
-        System.out.println("INFO found files: ");
-        for (File file : trailFiles) {
-            System.out.println(file.getAbsolutePath());
-        }
-        DayTaxiRecord dayTaxiRecord = new DayTaxiRecord();
-        // extract data from file and put into dayTaxiRecord
-        CsvFleetReader reader = new CsvFleetReader(dayTaxiRecord);
-        reader.populateFrom(trailFiles);
-        Network network = NetworkLoader.loadNetwork(networkFile);
 
+        File simulationDirectory = MultiFileTools.getWorkingDirectory();
         
-        // STEP 2: DayTaxiRecord to MATSimStaticDatabase
-        MatsimStaticDatabase.initializeSingletonInstance(network, referenceFrame);
-        // generate sim objects and store
-        File outputFolder = new File(simulationDirectory, "output");
-        System.out.println("INFO output Folder: " + outputFolder.getAbsolutePath());
-
-//        if (storageSupplierFile.exists()) {
-//            FileDelete.of(storageSupplierFile, 5, 100000);
+        File outputDirectory = new File(simulationDirectory, "output");
+        File configFile = new File(outputDirectory, "av_config.xml");        
+        System.out.println("INFO working folder: " + outputDirectory.getAbsolutePath());
+        System.out.println("INFO network file: " + configFile.getAbsolutePath());
+        Network network = NetworkLoader.loadNetwork(configFile);
+        GlobalAssert.that(network!=null);
+        System.out.println("networ has " + network.getNodes().size() + " nodes");
+        
+        
+//        ReferenceFrame referenceFrame = ReferenceFrame.SWITZERLAND;
+//        List<File> trailFiles = (new MultiFileReader(simulationDirectory, "Fahrtstrecken")).getFolderFiles();
+//        System.out.println("INFO found files: ");
+//        for (File file : trailFiles) {
+//            System.out.println(file.getAbsolutePath());
 //        }
-//        GlobalAssert.that(!storageSupplierFile.exists());
-//        storageSupplierFile.mkdir();
-
-        StorageUtils storageUtils = new StorageUtils(outputFolder);
-        SimulationFleetDump.of(dayTaxiRecord, network, MatsimStaticDatabase.INSTANCE, storageUtils);
+//        DayTaxiRecord dayTaxiRecord = new DayTaxiRecord();
+//        // extract data from file and put into dayTaxiRecord
+//        CsvFleetReader reader = new CsvFleetReader(dayTaxiRecord);
+//        reader.populateFrom(trailFiles);
+//        
+//
+//        
+//        // STEP 2: DayTaxiRecord to MATSimStaticDatabase
+//        MatsimStaticDatabase.initializeSingletonInstance(network, referenceFrame);
+//        // generate sim objects and store
+//        File outputFolder = new File(simulationDirectory, "output");
+//        System.out.println("INFO output Folder: " + outputFolder.getAbsolutePath());
+//
+////        if (storageSupplierFile.exists()) {
+////            FileDelete.of(storageSupplierFile, 5, 100000);
+////        }
+////        GlobalAssert.that(!storageSupplierFile.exists());
+////        storageSupplierFile.mkdir();
+//
+//        StorageUtils storageUtils = new StorageUtils(outputFolder);
+//        SimulationFleetDump.of(dayTaxiRecord, network, MatsimStaticDatabase.INSTANCE, storageUtils);
     }
 
 }
