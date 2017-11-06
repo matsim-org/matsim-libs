06b23ac311f3355cf7efe0d2697293ac27c20dcb
balacm <balacm@IVT-THKPD-18.d.ethz.ch>
2017-11-06 18:35:24 +0100

    activity duration is considered when choosing a freefloating vehicle if an agent wants to keep it during its next activity.

--- contribs/carsharing/src/main/java/org/matsim/contrib/carsharing/manager/demand/VehicleChoiceAgentImpl.java
+++ contribs/carsharing/src/main/java/org/matsim/contrib/carsharing/manager/demand/VehicleChoiceAgentImpl.java
@@ -3,6 +3,8 @@ package org.matsim.contrib.carsharing.manager.demand;
 import java.util.Set;
 
 import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.population.Leg;
+import org.matsim.api.core.v01.population.Person;
 import org.matsim.contrib.carsharing.manager.supply.CarsharingSupplyInterface;
 import org.matsim.contrib.carsharing.vehicles.CSVehicle;
 import org.matsim.core.utils.geometry.CoordUtils;
@@ -14,7 +16,7 @@ public class VehicleChoiceAgentImpl implements VehicleChoiceAgent {
 	@Inject private CarsharingSupplyInterface carsharingSupplyContainer;	
 	
 	@Override
-	public CSVehicle chooseVehicle(Set<CSVehicle> vehicleOptions, Link startLink) {
+	public CSVehicle chooseVehicle(Set<CSVehicle> vehicleOptions, Link startLink, Leg leg, double currentTime, Person person) {
 		
 		double distance = -1.0;
 		CSVehicle chosenVehicle = null;
@@ -29,5 +31,12 @@ public class VehicleChoiceAgentImpl implements VehicleChoiceAgent {
 		}
 		
 		return chosenVehicle;		
+	}
+
+	@Override
+	public CSVehicle chooseVehicleActivityTimeIncluded(Set<CSVehicle> vehicleOptions, Link startLink, Leg leg,
+			double currentTime, Person person, double durationOfNextActivity, boolean keepthecar) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
