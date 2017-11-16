15c5f79252e8c55f1e4e427d34c118e4f37b7787
Lukas Sieber <sieber.lukas@gmail.com>
2017-11-16 14:51:35 +0100

    Implementation of Combined Scenario Reducer

---
+++ playgrounds/ivtExt/src/main/java/playground/lsieber/scenario/reducer/AbstractScenarioReducer.java
@@ -0,0 +1,66 @@
+package playground.lsieber.scenario.reducer;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.network.NetworkWriter;
+import org.matsim.api.core.v01.population.Population;
+import org.matsim.api.core.v01.population.PopulationWriter;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.facilities.ActivityFacilities;
+import org.matsim.facilities.FacilitiesWriter;
+
+import ch.ethz.idsc.queuey.datalys.MultiFileTools;
+import playground.clruch.ScenarioOptions;
+import playground.clruch.utils.PropertiesExt;
+
+public abstract class AbstractScenarioReducer {
+    private File workingDirectory;
+    private Config config;
+    private PropertiesExt simOptions;
+    protected Scenario originalScenario;
+    protected Network network;
+    protected Population population;
+    protected ActivityFacilities facilities;
+    //TODO create Folder based on this Directory if it does not exist, Make this string Changeable
+    private String reducedScenarioDirectory = "reducedScenario";
+
+    public AbstractScenarioReducer() throws IOException {
+        this.loadScenario();
+        this.run();
+    }
+
+    private void loadScenario() throws IOException {
+        workingDirectory = MultiFileTools.getWorkingDirectory();
+        simOptions = PropertiesExt.wrap(ScenarioOptions.load(workingDirectory));
+        File file = new File(workingDirectory, simOptions.getString("simuConfig"));
+        config = ConfigUtils.loadConfig(file.toString());
+        originalScenario = ScenarioUtils.loadScenario(config);
+    }
+
+    public void run() throws MalformedURLException, IOException {
+        // Mandatory Fields of a Scenario
+        network = networkCutter();
+        population = populationCutter();
+        facilities = facilitiesCutter();
+    }
+
+    protected abstract Network networkCutter() throws MalformedURLException, IOException;
+
+    protected abstract Population populationCutter();
+
+    protected abstract ActivityFacilities facilitiesCutter();
+
+    public void writeToXML() {
+        // Mandatory Fields of a Scenario
+        new NetworkWriter(network).write(reducedScenarioDirectory + "/reducedNetwork.xml");
+        new PopulationWriter(population).write(reducedScenarioDirectory + "/reducedPopulation.xml");
+        new FacilitiesWriter(facilities).write(reducedScenarioDirectory + "/reducedFacilities.xml");
+    }
+
+}
---
+++ playgrounds/ivtExt/src/main/java/playground/lsieber/scenario/reducer/ShapeScenarioReducer.java
@@ -0,0 +1,49 @@
+package playground.lsieber.scenario.reducer;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.HashSet;
+
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.population.Population;
+import org.matsim.facilities.ActivityFacilities;
+
+import playground.lsieber.networkshapecutter.FacilityPopulationBasedCutter;
+import playground.lsieber.networkshapecutter.NetworkCutterShape;
+
+public class ShapeScenarioReducer extends AbstractScenarioReducer{
+
+    public ShapeScenarioReducer() throws IOException{
+        // TODO Auto-generated constructor stub
+        super();
+    }
+
+    @Override
+    protected Network networkCutter() throws MalformedURLException, IOException {
+        // TODO @ lukas Decouple Modes and Nettworkcutting
+        
+        HashSet<String> modes = new HashSet<String>();
+        // modes.add("car");
+        modes.add("pt");
+        modes.add("tram");
+        modes.add("bus");
+        
+        // TODO @ Lukas Implenment Shapefile from IDSC Options
+        File shapefile = new File("shapefile.shp");
+        
+        return new NetworkCutterShape(shapefile).filter(this.originalScenario.getNetwork(), modes);
+    }
+
+    @Override
+    protected Population populationCutter() {
+        // TODO @ Lukas generate Population Cutter
+        return originalScenario.getPopulation();
+    }
+
+    @Override
+    protected ActivityFacilities facilitiesCutter() {
+        return new FacilityPopulationBasedCutter(this.population).filter(originalScenario.getActivityFacilities());
+    }
+
+}
