fd40a776088f1e8fb0ca7d236fd5f16691567be4
jfbischoff <bischoff@vsp.tu-berlin.de>
2017-12-14 15:51:32 +0100

    adding another test

---
+++ code-examples/src/test/java/tutorial/dvrptc/RunTaxiExampleTest.java
@@ -0,0 +1,14 @@
+package tutorial.dvrptc;
+
+import org.junit.Test;
+
+import tutorial.dvrpetc.RunTaxiExample;
+
+public class RunTaxiExampleTest {
+
+	@Test
+	public void test() {
+		RunTaxiExample.main(null);
+	}
+
+}
