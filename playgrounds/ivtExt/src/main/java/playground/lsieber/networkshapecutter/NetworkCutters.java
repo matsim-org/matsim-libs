69ec0345c2d7b7237e82c895428519bff5402ced
clruch <claudio.ruch@gmail.com>
2017-12-14 17:38:44 +0100

    wip

--- playgrounds/ivtExt/src/main/java/playground/lsieber/networkshapecutter/NetworkCutters.java
+++ playgrounds/ivtExt/src/main/java/playground/lsieber/networkshapecutter/NetworkCutters.java
@@ -1,17 +1,21 @@
 package playground.lsieber.networkshapecutter;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
 import org.matsim.api.core.v01.network.Network;
 
+import ch.ethz.idsc.queuey.util.GlobalAssert;
 import playground.clruch.options.ScenarioOptions;
 
 public enum NetworkCutters {
     SHAPE {
         @Override
         public Network cut(Network originalNetwork, ScenarioOptions scenOptions) throws MalformedURLException, IOException {
-            Network modifiedNetwork = new NetworkCutterShape(scenOptions.getShapeFile()).process(originalNetwork);
+            File shapeFile = scenOptions.getShapeFile();
+            GlobalAssert.that(shapeFile.exists());
+            Network modifiedNetwork = new NetworkCutterShape(shapeFile).process(originalNetwork);
             return modifiedNetwork;
         }
     },
