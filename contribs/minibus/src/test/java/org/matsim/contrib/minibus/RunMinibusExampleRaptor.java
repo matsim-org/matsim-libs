068da54f04f63ef518d403be6f21ac31f0b72fc0
Gregor Leich <leich@vsp.tu-berlin.de>
2018-01-02 19:53:56 +0100

    add test that runs the minibus example with the Raptor router
    to see whether it runs through or fails

---
+++ contribs/minibus/src/test/java/org/matsim/contrib/minibus/RunMinibusExampleRaptor.java
@@ -0,0 +1,17 @@
+package org.matsim.contrib.minibus;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.matsim.testcases.MatsimTestUtils;
+
+public class RunMinibusExampleRaptor {
+	
+	@Rule public MatsimTestUtils utils = new MatsimTestUtils();
+	
+	@Test
+    public final void testRunScenarioWithRaptor() {
+		String[] args = {"test/input/org/matsim/contrib/minibus/example-scenario/config_raptor.xml"};
+		RunMinibus.main(args);
+	}
+
+}
