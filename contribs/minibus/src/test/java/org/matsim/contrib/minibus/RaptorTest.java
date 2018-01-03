6b655c6b267f4b5c44ef331da1298b6d6cd137b0
Amit Agarwal <amit.agarwal@campus.tu-berlin.de>
2018-01-03 15:35:06 +0100

    adding a test to reproduce the error which is specific to raptor transit router

---
+++ contribs/minibus/src/test/java/org/matsim/contrib/minibus/RaptorTest.java
@@ -0,0 +1,60 @@
+package org.matsim.contrib.minibus;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.contrib.minibus.performance.raptor.Raptor;
+import org.matsim.contrib.minibus.performance.raptor.RaptorDisutility;
+import org.matsim.contrib.minibus.performance.raptor.TransitRouterQuadTree;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.AbstractModule;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.pt.router.TransitRouter;
+import org.matsim.pt.router.TransitRouterConfig;
+import org.matsim.pt.transitSchedule.api.TransitSchedule;
+import org.matsim.testcases.MatsimTestUtils;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+public class RaptorTest {
+	
+	
+	@Rule
+	public MatsimTestUtils helper = new MatsimTestUtils();
+	
+	@Test
+	public void test() {
+		String config = "test/input/org/matsim/contrib/minibus/example-scenario/config_raptor.xml";
+		
+		Scenario scenario = ScenarioUtils.loadScenario(ConfigUtils.loadConfig(config));
+		scenario.getConfig().controler().setOutputDirectory(helper.getOutputDirectory());
+		
+		Controler controler = new Controler(scenario);
+		controler.addOverridingModule(new AbstractModule() {
+			
+			@Override
+			public void install() {
+				bind(TransitRouter.class).toProvider(RaptorTransitRouterProvider.class);
+			}
+		});
+		controler.run();
+	}
+	
+	
+	private static class RaptorTransitRouterProvider implements Provider<TransitRouter> {
+		@Inject private Config config;
+		@Inject private TransitSchedule schedule;
+
+		@Override
+		public TransitRouter get() {
+			TransitRouterConfig transitRouterConfig = new TransitRouterConfig(config.planCalcScore(), config.plansCalcRoute(), config.transitRouter(), config.vspExperimental());
+			RaptorDisutility raptorDisutility = new RaptorDisutility(transitRouterConfig, 0., 0.);
+			TransitRouterQuadTree transitRouterQuadTree = new TransitRouterQuadTree(raptorDisutility);
+			transitRouterQuadTree.initializeFromSchedule(schedule, transitRouterConfig.getBeelineWalkConnectionDistance());
+			return new Raptor(transitRouterQuadTree, raptorDisutility, transitRouterConfig);
+		}
+	}
+}
