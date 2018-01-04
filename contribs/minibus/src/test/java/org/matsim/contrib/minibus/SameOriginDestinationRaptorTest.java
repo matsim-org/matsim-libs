75a005ceb7c716569cc4e07c387178b83bc530ff
Amit Agarwal <amit.agarwal@campus.tu-berlin.de>
2018-01-04 13:26:28 +0100

    raptor transit router provider

--- contribs/minibus/src/test/java/org/matsim/contrib/minibus/SameOriginDestinationRaptorTest.java
+++ contribs/minibus/src/test/java/org/matsim/contrib/minibus/SameOriginDestinationRaptorTest.java
@@ -1,21 +1,14 @@
 package org.matsim.contrib.minibus;
 
-import com.google.inject.Inject;
-import com.google.inject.Provider;
 import org.junit.Rule;
 import org.junit.Test;
 import org.matsim.api.core.v01.Scenario;
-import org.matsim.contrib.minibus.performance.raptor.Raptor;
-import org.matsim.contrib.minibus.performance.raptor.RaptorDisutility;
-import org.matsim.contrib.minibus.performance.raptor.TransitRouterQuadTree;
-import org.matsim.core.config.Config;
+import org.matsim.contrib.minibus.performance.raptor.RaptorTransitRouterProvider;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.pt.router.TransitRouter;
-import org.matsim.pt.router.TransitRouterConfig;
-import org.matsim.pt.transitSchedule.api.TransitSchedule;
 import org.matsim.testcases.MatsimTestUtils;
 
 public class SameOriginDestinationRaptorTest {
@@ -43,20 +36,5 @@ public class SameOriginDestinationRaptorTest {
 			}
 		});
 		controler.run();
-	}
-	
-	
-	private static class RaptorTransitRouterProvider implements Provider<TransitRouter> {
-		@Inject private Config config;
-		@Inject private TransitSchedule schedule;
-
-		@Override
-		public TransitRouter get() {
-			TransitRouterConfig transitRouterConfig = new TransitRouterConfig(config.planCalcScore(), config.plansCalcRoute(), config.transitRouter(), config.vspExperimental());
-			RaptorDisutility raptorDisutility = new RaptorDisutility(transitRouterConfig, 0., 0.);
-			TransitRouterQuadTree transitRouterQuadTree = new TransitRouterQuadTree(raptorDisutility);
-			transitRouterQuadTree.initializeFromSchedule(schedule, transitRouterConfig.getBeelineWalkConnectionDistance());
-			return new Raptor(transitRouterQuadTree, raptorDisutility, transitRouterConfig);
-		}
 	}
 }
