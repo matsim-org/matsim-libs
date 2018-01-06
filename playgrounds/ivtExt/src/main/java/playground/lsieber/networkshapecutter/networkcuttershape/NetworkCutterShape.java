793d512f24092db5bdb1fecb2e05648e0168bfd5
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-01-06 18:06:11 +0100

    sync with queuey

--- playgrounds/ivtExt/src/main/java/playground/lsieber/networkshapecutter/networkcuttershape/NetworkCutterShape.java
+++ playgrounds/ivtExt/src/main/java/playground/lsieber/networkshapecutter/networkcuttershape/NetworkCutterShape.java
@@ -6,7 +6,7 @@ package playground.lsieber.networkshapecutter.networkcuttershape;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.util.HashSet;
+import java.util.Set;
 
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.core.network.NetworkUtils;
@@ -34,7 +34,7 @@ public class NetworkCutterShape extends NetworkCutter {
 
     public Network filterInternal(Network originalNetwork) throws IOException {
         modifiedNetwork = NetworkUtils.createNetwork();
-        HashSet<MultiPolygon> polygons = multipolygons.getPolygons();
+        Set<MultiPolygon> polygons = multipolygons.getPolygons();
         AddContainedNodes.of(originalNetwork).in(polygons).to(modifiedNetwork);
         AddContainedLinks.of(originalNetwork).to(modifiedNetwork);
         return modifiedNetwork;
