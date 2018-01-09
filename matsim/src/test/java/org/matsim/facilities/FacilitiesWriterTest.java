f9beb4a94eddb84c0cb61182d254c5bd91d83c3f
gabrielthunig <gabriel.thunig@web.de>
2018-01-09 12:16:01 +0100

    implement attributable for facilityWriter

--- matsim/src/test/java/org/matsim/facilities/FacilitiesWriterTest.java
+++ matsim/src/test/java/org/matsim/facilities/FacilitiesWriterTest.java
@@ -16,42 +16,49 @@ import org.matsim.testcases.MatsimTestUtils;
  */
 public class FacilitiesWriterTest {
 
-	@Rule
-	public MatsimTestUtils testUtil = new MatsimTestUtils();
-	
-	@Test
-	public void testWriteLinkId() {
-		Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
-		ActivityFacilities facilities = scenario.getActivityFacilities();
-		ActivityFacilitiesFactory factory = facilities.getFactory();
-		
-		ActivityFacility fac1 = factory.createActivityFacility(Id.create("1", ActivityFacility.class), new Coord(10.0, 15.0));
-		((ActivityFacilityImpl) fac1).setLinkId(Id.create("Abc", Link.class));
-		ActivityFacility fac2 = factory.createActivityFacility(Id.create("2", ActivityFacility.class), new Coord(20.0, 25.0));
-		((ActivityFacilityImpl) fac2).setLinkId(Id.create("Def", Link.class));
-		ActivityFacility fac3 = factory.createActivityFacility(Id.create("3", ActivityFacility.class), new Coord(30.0, 35.0));
-		
-		facilities.addActivityFacility(fac1);
-		facilities.addActivityFacility(fac2);
-		facilities.addActivityFacility(fac3);
-		
-		String filename = testUtil.getOutputDirectory() + "/facilities.xml";
-		new FacilitiesWriter(facilities).write(filename);
-		
-		scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
-		facilities = scenario.getActivityFacilities();
-		MatsimFacilitiesReader reader = new MatsimFacilitiesReader(scenario);
-		reader.readFile(filename);
-		
-		Assert.assertEquals(3, facilities.getFacilities().size());
-		
-		ActivityFacility fac1b = facilities.getFacilities().get(Id.create(1, ActivityFacility.class));
-		Assert.assertEquals(Id.create("Abc", Link.class), fac1b.getLinkId());
-		
-		ActivityFacility fac2b = facilities.getFacilities().get(Id.create(2, ActivityFacility.class));
-		Assert.assertEquals(Id.create("Def", Link.class), fac2b.getLinkId());
+    @Rule
+    public MatsimTestUtils testUtil = new MatsimTestUtils();
 
-		ActivityFacility fac3b = facilities.getFacilities().get(Id.create(3, ActivityFacility.class));
-		Assert.assertNull(fac3b.getLinkId());
-	}
+    @Test
+    public void testWriteLinkId() {
+        Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+        ActivityFacilities facilities = scenario.getActivityFacilities();
+        ActivityFacilitiesFactory factory = facilities.getFactory();
+
+        ActivityFacility fac1 = factory.createActivityFacility(Id.create("1", ActivityFacility.class), new Coord(10.0, 15.0));
+        ((ActivityFacilityImpl) fac1).setLinkId(Id.create("Abc", Link.class));
+        ActivityFacility fac2 = factory.createActivityFacility(Id.create("2", ActivityFacility.class), new Coord(20.0, 25.0));
+        ((ActivityFacilityImpl) fac2).setLinkId(Id.create("Def", Link.class));
+        ActivityFacility fac3 = factory.createActivityFacility(Id.create("3", ActivityFacility.class), new Coord(30.0, 35.0));
+
+        fac1.getAttributes().putAttribute("population", 1000);
+        fac2.getAttributes().putAttribute("population", 1200);
+        fac3.getAttributes().putAttribute("owner", "pepsiCo");
+
+        facilities.addActivityFacility(fac1);
+        facilities.addActivityFacility(fac2);
+        facilities.addActivityFacility(fac3);
+
+        String filename = testUtil.getOutputDirectory() + "/facilities.xml";
+        new FacilitiesWriter(facilities).write(filename);
+
+        scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+        facilities = scenario.getActivityFacilities();
+        MatsimFacilitiesReader reader = new MatsimFacilitiesReader(scenario);
+        reader.readFile(filename);
+
+        Assert.assertEquals(3, facilities.getFacilities().size());
+
+        ActivityFacility fac1b = facilities.getFacilities().get(Id.create(1, ActivityFacility.class));
+        Assert.assertEquals(Id.create("Abc", Link.class), fac1b.getLinkId());
+        Assert.assertEquals(1000, fac1b.getAttributes().getAttribute("population"));
+
+        ActivityFacility fac2b = facilities.getFacilities().get(Id.create(2, ActivityFacility.class));
+        Assert.assertEquals(Id.create("Def", Link.class), fac2b.getLinkId());
+        Assert.assertEquals(1200, fac2b.getAttributes().getAttribute("population"));
+
+        ActivityFacility fac3b = facilities.getFacilities().get(Id.create(3, ActivityFacility.class));
+        Assert.assertNull(fac3b.getLinkId());
+        Assert.assertEquals("pepsiCo", fac3b.getAttributes().getAttribute("owner"));
+    }
 }
