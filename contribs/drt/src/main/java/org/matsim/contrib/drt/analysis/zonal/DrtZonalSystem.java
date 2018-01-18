a7994348e8545dca680a984d38ea7f903ed2b22e
Michal Maciejewski <michal.mac@gmail.com>
2018-01-18 14:09:18 +0100

    NoRebalancingStrategy is default (not SendToStartLinkStrategy)

--- contribs/drt/src/main/java/org/matsim/contrib/drt/analysis/zonal/DrtZonalSystem.java
+++ contribs/drt/src/main/java/org/matsim/contrib/drt/analysis/zonal/DrtZonalSystem.java
@@ -37,67 +37,64 @@ import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.Point;
 
 /**
- * @author  jbischoff
- *
- */
-/**
+ * @author jbischoff
  *
  */
 public class DrtZonalSystem {
 
-	private final Map<Id<Link>,String> link2zone = new HashMap<>();
+	private final Map<Id<Link>, String> link2zone = new HashMap<>();
 	private final Network network;
-	private final Map<String,Geometry> zones;
-	
+	private final Map<String, Geometry> zones;
+
 	/**
 	 * 
 	 */
 	public DrtZonalSystem(Network network, double cellsize) {
 		this.network = network;
 		zones = DrtGridUtils.createGridFromNetwork(network, cellsize);
-		
+
 	}
-	
-	public DrtZonalSystem(Network network, Map<String,Geometry> zones) {
+
+	public DrtZonalSystem(Network network, Map<String, Geometry> zones) {
 		this.network = network;
 		this.zones = zones;
-		
-	}
-	
 
-	public Geometry getZone(String zone){
+	}
+
+	public Geometry getZone(String zone) {
 		return zones.get(zone);
 	}
-	
-	public String getZoneForLinkId(Id<Link> linkId){
-		if (this.link2zone.containsKey(linkId)){
+
+	public String getZoneForLinkId(Id<Link> linkId) {
+		if (this.link2zone.containsKey(linkId)) {
 			return link2zone.get(linkId);
 		}
-		
+
 		Point linkCoord = MGC.coord2Point(network.getLinks().get(linkId).getCoord());
-		
-		for (Entry<String, Geometry> e : zones.entrySet()){
-			if (e.getValue().contains(linkCoord)){
+
+		for (Entry<String, Geometry> e : zones.entrySet()) {
+			if (e.getValue().contains(linkCoord)) {
 				link2zone.put(linkId, e.getKey());
 				return e.getKey();
 			}
 		}
 		link2zone.put(linkId, null);
 		return null;
-		
-	}
-/**
- * @return the zones
- */
-public Map<String, Geometry> getZones() {
-	return zones;
-}
 
-public Coord getZoneCentroid(String zoneId){
-		
+	}
+
+	/**
+	 * @return the zones
+	 */
+	public Map<String, Geometry> getZones() {
+		return zones;
+	}
+
+	public Coord getZoneCentroid(String zoneId) {
+
 		Geometry zone = zones.get(zoneId);
-		if (zone == null){
-			Logger.getLogger(getClass()).error("Zone "+zoneId+" not found.");
+		if (zone == null) {
+			Logger.getLogger(getClass()).error("Zone " + zoneId + " not found.");
 			return null;
 		}
 		Coord c = MGC.point2Coord(zone.getCentroid());
