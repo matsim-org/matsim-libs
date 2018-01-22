c6d6b9cbd20293153e03b1ff2ddc86596a922c48
Michal Maciejewski <michal.mac@gmail.com>
2018-01-22 15:17:54 +0100

    clean Request interface and classses

--- contribs/dvrp/src/main/java/org/matsim/contrib/dvrp/data/Request.java
+++ contribs/dvrp/src/main/java/org/matsim/contrib/dvrp/data/Request.java
@@ -22,6 +22,18 @@ package org.matsim.contrib.dvrp.data;
 import org.matsim.api.core.v01.Identifiable;
 
 /**
+ * Represents a request in DVRP.
+ * 
+ * Default methods are typical to MATSim scenarios: a single agent (quantity = 1) wants to depart immediately
+ * (earliestStartTime = submissionTime), and will wait until the vehicle arrives (latestStartTime =
+ * {@link Double#MAX_VALUE})
+ * 
+ * For a capacitated VRP - adapt getQuantity()
+ * 
+ * For a VRP with time windows - adapt both getEarliestStartTime() and getLatestStartTime()
+ * 
+ * For a VRP with rejections - implement isRejected()
+ * 
  * @author michalm
  */
 public interface Request extends Identifiable<Request> {
@@ -29,17 +41,21 @@ public interface Request extends Identifiable<Request> {
 	/**
 	 * @return the amount of people/goods to serve/transport (see: {@link Vehicle#getCapacity()})
 	 */
-	double getQuantity();
+	default double getQuantity() {
+		return 1;
+	}
 
 	/**
-	 * @return begining of the time window (inclusive) - earliest time when the request can be served
+	 * @return beginning of the time window (inclusive) - earliest time when the request can be served
 	 */
 	double getEarliestStartTime();
 
 	/**
 	 * @return end of the time window (exclusive) - time by which the request should be served
 	 */
-	double getLatestStartTime();
+	default double getLatestStartTime() {
+		return Double.MAX_VALUE;
+	}
 
 	/**
 	 * @return time at which the request was submitted
@@ -49,5 +65,12 @@ public interface Request extends Identifiable<Request> {
 	/**
 	 * @return indicates whether the request has been rejected by the service provider (optimizer/dispatcher)
 	 */
-	boolean isRejected();
+	default boolean isRejected() {
+		return false;
+	}
+
+	public static String toString(Request request) {
+		return "[id=" + request.getId() + "][submissionTime=" + request.getSubmissionTime() + "][earliestStartTime="
+				+ request.getEarliestStartTime() + "]";
+	}
 }
