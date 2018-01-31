958e32d172c88e4aff068a455b76ae3d8dbfb182
Amit Agarwal <amit.agarwal@campus.tu-berlin.de>
2018-01-31 09:50:58 +0100

    adding facility source; it appears that these tests are still failing

--- contribs/carsharing/src/test/java/org/matsim/contrib/carsharing/runExample/RunCarsharingIT.java
+++ contribs/carsharing/src/test/java/org/matsim/contrib/carsharing/runExample/RunCarsharingIT.java
@@ -31,6 +31,7 @@ import org.matsim.contrib.carsharing.config.OneWayCarsharingConfigGroup;
 import org.matsim.contrib.carsharing.config.TwoWayCarsharingConfigGroup;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.FacilitiesConfigGroup;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
@@ -67,6 +68,7 @@ public class RunCarsharingIT {
 		config.plans().setInputPersonAttributeFile("1000desiresAttributes.xml");
 
 		config.facilities().setInputFile("facilities.xml" );
+		config.facilities().setFacilitiesSource(FacilitiesConfigGroup.FacilitiesSource.fromFile);
 		
 		config.plansCalcRoute().setInsertingAccessEgressWalk(false); // otherwise does not work. kai,feb'16
 
