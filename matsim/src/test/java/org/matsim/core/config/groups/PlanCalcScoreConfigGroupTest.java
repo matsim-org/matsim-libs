55cb02e0e7805344dcef57cc83177cd250cc2fea
Marcel Rieser <rieser@simunto.com>
2018-02-24 21:55:49 +0100

    replace calls to deprecated methods that resulted in test failures.

--- matsim/src/test/java/org/matsim/core/config/groups/PlanCalcScoreConfigGroupTest.java
+++ matsim/src/test/java/org/matsim/core/config/groups/PlanCalcScoreConfigGroupTest.java
@@ -1,22 +1,21 @@
 package org.matsim.core.config.groups;
 
-import java.util.Map;
-import java.util.Random;
-
-import org.junit.Assert;
-
 import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigGroup;
+import org.matsim.core.config.ConfigReader;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.config.ConfigWriter;
-import org.matsim.core.config.ConfigReader;
 import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ActivityParams;
 import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ModeParams;
 import org.matsim.testcases.MatsimTestUtils;
+
+import java.util.Map;
+import java.util.Random;
 
 public class PlanCalcScoreConfigGroupTest {
 	private static final Logger log =
@@ -77,99 +76,123 @@ public class PlanCalcScoreConfigGroupTest {
 		Assert.assertEquals(
 				"wrong brainExpBeta "+msg,
 				initialGroup.getBrainExpBeta(),
-				inputConfigGroup.getBrainExpBeta() );
+				inputConfigGroup.getBrainExpBeta(),
+				1e-7);
 		Assert.assertEquals(
 				"wrong constantBike "+msg,
 				initialGroup.getModes().get(TransportMode.bike).getConstant(),
-				inputConfigGroup.getModes().get(TransportMode.bike).getConstant());
+				inputConfigGroup.getModes().get(TransportMode.bike).getConstant(),
+				1e-7);
 		Assert.assertEquals(
 				"wrong constantCar "+msg,
 				initialGroup.getModes().get(TransportMode.car).getConstant(),
-				inputConfigGroup.getModes().get(TransportMode.car).getConstant());
+				inputConfigGroup.getModes().get(TransportMode.car).getConstant(),
+				1e-7);
 		Assert.assertEquals(
 				"wrong constantOther "+msg,
 				initialGroup.getModes().get(TransportMode.other).getConstant(),
-				inputConfigGroup.getModes().get(TransportMode.other).getConstant());
+				inputConfigGroup.getModes().get(TransportMode.other).getConstant(),
+				1e-7);
 		Assert.assertEquals(
 				"wrong constantPt "+msg,
 				initialGroup.getModes().get(TransportMode.pt).getConstant(),
-				inputConfigGroup.getModes().get(TransportMode.pt).getConstant());
+				inputConfigGroup.getModes().get(TransportMode.pt).getConstant(),
+				1e-7);
 		Assert.assertEquals(
 				"wrong constantWalk "+msg,
 				initialGroup.getModes().get(TransportMode.walk).getConstant(),
-				inputConfigGroup.getModes().get(TransportMode.walk).getConstant());
+				inputConfigGroup.getModes().get(TransportMode.walk).getConstant(),
+				1e-7);
 		Assert.assertEquals(
 				"wrong lateArrival_utils_hr "+msg,
 				initialGroup.getLateArrival_utils_hr(),
-				inputConfigGroup.getLateArrival_utils_hr() );
+				inputConfigGroup.getLateArrival_utils_hr(),
+				1e-7 );
 		Assert.assertEquals(
 				"wrong earlyDeparture_utils_hr "+msg,
 				initialGroup.getEarlyDeparture_utils_hr(),
-				inputConfigGroup.getEarlyDeparture_utils_hr() );
+				inputConfigGroup.getEarlyDeparture_utils_hr(),
+				1e-7 );
 		Assert.assertEquals(
 				"wrong learningRate "+msg,
 				initialGroup.getLearningRate(),
-				inputConfigGroup.getLearningRate() );
+				inputConfigGroup.getLearningRate(),
+				1e-7 );
 		Assert.assertEquals(
 				"wrong marginalUtilityOfMoney "+msg,
 				initialGroup.getMarginalUtilityOfMoney(),
-				inputConfigGroup.getMarginalUtilityOfMoney() );
+				inputConfigGroup.getMarginalUtilityOfMoney() ,
+				1e-7);
 		Assert.assertEquals(
 				"wrong marginalUtlOfDistanceOther "+msg,
 				initialGroup.getModes().get(TransportMode.other).getMarginalUtilityOfDistance(),
-				inputConfigGroup.getModes().get(TransportMode.other).getMarginalUtilityOfDistance());
+				inputConfigGroup.getModes().get(TransportMode.other).getMarginalUtilityOfDistance(),
+				1e-7);
 		Assert.assertEquals(
 				"wrong marginalUtlOfDistanceWalk "+msg,
 				initialGroup.getModes().get(TransportMode.walk).getMarginalUtilityOfDistance(),
-				inputConfigGroup.getModes().get(TransportMode.walk).getMarginalUtilityOfDistance());
+				inputConfigGroup.getModes().get(TransportMode.walk).getMarginalUtilityOfDistance(),
+				1e-7);
 		Assert.assertEquals(
 				"wrong marginalUtlOfWaiting_utils_hr "+msg,
 				initialGroup.getMarginalUtlOfWaiting_utils_hr(),
-				inputConfigGroup.getMarginalUtlOfWaiting_utils_hr() );
+				inputConfigGroup.getMarginalUtlOfWaiting_utils_hr(),
+				1e-7 );
 		Assert.assertEquals(
 				"wrong marginalUtlOfWaitingPt_utils_hr "+msg,
 				initialGroup.getMarginalUtlOfWaitingPt_utils_hr(),
-				inputConfigGroup.getMarginalUtlOfWaitingPt_utils_hr() );
+				inputConfigGroup.getMarginalUtlOfWaitingPt_utils_hr(),
+				1e-7 );
 		Assert.assertEquals(
 				"wrong monetaryDistanceCostRateCar "+msg,
 				initialGroup.getModes().get(TransportMode.car).getMonetaryDistanceRate(),
-				inputConfigGroup.getModes().get(TransportMode.car).getMonetaryDistanceRate());
+				inputConfigGroup.getModes().get(TransportMode.car).getMonetaryDistanceRate(),
+				1e-7);
 		Assert.assertEquals(
 				"wrong monetaryDistanceCostRatePt "+msg,
 				initialGroup.getModes().get(TransportMode.pt).getMonetaryDistanceRate(),
-				inputConfigGroup.getModes().get(TransportMode.pt).getMonetaryDistanceRate());
+				inputConfigGroup.getModes().get(TransportMode.pt).getMonetaryDistanceRate(),
+				1e-7);
 		Assert.assertEquals(
 				"wrong pathSizeLogitBeta "+msg,
 				initialGroup.getPathSizeLogitBeta(),
-				inputConfigGroup.getPathSizeLogitBeta() );
+				inputConfigGroup.getPathSizeLogitBeta(),
+				1e-7 );
 		Assert.assertEquals(
 				"wrong performing_utils_hr "+msg,
 				initialGroup.getPerforming_utils_hr(),
-				inputConfigGroup.getPerforming_utils_hr() );
+				inputConfigGroup.getPerforming_utils_hr(),
+				1e-7 );
 		Assert.assertEquals(
 				"wrong traveling_utils_hr "+msg,
 				initialGroup.getModes().get(TransportMode.car).getMarginalUtilityOfTraveling(),
-				inputConfigGroup.getModes().get(TransportMode.car).getMarginalUtilityOfTraveling());
+				inputConfigGroup.getModes().get(TransportMode.car).getMarginalUtilityOfTraveling(),
+				1e-7);
 		Assert.assertEquals(
 				"wrong travelingBike_utils_hr "+msg,
 				initialGroup.getModes().get(TransportMode.bike).getMarginalUtilityOfTraveling(),
-				inputConfigGroup.getModes().get(TransportMode.bike).getMarginalUtilityOfTraveling());
+				inputConfigGroup.getModes().get(TransportMode.bike).getMarginalUtilityOfTraveling(),
+				1e-7);
 		Assert.assertEquals(
 				"wrong travelingOther_utils_hr "+msg,
 				initialGroup.getModes().get(TransportMode.other).getMarginalUtilityOfTraveling(),
-				inputConfigGroup.getModes().get(TransportMode.other).getMarginalUtilityOfTraveling());
+				inputConfigGroup.getModes().get(TransportMode.other).getMarginalUtilityOfTraveling(),
+				1e-7);
 		Assert.assertEquals(
 				"wrong travelingPt_utils_hr "+msg,
 				initialGroup.getModes().get(TransportMode.pt).getMarginalUtilityOfTraveling(),
-				inputConfigGroup.getModes().get(TransportMode.pt).getMarginalUtilityOfTraveling());
+				inputConfigGroup.getModes().get(TransportMode.pt).getMarginalUtilityOfTraveling(),
+				1e-7);
 		Assert.assertEquals(
 				"wrong travelingWalk_utils_hr "+msg,
 				initialGroup.getModes().get(TransportMode.walk).getMarginalUtilityOfTraveling(),
-				inputConfigGroup.getModes().get(TransportMode.walk).getMarginalUtilityOfTraveling());
+				inputConfigGroup.getModes().get(TransportMode.walk).getMarginalUtilityOfTraveling(),
+				1e-7);
 		Assert.assertEquals(
 				"wrong utilityOfLineSwitch "+msg,
 				initialGroup.getUtilityOfLineSwitch(),
-				inputConfigGroup.getUtilityOfLineSwitch() );
+				inputConfigGroup.getUtilityOfLineSwitch(),
+				1e-7 );
 
 		for ( ActivityParams initialSettings : initialGroup.getActivityParams() ) {
 			final ActivityParams inputSettings =
@@ -182,31 +205,38 @@ public class PlanCalcScoreConfigGroupTest {
 			Assert.assertEquals(
 					"wrong closingTime "+msg,
 					initialSettings.getClosingTime(),
-					inputSettings.getClosingTime() );
+					inputSettings.getClosingTime(),
+					1e-7 );
 			Assert.assertEquals(
 					"wrong earliestEndTime "+msg,
 					initialSettings.getEarliestEndTime(),
-					inputSettings.getEarliestEndTime() );
+					inputSettings.getEarliestEndTime(),
+					1e-7 );
 			Assert.assertEquals(
 					"wrong latestStartTime "+msg,
 					initialSettings.getLatestStartTime(),
-					inputSettings.getLatestStartTime() );
+					inputSettings.getLatestStartTime(),
+					1e-7 );
 			Assert.assertEquals(
 					"wrong minimalDuration "+msg,
 					initialSettings.getMinimalDuration(),
-					inputSettings.getMinimalDuration() );
+					inputSettings.getMinimalDuration(),
+					1e-7 );
 			Assert.assertEquals(
 					"wrong openingTime "+msg,
 					initialSettings.getOpeningTime(),
-					inputSettings.getOpeningTime() );
+					inputSettings.getOpeningTime(),
+					1e-7 );
 			Assert.assertEquals(
 					"wrong priority "+msg,
 					initialSettings.getPriority(),
-					inputSettings.getPriority() );
+					inputSettings.getPriority(),
+					1e-7 );
 			Assert.assertEquals(
 					"wrong typicalDuration "+msg,
 					initialSettings.getTypicalDuration(),
-					inputSettings.getTypicalDuration() );
+					inputSettings.getTypicalDuration(),
+					1e-7 );
 		}
 
 		for ( ModeParams initialSettings : initialGroup.getModes().values() ) {
@@ -215,19 +245,23 @@ public class PlanCalcScoreConfigGroupTest {
 			Assert.assertEquals(
 					"wrong constant "+msg,
 					initialSettings.getConstant(),
-					inputSettings.getConstant() );
+					inputSettings.getConstant(),
+					1e-7 );
 			Assert.assertEquals(
 					"wrong marginalUtilityOfDistance "+msg,
 					initialSettings.getMarginalUtilityOfDistance(),
-					inputSettings.getMarginalUtilityOfDistance() );
+					inputSettings.getMarginalUtilityOfDistance(),
+					1e-7 );
 			Assert.assertEquals(
 					"wrong marginalUtilityOfTraveling "+msg,
 					initialSettings.getMarginalUtilityOfTraveling(),
-					inputSettings.getMarginalUtilityOfTraveling() );
+					inputSettings.getMarginalUtilityOfTraveling(),
+					1e-7 );
 			Assert.assertEquals(
 					"wrong monetaryDistanceRate "+msg,
 					initialSettings.getMonetaryDistanceRate(),
-					inputSettings.getMonetaryDistanceRate() );
+					inputSettings.getMonetaryDistanceRate(),
+					1e-7 );
 		}
 
 
