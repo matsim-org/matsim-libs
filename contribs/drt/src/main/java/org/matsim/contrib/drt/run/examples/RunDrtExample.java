538fe928fd270f1286f3151cfd11181db49cf7bc
Michal Maciejewski <michal.mac@gmail.com>
2018-02-24 17:37:48 +0100

    add drt examples derived from the taxi scenario for Mielec

--- contribs/drt/src/main/java/org/matsim/contrib/drt/run/examples/RunDrtExample.java
+++ contribs/drt/src/main/java/org/matsim/contrib/drt/run/examples/RunDrtExample.java
@@ -31,12 +31,15 @@ import org.matsim.vis.otfvis.OTFVisConfigGroup;
  * @author jbischoff
  */
 public class RunDrtExample {
+	private static final String MIELEC_CONFIG_FILE = "mielec_2014_02/mielec_drt_config.xml";
+	private static final String COTTBUS_CONFIG_FILE = "drt_example/drtconfig.xml";
+
 	public static void run(Config config, boolean otfvis) {
 		DrtControlerCreator.createControler(config, otfvis).run();
 	}
 
 	public static void main(String[] args) {
-		Config config = ConfigUtils.loadConfig("drt_example/drtconfig.xml", new DrtConfigGroup(), new DvrpConfigGroup(),
+		Config config = ConfigUtils.loadConfig(COTTBUS_CONFIG_FILE, new DrtConfigGroup(), new DvrpConfigGroup(),
 				new OTFVisConfigGroup());
 		config.qsim().setTrafficDynamics(QSimConfigGroup.TrafficDynamics.kinematicWaves);
 		config.qsim().setSnapshotStyle(QSimConfigGroup.SnapshotStyle.kinematicWaves);
