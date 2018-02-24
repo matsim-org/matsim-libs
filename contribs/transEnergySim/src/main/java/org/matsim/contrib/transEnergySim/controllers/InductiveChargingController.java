845839050a87776a128fe7a8e7a83f72f4645428
Sebastian Hörl <hoerl.sebastian@gmail.com>
2018-02-24 01:16:14 +0100

    another fix

--- contribs/transEnergySim/src/main/java/org/matsim/contrib/transEnergySim/controllers/InductiveChargingController.java
+++ contribs/transEnergySim/src/main/java/org/matsim/contrib/transEnergySim/controllers/InductiveChargingController.java
@@ -29,6 +29,7 @@ import org.matsim.contrib.transEnergySim.vehicles.energyConsumption.EnergyConsum
 import org.matsim.core.config.Config;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.mobsim.framework.MobsimTimer;
 import org.matsim.core.scenario.ScenarioUtils;
 
 import java.util.HashMap;
@@ -70,6 +71,7 @@ public class InductiveChargingController {
 			@Override
 			public void install() {
 				addEventHandlerBinding().toInstance(handlerGroup);
+				bind(MobsimTimer.class);
 			}
 		});
 	}
