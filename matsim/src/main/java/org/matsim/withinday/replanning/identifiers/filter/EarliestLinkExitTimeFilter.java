4c2e2dd65c2a94ec48485652f29753484261fa16
Marcel Rieser <rieser@simunto.com>
2018-02-25 13:49:15 +0100

    add missing import

--- matsim/src/main/java/org/matsim/withinday/replanning/identifiers/filter/EarliestLinkExitTimeFilter.java
+++ matsim/src/main/java/org/matsim/withinday/replanning/identifiers/filter/EarliestLinkExitTimeFilter.java
@@ -25,6 +25,7 @@ import java.util.Set;
 
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.population.Person;
+import org.matsim.core.utils.misc.Time;
 import org.matsim.withinday.replanning.identifiers.interfaces.AgentFilter;
 import org.matsim.withinday.trafficmonitoring.EarliestLinkExitTimeProvider;
 
