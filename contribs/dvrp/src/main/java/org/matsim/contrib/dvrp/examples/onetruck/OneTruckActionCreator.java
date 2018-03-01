f6cf550f982d5e8905449c724c0851e95e723ac8
Michal Maciejewski <michal.mac@gmail.com>
2018-03-01 14:21:28 +0100

    add OneTruckExample

---
+++ contribs/dvrp/src/main/java/org/matsim/contrib/dvrp/examples/onetruck/OneTruckActionCreator.java
@@ -0,0 +1,59 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2012 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.contrib.dvrp.examples.onetruck;
+
+import org.matsim.contrib.dvrp.data.Vehicle;
+import org.matsim.contrib.dvrp.schedule.DriveTask;
+import org.matsim.contrib.dvrp.schedule.StayTask;
+import org.matsim.contrib.dvrp.schedule.Task;
+import org.matsim.contrib.dvrp.vrpagent.VrpActivity;
+import org.matsim.contrib.dvrp.vrpagent.VrpAgentLogic;
+import org.matsim.contrib.dvrp.vrpagent.VrpLegs;
+import org.matsim.contrib.dynagent.DynAction;
+import org.matsim.contrib.dynagent.DynAgent;
+import org.matsim.contrib.dynagent.StaticDynActivity;
+import org.matsim.core.mobsim.framework.MobsimTimer;
+import org.matsim.core.mobsim.qsim.QSim;
+
+import com.google.inject.Inject;
+
+/**
+ * @author michalm
+ */
+final class OneTruckActionCreator implements VrpAgentLogic.DynActionCreator {
+	private final MobsimTimer timer;
+
+	@Inject
+	public OneTruckActionCreator(QSim qSim) {
+		this.timer = qSim.getSimTimer();
+	}
+
+	@Override
+	public DynAction createAction(DynAgent dynAgent, Vehicle vehicle, double now) {
+		Task task = vehicle.getSchedule().getCurrentTask();
+		if (task instanceof DriveTask) {
+			return VrpLegs.createLegWithOfflineTracker(vehicle, timer);
+		} else if (task instanceof OneTruckServeTask) { // PICKUP or DROPOFF
+			return new StaticDynActivity(((OneTruckServeTask)task).isPickup() ? "pickup" : "dropoff", task.getEndTime());
+		} else { // WAIT
+			return new VrpActivity("OneTaxiStay", (StayTask)task);
+		}
+	}
+}
