5abad4f4de96ae7b640fe3dd42fb3b8d8d15eee1
Michal Maciejewski <michal.mac@gmail.com>
2018-03-27 14:36:11 +0200

    import EV code from vsp-playgrounds/michalm, configure as mvn project

---
+++ main/java/org/matsim/contrib/ev/data/ElectricVehicle.java
@@ -0,0 +1,34 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2013 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.contrib.ev.data;
+
+import org.matsim.api.core.v01.Identifiable;
+import org.matsim.contrib.ev.discharging.*;
+import org.matsim.vehicles.Vehicle;
+
+public interface ElectricVehicle extends Identifiable<Vehicle> {
+	DriveEnergyConsumption getDriveEnergyConsumption();
+
+	AuxEnergyConsumption getAuxEnergyConsumption();
+
+	Battery getBattery();
+
+	Battery swapBattery(Battery battery);
+}
