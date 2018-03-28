a640d601470a6c7b96d302eedf8ae24a0ebd451f
Michal Maciejewski <michal.mac@gmail.com>
2018-03-28 12:12:08 +0200

    change package name org.matsim.contrib.* to org.matsim.vsp.*

---
+++ main/java/org/matsim/vsp/ev/IndividualSocTimeProfileCollectorProvider.java
@@ -0,0 +1,45 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2016 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.vsp.ev;
+
+import org.matsim.contrib.taxi.util.stats.TimeProfileCollector;
+import org.matsim.contrib.taxi.util.stats.TimeProfileCollector.ProfileCalculator;
+import org.matsim.core.controler.MatsimServices;
+import org.matsim.core.mobsim.framework.listeners.MobsimListener;
+import org.matsim.vsp.ev.data.EvData;
+
+import com.google.inject.*;
+
+public class IndividualSocTimeProfileCollectorProvider implements Provider<MobsimListener> {
+	private final EvData evData;
+	private final MatsimServices matsimServices;
+
+	@Inject
+	public IndividualSocTimeProfileCollectorProvider(EvData evData, MatsimServices matsimServices) {
+		this.evData = evData;
+		this.matsimServices = matsimServices;
+	}
+
+	@Override
+	public MobsimListener get() {
+		ProfileCalculator calc = EvTimeProfiles.createIndividualSocCalculator(evData);
+		return new TimeProfileCollector(calc, 300, "individual_soc_time_profiles", matsimServices);
+	}
+}
---
+++ main/java/org/matsim/vsp/ev/discharging/DriveEnergyConsumption.java
@@ -0,0 +1,30 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2015 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.vsp.ev.discharging;
+
+import org.matsim.api.core.v01.network.Link;
+
+/**
+ * Net energy taken from the battery to propel the vehicle, i.e. consumed by the motor minus effectively regenerated
+ * (returned back to the battery).
+ */
+public interface DriveEnergyConsumption {
+	double calcEnergyConsumption(Link link, double travelTime);
+}
