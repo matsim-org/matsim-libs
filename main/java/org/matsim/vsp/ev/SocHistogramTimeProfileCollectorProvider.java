378fa96015deb5af1a0905b2910dae4fcc977983
Michal Maciejewski <michal.mac@gmail.com>
2018-03-30 13:57:02 +0200

    replace TimeProfileCharts.Customizer with BiConsumer

--- main/java/org/matsim/vsp/ev/SocHistogramTimeProfileCollectorProvider.java
+++ main/java/org/matsim/vsp/ev/SocHistogramTimeProfileCollectorProvider.java
@@ -22,10 +22,8 @@ package org.matsim.vsp.ev;
 import java.awt.Color;
 import java.awt.Paint;
 
-import org.jfree.chart.JFreeChart;
 import org.matsim.contrib.util.timeprofile.TimeProfileCharts;
 import org.matsim.contrib.util.timeprofile.TimeProfileCharts.ChartType;
-import org.matsim.contrib.util.timeprofile.TimeProfileCharts.Customizer;
 import org.matsim.contrib.util.timeprofile.TimeProfileCollector;
 import org.matsim.contrib.util.timeprofile.TimeProfileCollector.ProfileCalculator;
 import org.matsim.core.controler.MatsimServices;
@@ -51,16 +49,13 @@ public class SocHistogramTimeProfileCollectorProvider implements Provider<Mobsim
 		TimeProfileCollector collector = new TimeProfileCollector(calc, 300, "soc_histogram_time_profiles",
 				matsimServices);
 
-		collector.setChartCustomizer(new Customizer() {
-			public void customize(JFreeChart chart, ChartType chartType) {
-				Paint[] paints = new Paint[10];
-				for (int i = 0; i < 10; i++) {
-					float f = (float)Math.sin(Math.PI * (9f - i) / 9 / 2);
-					paints[i] = new Color(f, (float)Math.sqrt(1 - f * f), 0f);
-				}
-
-				TimeProfileCharts.changeSeriesColors(chart, paints);
+		collector.setChartCustomizer((chart, chartType) -> {
+			Paint[] paints = new Paint[10];
+			for (int i = 0; i < 10; i++) {
+				float f = (float)Math.sin(Math.PI * (9f - i) / 9 / 2);
+				paints[i] = new Color(f, (float)Math.sqrt(1 - f * f), 0f);
 			}
+			TimeProfileCharts.changeSeriesColors(chart, paints);
 		});
 
 		collector.setChartTypes(ChartType.StackedArea);
