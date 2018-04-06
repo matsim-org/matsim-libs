3cd5e798caf9dffd32f66d0f9a6b66cb3945e6db
Michal Maciejewski <michal.mac@gmail.com>
2018-04-06 13:42:38 +0200

    add basic functionality for tracking energy consumption of ETasks

---
+++ main/java/org/matsim/vsp/ev/dvrp/tracker/OfflineETaskTracker.java
@@ -0,0 +1,48 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2018 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.vsp.ev.dvrp.tracker;
+
+import org.matsim.core.mobsim.framework.MobsimTimer;
+import org.matsim.vsp.ev.dvrp.ETask;
+import org.matsim.vsp.ev.dvrp.EvDvrpVehicle;
+
+/**
+ * @author michalm
+ */
+public class OfflineETaskTracker implements ETaskTracker {
+	private final MobsimTimer timer;
+	private final ETask task;
+	private final double socAtStart;
+
+	public OfflineETaskTracker(EvDvrpVehicle vehicle, MobsimTimer timer) {
+		this.timer = timer;
+		task = (ETask)vehicle.getSchedule().getCurrentTask();
+		socAtStart = vehicle.getElectricVehicle().getBattery().getSoc();
+	}
+
+	@Override
+	public double predictEndTime() {
+		return Math.max(task.getEndTime(), timer.getTimeOfDay());
+	}
+
+	@Override
+	public double predictSocAtEnd() {
+		return socAtStart - task.getTotalEnergy();
+	}
+}
