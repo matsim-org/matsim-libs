4b5d01a6697cef6e941a02335ede06e22e064f28
tthunig <thunig@vsp.tu-berlin.de>
2018-04-11 14:31:01 +0200

    imports

--- contribs/signals/src/main/java/org/matsim/contrib/signals/run/RunSignalSystemsExample.java
+++ contribs/signals/src/main/java/org/matsim/contrib/signals/run/RunSignalSystemsExample.java
@@ -27,7 +27,6 @@ import org.matsim.contrib.signals.data.SignalsDataLoader;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
-import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
 import org.matsim.core.scenario.ScenarioUtils;
 
 /**
