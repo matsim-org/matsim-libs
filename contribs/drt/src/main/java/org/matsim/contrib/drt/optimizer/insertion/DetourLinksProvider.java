80a1ebd66a48620b2ecce111bf41d5eb4bb46db0
Michal Maciejewski <michal.mac@gmail.com>
2018-05-18 22:53:26 +0200

    clean up DetourLinksProvider

--- contribs/drt/src/main/java/org/matsim/contrib/drt/optimizer/insertion/DetourLinksProvider.java
+++ contribs/drt/src/main/java/org/matsim/contrib/drt/optimizer/insertion/DetourLinksProvider.java
@@ -56,6 +56,11 @@ class DetourLinksProvider {
 	// used to prevent filtering out feasible insertions
 	private static final double OPTIMISTIC_BEELINE_SPEED_COEFF = 1.5;
 
+	// "insertion at end" means appending both pickup and dropoff at the end of the schedule, which means the ride
+	// is not shared (like a normal taxi). In this case, the best insertion-at-end is the one that is closest in time,
+	// so we just select the nearest (in straight-line) for the MultiNodeDijkstra (OneToManyPathSearch)
+	private static final int NEAREST_INSERTIONS_AT_END_LIMIT = 40;
+
 	private final InsertionGenerator insertionGenerator = new InsertionGenerator();
 	private final SingleVehicleInsertionFilter insertionFilter;
 
@@ -67,7 +72,8 @@ class DetourLinksProvider {
 	private final Map<Id<Link>, Link> linksFromDropoff;
 
 	// synchronised addition via addInsertionAtEndCandidate(InsertionAtEnd insertionAtEnd, double timeDistance)
-	private final PartialSort<InsertionAtEnd> nearestInsertionsAtEnd = new PartialSort<>(40);
+	private final PartialSort<InsertionAtEnd> nearestInsertionsAtEnd = new PartialSort<>(
+			NEAREST_INSERTIONS_AT_END_LIMIT);
 
 	private static class InsertionAtEnd {
 		private final Entry vEntry;
@@ -81,7 +87,7 @@ class DetourLinksProvider {
 
 	public DetourLinksProvider(DrtConfigGroup drtCfg, MobsimTimer timer, int vEntriesCount) {
 		filteredInsertionsPerVehicle = new ConcurrentHashMap<>(vEntriesCount);
-		linksToPickup = new ConcurrentHashMap<>(vEntriesCount / 2);
+		linksToPickup = new ConcurrentHashMap<>(NEAREST_INSERTIONS_AT_END_LIMIT);
 		linksFromPickup = new ConcurrentHashMap<>();
 		linksToDropoff = new ConcurrentHashMap<>();
 		linksFromDropoff = new ConcurrentHashMap<>();
@@ -149,32 +155,30 @@ class DetourLinksProvider {
 
 	private void addLinks(int i, int j, Entry vEntry, DrtRequest drtRequest) {
 		// i -> pickup
-		Link toPickupLink = (i == 0) ? vEntry.start.link : vEntry.stops.get(i - 1).task.getLink();
-		linksToPickup.putIfAbsent(toPickupLink.getId(), toPickupLink);
+		putLinkToMap(linksToPickup, (i == 0) ? vEntry.start.link : vEntry.stops.get(i - 1).task.getLink());
 
 		// XXX optimise: if pickup/dropoff is inserted at existing stop,
 		// no need to calc a path from pickup/dropoff to the next stop (the path is already in Schedule)
 
 		if (i == j) {
 			// pickup -> dropoff
-			Link fromPickupLink = drtRequest.getToLink();
-			linksFromPickup.putIfAbsent(fromPickupLink.getId(), fromPickupLink);
+			putLinkToMap(linksFromPickup, drtRequest.getToLink());
 		} else {
 			// pickup -> i + 1
-			Link fromPickupLink = vEntry.stops.get(i).task.getLink();
-			linksFromPickup.putIfAbsent(fromPickupLink.getId(), fromPickupLink);
+			putLinkToMap(linksFromPickup, vEntry.stops.get(i).task.getLink());
 
 			// j -> dropoff
-			Link toDropoffLink = vEntry.stops.get(j - 1).task.getLink();
-			linksToDropoff.putIfAbsent(toDropoffLink.getId(), toDropoffLink);
+			putLinkToMap(linksToDropoff, vEntry.stops.get(j - 1).task.getLink());
 		}
 
 		// dropoff -> j+1 // j+1 may not exist (dropoff appended after last stop)
 		if (j < vEntry.stops.size()) {
-			Link fromDropoffLink = vEntry.stops.get(j).task.getLink();
-			linksFromDropoff.putIfAbsent(fromDropoffLink.getId(), fromDropoffLink);
+			putLinkToMap(linksFromDropoff, vEntry.stops.get(j).task.getLink());
 		}
+	}
 
+	private void putLinkToMap(Map<Id<Link>, Link> map, Link link) {
+		map.putIfAbsent(link.getId(), link);
 	}
 
 	Map<Id<Vehicle>, List<Insertion>> getFilteredInsertionsPerVehicle() {
