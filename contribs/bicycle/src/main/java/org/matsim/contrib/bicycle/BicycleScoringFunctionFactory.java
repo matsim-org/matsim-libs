1e8443e688b818eb5496ec0888e3b42b8bf435b4
dziemke <ziemke@vsp.tu-berlin.de>
2018-06-14 21:26:12 +0200

    smaller revision

--- contribs/bicycle/src/main/java/org/matsim/contrib/bicycle/BicycleScoringFunctionFactory.java
+++ contribs/bicycle/src/main/java/org/matsim/contrib/bicycle/BicycleScoringFunctionFactory.java
@@ -24,11 +24,9 @@ import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.scoring.ScoringFunction;
 import org.matsim.core.scoring.ScoringFunctionFactory;
-//import org.matsim.core.scoring.ScoringFunctionsForPopulation;
 import org.matsim.core.scoring.SumScoringFunction;
 import org.matsim.core.scoring.functions.CharyparNagelActivityScoring;
 import org.matsim.core.scoring.functions.CharyparNagelAgentStuckScoring;
-import org.matsim.core.scoring.functions.CharyparNagelLegScoring;
 import org.matsim.core.scoring.functions.ScoringParameters;
 import org.matsim.core.scoring.functions.ScoringParametersForPerson;
 
@@ -39,8 +37,6 @@ import com.google.inject.Inject;
  */
 public class BicycleScoringFunctionFactory implements ScoringFunctionFactory {
 	@Inject ScoringParametersForPerson parameters;
-
-//	@Inject ScoringFunctionsForPopulation scoringFunctionsForPopulation;
 	
 	@Inject Scenario scenario;
 	@Inject BicycleTravelTime bicycleTravelTime;
@@ -48,32 +44,19 @@ public class BicycleScoringFunctionFactory implements ScoringFunctionFactory {
 	
 	@Inject EventsManager eventsManager;
 	
-	SumScoringFunction delegate; // Put here in preparation by Kai
-
 	@Override
 	public ScoringFunction createNewScoringFunction(Person person) {
-		// TODO Needs to be reorganized based on delegation
 		SumScoringFunction sumScoringFunction = new SumScoringFunction();
 
 		final ScoringParameters params = parameters.getScoringParameters(person);
-		// No leg scoring here as it would only double-count
-		// sumScoringFunction.addScoringFunction(new CharyparNagelLegScoring(params, scenario.getNetwork()));
 		sumScoringFunction.addScoringFunction(new CharyparNagelActivityScoring(params)) ;
 		sumScoringFunction.addScoringFunction(new CharyparNagelAgentStuckScoring(params));
 
-//		scoringFunctionsForPopulation.setPassLinkEventsToPerson(true);
-		// yyyyyy this is the ONLY place where we need to make ScoringFunctionsForPopulation public.  Somehow,
-		// this would imply to me that we should attach this switch to something else. kai, sep'17
-		// (now always switched on. kai, apr'18)
-		
-//		BicycleScoring bicycleScoring = new BicycleScoring(scenario, bicycleTravelTime, bicycleTravelDisutilityFactory);
-//		sumScoringFunction.addScoringFunction(bicycleScoring);
-//		
 //		CarCounter carCounter = new CarCounter(bicycleScoring);
 //		eventsManager.addHandler(carCounter);
 		
-//		sumScoringFunction.addScoringFunction(new CharyparNagelLegScoring(params, scenario.getNetwork()));
-		sumScoringFunction.addScoringFunction(new BicycleLegScoring(params, scenario.getNetwork(), (BicycleConfigGroup) scenario.getConfig().getModule("bicycle")));
+//		sumScoringFunction.addScoringFunction(new BicycleLegScoring(params, scenario.getNetwork(), (BicycleConfigGroup) scenario.getConfig().getModule("bicycle")));
+		sumScoringFunction.addScoringFunction(new BicycleLinkScoring(params, scenario, (BicycleConfigGroup) scenario.getConfig().getModule("bicycle")));
 
 		return sumScoringFunction;
 	}
