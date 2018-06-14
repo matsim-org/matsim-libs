dac284875afadd83d7d953d06afae5f241560280
dziemke <ziemke@vsp.tu-berlin.de>
2018-06-14 12:26:47 +0200

    clean up leg-based bicycle scoring, make it extension of CN leg scoring

--- contribs/bicycle/src/main/java/org/matsim/contrib/bicycle/BicycleLegScoring.java
+++ contribs/bicycle/src/main/java/org/matsim/contrib/bicycle/BicycleLegScoring.java
@@ -26,71 +26,39 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.TransportMode;
-import org.matsim.api.core.v01.events.ActivityEndEvent;
-import org.matsim.api.core.v01.events.Event;
-import org.matsim.api.core.v01.events.PersonDepartureEvent;
-import org.matsim.api.core.v01.events.PersonEntersVehicleEvent;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.Route;
 import org.matsim.core.gbl.Gbl;
 import org.matsim.core.population.routes.NetworkRoute;
+import org.matsim.core.scoring.functions.CharyparNagelLegScoring;
 import org.matsim.core.scoring.functions.ModeUtilityParameters;
 import org.matsim.core.scoring.functions.ScoringParameters;
-import org.matsim.core.utils.misc.Time;
-import org.matsim.pt.PtConstants;
 
 /**
- * This is a re-implementation of the original CharyparNagel function, based on a
- * modular approach.
- * @see <a href="http://www.matsim.org/node/263">http://www.matsim.org/node/263</a>
- * @author rashid_waraich
+ * @author dziemke
  */
-public class BicycleLegScoring implements org.matsim.core.scoring.SumScoringFunction.LegScoring, org.matsim.core.scoring.SumScoringFunction.ArbitraryEventScoring {
-	// yyyy URL in above javadoc is broken.  kai, feb'17
-	private static final Logger LOG = Logger.getLogger(BicycleLegScoring.class);
-
-
+public class BicycleLegScoring extends CharyparNagelLegScoring {
 	protected double score;
 
 	/** The parameters used for scoring */
 	protected final ScoringParameters params;
 	protected Network network;
-	private boolean nextEnterVehicleIsFirstOfTrip = true ;
-	private boolean nextStartPtLegIsFirstOfTrip = true ;
-	private boolean currentLegIsPtLeg = false;
-	private double lastActivityEndTime = Time.UNDEFINED_TIME ;
-	
-	//
+
 	private final double marginalUtilityOfInfrastructure_m;
 	private final double marginalUtilityOfComfort_m;
 	private final double marginalUtilityOfGradient_m_100m;
-	//
-	
-//	public BicycleLegScoring(final ScoringParameters params, Network network) {
+
 	public BicycleLegScoring(final ScoringParameters params, Network network, BicycleConfigGroup bicycleConfigGroup) {
+		super(params, network);
+		
 		this.params = params;
 		this.network = network;
-		this.nextEnterVehicleIsFirstOfTrip = true ;
-		this.nextStartPtLegIsFirstOfTrip = true ;
-		this.currentLegIsPtLeg = false;
-		
-		//
+
 		this.marginalUtilityOfInfrastructure_m = bicycleConfigGroup.getMarginalUtilityOfInfrastructure_m();
 		this.marginalUtilityOfComfort_m = bicycleConfigGroup.getMarginalUtilityOfComfort_m();
 		this.marginalUtilityOfGradient_m_100m = bicycleConfigGroup.getMarginalUtilityOfGradient_m_100m();
-		//
-	}
-
-	@Override
-	public void finish() {
-
-	}
-
-	@Override
-	public double getScore() {
-		return this.score;
 	}
 
 	private static int ccc=0 ;
@@ -171,47 +139,5 @@ public class BicycleLegScoring implements org.matsim.core.scoring.SumScoringFunc
 		double gradientFactor = BicycleUtilityUtils.getGradientFactor(link);
 		double gradientDisutility = marginalUtilityOfGradient_m_100m * gradientFactor * distance;
 		return (infrastructureDisutility + comfortDisutility + gradientDisutility);
-	}
-
-	
-	@Override
-	public void handleEvent(Event event) {
-		if ( event instanceof ActivityEndEvent ) {
-			// When there is a "real" activity, flags are reset:
-			if ( !PtConstants.TRANSIT_ACTIVITY_TYPE.equals( ((ActivityEndEvent)event).getActType()) ) {
-				this.nextEnterVehicleIsFirstOfTrip  = true ;
-				this.nextStartPtLegIsFirstOfTrip = true ;
-			}
-			this.lastActivityEndTime = event.getTime() ;
-		}
-
-		if ( event instanceof PersonEntersVehicleEvent && currentLegIsPtLeg ) {
-			if ( !this.nextEnterVehicleIsFirstOfTrip ) {
-				// all vehicle entering after the first triggers the disutility of line switch:
-				this.score  += params.utilityOfLineSwitch ;
-			}
-			this.nextEnterVehicleIsFirstOfTrip = false ;
-			// add score of waiting, _minus_ score of travelling (since it is added in the legscoring above):
-			this.score += (event.getTime() - this.lastActivityEndTime) * (this.params.marginalUtilityOfWaitingPt_s - this.params.modeParams.get(TransportMode.pt).marginalUtilityOfTraveling_s) ;
-		}
-
-		if ( event instanceof PersonDepartureEvent ) {
-			this.currentLegIsPtLeg = TransportMode.pt.equals( ((PersonDepartureEvent)event).getLegMode() );
-			if ( currentLegIsPtLeg ) {
-				if ( !this.nextStartPtLegIsFirstOfTrip ) {
-					this.score -= params.modeParams.get(TransportMode.pt).constant ;
-					// (yyyy deducting this again, since is it wrongly added above.  should be consolidated; this is so the code
-					// modification is minimally invasive.  kai, dec'12)
-				}
-				this.nextStartPtLegIsFirstOfTrip = false ;
-			}
-		}
-	}
-
-	
-	@Override
-	public void handleLeg(Leg leg) {
-		double legScore = calcLegScore(leg.getDepartureTime(), leg.getDepartureTime() + leg.getTravelTime(), leg);
-		this.score += legScore;
 	}
 }
