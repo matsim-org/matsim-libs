4a02f14810f53267894121fa27ae4a7e501f72b6
michalmac <michal.mac@gmail.com>
2018-06-22 14:49:24 +0200

    add VrpPath calculation to DrtRoutingModule
    in order to calculate unshared ride distance/time and maxTravelTime

--- contribs/drt/src/main/java/org/matsim/contrib/drt/routing/DrtRoutingModule.java
+++ contribs/drt/src/main/java/org/matsim/contrib/drt/routing/DrtRoutingModule.java
@@ -21,65 +21,97 @@ package org.matsim.contrib.drt.routing;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
-import org.matsim.api.core.v01.Coord;
+import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.PlanElement;
-import org.matsim.api.core.v01.population.Route;
+import org.matsim.api.core.v01.population.PopulationFactory;
+import org.matsim.contrib.drt.optimizer.DefaultDrtOptimizer;
 import org.matsim.contrib.drt.run.DrtConfigGroup;
-import org.matsim.core.config.Config;
-import org.matsim.core.population.PopulationUtils;
-import org.matsim.core.population.routes.RouteUtils;
+import org.matsim.contrib.dvrp.path.VrpPathWithTravelData;
+import org.matsim.contrib.dvrp.path.VrpPaths;
+import org.matsim.contrib.dvrp.router.DvrpRoutingNetworkProvider;
+import org.matsim.contrib.dvrp.trafficmonitoring.DvrpTravelTimeModule;
+import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.router.EmptyStageActivityTypes;
+import org.matsim.core.router.FastAStarEuclideanFactory;
 import org.matsim.core.router.RoutingModule;
 import org.matsim.core.router.StageActivityTypes;
-import org.matsim.core.utils.geometry.CoordUtils;
+import org.matsim.core.router.costcalculators.TravelDisutilityFactory;
+import org.matsim.core.router.util.LeastCostPathCalculator;
+import org.matsim.core.router.util.TravelTime;
 import org.matsim.facilities.Facility;
 
 import com.google.inject.Inject;
+import com.google.inject.name.Named;
 
 public class DrtRoutingModule implements RoutingModule {
-	private final DrtConfigGroup drtconfig;
+	private static final Logger LOGGER = Logger.getLogger(DrtRoutingModule.class);
+
+	private final DrtConfigGroup drtCfg;
 	private final Network network;
+	private final TravelTime travelTime;
+	private final LeastCostPathCalculator router;
+	private final PopulationFactory populationFactory;
+	private final RoutingModule walkRouter;
 
 	@Inject
-	public DrtRoutingModule(Config config, Network network) {
-		this.drtconfig = DrtConfigGroup.get(config);
+	public DrtRoutingModule(DrtConfigGroup drtCfg, @Named(DvrpRoutingNetworkProvider.DVRP_ROUTING) Network network,
+			@Named(DvrpTravelTimeModule.DVRP_ESTIMATED) TravelTime travelTime,
+			@Named(DefaultDrtOptimizer.DRT_OPTIMIZER) TravelDisutilityFactory travelDisutilityFactory,
+			PopulationFactory populationFactory, @Named(TransportMode.walk) RoutingModule walkRouter) {
+		this.drtCfg = drtCfg;
 		this.network = network;
+		this.travelTime = travelTime;
+		this.populationFactory = populationFactory;
+		this.walkRouter = walkRouter;
+
+		// Euclidean with overdoFactor > 1.0 could lead to 'experiencedTT < unsharedRideTT',
+		// while the benefit would be a marginal reduction of computation time ==> so stick to 1.0
+		router = new FastAStarEuclideanFactory()
+				.createPathCalculator(network, travelDisutilityFactory.createTravelDisutility(travelTime), travelTime);
 	}
 
 	@Override
 	public List<? extends PlanElement> calcRoute(Facility<?> fromFacility, Facility<?> toFacility, double departureTime,
 			Person person) {
-		Route route = RouteUtils.createGenericRouteImpl(fromFacility.getLinkId(), toFacility.getLinkId());
-		Coord fromCoord = fromFacility.getCoord();
-		Coord toCoord = toFacility.getCoord();
-
-		if (fromCoord == null) {
-			fromCoord = network.getLinks().get(fromFacility.getLinkId()).getCoord();
-		}
-		if (toCoord == null) {
-			toCoord = network.getLinks().get(toFacility.getLinkId()).getCoord();
-		}
-		double distanceEstimate = CoordUtils.calcEuclideanDistance(fromCoord, toCoord)
-				* drtconfig.getEstimatedBeelineDistanceFactor();
-		route.setDistance(distanceEstimate);
-		route.setTravelTime(distanceEstimate / drtconfig.getEstimatedDrtSpeed());
-
-		Leg leg = PopulationUtils.createLeg(TransportMode.drt);
-		leg.setDepartureTime(departureTime);
-		leg.setTravelTime(route.getTravelTime());
-		leg.setRoute(route);
-		if (fromFacility.getLinkId().equals(toFacility.getLinkId())) {
-			leg.setMode(TransportMode.walk);
+		Link fromLink = getLink(fromFacility);
+		Link toLink = getLink(toFacility);
+		if (toLink == fromLink) {
+			if (drtCfg.isPrintDetailedWarnings()) {
+				LOGGER.error("Start and end stop are the same, agent will walk. Agent Id:\t" + person.getId());
+			}
+			return walkRouter.calcRoute(fromFacility, toFacility, departureTime, person);
 		}
 
-		return Collections.singletonList(leg);
+		VrpPathWithTravelData unsharedPath = VrpPaths
+				.calcAndCreatePath(fromLink, toLink, departureTime, router, travelTime);
+		double unsharedRideTime = unsharedPath.getTravelTime();//includes first & last link
+		double maxTravelTime = drtCfg.getMaxTravelTimeAlpha() * unsharedRideTime + drtCfg.getMaxTravelTimeBeta();
+		double unsharedDistance = VrpPaths.calcDistance(unsharedPath);//includes last link
+
+		DrtRoute route = populationFactory.getRouteFactories()
+				.createRoute(DrtRoute.class, fromLink.getId(), toLink.getId());
+		route.setDistance(unsharedDistance);
+		route.setTravelTime(maxTravelTime);
+		route.setUnsharedRideTime(unsharedRideTime);
+
+		Leg drtLeg = populationFactory.createLeg(TransportMode.drt);
+		drtLeg.setDepartureTime(departureTime);
+		drtLeg.setTravelTime(maxTravelTime);
+		drtLeg.setRoute(route);
+		return Collections.singletonList(drtLeg);
 	}
 
+	private Link getLink(Facility<?> facility) {
+		Link link = network.getLinks().get(facility.getLinkId());
+		return link != null ? link : NetworkUtils.getNearestLink(network, facility.getCoord());
+	}
 
 	@Override
 	public StageActivityTypes getStageActivityTypes() {
