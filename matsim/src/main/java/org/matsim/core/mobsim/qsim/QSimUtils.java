aafa1043669e18067c69d276e41ae3893d0b6e9a
Sebastian Hörl <hoerl.sebastian@gmail.com>
2018-07-22 22:52:11 +0200

    fixing problem after merge

--- matsim/src/main/java/org/matsim/core/mobsim/qsim/QSimUtils.java
+++ matsim/src/main/java/org/matsim/core/mobsim/qsim/QSimUtils.java
@@ -77,24 +77,26 @@ public class QSimUtils {
 								   final List<AbstractModule> overrides, final Collection<AbstractQSimPlugin> plugins ) {
 		// First, load standard QSim module
 		AbstractModule module = new StandaloneQSimModule(scenario, eventsManager);
-
+	
 		// Add all overrides
 		for (AbstractModule override : overrides ) {
 			module = org.matsim.core.controler.AbstractModule.override(Collections.singleton(module), override) ;
 		}
 		
 		// Overide plugins
-		final AbstractModule override = AbstractModule.override(Collections.singleton(module),
-				new AbstractModule() {
-					@Override
-					public void install() {
-						bind(new TypeLiteral<Collection<AbstractQSimPlugin>>() {
-						}).toInstance(plugins);
-					}
-				});
+		if (plugins != null) {
+			module = AbstractModule.override(Collections.singleton(module),
+					new AbstractModule() {
+						@Override
+						public void install() {
+							bind(new TypeLiteral<Collection<AbstractQSimPlugin>>() {
+							}).toInstance(plugins);
+						}
+					});
+		}
 		
 		// Build QSim
-		Injector injector = org.matsim.core.controler.Injector.createInjector(scenario.getConfig(), override);
+		Injector injector = org.matsim.core.controler.Injector.createInjector(scenario.getConfig(), module);
 		return (QSim) injector.getInstance(Mobsim.class);
 	}
 
