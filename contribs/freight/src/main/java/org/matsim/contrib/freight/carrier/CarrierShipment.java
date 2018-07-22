9f162759dd676ee0f046565fcd2ddf59611196b7
kturner <martins-turner@vsp.tu-berlin.de>
2018-07-22 22:30:53 +0200

    hashCode() added to have same hashCode for same Id

--- contribs/freight/src/main/java/org/matsim/contrib/freight/carrier/CarrierShipment.java
+++ contribs/freight/src/main/java/org/matsim/contrib/freight/carrier/CarrierShipment.java
@@ -191,6 +191,17 @@ public final class CarrierShipment {
 	}
 
 	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	/* (non-Javadoc)
 	 * @see java.lang.Object#equals(java.lang.Object)
 	 */
 	@Override
