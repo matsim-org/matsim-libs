eee3ae4b64c7d1fc9f4959aa8b16483dde613efc
Michal Maciejewski <michal.mac@gmail.com>
2018-07-30 23:22:23 +0200

    reuse QSimModule.getDefaultQSimPlugins in DynQSimModule.provideQSimPlugins()

--- contribs/dvrp/src/main/java/org/matsim/contrib/dynagent/run/DynQSimModule.java
+++ contribs/dvrp/src/main/java/org/matsim/contrib/dynagent/run/DynQSimModule.java
@@ -19,51 +19,43 @@
 
 package org.matsim.contrib.dynagent.run;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 import org.matsim.core.config.Config;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.mobsim.qsim.AbstractQSimPlugin;
-import org.matsim.core.mobsim.qsim.PopulationPlugin;
-import org.matsim.core.mobsim.qsim.TeleportationPlugin;
-import org.matsim.core.mobsim.qsim.changeeventsengine.NetworkChangeEventsPlugin;
+import org.matsim.core.mobsim.qsim.ActivityEnginePlugin;
+import org.matsim.core.mobsim.qsim.QSimModule;
 import org.matsim.core.mobsim.qsim.components.QSimComponents;
 import org.matsim.core.mobsim.qsim.components.StandardQSimComponentsConfigurator;
-import org.matsim.core.mobsim.qsim.messagequeueengine.MessageQueuePlugin;
-import org.matsim.core.mobsim.qsim.pt.TransitEnginePlugin;
-import org.matsim.core.mobsim.qsim.qnetsimengine.QNetsimEnginePlugin;
 
 import com.google.inject.Provides;
 
 public class DynQSimModule extends AbstractModule {
 	@Override
 	public void install() {
-
 	}
 
 	@Provides
-	public Collection<AbstractQSimPlugin> provideQSimPlugins(Config config) {
-		final Collection<AbstractQSimPlugin> plugins = new ArrayList<>();
-		plugins.add(new MessageQueuePlugin(config));
-		plugins.add(new DynActivityEnginePlugin(config));
-		plugins.add(new QNetsimEnginePlugin(config));
-		if (config.network().isTimeVariantNetwork()) {
-			plugins.add(new NetworkChangeEventsPlugin(config));
-		}
-		if (config.transit().isUseTransit()) {
-			plugins.add(new TransitEnginePlugin(config));
-		}
-		plugins.add(new TeleportationPlugin(config));
-		plugins.add(new PopulationPlugin(config));
-		return plugins;
+	public Collection<AbstractQSimPlugin> provideDynQSimPlugins(Config config) {
+		return provideQSimPlugins(config);
+	}
+
+	public static Collection<AbstractQSimPlugin> provideQSimPlugins(Config config) {
+		//use the standard plugins, but replace ActivityEnginePlugin with DynActivityEnginePlugin
+		return QSimModule.getDefaultQSimPlugins(config)
+				.stream()
+				.map(p -> p instanceof ActivityEnginePlugin ? new DynActivityEnginePlugin(config) : p)
+				.collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
 	}
 
 	@Provides
 	private QSimComponents provideQSimComponents(Config config) {
 		QSimComponents components = new QSimComponents();
 		new StandardQSimComponentsConfigurator(config).configure(components);
-		new DynAgentQSimComponentsConfiurator().configure(components);
+		new DynAgentQSimComponentsConfigurator().configure(components);
 		return components;
 	}
 }
