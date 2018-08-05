2e11a7e6f2934b2023b742ec971689f05120f269
kainagel <nagel@vsp.tu-berlin.de>
2018-08-05 14:39:16 +0200

    move static methods in EditRoutes/Trips/Plans into WithinDayAgentUtils to reduce API

--- matsim/src/main/java/org/matsim/core/mobsim/qsim/agents/WithinDayAgentUtils.java
+++ matsim/src/main/java/org/matsim/core/mobsim/qsim/agents/WithinDayAgentUtils.java
@@ -22,6 +22,7 @@ package org.matsim.core.mobsim.qsim.agents;
 
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.Plan;
 import org.matsim.api.core.v01.population.PlanElement;
@@ -29,6 +30,8 @@ import org.matsim.core.mobsim.framework.Mobsim;
 import org.matsim.core.mobsim.framework.MobsimAgent;
 import org.matsim.core.mobsim.framework.PlanAgent;
 import org.matsim.core.mobsim.qsim.ActivityEndRescheduler;
+
+import java.util.List;
 
 /**
  * <p>
@@ -173,5 +176,111 @@ public class WithinDayAgentUtils {
 			return false;
 		}
 		return true ;
+	}
+	
+	public static Plan printPlan(MobsimAgent agent1) {
+		final Plan plan = getModifiablePlan(agent1);
+		return printPlan(plan) ;
+	}
+	
+	public static Plan printPlan(Plan plan) {
+		System.err.println( "plan=" + plan );
+		for ( int ii=0 ; ii<plan.getPlanElements().size() ; ii++ ) {
+			System.err.println( "\t" + ii + ":\t" + plan.getPlanElements().get(ii) );
+		}
+		return plan;
+	}
+	
+	/**
+	 * Only the PlanElements are changed - further Steps
+	 * like updating the Routes of the previous and next Leg
+	 * have to be done elsewhere.
+	 *
+	 * @author cdobler
+	 */
+	public static boolean replaceLegBlindly(Plan plan, Leg oldLeg, Leg newLeg) {
+
+		if (plan == null) return false;
+		if (oldLeg == null) return false;
+		if (newLeg == null) return false;
+
+		int index = plan.getPlanElements().indexOf(oldLeg);
+		// yyyy I can't say how safe this is.  There is no guarantee that the same entry is not used twice in the plan.  This will in
+		// particular be a problem if we override the "equals" contract, in the sense that two legs are equal if
+		// certain (or all) elements are equal.  kai, oct'10
+
+		if (index == -1) return false;
+
+		plan.getPlanElements().remove(index);
+		plan.getPlanElements().add(index,newLeg);
+
+		return true;
+	}
+	
+	/**
+	 * Only the PlanElements are changed - further Steps
+	 * like updating the Routes of the previous and next Leg
+	 * have to be done elsewhere.
+	 *
+	 * @author cdobler
+	 */
+	public static boolean replaceActivityBlindly( Plan plan, Activity oldActivity, Activity newActivity) {
+
+		if (plan == null) return false;
+		if (oldActivity == null) return false;
+		if (newActivity == null) return false;
+
+		int index = plan.getPlanElements().indexOf(oldActivity);
+		// yyyy I can't say how safe this is.  There is no guarantee that the same entry is not used twice in the plan.  This will in
+		// particular be a problem if we override the "equals" contract, in the sense that two activities are equal if
+		// certain (or all) elements are equal.  kai, oct'10
+
+		if (index == -1) return false;
+
+		//		/*
+		//		 *  If the new Activity takes place on a different Link
+		//		 *  we have to replan the Routes from an to that Activity.
+		//		 */
+		//		if (oldActivity.getLinkId() != newActivity.getLinkId())
+		//		{
+		//
+		//		}
+
+		plan.getPlanElements().remove(index);
+		plan.getPlanElements().add(index, newActivity);
+
+		return true;
+	}
+	
+	// === search methods: ===
+	public static int indexOfPlanElement(MobsimAgent agent, PlanElement pe) {
+		Plan plan = getModifiablePlan(agent) ;
+		List<PlanElement> planElements = plan.getPlanElements() ;
+
+		return planElements.indexOf(pe) ;
+	}
+	
+	public static int indexOfNextActivityWithType( MobsimAgent agent, String type ) {
+		Plan plan = getModifiablePlan(agent) ;
+		List<PlanElement> planElements = plan.getPlanElements() ;
+
+		for ( int index = getCurrentPlanElementIndex(agent) ; index < planElements.size() ; index++ ) {
+			PlanElement pe = planElements.get(index) ;
+			if ( pe instanceof Activity ) {
+				if ( ((Activity)pe).getType().equals(type) ) {
+					return index ;
+				}
+			}
+		}
+		return -1 ;
+	}
+	
+	public static Activity findNextActivityWithType( MobsimAgent agent, String type ) {
+		int index = indexOfNextActivityWithType( agent, type ) ;
+		return (Activity) getModifiablePlan(agent).getPlanElements().get(index) ;
+	}
+	
+	public static List<PlanElement> subList( MobsimAgent agent, int fromIndex, int toIndex) {
+		return getModifiablePlan(agent).getPlanElements().subList( fromIndex, toIndex ) ;
 	}
 }
