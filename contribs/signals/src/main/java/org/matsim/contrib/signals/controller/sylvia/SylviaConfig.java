0ebceb355ab64a50404c49ca1f6b91f195068d28
tthunig <thunig@vsp.tu-berlin.de>
2018-08-13 00:54:56 +0200

    make SylviaConfig extend ReflectiveConfigGroup

--- contribs/signals/src/main/java/org/matsim/contrib/signals/controller/sylvia/SylviaConfig.java
+++ contribs/signals/src/main/java/org/matsim/contrib/signals/controller/sylvia/SylviaConfig.java
@@ -19,74 +19,83 @@
  * *********************************************************************** */
 package org.matsim.contrib.signals.controller.sylvia;
 
+import java.util.Map;
+
+import org.matsim.core.config.ReflectiveConfigGroup;
 
 /**
- * @author dgrether
+ * @author dgrether, tthunig
  *
  */
-public final class SylviaConfig {
+public final class SylviaConfig extends ReflectiveConfigGroup {
+	
+	public static final String GROUP_NAME = "actuatedSylviaSignals";
+	
+	public SylviaConfig() {
+		super(GROUP_NAME);
+	}
+	
+	private static final String USE_FIXED_TIME_CYCLE_CMT = "limits the total amount of available extension time to the cycle time "
+			+ "of the corresponding fixed-time signal plan";
+	private static final String USE_FIXED_TIME_CYCLE = "useFixedTimeCycleAsMaximalExtension";
+	private boolean useFixedTimeCycleAsMaximalExtension = true;
+	
+	private static final String MAX_GREEN_SCALE_CMT = "The maximal green time for each signal group is determined by multiplying "
+			+ "this parameter with the green time of the signal group in the fixed-time signal plan that belongs to the sylvia signal plan. "
+			+ "The default scale is unbounded.";
+	private static final String MAX_GREEN_SCALE = "maxGreenScale";
+	private double signalGroupMaxGreenScale = Double.MAX_VALUE;
+	
+	@Override
+	public Map<String, String> getComments() {
+		Map<String, String> map = super.getComments();
+		
+		map.put(USE_FIXED_TIME_CYCLE, USE_FIXED_TIME_CYCLE_CMT);
+		map.put(MAX_GREEN_SCALE, MAX_GREEN_SCALE_CMT);
+		
+		return map;
+	}
 	
 	/**
-	 * limits the total amount of available extension time to the cycle time of the corresponding fixed-time signal plan
+	 * @param useFixedTimeCycleAsMaximalExtension -- {@value #USE_FIXED_TIME_CYCLE_CMT}
 	 */
-	private boolean useFixedTimeCycleAsMaximalExtension = true;
+	@StringSetter(USE_FIXED_TIME_CYCLE)
+	public void setUseFixedTimeCycleAsMaximalExtension(boolean useMaximalExtension) {
+		this.useFixedTimeCycleAsMaximalExtension = useMaximalExtension;
+	}
+	@StringGetter(USE_FIXED_TIME_CYCLE)
+	public boolean isUseFixedTimeCycleAsMaximalExtension() {
+		return useFixedTimeCycleAsMaximalExtension;
+	}
+
+	/**
+	 * @param maxGreenScale -- {@value #MAX_GREEN_SCALE_CMT}
+	 */
+	@StringSetter(MAX_GREEN_SCALE)
+	public void setSignalGroupMaxGreenScale(double maxGreenScale) {
+		this.signalGroupMaxGreenScale = maxGreenScale;
+	}
+	@StringGetter(MAX_GREEN_SCALE)
+	public double getSignalGroupMaxGreenScale() {
+		return signalGroupMaxGreenScale;
+	}
+	
 	/**
 	 * The distance from the downstream node where sensors are located on a link.
 	 */
 	private double sensorDistanceMeter= 10.0;
-	/**
-	 * The maximal green time for each signal group is determined by multiplying this parameter with the green
-	 * time of the signal group in the fixed-time signal plan that belongs to the sylvia signal plan.
-	 * The default scale is unbounded.
-	 */
-	private double signalGroupMaxGreenScale = Double.MAX_VALUE;
 	
 	/** extends the phase only if downstream links are empty. 
 	 * except forced extension points that are extended anyway. i.e. switch off useFixedTimeCycle... too to get meaningful results
 	 */
 	private boolean checkDownstream = false;
-
-	/**
-	 * currently not used
-	 */
-	private double gapSeconds = 5.0;
-	
-	public boolean isUseFixedTimeCycleAsMaximalExtension() {
-		return useFixedTimeCycleAsMaximalExtension;
-	}
-
-	public void setUseFixedTimeCycleAsMaximalExtension(boolean useMaximalExtension) {
-		this.useFixedTimeCycleAsMaximalExtension = useMaximalExtension;
-	}
-
 	
 	public double getSensorDistanceMeter() {
 		return sensorDistanceMeter;
 	}
-
 	
 	public void setSensorDistanceMeter(double sensorDistanceMeter) {
 		this.sensorDistanceMeter = sensorDistanceMeter;
-	}
-
-	
-	public double getGapSeconds() {
-		return gapSeconds;
-	}
-
-	
-	public void setGapSeconds(double gapSeconds) {
-		this.gapSeconds = gapSeconds;
-	}
-
-	
-	public double getSignalGroupMaxGreenScale() {
-		return signalGroupMaxGreenScale;
-	}
-
-	
-	public void setSignalGroupMaxGreenScale(double maxGreenScale) {
-		this.signalGroupMaxGreenScale = maxGreenScale;
 	}
 	
 	public void setCheckDownstream(boolean checkDownstream) {
