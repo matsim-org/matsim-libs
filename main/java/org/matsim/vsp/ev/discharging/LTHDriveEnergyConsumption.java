cbb929aac9bee366763c3160ac03fd7baba0ea5d
jfbischoff <bischoff@vsp.tu-berlin.de>
2018-08-24 11:41:00 +0200

    adding LTHDriveEnergyConsumption.java

---
+++ main/java/org/matsim/vsp/ev/discharging/LTHDriveEnergyConsumption.java
@@ -0,0 +1,105 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2016 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.vsp.ev.discharging;/*
+ * created by jbischoff, 23.08.2018
+ */
+
+import com.google.common.primitives.Doubles;
+import com.google.inject.OutOfScopeException;
+import org.apache.commons.math3.analysis.interpolation.PiecewiseBicubicSplineInterpolatingFunction;
+import org.apache.commons.math3.analysis.interpolation.PiecewiseBicubicSplineInterpolator;
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.vehicles.VehicleType;
+import org.matsim.vsp.ev.EvUnitConversions;
+
+
+public class LTHDriveEnergyConsumption implements DriveEnergyConsumption {
+
+    private PiecewiseBicubicSplineInterpolator splineInterpolater = new PiecewiseBicubicSplineInterpolator();
+    private PiecewiseBicubicSplineInterpolatingFunction function;
+
+    private final double minSpeed;
+    private final double maxSpeed;
+    private final double minSlope;
+    private final double maxSlope;
+
+    private final Id<VehicleType> vehicleTypeId;
+    private final boolean crashIfOutOfBoundValue;
+
+    public LTHDriveEnergyConsumption(double[] speeds, double[] slopes, double[][] consumptionPerSpeedAndSlope, Id<VehicleType> vehicleTypeId, boolean crashIfOutOfBoundValue) {
+        this.function = splineInterpolater.interpolate(speeds, slopes, consumptionPerSpeedAndSlope);
+        this.minSpeed = Doubles.min(speeds);
+        this.maxSpeed = Doubles.max(speeds);
+        this.minSlope = Doubles.min(slopes);
+        this.maxSlope = Doubles.max(slopes);
+        this.vehicleTypeId = vehicleTypeId;
+        this.crashIfOutOfBoundValue = crashIfOutOfBoundValue;
+    }
+
+    @Override
+    public double calcEnergyConsumption(Link link, double travelTime) {
+        double length = link.getLength();
+        double speed = length / travelTime;
+
+        if (speed > maxSpeed) {
+            if (crashIfOutOfBoundValue) {
+                throw new OutOfScopeException("Speed not covered" + speed);
+            } else {
+                Logger.getLogger(getClass()).warn("Assuming maxSpeed, as Speed not covered by consumption data" + speed);
+                speed = maxSpeed;
+            }
+        }
+
+
+        double[] linkslopes = (double[]) link.getAttributes().getAttribute("slopes");
+        if (linkslopes == null) {
+            linkslopes = new double[]{0.0};
+        }
+        double slopeSegmentTravelDistance = (link.getLength() / 1000.0) / (double) linkslopes.length;
+        double consumption = 0;
+        for (int i = 0; i < linkslopes.length; i++) {
+            double currentSlope = checkSlope(linkslopes[i]);
+            double currentEnergyuse = function.value(speed, currentSlope);
+            consumption += currentEnergyuse * slopeSegmentTravelDistance;
+        }
+        return consumption * EvUnitConversions.J_PER_kWh;
+    }
+
+    private double checkSlope(double currentSlope) {
+        if (currentSlope < minSlope) {
+            if (crashIfOutOfBoundValue) {
+                throw new OutOfScopeException("Slope not covered" + currentSlope);
+            } else {
+                Logger.getLogger(getClass()).warn("Assuming minSlope, as Slope not covered by consumption data" + currentSlope);
+                currentSlope = minSlope;
+            }
+        } else if (currentSlope > maxSlope) {
+            if (crashIfOutOfBoundValue) {
+                throw new OutOfScopeException("Slope not covered" + currentSlope);
+            } else {
+                Logger.getLogger(getClass()).warn("Assuming maxSlope, as Slope not covered by consumption data" + currentSlope);
+                currentSlope = maxSlope;
+            }
+        }
+        return currentSlope;
+    }
+}
---
+++ main/java/org/matsim/vsp/ev/routing/EVNetworkRoutingModule.java
@@ -0,0 +1,91 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2015 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package org.matsim.vsp.ev.routing;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.population.Leg;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.PlanElement;
+import org.matsim.core.gbl.Gbl;
+import org.matsim.core.router.EmptyStageActivityTypes;
+import org.matsim.core.router.NetworkRoutingModule;
+import org.matsim.core.router.RoutingModule;
+import org.matsim.core.router.StageActivityTypes;
+import org.matsim.core.router.util.LeastCostPathCalculator;
+import org.matsim.facilities.Facility;
+import org.matsim.vsp.ev.data.ElectricFleet;
+import org.matsim.vsp.ev.data.ElectricVehicle;
+
+import java.util.List;
+
+/**
+ * This network Routing module adds stages for re-charging into the Route.
+ * This wraps a "computer science" {@link LeastCostPathCalculator}, which routes from a node to another node, into something that
+ * routes from a {@link Facility} to another {@link Facility}, as we need in MATSim.
+ *
+ * @author jfbischoff
+ */
+
+public final class EVNetworkRoutingModule implements RoutingModule {
+
+    private final String mode;
+
+    private final Network network;
+    private final NetworkRoutingModule delegate;
+    private final ElectricFleet electricFleet;
+
+    public EVNetworkRoutingModule(
+            final String mode,
+            final Network network,
+            NetworkRoutingModule delegate,
+            ElectricFleet evs
+    ) {
+        Gbl.assertNotNull(network);
+        this.delegate = delegate;
+        this.network = network;
+        this.mode = mode;
+        this.electricFleet = evs;
+    }
+
+    @Override
+    public List<? extends PlanElement> calcRoute(final Facility<?> fromFacility, final Facility<?> toFacility, final double departureTime,
+                                                 final Person person) {
+        List<? extends PlanElement> basicRoute = delegate.calcRoute(fromFacility, toFacility, departureTime, person);
+        Id<ElectricVehicle> evId = Id.create(person.getId(), ElectricVehicle.class);
+        if (!electricFleet.getElectricVehicles().containsKey(evId)) {
+            return basicRoute;
+        } else {
+            Leg basicLeg = (Leg) basicRoute.get(0);
+            basicLeg.getRoute().getDistance();
+            return null;
+        }
+    }
+
+    @Override
+    public StageActivityTypes getStageActivityTypes() {
+        return EmptyStageActivityTypes.INSTANCE;
+    }
+
+    @Override
+    public String toString() {
+        return "[NetworkRoutingModule: mode=" + this.mode + "]";
+    }
+
+}
