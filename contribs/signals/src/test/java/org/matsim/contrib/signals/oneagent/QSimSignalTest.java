9141439eb3fb21b7f917c88e405c2330473c72a8
tthunig <thunig@vsp.tu-berlin.de>
2018-08-27 20:29:43 +0200

    use SignalsModule from the new location

--- contribs/signals/src/test/java/org/matsim/contrib/signals/oneagent/QSimSignalTest.java
+++ contribs/signals/src/test/java/org/matsim/contrib/signals/oneagent/QSimSignalTest.java
@@ -19,9 +19,9 @@
  * *********************************************************************** */
 package org.matsim.contrib.signals.oneagent;
 
-import java.util.Collections;
-
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Collections;
 
 import org.apache.log4j.Logger;
 import org.junit.Assert;
@@ -30,7 +30,7 @@ import org.junit.Test;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.events.LinkEnterEvent;
 import org.matsim.api.core.v01.events.handler.LinkEnterEventHandler;
-import org.matsim.contrib.signals.binder.SignalsModule;
+import org.matsim.contrib.signals.builder.SignalsModule;
 import org.matsim.contrib.signals.data.SignalsData;
 import org.matsim.contrib.signals.data.signalgroups.v20.SignalGroupSettingsData;
 import org.matsim.contrib.signals.data.signalgroups.v20.SignalPlanData;
