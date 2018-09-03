a15475719ca218daddadd07b997bcb54a50ff1c5
kturner <martins-turner@vsp.tu-berlin.de>
2018-09-03 23:13:26 +0200

    copy CarrierCapabilities; documentation; throw exception if selectedPlan is missing

--- contribs/freight/src/main/java/org/matsim/contrib/freight/utils/FreightUtils.java
+++ contribs/freight/src/main/java/org/matsim/contrib/freight/utils/FreightUtils.java
@@ -20,6 +20,7 @@ package org.matsim.contrib.freight.utils;
 
 import java.util.TreeMap;
 
+import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.contrib.freight.carrier.Carrier;
@@ -32,40 +33,73 @@ import org.matsim.contrib.freight.carrier.Tour.ServiceActivity;
 import org.matsim.contrib.freight.carrier.Tour.TourElement;
 
 public class FreightUtils {
+	
+	private static final Logger log = Logger.getLogger(FreightUtils.class);
 
+	/**
+	 * Creates a new Carries only with @link{CarrierShipment}s for creating a new VRP. 
+	 * As consequence of the transformation of @link{CarrierService}s to @link{CarrierShipment}s the solution of the VRP can have tours with
+	 * vehicles returning to the depot and load for another tour instead of creating another vehicle with additional (fix) costs. 
+	 * 
+	 * @param carriers	carriers with a Solution (result of solving the VRP).
+	 * @return Carriers carriersWithShipments
+	 */
 	public Carriers createShipmentVRPCarrierFromServiceVRPSolution(Carriers carriers) {
 		Carriers carriersWithShipments = new Carriers();
 		for (Carrier carrier : carriers.getCarriers().values()){
 			Carrier carrierWS = CarrierImpl.newInstance(carrier.getId());
 			copyShipments(carrierWS, carrier);
-			//			copyPickups(carrierWS, carrier);	//Not implemented yet, kmt Sep18
-			//			copyDeliveries(carrierWS, carrier); //Not implemented yet, kmt Sep18
+			//			copyPickups(carrierWS, carrier);	//Not implemented yet due to missing CarrierPickup in freight contrib, kmt Sep18
+			//			copyDeliveries(carrierWS, carrier); //Not implemented yet due to missing CarrierDelivery in freight contrib, kmt Sep18
 			createShipmentsFromServices(carrierWS, carrier); 
-			//TODO: Vehicles, Carriercapability
+			carrierWS.setCarrierCapabilities(carrier.getCarrierCapabilities()); //vehicles and other carrierCapabilites
 			carriersWithShipments.addCarrier(carrierWS);
 		}
-
-
 		return carriersWithShipments;
-
 	}
 
+	/**
+	 * NOT implemented yet due to missing CarrierDelivery in freight contrib, kmt Sep18
+	 * @param carrierWS
+	 * @param carrier
+	 */
 	private void copyDeliveries(Carrier carrierWS, Carrier carrier) {
-		// TODO Auto-generated method stub
+		log.warn("Coping of Deliveries is NOT implemented yet due to missing CarrierDelivery in freight contrib");
 	}
 
+	/**
+	 * NOT implemented yet due to missing CarrierPickup in freight contrib, kmt Sep18
+	 * @param carrierWS
+	 * @param carrier
+	 */
 	private void copyPickups(Carrier carrierWS, Carrier carrier) {
-		// TODO Auto-generated method stub
+		log.warn("Coping of Pickup is NOT implemented yet due to missing CarrierPickup in freight contrib");
 	}
 
+	/**
+	 * Copy all shipments from the existing carrier to the new carrier with shipments.
+	 * @param carrierWS		the "new" carrier with Shipments
+	 * @param carrier		the already existing
+	 */
 	private void copyShipments(Carrier carrierWS, Carrier carrier) {
 		for (CarrierShipment carrierShipment: carrier.getShipments()){
 			carrierWS.getShipments().add(carrierShipment);
 		}
 	}
 
+	/**
+	 * Transform all services from the existing carrier to the new carrier with shipments.
+	 * The location of the depot from which the "old" carrier starts the tour to the service is used as fromLocation for the new Shipment.
+	 * @param carrierWS		the "new" carrier with Shipments
+	 * @param carrier		the already existing
+	 */
 	private void createShipmentsFromServices(Carrier carrierWS, Carrier carrier) {
 		TreeMap<Id<CarrierService>, Id<Link>> depotServiceIsdeliveredFrom = new TreeMap<Id<CarrierService>, Id<Link>>();
+		try {
+			carrier.getSelectedPlan();
+		} catch (Exception e) {
+			throw new RuntimeException("Carrier " + carrier.getId() + " has NO selectedPlan. --> CanNOT create a new carrier from solution");
+		}
 		for (ScheduledTour tour : carrier.getSelectedPlan().getScheduledTours()) {
 			Id<Link> depotForTour = tour.getVehicle().getLocation();
 			for (TourElement te : tour.getTour().getTourElements()) {
@@ -74,7 +108,6 @@ public class FreightUtils {
 					depotServiceIsdeliveredFrom.put(act.getService().getId(), depotForTour);
 				}
 			}
-
 			for (CarrierService carrierService : carrier.getServices()) {
 				CarrierShipment carrierShipment = CarrierShipment.Builder.newInstance(Id.create(carrierService.getId().toString(), CarrierShipment.class), 
 						depotServiceIsdeliveredFrom.get(carrierService.getId()), 
@@ -84,7 +117,6 @@ public class FreightUtils {
 				carrierWS.getShipments().add(carrierShipment);
 			}
 		}
-
 	}
 
 }
