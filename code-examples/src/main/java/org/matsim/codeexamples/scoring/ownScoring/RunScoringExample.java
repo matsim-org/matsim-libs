03e41fd57fb1beef3a5db02013bd29bec9a9ee5f
kainagel <nagel@vsp.tu-berlin.de>
2018-09-06 15:47:50 +0200

    finish RunScoringExample

--- code-examples/src/main/java/org/matsim/codeexamples/scoring/ownScoring/RunScoringExample.java
+++ code-examples/src/main/java/org/matsim/codeexamples/scoring/ownScoring/RunScoringExample.java
@@ -1,13 +1,18 @@
 package org.matsim.codeexamples.scoring.ownScoring;
 
 import com.google.inject.Inject;
+import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.router.MainModeIdentifier;
+import org.matsim.core.router.StageActivityTypes;
+import org.matsim.core.router.TripRouter;
 import org.matsim.core.router.TripStructureUtils;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.scoring.ScoringFunction;
@@ -27,6 +32,7 @@ import java.net.URL;
 import static org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists;
 
 class RunScoringExample {
+	private static final Logger log = Logger.getLogger(RunScoringExample.class) ;
 	
 	public static void main( String [] args ) {
 		
@@ -62,12 +68,12 @@ class RunScoringExample {
 	private static class MyScoringFunctionFactory implements ScoringFunctionFactory {
 		@Inject private Network network;
 		@Inject private ScoringParametersForPerson pparams ;
-		
+		@Inject MainModeIdentifier mainModeIdentifier ;
 		@Override public ScoringFunction createNewScoringFunction( final Person person ) {
 			final ScoringParameters params = pparams.getScoringParameters( person );
 
 			SumScoringFunction ssf = new SumScoringFunction() ;
-			ssf.addScoringFunction(new CharyparNagelLegScoring(params, network ) ) ;
+//			ssf.addScoringFunction(new CharyparNagelLegScoring(params, network ) ) ;
 			ssf.addScoringFunction(new CharyparNagelActivityScoring( params ) );
 			ssf.addScoringFunction(new CharyparNagelMoneyScoring(params));
 			ssf.addScoringFunction(new CharyparNagelAgentStuckScoring(params));
@@ -76,12 +82,19 @@ class RunScoringExample {
 				private double score = 0. ;
 				@Override
 				public void handleTrip( final TripStructureUtils.Trip trip ) {
-					throw new RuntimeException( "not implemented" );
+					log.info("") ;
+					log.info( "trip=" + trip ) ;
+					log.info( "mainMode=" + mainModeIdentifier.identifyMainMode( trip.getTripElements() ) ) ;
+					log.info("") ;
+					
+					if ( TransportMode.pt.equals( mainModeIdentifier.identifyMainMode( trip.getTripElements() ) ) ) {
+						score -= 10. ;
+					}
 				}
 
 				@Override
 				public void finish() {
-					throw new RuntimeException( "not implemented" );
+				
 				}
 
 				@Override
