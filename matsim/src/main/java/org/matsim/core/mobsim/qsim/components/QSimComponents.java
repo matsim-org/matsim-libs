d26ca05c9474dba277365adccd86b834d5ae0d44
Michal Maciejewski <michal.mac@gmail.com>
2018-10-15 18:44:50 +0200

    use Set<Key<?>> for testing uniqueness of Components

--- matsim/src/main/java/org/matsim/core/mobsim/qsim/components/QSimComponents.java
+++ matsim/src/main/java/org/matsim/core/mobsim/qsim/components/QSimComponents.java
@@ -1,9 +1,13 @@
 package org.matsim.core.mobsim.qsim.components;
 
 import java.lang.annotation.Annotation;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
+import com.google.inject.Key;
 import com.google.inject.name.Names;
 
 /**
@@ -12,26 +16,23 @@ import com.google.inject.name.Names;
  */
 final public class QSimComponents {
 	private final List<Object> components = new LinkedList<>();
+	private final Set<Key<?>> keys = new HashSet<>();
 
 	public void addComponent(Class<? extends Annotation> annotation) {
-		if (components.contains(annotation)) {
-			throw new IllegalStateException("Annotation type " + annotation + " is already registered.");
-		}
-
+		addComponent(Key.get(Object.class, annotation));
 		components.add(annotation);
 	}
 
 	public void addComponent(Annotation annotation) {
-		if (components.contains(annotation)) {
-			throw new IllegalStateException("Annotation " + annotation + " is already registered.");
-		}
-
-		if (components.contains(annotation.getClass())) {
-			throw new IllegalStateException("Cannot register " + annotation + " because annotation type "
-					+ annotation.getClass() + " is registered already.");
-		}
-
+		addComponent(Key.get(Object.class, annotation));
 		components.add(annotation);
+	}
+
+	private void addComponent(Key<?> componentKey) {
+		if (keys.contains(componentKey)) {
+			throw new IllegalStateException(keyToString(componentKey) + " is already registered.");
+		}
+		keys.add(componentKey);
 	}
 
 	public void addNamedComponent(String name) {
@@ -39,22 +40,36 @@ final public class QSimComponents {
 	}
 
 	public void removeComponent(Class<? extends Annotation> annotation) {
+		addComponent(Key.get(Object.class, annotation));
 		components.remove(annotation);
 	}
 
 	public void removeComponent(Annotation annotation) {
+		removeComponent(Key.get(Object.class, annotation));
 		components.remove(annotation);
+	}
+
+	private void removeComponent(Key<?> componentKey) {
+		if (!keys.remove(componentKey)) {
+			throw new IllegalStateException(keyToString(componentKey) + " is not registered.");
+		}
 	}
 
 	public void removeNamedComponent(String name) {
 		removeComponent(Names.named(name));
 	}
 
+	private String keyToString(Key<?> componentKey) {
+		return "Annotation" + componentKey.getAnnotation() != null ?
+				" " + componentKey.getAnnotation() :
+				"Type " + componentKey.getAnnotationType();
+	}
+
 	public void clear() {
 		components.clear();
 	}
 
-	public List<Object> getActiveComponents() {
-		return components;
+	List<Object> getActiveComponents() {
+		return Collections.unmodifiableList(components);
 	}
 }
