8879ee4da18455b33e54da6ad35b28c727a0c1cb
Michal Maciejewski <michal.mac@gmail.com>
2018-10-16 11:15:09 +0200

    add annotation mix (binding @MockComponentAnnotation and component MockComponentAnnotation.class) to tests

--- matsim/src/test/java/org/matsim/core/mobsim/qsim/components/ComponentRegistryTest.java
+++ matsim/src/test/java/org/matsim/core/mobsim/qsim/components/ComponentRegistryTest.java
@@ -2,32 +2,25 @@ package org.matsim.core.mobsim.qsim.components;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.matsim.core.mobsim.qsim.InternalInterface;
+import org.matsim.core.mobsim.qsim.components.mock.MockComponentAnnotation;
 import org.matsim.core.mobsim.qsim.interfaces.MobsimEngine;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Key;
+import com.google.inject.Provides;
 import com.google.inject.name.Named;
 import com.google.inject.name.Names;
 
 public class ComponentRegistryTest {
 	@Test
 	public void testComponentFinder() {
-		Injector injector = Guice.createInjector(new AbstractModule() {
-			@Override
-			protected void configure() {
-				bind(Key.get(MobsimEngine.class, Names.named("A"))).toInstance(createFakeEngine());
-				bind(Key.get(MobsimEngine.class, Names.named("B"))).toInstance(createFakeEngine());
-				bind(Key.get(MobsimEngine.class, Named.class)).toInstance(createFakeEngine());
-				bind(MobsimEngine.class).toInstance(createFakeEngine());
-			}
-		});
+		Injector injector = Guice.createInjector(new ModuleWithProvides());
 
 		Assert.assertEquals(orderedComponents(injector, namedComponents("A")),
 				Arrays.asList(Key.get(MobsimEngine.class, Names.named("A"))));
@@ -51,9 +44,27 @@ public class ComponentRegistryTest {
 
 		QSimComponents qSimComponents = namedComponents("A", "B");
 		qSimComponents.addComponent(Named.class);
+		qSimComponents.addComponent(MockComponentAnnotation.class);
 		Assert.assertEquals(orderedComponents(injector, qSimComponents),
 				Arrays.asList(Key.get(MobsimEngine.class, Names.named("A")),
-						Key.get(MobsimEngine.class, Names.named("B")), Key.get(MobsimEngine.class, Named.class)));
+						Key.get(MobsimEngine.class, Names.named("B")), Key.get(MobsimEngine.class, Named.class),
+						Key.get(MobsimEngine.class, MockComponentAnnotation.class)));
+	}
+
+	private static class ModuleWithProvides extends AbstractModule {
+		@Override
+		protected void configure() {
+			bind(Key.get(MobsimEngine.class, Names.named("A"))).toInstance(createFakeEngine());
+			bind(Key.get(MobsimEngine.class, Names.named("B"))).toInstance(createFakeEngine());
+			bind(Key.get(MobsimEngine.class, Named.class)).toInstance(createFakeEngine());
+			bind(MobsimEngine.class).toInstance(createFakeEngine());
+		}
+
+		@Provides
+		@MockComponentAnnotation
+		MobsimEngine getMobsimEngine() {
+			return createFakeEngine();
+		}
 	}
 
 	private QSimComponents namedComponents(String... activeComponentNames) {
