39351c082c0baa5b0a9e78ae091dfb8bc844913b
Sebastian Hörl <hoerl.sebastian@gmail.com>
2018-10-24 13:23:13 +0200

    Add methods to check for QSim components

--- matsim/src/main/java/org/matsim/core/mobsim/qsim/components/QSimComponentsConfig.java
+++ matsim/src/main/java/org/matsim/core/mobsim/qsim/components/QSimComponentsConfig.java
@@ -58,6 +58,22 @@ final public class QSimComponentsConfig {
 	public void removeNamedComponent(String name) {
 		removeComponent(Names.named(name));
 	}
+	
+	public boolean hasComponent(Class<? extends Annotation> annotation) {
+		return hasComponent(Key.get(Object.class, annotation));
+	}
+
+	public boolean hasComponent(Annotation annotation) {
+		return hasComponent(Key.get(Object.class, annotation));
+	}
+
+	private boolean hasComponent(Key<?> componentKey) {
+		return keys.contains(componentKey);
+	}
+
+	public boolean hasNamedComponent(String name) {
+		return hasComponent(Names.named(name));
+	}
 
 	private String keyToString(Key<?> componentKey) {
 		return "Annotation" + componentKey.getAnnotation() != null ?
