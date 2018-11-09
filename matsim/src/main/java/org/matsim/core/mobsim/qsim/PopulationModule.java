86db3e73bd3e4e6f18263616ed0c3dc0f44c353c
kainagel <nagel@vsp.tu-berlin.de>
2018-11-09 19:34:40 +0100

    adapt component version of (Population)AgentSource

--- matsim/src/main/java/org/matsim/core/mobsim/qsim/PopulationModule.java
+++ matsim/src/main/java/org/matsim/core/mobsim/qsim/PopulationModule.java
@@ -18,7 +18,7 @@ public class PopulationModule extends AbstractQSimModule {
 	@Override
 	protected void configureQSim() {
 		bind( AgentSource.class).to(PopulationAgentSource.class).asEagerSingleton();
-		addNamedComponent(PopulationAgentSource.class, COMPONENT_NAME);
+		addNamedComponent(AgentSource.class, COMPONENT_NAME);
 	}
 
 	@Provides
