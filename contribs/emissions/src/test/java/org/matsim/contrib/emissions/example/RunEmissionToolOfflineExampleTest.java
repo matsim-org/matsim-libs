1412d77cc0e850a7abc1644f1ea6d43999816e20
ikaddoura <kaddoura@vsp.tu-berlin.de>
2018-11-22 18:27:27 +0100

    adding a test for the emission offline analysis

---
+++ contribs/emissions/src/test/java/org/matsim/contrib/emissions/example/RunEmissionToolOfflineExampleTest.java
@@ -0,0 +1,41 @@
+/* *********************************************************************** *
+ * project: org.matsim.*												   *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+package org.matsim.contrib.emissions.example;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.matsim.core.config.Config;
+import org.matsim.testcases.MatsimTestUtils;
+
+/**
+ * @author ihab
+ *
+ */
+public class RunEmissionToolOfflineExampleTest {
+	@Rule public MatsimTestUtils utils = new MatsimTestUtils() ;
+
+	@Test
+	public final void testMain() {
+		RunEmissionToolOfflineExampleV2 offlineExample = new RunEmissionToolOfflineExampleV2();
+		Config config = offlineExample.prepareConfig();
+		config.controler().setOutputDirectory(utils.getOutputDirectory());
+		offlineExample.run();
+	}
+
+}
