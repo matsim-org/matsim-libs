dc958118f6bbb8ec22f2d0990ceffcfa852a14eb
kainagel <nagel@vsp.tu-berlin.de>
2018-11-29 15:50:37 +0100

    run tests with the design proposal

--- matsim/src/main/java/org/matsim/core/mobsim/qsim/AbstractQSimModule.java
+++ matsim/src/main/java/org/matsim/core/mobsim/qsim/AbstractQSimModule.java
@@ -25,34 +25,41 @@ public abstract class AbstractQSimModule extends AbstractMobsimModule {
 	protected final void configureMobsim() {
 		configureQSim();
 	}
-	
-//	protected <T extends QSimComponent> AnnotatedBindingBuilder<T> bindComponent(Class<T> componentClass) {
-//		return binder().bind(componentClass);
-//	}
-//
-//	protected <T extends QSimComponent> LinkedBindingBuilder<T> bindComponent(Class<T> componentClass, Annotation annotation) {
-//		return bindComponent(componentClass).annotatedWith(annotation);
-//	}
-//
-//	protected <T extends QSimComponent> LinkedBindingBuilder<T> bindComponent(Class<T> componentClass, Class<? extends Annotation> annotationClass) {
-//		return bindComponent(componentClass).annotatedWith(annotationClass);
-//	}
-//
-//	protected <T extends QSimComponent> LinkedBindingBuilder<T> bindNamedComponent(Class<T> componentClass, String name) {
-//		return bindComponent(componentClass).annotatedWith(Names.named(name));
-//	}
-//
-//	protected <T extends QSimComponent> void addComponent(Class<T> componentClass, Annotation annotation) {
-//		bindComponent(componentClass, annotation).to(componentClass);
-//	}
-//
-//	protected <T extends QSimComponent> void addComponent(Class<T> componentClass, Class<? extends Annotation> annotationClass) {
-//		bindComponent(componentClass, annotationClass).to(componentClass);
-//	}
-//
-//	protected <T extends QSimComponent> void addNamedComponent(Class<T> componentClass, String name) {
-//		bindNamedComponent(componentClass, name).to(componentClass);
-//	}
+
+	@Deprecated // use addQSimComponentBinding
+	protected <T extends QSimComponent> AnnotatedBindingBuilder<T> bindComponent(Class<T> componentClass) {
+		return binder().bind(componentClass);
+	}
+
+	@Deprecated // use addQSimComponentBinding
+	protected <T extends QSimComponent> LinkedBindingBuilder<T> bindComponent(Class<T> componentClass, Annotation annotation) {
+		return bindComponent(componentClass).annotatedWith(annotation);
+	}
+
+	@Deprecated // use addQSimComponentBinding
+	protected <T extends QSimComponent> LinkedBindingBuilder<T> bindComponent(Class<T> componentClass, Class<? extends Annotation> annotationClass) {
+		return bindComponent(componentClass).annotatedWith(annotationClass);
+	}
+
+	@Deprecated // use addQSimComponentBinding
+	protected <T extends QSimComponent> LinkedBindingBuilder<T> bindNamedComponent(Class<T> componentClass, String name) {
+		return bindComponent(componentClass).annotatedWith(Names.named(name));
+	}
+
+	@Deprecated // use addQSimComponentBinding
+	protected <T extends QSimComponent> void addComponent(Class<T> componentClass, Annotation annotation) {
+		bindComponent(componentClass, annotation).to(componentClass);
+	}
+
+	@Deprecated // use addQSimComponentBinding
+	protected <T extends QSimComponent> void addComponent(Class<T> componentClass, Class<? extends Annotation> annotationClass) {
+		bindComponent(componentClass, annotationClass).to(componentClass);
+	}
+
+	@Deprecated // use addQSimComponentBinding
+	protected <T extends QSimComponent> void addNamedComponent(Class<T> componentClass, String name) {
+		bindNamedComponent(componentClass, name).to(componentClass);
+	}
 	// the above would need to be inlined to the below form before they could be removed
 
 	protected final com.google.inject.binder.LinkedBindingBuilder<QSimComponent> addQSimComponentBinding( String label ) {
