23d43824f9938c4ccf77f851fecf6b95af473497
matt_ti <matt_ti@VF-070183N.intra.dlr.de>
2018-12-11 15:34:33 +0100

    Make tests of LSP and DemandObject work again.

--- src/main/java/lsp/LSPImpl.java
+++ src/main/java/lsp/LSPImpl.java
@@ -198,8 +198,10 @@ public class LSPImpl implements LSP {
 	}
 
 	public void scoreSelectedPlan() {
-		double score = scorer.scoreCurrentPlan(this);
-		this.selectedPlan.setScore(score);
+		if(this.scorer != null) {
+			double score = scorer.scoreCurrentPlan(this);
+			this.selectedPlan.setScore(score);
+		}	
 	}
 	
 //	public LSPReplanner getReplanner() {
