ba5f0fbcd0862d4fdddff819d863f3d079b792dc
Gregor Leich <leich@vsp.tu-berlin.de>
2018-12-11 18:24:43 +0100

    add minFarePerTrip for TaxiFareModule, too

--- contribs/av/src/main/java/org/matsim/contrib/av/robotaxi/fares/taxi/TaxiFareConfigGroup.java
+++ contribs/av/src/main/java/org/matsim/contrib/av/robotaxi/fares/taxi/TaxiFareConfigGroup.java
@@ -24,6 +24,8 @@ package org.matsim.contrib.av.robotaxi.fares.taxi;
 
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ReflectiveConfigGroup;
+import org.matsim.core.config.ReflectiveConfigGroup.StringGetter;
+import org.matsim.core.config.ReflectiveConfigGroup.StringSetter;
 
 import java.util.Map;
 
@@ -37,12 +39,14 @@ public class TaxiFareConfigGroup extends ReflectiveConfigGroup {
 	public static final String GROUP_NAME = "taxifare";
 
 	public static final String BASEFARE = "basefare";
+    public static final String MINFARE_PER_TRIP = "minFarePerTrip";
 	public static final String DAILY_FEE = "dailySubscriptionFee";
 	public static final String TIMEFARE = "timeFare_h";
 	public static final String DISTANCEFARE = "distanceFare_m";
 	public static final String MODE = "mode";
 
 	private double basefare;
+    private double minFarePerTrip = 0;
 	private double dailySubscriptionFee;
 	private double timeFare_h;
 	private double distanceFare_m;
@@ -61,6 +65,7 @@ public class TaxiFareConfigGroup extends ReflectiveConfigGroup {
     {
         Map<String, String> map = super.getComments();
         map.put(BASEFARE, "Basefare per Trip (fare = positive value)");
+        map.put(MINFARE_PER_TRIP, "Minimum fare per trip (paid instead of the sum of base, time and distance fare if that sum would be lower than the minimum fare, fee = positive value).");
         map.put(DAILY_FEE, "Daily subscription fee (fee = positive value)");
         map.put(TIMEFARE , "taxi fare per hour (fee = positive value)");
         map.put(DISTANCEFARE, "taxi fare per meter (fee = positive value)");
@@ -78,6 +83,16 @@ public class TaxiFareConfigGroup extends ReflectiveConfigGroup {
 	public void setBasefare(double basefare) {
 		this.basefare = basefare;
 	}
+	
+    @StringGetter(MINFARE_PER_TRIP)
+    public double getMinFarePerTrip() {
+        return minFarePerTrip;
+    }
+
+    @StringSetter(MINFARE_PER_TRIP)
+    public void setMinFarePerTrip(double minFarePerTrip) {
+        this.minFarePerTrip = minFarePerTrip;
+    }
 
 	@StringGetter(DAILY_FEE)
 	public double getDailySubscriptionFee() {
