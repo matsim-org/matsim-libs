0ebfcf374a31dcf10ce77e88af1ce36fa675cd92
kainagel <nagel@vsp.tu-berlin.de>
2019-01-06 18:08:32 +0100

    repair RunAddAgentSourceExample

--- code-examples/src/main/java/org/matsim/codeexamples/mobsim/ownMobsimAgent/RunAddAgentSourceExample.java
+++ code-examples/src/main/java/org/matsim/codeexamples/mobsim/ownMobsimAgent/RunAddAgentSourceExample.java
@@ -21,10 +21,8 @@
 package org.matsim.codeexamples.mobsim.ownMobsimAgent;
 
 import com.google.inject.Inject;
-import com.google.inject.name.Names;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
-import org.matsim.api.core.v01.network.Link;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
@@ -32,11 +30,8 @@ import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
 import org.matsim.core.mobsim.framework.AgentSource;
 import org.matsim.core.mobsim.framework.MobsimAgent;
 import org.matsim.core.mobsim.qsim.AbstractQSimModule;
-import org.matsim.core.mobsim.qsim.InternalInterface;
 import org.matsim.core.mobsim.qsim.QSim;
-import org.matsim.core.mobsim.qsim.components.QSimComponent;
 import org.matsim.core.mobsim.qsim.components.QSimComponentsConfigGroup;
-import org.matsim.core.mobsim.qsim.interfaces.MobsimEngine;
 import org.matsim.core.mobsim.qsim.qnetsimengine.QVehicleImpl;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.vehicles.Vehicle;
@@ -55,7 +50,6 @@ public class RunAddAgentSourceExample{
 		config.controler().setOverwriteFileSetting( OverwriteFileSetting.deleteDirectoryIfExists );
 
 		Scenario scenario = ScenarioUtils.loadScenario( config );
-//		scenario.getPopulation().getPersons().clear();
 
 		final Controler controler = new Controler( scenario );
 
@@ -66,7 +60,7 @@ public class RunAddAgentSourceExample{
 			controler.addOverridingQSimModule( new AbstractQSimModule(){
 				@Override
 				protected void configureQSim(){
-					this.addNamedComponent( MyAgentSource.class, "newAgentSource" );
+					this.addQSimComponentBinding( "newAgentSource" ).to( MyAgentSource.class );
 				}
 			} );
 		}
@@ -85,10 +79,8 @@ public class RunAddAgentSourceExample{
 
 			// insert vehicle:
 			final Vehicle vehicle = VehicleUtils.getFactory().createVehicle( Id.create( ag.getId(), Vehicle.class ), VehicleUtils.getDefaultVehicleType() );
-			final Id<Link> linkId4VehicleInsertion = Id.createLinkId( 1 );
-			//								qsim.createAndParkVehicleOnLink(vehicle, linkId4VehicleInsertion);
 			QVehicleImpl qVeh = new QVehicleImpl( vehicle );
-			qsim.addParkedVehicle( qVeh, linkId4VehicleInsertion );
+			qsim.addParkedVehicle( qVeh, ag.getCurrentLinkId() );
 
 		}
 	}
