9add4b855566cdcf390e4564ff0eef1c5f920c4e
Michal Maciejewski <michal.mac@gmail.com>
2019-01-07 15:58:19 +0100

    make @Named(DVRP_VEHICLE_TYPE) VehicleType a non-optional binding

--- contribs/av/src/main/java/org/matsim/contrib/av/maxspeed/DvrpTravelTimeWithMaxSpeedLimitModule.java
+++ contribs/av/src/main/java/org/matsim/contrib/av/maxspeed/DvrpTravelTimeWithMaxSpeedLimitModule.java
@@ -21,18 +21,14 @@
 package org.matsim.contrib.av.maxspeed;
 
 import javax.inject.Inject;
-import javax.inject.Named;
 import javax.inject.Provider;
 
-import org.matsim.api.core.v01.TransportMode;
-import org.matsim.api.core.v01.network.Link;
-import org.matsim.api.core.v01.population.Person;
 import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
 import org.matsim.contrib.dvrp.trafficmonitoring.DvrpTravelTimeModule;
-import org.matsim.contrib.dvrp.vrpagent.VrpAgentSource;
+import org.matsim.contrib.dvrp.trafficmonitoring.TravelTimeUtils;
+import org.matsim.contrib.dvrp.vrpagent.VrpAgentSourceQSimModule;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.router.util.TravelTime;
-import org.matsim.vehicles.Vehicle;
 import org.matsim.vehicles.VehicleType;
 
 import com.google.inject.Injector;
@@ -54,7 +50,8 @@ public class DvrpTravelTimeWithMaxSpeedLimitModule extends AbstractModule {
 
 	@Override
 	public void install() {
-		bind(VehicleType.class).annotatedWith(Names.named(VrpAgentSource.DVRP_VEHICLE_TYPE)).toInstance(vehicleType);
+		bind(VehicleType.class).annotatedWith(Names.named(VrpAgentSourceQSimModule.DVRP_VEHICLE_TYPE))
+				.toInstance(vehicleType);
 		bind(QSimFreeSpeedTravelTimeWithMaxSpeedLimit.class).asEagerSingleton();
 		addTravelTimeBinding(DvrpTravelTimeModule.DVRP_INITIAL).to(QSimFreeSpeedTravelTimeWithMaxSpeedLimit.class);
 		String mobsimMode = dvrpCfg.getMobsimMode();
@@ -67,28 +64,10 @@ public class DvrpTravelTimeWithMaxSpeedLimitModule extends AbstractModule {
 
 					@Override
 					public TravelTime get() {
-						return new MaxTravelTime(qSimFreeSpeedTravelTimeWithMaxSpeedLimit,
+						return TravelTimeUtils.maxOfTravelTimes(qSimFreeSpeedTravelTimeWithMaxSpeedLimit,
 								injector.getInstance(Key.get(TravelTime.class, Names.named(mobsimMode))));
 					}
 				});
-	}
-
-	private static class MaxTravelTime implements TravelTime {
-		private final QSimFreeSpeedTravelTimeWithMaxSpeedLimit qSimFreeSpeedTravelTimeWithMaxSpeedLimit;
-		private final TravelTime observedCarTravelTime;
-
-		@Inject
-		public MaxTravelTime(QSimFreeSpeedTravelTimeWithMaxSpeedLimit qSimFreeSpeedTravelTimeWithMaxSpeedLimit,
-				@Named(TransportMode.car) TravelTime observedCarTravelTime) {
-			this.qSimFreeSpeedTravelTimeWithMaxSpeedLimit = qSimFreeSpeedTravelTimeWithMaxSpeedLimit;
-			this.observedCarTravelTime = observedCarTravelTime;
-		}
-
-		@Override
-		public double getLinkTravelTime(Link link, double time, Person person, Vehicle vehicle) {
-			return Math.max(qSimFreeSpeedTravelTimeWithMaxSpeedLimit.getLinkTravelTime(link, time, person, vehicle),
-					observedCarTravelTime.getLinkTravelTime(link, time, person, vehicle));
-		}
 	}
 }
 
