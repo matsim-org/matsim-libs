f667a3ab35336c9a7607302ab0327fad1db08d8f
Nico <nkuehnel2@gmail.com>
2019-01-28 16:56:20 +0100

    updates on noise shielding calculation

--- contribs/noise/src/main/java/org/matsim/contrib/noise/data/ShieldingContext.java
+++ contribs/noise/src/main/java/org/matsim/contrib/noise/data/ShieldingContext.java
@@ -1,14 +1,18 @@
 package org.matsim.contrib.noise.data;
 
-import com.vividsolutions.jts.geom.*;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.index.quadtree.Quadtree;
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.contrib.noise.handler.NoiseEquations;
-import org.matsim.core.utils.collections.QuadTree;
 import org.matsim.core.utils.geometry.GeometryUtils;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 /**
  * Separate from {@link NoiseContext} for better testability.
@@ -19,44 +23,22 @@ public class ShieldingContext {
 
     private final static Logger logger = Logger.getLogger(ShieldingContext.class);
 
-    private final QuadTree<NoiseBarrier> noiseBarriers;
+    private final Quadtree noiseBarriers;
     private final static double GROUND_HEIGHT = 0.5;
 
-    public ShieldingContext(Collection<? extends NoiseBarrier> noiseBarriers) {
-        double minX = Double.MAX_VALUE;
-        double minY = Double.MAX_VALUE;
-        double maxX = Double.MIN_VALUE;
-        double maxY = Double.MIN_VALUE;
+    ShieldingContext(Collection<? extends NoiseBarrier> noiseBarriers) {
 
-        for (NoiseBarrier barrier : noiseBarriers) {
-
-            final Envelope envelope = barrier.getGeometry().getEnvelopeInternal();
-
-            if (envelope.getMinX() < minX) {
-                minX = envelope.getMinX();
-            }
-            if (envelope.getMinY() < minY) {
-                minY = envelope.getMinY();
-            }
-            if (envelope.getMaxX() > maxX) {
-                maxX = envelope.getMaxX();
-            }
-            if (envelope.getMaxY() > maxY) {
-                maxY = envelope.getMaxY();
-            }
-        }
-        this.noiseBarriers = new QuadTree<>(minX - 100, minY - 100, maxX + 100, maxY + 100);
+        this.noiseBarriers = new Quadtree();
         for (NoiseBarrier barrier : noiseBarriers) {
             try {
-                this.noiseBarriers.put(barrier.getGeometry().getCentroid().getX(), barrier.getGeometry().getCentroid().getY(), barrier);
+                this.noiseBarriers.insert(barrier.getGeometry().getEnvelopeInternal(), barrier);
             } catch (IllegalArgumentException e) {
                 logger.warn("Could not add noise barrier " + barrier.getId() + " to quad tree. Ignoring it.");
             }
         }
     }
 
-
-    public double determineShieldingCorrection(ReceiverPoint receiverPoint, Link link, Coord projectedCoord) {
+    double determineShieldingCorrection(ReceiverPoint receiverPoint, Link link, Coord projectedCoord) {
         double correctionTermShielding = 0;
         final Point rpPoint = GeometryUtils.createGeotoolsPoint(receiverPoint.getCoord());
         final Point projectedPoint = GeometryUtils.createGeotoolsPoint(projectedCoord);
@@ -65,50 +47,48 @@ public class ShieldingContext {
         final Point fromPoint = GeometryUtils.createGeotoolsPoint(link.getFromNode().getCoord());
         final Point toPoint = GeometryUtils.createGeotoolsPoint(link.getToNode().getCoord());
 
-
         Geometry projectedLineOfSight = constructLineOfSight(rpPoint, projectedPoint);
         Geometry fromLineOfSight = constructLineOfSight(rpPoint, fromPoint);
         Geometry toLineOfSight = constructLineOfSight(rpPoint, toPoint);
-        final List<NoiseBarrier> obstructions = getObstructions(rpPoint, projectedPoint, projectedLineOfSight, fromLineOfSight, toLineOfSight);
-        if (!obstructions.isEmpty()) {
-            obstructions.sort(Comparator.comparingDouble(o -> rpPoint.distance(o.getGeometry())));
 
-            List<Coordinate> edgeCandidates = new ArrayList<>();
-            for (NoiseBarrier barrier : obstructions) {
-                Geometry intersection = projectedLineOfSight.intersection(barrier.getGeometry());
-                for (Coordinate coordinate : intersection.getCoordinates()) {
-                    coordinate.z = barrier.getHeight();
-                    edgeCandidates.add(coordinate);
-                }
-            }
-            edgeCandidates.add(projectedPoint.getCoordinate());
+        NavigableMap<Double, Coordinate> edgeCandidates = getObstructionEdges(rpPoint, projectedPoint, projectedLineOfSight, fromLineOfSight, toLineOfSight);
+        edgeCandidates.put(projectedLineOfSight.getLength(), projectedPoint.getCoordinate());
 
+        if(!edgeCandidates.isEmpty()) {
             Coordinate lastFixedEdge = rpPoint.getCoordinate();
             Coordinate tmpEdge = rpPoint.getCoordinate();
             double currentHeight = GROUND_HEIGHT;
-            double maxSlope = Double.NEGATIVE_INFINITY;
 
             List<Coordinate> consideredEdges = new ArrayList<>();
 
-            for (Coordinate edge : edgeCandidates) {
-                double slope = (edge.z - currentHeight) / lastFixedEdge.distance(edge);
-                if (slope > maxSlope) {
-                    maxSlope = slope;
-                    tmpEdge = edge;
-                } else {
-                    maxSlope = Double.NEGATIVE_INFINITY;
-                    lastFixedEdge = tmpEdge;
-                    currentHeight = tmpEdge.z;
-                    consideredEdges.add(lastFixedEdge);
-                    slope = (edge.z - currentHeight) / lastFixedEdge.distance(edge);
-                    if (slope > maxSlope) {
+            double distToCurrentEdge = 0;
+            while (lastFixedEdge != projectedPoint.getCoordinate()) {
+                Iterator<Coordinate> edgesIterator = edgeCandidates.values().iterator();
+                double maxSlope = Double.NEGATIVE_INFINITY;
+                double tmpDistance = 0;
+                while (edgesIterator.hasNext()) {
+                    Coordinate edge = edgesIterator.next();
+                    double distance = lastFixedEdge.distance(edge);
+                    double slope = (edge.z - currentHeight) / distance;
+                    if (slope >= maxSlope) {
                         maxSlope = slope;
                         tmpEdge = edge;
+                        tmpDistance = distance;
                     }
                 }
+                lastFixedEdge = tmpEdge;
+                distToCurrentEdge += tmpDistance;
+                currentHeight = tmpEdge.z;
+                consideredEdges.add(lastFixedEdge);
+                edgeCandidates = edgeCandidates.tailMap(distToCurrentEdge, false);
             }
 
-            consideredEdges.remove(projectedPoint);
+            consideredEdges.remove(projectedPoint.getCoordinate());
+
+            if(consideredEdges.isEmpty()) {
+                logger.warn("No edge candiates found. Skippig obstacle");
+                return correctionTermShielding;
+            }
 
             final double firstEdgeYDiff = GROUND_HEIGHT - consideredEdges.get(0).z;
             double firstEdgeDistance = rpPoint.getCoordinate().distance(consideredEdges.get(0));
@@ -116,11 +96,10 @@ public class ShieldingContext {
                     = Math.sqrt(firstEdgeYDiff * firstEdgeYDiff + firstEdgeDistance * firstEdgeDistance);
 
             double shieldingDepth = 0;
+
             Iterator<Coordinate> it = consideredEdges.iterator();
             Coordinate edgeTemp = it.next();
-            double heightTemp = edgeTemp.z;
-
-            for (; it.hasNext(); ) {
+            while(it.hasNext()) {
                 Coordinate edge = it.next();
                 double xyDiff = edgeTemp.distance(edge);
                 double zDiff = edgeTemp.z - edge.z;
@@ -139,23 +118,28 @@ public class ShieldingContext {
         return correctionTermShielding;
     }
 
-    private List<NoiseBarrier> getObstructions(Point receiver, Point source, Geometry directLineOfSight,
-                                               Geometry fromLineOfSight, Geometry toLineOfSight) {
+    private ConcurrentSkipListMap<Double, Coordinate> getObstructionEdges(Point receiver, Point source, Geometry directLineOfSight,
+                                                                          Geometry fromLineOfSight, Geometry toLineOfSight) {
         final Collection<NoiseBarrier> candidates =
-                noiseBarriers.getDisk(receiver.getX(), receiver.getY(), Math.max(100, directLineOfSight.getLength() *3));
-        final List<NoiseBarrier> obstructing = new ArrayList<>();
-        for (NoiseBarrier barrier : candidates) {
-            if (isObstructing(receiver, source, directLineOfSight, fromLineOfSight, toLineOfSight, barrier.getGeometry())) {
-                obstructing.add(barrier);
+                noiseBarriers.query(directLineOfSight.getEnvelopeInternal());
+        ConcurrentSkipListMap<Double, Coordinate> edgeCandidates = new ConcurrentSkipListMap<>();
+        candidates.stream().forEach(noiseBarrier -> {
+            if (isObstructing(receiver, source, directLineOfSight, fromLineOfSight, toLineOfSight, noiseBarrier.getGeometry())) {
+                Geometry intersection = directLineOfSight.intersection(noiseBarrier.getGeometry());
+                for (Coordinate coordinate : intersection.getCoordinates()) {
+                    coordinate.z = noiseBarrier.getHeight();
+                    final double distance = receiver.getCoordinate().distance(coordinate);
+                    edgeCandidates.put(distance, coordinate);
+                }
             }
-        }
-        return obstructing;
+        });
+        return edgeCandidates;
     }
 
     private boolean isObstructing(Geometry receiver, Geometry source, Geometry lineOfSight,
                                   Geometry fromLineOfSight, Geometry toLineOfSight, Geometry barrier) {
         return !barrier.contains(receiver)
-                && !barrier.intersects(source)
+                && !barrier.contains(source)
                 && lineOfSight.intersects(barrier)
                 && fromLineOfSight.intersects(barrier)
                 && toLineOfSight.intersects(barrier);
