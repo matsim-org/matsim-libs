138170bd0d988778f17f0d648a58753b4e34459b
Steffen Axer <->
2019-02-18 15:44:30 +0100

    Added a ModalSplitManupulator Script for Commuter, Enhancements in TravelDelayAnalysis

--- src/main/java/vwExamples/utils/delays/RunTravelDelayAnalysis.java
+++ src/main/java/vwExamples/utils/delays/RunTravelDelayAnalysis.java
@@ -25,6 +25,7 @@ import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Plan;
 import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.config.ConfigUtils;
@@ -35,6 +36,7 @@ import org.matsim.core.network.io.MatsimNetworkReader;
 import org.matsim.core.population.algorithms.PersonAlgorithm;
 import org.matsim.core.population.io.StreamingPopulationReader;
 import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.geometry.geotools.MGC;
 import org.matsim.core.utils.gis.ShapeFileReader;
 import org.opengis.feature.simple.SimpleFeature;
 
@@ -45,13 +47,13 @@ public class RunTravelDelayAnalysis {
     static Set<Id<Person>> relevantAgents = new HashSet<>();
     static Map<String, Geometry> zoneMap = new HashMap<>();
     static Set<String> zones = new HashSet<>();
-    static String shapeFile = "D:\\Axer\\MatsimDataStore\\WOB_BS_DRT\\BS\\input\\shapes\\wvi-zones-bsonly.shp";
+    static String shapeFile = "C:\\Temp\\shp\\parking-bs.shp";
     static String shapeFeature = "NO";
 
     public static void main(String[] args) {
 
-        String runDir = "D:\\Axer\\MatsimDataStore\\WOB_BS_DRT\\BS\\output\\bs_as_boston_1200veh_6pax\\";
-        String runId = "bs_as_boston_1200veh_6pax.";
+        String runDir = "Y:\\vw219\\";
+        String runId = "vw219.";
 
         readShape(shapeFile, shapeFeature);
 
@@ -62,22 +64,27 @@ public class RunTravelDelayAnalysis {
                 //relevantAgents.add(person.getId());
 
 
-                for (PlanElement pe : person.getSelectedPlan().getPlanElements()) {
-                    if (pe instanceof Activity) {
-                        if (((Activity) pe).getType().contains("home")) {
-
-                            Activity activity = ((Activity) pe);
-                            Coord coord = activity.getCoord();
-                            if (vwExamples.utils.modalSplitAnalyzer.modalSplitEvaluator.isWithinZone(coord, zoneMap)) {
-                                relevantAgents.add(person.getId());
-                                //System.out.println(person.getId().toString());
-                                break;
-
-                            }
-
-                        }
-                    }
-                }
+            	if (livesOutside(person.getSelectedPlan(),zoneMap) && worksInside(person.getSelectedPlan(),zoneMap) )
+            	{
+            		 relevantAgents.add(person.getId());
+            	}
+            	
+//                for (PlanElement pe : person.getSelectedPlan().getPlanElements()) {
+//                    if (pe instanceof Activity) {
+//                        if (((Activity) pe).getType().contains("home")) {
+//
+//                            Activity activity = ((Activity) pe);
+//                            Coord coord = activity.getCoord();
+//                            if (!isWithinZone(coord, zoneMap)) {
+//                                relevantAgents.add(person.getId());
+//                                //System.out.println(person.getId().toString());
+//                                break;
+//
+//                            }
+//
+//                        }
+//                    }
+//                }
 
             }
 
@@ -92,7 +99,7 @@ public class RunTravelDelayAnalysis {
         EventsManager events = EventsUtils.createEventsManager();
         events.addHandler(tdc);
         new MatsimEventsReader(events).readFile(runDir + runId + "output_events.xml.gz");
-        DynModeTripsAnalyser.collection2Text(tdc.getTrips(), runDir + "delays.csv", "PersonId;ArrivalTime;FreespeedTravelTime;ActualTravelTime");
+        DynModeTripsAnalyser.collection2Text(tdc.getTrips(), runDir + "delays.csv", "PersonId;ArrivalTime;FreespeedTravelTime;ActualTravelTime;Delay");
     }
 
     public static void readShape(String shapeFile, String featureKeyInShapeFile) {
@@ -104,4 +111,60 @@ public class RunTravelDelayAnalysis {
             zoneMap.put(id, geometry);
         }
     }
+    
+    public static boolean isWithinZone(Coord coord, Map<String, Geometry> zoneMap) {
+        // Function assumes Shapes are in the same coordinate system like MATSim
+        // simulation
+
+        for (String zone : zoneMap.keySet()) {
+            Geometry geometry = zoneMap.get(zone);
+            if (geometry.intersects(MGC.coord2Point(coord))) {
+                // System.out.println("Coordinate in "+ zone);
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
+    
+    public static boolean livesOutside(Plan plan, Map<String, Geometry> zoneMap)
+    {
+        for (PlanElement pe : plan.getPlanElements()) {
+            if (pe instanceof Activity) {
+                if (((Activity) pe).getType().contains("home")) {
+
+                    Activity activity = ((Activity) pe);
+                    Coord coord = activity.getCoord();
+                    //If home is not inside zoneMap return true
+                    if (!isWithinZone(coord, zoneMap)) {
+                    	return true;
+                    }
+
+                }
+            }
+        }
+    	return false;
+    }
+    
+    public static boolean worksInside(Plan plan, Map<String, Geometry> zoneMap)
+    {
+        for (PlanElement pe : plan.getPlanElements()) {
+            if (pe instanceof Activity) {
+                if (((Activity) pe).getType().contains("work")) {
+
+                    Activity activity = ((Activity) pe);
+                    Coord coord = activity.getCoord();
+                    //If work is inside zoneMap return true
+                    if (isWithinZone(coord, zoneMap)) {
+                    	return true;
+                    }
+
+                }
+            }
+        }
+    	return false;
+    	
+    }
+
 }
