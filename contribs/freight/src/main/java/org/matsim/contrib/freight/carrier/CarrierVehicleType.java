682f6e6f00693eb81a4a10d16410631d739fe5b8
kturner <martins-turner@vsp.tu-berlin.de>
2019-02-19 17:44:36 +0100

    Revert "add setVehicleCostInformation and setCarrierVehicleCapacity to CarrierVehicleType"
    This reverts commit 8d1bd812d76817c89bf73c42bb1707baa1994600.

--- contribs/freight/src/main/java/org/matsim/contrib/freight/carrier/CarrierVehicleType.java
+++ contribs/freight/src/main/java/org/matsim/contrib/freight/carrier/CarrierVehicleType.java
@@ -14,6 +14,10 @@ import org.matsim.vehicles.VehicleTypeImpl;
  * @author sschroeder
  *
  */
+/**
+ * @author kturner
+ *
+ */
 public class CarrierVehicleType extends ForwardingVehicleType {
 
 	/**
@@ -204,26 +208,6 @@ public class CarrierVehicleType extends ForwardingVehicleType {
 		return vehicleCostInformation;
 	}
 	
-	
-	/**
-	 * Sets the cost values for this vehicle Type
-	 * 
-	 * @param vehicleCostInformation
-	 */
-	public void setVehicleCostInformation(VehicleCostInformation vehicleCostInformation) {
-		this.vehicleCostInformation = vehicleCostInformation;
-	}
-	
-	/**
-	 * Sets the capacity of carrierVehicleType
-	 * 
-	 * <p>This might be replaced in future by a more complex concept of capacity (considering volume and different units).
-	 * 
-	 * @param capacity
-	 */
-	public void setCarrierVehicleCapacity(int capacity) {
-		this.capacity = capacity;
-	}
 
 	/**
 	 * Returns the capacity of carrierVehicleType.
@@ -235,7 +219,5 @@ public class CarrierVehicleType extends ForwardingVehicleType {
 	public int getCarrierVehicleCapacity(){
 		return capacity;
 	}
-
-
 	
 }
