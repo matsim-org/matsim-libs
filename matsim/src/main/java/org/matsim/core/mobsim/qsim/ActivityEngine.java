21d84ebdcf24e2643b072746b198241bfb7482fa
Michal Maciejewski <michal.mac@gmail.com>
2019-03-31 23:35:34 +0200

    add suggestions for renaming ActivityEngine

--- matsim/src/main/java/org/matsim/core/mobsim/qsim/ActivityEngine.java
+++ matsim/src/main/java/org/matsim/core/mobsim/qsim/ActivityEngine.java
@@ -7,5 +7,9 @@ import org.matsim.core.mobsim.qsim.interfaces.MobsimEngine;
  *  An interface that marks the "default" activity engine.
  */
 public interface ActivityEngine extends ActivityHandler, MobsimEngine {
+	//(default)ActivityEngine is very similar to TeleportationHandler (both are last handlers)
+	// The name should more clearly express that intend.
+	// Suggested names: NopActivityEngine, SleepActivityEngine, IdleActivityEngine, StaticActivityEngine???
+	// michalm, mar '19
 
 }
