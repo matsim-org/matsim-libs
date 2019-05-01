d94be4138f96c1e3c5c6acd4f47cea0ee6c04ef5
Michal Maciejewski <michal.mac@gmail.com>
2019-05-01 23:28:34 +0200

    execute DrtConfig checking directly from DrtConfigGroup.checkConsistency()

--- contribs/drt/src/main/java/org/matsim/contrib/drt/run/DrtConfigConsistencyChecker.java
+++ contribs/drt/src/main/java/org/matsim/contrib/drt/run/DrtConfigConsistencyChecker.java
@@ -20,15 +20,10 @@
 package org.matsim.contrib.drt.run;
 
 import org.apache.log4j.Logger;
-import org.matsim.contrib.drt.optimizer.rebalancing.mincostflow.MinCostFlowRebalancingParams;
-import org.matsim.contrib.drt.optimizer.rebalancing.mincostflow.MinCostFlowRebalancingParamsConsistencyChecker;
-import org.matsim.contrib.drt.run.DrtConfigGroup.OperationalScheme;
 import org.matsim.contrib.dvrp.run.ConfigConsistencyCheckers;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.consistency.ConfigConsistencyChecker;
 import org.matsim.core.config.groups.GlobalConfigGroup;
-import org.matsim.core.config.groups.QSimConfigGroup.EndtimeInterpretation;
-import org.matsim.core.utils.misc.Time;
 
 public class DrtConfigConsistencyChecker implements ConfigConsistencyChecker {
 	private static final Logger log = Logger.getLogger(DrtConfigConsistencyChecker.class);
@@ -37,44 +32,8 @@ public class DrtConfigConsistencyChecker implements ConfigConsistencyChecker {
 	public void checkConsistency(Config config) {
 		ConfigConsistencyCheckers.checkSingleOrMultiModeConsistency(DrtConfigGroup.get(config),
 				MultiModeDrtConfigGroup.get(config), drtCfg -> checkDrtConfigConsistency(drtCfg, config.global()));
-
-		if (Time.isUndefinedTime(config.qsim().getEndTime())
-				&& config.qsim().getSimEndtimeInterpretation() != EndtimeInterpretation.onlyUseEndtime) {
-			// Not an issue if all request rejections are immediate (i.e. happen during request submission)
-			log.warn("qsim.endTime should be specified and qsim.simEndtimeInterpretation should be 'onlyUseEndtime'"
-					+ " if postponed request rejection is allowed. Otherwise, rejected passengers"
-					+ " (who are stuck endlessly waiting for a DRT vehicle) will prevent QSim from stopping."
-					+ " Keep also in mind that not setting an end time may result in agents "
-					+ "attempting to travel without vehicles being available.");
-		}
-		if (config.qsim().getNumberOfThreads() != 1) {
-			throw new RuntimeException("Only a single-threaded QSim allowed");
-		}
 	}
 
 	private void checkDrtConfigConsistency(DrtConfigGroup drtCfg, GlobalConfigGroup global) {
-		if (drtCfg.getMaxWaitTime() < drtCfg.getStopDuration()) {
-			throw new RuntimeException(
-					DrtConfigGroup.MAX_WAIT_TIME + " must not be smaller than " + DrtConfigGroup.STOP_DURATION);
-		}
-		if (drtCfg.getOperationalScheme() == OperationalScheme.stopbased && drtCfg.getTransitStopFile() == null) {
-			throw new RuntimeException(DrtConfigGroup.TRANSIT_STOP_FILE
-					+ " must not be null when "
-					+ DrtConfigGroup.OPERATIONAL_SCHEME
-					+ " is "
-					+ DrtConfigGroup.OperationalScheme.stopbased);
-		}
-		if (drtCfg.getNumberOfThreads() > Runtime.getRuntime().availableProcessors()) {
-			throw new RuntimeException(
-					DrtConfigGroup.NUMBER_OF_THREADS + " is higher than the number of logical cores available to JVM");
-		}
-		if (global.getNumberOfThreads() < drtCfg.getNumberOfThreads()) {
-			log.warn("Consider increasing global.numberOfThreads to at least the value of drt.numberOfThreads"
-					+ " in order to speed up the DRT route update during the replanning phase.");
-		}
-		if (drtCfg.getParameterSets(MinCostFlowRebalancingParams.SET_NAME).size() > 1) {
-			throw new RuntimeException("More then one rebalancing parameter sets is specified");
-		}
-		new MinCostFlowRebalancingParamsConsistencyChecker().checkConsistency(drtCfg);
 	}
 }
