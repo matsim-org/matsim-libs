15880a3ff3d7bc7dd5867704550498e2f95849d6
Michal Maciejewski <michal.mac@gmail.com>
2019-05-04 21:52:43 +0200

    adapt VariableSpeedChargingTest to changes in ElectricVehicle

--- contribs/ev/src/test/java/org/matsim/contrib/ev/charging/VariableSpeedChargingTest.java
+++ contribs/ev/src/test/java/org/matsim/contrib/ev/charging/VariableSpeedChargingTest.java
@@ -24,11 +24,14 @@ import org.assertj.core.api.Assertions;
 import org.assertj.core.data.Percentage;
 import org.junit.Test;
 import org.matsim.contrib.ev.EvUnits;
-import org.matsim.contrib.ev.fleet.Battery;
-import org.matsim.contrib.ev.fleet.BatteryImpl;
 import org.matsim.contrib.ev.fleet.ElectricVehicle;
 import org.matsim.contrib.ev.fleet.ElectricVehicleImpl;
+import org.matsim.contrib.ev.fleet.ElectricVehicleSpecification;
+import org.matsim.contrib.ev.fleet.ImmutableElectricVehicleSpecification;
 
+/**
+ * @author Michal Maciejewski (michalm)
+ */
 public class VariableSpeedChargingTest {
 
 	@Test
@@ -70,10 +73,12 @@ public class VariableSpeedChargingTest {
 		assertCalcEnergyCharge(100, 100, 50, 5);
 	}
 
-	private void assertCalcEnergyCharge(double capacity_kWh, double soc_kWh, double power_kW,
-			double energyAfterOneHour_kWh) {
-		Battery battery = new BatteryImpl(EvUnits.kWh_to_J(capacity_kWh), EvUnits.kWh_to_J(soc_kWh));
-		ElectricVehicle electricVehicle = new ElectricVehicleImpl(null, battery);
+	private void assertCalcEnergyCharge(double capacity_kWh, double soc_kWh, double power_kW, double energyAfterOneHour_kWh) {
+		ElectricVehicleSpecification specification = ImmutableElectricVehicleSpecification.newBuilder()
+				.batteryCapacity(EvUnits.kWh_to_J(capacity_kWh))
+				.initialSoc(EvUnits.kWh_to_J(soc_kWh))
+				.build();
+		ElectricVehicle electricVehicle = new ElectricVehicleImpl(specification, null, null);
 		VariableSpeedCharging charging = VariableSpeedCharging.createStrategyForTesla(EvUnits.kW_to_W(power_kW), 1);
 		Assertions.assertThat(charging.calcEnergyCharge(electricVehicle, 3600))
 				.isCloseTo(EvUnits.kWh_to_J(energyAfterOneHour_kWh), Percentage.withPercentage(1e-13));
