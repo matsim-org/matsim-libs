ba50019db3d8bd7e86152e71d218c5899097c5bb
jfbischoff <bischoff@vsp.tu-berlin.de>
2019-05-22 15:59:02 +0200

    adding personId to PassengerRequestRejectedEvent

--- contribs/dvrp/src/main/java/org/matsim/contrib/dvrp/passenger/PassengerRequestRejectedEvent.java
+++ contribs/dvrp/src/main/java/org/matsim/contrib/dvrp/passenger/PassengerRequestRejectedEvent.java
@@ -20,31 +20,38 @@
 
 package org.matsim.contrib.dvrp.passenger;
 
-import java.util.Map;
-
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.events.Event;
+import org.matsim.api.core.v01.population.Person;
 import org.matsim.contrib.dvrp.optimizer.Request;
+import org.matsim.core.api.internal.HasPersonId;
+
+import java.util.Map;
 
 /**
  * @author michalm
  */
-public class PassengerRequestRejectedEvent extends Event {
+public class PassengerRequestRejectedEvent extends Event implements HasPersonId {
 	public static final String EVENT_TYPE = "PassengersRequest rejected";
 
 	public static final String ATTRIBUTE_MODE = "mode";
 	public static final String ATTRIBUTE_REQUEST = "request";
 	public static final String ATTRIBUTE_CAUSE = "cause";
+	public static final String ATTRIBUTE_PASSENGER = "personId";
+
 
 	private final String mode;
 	private final Id<Request> requestId;
 	private final String cause;
+	private final Id<Person> personId;
 
-	public PassengerRequestRejectedEvent(double time, String mode, Id<Request> requestId, String cause) {
+
+	public PassengerRequestRejectedEvent(double time, String mode, Id<Request> requestId, String cause, Id<Person> personId) {
 		super(time);
 		this.mode = mode;
 		this.requestId = requestId;
 		this.cause = cause;
+		this.personId = personId;
 	}
 
 	@Override
@@ -74,5 +81,10 @@ public class PassengerRequestRejectedEvent extends Event {
 		attr.put(ATTRIBUTE_REQUEST, requestId + "");
 		attr.put(ATTRIBUTE_CAUSE, cause);
 		return attr;
+	}
+
+	@Override
+	public Id<Person> getPersonId() {
+		return null;
 	}
 }
