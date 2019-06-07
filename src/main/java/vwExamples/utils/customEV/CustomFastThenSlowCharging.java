7b4230e962e4567c7b49e8a7d47d9956d9731e1c
Michal Maciejewski <michal.mac@gmail.com>
2019-06-07 13:07:10 +0200

    convert CustomFastThenSlowCharging from ChargingStrategy to ChargingPower

--- src/main/java/vwExamples/utils/customEV/CustomFastThenSlowCharging.java
+++ src/main/java/vwExamples/utils/customEV/CustomFastThenSlowCharging.java
@@ -1,48 +1,28 @@
 package vwExamples.utils.customEV;
 
-import org.matsim.contrib.ev.charging.ChargingStrategy;
+import org.matsim.contrib.ev.charging.ChargingPower;
 import org.matsim.contrib.ev.fleet.Battery;
 import org.matsim.contrib.ev.fleet.ElectricVehicle;
+import org.matsim.contrib.ev.infrastructure.Charger;
 
-public class CustomFastThenSlowCharging implements ChargingStrategy {
+public class CustomFastThenSlowCharging implements ChargingPower {
+	private final ElectricVehicle electricVehicle;
 
-	private final double chargingPower;
-	private final double maxRelativeSoc;
-
-	public CustomFastThenSlowCharging(double chargingPower,double maxRelativeSoc) {
-		if (chargingPower <= 0) {
-			throw new IllegalArgumentException("chargingPower must be positive");
-		}
-		
-		if (maxRelativeSoc <= 0 || maxRelativeSoc > 1) {
-			throw new IllegalArgumentException("maxRelativeSoc must be in (0,1]");
-		}
-		this.chargingPower = chargingPower;
-		this.maxRelativeSoc = maxRelativeSoc;
+	public CustomFastThenSlowCharging(ElectricVehicle electricVehicle) {
+		this.electricVehicle = electricVehicle;
 	}
 
 	@Override
-	public double calcChargingPower(ElectricVehicle ev) {
-		Battery b = ev.getBattery();
+	public double calcChargingPower(Charger charger) {
+		Battery b = electricVehicle.getBattery();
 		double relativeSoc = b.getSoc() / b.getCapacity();
 		double c = b.getCapacity() / 3600;
 		if (relativeSoc <= 0.5) {
-			return Math.min(chargingPower, 1.75 * c);
+			return Math.min(charger.getPower(), 1.75 * c);
 		} else if (relativeSoc <= 0.75) {
-			return Math.min(chargingPower, 1.25 * c);
+			return Math.min(charger.getPower(), 1.25 * c);
 		} else {
-			return Math.min(chargingPower, 0.5 * c);
+			return Math.min(charger.getPower(), 0.5 * c);
 		}
-	}
-
-	@Override
-	public double calcRemainingEnergyToCharge(ElectricVehicle ev) {
-		Battery b = ev.getBattery();
-		return maxRelativeSoc * b.getCapacity() - b.getSoc();
-	}
-
-	@Override
-	public double calcRemainingTimeToCharge(ElectricVehicle ev) {
-		return calcRemainingEnergyToCharge(ev) / chargingPower;//TODO should consider variable charging speed
 	}
 }
