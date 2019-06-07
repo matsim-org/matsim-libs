0128b837656f960f009274e5ad75b7dabddb90fd
Carlos Caldas <ccaldas@gmail.com>
2019-06-07 01:23:47 +0100

    added constructor to MutableScenario

--- matsim/src/main/java/org/matsim/core/scenario/MutableScenario.java
+++ matsim/src/main/java/org/matsim/core/scenario/MutableScenario.java
@@ -67,19 +67,25 @@ public final class MutableScenario implements Scenario, Lockable {
 
 	private Vehicles vehicles ;
 
-	MutableScenario(Config config) {
-		this.config = config;
-		this.network = NetworkUtils.createNetwork(this.config);
-		this.population = PopulationUtils.createPopulation(this.config, this.network);
-		this.facilities = new ActivityFacilitiesImpl();
-		this.households = new HouseholdsImpl();
-		this.lanes = LanesUtils.createLanesContainer();
-		this.vehicles = VehicleUtils.createVehiclesContainer();
-		this.transitVehicles = VehicleUtils.createVehiclesContainer();
-		this.transitSchedule = new TransitScheduleFactoryImpl().createTransitSchedule();
-		
-		this.config.network().setLocked();
-	}
+    MutableScenario(Config config, boolean buildInternalObjects) {
+        this.config = config;
+        if (buildInternalObjects) {
+            this.network = NetworkUtils.createNetwork(this.config);
+            this.population = PopulationUtils.createPopulation(this.config, this.network);
+            this.facilities = new ActivityFacilitiesImpl();
+            this.households = new HouseholdsImpl();
+            this.lanes = LanesUtils.createLanesContainer();
+            this.vehicles = VehicleUtils.createVehiclesContainer();
+            this.transitVehicles = VehicleUtils.createVehiclesContainer();
+            this.transitSchedule = new TransitScheduleFactoryImpl().createTransitSchedule();
+
+            this.config.network().setLocked();
+        }
+    }
+
+    MutableScenario(Config config) {
+        this(config, true);
+    }
 
 	@Override
 	public final ActivityFacilities getActivityFacilities() {
