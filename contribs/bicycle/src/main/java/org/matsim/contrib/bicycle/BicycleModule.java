82de2fb30ada786d994589874a760966303dd355
ikaddoura <kaddoura@vsp.tu-berlin.de>
2019-06-07 17:45:33 +0200

    consistency check extended

--- contribs/bicycle/src/main/java/org/matsim/contrib/bicycle/BicycleModule.java
+++ contribs/bicycle/src/main/java/org/matsim/contrib/bicycle/BicycleModule.java
@@ -81,11 +81,16 @@ public final class BicycleModule extends AbstractModule {
 		if (scenario.getVehicles().getVehicleTypes().get(bicycleVehTypeId) == null) {
 			LOG.warn("There is no vehicle type '" + bicycleConfigGroup.getBicycleMode() + "' specified in the vehicle types.");
 		} else {
-			if (scenario.getVehicles().getVehicleTypes().get(bicycleVehTypeId).getMaximumVelocity() - bicycleConfigGroup.getMaxBicycleSpeed() > 0.1) {
+			
+			double mobsimSpeed = scenario.getVehicles().getVehicleTypes().get(bicycleVehTypeId).getMaximumVelocity();
+			if (Math.abs(mobsimSpeed - bicycleConfigGroup.getMaxBicycleSpeed()) > 0.1) {
 				LOG.warn("There is an inconsistency in the specified maximum velocity for " + bicycleConfigGroup.getBicycleMode() + ":"
-						+ " Maximum speed specified in the 'bicycle' config group: " + bicycleConfigGroup.getMaxBicycleSpeed() + " vs."
-								+ " maximum speed specified for the vehicle type: " + scenario.getVehicles().getVehicleTypes().get(bicycleVehTypeId).getMaximumVelocity());
-			}
+						+ " Maximum speed specified in the 'bicycle' config group (used for routing): " + bicycleConfigGroup.getMaxBicycleSpeed() + " vs."
+								+ " maximum speed specified for the vehicle type (used in mobsim): " + mobsimSpeed);
+					if (scenario.getConfig().plansCalcRoute().getRoutingRandomness() == 0.) {
+						throw new RuntimeException("The recommended way to deal with the inconsistency between routing and scoring/mobsim is to have a randomized router. Aborting... ");
+					}
+				}
 		}
 		if (!scenario.getConfig().qsim().getMainModes().contains(bicycleConfigGroup.getBicycleMode())) {
 			LOG.warn(bicycleConfigGroup.getBicycleMode() + " not specified as main mode.");
