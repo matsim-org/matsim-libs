849cd36cab502e8d5ea3ca95da191514de5c9dad
leich <leich@vsp.tu-berlin.de>
2019-06-17 17:25:34 +0200

    repair RunDrtExample based on RunMultiModeDrtExample in contrib drt

--- code-examples/src/main/java/org/matsim/codeexamples/extensions/dvrp/RunDrtExample.java
+++ code-examples/src/main/java/org/matsim/codeexamples/extensions/dvrp/RunDrtExample.java
@@ -3,37 +3,23 @@ package org.matsim.codeexamples.extensions.dvrp;
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
-import org.matsim.api.core.v01.population.Person;
-import org.matsim.codeexamples.config.RunFromConfigfileExample;
 import org.matsim.contrib.drt.routing.DrtRoute;
 import org.matsim.contrib.drt.routing.DrtRouteFactory;
-import org.matsim.contrib.drt.routing.DrtRoutingModule;
+import org.matsim.contrib.drt.routing.DrtStageActivityType;
 import org.matsim.contrib.drt.run.*;
 import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
 import org.matsim.contrib.dvrp.run.DvrpModule;
 import org.matsim.contrib.dvrp.run.DvrpQSimComponents;
-import org.matsim.contrib.dynagent.run.DynActivityEngine;
-import org.matsim.contrib.dynagent.run.DynActivityEngineModule;
-import org.matsim.contrib.taxi.run.TaxiConfigGroup;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
 import org.matsim.core.config.groups.QSimConfigGroup;
 import org.matsim.core.config.groups.StrategyConfigGroup;
+import org.matsim.core.config.groups.QSimConfigGroup.SnapshotStyle;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
-import org.matsim.core.gbl.MatsimRandom;
-import org.matsim.core.mobsim.qsim.ActivityEngineModule;
-import org.matsim.core.mobsim.qsim.components.QSimComponentsConfigGroup;
 import org.matsim.core.replanning.strategies.DefaultPlanStrategiesModule;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.core.utils.io.IOUtils;
-import org.matsim.examples.ExamplesUtils;
-import org.matsim.vis.otfvis.OTFVisConfigGroup;
-
-import java.net.URL;
-import java.util.Iterator;
-import java.util.List;
 
 class RunDrtExample{
 	// todo:
@@ -48,49 +34,67 @@ class RunDrtExample{
 
 	public static void main( String[] args ){
 
-		URL url = ExamplesUtils.getTestScenarioURL( "equil" );
-		URL newUrl = IOUtils.newUrl( url, "config.xml" );;
-
-		Config config = ConfigUtils.loadConfig( "scenarios/cottbus_robotaxi/config_reduced.xml" ) ;
+		String configFile ;
+		if ( args!=null && args.length>=1 ) {
+			configFile = args[0] ;
+		} else {
+			configFile = "scenarios/multi_mode_one_shared_taxi/multi_mode_one_shared_taxi_config.xml";
+		}
+		
+		Config config = ConfigUtils.loadConfig( configFile );
 
 		config.qsim().setSimStarttimeInterpretation( QSimConfigGroup.StarttimeInterpretation.onlyUseStarttime );
+		config.qsim().setInsertingWaitingVehiclesBeforeDrivingVehicles(true);
+		config.qsim().setSnapshotStyle(SnapshotStyle.queue);
 
+		@SuppressWarnings("unused")
 		DvrpConfigGroup dvrpConfig = ConfigUtils.addOrGetModule( config, DvrpConfigGroup.class );
 
-//		QSimComponentsConfigGroup qsimComponentsConfig = ConfigUtils.addOrGetModule( config, QSimComponentsConfigGroup.class );
-//		List<String> cc = qsimComponentsConfig.getActiveComponents();
-//		for( String str : cc ){
-//			log.info( "component=" + str ) ;
-//			if ( str.startsWith( "@" ) ) {
-//				Class.forName(  ) ;
-//			}
-//		}
-//		cc.remove( ActivityEngineModule.COMPONENT_NAME ) ;
-//		cc.add( DynActivityEngineModule.COMPONENT_NAME ) ;
-
-		MultiModeDrtConfigGroup mm = ConfigUtils.addOrGetModule( config, MultiModeDrtConfigGroup.class );
+		MultiModeDrtConfigGroup multiModeDrtCfg = ConfigUtils.addOrGetModule(config, MultiModeDrtConfigGroup.class);
 		{
 			DrtConfigGroup drtConfig = new DrtConfigGroup();
-			drtConfig.setMaxTravelTimeAlpha( 1.3 );
-			drtConfig.setVehiclesFile( "taxis_2000.xml" );
-			drtConfig.setMaxTravelTimeBeta( 5. * 60. );
-			drtConfig.setStopDuration( 60. );
-			drtConfig.setMaxWaitTime( Double.MAX_VALUE );
-			drtConfig.setMode( TransportMode.drt );
-			mm.addParameterSet( drtConfig );
+			drtConfig.setMode("drt_A");
+			drtConfig.setStopDuration(60.);
+			drtConfig.setMaxWaitTime(900.);
+			drtConfig.setMaxTravelTimeAlpha(1.3);
+			drtConfig.setMaxTravelTimeBeta(10. * 60.);
+			drtConfig.setRequestRejection(false);
+			drtConfig.setVehiclesFile("one_shared_taxi_vehicles_A.xml");
+			drtConfig.setChangeStartLinkToLastLinkInSchedule(true);
+			multiModeDrtCfg.addParameterSet(drtConfig);
 		}
 		{
+
 			DrtConfigGroup drtConfig = new DrtConfigGroup();
-			drtConfig.setMaxTravelTimeAlpha( 1.3 );
-			drtConfig.setVehiclesFile( "taxis_5000.xml" );
-			drtConfig.setMaxTravelTimeBeta( 5. * 60. );
-			drtConfig.setStopDuration( 60. );
-			drtConfig.setMaxWaitTime( Double.MAX_VALUE );
-			drtConfig.setMode( "drt2" );
-			mm.addParameterSet( drtConfig );
+			drtConfig.setMode("drt_B");
+			drtConfig.setStopDuration(60.);
+			drtConfig.setMaxWaitTime(900.);
+			drtConfig.setMaxTravelTimeAlpha(1.3);
+			drtConfig.setMaxTravelTimeBeta(10. * 60.);
+			drtConfig.setRequestRejection(false);
+			drtConfig.setVehiclesFile("one_shared_taxi_vehicles_B.xml");
+			drtConfig.setChangeStartLinkToLastLinkInSchedule(true);
+			multiModeDrtCfg.addParameterSet(drtConfig);
+		}
+		{
+
+			DrtConfigGroup drtConfig = new DrtConfigGroup();
+			drtConfig.setMode("drt_C");
+			drtConfig.setStopDuration(60.);
+			drtConfig.setMaxWaitTime(900.);
+			drtConfig.setMaxTravelTimeAlpha(1.3);
+			drtConfig.setMaxTravelTimeBeta(10. * 60.);
+			drtConfig.setRequestRejection(false);
+			drtConfig.setVehiclesFile("one_shared_taxi_vehicles_C.xml");
+			drtConfig.setChangeStartLinkToLastLinkInSchedule(true);
+			multiModeDrtCfg.addParameterSet(drtConfig);
 		}
 
+		for (DrtConfigGroup drtCfg : multiModeDrtCfg.getModalElements()) {
+			DrtConfigs.adjustDrtConfig(drtCfg, config.planCalcScore());
+		}
 
+		config.controler().setOutputDirectory("scenarios/multi_mode_one_shared_taxi/output");
 		config.controler().setLastIteration( 1 );
 		config.controler().setOverwriteFileSetting( OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists );
 
@@ -100,7 +104,7 @@ class RunDrtExample{
 			stratSets.setWeight( 0.1 );
 			config.strategy().addStrategySettings( stratSets );
 			//
-			config.subtourModeChoice().setModes( new String [] {TransportMode.car, TransportMode.drt,"drt2"} );
+			config.subtourModeChoice().setModes( new String [] {TransportMode.car, "drt_A", "drt_B", "drt_C"} );
 		}
 		{
 			StrategyConfigGroup.StrategySettings stratSets = new StrategyConfigGroup.StrategySettings(  );
@@ -110,29 +114,31 @@ class RunDrtExample{
 		}
 
 		{
-			PlanCalcScoreConfigGroup.ModeParams modeParams = new PlanCalcScoreConfigGroup.ModeParams( TransportMode.drt ) ;
+			PlanCalcScoreConfigGroup.ModeParams modeParams = new PlanCalcScoreConfigGroup.ModeParams( "drt_A") ;
 			config.planCalcScore().addModeParams( modeParams );
 		}
 		{
-			PlanCalcScoreConfigGroup.ModeParams modeParams = new PlanCalcScoreConfigGroup.ModeParams( "drt2" ) ;
+			PlanCalcScoreConfigGroup.ModeParams modeParams = new PlanCalcScoreConfigGroup.ModeParams( "drt_B" ) ;
 			config.planCalcScore().addModeParams( modeParams );
 		}
 		{
-			PlanCalcScoreConfigGroup.ModeParams modeParams = new PlanCalcScoreConfigGroup.ModeParams( "drt_walk" ) ;
+			PlanCalcScoreConfigGroup.ModeParams modeParams = new PlanCalcScoreConfigGroup.ModeParams( "drt_C" ) ;
 			config.planCalcScore().addModeParams( modeParams );
 		}
-
-//		config.addConfigConsistencyChecker( new DrtConfigConsistencyChecker() );
-//		config.addConfigConsistencyChecker( new DvrpConfigConsistencyChecker() );
+		{
+			PlanCalcScoreConfigGroup.ModeParams modeParams = new PlanCalcScoreConfigGroup.ModeParams( "drt_A_walk" ) ;
+			config.planCalcScore().addModeParams( modeParams );
+		}
+		{
+			PlanCalcScoreConfigGroup.ModeParams modeParams = new PlanCalcScoreConfigGroup.ModeParams( "drt_B_walk" ) ;
+			config.planCalcScore().addModeParams( modeParams );
+		}
+		{
+			PlanCalcScoreConfigGroup.ModeParams modeParams = new PlanCalcScoreConfigGroup.ModeParams( "drt_C_walk" ) ;
+			config.planCalcScore().addModeParams( modeParams );
+		}
 
 		Scenario scenario = ScenarioUtils.loadScenario( config ) ;
-
-		for ( Iterator<? extends Person> it = scenario.getPopulation().getPersons().values().iterator() ; it.hasNext() ; ) {
-			it.next() ;
-			if ( MatsimRandom.getRandom().nextDouble()<0.99 ) {
-				it.remove();
-			}
-		}
 
 		scenario.getPopulation().getFactory().getRouteFactories().setRouteFactory( DrtRoute.class, new DrtRouteFactory() );
 
@@ -141,9 +147,41 @@ class RunDrtExample{
 		controler.addOverridingModule( new DvrpModule() ) ;
 		controler.addOverridingModule( new MultiModeDrtModule( ) ) ;
 
-		controler.configureQSimComponents( DvrpQSimComponents.activateModes( TransportMode.drt , "drt2") ) ;
+		controler.configureQSimComponents( DvrpQSimComponents.activateModes( "drt_A" , "drt_B", "drt_C") ) ;
 
 		controler.run() ;
+	}
+	
+	public static void adjustDrtConfig(DrtConfigGroup drtCfg, PlanCalcScoreConfigGroup planCalcScoreCfg) {
+		DrtStageActivityType drtStageActivityType = new DrtStageActivityType(drtCfg.getMode());
+		if (drtCfg.getOperationalScheme().equals(DrtConfigGroup.OperationalScheme.stopbased)) {
+			if (planCalcScoreCfg.getActivityParams(drtStageActivityType.drtStageActivity) == null) {
+				addDrtStageActivityParams(planCalcScoreCfg, drtStageActivityType.drtStageActivity);
+			}
+		}
+		if (!planCalcScoreCfg.getModes().containsKey(drtStageActivityType.drtWalk)) {
+			addDrtWalkModeParams(planCalcScoreCfg, drtStageActivityType.drtWalk);
+		}
+	}
+
+	private static void addDrtStageActivityParams(PlanCalcScoreConfigGroup planCalcScoreCfg, String stageActivityType) {
+		PlanCalcScoreConfigGroup.ActivityParams params = new PlanCalcScoreConfigGroup.ActivityParams(stageActivityType);
+		params.setTypicalDuration(1);
+		params.setScoringThisActivityAtAll(false);
+		planCalcScoreCfg.getScoringParametersPerSubpopulation().values().forEach(k -> k.addActivityParams(params));
+		planCalcScoreCfg.addActivityParams(params);
+		log.info("drt interaction scoring parameters not set. Adding default values (activity will not be scored).");
+	}
+
+	private static void addDrtWalkModeParams(PlanCalcScoreConfigGroup planCalcScoreCfg, String drtWalkMode) {
+		PlanCalcScoreConfigGroup.ModeParams drtWalk = new PlanCalcScoreConfigGroup.ModeParams(drtWalkMode);
+		PlanCalcScoreConfigGroup.ModeParams walk = planCalcScoreCfg.getModes().get(TransportMode.walk);
+		drtWalk.setConstant(walk.getConstant());
+		drtWalk.setMarginalUtilityOfDistance(walk.getMarginalUtilityOfDistance());
+		drtWalk.setMarginalUtilityOfTraveling(walk.getMarginalUtilityOfTraveling());
+		drtWalk.setMonetaryDistanceRate(walk.getMonetaryDistanceRate());
+		planCalcScoreCfg.getScoringParametersPerSubpopulation().values().forEach(k -> k.addModeParams(drtWalk));
+		log.info("drt_walk scoring parameters not set. Adding default values (same as for walk mode).");
 	}
 
 
