76c0c489041eddb8fcb51e441f14d678692c5899
Sebastian Hörl <hoerl.sebastian@gmail.com>
2019-06-19 19:10:31 +0200

    Add documentation

--- matsim/src/main/java/org/matsim/core/mobsim/qsim/qnetsimengine/vehicle_handler/DefaultVehicleHandler.java
+++ matsim/src/main/java/org/matsim/core/mobsim/qsim/qnetsimengine/vehicle_handler/DefaultVehicleHandler.java
@@ -3,6 +3,12 @@ package org.matsim.core.mobsim.qsim.qnetsimengine.vehicle_handler;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.core.mobsim.qsim.qnetsimengine.QVehicle;
 
+/**
+ * Default implementation of a {{@link #VehicleHandler()}. It always allows
+ * vehicles to arrive at a link.
+ * 
+ * @author Sebastian Hörl <sebastian.hoerl@ivt.baug.ethz.ch>
+ */
 public class DefaultVehicleHandler implements VehicleHandler {
 	@Override
 	public void handleVehicleDeparture(QVehicle vehicle, Link link) {
--- matsim/src/main/java/org/matsim/core/mobsim/qsim/qnetsimengine/vehicle_handler/VehicleHandler.java
+++ matsim/src/main/java/org/matsim/core/mobsim/qsim/qnetsimengine/vehicle_handler/VehicleHandler.java
@@ -3,10 +3,30 @@ package org.matsim.core.mobsim.qsim.qnetsimengine.vehicle_handler;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.core.mobsim.qsim.qnetsimengine.QVehicle;
 
+/**
+ * This interface provides functionality to decide what happens if a vehicle
+ * interacts with a link in the network simulation.
+ * 
+ * @author Sebastian Hörl <sebastian.hoerl@ivt.baug.ethz.ch>
+ */
 public interface VehicleHandler {
+	/**
+	 * Called when a vehicle departs in the network simulation.
+	 */
 	void handleVehicleDeparture(QVehicle vehicle, Link link);
 
+	/**
+	 * Called when a vehicle wants to arrive at a certain link the network
+	 * simulation.
+	 * 
+	 * @return Return value defines whether vehicle is currently allowed to arrive
+	 *         at the link. If not, the link will block until the next query or
+	 *         until the agent changes its plan dynamically.
+	 */
 	boolean handleVehicleArrival(QVehicle vehicle, Link link);
 
+	/**
+	 * Called when a vehicle is initially placed on a certain link.
+	 */
 	void handleInitialVehicleArrival(QVehicle vehicle, Link link);
 }
