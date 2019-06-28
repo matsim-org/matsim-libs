7c19816844f559e22b703a6ffc6230f5ce03f909
Michal Maciejewski <michal.mac@gmail.com>
2019-06-28 13:24:44 +0200

    move RunEvExample input outside resources; use input from GitHub in case no local example files

--- contribs/ev/src/main/java/org/matsim/contrib/ev/example/RunEvExample.java
+++ contribs/ev/src/main/java/org/matsim/contrib/ev/example/RunEvExample.java
@@ -21,7 +21,9 @@ package org.matsim.contrib.ev.example;/*
  * created by jbischoff, 19.03.2019
  */
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
@@ -39,27 +41,31 @@ import org.matsim.core.mobsim.qsim.AbstractQSimModule;
 import org.matsim.core.scenario.ScenarioUtils;
 
 public class RunEvExample {
+	static final String DEFAULT_CONFIG_FILE = "test/input/org/matsim/contrib/ev/example/RunEvExample/config.xml";
 	private static final Logger log = Logger.getLogger(RunEvExample.class);
 
 	public static void main(String[] args) throws IOException {
-
-		String configFile;
+		final URL configUrl;
 		if (args.length > 0) {
 			log.info("Starting simulation run with the following arguments:");
-			configFile = args[0];
-			log.info("config file: " + configFile);
-
+			configUrl = new URL(args[0]);
+			log.info("config URL: " + configUrl);
 		} else {
-			log.info("Starting simulation run with example config file from resource path");
-
-			//load config file from resource path (see src/main/resources folder for the example)
-			configFile = "config.xml";
+			File localConfigFile = new File(DEFAULT_CONFIG_FILE);
+			if (localConfigFile.exists()) {
+				log.info("Starting simulation run with the local example config file");
+				configUrl = localConfigFile.toURI().toURL();
+			} else {
+				log.info("Starting simulation run with the example config file from GitHub repository");
+				configUrl = new URL("https://raw.githubusercontent.com/matsim-org/matsim/master/contribs/ev/"
+						+ DEFAULT_CONFIG_FILE);
+			}
 		}
-		new RunEvExample().run(configFile);
+		new RunEvExample().run(configUrl);
 	}
 
-	public void run(String configFile) {
-		Config config = ConfigUtils.loadConfig(configFile, new EvConfigGroup());
+	public void run(URL configUrl) {
+		Config config = ConfigUtils.loadConfig(configUrl, new EvConfigGroup());
 		config.controler()
 				.setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
 		Scenario scenario = ScenarioUtils.loadScenario(config);
