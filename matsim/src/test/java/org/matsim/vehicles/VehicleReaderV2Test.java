aa9d80df28dff008794cd71cd82e1986b8bc7719
kturner <martins-turner@vsp.tu-berlin.de>
2019-06-29 17:10:35 +0200

    updating the VehicleReaderV2Test in preparation for further implementing. Not compiling yet due to missing implementation

--- matsim/src/test/java/org/matsim/vehicles/VehicleReaderV2Test.java
+++ matsim/src/test/java/org/matsim/vehicles/VehicleReaderV2Test.java
@@ -54,7 +54,7 @@ public class VehicleReaderV2Test extends MatsimTestCase {
 	@Test
 	public void test_NumberOfVehicleTypeisReadCorrectly() {
 		assertNotNull(vehicleTypes);
-		assertEquals(2, vehicleTypes.size());
+		assertEquals(3, vehicleTypes.size());
 	}
 
 	@Test
@@ -64,35 +64,37 @@ public class VehicleReaderV2Test extends MatsimTestCase {
 		assertEquals(9.5, vehTypeNormalCar.getLength(), EPSILON);
 		assertEquals(3.0, vehTypeNormalCar.getWidth(), EPSILON);
 		assertEquals(42.0, vehTypeNormalCar.getMaximumVelocity(), EPSILON);
+
 		assertNotNull(vehTypeNormalCar.getCapacity());
 		assertEquals(Integer.valueOf(5), vehTypeNormalCar.getCapacity().getSeats());
 		assertEquals(Integer.valueOf(20), vehTypeNormalCar.getCapacity().getStandingRoom());
-		assertNotNull(vehTypeNormalCar.getCapacity().getFreightCapacity());
-		assertEquals(23.23, vehTypeNormalCar.getCapacity().getFreightCapacity().getVolume(), EPSILON);
-		assertEquals(9.5, vehTypeNormalCar.getCapacity().getFreightCapacity().getWeight(), EPSILON);
-		assertEquals(200.0, VehicleUtils.getFreightCapacityUnits(vehTypeNormalCar), EPSILON);
+		assertEquals(23.23, vehTypeNormalCar.getCapacity().getVolume(), EPSILON);
+		assertEquals(9.5, vehTypeNormalCar.getCapacity()getWeight(), EPSILON);
+		assertEquals(200.0, VehicleUtils.getFreightCapacityUnits(vehTypeNormalCar.getCapacity()), EPSILON);
+
+		assertNotNull(vehTypeNormalCar.getCostInformation());
 		assertEquals(100, vehTypeNormalCar.getCostInformation().getFixedCosts(), EPSILON);
 		assertEquals(0.15, vehTypeNormalCar.getCostInformation().getCostsPerMeter(), EPSILON);
 		assertEquals(0.08, vehTypeNormalCar.getCostInformation().getCostsPerSecond(), EPSILON);
+		assertEquals(0.06, VehicleUtils.getCostsPerWaitingSecond(vehTypeNormalCar.getCostInformation()), EPSILON);
+
 		assertNotNull(vehTypeNormalCar.getEngineInformation());
-//		assertEquals(EngineInformation.FuelType.diesel, vehTypeNormalCar.getEngineInformation().getFuelType());
 		EngineInformation engineInformation = vehTypeNormalCar.getEngineInformation();;
 		assertEquals("pass. car", VehicleUtils.getHbefaVehicleCategory(engineInformation));
 		assertEquals("petrol", VehicleUtils.getHbefaTechnology(engineInformation));
 		assertEquals("< 1,4L", VehicleUtils.getHbefaSizeClass(engineInformation));
 		assertEquals("EURO-5", VehicleUtils.getHbefaEmissionsConcept(engineInformation));
 
+		assertEquals(2.0, vehTypeNormalCar.getPcuEquivalents());
+		assertEquals(1.5, vehTypeNormalCar.getFlowEfficiencyFactor());
+		assertEquals("pt", vehTypeNormalCar.getNetworkMode());
+
+		assertEquals("abc", vehTypeNormalCar.getAttributes().getAttribute("Attribute1"));
+		assertEquals(1.3, (double) vehTypeNormalCar.getAttributes().getAttribute("Attribute2"), EPSILON);
 		assertEquals(0.23, VehicleUtils.getFuelConsumption(vehTypeNormalCar), EPSILON);
 		assertEquals(23.23, VehicleUtils.getAccessTime(vehTypeNormalCar), EPSILON);
 		assertEquals(42.42, VehicleUtils.getEgressTime(vehTypeNormalCar), EPSILON);
 		assertEquals(DoorOperationMode.parallel, VehicleUtils.getDoorOperationMode(vehTypeNormalCar));
-
-		assertEquals(2.0, vehTypeNormalCar.getPcuEquivalents());
-		assertEquals(1.5, vehTypeNormalCar.getFlowEfficiencyFactor());
-		assertEquals("abc", vehTypeNormalCar.getAttributes().getAttribute("Attribute1"));
-		assertEquals(1.3, (double) vehTypeNormalCar.getAttributes().getAttribute("Attribute2"), EPSILON);
-		assertEquals("pt", vehTypeNormalCar.getNetworkMode());
-
 	}
 
 	@Test
@@ -108,6 +110,22 @@ public class VehicleReaderV2Test extends MatsimTestCase {
 		assertEquals(1.0, vehTypeDefaultCar.getFlowEfficiencyFactor());
 		assertEquals("def", vehTypeDefaultCar.getAttributes().getAttribute("Attribute1"));
 		assertEquals(2, vehTypeDefaultCar.getAttributes().getAttribute("Attribute2"));
+	}
+
+	@Test
+	public void test_VehicleTypeValuesAreReadCorrectly_smallTruck() {
+		VehicleType vehTypeSmallTruck = vehicleTypes.get(Id.create("smallTruck", VehicleType.class));
+		assertNotNull(vehTypeSmallTruck);
+		assertEquals("This is a small truck", vehTypeSmallTruck.getDescription());
+		assertEquals(7.5, vehTypeSmallTruck.getLength(), EPSILON);
+		assertEquals(1.0, vehTypeSmallTruck.getWidth(), EPSILON);
+		assertEquals("diesel", VehicleUtils.getHbefaTechnology(vehTypeSmallTruck.getEngineInformation()));
+		assertEquals("EURO-6", VehicleUtils.getHbefaEmissionsConcept(vehTypeSmallTruck.getEngineInformation()));
+		assertEquals(100.0, vehTypeSmallTruck.getCostInformation().getFixedCosts(), EPSILON);
+		assertEquals(0.2,vehTypeSmallTruck.getCostInformation().getCostsPerMeter(), EPSILON);
+		assertEquals(0.10, vehTypeSmallTruck.getCostInformation().getCostsPerSecond(),EPSILON);
+		assertEquals(0.05, VehicleUtils.getCostsPerWaitingSecond(vehTypeSmallTruck.getCostInformation()), EPSILON);
+		assertEquals(0.05, VehicleUtils.getCostsPerServiceSecond(vehTypeSmallTruck.getCostInformation()), EPSILON);
 	}
 
 	@Test
