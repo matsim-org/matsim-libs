30d71cfb00e143f05029df7cf0ea1af54875ddc2
Michal Maciejewski <michal.mac@gmail.com>
2019-06-30 13:54:02 +0200

    reformat code in dvrp

--- contribs/dvrp/src/main/java/org/matsim/contrib/zone/ZonalSystems.java
+++ contribs/dvrp/src/main/java/org/matsim/contrib/zone/ZonalSystems.java
@@ -37,8 +37,9 @@ public class ZonalSystems {
 	public static Map<Id<Zone>, List<Zone>> initZonesByDistance(Map<Id<Zone>, Zone> zones) {
 		Map<Id<Zone>, List<Zone>> zonesByDistance = Maps.newHashMapWithExpectedSize(zones.size());
 		for (final Zone currentZone : zones.values()) {
-			List<Zone> sortedZones = zones.values().stream()//
-					.sorted(Comparator.comparing(z -> DistanceUtils.calculateSquaredDistance(currentZone, z)))//
+			List<Zone> sortedZones = zones.values()
+					.stream()
+					.sorted(Comparator.comparing(z -> DistanceUtils.calculateSquaredDistance(currentZone, z)))
 					.collect(Collectors.toList());
 			zonesByDistance.put(currentZone.getId(), sortedZones);
 		}
