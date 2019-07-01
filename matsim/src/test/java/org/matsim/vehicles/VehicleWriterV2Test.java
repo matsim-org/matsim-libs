d0cf28c43a0a81e3cb06851d99b830fcb94a15dc
kturner <martins-turner@vsp.tu-berlin.de>
2019-07-01 21:13:49 +0200

    same for VehicleWriterV2Test. Also updating test settings.

--- matsim/src/test/java/org/matsim/vehicles/VehicleWriterV2Test.java
+++ matsim/src/test/java/org/matsim/vehicles/VehicleWriterV2Test.java
@@ -20,12 +20,12 @@
 package org.matsim.vehicles;
 
 import org.apache.log4j.Logger;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.matsim.api.core.v01.Id;
 import org.matsim.testcases.MatsimTestCase;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -40,82 +40,127 @@ public class VehicleWriterV2Test extends MatsimTestCase {
 	private final Id<Vehicle> id42 = Id.create("42", Vehicle.class);
 	private final Id<Vehicle> id42_23 = Id.create(" 42  23", Vehicle.class); //indeed this should be double blank in the middle but due to collapse this is only one blank
 
-	public void testWriter() throws FileNotFoundException, IOException {
+	private Map<Id<VehicleType>, VehicleType> vehicleTypes ;
+	private Map<Id<Vehicle>, Vehicle> vehicles;
 
+	@BeforeClass
+	public void setUp() throws Exception {
+		super.setUp();
 		String outfileName = this.getOutputDirectory() + "testOutputVehicles.xml";
 
 		//read it
-		Vehicles vehicles = VehicleUtils.createVehiclesContainer();
-		MatsimVehicleReader reader = new MatsimVehicleReader(vehicles);
+		Vehicles vehicles1 = VehicleUtils.createVehiclesContainer();
+		MatsimVehicleReader reader = new MatsimVehicleReader(vehicles1);
 		reader.readFile(this.getPackageInputDirectory() + TESTXML);
 
 		//write it
-		VehicleWriterV2 writer = new VehicleWriterV2(vehicles);
+		VehicleWriterV2 writer = new VehicleWriterV2(vehicles1);
 		writer.writeFile(outfileName);
 		assertTrue(new File(outfileName).exists()); 
 		//read it again
-		vehicles = VehicleUtils.createVehiclesContainer();
-		reader = new MatsimVehicleReader(vehicles);
+		Vehicles vehicles2 = VehicleUtils.createVehiclesContainer();
+		reader = new MatsimVehicleReader(vehicles2);
 		reader.readFile(this.getOutputDirectory() + "testOutputVehicles.xml");
 
-		//check it, check it, check it now!
-		this.checkContent(vehicles);
+		vehicleTypes = vehicles2.getVehicleTypes();
+		vehicles = vehicles2.getVehicles();
 	}
 
-	private void checkContent(Vehicles vehdef) {
-		Map<Id<VehicleType>, VehicleType> vehicleTypes = vehdef.getVehicleTypes();
-		Map<Id<Vehicle>, Vehicle> vehicles = vehdef.getVehicles();
-
+	@Test
+	public void test_NumberOfVehicleTypeisReadCorrectly() {
 		assertNotNull(vehicleTypes);
-		assertEquals(2, vehicleTypes.size());
-		VehicleType vehType = vehicleTypes.get(Id.create("normal&Car", VehicleType.class));
-		assertNotNull(vehType);
-		assertEquals(9.5, vehType.getLength(), EPSILON);
-		assertEquals(3.0, vehType.getWidth(), EPSILON);
-		assertEquals(42.0, vehType.getMaximumVelocity(), EPSILON);
-		assertNotNull(vehType.getCapacity());
-		assertEquals(Integer.valueOf(5), vehType.getCapacity().getSeats());
-		assertEquals(Integer.valueOf(20), vehType.getCapacity().getStandingRoom());
-		assertNotNull(vehType.getCapacity().getFreightCapacity());
-		assertEquals(23.23, vehType.getCapacity().getFreightCapacity().getVolume(), EPSILON);
-		assertEquals(9.5, vehType.getCapacity().getFreightCapacity().getWeight(), EPSILON);
-		assertNotNull(vehType.getEngineInformation());
-		assertEquals(EngineInformation.FuelType.diesel, vehType.getEngineInformation().getFuelType());
-		assertEquals(0.23, VehicleUtils.getFuelConsumption(vehType), EPSILON);
-		assertEquals(23.23, VehicleUtils.getAccessTime(vehType), EPSILON);
-		assertEquals(42.42, VehicleUtils.getEgressTime(vehType), EPSILON);
-		assertEquals(DoorOperationMode.parallel, VehicleUtils.getDoorOperationMode(vehType));
-		assertEquals(2.0, vehType.getPcuEquivalents());
-		assertEquals("pt", vehType.getNetworkMode());
-		assertEquals(1.5, vehType.getFlowEfficiencyFactor());
-		assertEquals("abc", vehType.getAttributes().getAttribute("Attribute1"));
-		assertEquals(1.3, (double) vehType.getAttributes().getAttribute("Attribute2"), EPSILON);
+		assertEquals(3, vehicleTypes.size());
+	}
 
+	@Test
+	public void test_VehicleTypeValuesAreReadCorrectly_normalCar() {
+		VehicleType vehTypeNormalCar = vehicleTypes.get(Id.create("normal&Car", VehicleType.class));
+		assertNotNull(vehTypeNormalCar);
+		assertEquals(9.5, vehTypeNormalCar.getLength(), EPSILON);
+		assertEquals(3.0, vehTypeNormalCar.getWidth(), EPSILON);
+		assertEquals(42.0, vehTypeNormalCar.getMaximumVelocity(), EPSILON);
 
-		vehType = vehicleTypes.get(Id.create("defaultValue>Car", VehicleType.class));
-		assertNotNull(vehType);
-		assertEquals(7.5, vehType.getLength(), EPSILON);
-		assertEquals(1.0, vehType.getWidth(), EPSILON);
-		assertTrue(Double.isInfinite(vehType.getMaximumVelocity()));
-		assertNull(vehType.getCapacity());
-		assertEquals(DoorOperationMode.serial, VehicleUtils.getDoorOperationMode(vehType));
-		assertEquals(1.0, vehType.getPcuEquivalents());
+		assertNotNull(vehTypeNormalCar.getCapacity());
+		assertEquals(Integer.valueOf(5), vehTypeNormalCar.getCapacity().getSeats());
+		assertEquals(Integer.valueOf(20), vehTypeNormalCar.getCapacity().getStandingRoom());
+		assertEquals(23.23, vehTypeNormalCar.getCapacity().getVolume(), EPSILON);
+		assertEquals(9.5, vehTypeNormalCar.getCapacity()getWeight(), EPSILON);
+		assertEquals(200.0, VehicleUtils.getFreightCapacityUnits(vehTypeNormalCar.getCapacity()), EPSILON);
 
-		assertNotNull(vehicles);
-		assertEquals(3, vehicles.size());
+		assertNotNull(vehTypeNormalCar.getCostInformation());
+		assertEquals(100, vehTypeNormalCar.getCostInformation().getFixedCosts(), EPSILON);
+		assertEquals(0.15, vehTypeNormalCar.getCostInformation().getCostsPerMeter(), EPSILON);
+		assertEquals(0.08, vehTypeNormalCar.getCostInformation().getCostsPerSecond(), EPSILON);
+		assertEquals(0.06, VehicleUtils.getCostsPerWaitingSecond(vehTypeNormalCar.getCostInformation()), EPSILON);
 
-		assertNotNull(vehicles.get(id23));
-		assertEquals(id23, vehicles.get(id23).getId());
-		assertEquals(Id.create("normal&Car", VehicleType.class), vehicles.get(id23).getType().getId());
+		assertNotNull(vehTypeNormalCar.getEngineInformation());
+		EngineInformation engineInformation = vehTypeNormalCar.getEngineInformation();;
+		assertEquals("pass. car", VehicleUtils.getHbefaVehicleCategory(engineInformation));
+		assertEquals("petrol", VehicleUtils.getHbefaTechnology(engineInformation));
+		assertEquals("< 1,4L", VehicleUtils.getHbefaSizeClass(engineInformation));
+		assertEquals("EURO-5", VehicleUtils.getHbefaEmissionsConcept(engineInformation));
 
-		assertNotNull(vehicles.get(id42));
-		assertEquals(id42, vehicles.get(id42).getId());
-		assertEquals(Id.create("defaultValue>Car", VehicleType.class), vehicles.get(id42).getType().getId());
+		assertEquals(2.0, vehTypeNormalCar.getPcuEquivalents());
+		assertEquals(1.5, vehTypeNormalCar.getFlowEfficiencyFactor());
+		assertEquals("pt", vehTypeNormalCar.getNetworkMode());
 
-		assertNotNull(vehicles.get(id42_23));
-		assertEquals(id42_23, vehicles.get(id42_23).getId());
+		assertEquals("abc", vehTypeNormalCar.getAttributes().getAttribute("Attribute1"));
+		assertEquals(1.3, (double) vehTypeNormalCar.getAttributes().getAttribute("Attribute2"), EPSILON);
+		assertEquals(0.23, VehicleUtils.getFuelConsumption(vehTypeNormalCar), EPSILON);
+		assertEquals(23.23, VehicleUtils.getAccessTime(vehTypeNormalCar), EPSILON);
+		assertEquals(42.42, VehicleUtils.getEgressTime(vehTypeNormalCar), EPSILON);
+		assertEquals(DoorOperationMode.parallel, VehicleUtils.getDoorOperationMode(vehTypeNormalCar));
+	}
 
+	@Test
+	public void test_VehicleTypeValuesAreReadCorrectly_defaultCar() {
+		VehicleType vehTypeDefaultCar = vehicleTypes.get(Id.create("defaultValue>Car", VehicleType.class));
+		assertNotNull(vehTypeDefaultCar);
+		assertEquals(7.5, vehTypeDefaultCar.getLength(), EPSILON);
+		assertEquals(1.0, vehTypeDefaultCar.getWidth(), EPSILON);
+		assertTrue(Double.isInfinite(vehTypeDefaultCar.getMaximumVelocity()));
+		assertNull(vehTypeDefaultCar.getCapacity());
+		assertEquals(DoorOperationMode.serial, VehicleUtils.getDoorOperationMode(vehTypeDefaultCar));
+		assertEquals(1.0, vehTypeDefaultCar.getPcuEquivalents());
+		assertEquals(1.0, vehTypeDefaultCar.getFlowEfficiencyFactor());
+		assertEquals("def", vehTypeDefaultCar.getAttributes().getAttribute("Attribute1"));
+		assertEquals(2, vehTypeDefaultCar.getAttributes().getAttribute("Attribute2"));
+	}
 
+	@Test
+	public void test_VehicleTypeValuesAreReadCorrectly_smallTruck() {
+		VehicleType vehTypeSmallTruck = vehicleTypes.get(Id.create("smallTruck", VehicleType.class));
+		assertNotNull(vehTypeSmallTruck);
+		assertEquals("This is a small truck", vehTypeSmallTruck.getDescription());
+		assertEquals(7.5, vehTypeSmallTruck.getLength(), EPSILON);
+		assertEquals(1.0, vehTypeSmallTruck.getWidth(), EPSILON);
+		assertEquals("diesel", VehicleUtils.getHbefaTechnology(vehTypeSmallTruck.getEngineInformation()));
+		assertEquals("EURO-6", VehicleUtils.getHbefaEmissionsConcept(vehTypeSmallTruck.getEngineInformation()));
+		assertEquals(100.0, vehTypeSmallTruck.getCostInformation().getFixedCosts(), EPSILON);
+		assertEquals(0.2,vehTypeSmallTruck.getCostInformation().getCostsPerMeter(), EPSILON);
+		assertEquals(0.10, vehTypeSmallTruck.getCostInformation().getCostsPerSecond(),EPSILON);
+		assertEquals(0.05, VehicleUtils.getCostsPerWaitingSecond(vehTypeSmallTruck.getCostInformation()), EPSILON);
+		assertEquals(0.05, VehicleUtils.getCostsPerServiceSecond(vehTypeSmallTruck.getCostInformation()), EPSILON);
+	}
+
+	@Test
+	public void test_NumberOfVehiclesIsReadCorrectly() {
+		assertNotNull(vehicles2);
+		assertEquals(3, vehicles2.size());
+	}
+
+	@Test
+	public void test_VehicleTypeToVehiclesAssignmentIsReadCorrectly(){
+		assertNotNull(vehicles2.get(id23));
+		assertEquals(id23, vehicles2.get(id23).getId());
+		assertEquals(Id.create("normal&Car", VehicleType.class), vehicles2.get(id23).getType().getId());
+
+		assertNotNull(vehicles2.get(id42));
+		assertEquals(id42, vehicles2.get(id42).getId());
+		assertEquals(Id.create("defaultValue>Car", VehicleType.class), vehicles2.get(id42).getType().getId());
+
+		assertNotNull(vehicles2.get(id42_23));
+		assertEquals(id42_23, vehicles2.get(id42_23).getId());
 	}
 
 }
