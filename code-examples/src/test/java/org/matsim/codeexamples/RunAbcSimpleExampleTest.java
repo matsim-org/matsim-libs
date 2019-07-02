4774e0907034d91014763d62850dd0a5c1983922
kainagel <nagel@vsp.tu-berlin.de>
2019-07-02 09:03:03 +0200

    a longer plans file; a test

--- code-examples/src/test/java/org/matsim/codeexamples/RunAbcSimpleExampleTest.java
+++ code-examples/src/test/java/org/matsim/codeexamples/RunAbcSimpleExampleTest.java
@@ -1,12 +1,21 @@
 package org.matsim.codeexamples;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class RunAbcSimpleExampleTest{
+	private static final Logger log = Logger.getLogger(RunAbcSimpleExampleTest.class) ;
 
 	@Test
 	public void main(){
+		try{
+			RunAbcExample.main( null );
+		} catch ( Exception ee ) {
+			log.fatal(ee) ;
+			fail() ;
+		}
+
 	}
 }
