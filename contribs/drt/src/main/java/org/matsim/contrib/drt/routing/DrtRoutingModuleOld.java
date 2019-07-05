ddc42423384dc886520b3b7c5df4f757e7b500a2
kainagel <nagel@vsp.tu-berlin.de>
2019-07-05 19:20:07 +0200

    add the nonNetworkWalk into the drt router the same way it is done in DynRoutingModule (switch inside class rather than having separate class)

--- contribs/drt/src/main/java/org/matsim/contrib/drt/routing/DrtRoutingModuleOld.java
+++ contribs/drt/src/main/java/org/matsim/contrib/drt/routing/DrtRoutingModuleOld.java
@@ -17,10 +17,11 @@
  *                                                                         *
  * *********************************************************************** */
 
-package org.matsim.contrib.drt.run;
+package org.matsim.contrib.drt.routing;
 
-import com.google.inject.name.Named;
-import org.apache.log4j.Level;
+import java.util.Collections;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Link;
@@ -29,32 +30,29 @@ import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.api.core.v01.population.PopulationFactory;
-import org.matsim.contrib.drt.routing.DrtRoute;
-import org.matsim.contrib.drt.routing.DrtStageActivityType;
+import org.matsim.contrib.drt.run.DrtConfigGroup;
 import org.matsim.contrib.dvrp.path.VrpPathWithTravelData;
 import org.matsim.contrib.dvrp.path.VrpPaths;
 import org.matsim.contrib.dvrp.router.DvrpRoutingNetworkProvider;
 import org.matsim.contrib.dvrp.trafficmonitoring.DvrpTravelTimeModule;
-import org.matsim.core.gbl.Gbl;
-import org.matsim.core.router.*;
+import org.matsim.core.network.NetworkUtils;
+import org.matsim.core.router.EmptyStageActivityTypes;
+import org.matsim.core.router.FastAStarEuclideanFactory;
+import org.matsim.core.router.RoutingModule;
+import org.matsim.core.router.StageActivityTypes;
 import org.matsim.core.router.costcalculators.TravelDisutilityFactory;
 import org.matsim.core.router.util.LeastCostPathCalculator;
 import org.matsim.core.router.util.TravelTime;
-import org.matsim.facilities.FacilitiesUtils;
 import org.matsim.facilities.Facility;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import static org.matsim.core.router.NetworkRoutingInclAccessEgressModule.*;
+import com.google.inject.name.Named;
 
 /**
  * @author jbischoff
  * @author michalm (Michal Maciejewski)
  */
-class DrtInclAccessEgressRoutingModule implements RoutingModule {
-	private static final Logger LOGGER = Logger.getLogger( DrtInclAccessEgressRoutingModule.class );
+class DrtRoutingModuleOld implements RoutingModule {
+	private static final Logger LOGGER = Logger.getLogger(DrtRoutingModuleOld.class);
 
 	private final DrtConfigGroup drtCfg;
 	private final Network network;
@@ -64,12 +62,10 @@ class DrtInclAccessEgressRoutingModule implements RoutingModule {
 	private final RoutingModule walkRouter;
 	private final DrtStageActivityType drtStageActivityType;
 
-	DrtInclAccessEgressRoutingModule( DrtConfigGroup drtCfg, @Named(DvrpRoutingNetworkProvider.DVRP_ROUTING) Network network,
-						    @Named(DvrpTravelTimeModule.DVRP_ESTIMATED) TravelTime travelTime,
-						    TravelDisutilityFactory travelDisutilityFactory, PopulationFactory populationFactory,
-						    @Named(TransportMode.walk) RoutingModule walkRouter ) {
-		LOGGER.setLevel( Level.DEBUG );
-
+	public DrtRoutingModuleOld(DrtConfigGroup drtCfg, @Named(DvrpRoutingNetworkProvider.DVRP_ROUTING) Network network,
+			@Named(DvrpTravelTimeModule.DVRP_ESTIMATED) TravelTime travelTime,
+			TravelDisutilityFactory travelDisutilityFactory, PopulationFactory populationFactory,
+			@Named(TransportMode.walk) RoutingModule walkRouter) {
 		this.drtCfg = drtCfg;
 		this.network = network;
 		this.travelTime = travelTime;
@@ -84,68 +80,45 @@ class DrtInclAccessEgressRoutingModule implements RoutingModule {
 	}
 
 	@Override
-	public List<? extends PlanElement> calcRoute(Facility fromFacility, Facility toFacility, double departureTime, Person person) {
-		LOGGER.debug( "entering calcRoute ..." );
-		LOGGER.debug("fromFacility=" + fromFacility.toString() ) ;
-		LOGGER.debug( "toFacility=" + toFacility.toString() );
-
-		Gbl.assertNotNull( fromFacility );
-		Gbl.assertNotNull( toFacility );
-
-		Link accessActLink = FacilitiesUtils.decideOnLink( fromFacility, network ) ;
-		Link egressActLink = FacilitiesUtils.decideOnLink( toFacility, network ) ;
-
-		double now = departureTime ;
-
-		if (accessActLink == egressActLink){
-			if( drtCfg.isPrintDetailedWarnings() ){
-				LOGGER.error( "Start and end stop are the same, agent will walk using mode "
-							    + drtStageActivityType.drtWalk
-							    + ". Agent Id:\t"
-							    + person.getId() );
+	public List<? extends PlanElement> calcRoute(Facility fromFacility, Facility toFacility, double departureTime,
+			Person person) {
+		Link fromLink = getLink(fromFacility);
+		Link toLink = getLink(toFacility);
+		if (toLink == fromLink) {
+			if (drtCfg.isPrintDetailedWarnings()) {
+				LOGGER.error("Start and end stop are the same, agent will walk using mode "
+						+ drtStageActivityType.drtWalk
+						+ ". Agent Id:\t"
+						+ person.getId());
 			}
-			Leg leg = (Leg) walkRouter.calcRoute( fromFacility, toFacility, departureTime, person ).get( 0 );
-			leg.setDepartureTime( now );
-			leg.setMode( drtStageActivityType.drtWalk );
-			LOGGER.debug( "travel time on walk leg=" + leg.getTravelTime() );
-			return Collections.singletonList( leg ) ;
+			Leg leg = (Leg)walkRouter.calcRoute(fromFacility, toFacility, departureTime, person).get(0);
+			leg.setMode(drtStageActivityType.drtWalk);
+			return (Collections.singletonList(leg));
 		}
 
-		List<PlanElement> result = new ArrayList<>() ;
+		VrpPathWithTravelData unsharedPath = VrpPaths.calcAndCreatePath(fromLink, toLink, departureTime, router,
+				travelTime);
+		double unsharedRideTime = unsharedPath.getTravelTime();//includes first & last link
+		double maxTravelTime = getMaxTravelTime(drtCfg, unsharedRideTime);
+		double unsharedDistance = VrpPaths.calcDistance(unsharedPath);//includes last link
 
-		// === access:
-		{
-			now = addBushwhackingLegFromFacilityToLinkIfNecessary( fromFacility, person, accessActLink, now, result, populationFactory, drtStageActivityType.drtStageActivity ) ;
-		}
+		DrtRoute route = populationFactory.getRouteFactories()
+				.createRoute(DrtRoute.class, fromLink.getId(), toLink.getId());
+		route.setDistance(unsharedDistance);
+		route.setTravelTime(maxTravelTime);
+		route.setUnsharedRideTime(unsharedRideTime);
+		route.setMaxWaitTime(drtCfg.getMaxWaitTime());
 
-		// === drt proper:
-		{
-			VrpPathWithTravelData unsharedPath = VrpPaths.calcAndCreatePath( accessActLink, egressActLink, departureTime, router, travelTime );
-			double unsharedRideTime = unsharedPath.getTravelTime();//includes first & last link
-			double maxTravelTime = getMaxTravelTime( drtCfg, unsharedRideTime );
-			double unsharedDistance = VrpPaths.calcDistance( unsharedPath );//includes last link
+		Leg drtLeg = populationFactory.createLeg(drtCfg.getMode());
+		drtLeg.setDepartureTime(departureTime);
+		drtLeg.setTravelTime(maxTravelTime);
+		drtLeg.setRoute(route);
+		return Collections.singletonList(drtLeg);
+	}
 
-			DrtRoute route = populationFactory.getRouteFactories().createRoute( DrtRoute.class, accessActLink.getId(), egressActLink.getId() );
-			route.setDistance( unsharedDistance );
-			route.setTravelTime( maxTravelTime );
-			route.setUnsharedRideTime( unsharedRideTime );
-			route.setMaxWaitTime( drtCfg.getMaxWaitTime() );
-
-			Leg leg = populationFactory.createLeg( drtCfg.getMode() );
-			leg.setDepartureTime( departureTime );
-			leg.setTravelTime( maxTravelTime );
-			leg.setRoute( route );
-
-			result.add( leg ) ;
-			now += maxTravelTime ;
-		}
-
-		// === egress:
-		{
-			addBushwhackingLegFromLinkToFacilityIfNecessary( toFacility, person, egressActLink, now, result, populationFactory, drtStageActivityType.drtStageActivity) ;
-		}
-
-		return result ;
+	private Link getLink(Facility facility) {
+		Link link = network.getLinks().get(facility.getLinkId());
+		return link != null ? link : NetworkUtils.getNearestLink(network, facility.getCoord());
 	}
 
 	@Override
