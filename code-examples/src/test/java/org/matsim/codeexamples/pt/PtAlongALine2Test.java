d27265bc0a2c05d12a40cce4142404734c22a8b0
kainagel <nagel@vsp.tu-berlin.de>
2019-07-05 18:20:33 +0200

    add a test that shows the problem that drt-drt is preferred over the faster drt-pt-drt

--- code-examples/src/test/java/org/matsim/codeexamples/pt/PtAlongALine2Test.java
+++ code-examples/src/test/java/org/matsim/codeexamples/pt/PtAlongALine2Test.java
@@ -3,12 +3,19 @@ package org.matsim.codeexamples.pt;
 import ch.sbb.matsim.config.SwissRailRaptorConfigGroup;
 import ch.sbb.matsim.config.SwissRailRaptorConfigGroup.IntermodalAccessEgressParameterSet;
 import ch.sbb.matsim.routing.pt.raptor.SwissRailRaptorModule;
+import com.google.inject.Inject;
+import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
+import org.locationtech.jts.awt.PointShapeFactory;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.PlanElement;
+import org.matsim.api.core.v01.population.Population;
 import org.matsim.contrib.drt.routing.DrtRoute;
 import org.matsim.contrib.drt.routing.DrtRouteFactory;
 import org.matsim.contrib.drt.run.DrtConfigGroup;
@@ -26,18 +33,26 @@ import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
 import org.matsim.core.config.groups.PlansCalcRouteConfigGroup;
 import org.matsim.core.config.groups.QSimConfigGroup;
 import org.matsim.core.config.groups.VspExperimentalConfigGroup;
+import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.events.IterationStartsEvent;
+import org.matsim.core.controler.events.ShutdownEvent;
+import org.matsim.core.controler.events.StartupEvent;
+import org.matsim.core.controler.listener.*;
+import org.matsim.core.router.MainModeIdentifier;
+import org.matsim.core.router.StageActivityTypes;
+import org.matsim.core.router.TripRouter;
+import org.matsim.core.router.TripStructureUtils;
 import org.matsim.testcases.MatsimTestUtils;
 import org.matsim.vehicles.VehicleType;
 import org.matsim.vehicles.VehiclesFactory;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 import static org.matsim.core.config.groups.PlansCalcRouteConfigGroup.*;
 
 public class PtAlongALine2Test{
+	private static final Logger log = Logger.getLogger( PtAlongALine2Test.class );
 
 	@Rule public MatsimTestUtils utils = new MatsimTestUtils() ;
 
@@ -82,7 +97,8 @@ public class PtAlongALine2Test{
 			// teleportation router for walk or bike is automatically defined.
 		} else if( drtMode == DrtMode.teleportBasedOnNetworkRoute ){// (route as network route)
 			//			config.plansCalcRoute().removeModeRoutingParams( TransportMode.walk );
-			Set<String> networkModes = new HashSet<>( config.plansCalcRoute().getNetworkModes() );
+			//			Set<String> networkModes = new HashSet<>( config.plansCalcRoute().getNetworkModes() );
+			Set<String> networkModes = new HashSet<>( );
 			networkModes.add( TransportMode.drt );
 			//			networkModes.add( TransportMode.walk );
 			if( drt2 ){
@@ -91,6 +107,7 @@ public class PtAlongALine2Test{
 			config.plansCalcRoute().setNetworkModes( networkModes );
 		}
 
+		// set up walk2 so we don't need walk in raptor:
 		{
 			ModeRoutingParams wlk = new ModeRoutingParams(  ) ;
 			wlk.setMode( "walk2" ) ;
@@ -107,7 +124,7 @@ public class PtAlongALine2Test{
 				{
 					// Xxx
 					IntermodalAccessEgressParameterSet paramSetXxx = new IntermodalAccessEgressParameterSet();
-//					paramSetXxx.setMode( TransportMode.walk ); // this does not work because sbb raptor treats it in a special way
+					//					paramSetXxx.setMode( TransportMode.walk ); // this does not work because sbb raptor treats it in a special way
 					paramSetXxx.setMode( "walk2" );
 					paramSetXxx.setRadius( 1000000 );
 					configRaptor.addIntermodalAccessEgress( paramSetXxx );
@@ -138,13 +155,13 @@ public class PtAlongALine2Test{
 		double margUtlTravPt = config.planCalcScore().getModes().get( TransportMode.pt ).getMarginalUtilityOfTraveling();;
 		if ( drtMode!=DrtMode.none ) {
 			//			{
-//				PlanCalcScoreConfigGroup.ModeParams modeParams = new PlanCalcScoreConfigGroup.ModeParams("access_walk");
-//				config.planCalcScore().addModeParams(modeParams);
-//			}
-//			{
-//				PlanCalcScoreConfigGroup.ModeParams modeParams = new PlanCalcScoreConfigGroup.ModeParams("egress_walk");
-//				config.planCalcScore().addModeParams(modeParams);
-//			}
+			//				PlanCalcScoreConfigGroup.ModeParams modeParams = new PlanCalcScoreConfigGroup.ModeParams("access_walk");
+			//				config.planCalcScore().addModeParams(modeParams);
+			//			}
+			//			{
+			//				PlanCalcScoreConfigGroup.ModeParams modeParams = new PlanCalcScoreConfigGroup.ModeParams("egress_walk");
+			//				config.planCalcScore().addModeParams(modeParams);
+			//			}
 			// (scoring parameters for drt modes)
 			{
 				PlanCalcScoreConfigGroup.ModeParams modeParams = new PlanCalcScoreConfigGroup.ModeParams(TransportMode.drt);
@@ -163,8 +180,13 @@ public class PtAlongALine2Test{
 			config.planCalcScore().addModeParams(modeParams);
 		}
 
+		// === QSIM: ===
+
 		config.qsim().setSimStarttimeInterpretation( QSimConfigGroup.StarttimeInterpretation.onlyUseStarttime );
 		// yy why?  kai, jun'19
+
+		config.qsim().setMainModes( Arrays.asList( TransportMode.car, TransportMode.drt, "drt2") );
+		// yyyy buses use the car network and so that needs to be defined as network mode.   !!!! :-( :-(
 
 		// === DRT: ===
 
@@ -245,7 +267,9 @@ public class PtAlongALine2Test{
 			VehicleType vehType = vf.createVehicleType( Id.create( TransportMode.drt, VehicleType.class ) );
 			vehType.setMaximumVelocity( 25./3.6 );
 			scenario.getVehicles().addVehicleType( vehType );
-		}{
+		}
+		{
+			// (does not work without; I don't really know why. kai)
 			VehicleType vehType = vf.createVehicleType( Id.create( TransportMode.car, VehicleType.class ) );
 			vehType.setMaximumVelocity( 25./3.6 );
 			scenario.getVehicles().addVehicleType( vehType );
@@ -276,7 +300,189 @@ public class PtAlongALine2Test{
 	}
 
 	@Test
+	public void intermodalAccessEgressPicksWrongVariant() {
+		// this test fails because it picks a
+		//    drt-nonNetworkWalk-nonNetworkWalk-drt
+		//  trip over a faster
+		//    drt-nonNetworkWalk-pt-nonNetworkWalk-drt
+		// trip.  Commenting out the method
+		//    handleTransfers(...)
+		// in SwissRailRaptorCore
+		//         if (hasIntermodalAccess) {
+		//            // allow transfering from the initial stop to another one if we have intermodal access,
+		//            // as not all stops might be intermodal
+		//            handleTransfers(true, parameters);
+		//        }
+		// makes it pass.  I have no idea why, or if this would be a good direction to go for a fix.  kai, jul'19
+
+		Config config = PtAlongALineTest.createConfig( utils.getOutputDirectory() );
+
+		// === GBL: ===
+
+		config.controler().setLastIteration( 0 );
+
+		// === ROUTER: ===
+
+		config.plansCalcRoute().setInsertingAccessEgressWalk( true );
+
+		config.qsim().setVehiclesSource( QSimConfigGroup.VehiclesSource.modeVehicleTypesFromVehiclesData );
+		// (as of today, will also influence router. kai, jun'19)
+		{
+			Set<String> networkModes = new HashSet<>( );
+			networkModes.add( TransportMode.drt );
+			networkModes.add( "drt2" );
+			config.plansCalcRoute().setNetworkModes( networkModes );
+		}
+
+		// set up walk2 so we don't use faulty walk in raptor:
+		{
+			ModeRoutingParams wlk = new ModeRoutingParams(  ) ;
+			wlk.setMode( "walk2" ) ;
+			wlk.setTeleportedModeSpeed( 5./3.6 ) ;
+			config.plansCalcRoute().addModeRoutingParams( wlk );
+		}
+
+		// === RAPTOR: ===
+		{
+			SwissRailRaptorConfigGroup configRaptor = ConfigUtils.addOrGetModule( config, SwissRailRaptorConfigGroup.class ) ;
+
+			{
+				configRaptor.setUseIntermodalAccessEgress(true);
+				{
+					// Xxx
+					IntermodalAccessEgressParameterSet paramSetXxx = new IntermodalAccessEgressParameterSet();
+					//					paramSetXxx.setMode( TransportMode.walk ); // this does not work because sbb raptor treats it in a special way
+					paramSetXxx.setMode( "walk2" );
+					paramSetXxx.setRadius( 1000000 );
+					configRaptor.addIntermodalAccessEgress( paramSetXxx );
+					// (in principle, walk as alternative to drt will not work, since drt is always faster.  Need to give the ASC to the router!  However, with
+					// the reduced drt network we should be able to see differentiation.)
+				}
+				{
+					// drt
+					IntermodalAccessEgressParameterSet paramSetDrt = new IntermodalAccessEgressParameterSet();
+					paramSetDrt.setMode( TransportMode.drt );
+					paramSetDrt.setRadius( 1000000 );
+					configRaptor.addIntermodalAccessEgress( paramSetDrt );
+				}
+				if ( drt2 ){
+					IntermodalAccessEgressParameterSet paramSetDrt2 = new IntermodalAccessEgressParameterSet();
+					paramSetDrt2.setMode( "drt2" );
+					paramSetDrt2.setRadius( 1000000 );
+					//				paramSetDrt2.setPersonFilterAttribute( null );
+					//				paramSetDrt2.setStopFilterAttribute( null );
+					configRaptor.addIntermodalAccessEgress( paramSetDrt2 );
+				}
+			}
+
+		}
+
+		// === SCORING: ===
+
+		double margUtlTravPt = config.planCalcScore().getModes().get( TransportMode.pt ).getMarginalUtilityOfTraveling();;
+		{
+			PlanCalcScoreConfigGroup.ModeParams modeParams = new PlanCalcScoreConfigGroup.ModeParams(TransportMode.drt);
+			modeParams.setMarginalUtilityOfTraveling( margUtlTravPt );
+			config.planCalcScore().addModeParams(modeParams);
+		}
+		{
+				PlanCalcScoreConfigGroup.ModeParams modeParams = new PlanCalcScoreConfigGroup.ModeParams("drt2");
+				modeParams.setMarginalUtilityOfTraveling( margUtlTravPt );
+				config.planCalcScore().addModeParams(modeParams);
+		}
+		{
+			PlanCalcScoreConfigGroup.ModeParams modeParams = new PlanCalcScoreConfigGroup.ModeParams("walk2");
+			modeParams.setMarginalUtilityOfTraveling( margUtlTravPt );
+			config.planCalcScore().addModeParams(modeParams);
+		}
+
+		// === QSIM: ===
+
+		config.qsim().setSimStarttimeInterpretation( QSimConfigGroup.StarttimeInterpretation.onlyUseStarttime );
+		// yy why?  kai, jun'19
+
+		config.qsim().setMainModes( Arrays.asList( TransportMode.car, TransportMode.drt, "drt2") );
+		// yyyy buses use the car network and so that needs to be defined as network mode.   !!!! :-( :-(
+
+		// === VSP: ===
+
+		config.vspExperimental().setVspDefaultsCheckingLevel( VspExperimentalConfigGroup.VspDefaultsCheckingLevel.warn );
+
+		// ### SCENARIO: ###
+
+		Scenario scenario = PtAlongALineTest.createScenario(config , 100 );
+
+		PtAlongALineTest.addModeToAllLinksBtwnGivenNodes(scenario.getNetwork(), 0, 400, TransportMode.drt );
+		PtAlongALineTest.addModeToAllLinksBtwnGivenNodes( scenario.getNetwork(), 600, 1000, "drt2" );
+
+
+		// The following is for the _router_, not the qsim!  kai, jun'19
+		VehiclesFactory vf = scenario.getVehicles().getFactory();
+		if ( drt2 ) {
+			VehicleType vehType = vf.createVehicleType( Id.create( "drt2", VehicleType.class ) );
+			vehType.setMaximumVelocity( 25./3.6 );
+			scenario.getVehicles().addVehicleType( vehType );
+		}{
+			VehicleType vehType = vf.createVehicleType( Id.create( TransportMode.drt, VehicleType.class ) );
+			vehType.setMaximumVelocity( 25./3.6 );
+			scenario.getVehicles().addVehicleType( vehType );
+		}
+		{
+			// (does not work without; I don't really know why. kai)
+			VehicleType vehType = vf.createVehicleType( Id.create( TransportMode.car, VehicleType.class ) );
+			vehType.setMaximumVelocity( 25./3.6 );
+			scenario.getVehicles().addVehicleType( vehType );
+		}
+
+		scenario.getPopulation().getPersons().values().removeIf( person -> !person.getId().toString().equals( "3" ) );
+
+		// ### CONTROLER: ###
+
+		Controler controler = new Controler(scenario);
+
+		controler.addOverridingModule(new SwissRailRaptorModule() ) ;
+
+		// This will start otfvis.  Comment in if desired.
+		controler.addOverridingModule( new OTFVisLiveModule() );
+
+		controler.addOverridingModule( new AbstractModule(){
+			@Override public void install(){
+				this.addControlerListenerBinding().toInstance( new IterationStartsListener(){
+					@Inject private Population population ;
+					@Override public void notifyIterationStarts( IterationStartsEvent event ){
+						for( Person person : population.getPersons().values() ){
+
+							// output to help with debugging:
+							log.warn("") ;
+							log.warn("selected plan for personId=" + person.getId() ) ;
+							for( PlanElement planElement : person.getSelectedPlan().getPlanElements() ){
+								log.warn( planElement ) ;
+							}
+							log.warn("");
+
+							// the trip should contain a true pt leg but does not:
+							List<Leg> legs = TripStructureUtils.getLegs( person.getSelectedPlan() );
+							boolean problem = true ;
+							for( Leg leg : legs ){
+								if ( TransportMode.pt.equals( leg.getMode() ) ) {
+									problem = false ;
+									break ;
+								}
+							}
+							Assert.assertFalse( problem );
+
+						}
+					}
+				} ) ;
+			}
+		} ) ;
+
+		controler.run();
+	}
+
+	@Test
 	public void networkWalkDoesNotWorkWithRaptor() {
+		// test fails with null pointer exception
 
 		Config config = PtAlongALineTest.createConfig( utils.getOutputDirectory() );
 
