8c4aedc8d60b770e898640ed587ebfc09cf4d6d3
Michal Maciejewski <michal.mac@gmail.com>
2019-07-21 11:00:28 +0200

    generalise Battery.(dis)charge() to changeSoc()

--- contribs/ev/src/main/java/org/matsim/contrib/ev/charging/ChargingWithQueueingLogic.java
+++ contribs/ev/src/main/java/org/matsim/contrib/ev/charging/ChargingWithQueueingLogic.java
@@ -55,7 +55,7 @@ public class ChargingWithQueueingLogic implements ChargingLogic {
 			ElectricVehicle ev = evIter.next();
 			// with fast charging, we charge around 4% of SOC per minute,
 			// so when updating SOC every 10 seconds, SOC increases by less then 1%
-			ev.getBattery().charge(ev.getChargingPower().calcChargingPower(charger) * chargePeriod);
+			ev.getBattery().changeSoc(ev.getChargingPower().calcChargingPower(charger) * chargePeriod);
 
 			if (chargingStrategy.isChargingCompleted(ev)) {
 				evIter.remove();
