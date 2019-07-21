cdb269bd630047b72ce1a23776253f87569b7b0d
kainagel <nagel@vsp.tu-berlin.de>
2019-07-21 21:41:46 +0200

    fix test.  Simply run class

--- code-examples/src/test/java/org/matsim/codeexamples/integration/RunMultipleModesExampleTest.java
+++ code-examples/src/test/java/org/matsim/codeexamples/integration/RunMultipleModesExampleTest.java
@@ -3,7 +3,9 @@ package org.matsim.codeexamples.integration;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
+import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.config.Config;
+import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
 import org.matsim.testcases.MatsimTestUtils;
 
@@ -14,14 +16,17 @@ public class RunMultipleModesExampleTest{
 	@Test
 	public void testRun() {
 		try{
-			RunMultipleModesExample runner = new RunMultipleModesExample();
-			Config config = runner.prepareConfig();
+			Config config = RunMultipleModesExample.prepareConfig( null ) ;
 
 			config.controler().setOverwriteFileSetting( OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists );
 			config.controler().setOutputDirectory( utils.getOutputDirectory() );
-			config.controler().setLastIteration( 1 );
+			config.controler().setLastIteration( 2 );
 
-			runner.run();
+			Scenario scenario = RunMultipleModesExample.prepareScenario( config );
+
+			Controler controler = RunMultipleModesExample.prepareControler( scenario );
+
+			controler.run() ;
 		} catch ( Exception ee ) {
 			ee.printStackTrace();
 			Assert.fail("something went wrong; see stack trace" ) ;
