259af3c8d92da846fc320b0a16a51ce960254d0e
Marcel Rieser <rieser@simunto.com>
2019-08-08 22:08:12 +0200

    missed changed imports.

--- contribs/roadpricing/src/main/java/org/matsim/contrib/roadpricing/RoadPricingSchemeUsingTollFactor.java
+++ contribs/roadpricing/src/main/java/org/matsim/contrib/roadpricing/RoadPricingSchemeUsingTollFactor.java
@@ -20,11 +20,6 @@
 
 package org.matsim.contrib.roadpricing;
 
-import java.io.File;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
@@ -32,6 +27,11 @@ import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.contrib.roadpricing.RoadPricingSchemeImpl.Cost;
 import org.matsim.vehicles.Vehicle;
+
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author nagel
