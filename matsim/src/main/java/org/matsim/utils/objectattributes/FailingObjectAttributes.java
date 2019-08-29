7fe7d7273fc9063074e4517210bd48bb0dd393de
Thibaut Dubernet <thibaut.dubernet@ivt.baug.ethz.ch>
2019-08-29 12:18:13 +0200

    create class to help using the same refactoring approach as for population object attributes

---
+++ matsim/src/main/java/org/matsim/utils/objectattributes/FailingObjectAttributes.java
@@ -0,0 +1,52 @@
+package org.matsim.utils.objectattributes;
+
+import java.util.function.Function;
+
+@Deprecated // for refactoring only
+public class FailingObjectAttributes extends ObjectAttributes{
+	private final Function<String, String> msgFunction;
+
+	// This is only meant as a transitory refactoring device. For this reason,
+	// all "specialized" versions are created in static methods in this class,
+	// to keep it all contained here
+	private FailingObjectAttributes(Function<String, String> msgFunction) {
+		this.msgFunction = msgFunction;
+	}
+
+	public static FailingObjectAttributes createPersonAttributes() {
+		return new FailingObjectAttributes(
+				str -> "population.getPersonAttributes()." + str + " will be deprecated; use PopulationUtils" +
+				".get/put/...PersonAttribute... instead.  kai, may'19"
+		);
+	}
+
+	@Override public Object getAttribute( String personId, String key) {
+		throw new RuntimeException( msgFunction.apply("getAttribute"));
+	}
+	@Override public Object putAttribute( String personId, String key, Object value) {
+		throw new RuntimeException( msgFunction.apply("putAttribute"));
+	}
+	@Override public Object removeAttribute( String personId, String key ) {
+		throw new RuntimeException( msgFunction.apply("removeAttribute"));
+	}
+	@Override public void removeAllAttributes( String personId ) {
+		throw new RuntimeException( msgFunction.apply("removeAllAttributes"));
+	}
+	@Override public void clear() {
+		throw new RuntimeException( msgFunction.apply( "clear"));
+	}
+	@Override public String toString() {
+		return super.toString() ;
+	}
+
+	// for retrofitting.  Called from PopulationUtils only.  Remove eventually.  kai, may'19
+	Object getAttributeDirectly( String personId, String key ){
+		return super.getAttribute( personId, key ) ;
+	}
+	Object removeAttributeDirectly( String personId, String key ){
+		return super.removeAttribute( personId, key ) ;
+	}
+	void removeAllAttributesDirectly( String personId ) {
+		super.removeAllAttributes( personId );
+	}
+}
