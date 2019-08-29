8f1b55f24c6daba30b93d9635dd605a95cbce77b
kturner <martins-turner@vsp.tu-berlin.de>
2019-08-29 15:49:14 +0200

    working on VehicleWriteReadTest. Files are not identical yet, so tests are failing at the moment.

--- matsim/src/test/java/org/matsim/vehicles/VehicleWriteReadTest.java
+++ matsim/src/test/java/org/matsim/vehicles/VehicleWriteReadTest.java
@@ -1,8 +1,88 @@
 package org.matsim.vehicles;
 
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
+import org.junit.Test;
+import org.matsim.api.core.v01.Id;
+import org.matsim.core.utils.io.IOUtils;
 import org.matsim.testcases.MatsimTestUtils;
 
+import java.io.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class VehicleWriteReadTest{
+	private static final Logger log = Logger.getLogger( VehicleWriteReadTest.class ) ;
+
 	@Rule public MatsimTestUtils utils = new MatsimTestUtils() ;
+
+
+	private static final String TESTXML_v1 = "testVehicles_v1.xml";
+	private static final String OUTXML_v1 = "testOutputVehicles_v1.xml";
+
+	private static final String TESTXML_v2 = "testVehicles_v2.xml";
+	private static final String OUTXML_v2 = "testOutputVehicles_v2.xml";
+
+
+	@Before
+	public void setUp() throws IOException {
+		//----- V1 --------
+		//read it
+		Vehicles vehicles1 = VehicleUtils.createVehiclesContainer();
+		MatsimVehicleReader reader1 = new MatsimVehicleReader(vehicles1);
+		reader1.readFile(utils.getPackageInputDirectory() + TESTXML_v1);
+
+		//write it
+		VehicleWriterV1 writerV1 = new VehicleWriterV1(vehicles1);
+		writerV1.writeFile(utils.getOutputDirectory() + OUTXML_v1);
+
+		//----- V2 --------
+		//read it
+		Vehicles vehicles2 = VehicleUtils.createVehiclesContainer();
+		MatsimVehicleReader reader2 = new MatsimVehicleReader(vehicles2);
+		reader2.readFile(utils.getPackageInputDirectory() + TESTXML_v2);
+
+		//write it
+		VehicleWriterV2 writerV2 = new VehicleWriterV2(vehicles2);
+		writerV2.writeFile(utils.getOutputDirectory() + OUTXML_v2);
+	}
+
+	@Test
+	public void v1_isWrittenCorrect () throws FileNotFoundException, IOException {
+		assertTrue(new File(utils.getOutputDirectory() + OUTXML_v1).exists());
+
+		BufferedReader readerV1Input = IOUtils.getBufferedReader(utils.getPackageInputDirectory() + TESTXML_v1);
+		BufferedReader readerV1Output = IOUtils.getBufferedReader(utils.getOutputDirectory() + OUTXML_v1);
+
+		String lineInput;
+		String lineOutput;
+
+		while (((lineInput = readerV1Input.readLine()) != null) && ((lineOutput = readerV1Output.readLine()) != null)) {
+			log.info("Reading line... value in input file: " + lineInput + " , value in output File: "  + lineOutput);
+			assertEquals("Lines have different content: ", lineInput, lineOutput);
+		}
+		readerV1Input.close();
+		readerV1Output.close();
+	}
+
+	@Test
+	public void v2_isWrittenCorrect () throws FileNotFoundException, IOException {
+		assertTrue(new File(utils.getOutputDirectory() + OUTXML_v1).exists());
+
+		BufferedReader readerV2Input = IOUtils.getBufferedReader(utils.getPackageInputDirectory() + TESTXML_v2);
+		BufferedReader readerV2Output = IOUtils.getBufferedReader(utils.getOutputDirectory() + OUTXML_v2);
+
+		String lineInput;
+		String lineOutput;
+
+		while (((lineInput = readerV2Input.readLine()) != null) && ((lineOutput = readerV2Output.readLine()) != null)) {
+			log.info("Reading line... value in input file: " + lineInput + " , value in output File: "  + lineOutput);
+			assertEquals("Lines have different content: ", lineInput, lineOutput);
+		}
+		readerV2Input.close();
+		readerV2Output.close();
+	}
 }
