79c45b3cd3bc2c9b4f3a615e7f4aca4c8aa2a527
kainagel <nagel@vsp.tu-berlin.de>
2019-08-29 07:35:16 +0200

    start a write-read test for vehicles

---
+++ matsim/src/test/java/org/matsim/vehicles/VehicleWriteReadTest.java
@@ -0,0 +1,8 @@
+package org.matsim.vehicles;
+
+import org.junit.Rule;
+import org.matsim.testcases.MatsimTestUtils;
+
+public class VehicleWriteReadTest{
+	@Rule public MatsimTestUtils utils = new MatsimTestUtils() ;
+}
