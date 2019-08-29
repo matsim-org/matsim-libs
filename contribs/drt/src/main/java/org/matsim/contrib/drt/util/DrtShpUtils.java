76e5694714d499b6e5ff8b163724f7c70c12b782
leich <leich@vsp.tu-berlin.de>
2019-08-29 11:20:01 +0200

    add shape utils and network modification class, currently not used

---
+++ contribs/drt/src/main/java/org/matsim/contrib/drt/util/DrtShpUtils.java
@@ -0,0 +1,52 @@
+package org.matsim.contrib.drt.util;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.Point;
+import org.matsim.api.core.v01.Coord;
+import org.matsim.core.utils.geometry.geotools.MGC;
+import org.matsim.core.utils.gis.ShapeFileReader;
+import org.opengis.feature.simple.SimpleFeature;
+
+public class DrtShpUtils {
+	
+	public static List<Geometry> loadShapeFile(String shapeFile) {
+		List<Geometry> geometries = new ArrayList<>();
+
+		Collection<SimpleFeature> features = null;
+		if (new File(shapeFile).exists()) {
+			features = ShapeFileReader.getAllFeatures(shapeFile);	
+		} else {
+			try {
+				features = ShapeFileReader.getAllFeatures(new URL(shapeFile));
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if (features == null) throw new RuntimeException("Aborting...");
+		
+		for (SimpleFeature feature : features) {
+			geometries.add( (Geometry) feature.getDefaultGeometry() );
+		}
+		return geometries;
+	}
+
+	public static boolean isCoordInGeometries( Coord coord, List<Geometry> geometries ) {
+		Point p = MGC.coord2Point(coord);
+		
+		for (Geometry geometry : geometries) {
+			if (p.within(geometry)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
