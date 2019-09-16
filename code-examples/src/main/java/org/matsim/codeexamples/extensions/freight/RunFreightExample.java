a3e965c525caf62c252e052801a3e29872a1b793
Kai Martins-Turner <kt86@users.noreply.github.com>
2019-09-16 22:03:00 +0200

    Update RunFreightExample
    For MATSim13

--- code-examples/src/main/java/org/matsim/codeexamples/extensions/freight/RunFreightExample.java
+++ code-examples/src/main/java/org/matsim/codeexamples/extensions/freight/RunFreightExample.java
@@ -18,22 +18,10 @@
 
 package org.matsim.codeexamples.extensions.freight;
 
-import com.graphhopper.jsprit.analysis.toolbox.Plotter;
-import com.graphhopper.jsprit.analysis.toolbox.StopWatch;
-import com.graphhopper.jsprit.core.algorithm.VehicleRoutingAlgorithm;
-import com.graphhopper.jsprit.core.algorithm.box.Jsprit;
-import com.graphhopper.jsprit.core.algorithm.listener.VehicleRoutingAlgorithmListeners.Priority;
-import com.graphhopper.jsprit.core.problem.VehicleRoutingProblem;
-import com.graphhopper.jsprit.core.problem.solution.VehicleRoutingProblemSolution;
-import com.graphhopper.jsprit.core.util.Solutions;
 import org.matsim.api.core.v01.Scenario;
-import org.matsim.api.core.v01.network.Network;
 import org.matsim.contrib.freight.Freight;
-import org.matsim.contrib.freight.carrier.*;
-import org.matsim.contrib.freight.jsprit.MatsimJspritFactory;
-import org.matsim.contrib.freight.jsprit.NetworkBasedTransportCosts;
-import org.matsim.contrib.freight.jsprit.NetworkBasedTransportCosts.Builder;
-import org.matsim.contrib.freight.jsprit.NetworkRouter;
+import org.matsim.contrib.freight.FreightConfigGroup;
+import org.matsim.contrib.freight.carrier.CarrierPlanXmlWriterV2;
 import org.matsim.contrib.freight.utils.FreightUtils;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
@@ -59,7 +47,7 @@ public class RunFreightExample {
 		scenarioUrl = ExamplesUtils.getTestScenarioURL( "freight-chessboard-9x9" ) ;
 	}
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 
 		// ### config stuff: ###
 		Config config = createConfig();
@@ -69,22 +57,25 @@ public class RunFreightExample {
 		// ### scenario stuff: ###
 		Scenario scenario = ScenarioUtils.loadScenario(config);
 
-		//Building the Carriers, running jsprit for solving the VRP:
-		final Carriers carriers = jspritRun( config, scenario.getNetwork() );
-		scenario.addScenarioElement( FreightUtils.CARRIERS, carriers );
-
-		//final MATSim configurations and start of the MATSim-Run:
+		//MATSim configurations
+			// yyyy This needs to be done before the jspritRun, because otherwise it throws an Exception regarding the CarrierModule:
+			// "carriers are provided as scenario element AND per the CarrierModule constructor [...]" KMT Nov'20
 		final Controler controler = new Controler( scenario ) ;
 		Freight.configure( controler );
 
-		// run the matsim controler:
+		//Building the Carriers, running jsprit for solving the VRP:
+		jspritRun( scenario );
+
+		//start of the MATSim-Run:
 		controler.run();
 	}
+	
 
 	private static Config createConfig() {
 
 		Config config = ConfigUtils.loadConfig( IOUtils.extendUrl(scenarioUrl, "config.xml" ) );
 
+		//more general settings
 		config.controler().setOutputDirectory("./output/freight");
 		config.controler().setOverwriteFileSetting( OverwriteFileSetting.deleteDirectoryIfExists );
 		new OutputDirectoryHierarchy( config.controler().getOutputDirectory(), config.controler().getRunId(),
@@ -96,84 +87,40 @@ public class RunFreightExample {
 
 		config.controler().setLastIteration(0);
 		// yyyyyy iterations currently do not work; needs to be fixed.
+		
+		//freight settings
+		FreightConfigGroup freightConfigGroup = ConfigUtils.addOrGetModule( config, FreightConfigGroup.class ) ;
+	    freightConfigGroup.setCarriersFile( "singleCarrierFiveActivitiesWithoutRoutes.xml");
+	    freightConfigGroup.setCarriersVehicleTypesFile( "vehicleTypes.xml");
+	    //
 
 		return config;
 	}
 
-	private static Carriers jspritRun(Config config, Network network) {
+	private static void jspritRun(Scenario scenario) {
 
-		//create or load carrier vehicle types
-		CarrierVehicleTypes vehicleTypes = new CarrierVehicleTypes() ;
-		new CarrierVehicleTypeReader( vehicleTypes ).readURL( IOUtils.extendUrl(scenarioUrl, "vehicleTypes.xml" ) ) ;
+		//load carriers according to freight config
+		FreightUtils.loadCarriersAccordingToFreightConfig(scenario);
 
-		//create or laod the carrier(s) including assignment of vehicle types to the carrier(s)
-		Carriers carriers = new Carriers() ;
-		new CarrierPlanXmlReader( carriers ).readURL( IOUtils.extendUrl(scenarioUrl, "singleCarrierFiveActivitiesWithoutRoutes.xml" ) ) ;
 
-		// assign vehicle types to the carriers
-		new CarrierVehicleTypeLoader( carriers ).loadVehicleTypes( vehicleTypes ) ;
-
-		//### Output before jsprit run
-		new CarrierPlanXmlWriterV2(carriers).write( config.controler().getOutputDirectory() + "/jsprit_unplannedCarriers.xml") ;
+		//### Output before jsprit run (not necessary)
+		new CarrierPlanXmlWriterV2(FreightUtils.getCarriers(scenario)).write( scenario.getConfig().controler().getOutputDirectory() + "/jsprit_unplannedCarriers.xml") ;
 
 		//Solving the VRP (generate carrier's tour plans)
-		generateCarrierPlans(network, carriers, vehicleTypes, config);
-
-		//### Output after jsprit run
-		new CarrierPlanXmlWriterV2(carriers).write( config.controler().getOutputDirectory() + "/jsprit_plannedCarriers.xml") ;
-
-		return carriers;
-	}
-
-
-	/**
-	 * Creates and solves the Vehicle Routing Problem using jsprit
-	 * @param network
-	 * @param carriers
-	 * @param vehicleTypes
-	 * @param config
-	 */
-	private static void generateCarrierPlans(Network network, Carriers carriers, CarrierVehicleTypes vehicleTypes, Config config) {
-		Builder netBuilder = NetworkBasedTransportCosts.Builder.newInstance( network, vehicleTypes.getVehicleTypes().values() );
-
-		netBuilder.setTimeSliceWidth(1800) ; // !!!!, otherwise it will not do anything.
-		final NetworkBasedTransportCosts netBasedCosts = netBuilder.build() ;
-
-		//One independent VRP for each carrier.
-		for ( Carrier carrier : carriers.getCarriers().values() ) {
-			VehicleRoutingProblem.Builder vrpBuilder = MatsimJspritFactory.createRoutingProblemBuilder( carrier, network ) ;
-			vrpBuilder.setRoutingCost(netBasedCosts) ;
-			VehicleRoutingProblem vrp = vrpBuilder.build() ;
-
-			//Build algorithm out of the box
-			final Jsprit.Builder algoBuilder = Jsprit.Builder.newInstance( vrp );
-			algoBuilder.setProperty( Jsprit.Parameter.THREADS,"5" ) ;
-			VehicleRoutingAlgorithm vra = algoBuilder.buildAlgorithm();
-
-			//			// or read it from file
-//			final URL algorithmURL = IOUtils.extendUrl(scenarioUrl, "algorithm_v2.xml");
-//			VehicleRoutingAlgorithm vra = VehicleRoutingAlgorithms.readAndCreateAlgorithm(vrp, algorithmURL);
-//			//TODO initial soultion needed
-			
-			vra.getAlgorithmListeners().addListener(new StopWatch(), Priority.HIGH);
-			vra.setMaxIterations(100);
-			VehicleRoutingProblemSolution solution = Solutions.bestOf(vra.searchSolutions());
-
-			//Route the tour plan in MATSim
-			CarrierPlan newPlan = MatsimJspritFactory.createPlan(carrier, solution) ;
-			NetworkRouter.routePlan(newPlan,netBasedCosts) ;
-
-			carrier.setSelectedPlan(newPlan) ;
-
-			//Plot of jesprit solution
-			Plotter plotter = new Plotter(vrp,solution);
-			plotter.plot(config.controler().getOutputDirectory()+ "/jsprit_solution_" + carrier.getId().toString() +".png", carrier.getId().toString());
-
-			//Print results in console
-			//SolutionPrinter.print(vrp,solution,Print.VERBOSE);
-
+		try {
+			FreightUtils.runJsprit(scenario, ConfigUtils.addOrGetModule(scenario.getConfig(), FreightConfigGroup.class));
+		} catch (Exception e) {
+			throw new RuntimeException(e);
 		}
+
+
+		//### Output after jsprit run (not necessary)
+		new CarrierPlanXmlWriterV2(FreightUtils.getCarriers(scenario)).write( scenario.getConfig().controler().getOutputDirectory() + "/jsprit_plannedCarriers.xml") ;
+
+
 	}
+
+
 
 
 }
