cffd787c9de156c685138fcb29745d35db17d0ce
Michal Maciejewski <michal.mac@gmail.com>
2019-09-18 11:48:31 +0200

    add uam-only scenario

---
+++ src/main/java/uam/run/RunUamOnlyScenario.java
@@ -0,0 +1,71 @@
+/*
+ * *********************************************************************** *
+ * project: org.matsim.*
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2019 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** *
+ */
+
+package uam.run;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
+import org.matsim.contrib.dvrp.run.DvrpModule;
+import org.matsim.contrib.dvrp.run.DvrpQSimComponents;
+import org.matsim.contrib.otfvis.OTFVisLiveModule;
+import org.matsim.contrib.taxi.run.MultiModeTaxiConfigGroup;
+import org.matsim.contrib.taxi.run.MultiModeTaxiModule;
+import org.matsim.contrib.taxi.run.TaxiConfigGroup;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.vis.otfvis.OTFVisConfigGroup;
+
+/**
+ * @author Michal Maciejewski (michalm)
+ */
+public class RunUamOnlyScenario {
+	public static final String CONFIG_FILE_UAM_ONLY = "input/uam/uam_only_config.xml";
+
+	public static void run(String configFile, boolean otfvis, int lastIteration) {
+		// load config
+		Config config = ConfigUtils.loadConfig(configFile, new MultiModeTaxiConfigGroup(), new DvrpConfigGroup(),
+				new OTFVisConfigGroup());
+		config.controler().setLastIteration(lastIteration);
+
+		// load scenario
+		Scenario scenario = ScenarioUtils.loadScenario(config);
+
+		// setup controler
+		Controler controler = new Controler(scenario);
+
+		String mode = TaxiConfigGroup.getSingleModeTaxiConfig(config).getMode();
+		controler.addOverridingModule(new DvrpModule());
+		controler.addOverridingModule(new MultiModeTaxiModule());
+		controler.configureQSimComponents(DvrpQSimComponents.activateModes(mode));
+
+		if (otfvis) {
+			controler.addOverridingModule(new OTFVisLiveModule()); // OTFVis visualisation
+		}
+
+		// run simulation
+		controler.run();
+	}
+
+	public static void main(String[] args) {
+		run(CONFIG_FILE_UAM_ONLY, true, 0); // switch to 'true' to turn on visualisation
+	}
+}
