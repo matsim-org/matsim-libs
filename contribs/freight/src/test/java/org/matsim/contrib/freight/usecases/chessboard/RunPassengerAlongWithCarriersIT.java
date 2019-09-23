3b3d8b6a52368480c397b76d941ac11bd18464c5
kturner <martins-turner@vsp.tu-berlin.de>
2019-09-23 14:18:23 +0200

    fix freight example and (re)enable ignored IT

--- contribs/freight/src/test/java/org/matsim/contrib/freight/usecases/chessboard/RunPassengerAlongWithCarriersIT.java
+++ contribs/freight/src/test/java/org/matsim/contrib/freight/usecases/chessboard/RunPassengerAlongWithCarriersIT.java
@@ -1,13 +1,30 @@
 package org.matsim.contrib.freight.usecases.chessboard;
 
-import org.junit.Ignore;
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.matsim.core.config.Config;
+import org.matsim.testcases.MatsimTestUtils;
 
 public class RunPassengerAlongWithCarriersIT {
+   
+    @Rule
+    public MatsimTestUtils utils = new MatsimTestUtils() ;
 
-    @Test @Ignore
-    public void testPassengerAlongWithCarriers() {
-        RunPassengerAlongWithCarriers.main(new String[]{});
+    @Test
+    public void runChessboard() {
+        try{
+        	RunPassengerAlongWithCarriers abc = new RunPassengerAlongWithCarriers();
+            // ---
+            Config config = abc.prepareConfig();
+            config.controler().setLastIteration( 1 );
+            config.controler().setOutputDirectory( utils.getOutputDirectory() );
+            // ---
+            abc.run();
+        } catch (Exception ee ) {
+            ee.printStackTrace();
+            Assert.fail("something went wrong");
+        }
     }
 
 }
