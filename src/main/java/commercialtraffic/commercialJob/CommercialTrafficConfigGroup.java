ab461b2f69cc740cf9136953191d06a51955b6fe
Tilmann Schlenther <schlenther@vsp.tu-berlin.de>
2019-10-07 15:47:54 +0200

    moved input file declaration to upstream to FreightConfigGroup... may not be in matsim-master yet

--- src/main/java/commercialtraffic/commercialJob/CommercialTrafficConfigGroup.java
+++ src/main/java/commercialtraffic/commercialJob/CommercialTrafficConfigGroup.java
@@ -22,27 +22,12 @@ package commercialtraffic.commercialJob;/*
  */
 
 import org.matsim.core.config.Config;
-import org.matsim.core.config.ConfigGroup;
 import org.matsim.core.config.ReflectiveConfigGroup;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
-import java.net.URL;
 import java.util.Map;
 
 public class CommercialTrafficConfigGroup extends ReflectiveConfigGroup {
-
-
-    @NotBlank
-    private String carriersFile;
-    public static final String CARRIERSFILEDE = "carriersFile";
-    private static final String CARRIERSFILEDESC = "Freight Carriers File, according to MATSim freight contrib";
-
-
-    @NotBlank
-    private String carriersVehicleTypesFile;
-    public static final String CARRIERSVEHICLETYPED = "carriersVehicleTypeFile";
-    private static final String CARRIERSVEHICLETYPEDESC = "Carrier Vehicle Types file, according to MATSim freight contrib";
 
     @Positive
     private double firstLegTraveltimeBufferFactor = 2.0;
@@ -53,14 +38,6 @@ public class CommercialTrafficConfigGroup extends ReflectiveConfigGroup {
     public  static final String RUNJSPRIT = "runTourPlanning";
     private static final String RUNJSPRITDESC = "Defines whether JSprit is run. " +
             "If this is set to false, ChangeDeliveryOperator strategy must not be switched on and all carriers need to have at least one plan containing at least one tour.";
-
-    @Positive
-    private int jSpritTimeSliceWidth = 1800;
-    public static final String JSPRITTIMESLICEWIDTH = "jSpritTimeSliceWidth";
-    private static final String JSPRITTIMESLICEWIDTHDESC = "time slice width used in JSprit in seconds." +
-            " The smaller the value, the more precise the calculation of routing costs but the longer the computation time." +
-            " Default value is 1800 seconds.";
-
 
     @Positive
     private double zeroUtilityDelay = 1800;
@@ -87,48 +64,6 @@ public class CommercialTrafficConfigGroup extends ReflectiveConfigGroup {
         return (CommercialTrafficConfigGroup) config.getModules().get(GROUP_NAME);
     }
 
-
-    /**
-     * @return -- {@value #CARRIERSFILEDESC}
-     */
-//    @StringGetter(CARRIERSFILEDE)
-    public String getCarriersFile() {
-        return carriersFile;
-    }
-
-    URL getCarriersFileUrl(URL context) {
-        return ConfigGroup.getInputFileURL(context, this.carriersFile);
-    }
-
-    /**
-     * @param -- {@value #CARRIERSFILEDESC}
-     */
-//    @StringSetter(CARRIERSFILEDE)
-    public void setCarriersFile(String carriersFile) {
-        this.carriersFile = carriersFile;
-    }
-
-    /**
-     * @return -- {@value #CARRIERSVEHICLETYPEDESC}
-     */
-//    @StringGetter(CARRIERSVEHICLETYPED)
-    public String getCarriersVehicleTypesFile() {
-        return carriersVehicleTypesFile;
-    }
-
-    URL getCarriersVehicleTypesFileUrl(URL context) {
-        return ConfigGroup.getInputFileURL(context, this.carriersVehicleTypesFile);
-    }
-
-
-    /**
-     * @param -- {@value #CARRIERSVEHICLETYPEDESC}
-     */
-//    @StringSetter(CARRIERSVEHICLETYPED)
-    public void setCarriersVehicleTypesFile(String carriersVehicleTypesFile) {
-        this.carriersVehicleTypesFile = carriersVehicleTypesFile;
-    }
-
     /**
      * @return -- {@value #FIRSTLEGBUFFERDESC}
      */
@@ -151,24 +86,7 @@ public class CommercialTrafficConfigGroup extends ReflectiveConfigGroup {
 //    @StringGetter(RUNJSPRIT)
     public boolean getRunTourPlanning(){ return runTourPlanning; }
 
-    /**
-     * @return jspritTimeSliceWidth --{@value #JSPRITTIMESLICEWIDTHDESC}
-     */
-//    @StringGetter(JSPRITTIMESLICEWIDTH)
-    int getJspritTimeSliceWidth() {
-        return jSpritTimeSliceWidth;
-    }
-
-    /**
-     * @param jspritTimeSliceWidth --{@value #JSPRITTIMESLICEWIDTHDESC}
-     */
-//    @StringSetter(JSPRITTIMESLICEWIDTH)
-    public void setjSpritTimeSliceWidth(int jspritTimeSliceWidth) {
-        this.jSpritTimeSliceWidth = jspritTimeSliceWidth;
-    }
-
     // ---
-
     /**
      * @return zeroUtilityDelay --{@value #ZEROUTILDELAYDESC}
      */
@@ -220,10 +138,7 @@ public class CommercialTrafficConfigGroup extends ReflectiveConfigGroup {
     @Override
     public Map<String, String> getComments() {
         Map<String, String> map = super.getComments();
-        map.put(CARRIERSFILEDE, CARRIERSFILEDESC);
-        map.put(CARRIERSVEHICLETYPED, CARRIERSVEHICLETYPEDESC);
         map.put(FIRSTLEGBUFFER, FIRSTLEGBUFFERDESC);
-        map.put(JSPRITTIMESLICEWIDTH,JSPRITTIMESLICEWIDTHDESC);
         map.put(RUNJSPRIT,RUNJSPRITDESC);
         map.put(MAXDELIVERYSCORE, MAXDELIVERYSCOREDESC);
         map.put(MINDELIVERYSCORE, MINDELIVERYSCOREDESC);
