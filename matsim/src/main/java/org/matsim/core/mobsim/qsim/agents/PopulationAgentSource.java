9d8ae6c95859b6068c1eee1ba99cd747c75f971a
kainagel <nagel@vsp.tu-berlin.de>
2019-11-19 22:51:16 +0100

    fix but where second vehicle for same mode was not allowed

--- matsim/src/main/java/org/matsim/core/mobsim/qsim/agents/PopulationAgentSource.java
+++ matsim/src/main/java/org/matsim/core/mobsim/qsim/agents/PopulationAgentSource.java
@@ -26,6 +26,7 @@ import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.population.*;
 import org.matsim.core.config.Config;
+import org.matsim.core.config.groups.QSimConfigGroup;
 import org.matsim.core.controler.PrepareForSimImpl;
 import org.matsim.core.gbl.Gbl;
 import org.matsim.core.mobsim.framework.AgentSource;
@@ -68,7 +69,7 @@ public final class PopulationAgentSource implements AgentSource {
 			insertVehicles(p);
 		}
 	}
-	
+	private static int cnt = 5 ;
 	private void insertVehicles(Person person) {
 		// this is called in every iteration.  So if a route without a vehicle id is found (e.g. after mode choice),
 		// then the id is generated here.  kai/amit, may'18
@@ -96,14 +97,32 @@ public final class PopulationAgentSource implements AgentSource {
 			
 			// determine the vehicle (not the same as just its ID):
 
-			// we have seen the mode before in the plan:
-			if ( seenModes.keySet().contains( leg.getMode() ) ) {
-				if (vehicleId == null && route != null) {
-					vehicleId = seenModes.get(leg.getMode());
-					route.setVehicleId(vehicleId);
-					// yyyy what is the meaning of route==null? kai, jun'18
+			final QSimConfigGroup.VehiclesSource vehiclesSource = qsim.getScenario().getConfig().qsim().getVehiclesSource();
+
+			if ( vehiclesSource!= QSimConfigGroup.VehiclesSource.fromVehiclesData ){
+				// without this condition, it is surely wrong when the vehicles come from the vehicles data ... because a second vehicle for
+				// the same mode is not placed.  kai, nov'19
+
+				// yyyy I am not sure if at this point vehicleId actually can be null ... because the above VehicleUtils.getVehicleId(...,
+				// mode) already should contain a map of all the vehicles per person.   ???  kai, nov'19
+
+				// we have seen the mode before in the plan:
+				if( seenModes.keySet().contains( leg.getMode() ) ){
+					if( vehicleId == null && route != null ){
+						vehicleId = seenModes.get( leg.getMode() );
+						route.setVehicleId( vehicleId );
+						// yyyy what is the meaning of route==null? kai, jun'18
+
+						if ( cnt > 0 ) {
+							log.warn( "encountered vehicleId=null where I am not sure how and why this should happen ..." );
+							cnt-- ;
+							if ( cnt==0 ) {
+								log.warn(  Gbl.FUTURE_SUPPRESSED );
+							}
+						}
+					}
+					continue;
 				}
-				continue;
 			}
 			
 			// if we are here, we haven't seen the mode before in this plan
@@ -113,7 +132,23 @@ public final class PopulationAgentSource implements AgentSource {
 			
 			// find the vehicle from the vehicles container.  It should be there, see automatic vehicle creation in PrepareForSim.
 			Vehicle vehicle = qsim.getScenario().getVehicles().getVehicles().get(vehicleId);
-			Gbl.assertNotNull(vehicle);
+			if ( vehicle==null ) {
+				String msg = "Could not get the requested vehicle with ID=" + vehicleId + " from the vehicles container. " ;
+				switch ( vehiclesSource ) {
+					case defaultVehicle:
+					case modeVehicleTypesFromVehiclesData:
+						msg += "You are using the config switch qsim.vehiclesSource=" + vehiclesSource.name() + "; this should have worked so " +
+								   "please report under matsim.org/faq ." ;
+						break;
+					case fromVehiclesData:
+						msg += "You are using the config switch qsim.vehiclesSource=" + vehiclesSource.name() + "; with that setting, you have to" +
+								   " provide all needed vehicles yourself." ;
+						break;
+					default:
+						throw new RuntimeException( Gbl.NOT_IMPLEMENTED ) ;
+				}
+				throw new RuntimeException( msg ) ;
+			}
 				
 			// find the link ID of where to place the vehicle:
 			Id<Link> vehicleLinkId = findVehicleLink(person);
