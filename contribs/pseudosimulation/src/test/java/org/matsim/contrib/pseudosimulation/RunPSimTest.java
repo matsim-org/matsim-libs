b91e9a61aee29008b454d664c8b10f0a9bd984fb
kainagel <nagel@vsp.tu-berlin.de>
2019-11-27 18:09:30 +0100

    material around RunPSimTest

--- contribs/pseudosimulation/src/test/java/org/matsim/contrib/pseudosimulation/RunPSimTest.java
+++ contribs/pseudosimulation/src/test/java/org/matsim/contrib/pseudosimulation/RunPSimTest.java
@@ -53,18 +53,37 @@ public class RunPSimTest {
 		//identify selector strategies
 		Field[] selectors = DefaultPlanStrategiesModule.DefaultSelector.class.getDeclaredFields();
 		List<String> selectorNames = new ArrayList<>();
-		for (Field selector : selectors) {
-			selectorNames.add(selector.toString());
-		}
+//		for (Field selector : selectors) {
+//			selectorNames.add(selector.toString());
+//			logger.warn( selector.toString() );
+//		}
+		// yyyyyy does not work as designed
+		selectorNames.add( DefaultPlanStrategiesModule.DefaultSelector.ChangeExpBeta );
+		selectorNames.add( DefaultPlanStrategiesModule.DefaultSelector.BestScore );
+		selectorNames.add( DefaultPlanStrategiesModule.DefaultSelector.KeepLastSelected );
+		selectorNames.add( DefaultPlanStrategiesModule.DefaultSelector.SelectPathSizeLogit );
+		selectorNames.add( DefaultPlanStrategiesModule.DefaultSelector.SelectRandom );
+		selectorNames.add( DefaultPlanStrategiesModule.DefaultSelector.SelectExpBeta );
 
 		//lower the weight of non-selector strategies, as we will run many iters
 		for( StrategyConfigGroup.StrategySettings settings : config.strategy().getStrategySettings() ){
-			if( !selectorNames.contains( settings.getStrategyName() ) )
-				settings.setWeight( settings.getWeight() / 20 );
+			if( !selectorNames.contains( settings.getStrategyName() ) ){
+				logger.warn( settings.getStrategyName() );
+				settings.setWeight( settings.getWeight() * 20 );
+				settings.setDisableAfter( 18 );
+			}
 		}
-		config.controler().setOutputDirectory(utils.getOutputDirectory());
+		// yyyyyy I think that the above also includes the selector strategies.  ??
+//		System.exit( -1);
+
+		final String outDir = utils.getOutputDirectory();
+		config.controler().setOutputDirectory( outDir );
 		config.controler().setLastIteration(20);
 //		config.controler().setDumpDataAtEnd(false);
+
+//		config.strategy().setFractionOfIterationsToDisableInnovation( 0.8 ); // crashes
+
+
 		RunPSim runPSim = new RunPSim(config, pSimConfigGroup);
 		ExecScoreTracker execScoreTracker = new ExecScoreTracker(runPSim.getMatsimControler());
 		runPSim.getMatsimControler().addControlerListener(execScoreTracker);
@@ -81,8 +100,10 @@ public class RunPSimTest {
 		double psimScore = execScoreTracker.executedScore;
 		logger.info("RunPSim score was " + psimScore);
 		Population popExpected = PopulationUtils.createPopulation( config ) ;
-//		PopulationUtils.readPopulation( popExpected, utils.getClassInputDirectory() + "" );
-//		new PopulationComparison().compare( runPSim.getMatsimControler().getScenario().getPopulation() ) ;
+		PopulationUtils.readPopulation( popExpected, utils.getInputDirectory() + "/output_plans.xml.gz" );
+		Population popActual = PopulationUtils.createPopulation( config );
+		PopulationUtils.readPopulation( popActual, outDir + "/output_plans.xml.gz" );
+		new PopulationComparison().compare( popExpected, popActual ) ;
 		Assert.assertEquals("RunPsim score changed.", 134.52369453719413d, psimScore, MatsimTestUtils.EPSILON);
 //		Assert.assertEquals("RunPsim score changed.", 132.73129073101293d, psimScore, MatsimTestUtils.EPSILON);
 	}
