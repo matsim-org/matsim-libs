f076769962628273cdbf45131f0d70ea18b1abaa
kainagel <nagel@vsp.tu-berlin.de>
2019-12-09 11:26:57 +0100

    add some info to vsp openberlinscenario

---
+++ contribs/vsp/src/main/java/playground/vsp/openberlinscenario/package-info.java
@@ -0,0 +1,13 @@
+package playground.vsp.openberlinscenario;
+
+
+/**
+ * The {@link playground.vsp.openberlinscenario.cemdap.input} package is geared towards German input data.
+ *
+ * The {@link playground.vsp.openberlinscenario.cemdap.output} package would work everywhere together with cemdap output.
+ *
+ * The {@link playground.vsp.openberlinscenario.planmodification} package is used after the cemdap run in order to enhance the plans
+ * before they are given to matsim.
+ *
+ * kai/dominik, dec'19
+ */
