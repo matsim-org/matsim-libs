ef0a1753d49b44b22a7060804f9c134b650ef03a
sbraun92 <apple1992A>
2019-12-10 13:57:23 +0100

    renaming stuff

--- contribs/signals/src/test/java/org/matsim/contrib/signals/network/SignalsAndLanesOsmNetworkReaderIT.java
+++ contribs/signals/src/test/java/org/matsim/contrib/signals/network/SignalsAndLanesOsmNetworkReaderIT.java
@@ -49,37 +49,37 @@ public class SignalsAndLanesOsmNetworkReaderIT {
         prepareContext(config);
 
         // create a scenario
-        Scenario scenario1 = ScenarioUtils.createScenario(config);
+        Scenario scenario = ScenarioUtils.createScenario(config);
 
-        scenario1.addScenarioElement(SignalsData.ELEMENT_NAME, new SignalsDataLoader(config).loadSignalsData());
+        scenario.addScenarioElement(SignalsData.ELEMENT_NAME, new SignalsDataLoader(config).loadSignalsData());
         // pick network, lanes and signals data from the scenario
-        SignalsData signalsData1 = (SignalsData) scenario1.getScenarioElement(SignalsData.ELEMENT_NAME);
+        SignalsData signalsData = (SignalsData) scenario.getScenarioElement(SignalsData.ELEMENT_NAME);
 
-        Lanes lanes1 = scenario1.getLanes();
-        Network network1 = scenario1.getNetwork();
+        Lanes lanes = scenario.getLanes();
+        Network network = scenario.getNetwork();
 
-        SignalsAndLanesOsmNetworkReader reader1 = new SignalsAndLanesOsmNetworkReader(network1, ct, signalsData1, lanes1);
+        SignalsAndLanesOsmNetworkReader signalReader = new SignalsAndLanesOsmNetworkReader(network, ct, signalsData, lanes);
 
 
         //TODO Parameterize - but than we have 2^5= 32 combinations
-        reader1.setMinimizeSmallRoundabouts(true);
-        reader1.setMergeOnewaySignalSystems(true);
-        reader1.setUseRadiusReduction(true);
-        reader1.setAllowUTurnAtLeftLaneOnly(true);
-        reader1.setMakePedestrianSignals(true);
+        signalReader.setMinimizeSmallRoundabouts(true);
+        signalReader.setMergeOnewaySignalSystems(true);
+        signalReader.setUseRadiusReduction(true);
+        signalReader.setAllowUTurnAtLeftLaneOnly(true);
+        signalReader.setMakePedestrianSignals(false);
 
-        reader1.setBoundingBox(51.7464, 14.3087, 51.7761, 14.3639); // setting Bounding Box for signals and lanes
+        signalReader.setBoundingBox(51.7464, 14.3087, 51.7761, 14.3639); // setting Bounding Box for signals and lanes
         // (south,west,north,east)
 
 
-        reader1.parse(inputOSM);
-        new NetworkCleaner().run(network1);
-        new LanesAndSignalsCleaner().run(scenario1);
+        signalReader.parse(inputOSM);
+        new NetworkCleaner().run(network);
+        new LanesAndSignalsCleaner().run(scenario);
 
 
-        int noNodes1 = network1.getNodes().size();
+        int noNodes1 = network.getNodes().size();
         int noNodes2 = this.origNetwork.getNodes().size();
-        int noLinks1 = network1.getLinks().size();
+        int noLinks1 = network.getLinks().size();
         int noLinks2 = this.origNetwork.getLinks().size();
 
 //        Assert.assertEquals("Number of Links", noLinks1,noLinks2);
