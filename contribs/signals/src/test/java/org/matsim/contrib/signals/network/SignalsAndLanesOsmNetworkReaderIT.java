4bd1dc1026fb81cf016abb5919359f09b6838067
Söhnke Thorben Braun <apple1992A>
2020-01-28 15:40:58 +0100

    Add a Coordinate Comparison between Benchmark Reader and Working Version

--- contribs/signals/src/test/java/org/matsim/contrib/signals/network/SignalsAndLanesOsmNetworkReaderIT.java
+++ contribs/signals/src/test/java/org/matsim/contrib/signals/network/SignalsAndLanesOsmNetworkReaderIT.java
@@ -15,12 +15,15 @@ import org.matsim.contrib.signals.data.SignalsDataLoader;
 import org.matsim.contrib.signals.data.consistency.LanesAndSignalsCleaner;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.network.NetworkUtils;
+import org.matsim.core.network.NetworkUtilsTest;
 import org.matsim.core.network.algorithms.NetworkCleaner;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.geometry.CoordinateTransformation;
 import org.matsim.core.utils.geometry.transformations.TransformationFactory;
 import org.matsim.core.utils.io.OsmNetworkReader;
 import org.matsim.lanes.data.Lanes;
+import org.matsim.population.algorithms.TestsUtil;
 import org.matsim.testcases.MatsimTestUtils;
 
 import java.util.HashSet;
@@ -41,6 +44,110 @@ public class SignalsAndLanesOsmNetworkReaderIT {
     Lanes origLanes;
     CoordinateTransformation ct;
 
+
+    @Test
+    public void testAgainstBenchmark(){
+        // create a config
+
+        this.ct = TransformationFactory.getCoordinateTransformation(TransformationFactory.WGS84,
+                TransformationFactory.WGS84_UTM33N);
+
+        Config configBase = ConfigUtils.createConfig();
+        Config config = ConfigUtils.createConfig();
+        SignalSystemsConfigGroup signalSystemsConfigGroupBase = ConfigUtils.addOrGetModule(configBase,
+                SignalSystemsConfigGroup.GROUPNAME, SignalSystemsConfigGroup.class);
+
+        SignalSystemsConfigGroup signalSystemsConfigGroup = ConfigUtils.addOrGetModule(config,
+                SignalSystemsConfigGroup.GROUPNAME, SignalSystemsConfigGroup.class);
+
+        signalSystemsConfigGroupBase.setUseSignalSystems(true);
+        signalSystemsConfigGroup.setUseSignalSystems(true);
+
+        config.qsim().setUseLanes(true);
+        configBase.qsim().setUseLanes(true);
+
+        // create a scenario
+        Scenario scenarioBase = ScenarioUtils.createScenario(configBase);
+        Scenario scenario = ScenarioUtils.createScenario(config);
+
+
+
+        scenarioBase.addScenarioElement(SignalsData.ELEMENT_NAME, new SignalsDataLoader(configBase).loadSignalsData());
+        scenario.addScenarioElement(SignalsData.ELEMENT_NAME, new SignalsDataLoader(config).loadSignalsData());
+
+        // pick network, lanes and signals data from the scenario
+        SignalsData signalsDataBase = (SignalsData) scenarioBase.getScenarioElement(SignalsData.ELEMENT_NAME);
+        SignalsData signalsData = (SignalsData) scenario.getScenarioElement(SignalsData.ELEMENT_NAME);
+
+        Lanes lanesBase = scenarioBase.getLanes();
+        Lanes lanes = scenario.getLanes();
+
+        Network networkBase = scenarioBase.getNetwork();
+        Network network = scenario.getNetwork();
+
+        SignalsAndLanesOsmNetworkReaderBenchmark signalReaderBase = new SignalsAndLanesOsmNetworkReaderBenchmark(networkBase, ct, signalsDataBase, lanesBase);
+        SignalsAndLanesOsmNetworkReader signalReader = new SignalsAndLanesOsmNetworkReader(network, ct, signalsData, lanes);
+
+        signalReaderBase.setBoundingBox(51.7464, 14.3087, 51.7761, 14.3639);
+        signalReader.setBoundingBox(51.7464, 14.3087, 51.7761, 14.3639);
+
+        signalReaderBase.parse(inputOSM);
+        new NetworkCleaner().run(networkBase);
+        new LanesAndSignalsCleaner().run(scenarioBase);
+
+        signalReader.parse(inputOSM);
+        new NetworkCleaner().run(network);
+        new LanesAndSignalsCleaner().run(scenario);
+
+
+
+        //Test if Networks are equal:
+        int noNodes1 = networkBase.getNodes().size();
+        int noNodes2 = network.getNodes().size();
+
+        Assert.assertEquals(noNodes1,noNodes2);
+
+        int noLinks1 = networkBase.getLinks().size();
+        int noLinks2 = network.getLinks().size();
+
+        Assert.assertEquals(noLinks1,noLinks2);
+
+        int noLanes1 = lanesBase.getLanesToLinkAssignments().size();
+        int noLanes2 = lanes.getLanesToLinkAssignments().size();
+
+        Assert.assertEquals(noLanes1,noLanes2);
+
+        int noSignalSystems1 = signalsDataBase.getSignalSystemsData().getSignalSystemData().size();
+        int noSignalSystems2 = signalsData.getSignalSystemsData().getSignalSystemData().size();
+
+        Assert.assertEquals(noSignalSystems1,noSignalSystems2);
+
+        //Check in detail if all nodes are identical by coordinate
+        Set nodeCoordinatesBase = new HashSet();
+        Set nodeCoordinates = new HashSet();
+        for(Node node : networkBase.getNodes().values()){
+            nodeCoordinatesBase.add(node.getCoord());
+        }
+        boolean allNodesInWorkingVersionAreInBase = true;
+        for (Node node: network.getNodes().values()){
+            nodeCoordinates.add(node.getCoord());
+            if(!(nodeCoordinatesBase.contains(node.getCoord()))){
+                allNodesInWorkingVersionAreInBase = false;
+                System.out.println("Node "+node.getId()+" is not in Benchmark");
+            }
+        }
+        boolean allNodesInBaseAreInWorkingVersion = true;
+        for (Node node: networkBase.getNodes().values()){
+            if(!(nodeCoordinates.contains(node.getCoord()))){
+                allNodesInBaseAreInWorkingVersion = false;
+                System.out.println("Node "+node.getId()+"  from Benchmark is not in Working Version");
+            }
+        }
+
+
+        Assert.assertTrue("Not all nodes of working version where in network of Benchmark",allNodesInWorkingVersionAreInBase);
+        Assert.assertTrue("Not all nodes of benchmark version where in network of working version",allNodesInBaseAreInWorkingVersion);
+    }
 
     @Ignore
     public void testLinkLaneStructure(){
