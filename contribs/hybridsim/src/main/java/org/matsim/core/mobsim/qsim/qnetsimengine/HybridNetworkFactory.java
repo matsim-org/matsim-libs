06df3e2fed70ef19fdee80e66e4ea63670cef1d5
Daniel Roeder <daniel.roeder@senozon.com>
2020-02-26 08:40:55 +0100

    fix imports

--- contribs/hybridsim/src/main/java/org/matsim/core/mobsim/qsim/qnetsimengine/HybridNetworkFactory.java
+++ contribs/hybridsim/src/main/java/org/matsim/core/mobsim/qsim/qnetsimengine/HybridNetworkFactory.java
@@ -30,7 +30,7 @@ import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.config.groups.QSimConfigGroup;
 import org.matsim.core.mobsim.framework.MobsimTimer;
 import org.matsim.core.mobsim.qsim.interfaces.AgentCounter;
-import org.matsim.core.mobsim.qsim.qnetsimengine.QNetsimEngine.NetsimInternalInterface;
+import org.matsim.core.mobsim.qsim.qnetsimengine.QNetsimEngineI.NetsimInternalInterface;
 import org.matsim.core.mobsim.qsim.qnetsimengine.linkspeedcalculator.DefaultLinkSpeedCalculator;
 import org.matsim.core.mobsim.qsim.qnetsimengine.linkspeedcalculator.LinkSpeedCalculator;
 import org.matsim.core.mobsim.qsim.qnetsimengine.vehicle_handler.DefaultVehicleHandler;
