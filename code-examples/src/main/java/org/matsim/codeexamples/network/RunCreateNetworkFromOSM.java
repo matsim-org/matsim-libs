16b51095b3dc1e36f29233c9012e90ff395b415f
Janekdererste <laudan@tu-berlin.de>
2020-04-10 08:58:13 +0200

    Update Example for osm-network-reader

--- code-examples/src/main/java/org/matsim/codeexamples/network/RunCreateNetworkFromOSM.java
+++ code-examples/src/main/java/org/matsim/codeexamples/network/RunCreateNetworkFromOSM.java
@@ -1,22 +1,23 @@
 package org.matsim.codeexamples.network;
 
-import org.locationtech.jts.geom.Geometry;
-import org.matsim.api.core.v01.Coord;
+import org.locationtech.jts.geom.prep.PreparedGeometry;
+import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Network;
-import org.matsim.core.network.NetworkUtils;
+import org.matsim.contrib.osm.networkReader.LinkProperties;
+import org.matsim.contrib.osm.networkReader.OsmTags;
+import org.matsim.contrib.osm.networkReader.SupersonicOsmNetworkReader;
 import org.matsim.core.network.algorithms.NetworkCleaner;
 import org.matsim.core.network.io.NetworkWriter;
 import org.matsim.core.utils.geometry.CoordinateTransformation;
-import org.matsim.core.utils.geometry.geotools.MGC;
 import org.matsim.core.utils.geometry.transformations.TransformationFactory;
-import org.matsim.core.utils.gis.ShapeFileReader;
-import org.matsim.core.utils.io.OsmNetworkReader;
-import org.opengis.feature.simple.SimpleFeature;
+import org.matsim.utils.gis.shp2matsim.ShpGeometryUtils;
 
+import java.net.MalformedURLException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Example on how to convert osm data from e.g. http://download.geofabrik.de into a MATSim network. This examle puts all
@@ -28,17 +29,14 @@ import java.util.Collection;
 public class RunCreateNetworkFromOSM {
 
 	private static String UTM32nAsEpsg = "EPSG:25832";
-	private static Path input = Paths.get("/path/to/your/input/data.osm");
+	private static Path input = Paths.get("/path/to/your/input/data.osm.pbf");
 	private static Path filterShape = Paths.get("/path/to/your/filter/shape-file.shp");
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedURLException {
 		new RunCreateNetworkFromOSM().create();
 	}
 
-	private void create() {
-
-		// create an empty network which will contain the parsed network data
-		Network network = NetworkUtils.createNetwork();
+	private void create() throws MalformedURLException {
 
 		// choose an appropriate coordinate transformation. OSM Data is in WGS84. When working in central Germany,
 		// EPSG:25832 or EPSG:25833 as target system is a good choice
@@ -46,45 +44,40 @@ public class RunCreateNetworkFromOSM {
 				TransformationFactory.WGS84, UTM32nAsEpsg
 		);
 
+		// load the geometries of the shape file, so they can be used as a filter during network creation
+		// using PreparedGeometry instead of Geometry increases speed a lot (usually)
+		List<PreparedGeometry> filterGeometries = ShpGeometryUtils.loadPreparedGeometries(filterShape.toUri().toURL());
+
 		// create an osm network reader with a filter
-		OsmNetworkReader reader = new OsmNetworkReader(network, transformation, true, true);
-		reader.addOsmFilter(new NetworkFilter(filterShape));
+		SupersonicOsmNetworkReader reader = new SupersonicOsmNetworkReader.Builder()
+				.setCoordinateTransformation(transformation)
+				.setIncludeLinkAtCoordWithHierarchy((coord, hierarchyLevel) -> {
+
+					// take all links which are motorway, trunk, or primary-street regardless of their location
+					if (hierarchyLevel <= LinkProperties.LEVEL_PRIMARY) return true;
+
+					// whithin the shape, take all links which are contained in the osm-file
+					return ShpGeometryUtils.isCoordInPreparedGeometries(coord, filterGeometries);
+				})
+				.setAfterLinkCreated((link, osmTags, direction) -> {
+
+					// if the original osm-link contains a cycleway tag, add bicycle as allowed transport mode
+					// although for serious bicycle networks use OsmBicycleNetworkReader
+					if (osmTags.containsKey(OsmTags.CYCLEWAY)) {
+						Set<String> modes = new HashSet<>(link.getAllowedModes());
+						modes.add(TransportMode.bike);
+						link.setAllowedModes(modes);
+					}
+				})
+				.build();
 
 		// the actual work is done in this call. Depending on the data size this may take a long time
-		reader.parse(input.toString());
+		Network network = reader.read(input.toString());
 
 		// clean the network to remove unconnected parts where agents might get stuck
 		new NetworkCleaner().run(network);
 
 		// write out the network into a file
 		new NetworkWriter(network).write("/path/to/your/output/network.xml.gz");
-	}
-
-	/**
-	 * Includes motorways and primary roads for the whole dataset. If roads are contained within the supplied shape, also
-	 * secondary and residential roads are included
-	 */
-	private static class NetworkFilter implements OsmNetworkReader.OsmFilter {
-
-		private final Collection<Geometry> geometries = new ArrayList<>();
-
-		NetworkFilter(Path shapeFile) {
-			for (SimpleFeature feature : ShapeFileReader.getAllFeatures(shapeFile.toString())) {
-				geometries.add((Geometry) feature.getDefaultGeometry());
-			}
-		}
-
-		@Override
-		public boolean coordInFilter(Coord coord, int hierarchyLevel) {
-			// hierachy levels 1 - 3 are motorways and primary roads, as well as their trunks
-			if (hierarchyLevel <= 4) return true;
-
-			// if coord is within the supplied shape use every street above level of tracks and cycle ways
-			return hierarchyLevel <= 8 && containsCoord(coord);
-		}
-
-		private boolean containsCoord(Coord coord) {
-			return geometries.stream().anyMatch(geom -> geom.contains(MGC.coord2Point(coord)));
-		}
 	}
 }
