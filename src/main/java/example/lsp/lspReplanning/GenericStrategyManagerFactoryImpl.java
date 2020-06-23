08b86792e36a01b08c18c8a412467809518cc848
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2020-06-23 12:34:50 +0200

    rename packages: starting with lower cases

---
+++ src/main/java/example/lsp/lspReplanning/GenericStrategyManagerFactoryImpl.java
@@ -0,0 +1,23 @@
+package example.lsp.lspReplanning;
+
+import org.matsim.core.replanning.GenericStrategyManager;
+
+import lsp.LSP;
+import lsp.LSPPlan;
+import lsp.ShipmentAssigner;
+import lsp.replanning.LSPPlanStrategyManagerFactory;
+
+/*package-private*/ class GenericStrategyManagerFactoryImpl implements LSPPlanStrategyManagerFactory {
+
+	@Override
+	public GenericStrategyManager<LSPPlan, LSP> createStrategyManager(LSP lsp) {
+		GenericStrategyManager<LSPPlan, LSP> strategyManager = new GenericStrategyManager<LSPPlan, LSP>();
+		ShipmentAssigner tomorrowAssigner = new TomorrowAssigner();
+		tomorrowAssigner.setLSP(lsp);
+
+		//Warum wird hier an der Stelle im GenericStrategyManager eine spezifische Strategy hinzugefügt? KMT Jun'20
+		strategyManager.addStrategy(new TomorrowShipmentAssignerStrategyFactory(tomorrowAssigner).createStrategy(), null, 1);
+		return strategyManager;
+	}
+
+}
---
+++ src/main/java/example/lsp/lspReplanning/TomorrowShipmentAssignerStrategyFactory.java
@@ -0,0 +1,57 @@
+package example.lsp.lspReplanning;
+
+import java.util.Collection;
+
+import org.matsim.core.replanning.GenericPlanStrategy;
+import org.matsim.core.replanning.GenericPlanStrategyImpl;
+import org.matsim.core.replanning.ReplanningContext;
+import org.matsim.core.replanning.modules.GenericPlanStrategyModule;
+import org.matsim.core.replanning.selectors.BestPlanSelector;
+
+import lsp.LSP;
+import lsp.LSPPlan;
+import lsp.ShipmentAssigner;
+import lsp.shipment.LSPShipment;
+
+/*package-private*/ class TomorrowShipmentAssignerStrategyFactory  {
+	
+	private ShipmentAssigner assigner;
+
+	/*package-private*/ TomorrowShipmentAssignerStrategyFactory(ShipmentAssigner assigner) {
+		this.assigner = assigner;
+	}
+
+	/*package-private*/ GenericPlanStrategy<LSPPlan, LSP> createStrategy(){
+		GenericPlanStrategyImpl<LSPPlan, LSP> strategy = new GenericPlanStrategyImpl<LSPPlan, LSP>( new BestPlanSelector<LSPPlan, LSP>());
+		
+		GenericPlanStrategyModule<LSPPlan> tomorrowModule = new GenericPlanStrategyModule<LSPPlan>() {
+
+			@Override
+			public void prepareReplanning(ReplanningContext replanningContext) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void handlePlan(LSPPlan plan) {
+				plan.setAssigner(assigner);
+				LSP lsp = assigner.getLSP();
+				Collection<LSPShipment> shipments = lsp.getShipments();
+				for(LSPShipment shipment : shipments) {
+					assigner.assignShipment(shipment);
+				}
+			}
+
+			@Override
+			public void finishReplanning() {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		};
+		
+		strategy.addStrategyModule(tomorrowModule);
+		return strategy;
+	}
+	
+}
