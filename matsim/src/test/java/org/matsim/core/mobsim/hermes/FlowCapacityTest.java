0bcbba9054441b529da1cd9006ea299cf25349ee
Joschka Bischoff <joschka.bischoff@sbb.ch>
2020-07-01 14:40:10 +0200

    revert test to original state

--- matsim/src/test/java/org/matsim/core/mobsim/hermes/FlowCapacityTest.java
+++ matsim/src/test/java/org/matsim/core/mobsim/hermes/FlowCapacityTest.java
@@ -31,8 +31,7 @@ public class FlowCapacityTest {
 	@Test
 	public void testFlowCapacityDriving() {
 		Fixture f = new Fixture();
-		f.scenario.getConfig().hermes().setStorageCapacityFactor(0.1);
-		f.scenario.getConfig().hermes().setFlowCapacityFactor(0.1);
+
 		// add a lot of persons with legs from link1 to link3, starting at 6:30
 		for (int i = 1; i <= 12000; i++) {
 			Person person = PopulationUtils.getFactory().createPerson(Id.create(i, Person.class));
