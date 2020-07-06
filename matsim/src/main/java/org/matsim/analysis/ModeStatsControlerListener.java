08af697aee51edb6f6ae11058ba4bb9519bffe29
Jakob Rehmann <jakob.rehmann@gmail.com>
2020-07-06 21:04:35 +0200

    made class and method (getModeHistories) final

--- matsim/src/main/java/org/matsim/analysis/ModeStatsControlerListener.java
+++ matsim/src/main/java/org/matsim/analysis/ModeStatsControlerListener.java
@@ -56,7 +56,7 @@ import org.matsim.core.utils.io.UncheckedIOException;
  *
  * @author mrieser
  */
-public class ModeStatsControlerListener implements StartupListener, IterationEndsListener, 
+public final class ModeStatsControlerListener implements StartupListener, IterationEndsListener,
 ShutdownListener {
 
 	public static final String FILENAME_MODESTATS = "modestats";
@@ -189,7 +189,7 @@ ShutdownListener {
 
 	}
 
-	public Map<String, Map<Integer, Double>> getModeHistories() {
+	public final Map<String, Map<Integer, Double>> getModeHistories() {
 		return Collections.unmodifiableMap( this.modeHistories ) ;
 	}
 
