df45293d7edebf9233ec0309c53e4d8f83ff796d
Jakob Rehmann <jakob.rehmann@gmail.com>
2020-07-06 12:59:58 +0200

    added getter for modeHistories

--- matsim/src/main/java/org/matsim/analysis/ModeStatsControlerListener.java
+++ matsim/src/main/java/org/matsim/analysis/ModeStatsControlerListener.java
@@ -22,13 +22,8 @@ package org.matsim.analysis;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
 
 import javax.inject.Inject;
 
@@ -193,4 +188,9 @@ ShutdownListener {
 		}
 
 	}
+
+	public Map<String, Map<Integer, Double>> getModeHistories() {
+		return Collections.unmodifiableMap( this.modeHistories ) ;
+	}
+
 }
