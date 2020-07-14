319aab583d53365240a27272a72d06a32ae8265d
u229187 <u229187@sbb.ch>
2020-07-14 09:33:01 +0200

    use builder for PlanStrategyImpl

--- matsim/src/main/java/org/matsim/core/replanning/strategies/SubtourModeChoice.java
+++ matsim/src/main/java/org/matsim/core/replanning/strategies/SubtourModeChoice.java
@@ -19,17 +19,17 @@
 
 package org.matsim.core.replanning.strategies;
 
+import javax.inject.Inject;
+import javax.inject.Provider;
 import org.matsim.core.config.groups.GlobalConfigGroup;
 import org.matsim.core.config.groups.SubtourModeChoiceConfigGroup;
 import org.matsim.core.replanning.PlanStrategy;
 import org.matsim.core.replanning.PlanStrategyImpl;
+import org.matsim.core.replanning.PlanStrategyImpl.Builder;
 import org.matsim.core.replanning.modules.ReRoute;
 import org.matsim.core.replanning.selectors.RandomPlanSelector;
 import org.matsim.core.router.TripRouter;
 import org.matsim.facilities.ActivityFacilities;
-
-import javax.inject.Inject;
-import javax.inject.Provider;
 
 public class SubtourModeChoice implements Provider<PlanStrategy> {
 
@@ -40,10 +40,10 @@ public class SubtourModeChoice implements Provider<PlanStrategy> {
 
     @Override
 	public PlanStrategy get() {
-		PlanStrategyImpl strategy = new PlanStrategyImpl(new RandomPlanSelector());
-		strategy.addStrategyModule(new org.matsim.core.replanning.modules.SubtourModeChoice(tripRouterProvider, globalConfigGroup, subtourModeChoiceConfigGroup));
-		strategy.addStrategyModule(new ReRoute(facilities, tripRouterProvider, globalConfigGroup));
-		return strategy;
+		PlanStrategyImpl.Builder builder = new Builder(new RandomPlanSelector<>());
+		builder.addStrategyModule(new org.matsim.core.replanning.modules.SubtourModeChoice(tripRouterProvider, globalConfigGroup, subtourModeChoiceConfigGroup));
+		builder.addStrategyModule(new ReRoute(facilities, tripRouterProvider, globalConfigGroup));
+		return builder.build();
 	}
 
 }
