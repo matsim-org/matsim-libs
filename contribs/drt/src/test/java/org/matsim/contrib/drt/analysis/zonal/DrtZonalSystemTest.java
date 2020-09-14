8fae7f6225ab2c129b6caad359f377378e7ac588
hacas <hacas@DESKTOP-E1PQH7N>
2020-09-14 09:37:36 +0200

    New Test for Service Area

--- contribs/drt/src/test/java/org/matsim/contrib/drt/analysis/zonal/DrtZonalSystemTest.java
+++ contribs/drt/src/test/java/org/matsim/contrib/drt/analysis/zonal/DrtZonalSystemTest.java
@@ -21,13 +21,20 @@
 package org.matsim.contrib.drt.analysis.zonal;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.matsim.contrib.drt.analysis.zonal.DrtGridUtilsTest.createNetwork;
 import static org.matsim.contrib.drt.analysis.zonal.DrtZonalSystem.createFromPreparedGeometries;
 
 import org.junit.Test;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Polygon;
 import org.locationtech.jts.geom.prep.PreparedGeometry;
+import org.locationtech.jts.geom.prep.PreparedGeometryFactory;
 import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Link;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -52,17 +59,19 @@ public class DrtZonalSystemTest {
 	@Test
 	public void test_gridWithinServiceArea(){
 		// abgucken von createGridFromNetwork
-		List<PreparedGeometry> serviceArea = createServiceArea();
-
+		Coordinate min = new Coordinate(-500, 500);
+		Coordinate max = new Coordinate(1500, 1500);
+		List<PreparedGeometry> serviceArea = createServiceArea(min,max);
 		DrtZonalSystem zonalSystem = createFromPreparedGeometries(createNetwork(),
 				DrtGridUtils.createGridFromNetworkWithinServiceArea(createNetwork(), 100, serviceArea));
-
-
-
+		
 		//teste anzahl zonen
-
+		assertEquals(4, zonalSystem.getZones().size());
+		
+		
 		//gib mir zone für einen link außerhalb und prüfe ob null
-		assertThat(zonalSystem.getZoneForLinkId(linkAUßerhalbServiceArea).isEqualTo(null);
+		Id<Link> id = Id.createLinkId("da");
+		assertThat(zonalSystem.getZoneForLinkId(id).equals(null));
 	}
 
 	@Test
@@ -70,6 +79,20 @@ public class DrtZonalSystemTest {
 		//baue eine serviceArea, die ganz woanders als das Netzwerk liegt
 
 		//teste, dass drtZonalSystem keine Zone hat
+	}
+	
+	public List<PreparedGeometry> createServiceArea(Coordinate min, Coordinate max){
+		GeometryFactory gf = new GeometryFactory();
+		PreparedGeometryFactory preparedGeometryFactory = new PreparedGeometryFactory();
+		List<PreparedGeometry> ServiceArea =  new ArrayList<>();
+		Coordinate p1 = new Coordinate(min.x, min.y);
+		Coordinate p2 = new Coordinate(min.x, max.y);
+		Coordinate p3 = new Coordinate(max.x, max.y);
+		Coordinate p4 = new Coordinate(max.x, min.y);
+		Coordinate[] ca = { p1, p2, p3, p4, p1 };
+		Polygon polygon = new Polygon(gf.createLinearRing(ca), null, gf);
+		ServiceArea.add(preparedGeometryFactory.create(polygon));
+		return ServiceArea;
 	}
 
 
