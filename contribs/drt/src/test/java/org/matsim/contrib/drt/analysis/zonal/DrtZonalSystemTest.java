7f0120f706efb30a7c4238ea7d80083577d444d1
tschlenther <schlenther@vsp.tu-berlin.de>
2020-09-16 13:52:23 +0200

    english comments

--- contribs/drt/src/test/java/org/matsim/contrib/drt/analysis/zonal/DrtZonalSystemTest.java
+++ contribs/drt/src/test/java/org/matsim/contrib/drt/analysis/zonal/DrtZonalSystemTest.java
@@ -59,38 +59,33 @@ public class DrtZonalSystemTest {
 
 	@Test
 	public void test_gridWithinServiceArea(){
-		// abgucken von createGridFromNetwork
 		Coordinate min = new Coordinate(-500, 500);
 		Coordinate max = new Coordinate(1500, 1500);
 		List<PreparedGeometry> serviceArea = createServiceArea(min,max);
 		Map<String, PreparedGeometry> grid = DrtGridUtils.createGridFromNetworkWithinServiceArea(createNetwork(), 100, serviceArea);
-		DrtZonalSystem zonalSystem = createFromPreparedGeometries(createNetwork(), 
+		DrtZonalSystem zonalSystem = createFromPreparedGeometries(createNetwork(),
 				grid);
 
-		
-		//teste anzahl zonen
 		assertEquals(3, zonalSystem.getZones().size());
-		
-		
-		//gib mir zone für einen link außerhalb und prüfe ob null
+
+		//link 'da' is outside of the service area
 		Id<Link> id = Id.createLinkId("da");
 		assertThat(zonalSystem.getZoneForLinkId(id) == null);
 	}
 
 	@Test
 	public void test_noZonesWithoutLinks(){
-		//baue eine serviceArea, die ganz woanders als das Netzwerk liegt
 		Coordinate min = new Coordinate(1500, 1500);
 		Coordinate max = new Coordinate(2500, 2500);
 		List<PreparedGeometry> serviceArea = createServiceArea(min,max);
 		Map<String, PreparedGeometry> grid = DrtGridUtils.createGridFromNetworkWithinServiceArea(createNetwork(), 100, serviceArea);
-		DrtZonalSystem zonalSystem = createFromPreparedGeometries(createNetwork(), 
+		DrtZonalSystem zonalSystem = createFromPreparedGeometries(createNetwork(),
 				grid);
 
-		//teste, dass drtZonalSystem keine Zone hat
+		//service area is off the network - so we should have 0 zones..
 		assertEquals(0, zonalSystem.getZones().size());
 	}
-	
+
 	public List<PreparedGeometry> createServiceArea(Coordinate min, Coordinate max){
 		GeometryFactory gf = new GeometryFactory();
 		PreparedGeometryFactory preparedGeometryFactory = new PreparedGeometryFactory();
