d08292fb18e865434c6b52361658082b746b85e0
Söhnke Thorben Braun <apple1992A>
2020-10-23 15:28:30 +0200

    osm4j test writes now lanes and signal data

--- contribs/signals/src/test/java/org/matsim/contrib/signals/network/basicTests.java
+++ contribs/signals/src/test/java/org/matsim/contrib/signals/network/basicTests.java
@@ -17,6 +17,7 @@ import org.matsim.api.core.v01.network.Network;
 import org.matsim.contrib.signals.SignalSystemsConfigGroup;
 import org.matsim.contrib.signals.data.SignalsData;
 import org.matsim.contrib.signals.data.SignalsDataLoader;
+import org.matsim.contrib.signals.data.SignalsScenarioWriter;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.network.io.NetworkWriter;
@@ -24,6 +25,7 @@ import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.geometry.CoordinateTransformation;
 import org.matsim.core.utils.geometry.transformations.IdentityTransformation;
 import org.matsim.lanes.data.Lanes;
+import org.matsim.lanes.data.LanesWriter;
 import org.matsim.testcases.MatsimTestUtils;
 
 import java.io.IOException;
@@ -72,32 +74,61 @@ public class basicTests {
         Node node21 = new Node(21, 200., 0.);
         Node node25 = new Node(25,200.,400.);
 
+        Node signal1 = new Node(31, 190., 200., Arrays.asList(new Tag("highway", "traffic_signals")));
+        Node signal2 = new Node(32, 210., 200., Arrays.asList(new Tag("highway", "traffic_signals")));
+        Node signal3 = new Node(33, 200., 190., Arrays.asList(new Tag("highway", "traffic_signals")));
+        Node signal4 = new Node(34, 200., 210., Arrays.asList(new Tag("highway", "traffic_signals")));
 
         TLongArrayList nodeWay1 = new TLongArrayList(new long[]{node11.getId(),
-                                                                node13.getId(),
-                                                                node15.getId()});
+                                                                signal1.getId(),
+                                                                node13.getId()});
         TLongArrayList nodeWay2 = new TLongArrayList(new long[]{node21.getId(),
-                                                                node13.getId(),
-                                                                node25.getId()});
+                                                                signal3.getId(),
+                                                                node13.getId()});
 
+        TLongArrayList nodeWay3 = new TLongArrayList(new long[]{node13.getId(),
+                                                                signal4.getId(),
+                                                                node25.getId()});
+        TLongArrayList nodeWay4 = new TLongArrayList(new long[]{node13.getId(),
+                                                                signal2.getId(),
+                                                                node15.getId()});
 
         List<Way> ways = new LinkedList<>();
 
-        Way way1 = new Way(1,nodeWay1,Arrays.asList(new Tag("highway", "motorway"), new Tag("oneway","no")));
-        Way way2 = new Way(2,nodeWay2,Arrays.asList(new Tag("highway", "motorway"), new Tag("oneway","no")));
-
-
-
-
-
-
+        Way way1 = new Way(1,nodeWay1, Arrays.asList(new Tag("highway", "motorway"),
+                new Tag("oneway","no"),
+                new Tag("lanes","3"),
+                new Tag("turn:lanes","left|through|right"),
+                new Tag("crossing","no")));
         ways.add(way1);
+
+        Way way2 = new Way(2,nodeWay2,Arrays.asList(new Tag("highway", "motorway"),
+                new Tag("oneway","no"),
+                new Tag("turn:lanes","left|through|right"),
+                new Tag("lanes","3"),
+                new Tag("crossing","no")));
         ways.add(way2);
+        Way way3 = new Way(3,nodeWay3,Arrays.asList(
+                new Tag("highway", "motorway"),
+                new Tag("oneway","no"),
+                new Tag("turn:lanes","left|through|right"),
+                new Tag("lanes","3"),
+                new Tag("crossing","no")
+        ));
+        ways.add(way3);
+        Way way4 = new Way(4,nodeWay4,Arrays.asList(
+                new Tag("highway", "motorway"),
+                new Tag("oneway","no"),
+                new Tag("turn:lanes","left|through|right"),
+                new Tag("lanes","3"),
+                new Tag("crossing","no")
+        ));
+        ways.add(way4);
 
 
         OsmData osmData = new OsmData(Arrays.asList(node11, node13, node15,
-                                                    node21, node25),
-                                                    Arrays.asList(way1, way2));
+                                                    node21, node25, signal1, signal2, signal3,signal4),
+                                                    Arrays.asList(way1, way2, way3, way4));
 
         Path file = Paths.get(matsimTestUtils.getOutputDirectory(), "singleJunction.xml");
 
@@ -114,7 +145,6 @@ public class basicTests {
 
         // create a scenario
         Scenario scenario = ScenarioUtils.createScenario(config);
-
         scenario.addScenarioElement(SignalsData.ELEMENT_NAME, new SignalsDataLoader(config).loadSignalsData());
         // pick network, lanes and signals data from the scenario
         SignalsData signalsData = (SignalsData) scenario.getScenarioElement(SignalsData.ELEMENT_NAME);
@@ -129,7 +159,16 @@ public class basicTests {
         signalReader.setMakePedestrianSignals(false);
 
         signalReader.parse(file.toAbsolutePath().toString());
-        new NetworkWriter(network).write(file.getParent().toAbsolutePath().toString() + "\\network.xml");
+
+        String parentPath = file.getParent().toAbsolutePath().toString();
+        new NetworkWriter(network).write(parentPath + "\\network.xml");
+        new LanesWriter(lanes).write(parentPath + "\\lanes.xml");
+        SignalsScenarioWriter signalsWriter = new SignalsScenarioWriter();
+        signalsWriter.setSignalSystemsOutputFilename(parentPath + "\\signalSystems.xml");
+        signalsWriter.setSignalGroupsOutputFilename(parentPath + "\\signalGroups.xml");
+        signalsWriter.setSignalControlOutputFilename(parentPath + "\\signalControl.xml");
+        signalsWriter.writeSignalsData(scenario);
+
 
     }
 
