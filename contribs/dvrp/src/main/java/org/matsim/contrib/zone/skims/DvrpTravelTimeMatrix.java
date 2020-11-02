c78c71e6843737c4cff1339893edd9222253890b
Michal Maciejewski <michal.mac@gmail.com>
2020-11-02 12:02:10 +0100

    query DVRP-global TT matrix with node-node pairs

--- contribs/dvrp/src/main/java/org/matsim/contrib/zone/skims/DvrpTravelTimeMatrix.java
+++ contribs/dvrp/src/main/java/org/matsim/contrib/zone/skims/DvrpTravelTimeMatrix.java
@@ -20,15 +20,12 @@
 
 package org.matsim.contrib.zone.skims;
 
-import java.util.Map;
-
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.network.Node;
 import org.matsim.contrib.dvrp.router.TimeAsTravelDisutility;
 import org.matsim.contrib.zone.SquareGridSystem;
 import org.matsim.contrib.zone.ZonalSystems;
 import org.matsim.contrib.zone.Zone;
-import org.matsim.core.router.util.TravelTime;
 import org.matsim.core.trafficmonitoring.FreeSpeedTravelTime;
 
 import ch.sbb.matsim.analysis.skims.FloatMatrix;
@@ -37,18 +34,18 @@ import ch.sbb.matsim.analysis.skims.FloatMatrix;
  * @author Michal Maciejewski (michalm)
  */
 public class DvrpTravelTimeMatrix {
+	private final SquareGridSystem gridSystem;
 	private final FloatMatrix<Zone> freeSpeedTravelTimeMatrix;
 
 	public DvrpTravelTimeMatrix(Network dvrpNetwork, DvrpTravelTimeMatrixParams params, int numberOfThreads) {
-		SquareGridSystem gridSystem = new SquareGridSystem(dvrpNetwork.getNodes().values(), params.getCellSize());
-		Map<Zone, Node> centralNodes = ZonalSystems.computeMostCentralNodes(dvrpNetwork.getNodes().values(),
-				gridSystem);
-		TravelTime travelTime = new FreeSpeedTravelTime();
+		gridSystem = new SquareGridSystem(dvrpNetwork.getNodes().values(), params.getCellSize());
+		var centralNodes = ZonalSystems.computeMostCentralNodes(dvrpNetwork.getNodes().values(), gridSystem);
+		var travelTime = new FreeSpeedTravelTime();
 		freeSpeedTravelTimeMatrix = TravelTimeMatrices.calculateTravelTimeMatrix(dvrpNetwork, centralNodes, 0,
 				travelTime, new TimeAsTravelDisutility(travelTime), numberOfThreads);
 	}
 
-	public float getFreeSpeedTravelTime(Zone fromZone, Zone toZone) {
-		return freeSpeedTravelTimeMatrix.get(fromZone, toZone);
+	public float getFreeSpeedTravelTime(Node fromNode, Node toNode) {
+		return freeSpeedTravelTimeMatrix.get(gridSystem.getZone(fromNode), gridSystem.getZone(toNode));
 	}
 }
