2248d201cb67de8aa1157908598c8d60d96c23be
leich <leich@vsp.tu-berlin.de>
2020-11-06 23:37:49 +0100

    write out .shp with zone geometries and statistics

--- contribs/drt/src/main/java/org/matsim/contrib/drt/analysis/zonal/DrtZonalWaitTimesAnalyzer.java
+++ contribs/drt/src/main/java/org/matsim/contrib/drt/analysis/zonal/DrtZonalWaitTimesAnalyzer.java
@@ -27,14 +27,23 @@ import java.text.DecimalFormatSymbols;
 import java.util.*;
 
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.Polygon;
 import org.matsim.contrib.drt.analysis.DrtRequestAnalyzer;
 import org.matsim.contrib.drt.analysis.DrtRequestAnalyzer.PerformedRequestEventSequence;
 import org.matsim.contrib.drt.run.DrtConfigGroup;
 import org.matsim.core.controler.events.IterationEndsEvent;
+import org.matsim.core.controler.events.ShutdownEvent;
 import org.matsim.core.controler.listener.IterationEndsListener;
+import org.matsim.core.controler.listener.ShutdownListener;
+import org.matsim.core.utils.geometry.geotools.MGC;
+import org.matsim.core.utils.gis.ShapeFileWriter;
 import org.matsim.core.utils.io.IOUtils;
+import org.opengis.feature.simple.SimpleFeature;
 
-public final class DrtZonalWaitTimesAnalyzer implements IterationEndsListener {
+public final class DrtZonalWaitTimesAnalyzer implements IterationEndsListener, ShutdownListener {
 
 	private final DrtConfigGroup drtCfg;
 	private final DrtRequestAnalyzer requestAnalyzer;
@@ -129,5 +138,70 @@ public final class DrtZonalWaitTimesAnalyzer implements IterationEndsListener {
 			}
 		}
 		return zoneStats;
+	}
+
+	/** Write shp file with last iteration's stats */
+	@Override
+	public void notifyShutdown(ShutdownEvent event) {
+		String crs = event.getServices().getConfig().global().getCoordinateSystem();
+		Collection<SimpleFeature> features = convertGeometriesToSimpleFeatures(crs);
+		String fileName = event.getServices()
+				.getControlerIO()
+				.getOutputFilename("drt_waitStats" + "_" + drtCfg.getMode() + "_zonal.shp");
+		ShapeFileWriter.writeGeometries(features, fileName);
+	}
+
+	private Collection<SimpleFeature> convertGeometriesToSimpleFeatures(String targetCoordinateSystem) {
+		SimpleFeatureTypeBuilder simpleFeatureBuilder = new SimpleFeatureTypeBuilder();
+		simpleFeatureBuilder.setCRS(MGC.getCRS(targetCoordinateSystem));
+		simpleFeatureBuilder.setName("drtZoneFeature");
+		// note: column names may not be longer than 10 characters. Otherwise the name is cut after the 10th character and the avalue is NULL in QGis
+		simpleFeatureBuilder.add("the_geom", Polygon.class);
+		simpleFeatureBuilder.add("zoneIid", String.class);
+		simpleFeatureBuilder.add("centerX", Double.class);
+		simpleFeatureBuilder.add("centerY", Double.class);
+		simpleFeatureBuilder.add("nRequests", Integer.class);
+		simpleFeatureBuilder.add("sumWait", Double.class);
+		simpleFeatureBuilder.add("meanWait", Double.class);
+		simpleFeatureBuilder.add("min", Double.class);
+		simpleFeatureBuilder.add("max", Double.class);
+		simpleFeatureBuilder.add("p95", Double.class);
+		simpleFeatureBuilder.add("p90", Double.class);
+		simpleFeatureBuilder.add("p80", Double.class);
+		simpleFeatureBuilder.add("p75", Double.class);
+		simpleFeatureBuilder.add("p50", Double.class);
+		SimpleFeatureBuilder builder = new SimpleFeatureBuilder(simpleFeatureBuilder.buildFeatureType());
+
+		Collection<SimpleFeature> features = new ArrayList<>();
+
+		Map<String, DescriptiveStatistics> zoneStats = createZonalStats();
+
+		for (DrtZone zone: zones.getZones().values()) {
+			Object[] routeFeatureAttributes = new Object[14];
+			Geometry geometry = zone.getPreparedGeometry().getGeometry();
+			DescriptiveStatistics stats = zoneStats.get(zone.getId());
+			routeFeatureAttributes[0] = geometry;
+			routeFeatureAttributes[1] = zone.getId();
+			routeFeatureAttributes[2] = zone.getCentroid().getX();
+			routeFeatureAttributes[3] = zone.getCentroid().getY();
+			routeFeatureAttributes[4] = stats.getN();
+			routeFeatureAttributes[5] = stats.getSum();
+			routeFeatureAttributes[6] = stats.getMean();
+			routeFeatureAttributes[7] = stats.getMin();
+			routeFeatureAttributes[8] = stats.getMax();
+			routeFeatureAttributes[9] = stats.getPercentile(95);
+			routeFeatureAttributes[10] = stats.getPercentile(90);
+			routeFeatureAttributes[11] = stats.getPercentile(80);
+			routeFeatureAttributes[12] = stats.getPercentile(75);
+			routeFeatureAttributes[13] = stats.getPercentile(50);
+
+			try {
+				features.add(builder.buildFeature(zone.getId(), routeFeatureAttributes));
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return features;
 	}
 }
