8a637dda37554b1f9fde53e94a95f2bd79748012
tschlenther <schlenther@vsp.tu-berlin.de>
2020-12-11 10:02:02 +0100

    Flow efficiency calculation (#1283)
    * include the following vehicle in the calculation of the flow efficiency consumption.
    pass QVehicle instead of Vehicle in order to derive information on the next link.
    TODO: - get and store previousVehicle for calculation of accumulatedInFlowCap
    
    * extended flow efficiency calculate to include previous vehicle and time difference
    
    * typos
    
    * improve error message
    
    * store vehicle and time of last entry into buffer
    
    * store flowEfficiency of vehicles in the buffer
    
    * rename variable
    
    * rename parameter and improve java doc
    
    Co-authored-by: Christian Rakow <rakow@vsp.tu-berlin.de>
    Co-authored-by: rakow <rakow@users.noreply.github.com>

--- matsim/src/main/java/org/matsim/core/mobsim/qsim/qnetsimengine/flow_efficiency/FlowEfficiencyCalculator.java
+++ matsim/src/main/java/org/matsim/core/mobsim/qsim/qnetsimengine/flow_efficiency/FlowEfficiencyCalculator.java
@@ -1,8 +1,25 @@
 package org.matsim.core.mobsim.qsim.qnetsimengine.flow_efficiency;
 
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Link;
-import org.matsim.vehicles.Vehicle;
+import org.matsim.core.mobsim.qsim.qnetsimengine.QVehicle;
+import org.matsim.lanes.Lane;
 
+import javax.annotation.Nullable;
+
+/**
+ * Calculator for the amount of "flow capacity" that a vehicle consumes when traveling over a link.
+ */
 public interface FlowEfficiencyCalculator {
-	double calculateFlowEfficiency(Vehicle vehicle, Link link);
+
+    /**
+     * Calculate the amount of flow capacity consumed by a vehicle.
+     *
+     * @param qVehicle         the vehicle that consumes efficiency.
+     * @param previousQVehicle the vehicle previously traveling on this link before {@code qVehicle}, may be null
+     * @param timeGapToPreviousVeh time gap to the previous vehicle traveling on this {@code link}. May be null if {@code qVehicle} is the first to travel on {@code link}.
+     * @param link             the link the {@code qVehicle} is currently on.
+     * @param laneId           id of the lane the {@code qVehicle} is traveling on
+     */
+    double calculateFlowEfficiency(QVehicle qVehicle, @Nullable QVehicle previousQVehicle, @Nullable Double timeGapToPreviousVeh, Link link, Id<Lane> laneId);
 }
