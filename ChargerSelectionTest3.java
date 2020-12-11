ce4404c68a5c52b12c7ec71a330d691ea92d18cb
Jonas116 <mcjonas@live.de>
2020-12-11 11:50:39 +0100

    Test nach Bugfix

--- ChargerSelectionTest3.java
+++ ChargerSelectionTest3.java
@@ -41,11 +41,11 @@ public class ChargerSelectionTest3 {
 		EvConfigGroup evConfigGroup = new EvConfigGroup();
 		evConfigGroup.setVehiclesFile("this is not important because we use standard matsim vehicles");
 		evConfigGroup.setTimeProfiles(true);
-//		evConfigGroup.setChargersFile("C:/Users/admin/Desktop/chargers.xml");
-		evConfigGroup.setChargersFile("chessboard-chargers-1-plugs-1.xml");
+		evConfigGroup.setChargersFile("C:/Users/admin/Desktop/chargers.xml");
+		//evConfigGroup.setChargersFile("chessboard-chargers-1-plugs-1.xml");
 		Config config = ConfigUtils.loadConfig(IOUtils.extendUrl(ExamplesUtils.getTestScenarioURL("chessboard"), "config.xml"),
 				evConfigGroup);
-//		config.network().setInputFile("C:/Users/admin/IdeaProjects/matsim-berlin/test/input/1%network.xml");
+		config.network().setInputFile("C:/Users/admin/IdeaProjects/matsim-berlin/test/input/1%network.xml");
 
 		//prepare config
 		RunUrbanEVExample.prepareConfig(config);
@@ -68,11 +68,11 @@ public class ChargerSelectionTest3 {
 		VehicleType carVehicleType = vehiclesFactory.createVehicleType(Id.create(TransportMode.car, VehicleType.class));
 		VehicleUtils.setHbefaTechnology(carVehicleType.getEngineInformation(), "electricity");
 		VehicleUtils.setEnergyCapacity(carVehicleType.getEngineInformation(), 10);
-		EVUtils.setInitialEnergy(carVehicleType.getEngineInformation(), 4);
+		EVUtils.setInitialEnergy(carVehicleType.getEngineInformation(), 5);
 		EVUtils.setChargerTypes(carVehicleType.getEngineInformation(), Arrays.asList("a", "b", "default"));
 
 		VehicleType bikeVehicleType = vehiclesFactory.createVehicleType(Id.create(TransportMode.bike, VehicleType.class));
-//		VehicleUtils.setHbefaTechnology(bikeVehicleType.getEngineInformation(), "electricity");
+		VehicleUtils.setHbefaTechnology(bikeVehicleType.getEngineInformation(), "electricity");
 		VehicleUtils.setEnergyCapacity(bikeVehicleType.getEngineInformation(), 10);
 		EVUtils.setInitialEnergy(bikeVehicleType.getEngineInformation(), 4);
 		EVUtils.setChargerTypes(bikeVehicleType.getEngineInformation(), Arrays.asList("a", "b", "default"));
@@ -107,16 +107,34 @@ public class ChargerSelectionTest3 {
 		plan.addActivity(home);
 		plan.addLeg(factory.createLeg(TransportMode.car));
 
-		Activity work = factory.createActivityFromLinkId("work", Id.createLinkId("176"));
+		Activity work = factory.createActivityFromLinkId("work", Id.createLinkId("24"));
 		work.setEndTime(10 * 3600);
 		plan.addActivity(work);
+		plan.addLeg(factory.createLeg(TransportMode.car));
+
+		Activity work2 = factory.createActivityFromLinkId("work", Id.createLinkId("172"));
+		work2.setEndTime(11 * 3600);
+		plan.addActivity(work2);
 
 		plan.addLeg(factory.createLeg(TransportMode.car));
 
 		Activity leisure = factory.createActivityFromLinkId("leisure", Id.createLinkId("90"));
-		leisure.setEndTime(11 * 3600);
+		leisure.setEndTime(12 * 3600);
 		plan.addActivity(leisure);
 		plan.addLeg(factory.createLeg(TransportMode.bike));
+
+
+
+		Activity leisure2 = factory.createActivityFromLinkId("leisure", Id.createLinkId("89"));
+		leisure2.setEndTime(12.5 * 3600);
+		plan.addActivity(leisure2);
+		plan.addLeg(factory.createLeg(TransportMode.bike));
+
+		Activity leisure3 = factory.createActivityFromLinkId("leisure", Id.createLinkId("90"));
+		leisure3.setEndTime(13 * 3600);
+		plan.addActivity(leisure3);
+		plan.addLeg(factory.createLeg(TransportMode.car));
+
 
 		Activity home2 = factory.createActivityFromLinkId("home", Id.createLinkId("95"));
 		home2.setEndTime(14 * 3600);
@@ -126,7 +144,7 @@ public class ChargerSelectionTest3 {
 
 		scenario.getPopulation().addPerson(person);
 
-		Person person2 = factory.createPerson(Id.createPersonId("Jonas' kleiner GeheimAgent2"));
+		Person person2 = factory.createPerson(Id.createPersonId("Dummy"));
 
 		Plan plan2 = factory.createPlan();
 
@@ -135,9 +153,9 @@ public class ChargerSelectionTest3 {
 		plan2.addActivity(home3);
 		plan2.addLeg(factory.createLeg(TransportMode.car));
 
-		Activity work2 = factory.createActivityFromLinkId("work", Id.createLinkId("175"));
-		work2.setEndTime(10 * 3600);
-		plan2.addActivity(work2);
+		Activity work3 = factory.createActivityFromLinkId("work", Id.createLinkId("175"));
+		work3.setEndTime(10 * 3600);
+		plan2.addActivity(work3);
 
 		plan2.addLeg(factory.createLeg(TransportMode.car));
 
