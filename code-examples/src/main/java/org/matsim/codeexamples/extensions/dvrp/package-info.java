9501b4e50ec643215fa9f3fa361005e29ea1b91e
kainagel <nagel@vsp.tu-berlin.de>
2020-12-14 13:09:24 +0100

    repair the href

--- code-examples/src/main/java/org/matsim/codeexamples/extensions/dvrp/package-info.java
+++ code-examples/src/main/java/org/matsim/codeexamples/extensions/dvrp/package-info.java
@@ -4,7 +4,7 @@
  * <pre>
  *         controler.addOverridingModule( new OTFVisLiveModule() );
  * </pre>
- * just before <code>controler.run()</code> to see something while the simulation is running.  If you get stuck, ask questions under <href a="https://matsim.org/faq">https://matsim.org/faq</href>.
+ * just before <code>controler.run()</code> to see something while the simulation is running.  If you get stuck, ask questions under <a href="https://matsim.org/faq">https://matsim.org/faq</a>.
  *
  * The other examples in this package point to examples in matsim-libs; please go to the source code there for more information.
  *
