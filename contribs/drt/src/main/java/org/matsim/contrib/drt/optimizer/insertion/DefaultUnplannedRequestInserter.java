762f030e514454028039084a311ace737e63978a
Michal Maciejewski <michal.mac@gmail.com>
2021-02-07 14:30:57 +0100

    DRT: add unit tests for DefaultUnplannedRequestInserter

--- contribs/drt/src/main/java/org/matsim/contrib/drt/optimizer/insertion/DefaultUnplannedRequestInserter.java
+++ contribs/drt/src/main/java/org/matsim/contrib/drt/optimizer/insertion/DefaultUnplannedRequestInserter.java
@@ -23,6 +23,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ForkJoinPool;
+import java.util.function.DoubleSupplier;
 
 import org.apache.log4j.Logger;
 import org.matsim.contrib.drt.optimizer.VehicleData;
@@ -36,6 +37,8 @@ import org.matsim.contrib.dvrp.path.OneToManyPathSearch.PathData;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.mobsim.framework.MobsimTimer;
 
+import com.google.common.annotations.VisibleForTesting;
+
 /**
  * @author michalm
  */
@@ -43,9 +46,9 @@ public class DefaultUnplannedRequestInserter implements UnplannedRequestInserter
 	private static final Logger log = Logger.getLogger(DefaultUnplannedRequestInserter.class);
 	public static final String NO_INSERTION_FOUND_CAUSE = "no_insertion_found";
 
-	private final DrtConfigGroup drtCfg;
+	private final String mode;
 	private final Fleet fleet;
-	private final MobsimTimer mobsimTimer;
+	private final DoubleSupplier timeOfDay;
 	private final EventsManager eventsManager;
 	private final RequestInsertionScheduler insertionScheduler;
 	private final VehicleData.EntryFactory vehicleDataEntryFactory;
@@ -58,22 +61,30 @@ public class DefaultUnplannedRequestInserter implements UnplannedRequestInserter
 			EventsManager eventsManager, RequestInsertionScheduler insertionScheduler,
 			VehicleData.EntryFactory vehicleDataEntryFactory, DrtInsertionSearch<PathData> insertionSearch,
 			ForkJoinPool forkJoinPool) {
-		this.drtCfg = drtCfg;
+		this(drtCfg.getMode(), fleet, mobsimTimer::getTimeOfDay, eventsManager, insertionScheduler,
+				vehicleDataEntryFactory, new DrtRequestInsertionRetryQueue(drtCfg.getDrtRequestInsertionRetryParams().
+						orElse(new DrtRequestInsertionRetryParams())), forkJoinPool, insertionSearch);
+	}
+
+	@VisibleForTesting
+	DefaultUnplannedRequestInserter(String mode, Fleet fleet, DoubleSupplier timeOfDay, EventsManager eventsManager,
+			RequestInsertionScheduler insertionScheduler, VehicleData.EntryFactory vehicleDataEntryFactory,
+			DrtRequestInsertionRetryQueue insertionRetryQueue, ForkJoinPool forkJoinPool,
+			DrtInsertionSearch<PathData> insertionSearch) {
+		this.mode = mode;
 		this.fleet = fleet;
-		this.mobsimTimer = mobsimTimer;
+		this.timeOfDay = timeOfDay;
 		this.eventsManager = eventsManager;
 		this.insertionScheduler = insertionScheduler;
 		this.vehicleDataEntryFactory = vehicleDataEntryFactory;
+		this.insertionRetryQueue = insertionRetryQueue;
 		this.forkJoinPool = forkJoinPool;
 		this.insertionSearch = insertionSearch;
-
-		insertionRetryQueue = new DrtRequestInsertionRetryQueue(drtCfg.getDrtRequestInsertionRetryParams().
-				orElse(new DrtRequestInsertionRetryParams()));
 	}
 
 	@Override
 	public void scheduleUnplannedRequests(Collection<DrtRequest> unplannedRequests) {
-		double now = mobsimTimer.getTimeOfDay();
+		double now = timeOfDay.getAsDouble();
 
 		List<DrtRequest> requestsToRetry = insertionRetryQueue.getRequestsToRetryNow(now);
 		if (unplannedRequests.isEmpty() && requestsToRetry.isEmpty()) {
@@ -84,7 +95,7 @@ public class DefaultUnplannedRequestInserter implements UnplannedRequestInserter
 				forkJoinPool);
 
 		//first retry scheduling old requests
-		insertionRetryQueue.getRequestsToRetryNow(now).forEach(req -> scheduleUnplannedRequest(req, vData, now));
+		requestsToRetry.forEach(req -> scheduleUnplannedRequest(req, vData, now));
 
 		//then schedule new requests
 		for (var reqIter = unplannedRequests.iterator(); reqIter.hasNext(); ) {
@@ -98,7 +109,7 @@ public class DefaultUnplannedRequestInserter implements UnplannedRequestInserter
 		if (best.isEmpty()) {
 			if (!insertionRetryQueue.tryAddFailedRequest(req, now)) {
 				eventsManager.processEvent(
-						new PassengerRequestRejectedEvent(now, drtCfg.getMode(), req.getId(), req.getPassengerId(),
+						new PassengerRequestRejectedEvent(now, mode, req.getId(), req.getPassengerId(),
 								NO_INSERTION_FOUND_CAUSE));
 				log.debug("No insertion found for drt request "
 						+ req
@@ -111,10 +122,9 @@ public class DefaultUnplannedRequestInserter implements UnplannedRequestInserter
 			InsertionWithDetourData<PathData> insertion = best.get();
 			var pickupDropoffTaskPair = insertionScheduler.scheduleRequest(req, insertion);
 			vData.updateEntry(insertion.getVehicleEntry().vehicle);
-			eventsManager.processEvent(
-					new PassengerRequestScheduledEvent(now, drtCfg.getMode(), req.getId(), req.getPassengerId(),
-							insertion.getVehicleEntry().vehicle.getId(), pickupDropoffTaskPair.pickupTask.getEndTime(),
-							pickupDropoffTaskPair.dropoffTask.getBeginTime()));
+			eventsManager.processEvent(new PassengerRequestScheduledEvent(now, mode, req.getId(), req.getPassengerId(),
+					insertion.getVehicleEntry().vehicle.getId(), pickupDropoffTaskPair.pickupTask.getEndTime(),
+					pickupDropoffTaskPair.dropoffTask.getBeginTime()));
 		}
 	}
 }
