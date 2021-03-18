72a17b5f5261f4537f09924e55146880ba85c403
Jakob Rehmann <jakob.rehmann@gmail.com>
2021-03-18 12:06:48 +0100

    Added early warning mechanisms that check for drastic changes in drt performance with respect to customers
    RunDrtExampleIT is modified to read drtCustomerStats output file and confirm that values are not more than +-20% than the values from commit ee6b608 (March 2021).

--- contribs/drt/src/test/java/org/matsim/contrib/drt/run/examples/RunDrtExampleIT.java
+++ contribs/drt/src/test/java/org/matsim/contrib/drt/run/examples/RunDrtExampleIT.java
@@ -17,13 +17,17 @@
  *                                                                         *
  * *********************************************************************** */
 
-/**
- *
- */
 package org.matsim.contrib.drt.run.examples;
 
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.matsim.contrib.drt.optimizer.insertion.DrtRequestInsertionRetryParams;
@@ -45,6 +49,7 @@ public class RunDrtExampleIT {
 	@Rule
 	public MatsimTestUtils utils = new MatsimTestUtils();
 
+
 	@Test
 	public void testRunDrtExampleWithRequestRetry() {
 		URL configUrl = IOUtils.extendUrl(ExamplesUtils.getTestScenarioURL("mielec"), "mielec_drt_config.xml");
@@ -59,7 +64,12 @@ public class RunDrtExampleIT {
 		config.controler().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
 		config.controler().setOutputDirectory(utils.getOutputDirectory());
 		RunDrtExample.run(config, false);
+
+		// Early warning system for drastic changes in drt performance with regards to customer experience (see note below)
+		verifyDrtCustomerStatsInReasonableRange(utils.getOutputDirectory(), 0.01,
+				5, 700.11, 381.06, 1081.17);
 	}
+
 
 	@Test
 	public void testRunDrtStopbasedExample() {
@@ -71,6 +81,10 @@ public class RunDrtExampleIT {
 		config.controler().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
 		config.controler().setOutputDirectory(utils.getOutputDirectory());
 		RunDrtExample.run(config, false);
+
+		// Early warning system for drastic changes in drt performance with regards to customer experience (see note below)
+		verifyDrtCustomerStatsInReasonableRange(utils.getOutputDirectory(), 0.05, 18, 255.6,
+				378.99, 634.59);
 	}
 
 	@Test
@@ -83,5 +97,66 @@ public class RunDrtExampleIT {
 		config.controler().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
 		config.controler().setOutputDirectory(utils.getOutputDirectory());
 		RunDrtExample.run(config, false);
+
+
+		// Early warning system for drastic changes in drt performance with regards to customer experience (see note below)
+		verifyDrtCustomerStatsInReasonableRange(utils.getOutputDirectory(), 0.03, 11, 227.56,
+				385.43, 612.98);
 	}
+
+	/**
+	 *	Early warning system: if customer parameters vary more than 20% above or below the values from commit ee6b608
+	 * 	(March 2021), then the following unit tests will fail. This is meant to serve as a red flag if drt performance
+	 * 	with respect to customer experience changes with drastically. The following customer parameter checked are:
+	 * 	rejectionRate, rejections, waitAverage, inVehicleTravelTimeMean, & totalTravelTimeMean
+	 *
+	 */
+
+	private void verifyDrtCustomerStatsInReasonableRange(String outputDirectory, double rejectionRateExpected, double rejectionsExpected,
+														 double waitAverageExpected, double inVehicleTravelTimeMeanExpected, double totalTravelTimeMeanExpected) {
+
+		String filename = outputDirectory + "/drt_customer_stats_drt.csv";
+
+		List<String> collect;
+
+		try (
+				Stream<String> lines = Files.lines(Paths.get(filename))
+		) {
+			collect = lines.collect(Collectors.toList());
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("output file not found");
+		}
+
+		int size = collect.size();
+
+		List<String> keys = Arrays.asList(collect.get(0).split(";"));
+		List<String> values = Arrays.asList(collect.get(size - 1).split(";"));
+
+		Map<String, String> params = new HashMap<>();
+		for (int i = 0; i < keys.size(); i++) {
+			params.put(keys.get(i), values.get(i));
+		}
+
+		double inVehicleTravelTimeMean = Double.parseDouble(params.get("inVehicleTravelTime_mean"));
+		double waitAverage = Double.parseDouble(params.get("wait_average"));
+		double rejections = Double.parseDouble(params.get("rejections"));
+		double rejectionRate = Double.parseDouble(params.get("rejectionRate"));
+		double totalTravelTimeMean = Double.parseDouble(params.get("totalTravelTime_mean"));
+
+
+		Assert.assertEquals("rejection rate is within +-20% of expected value",
+				rejectionRateExpected, rejectionRate, rejectionRateExpected * 0.2);
+		Assert.assertEquals("rejections are within +-20% of expected value",
+				rejectionsExpected, rejections, rejectionsExpected * 0.2);
+		Assert.assertEquals("waitAverage is within +-20% of expected value",
+				waitAverageExpected, waitAverage, waitAverageExpected * 0.2);
+		Assert.assertEquals("inVehicleTravelTimeMean is within +-20% of expected value",
+				inVehicleTravelTimeMeanExpected, inVehicleTravelTimeMean, inVehicleTravelTimeMeanExpected * 0.2);
+		Assert.assertEquals("totalTravelTimeMean is within +-20% of expected value",
+				totalTravelTimeMeanExpected, totalTravelTimeMean, totalTravelTimeMeanExpected * 0.2);
+
+	}
+
+
 }
