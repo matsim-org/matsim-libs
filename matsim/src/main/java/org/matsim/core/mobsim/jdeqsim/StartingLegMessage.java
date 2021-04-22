bdb88f7d77986293095b0338c32769f4c31de070
leich <leich@vsp.tu-berlin.de>
2021-04-23 00:51:23 +0200

    set coord in more places

--- matsim/src/main/java/org/matsim/core/mobsim/jdeqsim/StartingLegMessage.java
+++ matsim/src/main/java/org/matsim/core/mobsim/jdeqsim/StartingLegMessage.java
@@ -72,7 +72,7 @@ public class StartingLegMessage extends EventMessage {
 
 		// schedule ActEndEvent
 		event = new ActivityEndEvent(this.getMessageArrivalTime(), vehicle.getOwnerPerson().getId(), vehicle.getCurrentLinkId(), vehicle
-				.getPreviousActivity().getFacilityId(), vehicle.getPreviousActivity().getType());
+				.getPreviousActivity().getFacilityId(), vehicle.getPreviousActivity().getType(), vehicle.getPreviousActivity().getCoord());
 		eventsManager.processEvent(event);
 
 		// schedule AgentDepartureEvent
