c44b86d2b35a5af5902400f2f0065edc98f8af1c
david.wedekind <davidj.wedekind@gmail.com>
2021-05-14 11:57:18 +0200

    discussions & plans

--- contribs/vsp/src/main/java/playground/vsp/matsimToPostgres/MatsimToPostgresControlerListener.java
+++ contribs/vsp/src/main/java/playground/vsp/matsimToPostgres/MatsimToPostgresControlerListener.java
@@ -1,5 +1,6 @@
 package playground.vsp.matsimToPostgres;
 
+import org.matsim.core.config.Config;
 import org.matsim.core.controler.events.ShutdownEvent;
 import org.matsim.core.controler.listener.ShutdownListener;
 
@@ -8,11 +9,26 @@ import javax.inject.Inject;
 public class MatsimToPostgresControlerListener implements ShutdownListener {
 
     @Inject
-    private PostgresExporterConfigGroup exporterConfigGroup;
+    private Config config;
 
     @Override
     public void notifyShutdown(ShutdownEvent event) {
-        PostgresExporter postgresExporter = new PostgresExporter(exporterConfigGroup);
+        PostgresExporter postgresExporter = new PostgresExporter(config);
+
+        // => David
+
+        // Check if initial data is existing
+        // If not, regard exporterConfigGroup setting and do action
+        // Import initial data (if not exist and 'importIfNotExist') or 'importOverwrite'
+
+        // Import trips table via postgresExporter
+        // String tripsCsv = config.controler().getOutputDirectory() + csvFile;
+        // postgresExporter.export(tripsCsv);
+
+        // Import legs table via postgresExporter
+        // postgresExporter.export(legsCsv);
+
+        // sql views
 
     }
 
--- contribs/vsp/src/main/java/playground/vsp/matsimToPostgres/PostgresExporter.java
+++ contribs/vsp/src/main/java/playground/vsp/matsimToPostgres/PostgresExporter.java
@@ -1,13 +1,33 @@
 package playground.vsp.matsimToPostgres;
 
-public class PostgresExporter {
-    PostgresExporterConfigGroup configGroup;
+import org.matsim.core.config.Config;
 
-    PostgresExporter(PostgresExporterConfigGroup configGroup){
-        this.configGroup = configGroup;
+public class PostgresExporter {
+    Config config;
+
+    PostgresExporter(Config config){
+        this.config = config;
     }
 
-    public void export(){
+    public void export(String csvPath){
+
+
+        // => Friedrich
+
+        // Convert a csv file to a postgres table
+
+        // Check if table exists
+        // If not create table with columns and primary key ...
+
+        // Does the table have all columns needed? Does it match the csv?
+        // If not, error with comparison
+
+        // Check if run id exists in table?
+        // If failIfRunIdExists then exit
+        // Otherwise drop all lines with runId
+
+        // Copy instead of insert query
+        // https://www.postgresqltutorial.com/import-csv-file-into-posgresql-table/
 
     }
 
