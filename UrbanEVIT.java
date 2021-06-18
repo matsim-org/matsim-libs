669425385b2b403dd1652859bd271499d21df62d
Jonas116 <mcjonas@live.de>
2021-06-18 09:59:29 +0200

    fixed problem where multiple charging acts had the same act type

--- UrbanEVIT.java
+++ UrbanEVIT.java
@@ -7,6 +7,10 @@ import org.junit.rules.Timeout;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.population.Activity;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Plan;
+import org.matsim.api.core.v01.population.PopulationFactory;
 import org.matsim.contrib.ev.EvConfigGroup;
 import org.matsim.contrib.ev.infrastructure.*;
 import org.matsim.core.api.experimental.events.EventsManager;
@@ -46,11 +50,79 @@ public class UrbanEVIT {
 	@Test
 	public void run() {
 		Scenario scenario = CreateUrbanEVTestScenario.createTestScenario();
+		overridePopulation(scenario);
+		scenario.getVehicles().getVehicles().keySet().forEach(vehicleId -> scenario.getVehicles().removeVehicle(vehicleId));
+		scenario.getConfig().controler().setOutputDirectory("test/output/urbanEV/UrbanEVRun");
+		CreateUrbanEVTestScenario.createAndRegisterPersonalCarAndBikeVehicles(scenario);
 		///controler with Urban EV module
 		Controler controler = RunUrbanEVExample.prepareControler(scenario);
 		controler.run();
 
 
+	}
+
+	private static void overridePopulation(Scenario scenario) {
+
+		//delete all persons that are there already
+		scenario.getPopulation().getPersons().clear();
+
+		PopulationFactory factory = scenario.getPopulation().getFactory();
+		Person person = factory.createPerson(Id.createPersonId("Charge during leisure + bike"));
+
+
+		Plan plan = factory.createPlan();
+
+		Activity home1 = factory.createActivityFromLinkId("home", Id.createLinkId("95"));
+		home1.setEndTime(8 * 3600);
+		plan.addActivity(home1);
+
+		plan.addLeg(factory.createLeg(TransportMode.car));
+
+		Activity work1 = factory.createActivityFromLinkId("work", Id.createLinkId("24"));
+		work1.setEndTime(10 * 3600);
+		plan.addActivity(work1);
+
+		plan.addLeg(factory.createLeg(TransportMode.car));
+
+		Activity work12 = factory.createActivityFromLinkId("work", Id.createLinkId("172"));
+		work12.setEndTime(11 * 3600);
+		plan.addActivity(work12);
+
+		plan.addLeg(factory.createLeg(TransportMode.car));
+
+		Activity leisure1 = factory.createActivityFromLinkId("leisure", Id.createLinkId("90"));
+		leisure1.setEndTime(12 * 3600);
+		plan.addActivity(leisure1);
+
+		plan.addLeg(factory.createLeg(TransportMode.bike));
+
+		Activity leisure12 = factory.createActivityFromLinkId("leisure", Id.createLinkId("89"));
+		leisure12.setEndTime(13 * 3600);
+		plan.addActivity(leisure12);
+
+		plan.addLeg(factory.createLeg(TransportMode.bike));
+
+		Activity leisure13 = factory.createActivityFromLinkId("leisure", Id.createLinkId("90"));
+		leisure13.setEndTime(14 * 3600);
+		plan.addActivity(leisure13);
+
+		plan.addLeg(factory.createLeg(TransportMode.car));
+
+
+		Activity home12 = factory.createActivityFromLinkId("home", Id.createLinkId("95"));
+		home12.setEndTime(15 * 3600);
+		plan.addActivity(home12);
+		person.addPlan(plan);
+		person.setSelectedPlan(plan);
+
+		plan.addLeg(factory.createLeg(TransportMode.bike));
+
+		Activity leisure14 = factory.createActivityFromLinkId("leisure", Id.createLinkId("90"));
+		leisure14.setEndTime(16 * 3600);
+		plan.addActivity(leisure14);
+
+
+		scenario.getPopulation().addPerson(person);
 
 	}
 }
