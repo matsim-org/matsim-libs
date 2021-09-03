7389af726cb5c0f07e8312d740eb99ba93cb628b
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2021-09-03 15:30:54 +0200

    Update DrtIdleVehicleWriter.java

--- contribs/drt/src/main/java/org/matsim/contrib/drt/analysis/afterSimAnalysis/DrtIdleVehicleWriter.java
+++ contribs/drt/src/main/java/org/matsim/contrib/drt/analysis/afterSimAnalysis/DrtIdleVehicleWriter.java
@@ -9,7 +9,6 @@ import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.events.EventsUtils;
 import org.matsim.core.events.MatsimEventsReader;
-import org.matsim.core.events.ParallelEventsManager;
 import org.matsim.core.network.io.MatsimNetworkReader;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.contrib.drt.analysis.afterSimAnalysis.StayTaskRecorder.StayTaskDataEntry;
