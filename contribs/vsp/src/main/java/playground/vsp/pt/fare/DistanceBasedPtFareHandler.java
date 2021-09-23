90343fae15a06a087c04bdea74912fc1314503f8
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2021-09-23 11:29:23 +0200

    Use PT interaction to decide the location of Departure and Arrival PT stop

--- contribs/vsp/src/main/java/playground/vsp/pt/fare/DistanceBasedPtFareHandler.java
+++ contribs/vsp/src/main/java/playground/vsp/pt/fare/DistanceBasedPtFareHandler.java
@@ -1,17 +1,12 @@
 package playground.vsp.pt.fare;
 
 import com.google.inject.Inject;
+import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.events.ActivityStartEvent;
-import org.matsim.api.core.v01.events.PersonArrivalEvent;
-import org.matsim.api.core.v01.events.PersonDepartureEvent;
 import org.matsim.api.core.v01.events.PersonMoneyEvent;
 import org.matsim.api.core.v01.events.handler.ActivityStartEventHandler;
-import org.matsim.api.core.v01.events.handler.PersonArrivalEventHandler;
-import org.matsim.api.core.v01.events.handler.PersonDepartureEventHandler;
-import org.matsim.api.core.v01.network.Link;
-import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.router.StageActivityTypeIdentifier;
@@ -20,51 +15,33 @@ import org.matsim.core.utils.geometry.CoordUtils;
 import java.util.HashMap;
 import java.util.Map;
 
-public class DistanceBasedPtFareHandler implements PersonDepartureEventHandler, PersonArrivalEventHandler, ActivityStartEventHandler {
+public class DistanceBasedPtFareHandler implements ActivityStartEventHandler {
     @Inject
     private EventsManager events;
 
     private final double baseFare;
     private final double distanceFare;
-    private final Network network;
 
-    private final Map<Id<Person>, Id<Link>> personDepartureMap = new HashMap<>();
-    private final Map<Id<Person>, Id<Link>> personArrivalMap = new HashMap<>();
+    private final Map<Id<Person>, Coord> personDepartureCoordMap = new HashMap<>();
+    private final Map<Id<Person>, Coord> personArrivalCoordMap = new HashMap<>();
 
-    public DistanceBasedPtFareHandler(DistanceBasedPtFareParams params,Network network){
+    public DistanceBasedPtFareHandler(DistanceBasedPtFareParams params) {
         this.baseFare = params.getBaseFare();
         this.distanceFare = params.getDistanceFare();
-        this.network = network;
-    }
-
-    @Override
-    public void handleEvent(PersonDepartureEvent event) {
-        if (event.getLegMode().equals(TransportMode.pt)){
-            if (!personDepartureMap.containsKey(event.getPersonId())){
-                personDepartureMap.put(event.getPersonId(), event.getLinkId()); // Departure place is fixed
-            }
-        }
-    }
-
-    @Override
-    public void handleEvent(PersonArrivalEvent event) {
-        if (event.getLegMode().equals(TransportMode.pt)){
-            personArrivalMap.put(event.getPersonId(), event.getLinkId()); // Arrival place can be updated (e.g. transfer to another PT line)
-        }
     }
 
     @Override
     public void handleEvent(ActivityStartEvent event) {
-        if (!StageActivityTypeIdentifier.isStageActivity(event.getActType())){
+        if (event.getActType().equals("pt interaction")) {
+            personDepartureCoordMap.computeIfAbsent(event.getPersonId(), c -> event.getCoord()); // The first PT interaction activity for an agent is departure PT stop, therefore it is fixed
+            personArrivalCoordMap.put(event.getPersonId(), event.getCoord()); // The arrival stop will keep updating until the agent start a real activity
+        }
+
+        if (!StageActivityTypeIdentifier.isStageActivity(event.getActType())) {
             Id<Person> personId = event.getPersonId();
-            if (personDepartureMap.containsKey(personId)){
-                if (!personArrivalMap.containsKey(personId)){
-                    throw new RuntimeException("A person has departed but not arrived. This should not happen! " +
-                            "Please make sure the event is read/handled in the normal sequence");
-                }
-                Link fromLink = network.getLinks().get(personDepartureMap.get(personId));
-                Link toLink = network.getLinks().get(personArrivalMap.get(personId));
-                double distance = CoordUtils.calcEuclideanDistance(fromLink.getToNode().getCoord(), toLink.getToNode().getCoord());
+            if (personDepartureCoordMap.containsKey(personId)) {
+                double distance = CoordUtils.calcEuclideanDistance
+                        (personDepartureCoordMap.get(personId), personArrivalCoordMap.get(personId));
                 double fare = baseFare + distance * distanceFare;
 
                 // charge fare to the person
@@ -72,15 +49,15 @@ public class DistanceBasedPtFareHandler implements PersonDepartureEventHandler, 
                         new PersonMoneyEvent(event.getTime(), event.getPersonId(), -fare,
                                 DistanceBasedPtFareParams.PT_DISTANCE_BASED_FARE, TransportMode.pt, event.getPersonId().toString()));
 
-                personDepartureMap.remove(personId);
-                personArrivalMap.remove(personId);
+                personDepartureCoordMap.remove(personId);
+                personArrivalCoordMap.remove(personId);
             }
         }
     }
 
     @Override
     public void reset(int iteration) {
-        personDepartureMap.clear();
-        personArrivalMap.clear();
+        personArrivalCoordMap.clear();
+        personDepartureCoordMap.clear();
     }
 }
--- contribs/vsp/src/main/java/playground/vsp/pt/fare/PtFareModule.java
+++ contribs/vsp/src/main/java/playground/vsp/pt/fare/PtFareModule.java
@@ -1,14 +1,8 @@
 package playground.vsp.pt.fare;
 
-import org.matsim.api.core.v01.network.Network;
 import org.matsim.core.controler.AbstractModule;
 
 public class PtFareModule extends AbstractModule {
-    private final Network network;
-
-    public PtFareModule(Network network) {
-        this.network = network;
-    }
 
     @Override
     public void install() {
@@ -16,12 +10,12 @@ public class PtFareModule extends AbstractModule {
 
         if (configGroup.getPtFareCalculation().equals(DistanceBasedPtFareParams.SET_NAME)) {
             DistanceBasedPtFareParams params = new DistanceBasedPtFareParams();
-            addEventHandlerBinding().toInstance(new DistanceBasedPtFareHandler(params, network));
+            addEventHandlerBinding().toInstance(new DistanceBasedPtFareHandler(params));
         } else {
             throw new RuntimeException("Please choose from the following fare Calculation method: [" + DistanceBasedPtFareParams.SET_NAME + "]");
         }
 
-        if (configGroup.getApplyUpperBound()){
+        if (configGroup.getApplyUpperBound()) {
             PtFareUpperBoundHandler ptFareUpperBoundHandler = new PtFareUpperBoundHandler(configGroup.getUpperBoundFactor());
             addEventHandlerBinding().toInstance(ptFareUpperBoundHandler);
             addControlerListenerBinding().toInstance(ptFareUpperBoundHandler);
