b7449c23cca639b90accedcbc0bdf20154e2a6f0
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2021-10-04 16:41:34 +0200

    Update PtFareModule.java

--- contribs/vsp/src/main/java/playground/vsp/pt/fare/PtFareModule.java
+++ contribs/vsp/src/main/java/playground/vsp/pt/fare/PtFareModule.java
@@ -7,11 +7,21 @@ public class PtFareModule extends AbstractModule {
     @Inject
     private PtFareConfigGroup ptFareConfigGroup;
 
+    @Inject
+    private DistanceBasedPtFareParams distanceBasedPtFareParams;
+
     @Override
     public void install() {
+        if (ptFareConfigGroup == null){
+            System.err.println("No PT Fare Config Group defined in the run script or config file. Will use the default setting!");
+            ptFareConfigGroup = new PtFareConfigGroup();
+        }
         if (ptFareConfigGroup.getPtFareCalculation().equals(DistanceBasedPtFareParams.SET_NAME)) {
-            DistanceBasedPtFareParams params = new DistanceBasedPtFareParams();
-            addEventHandlerBinding().toInstance(new DistanceBasedPtFareHandler(params));
+            if (distanceBasedPtFareParams == null){
+                System.err.println("No Distance Based PT Fare Params defined. Will use the default setting!");
+                distanceBasedPtFareParams = new DistanceBasedPtFareParams();
+            }
+            addEventHandlerBinding().toInstance(new DistanceBasedPtFareHandler(distanceBasedPtFareParams));
         } else {
             throw new RuntimeException("Please choose from the following fare Calculation method: [" + DistanceBasedPtFareParams.SET_NAME + "]");
         }
