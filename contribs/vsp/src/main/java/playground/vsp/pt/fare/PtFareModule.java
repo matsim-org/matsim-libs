1ba0b0ab7fdf346d829942fda24fca9789f8c852
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2021-10-04 16:03:39 +0200

    Update PtFareModule.java

--- contribs/vsp/src/main/java/playground/vsp/pt/fare/PtFareModule.java
+++ contribs/vsp/src/main/java/playground/vsp/pt/fare/PtFareModule.java
@@ -1,22 +1,23 @@
 package playground.vsp.pt.fare;
 
+import com.google.inject.Inject;
 import org.matsim.core.controler.AbstractModule;
 
 public class PtFareModule extends AbstractModule {
+    @Inject
+    private PtFareConfigGroup ptFareConfigGroup;
 
     @Override
     public void install() {
-        PtFareConfigGroup configGroup = new PtFareConfigGroup();
-
-        if (configGroup.getPtFareCalculation().equals(DistanceBasedPtFareParams.SET_NAME)) {
+        if (ptFareConfigGroup.getPtFareCalculation().equals(DistanceBasedPtFareParams.SET_NAME)) {
             DistanceBasedPtFareParams params = new DistanceBasedPtFareParams();
             addEventHandlerBinding().toInstance(new DistanceBasedPtFareHandler(params));
         } else {
             throw new RuntimeException("Please choose from the following fare Calculation method: [" + DistanceBasedPtFareParams.SET_NAME + "]");
         }
 
-        if (configGroup.getApplyUpperBound()) {
-            PtFareUpperBoundHandler ptFareUpperBoundHandler = new PtFareUpperBoundHandler(configGroup.getUpperBoundFactor());
+        if (ptFareConfigGroup.getApplyUpperBound()) {
+            PtFareUpperBoundHandler ptFareUpperBoundHandler = new PtFareUpperBoundHandler(ptFareConfigGroup.getUpperBoundFactor());
             addEventHandlerBinding().toInstance(ptFareUpperBoundHandler);
             addControlerListenerBinding().toInstance(ptFareUpperBoundHandler);
         }
