cc54568e8e92c3715abe04d73538c8f70518d63f
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2021-10-05 13:50:28 +0200

    Naming change and small bug fixing
    -Change the name of some parameter for easier understating.
    -A small bug fixing in the upper bound fare

--- contribs/vsp/src/main/java/playground/vsp/pt/fare/PtFareModule.java
+++ contribs/vsp/src/main/java/playground/vsp/pt/fare/PtFareModule.java
@@ -1,26 +1,14 @@
 package playground.vsp.pt.fare;
 
-import com.google.inject.Inject;
 import org.matsim.core.controler.AbstractModule;
 
 public class PtFareModule extends AbstractModule {
-    @Inject
-    private PtFareConfigGroup ptFareConfigGroup;
-
-    @Inject
-    private DistanceBasedPtFareParams distanceBasedPtFareParams;
 
     @Override
     public void install() {
-        if (ptFareConfigGroup == null){
-            System.err.println("No PT Fare Config Group defined in the run script or config file. Will use the default setting!");
-            ptFareConfigGroup = new PtFareConfigGroup();
-        }
+        PtFareConfigGroup ptFareConfigGroup = new PtFareConfigGroup();
         if (ptFareConfigGroup.getPtFareCalculation().equals(DistanceBasedPtFareParams.SET_NAME)) {
-            if (distanceBasedPtFareParams == null){
-                System.err.println("No Distance Based PT Fare Params defined. Will use the default setting!");
-                distanceBasedPtFareParams = new DistanceBasedPtFareParams();
-            }
+            DistanceBasedPtFareParams distanceBasedPtFareParams = new DistanceBasedPtFareParams();
             addEventHandlerBinding().toInstance(new DistanceBasedPtFareHandler(distanceBasedPtFareParams));
         } else {
             throw new RuntimeException("Please choose from the following fare Calculation method: [" + DistanceBasedPtFareParams.SET_NAME + "]");
@@ -31,6 +19,5 @@ public class PtFareModule extends AbstractModule {
             addEventHandlerBinding().toInstance(ptFareUpperBoundHandler);
             addControlerListenerBinding().toInstance(ptFareUpperBoundHandler);
         }
-
     }
 }
