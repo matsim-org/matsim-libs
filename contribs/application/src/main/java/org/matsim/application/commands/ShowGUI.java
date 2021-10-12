d4dc3cf6ba1f405702782d5c996b2ed76cabeb80
Christian Rakow <rakow@vsp.tu-berlin.de>
2021-10-12 14:33:25 +0200

    update MATSIM gui to work with application contrib

--- contribs/application/src/main/java/org/matsim/application/commands/ShowGUI.java
+++ contribs/application/src/main/java/org/matsim/application/commands/ShowGUI.java
@@ -4,11 +4,14 @@ import org.matsim.application.MATSimApplication;
 import org.matsim.run.gui.Gui;
 import picocli.CommandLine;
 
-import javax.swing.*;
 import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
 
 @CommandLine.Command(name = "gui", description = "Show the graphical user interface")
 public class ShowGUI implements Callable<Integer> {
+
+    @CommandLine.ParentCommand
+    private MATSimApplication parent;
 
     @CommandLine.Spec
     private CommandLine.Model.CommandSpec spec;
@@ -23,7 +26,12 @@ public class ShowGUI implements Callable<Integer> {
             name = name.substring(MATSimApplication.COLOR.length(), name.length() - 4);
         }
 
-        Gui.show(name, MATSimApplication.class);
+        Future<Gui> f = Gui.show(name, parent.getClass());
+
+        Gui gui = f.get();
+
+        while (gui.isShowing())
+            Thread.sleep(250);
 
         return 0;
     }
