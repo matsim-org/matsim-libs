f4f6a83170700bd1089b8758255651cd930bb1dd
Christian Rakow <rakow@vsp.tu-berlin.de>
2021-11-19 08:17:39 +0100

    add consttructors to the csv options

--- contribs/application/src/main/java/org/matsim/application/options/CsvOptions.java
+++ contribs/application/src/main/java/org/matsim/application/options/CsvOptions.java
@@ -17,28 +17,61 @@ import java.nio.file.Path;
  */
 public final class CsvOptions {
 
-    @CommandLine.Option(names = "--csv-format", description = "CSV Format", defaultValue = "Default")
-    private CSVFormat.Predefined csvFormat;
+	@CommandLine.Option(names = "--csv-format", description = "CSV Format", defaultValue = "Default")
+	private CSVFormat.Predefined csvFormat;
 
-    @CommandLine.Option(names = "--csv-delimiter", description = "CSV Delimiter", defaultValue = ",")
-    private Character csvDelimiter;
+	@CommandLine.Option(names = "--csv-delimiter", description = "CSV Delimiter", required = false)
+	private Character csvDelimiter;
 
-    @CommandLine.Option(names = "--csv-charset", description = "CSV input encoding", defaultValue = "UTF8")
-    private Charset csvCharset;
+	@CommandLine.Option(names = "--csv-charset", description = "CSV input encoding", defaultValue = "UTF8")
+	private Charset csvCharset;
+
+	/**
+	 * Default constructor.
+	 */
+	public CsvOptions() {
+	}
+
+	/**
+	 * Constructor for a default CSV Format.
+	 */
+	public CsvOptions(CSVFormat.Predefined csvFormat) {
+		this.csvFormat = csvFormat;
+	}
 
     /**
-     * Creates a new csv parser from specified options.
+     * Constructor with all available options.
      */
-    public CSVParser createParser(Path path) throws IOException {
-        return new CSVParser(Files.newBufferedReader(path, csvCharset), csvFormat.getFormat().withFirstRecordAsHeader().withDelimiter(csvDelimiter));
+    public CsvOptions(CSVFormat.Predefined csvFormat, Character csvDelimiter, Charset csvCharset) {
+        this.csvFormat = csvFormat;
+        this.csvDelimiter = csvDelimiter;
+        this.csvCharset = csvCharset;
     }
 
     /**
-     * Creates a new csv writer.
-     */
-    public CSVPrinter createPrinter(Path path) throws IOException {
-        return new CSVPrinter(Files.newBufferedWriter(path, csvCharset), csvFormat.getFormat().withFirstRecordAsHeader().withDelimiter(csvDelimiter));
-    }
+	 * Get the CSV format defined by the options.
+	 */
+	public CSVFormat getFormat() {
+		CSVFormat format = this.csvFormat.getFormat().withFirstRecordAsHeader();
+		if (csvDelimiter != null)
+			format = format.withDelimiter(csvDelimiter);
+
+		return format;
+	}
+
+	/**
+	 * Creates a new csv parser from specified options.
+	 */
+	public CSVParser createParser(Path path) throws IOException {
+		return new CSVParser(Files.newBufferedReader(path, csvCharset), getFormat());
+	}
+
+	/**
+	 * Creates a new csv writer.
+	 */
+	public CSVPrinter createPrinter(Path path) throws IOException {
+		return new CSVPrinter(Files.newBufferedWriter(path, csvCharset), getFormat());
+	}
 
 
 }
