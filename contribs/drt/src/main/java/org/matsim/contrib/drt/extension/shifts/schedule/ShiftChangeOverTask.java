68b49c1deac7aa712cf63299c4d78a4f220ee562
nkuehnel <nkuehnel2@gmail.com>
2021-12-15 21:10:23 +0100

    update shift task types

--- contribs/drt/src/main/java/org/matsim/contrib/drt/extension/shifts/schedule/ShiftChangeOverTask.java
+++ contribs/drt/src/main/java/org/matsim/contrib/drt/extension/shifts/schedule/ShiftChangeOverTask.java
@@ -1,12 +1,12 @@
 package org.matsim.contrib.drt.extension.shifts.schedule;
 
 import org.matsim.api.core.v01.network.Link;
-import org.matsim.contrib.dvrp.schedule.Task;
+import org.matsim.contrib.drt.schedule.DrtStopTask;
 
 /**
  * @author nkuehnel / MOIA
  */
-public interface ShiftChangeOverTask extends Task, OperationalStop {
+public interface ShiftChangeOverTask extends DrtStopTask, OperationalStop {
 
     Link getLink();
 
--- contribs/drt/src/main/java/org/matsim/contrib/drt/extension/shifts/schedule/WaitForShiftStayTask.java
+++ contribs/drt/src/main/java/org/matsim/contrib/drt/extension/shifts/schedule/WaitForShiftStayTask.java
@@ -1,20 +1,23 @@
 package org.matsim.contrib.drt.extension.shifts.schedule;
 
 import org.matsim.api.core.v01.network.Link;
-import org.matsim.contrib.drt.schedule.DrtStayTask;
-import org.matsim.contrib.drt.schedule.DrtStopTask;
 import org.matsim.contrib.drt.extension.shifts.operationFacilities.OperationFacility;
+import org.matsim.contrib.drt.schedule.DrtTaskType;
+import org.matsim.contrib.dvrp.schedule.DefaultStayTask;
+
+import static org.matsim.contrib.drt.schedule.DrtTaskBaseType.STAY;
 
 /**
  * @author nkuehnel / MOIA
  */
-public class WaitForShiftStayTask extends DrtStayTask implements OperationalStop {
+public class WaitForShiftStayTask extends DefaultStayTask implements OperationalStop {
 
-    private final OperationFacility facility;
-    private DrtStopTask stopTask;
+	public static final DrtTaskType TYPE = new DrtTaskType("WAIT_FOR_SHIFT", STAY);
+
+	private final OperationFacility facility;
 
     public WaitForShiftStayTask(double beginTime, double endTime, Link link, OperationFacility facility) {
-        super(beginTime, endTime, link);
+        super(TYPE, beginTime, endTime, link);
         this.facility = facility;
     }
 
