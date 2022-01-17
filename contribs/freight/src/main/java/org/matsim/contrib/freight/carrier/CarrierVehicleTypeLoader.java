ceabdaf003905e5406784144cd308c0d4e85e724
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2022-01-17 22:57:23 +0100

    cleanup CarrierVehicleTypeLoader

--- contribs/freight/src/main/java/org/matsim/contrib/freight/carrier/CarrierVehicleTypeLoader.java
+++ contribs/freight/src/main/java/org/matsim/contrib/freight/carrier/CarrierVehicleTypeLoader.java
@@ -1,28 +1,30 @@
 package org.matsim.contrib.freight.carrier;
 
-import java.util.Collection;
-
 import org.apache.log4j.Logger;
-import org.matsim.api.core.v01.Id;
-import org.matsim.vehicles.VehicleType;
 
 /**
+ * @deprecated Functionality is removed. VehicleTypes must be set (and available) when creating the vehicle. kai/kai jan'22
+ *
  * Loader that loads/assigns vehicleTypes to their vehicles and carriers respectively.
  * 
  * @author sschroeder
  *
  */
+@Deprecated
 public class CarrierVehicleTypeLoader {
 	
 	private static final  Logger logger = Logger.getLogger(CarrierVehicleTypeLoader.class);
-	
-	private Carriers carriers;
+
+	private final Carriers carriers;
 
 	/**
 	 * Constructs the loader with the carriers the types should be assigned to.
 	 * 
 	 * @param carriers
+	 *
+	 *  * @deprecated Functionality is removed. VehicleTypes must be set (and available) when creating the vehicle. kai/kai jan'22
 	 */
+	@Deprecated
 	public CarrierVehicleTypeLoader(Carriers carriers) {
 		super();
 		this.carriers = carriers;
@@ -32,29 +34,12 @@ public class CarrierVehicleTypeLoader {
 	 * Assigns types to carriers and their vehicles.
 	 * 
 	 * @param types
+	 *
+	 * @deprecated Functionality is removed. VehicleTypes must be set (and available) when creating the vehicle. kai/kai jan'22
 	 */
+	@Deprecated
 	public void loadVehicleTypes(CarrierVehicleTypes types){
-//		for(Carrier c : carriers.getCarriers().values()){
-//			for(CarrierVehicle v : c.getCarrierCapabilities().getCarrierVehicles().values()){
-//				Id<org.matsim.vehicles.VehicleType> typeId = v.getVehicleTypeId();
-//				if(typeId != null){
-//					VehicleType vehicleType = types.getVehicleTypes().get(typeId);
-//					if(vehicleType != null){
-//						v.setType(vehicleType);
-//						Collection<VehicleType> vTypes = c.getCarrierCapabilities().getVehicleTypes();
-//						if(!vTypes.contains(vehicleType)){
-//							vTypes.add(vehicleType);
-//						}
-//					}
-//					else{
-//						throw new IllegalStateException("cannot assign all vehicleTypes, since vehicleType to typeId \"" + typeId + "\" is missing.");
-//					}
-//				}
-//				else{
-//					logger.warn("vehicleTypeId is missing, thus no vehicleType can be assigned.");
-//				}
-//			}
-//		}
+		logger.error("Functionality is removed. VehicleTypes must be set (and available) when creating the vehicle.");
 	}
 
 }
