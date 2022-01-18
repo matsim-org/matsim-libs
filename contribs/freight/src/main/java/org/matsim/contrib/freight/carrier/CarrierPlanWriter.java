1856823a55b5ae2c816a3eed3153ee6889ec960a
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2022-01-18 23:35:40 +0100

    javadoc and deprecation of old writing method

--- contribs/freight/src/main/java/org/matsim/contrib/freight/carrier/CarrierPlanWriter.java
+++ contribs/freight/src/main/java/org/matsim/contrib/freight/carrier/CarrierPlanWriter.java
@@ -26,10 +26,21 @@ public class CarrierPlanWriter {
 		this.writeV2(filename);
 	}
 
+	/**
+	 * @deprecated The underlining {@Link{CarrierPlanXmlWriterV1} is deprecated since April21
+	 */
+	@Deprecated
 	public void writeV1(String filename) {
 		new CarrierPlanXmlWriterV1(this.carriers.getCarriers().values()).write(filename);
 	}
 
+
+	/**
+	 * Writes out the Carriers file in version 2.
+//	 * Please use the method {@link #write(String)} instead to always ensure writing out to the newest format.
+	 *
+	 * @param filename Name of the file that should be written.
+	 */
 	public void writeV2(String filename) {
 		CarrierPlanXmlWriterV2 writer = new CarrierPlanXmlWriterV2(this.carriers);
 		if (this.attributeConverters != null) {
