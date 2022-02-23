3337515ca2d5c4b46300887413a531b786ea34c1
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2022-02-23 14:32:42 +0100

    Doku

--- src/main/java/lsp/SolutionScheduler.java
+++ src/main/java/lsp/SolutionScheduler.java
@@ -2,7 +2,9 @@ package lsp;
 
 
 /**
- * Was macht das?
+ * Serve the purpose of routing a set of {@link lsp.shipment.LSPShipment}s through a set of
+ * {@link LogisticsSolution}s, which, in turn, consist of several {@link LogisticsSolutionElement}s
+ * and the corresponding {@link lsp.resources.LSPResource}s.
  */
 public interface SolutionScheduler {
 
