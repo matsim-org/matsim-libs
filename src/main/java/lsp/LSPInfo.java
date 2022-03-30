6b057875726c66b1765d167e7c8be67b36bd8b62
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2022-03-30 22:44:18 +0200

    comment

--- src/main/java/lsp/LSPInfo.java
+++ src/main/java/lsp/LSPInfo.java
@@ -15,6 +15,9 @@ import java.util.Set;
  *
  * Further, they can be valid only during a certain period of time.
  *
+ * Note (KMT, KN Mrz22): If we need to store more infos inside an attribute, e.g. a Collection for the {@link CostInfo}:
+ * There are the AttributeConverter(s) in MATSim-libs.
+ * One could easily create a DoubleCollectionConverter based on the StringCollectionConverter if needed.
  */
 public abstract class LSPInfo implements Attributable {
 
