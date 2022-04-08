c4f22f9e9b875044148778e740ac59a8515bff61
Christian Rakow <rakow@vsp.tu-berlin.de>
2022-04-08 12:10:45 +0200

    less restrictive behaviour option for subtour mode choice

--- matsim/src/main/java/org/matsim/core/replanning/modules/SubtourModeChoice.java
+++ matsim/src/main/java/org/matsim/core/replanning/modules/SubtourModeChoice.java
@@ -50,7 +50,7 @@ public class SubtourModeChoice extends AbstractMultithreadedModule {
 
 	private final double probaForChangeSingleTripMode;
 
-	public enum Behavior {fromAllModesToSpecifiedModes, fromSpecifiedModesToSpecifiedModes}
+	public enum Behavior {fromAllModesToSpecifiedModes, fromSpecifiedModesToSpecifiedModes, betweenAllLessConstraints}
 
 	private Behavior behavior = Behavior.fromSpecifiedModesToSpecifiedModes;
 
