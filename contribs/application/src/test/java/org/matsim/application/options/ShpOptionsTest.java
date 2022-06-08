ae562b0694777d9e3515fd4ff2ecd27c604628d1
simei94 <simonmeinhardt94@gmail.com>
2022-06-08 13:23:05 +0200

    new test for getGeometry method

--- contribs/application/src/test/java/org/matsim/application/options/ShpOptionsTest.java
+++ contribs/application/src/test/java/org/matsim/application/options/ShpOptionsTest.java
@@ -1,8 +1,10 @@
 package org.matsim.application.options;
 
+import org.junit.Assert;
 import org.junit.Assume;
 import org.junit.Rule;
 import org.junit.Test;
+import org.locationtech.jts.geom.Geometry;
 import org.matsim.testcases.MatsimTestUtils;
 import org.opengis.feature.simple.SimpleFeature;
 
@@ -59,6 +61,37 @@ public class ShpOptionsTest {
 		assertThat(ft)
 				.hasSize(578)
 				.hasSize(Set.copyOf(ft).size());
+
+	}
+
+	@Test
+	public void testGetGeometry() {
+
+		Path input = Path.of(utils.getClassInputDirectory()
+						.replace("ShpOptionsTest", "CreateLandUseShpTest")
+						.replace("options", "prepare"))
+				.resolve("andorra-latest-free.shp.zip");
+
+//		Assume.assumeTrue(Files.exists(input));
+
+		ShpOptions shp = new ShpOptions(input, null, null);
+		Geometry geometry1 = shp.getGeometry() ;
+		Geometry geometry2 = null ;
+
+		List<SimpleFeature> features = shp.readFeatures();
+
+		for(SimpleFeature feature : features) {
+			if(geometry2 == null) {
+				geometry2 = (Geometry) feature.getDefaultGeometry();
+			} else {
+				geometry2 = geometry2.union((Geometry) feature.getDefaultGeometry());
+			}
+		}
+
+
+
+		//read in shp with getGeometry and with readFeatures and then compare
+		Assert.assertEquals(geometry1, geometry2);
 
 	}
 }
