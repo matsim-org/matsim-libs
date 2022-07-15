9108baf45d90b53fed9be510853dcda3c9178acb
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2022-07-15 20:18:11 +0200

    fix compile error and add simple RuntimeException Test

---
+++ src/test/java/example/lsp/initialPlans/ExampleTwoEchelonGridTest.java
@@ -0,0 +1,48 @@
+/*
+ *  *********************************************************************** *
+ *  * project: org.matsim.*
+ *  * *********************************************************************** *
+ *  *                                                                         *
+ *  * copyright       : (C) 2022 by the members listed in the COPYING,        *
+ *  *                   LICENSE and WARRANTY file.                            *
+ *  * email           : info at matsim dot org                                *
+ *  *                                                                         *
+ *  * *********************************************************************** *
+ *  *                                                                         *
+ *  *   This program is free software; you can redistribute it and/or modify  *
+ *  *   it under the terms of the GNU General Public License as published by  *
+ *  *   the Free Software Foundation; either version 2 of the License, or     *
+ *  *   (at your option) any later version.                                   *
+ *  *   See also COPYING, LICENSE and WARRANTY file                           *
+ *  *                                                                         *
+ *  * ***********************************************************************
+ */
+
+package example.lsp.initialPlans;
+
+import org.apache.log4j.Logger;
+import org.junit.Rule;
+import org.junit.Test;
+import org.matsim.testcases.MatsimTestUtils;
+
+import static org.junit.Assert.fail;
+
+public class ExampleTwoEchelonGridTest {
+	private static final Logger log = Logger.getLogger( ExampleTwoEchelonGridTest.class );
+	@Rule public final MatsimTestUtils utils = new MatsimTestUtils();
+
+	@Test
+	public void testForRuntimeExceptions(){
+		try{
+			ExampleTwoEchelonGrid.main( new String []{
+					"--config:controler.outputDirectory=" + utils.getOutputDirectory()
+					, "--config:controler.lastIteration=2"
+			} );
+
+		} catch ( Exception ee ) {
+			log.fatal(ee) ;
+			fail() ;
+		}
+	}
+
+}
