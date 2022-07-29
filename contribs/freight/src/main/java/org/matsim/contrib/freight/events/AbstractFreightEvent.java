a50b8a87e8f545a3f682e51e561ae89a9a0242e4
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2022-07-29 15:48:53 +0200

    create AbstractFreightEvent to reduce code duplications

---
+++ contribs/freight/src/main/java/org/matsim/contrib/freight/events/AbstractFreightEvent.java
@@ -0,0 +1,56 @@
+package org.matsim.contrib.freight.events;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.events.Event;
+import org.matsim.api.core.v01.events.HasLinkId;
+import org.matsim.api.core.v01.events.HasVehicleId;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.contrib.freight.carrier.Carrier;
+import org.matsim.vehicles.Vehicle;
+
+import java.util.Map;
+
+/**
+ * @author Kai Martins-Turner (kturner)
+ */
+public abstract class AbstractFreightEvent extends Event implements HasCarrierId, HasLinkId, HasVehicleId {
+
+	private final Id<Carrier> carrierId;
+	private final Id<Link> linkId;
+	private final Id<Vehicle> vehicleId;
+
+	public AbstractFreightEvent(double time, Id<Carrier> carrierId, Id<Link> linkId, Id<Vehicle> vehicleId) {
+		super(time);
+		this.carrierId = carrierId;
+		this.linkId = linkId;
+		this.vehicleId = vehicleId;
+	}
+
+	/**
+	 * @return id of the {@link Carrier}
+	 */
+	@Override public final Id<Carrier> getCarrierId() {
+		return carrierId;
+	}
+
+	@Override public final Id<Link> getLinkId() {
+		return linkId;
+	}
+
+	@Override public final Id<Vehicle> getVehicleId() {
+		return vehicleId;
+	}
+
+	/**
+	 * Adds the {@link Id<Carrier>} to the list of attributes.
+	 * {@link Id<Vehicle>} and {@link Id<Link>} are handled by superclass {@link Event}
+	 *
+	 * @return The map of attributes
+	 */
+	@Override
+	public Map<String, String> getAttributes() {
+		Map<String, String> attr = super.getAttributes();
+		attr.put(ATTRIBUTE_CARRIER_ID, carrierId.toString());
+		return attr;
+	}
+}
