539f82245da0643f89c83d62ca759c49ead49832
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2022-10-19 18:33:46 +0200

    Minor Update to DRT stopping task writer (#2241)
    * Update DRT stopping task writer
    
    Merge the stop task and stay task into one single file;
    Add occupancy as another column.
    
    * Update DrtVehicleStoppingTaskWriter.java
    
    Add constructor with customized stopping task recorder
    
    * Improve structure
    
    * Update StoppingTaskRecorder.java
    
    Reduce the script size
    
    * Update StoppingTaskRecorder.java
    
    * Update DrtVehicleStoppingTaskWriter.java
    
    Update description and the title row of the output file
    
    * Update DrtVehicleStoppingTaskWriter.java
    
    Use buffered writer
    Use log to print out information

--- contribs/drt/src/main/java/org/matsim/contrib/drt/analysis/afterSimAnalysis/DrtVehicleStoppingTaskWriter.java
+++ contribs/drt/src/main/java/org/matsim/contrib/drt/analysis/afterSimAnalysis/DrtVehicleStoppingTaskWriter.java
@@ -6,52 +6,61 @@ import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.contrib.drt.schedule.DrtTaskType;
 import org.matsim.contrib.drt.util.DrtEventsReaders;
+import org.matsim.contrib.dvrp.schedule.Task;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.events.EventsUtils;
 import org.matsim.core.events.MatsimEventsReader;
 import org.matsim.core.network.io.MatsimNetworkReader;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.contrib.drt.analysis.afterSimAnalysis.StoppingTaskRecorder.DrtStoppingTaskDataEntry;
 
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 public class DrtVehicleStoppingTaskWriter {
 	private static final Logger log = LogManager.getLogger(DrtVehicleStoppingTaskWriter.class);
-	private final double endTime = 30 * 3600;
 
 	private final String eventsFile;
 	private final String networkFile;
-	private final String stayTasksOutputPath;
-	private final String stopTasksOutputPath;
+	private final String stoppingTasksOutputPath;
+	private final StoppingTaskRecorder stoppingTaskRecorder;
 
+	/**
+	 * Constructor with default StoppingTaskRecorder. Stay task and stop task will be written down. *
+	 */
 	public DrtVehicleStoppingTaskWriter(Path directory) {
-		Path eventsPath = glob(directory, "*output_events*", true).orElseThrow(() -> new IllegalStateException("No events file found."));
-		Path networkPath = glob(directory, "*output_network*", true).orElseThrow(() -> new IllegalStateException("No network file found."));
+		Path eventsPath = glob(directory, "*output_events*").orElseThrow(() -> new IllegalStateException("No events file found."));
+		Path networkPath = glob(directory, "*output_network*").orElseThrow(() -> new IllegalStateException("No network file found."));
 		this.eventsFile = eventsPath.toString();
 		this.networkFile = networkPath.toString();
-		this.stayTasksOutputPath = directory + "/stay-tasks-XY-plot.csv";
-		this.stopTasksOutputPath = directory + "/stop-tasks-XY-plot.csv";
+		this.stoppingTasksOutputPath = directory + "/drt-stopping-tasks-XY-plot.csv";
+		this.stoppingTaskRecorder = new StoppingTaskRecorder();
+	}
+
+	/**
+	 * Use this function to add customized task type to be included in the analysis. The customized task type should *
+	 * also be added to the arguments of the run function (as they are not standard task type)*
+	 */
+	public DrtVehicleStoppingTaskWriter addingCustomizedTaskToAnalyze(Task.TaskType customizedTaskType) {
+		stoppingTaskRecorder.addExtraTaskTypeToAnalyze(customizedTaskType);
+		return this;
 	}
 
 	public static void main(String[] args) throws IOException {
 		// Input in argument: directory of the simulation output
-		DrtVehicleStoppingTaskWriter drtVehicleStoppingTaskWriter = new DrtVehicleStoppingTaskWriter(Path.of(args[0]));
-		drtVehicleStoppingTaskWriter.run();
+		new DrtVehicleStoppingTaskWriter(Path.of(args[0])).run();
 	}
 
 	public void run(DrtTaskType... nonStandardTaskTypes) throws IOException {
 		Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
 		new MatsimNetworkReader(scenario.getNetwork()).readFile(networkFile);
 		Network network = scenario.getNetwork();
-		StoppingTaskRecorder stoppingTaskRecorder = new StoppingTaskRecorder();
 
 		EventsManager eventManager = EventsUtils.createEventsManager();
 		eventManager.addHandler(stoppingTaskRecorder);
@@ -61,41 +70,36 @@ public class DrtVehicleStoppingTaskWriter {
 		matsimEventsReader.readFile(eventsFile);
 		eventManager.finishProcessing();
 
-		List<DrtStoppingTaskDataEntry> stayTaskDataEntries = stoppingTaskRecorder.getStayTaskDataEntries();
-		// Adding in the final stay tasks (which ends after the dvrpTaskEnded event)
-		Collection<DrtStoppingTaskDataEntry> finalStayTasksEntries = stoppingTaskRecorder.getStartedStayTasksMap().values();
-		for (DrtStoppingTaskDataEntry drtStoppingTaskDataEntry : finalStayTasksEntries) {
-			drtStoppingTaskDataEntry.setEndTime(endTime);
-			stayTaskDataEntries.add(drtStoppingTaskDataEntry);
-		}
-		System.out.println("There are " + stayTaskDataEntries.size() + " stay tasks in total");
-		System.out.println("There are " + stoppingTaskRecorder.getStopTaskDataEntries().size() + " stop tasks in total");
-		writeResultIntoCSVFile(stayTaskDataEntries, network, stayTasksOutputPath);
-		writeResultIntoCSVFile(stoppingTaskRecorder.getStopTaskDataEntries(), network, stopTasksOutputPath);
+		List<StoppingTaskRecorder.DrtTaskInformation> drtStoppingTaskEntries = stoppingTaskRecorder.getDrtTasksEntries();
+
+		log.info("There are " + drtStoppingTaskEntries.size() + " drt stopping tasks in total");
+
+		writeResultIntoCSVFile(drtStoppingTaskEntries, network, stoppingTasksOutputPath);
 	}
 
-	private void writeResultIntoCSVFile(List<DrtStoppingTaskDataEntry> stayTaskDataEntries, Network network, String outputFile)
+	private void writeResultIntoCSVFile(List<StoppingTaskRecorder.DrtTaskInformation> drtStoppingTaskEntries, Network network, String outputFile)
 			throws IOException {
-		System.out.println("Writing CSV File now");
-		FileWriter csvWriter = new FileWriter(outputFile);
+		BufferedWriter csvWriter = new BufferedWriter(new FileWriter(outputFile));
 
-		csvWriter.append("Task ID");
+		csvWriter.append("Task_name");
 		csvWriter.append(",");
 		csvWriter.append("X");
 		csvWriter.append(",");
 		csvWriter.append("Y");
 		csvWriter.append(",");
-		csvWriter.append("Start Time");
+		csvWriter.append("Start_time");
 		csvWriter.append(",");
-		csvWriter.append("End Time");
+		csvWriter.append("End_time");
 		csvWriter.append(",");
-		csvWriter.append("Driver Id");
+		csvWriter.append("Driver_id");
+		csvWriter.append(",");
+		csvWriter.append("Occupancy_at_task_start");
 		csvWriter.append("\n");
 
-		for (DrtStoppingTaskDataEntry drtStoppingTaskDataEntry : stayTaskDataEntries) {
+		for (StoppingTaskRecorder.DrtTaskInformation drtStoppingTaskDataEntry : drtStoppingTaskEntries) {
 			double X = network.getLinks().get(drtStoppingTaskDataEntry.getLinkId()).getToNode().getCoord().getX();
 			double Y = network.getLinks().get(drtStoppingTaskDataEntry.getLinkId()).getToNode().getCoord().getY();
-			csvWriter.append(drtStoppingTaskDataEntry.getTaskId());
+			csvWriter.append(drtStoppingTaskDataEntry.getTaskName());
 			csvWriter.append(",");
 			csvWriter.append(Double.toString(X));
 			csvWriter.append(",");
@@ -105,7 +109,9 @@ public class DrtVehicleStoppingTaskWriter {
 			csvWriter.append(",");
 			csvWriter.append(Double.toString(drtStoppingTaskDataEntry.getEndTime()));
 			csvWriter.append(",");
-			csvWriter.append(drtStoppingTaskDataEntry.getPersonId().toString());
+			csvWriter.append(drtStoppingTaskDataEntry.getVehicleId().toString());
+			csvWriter.append(",");
+			csvWriter.append(Integer.toString(drtStoppingTaskDataEntry.getOccupancy()));
 			csvWriter.append("\n");
 		}
 		csvWriter.flush();
@@ -115,7 +121,7 @@ public class DrtVehicleStoppingTaskWriter {
 	/**
 	 * Glob pattern from path, if not found tries to go into the parent directory.
 	 */
-	public static Optional<Path> glob(Path path, String pattern, boolean parent) {
+	public static Optional<Path> glob(Path path, String pattern) {
 		PathMatcher m = path.getFileSystem().getPathMatcher("glob:" + pattern);
 		try {
 			Optional<Path> match = Files.list(path).filter(p -> m.matches(p.getFileName())).findFirst();
