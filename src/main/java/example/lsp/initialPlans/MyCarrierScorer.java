b075478bc3023c9a7457e5848325b73a790b0254
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2022-10-31 12:58:35 +0100

    CarrierScorer: score leg

--- src/main/java/example/lsp/initialPlans/MyCarrierScorer.java
+++ src/main/java/example/lsp/initialPlans/MyCarrierScorer.java
@@ -1,10 +1,13 @@
 package example.lsp.initialPlans;
 
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
-import org.matsim.contrib.freight.carrier.Carrier;
-import org.matsim.contrib.freight.carrier.CarrierPlan;
-import org.matsim.contrib.freight.carrier.ScheduledTour;
+import org.matsim.api.core.v01.population.Leg;
+import org.matsim.contrib.freight.carrier.*;
 import org.matsim.contrib.freight.controler.CarrierScoringFunctionFactory;
+import org.matsim.core.gbl.Gbl;
+import org.matsim.core.population.routes.NetworkRoute;
 import org.matsim.core.scoring.ScoringFunction;
 import org.matsim.core.scoring.SumScoringFunction;
 
@@ -58,17 +61,54 @@ class MyCarrierScorer implements CarrierScoringFunctionFactory {
 		}
 	}
 
-	private static class DriversLegScoring implements SumScoringFunction.BasicScoring {
+	private static class DriversLegScoring implements SumScoringFunction.LegScoring {
+
+		private double score = 0.0;
+		private final Network network;
+		private final Carrier carrier;
+
 
 		public DriversLegScoring(Carrier carrier, Network network) {
+			super();
+			this.network = network;
+			this.carrier = carrier;
 		}
 
-		@Override public void finish() {
-		}
+		@Override public void finish() {}
+
 		@Override public double getScore() {
-			return -10;
+			return score;
 		}
 
+		@Override public void handleLeg(Leg leg) {
+			if(leg.getRoute() instanceof NetworkRoute nRoute){
+				CarrierVehicle vehicle = CarrierUtils.getCarrierVehicle(carrier, nRoute.getVehicleId());
+				Gbl.assertNotNull(vehicle);
+
+				//Distance based costs / score
+				{
+					double distance = 0.0;
+					//TODO KMT: Warum sind Start und EndLink hier enthalten? Ist das dann nicht ggf. doppelt gezählt - bei mehreren Legs in Folge?
+					distance += network.getLinks().get(nRoute.getStartLinkId()).getLength();
+					for (Id<Link> linkId : nRoute.getLinkIds()) {
+						distance += network.getLinks().get(linkId).getLength();
+					}
+					distance += network.getLinks().get(nRoute.getEndLinkId()).getLength();
+
+
+					double distanceCosts = distance * vehicle.getType().getCostInformation().getCostsPerMeter();
+					if (!(distanceCosts >= 0.0)) throw new AssertionError("distanceCosts must be positive");
+					score += (-1) * distanceCosts;
+				}
+
+				//Time-based (driving) costs /score
+				{
+					double timeCosts = leg.getTravelTime().seconds() * vehicle.getType().getCostInformation().getCostsPerSecond();
+					if (!(timeCosts >= 0.0)) throw new AssertionError("timeCosts of leg must be positive");
+					score += (-1) * timeCosts;
+				}
+			}
+		}
 	}
 
 	private static class DriversActivityScoring implements SumScoringFunction.BasicScoring{
