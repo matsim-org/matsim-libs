7d548fc3d57ff8af519057513c36d393789564d5
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2022-10-31 11:24:13 +0100

    CarrierScorer: score fixed costs of Vehicle

--- src/main/java/example/lsp/initialPlans/MyCarrierScorer.java
+++ src/main/java/example/lsp/initialPlans/MyCarrierScorer.java
@@ -2,6 +2,8 @@ package example.lsp.initialPlans;
 
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.contrib.freight.carrier.Carrier;
+import org.matsim.contrib.freight.carrier.CarrierPlan;
+import org.matsim.contrib.freight.carrier.ScheduledTour;
 import org.matsim.contrib.freight.controler.CarrierScoringFunctionFactory;
 import org.matsim.core.scoring.ScoringFunction;
 import org.matsim.core.scoring.SumScoringFunction;
@@ -18,8 +20,8 @@ class MyCarrierScorer implements CarrierScoringFunctionFactory {
 
 	public ScoringFunction createScoringFunction(Carrier carrier) {
 		SumScoringFunction sf = new SumScoringFunction();
-		DriversLegScoring driverLegScoring = new DriversLegScoring(carrier, this.network);
 		VehicleEmploymentScoring vehicleEmploymentScoring = new VehicleEmploymentScoring(carrier);
+		DriversLegScoring driverLegScoring = new DriversLegScoring(carrier, this.network);
 		DriversActivityScoring actScoring = new DriversActivityScoring();
 		sf.addScoringFunction(driverLegScoring);
 		sf.addScoringFunction(vehicleEmploymentScoring);
@@ -28,28 +30,45 @@ class MyCarrierScorer implements CarrierScoringFunctionFactory {
 	}
 
 
+	/**
+	 * Score the (daily) fixed costs of each vehicle that is used.
+	 * Summing up for all tours. The values are taken from the vehicleCostInformation in the vehicleType.
+	 */
+	private static class VehicleEmploymentScoring implements SumScoringFunction.BasicScoring{
+
+		private final Carrier carrier;
+
+		public VehicleEmploymentScoring(Carrier carrier) {
+			super();
+			this.carrier = carrier;
+		}
+
+		@Override public void finish() {}
+
+		@Override public double getScore() {
+			double score = 0.;
+			CarrierPlan selectedPlan = carrier.getSelectedPlan();
+			if(selectedPlan == null) return 0.;
+			for(ScheduledTour tour : selectedPlan.getScheduledTours()){
+				if(!tour.getTour().getTourElements().isEmpty()){
+					score += (-1)*tour.getVehicle().getType().getCostInformation().getFixedCosts();
+				}
+			}
+			return score;
+		}
+	}
+
 	private static class DriversLegScoring implements SumScoringFunction.BasicScoring {
+
 		public DriversLegScoring(Carrier carrier, Network network) {
 		}
 
 		@Override public void finish() {
 		}
-
 		@Override public double getScore() {
 			return -10;
 		}
-	}
 
-	private static class VehicleEmploymentScoring implements SumScoringFunction.BasicScoring{
-		public VehicleEmploymentScoring(Carrier carrier) {
-		}
-
-		@Override public void finish() {
-		}
-
-		@Override public double getScore() {
-			return -100;
-		}
 	}
 
 	private static class DriversActivityScoring implements SumScoringFunction.BasicScoring{
