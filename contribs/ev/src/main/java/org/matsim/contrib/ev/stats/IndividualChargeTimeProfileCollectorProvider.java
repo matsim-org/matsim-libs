947ee7e6b76512de0e0de9c7447d1232117cba55
u229187 <joschka.bischoff@sbb.ch>
2022-11-25 15:23:41 +0100

    allow a configurable number of vehicles in IndividualChargeTimeProfileCollectorProvider

--- contribs/ev/src/main/java/org/matsim/contrib/ev/stats/IndividualChargeTimeProfileCollectorProvider.java
+++ contribs/ev/src/main/java/org/matsim/contrib/ev/stats/IndividualChargeTimeProfileCollectorProvider.java
@@ -19,46 +19,42 @@
 
 package org.matsim.contrib.ev.stats;
 
-import static com.google.common.collect.ImmutableList.toImmutableList;
-import static com.google.common.collect.ImmutableMap.toImmutableMap;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.matsim.contrib.ev.EvUnits;
-import org.matsim.contrib.ev.fleet.ElectricFleet;
-import org.matsim.contrib.ev.fleet.ElectricVehicle;
-import org.matsim.contrib.common.timeprofile.TimeProfileCollector;
-import org.matsim.contrib.common.timeprofile.TimeProfileCollector.ProfileCalculator;
-import org.matsim.contrib.common.timeprofile.TimeProfiles;
-import org.matsim.core.controler.MatsimServices;
-import org.matsim.core.mobsim.framework.listeners.MobsimListener;
-
 import com.google.common.collect.ImmutableList;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
+import org.matsim.contrib.common.timeprofile.TimeProfileCollector;
+import org.matsim.contrib.common.timeprofile.TimeProfileCollector.ProfileCalculator;
+import org.matsim.contrib.common.timeprofile.TimeProfiles;
+import org.matsim.contrib.ev.EvConfigGroup;
+import org.matsim.contrib.ev.EvUnits;
+import org.matsim.contrib.ev.fleet.ElectricFleet;
+import org.matsim.contrib.ev.fleet.ElectricVehicle;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.MatsimServices;
+import org.matsim.core.mobsim.framework.listeners.MobsimListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.common.collect.ImmutableMap.toImmutableMap;
 
 public class IndividualChargeTimeProfileCollectorProvider implements Provider<MobsimListener> {
 	private final ElectricFleet evFleet;
 	private final MatsimServices matsimServices;
+	private final int maxVehicleColumns;
 
 	@Inject
-	public IndividualChargeTimeProfileCollectorProvider(ElectricFleet evFleet, MatsimServices matsimServices) {
+	public IndividualChargeTimeProfileCollectorProvider(ElectricFleet evFleet, MatsimServices matsimServices, Config config) {
 		this.evFleet = evFleet;
 		this.matsimServices = matsimServices;
+		maxVehicleColumns = ConfigUtils.addOrGetModule(config, EvConfigGroup.class).numberOfIndividualTimeProfiles;
 	}
 
-	@Override
-	public MobsimListener get() {
-		ProfileCalculator calc = createIndividualChargeCalculator(evFleet);
-		return new TimeProfileCollector(calc, 300, "individual_charge_time_profiles", matsimServices);
-	}
+	public static ProfileCalculator createIndividualChargeCalculator(final ElectricFleet evFleet, int maxVehicleColumns) {
 
-	private static final int MAX_VEHICLE_COLUMNS = 50;
-
-	public static ProfileCalculator createIndividualChargeCalculator(final ElectricFleet evFleet) {
-		int columns = Math.min(evFleet.getElectricVehicles().size(), MAX_VEHICLE_COLUMNS);
+		int columns = Math.min(evFleet.getElectricVehicles().size(), maxVehicleColumns);
 		List<ElectricVehicle> allEvs = new ArrayList<>(evFleet.getElectricVehicles().values());
 		List<ElectricVehicle> selectedEvs = allEvs.stream().limit(columns).toList();
 
@@ -67,5 +63,11 @@ public class IndividualChargeTimeProfileCollectorProvider implements Provider<Mo
 		return TimeProfiles.createProfileCalculator(header, () -> selectedEvs.stream()
 				.collect(toImmutableMap(ev -> ev.getId() + "",
 						ev -> EvUnits.J_to_kWh(ev.getBattery().getCharge()))));/*in [kWh]*/
+	}
+
+	@Override
+	public MobsimListener get() {
+		ProfileCalculator calc = createIndividualChargeCalculator(evFleet, maxVehicleColumns);
+		return new TimeProfileCollector(calc, 300, "individual_charge_time_profiles", matsimServices);
 	}
 }
