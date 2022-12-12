3b8eb197384b9fef80231b396160d597cde94eb4
Kasia <kasia.kozlowska@arup.com>
2022-12-12 15:14:14 +0000

    remove decoding from encoding tests

--- matsim/src/test/java/org/matsim/core/mobsim/hermes/SpeedTest.java
+++ matsim/src/test/java/org/matsim/core/mobsim/hermes/SpeedTest.java
@@ -19,50 +19,38 @@ public class SpeedTest {
     @Test
     public void encodingFastDecimalPointSpeedRoundsDownToNearestInteger() {
         int encoded = Agent.prepareVelocityForLinkEntry(15.3);
-        double decoded = Agent.decodeVelocityFromLinkEntry(encoded);
-        Assert.assertEquals(15.0, decoded, 0.0);
+	    Assert.assertEquals(15 + 90, encoded);
+
     }
 
     @Test
     public void encodingFastDecimalPointSpeedRoundsUpToNearestInteger() {
         int encoded = Agent.prepareVelocityForLinkEntry(15.6);
-        double decoded = Agent.decodeVelocityFromLinkEntry(encoded);
-        Assert.assertEquals(16.0, decoded, 0.0);
+	    Assert.assertEquals(16 + 90, encoded);
     }
 
     @Test
     public void slowSpeedIsEncodedByMultiplyingBy10() {
         int encoded = Agent.prepareVelocityForLinkEntry(4.6);
-        Assert.assertEquals((int) 4.6 * 10, encoded);
+        Assert.assertEquals((int) (4.6 * 10), encoded);
     }
 
     @Test
     public void slowSpeedIsDecodedByDividingBy10() {
-        double decoded = Agent.decodeVelocityFromLinkEntry((int) 4.6 * 10);
+        double decoded = Agent.decodeVelocityFromLinkEntry((int) (4.6 * 10));
         Assert.assertEquals(4.6, decoded, 0.0);
-    }
-
-    @Test
-    public void slowIntegerSpeedIsEncodedCorrectly() {
-        int encoded = Agent.prepareVelocityForLinkEntry(4);
-        Assert.assertEquals(40, encoded);
-
-        double decoded = Agent.decodeVelocityFromLinkEntry(encoded);
-        Assert.assertEquals(4.0, decoded, 0.0);
     }
 
     @Test
     public void encodingSlowDecimalPointSpeedRoundsDown() {
         int encoded = Agent.prepareVelocityForLinkEntry(5.33);
-        double decoded = Agent.decodeVelocityFromLinkEntry(encoded);
-        Assert.assertEquals(5.3, decoded, 0.0);
+	    Assert.assertEquals((int) (5.3 * 10), encoded);
     }
 
     @Test
     public void encodingSlowDecimalPointSpeedRoundsUp() {
         int encoded = Agent.prepareVelocityForLinkEntry(5.66);
-        double decoded = Agent.decodeVelocityFromLinkEntry(encoded);
-        Assert.assertEquals(5.7, decoded, 0.0);
+	    Assert.assertEquals((int) (5.7 * 10), encoded);
     }
 
     @Test
