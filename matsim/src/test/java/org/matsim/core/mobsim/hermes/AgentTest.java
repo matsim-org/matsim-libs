6d7856c1084932bbbd37981481cb9393f3ea65cf
Kasia <kasia.kozlowska@arup.com>
2022-12-12 16:37:38 +0000

    import junit assert

--- matsim/src/test/java/org/matsim/core/mobsim/hermes/AgentTest.java
+++ matsim/src/test/java/org/matsim/core/mobsim/hermes/AgentTest.java
@@ -21,7 +21,7 @@ package org.matsim.core.mobsim.hermes;
 
 import org.junit.Test;
 import org.locationtech.jts.util.Assert;
-
+import static org.junit.Assert.assertEquals;
 
 public class AgentTest {
 
@@ -51,50 +51,50 @@ public class AgentTest {
 	@Test
 	public void fastSpeedIsEncodedByAdding90() {
 		int encoded = Agent.prepareVelocityForLinkEntry(15);
-		org.junit.Assert.assertEquals(15 + 90, encoded);
+		assertEquals(15 + 90, encoded);
 	}
 
 	@Test
 	public void fastSpeedIsDecodedBySubtracting90() {
 		double decoded = Agent.decodeVelocityFromLinkEntry(15 + 90);
-		org.junit.Assert.assertEquals(15.0, decoded, 0.0);
+		assertEquals(15.0, decoded, 0.0);
 	}
 
 	@Test
 	public void encodingFastDecimalPointSpeedRoundsDownToNearestInteger() {
 		int encoded = Agent.prepareVelocityForLinkEntry(15.3);
-		org.junit.Assert.assertEquals(15 + 90, encoded);
+		assertEquals(15 + 90, encoded);
 
 	}
 
 	@Test
 	public void encodingFastDecimalPointSpeedRoundsUpToNearestInteger() {
 		int encoded = Agent.prepareVelocityForLinkEntry(15.6);
-		org.junit.Assert.assertEquals(16 + 90, encoded);
+		assertEquals(16 + 90, encoded);
 	}
 
 	@Test
 	public void slowSpeedIsEncodedByMultiplyingBy10() {
 		int encoded = Agent.prepareVelocityForLinkEntry(4.6);
-		org.junit.Assert.assertEquals(46, encoded);
+		assertEquals(46, encoded);
 	}
 
 	@Test
 	public void slowSpeedIsDecodedByDividingBy10() {
 		double decoded = Agent.decodeVelocityFromLinkEntry(46);
-		org.junit.Assert.assertEquals(4.6, decoded, 0.0);
+		assertEquals(4.6, decoded, 0.0);
 	}
 
 	@Test
 	public void encodingSlowDecimalPointSpeedRoundsDown() {
 		int encoded = Agent.prepareVelocityForLinkEntry(5.33);
-		org.junit.Assert.assertEquals(53, encoded);
+		assertEquals(53, encoded);
 	}
 
 	@Test
 	public void encodingSlowDecimalPointSpeedRoundsUp() {
 		int encoded = Agent.prepareVelocityForLinkEntry(5.66);
-		org.junit.Assert.assertEquals(57, encoded);
+		assertEquals(57, encoded);
 	}
 
 	@Test
@@ -105,7 +105,7 @@ public class AgentTest {
 		int pcecategory = 0;
 		long flatplanentry = Agent.prepareLinkEntry(eventid, linkid, velocity, pcecategory);
 		double testedVelocity = Agent.getVelocityPlanEntry(flatplanentry);
-		org.junit.Assert.assertEquals(15.0, testedVelocity, 0.0);
+		assertEquals(15.0, testedVelocity, 0.0);
 	}
 
 	@Test
@@ -116,6 +116,6 @@ public class AgentTest {
 		int pcecategory = 0;
 		long flatplanentry = Agent.prepareLinkEntry(eventid, linkid, velocity, pcecategory);
 		double testedVelocity = Agent.getVelocityPlanEntry(flatplanentry);
-		org.junit.Assert.assertEquals(3.4, testedVelocity, 0.0);
+		assertEquals(3.4, testedVelocity, 0.0);
 	}
 }
