c61cad6045bdc93737530d960cc9415219b7e666
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2023-01-04 13:57:11 +0100

    maintenance: Java language improvement

--- contribs/freight/src/main/java/org/matsim/contrib/freight/jsprit/VehicleTypeDependentRoadPricingCalculator.java
+++ contribs/freight/src/main/java/org/matsim/contrib/freight/jsprit/VehicleTypeDependentRoadPricingCalculator.java
@@ -12,11 +12,7 @@
  ******************************************************************************/
 package org.matsim.contrib.freight.jsprit;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import com.graphhopper.jsprit.core.problem.vehicle.VehicleType;
 
@@ -28,27 +24,27 @@ import org.matsim.contrib.roadpricing.RoadPricingSchemeImpl.Cost;
 
 /**
  * Calculator that manages and calculates vehicle type dependent road pricing schemas.
- * 
+ *
  * @author stefan schröder
  *
  */
 public class VehicleTypeDependentRoadPricingCalculator {
-	
+
 	interface TollCalculator {
 		double getTollAmount(Cost cost, Link link);
 	}
-	
+
 	static class LinkCalc implements TollCalculator {
 
-		
+
 		@Override
 		public double getTollAmount(Cost cost, Link link) {
 			if(cost == null) return 0.0;
 			return cost.amount;
 		}
-		
+
 	}
-	
+
 	static class DistanceCalc implements TollCalculator{
 
 		@Override
@@ -56,62 +52,61 @@ public class VehicleTypeDependentRoadPricingCalculator {
 			if(cost == null) return 0.0;
 			return cost.amount*link.getLength();
 		}
-		
+
 	}
-	
-	
+
+
 	private Map<Id<org.matsim.vehicles.VehicleType>, Collection<RoadPricingScheme>> schemes = new HashMap<>();
 
 	private Map<String,TollCalculator> calculators = new HashMap<>();
-	
+
 	/**
 	 * Gets an unmodifiable list of {@link RoadPricingScheme} for a {@link VehicleType} with input-id.
 	 */
 	public Collection<RoadPricingScheme> getPricingSchemes(Id<org.matsim.vehicles.VehicleType> vehicleType){
 		Collection<RoadPricingScheme> collection = schemes.get(vehicleType);
-		if(collection == null) return Collections.unmodifiableCollection(Collections.emptyList());
-		return Collections.unmodifiableCollection(collection);
+		return Collections.unmodifiableCollection(Objects.requireNonNullElse(collection, Collections.emptyList()));
 	}
-	
+
 	/**
 	 * Gets the toll-amount for vehicleType on {@link Link} link at time 'time'.
-	 * 
+	 *
 	 * <p>In case of <code>RoadPricingScheme.TOLL_TYPE_CORDON</code> the toll amount on a link is equivalent to <code>rps.getLinkCostInfo(...).amount</code>.
-	 * 
+	 *
 	 * <p>In case of <code>RoadPricingScheme.TOLL_TYPE_DISTANCE</code> the toll amount on a link is equivalent to <code>rps.getLinkCostInfo(...).amount*link.getLength()</code>.
-	 * 
+	 *
 	 * @param vehicleType type of vehicle
 	 * @param link the link
 	 * @param time the time, measured in seconds from midnight
 	 * @return the toll amount (positive for cost)
 	 */
-	public double getTollAmount(Id<org.matsim.vehicles.VehicleType> vehicleType, Link link, double time){ 
+	public double getTollAmount(Id<org.matsim.vehicles.VehicleType> vehicleType, Link link, double time){
 		Collection<RoadPricingScheme> pricingSchemes = getPricingSchemes(vehicleType);
-		if(pricingSchemes == null) return 0.0; 
+		if(pricingSchemes == null) return 0.0;
 		double toll = 0.0;
 		for(RoadPricingScheme rps : pricingSchemes){
 			Cost linkCostInfo = rps.getLinkCostInfo(link.getId(), time, null, null);
 			toll += calculators.get(rps.getType()).getTollAmount(linkCostInfo,link);
 		}
-		return toll; 
+		return toll;
 	}
-	
-	
+
+
 	public void addPricingScheme(String vehicleTypeId, RoadPricingScheme pricingScheme){
 		Id<org.matsim.vehicles.VehicleType> typeId = Id.create(vehicleTypeId, org.matsim.vehicles.VehicleType.class);
 		addPricingScheme(typeId, pricingScheme);
 	}
-	
+
 	/**
 	 * Adds a {@link RoadPricingScheme} for vehicleTypeId.
-	 * 
+	 *
 	 * <p>Currently, only Cordon and Distance tolls can be calculated.
-	 * 
+	 *
 	 * <p>In case of <code>RoadPricingScheme.TOLL_TYPE_CORDON</code> the toll amount on a link is equivalent to <code>rps.getLinkCostInfo(...).amount</code>.
-	 * 
+	 *
 	 * <p>In case of <code>RoadPricingScheme.TOLL_TYPE_DISTANCE</code> the toll amount on a link is equivalent to <code>rps.getLinkCostInfo(...).amount*link.getLength()</code>.
-	 * 
-	 * 
+	 *
+	 *
 	 * @param vehicleTypeId vehicle type
 	 * @param pricingScheme the {@link RoadPricingScheme} the road pricing scheme
 	 */
@@ -131,10 +126,10 @@ public class VehicleTypeDependentRoadPricingCalculator {
         else throw new IllegalStateException("toll type specification missing. if you use xml add specification to root node like this\n"
             + "<roadpricing type=\"cordon\" name=\"link\"> if it is a link toll and \n<roadpricing type=\"distance\" name=\"distance\"> if it is a distance toll");
 	}
-	
+
 	/**
 	 * Removes {@link RoadPricingScheme} for vehicleTypeId.
-	 * 
+	 *
 	 * @param vehicleTypeId vehicle type
 	 */
 	@SuppressWarnings("unused")
@@ -144,14 +139,14 @@ public class VehicleTypeDependentRoadPricingCalculator {
 
 	/**
 	 * Checks whether there is a {@link RoadPricingScheme} for vehicleType 'typeId'.
-	 * 
+	 *
 	 * @param typeId vehicle type
 	 * @return true if there is a pricing-scheme, false otherwise
 	 */
 	public boolean containsKey(Id<org.matsim.vehicles.VehicleType> typeId) {
 		return schemes.containsKey(typeId);
 	}
-	
+
 	/**
 	 * Gets an unmodifiable map of pricing schemes.
 	 */
