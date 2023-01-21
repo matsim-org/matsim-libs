835ded965637d1010f0923b3d945eaba12cd3292
Kai Nagel <nagel@vsp.tu-berlin.de>
2023-01-21 22:13:28 +0100

    add regression test to the DMC contrib.  Use that regression test plus documentation plus code to understand config options, and write that as comment into example class.

--- code-examples/src/main/java/org/matsim/codeexamples/extensions/discrete_mode_choice/RunDiscreteModeChoiceExample.java
+++ code-examples/src/main/java/org/matsim/codeexamples/extensions/discrete_mode_choice/RunDiscreteModeChoiceExample.java
@@ -19,8 +19,9 @@ public final class RunDiscreteModeChoiceExample{
 	// The code below works with v14 since approx jun'21.  kai
 
 	public static void main ( String [] args ) {
-
-		args = new String [] {"scenarios/equil/config.xml"};
+		if ( args==null || args.length==0 ){
+			args = new String[]{"scenarios/equil/config.xml"};
+		}
 
 		Config config = ConfigUtils.loadConfig( args );
 
@@ -29,52 +30,59 @@ public final class RunDiscreteModeChoiceExample{
 		config.controler().setLastIteration( 2 );
 
 		String modeB = "modeB";
-		final String[] modes = {TransportMode.car, modeB};
+		String modeC = "modeC";
+		final String[] modes = {TransportMode.car, modeB, modeC};
 
-		// we have to do this ugly cast because TeleportedModeParams extends ModeParams and the setter methods are part
-		// of ModeParams and the return signature is ModeParams :-/ . This has to be changed in the Config Group itself
-		var paramB = (PlansCalcRouteConfigGroup.TeleportedModeParams) new PlansCalcRouteConfigGroup.TeleportedModeParams(modeB).setTeleportedModeSpeed(10.);
-		var paramWalk = (PlansCalcRouteConfigGroup.TeleportedModeParams) new PlansCalcRouteConfigGroup.TeleportedModeParams(TransportMode.walk)
-				.setTeleportedModeSpeed(4./3.6);
+		config.plansCalcRoute().addTeleportedModeParams( new PlansCalcRouteConfigGroup.TeleportedModeParams(modeB).setTeleportedModeSpeed(10. ) );
+		config.plansCalcRoute().addTeleportedModeParams( new PlansCalcRouteConfigGroup.TeleportedModeParams(modeC).setTeleportedModeSpeed(10. ) );
+		config.plansCalcRoute().addTeleportedModeParams( new PlansCalcRouteConfigGroup.TeleportedModeParams(TransportMode.walk).setTeleportedModeSpeed(4./3.6 ) );
 
-		config.plansCalcRoute().addTeleportedModeParams(paramB);
-		config.plansCalcRoute().addTeleportedModeParams(paramWalk);
 		config.planCalcScore().addModeParams( new PlanCalcScoreConfigGroup.ModeParams( modeB ).setConstant( 13. ) );
+		config.planCalcScore().addModeParams( new PlanCalcScoreConfigGroup.ModeParams( modeC ).setConstant( 12. ) );
 
 		// the following is first inlined and then adapted from DiscreteModeChoiceConfigurator.configureAsSubtourModeChoiceReplacement( config );
-		config.strategy().addStrategySettings( new StrategySettings().setStrategyName( DiscreteModeChoiceModule.STRATEGY_NAME ).setWeight( 0.1 ) );
+		config.strategy().addStrategySettings( new StrategySettings().setStrategyName( DiscreteModeChoiceModule.STRATEGY_NAME ).setWeight( 1. ) );
 		{
 			DiscreteModeChoiceConfigGroup dmcConfig = ConfigUtils.addOrGetModule( config, DiscreteModeChoiceConfigGroup.class );
 
 			dmcConfig.setCachedModes( CollectionUtils.stringArrayToSet( modes ) );
 			// (I think that this is only a hint to the computation, influencing performance, but not behavior.  kai, jun'21)
 
-			dmcConfig.getSubtourConstraintConfig().setConstrainedModes( CollectionUtils.stringArrayToSet( modes ) );
-			// (These are, I think, the chain-based modes. kai, jun'21)
-
-			dmcConfig.getVehicleTourConstraintConfig().setRestrictedModes( CollectionUtils.stringArrayToSet( modes ) );
-			// (yy I don't know the difference to the subtour constraint.  kai, jun'21)
-
 			dmcConfig.setModelType( ModelModule.ModelType.Tour );
-			// (yy I assume that this means that the algorithm is working at the tour level.  As opposed to trip level.  kai, jun'21)
+			{ // material that is only relevant if ModelType=Tour:
+				// (Means that the algo makes choices for each tour, configured below.  As opposed to making choices for each trip.)
+				dmcConfig.setTourFinder( TourFinderModule.PLAN_BASED );
+				// (A tour is something that returns to its starting point, but this still leaves several options.  "PLAN_BASED" means the
+				// whole plan is considered as the tour ((what happens if it does not return to its starting location?)); there is also
+				// "HOME_BASED" and "ACTIVITY_BASED" (trips starting from a certain activity type).)
+//				dmcConfig.setTourConstraints( CollectionUtils.stringArrayToSet( new String[]{ConstraintModule.VEHICLE_CONTINUITY, ConstraintModule.SUBTOUR_MODE} ) );
+				dmcConfig.setTourConstraints( CollectionUtils.stringArrayToSet( new String[]{ConstraintModule.VEHICLE_CONTINUITY} ) );
+				// (This sets the constraints explicitly.  Can be configured with the next two options. Since backwards compatibility
+				// with the old subtour mode choice behavior is not important for us, we remove the related constraint.)
+				{
+					dmcConfig.getVehicleTourConstraintConfig().setRestrictedModes( CollectionUtils.stringArrayToSet( new String[]{TransportMode.car} ) );
+					// (config group for VehicleTourConstraint.  Vehicles in these modes need to be conserved.)
 
-			dmcConfig.setSelector( SelectorModule.RANDOM );
-			// (yyyy I don't know what this does.  kai, jun'21)
-
-			dmcConfig.setTourConstraints( CollectionUtils.stringArrayToSet( new String[]{ConstraintModule.VEHICLE_CONTINUITY, ConstraintModule.SUBTOUR_MODE} ) );
-			// (yyyy I don't know what this does.  kai, jun'21)
+//					dmcConfig.getSubtourConstraintConfig().setConstrainedModes( CollectionUtils.stringArrayToSet( modes ) );
+					// (config group for SubtourModeConstraint, which needs to be activated above for this here to have effect.
+					// For modes listed here, the whole subtour has to use the same mode.  This is mostly for backwards
+					// compatibility, since this is the behavior of the old subtour mode choice.)
+				}
+			}
+			dmcConfig.setSelector( SelectorModule.MULTINOMIAL_LOGIT );
+			// (This selects between the alternatives. Only "MULTINOMIAL_LOGIT" will actually use a logic model; "RANDOM" selects randomly
+			// between the candidates, and "MAXIMUM" returns the best alternative.  "RANDOM" is what subtour mode choice does.)
 
 			dmcConfig.setTourEstimator( EstimatorModule.MATSIM_DAY_SCORING );
-			// (yyyy I don't know what this does.  kai, jun'21)
+			// (The estimator that is used to come up with utilities for the mode choice.)
+			{
+				dmcConfig.setModeAvailability( ModeAvailabilityModule.DEFAULT );
+				// (do not consider car availability.  If one looks into the code, this is in fact cleverly programmed as a user-replaceable guice binding.)
 
-			dmcConfig.setTourFinder( TourFinderModule.PLAN_BASED );
-			// (yyyy I don't know what this does.  kai, jun'21)
-
-			dmcConfig.setModeAvailability( ModeAvailabilityModule.DEFAULT );
-			// (do not consider car availability.  If one looks into the code, this is in fact cleverly programmed as a replacable guice binding.)
-
-			dmcConfig.getDefaultModeAvailabilityConfig().setAvailableModes( CollectionUtils.stringArrayToSet( modes ) );
-			// (to configure the "detault" mode availability.  Don't know what it does. Presumably configures the modes between which can be switched.  kai, jun'21)
+				dmcConfig.getDefaultModeAvailabilityConfig().setAvailableModes( CollectionUtils.stringArrayToSet( new String[]{TransportMode.car, modeB, modeC} ) );
+				// (_If_ default mode availability is configured above, this configures which modes this will include.  Other modes will not
+				// be considered.  So if one removes modeC above, it will no longer be considered.)
+			}
 		}
 
 		Scenario scenario = ScenarioUtils.loadScenario( config );
