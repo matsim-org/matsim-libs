455bc5d05ef0d52a8a42bd78c964224d135abc34
Johan W. Joubert <johan.joubert@kuleuven.be>
2023-04-13 21:53:33 +0200

    Renamed contrib and package names to lowercase-only.

---
+++ contribs/freightreceiver/src/test/java/org/matsim/contrib/freightreceiver/ReceiverPlanTest.java
@@ -0,0 +1,40 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2018 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.contrib.freightreceiver;
+
+import org.matsim.api.core.v01.Id;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class ReceiverPlanTest {
+
+
+	@Test
+	public void testBuilderTwo() {
+		Receiver receiver = ReceiverUtils.newInstance( Id.create( "1", Receiver.class ) );
+		ReceiverPlan.Builder builder = ReceiverPlan.Builder.newInstance(receiver, true);
+		ReceiverPlan plan = builder.build();
+		Assert.assertEquals("Wrong receiver Id", Id.create("1", Receiver.class), plan.getReceiver().getId());
+		Assert.assertNull("Score should be null", plan.getScore());
+	}
+
+	/* TODO Add tests to check ReceiverOrders */
+
+}
