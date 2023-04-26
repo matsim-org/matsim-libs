799795a97051919e349c2f7d23393edea99f77c2
hzoerner <h.zoerner@campus.tu-berlin.de>
2023-04-26 17:02:46 +0200

    analysis and dashboard for count comparsion

--- contribs/simwrapper/src/main/java/org/matsim/simwrapper/analysis/CountComparisonAnalysis.java
+++ contribs/simwrapper/src/main/java/org/matsim/simwrapper/analysis/CountComparisonAnalysis.java
@@ -2,6 +2,8 @@ package org.matsim.simwrapper.analysis;
 
 import org.apache.commons.csv.CSVPrinter;
 import org.matsim.analysis.VolumesAnalyzer;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.application.CommandSpec;
@@ -11,21 +13,25 @@ import org.matsim.application.options.InputOptions;
 import org.matsim.application.options.OutputOptions;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.events.EventsUtils;
+import org.matsim.core.network.NetworkUtils;
+import org.matsim.counts.Count;
 import org.matsim.counts.Counts;
 import org.matsim.counts.MatsimCountsReader;
+import org.matsim.counts.Volume;
 import picocli.CommandLine;
 
 import java.io.IOException;
+import java.util.Map;
 
 @CommandLine.Command(name = "todo", description = "todo")
 @CommandSpec(requireEvents = true, requireCounts = true, requireNetwork = true, produces = {"count_comparison.csv"})
 public class CountComparisonAnalysis implements MATSimAppCommand {
 
 	@CommandLine.Mixin
-	private final InputOptions input = InputOptions.ofCommand(StuckAgentAnalysis.class);
+	private final InputOptions input = InputOptions.ofCommand(CountComparisonAnalysis.class);
 
 	@CommandLine.Mixin
-	private final OutputOptions output = OutputOptions.ofCommand(StuckAgentAnalysis.class);
+	private final OutputOptions output = OutputOptions.ofCommand(CountComparisonAnalysis.class);
 
 	@CommandLine.Mixin
 	private CsvOptions csv;
@@ -62,16 +68,36 @@ public class CountComparisonAnalysis implements MATSimAppCommand {
 
 	private void writeOutput(Counts<Link> counts, Network network, VolumesAnalyzer volumes) throws IOException {
 
+		Map<Id<Link>, ? extends Link> links = network.getLinks();
+
 		try (CSVPrinter printer = csv.createPrinter(output.getPath("count_comparison.csv"))) {
 
 			printer.printRecord("link_id", "name", "road_type", "observed_traffic_volume", "simulated_traffic_volume");
 
-//			NetworkUtils.getType()
+			for (Map.Entry<Id<Link>, Count<Link>> entry : counts.getCounts().entrySet()) {
+				Id<Link> key = entry.getKey();
+				Map<Integer, Volume> countVolume = entry.getValue().getVolumes();
+				String name = entry.getValue().getCsLabel();
 
+				if (countVolume.isEmpty())
+					continue;
+
+				Double observedTrafficVolume = countVolume.values().stream().map(Volume::getValue).reduce(Double::sum).get();
+				int[] volumesForLink = volumes.getVolumesForLink(key, TransportMode.car);
+
+				Integer simulatedTrafficVolume;
+
+				if (volumesForLink == null) {
+					simulatedTrafficVolume = 0;
+				} else {
+					simulatedTrafficVolume = volumesForLink[0];
+				}
+
+				Link link = links.get(key);
+				String type = NetworkUtils.getType(link);
+
+				printer.printRecord(key, name, type, observedTrafficVolume, simulatedTrafficVolume);
+			}
 		}
-
-
 	}
-
-
 }
---
+++ contribs/simwrapper/src/main/java/org/matsim/simwrapper/dashboard/CountCamparisonDashboard.java
@@ -0,0 +1,31 @@
+package org.matsim.simwrapper.dashboard;
+
+import org.matsim.simwrapper.Dashboard;
+import org.matsim.simwrapper.Header;
+import org.matsim.simwrapper.Layout;
+import org.matsim.simwrapper.analysis.CountComparisonAnalysis;
+import org.matsim.simwrapper.viz.Scatter;
+
+public class CountCamparisonDashboard implements Dashboard {
+
+	@Override
+	public void configure(Header header, Layout layout) {
+
+		header.title = "Count Comparsion Dashboard";
+		header.description = "Comparsion of observed and simulated daily traffic volumes";
+
+		layout.row("scatterplot").el(Scatter.class, ((viz, data) -> {
+			viz.dataset = data.compute(CountComparisonAnalysis.class, "count_comparison.csv");
+
+			viz.title = "Observed and simulated daily traffic volumes";
+
+			viz.legendName = "Road type";
+
+			viz.x = "observed_traffic_volume";
+			viz.y = "simulated_traffic_volume";
+
+			viz.xAxisName = "Observed traffic volume";
+			viz.yAxisName = "Simulated traffic volume";
+		}));
+	}
+}
