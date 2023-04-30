a01e049ceee6c6014c0df19326af41da41387b37
Michal Maciejewski <michal.mac@gmail.com>
2023-04-30 22:52:15 +0200

    common: generalise ProfileWriter to use any time array (instead of fixed-interval TimeDiscretizer)

--- contribs/dvrp/src/main/java/org/matsim/contrib/dvrp/analysis/VehicleTaskProfileView.java
+++ contribs/dvrp/src/main/java/org/matsim/contrib/dvrp/analysis/VehicleTaskProfileView.java
@@ -29,7 +29,6 @@ import java.util.Map;
 import org.apache.commons.lang3.tuple.Pair;
 import org.matsim.contrib.common.timeprofile.ProfileWriter;
 import org.matsim.contrib.dvrp.schedule.Task;
-import org.matsim.contrib.common.timeprofile.TimeDiscretizer;
 
 import com.google.common.collect.ImmutableMap;
 
@@ -65,7 +64,7 @@ public class VehicleTaskProfileView implements ProfileWriter.ProfileView {
 	}
 
 	@Override
-	public TimeDiscretizer timeDiscretizer() {
-		return calculator.getTimeDiscretizer();
+	public int[] times() {
+		return calculator.getTimeDiscretizer().getTimes();
 	}
 }
