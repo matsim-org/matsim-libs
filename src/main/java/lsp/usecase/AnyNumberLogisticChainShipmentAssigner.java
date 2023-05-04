ded23d75894fde45926658664d31e60217773d18
nixlaos <niclas.richter@outlook.com>
2023-05-04 09:21:52 +0200

    AnyNumberLogisticChainShipmentAssigner assigns shipments randomly, regardless of 1 or multiple logistic chains

--- src/main/java/lsp/usecase/AnyNumberLogisticChainShipmentAssigner.java
+++ src/main/java/lsp/usecase/AnyNumberLogisticChainShipmentAssigner.java
@@ -6,6 +6,10 @@ import lsp.ShipmentAssigner;
 import lsp.shipment.LSPShipment;
 import org.matsim.core.gbl.Gbl;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 
 class AnyNumberLogisticChainShipmentAssigner implements ShipmentAssigner {
 
@@ -26,18 +30,32 @@ class AnyNumberLogisticChainShipmentAssigner implements ShipmentAssigner {
 	@Override
 	public void assignToLogisticChain(LSPShipment shipment) {
 		Gbl.assertIf(lsp.getSelectedPlan().getLogisticChains().size() != 0);
-		if (lsp.getSelectedPlan().getLogisticChains().size() == 1) {
-			LogisticChain logisticChain = lsp.getSelectedPlan().getLogisticChains().iterator().next();
-			logisticChain.assignShipment(shipment);
-		} else {
-			for (LogisticChain logisticChain : lsp.getSelectedPlan().getLogisticChains()) {
-				if (logisticChain.getShipments().size() == 0) {
-					logisticChain.assignShipment(shipment);
-					break;
-				}
-			}
-		}
-	}
+		List<LogisticChain> logisticChains = new ArrayList<>(lsp.getSelectedPlan().getLogisticChains());
+//		Random rand = MatsimRandom.getRandom();
+		Random rand = new Random();
+		int index = rand.nextInt(logisticChains.size());
+		LogisticChain logisticChain = logisticChains.get(index);
+		logisticChain.assignShipment(shipment);
+//		if (lsp.getSelectedPlan().getLogisticChains().size() == 1) {
+//			LogisticChain logisticChain = lsp.getSelectedPlan().getLogisticChains().iterator().next();
+//			logisticChain.assignShipment(shipment);
 
+			//for successive distribution of shipments over logistic chains in case number of logisticChains and shipments is equal
+//		} else {
+//			for (LogisticChain logisticChain : lsp.getSelectedPlan().getLogisticChains()) {
+//				if (logisticChain.getShipments().size() == 0) {
+//					logisticChain.assignShipment(shipment);
+//					break;
+//				}
+//			}
+//		} else {
+//			logisticChains.addAll(lsp.getSelectedPlan().getLogisticChains());
+//			Random rand = MatsimRandom.getRandom();
+////			Random rand = new Random();
+//			int index = rand.nextInt(logisticChains.size());
+//			LogisticChain logisticChain = logisticChains.get(index);
+//			logisticChain.assignShipment(shipment);
+//		}
+	}
 }
 
