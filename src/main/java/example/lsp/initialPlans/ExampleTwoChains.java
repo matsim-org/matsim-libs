b721aced757c36c85207841979bc83f4ab22dbdd
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2023-05-05 15:11:30 +0200

    add VspConsistencyCheck. Level WARN is ok.

--- src/main/java/example/lsp/initialPlans/ExampleTwoChains.java
+++ src/main/java/example/lsp/initialPlans/ExampleTwoChains.java
@@ -19,6 +19,7 @@ import org.matsim.contrib.freight.controler.FreightUtils;
 import org.matsim.core.config.CommandLine;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.VspExperimentalConfigGroup;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
@@ -94,6 +95,9 @@ public class ExampleTwoChains {
 		});
 
 		log.info("Run MATSim");
+
+		//The VSP default settings are designed for person transport simulation. After talking to Kai, they will be set to WARN here. Kai MT may'23
+		controler.getConfig().vspExperimental().setVspDefaultsCheckingLevel(VspExperimentalConfigGroup.VspDefaultsCheckingLevel.warn);
 		controler.run();
 
 		new LSPPlanXmlWriter(LSPUtils.getLSPs(controler.getScenario())).write(controler.getConfig().controler().getOutputDirectory() + "/lsps.xml");
