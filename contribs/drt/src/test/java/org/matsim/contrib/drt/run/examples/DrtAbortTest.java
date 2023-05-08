b18734472af45708d1355a734d30e9446b7c8e9b
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2023-05-08 18:21:27 +0200

    Add iterations test and relevant files
    In addition to the functionality test (simpleTest), we also have iterations test, where mode choice is enabled. The necessary vehicle file for this purpose is also added.

--- contribs/drt/src/test/java/org/matsim/contrib/drt/run/examples/DrtAbortTest.java
+++ contribs/drt/src/test/java/org/matsim/contrib/drt/run/examples/DrtAbortTest.java
@@ -1,6 +1,7 @@
 package org.matsim.contrib.drt.run.examples;
 
 import com.google.inject.Inject;
+import com.google.inject.Singleton;
 import org.apache.commons.lang3.mutable.MutableInt;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -20,15 +21,14 @@ import org.matsim.contrib.dvrp.passenger.PassengerRequestRejectedEventHandler;
 import org.matsim.contrib.dvrp.passenger.PassengerRequestSubmittedEvent;
 import org.matsim.contrib.dvrp.passenger.PassengerRequestSubmittedEventHandler;
 import org.matsim.contrib.dvrp.path.VrpPaths;
-import org.matsim.contrib.dvrp.run.AbstractDvrpModeQSimModule;
 import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
 import org.matsim.contrib.dvrp.run.DvrpModule;
 import org.matsim.contrib.dvrp.run.DvrpQSimComponents;
-import org.matsim.contrib.dvrp.trafficmonitoring.QSimFreeSpeedTravelTime;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
+import org.matsim.core.config.groups.StrategyConfigGroup;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
@@ -42,8 +42,8 @@ import org.matsim.core.mobsim.qsim.InternalInterface;
 import org.matsim.core.mobsim.qsim.agents.WithinDayAgentUtils;
 import org.matsim.core.mobsim.qsim.components.QSimComponentsConfigGroup;
 import org.matsim.core.mobsim.qsim.interfaces.MobsimEngine;
-import org.matsim.core.modal.AbstractModalQSimModule;
 import org.matsim.core.population.routes.GenericRouteImpl;
+import org.matsim.core.replanning.strategies.DefaultPlanStrategiesModule;
 import org.matsim.core.router.TripStructureUtils;
 import org.matsim.core.router.costcalculators.OnlyTimeDependentTravelDisutility;
 import org.matsim.core.router.speedy.SpeedyALTFactory;
@@ -68,16 +68,93 @@ public class DrtAbortTest{
 	public MatsimTestUtils utils = new MatsimTestUtils();
 
 	static final String walkAfterRejectMode = "walkAfterReject";
-	@Test
-	public void testAbortHandler() {
+
+	enum Variant {simpleTest, iterations, benchmark };
+
+	@Test public void testAbortHandler() {
+		run( Variant.simpleTest);
+	}
+
+	@Test public void testIterations() {
+		run( Variant.iterations );
+	}
+
+	@Test public void testBenchmark() {
+		run( Variant.benchmark );
+	}
+
+	private void run( Variant variant) {
 		Id.resetCaches();
 		URL configUrl = IOUtils.extendUrl( ExamplesUtils.getTestScenarioURL("mielec" ), "mielec_drt_config.xml" );
 		Config config = ConfigUtils.loadConfig(configUrl, new MultiModeDrtConfigGroup(), new DvrpConfigGroup(),
 				new OTFVisConfigGroup() );
 
-		config.controler().setLastIteration(1);
-		config.plans().setInputFile("plans_only_drt_rejection_test.xml");
-		// Chengqi: I have created a special plan for the rejection handler test: 3 requests within 1 time bin (6:45 - 7:00)
+		boolean rejectionModule = true;
+		config.controler().setLastIteration(50);
+		{
+			StrategyConfigGroup.StrategySettings settings = new StrategyConfigGroup.StrategySettings();
+			settings.setStrategyName(DefaultPlanStrategiesModule.DefaultStrategy.ChangeSingleTripMode);
+			settings.setWeight(0.1);
+			config.strategy().addStrategySettings(settings);
+			config.strategy().setFractionOfIterationsToDisableInnovation(0.9);
+		}
+		{
+			config.changeMode().setModes( new String[] { TransportMode.drt, TransportMode.bike });
+		}
+		{
+			PlanCalcScoreConfigGroup.ModeParams params = new PlanCalcScoreConfigGroup.ModeParams( TransportMode.bike );
+			params.setMarginalUtilityOfTraveling(-12.);
+			config.planCalcScore().addModeParams( params );
+		}
+
+		for ( DrtConfigGroup drtCfg : MultiModeDrtConfigGroup.get( config ).getModalElements()) {
+			drtCfg.vehiclesFile = "vehicles-2-cap-4.xml";
+
+			drtCfg.maxTravelTimeAlpha = 1.5;
+			drtCfg.maxTravelTimeBeta = 600.;
+			drtCfg.maxWaitTime = 300.;
+			drtCfg.stopDuration = 10.;
+		}
+
+		switch ( variant ) {
+			case simpleTest -> {
+				config.controler().setLastIteration(1);
+				config.plans().setInputFile("plans_only_drt_rejection_test.xml");
+				// Chengqi: I have created a special plan for the rejection handler test: 3 requests within 1 time bin (6:45 - 7:00)
+				for ( DrtConfigGroup drtCfg : MultiModeDrtConfigGroup.get( config ).getModalElements()) {
+					drtCfg.vehiclesFile = "vehicles-rejection-test.xml";
+					// Chengqi: I have created a special vehicle file for the rejection handler test: 1 vehicle locates at the departure place of one request
+
+					drtCfg.maxTravelTimeAlpha = 1.2;
+					drtCfg.maxTravelTimeBeta = 100.;
+					drtCfg.maxWaitTime = 10.;
+					drtCfg.stopDuration = 1.;
+					// (Trying to force abort(s); can't say if this is the correct syntax.  kai, apr'23)
+					// Chengqi: With this parameter, 2 out of the 3 requests during 6:45-7:00 will be rejected
+					// -> 2/3 probability of being rejected -> 2/3 of penalty to everyone who submit DRT requests
+					// Based on current setup, at iteration 1, we should see person score event for each person
+					// with a negative score of -6: 12 (base penalty) * 2/3 (probability) * 0.75 (learning rate, current) + 0 (previous penalty) * 0.25 (learning rate, previous)
+					// Currently a manual check is performed and passed. Perhaps an integrated test can be implemented here (TODO).
+				}
+			}
+			case benchmark -> rejectionModule = false;
+			case iterations -> {
+			}
+			// What do we want to see?
+
+			// In early iterations, we want many (maybe 20% or 50%) drt_teleported (because of rejections).
+
+			// In late iterations, we want few drt_teleported (because of mode choice).
+
+			// Need to look at the numbers.
+
+			// There should be a certain rejection rate in a given time bin.  That should translate into a penalty.  The penalty should be reasonable for us.
+
+			// The drt_teleported score should be plausible.
+
+			default -> throw new IllegalStateException("Unexpected value: " + variant);
+		}
+
 		config.controler().setOverwriteFileSetting( OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists );
 		config.controler().setOutputDirectory(utils.getOutputDirectory());
 
@@ -92,35 +169,14 @@ public class DrtAbortTest{
 		}
 
 		config.planCalcScore().setWriteExperiencedPlans( true );
-		for ( DrtConfigGroup drtCfg : MultiModeDrtConfigGroup.get(config ).getModalElements()) {
+
+//		for ( DrtConfigGroup drtCfg : MultiModeDrtConfigGroup.get(config ).getModalElements()) {
 		//relatively high max age to prevent rejections
 //			var drtRequestInsertionRetryParams = new DrtRequestInsertionRetryParams();
 //			drtRequestInsertionRetryParams.maxRequestAge = 7200;
 //			drtCfg.addParameterSet(drtRequestInsertionRetryParams);
 			// I don't know what the above does; might be useful to understand.
-
-			drtCfg.maxTravelTimeAlpha = 1.2;
-			drtCfg.maxTravelTimeBeta = 100.;
-			drtCfg.maxWaitTime = 10.;
-			drtCfg.stopDuration = 1.;
-
-			drtCfg.vehiclesFile = "vehicles-rejection-test.xml";
-			// Chengqi: I have created a special vehicle file for the rejection handler test: 1 vehicle locates at the departure place of one request
-
-			// (Trying to force abort(s); can't say if this is the correct syntax.  kai, apr'23)
-			// Chengqi: With this parameter, 2 out of the 3 requests during 6:45-7:00 will be rejected
-			// -> 2/3 probability of being rejected -> 2/3 of penalty to everyone who submit DRT requests
-			// Based on current setup, at iteration 1, we should see person score event for each person
-			// with a negative score of -6: 12 (base penalty) * 2/3 (probability) * 0.75 (learning rate, current) + 0 (previous penalty) * 0.25 (learning rate, previous)
-			// Currently a manual check is performed and passed. Perhaps an integrated test can be implemtned here (TODO).
-
-		}
-
-
-		QSimComponentsConfigGroup qsimComponents = ConfigUtils.addOrGetModule( config, QSimComponentsConfigGroup.class );
-		List<String> components = qsimComponents.getActiveComponents();
-		components.add( MyAbortHandler.COMPONENT_NAME );
-		qsimComponents.setActiveComponents( components );
+//		}
 
 		MultiModeDrtConfigGroup multiModeDrtConfig = MultiModeDrtConfigGroup.get( config );
 		DrtConfigs.adjustMultiModeDrtConfig(multiModeDrtConfig, config.planCalcScore(), config.plansCalcRoute() );
@@ -128,41 +184,31 @@ public class DrtAbortTest{
 		Scenario scenario = DrtControlerCreator.createScenarioWithDrtRouteFactory( config );
 		ScenarioUtils.loadScenario(scenario );
 
-		// reduce to one person: Chengqi: with my special plans, this is no longer necessary :)
-//		{
-//			List<Id<Person>> keys = new ArrayList<>( scenario.getPopulation().getPersons().keySet() );
-//			keys.remove( 0 );
-//			keys.remove( 0 );
-//			keys.remove( 0 );
-//
-//			for( Id<Person> personId : keys ){
-//				scenario.getPopulation().removePerson( personId );
-//			}
-//		}
-
 		Controler controler = new Controler(scenario);
 		controler.addOverridingModule(new DvrpModule() );
 		controler.addOverridingModule(new MultiModeDrtModule() );
 		controler.configureQSimComponents( DvrpQSimComponents.activateAllModes(multiModeDrtConfig ) );
 
-		controler.addOverridingQSimModule( new AbstractQSimModule(){
-			@Override protected void configureQSim(){
-				this.addQSimComponentBinding( MyAbortHandler.COMPONENT_NAME ).to( MyAbortHandler.class );
-			}
-		} );
+		if (rejectionModule){
+			QSimComponentsConfigGroup qsimComponents = ConfigUtils.addOrGetModule( config, QSimComponentsConfigGroup.class );
+			List<String> components = qsimComponents.getActiveComponents();
+			components.add( MyAbortHandler.COMPONENT_NAME );
+			qsimComponents.setActiveComponents( components );
 
-
-		// Prescription for the DRT Rejection module and add handler bindings to DRT Rejection Handler
-		controler.addOverridingModule(new AbstractModule() {
-			@Override
-			public void install() {
-				// TODO Chengqi: perhaps there is a better way to do this? I remember you mentioned in class that bind to instance is not the best way...
-				bind(DrtRejectionHandler.class).toInstance(new DrtRejectionHandler());
-
-				addEventHandlerBinding().to(DrtRejectionHandler.class);
-				addControlerListenerBinding().to(DrtRejectionHandler.class);
-			}
-		});
+			controler.addOverridingQSimModule( new AbstractQSimModule(){
+				@Override protected void configureQSim(){
+					this.addQSimComponentBinding( MyAbortHandler.COMPONENT_NAME ).to( MyAbortHandler.class );
+				}
+			} );
+			controler.addOverridingModule(new AbstractModule() {
+				@Override
+				public void install() {
+					bind(DrtRejectionEventHandler.class).in(Singleton.class);
+					addEventHandlerBinding().to(DrtRejectionEventHandler.class);
+					addControlerListenerBinding().to(DrtRejectionEventHandler.class);
+				}
+			});
+		}
 
 		controler.run();
 
@@ -297,7 +343,7 @@ public class DrtAbortTest{
 		}
 	}
 
-	private static class DrtRejectionHandler implements PassengerRequestRejectedEventHandler,
+	private static class DrtRejectionEventHandler implements PassengerRequestRejectedEventHandler,
 			PassengerRequestSubmittedEventHandler, IterationEndsListener {
 		private final Map<Integer, MutableInt> numberOfRejectionsPerTimeBin = new HashMap<>();
 		private final Map<Integer, MutableInt> numberOfSubmissionsPerTimeBin = new HashMap<>();
