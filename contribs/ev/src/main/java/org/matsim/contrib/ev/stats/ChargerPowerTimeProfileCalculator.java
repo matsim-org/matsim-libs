7ed56878395e6a6ed6bcae8bc110e5e892d202a9
Michal Maciejewski <michal.mac@gmail.com>
2023-05-10 16:07:08 +0200

    fix bindings etc. for charger power time profiles

--- contribs/ev/src/main/java/org/matsim/contrib/ev/stats/ChargerPowerTimeProfileCalculator.java
+++ contribs/ev/src/main/java/org/matsim/contrib/ev/stats/ChargerPowerTimeProfileCalculator.java
@@ -1,56 +1,38 @@
 package org.matsim.contrib.ev.stats;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import com.google.inject.Inject;
-import com.google.inject.Provider;
 import org.matsim.api.core.v01.Id;
 import org.matsim.contrib.common.timeprofile.TimeDiscretizer;
 import org.matsim.contrib.ev.EvConfigGroup;
-import org.matsim.contrib.ev.EvUnits;
 import org.matsim.contrib.ev.charging.ChargingEndEvent;
 import org.matsim.contrib.ev.charging.ChargingEndEventHandler;
 import org.matsim.contrib.ev.charging.ChargingStartEvent;
 import org.matsim.contrib.ev.charging.ChargingStartEventHandler;
-import org.matsim.contrib.ev.fleet.ElectricFleet;
 import org.matsim.contrib.ev.infrastructure.Charger;
-import org.matsim.contrib.ev.infrastructure.ChargingInfrastructure;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.config.groups.QSimConfigGroup;
-import org.matsim.core.controler.MatsimServices;
-import org.matsim.core.events.MobsimScopeEventHandler;
-import org.matsim.core.mobsim.framework.events.MobsimAfterSimStepEvent;
-import org.matsim.core.mobsim.framework.listeners.MobsimAfterSimStepListener;
-import org.matsim.core.mobsim.framework.listeners.MobsimListener;
 import org.matsim.vehicles.Vehicle;
 
-import java.util.*;
-
-import static com.google.common.collect.ImmutableMap.toImmutableMap;
+import com.google.inject.Inject;
 
 
-public class ChargerPowerTimeProfileCalculator implements  ChargingStartEventHandler,ChargingEndEventHandler,
-		 MobsimAfterSimStepListener {
+public class ChargerPowerTimeProfileCalculator implements  ChargingStartEventHandler, ChargingEndEventHandler {
 
-	private Map<Id<Charger>, List<Double>> chargerProfiles;
+	private Map<Id<Charger>, List<Double>> chargerProfiles = new HashMap<>();
 	private final TimeDiscretizer timeDiscretizer;
-	private final ElectricFleet evFleet;
-	private final ChargingInfrastructure chargingInfrastructure;
-	private final MatsimServices matsimServices;
 	private final int chargeTimeStep;
 
 	@Inject
-	public ChargerPowerTimeProfileCalculator(ElectricFleet evFleet, ChargingInfrastructure chargingInfrastructure,
-											 MatsimServices matsimServices, Config config) {
-		this.evFleet = evFleet;
-		this.chargingInfrastructure = chargingInfrastructure;
-		this.matsimServices = matsimServices;
+	public ChargerPowerTimeProfileCalculator(Config config) {
 		chargeTimeStep = ConfigUtils.addOrGetModule(config, EvConfigGroup.class).chargeTimeStep;
 		double qsimEndTime = ConfigUtils.addOrGetModule(config, QSimConfigGroup.class).getEndTime().orElse(0.0);
 		timeDiscretizer = new TimeDiscretizer((int)Math.ceil(qsimEndTime), chargeTimeStep);
 	}
-
-	private static final Map<Id<Charger>, Double> chargerEnergy = new HashMap<>();
 
 	private static final Map<Id<Charger>, List<Id<Vehicle>>> vehiclesAtCharger = new HashMap<>();
 	private static final Map<Id<Vehicle>, Double> vehiclesEnergyPreviousTimeStep = new HashMap<>();
@@ -108,28 +90,28 @@ public class ChargerPowerTimeProfileCalculator implements  ChargingStartEventHan
 	}
 
 
-	public void notifyMobsimAfterSimStep(MobsimAfterSimStepEvent event) {
-		if ((event.getSimulationTime() + 1) % chargeTimeStep == 0) {
-			vehiclesAtCharger.forEach((charger, vehicleList) -> {
-				List<Double> previousValues = chargerProfiles.get(charger);
-				if (!vehicleList.isEmpty()) {
-					double energy = vehicleList.stream().mapToDouble(vehicleId -> EvUnits.J_to_kWh((Objects.requireNonNull(evFleet.getElectricVehicles().get(vehicleId)).getBattery()
-							.getCharge() - vehiclesEnergyPreviousTimeStep.get(vehicleId)) * (3600.0 / chargeTimeStep))).sum();
-					if (!Double.isNaN(energy) && !(energy == 0.0)) {
-						previousValues.add(energy);
-						chargerProfiles.put(charger, previousValues);
-						vehicleList.forEach(vehicleId -> vehiclesEnergyPreviousTimeStep.put(vehicleId, Objects.requireNonNull(evFleet.getElectricVehicles().get(vehicleId)).getBattery().getCharge()));
-					} else {
-						previousValues.add(0.0);
-						chargerProfiles.put(charger, previousValues);
-					}
-				} else {
-					previousValues.add(0.0);
-					chargerProfiles.put(charger,previousValues);
-				}
-			});
-		}
-	}
+//	public void notifyMobsimAfterSimStep(MobsimAfterSimStepEvent event) {
+//		if ((event.getSimulationTime() + 1) % chargeTimeStep == 0) {
+//			vehiclesAtCharger.forEach((charger, vehicleList) -> {
+//				List<Double> previousValues = chargerProfiles.get(charger);
+//				if (!vehicleList.isEmpty()) {
+//					double energy = vehicleList.stream().mapToDouble(vehicleId -> EvUnits.J_to_kWh((Objects.requireNonNull(evFleet.getElectricVehicles().get(vehicleId)).getBattery()
+//							.getCharge() - vehiclesEnergyPreviousTimeStep.get(vehicleId)) * (3600.0 / chargeTimeStep))).sum();
+//					if (!Double.isNaN(energy) && !(energy == 0.0)) {
+//						previousValues.add(energy);
+//						chargerProfiles.put(charger, previousValues);
+//						vehicleList.forEach(vehicleId -> vehiclesEnergyPreviousTimeStep.put(vehicleId, Objects.requireNonNull(evFleet.getElectricVehicles().get(vehicleId)).getBattery().getCharge()));
+//					} else {
+//						previousValues.add(0.0);
+//						chargerProfiles.put(charger, previousValues);
+//					}
+//				} else {
+//					previousValues.add(0.0);
+//					chargerProfiles.put(charger,previousValues);
+//				}
+//			});
+//		}
+//	}
 
 	public TimeDiscretizer getTimeDiscretizer() {
 		return timeDiscretizer;
--- contribs/ev/src/main/java/org/matsim/contrib/ev/stats/ChargerPowerTimeProfileView.java
+++ contribs/ev/src/main/java/org/matsim/contrib/ev/stats/ChargerPowerTimeProfileView.java
@@ -1,30 +1,22 @@
 package org.matsim.contrib.ev.stats;
 
-import com.google.common.collect.ImmutableMap;
+import java.awt.Paint;
+import java.util.Map;
+
 import org.apache.commons.lang3.tuple.Pair;
 import org.matsim.api.core.v01.Id;
 import org.matsim.contrib.common.timeprofile.ProfileWriter;
-import org.matsim.contrib.ev.infrastructure.Charger;
 
-import java.awt.*;
-import java.util.Comparator;
-import java.util.Map;
+import com.google.common.collect.ImmutableMap;
 
-import one.util.streamex.EntryStream;
 
 public class ChargerPowerTimeProfileView implements ProfileWriter.ProfileView {
 	private final ChargerPowerTimeProfileCalculator calculator;
-	private final Comparator<Id<Charger>> comparator;
 
-	private final Map<String, Paint> seriesPaints;
-
-	public ChargerPowerTimeProfileView(ChargerPowerTimeProfileCalculator calculator, Comparator<Id<Charger>> comparator,
-									   Map<Id<Charger>, Paint> chargerPaint) {
+	public ChargerPowerTimeProfileView(ChargerPowerTimeProfileCalculator calculator) {
 		this.calculator = calculator;
-		this.comparator = comparator;
-		seriesPaints = EntryStream.of(chargerPaint).mapKeys(Id<Charger>::toString).toMap();
-
 	}
+
 	@Override
 	public int[] times() {
 		return calculator.getTimeDiscretizer().getTimes();
@@ -35,15 +27,13 @@ public class ChargerPowerTimeProfileView implements ProfileWriter.ProfileView {
 		return calculator.getChargerProfiles()
 				.entrySet()
 				.stream()
-				.sorted(Map.Entry.comparingByKey(comparator))
+				.sorted(Map.Entry.comparingByKey(Id::compareTo))
 				.map(e -> Pair.of(e.getKey().toString(), e.getValue()))
 				.collect(ImmutableMap.toImmutableMap(Map.Entry::getKey, Map.Entry::getValue));
 	}
 
 	@Override
 	public Map<String, Paint> seriesPaints() {
-		return seriesPaints;
+		return Map.of();
 	}
-
-
 }
