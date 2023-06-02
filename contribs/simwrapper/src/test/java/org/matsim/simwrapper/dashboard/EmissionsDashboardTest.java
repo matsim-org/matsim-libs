ecd13c0cdcd14b8fd9a007015cb92a4436aea84c
frievoe97 <friedrich.voelkers@me.com>
2023-06-02 11:25:25 +0200

    Created first emissions dashboard

--- contribs/simwrapper/src/test/java/org/matsim/simwrapper/dashboard/EmissionsDashboardTest.java
+++ contribs/simwrapper/src/test/java/org/matsim/simwrapper/dashboard/EmissionsDashboardTest.java
@@ -25,7 +25,6 @@ import org.matsim.vehicles.VehicleUtils;
 public class EmissionsDashboardTest {
 
 	private static final String HBEFA_2020_PATH = "https://svn.vsp.tu-berlin.de/repos/public-svn/3507bb3997e5657ab9da76dbedbb13c9b5991d3e/0e73947443d68f95202b71a156b337f7f71604ae/";
-
 	private static final String HBEFA_FILE_COLD_DETAILED = HBEFA_2020_PATH + "82t7b02rc0rji2kmsahfwp933u2rfjlkhfpi2u9r20.enc";
 	private static final String HBEFA_FILE_WARM_DETAILED = HBEFA_2020_PATH + "944637571c833ddcf1d0dfcccb59838509f397e6.enc";
 	private static final String HBEFA_FILE_COLD_AVERAGE = HBEFA_2020_PATH + "r9230ru2n209r30u2fn0c9rn20n2rujkhkjhoewt84202.enc" ;
@@ -33,7 +32,6 @@ public class EmissionsDashboardTest {
 
 	@Rule
 	public MatsimTestUtils utils = new MatsimTestUtils();
-
 
 	@Test
 	public void generate() {
