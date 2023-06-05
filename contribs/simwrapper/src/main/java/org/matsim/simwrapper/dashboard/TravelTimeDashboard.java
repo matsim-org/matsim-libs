35194f0b99311052e11b51fe7283e838d81e2755
hzoerner <h.zoerner@campus.tu-berlin.de>
2023-06-05 11:07:43 +0200

    some dummy set up (for test reasons)

--- contribs/simwrapper/src/main/java/org/matsim/simwrapper/dashboard/TravelTimeDashboard.java
+++ contribs/simwrapper/src/main/java/org/matsim/simwrapper/dashboard/TravelTimeDashboard.java
@@ -1,8 +1,12 @@
 package org.matsim.simwrapper.dashboard;
 
+import org.matsim.application.analysis.traffic.CongestionAnalysis;
 import org.matsim.simwrapper.Dashboard;
 import org.matsim.simwrapper.Header;
 import org.matsim.simwrapper.Layout;
+import org.matsim.simwrapper.viz.Line;
+
+import java.util.ArrayList;
 
 public class TravelTimeDashboard implements Dashboard {
 
@@ -10,6 +14,12 @@ public class TravelTimeDashboard implements Dashboard {
 	private final int startTime;
 	private final int maxTime;
 	private final int timeSlice;
+
+	public TravelTimeDashboard(){
+		this.startTime = 0;
+		this.maxTime = 86400;
+		this.timeSlice = 900;
+	}
 
 	public TravelTimeDashboard(int startTime, int maxTime, int timeSlice){
 
@@ -24,6 +34,17 @@ public class TravelTimeDashboard implements Dashboard {
 		header.title = "Travel times and congestion";
 		header.description = "Analysis of super important stuff";
 
-		//layout.row();
+		layout.row("test_row").el(Line.class, (viz, data) -> {
+
+			viz.title = "Overall network congestion index";
+			viz.description = "by time";
+
+			viz.dataset = data.compute(CongestionAnalysis.class, "network_congestion_total.csv");
+
+			viz.title = "Congestion index";
+			viz.x = "time";
+			viz.columns = new ArrayList<>();
+			viz.columns.add("congestion_index");
+		});
 	}
 }
