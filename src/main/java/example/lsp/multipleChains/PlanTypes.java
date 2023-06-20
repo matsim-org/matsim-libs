392f3bb6662b84523cf82872baaba2dbe1e0f967
nixlaos <niclas.richter@outlook.com>
2023-06-20 12:29:55 +0200

    defined possible plan types

---
+++ src/main/java/example/lsp/multipleChains/PlanTypes.java
@@ -0,0 +1,10 @@
+package example.lsp.multipleChains;
+
+public class PlanTypes {
+		static final String SINGLE_ECHELON_SINGLE_CHAIN = "singleEchelonSingleChain";
+		static final String DOUBLE_ECHELON_SINGLE_CHAIN = "doubleEchelonSingleChain";
+		static final String SINGLE_ECHELON_MULTIPLE_CHAIN = "singleEchelonMultipleChain";
+		static final String DOUBLE_ECHELON_MULTIPLE_CHAIN = "doubleEchelonMultipleChain";
+		static final String HYBRID_PLAN = "hybridPlan";
+}
+
