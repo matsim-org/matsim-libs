f174d7dc1c2b74b7c713917e5057d83509e84919
Kai Nagel <nagel@vsp.tu-berlin.de>
2023-07-12 15:31:12 +0200

    subpopulation regression test

--- code-examples/src/main/java/org/matsim/codeexamples/RunAbcSimpleExample.java
+++ code-examples/src/main/java/org/matsim/codeexamples/RunAbcSimpleExample.java
@@ -1,12 +1,22 @@
 package org.matsim.codeexamples;
 
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.NetworkFactory;
+import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.network.NetworkChangeEvent;
+import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.io.IOUtils;
 import org.matsim.examples.ExamplesUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.matsim.core.network.NetworkChangeEvent.ChangeType.*;
 
 public final class RunAbcSimpleExample{
 
@@ -14,7 +24,40 @@ public final class RunAbcSimpleExample{
 
 		Config config = ConfigUtils.loadConfig( args );
 
+		config.network().setTimeVariantNetwork( true );
+
 		Scenario scenario = ScenarioUtils.loadScenario( config );
+
+		for( Person person : scenario.getPopulation().getPersons().values() ){
+			person.getAttributes().putAttribute( "income", 20. );
+
+		}
+
+
+
+		NetworkFactory nf = scenario.getNetwork().getFactory();
+
+
+		List<NetworkChangeEvent> changeEvents = new ArrayList<>();
+
+		for( Link link : scenario.getNetwork().getLinks().values() ){
+
+//			NetworkChangeEvent changeEvent = new NetworkChangeEvent( 8*3600. );
+//			changeEvent.addLink( link );
+//			NetworkChangeEvent.ChangeValue changeValue = new NetworkChangeEvent.ChangeValue( OFFSET_IN_SI_UNITS, -10.*3.6 );
+//			changeEvent.setFreespeedChange( changeValue );
+//
+//			changeEvents.add( changeEvent );
+
+			link.getAttributes().putAttribute( "cobblestone", 20 );
+
+		}
+
+		NetworkUtils.writeNetwork( scenario.getNetwork(), "abc.xml" );
+
+		System.exit(-1);
+
+		NetworkUtils.setNetworkChangeEvents( scenario.getNetwork(), changeEvents );
 
 		Controler controler = new Controler( scenario );
 
--- code-examples/src/main/java/org/matsim/codeexamples/programming/multipleSubpopulations/RunSubpopulationsExample.java
+++ code-examples/src/main/java/org/matsim/codeexamples/programming/multipleSubpopulations/RunSubpopulationsExample.java
@@ -24,6 +24,7 @@ import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Node;
 import org.matsim.api.core.v01.population.*;
+import org.matsim.contrib.otfvis.OTFVisLiveModule;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
@@ -32,12 +33,10 @@ import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
 import org.matsim.core.population.PopulationUtils;
 import org.matsim.core.replanning.modules.ReRoute;
-import org.matsim.core.replanning.strategies.DefaultPlanStrategiesModule;
 import org.matsim.core.replanning.strategies.DefaultPlanStrategiesModule.DefaultStrategy;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.io.IOUtils;
 import org.matsim.examples.ExamplesUtils;
-import org.matsim.utils.objectattributes.ObjectAttributesXmlWriter;
 
 import java.io.File;
 import java.net.URL;
@@ -69,11 +68,6 @@ import static org.matsim.core.replanning.strategies.DefaultPlanStrategiesModule.
 public class RunSubpopulationsExample {
 	final static String EQUIL_NETWORK = new File("scenarios/equil-extended/network.xml").getAbsolutePath();
 	final static String PLANS = new File("scenarios/equil-extended/plans-with-subpopulation.xml").getAbsolutePath();
-//	final static String OBJECT_ATTRIBUTES = new File("scenarios/equil-extended/personAtrributes-with-subpopulation.xml").getAbsolutePath();
-	final static String CONFIG = "scenarios/equil-extended/config-with-subpopulation.xml";
-	final static String OUTPUT = "./output/example";
-
-//	private static final String SUBPOP_ATTRIB_NAME = "subpopulation";
 	private static final String SUBPOP1_NAME = "time";
 	private static final String SUBPOP2_NAME = "reroute";
 	/**
@@ -84,15 +78,20 @@ public class RunSubpopulationsExample {
 		// already exist.  
 				
 		// building and running the simulation based on the example scenario:
-		Config config = ConfigUtils.createConfig(); 
-		ConfigUtils.loadConfig(config, CONFIG);
-		config.plans().setInputFile(PLANS);
-//		config.plans().setInputPersonAttributeFile(OBJECT_ATTRIBUTES);
-//		config.plans().setSubpopulationAttributeName(SUBPOP_ATTRIB_NAME); /* This is the default anyway. */
-		config.network().setInputFile(EQUIL_NETWORK);
-		config.controler().setOutputDirectory(OUTPUT);
-		config.controler().setLastIteration( 1 );
-		
+		Config config;
+		if ( args != null && args.length!=0 && args[0]==null ) {
+			config = ConfigUtils.loadConfig( args );
+		} else {
+			config = ConfigUtils.loadConfig( "scenarios/equil-extended/config-with-subpopulation.xml" );
+			config.controler().setOutputDirectory( "./output/example" );
+			config.controler().setLastIteration( 1 );
+		}
+
+//		Config config = ConfigUtils.createConfig();
+//		ConfigUtils.loadConfig(config, "scenarios/equil-extended/config-with-subpopulation.xml" );
+//		config.plans().setInputFile(PLANS);
+//		config.network().setInputFile(EQUIL_NETWORK);
+
 		/* Strategies set up in the 'strategy' module of the config file are only applicable to the 'default' population, i.e. those agents without a subpopulation
 		 * attribute. For all agents with a subpopulation attribute, you need to set up the suite of strategies for the subpopulation(s).
 		 */
@@ -102,7 +101,6 @@ public class RunSubpopulationsExample {
 			/* Set up the 'time' subpopulation to only consider time allocation 
 			 * as a strategy, 20% of the time, and the balance using ChangeExpBeta. */
 			config.strategy().addStrategySettings( new StrategySettings().setStrategyName( DefaultStrategy.TimeAllocationMutator ).setSubpopulation(SUBPOP1_NAME ).setWeight(0.2 ) );
-
 			config.strategy().addStrategySettings( new StrategySettings().setStrategyName( DefaultSelector.ChangeExpBeta ).setSubpopulation(SUBPOP1_NAME ).setWeight(0.8 ) );
 		}
 		{
@@ -126,6 +124,8 @@ public class RunSubpopulationsExample {
 		
 		/* Run the model. */
 		Controler controler = new Controler(config);
+
+//		controler.addOverridingModule( new OTFVisLiveModule() );
 		
 		controler.run();
 	}
@@ -140,7 +140,6 @@ public class RunSubpopulationsExample {
 			createPopulation(sc, SUBPOP1_NAME, 100);
 			createPopulation(sc, SUBPOP2_NAME, 100);
 			new PopulationWriter(sc.getPopulation(), sc.getNetwork()).write(PLANS);
-//			new ObjectAttributesXmlWriter(sc.getPopulation().getPersonAttributes()).writeFile(OBJECT_ATTRIBUTES);
 		}
 	}
 
