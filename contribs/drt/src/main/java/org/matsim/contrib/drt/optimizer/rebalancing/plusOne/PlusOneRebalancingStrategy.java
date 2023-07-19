154bf4133f02c17bf17df07b18003bc198685d3b
Michal Maciejewski <michal.mac@gmail.com>
2023-07-19 23:43:16 +0200

    drt: improve field names in PlusOneRebalancingStrategy

--- contribs/drt/src/main/java/org/matsim/contrib/drt/optimizer/rebalancing/plusOne/PlusOneRebalancingStrategy.java
+++ contribs/drt/src/main/java/org/matsim/contrib/drt/optimizer/rebalancing/plusOne/PlusOneRebalancingStrategy.java
@@ -32,19 +32,18 @@ import org.matsim.core.events.MobsimScopeEventHandler;
  * @author Chengqi Lu
  */
 public class PlusOneRebalancingStrategy
-		implements RebalancingStrategy, PassengerRequestScheduledEventHandler, DrtRequestSubmittedEventHandler,
-		PassengerRequestRejectedEventHandler, MobsimScopeEventHandler {
+	implements RebalancingStrategy, PassengerRequestScheduledEventHandler, DrtRequestSubmittedEventHandler, PassengerRequestRejectedEventHandler,
+	MobsimScopeEventHandler {
 	private static final Logger log = LogManager.getLogger(PlusOneRebalancingStrategy.class);
 
 	private final String mode;
 	private final Network network;
 	private final LinkBasedRelocationCalculator linkBasedRelocationCalculator;
 
-	private final List<Id<Link>> targetLinkIdList = new ArrayList<>();
-	private final Map<Id<Request>, Id<Link>> potentialDrtTripMap = new HashMap<>();
+	private final List<Id<Link>> targetLinks = new ArrayList<>();
+	private final Map<Id<Request>, Id<Link>> potentialTargetLinks = new HashMap<>();
 
-	public PlusOneRebalancingStrategy(String mode, Network network,
-			LinkBasedRelocationCalculator linkBasedRelocationCalculator) {
+	public PlusOneRebalancingStrategy(String mode, Network network, LinkBasedRelocationCalculator linkBasedRelocationCalculator) {
 		this.mode = mode;
 		this.network = network;
 		this.linkBasedRelocationCalculator = linkBasedRelocationCalculator;
@@ -54,16 +53,14 @@ public class PlusOneRebalancingStrategy
 	public List<Relocation> calcRelocations(Stream<? extends DvrpVehicle> rebalancableVehicles, double time) {
 		List<? extends DvrpVehicle> rebalancableVehicleList = rebalancableVehicles.collect(toList());
 
-		final List<Id<Link>> copiedTargetLinkIdList;
+		final List<Id<Link>> copiedTargetLinks;
 		synchronized (this) {
 			//may happen in parallel to handling PassengerRequestScheduledEvent emitted by UnplannedRequestInserter
-			copiedTargetLinkIdList = new ArrayList<>(targetLinkIdList);
-			targetLinkIdList.clear(); // clear the target map for next rebalancing cycle
+			copiedTargetLinks = new ArrayList<>(targetLinks);
+			targetLinks.clear(); // clear the target map for next rebalancing cycle
 		}
 
-		final List<Link> targetLinkList = copiedTargetLinkIdList.stream()
-				.map(network.getLinks()::get)
-				.collect(toList());
+		final List<Link> targetLinkList = copiedTargetLinks.stream().map(network.getLinks()::get).collect(toList());
 
 		log.debug("There are in total " + targetLinkList.size() + " rebalance targets at this time period");
 		log.debug("There are " + rebalancableVehicleList.size() + " vehicles that can be rebalanced");
@@ -75,17 +72,17 @@ public class PlusOneRebalancingStrategy
 	@Override
 	public void handleEvent(DrtRequestSubmittedEvent event) {
 		if (event.getMode().equals(mode)) {
-			potentialDrtTripMap.put(event.getRequestId(), event.getFromLinkId());
+			potentialTargetLinks.put(event.getRequestId(), event.getFromLinkId());
 		}
 	}
 
 	@Override
 	public void handleEvent(PassengerRequestScheduledEvent event) {
 		if (event.getMode().equals(mode)) {
-			Id<Link> linkId = potentialDrtTripMap.remove(event.getRequestId());
+			Id<Link> linkId = potentialTargetLinks.remove(event.getRequestId());
 			synchronized (this) {
 				// event was emitted by UnplannedRequestInserter, it may arrive during calcRelocations()
-				targetLinkIdList.add(linkId);
+				targetLinks.add(linkId);
 			}
 		}
 	}
@@ -93,7 +90,7 @@ public class PlusOneRebalancingStrategy
 	@Override
 	public void handleEvent(PassengerRequestRejectedEvent event) {
 		if (event.getMode().equals(mode)) {
-			potentialDrtTripMap.remove(event.getRequestId());
+			potentialTargetLinks.remove(event.getRequestId());
 		}
 	}
 }
