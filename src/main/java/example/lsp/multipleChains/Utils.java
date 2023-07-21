789990da752a1be849d29fc2279cb3ffbce9e7c8
nixlaos <niclas.richter@outlook.com>
2023-07-21 18:02:38 +0200

    new method to create lsp shipments from carrier shipments

--- src/main/java/example/lsp/multipleChains/Utils.java
+++ src/main/java/example/lsp/multipleChains/Utils.java
@@ -1,6 +1,15 @@
 package example.lsp.multipleChains;
 
+import lsp.shipment.LSPShipment;
+import lsp.shipment.ShipmentUtils;
+import org.matsim.api.core.v01.Id;
+import org.matsim.contrib.freight.carrier.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toMap;
@@ -41,4 +50,25 @@ public class Utils {
 
 		}
 	}
+
+	public static Collection<LSPShipment> createLSPShipmentsFromCarrierShipments(Carrier carrier) {
+		List<LSPShipment> shipmentList = new ArrayList<>();
+
+		List<CarrierShipment> carrierShipments = carrier.getShipments().values().stream()
+				.toList();
+
+		for (CarrierShipment shipment : carrierShipments) {
+			ShipmentUtils.LSPShipmentBuilder builder = ShipmentUtils.LSPShipmentBuilder.newInstance(Id.create(shipment.getId().toString(), LSPShipment.class));
+			builder.setCapacityDemand(shipment.getSize());
+			builder.setFromLinkId(shipment.getFrom());
+			builder.setToLinkId(shipment.getTo());
+			builder.setStartTimeWindow(shipment.getPickupTimeWindow());
+			builder.setEndTimeWindow(shipment.getDeliveryTimeWindow());
+			builder.setPickupServiceTime(shipment.getPickupServiceTime());
+			builder.setDeliveryServiceTime(shipment.getDeliveryServiceTime());
+			shipmentList.add(builder.build());
+		}
+		return shipmentList;
+	}
+
 }
