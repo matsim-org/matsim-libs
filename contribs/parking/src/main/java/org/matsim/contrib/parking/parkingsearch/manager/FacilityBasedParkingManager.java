772c004ae8c4149ee03503e31564e3afeb1aaad2
Ricardo Ewert <ewert@vsp.tu-berlin.de>
2023-09-08 12:01:39 +0200

    formatting

--- contribs/parking/src/main/java/org/matsim/contrib/parking/parkingsearch/manager/FacilityBasedParkingManager.java
+++ contribs/parking/src/main/java/org/matsim/contrib/parking/parkingsearch/manager/FacilityBasedParkingManager.java
@@ -37,8 +37,7 @@ import java.util.*;
 import java.util.Map.Entry;
 
 /**
- * @author  jbischoff, schlenther
- *
+ * @author jbischoff, schlenther
  */
 public class FacilityBasedParkingManager implements ParkingSearchManager {
 
@@ -55,7 +54,7 @@ public class FacilityBasedParkingManager implements ParkingSearchManager {
 	protected Map<Id<Vehicle>, Id<ActivityFacility>> parkingReservation = new HashMap<>();
 	protected Map<Id<Vehicle>, Id<Link>> parkingLocationsOutsideFacilities = new HashMap<>();
 	protected Map<Id<Link>, Set<Id<ActivityFacility>>> facilitiesPerLink = new HashMap<>();
-    protected Network network;
+	protected Network network;
 	protected boolean canParkOnlyAtFacilities;
 	private final int maxSlotIndex;
 	private final int maxTime;
@@ -64,14 +63,15 @@ public class FacilityBasedParkingManager implements ParkingSearchManager {
 
 	@Inject
 	public FacilityBasedParkingManager(Scenario scenario) {
-		ParkingSearchConfigGroup psConfigGroup = (ParkingSearchConfigGroup) scenario.getConfig().getModules().get(ParkingSearchConfigGroup.GROUP_NAME);
+		ParkingSearchConfigGroup psConfigGroup = (ParkingSearchConfigGroup) scenario.getConfig().getModules().get(
+			ParkingSearchConfigGroup.GROUP_NAME);
 		canParkOnlyAtFacilities = psConfigGroup.getCanParkOnlyAtFacilities();
 		this.network = scenario.getNetwork();
 		parkingFacilities = scenario.getActivityFacilities()
-				.getFacilitiesForActivityType(ParkingUtils.PARKACTIVITYTYPE);
+			.getFacilitiesForActivityType(ParkingUtils.PARKACTIVITYTYPE);
 		LogManager.getLogger(getClass()).info(parkingFacilities.toString());
-		this.timeBinSize = 15*60;
-		this.maxTime =  24 * 3600 -1;
+		this.timeBinSize = 15 * 60;
+		this.maxTime = 24 * 3600 - 1;
 		this.maxSlotIndex = (this.maxTime / this.timeBinSize) + 1;
 		this.startTime = 9 * 3600;
 
@@ -160,7 +160,7 @@ public class FacilityBasedParkingManager implements ParkingSearchManager {
 		Set<Id<ActivityFacility>> parkingFacilitiesAtLink = this.facilitiesPerLink.get(linkId);
 		for (Id<ActivityFacility> fac : parkingFacilitiesAtLink) {
 			double cap = this.parkingFacilities.get(fac).getActivityOptions().get(ParkingUtils.PARKACTIVITYTYPE)
-					.getCapacity();
+				.getCapacity();
 			this.reservationsRequests.get(fac).increment();
 			if (this.occupation.get(fac).doubleValue() < cap) {
 				// LogManager.getLogger(getClass()).info("occ:
@@ -184,7 +184,7 @@ public class FacilityBasedParkingManager implements ParkingSearchManager {
 
 	@Override
 	public boolean parkVehicleHere(Id<Vehicle> vehicleId, Id<Link> linkId, double time) {
-        return parkVehicleAtLink(vehicleId, linkId, time);
+		return parkVehicleAtLink(vehicleId, linkId, time);
 	}
 
 	protected boolean parkVehicleAtLink(Id<Vehicle> vehicleId, Id<Link> linkId, double time) {
@@ -201,7 +201,7 @@ public class FacilityBasedParkingManager implements ParkingSearchManager {
 				return true;
 			} else {
 				throw new RuntimeException("no parking reservation found for vehicle " + vehicleId.toString()
-						+ "arrival on link " + linkId + " with parking restriction");
+					+ "arrival on link " + linkId + " with parking restriction");
 			}
 		}
 	}
@@ -227,11 +227,13 @@ public class FacilityBasedParkingManager implements ParkingSearchManager {
 		for (Entry<Id<ActivityFacility>, MutableLong> e : this.occupation.entrySet()) {
 			Id<Link> linkId = this.parkingFacilities.get(e.getKey()).getLinkId();
 			double capacity = this.parkingFacilities.get(e.getKey()).getActivityOptions()
-					.get(ParkingUtils.PARKACTIVITYTYPE).getCapacity();
+				.get(ParkingUtils.PARKACTIVITYTYPE).getCapacity();
 			double x = this.parkingFacilities.get(e.getKey()).getCoord().getX();
 			double y = this.parkingFacilities.get(e.getKey()).getCoord().getY();
 
-			String s = linkId.toString() + ";" + x + ";" + y + ";" + e.getKey().toString() + ";" + capacity + ";" + e.getValue().toString() + ";" + this.reservationsRequests.get(e.getKey()).toString() + ";" + this.numberOfParkedVehicles.get(e.getKey()).toString() + ";" + this.rejectedReservations.get(e.getKey()).toString();
+			String s = linkId.toString() + ";" + x + ";" + y + ";" + e.getKey().toString() + ";" + capacity + ";" + e.getValue().toString() + ";" + this.reservationsRequests.get(
+				e.getKey()).toString() + ";" + this.numberOfParkedVehicles.get(e.getKey()).toString() + ";" + this.rejectedReservations.get(
+				e.getKey()).toString();
 			stats.add(s);
 		}
 		return stats;
@@ -242,30 +244,31 @@ public class FacilityBasedParkingManager implements ParkingSearchManager {
 		for (int time : rejectedReservationsByTime.keySet()) {
 
 			String s = Time.writeTime(time, Time.TIMEFORMAT_HHMM) + ";" + rejectedReservationsByTime.get(time) + ";" + foundParkingByTime.get(
-					time) + ";" + unparkByTime.get(time);
+				time) + ";" + unparkByTime.get(time);
 			stats.add(s);
 
 		}
 		return stats;
 	}
-	public double getNrOfAllParkingSpacesOnLink (Id<Link> linkId){
+
+	public double getNrOfAllParkingSpacesOnLink(Id<Link> linkId) {
 		double allSpaces = 0;
 		Set<Id<ActivityFacility>> parkingFacilitiesAtLink = this.facilitiesPerLink.get(linkId);
 		if (!(parkingFacilitiesAtLink == null)) {
-			for (Id<ActivityFacility> fac : parkingFacilitiesAtLink){
+			for (Id<ActivityFacility> fac : parkingFacilitiesAtLink) {
 				allSpaces += this.parkingFacilities.get(fac).getActivityOptions().get(ParkingUtils.PARKACTIVITYTYPE).getCapacity();
 			}
 		}
 		return allSpaces;
 	}
 
-	public double getNrOfFreeParkingSpacesOnLink (Id<Link> linkId){
+	public double getNrOfFreeParkingSpacesOnLink(Id<Link> linkId) {
 		double allFreeSpaces = 0;
 		Set<Id<ActivityFacility>> parkingFacilitiesAtLink = this.facilitiesPerLink.get(linkId);
 		if (parkingFacilitiesAtLink == null) {
 			return 0;
 		} else {
-			for (Id<ActivityFacility> fac : parkingFacilitiesAtLink){
+			for (Id<ActivityFacility> fac : parkingFacilitiesAtLink) {
 				int cap = (int) this.parkingFacilities.get(fac).getActivityOptions().get(ParkingUtils.PARKACTIVITYTYPE).getCapacity();
 				allFreeSpaces += (cap - this.occupation.get(fac).intValue());
 			}
@@ -277,11 +280,11 @@ public class FacilityBasedParkingManager implements ParkingSearchManager {
 		return this.parkingFacilities;
 	}
 
-	public void registerRejectedReservation(double now){
+	public void registerRejectedReservation(double now) {
 		rejectedReservationsByTime.get(getTimeSlotIndex(now) * timeBinSize).increment();
 	}
 
-	public TreeSet<Integer> getTimeSteps(){
+	public TreeSet<Integer> getTimeSteps() {
 		TreeSet<Integer> timeSteps = new TreeSet<>();
 		int slotIndex = 0;
 		while (slotIndex <= maxSlotIndex) {
@@ -295,7 +298,7 @@ public class FacilityBasedParkingManager implements ParkingSearchManager {
 		if (time > this.maxTime) {
 			return this.maxSlotIndex;
 		}
-		return ((int)time / this.timeBinSize);
+		return ((int) time / this.timeBinSize);
 	}
 
 	@Override
