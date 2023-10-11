be3cd5a2796aa664b02d349270618adcda1a3b2e
u229187 <joschka.bischoff@sbb.ch>
2023-10-11 15:41:35 +0200

    remove old Attribute reference in TransitScheduleUtils

--- matsim/src/main/java/org/matsim/pt/transitSchedule/TransitScheduleUtils.java
+++ matsim/src/main/java/org/matsim/pt/transitSchedule/TransitScheduleUtils.java
@@ -29,43 +29,20 @@ import org.matsim.pt.transitSchedule.api.TransitStopFacility;
 
 /**
  * Helper class for commonly used operations on TransitSchedules
- * 
+ *
  * @author Thibaut Dubernet, gleich
  *
  */
 public final class TransitScheduleUtils {
-	// Logic gotten from PopulationUtils, but I am actually a bit unsure about the value of those methods now that
-	// attributable is the only way to get attributes...
 
-	public static Object getStopFacilityAttribute(TransitStopFacility facility, String key) {
-		return facility.getAttributes().getAttribute( key );
+	private TransitScheduleUtils() {
 	}
 
-	public static void putStopFacilityAttribute(TransitStopFacility facility, String key, Object value ) {
-		facility.getAttributes().putAttribute( key, value ) ;
-	}
-
-	public static Object removeStopFacilityAttribute( TransitStopFacility facility, String key ) {
-		return facility.getAttributes().removeAttribute( key );
-	}
-
-	public static Object getLineAttribute(TransitLine facility, String key) {
-		return facility.getAttributes().getAttribute( key );
-	}
-
-	public static void putLineAttribute(TransitLine facility, String key, Object value ) {
-		facility.getAttributes().putAttribute( key, value ) ;
-	}
-
-	public static Object removeLineAttribute( TransitLine facility, String key ) {
-		return facility.getAttributes().removeAttribute( key );
-	}
-	
-	public final static QuadTree<TransitStopFacility> createQuadTreeOfTransitStopFacilities(TransitSchedule transitSchedule) {
+	public static QuadTree<TransitStopFacility> createQuadTreeOfTransitStopFacilities(TransitSchedule transitSchedule) {
 		return createQuadTreeOfTransitStopFacilities(transitSchedule.getFacilities().values());
 	}
-	
-	public final static QuadTree<TransitStopFacility> createQuadTreeOfTransitStopFacilities(Collection<TransitStopFacility> transitStopFacilities) {
+
+	public static QuadTree<TransitStopFacility> createQuadTreeOfTransitStopFacilities(Collection<TransitStopFacility> transitStopFacilities) {
 		double minX = Double.POSITIVE_INFINITY;
 		double minY = Double.POSITIVE_INFINITY;
 		double maxX = Double.NEGATIVE_INFINITY;
