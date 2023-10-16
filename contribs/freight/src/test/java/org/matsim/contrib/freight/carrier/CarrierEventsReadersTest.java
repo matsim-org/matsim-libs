32142fce553b3397eb3f3caec202cb035ace2d01
nixlaos <niclas.richter@outlook.com>
2023-10-16 11:50:07 +0200

    completing Test for CarrierEventsReaders

--- contribs/freight/src/test/java/org/matsim/contrib/freight/carrier/CarrierEventsReadersTest.java
+++ contribs/freight/src/test/java/org/matsim/contrib/freight/carrier/CarrierEventsReadersTest.java
@@ -2,14 +2,18 @@ package org.matsim.contrib.freight.carrier;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.matsim.contrib.freight.events.CarrierEventsReaders;
+import org.matsim.contrib.freight.events.*;
 import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.config.groups.ControlerConfigGroup;
 import org.matsim.core.events.EventsUtils;
-import org.matsim.core.events.MatsimEventsReader;
+import org.matsim.core.events.algorithms.EventWriterXML;
 import org.matsim.testcases.MatsimTestUtils;
 import org.matsim.testcases.utils.EventsCollector;
 
-import static org.junit.Assert.assertEquals;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class CarrierEventsReadersTest {
 
@@ -18,37 +22,58 @@ public class CarrierEventsReadersTest {
 
 	@Test
 	public void testWriteReadServiceBasedEvents() {
-		EventsManager eventsManager = EventsUtils.createEventsManager();
-		EventsCollector collector = new EventsCollector();
-		MatsimEventsReader carrierEventsReaders = CarrierEventsReaders.createEventsReader(eventsManager);
+		EventsManager eventsManager1 = EventsUtils.createEventsManager();
+		EventsManager eventsManager2 = EventsUtils.createEventsManager();
+		EventsCollector collector1 = new EventsCollector();
+		EventsCollector collector2 = new EventsCollector();
 
-		eventsManager.addHandler(collector);
-		eventsManager.initProcessing();
-		carrierEventsReaders.readFile(utils.getClassInputDirectory() + "serviceBasedEvents.xml");
-		eventsManager.finishProcessing();
+		eventsManager1.addHandler(collector1);
+		eventsManager1.initProcessing();
+		CarrierEventsReaders.createEventsReader(eventsManager1)
+				.readFile(utils.getClassInputDirectory() + "serviceBasedEvents.xml");
+		eventsManager1.finishProcessing();
 
-		//Hier müsste dann wohl "serviceBasedEventsWritten.xml" geschrieben werden und dann auch nochmal der eventsManager für die ausgeschriebene Datei durchlaufen werden
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		EventWriterXML writer = new EventWriterXML(outputStream);
+		collector1.getEvents().forEach(writer::handleEvent);
+		writer.closeFile();
 
-		assertEquals("number of events should be same", collector.getEvents().size(), collector.getEvents().size());
-		MatsimTestUtils.assertEqualEventsFiles(utils.getClassInputDirectory() + "serviceBasedEvents.xml", utils.getClassInputDirectory() + "serviceBasedEventsWritten.xml");
-		MatsimTestUtils.assertEqualFilesLineByLine(utils.getClassInputDirectory() + "serviceBasedEvents.xml", utils.getClassInputDirectory() + "serviceBasedEventsWritten.xml");
+		eventsManager2.addHandler(collector2);
+		eventsManager2.initProcessing();
+		CarrierEventsReaders.createEventsReader(eventsManager2)
+				.readStream(new ByteArrayInputStream(outputStream.toByteArray()), ControlerConfigGroup.EventsFileFormat.xml);
+		eventsManager2.finishProcessing();
+
+		assertThat(collector1.getEvents()).usingRecursiveFieldByFieldElementComparator()
+				.containsExactlyElementsOf(collector2.getEvents());
 	}
 
 	@Test
 	public void testWriteReadShipmentBasedEvents() {
-		EventsManager eventsManager = EventsUtils.createEventsManager();
-		EventsCollector collector = new EventsCollector();
-		MatsimEventsReader carrierEventsReaders = CarrierEventsReaders.createEventsReader(eventsManager);
+		EventsManager eventsManager1 = EventsUtils.createEventsManager();
+		EventsManager eventsManager2 = EventsUtils.createEventsManager();
+		EventsCollector collector1 = new EventsCollector();
+		EventsCollector collector2 = new EventsCollector();
 
-		eventsManager.addHandler(collector);
-		eventsManager.initProcessing();
-		carrierEventsReaders.readFile(utils.getClassInputDirectory() + "shipmentBasedEvents.xml");
-		eventsManager.finishProcessing();
+		eventsManager1.addHandler(collector1);
+		eventsManager1.initProcessing();
+		CarrierEventsReaders.createEventsReader(eventsManager1)
+						.readFile(utils.getClassInputDirectory() + "shipmentBasedEvents.xml");
+		eventsManager1.finishProcessing();
 
-		assertEquals("number of events should be same", collector.getEvents().size(), collector.getEvents().size());
-		MatsimTestUtils.assertEqualEventsFiles(utils.getClassInputDirectory() + "shipmentBasedEvents.xml", utils.getClassInputDirectory() + "shipmentBasedEventsWritten.xml");
-		MatsimTestUtils.assertEqualFilesLineByLine(utils.getClassInputDirectory() + "shipmentBasedEvents.xml", utils.getClassInputDirectory() + "shipmentBasedEventsWritten.xml");
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		EventWriterXML writer = new EventWriterXML(outputStream);
+		collector1.getEvents().forEach(writer::handleEvent);
+		writer.closeFile();
+
+		eventsManager2.addHandler(collector2);
+		eventsManager2.initProcessing();
+		CarrierEventsReaders.createEventsReader(eventsManager2)
+				.readStream(new ByteArrayInputStream(outputStream.toByteArray()), ControlerConfigGroup.EventsFileFormat.xml);
+		eventsManager2.finishProcessing();
+
+		assertThat(collector1.getEvents()).usingRecursiveFieldByFieldElementComparator()
+				.containsExactlyElementsOf(collector2.getEvents());
 	}
-
 
 }
