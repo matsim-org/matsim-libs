e2a10c2746aa6ec3625b07afe952530dd8274fd0
Sebastian Hörl <sebastian.horl@irt-systemx.fr>
2023-11-25 21:29:35 +0100

    feat: flexibly configure the drt insertion process (#2947)
    * drt insertion extension
    
    * some last updates
    
    * add warning about distance calculations
    
    * switch Pair in InsertionDistanceCalculator to record
    
    * integrate latest changes about group bookings
    
    * take into account multi-person requests when calculating occupancy

---
+++ contribs/drt-extensions/src/main/java/org/matsim/contrib/drt/extension/insertion/DrtInsertionModule.java
@@ -0,0 +1,448 @@
+package org.matsim.contrib.drt.extension.insertion;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.contrib.drt.extension.insertion.ConfigurableCostCalculatorStrategy.SoftInsertionParams;
+import org.matsim.contrib.drt.extension.insertion.constraints.ExclusivityConstraint;
+import org.matsim.contrib.drt.extension.insertion.constraints.ExclusivityConstraint.ExclusivityVoter;
+import org.matsim.contrib.drt.extension.insertion.constraints.SingleRequestConstraint;
+import org.matsim.contrib.drt.extension.insertion.constraints.SkillsConstraint;
+import org.matsim.contrib.drt.extension.insertion.constraints.SkillsConstraint.RequestRequirementsSupplier;
+import org.matsim.contrib.drt.extension.insertion.constraints.SkillsConstraint.VehicleSkillsSupplier;
+import org.matsim.contrib.drt.extension.insertion.constraints.VehicleRangeConstraint;
+import org.matsim.contrib.drt.extension.insertion.constraints.VehicleRangeConstraint.VehicleRangeSupplier;
+import org.matsim.contrib.drt.extension.insertion.distances.ApproximateDistanceCalculator;
+import org.matsim.contrib.drt.extension.insertion.distances.DistanceCalculator;
+import org.matsim.contrib.drt.extension.insertion.distances.RoutingDistanceCalculator;
+import org.matsim.contrib.drt.extension.insertion.objectives.PassengerDelayObjective;
+import org.matsim.contrib.drt.extension.insertion.objectives.VehicleActiveTimeObjective;
+import org.matsim.contrib.drt.extension.insertion.objectives.VehicleDistanceObjective;
+import org.matsim.contrib.drt.extension.insertion.objectives.VehicleDistanceObjective.VehcileDistanceWeights;
+import org.matsim.contrib.drt.optimizer.insertion.CostCalculationStrategy;
+import org.matsim.contrib.drt.passenger.DrtOfferAcceptor;
+import org.matsim.contrib.drt.run.DrtConfigGroup;
+import org.matsim.contrib.dvrp.run.AbstractDvrpModeQSimModule;
+import org.matsim.core.router.costcalculators.OnlyTimeDependentTravelDisutility;
+import org.matsim.core.router.speedy.SpeedyALTFactory;
+import org.matsim.core.router.util.LeastCostPathCalculatorFactory;
+import org.matsim.core.router.util.TravelDisutility;
+import org.matsim.core.router.util.TravelTime;
+
+import com.google.common.base.Preconditions;
+import com.google.inject.Key;
+import com.google.inject.Singleton;
+
+public class DrtInsertionModule extends AbstractDvrpModeQSimModule {
+	public DrtInsertionModule(DrtConfigGroup drtConfig) {
+		super(drtConfig.getMode());
+	}
+
+	// EXCLUSIVITY CONSTRAINT
+
+	private boolean useExclusivityContraint = false;
+	private Class<? extends ExclusivityVoter> exclusivityVoterClass;
+	private ExclusivityVoter exclusivityVoter;
+
+	public DrtInsertionModule withExclusivity(Class<? extends ExclusivityVoter> exclusivityVoterClass) {
+		Preconditions.checkState(!this.useExclusivityContraint);
+		this.useExclusivityContraint = true;
+		this.exclusivityVoterClass = exclusivityVoterClass;
+		return this;
+	}
+
+	public DrtInsertionModule withExclusivity(ExclusivityVoter exclusivityVoter) {
+		Preconditions.checkState(!this.useExclusivityContraint);
+		this.useExclusivityContraint = true;
+		this.exclusivityVoter = exclusivityVoter;
+		return this;
+	}
+
+	private void configureExclusivityConstraint(List<Key<? extends DrtInsertionConstraint>> constraintBindings) {
+		if (useExclusivityContraint) {
+			if (exclusivityVoter != null) {
+				bindModal(ExclusivityVoter.class).toInstance(exclusivityVoter);
+			}
+
+			if (exclusivityVoterClass != null) {
+				bindModal(ExclusivityVoter.class).to(modalKey(exclusivityVoterClass));
+			}
+
+			bindModal(ExclusivityConstraint.class).toProvider(modalProvider(getter -> {
+				ExclusivityVoter exclusivityVoter = getter.getModal(ExclusivityVoter.class);
+				return new ExclusivityConstraint(exclusivityVoter);
+			})).in(Singleton.class);
+
+			constraintBindings.add(modalKey(ExclusivityConstraint.class));
+		}
+	}
+
+	// SINGLE REQUEST CONSTRAINT
+
+	private boolean useSingleRequestContraint = false;
+
+	public DrtInsertionModule withSingleRequestPerPerson() {
+		Preconditions.checkState(!this.useSingleRequestContraint);
+		this.useSingleRequestContraint = true;
+		return this;
+	}
+
+	private void configureSingleRequestConstraint(List<Key<? extends DrtInsertionConstraint>> constraintBindings) {
+		if (useSingleRequestContraint) {
+			bindModal(SingleRequestConstraint.class).toProvider(modalProvider(getter -> {
+				return new SingleRequestConstraint();
+			})).in(Singleton.class);
+
+			addMobsimScopeEventHandlerBinding().to(SingleRequestConstraint.class);
+			constraintBindings.add(modalKey(SingleRequestConstraint.class));
+		}
+	}
+
+	// SKILLS CONSTRAINT
+
+	private boolean useSkillsConstraint = false;
+
+	private Class<? extends VehicleSkillsSupplier> vehicleSkillsSupplierClass;
+	private VehicleSkillsSupplier vehicleSkillsSupplier;
+
+	private Class<? extends RequestRequirementsSupplier> requestRequirementsSupplierClass;
+	private RequestRequirementsSupplier requestRequirementsSupplier;
+
+	public DrtInsertionModule withSkills(VehicleSkillsSupplier vehicleSkillsSupplier,
+			RequestRequirementsSupplier requestRequirementsSupplier) {
+		Preconditions.checkState(!this.useSkillsConstraint);
+		this.vehicleSkillsSupplier = vehicleSkillsSupplier;
+		this.requestRequirementsSupplier = requestRequirementsSupplier;
+		this.useSkillsConstraint = true;
+		return this;
+	}
+
+	public DrtInsertionModule withSkills(Class<? extends VehicleSkillsSupplier> vehicleSkillsSupplierClass,
+			Class<? extends RequestRequirementsSupplier> requestRequirementsSupplierClass) {
+		Preconditions.checkState(!this.useSkillsConstraint);
+		this.vehicleSkillsSupplierClass = vehicleSkillsSupplierClass;
+		this.requestRequirementsSupplierClass = requestRequirementsSupplierClass;
+		this.useSkillsConstraint = true;
+		return this;
+	}
+
+	private void configureSkillsContraint(List<Key<? extends DrtInsertionConstraint>> constraintBindings) {
+		if (useSkillsConstraint) {
+			if (vehicleSkillsSupplier != null) {
+				bindModal(VehicleSkillsSupplier.class).toInstance(vehicleSkillsSupplier);
+			}
+
+			if (vehicleSkillsSupplierClass != null) {
+				bindModal(VehicleSkillsSupplier.class).to(modalKey(vehicleSkillsSupplierClass));
+			}
+
+			if (requestRequirementsSupplier != null) {
+				bindModal(RequestRequirementsSupplier.class).toInstance(requestRequirementsSupplier);
+			}
+
+			if (requestRequirementsSupplierClass != null) {
+				bindModal(RequestRequirementsSupplier.class).to(modalKey(requestRequirementsSupplierClass));
+			}
+
+			bindModal(SkillsConstraint.class).toProvider(modalProvider(getter -> {
+				VehicleSkillsSupplier vehicleSkillsSupplier = getter.getModal(VehicleSkillsSupplier.class);
+				RequestRequirementsSupplier requestRequirementsSupplier = getter
+						.getModal(RequestRequirementsSupplier.class);
+				return new SkillsConstraint(vehicleSkillsSupplier, requestRequirementsSupplier);
+			})).in(Singleton.class);
+
+			constraintBindings.add(modalKey(SkillsConstraint.class));
+		}
+	}
+
+	// CUSTOM CONSTRAINTS
+
+	private final List<Class<? extends DrtInsertionConstraint>> customConstraintClasses = new LinkedList<>();
+	private final List<DrtInsertionConstraint> customConstraints = new LinkedList<>();
+
+	public DrtInsertionModule withConstraint(Class<? extends DrtInsertionConstraint> constraintClass) {
+		customConstraintClasses.add(constraintClass);
+		return this;
+	}
+
+	public DrtInsertionModule withConstraint(DrtInsertionConstraint constraint) {
+		customConstraints.add(constraint);
+		return this;
+	}
+
+	// RANGE CONSTRAINT
+
+	private boolean useRangeConstraint = false;
+	private double rangeEstimationFactor = Double.NaN;
+
+	private VehicleRangeSupplier vehicleRangeSupplier;
+	private Class<? extends VehicleRangeSupplier> vehicleRangeSupplierClass;
+
+	public DrtInsertionModule withVehicleRange(VehicleRangeSupplier rangeSupplier) {
+		Preconditions.checkState(!useRangeConstraint);
+		useRangeConstraint = true;
+		this.vehicleRangeSupplier = rangeSupplier;
+		return this;
+	}
+
+	public DrtInsertionModule withVehicleRange(double vehicleRange) {
+		withVehicleRange(v -> vehicleRange);
+		return this;
+	}
+
+	public DrtInsertionModule withVehicleRange(Class<? extends VehicleRangeSupplier> rangeSupplierClass) {
+		Preconditions.checkState(!useRangeConstraint);
+		useRangeConstraint = true;
+		this.vehicleRangeSupplierClass = rangeSupplierClass;
+		return this;
+	}
+
+	public DrtInsertionModule withRangeEstimationFactor(double rangeEstimationFactor) {
+		this.rangeEstimationFactor = rangeEstimationFactor;
+		return this;
+	}
+
+	private void configureRangeContraint(List<Key<? extends DrtInsertionConstraint>> constraintBindings) {
+		if (useRangeConstraint) {
+			if (vehicleRangeSupplier != null) {
+				bindModal(VehicleRangeSupplier.class).toInstance(vehicleRangeSupplier);
+			}
+
+			if (vehicleRangeSupplierClass != null) {
+				bindModal(VehicleRangeSupplier.class).to(modalKey(vehicleRangeSupplierClass));
+			}
+
+			bindModal(VehicleRangeConstraint.class).toProvider(modalProvider(getter -> {
+				DistanceCalculator distanceCalculator = getter.getModal(RoutingDistanceCalculator.class);
+				DistanceCalculator distanceApproximator = Double.isFinite(rangeEstimationFactor)
+						? new ApproximateDistanceCalculator(rangeEstimationFactor)
+						: null;
+
+				return new VehicleRangeConstraint(vehicleRangeSupplier, distanceCalculator, distanceApproximator);
+			}));
+
+			constraintBindings.add(modalKey(VehicleRangeConstraint.class));
+		}
+	}
+
+	// SOFT INSERTION
+
+	private boolean useSoftInsertionPenalties = false;
+	private ConfigurableCostCalculatorStrategy.SoftInsertionParams softInsertionParams;
+
+	public DrtInsertionModule withSoftInsertionPenalty(double pickupWeight, double dropoffWeight) {
+		Preconditions.checkState(!this.useSoftInsertionPenalties);
+		this.useSoftInsertionPenalties = true;
+		this.softInsertionParams = new SoftInsertionParams(pickupWeight, dropoffWeight);
+		return this;
+	}
+
+	// PICKUP AND DROPOFF TIME WINDOW
+
+	private double promisedPickupTimeWindow = Double.POSITIVE_INFINITY;
+	private double promisedDropoffTimeWindow = Double.POSITIVE_INFINITY;
+
+	public DrtInsertionModule withPromisedPickupTimeWindow(double promisedPickupTimeWindow) {
+		this.promisedPickupTimeWindow = promisedPickupTimeWindow;
+		return this;
+	}
+
+	public DrtInsertionModule withPromisedDropoffTimeWindow(double promisedDropoffTimeWindow) {
+		this.promisedDropoffTimeWindow = promisedDropoffTimeWindow;
+		return this;
+	}
+
+	// OBJECTIVES
+
+	private enum Objective {
+		Custom, VehicleActiveTime, PassengerDelay, VehicleDistance
+	}
+
+	private Objective selectedObjective = null;
+
+	// Vehicle active time
+
+	public DrtInsertionModule minimizeVehicleActiveTime() {
+		Preconditions.checkState(selectedObjective == null);
+		selectedObjective = Objective.VehicleActiveTime;
+		return this;
+	}
+
+	private void configureVehicleActiveTimeObjective() {
+		bindModal(DrtInsertionObjective.class).toInstance(new VehicleActiveTimeObjective());
+	}
+
+	// Passenger delay
+
+	private double passengerDelayPickupWeight = 1.0;
+	private double passengerDelayDropoffWeight = 1.0;
+
+	public DrtInsertionModule minimizePassengerDelay() {
+		return minimizePassengerDelay(1.0, 1.0);
+	}
+
+	public DrtInsertionModule minimizePassengerDelay(double pickupWeight, double dropoffWeight) {
+		Preconditions.checkState(selectedObjective == null);
+		selectedObjective = Objective.PassengerDelay;
+		passengerDelayPickupWeight = pickupWeight;
+		passengerDelayDropoffWeight = dropoffWeight;
+		return this;
+	}
+
+	private void configurePassengerDelayObjective() {
+		bindModal(DrtInsertionObjective.class)
+				.toInstance(new PassengerDelayObjective(passengerDelayPickupWeight, passengerDelayDropoffWeight));
+	}
+
+	// Custom objective
+
+	private DrtInsertionObjective configuredObjective;
+	private Class<? extends DrtInsertionObjective> configuredObjectiveClass;
+
+	public DrtInsertionModule minimize(DrtInsertionObjective objective) {
+		Preconditions.checkState(selectedObjective == null);
+		selectedObjective = Objective.Custom;
+		configuredObjective = objective;
+		return this;
+	}
+
+	public DrtInsertionModule minimize(Class<? extends DrtInsertionObjective> objective) {
+		Preconditions.checkState(selectedObjective == null);
+		selectedObjective = Objective.Custom;
+		configuredObjectiveClass = objective;
+		return this;
+	}
+
+	private void configureCustomObjective() {
+		if (configuredObjective != null) {
+			bindModal(DrtInsertionObjective.class).toInstance(configuredObjective);
+		}
+
+		if (configuredObjectiveClass != null) {
+			bindModal(DrtInsertionObjective.class).to(modalKey(configuredObjectiveClass));
+		}
+	}
+
+	// Vehicle distance
+
+	private VehcileDistanceWeights vehicleDistanceWeights = null;
+	private double distanceObjectiveEstimationFactor = Double.NaN;
+
+	public DrtInsertionModule minimizeDistances(double occupiedWeight, double emptyWeight, double passengerWeight) {
+		Preconditions.checkState(selectedObjective == null);
+		selectedObjective = Objective.VehicleDistance;
+		vehicleDistanceWeights = new VehcileDistanceWeights(occupiedWeight, emptyWeight, passengerWeight);
+		return this;
+	}
+
+	public DrtInsertionModule minimizeVehicleDistance() {
+		Preconditions.checkState(selectedObjective == null);
+		selectedObjective = Objective.VehicleDistance;
+		vehicleDistanceWeights = new VehcileDistanceWeights(1.0, 1.0, 0.0);
+		return this;
+	}
+
+	public DrtInsertionModule minimizeEmptyDistance() {
+		Preconditions.checkState(selectedObjective == null);
+		selectedObjective = Objective.VehicleDistance;
+		vehicleDistanceWeights = new VehcileDistanceWeights(0.0, 1.0, 0.0);
+		return this;
+	}
+
+	public DrtInsertionModule withDistanceObjectiveFactor(double estimationFactor) {
+		this.distanceObjectiveEstimationFactor = estimationFactor;
+		return this;
+	}
+
+	private void configureVehicleDistanceObjective() {
+		bindModal(DrtInsertionObjective.class).toProvider(modalProvider(getter -> {
+			final DistanceCalculator distanceCalculator;
+
+			if (Double.isFinite(distanceObjectiveEstimationFactor)) {
+				distanceCalculator = new ApproximateDistanceCalculator(distanceObjectiveEstimationFactor);
+			} else {
+				distanceCalculator = getter.getModal(RoutingDistanceCalculator.class);
+			}
+
+			return new VehicleDistanceObjective(vehicleDistanceWeights, distanceCalculator);
+		}));
+	}
+
+	@Override
+	protected void configureQSim() {
+		List<Key<? extends DrtInsertionConstraint>> constraintBindings = new LinkedList<>();
+		configureExclusivityConstraint(constraintBindings);
+		configureSingleRequestConstraint(constraintBindings);
+		configureSkillsContraint(constraintBindings);
+		configureRangeContraint(constraintBindings);
+
+		if (selectedObjective == null) {
+			selectedObjective = Objective.VehicleActiveTime;
+		}
+
+		switch (selectedObjective) {
+		case Custom:
+			configureCustomObjective();
+			break;
+		case PassengerDelay:
+			configurePassengerDelayObjective();
+			break;
+		case VehicleDistance:
+			configureVehicleDistanceObjective();
+			break;
+		case VehicleActiveTime:
+		default:
+			configureVehicleActiveTimeObjective();
+			break;
+		}
+
+		bindModal(ConfigurableCostCalculatorStrategy.class).toProvider(modalProvider(getter -> {
+			List<DrtInsertionConstraint> constraints = new LinkedList<>();
+
+			for (Key<? extends DrtInsertionConstraint> constraintBinding : constraintBindings) {
+				constraints.add(getter.get(constraintBinding));
+			}
+
+			for (Class<? extends DrtInsertionConstraint> constraintClass : customConstraintClasses) {
+				constraints.add(getter.getModal(constraintClass));
+			}
+
+			for (DrtInsertionConstraint constraint : customConstraints) {
+				constraints.add(constraint);
+			}
+
+			return new ConfigurableCostCalculatorStrategy(getter.getModal(DrtInsertionObjective.class), constraints,
+					softInsertionParams);
+		})).in(Singleton.class);
+
+		bindModal(PromisedTimeWindowOfferAcceptor.class).toProvider(modalProvider(getter -> {
+			return new PromisedTimeWindowOfferAcceptor(promisedPickupTimeWindow, promisedDropoffTimeWindow);
+		})).in(Singleton.class);
+
+		bindModal(CostCalculationStrategy.class).to(modalKey(ConfigurableCostCalculatorStrategy.class));
+		bindModal(DrtOfferAcceptor.class).to(modalKey(PromisedTimeWindowOfferAcceptor.class));
+
+		bindModal(RoutingDistanceCalculator.class).toProvider(modalProvider(getter -> {
+			LeastCostPathCalculatorFactory factory = new SpeedyALTFactory();
+
+			TravelTime travelTime = getter.getModal(TravelTime.class);
+			TravelDisutility travelDisutility = new OnlyTimeDependentTravelDisutility(travelTime);
+			Network network = getter.getModal(Network.class);
+
+			printDistanceWarning();
+
+			return new RoutingDistanceCalculator(factory.createPathCalculator(network, travelDisutility, travelTime),
+					travelTime);
+		}));
+	}
+
+	private final static Logger logger = LogManager.getLogger(DrtInsertionModule.class);
+
+	private static void printDistanceWarning() {
+		logger.warn(
+				"Depending on your configruation, distance-based objectives and constraints may be very impacting on performance. The features should be considered experimental. See discussion of #2947 on Github.");
+	}
+}
---
+++ contribs/drt-extensions/src/main/java/org/matsim/contrib/drt/extension/insertion/distances/InsertionDistanceCalculator.java
@@ -0,0 +1,213 @@
+package org.matsim.contrib.drt.extension.insertion.distances;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.contrib.drt.optimizer.VehicleEntry;
+import org.matsim.contrib.drt.optimizer.Waypoint;
+import org.matsim.contrib.drt.optimizer.Waypoint.End;
+import org.matsim.contrib.drt.optimizer.insertion.InsertionDetourTimeCalculator.DetourTimeInfo;
+import org.matsim.contrib.drt.optimizer.insertion.InsertionGenerator.Insertion;
+import org.matsim.contrib.drt.passenger.AcceptedDrtRequest;
+import org.matsim.contrib.drt.schedule.DrtStopTask;
+import org.matsim.contrib.dvrp.schedule.DriveTask;
+import org.matsim.contrib.dvrp.schedule.Schedule;
+import org.matsim.contrib.dvrp.schedule.Schedule.ScheduleStatus;
+import org.matsim.contrib.dvrp.schedule.Task;
+import org.matsim.contrib.dvrp.tracker.OnlineDriveTaskTracker;
+
+public class InsertionDistanceCalculator {
+	public VehicleDistance calculateScheduledDistance(VehicleEntry vehicleEntry) {
+		Schedule schedule = vehicleEntry.vehicle.getSchedule();
+
+		double occupiedDistance = 0.0;
+		double emptyDistance = 0.0;
+		double passengerDistance = 0.0;
+
+		if (!schedule.getStatus().equals(ScheduleStatus.UNPLANNED)) {
+			int occupancy = 0;
+
+			for (Task task : schedule.getTasks()) {
+				if (task instanceof DrtStopTask) {
+					occupancy += getPassengers(((DrtStopTask) task).getPickupRequests().values());
+					occupancy -= getPassengers(((DrtStopTask) task).getDropoffRequests().values());
+				}
+
+				if (task instanceof DriveTask) {
+					double taskDistance = calculateTaskDistance((DriveTask) task);
+
+					if (occupancy == 0) {
+						emptyDistance += taskDistance;
+					} else {
+						occupiedDistance += taskDistance;
+						passengerDistance += taskDistance * occupancy;
+					}
+				}
+			}
+		}
+
+		return new VehicleDistance(occupiedDistance, emptyDistance, passengerDistance);
+	}
+
+	private int getPassengers(Collection<AcceptedDrtRequest> requests) {
+		return requests.stream().mapToInt(r -> r.getPassengerIds().size()).sum();
+	}
+
+	public VehicleDistance calculateInsertionDistance(Insertion insertion, DetourTimeInfo detourTimeInfo,
+			DistanceCalculator distanceEstimator) {
+		// pairs containing driven distance and occupany
+		List<DistanceEntry> addedDistances = new LinkedList<>();
+		List<DistanceEntry> removedDistances = new LinkedList<>();
+
+		final Link pickupFromLink = insertion.pickup.previousWaypoint.getLink();
+		final Link pickupNewLink = insertion.pickup.newWaypoint.getLink();
+		final Link pickupToLink = insertion.pickup.nextWaypoint.getLink();
+
+		int beforePickupOccupancy = insertion.pickup.previousWaypoint.getOutgoingOccupancy();
+
+		double beforePickupDistance = distanceEstimator
+				.estimateDistance(insertion.pickup.previousWaypoint.getDepartureTime(), pickupFromLink, pickupNewLink);
+
+		double afterPickupDistance = distanceEstimator.estimateDistance(detourTimeInfo.pickupDetourInfo.departureTime,
+				pickupNewLink, pickupToLink);
+
+		addedDistances.add(new DistanceEntry(beforePickupDistance, beforePickupOccupancy));
+		addedDistances.add(new DistanceEntry(afterPickupDistance, beforePickupOccupancy + 1));
+
+		if (insertion.pickup.previousWaypoint instanceof Waypoint.Start) {
+			Task currentTask = insertion.vehicleEntry.vehicle.getSchedule().getCurrentTask();
+
+			if (currentTask instanceof DriveTask) {
+				OnlineDriveTaskTracker tracker = (OnlineDriveTaskTracker) ((DriveTask) currentTask).getTaskTracker();
+
+				double removedStartDistance = 0.0;
+
+				for (int k = tracker.getCurrentLinkIdx(); k < tracker.getPath().getLinkCount(); k++) {
+					removedStartDistance += tracker.getPath().getLink(k).getLength();
+				}
+
+				int startOccupancy = insertion.pickup.previousWaypoint.getOutgoingOccupancy();
+				removedDistances.add(new DistanceEntry(removedStartDistance, startOccupancy));
+			}
+		} else {
+			int startIndex = ((Waypoint.Stop) insertion.pickup.previousWaypoint).task.getTaskIdx();
+			int occupancy = insertion.pickup.previousWaypoint.getOutgoingOccupancy();
+
+			final int endIndex;
+			if (insertion.pickup.nextWaypoint instanceof Waypoint.End) {
+				endIndex = insertion.vehicleEntry.vehicle.getSchedule().getTaskCount() - 1;
+			} else if (!(insertion.pickup.nextWaypoint instanceof Waypoint.Dropoff)) {
+				endIndex = ((Waypoint.Stop) insertion.pickup.nextWaypoint).task.getTaskIdx();
+			} else {
+				endIndex = startIndex;
+			}
+
+			for (int index = startIndex; index < endIndex; index++) {
+				Task task = insertion.vehicleEntry.vehicle.getSchedule().getTasks().get(index);
+
+				if (task instanceof DrtStopTask) {
+					occupancy += getPassengers(((DrtStopTask) task).getPickupRequests().values());
+					occupancy -= getPassengers(((DrtStopTask) task).getDropoffRequests().values());
+				}
+
+				if (task instanceof DriveTask) {
+					double taskDistance = calculateTaskDistance((DriveTask) task);
+					removedDistances.add(new DistanceEntry(taskDistance, occupancy));
+				}
+			}
+		}
+
+		final Link dropoffFromLink = insertion.dropoff.previousWaypoint.getLink();
+		final Link dropoffNewLink = insertion.dropoff.newWaypoint.getLink();
+		final Link dropoffToLink = (insertion.dropoff.nextWaypoint instanceof End)
+				? insertion.dropoff.newWaypoint.getLink()
+				: insertion.dropoff.nextWaypoint.getLink();
+
+		final int beforeDropoffOccupancy;
+		if (insertion.dropoff.index > insertion.pickup.index) {
+			beforeDropoffOccupancy = insertion.dropoff.previousWaypoint.getOutgoingOccupancy() + 1;
+			double beforeDropoffDistance = distanceEstimator.estimateDistance(
+					insertion.dropoff.previousWaypoint.getDepartureTime(), dropoffFromLink, dropoffNewLink);
+
+			addedDistances.add(new DistanceEntry(beforeDropoffDistance, beforeDropoffOccupancy));
+		} else {
+			beforeDropoffOccupancy = beforePickupOccupancy + 1;
+		}
+
+		double afterDropoffDistance = distanceEstimator.estimateDistance(detourTimeInfo.dropoffDetourInfo.arrivalTime,
+				dropoffNewLink, dropoffToLink);
+		addedDistances.add(new DistanceEntry(afterDropoffDistance, beforeDropoffOccupancy - 1));
+
+		if (insertion.dropoff.index > insertion.pickup.index) {
+			int startIndex = ((Waypoint.Stop) insertion.dropoff.previousWaypoint).task.getTaskIdx();
+			int occupancy = insertion.dropoff.previousWaypoint.getOutgoingOccupancy();
+
+			final int endIndex;
+			if (insertion.dropoff.nextWaypoint instanceof Waypoint.End) {
+				endIndex = insertion.vehicleEntry.vehicle.getSchedule().getTaskCount() - 1;
+			} else {
+				endIndex = ((Waypoint.Stop) insertion.dropoff.nextWaypoint).task.getTaskIdx();
+			}
+
+			for (int index = startIndex; index < endIndex; index++) {
+				Task task = insertion.vehicleEntry.vehicle.getSchedule().getTasks().get(index);
+
+				if (task instanceof DrtStopTask) {
+					occupancy += getPassengers(((DrtStopTask) task).getPickupRequests().values());
+					occupancy -= getPassengers(((DrtStopTask) task).getDropoffRequests().values());
+				}
+
+				if (task instanceof DriveTask) {
+					double taskDistance = calculateTaskDistance((DriveTask) task);
+					removedDistances.add(new DistanceEntry(taskDistance, occupancy));
+				}
+			}
+		}
+
+		double occupiedDriveDistance = 0.0;
+		double emptyDriveDistance = 0.0;
+		double passengerDistance = 0.0;
+
+		for (var entry : addedDistances) {
+			if (entry.occupancy == 0) {
+				emptyDriveDistance += entry.distance;
+			} else {
+				occupiedDriveDistance += entry.distance;
+				passengerDistance += entry.distance * entry.occupancy;
+			}
+		}
+
+		for (var entry : removedDistances) {
+			if (entry.occupancy == 0) {
+				emptyDriveDistance -= entry.distance;
+			} else {
+				occupiedDriveDistance -= entry.distance;
+				passengerDistance -= entry.hashCode() * entry.occupancy;
+			}
+		}
+
+		return new VehicleDistance(occupiedDriveDistance, emptyDriveDistance, passengerDistance);
+	}
+
+	private double calculateTaskDistance(DriveTask task) {
+		double distance = 0.0;
+
+		for (int k = 0; k < task.getPath().getLinkCount(); k++) {
+			distance += task.getPath().getLink(k).getLength();
+		}
+
+		return distance;
+	}
+
+	static public record VehicleDistance(double occupiedDriveDistance, double emptyDriveDistance,
+			double passengerDistance) {
+		public double totalDriveDistance() {
+			return occupiedDriveDistance + emptyDriveDistance;
+		}
+	}
+
+	private record DistanceEntry(double distance, int occupancy) {
+	}
+}
