b029e9d573f9181959b59a46f304db0df1a769ab
rakow <rakow@vsp.tu-berlin.de>
2023-12-19 15:26:38 +0100

    fix compilation error and test

--- contribs/informed-mode-choice/src/test/java/org/matsim/modechoice/pruning/CandidatePrunerTest.java
+++ contribs/informed-mode-choice/src/test/java/org/matsim/modechoice/pruning/CandidatePrunerTest.java
@@ -6,6 +6,7 @@ import org.matsim.modechoice.PlanCandidate;
 import org.matsim.modechoice.PlanModel;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -20,12 +21,12 @@ class CandidatePrunerTest {
 
 		PlanModel model = Mockito.mock(PlanModel.class);
 
-		List<PlanCandidate> candidates = List.of(
+		List<PlanCandidate> candidates = new ArrayList<>(List.of(
 			new PlanCandidate(new String[]{"car", "car", "car", "car"}, Double.NaN),
 			new PlanCandidate(new String[]{"car", "bike", "pt", "car"}, Double.NaN),
 			new PlanCandidate(new String[]{"car", "drt", "car", "car"}, Double.NaN),
 			new PlanCandidate(new String[]{"drt"}, Double.NaN)
-		);
+		));
 
 		pruner.pruneCandidates(model, candidates, new Random());
 
