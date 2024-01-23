13025784735a1cbd4d7b9e2f33ae67175f355584
Ricardo Ewert <ewert@vsp.tu-berlin.de>
2024-01-23 16:44:40 +0100

    formatting

--- contribs/application/src/main/java/org/matsim/application/prepare/freight/tripGeneration/LongDistanceFreightUtils.java
+++ contribs/application/src/main/java/org/matsim/application/prepare/freight/tripGeneration/LongDistanceFreightUtils.java
@@ -2,7 +2,32 @@ package org.matsim.application.prepare.freight.tripGeneration;
 
 import org.matsim.api.core.v01.population.Person;
 
+import java.util.List;
+import java.util.Map;
+
 public class LongDistanceFreightUtils {
+
+	public enum TransportType {
+		FTL, LTL
+	}
+
+	private final static Map <TransportType, List<Integer>> transportTypeMap = Map.of(
+			TransportType.FTL, List.of(10, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 71, 72, 32, 33, 80, 90, 100, 110, 120, 160, 170, 190, 200),
+			TransportType.LTL, List.of(40, 50, 60, 130, 140, 150, 180)
+	);
+
+	/** Returns the transport type ('FTL' = full truck load; 'LTL' = less than truck load) for a given goods type
+	 * @param goodsType
+	 * @return
+	 */
+	public static TransportType getTransportType(int goodsType) {
+		for (Map.Entry<TransportType, List<Integer>> entry : transportTypeMap.entrySet()) {
+			if (entry.getValue().contains(goodsType)) {
+				return entry.getKey();
+			}
+		}
+		throw new IllegalArgumentException("Goods type " + goodsType + " not found in transportTypeMap");
+	}
 
 	static void writeCommonAttributesV1(Person person, TripRelation tripRelation, String tripRelationId){
 		setFreightSubpopulation(person);
@@ -25,12 +50,18 @@ public class LongDistanceFreightUtils {
 		setDestinationTerminal(person, tripRelation);
 		setGoodsTypePreRun(person, tripRelation);
 		setGoodsTypePostRun(person, tripRelation);
+		setTransportType(person, tripRelation);
 		setTonsPerYearPreRun(person, tripRelation);
 		setTonsPerYearPostRun(person, tripRelation);
 		setTonKMPerYearPreRun(person, tripRelation);
 		setTonKMPerYearMainRun(person, tripRelation);
 		setTonKMPerYearPostRun(person, tripRelation);
 	}
+
+	private static void setTransportType(Person person, TripRelation tripRelation) {
+		person.getAttributes().putAttribute("transport_type_main-run", String.valueOf(getTransportType(Integer.parseInt(tripRelation.getGoodsTypeMainRun()))));
+	}
+
 	static void setFreightSubpopulation(Person person) {
 		person.getAttributes().putAttribute("subpopulation", "freight");
 	}
