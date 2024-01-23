2e03c9b3ea5a4776d6dca51309bd1194bfe97f56
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2024-01-23 15:50:49 +0100

    include planType in GenericWorstPlanForRemovalSelector. Has still one compileError.

--- matsim/src/main/java/org/matsim/core/replanning/selectors/GenericWorstPlanForRemovalSelector.java
+++ matsim/src/main/java/org/matsim/core/replanning/selectors/GenericWorstPlanForRemovalSelector.java
@@ -25,54 +25,82 @@ package org.matsim.core.replanning.selectors;
 import org.matsim.api.core.v01.population.BasicPlan;
 import org.matsim.api.core.v01.population.HasPlansAndId;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
- * <p>Selects the worst plan of a person (most likely for removal).
+ * <p>Selects the worst plan of a person (most likely for removal), but respects
+ * the set plan types in a way the no plan is selected that is the last one of
+ * its type.</p>
+ * <p>(I would say that it can select the last of its type if it finds nothing else.  However, this algo should only
+ * be used if an agent has more plans than maxPlansPerAgent, so make sure that that parameter is set large enough for
+ * your purposes.  kai, oct'09)</p>
  * <p>Plans without a score are seen as worst and selected accordingly.</p>
  *
  * @author mrieser
  */
 public class GenericWorstPlanForRemovalSelector<T extends BasicPlan, I> implements PlanSelector<T, I> {
 
-    private static final String UNDEFINED_TYPE = "undefined";
+	private static final String UNDEFINED_TYPE = "undefined";
 
-    @Override
-    public T selectPlan(HasPlansAndId<T, I> person) {
+	@Override
+	public T selectPlan(HasPlansAndId<T, I> person) {
 
-        T worst = null;
-        double worstScore = Double.POSITIVE_INFINITY;
-        for (T plan : person.getPlans()) {
+		// hashmap that returns "Integer" count for given plans type:
+		Map<String, Integer> typeCounts = new ConcurrentHashMap<String, Integer>();
 
-            // if this plan has no score yet:
-            if (plan.getScore() == null || plan.getScore().isNaN() ) {
-                // say that the plan without score now is the "worst":
-                worst = plan;
+		// count how many plans per type an agent has:
+		for (T plan : person.getPlans()) {
+			String type = plan.getType();
+			if ( type==null ) {
+				type = UNDEFINED_TYPE ;
+			}
+			typeCounts.merge( type, 1, ( a, b ) -> a + b );
+		}
 
-                // make sure that this one remains the selected plan:
-                worstScore = Double.NEGATIVE_INFINITY;
+		T worst = null;
+		double worstScore = Double.POSITIVE_INFINITY;
+		for (T plan : person.getPlans()) {
 
-                // otherwise do the usual logic to find the plan with the minimum score:
-            } else if (plan.getScore() < worstScore) {
-                worst = plan;
-                worstScore = plan.getScore();
-            }
-            // (otherwise we just keep "worst=null")
+			String type = plan.getType();
+			if ( type==null ) {
+				type = UNDEFINED_TYPE;
+			}
+			if ( typeCounts.get( type ) > 1) {
+				// (if we have more than one plan of the same type:)
 
-        }
+				// if this plan has no score yet:
+				if (plan.getScore() == null || plan.getScore().isNaN() ) {
+					// say that the plan without score now is the "worst":
+					worst = plan;
 
-        if (worst == null) {
-            // there is exactly one plan, or we have of each plan-type exactly one.
-            // select the one with worst score globally, or the first one with score=null
-            for (T plan : person.getPlans()) {
-                if (plan.getScore() == null || plan.getScore().isNaN() ) {
-                    return plan;
-                }
-                if (plan.getScore() < worstScore) {
-                    worst = plan;
-                    worstScore = plan.getScore();
-                }
-            }
-        }
-        return worst;
-    }
+					// make sure that this one remains the selected plan:
+					worstScore = Double.NEGATIVE_INFINITY;
+
+					// otherwise do the usual logic to find the plan with the minimum score:
+				} else if (plan.getScore() < worstScore) {
+					worst = plan;
+					worstScore = plan.getScore();
+				}
+			}
+			// (otherwise we just keep "worst=null")
+
+		}
+
+		if (worst == null) {
+			// there is exactly one plan, or we have of each plan-type exactly one.
+			// select the one with worst score globally, or the first one with score=null
+			for (T plan : person.getPlans()) {
+				if (plan.getScore() == null || plan.getScore().isNaN() ) {
+					return plan;
+				}
+				if (plan.getScore() < worstScore) {
+					worst = plan;
+					worstScore = plan.getScore();
+				}
+			}
+		}
+		return worst;
+	}
 
 }
