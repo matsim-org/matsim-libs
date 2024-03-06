da666636983ce03b255e768afa932d62ee6dfa74
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2024-03-06 13:26:02 +0100

    cleanup, reformat

--- contribs/emissions/src/main/java/org/matsim/contrib/emissions/analysis/EmissionsOnLinkEventHandler.java
+++ contribs/emissions/src/main/java/org/matsim/contrib/emissions/analysis/EmissionsOnLinkEventHandler.java
@@ -39,74 +39,73 @@ import java.util.Map;
  */
 public class EmissionsOnLinkEventHandler implements WarmEmissionEventHandler, ColdEmissionEventHandler {
 
-    private final TimeBinMap<Map<Id<Link>, EmissionsByPollutant>> timeBins;
-    private final Map<Id<Link>, Map<Pollutant, Double>> link2pollutants = new HashMap<>();
+	private final TimeBinMap<Map<Id<Link>, EmissionsByPollutant>> timeBins;
+	private final Map<Id<Link>, Map<Pollutant, Double>> link2pollutants = new HashMap<>();
 
 	/**
 	 * Drop events after end time.
 	 */
 	private final double endTime;
 
-    public EmissionsOnLinkEventHandler(double timeBinSizeInSeconds) {
+	public EmissionsOnLinkEventHandler(double timeBinSizeInSeconds) {
 		this(timeBinSizeInSeconds, Double.POSITIVE_INFINITY);
-    }
+	}
 
 	public EmissionsOnLinkEventHandler(double timeBinSizeInSeconds, double endTime) {
 		this.timeBins = new TimeBinMap<>(timeBinSizeInSeconds);
 		this.endTime = endTime;
 	}
 
-    /**
-     * Yields collected emissions
-     *
-     * @return Collected emissions by time bin and by link id
-     */
-    public TimeBinMap<Map<Id<Link>, EmissionsByPollutant>> getTimeBins() {
-        return timeBins;
-    }
-    /**
-     * Yields summed link emissions
-     *
-     * @return Total emissions per pollutant by link id
-     */
-    public Map<Id<Link>, Map<Pollutant, Double>> getLink2pollutants() { return link2pollutants; }
+	/**
+	 * Yields collected emissions
+	 *
+	 * @return Collected emissions by time bin and by link id
+	 */
+	public TimeBinMap<Map<Id<Link>, EmissionsByPollutant>> getTimeBins() {
+		return timeBins;
+	}
+	/**
+	 * Yields summed link emissions
+	 *
+	 * @return Total emissions per pollutant by link id
+	 */
+	public Map<Id<Link>, Map<Pollutant, Double>> getLink2pollutants() { return link2pollutants; }
 
-    @Override
-    public void reset(int iteration) {
-        timeBins.clear();
-        link2pollutants.clear();
-    }
+	@Override
+	public void reset(int iteration) {
+		timeBins.clear();
+		link2pollutants.clear();
+	}
 
-    @Override
-    public void handleEvent(WarmEmissionEvent event) {
+	@Override
+	public void handleEvent(WarmEmissionEvent event) {
 		if (event.getTime() >= endTime)
 			return;
 
-        Map<Pollutant, Double> map = new HashMap<>(event.getWarmEmissions());
-        handleEmissionEvent(event.getTime(), event.getLinkId(), map );
-    }
+		handleEmissionEvent(event.getTime(), event.getLinkId(), event.getWarmEmissions() );
+	}
 
-    @Override
-    public void handleEvent(ColdEmissionEvent event) {
+	@Override
+	public void handleEvent(ColdEmissionEvent event) {
 		if (event.getTime() >= endTime)
 			return;
 
-        handleEmissionEvent(event.getTime(), event.getLinkId(), event.getColdEmissions());
-    }
+		handleEmissionEvent(event.getTime(), event.getLinkId(), event.getColdEmissions());
+	}
 
-    private void handleEmissionEvent(double time, Id<Link> linkId, Map<Pollutant, Double> emissions) {
+	private void handleEmissionEvent(double time, Id<Link> linkId, Map<Pollutant, Double> emissions) {
 
-        TimeBinMap.TimeBin<Map<Id<Link>, EmissionsByPollutant>> currentBin = timeBins.getTimeBin(time);
-        if (!currentBin.hasValue()){ currentBin.setValue( new HashMap<>() ); }
-        if (!currentBin.getValue().containsKey(linkId)){
-            currentBin.getValue().put( linkId, new EmissionsByPollutant( new HashMap<>( emissions ) ) );
-        } else { currentBin.getValue().get( linkId ).addEmissions( emissions ); }
+		TimeBinMap.TimeBin<Map<Id<Link>, EmissionsByPollutant>> currentBin = timeBins.getTimeBin(time);
+		if (!currentBin.hasValue()){ currentBin.setValue( new HashMap<>() ); }
+		if (!currentBin.getValue().containsKey(linkId)){
+			currentBin.getValue().put( linkId, new EmissionsByPollutant( new HashMap<>( emissions ) ) );
+		} else { currentBin.getValue().get( linkId ).addEmissions( emissions ); }
 
-        if (link2pollutants.get(linkId) == null) { link2pollutants.put(linkId, emissions); }
-        else {
-            for (Pollutant key : emissions.keySet()) {
-                link2pollutants.get(linkId).merge(key, emissions.get(key), Double::sum);
-            }
-        }
-    }
+		if (link2pollutants.get(linkId) == null) { link2pollutants.put(linkId, emissions); }
+		else {
+			for (Pollutant key : emissions.keySet()) {
+				link2pollutants.get(linkId).merge(key, emissions.get(key), Double::sum);
+			}
+		}
+	}
 }
