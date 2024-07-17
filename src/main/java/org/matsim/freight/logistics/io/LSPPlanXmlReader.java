80ac88b32c1d307302014723f43b00f81b71f7c0
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2024-07-17 13:35:39 +0200

    remove one layer of "LSPs"

--- src/main/java/org/matsim/freight/logistics/io/LSPPlanXmlReader.java
+++ src/main/java/org/matsim/freight/logistics/io/LSPPlanXmlReader.java
@@ -88,7 +88,7 @@ public final class LSPPlanXmlReader implements MatsimReader {
     }
 
     public void startTag(String name, Attributes attributes, Stack<String> context) {
-      if (LSPConstants.LSPS_DEFINITIONS.equalsIgnoreCase(name)) {
+      if (LSPConstants.LSPS.equalsIgnoreCase(name)) {
         String str = attributes.getValue("xsi:schemaLocation");
         log.info("Found following schemaLocation in lsPs definition file: {}", str);
         if (str.contains("lspsDefinitions_v1.xsd")) {
