ad718989695b4c332a41d953a090a810adabd546
sime94 <meinhardt@vsp.tu-berlin.de>
2024-08-06 14:37:34 +0200

    put deutschlandtarif price linear functions into code

--- contribs/vsp/src/main/java/playground/vsp/pt/fare/FareZoneBasedPtFareHandler.java
+++ contribs/vsp/src/main/java/playground/vsp/pt/fare/FareZoneBasedPtFareHandler.java
@@ -1,6 +1,7 @@
 package playground.vsp.pt.fare;
 
 import com.google.inject.Inject;
+import org.apache.commons.math.stat.regression.SimpleRegression;
 import org.geotools.api.feature.simple.SimpleFeature;
 import org.locationtech.jts.geom.Geometry;
 import org.matsim.api.core.v01.Coord;
@@ -16,6 +17,7 @@ import org.matsim.core.utils.geometry.CoordUtils;
 import org.matsim.core.utils.geometry.geotools.MGC;
 import org.matsim.pt.PtConstants;
 
+import java.util.AbstractMap;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,26 +26,22 @@ public class FareZoneBasedPtFareHandler implements ActivityStartEventHandler {
 	@Inject
 	private EventsManager events;
 
-	private static final String FARE = "fare";
+	public static final String FARE = "fare";
 	public static final String PT_FARE_ZONE_BASED = "fare zone based pt fare";
+	public static final String PT_GERMANWIDE_FARE_BASED = "german-wide fare based pt fare";
 
 	private final ShpOptions shp;
 
 	private final Map<Id<Person>, Coord> personDepartureCoordMap = new HashMap<>();
 	private final Map<Id<Person>, Coord> personArrivalCoordMap = new HashMap<>();
 
-//	TODO: integrate this new handler to the upper level infrastructure -> some kind of switch to choose between old distance based handler and this one
-
-	public FareZoneBasedPtFareHandler(ShpOptions shp) {
-//		TODO: fill constructor. base fare and shp is needed here
-		this.shp = shp;
+	public FareZoneBasedPtFareHandler(DistanceBasedPtFareParams params) {
+		this.shp = new ShpOptions(params.getFareZoneShp(), null, null);
 	}
 
 	@Override
 	public void handleEvent(ActivityStartEvent event) {
 
-//		event sequence:
-//		act start pt interaction (at departure) -> act start pt interaction (at arrival) -> act start work / home / whatever
 		if (event.getActType().equals(PtConstants.TRANSIT_ACTIVITY_TYPE)) {
 			personDepartureCoordMap.computeIfAbsent(event.getPersonId(), c -> event.getCoord()); // The departure place is fixed to the place of first pt interaction an agent has in the whole leg
 			personArrivalCoordMap.put(event.getPersonId(), event.getCoord()); // The arrival stop will keep updating until the agent start a real activity (i.e. finish the leg)
@@ -58,11 +56,11 @@ public class FareZoneBasedPtFareHandler implements ActivityStartEventHandler {
 				SimpleFeature departureZone = determineFareZone(personDepartureCoordMap.get(personId), shp.readFeatures());
 				SimpleFeature arrivalZone = determineFareZone(personArrivalCoordMap.get(personId), shp.readFeatures());
 
-				double fare = computeFare(distance, departureZone, arrivalZone);
+				Map.Entry<String, Double> fareEntry = computeFare(distance, departureZone, arrivalZone);
 				// charge fare to the person
 				events.processEvent(
-					new PersonMoneyEvent(event.getTime(), event.getPersonId(), -fare,
-						PtFareConfigGroup.PT_FARE, PT_FARE_ZONE_BASED, event.getPersonId().toString()));
+					new PersonMoneyEvent(event.getTime(), event.getPersonId(), -fareEntry.getValue(),
+						PtFareConfigGroup.PT_FARE, fareEntry.getKey(), event.getPersonId().toString()));
 
 				personDepartureCoordMap.remove(personId);
 				personArrivalCoordMap.remove(personId);
@@ -70,23 +68,77 @@ public class FareZoneBasedPtFareHandler implements ActivityStartEventHandler {
 		}
 	}
 
-	public static double computeFare(double distance, SimpleFeature departureZone, SimpleFeature arrivalZone) {
+	public static Map.Entry<String, Double> computeFare(double distance, SimpleFeature departureZone, SimpleFeature arrivalZone) {
 
 		if (departureZone != null && arrivalZone != null) {
 //			if both zones are not null -> departure and arrival point are inside of one of the tarifzonen
-			if (departureZone == arrivalZone) {
-				return (double) departureZone.getAttribute(FARE);
+			if (departureZone.getID().equals(arrivalZone.getID())) {
+				return new AbstractMap.SimpleEntry<>(PT_FARE_ZONE_BASED ,(double) departureZone.getAttribute(FARE));
 			}
 		}
-//		in every other case return german wide fare or Deutschlandtarif
+//		in every other case return german wide fare / Deutschlandtarif
 		return getGermanWideFare(distance);
 	}
 
-	private static double getGermanWideFare(double distance) {
-//		TODO: find a way to parse deutschlandtarif prices from pdf to csv or manually implement it here as switch case
-		double fare = 0.0;
+	private static Map.Entry<String, Double> getGermanWideFare(double distance) {
 
-		return fare;
+		SimpleRegression regression = new SimpleRegression();
+
+//		in Deutschlandtarif, the linear function for the prices above 100km seem to have a different steepness
+//		hence the following difference in data points
+//		prices taken from https://deutschlandtarifverbund.de/wp-content/uploads/2024/07/20231201_TBDT_J_10_Preisliste_V07.pdf
+		if (distance / 1000 <= 100.) {
+			regression.addData(1, 1.70);
+			regression.addData(2,1.90);
+			regression.addData(3,2.00);
+			regression.addData(4,2.10);
+			regression.addData(5,2.20);
+			regression.addData(6,3.20);
+			regression.addData(7,3.70);
+			regression.addData(8,3.80);
+			regression.addData(9,3.90);
+			regression.addData(10,4.10);
+			regression.addData(11,5.00);
+			regression.addData(12,5.40);
+			regression.addData(13,5.60);
+			regression.addData(14,5.80);
+			regression.addData(15,5.90);
+			regression.addData(16,6.40);
+			regression.addData(17,6.50);
+			regression.addData(18,6.60);
+			regression.addData(19,6.70);
+			regression.addData(20,6.90);
+			regression.addData(30,9.90);
+			regression.addData(40,13.70);
+			regression.addData(50,16.30);
+			regression.addData(60,18.10);
+			regression.addData(70,20.10);
+			regression.addData(80,23.20);
+			regression.addData(90,26.20);
+			regression.addData(100,28.10);
+		} else {
+			regression.addData(100,28.10);
+			regression.addData(200,47.20);
+			regression.addData(300,59.70);
+			regression.addData(400,71.70);
+			regression.addData(500,83.00);
+			regression.addData(600,94.60);
+			regression.addData(700,106.30);
+			regression.addData(800,118.20);
+			regression.addData(900,130.10);
+			regression.addData(1000,141.00);
+			regression.addData(1100,148.60);
+			regression.addData(1200,158.10);
+			regression.addData(1300,169.20);
+			regression.addData(1400,179.80);
+			regression.addData(1500,190.10);
+			regression.addData(1600,201.50);
+			regression.addData(1700,212.80);
+			regression.addData(1800,223.30);
+			regression.addData(1900,233.90);
+			regression.addData(2000,244.00);
+		}
+		return new AbstractMap.SimpleEntry<>(PT_GERMANWIDE_FARE_BASED, regression.getSlope() * distance / 1000 + regression.getIntercept());
 	}
 
 	static SimpleFeature determineFareZone(Coord coord, List<SimpleFeature> features) {
--- contribs/vsp/src/main/java/playground/vsp/pt/fare/PtFareModule.java
+++ contribs/vsp/src/main/java/playground/vsp/pt/fare/PtFareModule.java
@@ -1,31 +1,22 @@
 package playground.vsp.pt.fare;
 
 import org.matsim.api.core.v01.TransportMode;
-import org.matsim.application.options.ShpOptions;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.AbstractModule;
 
 public class PtFareModule extends AbstractModule {
-	final ShpOptions shp;
-
-	public PtFareModule() {
-		this.shp = null;
-	}
-
-	public PtFareModule(final ShpOptions shp) {
-		this.shp = shp;
-	}
 
     @Override
     public void install() {
         getConfig().scoring().getModes().get(TransportMode.pt).setDailyMonetaryConstant(0);
         getConfig().scoring().getModes().get(TransportMode.pt).setMarginalUtilityOfDistance(0);
         PtFareConfigGroup ptFareConfigGroup = ConfigUtils.addOrGetModule(this.getConfig(), PtFareConfigGroup.class);
+		DistanceBasedPtFareParams distanceBasedPtFareParams = ConfigUtils.addOrGetModule(this.getConfig(), DistanceBasedPtFareParams.class);
+
         if (ptFareConfigGroup.getPtFareCalculation() == PtFareConfigGroup.PtFareCalculationModels.distanceBased) {
-			DistanceBasedPtFareParams distanceBasedPtFareParams = ConfigUtils.addOrGetModule(this.getConfig(), DistanceBasedPtFareParams.class);
 			addEventHandlerBinding().toInstance(new DistanceBasedPtFareHandler(distanceBasedPtFareParams));
 		} else if (ptFareConfigGroup.getPtFareCalculation() == PtFareConfigGroup.PtFareCalculationModels.fareZoneBased) {
-			addEventHandlerBinding().toInstance(new FareZoneBasedPtFareHandler(shp));
+			addEventHandlerBinding().toInstance(new FareZoneBasedPtFareHandler(distanceBasedPtFareParams));
 		} else {
             throw new RuntimeException("Please choose from the following fare Calculation method: [" +
                     PtFareConfigGroup.PtFareCalculationModels.distanceBased + ", " + PtFareConfigGroup.PtFareCalculationModels.fareZoneBased + "]");
