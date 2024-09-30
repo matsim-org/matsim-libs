7794d00bb7990fcca27a9cbc8e8f7ca4db909983
frievoe97 <friedrich.voelkers@me.com>
2024-09-30 13:28:47 +0200

    update yaml structure for transit viewer

--- contribs/simwrapper/src/main/java/org/matsim/simwrapper/viz/TransitViewer.java
+++ contribs/simwrapper/src/main/java/org/matsim/simwrapper/viz/TransitViewer.java
@@ -2,6 +2,8 @@ package org.matsim.simwrapper.viz;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 /**
  * Transit viewer for pt schedules.
  */
@@ -13,7 +15,35 @@ public class TransitViewer extends Viz {
 	@JsonProperty(required = true)
 	public String transitSchedule;
 
+	public List<CustomRouteType> customRouteTypes;
+
 	public TransitViewer() {
 		super("transit");
+	}
+
+	public void addCustomRouteType(String name, String color, List<Integer> routeTypes) {
+		CustomRouteType crt = new CustomRouteType();
+		crt.label = name;
+		crt.color = color;
+		customRouteTypes.add(crt);
+	}
+
+	private class CustomRouteType {
+		String label;
+		String color;
+		Match match;
+	}
+
+	public void addMatch(String transportMode, String id, List<Integer> gtfsRouteType) {
+		Match m = new Match();
+		m.transportMode = transportMode;
+		m.id = id;
+		m.gtfsRouteType = gtfsRouteType;
+	}
+
+	private class Match {
+		String transportMode;
+		String id;
+		List<Integer> gtfsRouteType;
 	}
 }
