f794d0ea5f5331c25afce7098980b6c4f3421c2c
anaue <ana.ueberhorst@campus.tu-berlin.de>
2024-10-02 11:08:42 +0200

    PLZ-runs

--- contribs/application/src/test/java/org/matsim/freightDemandGeneration/FreightDemandGenerationTest.java
+++ contribs/application/src/test/java/org/matsim/freightDemandGeneration/FreightDemandGenerationTest.java
@@ -26,16 +26,16 @@ public class FreightDemandGenerationTest {
 			Path carrierCSVLocation = Path.of(utils.getPackageInputDirectory() + "DHL_CarrierCSV_small.csv");
 			Path demandCSVLocation = Path.of(utils.getPackageInputDirectory() + "DHL_DemandCSV_small.csv");
 			//Path shapeFilePath = Path.of(utils.getPackageInputDirectory() + "testShape/testShape.shp");
-			Path shapeFilePath = Path.of(utils.getPackageInputDirectory() + "LOR_Liefergebiete/LOR_Liefergebiete.shp");
+			Path shapeFilePath = Path.of(utils.getPackageInputDirectory() + "PLZ/PLZ.shp");
 			//Path shapeFilePath = Path.of(utils.getPackageInputDirectory() + "Liefergebiete_16_manuell/Liefergebiete_16_manuell.shp");
 			//String populationLocation = utils.getPackageInputDirectory() + "testPopulation.xml";
-			//String populationLocation = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v6.3/input/berlin-only-v6.3-100pct.plans_NOT-fully-calibrated.xml.gz";
-			String populationLocation = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v6.3/input/berlin-v6.3-1pct.plans.xml.gz";
+			String populationLocation = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v6.3/input/berlin-only-v6.3-100pct.plans_NOT-fully-calibrated.xml.gz";
+			//String populationLocation = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v6.3/input/berlin-v6.3-1pct.plans.xml.gz";
 			//String network = "https://raw.githubusercontent.com/matsim-org/matsim-libs/master/examples/scenarios/freight-chessboard-9x9/grid9x9.xml";
 			String network = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v6.3/input/berlin-v6.3-network.xml.gz";
 			//String shapeCategory = "Ortsteil";
 			//String shapeCategory = "BEZIRK";
-			String shapeCategory = "PLR_NAM";
+			String shapeCategory = "plz";
 			//String shapeCategory = "Liefergebi";
 			new FreightDemandGeneration().execute(
 					"--output", output.toString(),
@@ -63,8 +63,8 @@ public class FreightDemandGenerationTest {
 					"--carrierVehicleFileLocation", vehicleFilePath.toString(),
 
 					"--shapeFileLocation", shapeFilePath.toString(),
-					//"--shapeCRS", "WGS84",
-					"--shapeCRS", "EPSG:25832",
+					"--shapeCRS", "WGS84",
+					//"--shapeCRS", "EPSG:25832",
 
 					"--populationFileLocation", populationLocation,
 					"--populationCRS", "EPSG:25832",
