564140fe9b9f59e7d0a684050507d26aafb4c143
rakow <rakow@vsp.tu-berlin.de>
2024-11-05 11:15:57 +0100

    inline writing of xml for most common attributes

--- matsim/src/main/java/org/matsim/core/utils/io/XmlUtils.java
+++ matsim/src/main/java/org/matsim/core/utils/io/XmlUtils.java
@@ -82,6 +82,45 @@ public final class XmlUtils {
 		return attributeValue;
 	}
 
+	/**
+	 * Write encoded attribute value to the given StringBuilder.
+	 * This is an optimized version of {@link #encodeAttributeValue(String)}, which does not create any intermediate objects.
+	 */
+	public static StringBuilder writeEncodedAttributeValue(StringBuilder out, String attributeValue) {
+
+		if (attributeValue == null) {
+			// By convention, null values are written as "null" in the xml output.
+			out.append("null");
+			return out;
+		}
+
+		int len = attributeValue.length();
+
+		for (int pos = 0; pos < len; pos++) {
+			char ch = attributeValue.charAt(pos);
+			switch (ch) {
+				case '<' -> out.append("&lt;");
+				case '>' -> out.append("&gt;");
+				case '\"' -> out.append("&quot;");
+				case '&' -> out.append("&amp;");
+				default -> out.append(ch);
+			};
+		}
+
+		return out;
+	}
+
+	/**
+	 * Helper function to write an attribute key-value pair to the given StringBuilder.
+	 * Note, do not use this for primitive types as these don't need to be encoded and the {@link StringBuilder} has specialized methods fot these.
+	 */
+	public static StringBuilder writeEncodedAttributeKeyValue(StringBuilder out, String key, String value) {
+		out.append(key).append("=\"");
+		writeEncodedAttributeValue(out, value);
+		out.append("\" ");
+		return out;
+	}
+
 	public static String encodeContent(final String content) {
 		if (content.contains("&") || content.contains("<") || content.contains(">")) {
 			return content.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;");
