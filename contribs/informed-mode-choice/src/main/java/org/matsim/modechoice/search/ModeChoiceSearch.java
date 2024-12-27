f621f265f589c81496104d473c4fa24741dcd37f
rakow <rakow@vsp.tu-berlin.de>
2024-12-27 15:22:03 +0100

    support plans with huge number of combinations

--- contribs/informed-mode-choice/src/main/java/org/matsim/modechoice/search/ModeChoiceSearch.java
+++ contribs/informed-mode-choice/src/main/java/org/matsim/modechoice/search/ModeChoiceSearch.java
@@ -2,15 +2,10 @@ package org.matsim.modechoice.search;
 
 import it.unimi.dsi.fastutil.bytes.Byte2ObjectMap;
 import it.unimi.dsi.fastutil.bytes.Byte2ObjectOpenHashMap;
-import it.unimi.dsi.fastutil.doubles.DoubleIterator;
-import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
-import it.unimi.dsi.fastutil.longs.LongSet;
 import it.unimi.dsi.fastutil.objects.Object2ByteMap;
 import it.unimi.dsi.fastutil.objects.Object2ByteOpenHashMap;
-import it.unimi.dsi.fastutil.objects.ObjectHeapPriorityQueue;
 
 import java.util.Arrays;
-import java.util.Objects;
 
 /**
  * This class finds the best solutions by doing an exhaustive search over the best possible combinations of modes.
@@ -20,15 +15,16 @@ final class ModeChoiceSearch {
 	/**
 	 * Stores the estimates for all modes by trip x mode
 	 */
-	private final double[][] estimates;
+	final double[][] estimates;
 
 	/**
 	 * Mode to index mapping.
 	 */
+	final Byte2ObjectMap<String> inv;
+	final int depth;
+
 	private final Object2ByteMap<String> mapping;
-	private final Byte2ObjectMap<String> inv;
-	private final int depth;
-	private final long base;
+	private final double maxSize;
 
 	/**
 	 * Constructor
@@ -44,14 +40,8 @@ final class ModeChoiceSearch {
 		// Number of options per trip, +1 to encode null value
 		this.depth = modes + 1;
 
-		long b = 1;
-		for (int i = 0; i < trips - 1; i++) {
-			b *= depth;
-		}
-		// Base for the index calculation
-		this.base = b;
-
-		if (Math.pow(depth, trips) > Long.MAX_VALUE)
+		maxSize = Math.pow(depth, trips);
+		if (!Double.isFinite(maxSize))
 			throw new IllegalArgumentException("Too many mode combinations: %s ^ %s".formatted(modes, trips));
 
 		clear();
@@ -64,7 +54,7 @@ final class ModeChoiceSearch {
 	 * @param result mode assignment will be written into this array, no memory is allocated during search.
 	 * @return iterator returning the summed utility.
 	 */
-	public DoubleIterator iter(String[] result) {
+	public ModeIterator iter(String[] result) {
 
 		assert result.length == estimates.length;
 
@@ -94,8 +84,20 @@ final class ModeChoiceSearch {
 				total += max;
 		}
 
+		long b = 1;
+		for (int i = 0; i < this.estimates.length - 1; i++) {
+			b *= depth;
+		}
 
-		return new Iterator(result, maxs, new Entry(path, depth, 0), total);
+		if (maxSize < Integer.MAX_VALUE) {
+			return new ModeIntIterator(result, maxs, new ModeIntIterator.Entry(path, depth, 0), total, (int) b, this);
+		}
+
+		if (maxSize < Long.MAX_VALUE) {
+			return new ModeLongIterator(result, maxs, new ModeLongIterator.Entry(path, depth, 0), total, b, this);
+		}
+
+		return new ModeArrayIterator(result, maxs, new ModeArrayIterator.Entry(path, 0), total, this);
 	}
 
 	/**
@@ -162,190 +164,12 @@ final class ModeChoiceSearch {
 	/**
 	 * Byte representation as string array.
 	 */
-	private void convert(byte[] path, String[] modes) {
+	void convert(byte[] path, String[] modes) {
 		for (int i = 0; i < path.length; i++) {
 			String m = inv.get(path[i]);
 			// Pre-defined entries are not touched
 			if (m != null)
 				modes[i] = m;
-		}
-	}
-
-	static final class Entry implements Comparable<Entry> {
-		private final long index;
-		private final double deviation;
-
-		Entry(byte[] modes, int depth, double deviation) {
-			this.index = toIndex(modes, depth);
-			this.deviation = deviation;
-		}
-
-		Entry(long index, double deviation) {
-			this.index = index;
-			this.deviation = deviation;
-		}
-
-		static long toIndex(byte[] modes, int depth) {
-			long result = modes[0] + 1;
-			long base = depth;
-			for (int i = 1; i < modes.length; i++) {
-				result += (modes[i] + 1) * base;
-				base *= depth;
-			}
-
-			return result;
-		}
-
-		/**
-		 * Convert the mode array to an index, where one mode at index {@code idx} is replaced.
-		 */
-		static long toIndex(byte[] modes, int depth, int idx, byte mode) {
-			long result = (idx == 0 ? mode : modes[0]) + 1;
-			long base = depth;
-			for (int i = 1; i < modes.length; i++) {
-				result += ((idx == i ? mode : modes[i]) + 1) * base;
-				base *= depth;
-			}
-
-			return result;
-		}
-
-		@Override
-		public int compareTo(Entry o) {
-			return Double.compare(deviation, o.deviation);
-		}
-
-		@Override
-		public boolean equals(Object o) {
-			if (this == o) return true;
-			if (o == null || getClass() != o.getClass()) return false;
-			Entry entry = (Entry) o;
-			return Double.compare(entry.deviation, deviation) == 0 && index == entry.index;
-		}
-
-		@Override
-		public int hashCode() {
-			int result = Objects.hash(deviation);
-			result = 31 * result + Long.hashCode(index);
-			return result;
-		}
-
-		@Override
-		public String toString() {
-			return "idx: " + index + " = " + deviation;
-		}
-
-		public long getIndex() {
-			return index;
-		}
-
-		void toArray(byte[] array, long base, int depth) {
-			long idx = index;
-			for (int i = array.length - 1; i > 0; i--) {
-
-				long v = idx / base;
-				array[i] = (byte) (v - 1);
-
-				idx -= v * base;
-				base /= depth;
-			}
-
-			array[0] = (byte) (idx - 1);
-		}
-	}
-
-	/**
-	 * Searches all possible combination by using a heap.
-	 */
-	final class Iterator implements DoubleIterator {
-
-		// This implementation uses a heap and stores all seen combinations
-		// Because the graph structure is fixed, it does not need to be as complicated as other k shortest path algorithms
-		// Two major improvements may be investigated:
-		// Could this be implemented with pre allocated memory and without objects ?
-		// Do all seen combinations need to be stored ?
-		// This is the case in Yens algorithms and other and will consume a lot of memory, when many top k paths are generated and a lot are thrown away.
-
-		private final String[] result;
-		private final byte[] modes;
-		private final double[] max;
-		private final double best;
-		private final ObjectHeapPriorityQueue<Entry> heap;
-		private final LongSet seen;
-
-
-		Iterator(String[] result, double[] max, Entry shortest, double best) {
-			this.result = result;
-			this.modes = new byte[result.length];
-			this.max = max;
-			this.best = best;
-			this.heap = new ObjectHeapPriorityQueue<>();
-			this.seen = new LongOpenHashSet();
-
-			heap.enqueue(shortest);
-			seen.add(shortest.index);
-		}
-
-		@Override
-		public double nextDouble() {
-
-			Entry entry = heap.dequeue();
-			// Create the array of indices from the entry
-			entry.toArray(modes, base, depth);
-
-			for (int i = 0; i < result.length; i++) {
-
-				byte mode = -1;
-				byte originalMode = modes[i];
-
-				// This mode had no options
-				if (originalMode == -1)
-					continue;
-
-				double min = estimates[i][originalMode];
-				double max = Double.NEGATIVE_INFINITY;
-
-				// search for a deviation that is worse than the current mode
-
-				for (byte j = 0; j < estimates[i].length; j++) {
-					if (estimates[i][j] <= min && j != originalMode && estimates[i][j] > max) {
-						max = estimates[i][j];
-						mode = j;
-					}
-				}
-
-				if (mode != -1) {
-					long newIdx = Entry.toIndex(modes, depth, i, mode);
-
-					// recompute the deviation from the maximum
-					// there might be a way to store and update this, without recomputing
-					double dev = 0;
-					for (int j = 0; j < result.length; j++) {
-						// Use either the replaced mode or the original mode
-						byte legMode = j == i ? mode : modes[j];
-
-						if (legMode == -1)
-							continue;
-
-						dev += this.max[j] - estimates[j][legMode];
-					}
-
-					Entry e = new Entry(newIdx, dev);
-
-					if (!seen.contains(e.index)) {
-						heap.enqueue(e);
-						seen.add(e.index);
-					}
-				}
-			}
-
-			convert(modes, result);
-			return best - entry.deviation;
-		}
-
-		@Override
-		public boolean hasNext() {
-			return !heap.isEmpty();
 		}
 	}
 
--- contribs/informed-mode-choice/src/test/java/org/matsim/modechoice/search/ModeChoiceSearchTest.java
+++ contribs/informed-mode-choice/src/test/java/org/matsim/modechoice/search/ModeChoiceSearchTest.java
@@ -100,7 +100,41 @@ public class ModeChoiceSearchTest {
 	}
 
 	@Test
-	void entry() {
+	void huge() {
+
+		ModeChoiceSearch search = new ModeChoiceSearch(25, 5);
+
+		for (int i = 0; i < 5; i++) {
+			double[] values = new double[25];
+
+			Arrays.fill(values, i);
+
+			search.addEstimates(String.valueOf(i), values);
+		}
+
+		String[] result = new String[25];
+
+		ModeIterator it = search.iter(result);
+
+		double v = it.nextDouble();
+
+		assertThat(v)
+			.isEqualTo(100);
+
+		assertThat(result)
+			.containsOnly("4");
+
+		// All combinations with 99 util
+		for (int i = 0; i < 25; i++) {
+			assertThat(it.nextDouble()).isEqualTo(99);
+		}
+
+		assertThat(it.nextDouble()).isEqualTo(98);
+
+	}
+
+	@Test
+	void longEntry() {
 
 		int depth = 6;
 		long base = (long) Math.pow(depth, 9);
@@ -116,12 +150,12 @@ public class ModeChoiceSearchTest {
 		byte[] modes = new byte[10];
 		Arrays.fill(modes, (byte) -1);
 
-		assertThat(ModeChoiceSearch.Entry.toIndex(modes, depth))
+		assertThat(ModeLongIterator.Entry.toIndex(modes, depth))
 			.isEqualTo(0);
 
 		modes[0] = 0;
 
-		assertThat(ModeChoiceSearch.Entry.toIndex(modes, depth))
+		assertThat(ModeLongIterator.Entry.toIndex(modes, depth))
 			.isEqualTo(1);
 
 		modes[1] = 1;
@@ -129,7 +163,7 @@ public class ModeChoiceSearchTest {
 		modes[3] = 3;
 		modes[4] = 4;
 
-		ModeChoiceSearch.Entry e = new ModeChoiceSearch.Entry(modes, depth, 0);
+		ModeLongIterator.Entry e = new ModeLongIterator.Entry(modes, depth, 0);
 
 		assertThat(e.getIndex())
 			.isEqualTo(7465);
@@ -144,7 +178,64 @@ public class ModeChoiceSearchTest {
 
 		Arrays.fill(modes, (byte) 4);
 
-		e = new ModeChoiceSearch.Entry(modes, depth, 0);
+		e = new ModeLongIterator.Entry(modes, depth, 0);
+
+		e.toArray(result, base, depth);
+
+		assertThat(result)
+			.isEqualTo(modes);
+
+		assertThat(e.getIndex())
+			.isEqualTo(base * depth - 1);
+
+	}
+
+	@Test
+	void intEntry() {
+
+		int depth = 6;
+		int base = (int) Math.pow(depth, 5);
+
+		int b = 1;
+		for (int i = 0; i < 5; i++) {
+			b *= depth;
+		}
+
+		assertThat(b)
+			.isEqualTo(base);
+
+		byte[] modes = new byte[6];
+		Arrays.fill(modes, (byte) -1);
+
+		assertThat(ModeIntIterator.Entry.toIndex(modes, depth))
+			.isEqualTo(0);
+
+		modes[0] = 0;
+
+		assertThat(ModeIntIterator.Entry.toIndex(modes, depth))
+			.isEqualTo(1);
+
+		modes[1] = 1;
+		modes[2] = 2;
+		modes[3] = 3;
+		modes[4] = 4;
+
+		ModeIntIterator.Entry e = new ModeIntIterator.Entry(modes, depth, 0);
+
+		assertThat(e.getIndex())
+			.isEqualTo(7465);
+
+		byte[] result = new byte[6];
+		Arrays.fill(result, (byte) -1);
+
+		e.toArray(result, base, depth);
+
+		assertThat(result)
+			.isEqualTo(modes);
+
+		Arrays.fill(modes, (byte) 4);
+
+		e = new ModeIntIterator.Entry(modes, depth, 0);
 
 		e.toArray(result, base, depth);
 
