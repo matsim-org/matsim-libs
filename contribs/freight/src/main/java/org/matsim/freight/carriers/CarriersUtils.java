c8f9bc09310066737dea0e699217bab8c86294fd
Ricardo Ewert <ewert@vsp.tu-berlin.de>
2025-01-10 13:17:29 +0100

    add more options for writer

--- contribs/freight/src/main/java/org/matsim/freight/carriers/CarriersUtils.java
+++ contribs/freight/src/main/java/org/matsim/freight/carriers/CarriersUtils.java
@@ -40,6 +40,7 @@ import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.population.Plan;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controller;
 import org.matsim.core.utils.io.IOUtils;
 import org.matsim.freight.carriers.jsprit.MatsimJspritFactory;
 import org.matsim.freight.carriers.jsprit.NetworkBasedTransportCosts;
