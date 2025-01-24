46ba58ded3783e746026e65111b9cb093a8fc11c
Kai Nagel <nagel@vsp.tu-berlin.de>
2025-01-24 22:11:12 +0100

    FISS uses two departure handlers by design.  dead end?

--- contribs/drt-extensions/src/main/java/org/matsim/contrib/drt/extension/fiss/FISSConfigGroup.java
+++ contribs/drt-extensions/src/main/java/org/matsim/contrib/drt/extension/fiss/FISSConfigGroup.java
@@ -21,25 +21,25 @@ import java.util.Set;
  */
 public class FISSConfigGroup extends ReflectiveConfigGroup {
 
-    public static final String GROUP_NAME = "fiss";
+	public static final String GROUP_NAME = "fiss";
 
-    @Parameter
-    @Comment("Defines the share of agents that should be explicitly assigned in the QSim. " +
-            "Values between (0,1]")
-    @Positive
-    public double sampleFactor = 1.; // TODO: sample factors by mode?
+	@Parameter
+	@Comment("Defines the share of agents that should be explicitly assigned in the QSim. " +
+				 "Values between (0,1]")
+	@Positive
+	public double sampleFactor = 1.; // TODO: sample factors by mode?
 
-    @Parameter
-    @Comment("Defines the mods that will be considered for the FISS. Defaults to {car}")
-    @NotNull
-    public Set<String> sampledModes = Collections.singleton(TransportMode.car);
+	@Parameter
+	@Comment("Defines the mods that will be considered for the FISS. Defaults to {car}")
+	@NotNull
+	public Set<String> sampledModes = Collections.singleton(TransportMode.car);
 
 	@Parameter
 	@Comment("Disable FISS in the last iteration to get events of all agents. May be required for post-processing")
 	public boolean switchOffFISSLastIteration = true;
 
-    public FISSConfigGroup() {
-        super(GROUP_NAME);
-    }
+	public FISSConfigGroup() {
+		super(GROUP_NAME);
+	}
 
 }
