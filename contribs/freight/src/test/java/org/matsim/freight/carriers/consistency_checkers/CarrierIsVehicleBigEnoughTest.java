298e85214f67951c245647ba19c548ae795af153
ASt <mail@antonstock.de>
2025-02-01 21:16:07 +0100

    Code lädt Fahrzeuge und Sendungen, prüft ob Fahrzeuge ausreichend groß sind und gibt zu große Sendungen aus => RUNNING

--- contribs/freight/src/test/java/org/matsim/freight/carriers/consistency_checkers/CarrierIsVehicleBigEnoughTest.java
+++ contribs/freight/src/test/java/org/matsim/freight/carriers/consistency_checkers/CarrierIsVehicleBigEnoughTest.java
@@ -18,16 +18,7 @@ import static org.matsim.core.config.ConfigUtils.addOrGetModule;
  */
 public class CarrierIsVehicleBigEnoughTest {
 
-		static class Pair<T, U> {
-			T first;
-			U second;
-
-			public Pair(T first, U second) {
-				this.first = first;
-				this.second = second;
-			}}
-
-		public static void main(String[] args){
+	public static void main(String[] args){
 
 		// Relativer Pfad zu Freight/Scenarios/CCTestInput/
 		String pathToInput = "contribs/freight/scenarios/CCTestInput/"; //Ich zeige dir später, wie wir das in als Input für die Tests ablegen und dann entsprechend nutzen können.
@@ -56,37 +47,48 @@ public class CarrierIsVehicleBigEnoughTest {
 		CarriersUtils.loadCarriersAccordingToFreightConfig( scenario );
 
 		Carriers carriers = CarriersUtils.getCarriers(scenario);
-
-		System.out.println("Starting 'IsVehicleBigEnoughTest'...");
+		/**
+		* System.out.println("Starting 'IsVehicleBigEnoughTest'...");
+		*/
 
 		List<Double> vehicleCapacityList = new ArrayList<>();
+		Map<String, Double> shipmentSizes = new HashMap<>();
 
-		//Ermitteln der 'other' capacity aller vehicle types
+		//determine the capacity of all available vehicles
 			for (Carrier carrier : carriers.getCarriers().values()) {
 			for (CarrierVehicle carrierVehicle : carrier.getCarrierCapabilities().getCarrierVehicles().values()) {
 				//In CCTestVeh XML: 'vehicle id'
 				var vehicleType = carrierVehicle.getType().getId();
 				//In CCTestVeh XML: 'capacity other'
 				var capacity = carrierVehicle.getType().getCapacity().getOther();
-				//#### Test Output:
-				System.out.println("Carrier ID: '" + vehicleType + "'. Vehicle capacity: " + capacity);
-				vehicleCapacityList.add(capacity);
+				 vehicleCapacityList.add(capacity);
 			}
 		}
-		//Fahrzeugkapazität wird aufsteigend sortiert (Kleinstes -> Größtes)
-		Collections.sort(vehicleCapacityList);
-		//#### Test Output:
-		System.out.println("Kapa Liste: "+vehicleCapacityList);
 
-		//Ermitteln der größten Sendung aus CCTestCarriers
-
+		//determine capacity demand of all shipments
+		//Shipment ID (key as string) and capacity demand (value as double) are beeing stored in HashMap 'shipmentSizes'
 		for (Carrier carrier : carriers.getCarriers().values()) {
 			for (CarrierShipment shipment : carrier.getShipments().values()) {
-				double shipmentSize = shipment.getSize();
+				double shipmentSize = shipment.getCapacityDemand();
 				String shipmentID = shipment.getId().toString();
-				System.out.println("Shipment ID: '" + shipmentID + "'. Shipment size: " + shipmentSize);
+				shipmentSizes.put(shipmentID, shipmentSize);
 			}
 		}
+		//is there a sufficient vehicle for every shipment?
+		double maxCapacity = Collections.max(vehicleCapacityList);
+		shipmentSizes.entrySet().removeIf(entry -> entry.getValue() <= maxCapacity);
+
+
+		//if map is empty, there is a sufficient vehicle for every shipment
+		if(shipmentSizes.isEmpty()){
+			System.out.println("At least one available vehicle has sufficient capacity.");
+		} else {
+			//if map is not empty, these shipments are too large for the existing fleet.
+			System.out.println("WARNING: At least one shipment is too large for the available vehicles.");
+			for (Map.Entry<String, Double> entry : shipmentSizes.entrySet()) {
+				System.out.println("Shipment '" + entry.getKey() + "' sized '" + entry.getValue()+"' is too big for available vehicles.");
+			}
 		}
+	}
 }
 
