c406e7333a9c38419c0d18891110593ed564d864
paulh <heinrich@vsp.tu-berlin.de>
2025-02-14 14:30:35 +0100

    refactor such that kernel distance is settable

--- matsim/src/main/java/org/matsim/core/network/kernel/NetworkKernelFunction.java
+++ matsim/src/main/java/org/matsim/core/network/kernel/NetworkKernelFunction.java
@@ -2,9 +2,10 @@ package org.matsim.core.network.kernel;
 
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Link;
+import org.matsim.core.mobsim.qsim.qnetsimengine.QVehicle;
 
 import java.util.Map;
 
 public interface NetworkKernelFunction {
-	Map<Id<Link>, Double> calculateWeightedKernel(Link link, double distance);
+	Map<Id<Link>, Double> calculateWeightedKernel(QVehicle vehicle, Link link);
 }
