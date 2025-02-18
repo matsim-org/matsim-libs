bbef17f126e93ab59c1b349d739cae66082dbec6
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2025-02-18 11:25:14 +0100

    ganz viel Infrastruktur

---
+++ contribs/freight/src/main/java/org/matsim/freight/logistics/consistency_checker/LogisticsConsitencyChecker.java
@@ -0,0 +1,67 @@
+package org.matsim.freight.logistics.consistency_checker;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.matsim.freight.carriers.Carriers;
+import org.matsim.freight.carriers.consistency_checkers.CarrierConsistencyCheckers;
+import org.matsim.freight.logistics.LSP;
+import org.matsim.freight.logistics.LSPResource;
+import org.matsim.freight.logistics.LSPs;
+
+public class LogisticsConsitencyChecker {
+
+	public enum CheckResult {
+		CHECK_SUCCESSFUL, CHECK_FAILED, ERROR
+	}
+
+	//needed for log messages, log level etc.
+	private static final Logger log = LogManager.getLogger(LogisticsConsitencyChecker.class);
+	private static Level currentLevel;
+	public static void setLogLevel(Level level) {
+		currentLevel = level;
+		Configurator.setLevel(log.getName(), level);
+	}
+
+	private static void logMessage(String msg, Object... params) {
+		if (currentLevel==Level.WARN){
+			log.warn(msg, params);
+		} else if (currentLevel==Level.ERROR) {
+			log.error(msg, params);
+		} else {
+			log.info(msg, params);
+		}
+	}
+
+	/**
+	 * this method will run through allJobsInToursCheck to check, if every job is part of only one tour
+	 * allJobsInToursCheck returns enum CheckResult:
+	 * CHECK_SUCCESSFUL if allJobsInToursCheck returns CHECK_SUCCESSFUL
+	 * CHECK_FAILED if allJobsInToursCheck returns CHECK_FAIL
+	 * @param lsps the LSPs to check
+	 * @param lvl level of log messages / errors
+	 * @return CheckResult
+	 */
+	public static CheckResult uniqueResourcesCheck(LSPs lsps, Level lvl) {
+		setLogLevel(lvl);
+		int checkFailed = 0;
+		if (resourcesAreUnique(lsps, lvl) == CheckResult.CHECK_FAILED) {
+			checkFailed++;
+		}
+		return CheckResult.CHECK_FAILED;
+	}
+
+	private static LogisticsConsitencyChecker.CheckResult resourcesAreUnique(LSPs lsps, Level lvl) {
+		var result =  LogisticsConsitencyChecker.CheckResult.CHECK_SUCCESSFUL; //TODO...
+
+		for (LSP lsp : lsps.getLSPs().values()) {
+			for (LSPResource resource : lsp.getResources()) {
+				resource.getId(); //TODO...
+			}
+		}
+
+		return result;
+	}
+
+}
--- contribs/freight/src/test/java/org/matsim/freight/logistics/consistency_checkers/UniqueIDs.java
+++ contribs/freight/src/test/java/org/matsim/freight/logistics/consistency_checkers/UniqueIDs.java
@@ -1,14 +1,27 @@
 package org.matsim.freight.logistics.consistency_checkers;
 
+import org.apache.logging.log4j.Level;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.config.Config;
 
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.scenario.ScenarioUtils;
 
+import org.matsim.freight.carriers.CarriersUtils;
+import org.matsim.freight.carriers.FreightCarriersConfigGroup;
 import org.matsim.freight.logistics.FreightLogisticsConfigGroup;
+import org.matsim.freight.logistics.LSP;
+import org.matsim.freight.logistics.LSPUtils;
+import org.matsim.freight.logistics.LSPs;
+import org.matsim.freight.logistics.consistency_checker.LogisticsConsitencyChecker;
+import org.matsim.freight.logistics.io.LSPPlanXmlReader;
 import org.matsim.testcases.MatsimTestUtils;
 
+
+import java.util.Collection;
+import java.util.Collections;
 
 import static org.matsim.core.config.ConfigUtils.addOrGetModule;
 import static org.matsim.core.config.ConfigUtils.createConfig;
@@ -18,12 +31,36 @@ import static org.matsim.core.config.ConfigUtils.createConfig;
  *
  */
 public class UniqueIDs {
+	//Please specify wanted logger level here:
+	//Level.ERROR -> all log-messages will be displayed as errors in red.
+	//Level.WARN -> all log-messages will be displayed as warnings in red.
+	//Level.INFO -> all log-messages will be displayed as information in white.
+	private final Level lvl = Level.ERROR;
+
+	@RegisterExtension
 	private final MatsimTestUtils utils = new MatsimTestUtils();
 
-	//	Config config = ConfigUtils.createConfig();
+	@Test
+	public void myFirstLSPTst() {
 
-	//	Scenario scenario = ScenarioUtils.createScenario(config);
+		Config config = ConfigUtils.createConfig();
 
-	//	FreightLogisticsConfigGroup logisticsConfigGroup = addOrGetModule(config, FreightLogisticsConfigGroup.class);
+		FreightLogisticsConfigGroup logisticsConfigGroup = addOrGetModule(config, FreightLogisticsConfigGroup.class);
+		//logisticsConfigGroup.setLspsFile(utils.getPackageInputDirectory() + "lspTestFile.xml"); //Does not work atm.
+
+		FreightCarriersConfigGroup freightConfigGroup = addOrGetModule(config, FreightCarriersConfigGroup.class);
+		freightConfigGroup.setCarriersFile(utils.getPackageInputDirectory() + "carriers.xml"); //Dahin kopieren..
+		freightConfigGroup.setCarriersVehicleTypesFile(utils.getPackageInputDirectory() + "vehicles.xml");
+
+		Scenario scenario = ScenarioUtils.createScenario(config);
+		CarriersUtils.loadCarriersAccordingToFreightConfig(scenario);
+
+
+		LSPUtils.addLSPs(scenario, new LSPs(Collections.emptyList()));
+
+		new LSPPlanXmlReader(LSPUtils.getLSPs(scenario), CarriersUtils.getCarriers(scenario)).readFile(utils.getPackageInputDirectory() + "lsps.xml");
+
+		LogisticsConsitencyChecker.uniqueResourcesCheck(LSPUtils.getLSPs(scenario), lvl);
 
 	}
+}
