64f1145c6fafc159f4391160041419927dde9ee3
Tarek Chouaki <tarek.chouaki@irt-systemx.fr>
2025-02-24 14:53:01 +0100

    fix: fast fail when DrtConfigGroup.simulationType=estimateAndTeleport

--- contribs/drt/src/main/java/org/matsim/contrib/drt/run/DrtModeModule.java
+++ contribs/drt/src/main/java/org/matsim/contrib/drt/run/DrtModeModule.java
@@ -24,6 +24,7 @@ import org.matsim.api.core.v01.network.Network;
 import org.matsim.contrib.drt.analysis.DrtEventSequenceCollector;
 import org.matsim.contrib.drt.analysis.zonal.DrtModeZonalSystemModule;
 import org.matsim.contrib.drt.estimator.DrtEstimatorModule;
+import org.matsim.contrib.drt.estimator.DrtEstimatorParams;
 import org.matsim.contrib.drt.fare.DrtFareHandler;
 import org.matsim.contrib.drt.optimizer.rebalancing.RebalancingModule;
 import org.matsim.contrib.drt.prebooking.analysis.PrebookingModeAnalysisModule;
@@ -52,6 +53,8 @@ import org.matsim.core.router.util.TravelTime;
 import com.google.inject.Key;
 import com.google.inject.Singleton;
 import com.google.inject.name.Names;
+
+import java.util.Optional;
 
 /**
  * @author michalm (Michal Maciejewski)
@@ -118,7 +121,11 @@ public final class DrtModeModule extends AbstractDvrpModeModule {
 		install(new AdaptiveTravelTimeMatrixModule(drtCfg.mode));
 
 		if (drtCfg.simulationType == DrtConfigGroup.SimulationType.estimateAndTeleport ) {
-			install(new DrtEstimatorModule(getMode(), drtCfg, drtCfg.getDrtEstimatorParams().get()));
+			Optional<DrtEstimatorParams> drtEstimatorParams = drtCfg.getDrtEstimatorParams();
+			if(drtEstimatorParams.isEmpty()) {
+				throw new IllegalStateException("parameter set 'estimator' is required when 'simulationType' is set to 'estimateAndTeleport'");
+			}
+			install(new DrtEstimatorModule(getMode(), drtCfg, drtEstimatorParams.get()));
 		}
 
 		bindModal(DvrpLoadFromTrip.class).toProvider(modalProvider(getter -> {
