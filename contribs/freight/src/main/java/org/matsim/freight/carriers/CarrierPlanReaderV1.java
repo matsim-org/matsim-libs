8b441ae275fc54faf3b8362d928678e1b72b8d50
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2025-02-27 14:19:29 +0100

    do the bidirectional setting manually, to avoid automatically  setting a plan as selected. This is only an issue in the old V1 reader.

--- contribs/freight/src/main/java/org/matsim/freight/carriers/CarrierPlanReaderV1.java
+++ contribs/freight/src/main/java/org/matsim/freight/carriers/CarrierPlanReaderV1.java
@@ -270,7 +270,10 @@ class CarrierPlanReaderV1 extends MatsimXmlParser {
 		if (name.equals("plan")) {
 			CarrierPlan currentPlan = new CarrierPlan(scheduledTours );
 			currentPlan.setScore(currentScore );
-			currentCarrier.addPlan(currentPlan);
+			//The following is done instead of currentCarrier.addPlan(currentPlan), because addPlan() sets the selected plan, if now plan is already selected.
+			//In this version here it seems to be ok, to have also plans without any decision if they are selected or not.
+			currentCarrier.getPlans().add(currentPlan); // Just add the plan without making it the selected one.
+			currentPlan.setCarrier(currentCarrier); // This is necessary for the bidirectional reference.
 			if(this.selected){
 				currentCarrier.setSelectedPlan( currentPlan );
 			}
