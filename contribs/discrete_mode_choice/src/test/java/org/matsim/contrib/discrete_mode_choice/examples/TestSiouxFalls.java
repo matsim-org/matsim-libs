1c14a8573516051bf507b4d19934879d4aac1399
tkchouaki <tarek.chouaki@irt-systemx.fr>
2025-03-24 14:45:21 +0100

    feat: EfficientTourBasedModel

--- contribs/discrete_mode_choice/src/test/java/org/matsim/contrib/discrete_mode_choice/examples/TestSiouxFalls.java
+++ contribs/discrete_mode_choice/src/test/java/org/matsim/contrib/discrete_mode_choice/examples/TestSiouxFalls.java
@@ -12,6 +12,9 @@ import org.matsim.api.core.v01.events.PersonArrivalEvent;
 import org.matsim.api.core.v01.events.handler.PersonArrivalEventHandler;
 import org.matsim.contribs.discrete_mode_choice.modules.DiscreteModeChoiceConfigurator;
 import org.matsim.contribs.discrete_mode_choice.modules.DiscreteModeChoiceModule;
+import org.matsim.contribs.discrete_mode_choice.modules.EstimatorModule;
+import org.matsim.contribs.discrete_mode_choice.modules.ModelModule;
+import org.matsim.contribs.discrete_mode_choice.modules.config.DiscreteModeChoiceConfigGroup;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.AbstractModule;
@@ -20,7 +23,6 @@ import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.io.IOUtils;
 import org.matsim.examples.ExamplesUtils;
-import org.matsim.pt.config.TransitConfigGroup.TransitRoutingAlgorithmType;
 
 public class TestSiouxFalls {
 	@Test
@@ -65,6 +67,55 @@ public class TestSiouxFalls {
 		assertEquals(82139, listener.counts.get("walk"), 2);
 
 	}
+
+	@Test
+	void testSiouxFallsWithEfficientTourBasedModel() {
+		URL scenarioURL = ExamplesUtils.getTestScenarioURL("siouxfalls-2014");
+
+		Config config = ConfigUtils.loadConfig(IOUtils.extendUrl(scenarioURL, "config_default.xml"));
+		DiscreteModeChoiceConfigurator.configureAsSubtourModeChoiceReplacement(config);
+
+		DiscreteModeChoiceConfigGroup dmcConfig = (DiscreteModeChoiceConfigGroup) config.getModules().get(DiscreteModeChoiceConfigGroup.GROUP_NAME);
+
+		// Enabling the EfficientTourBasedModel
+		dmcConfig.setModelType(ModelModule.ModelType.EfficientTour);
+		dmcConfig.setTourEstimator(EstimatorModule.CUMULATIVE);
+
+		config.controller().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
+		config.controller().setLastIteration(1);
+		// save some bandwidth/time:
+		config.controller().setCreateGraphs( false );
+		config.controller().setWritePlansInterval( 0 );
+		config.controller().setWriteEventsInterval( 0 );
+		config.controller().setDumpDataAtEnd( false );
+
+		config.qsim().setFlowCapFactor(10000.0);
+		config.qsim().setStorageCapFactor(10000.0);
+
+		Scenario scenario = ScenarioUtils.loadScenario(config);
+
+		Controler controller = new Controler(scenario);
+		controller.addOverridingModule(new DiscreteModeChoiceModule());
+
+		ModeListener listener = new ModeListener();
+		controller.addOverridingModule(new AbstractModule() {
+			@Override
+			public void install() {
+				addEventHandlerBinding().toInstance(listener);
+			}
+		});
+
+		controller.run();
+
+		System.out.println((int) listener.counts.get("pt"));
+		System.out.println((int) listener.counts.get("car"));
+		System.out.println(listener.counts.get("walk"));
+
+		assertEquals(44195, listener.counts.get("pt"), 2);
+		assertEquals(132316, listener.counts.get("car"), 2);
+		assertEquals(82139, listener.counts.get("walk"), 2);
+	}
+
 
 	static class ModeListener implements PersonArrivalEventHandler {
 		private final Map<String, Integer> counts = new HashMap<>();
