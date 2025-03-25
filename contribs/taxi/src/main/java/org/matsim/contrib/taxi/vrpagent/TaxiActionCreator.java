4435515b1801a307c17e19527ef03e1a835eafb1
Nico Kühnel <nico.kuehnel@moia.io>
2025-03-25 14:06:06 +0100

    update dvrp config groups to the conventional config format (#3831)

--- contribs/taxi/src/main/java/org/matsim/contrib/taxi/vrpagent/TaxiActionCreator.java
+++ contribs/taxi/src/main/java/org/matsim/contrib/taxi/vrpagent/TaxiActionCreator.java
@@ -52,9 +52,9 @@ public class TaxiActionCreator implements VrpAgentLogic.DynActionCreator {
 	public TaxiActionCreator(PassengerHandler passengerHandler, TaxiConfigGroup taxiCfg, MobsimTimer timer,
 			DvrpConfigGroup dvrpCfg) {
 		this(passengerHandler, taxiCfg.onlineVehicleTracker ?
-				v -> VrpLegFactory.createWithOnlineTracker(dvrpCfg.mobsimMode, v,
+				v -> VrpLegFactory.createWithOnlineTracker(dvrpCfg.getMobsimMode(), v,
 						OnlineTrackerListener.NO_LISTENER, timer) :
-				v -> VrpLegFactory.createWithOfflineTracker(dvrpCfg.mobsimMode, v, timer));
+				v -> VrpLegFactory.createWithOfflineTracker(dvrpCfg.getMobsimMode(), v, timer));
 	}
 
 	public TaxiActionCreator(PassengerHandler passengerHandler, VrpLegFactory legFactory) {
