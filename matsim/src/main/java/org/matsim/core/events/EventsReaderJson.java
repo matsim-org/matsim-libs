69e9c5c05b8e810620966639c4b286b88c043e60
Tobias Kohl <tobiko10@aol.com>
2025-03-28 12:02:29 +0100

    feat: Introduce PersonInitializedEvent (#3865)
    * add PersonCreationEvent
    
    * copy a test input file from NetworkChangeEventsParserWriterTest into the appropriate directory because it was missing and had to be loaded from the resources
    
    * add a test to ensure the correct functionality of the new event
    
    * rename the event to PersonInitializedEvent
    
    * some small javadoc
    
    ---------
    
    Co-authored-by: Tobias Kohl <tobias.kohl@senozon.com>
    # Conflicts:
    #	matsim/src/main/java/org/matsim/core/events/EventsReaderXMLv1.java

--- matsim/src/main/java/org/matsim/core/events/EventsReaderJson.java
+++ matsim/src/main/java/org/matsim/core/events/EventsReaderJson.java
@@ -45,6 +45,7 @@ import org.matsim.api.core.v01.events.LinkLeaveEvent;
 import org.matsim.api.core.v01.events.PersonArrivalEvent;
 import org.matsim.api.core.v01.events.PersonDepartureEvent;
 import org.matsim.api.core.v01.events.PersonEntersVehicleEvent;
+import org.matsim.api.core.v01.events.PersonInitializedEvent;
 import org.matsim.api.core.v01.events.PersonLeavesVehicleEvent;
 import org.matsim.api.core.v01.events.PersonMoneyEvent;
 import org.matsim.api.core.v01.events.PersonScoreEvent;
@@ -332,6 +333,15 @@ public final class EventsReaderJson {
 			Id<TransitStopFacility> waitStopId = Id.create(o.get(AgentWaitingForPtEvent.ATTRIBUTE_WAITSTOP).asText(), TransitStopFacility.class);
 			Id<TransitStopFacility> destinationStopId = Id.create(o.get(AgentWaitingForPtEvent.ATTRIBUTE_DESTINATIONSTOP).asText(), TransitStopFacility.class);
 			this.events.processEvent(new AgentWaitingForPtEvent(time, agentId, waitStopId, destinationStopId));
+		} else if (PersonInitializedEvent.EVENT_TYPE.equals(eventType)){
+			Id<Person> personId = Id.create(o.get(PersonInitializedEvent.ATTRIBUTE_PERSON).asText(), Person.class);
+			Coord coord = null;
+			if (o.get(Event.ATTRIBUTE_X ) != null) {
+				double xx = o.get(Event.ATTRIBUTE_X).asDouble();
+				double yy = o.get(Event.ATTRIBUTE_Y).asDouble();
+				coord = new Coord(xx, yy) ;
+			}
+			this.events.processEvent(new PersonInitializedEvent(time, personId, coord));
 		} else {
 			GenericEvent event = new GenericEvent(eventType, time);
 
