44c86e4c1e97d3ca5359680833c2350e5c22ac72
stendler <git@stendler.de>
2025-04-25 17:20:37 +0200

    contrib(profiling): expand comments in AspectJTest

--- contribs/profiling/src/test/java/org/matsim/contrib/profiling/aop/AspectJTest.java
+++ contribs/profiling/src/test/java/org/matsim/contrib/profiling/aop/AspectJTest.java
@@ -1,5 +1,7 @@
 package org.matsim.contrib.profiling.aop;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,11 +19,20 @@ import java.io.PrintStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * Test if aspectj is configured properly and aspects are working.
+ * <p>Aspects are expected to be woven into existing code during compile time or post compile time.
+ * For the test aspects within this test source, this should happen in maven's test-compile lifecycle
+ * via the aspectj-maven-plugin configured in the pom.xml
+ */
 public class AspectJTest {
 
 	private final PrintStream standardOut = System.out;
 	private final OutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
+	/**
+	 * Used in {@link #testAspectJ_compileTime()}
+	 */
 	static class MyScoringListener implements ScoringListener {
 
 		@Override
@@ -30,6 +41,9 @@ public class AspectJTest {
 		}
 	}
 
+	/**
+	 * Used in {@link #whenCompileTimeWeaving_usingAnnotatedAspect_expectPrintFromAdvice()}
+	 */
 	static final class MyReplanningContext implements ReplanningContext {
 
 		@Override
@@ -38,11 +52,18 @@ public class AspectJTest {
 		}
 	}
 
+	/**
+	 * Redirect standard out {@link System#out} to our own output stream
+	 * to be able to capture and verify expected print statements.
+	 */
 	@BeforeEach
 	public void setUp() {
 		System.setOut(new PrintStream(outputStreamCaptor));
 	}
 
+	/**
+	 * Reset {@link System#out} to standard out to not interfere with other tests.
+	 */
 	@AfterEach
 	public void resetSystemOut() {
 		System.setOut(standardOut);
@@ -72,6 +93,10 @@ public class AspectJTest {
 		assertThat(result.toString()).isEqualTo("aspect");
 	}
 
+	/**
+	 * Testing aspectj post-compile-time weaving of an aspect declared via annotations into methods of dependencies.
+	 * I.e. if the {@link AnnotationBasedExampleAspect#BeforeGetFactory(JoinPoint.StaticPart)} is properly applied to the code of a library ({@link VehicleUtils}).
+	 */
 	@Test
 	public void whenPostCompileTimeWeaving_usingAnnotatedAspect_expectPrintFromAdvice() {
 		var factory = VehicleUtils.getFactory();
@@ -79,6 +104,10 @@ public class AspectJTest {
 		assertThat(outputStreamCaptor.toString()).isEqualTo("aspect via annotations: VehiclesFactory org.matsim.vehicles.VehicleUtils.getFactory()\n");
 	}
 
+	/**
+	 * Testing aspectj compile-time weaving of an aspect into methods of dependencies.
+	 * I.e. if the {@link AnnotationBasedExampleAspect#AroundReplanningContextGetIteration(ProceedingJoinPoint)} is properly applied to the code above ({@link MyReplanningContext}).
+	 */
 	@Test
 	public void whenCompileTimeWeaving_usingAnnotatedAspect_expectPrintFromAdvice() {
 		var myContext = new MyReplanningContext();
