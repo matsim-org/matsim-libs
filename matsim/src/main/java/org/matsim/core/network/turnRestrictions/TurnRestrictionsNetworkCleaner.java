5f8e19c49bc36f77a8efccea5454ca59f6912589
nkuehnel <nico.kuehnel@moia.io>
2025-05-09 11:18:14 +0200

    ensure that resulting network always contains the cleaning mode

--- matsim/src/main/java/org/matsim/core/network/turnRestrictions/TurnRestrictionsNetworkCleaner.java
+++ matsim/src/main/java/org/matsim/core/network/turnRestrictions/TurnRestrictionsNetworkCleaner.java
@@ -128,42 +128,45 @@ public class TurnRestrictionsNetworkCleaner {
 
     private void checkRealLinkExistence(Network network, TurnRestrictionsContext.ColoredLink coloredLink, String mode) {
         Id<Link> copyId = getColoredLinkId(coloredLink);
-        if (network.getLinks().containsKey(copyId) && !network.getLinks().containsKey(coloredLink.link.getId())) {
-            Link link = coloredLink.link;
-            Link linkCopy = NetworkUtils.createLink(
-                    link.getId(),
-                    network.getNodes().get(link.getFromNode().getId()),
-                    network.getNodes().get(link.getToNode().getId()),
-                    network,
-                    link.getLength(),
-                    link.getFreespeed(),
-                    link.getCapacity(),
-                    link.getNumberOfLanes()
-            );
-            // copy all attributes except initial turn restrictions
-            NetworkUtils.copyAttributesExceptDisallowedNextLinks(coloredLink.link, linkCopy);
+        if (network.getLinks().containsKey(copyId)) {
+            if (!network.getLinks().containsKey(coloredLink.link.getId())) {
+                Link link = coloredLink.link;
+                Link linkCopy = NetworkUtils.createLink(
+                        link.getId(),
+                        network.getNodes().get(link.getFromNode().getId()),
+                        network.getNodes().get(link.getToNode().getId()),
+                        network,
+                        link.getLength(),
+                        link.getFreespeed(),
+                        link.getCapacity(),
+                        link.getNumberOfLanes()
+                );
+                // copy all attributes except initial turn restrictions
+                NetworkUtils.copyAttributesExceptDisallowedNextLinks(coloredLink.link, linkCopy);
 
-            // copy all other modes
-            for (String allowedMode : coloredLink.link.getAllowedModes()) {
-                NetworkUtils.addAllowedMode(linkCopy, allowedMode);
-            }
+                // copy all other modes
+                linkCopy.setAllowedModes(coloredLink.link.getAllowedModes());
 
-            // copy all turn restrictions of all other modes
-            DisallowedNextLinks originalDisallowedNextLinks = NetworkUtils.getDisallowedNextLinks(coloredLink.link);
-            if(originalDisallowedNextLinks != null) {
-                DisallowedNextLinks disallowedNextLinks = NetworkUtils.getOrCreateDisallowedNextLinks(linkCopy);
-                for (Map.Entry<String, List<List<Id<Link>>>> restriction : originalDisallowedNextLinks.getAsMap().entrySet()) {
-                    if(!mode.equals(restriction.getKey())) {
-                        for (List<Id<Link>> sequence : restriction.getValue()) {
-                            disallowedNextLinks.addDisallowedLinkSequence(restriction.getKey(), sequence);
+                // copy all turn restrictions of all other modes
+                DisallowedNextLinks originalDisallowedNextLinks = NetworkUtils.getDisallowedNextLinks(coloredLink.link);
+                if (originalDisallowedNextLinks != null) {
+                    DisallowedNextLinks disallowedNextLinks = NetworkUtils.getOrCreateDisallowedNextLinks(linkCopy);
+                    for (Map.Entry<String, List<List<Id<Link>>>> restriction : originalDisallowedNextLinks.getAsMap().entrySet()) {
+                        if (!mode.equals(restriction.getKey())) {
+                            for (List<Id<Link>> sequence : restriction.getValue()) {
+                                disallowedNextLinks.addDisallowedLinkSequence(restriction.getKey(), sequence);
+                            }
                         }
                     }
+                    if (disallowedNextLinks.isEmpty()) {
+                        NetworkUtils.removeDisallowedNextLinks(linkCopy);
+                    }
                 }
-                if (disallowedNextLinks.isEmpty()) {
-                    NetworkUtils.removeDisallowedNextLinks(linkCopy);
-                }
+                network.addLink(linkCopy);
             }
-            network.addLink(linkCopy);
+
+            // make sure that current mode is allowed on the link
+            NetworkUtils.addAllowedMode(network.getLinks().get(coloredLink.link.getId()), mode);
         }
     }
 
@@ -199,6 +202,8 @@ public class TurnRestrictionsNetworkCleaner {
                     .stream()
                     .map(Identifiable::getId)
                     .filter(link -> network.getLinks().containsKey(link))
+                    //only consider links that are reachable by the current mode.
+                    .filter(link -> network.getLinks().get(link).getAllowedModes().contains(mode))
                     .collect(Collectors.toSet());
 
 
