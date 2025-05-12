4e92a4e4d14583ed492fc2a574c99afc8c36175a
stendler <git@stendler.de>
2025-05-12 12:11:27 +0200

    contrib(profiling): WIP JFRStopwatch - record operations from execution samples
    issues:
    - "wrong" names
    - wrong operations order
    - negative values?

--- contribs/profiling/src/main/java/org/matsim/contrib/profiling/analysis/JFRStopwatch.java
+++ contribs/profiling/src/main/java/org/matsim/contrib/profiling/analysis/JFRStopwatch.java
@@ -2,13 +2,16 @@ package org.matsim.contrib.profiling.analysis;
 
 import jdk.jfr.consumer.EventStream;
 import jdk.jfr.consumer.RecordedFrame;
+import org.apache.commons.lang3.tuple.Pair;
 import org.matsim.analysis.IterationStopWatch;
 import org.matsim.contrib.profiling.events.JFRIterationEvent;
 
 import java.awt.*;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Stream;
 
 /**
  * Service to generate a stopwatch.png from jfr profiling recording files
@@ -17,6 +20,10 @@ public class JFRStopwatch implements AutoCloseable {
 
 	private final IterationStopWatch stopwatch = new IterationStopWatch();
 	private final EventStream eventStream;
+
+	//private final Queue<Pair<String, Long>> stages = new LinkedBlockingQueue<>(); // for using just the custom events
+	private final Map<String, Long> stagesBegin = Collections.synchronizedMap(new LinkedHashMap<>());
+	private final Map<String, Long> stagesEnd = Collections.synchronizedMap(new LinkedHashMap<>());
 
 	public static void main(String[] args) throws IOException {
 
@@ -39,7 +46,7 @@ public class JFRStopwatch implements AutoCloseable {
 			}
 
 			stopwatch.stopwatch.writeSeparatedFile(fileChooser.getDirectory() + "/stopwatch-" + fileChooser.getFile() + ".csv", ";");
-			// todo also create png
+			stopwatch.stopwatch.writeGraphFile(fileChooser.getDirectory() + "/stopwatch-" + fileChooser.getFile());
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		} finally {
@@ -61,7 +68,18 @@ public class JFRStopwatch implements AutoCloseable {
 			// start iteration in stopwatch
 			stopwatch.beginIteration(event.getInt("iteration"), event.getStartTime().toEpochMilli());
 			// add all other recorded events to stopwatch
-			// todo
+			synchronized (stagesBegin) {
+				// todo sort the timestamp before putting here
+				stagesBegin.forEach((stage, startTime) -> {
+					stopwatch.beginOperation(stage, startTime);
+					stopwatch.endOperation(stage, stagesEnd.getOrDefault(stage, startTime));
+					if (!stagesEnd.containsKey(stage)) {
+						System.out.println("Stage began but no ending recorded: " + stage + " - resorting to startTime"); // todo might happen for scoring, use iteration end instead?
+					}
+				});
+				stagesBegin.clear();
+				stagesEnd.clear();
+			}
 			// end iteration in stopwatch & flush recordings
 			stopwatch.endIteration(event.getEndTime().toEpochMilli());
 		});
@@ -73,6 +91,23 @@ public class JFRStopwatch implements AutoCloseable {
 			System.out.println(thread.getJavaName() + "@");
 
 			if ("main".equals(thread.getJavaName())) {
+
+				// for all started but not ended operations
+				synchronized (stagesBegin) {
+					stagesBegin.entrySet()
+						.stream()
+						.filter(entry -> !stagesEnd.containsKey(entry.getKey()))
+						.filter(entry ->
+							// if no mention anymore in the stacktrace, the operation is assumed to be over
+							event.getStackTrace()
+								.getFrames()
+								.stream()
+								.filter(RecordedFrame::isJavaFrame)
+								.map(RecordedFrame::getMethod)
+								.noneMatch(frameMethod -> entry.getKey().equals(frameMethod.getName()))
+						).forEach(entry -> stagesEnd.put(entry.getKey(), event.getEndTime().toEpochMilli()));
+				}
+
 				event.getStackTrace()
 					.getFrames()
 					.stream()
@@ -82,39 +117,30 @@ public class JFRStopwatch implements AutoCloseable {
 
 						var type = recordedMethod.getType().getName();
 						var method = recordedMethod.getName();
+						var time = event.getStartTime().toEpochMilli(); // start and end time are equal on marker events like execution sample
 
 						System.out.println(type + "#" + method);
-						if ("org.matsim.core.controler.AbstractController".equals(type) && "iteration".equals(method)) {
 
-						}
+						// todo missing stage names equal to current stopwatch
+						Stream.of(
+							Pair.of("org.matsim.core.controler.ControlerListenerManagerImpl", "fireControlerIterationStartsEvent"), // iterationStartListeners
+							Pair.of("org.matsim.core.controler.ControlerListenerManagerImpl", "fireControlerReplanningEvent"), // replanning
+							Pair.of("org.matsim.core.controler.ControlerListenerManagerImpl", "fireControlerBeforeMobsimEvent"), // beforeMobsimListeners
+							Pair.of("org.matsim.core.controler.corelisteners.PlansDumpingImpl", "notifyBeforeMobsim"), // dump all plans
+							Pair.of("org.matsim.core.controler.NewControler", "prepareForMobsim"), // prepareForMobsim - needs to be actual impl and not an interface/abstract method
+							Pair.of("org.matsim.core.controler.NewControler", "runMobSim"), // mobsim - needs to be actual impl and not an interface/abstract method
+							Pair.of("org.matsim.core.controler.ControlerListenerManagerImpl", "fireControlerAfterMobsimEvent"), // afterMobsimListeners
+							Pair.of("org.matsim.core.controler.ControlerListenerManagerImpl", "fireControlerScoringEvent"), // scoring
+							Pair.of("org.matsim.core.controler.ControlerListenerManagerImpl", "fireControlerIterationEndsEvent") // iterationEndsListeners
+						)
+							.filter(entry -> entry.getKey().equals(recordedMethod.getType().getName()) && entry.getValue().equals(recordedMethod.getName()))
+							.findFirst()
+							.ifPresent(entry -> stagesBegin.putIfAbsent(method, time));
+
 					});
 				System.out.println("---");
 			}
 		});
-
-		// iteration start (and its number)
-
-		// iteration start listeners
-
-		// replanning
-
-		// beforeMobsimListeners
-
-		// dumpall
-
-		// end beforeMobsimListeners
-
-		// prepare for Mobsim
-
-		// mobsim
-
-		// afterMobsimListeners
-
-		// scoring
-
-		// iterationEndsListeners
-
-		// end iteration
 
 	}
 
