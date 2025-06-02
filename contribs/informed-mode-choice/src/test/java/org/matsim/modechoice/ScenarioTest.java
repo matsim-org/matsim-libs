0020aafe2182233e6ffd6f2314a6227684796947
apabis <aleksanderpabis@gmail.com>
2025-06-02 18:17:00 +0200

    Adapted ScenarioTest (-> TopKSubtourGeneratorTest, SingleTripChoicesGeneratorTest, DifferentModesTest, BestChoiceGeneratorTest, SelectVsRandomSubtourTest, RandomSubtourModeStrategyTest, SelectSingleTripModeStrategyTest)

--- contribs/informed-mode-choice/src/test/java/org/matsim/modechoice/ScenarioTest.java
+++ contribs/informed-mode-choice/src/test/java/org/matsim/modechoice/ScenarioTest.java
@@ -3,11 +3,17 @@ package org.matsim.modechoice;
 import com.google.inject.Injector;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.population.Person;
 import org.matsim.application.MATSimApplication;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 import org.matsim.testcases.MatsimTestUtils;
+import org.matsim.vehicles.PersonVehicles;
+import org.matsim.vehicles.VehicleType;
+import org.matsim.vehicles.VehicleUtils;
 
 public class ScenarioTest {
 
@@ -28,6 +34,22 @@ public class ScenarioTest {
 		prepareConfig(config);
 
 		controler = MATSimApplication.prepare(TestScenario.class, config, getArgs());
+
+
+		// We need to add a vehicle, it however does not affect the results
+		Id<VehicleType> typeId = Id.create(1, VehicleType.class);
+		controler.getScenario().getVehicles().addVehicleType(VehicleUtils.createVehicleType(typeId));
+		controler.getScenario().getVehicles().addVehicle(VehicleUtils.createVehicle(Id.createVehicleId(1), controler.getScenario().getVehicles().getVehicleTypes().get(typeId)));
+
+		PersonVehicles vehicles = new PersonVehicles();
+		vehicles.addModeVehicle(TransportMode.car, Id.createVehicleId(1));
+		vehicles.addModeVehicle(TransportMode.ride, Id.createVehicleId(1));
+		vehicles.addModeVehicle(TransportMode.walk, Id.createVehicleId(1));
+		vehicles.addModeVehicle("freight", Id.createVehicleId(1));
+		for (Person p : controler.getScenario().getPopulation().getPersons().values()){
+			VehicleUtils.insertVehicleIdsIntoPersonAttributes(p, vehicles.getModeVehicles());
+		}
+
 		injector = controler.getInjector();
 
 	}
