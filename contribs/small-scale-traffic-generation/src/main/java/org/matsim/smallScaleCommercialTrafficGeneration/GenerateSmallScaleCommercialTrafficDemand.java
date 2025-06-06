0f5ce5ee612c4f6773a994d1abd23fb2be790fb9
Ricardo Ewert <ewert@vsp.tu-berlin.de>
2025-06-06 14:36:07 +0200

    clean up

--- contribs/small-scale-traffic-generation/src/main/java/org/matsim/smallScaleCommercialTrafficGeneration/GenerateSmallScaleCommercialTrafficDemand.java
+++ contribs/small-scale-traffic-generation/src/main/java/org/matsim/smallScaleCommercialTrafficGeneration/GenerateSmallScaleCommercialTrafficDemand.java
@@ -20,7 +20,6 @@
 
 package org.matsim.smallScaleCommercialTrafficGeneration;
 
-import com.google.inject.Inject;
 import it.unimi.dsi.fastutil.objects.Object2DoubleMap;
 import org.apache.commons.math3.distribution.EnumeratedDistribution;
 import org.apache.commons.math3.random.MersenneTwister;
@@ -36,8 +35,6 @@ import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
-import org.matsim.api.core.v01.population.Activity;
-import org.matsim.api.core.v01.population.Leg;
 import org.matsim.application.MATSimAppCommand;
 import org.matsim.application.options.ShpOptions.Index;
 import org.matsim.core.config.Config;
@@ -45,20 +42,17 @@ import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.config.consistency.UnmaterializedConfigGroupChecker;
 import org.matsim.core.config.groups.VspExperimentalConfigGroup;
 import org.matsim.core.controler.*;
-import org.matsim.core.gbl.Gbl;
 import org.matsim.core.gbl.MatsimRandom;
 import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.network.algorithms.TransportModeNetworkFilter;
-import org.matsim.core.population.routes.NetworkRoute;
 import org.matsim.core.scenario.ProjectionUtils;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.core.scoring.ScoringFunction;
-import org.matsim.core.scoring.SumScoringFunction;
 import org.matsim.core.utils.geometry.CoordUtils;
 import org.matsim.core.utils.geometry.CoordinateTransformation;
 import org.matsim.facilities.ActivityFacility;
 import org.matsim.freight.carriers.*;
 import org.matsim.freight.carriers.controller.*;
+import org.matsim.freight.carriers.usecases.chessboard.CarrierScoringFunctionFactoryImpl;
 import org.matsim.smallScaleCommercialTrafficGeneration.data.CommercialTourSpecifications;
 import org.matsim.smallScaleCommercialTrafficGeneration.data.DefaultTourSpecificationsByUsingKID2002;
 import org.matsim.vehicles.CostInformation;
@@ -567,7 +561,7 @@ public class GenerateSmallScaleCommercialTrafficDemand implements MATSimAppComma
 		controller.addOverridingModule(new AbstractModule() {
 			@Override
 			public void install() {
-				bind(CarrierScoringFunctionFactory.class).toInstance(new MyCarrierScoringFunctionFactory());
+				bind(CarrierScoringFunctionFactory.class).to(CarrierScoringFunctionFactoryImpl.class);
 			}
 		});
 
@@ -981,172 +975,6 @@ public class GenerateSmallScaleCommercialTrafficDemand implements MATSimAppComma
 
 	public int getAdditionalTravelBufferPerIterationInMinutes(){
 		return additionalTravelBufferPerIterationInMinutes;
-	}
-
-	private static class MyCarrierScoringFunctionFactory implements CarrierScoringFunctionFactory {
-
-		@Inject
-		private Network network;
-
-		@Override
-		public ScoringFunction createScoringFunction(Carrier carrier) {
-			SumScoringFunction sf = new SumScoringFunction();
-			DriversLegScoring driverLegScoring = new DriversLegScoring(carrier, network);
-			VehicleEmploymentScoring vehicleEmploymentScoring = new VehicleEmploymentScoring(carrier);
-			DriversActivityScoring actScoring = new DriversActivityScoring();
-			sf.addScoringFunction(driverLegScoring);
-			sf.addScoringFunction(vehicleEmploymentScoring);
-			sf.addScoringFunction(actScoring);
-			return sf;
-		}
-
-	}
-
-	static class DriversActivityScoring implements SumScoringFunction.BasicScoring, SumScoringFunction.ActivityScoring {
-
-
-		private double score;
-
-		public DriversActivityScoring() {
-			super();
-		}
-
-		@Override
-		public void finish() {
-		}
-
-		@Override
-		public double getScore() {
-			return score;
-		}
-
-		@Override
-		public void handleFirstActivity(Activity act) {
-			handleActivity(act);
-		}
-
-		@Override
-		public void handleActivity(Activity act) {
-			if (act instanceof FreightActivity) {
-				double actStartTime = act.getStartTime().seconds();
-
-				// log.info(act + " start: " + Time.writeTime(actStartTime));
-				TimeWindow tw = ((FreightActivity) act).getTimeWindow();
-				if (actStartTime > tw.getEnd()) {
-					double missedTimeWindowPenalty = 0.01;
-					double penalty_score = (-1) * (actStartTime - tw.getEnd()) * missedTimeWindowPenalty;
-					if (!(penalty_score <= 0.0))
-						throw new AssertionError("penalty score must be negative");
-					// log.info("penalty " + penalty_score);
-					score += penalty_score;
-
-				}
-				double timeParameter = 0.008;
-				double actTimeCosts = (act.getEndTime().seconds() - actStartTime) * timeParameter;
-				// log.info("actCosts " + actTimeCosts);
-				if (!(actTimeCosts >= 0.0))
-					throw new AssertionError("actTimeCosts must be positive");
-				score += actTimeCosts * (-1);
-			}
-		}
-
-		@Override
-		public void handleLastActivity(Activity act) {
-			handleActivity(act);
-		}
-
-	}
-
-	static class DriversLegScoring implements SumScoringFunction.BasicScoring, SumScoringFunction.LegScoring {
-
-		// private static final Logger log = Logger.getLogger(DriversLegScoring.class);
-
-		private double score = 0.0;
-		private final Network network;
-		private final Carrier carrier;
-		private final Set<CarrierVehicle> employedVehicles;
-
-		public DriversLegScoring(Carrier carrier, Network network) {
-			super();
-			this.network = network;
-			this.carrier = carrier;
-			employedVehicles = new HashSet<>();
-		}
-
-		@Override
-		public void finish() {
-
-		}
-
-		@Override
-		public double getScore() {
-			return score;
-		}
-
-		private double getTimeParameter(CarrierVehicle vehicle) {
-			return vehicle.getType().getCostInformation().getCostsPerSecond();
-		}
-
-		private double getDistanceParameter(CarrierVehicle vehicle) {
-			return vehicle.getType().getCostInformation().getCostsPerMeter();
-		}
-
-		@Override
-		public void handleLeg(Leg leg) {
-			if (leg.getRoute() instanceof NetworkRoute nRoute) {
-				Id<Vehicle> vehicleId = nRoute.getVehicleId();
-				CarrierVehicle vehicle = CarriersUtils.getCarrierVehicle(carrier, vehicleId);
-				Gbl.assertNotNull(vehicle);
-				employedVehicles.add(vehicle);
-				double distance = 0.0;
-				if (leg.getRoute() instanceof NetworkRoute) {
-					Link startLink = network.getLinks().get(leg.getRoute().getStartLinkId());
-					distance += startLink.getLength();
-					for (Id<Link> linkId : ((NetworkRoute) leg.getRoute()).getLinkIds()) {
-						distance += network.getLinks().get(linkId).getLength();
-					}
-					distance += network.getLinks().get(leg.getRoute().getEndLinkId()).getLength();
-				}
-				double distanceCosts = distance * getDistanceParameter(vehicle);
-				if (!(distanceCosts >= 0.0))
-					throw new AssertionError("distanceCosts must be positive");
-				score += (-1) * distanceCosts;
-				double timeCosts = leg.getTravelTime().seconds() * getTimeParameter(vehicle);
-				if (!(timeCosts >= 0.0))
-					throw new AssertionError("distanceCosts must be positive");
-				score += (-1) * timeCosts;
-			}
-		}
-	}
-
-	static class VehicleEmploymentScoring implements SumScoringFunction.BasicScoring {
-
-		private final Carrier carrier;
-
-		public VehicleEmploymentScoring(Carrier carrier) {
-			super();
-			this.carrier = carrier;
-		}
-
-		@Override
-		public void finish() {
-
-		}
-
-		@Override
-		public double getScore() {
-			double score = 0.;
-			CarrierPlan selectedPlan = carrier.getSelectedPlan();
-			if (selectedPlan == null)
-				return 0.;
-			for (ScheduledTour tour : selectedPlan.getScheduledTours()) {
-				if (!tour.getTour().getTourElements().isEmpty()) {
-					score += (-1) * tour.getVehicle().getType().getCostInformation().getFixedCosts();
-				}
-			}
-			return score;
-		}
-
 	}
 
 	public record ServiceDurationPerCategoryKey(String employeeCategory, String vehicleType, String smallScaleCommercialTrafficType) {}
