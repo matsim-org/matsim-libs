3ddc964b0e6fa96e411570e90e6239eb4ddc573a
Ricardo Ewert <ewert@vsp.tu-berlin.de>
2025-06-17 14:37:31 +0200

    merge PopulationUtilsTest classes into one class

--- matsim/src/test/java/org/matsim/core/utils/misc/PopulationUtilsTest.java
+++ matsim/src/test/java/org/matsim/core/utils/misc/PopulationUtilsTest.java
@@ -40,6 +40,8 @@ import org.matsim.core.population.PopulationUtils;
 import org.matsim.core.population.io.PopulationReader;
 import org.matsim.core.router.TripStructureUtils.StageActivityHandling;
 import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.io.IOUtils;
+import org.matsim.examples.ExamplesUtils;
 import org.matsim.testcases.MatsimTestUtils;
 
 /**
@@ -176,4 +178,133 @@ public class PopulationUtilsTest {
 		Assertions.assertTrue(PopulationUtils.equalPopulation(s1.getPopulation(), s1.getPopulation()));
 	}
 
+	@Test
+	void testPlanAttributesCopy() {
+		final Population population = PopulationUtils.createPopulation(ConfigUtils.createConfig());
+
+		final Person person = population.getFactory().createPerson(Id.createPersonId("Donald Trump"));
+
+		final Plan plan = population.getFactory().createPlan();
+		person.addPlan(plan);
+
+		final Activity act = population.getFactory().createActivityFromCoord("speech", new Coord(0, 0));
+		plan.addActivity(act);
+
+		act.getAttributes().putAttribute("makes sense", false);
+		act.getAttributes().putAttribute("length", 1895L);
+
+		final Leg leg = population.getFactory().createLeg("SUV");
+		plan.addLeg(leg);
+		plan.addActivity(population.getFactory().createActivityFromLinkId("tweet", Id.createLinkId(2)));
+
+		leg.getAttributes().putAttribute("mpg", 0.000001d);
+
+
+		final Plan planCopy = population.getFactory().createPlan();
+		PopulationUtils.copyFromTo(plan, planCopy);
+
+		Assertions.assertEquals(plan.getPlanElements().size(),
+			planCopy.getPlanElements().size(),
+			"unexpected plan length");
+
+		final Activity activityCopy = (Activity) planCopy.getPlanElements().getFirst();
+
+		Assertions.assertEquals(act.getAttributes().getAttribute("makes sense"),
+			activityCopy.getAttributes().getAttribute("makes sense"),
+			"unexpected attribute");
+
+		Assertions.assertEquals(act.getAttributes().getAttribute("length"),
+			activityCopy.getAttributes().getAttribute("length"),
+			"unexpected attribute");
+
+		final Leg legCopy = (Leg) planCopy.getPlanElements().get(1);
+
+		Assertions.assertEquals(leg.getAttributes().getAttribute("mpg"),
+			legCopy.getAttributes().getAttribute("mpg"),
+			"unexpected attribute");
+	}
+
+	@Test
+	void testFirstActivityBeforeCar_carFirstLeg() {
+		PopulationFactory pf = getPopulationFactory();
+
+		{
+			Plan plan = pf.createPlan();
+			Activity h = pf.createActivityFromLinkId("h", Id.createLinkId("1"));
+			plan.addActivity(h);
+			plan.addLeg(pf.createLeg(TransportMode.car));
+			plan.addActivity(pf.createActivityFromLinkId("w", Id.createLinkId("2")));
+			plan.addLeg(pf.createLeg(TransportMode.pt));
+			plan.addActivity(pf.createActivityFromLinkId("h", Id.createLinkId("1")));
+
+			Activity firstActivityAfterLastCarLegOfDay = PopulationUtils.getFirstActivityOfDayBeforeDepartingWithCar(plan);
+			Assertions.assertEquals(h, firstActivityAfterLastCarLegOfDay);
+		}
+	}
+
+	@Test
+	void testFirstActivityBeforeCar_noCar() {
+		PopulationFactory pf = getPopulationFactory();
+
+		{
+			Plan plan = pf.createPlan();
+			Activity h = pf.createActivityFromLinkId("h", Id.createLinkId("1"));
+			plan.addActivity(h);
+			plan.addLeg(pf.createLeg(TransportMode.pt));
+			plan.addActivity(pf.createActivityFromLinkId("w", Id.createLinkId("2")));
+			plan.addLeg(pf.createLeg(TransportMode.pt));
+			plan.addActivity(pf.createActivityFromLinkId("h", Id.createLinkId("1")));
+
+			Activity firstActivityAfterLastCarLegOfDay = PopulationUtils.getFirstActivityOfDayBeforeDepartingWithCar(plan);
+			Assertions.assertNull(firstActivityAfterLastCarLegOfDay);
+		}
+	}
+
+	@Test
+	void testFirstActivityBeforeCar_carSecond() {
+		PopulationFactory pf = getPopulationFactory();
+
+		{
+			Plan plan = pf.createPlan();
+			plan.addActivity(pf.createActivityFromLinkId("h", Id.createLinkId("1")));
+			plan.addLeg(pf.createLeg(TransportMode.pt));
+			Activity w = pf.createActivityFromLinkId("w", Id.createLinkId("2"));
+			plan.addActivity(w);
+			plan.addLeg(pf.createLeg(TransportMode.car));
+			plan.addActivity(pf.createActivityFromLinkId("h", Id.createLinkId("1")));
+
+			Activity firstActivityAfterLastCarLegOfDay = PopulationUtils.getFirstActivityOfDayBeforeDepartingWithCar(plan);
+			Assertions.assertEquals(w, firstActivityAfterLastCarLegOfDay);
+		}
+	}
+
+	@Test
+	void testFirstActivityBeforeCar_twoCar() {
+		PopulationFactory pf = getPopulationFactory();
+
+		{
+			Plan plan = pf.createPlan();
+			Activity h = pf.createActivityFromLinkId("h", Id.createLinkId("1"));
+			plan.addActivity(h);
+			plan.addLeg(pf.createLeg(TransportMode.car));
+			plan.addActivity(pf.createActivityFromLinkId("w", Id.createLinkId("2")));
+			plan.addLeg(pf.createLeg(TransportMode.pt));
+			plan.addActivity(pf.createActivityFromLinkId("l", Id.createLinkId("3")));
+			plan.addLeg(pf.createLeg(TransportMode.car));
+			plan.addActivity(pf.createActivityFromLinkId("h", Id.createLinkId("1")));
+
+			Activity firstActivityAfterLastCarLegOfDay = PopulationUtils.getFirstActivityOfDayBeforeDepartingWithCar(plan);
+			Assertions.assertEquals(h, firstActivityAfterLastCarLegOfDay);
+		}
+	}
+
+	private static PopulationFactory getPopulationFactory() {
+		Config config = ConfigUtils.loadConfig(IOUtils.extendUrl(ExamplesUtils.getTestScenarioURL("equil"), "config_plans1.xml"));
+		Scenario scenario = ScenarioUtils.createScenario(config);
+		Population pop = scenario.getPopulation();
+		pop.removePerson(Id.createPersonId("1"));
+		Assertions.assertTrue(pop.getPersons().isEmpty());
+		PopulationFactory pf = pop.getFactory();
+		return pf;
+	}
 }
