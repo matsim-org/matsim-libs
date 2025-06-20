5fb6e3cc2423b4b9d5a546f2b2add3b187726dcb
Ricardo Ewert <ewert@vsp.tu-berlin.de>
2025-06-20 14:23:20 +0200

    add missing input

--- contribs/small-scale-traffic-generation/src/main/java/org/matsim/smallScaleCommercialTrafficGeneration/TripDistributionMatrix.java
+++ contribs/small-scale-traffic-generation/src/main/java/org/matsim/smallScaleCommercialTrafficGeneration/TripDistributionMatrix.java
@@ -49,6 +49,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 /**
  * @author Ricardo Ewert
