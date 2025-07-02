9e67ce0d04c0ef28df2ba754748865d57f98c588
Marcel <rieser@simunto.com>
2025-07-02 16:52:21 +0200

    fix(pt): Fix a bug calculating pt routes using chained departures (#4083)
    * improve debug-ability by having more informative toString methods
    
    * fix an issue with calculating routes with chained departures

--- matsim/src/main/java/ch/sbb/matsim/routing/pt/raptor/SwissRailRaptorCore.java
+++ matsim/src/main/java/ch/sbb/matsim/routing/pt/raptor/SwissRailRaptorCore.java
@@ -719,6 +719,14 @@ public class SwissRailRaptorCore {
 		boolean hasChains = chains != null;
 
 		for (int toRouteStopIndex = firstRouteStopIndex + 1; toRouteStopIndex < route.indexFirstRouteStop + route.countRouteStops; toRouteStopIndex++) {
+			// In the original raptor algorithm, we step once through all route-stops in order,
+			// which ensures that we don't handle any stop or route twice.
+			// With chained departures, we actually jump out of this sequence,
+			// resulting in the state that the same stop or route might be handled a second time in the same round.
+			// To prevent this (as it could have unintended side-effects and even produce wrong results), we
+			// proactively clear the handled route stop. So if this a chained route, it already gets handled now
+			// and will not have to be handled (regularly) later on, thus we can just clear the corresponding bits.
+			this.improvedRouteStopIndices.clear(toRouteStopIndex);
 			RRouteStop toRouteStop = this.data.routeStops[toRouteStopIndex];
 			if (!toRouteStop.routeStop.isAllowAlighting()) {
 				continue;
