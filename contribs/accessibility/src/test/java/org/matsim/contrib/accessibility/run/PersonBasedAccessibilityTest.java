f154d8a0aa0fbdcebaecb700ad125f529e37edc7
Jakob Rehmann <jakob.rehmann@gmail.com>
2025-08-29 10:53:37 +0200

    changed name

--- contribs/accessibility/src/test/java/org/matsim/contrib/accessibility/run/PersonBasedAccessibilityTest.java
+++ contribs/accessibility/src/test/java/org/matsim/contrib/accessibility/run/PersonBasedAccessibilityTest.java
@@ -35,7 +35,7 @@ public class PersonBasedAccessibilityTest {
 
 
 	@Test
-	void runFromEvents() {
+	void testAgeBasedAccessibilityForTeleportedWalk() {
 		final Config config = ConfigUtils.createConfig();
 
 		final AccessibilityConfigGroup acg = ConfigUtils.addOrGetModule(config, AccessibilityConfigGroup.class);
