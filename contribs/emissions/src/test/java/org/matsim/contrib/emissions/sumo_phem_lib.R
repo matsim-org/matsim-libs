c18b00f517a4373e225084c811e7d6d59b623cf1
apabis <aleksanderpabis@gmail.com>
2025-10-08 15:38:43 +0200

    Added a lib file for functions of regularly used patterns

---
+++ contribs/emissions/src/test/java/org/matsim/contrib/emissions/sumo_phem_lib.R
@@ -0,0 +1,125 @@
+{
+  library(tidyverse)
+  library(glue)
+
+  # ==== Paths to ressources ====
+  sumo_path <- "/Users/aleksander/Documents/VSP/PHEMTest/sumo"
+  diff_path <- "/Users/aleksander/Documents/VSP/PHEMTest/diff"
+  hbefa_path <- "/Users/aleksander/Documents/VSP/PHEMTest/hbefa"
+
+}
+
+# ==== Helper functions ====
+
+# Reads in the csv from matsim at the given path and returns the datasheet and intervals from this sheet
+# model_suffix: optional, adds a suffix to the 'model'-column which is just "MATSIM" by default
+read_matsim <- function(path, model_suffix = ""){
+  # Load data from MATSim csv
+  diff_out <- read_csv(path)
+
+  # Create summarized data frame from MATSim results
+  dataframe <- diff_out %>%
+    select(segment, "CO-MATSIM", "CO2(total)-MATSIM", "HC-MATSIM", "PM-MATSIM", "NOx-MATSIM") %>%
+    rename("CO2-MATSIM" = "CO2(total)-MATSIM", "PMx-MATSIM" = "PM-MATSIM") %>%
+    pivot_longer(cols = c("CO-MATSIM",
+                          "CO2-MATSIM",
+                          "HC-MATSIM",
+                          "PMx-MATSIM",
+                          "NOx-MATSIM"), names_to="model", values_to="value") %>%
+    separate(model, c("component", "model"), "-") %>%
+    mutate(segment = as.integer(segment), model = glue("{model}_{model_suffix}"))
+
+  # Extract the interval times from the matsim-test-file
+  intervals <- diff_out %>%
+    mutate(endTime = startTime+travelTime) %>%
+    select(segment, startTime, endTime, travelTime, lengths) %>%
+    mutate(across(
+      .cols = everything(),
+      .fns = ~ as.integer(.x)
+    ))
+
+  return (list(dataframe, intervals))
+}
+
+# Reads in the csv format from SUMO at the given paths and returns the tidied datasheet
+# model_suffix: optional, adds a suffix to the 'model'-column which is just "MATSIM" by default
+read_sumo <- function(path, intervals, model_suffix = ""){
+  d <- read_delim(path,
+                  delim = ";",
+                  col_names = c("time", "velocity", "acceleration", "slope", "CO", "CO2", "HC", "PMx", "NOx", "fuel", "electricity"),
+                  col_types = cols(
+                    time = col_integer(),
+                    velocity = col_double(),
+                    acceleration = col_double(),
+                    slope = col_double(),
+                    CO = col_double(),
+                    CO2 = col_double(),
+                    HC = col_double(),
+                    PMx = col_double(),
+                    NOx = col_double(),
+                    fuel = col_double(),
+                    electricity = col_double())) %>%
+    pivot_longer(cols = c("CO", "CO2", "HC", "PMx", "NOx"), names_to = "component", values_to="value") %>%
+    mutate(segment = cut(time, breaks = c(0, intervals$endTime), labels = FALSE, right = FALSE, include.lowest = TRUE)-as.integer(1)) %>%
+    group_by(segment, component) %>%
+    summarize(value = sum(value)) %>%
+    mutate(model = glue("SUMO_{model_suffix}"), value=value/1000)
+
+  return (d)
+}
+
+# Creates the default plot with all components
+# Additional datasets, can be attached (however, the function assumes, that it is formatted correctly)
+# Example: plot_main(extra_notes = "Test", data.TEST)
+plot_main <- function(..., fuel = "petrol", segment_method = "fixedIntervalLength_60", extra_notes = ""){
+  # Load data from MATSim
+  # diff_out <- read_csv("contribs/emissions/test/input/org/matsim/contrib/emissions/PHEMTest/diff_petrol_ref.csv")
+  r <- read_matsim(glue("{diff_path}/diff_{fuel}_output_{segment_method}.csv"), "")
+  data.MATSIM <- r[[1]]
+  intervals <- r[[2]]
+
+  # Load data from SUMO with PHEMLight and summarize for each interval
+  data.SUMO_PHEMLight <- read_sumo(glue("{sumo_path}/sumo_{fuel}_output.csv"), intervals, "PHEMLight")
+
+  # Load data from SUMO with PHEMLight5 and summarize for each interval
+  data.SUMO_PHEMLight5 <- read_sumo(glue("{sumo_path}/sumo_{fuel}_output_pl5.csv"), intervals, "PHEMLight5")
+
+  # Append all datasets together
+  data_list <- list(data.MATSIM, data.SUMO_PHEMLight, data.SUMO_PHEMLight5, ...)
+
+  # recalc: gram -> gram per kilometer
+  data <- do.call(rbind, data_list) %>%
+    merge(intervals, by="segment") %>%
+    mutate(gPkm = value/lengths)
+
+  # Generate colors (first 3 always the same)
+  colors <- c("#d21717", "#17d2a4", "#7d23cc")
+  extra <- max(0, length(data_list)-3)
+  all_colors <- c(colors, hcl.colors(extra, palette = "viridis"))
+  names(all_colors) <- c("MATSIM_", "SUMO_PHEMLight", "SUMO_PHEMLight5", unlist(lapply(list(...), function(df) unique(df$model))))
+
+  # Bar-Plot
+  ggplot(data) +
+  geom_bar(aes(x=segment, y=gPkm, fill=model), stat="identity", position="dodge") +
+  scale_fill_manual(values=all_colors) +
+  facet_wrap(~component, scales="free") +
+  ylab("emissions in g/km") +
+  theme(text = element_text(size=18)) +
+  #geom_rect(data=min_max_vals_used, aes(xmin=0, xmax=3, ymin=min, ymax=max, fill=table), alpha=0.2) +
+  ggtitle(glue("Comparison across WLTP-cycle for {fuel}")) +
+  theme_minimal()
+
+  # Line-Plot (for scenarios with more links)
+  ggplot(data) +
+  geom_line(aes(x=startTime, y=gPkm, color=model), size=12/nrow(intervals)) +
+  geom_point(aes(x=startTime, y=gPkm, color=model), size=6/nrow(intervals)) +
+  scale_color_manual(values=all_colors) +
+  facet_wrap(~component, scales="free") +
+  ylab("emissions in g/km") +
+  theme(text = element_text(size=18)) +
+  ggtitle(glue("Comparison across WLTP-cycle for {fuel}"),
+          subtitle=glue("{segment_method} {extra_notes}")) +
+  theme_minimal()
+
+}
+
