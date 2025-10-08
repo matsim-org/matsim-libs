10405b0018ca9ee89e93a1d7d64240d6994e6c70
Nico Kühnel <nico.kuehnel@moia.io>
2025-10-08 10:46:42 +0200

    make drt constrains relative again (#4327)

--- contribs/drt/src/main/java/org/matsim/contrib/drt/passenger/DefaultOfferAcceptor.java
+++ contribs/drt/src/main/java/org/matsim/contrib/drt/passenger/DefaultOfferAcceptor.java
@@ -32,7 +32,7 @@ public class DefaultOfferAcceptor implements DrtOfferAcceptor{
 			.request(request)
 			.earliestStartTime(request.getEarliestStartTime())
 			.maxRideDuration(request.getConstraints().maxRideDuration())
-			.latestArrivalTime(request.getConstraints().latestArrivalTime())
+			.latestArrivalTime(request.getLatestArrivalTime())
 			.latestStartTime(updatedLatestStartTime)
 			.dropoffDuration(dropoffDuration)
 			.plannedPickupTime(departureTime)
