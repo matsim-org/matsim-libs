1e433e9d1c9f2fc0194cd506e2e83d0d8176be9c
sime94 <meinhardt@vsp.tu-berlin.de>
2025-10-27 17:36:44 +0100

    add new dashboard after removal of old dashboard

--- contribs/simwrapper/src/main/java/org/matsim/simwrapper/SimWrapper.java
+++ contribs/simwrapper/src/main/java/org/matsim/simwrapper/SimWrapper.java
@@ -120,10 +120,11 @@ public final class SimWrapper {
 		return dashboards.stream().anyMatch(o -> d.isAssignableFrom(o.getClass()) && Objects.equals(o.context(), context));
 	}
 
-	public void replaceDashboard(Class<? extends Dashboard> oldDashboard, Class<? extends Dashboard> newDashboard, String context) {
-		if (hasDashboard(oldDashboard, context)) {
+	public void replaceDashboard(Dashboard oldDashboard, Dashboard newDashboard, String context) {
+		if (hasDashboard(oldDashboard.getClass(), context)) {
 			log.info("Replacing dashboard {} with context {} by dashboard {}", oldDashboard, context, newDashboard);
 			dashboards.remove(oldDashboard);
+			dashboards.add(newDashboard);
 		} else {
 			log.info("Dashboard {} with context {} not found in list of dashboards. Not replacing anything.", oldDashboard, context);
 		}
