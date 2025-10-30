99d02f7abf05b2a6f0cd8c648da8e9de766f72e9
Christian Rakow <rakow@simunto.com>
2025-10-30 17:22:34 +0100

    add a test for minimum stop duration and rerouting

--- contribs/railsim/src/test/java/ch/sbb/matsim/contrib/railsim/integration/RailsimMinimumStopDurationTest.java
+++ contribs/railsim/src/test/java/ch/sbb/matsim/contrib/railsim/integration/RailsimMinimumStopDurationTest.java
@@ -1,8 +1,13 @@
 package ch.sbb.matsim.contrib.railsim.integration;
 
 import java.io.File;
+import java.util.function.Consumer;
 
 import org.junit.jupiter.api.Test;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.pt.transitSchedule.api.TransitLine;
+import org.matsim.pt.transitSchedule.api.TransitRoute;
+import org.matsim.pt.transitSchedule.api.TransitRouteStop;
 
 public class RailsimMinimumStopDurationTest extends AbstractIntegrationTest {
 
@@ -32,6 +37,26 @@ public class RailsimMinimumStopDurationTest extends AbstractIntegrationTest {
 		assertThat(result)
 			.allTrainsHaveNumberOfStops(4)
 			.allStopTimesExceptFirstAndLastSatisfy(t -> t.getStopDuration() >= 60);
+
+	}
+
+	@Test
+	void reroute() {
+
+		Consumer<Scenario> setup = scenario -> {
+
+			for (TransitLine line : scenario.getTransitSchedule().getTransitLines().values()) {
+				for (TransitRoute route : line.getRoutes().values()) {
+					TransitRouteStop middleStop = route.getStops().get(1);
+					middleStop.setMinimumStopDuration(10);
+				}
+			}
+		};
+
+		SimulationResult result = runSimulation(new File(utils.getPackageInputDirectory(), "microStationRerouting"), setup);
+
+		assertThat(result)
+			.allStopTimesExceptFirstAndLastSatisfy(t -> t.getStopDuration() >= 10);
 
 	}
 }
