a527f9d8ae07eabd1c987a8486587f36ddd3811f
Kai Nagel <nagel@vsp.tu-berlin.de>
2025-11-02 23:01:01 +0100

    finalize the regression test

--- contribs/application/src/test/java/org/matsim/application/analysis/population/ExperiencedPlansWriterTest.java
+++ contribs/application/src/test/java/org/matsim/application/analysis/population/ExperiencedPlansWriterTest.java
@@ -1,7 +1,10 @@
 package org.matsim.application.analysis.population;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.matsim.core.population.PopulationUtils;
+import org.matsim.core.population.routes.PopulationComparison;
 import org.matsim.testcases.MatsimTestUtils;
 
 class ExperiencedPlansWriterTest{
@@ -14,7 +17,15 @@ class ExperiencedPlansWriterTest{
 			"--runId", "test",
 			"--threads", "1"
 		};
+
+		// yy the way ExperiencedPlansWriter is set up, it is not possible to have the output in a directory different from the input directory. kai, nov'25
+
 		ExperiencedPlansWriter.main( args );
+
+		String expected = utils.getInputDirectory() + "/test.reference_experienced_plans.xml.gz";
+		String actual = utils.getInputDirectory() + "/test.output_experienced_plans.xml.gz";
+		PopulationComparison.Result result = PopulationUtils.comparePopulations( expected, actual );
+		Assertions.assertTrue( result== PopulationComparison.Result.equal );
 	}
 
 }
