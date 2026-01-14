5f6bd6df42113ff6e242b78a4ab815a406a5a2ea
apabis <aleksanderpabis@gmail.com>
2026-01-14 17:22:11 +0100

    Segment A,B,C comparison

--- contribs/emissions/src/test/java/org/matsim/contrib/emissions/pretoria_test.R
+++ contribs/emissions/src/test/java/org/matsim/contrib/emissions/pretoria_test.R
@@ -6,11 +6,11 @@
   pretoria_path <- "/Users/aleksander/Documents/VSP/PHEMTest/Pretoria"
 }
 
-# ==== Pretoria Analysis ====
+# ==== General Pretoria Analysis ====
 {
   vehicle <- "FIGO"
 
-  pretoria_output <- read_csv(glue("/Users/aleksander/Documents/VSP/PHEMTest/Pretoria/output_{vehicle}.csv")) %>%
+  pretoria_output <- read_csv(glue("{pretoria_path}/output_{vehicle}.csv")) %>%
     filter(linkId != 6555) %>%
     mutate(n = row_number())
 
@@ -22,14 +22,46 @@
     group_by() %>%
     summarize(CO_MATSim = mean(CO_MATSim), CO_pems = mean(CO_pems),
               CO2_MATSim = mean(CO2_MATSim), CO2_pems = mean(CO2_pems),
-              NOx_MATSim = mean(NOx_MATSim), NOx_pems = mean(NOx_pems))
+              NOx_MATSim = mean(NOx_MATSim), NOx_pems = mean(NOx_pems)) %>%
+    mutate(CO_diff = 100*CO_MATSim/CO_pems-100,
+           CO2_diff = 100*CO2_MATSim/CO2_pems-100,
+           NOx_diff = 100*NOx_MATSim/NOx_pems-100)
 
   # ggplot(pretoria_output) +
   #   geom_line(aes(x=n, y=CO_pems), color = "orange") +
   #   geom_line(aes(x=n, y=CO_MATSim), color = "red")
 
-  100*sum(pretoria_output$CO_MATSim)/sum(pretoria_output$CO_pems)-100
-  100*sum(pretoria_output$CO2_MATSim)/sum(pretoria_output$CO2_pems)-100
-  100*sum(pretoria_output$NOx_MATSim)/sum(pretoria_output$NOx_pems)-100
 
+}
+
+# ==== Accumulative Plots ===
+{
+  #TODO
+}
+
+# ==== Segment Wise Pretoria Analysis ====
+{
+  # In the JWJ Paper, 3 segments were defined
+  # A: Urban (from 28948 to 14100)
+  # B: Freeway (from 11614 to 28906)
+  # C: Steep, suburban (from waterkloof4_waterkloof5 to 37156)
+
+  vehicle <- "FIGO"
+
+  pretoria_output <- read_csv(glue("/Users/aleksander/Documents/VSP/PHEMTest/Pretoria/output_{vehicle}.csv")) %>%
+    filter(linkId != 6555, segment != "none") %>%
+    mutate(n = row_number())
+
+  pretoria_segment_avg <- pretoria_output %>%
+    group_by(tripId, segment) %>%
+    summarize(CO_MATSim = sum(CO_MATSim), CO_pems = sum(CO_pems),
+              CO2_MATSim = sum(CO2_MATSim), CO2_pems = sum(CO2_pems),
+              NOx_MATSim = sum(NOx_MATSim), NOx_pems = sum(NOx_pems)) %>%
+    group_by(segment) %>%
+    summarize(CO_MATSim = mean(CO_MATSim), CO_pems = mean(CO_pems),
+              CO2_MATSim = mean(CO2_MATSim), CO2_pems = mean(CO2_pems),
+              NOx_MATSim = mean(NOx_MATSim), NOx_pems = mean(NOx_pems)) %>%
+    mutate(CO_diff = 100*CO_MATSim/CO_pems-100,
+           CO2_diff = 100*CO2_MATSim/CO2_pems-100,
+           NOx_diff = 100*NOx_MATSim/NOx_pems-100)
 }
