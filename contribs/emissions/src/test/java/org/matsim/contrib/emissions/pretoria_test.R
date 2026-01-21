9d09adf17d7c9303128077431f05da5e9d5d4275
apabis <aleksanderpabis@gmail.com>
2026-01-21 14:46:03 +0100

    Added driver analysis and PHEM C-Test code

--- contribs/emissions/src/test/java/org/matsim/contrib/emissions/pretoria_test.R
+++ contribs/emissions/src/test/java/org/matsim/contrib/emissions/pretoria_test.R
@@ -15,11 +15,11 @@
     mutate(n = row_number())
 
   pretoria_avg <- pretoria_output %>%
-    group_by(tripId) %>%
+    group_by(tripId, load) %>%
     summarize(CO_MATSim = sum(CO_MATSim), CO_pems = sum(CO_pems),
               CO2_MATSim = sum(CO2_MATSim), CO2_pems = sum(CO2_pems),
               NOx_MATSim = sum(NOx_MATSim), NOx_pems = sum(NOx_pems)) %>%
-    group_by() %>%
+    group_by(load) %>%
     summarize(CO_MATSim = mean(CO_MATSim), CO_pems = mean(CO_pems),
               CO2_MATSim = mean(CO2_MATSim), CO2_pems = mean(CO2_pems),
               NOx_MATSim = mean(NOx_MATSim), NOx_pems = mean(NOx_pems)) %>%
@@ -34,9 +34,47 @@
 
 }
 
-# ==== Accumulative Plots ===
+# ==== Driving Style ====
 {
-  #TODO
+  vehicle <- "FIGO"
+
+  gps_input <- read_csv(glue("{pretoria_path}/data/public-{vehicle}.csv")) %>%
+    mutate(gps_acc = lead(gps_speed) - gps_speed, veh_acc = lead(speed_vehicle) - speed_vehicle)
+
+  gps_avg <- gps_input %>%
+    group_by(driver) %>%
+    summarize(veh_vel_avg = mean(speed_vehicle, na.rm = TRUE),
+              veh_acc_avg = mean(veh_acc[veh_acc > 0], na.rm = TRUE),
+              gps_vel_avg = mean(gps_speed, na.rm = TRUE),
+              gps_acc_avg = mean(gps_acc[gps_acc > 0], na.rm = TRUE))
+
+}
+
+# == Driver Wise Pretoria Analysis ===
+{
+  # Three different drivers were participating:
+  # 1: Matches the profile of an aggressive driver  (avg.vel.=40.53; avg.pos.acc.=2.59)
+  # 2: Matches the profile of a moderate driver     (avg.vel.=40.19; avg.pos.acc.=2.38)
+  # 3: Matches the profile of a passive driver      (avg.vel.=37.02; avg.pos.acc.=2.17)
+
+  vehicle <- "FIGO"
+
+  pretoria_output <- read_csv(glue("{pretoria_path}/output_{vehicle}.csv")) %>%
+    filter(linkId != 6555) %>%
+    mutate(n = row_number())
+
+  pretoria_driver_avg <- pretoria_output %>%
+    group_by(tripId, load, driver) %>%
+    summarize(CO_MATSim = sum(CO_MATSim), CO_pems = sum(CO_pems),
+              CO2_MATSim = sum(CO2_MATSim), CO2_pems = sum(CO2_pems),
+              NOx_MATSim = sum(NOx_MATSim), NOx_pems = sum(NOx_pems)) %>%
+    group_by(load, driver) %>%
+    summarize(CO_MATSim = mean(CO_MATSim), CO_pems = mean(CO_pems),
+              CO2_MATSim = mean(CO2_MATSim), CO2_pems = mean(CO2_pems),
+              NOx_MATSim = mean(NOx_MATSim), NOx_pems = mean(NOx_pems)) %>%
+    mutate(CO_diff = 100*CO_MATSim/CO_pems-100,
+           CO2_diff = 100*CO2_MATSim/CO2_pems-100,
+           NOx_diff = 100*NOx_MATSim/NOx_pems-100)
 }
 
 # ==== Segment Wise Pretoria Analysis ====
@@ -53,11 +91,11 @@
     mutate(n = row_number())
 
   pretoria_segment_avg <- pretoria_output %>%
-    group_by(tripId, segment) %>%
+    group_by(tripId, load, segment) %>%
     summarize(CO_MATSim = sum(CO_MATSim), CO_pems = sum(CO_pems),
               CO2_MATSim = sum(CO2_MATSim), CO2_pems = sum(CO2_pems),
               NOx_MATSim = sum(NOx_MATSim), NOx_pems = sum(NOx_pems)) %>%
-    group_by(segment) %>%
+    group_by(load, segment) %>%
     summarize(CO_MATSim = mean(CO_MATSim), CO_pems = mean(CO_pems),
               CO2_MATSim = mean(CO2_MATSim), CO2_pems = mean(CO2_pems),
               NOx_MATSim = mean(NOx_MATSim), NOx_pems = mean(NOx_pems)) %>%
@@ -65,3 +103,60 @@
            CO2_diff = 100*CO2_MATSim/CO2_pems-100,
            NOx_diff = 100*NOx_MATSim/NOx_pems-100)
 }
+
+# ==== Accumulative Plots ====
+{
+  #TODO
+}
+
+# ==== PHEM C-Route Test Preparation ====
+{
+  vehicle <- "FIGO"
+
+  cRoute <- read_csv(glue("{pretoria_path}/data/public-{vehicle}.csv")) %>%
+    mutate(acc = lead(speed_vehicle) - speed_vehicle, n = row_number()) %>%
+    mutate(acc = ifelse(is.na(acc), 0, acc)) %>%
+    select(n, speed_vehicle, acc, trip)
+
+  write_delim(cRoute, glue("{pretoria_path}/phem-in/phem-in-{vehicle}.csv"), delim=";", col_names=FALSE)
+}
+
+# ==== PHEM C-Route Test Analysis ====
+{
+  vehicle <- "FIGO"
+
+  phem_avg <- read_delim(glue("{pretoria_path}/phem-out/phem-out-{vehicle}.csv"), delim = ";", col_names = c("n", "velocity", "acceleration", "slope", "CO", "CO2", "HC", "PMx", "NOx", "fuel", "electricity")) %>%
+    merge(cRoute, by="n") %>%
+    group_by(trip) %>%
+    summarize(CO = sum(CO)/1000, CO2 = sum(CO2)/1000, NOx = sum(NOx)/1000) %>%
+    group_by() %>%
+    summarize(CO_phem = mean(CO), CO2_phem = mean(CO2), NOx_phem = mean(NOx))
+
+
+  pretoria_avg <- read_csv(glue("{pretoria_path}/output_{vehicle}.csv")) %>%
+    filter(linkId != 6555) %>%
+    group_by(tripId) %>%
+    summarize(CO_MATSim = sum(CO_MATSim), CO_pems = sum(CO_pems),
+              CO2_MATSim = sum(CO2_MATSim), CO2_pems = sum(CO2_pems),
+              NOx_MATSim = sum(NOx_MATSim), NOx_pems = sum(NOx_pems)) %>%
+    summarize(CO_MATSim = mean(CO_MATSim), CO_pems = mean(CO_pems),
+              CO2_MATSim = mean(CO2_MATSim), CO2_pems = mean(CO2_pems),
+              NOx_MATSim = mean(NOx_MATSim), NOx_pems = mean(NOx_pems))
+
+  all_avg <- merge(phem_avg, pretoria_avg) %>%
+    pivot_longer(cols = c("CO_phem",
+                          "CO2_phem",
+                          "NOx_phem",
+                          "CO_MATSim",
+                          "CO2_MATSim",
+                          "NOx_MATSim",
+                          "CO_pems",
+                          "CO2_pems",
+                          "NOx_pems"), names_to="model", values_to="value") %>%
+    separate(model, c("component", "model"), "_")
+
+  ggplot(all_avg) +
+    geom_bar(aes(x=model, y=value, fill=model), stat="identity") +
+    facet_wrap(~component, scales="free")
+
+}^
