3a3ad35fda981da031b5d561ff322aefabc97224
Sebastian Hörl <hoerl.sebastian@gmail.com>
2026-01-24 11:24:01 +0100

    make threads configurable

--- matsim/src/main/java/org/matsim/core/router/speedy/SpeedyALTFactory.java
+++ matsim/src/main/java/org/matsim/core/router/speedy/SpeedyALTFactory.java
@@ -1,10 +1,14 @@
 package org.matsim.core.router.speedy;
 
 import org.matsim.api.core.v01.network.Network;
+import org.matsim.core.config.groups.GlobalConfigGroup;
 import org.matsim.core.router.util.LeastCostPathCalculator;
 import org.matsim.core.router.util.LeastCostPathCalculatorFactory;
 import org.matsim.core.router.util.TravelDisutility;
 import org.matsim.core.router.util.TravelTime;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -13,10 +17,25 @@ import java.util.concurrent.ConcurrentHashMap;
  * @author mrieser / Simunto, sponsored by SBB Swiss Federal Railways
  * @author sebhoerl, IRT SystemX
  */
+@Singleton
 public class SpeedyALTFactory implements LeastCostPathCalculatorFactory {
+	private final int threads;
 
 	private final Map<Network, SpeedyGraph> graphs = new ConcurrentHashMap<>();
 	private final Map<SpeedyGraph, SpeedyALTData> landmarksData = new ConcurrentHashMap<>();
+
+	@Inject
+	public SpeedyALTFactory(final GlobalConfigGroup globalConfigGroup) {
+		this(globalConfigGroup.getNumberOfThreads());
+	}
+
+	public SpeedyALTFactory(int threads) {
+		this.threads = threads;
+	}
+
+	public SpeedyALTFactory() {
+		this(4);
+	}
 
 	@Override
 	public LeastCostPathCalculator createPathCalculator(Network network, TravelDisutility travelCosts, TravelTime travelTimes) {
@@ -24,7 +43,7 @@ public class SpeedyALTFactory implements LeastCostPathCalculatorFactory {
 		
 		SpeedyALTData landmarks = landmarksData.computeIfAbsent(graph, g -> {
 			int landmarksCount = Math.min(16, g.nodeCount);
-			return new SpeedyALTData(g, landmarksCount, travelCosts);
+			return new SpeedyALTData(g, landmarksCount, travelCosts, threads);
 		});
 		
 		return new SpeedyALT(landmarks, travelTimes, travelCosts);
