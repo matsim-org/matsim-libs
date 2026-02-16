57457cb689e84f895004505c559ac244d72eb3f1
apabis <aleksanderpabis@gmail.com>
2026-02-16 09:19:47 +0100

    Added EmissionMethodComparisonTest and new plots

---
+++ contribs/emissions/src/test/java/org/matsim/contrib/emissions/EmissionMethodComparisonTest.java
@@ -0,0 +1,145 @@
+package org.matsim.contrib.emissions;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.params.provider.Arguments;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.contrib.emissions.utils.EmissionsConfigGroup;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.events.EventsUtils;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.collections.Tuple;
+import org.matsim.core.utils.io.IOUtils;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Stream;
+
+public class EmissionMethodComparisonTest {
+
+	private final static String HBEFA_4_1_PATH = "/Users/aleksander/Documents/VSP/PHEMTest/hbefa/";
+	private final static String HBEFA_HOT_AVG = HBEFA_4_1_PATH + "EFA_HOT_Vehcat_2020_Average.csv";
+	private final static String HBEFA_COLD_AVG = HBEFA_4_1_PATH + "EFA_ColdStart_Vehcat_2020_Average.csv";
+	private final static String HBEFA_HOT_DET = HBEFA_4_1_PATH + "EFA_HOT_Subsegm_detailed_Car_Aleks_filtered.csv";
+	private final static String HBEFA_COLD_DET = HBEFA_4_1_PATH + "EFA_ColdStart_Concept_2020_detailed_perTechAverage.csv";
+
+	private static EmissionModulePack modulePack;
+
+	private static EmissionsConfigGroup getEmissionsConfigGroup(EmissionsConfigGroup.EmissionsComputationMethod method) {
+		EmissionsConfigGroup ecg = new EmissionsConfigGroup();
+		ecg.setHbefaVehicleDescriptionSource( EmissionsConfigGroup.HbefaVehicleDescriptionSource.usingVehicleTypeId );
+		ecg.setEmissionsComputationMethod( method );
+		ecg.setDetailedVsAverageLookupBehavior( EmissionsConfigGroup.DetailedVsAverageLookupBehavior.onlyTryDetailedElseAbort );
+		ecg.setDuplicateSubsegments( EmissionsConfigGroup.DuplicateSubsegments.useFirstDuplicate );
+		ecg.setAverageWarmEmissionFactorsFile(HBEFA_HOT_AVG);
+		ecg.setAverageColdEmissionFactorsFile(HBEFA_COLD_AVG);
+		ecg.setDetailedWarmEmissionFactorsFile(HBEFA_HOT_DET);
+		ecg.setDetailedColdEmissionFactorsFile(HBEFA_COLD_DET);
+		return ecg;
+	}
+
+	private static Tuple<HbefaVehicleCategory, HbefaVehicleAttributes> getVehicleAttributesTuple(String technology, String emConcept) {
+		HbefaVehicleAttributes vehicleAttributes = new HbefaVehicleAttributes();
+		vehicleAttributes.setHbefaTechnology(technology);
+		vehicleAttributes.setHbefaEmConcept(emConcept);
+		vehicleAttributes.setHbefaSizeClass("average");
+		Tuple<HbefaVehicleCategory, HbefaVehicleAttributes> vehHbefaInfo = new Tuple<>(
+			HbefaVehicleCategory.PASSENGER_CAR,
+			vehicleAttributes);
+		return vehHbefaInfo;
+	}
+
+	private static class EmissionModulePack {
+		private final Map<EmissionsConfigGroup.EmissionsComputationMethod, EmissionModule> emissionMethod2emissionModule;
+
+		EmissionModulePack() {
+			List<EmissionsConfigGroup> emissionsConfigGroups = new ArrayList<>();
+			List<Config> configs = new ArrayList<>();
+			List<Scenario> scenarios = new ArrayList<>();
+			List<EventsManager> eventsManagers = new ArrayList<>();
+			emissionMethod2emissionModule = new HashMap<>();
+
+			for(var method : EmissionsConfigGroup.EmissionsComputationMethod.values()){
+				emissionsConfigGroups.add(getEmissionsConfigGroup(method));
+				configs.add(ConfigUtils.createConfig(emissionsConfigGroups.getLast()));
+				scenarios.add(ScenarioUtils.loadScenario(configs.getLast()));
+				eventsManagers.add(EventsUtils.createEventsManager(configs.getLast()));
+				emissionMethod2emissionModule.put(method, new EmissionModule(scenarios.getLast(), eventsManagers.getLast()));
+			}
+		}
+
+		public EmissionModule getModule(EmissionsConfigGroup.EmissionsComputationMethod method){
+			return emissionMethod2emissionModule.get(method);
+		}
+	}
+
+	@BeforeAll
+	public static void prepare(){
+		modulePack = new EmissionModulePack();
+	}
+
+	@TestFactory
+	Collection<DynamicTest> curveFactory(){
+		List<String> roadTypes = List.of(
+			"URB/Local/50",
+			"URB/Local/60",
+			"RUR/MW/100",
+			"RUR/MW/130",
+			"RUR/MW/>130"
+		);
+
+		List<Tuple<HbefaVehicleCategory, HbefaVehicleAttributes>> vehHbefaInfo = List.of(
+			getVehicleAttributesTuple("petrol (4S)", "PC P Euro-4"),
+			getVehicleAttributesTuple("petrol (4S)", "PC P Euro-6"),
+			getVehicleAttributesTuple("diesel", "PC D Euro-4"),
+			getVehicleAttributesTuple("diesel", "PC D Euro-6")
+		);
+
+		return roadTypes.stream().flatMap(r ->
+			vehHbefaInfo.stream().map(v ->
+				DynamicTest.dynamicTest(r + "; " + v, () -> curves(r, v))
+		)).toList();
+	}
+
+	void curves(String roadType, Tuple<HbefaVehicleCategory, HbefaVehicleAttributes> vehHbefaInfo) throws IOException {
+		String filename = roadType.replace("/", "_") + "_" + vehHbefaInfo.getSecond().getHbefaEmConcept().replace(" ", "_");
+		CSVPrinter writer = new CSVPrinter(IOUtils.getBufferedWriter("/Users/aleksander/Documents/VSP/PHEMTest/Pretoria/PAPER/InterpolationCurves/" + filename + ".csv"), CSVFormat.DEFAULT);
+
+		writer.print("vel");
+
+		for (var method : EmissionsConfigGroup.EmissionsComputationMethod.values()) {
+			for (var component : List.of("CO", "CO2", "NOx")){
+				writer.print(component + "_" + method);
+			}
+		}
+
+		writer.println();
+
+		for(double v = 0.05; v < 140; v += 0.05){
+			writer.print(v);
+			for (var method : EmissionsConfigGroup.EmissionsComputationMethod.values()) {
+				var values = modulePack.getModule(method).getWarmEmissionAnalysisModule().calculateWarmEmissions(
+					3600 / v,
+					roadType,
+					140,
+					1000,
+					vehHbefaInfo
+				);
+
+				writer.print(values.get(Pollutant.CO));
+				writer.print(values.get(Pollutant.CO2_TOTAL));
+				writer.print(values.get(Pollutant.NOx));
+			}
+			writer.println();
+		}
+
+		writer.flush();
+		writer.close();
+	}
+}
--- contribs/emissions/src/test/java/org/matsim/contrib/emissions/paper.R
+++ contribs/emissions/src/test/java/org/matsim/contrib/emissions/paper.R
@@ -78,4 +78,189 @@
          dpi = 300)
 }
 
+# Plots New Model (Int. Petrol)
+{
+  # Load data from MATSim
+  # diff_out <- read_csv("contribs/emissions/test/input/org/matsim/contrib/emissions/PHEMTest/diff_petrol_ref.csv")
+  r <- read_matsim(glue("{data_path}/ExplorativeAnalysis/ImprovedModelResults/diff_WLTP_petrol_output_useFirstDuplicate_fromLinkAttributes_0.csv"), "")
+  data.MATSIM <- r[[1]]
+  intervals <- r[[2]]
 
+  # Load data from SUMO with PHEMLight5 and summarize for each interval
+  data.SUMO_PHEMLight5 <- read_sumo(glue("{data_path}/ExplorativeAnalysis/sumo_petrol_a_output_pl5.csv"), intervals, "PHEMLight5") %>%
+    mutate(model = "PHEMLightV5")
+
+  # recalc: gram -> gram per kilometer
+  data <- rbind(data.MATSIM, data.SUMO_PHEMLight5) %>%
+    merge(intervals, by="segment") %>%
+    mutate(gPkm = value/lengths)
+
+  # Generate colors
+  colors <- c("#d21717", "#17d2a4")
+
+  # Bar-Plot
+  ggplot(data) +
+    geom_bar(aes(x=segment, y=gPkm, fill=model), stat="identity", position="dodge") +
+    scale_fill_manual(values=colors) +
+    facet_wrap(~component, scales="free") +
+    ylab("emissions in g/km") +
+    theme_minimal() +
+    ggtitle("Comparison across WLTP-cycle for petrol") +
+    theme(text = element_text(size=12))
+
+  ggsave("/Users/aleksander/Documents/VSP/PHEMTest/Pretoria/PAPER/ExplorativeAnalysis/ImprovedModelResultsPetrol.png",
+         width = 16,
+         height = 9,
+         dpi = 300)
+}
+
+# Plots New Model (Int. Diesel)
+{
+  # Load data from MATSim
+  # diff_out <- read_csv("contribs/emissions/test/input/org/matsim/contrib/emissions/PHEMTest/diff_petrol_ref.csv")
+  r <- read_matsim(glue("{data_path}/ExplorativeAnalysis/ImprovedModelResults/diff_WLTP_diesel_output_useFirstDuplicate_fromLinkAttributes_0.csv"), "")
+  data.MATSIM <- r[[1]]
+  intervals <- r[[2]]
+
+  # Load data from SUMO with PHEMLight5 and summarize for each interval
+  data.SUMO_PHEMLight5 <- read_sumo(glue("{data_path}/ExplorativeAnalysis/sumo_diesel_a_output_pl5.csv"), intervals, "") %>%
+    mutate(model = "PHEMLightV5")
+
+  # recalc: gram -> gram per kilometer
+  data <- rbind(data.MATSIM, data.SUMO_PHEMLight5) %>%
+    merge(intervals, by="segment") %>%
+    mutate(gPkm = value/lengths)
+
+  # Generate colors
+  colors <- c("#d21717", "#17d2a4")
+
+  # Bar-Plot
+  ggplot(data) +
+    geom_bar(aes(x=segment, y=gPkm, fill=model), stat="identity", position="dodge") +
+    scale_fill_manual(values=colors) +
+    facet_wrap(~component, scales="free") +
+    ylab("emissions in g/km") +
+    theme_minimal() +
+    ggtitle("Comparison across WLTP-cycle for diesel") +
+    theme(text = element_text(size=12))
+
+  ggsave("/Users/aleksander/Documents/VSP/PHEMTest/Pretoria/PAPER/ExplorativeAnalysis/ImprovedModelResultsDiesel.png",
+         width = 16,
+         height = 9,
+         dpi = 300)
+}
+
+# Plot CO emission keys and values
+{
+  hbefa_det <- read_delim("/Users/aleksander/Documents/VSP/PHEMTest/hbefa/EFA_HOT_Subsegm_detailed_Car_Aleks_filtered.csv", delim = ";")
+
+  hbefa_det_split <- hbefa_det %>%
+    separate_wider_delim(TrafficSit, "/", names=c("Region", "RoadType", "Freespeed", "TrafficSituation"))
+
+  tech <- "diesel"
+  concept <- "PC D Euro-4"
+  component <- "CO"
+
+  d <- hbefa_det_split %>%
+    mutate(Freespeed = as.numeric(Freespeed)) %>%
+    filter(VehCat == "pass. car" &
+             Technology == tech &
+             EmConcept == concept &
+             Component == component &
+             # Freespeed < 75 &
+             Subsegment == "PC diesel Euro-4 (DPF)")
+
+  colors <- c("#17d2a4", "#70aef4", "#88e72f", "#f1e843", "#eb4949", "#eb49ad")
+
+  ggplot(d) +
+    geom_line(aes(x=as.numeric(Freespeed), y=EFA, color=TrafficSituation)) +
+    # geom_point(aes(x=as.numeric(Freespeed), y=EFA, color=TrafficSituation)) +
+    facet_wrap(Region~RoadType, scales = "free") +
+    theme_minimal() +
+    theme(text = element_text(size=12)) +
+    scale_color_manual(values=colors) +
+    # scale_x_continuous(breaks = seq(0, max(as.numeric(d$Freespeed)), by = 10)) +
+    geom_hline(aes(yintercept = 0.042, color=" PLV5"), linetype="dashed") +
+    labs(title=glue("Emissions for all HBEFA keys with: {tech}, {concept}, {component}")) +
+    xlab("Speed (km/h)") +
+    ylab("Emissions (g/km)")
+
+  ggsave("/Users/aleksander/Documents/VSP/PHEMTest/Pretoria/PAPER/HBEFA_Diesel_EURO4_CO.png",
+         width = 11,
+         height = 10,
+         dpi = 300)
+}
+
+# Plot NOx emission keys and values
+{
+  hbefa_det <- read_delim("/Users/aleksander/Documents/VSP/PHEMTest/hbefa/EFA_HOT_Subsegm_detailed_Car_Aleks_filtered.csv", delim = ";")
+
+  hbefa_det_split <- hbefa_det %>%
+    separate_wider_delim(TrafficSit, "/", names=c("Region", "RoadType", "Freespeed", "TrafficSituation"))
+
+  tech <- "petrol (4S)"
+  concept <- "PC P Euro-4"
+  component <- "NOx"
+
+  d <- hbefa_det_split %>%
+    mutate(Freespeed = as.numeric(Freespeed)) %>%
+    filter(VehCat == "pass. car" &
+             Technology == tech &
+             EmConcept == concept &
+             Component == component)
+             # Freespeed < 75 &)
+
+  colors <- c("#17d2a4", "#177bd2", "#1720d2", "#8146e5", "#70aef4", "#88e72f", "#f1e843", "#eb4949", "#eb49ad")
+
+  ggplot(d) +
+    geom_line(aes(x=as.numeric(Freespeed), y=EFA, color=TrafficSituation)) +
+    # geom_point(aes(x=as.numeric(Freespeed), y=EFA, color=TrafficSituation)) +
+    facet_wrap(Region~RoadType, scales = "free") +
+    theme_minimal() +
+    theme(text = element_text(size=12)) +
+    scale_color_manual(values=colors) +
+    # scale_x_continuous(breaks = seq(0, max(as.numeric(d$Freespeed)), by = 10)) +
+    geom_hline(aes(yintercept = 0.057 , color=" PLV5 (Low)"), linetype="dashed") +
+    geom_hline(aes(yintercept = 0.049, color=" PLV5 (Medium)"), linetype="dashed") +
+    geom_hline(aes(yintercept = 0.044, color=" PLV5 (High)"), linetype="dashed") +
+    geom_hline(aes(yintercept = 0.070, color=" PLV5 (Extra High)"), linetype="dashed") +
+    labs(title=glue("Emissions for all HBEFA keys with: {tech}, {concept}, {component}")) +
+    xlab("Speed (km/h)") +
+    ylab("Emissions (g/km)")
+
+  ggsave("/Users/aleksander/Documents/VSP/PHEMTest/Pretoria/PAPER/HBEFA_Petrol_EURO4_NOx.png",
+         width = 11,
+         height = 10,
+         dpi = 300)
+}
+
+# Plot interpolation curves for methods
+{
+  trafficSit <- "RUR/MW/100"
+  emConcept <- "PC P Euro-4"
+  trafficSit.u <- gsub( "/", "_", trafficSit)
+  emConcept.u <- gsub( " ", "_", emConcept)
+
+  hbefa_det <- read_delim("/Users/aleksander/Documents/VSP/PHEMTest/hbefa/EFA_HOT_Subsegm_detailed_Car_Aleks_filtered.csv", delim = ";") %>%
+    filter(Component == "CO" | Component == "CO2(total)" | Component == "NOx") %>%
+    filter(EmConcept == emConcept) %>%
+    filter(startsWith(TrafficSit, trafficSit)) %>%
+    mutate(component = ifelse(Component == "CO2(total)", "CO2", Component))
+
+  curves <- read_csv(glue("/Users/aleksander/Documents/VSP/PHEMTest/Pretoria/PAPER/InterpolationCurves/{trafficSit.u}_{emConcept.u}.csv")) %>%
+    pivot_longer(cols = c(
+      "CO_StopAndGoFraction", "CO2_StopAndGoFraction", "NOx_StopAndGoFraction",
+      "CO_AverageSpeed", "CO2_AverageSpeed", "NOx_AverageSpeed",
+      "CO_InterpolationFraction", "CO2_InterpolationFraction", "NOx_InterpolationFraction",
+      "CO_BilinearInterpolationFraction", "CO2_BilinearInterpolationFraction", "NOx_BilinearInterpolationFraction"),
+           names_to = "method", values_to = "value") %>%
+    separate("method", c("component", "method"), "_")
+
+  ggplot() +
+    geom_line(data=curves, aes(x=vel, y=value, color=method)) +
+    geom_point(data=hbefa_det, aes(x=V, y=EFA)) +
+    facet_wrap(~component, scales = "free") +
+    ggtitle(glue("Comparison of emission development for different methods ({trafficSit}, {emConcept})")) +
+    theme_minimal() +
+    theme(text = element_text(size=12))
+}
