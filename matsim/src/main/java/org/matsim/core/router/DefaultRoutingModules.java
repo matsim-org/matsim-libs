bfe2b36a8d2ba71e94492c71adfa2d89e0d4150e
Samuel Hönle <samuel.hoenle@ait.ac.at>
2026-02-16 18:48:33 +0100

    fix(core): Make freespeed teleportation threadsafe

--- matsim/src/main/java/org/matsim/core/router/DefaultRoutingModules.java
+++ matsim/src/main/java/org/matsim/core/router/DefaultRoutingModules.java
@@ -24,91 +24,72 @@ import org.matsim.api.core.v01.population.PopulationFactory;
 import org.matsim.core.config.groups.RoutingConfigGroup;
 import org.matsim.core.config.groups.RoutingConfigGroup.TeleportedModeParams;
 import org.matsim.core.router.util.LeastCostPathCalculator;
+import org.matsim.core.router.util.LeastCostPathCalculatorFactory;
+import org.matsim.core.router.util.TravelDisutility;
+import org.matsim.core.router.util.TravelTime;
 import org.matsim.core.utils.timing.TimeInterpretation;
 
 import jakarta.annotation.Nullable;
 
 /**
  * @author nagel
- *
  */
 public final class DefaultRoutingModules {
-	// despite the fact that this class is in "old", I actually think that it is ok, providing a public interface to functionality that could be changed
-	// underneath.  Have to keep in in the "old" package in order to package-protect the infrastructure that is used underneath. kai, mar'15
+	// despite the fact that this class is in "old", I actually think that it is ok, providing a public interface to functionality that could be
+	// changed underneath. Have to keep in in the "old" package in order to package-protect the infrastructure that is used underneath. kai, mar'15
 
+	private DefaultRoutingModules() {} // do not instantiate
 
-	private DefaultRoutingModules(){} // do not instantiate
-
-	public static RoutingModule createPseudoTransitRouter( String mode, PopulationFactory popFac, Network net, LeastCostPathCalculator routeAlgo,
-			RoutingConfigGroup.TeleportedModeParams params ) {
-		return new FreespeedFactorRoutingModule(
-				mode,
-				popFac,
-				net,
-				routeAlgo,
-				params) ;
+	public static RoutingModule createPseudoTransitRouter(String mode, PopulationFactory popFac, Network net,
+			LeastCostPathCalculatorFactory calculatorFactory, TravelTime travelTime, TravelDisutility travelDisutility,
+			RoutingConfigGroup.TeleportedModeParams params) {
+		return new FreespeedFactorRoutingModule(mode, popFac, net, calculatorFactory, travelTime, travelDisutility, params);
 	}
 
-	public static RoutingModule createTeleportationRouter( String mode, Scenario scenario, TeleportedModeParams params ) {
-		return new TeleportationRoutingModule(
-				mode,
-			  scenario,
-				params.getTeleportedModeSpeed(),
-                params.getBeelineDistanceFactor(),
-				params.getPersonSpeedAttribute() );
+	public static RoutingModule createTeleportationRouter(String mode, Scenario scenario, TeleportedModeParams params) {
+		return new TeleportationRoutingModule(mode, scenario, params.getTeleportedModeSpeed(), params.getBeelineDistanceFactor(),
+				params.getPersonSpeedAttribute());
 	}
 
 	/**
 	 * Creates network router without access/egress.
 	 */
 	@Deprecated // use AccessEgressNetworkRouter instead
-	public static RoutingModule createPureNetworkRouter( String mode, PopulationFactory popFact, Network net, final LeastCostPathCalculator routeAlgo ) {
-		return new NetworkRoutingModule(
-			mode,
-			popFact,
-			net,
-			routeAlgo);
-	}
-
-
-	// TODO: make package private again
-	// Please use injection (NetworkRoutingProvider) to get a NetworkRoutingInclAccessEgressModule - kn/gl nov'19
-	public static RoutingModule createAccessEgressNetworkRouter( String mode,
-											 final LeastCostPathCalculator routeAlgo, Scenario scenario,
-											 Network filteredNetwork, RoutingModule accessEgressToNetworkRouter,
-											 TimeInterpretation timeInterpretation, MultimodalLinkChooser multimodalLinkChooser) {
-
-		return new NetworkRoutingInclAccessEgressModule(
-				mode,
-			  routeAlgo,
-			  scenario, filteredNetwork, null, accessEgressToNetworkRouter, accessEgressToNetworkRouter, timeInterpretation, multimodalLinkChooser);
+	public static RoutingModule createPureNetworkRouter(String mode, PopulationFactory popFact, Network net,
+			final LeastCostPathCalculator routeAlgo) {
+		return new NetworkRoutingModule(mode, popFact, net, routeAlgo);
 	}
 
 	// TODO: make package private again
 	// Please use injection (NetworkRoutingProvider) to get a NetworkRoutingInclAccessEgressModule - kn/gl nov'19
-	public static RoutingModule createAccessEgressNetworkRouter( String mode,
-																 final LeastCostPathCalculator routeAlgo, Scenario scenario,
-																 Network filteredNetwork, RoutingModule accessToNetworkRouter, RoutingModule egressFromNetworkRouter,
-																 TimeInterpretation timeInterpretation, MultimodalLinkChooser multimodalLinkChooser) {
-		return new NetworkRoutingInclAccessEgressModule(
-				mode,
-				routeAlgo,
-				scenario, filteredNetwork,null, accessToNetworkRouter, egressFromNetworkRouter, timeInterpretation, multimodalLinkChooser);
+	public static RoutingModule createAccessEgressNetworkRouter(String mode, final LeastCostPathCalculator routeAlgo, Scenario scenario,
+			Network filteredNetwork, RoutingModule accessEgressToNetworkRouter, TimeInterpretation timeInterpretation,
+			MultimodalLinkChooser multimodalLinkChooser) {
+
+		return new NetworkRoutingInclAccessEgressModule(mode, routeAlgo, scenario, filteredNetwork, null, accessEgressToNetworkRouter,
+				accessEgressToNetworkRouter, timeInterpretation, multimodalLinkChooser);
+	}
+
+	// TODO: make package private again
+	// Please use injection (NetworkRoutingProvider) to get a NetworkRoutingInclAccessEgressModule - kn/gl nov'19
+	public static RoutingModule createAccessEgressNetworkRouter(String mode, final LeastCostPathCalculator routeAlgo, Scenario scenario,
+			Network filteredNetwork, RoutingModule accessToNetworkRouter, RoutingModule egressFromNetworkRouter,
+			TimeInterpretation timeInterpretation, MultimodalLinkChooser multimodalLinkChooser) {
+		return new NetworkRoutingInclAccessEgressModule(mode, routeAlgo, scenario, filteredNetwork, null, accessToNetworkRouter,
+				egressFromNetworkRouter, timeInterpretation, multimodalLinkChooser);
 	}
 
 	/**
 	 * Creates a new access egress network router.
 	 *
-	 * @param invertedNetwork       if not null, routing will be on the inverted network, in which case routeAlgo needs to be an {@link InvertedLeastPathCalculator}
+	 * @param invertedNetwork if not null, routing will be on the inverted network, in which case routeAlgo needs to be an
+	 *        {@link InvertedLeastPathCalculator}
 	 * @param multimodalLinkChooser
 	 */
 	static RoutingModule createAccessEgressNetworkRouter(String mode, final LeastCostPathCalculator routeAlgo, Scenario scenario,
-														 Network filteredNetwork, @Nullable Network invertedNetwork,
-														 RoutingModule accessToNetworkRouter, RoutingModule egressFromNetworkRouter,
-														 TimeInterpretation timeInterpretation, MultimodalLinkChooser multimodalLinkChooser) {
-		return new NetworkRoutingInclAccessEgressModule(
-				mode, routeAlgo, scenario, filteredNetwork, invertedNetwork, accessToNetworkRouter, egressFromNetworkRouter, timeInterpretation,
-				multimodalLinkChooser);
+			Network filteredNetwork, @Nullable Network invertedNetwork, RoutingModule accessToNetworkRouter, RoutingModule egressFromNetworkRouter,
+			TimeInterpretation timeInterpretation, MultimodalLinkChooser multimodalLinkChooser) {
+		return new NetworkRoutingInclAccessEgressModule(mode, routeAlgo, scenario, filteredNetwork, invertedNetwork, accessToNetworkRouter,
+				egressFromNetworkRouter, timeInterpretation, multimodalLinkChooser);
 	}
-
 }
