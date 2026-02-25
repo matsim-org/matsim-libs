916bb957d217fe505433ae659bd760c2c92cbace
Samuel Hönle <samuel.hoenle@ait.ac.at>
2026-02-25 17:38:49 +0100

    Revert "fix(core): Make freespeed teleportation threadsafe"
    This reverts commit bfe2b36a8d2ba71e94492c71adfa2d89e0d4150e.

--- matsim/src/main/java/org/matsim/core/router/FreespeedFactorRoutingModule.java
+++ matsim/src/main/java/org/matsim/core/router/FreespeedFactorRoutingModule.java
@@ -22,13 +22,11 @@ package org.matsim.core.router;
 
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.network.Node;
 import org.matsim.api.core.v01.population.*;
 import org.matsim.core.config.groups.RoutingConfigGroup.TeleportedModeParams;
 import org.matsim.core.router.util.LeastCostPathCalculator;
 import org.matsim.core.router.util.LeastCostPathCalculator.Path;
-import org.matsim.core.router.util.LeastCostPathCalculatorFactory;
-import org.matsim.core.router.util.TravelDisutility;
-import org.matsim.core.router.util.TravelTime;
 import org.matsim.core.utils.geometry.CoordUtils;
 import org.matsim.facilities.Facility;
 
@@ -41,18 +39,17 @@ public final class FreespeedFactorRoutingModule implements RoutingModule {
 	private final PopulationFactory populationFactory;
 
 	private final Network network;
-	private final LeastCostPathCalculatorFactory calculatorFactory;
-	private final TravelTime travelTime;
-	private final TravelDisutility travelDisutility;
+	private final LeastCostPathCalculator routeAlgo;
 	private final TeleportedModeParams params;
 
-	FreespeedFactorRoutingModule(final String mode, final PopulationFactory populationFactory, final Network network,
-			final LeastCostPathCalculatorFactory calculatorFactory, final TravelTime travelTime, final TravelDisutility travelDisutility,
-			TeleportedModeParams params) {
+	FreespeedFactorRoutingModule(
+			final String mode,
+			final PopulationFactory populationFactory,
+            final Network network,
+			final LeastCostPathCalculator routeAlgo,
+			TeleportedModeParams params ) {
 		this.network = network;
-		this.calculatorFactory = calculatorFactory;
-		this.travelTime = travelTime;
-		this.travelDisutility = travelDisutility;
+		this.routeAlgo = routeAlgo;
 		this.params = params;
 		this.mode = mode;
 		this.populationFactory = populationFactory;
@@ -65,45 +62,45 @@ public final class FreespeedFactorRoutingModule implements RoutingModule {
 		final double departureTime = request.getDepartureTime();
 		final Person person = request.getPerson();
 
-		Leg newLeg = this.populationFactory.createLeg(this.mode);
-		newLeg.setDepartureTime(departureTime);
+		Leg newLeg = this.populationFactory.createLeg( this.mode );
+		newLeg.setDepartureTime( departureTime );
 
-		double travTime = routeLeg(person, newLeg, new FacilityWrapperActivity(fromFacility), new FacilityWrapperActivity(toFacility), departureTime);
+		double travTime = routeLeg(
+				person,
+				newLeg,
+				new FacilityWrapperActivity( fromFacility ),
+				new FacilityWrapperActivity( toFacility ),
+				departureTime);
 
 		// otherwise, information may be lost
-		newLeg.setTravelTime(travTime);
+		newLeg.setTravelTime( travTime );
 
-		return Arrays.asList(newLeg);
+		return Arrays.asList( newLeg );
 	}
 
 	@Override
 	public String toString() {
-		return "[LegRouterWrapper: mode=" + this.mode + "]";
+		return "[LegRouterWrapper: mode="+this.mode+"]";
 	}
 
 	/* package (for tests) */ final double routeLeg(Person person, Leg leg, Activity fromAct, Activity toAct, double depTime) {
 		int travTime = 0;
 		final Link fromLink = this.network.getLinks().get(fromAct.getLinkId());
 		final Link toLink = this.network.getLinks().get(toAct.getLinkId());
-		if (fromLink == null)
-			throw new RuntimeException("fromLink missing.");
-		if (toLink == null)
-			throw new RuntimeException("toLink missing.");
+		if (fromLink == null) throw new RuntimeException("fromLink missing.");
+		if (toLink == null) throw new RuntimeException("toLink missing.");
 		if (toLink != fromLink) {
 			// do not drive/walk around, if we stay on the same link
-			// Create algo per-call for thread-safety
-			LeastCostPathCalculator routeAlgo = this.calculatorFactory.createPathCalculator(this.network, this.travelDisutility, this.travelTime);
-			Path path = routeAlgo.calcLeastCostPath(fromLink, toLink, depTime, person, null);
-			if (path == null)
-				throw new RuntimeException("No route found from link " + fromLink.getId() + " to link " + toLink.getId() + ".");
+			Path path = this.routeAlgo.calcLeastCostPath(fromLink, toLink, depTime, person, null);
+			if (path == null) throw new RuntimeException("No route found from link " + fromLink.getId() + " to link " + toLink.getId() + ".");
 
 			// we're still missing the time on the final link, which the agent has to drive on in the java mobsim
 			// so let's calculate the final part.
 			double speed = toLink.getFreespeed(depTime + path.travelTime);
 
 			// correct by speed limit:
-			if (speed > params.getTeleportedModeFreespeedLimit()) {
-				speed = params.getTeleportedModeFreespeedLimit();
+			if ( speed > params.getTeleportedModeFreespeedLimit() ) {
+				speed = params.getTeleportedModeFreespeedLimit() ;
 			}
 
 			// now correct the travel time:
@@ -113,7 +110,7 @@ public final class FreespeedFactorRoutingModule implements RoutingModule {
 			Route route = this.populationFactory.getRouteFactories().createRoute(Route.class, fromLink.getId(), toLink.getId());
 			route.setTravelTime(travTime);
 
-			// yyyyyy the following should actually rather come from the route! There is a RouteUtils.calcDistance( route ) . kai, nov'16
+			// yyyyyy the following should actually rather come from the route!  There is a RouteUtils.calcDistance( route ) .  kai, nov'16
 			double dist = 0;
 			if ((fromAct.getCoord() != null) && (toAct.getCoord() != null)) {
 				dist = CoordUtils.calcEuclideanDistance(fromAct.getCoord(), toAct.getCoord());
@@ -133,8 +130,8 @@ public final class FreespeedFactorRoutingModule implements RoutingModule {
 		leg.setDepartureTime(depTime);
 		leg.setTravelTime(travTime);
 		Leg r = (leg);
-		// yy something needs to be done once there are alternative implementations of the interface. kai, apr'10
-		r.setTravelTime(depTime + travTime - r.getDepartureTime().seconds());
+		r.setTravelTime( depTime + travTime - r.getDepartureTime().seconds()); // yy something needs to be done once there are alternative implementations of the interface.  kai, apr'10
 		return travTime;
 	}
+
 }
