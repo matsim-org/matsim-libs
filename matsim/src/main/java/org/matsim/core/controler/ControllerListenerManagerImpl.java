d55df4c1c533e4f6e3b529f9b06e3be05e88f944
Ricardo Ewert <ewert@vsp.tu-berlin.de>
2026-03-20 16:17:54 +0100

    add null check

--- matsim/src/main/java/org/matsim/core/controler/ControllerListenerManagerImpl.java
+++ matsim/src/main/java/org/matsim/core/controler/ControllerListenerManagerImpl.java
@@ -181,7 +181,8 @@ public final class ControllerListenerManagerImpl implements ControllerListenerMa
 		IterationStartsEvent event = new IterationStartsEvent(this.controller, iteration, isLastIteration);
 		IterationStartsListener[] listener = this.coreListenerList.getListeners(IterationStartsListener.class);
 		Arrays.sort(listener, Comparator.comparingDouble(ControllerListener::priority).reversed());
-		new ConfigWriter(controller.getConfig()).write(controller.getControllerIO().getIterationFilename(iteration, Controler.DefaultFiles.config, ControllerConfigGroup.CompressionType.none));
+		if (controller != null)
+			new ConfigWriter(controller.getConfig()).write(controller.getControllerIO().getIterationFilename(iteration, Controler.DefaultFiles.config, ControllerConfigGroup.CompressionType.none));
         for (IterationStartsListener aListener : listener) {
             log.info("calling notifyIterationStarts on " + aListener.getClass().getName() + " with priority " + aListener.priority());
             aListener.notifyIterationStarts(event);
