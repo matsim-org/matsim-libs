7e423d7a9568c85885f1b999a1b92d7805fe4c9d
vizsim <simon.metzler@dlr.de>
2026-04-15 16:09:52 +0200

    fix getInfrastructureFactor to make tests work

--- contribs/bicycle/src/main/java/org/matsim/contrib/bicycle/BicycleParams.java
+++ contribs/bicycle/src/main/java/org/matsim/contrib/bicycle/BicycleParams.java
@@ -5,9 +5,17 @@ import org.matsim.api.core.v01.network.Link;
 public interface BicycleParams {
 	public double getComfortFactor(String surface);
 
-	//double getInfrastructureFactor(String type, String cyclewaytype); // bicycleInfraType
-	//double getInfrastructureFactor(String type, String cyclewayType, String bicycleInfraType,
-	//							   BicycleInfrastructureMode infrastructureMode);
+
+	// legacy: just here to make the preceived-safety tests work
+	@Deprecated(forRemoval = true)
+	default double getInfrastructureFactor(String type, String infrastructureValue) {
+		return getInfrastructureFactor(
+			type,
+			infrastructureValue,
+			BicycleUtils.CYCLEWAY
+		);
+	}
+
 	double getInfrastructureFactor(String type, String infrastructureValue, String infrastructureAttribute);
 
 	double getGradient_pct(Link link);
