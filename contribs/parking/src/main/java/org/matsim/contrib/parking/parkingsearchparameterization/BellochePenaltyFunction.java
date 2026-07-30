0cf3de5dd5ff39b7b866781449bbc9c5d1a180fa
GregorRyb <rybczak@vsp.tu-berlin.de>
2026-07-30 21:27:41 +0200

    specify comment

--- contribs/parking/src/main/java/org/matsim/contrib/parking/parkingsearchparameterization/BellochePenaltyFunction.java
+++ contribs/parking/src/main/java/org/matsim/contrib/parking/parkingsearchparameterization/BellochePenaltyFunction.java
@@ -32,6 +32,11 @@ public class BellochePenaltyFunction implements ParkingSearchTimeFunction {
 			// destination link does not support parking, for example on a motorway.
 			// In that case, parking search is considered not applicable and no penalty is applied.
 			// To model parking scarcity, provide an explicit positive parking capacity.
+			// A parking-free area must be represented by relocating the parking destination
+			// to a link outside that area. This function does not perform that relocation.
+			// Once relocated, the search-time penalty is calculated from the parking supply
+			// and occupancy around the actual parking link. Therefore, returning zero here is
+			// appropriate when the considered kernel contains no applicable parking capacity.
 			return 0.0;
 		}
 
